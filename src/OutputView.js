25b41af63705edcedeb31c20713b6b2f5d58a25b
Yoo Seung Guk <dbtmd878@naver.com>
2022-11-16 18:42:32 +0900

    feat(printMap): 현재의 다리 상태를 출력하는 기능 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,3 +1,6 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Console = MissionUtils.Console;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -7,7 +10,12 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {},
+  printMap(map) {
+    [upArray, downArray] = map;
+    return Console.print(
+      `[ ${downArray.join(" | ")} ]\n[ ${upArray.join(" | ")} ]`
+    );
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
