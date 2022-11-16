2ca331b5daba9af03782b2243afa4688da372ef8
Mcdonalds Trump <whdmschd2@gmail.com>
2022-11-16 18:32:08 +0900

    feat: input, output 메시지 상수

---
+++ src/utils/constans.js
@@ -0,0 +1,16 @@
+const INPUT_MESSAGE = {
+  BRIDGE_SIZE: "다리의 길이를 입력해주세요.",
+  PLAYER_MOVING: "이동할 칸을 선택해주세요. (위: U, 아래: D)",
+  GAEM_COMMAND:
+    "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
+};
+
+const OUTPUT_MESSAGE = {
+  GAME_START: "다리 건너기 게임을 시작합니다.",
+  GAME_SUCCESS_STATUS: (isGameSuccess) =>
+    `게임 성공 여부: ${isGameSuccess ? "성공" : "실패"}`,
+  GAME_TRY_COUNT: (tryCount) => `총 시도한 횟수: ${tryCount}`,
+  BRIDGE_MAP: "",
+};
+
+module.exports = { INPUT_MESSAGE, OUTPUT_MESSAGE };
