bb7a35ca363cdc35be409aba5d3ffad8249b29ef
Rki0 <pky00823@gmail.com>
2022-11-16 18:19:36 +0900

    test(utils): 다리 길이 입력값 타입 변환 테스트 추가
    추가적인 기능 구현을 위해서는 string 타입의 입력값을 number 타입으로 변환해야한다.
    다리 길이 입력값이 들어왔을 때 number 타입으로 변환이 되는지 테스트한다.

--- __tests__/utilsTest.js
+++ __tests__/utilsTest.js
@@ -6,4 +6,11 @@ describe("제한 조건 테스트", () => {
       new BridgeSize("3 ");
     }).toThrow("[ERROR] 숫자만 입력 가능합니다.");
   });
+
+  test("다리 길이 입력시 number 타입 변환 테스트", () => {
+    const bridgeSize = new BridgeSize("3");
+    const size = bridgeSize.makeStringToNumber();
+
+    expect(size).toBe(3);
+  });
 });
