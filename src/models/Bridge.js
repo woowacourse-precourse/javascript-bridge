ec4c6afc84a2733801436bdad5a2bcafa7f2213c
Leejin-Yang <ejin1996@gmail.com>
2022-11-16 21:16:48 +0900

    refactor: 다리 생성 메서드 이동 Bridge -> BridgeGame

--- src/models/Bridge.js
+++ src/models/Bridge.js
@@ -1,24 +1,8 @@
 class Bridge {
-  constructor(size) {
-    Bridge.#validateSize(size);
-  }
+  #data;
 
-  static #validateSize(size) {
-    if (size === '') {
-      throw new Error('[ERROR] 빈 값을 입력했습니다.');
-    }
-
-    if (size.includes(' ')) {
-      throw new Error('[ERROR] 공백을 포함해 입력했습니다.');
-    }
-
-    if (isNaN(size)) {
-      throw new Error('[ERROR] 다리의 길이 입력 값은 숫자여야 합니다.');
-    }
-
-    if (+size < BRIDGE_RULE.LENGTH_MIN || +size > BRIDGE_RULE.LENGTH_MAX) {
-      throw new Error('[ERROR] 다리의 길이는 3이상 20이하의 숫자여야 합니다.');
-    }
+  constructor(bridge) {
+    this.#data = bridge;
   }
 }
 
--- src/models/BridgeGame.js
+++ src/models/BridgeGame.js
@@ -1,7 +1,20 @@
+const { BRIDGE_RULE } = require('../constants');
+const Bridge = require('./Bridge');
+const BridgeMaker = require('../BridgeMaker');
+const BridgeRandomNumberGenerator = require('../BridgeRandomNumberGenerator');
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  #bridge;
+
+  setBridge(size) {
+    BridgeGame.#validateSize(size);
+    const bridge = BridgeMaker.makeBridge(+size, BridgeRandomNumberGenerator.generate);
+    this.#bridge = new Bridge(bridge);
+  }
+
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
@@ -15,6 +28,24 @@ class BridgeGame {
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   retry() {}
+
+  static #validateSize(size) {
+    if (size === '') {
+      throw new Error('[ERROR] 빈 값을 입력했습니다.');
+    }
+
+    if (size.includes(' ')) {
+      throw new Error('[ERROR] 공백을 포함해 입력했습니다.');
+    }
+
+    if (isNaN(size)) {
+      throw new Error('[ERROR] 다리의 길이 입력 값은 숫자여야 합니다.');
+    }
+
+    if (+size < BRIDGE_RULE.LENGTH_MIN || +size > BRIDGE_RULE.LENGTH_MAX) {
+      throw new Error('[ERROR] 다리의 길이는 3이상 20이하의 숫자여야 합니다.');
+    }
+  }
 }
 
 module.exports = BridgeGame;
