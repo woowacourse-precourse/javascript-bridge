c0c5fd1ac305693405563754105413a04c3da154
moonelysian <dandyblack@naver.com>
2022-11-16 20:01:49 +0900

    feat: constructor 추가
    - MOVEMENT: 위, 아래
    - COMMAND: 재시도, 나가기
    
    - BRIDGE_SIZE: 최소길이, 최대길이

---
+++ src/constructor.js
@@ -0,0 +1,20 @@
+const MOVEMENT = {
+  UP: 'U',
+  DOWN: 'D',
+}
+
+const COMMAND = {
+  RETRY: 'R',
+  QUIT: 'Q',
+}
+
+const BRIDGE_SIZE = {
+  MIN: 3,
+  MAX: 20,
+}
+
+module.exports = {
+  MOVEMENT,
+  COMMAND,
+  BRIDGE_SIZE,
+}
