2697c4c0d19443f4872d6b71719b61769d27c974
Narcoker <jykim9868@gmail.com>
2022-11-16 21:48:47 +0900

    Feat: describe "Bridge 테스트" 추가

--- __tests__/BridgeTest.js
+++ __tests__/BridgeTest.js
@@ -1,16 +1,18 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const BridgeMaker = require("../src/BridgeMaker");
 
-test.each([
-    [["1", "0", "0"], ["U", "D", "D"]],
-    [["0", "1", "0"], ["D", "U", "D"]],
-    [["0", "1", "1"], ["D", "U", "U"]]
-])("다리 생성 테스트", (input, expectedBridge) => {
-    const randomNumbers = input;
-    const mockGenerator = randomNumbers.reduce((acc, number) => {
-        return acc.mockReturnValueOnce(number);
-    }, jest.fn());
+describe("Bridge 테스트", () => {
+    test.each([
+        [["1", "0", "0"], ["U", "D", "D"]],
+        [["0", "1", "0"], ["D", "U", "D"]],
+        [["0", "1", "1"], ["D", "U", "U"]]
+    ])("다리 생성 테스트", (input, expectedBridge) => {
+        const randomNumbers = input;
+        const mockGenerator = randomNumbers.reduce((acc, number) => {
+            return acc.mockReturnValueOnce(number);
+        }, jest.fn());
 
-    const bridge = BridgeMaker.makeBridge(3, mockGenerator);
-    expect(bridge).toEqual(expectedBridge);
+        const bridge = BridgeMaker.makeBridge(3, mockGenerator);
+        expect(bridge).toEqual(expectedBridge);
+    });
 });
