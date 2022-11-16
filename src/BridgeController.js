ca6e42487badab2cdae9710a45c7f5bebfa056fa
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-16 16:48:07 +0900

    feat(Controller): Contoller 생성 및 gameStart 메서드 생성

---
+++ src/BridgeController.js
@@ -0,0 +1,9 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const InputView = require("./InputView");
+const Validation = require("./Validation");
+
+class BridgeController {
+  gameStart() {}
+}
+
+module.exports = BridgeController;
