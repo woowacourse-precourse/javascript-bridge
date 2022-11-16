5df724882839a56ccdfd8ba2d28791b0cf3edee1
AnJaeSeong <dktmzh6@gmail.com>
2022-11-17 04:34:47 +0900

    feat(App): play() to start point

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
+const InputView = require("./InputView");
 const OutputView = require("./OutputView");
 
 class App {
   play() {
-    OutputView.printString("다리 건너기 게임을 시작합니다.");
+    OutputView.printString("다리 건너기 게임을 시작합니다.\n");
+    InputView.readBridgeSize();
   }
 }
-
-const app = new App();
-app.play();
 
 module.exports = App;
