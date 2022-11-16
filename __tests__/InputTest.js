21e398e6a33b4b79158bbfc586e70129ae152ee3
Adult96 <djxor007@gmail.com>
2022-11-16 18:32:00 +0900

    test(InputTest.js): 재시작 종료 유효성 검사 테스트 코드 작성

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -1,5 +1,6 @@
 const bridgeValidation = require('../src/validation/bridgeValidation');
 const movingValidation = require('../src/validation/movingValidation');
+const commandValidation = require('../src/validation/commandValidation');
 
 describe('User Input 테스트', () => {
   test('다리 갯수 3~20까지의 숫자', () => {
@@ -17,5 +18,14 @@ describe('User Input 테스트', () => {
     expect(movingValidation('a')).toBeFalsy();
     expect(movingValidation('')).toBeFalsy();
     expect(movingValidation(' ')).toBeFalsy();
+  });
+
+  test('재시작,종료 R/Q의 문자 확인', () => {
+    expect(commandValidation('R')).toBeTruthy();
+    expect(commandValidation('Q')).toBeTruthy();
+    expect(commandValidation(1)).toBeFalsy();
+    expect(commandValidation('a')).toBeFalsy();
+    expect(commandValidation('')).toBeFalsy();
+    expect(commandValidation(' ')).toBeFalsy();
   });
 });
