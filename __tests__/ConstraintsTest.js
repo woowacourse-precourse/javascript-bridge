c191cb7a2ee9c1b9d796dae997ef81a0fa110c41
Rki0 <pky00823@gmail.com>
2022-11-16 18:06:51 +0900

    test(Constraints): 범위 외 다리 길이 입력값 테스트 추가
    3 이상 20 이하의 범위를 만족하지 않는 입력값이 들어올 경우,
    예외 처리하는 메서드에 대한 테스트 추가.

--- __tests__/ConstraintsTest.js
+++ __tests__/ConstraintsTest.js
@@ -1,11 +1,17 @@
 const { SizeConstraints } = require("../src/Constraints");
 
 describe("제한 조건 테스트", () => {
-  // checkOnlyNumber
-  test("다리 길이 제한 조건 테스트", () => {
+  test("숫자가 아닌 다리 길이 입력값 테스트", () => {
     expect(() => {
       const sizeConstraints = new SizeConstraints("3 ");
       sizeConstraints.checkOnlyNumber();
     }).toThrow("[ERROR] 숫자만 입력 가능합니다.");
+  });
+
+  test("범위 외 다리 길이 입력값 테스트", () => {
+    expect(() => {
+      const sizeConstraints = new SizeConstraints("1");
+      sizeConstraints.checkNumberRange();
+    }).toThrow("[ERROR] 다리 길이는 3 이상 20 이하만 가능합니다.");
   });
 });
