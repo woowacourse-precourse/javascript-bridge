43a5a24965513a237236d203b282f3cee861705b
kimeuns <dmstn2006@gmail.com>
2022-11-16 20:21:06 +0900

    feat: 다리 길이를 입력 받는 기능 추가

--- src/InputView.js
+++ src/InputView.js
@@ -1,11 +1,26 @@
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
+//  제공된 InputView 객체를 활용해 구현해야 한다.
+//  InputView의 파일 경로는 변경할 수 있다.
+//  InputView의 메서드의 인자는 변경할 수 있다.
+//  사용자 값 입력을 위해 필요한 메서드를 추가할 수 있다.
+
+const { Console } = require('@woowacourse/mission-utils');
+const BridgeMaker = require('./BridgeMaker');
+const Validator = require('./Validator')
+
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine('', (input) => {
+      if (Validator.isInRange(input) && Validator.isNumber(input)) {
+        return input;
+      }
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
