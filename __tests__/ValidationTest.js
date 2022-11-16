6e43da1f11df9e3860a34225e2e4a8381c37961f
Jeong seulho <wjdtmfgh@yu.ac.kr>
2022-11-16 23:01:48 +0900

    Test(feature No.1): add test code to check bridge size

---
+++ __tests__/ValidationTest.js
@@ -0,0 +1,12 @@
+const Validation = require("../src/Validation");
+
+describe("input 유효성 테스트", () => {
+  test.each([["2"], ["21"], ["-1"], ["abcd"]])(
+    "다리 길이가 3이상 20이하인 숫자가 아니면 예외 발생",
+    (bridgeSize) => {
+      expect((bridgeSize) => {
+        Validation.validateBridgeSize(bridgeSize);
+      }).toThrow("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+    }
+  );
+});
