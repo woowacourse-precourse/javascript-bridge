b5aac4f212daef70fa79ab5f5c20a4e3bd9ac1b0
banma70 <banma1234@gmail.com>
2022-11-16 17:31:34 +0900

    test: 다리생성 관련 예외처리 테스트코드 작성

--- __tests__/bridgeTest.js
+++ __tests__/bridgeTest.js
@@ -2,8 +2,8 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const App = require("../src/Feature");
 const BridgeMaker = require("../src/BridgeMaker");
 
-describe("다리 건너기 테스트", () => {
-  test("다리 생성 테스트", () => {
+describe("다리 생성 테스트", () => {
+  test("다리 생성", () => {
     const randomNumbers = ["1", "0", "0"];
     const mockGenerator = randomNumbers.reduce((acc, number) => {
       return acc.mockReturnValueOnce(number);
@@ -12,4 +12,15 @@ describe("다리 건너기 테스트", () => {
     const bridge = BridgeMaker.makeBridge(3, mockGenerator);
     expect(bridge).toEqual(["U", "D", "D"]);
   });
+
+  test("예외처리 : size가 숫자가 아닌 경우", () => {
+    const randomNumbers = ["1", "0", "0"];
+    const mockGenerator = randomNumbers.reduce((acc, number) => {
+      return acc.mockReturnValueOnce(number);
+    }, jest.fn());
+
+    expect(() => {
+        BridgeMaker.makeBridge("string", mockGenerator)
+    }).toThrow("[Error]");
+  })
 });
