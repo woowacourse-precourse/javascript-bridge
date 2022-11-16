3021a1fd445114d8f542224f7cf547743df37e07
Rki0 <pky00823@gmail.com>
2022-11-16 21:17:19 +0900

    test(utils): 움직임 제한 사항 검증 테스트 추가
    움직임 입력값에 대한 검증이 제대로 진행되는지 테스트한다.
    잘못된 값이 들어올 경우 예외 처리를 해야한다.

--- __tests__/utilsTest.js
+++ __tests__/utilsTest.js
@@ -1,4 +1,4 @@
-const { BridgeSize } = require("../src/utils");
+const { BridgeSize, MoveInput } = require("../src/utils");
 
 describe("제한 조건 테스트", () => {
   test("다리 길이 입력시 유효성 검사 진행 여부 테스트", () => {
@@ -12,5 +12,11 @@ describe("제한 조건 테스트", () => {
     const size = bridgeSize.makeStringToNumber();
 
     expect(size).toBe(3);
+  });
+
+  test("움직임 입력값 유효성 검사 진행 여부 테스트", () => {
+    expect(() => {
+      new MoveInput("A");
+    }).toThrow("[ERROR] U 혹은 D만 입력 가능합니다.");
   });
 });
