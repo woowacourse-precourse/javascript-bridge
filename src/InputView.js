5b1cfde80196bbf7987b4ae84944ec3f74ac3fc8
AnJaeSeong <dktmzh6@gmail.com>
2022-11-17 01:25:12 +0900

    feat(InputView): readMoving()
    InputView의 한 사이클이 게임의 한 라운드를 의미
    해당 라운드의 결과에 따라 분기 발생
    - 해당 라운드 실패시: readGameCommand()
    - 해당 라운드 성공시: 도착여부에 따라 라운드 진행 or 게임 종료

--- src/InputView.js
+++ src/InputView.js
@@ -1,6 +1,8 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const BridgeGame = require("./BridgeGame");
 const BridgeMaker = require("./BridgeMaker");
 const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+const OutputView = require("./OutputView");
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -10,15 +12,26 @@ const InputView = {
    */
   readBridgeSize() {
     MissionUtils.Console.readLine("다리의 길이를 입력해주세요.\n", (input) => {
-      const bridge = BridgeMaker.makeBridge(input, BridgeRandomNumberGenerator.generate);
-      this.readMoving(bridge);
+      const bridgeGame = new BridgeGame(BridgeMaker.makeBridge(input, BridgeRandomNumberGenerator.generate));
+      this.readMoving(bridgeGame);
     });
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving(bridge) {},
+  readMoving(bridgeGame) {
+    MissionUtils.Console.readLine("이동할 칸을 선택해주세요. (위: U, 아래: D)\n", (input) => {
+      if(!bridgeGame.move(input)) {
+        this.readGameCommand();
+      } else if (bridgeGame.isArrived()){
+        OutputView.printResult(bridgeGame.result());
+        return null;
+      } 
+      OutputView.printMap(bridgeGame.state());
+      this.readMoving(bridgeGame);
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
