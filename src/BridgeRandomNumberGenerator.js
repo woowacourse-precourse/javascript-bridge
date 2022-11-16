5d6b3cf4d2dbe0d384764342c66f5d8e039d30a7
Yoo Seung Guk <dbtmd878@naver.com>
2022-11-16 18:40:23 +0900

    style: 오타 수정

--- src/BridgeRandomNumberGenerator.js
+++ src/BridgeRandomNumberGenerator.js
@@ -1,5 +1,7 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-
+// generate() 를 사용
+// 코드 변경 불가
+//  다리 칸을 생성하기 위해 Radom값은 const number = generateRandomNumber()을 사용
 const BridgeRandomNumberGenerator = {
   RANDOM_LOWER_INCLUSIVE: 0,
   RANDOM_UPPER_INCLUSIVE: 1,
