c78ddbd0a0b357e4720d5b10b868362629b2fc72
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-16 22:25:31 +0900

    feat(Model): 유저가 이동 할 때 정답의 유무를 판단하는 기능 추가

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,13 +1,38 @@
+const { Console } = require("@woowacourse/mission-utils");
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  #count;
+  constructor(count) {
+    this.#count = count;
+  }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  //여기서 반복문
+  wordConverse(word) {
+    if (word === "U") {
+      return 1;
+    }
+    if (word === "D") {
+      return 0;
+    }
+  }
+
+  countUp() {
+    this.#count = this.#count + 1;
+  }
+
+  move(safeBridge, userMove) {
+    if (safeBridge[this.#count] === this.wordConverse(userMove)) {
+      this.countUp();
+      return true;
+    }
+    return false;
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
