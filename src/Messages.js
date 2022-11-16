b3561056b83c508133db08145d63973092eed540
Narcoker <jykim9868@gmail.com>
2022-11-16 20:28:49 +0900

    Feat: ENTER_MOVE_TYPE 메세지 문구 추가

--- src/Messages.js
+++ src/Messages.js
@@ -1,6 +1,7 @@
 const MESSAGE = Object.freeze({
-    START: "다리 건너기 게임을 시작합니다.",
+    START: "다리 건너기 게임을 시작합니다.\n",
     ENTER_BRIDGE_SIZE: "다리의 길이를 입력해주세요.\n",
+    ENTER_MOVE_TYPE: "이동할 칸을 선택해주세요. (위: U, 아래: D)\n"
 });
 
 const ERROR = Object.freeze({
