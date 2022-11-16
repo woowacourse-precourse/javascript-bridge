13814ab9e3ffb8792c6227bcc4039d0701c2e091
jeonjeunghoon <jeonjeunghoon@gmail.com>
2022-11-17 01:32:41 +0900

    feat: 다리 길이 입력 받는 기능 추가

--- src/controller/GameController.js
+++ src/controller/GameController.js
@@ -1,11 +1,14 @@
 const OutputView = require('../views/OutputView');
 const InputView = require('../views/InputView');
+const Validator = require('./Validator');
 
 class GameController {
   playGame() {
     OutputView.printStartMessage();
-    InputView.readBridgeSize();
+    InputView.readBridgeSize(this.#makeBridge);
   }
+
+  #makeBridge(rowDataOfBridgeSize) {}
 }
 
 module.exports = GameController;
