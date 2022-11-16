d60585aac5f58fea91fc1b8197ba7b64a04bae83
gilpop8663 <wolfye@naver.com>
2022-11-17 07:40:55 +0900

    feat: 다리 길이 잘못된 입력 시 예외 처리 기능 구현

--- src/InputView.js
+++ src/InputView.js
@@ -1,5 +1,9 @@
 const { Console } = require('@woowacourse/mission-utils');
-const { GAME_MESSAGE } = require('./constants');
+const {
+  GAME_MESSAGE,
+  BRIDGE_RANGE,
+  ERROR_BRIDGE_MESSAGE,
+} = require('./constants');
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,9 +12,32 @@ const InputView = {
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize() {
-    Console.readLine(GAME_MESSAGE.inputLength, (bridgeLength) =>
-      console.log(bridgeLength)
-    );
+    Console.readLine(GAME_MESSAGE.inputLength, (userInput) => {
+      const bridgeSize = Number(userInput);
+      this.bridgeValidation(bridgeSize);
+    });
+  },
+
+  checkBridgeInteger(bridgeSize) {
+    if (!Number.isInteger(bridgeSize)) {
+      throw new Error(ERROR_BRIDGE_MESSAGE.integer);
+    }
+  },
+  checkBridgeNumber(bridgeSize) {
+    if (Number.isNaN(bridgeSize)) {
+      throw new Error(ERROR_BRIDGE_MESSAGE.number);
+    }
+  },
+  checkBridgeRange(bridgeSize) {
+    if (bridgeSize < BRIDGE_RANGE.start || bridgeSize > BRIDGE_RANGE.end) {
+      throw new Error(ERROR_BRIDGE_MESSAGE.range);
+    }
+  },
+
+  bridgeValidation(bridgeSize) {
+    this.checkBridgeNumber(bridgeSize);
+    this.checkBridgeInteger(bridgeSize);
+    this.checkBridgeRange(bridgeSize);
   },
 
   /**
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 const ERROR_SUBJECT = '[ERROR]';
 
-const RANGE = Object.freeze({
+const BRIDGE_RANGE = Object.freeze({
   start: 3,
   end: 20,
 });
@@ -12,23 +12,29 @@ const SHORT_CUT = Object.freeze({
   quit: 'Q',
 });
 
+const ERROR_RULE_MESSAGE = Object.freeze({
+  bridge: `다리 길이는 ${BRIDGE_RANGE.start}부터 ${BRIDGE_RANGE.end} 사이의 숫자여야 합니다.`,
+  playing: `이동할 칸의 입력은 ${SHORT_CUT.up} 혹은 ${SHORT_CUT.down}여야 합니다.`,
+  retry: `게임 종료 여부의 입력은 ${SHORT_CUT.retry} 혹은 ${SHORT_CUT.quit}여야 합니다.`,
+});
+
 const ERROR_BRIDGE_MESSAGE = Object.freeze({
-  message: `다리 길이는 ${RANGE.start}부터 ${RANGE.end} 사이의 숫자여야 합니다.`,
-  number: `${ERROR_SUBJECT} 숫자가 아닌 다른 문자가 포함되어 있습니다. ${this.message}`,
-  integer: `${ERROR_SUBJECT} 자연수가 아닌 소수를 입력하였습니다. ${this.message}`,
-  range: `${ERROR_SUBJECT} 숫자가 범위를 벗어났습니다. ${this.message}`,
+  message: `다리 길이는 ${BRIDGE_RANGE.start}부터 ${BRIDGE_RANGE.end} 사이의 숫자여야 합니다.`,
+  number: `${ERROR_SUBJECT} 숫자가 아닌 다른 문자가 포함되어 있습니다. ${ERROR_RULE_MESSAGE.bridge}`,
+  integer: `${ERROR_SUBJECT} 자연수가 아닌 소수를 입력하였습니다. ${ERROR_RULE_MESSAGE.bridge}`,
+  range: `${ERROR_SUBJECT} 숫자가 범위를 벗어났습니다. ${ERROR_RULE_MESSAGE.bridge}`,
 });
 
 const ERROR_PLAYING_MESSAGE = Object.freeze({
   message: `이동할 칸의 입력은 ${SHORT_CUT.up} 혹은 ${SHORT_CUT.down}여야 합니다.`,
-  lowercase: `${ERROR_SUBJECT} 대문자가 아닌 소문자를 입력하였습니다. ${this.message}`,
-  wrong: `${ERROR_SUBJECT} ${SHORT_CUT.up} 또는 ${SHORT_CUT.down}이 아닌 다른 문자를 입력하였습니다. ${this.message}`,
+  lowercase: `${ERROR_SUBJECT} 대문자가 아닌 소문자를 입력하였습니다. ${ERROR_RULE_MESSAGE.playing}`,
+  wrong: `${ERROR_SUBJECT} ${SHORT_CUT.up} 또는 ${SHORT_CUT.down}이 아닌 다른 문자를 입력하였습니다. ${ERROR_RULE_MESSAGE.playing}`,
 });
 
 const ERROR_RETRY_MESSAGE = Object.freeze({
   message: `게임 종료 여부의 입력은 ${SHORT_CUT.retry} 혹은 ${SHORT_CUT.quit}여야 합니다.`,
-  lowercase: `${ERROR_SUBJECT} 대문자가 아닌 소문자를 입력하였습니다. ${this.message}`,
-  wrong: `${ERROR_SUBJECT} ${SHORT_CUT.retry} 또는 ${SHORT_CUT.quit}이 아닌 다른 문자를 입력하였습니다. ${this.message}`,
+  lowercase: `${ERROR_SUBJECT} 대문자가 아닌 소문자를 입력하였습니다. ${ERROR_RULE_MESSAGE.retry}`,
+  wrong: `${ERROR_SUBJECT} ${SHORT_CUT.retry} 또는 ${SHORT_CUT.quit}이 아닌 다른 문자를 입력하였습니다. ${ERROR_RULE_MESSAGE.retry}`,
 });
 
 const GAME_MESSAGE = Object.freeze({
@@ -50,6 +56,7 @@ module.exports = {
   ERROR_BRIDGE_MESSAGE,
   ERROR_PLAYING_MESSAGE,
   ERROR_RETRY_MESSAGE,
+  BRIDGE_RANGE,
   SHORT_CUT,
   GAME_MESSAGE,
   GAME_RESULT,
