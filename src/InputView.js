4e603689d62dd108eaac6d17796c1d1276036e10
Jeong seulho <wjdtmfgh@yu.ac.kr>
2022-11-17 00:16:57 +0900

    Style: modify prettier setting

--- src/InputView.js
+++ src/InputView.js
@@ -2,21 +2,21 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const Validation = require("./Validation");
 
 const InputView = {
-  START_MSG: "다리 건너기 게임을 시작합니다.",
-  READ_BRIDGE_SIZE_MSG: "다리의 길이를 입력해주세요.",
+	START_MSG: "다리 건너기 게임을 시작합니다.",
+	READ_BRIDGE_SIZE_MSG: "다리의 길이를 입력해주세요.",
 
-  readBridgeSize() {
-    MissionUtils.Console.readLine(
-      InputView.START_MSG + "\n" + InputView.READ_BRIDGE_SIZE_MSG,
-      (bridgeSize) => {
-        Validation.validateBridgeSize(bridgeSize);
-      }
-    );
-  },
+	readBridgeSize() {
+		MissionUtils.Console.readLine(
+			InputView.START_MSG + "\n" + InputView.READ_BRIDGE_SIZE_MSG,
+			(bridgeSize) => {
+				Validation.validateBridgeSize(bridgeSize);
+			},
+		);
+	},
 
-  readMoving() {},
+	readMoving() {},
 
-  readGameCommand() {},
+	readGameCommand() {},
 };
 
 module.exports = InputView;
