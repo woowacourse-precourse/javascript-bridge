428bd72365c4ad96050421b99731e7023d06fc61
moonelysian <dandyblack@naver.com>
2022-11-16 19:59:41 +0900

    feat: InputView 내부 함수들 기능 작성
    - readBridgeSize: 다리 길이를 입력받고 길이 return
    - readMoving: 어디로 이동할지 입력받고 검증 후 이동 경로 return
    - readGameCommand: 종료할지 계속할지 입력받고 결과 return

--- src/InputView.js
+++ src/InputView.js
@@ -1,21 +1,41 @@
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
+const { MOVEMENT, COMMAND } = require('./constructor.js');
+const { input, throwError } = require('./utill.js');
+
 const InputView = {
-  /**
-   * 다리의 길이를 입력받는다.
-   */
-  readBridgeSize() {},
+  async readBridgeSize() {
+    const MIN_BRIDGE_SIZE = 3;
+    const MAX_BRIDGE_SIZE = 20;
 
-  /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
-  readMoving() {},
+    const answer = await input('다리의 길이를 입력해주세요\n');
+    if (isNaN(answer)) {
+      return throwError(`숫자를 입력해주세요.`);
+    } 
+    
+    const bridgeSize = parseInt(answer);
+    if (bridgeSize < MIN_BRIDGE_SIZE || bridgeSize > MAX_BRIDGE_SIZE) {
+      return throwError(`${MIN_BRIDGE_SIZE} ~ ${MAX_BRIDGE_SIZE}까지 수를 입력해주세요.`);
+    }
+    return bridgeSize;
+  },
 
-  /**
-   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-   */
-  readGameCommand() {},
+  async readMoving() {
+    const answer = await input('이동할 칸을 선택해주세요. (위: U, 아래: D)\n');
+    if (answer !== MOVEMENT.UP || answer !== MOVEMENT.DOWN) {
+      return throwError('잘못된 입력입니다.');
+    }
+    return answer;
+  },
+
+  async readGameCommand() {
+    const answer = await input('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n');
+    if (answer !== COMMAND.RETRY || answer !== COMMAND.QUIT) {
+      return throwError('잘못된 입력입니다.')
+    }
+    return answer;
+  },
 };
 
 module.exports = InputView;
