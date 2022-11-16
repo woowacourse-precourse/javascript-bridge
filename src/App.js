965e28f7f9525e5a2643b4d909ae4c109aedad26
AnJaeSeong <dktmzh6@gmail.com>
2022-11-16 21:54:05 +0900

    feat(OutputView): printString()

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+const OutputView = require("./OutputView");
+
 class App {
-  play() {}
+  play() {
+    OutputView.printString("다리 건너기 게임을 시작합니다.");
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
