0c4aa89ef5f67983b12137353bde9ec45f9e65c4
kimeuns <dmstn2006@gmail.com>
2022-11-16 22:39:39 +0900

    feat: 길이 데이터를 입력받아 다리 생성까지 진행
    - controller 역할
    - InputView에서 데이터를 입력받아 BridgeMaker로 전달하는 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+/* eslint-disable class-methods-use-this */
+const BridgeMaker = require("./BridgeMaker");
+const { Console } = require('@woowacourse/mission-utils');
+const { ERROR, GAME, INPUT, BRIDGE, BRIDGE_MOVEMENT } = require("./constants");
+const InputView = require('./InputView')
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator')
+const Validator = require('./Validator');
+
 class App {
-  play() {}
+  bridge;
+
+  play() {
+    Console.print(GAME.START);
+    this.setBridgeSize();
+  }
+
+  setBridgeSize() {
+    InputView.readBridgeSize(this.makeBridge.bind(this));
+  }
+
+  makeBridge(size) {
+    Validator.isValidBridgeSize(size);
+    const generateRandomNumber = () => BridgeRandomNumberGenerator.generate();
+    this.bridge = BridgeMaker.makeBridge(size, generateRandomNumber);
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
