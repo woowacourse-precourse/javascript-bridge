5f0554218a2f11ac9900d4c29bd3981daf842b32
kangyeongmin <kym6050@hanyang.ac.kr>
2022-11-17 01:08:58 +0900

    📌feat: 올바른 다리의 길이인지 판별하는 기능

--- src/InputView.js
+++ src/InputView.js
@@ -9,6 +9,23 @@ const InputView = {
       this.handleBrideSizeException(input);
     });
   },
+
+  handleBrideSizeException(size) {
+    try {
+      if (!validateBridgeSize(size)) throw new Error();
+      const bridgeGame = new BridgeGame(size);
+      this.readMoving(bridgeGame);
+    } catch (e) {
+      MissionUtils.Console.print(
+        "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다."
+      );
+      this.readBridgeSize();
+    }
+  },
 };
+
+function validateBridgeSize(input) {
+  return input.toString().match(REGEX.SIZE_RANGE);
+}
 
 module.exports = InputView;
