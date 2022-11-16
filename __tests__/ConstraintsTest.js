ae4884e42ffb5b2973dcf49879da953e1af52d2d
Rki0 <pky00823@gmail.com>
2022-11-16 20:57:45 +0900

    test(Constraints): 움직임 입력값 테스트 추가
    플레이어는 움직이고자 할 때, U or D만을 입력할 수 있다.
    그 외의 값은 예외 처리된다.

--- __tests__/ConstraintsTest.js
+++ __tests__/ConstraintsTest.js
@@ -21,4 +21,11 @@ describe("제한 조건 테스트", () => {
       sizeConstraints.checkStartZero();
     }).toThrow("[ERROR] 0으로 시작하는 숫자는 입력할 수 없습니다.");
   });
+
+  test("움직임 입력에 U나 D가 아닌 입력값 테스트", () => {
+    expect(() => {
+      const moveConstraints = new MoveConstraints("A");
+      moveConstraints.checkInputUorD();
+    }).toThrow("[ERROR] U 혹은 D만 입력 가능합니다.");
+  });
 });
