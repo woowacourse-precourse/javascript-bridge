2bbf12eb97dbe2fc8dd2b5eb22e4ba0bc3e4c5ee
shackstack <kms1061@naver.com>
2022-11-17 08:16:44 +0900

    feat: 다리길이 및 이동할 칸 입력받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+const InputView = require("./InputView");
+
 class App {
-  play() {}
+  #length;
+  #moving;
+
+  play() {
+    InputView.readBridgeSize((size) => this.getlength(size));
+  }
+
+  getlength(size) {
+    this.#length = size;
+
+    InputView.readMoving((moving) => this.getMoving(moving));
+  }
+
+  getMoving(moving) {
+    this.#moving = moving;
+  }
 }
 
 module.exports = App;
+
+const app = new App();
+app.play();
--- src/InputView.js
+++ src/InputView.js
@@ -1,16 +1,18 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
-  /**
-   * 다리의 길이를 입력받는다.
-   */
-  readBridgeSize() {},
+  readBridgeSize(getlength) {
+    MissionUtils.Console.readLine("다리의 길이를 입력해주세요.", getlength);
+  },
 
-  /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
-  readMoving() {},
+  readMoving(getMoving) {
+    MissionUtils.Console.readLine(
+      "이동할 칸을 선택해주세요. (위: U, 아래: D)",
+      getMoving
+    );
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
