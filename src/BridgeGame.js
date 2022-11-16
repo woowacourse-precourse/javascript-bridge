ed7e9f9ff11810b969c8436edf0afe2f289b6464
Rki0 <pky00823@gmail.com>
2022-11-16 23:12:40 +0900

    feat(BridgeGame): 플레이어가 가는 칸이 맞는 칸인지 판별하는 기능 추가
    랜덤으로 정해진, 갈 수 있는 칸을 플레이어의 움직임 입력값과 비교하여
    갈 수 있는 길이면 true, 아니라면 false를 반환하도록 하는 메서드 추가.

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,13 +1,32 @@
+const Player = require("./Player");
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  #canWalkBridge;
+  #wantGo;
+
+  constructor(canWalkBridge, wantGo) {
+    this.#canWalkBridge = canWalkBridge;
+    this.#wantGo = wantGo;
+  }
+
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move() {
+    Player.movingArr.push(this.#wantGo);
+    const currentIndex = Player.movingArr.length - 1;
+
+    if (Player.movingArr[currentIndex] === this.#canWalkBridge[currentIndex]) {
+      return true;
+    }
+
+    return false;
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
