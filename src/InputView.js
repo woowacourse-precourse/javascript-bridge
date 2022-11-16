e39f95b4dfffefd2943d00743dceb23cfbc111b5
Adult96 <djxor007@gmail.com>
2022-11-16 20:48:29 +0900

    feat(InputView): 다리 길이 입력 기능 구현
    InputView 클래스 안에 다리 길이 입력 기능 구현

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,5 @@
+const { readLine, print } = require('./utils/MissionUtils');
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,7 +7,12 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize(callback) {
+    print('\n다리의 길이를 입력해주세요.');
+    readLine('', (bridgeSize) => {
+      callback(bridgeSize);
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
