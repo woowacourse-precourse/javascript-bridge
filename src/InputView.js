4a88a2853f4461ab76f98ad77930b07ac31c4f13
Adult96 <djxor007@gmail.com>
2022-11-16 20:49:06 +0900

    feat(InputView): 종료/재시작 입력 기능 구현
    InputView 클래스 안에 종료/재시작 입력 기능 구현

--- src/InputView.js
+++ src/InputView.js
@@ -27,7 +27,12 @@ const InputView = {
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand(callback) {
+    print('\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)');
+    readLine('', (commandInput) => {
+      callback(commandInput);
+    });
+  },
 };
 
 module.exports = InputView;
