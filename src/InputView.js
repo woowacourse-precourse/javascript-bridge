9407ef3d510c52622df9c2cfd05e56314ee3e132
imb96 <kimminje7267@naver.com>
2022-11-16 17:16:25 +0900

    feat : 다리의 길이를 입력받는 기능 구현

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,5 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const { MESSAGE, ERROR, BRIDGE } = require("./constant");
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,7 +7,22 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    MissionUtils.Console.readLine(MESSAGE.INPUT_BRIDGE_LENGTH, (answer) => {
+      if (this.bridgeSizeValidate(answer)) {
+        return answer;
+      }
+    });
+  },
+
+  bridgeSizeValidate(size) {
+    if (+size < BRIDGE.MIN_LENGTH || BRIDGE.MAX_LENGTH < +size) {
+      throw new Error(ERROR.BRIDGE_LENGTH_RANGE);
+    }
+    if (isNaN(+size)) {
+      throw new Error(ERROR.BRIDGE_LENGTH_ISNAN);
+    }
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
--- src/constant.js
+++ src/constant.js
@@ -1,7 +1,19 @@
 const MESSAGE = {
   GAME_START: "다리 건너기 게임을 시작합니다.",
+  INPUT_BRIDGE_LENGTH: "다리의 길이를 입력해주세요.",
+};
+
+const BRIDGE = {
+  MIN_LENGTH: 3,
+  MAX_LENGTH: 20,
+};
+const ERROR = {
+  BRIDGE_LENGTH_RANGE: "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.",
+  BRIDGE_LENGTH_ISNAN: "[ERROR] 다리 길이는 숫자여야 합니다.",
 };
 
 module.exports = {
   MESSAGE,
+  BRIDGE,
+  ERROR,
 };
