97781044da60bfdaee0b3448982f08bbe58f2e67
hyunjin <leehj0110@kakao.com>
2022-11-17 00:35:28 +0900

    feat: 사용자로부터 다리 길이를 입력받을 수 있다.

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,5 @@
+const { Console } = require("@woowacourse/mission-utils")
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,7 +7,11 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine("다리 길이를 입력해주세요.", (userInput) => {
+      console.log(userInput)
+    })
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
@@ -16,6 +22,6 @@ const InputView = {
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand() {},
-};
+}
 
-module.exports = InputView;
+module.exports = InputView
