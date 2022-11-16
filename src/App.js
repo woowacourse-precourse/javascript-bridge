7bd9fddc8188d2444051eeb6119039a47400ce66
MyungAe <ladiescheong@naver.com>
2022-11-16 21:19:51 +0900

    feat(InputView, App): 다리의 길이를 입력받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+const InputView = require("./InputView");
 const OutputView = require("./OutputView");
 
 class App {
   play() {
     OutputView.printGetStarted();
+    InputView.readBridgeSize();
   }
 }
 
--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,8 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const BridgeMaker = require("./BridgeMaker");
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+const Validate = require("./Validate");
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,7 +10,16 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    MissionUtils.Console.readLine("다리의 길이를 입력해주세요.\n", (input) => {
+      this.ValidateBridgeSize(input);
+      const bridgeNumbers = BridgeMaker.makeBridge(
+        parseInt(input),
+        BridgeRandomNumberGenerator.generate
+      );
+      this.readMoving(bridgeNumbers);
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
@@ -16,6 +30,16 @@ const InputView = {
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand() {},
+
+  /**
+   * 사용자가 입력한 다리의 길이가 정상적으로 입력되었는지 검증한다.
+   * @param {string} input
+   */
+  ValidateBridgeSize(input) {
+    Validate.isNumber(input);
+    Validate.isBigger(input);
+    Validate.isLower(input);
+  },
 };
 
 module.exports = InputView;
