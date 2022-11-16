7672810ef071d52d59d34340879a8014b83041ad
unsnruu <unsnruu@gmail.com>
2022-11-16 16:55:55 +0900

    feat: readBridgeSize의 인자로 함수를 전달하여 구현

--- src/InputView.js
+++ src/InputView.js
@@ -1,11 +1,18 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const { Console } = MissionUtils;
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
+   *
+   * @param {function} next
    */
-  readBridgeSize() {},
+  readBridgeSize(next) {
+    const message = "다리의 길이를 입력해주세요.";
+    Console.readLine(message, (bridgeLength) => next(bridgeLength));
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
