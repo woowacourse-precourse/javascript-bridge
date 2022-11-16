e2cdc4b2c07b8e6d4796a2ec594201118df7c48e
Suyoung <jsy8492@gmail.com>
2022-11-16 18:57:34 +0900

    refactor(BridgeValidator): jsdoc 양식에 맞춰 각 메서드에 대한 상세 설명 작성

--- src/BridgeValidator.js
+++ src/BridgeValidator.js
@@ -4,8 +4,12 @@
 const BridgeValidator = {
   MIN_BRIDGE_SIZE: 3,
   MAX_BRIDGE_SIZE: 20,
+
   /**
+   * 사용자가 입력한 다리 길이의 값에 대한 유효성을 검사하는 메서드
    * @param {number} bridgeSize
+   * @throws 숫자가 아닌 문자가 포함된 경우 error를 throw한다.
+   * @throws 다리 길이 범위 이외의 값인 경우 error를 throw한다.
    */
   validateBridgeSize(bridgeSizeInput) {
     if (this.isNotNumber(bridgeSizeInput)) {
@@ -20,6 +24,7 @@ const BridgeValidator = {
   },
 
   /**
+   * 사용자가 입력한 값에 숫자가 아닌 문자가 포함되는지 판단하는 메서드
    * @param {string} inputValue 사용자로부터 입력받은 값
    * @returns {boolean} 숫자가 아닌 문자가 포함된 경우 true, 아니면 false를 반환한다.
    */
@@ -28,6 +33,7 @@ const BridgeValidator = {
   },
 
   /**
+   * 사용자가 입력한 값이 다리 길이 범위 이외의 값인지 판단하는 메서드
    * @param {number} bridgeSize 사용자로부터 입력받은 다리의 길이
    * @returns {boolean} 다리 길이 범위 이외의 값이면 true, 아니면 false를 반환한다.
    */
@@ -38,8 +44,9 @@ const BridgeValidator = {
   },
 
   /**
-   *
+   * 사용자가 입력한 이동할 칸에 대한 유효성을 검사하는 메서드
    * @param {string} moving
+   * @throws 유효하지 않은 이동인 경우 error를 throw한다.
    */
   validateMoving(moving) {
     if (this.isInvalidMoving(moving)) {
@@ -50,7 +57,7 @@ const BridgeValidator = {
   },
 
   /**
-   *
+   * 사용자가 입력한 이동이 유효하지 않은 이동인지 판단하는 메서드
    * @param {string} moving
    * @returns {boolean} 유효하지 않은 이동인 경우 true, 아니면 false를 반환한다.
    */
