bd5fef6e5a4191e9287e5d5fd2c4632a4b39fc75
MyungAe <ladiescheong@naver.com>
2022-11-16 20:45:56 +0900

    feat(Validate): 검증 객체 구현
    구현된 검증 메소드
    - isNumber : 입력값이 숫자인지 판별
    - isBigger : 입력값이 20이 넘는지 판별
    - isLower : 입력값이 3보다 작은지 판별
    - isCorrectMove : 입력값이 U, D가 아닌지 판별
    - isCorrectRetry : 입력값이 R, Q가 아닌지 판별

---
+++ src/Validate.js
@@ -0,0 +1,26 @@
+const Validate = {
+  isNumber(input) {
+    const redex = /[^0-9]/g;
+    if (input.match(redex))
+      throw new Error("[ERROR] 다리 길이가 숫자가 아닙니다.", input);
+  },
+  isBigger(input) {
+    const number = parseInt(input);
+    if (number > 20) throw new Error("[ERROR] 다리 길이가 너무 큽니다.", input);
+  },
+  isLower(input) {
+    const number = parseInt(input);
+    if (number < 3)
+      throw new Error("[ERROR] 다리 길이가 너무 작습니다.", input);
+  },
+  isCorrectMove(input) {
+    if (input !== "U" && input !== "D")
+      throw new Error("[ERROR] 이동할 칸이 올바르지 않습니다.", input);
+  },
+  isCorrectRetry(input) {
+    if (input !== "R" && input !== "Q")
+      throw new Error("[ERROR] 이동할 칸이 올바르지 않습니다.", input);
+  },
+};
+
+module.exports = Validate;
