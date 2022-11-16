ba9a5eabaaf1fdcdee25c590579af2d0664a99fa
AnJaeSeong <dktmzh6@gmail.com>
2022-11-17 04:31:50 +0900

    feat(BridgeMaker): makeBridge()

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -8,7 +8,7 @@ const BridgeMaker = {
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
   makeBridge(size, generateRandomNumber) {
-    const bridge = '';
+    let bridge = '';
     for(let i=0; i<size; i++){
       const isUpsidePossible = generateRandomNumber();
       bridge += isUpsidePossible == 0 ? 'D' : 'U';
