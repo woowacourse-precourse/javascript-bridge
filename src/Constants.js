95d6977c0d2bbd0174b14b074a63c40e810dc053
BBIYAC <kiy7833@gmail.com>
2022-11-17 05:02:12 +0900

    refactor(validateBridgeSize): constantize bridge size range

--- src/Constants.js
+++ src/Constants.js
@@ -1,6 +1,7 @@
 const MESSAGE_PROCESS = Object.freeze({
     GAME_START: "다리 건너기 게임을 시작합니다.\n",
-    INPUT_BRIDGE_LENGTH: "다리의 길이를 입력해주세요.\n"
+    INPUT_BRIDGE_LENGTH: "다리의 길이를 입력해주세요.\n",
+    INPUT_MOVING: "\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n"
 });
 
 const MESSAGE_ERROR = Object.freeze({
@@ -8,7 +9,13 @@ const MESSAGE_ERROR = Object.freeze({
     BRIDGE_OUT_OF_RANGE: "[ERROR] 다리의 길이는 3 이상 20 이하의 숫자여야 합니다."
 });
 
+const BRIDGE_SIZE_RANGE = Object.freeze({
+    MIN: 3,
+    MAX: 20
+});
+
 module.exports = {
     MESSAGE_PROCESS,
-    MESSAGE_ERROR
+    MESSAGE_ERROR,
+    BRIDGE_SIZE_RANGE
 };
--- src/InputView.js
+++ src/InputView.js
@@ -1,5 +1,5 @@
 const { Console } = require("@woowacourse/mission-utils");
-const { MESSAGE_PROCESS, MESSAGE_ERROR } = require('./Constants');
+const { MESSAGE_PROCESS, MESSAGE_ERROR, BRIDGE_SIZE_RANGE } = require('./Constants');
 const OutputView = require('./OutputView');
 const BridgeMaker = require('./BridgeMaker');
 const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
@@ -14,12 +14,15 @@ const InputView = {
   setBridgeSize(bridgeSize) {
     this.validateBridgeSize(bridgeSize);
     bridgeGame.setBridge(BridgeMaker.makeBridge(bridgeSize, BridgeRandomNumberGenerator));
+    this.readMoving();
   },
 
   validateBridgeSize(bridgeSize) {
     try {
       if (/[^0-9]/.test(bridgeSize)) throw MESSAGE_ERROR.BRIDGE_ONLY_NUMBER;
-      if (Number(bridgeSize) < 3 || Number(bridgeSize) > 20) throw MESSAGE_ERROR.BRIDGE_OUT_OF_RANGE;
+      if (Number(bridgeSize) < BRIDGE_SIZE_RANGE.MIN || Number(bridgeSize) > BRIDGE_SIZE_RANGE.MAX) {
+        throw MESSAGE_ERROR.BRIDGE_OUT_OF_RANGE;
+      }
     } catch(e) {
       OutputView.printError(e);
       this.readBridgeSize();
