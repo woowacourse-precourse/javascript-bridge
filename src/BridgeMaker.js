9b49d897093da8d603b5508343ca14d6805dc420
unsnruu <unsnruu@gmail.com>
2022-11-16 17:24:32 +0900

    feat: makeBridge 메서드 로직 구현
    반복문을 사용하여 다리의 정/오답을 생성하는 메서드를 구현하였습니다.
    이 경우 if/else를 사용하는 편이 불필요하게 break 혹은 return의 사용이 필요하지 않아
    보다 깔끔하게 코드가 유지되는 것처럼 보였습니다.

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,3 +1,4 @@
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  * 1. 프로퍼티를 추가할 수 없다.
@@ -11,8 +12,15 @@ const BridgeMaker = {
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
   makeBridge(size, generateRandomNumber) {
-    //randomNumber === 0 => D
-    //randomNumber === 1 => U
+    const bridge = [];
+
+    for (let i = 0; i < size; i++) {
+      const number = generateRandomNumber();
+      if (number === 0) bridge.push("D");
+      else bridge.push("U");
+    }
+
+    return bridge;
   },
 };
 
