ef05542fa22b3db81421e079311dd760e9f6c5d0
chiman <rlaclghks123@naver.com>
2022-11-16 23:25:49 +0900

    feat: 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-class App {
-  play() {}
-}
+const OutputView = require('./OutputView');
 
+class App {
+  play() {
+    OutputView.printStartAnnouncement();
+  }
+}
+const app = new App();
+app.play();
 module.exports = App;
