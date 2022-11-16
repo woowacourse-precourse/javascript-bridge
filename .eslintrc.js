8ac29bd186ed5ed57b5284705087019ad58a2ffe
Quartz <skyu.dev@gmail.com>
2022-11-16 15:47:24 +0900

    chore: eslint 추가

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ["airbnb", "airbnb/hooks"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    quotes: ["off", "single"],
+    "class-methods-use-this": "off",
+  },
+};
