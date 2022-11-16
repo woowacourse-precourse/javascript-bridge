b9aeb3f1a65c7468312c0ad131ca246128fa0a91
kinoasm <kinoasm@gmail.com>
2022-11-16 23:07:54 +0900

    feat: 길이에 맞는 다리 만들기

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ const BridgeGame = require("./BridgeGame");
 const BridgeMaker = require("./BridgeMaker");
 const InputView = require("./InputView");
 const OutputView = require("./OutputView");
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
 
 class App {
+    COUNT = 0;
+    BRIDGE = [];
+
     play() {
         OutputView.printHello();
         const length = InputView.readBridgeSize();
+        this.BRIDGE = BridgeMaker.makeBridge(
+            length,
+            BridgeRandomNumberGenerator
+        );
     }
 }
 
