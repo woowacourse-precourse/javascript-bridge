a2dc9e0fdce252faa73ec9b0dc0bf6c60036556f
Mcdonalds Trump <whdmschd2@gmail.com>
2022-11-16 23:51:47 +0900

    feat: InputView readLine

--- src/utils/constans.js
+++ src/utils/constans.js
@@ -1,6 +1,6 @@
 const INPUT_MESSAGE = {
   BRIDGE_SIZE: "다리의 길이를 입력해주세요.",
-  PLAYER_MOVING: "이동할 칸을 선택해주세요. (위: U, 아래: D)",
+  MOVING_COMMAND: "이동할 칸을 선택해주세요. (위: U, 아래: D)",
   GAEM_COMMAND:
     "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
 };
