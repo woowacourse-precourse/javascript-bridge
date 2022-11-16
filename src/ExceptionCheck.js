c0adcda9efa97c7ff952604f06a8d96dc2f14918
unknown <chn0202@gmail.com>
2022-11-17 07:01:36 +0900

    feat(ExcptionCheck): 입력된 다리 길이가 3보다 작거나 20보다 크면 예외 처리

--- src/ExceptionCheck.js
+++ src/ExceptionCheck.js
@@ -1,11 +1,18 @@
 const ExceptionCheck = {
+  LOWER_INCLUSIVE: 3,
+  UPPER_INCLUSIVE: 20,
   bridgeSize(bridgeSize) {
     this.number(bridgeSize);
+    this.range(bridgeSize);
   },
   number(bridgeSize) {
     const numberRegEx = /[^0-9]/g;
 
     if (numberRegEx.test(bridgeSize))
+      throw new Error("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+  },
+  range(bridgeSize) {
+    if (bridgeSize < this.LOWER_INCLUSIVE || bridgeSize > this.UPPER_INCLUSIVE)
       throw new Error("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
   },
 };
