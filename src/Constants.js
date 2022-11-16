0f9160a26599b5c7f2f1a7ece20c47734554b5ef
hae-on <solbi2004@naver.com>
2022-11-17 00:41:32 +0900

    refactor: exports 방식 변경

--- src/Constants.js
+++ src/Constants.js
@@ -1,13 +1,27 @@
-export const BRIDGE_MIN_LENGTH = 3;
-export const BRIDGE_MAX_LENGTH = 5;
-export const UP = 'U';
-export const DOWN = 'D';
+const BRIDGE_MIN_LENGTH = 3;
+const BRIDGE_MAX_LENGTH = 5;
+const UP = 'U';
+const DOWN = 'D';
 
-export const START_GAME = '다리 건너기 게임을 시작합니다.';
-export const INPUT_BRIDGE_LENGTH = '다리의 길이를 입력해주세요.';
-export const INPUT_MOVE_ROOM = '이동할 칸을 선택해주세요. (위: U, 아래: D)';
-export const INPUT_RETRY =
+const START_GAME = '다리 건너기 게임을 시작합니다.';
+const INPUT_BRIDGE_LENGTH = '다리의 길이를 입력해주세요.';
+const INPUT_MOVE_ROOM = '이동할 칸을 선택해주세요. (위: U, 아래: D)';
+const INPUT_RETRY =
   '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)';
-export const GAME_RESULT = '최종 게임 결과';
-export const GAME_SUCCESS = '게임 성공 여부: 성공';
-export const GAME_COUNT = '총 시도한 횟수: ';
+const GAME_RESULT = '최종 게임 결과';
+const GAME_SUCCESS = '게임 성공 여부: 성공';
+const GAME_COUNT = '총 시도한 횟수: ';
+
+module.exports = {
+  BRIDGE_MIN_LENGTH,
+  BRIDGE_MAX_LENGTH,
+  UP,
+  DOWN,
+  START_GAME,
+  INPUT_BRIDGE_LENGTH,
+  INPUT_MOVE_ROOM,
+  INPUT_RETRY,
+  GAME_RESULT,
+  GAME_SUCCESS,
+  GAME_COUNT,
+};
