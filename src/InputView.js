1e22004a7c870ed1b7628b2031b2dd6e0aa38bf6
Rki0 <pky00823@gmail.com>
2022-11-16 21:19:05 +0900

    feat(InputView): 플레이어가 이동할 칸 입력받는 기능 추가
    플레이어가 어느 칸(위, 아래)로 이동할지 입력하는 메서드를 구현.
    입력이 됨과 동시에 유효성 검사를 시작한다.

--- src/InputView.js
+++ src/InputView.js
@@ -1,9 +1,10 @@
 const { Console } = require("@woowacourse/mission-utils");
-const { BridgeSize } = require("./utils");
+const { BridgeSize, MoveInput } = require("./utils");
 const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
 const BridgeMaker = require("./BridgeMaker");
 
 const ASK_BRIDGE_LENGTH = "다리의 길이를 입력해주세요.\n";
+const ASK_WHERE_WANT_TO_GO = "\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n";
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -19,13 +20,19 @@ const InputView = {
 
       const generater = BridgeRandomNumberGenerator.generate;
       const canWalkBridge = BridgeMaker.makeBridge(size, generater);
+
+      this.readMoving();
     });
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    Console.readLine(ASK_WHERE_WANT_TO_GO, (wantGo) => {
+      new MoveInput(wantGo);
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
