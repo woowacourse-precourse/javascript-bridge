15fc26119897ebc4f8bc137e11af3e22c775efb1
dlwl98 <ff981113@hanyang.ac.kr>
2022-11-16 17:12:07 +0900

    test(ValidateTest): 입력값의 범위 테스트 작성

--- __tests__/ValidateTest.js
+++ __tests__/ValidateTest.js
@@ -7,4 +7,11 @@ describe('예외처리 테스트', () => {
       expect(() => Validator.validateIsNumber(input)).toThrow('[ERROR]');
     },
   );
+
+  test.each([1, 21, -1, 2, -30])(
+    '범위를 벗어나면 예외처리',
+    (input) => {
+      expect(() => Validator.validateInRange(3, 20, input)).toThrow('[ERROR]');
+    },
+  );
 });
