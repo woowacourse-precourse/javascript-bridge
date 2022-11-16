391a2b0181f6505680aa0d24c4882e0e451141a3
unsnruu <unsnruu@gmail.com>
2022-11-16 16:31:43 +0900

    docs: BridgeMaker에 대한 요구사항 명시

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,5 +1,8 @@
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
+ * 1. 프로퍼티를 추가할 수 없다.
+ * 2. 파일 경로는 변경할 수 없다.
+ * 3. 시그니처와 반환 타입을 변경할 수 없다.
  */
 const BridgeMaker = {
   /**
@@ -7,7 +10,10 @@ const BridgeMaker = {
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
-  makeBridge(size, generateRandomNumber) {},
+  makeBridge(size, generateRandomNumber) {
+    //randomNumber === 0 => D
+    //randomNumber === 1 => U
+  },
 };
 
 module.exports = BridgeMaker;
