5237de3f251d67e884b10a47ab253dec9ecae0ed
kangyeongmin <kym6050@hanyang.ac.kr>
2022-11-17 01:08:02 +0900

    📌feat: 다리의 길이를 입력받는 기능

--- src/InputView.js
+++ src/InputView.js
@@ -1,21 +1,14 @@
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+const MissionUtils = require("@woowacourse/mission-utils");
+const { REGEX } = require("./constant");
+const OutputView = require("./OutputView");
+const BridgeGame = require("./BridgeGame");
+
 const InputView = {
-  /**
-   * 다리의 길이를 입력받는다.
-   */
-  readBridgeSize() {},
-
-  /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
-  readMoving() {},
-
-  /**
-   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-   */
-  readGameCommand() {},
+  readBridgeSize() {
+    MissionUtils.Console.readLine("다리의 길이를 입력해주세요.\n", (input) => {
+      this.handleBrideSizeException(input);
+    });
+  },
 };
 
 module.exports = InputView;
