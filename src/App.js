ba8439c0e5c29816309aade839adff4ba6800898
Yoo Seung Guk <dbtmd878@naver.com>
2022-11-16 18:33:25 +0900

    feat(Functions): 다리의 길이를 받는 기능 추가, 길이에 따른 정답 배열 생성 기능 추가, 정답 배열을 바탕으로 게임 시작 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+const BridgeGame = require("./BridgeGame");
+const BridgeMaker = require("./BridgeMaker");
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+const InputView = require("./InputView");
+
 class App {
-  play() {}
+  async play() {
+    const SIZE = await InputView.readBridgeSize();
+    const RANDOM_NUMBER_GENERATOR = BridgeRandomNumberGenerator.generate;
+    const ANSWER_BRIDGE_ARRAY = BridgeMaker.makeBridge(
+      SIZE,
+      RANDOM_NUMBER_GENERATOR
+    );
+    const BRIDGE_GAME = new BridgeGame(ANSWER_BRIDGE_ARRAY);
+    BRIDGE_GAME.play();
+  }
 }
 
+const app = new App();
+app.play();
 module.exports = App;
