3a02ee7026d268aa98b1ad7b3809d5b053832dbd
hae-on <solbi2004@naver.com>
2022-11-17 00:29:30 +0900

    feat: 상수 설정

---
+++ src/Constants.js
@@ -0,0 +1,13 @@
+export const BRIDGE_MIN_LENGTH = 3;
+export const BRIDGE_MAX_LENGTH = 5;
+export const UP = 'U';
+export const DOWN = 'D';
+
+export const START_GAME = '다리 건너기 게임을 시작합니다.';
+export const INPUT_BRIDGE_LENGTH = '다리의 길이를 입력해주세요.';
+export const INPUT_MOVE_ROOM = '이동할 칸을 선택해주세요. (위: U, 아래: D)';
+export const INPUT_RETRY =
+  '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)';
+export const GAME_RESULT = '최종 게임 결과';
+export const GAME_SUCCESS = '게임 성공 여부: 성공';
+export const GAME_COUNT = '총 시도한 횟수: ';
