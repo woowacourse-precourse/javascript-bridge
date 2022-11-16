16b638fed16f72d1e080ee3b879bb6b51cd1d2bb
Narcoker <jykim9868@gmail.com>
2022-11-16 20:23:24 +0900

    Feat: 입력 값 검증 후 다리 생성 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 const OutputView = require('./OutputView');
+const InputView = require('./InputView');
 
 class App {
   play() {
     OutputView.printStart();
+    InputView.readBridgeSize();
   }
 }
+
+const app = new App();
+app.play();
 module.exports = App;
--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,6 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Bridge = require("./Bridge");
+const { MESSAGE } = require("./Messages");
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,17 +8,21 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    MissionUtils.Console.readLine(MESSAGE.ENTER_BRIDGE_SIZE, (size) => {
+      const bridge = new Bridge(size);
+    })
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() { },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() { },
 };
 
 module.exports = InputView;
