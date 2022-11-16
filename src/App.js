b925e3262516903ef1468b16aea05a0ff2a5b686
Suyoung <jsy8492@gmail.com>
2022-11-16 15:42:58 +0900

    feat(bridge): 자동으로 생성할 다리 길이를 입력받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const { Console } = require('@woowacourse/mission-utils');
+
+const InputView = require('./InputView');
+const OutputView = require('./OutputView');
+
 class App {
-  play() {}
+  play() {
+    Console.print('다리 건너기 게임을 시작합니다.');
+    this.inputBridgeSize();
+  }
+
+  inputBridgeSize() {
+    InputView.readBridgeSize();
+  }
 }
+
+new App().play();
 
 module.exports = App;
