2eb11b64819d9ce5271b61e743c99c2ee8d1fe53
Hchan2 <seongchan_@naver.com>
2022-11-16 22:23:40 +0900

    입력 값, 출력 값 상수화

---
+++ src/constant/outputMessage.js
@@ -0,0 +1,7 @@
+const GAME_RESULT = (result) => { return `게임 성공 여부: ${result}` };
+const TOTAL_ATTEMPT = (attemptNumber) => { return `총 시도한 횟수: ${attemptNumber}` };
+
+module.exports = {
+    GAME_RESULT,
+    TOTAL_ATTEMPT
+}
