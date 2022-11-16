3cc8dbfde1eebad62618c90966a659cfab261d4c
banma70 <banma1234@gmail.com>
2022-11-16 17:32:35 +0900

    feature: 다리생성 관련 예외처리 함수 작성

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -8,6 +8,7 @@ const BridgeMaker = {
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
   makeBridge(size, generateRandomNumber) {
+    BridgeMaker.checkSize(size);
     let bridge = [];
     for (let i=0; i<size; i++) {
       let RANDOM_NUMBER = generateRandomNumber();
@@ -18,6 +19,16 @@ const BridgeMaker = {
     }
 
     return bridge;
+  },
+
+  checkSize(size) {
+    if (typeof(size) != "number") {
+      throw new Error("[Error] 다리의 크기는 숫자를 입력해주세요");
+    }
+
+    if (size < 3 | size > 20) {
+      throw new Error("[Error] 다리의 크기는 3 ~ 20 사이의 숫자만 가능합니다.");
+    }
   },
 };
 
