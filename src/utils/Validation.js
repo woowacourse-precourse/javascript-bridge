07404071cbb3eec3c3a8405bc2eff66442be0fcc
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-11-17 00:59:55 +0900

    Refactor: throw 에러 출력문 위치 변경 InputView' MissionUtils 이동

--- src/utils/Validation.js
+++ src/utils/Validation.js
@@ -1,22 +1,25 @@
 const {DEFAULTS, ERRORLINE} = require('./Constants');
-
+const MissionUtils = require('@woowacourse/mission-utils');
 const onlyNmbr = /^[0-9]+$/;
 
 class Validation{
   checkBridgeSize(input){
     if (input < DEFAULTS.START_RANGE_cnt || input > DEFAULTS.END_RANGE_cnt || !(onlyNmbr).test(input)){
+      MissionUtils.Console.print(ERRORLINE.BRIDGE_LENGTH_ERROR);
       throw new Error(ERRORLINE.BRIDGE_LENGTH_ERROR);
     }
   }
 
   checkCanMove(input){
     if (input !== DEFAULTS.UP && input !== DEFAULTS.DOWN){
+      MissionUtils.Console.print(ERRORLINE.MOVE_ERROR);
       throw new Error(ERRORLINE.MOVE_ERROR);
     }
   }
 
   checkRestartOrNot(input){
     if (input !== DEFAULTS.RESTART && input !== DEFAULTS.QUIT){
+      MissionUtils.Console.print(ERRORLINE.RESTART_ERROR);
       throw new Error(ERRORLINE.RESTART_ERROR);
     }
   }
