835a0030967ba3585769237c267635068f997efe
banma70 <banma1234@gmail.com>
2022-11-16 16:46:25 +0900

    test: 다리생성 테스트코드 작성

--- __tests__/bridgeTest.js
+++ __tests__/bridgeTest.js
@@ -1,2 +1,15 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-const Test = require('../src/Feature');
+const App = require("../src/Feature");
+const BridgeMaker = require("../src/BridgeMaker");
+
+describe("다리 건너기 테스트", () => {
+  test("다리 생성 테스트", () => {
+    const randomNumbers = ["1", "0", "0"];
+    const mockGenerator = randomNumbers.reduce((acc, number) => {
+      return acc.mockReturnValueOnce(number);
+    }, jest.fn());
+
+    const bridge = BridgeMaker.makeBridge(3, mockGenerator);
+    expect(bridge).toEqual(["U", "D", "D"]);
+  });
+});
