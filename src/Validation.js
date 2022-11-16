67172f527849b6151007d61bd58656a84700e361
unsnruu <unsnruu@gmail.com>
2022-11-16 17:59:09 +0900

    feat: length에 대해서 숫자인지와 범위를 초과하는지 여부를 확인하는 메서드 구현

--- src/Validation.js
+++ src/Validation.js
@@ -1,7 +1,27 @@
 const Validation = {
+  /**
+   * @param {number} start : 범위의 시작값에 해당하는 숫자
+   * @param {number} end : 범위의 끝값에 해당하는 숫자
+   * @param {number} number : 식별의 대상이 되는 숫자
+   * @return {boolean}
+   */
+  isInRange(start, end, number) {
+    if (number < start || number > end) return false;
+    return true;
+  },
+  /**
+   * @param {string} length : 다리의 길이 값
+   * @returns {number}
+   */
   validateBridgeLength(length) {
-    const errorMessage = "[ERROR] 입력할 수 있는 숫자의 범위를 초과하였습니다.";
-    if (length < 2 || length > 20) throw new Error(errorMessage);
+    if (isNaN(length)) {
+      throw new Error("[ERROR] 2~20 사이의 숫자를 입력해 주세요");
+    }
+    const parsedLength = parseInt(length, 10);
+    if (this.isInRange(2, 20, parsedLength)) {
+      throw new Error("[ERROR] 입력할 수 있는 숫자의 범위를 초과하였습니다.");
+    }
+    return parsedLength;
   },
 };
 
