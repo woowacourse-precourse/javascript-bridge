aa39d965b793d90e5dafd4b373e7d049761b32c7
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2022-11-16 21:09:50 +0900

    feat: 입력값을 받고 기준에 따라 예외기준 출력, 예외처리 하는 기능 구현

--- src/InputView.js
+++ src/InputView.js
@@ -10,7 +10,28 @@ const InputView = {
    */
   readBridgeSize() {
     MissionUtils.Console.print('다리의 길이를 입력해주세요.');
-    MissionUtils.Console.readLine('', (userInput) => {});
+    MissionUtils.Console.readLine('', (userInput) => {
+      this.validateRange(userInput);
+      this.validatrForm(userInput);
+    });
+  },
+
+  validateRange(userInput) {
+    try {
+      Validate.sizeInputRange(userInput);
+    } catch {
+      MissionUtils.Console.print('\n[ERROR] 공백없이 3 ~ 20 사이의 숫자를 입력 해주세요.\n');
+      this.readBridgeSize(userInput);
+    }
+  },
+
+  validatrForm(userInput) {
+    try {
+      Validate.sizeInputForm(userInput);
+    } catch {
+      MissionUtils.Console.print('\n[ERROR] 다리 길이는 숫자로 입력해 주세요.\n');
+      this.readBridgeSize(userInput);
+    }
   },
 
   /**
