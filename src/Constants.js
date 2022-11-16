698b8edf419390b5b22953ca0516da5b6b3281a7
pakxe <pigkill40@naver.com>
2022-11-17 05:04:32 +0900

    refactor(Constants): 객체의 프로퍼티 키를 카멜 케이스로 작성

--- src/Constants.js
+++ src/Constants.js
@@ -1,39 +1,39 @@
 const BRIDGE = Object.freeze({
-  MIN: 3,
-  MAX: 20,
-  UP: 'U',
-  DOWN: 'D',
+  min: 3,
+  max: 20,
+  up: 'U',
+  down: 'D',
 });
 
 const GAME = Object.freeze({
-  RESTART: 'R',
-  QUIT: 'Q',
-  CLEAR: '성공',
-  FAIL: '실패',
+  restart: 'R',
+  quit: 'Q',
+  clear: '성공',
+  fail: '실패',
 });
 
 const SENTENCE = Object.freeze({
-  START: '다리 건너기 게임을 시작합니다.',
-  LENGTH: '다리의 길이를 입력해주세요.',
-  SELECT_STEP: `이동할 칸을 선택해주세요. (위: ${BRIDGE.UP}, 아래: ${BRIDGE.DOWN})`,
-  SELECT_RESTART: `게임을 다시 시도할지 여부를 입력해주세요. (재시도: ${GAME.RESTART}, 종료: ${GAME.QUIT})`,
+  start: '다리 건너기 게임을 시작합니다.',
+  length: '다리의 길이를 입력해주세요.',
+  selectStep: `이동할 칸을 선택해주세요. (위: ${BRIDGE.UP}, 아래: ${BRIDGE.DOWN})`,
+  selectRestart: `게임을 다시 시도할지 여부를 입력해주세요. (재시도: ${GAME.RESTART}, 종료: ${GAME.QUIT})`,
 });
 
 const RESULT = Object.freeze({
-  SENTENCE: '최종 게임 결과',
-  OPEN_BRACKET: '[',
-  CLOSE_BRACKET: ']',
-  DIVIDER: '|',
-  IS_CLEAR: '게임 성공 여부: ',
-  TRY_COUNT: '총 시도한 횟수: ',
+  sentence: '최종 게임 결과',
+  openBracket: '[',
+  closeBracket: ']',
+  divider: '|',
+  isClear: '게임 성공 여부: ',
+  tryCount: '총 시도한 횟수: ',
 });
 
 const ERROR = Object.freeze({
-  PREFIX: '[ERROR]',
-  LENGTH: `다리 길이는 ${BRIDGE.MIN}부터 ${BRIDGE.MAX} 사이의 숫자여야 합니다.`,
-  IS_NAN: '다리 길이는 숫자로 입력해야 합니다.',
-  STEP: `다리 선택은 위: ${BRIDGE.UP}, 아래: ${BRIDGE.DOWN} 둘 중 하나를 입력해야 합니다.`,
-  RESTART: `재시도: ${GAME.RESTART}, 종료: ${GAME.QUIT} 둘 중 하나를 입력해야 합니다. `,
+  prefix: '[ERROR]',
+  length: `다리 길이는 ${BRIDGE.MIN}부터 ${BRIDGE.MAX} 사이의 숫자여야 합니다.`,
+  isNan: '다리 길이는 숫자로 입력해야 합니다.',
+  step: `다리 선택은 위: ${BRIDGE.UP}, 아래: ${BRIDGE.DOWN} 둘 중 하나를 입력해야 합니다.`,
+  restart: `재시도: ${GAME.RESTART}, 종료: ${GAME.QUIT} 둘 중 하나를 입력해야 합니다. `,
 });
 
 module.exports = { BRIDGE, GAME, SENTENCE, RESULT, ERROR };
