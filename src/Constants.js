e421ec2b8f27ca144bce6f448d2b92c59fc33430
hae-on <solbi2004@naver.com>
2022-11-17 00:43:54 +0900

    style: 개행 추가

--- src/Constants.js
+++ src/Constants.js
@@ -4,10 +4,10 @@ const UP = 'U';
 const DOWN = 'D';
 
 const START_GAME = '다리 건너기 게임을 시작합니다.';
-const INPUT_BRIDGE_LENGTH = '다리의 길이를 입력해주세요.';
-const INPUT_MOVE_ROOM = '이동할 칸을 선택해주세요. (위: U, 아래: D)';
+const INPUT_BRIDGE_LENGTH = '다리의 길이를 입력해주세요.\n';
+const INPUT_MOVE_ROOM = '이동할 칸을 선택해주세요. (위: U, 아래: D)\n';
 const INPUT_RETRY =
-  '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)';
+  '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n';
 const GAME_RESULT = '최종 게임 결과';
 const GAME_SUCCESS = '게임 성공 여부: 성공';
 const GAME_COUNT = '총 시도한 횟수: ';
