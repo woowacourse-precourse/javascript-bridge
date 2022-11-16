e7d519206d8e9d82537f1e912d427b5df189749b
Narcoker <jykim9868@gmail.com>
2022-11-16 20:15:11 +0900

    Feat: 0,1 무작위 값 생성 후 문자열로 변경

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -8,7 +8,7 @@ const BridgeMaker = {
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
   makeBridge(size, generateRandomNumber) {
-    return Array.from({ length: size }, () => generateRandomNumber() === "0" ? 'D' : 'U');
+    return Array.from({ length: size }, () => generateRandomNumber().toString() === '0' ? 'D' : 'U');
   },
 };
 
