40350dc811ab2fbd688557d0b59b59b87c051c06
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-11-16 23:47:09 +0900

    Feat: 재시작 여부 메서드 추가

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,48 +1,87 @@
 const MissionUtils = require('@woowacourse/mission-utils');
-const controller = require('../Controller');
+const BridgeMaker = require('../BridgeMaker');
+const BridgeRandomNumberGenerator = require('../BridgeRandomNumberGenerator');
+const bridgeGame = require('../model/BridgeGame');
 const {DEFAULTS, CONSOLELINE, ERRORLINE} = require('../utils/Constants');
 const validation = require('../utils/Validation');
 const OutputView = require('./OutputView');
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
+
+let answer = [];
+let cnt_move = 0;
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
+
   readBridgeSize(){
     MissionUtils.Console.readLine(CONSOLELINE.BRIDGE_LENGTH_INPUT, (input) => {
       try{
         validation.checkBridgeSize(input);
-        return controller.getBridge(input);
       } catch(err){
         MissionUtils.Console.print(ERRORLINE.BRIDGE_LENGTH_ERROR);
         return this.readBridgeSize();
       }
+      this.getBridge(input);
     })
+  },
+
+  getBridge(input){
+    const bridgeAnswer = BridgeMaker.makeBridge(input, BridgeRandomNumberGenerator.generate);
+    answer = bridgeAnswer;
+    this.readMoving(bridgeAnswer);
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving(answer) {
-    MissionUtils.Console.readLine(CONSOLELINE.MOVE_INPUT, (input) => {
+    cnt_move += 1;
+    MissionUtils.Console.readLine(CONSOLELINE.MOVE_INPUT, (upOrdown) => {
       try{
-        validation.checkCanMove(input);
-        return OutputView.printMap(answer, input);
+        validation.checkCanMove(upOrdown);
       } catch(err){
-        MissionUtils.Console.print(ERRORLINE.BRIDGE_LENGTH_ERROR);
-        return this.readBridgeSize();
+        MissionUtils.Console.print(ERRORLINE.MOVE_ERROR);
+        return this.readMoving(answer);
       }
+      const gameLog = OutputView.printMap(answer, upOrdown);
+      if(this.restartCheck(cnt_move, gameLog)){
+        cnt_move = 0;
+        return this.readGameCommand();
+      }
+      this.readMoving(answer);
     })
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+    MissionUtils.Console.readLine(CONSOLELINE.RESTART_CHECK, (restart) => {
+      try{
+        validation.checkRestartOrNot(restart);
+      } catch(err){
+        MissionUtils.Console.print(ERRORLINE.RESTART_ERROR);
+        return this.readGameCommand();
+      }
+      this.readMoving(answer);
+    })
+  },
+
+  restartCheck(cnt_move, gameLog){
+    if (cnt_move == answer.length){
+      bridgeGame.init();
+      return true;
+    }
+    if (gameLog[0][gameLog[0].length-1] === 'X' || gameLog[1][gameLog[1].length-1] === 'X'){
+      bridgeGame.init();
+      return true;
+    }
+    return false;
+  }
 };
 
 module.exports = InputView;
-
-InputView.readBridgeSize();
