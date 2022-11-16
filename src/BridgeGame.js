f85797e520a80590d370e4e18a15a3e6cd44de48
AnJaeSeong <dktmzh6@gmail.com>
2022-11-17 03:01:55 +0900

    feat(BridgeGame): move()
    move()에서 입력값에 따라 현재 상태 정보 업데이트 및
    해당 라운드의 결과 반환

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -3,20 +3,27 @@
  */
 class BridgeGame {
   #bridge     // 생성된 다리 정보
-  #curr       // 현재 플레이어의 다리 위치
+  #curr       // 현재 상태 정보
   #try        // 현재까지 시도 횟수
 
   constructor(bridge) {
-    this.#bridge = bridge;
-    this.#curr = 0;
-    this.#try = 0;
+    this.#bridge = [...bridge];
+    this.#curr = [];
+    this.#try = 1;
   }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+   * 입력받은 방향(U or D)에 따라
+   * 이동 가능한 칸일 경우 true, 불가능한 칸일 경우 false를 현재 상태에 업데이트 하고 반환
    */
-  move() {}
+  move(input) {
+    if(this.#bridge[this.#curr.length] == input) {
+      this.#curr.push(true);
+      return true;
+    }
+    this.#curr.push(false);
+    return false;
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
