817a9c3493f2020a7f8170ee4ae3b3e76e10ba09
dlwl98 <ff981113@hanyang.ac.kr>
2022-11-16 16:52:56 +0900

    refactor(Validator): 숫자인지 확인하는 메서드의 if문 한 개로 변경

--- src/Validator.js
+++ src/Validator.js
@@ -1,9 +1,6 @@
 const Validator = {
   validateIsNumber(input) {
-    if (input.trim() === '') {
-      throw new Error('[ERROR] 숫자만 입력 가능합니다');
-    }
-    if (Number.isNaN(Number(input))) {
+    if (!input.trim() || Number.isNaN(Number(input))) {
       throw new Error('[ERROR] 숫자만 입력 가능합니다');
     }
   },
