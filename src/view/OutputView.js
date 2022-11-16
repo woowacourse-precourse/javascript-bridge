e7d152565058a1d888143bf3e6a56a5c5f4a9596
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-11-17 01:27:24 +0900

    Feat: 최종 성공 여부, 시도횟수 출력 구현

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -13,27 +13,44 @@ const OutputView = {
    */
   printMap(answer, upOrdown) {
     const returns = (bridgeGame.move(answer, upOrdown));
-    const up_list = returns[0];
-    const down_list = returns[1];
     const cnt = returns[2];
-    MissionUtils.Console.print(RESULTLINE.LOG(up_list));
-    MissionUtils.Console.print(RESULTLINE.LOG(down_list));
-    if (cnt === answer.length){
-      this.printResult(up_list, down_list);
-    }
+    MissionUtils.Console.print(RESULTLINE.LOG(returns[0]));
+    MissionUtils.Console.print(RESULTLINE.LOG(returns[1]));
+    //this.justFinishOrSuccess(answer, returns, cnt);
     return returns;
   },
+  
+  failOutputandNomoreGame(gameLog){
+    this.printFail(gameLog[0], gameLog[1]);
+    MissionUtils.Console.close();
+  },
 
+  SuccessOutput(gameLog){
+    this.printSuccess(gameLog[0], gameLog[1]);
+    MissionUtils.Console.close();
+  },
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult(up_list, down_list) {
+  printResultLog(up_list, down_list) {
     MissionUtils.Console.print(CONSOLELINE.FIN_RESULT_LOG);
     MissionUtils.Console.print(RESULTLINE.LOG(up_list));
     MissionUtils.Console.print(RESULTLINE.LOG(down_list));
   },
+
+  printFail(up_list, down_list){
+    this.printResultLog(up_list, down_list);
+    MissionUtils.Console.print(CONSOLELINE.FAIL_RESULT);
+    MissionUtils.Console.print(RESULTLINE.COUNT(bridgeGame.restartcnt))
+  },
+
+  printSuccess(up_list, down_list){
+    this.printResultLog(up_list, down_list);
+    MissionUtils.Console.print(CONSOLELINE.SUCCESS_RESULT);
+    MissionUtils.Console.print(RESULTLINE.COUNT(bridgeGame.restartcnt))
+  }
 };
 
 module.exports = OutputView;
