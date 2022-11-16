7f71d34f2e6a0e2df2503759b57c5ccde611ea15
Narcoker <jykim9868@gmail.com>
2022-11-16 20:21:22 +0900

    Feat: BRIDGE_SIZE_NOT_NUMBER 에러 문구 추가

--- src/Messages.js
+++ src/Messages.js
@@ -5,6 +5,7 @@ const MESSAGE = Object.freeze({
 
 const ERROR = Object.freeze({
     BRIDGE_SIZE_OUT_BOUNDARY: "[ERROR] 3이상 20이하 사이의 숫자여야 합니다.",
+    BRIDGE_SIZE_NOT_NUMBER: "[ERROR] 입력값은 숫자여야 합니다.",
 })
 
 module.exports = { MESSAGE, ERROR };
