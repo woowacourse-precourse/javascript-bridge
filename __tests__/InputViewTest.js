21f30a2490e67c2ff807cace8b97d540a442e1da
pakxe <pigkill40@naver.com>
2022-11-17 05:17:54 +0900

    test(InputViewTest): 다리 크기를 잘 읽어오는지 테스트

---
+++ __tests__/InputViewTest.js
@@ -0,0 +1,15 @@
+const InputView = require('../src/InputView');
+const { mockQuestions } = require('./ApplicationTest');
+
+describe('입력 테스트', () => {
+  test('다리길이 입력받기', () => {
+    mockQuestions(['10']);
+    const length = InputView.readBridgeSize();
+
+    expect(length).toEqual(10);
+  });
+
+  test('jio', () => {
+    expect('hi').toEqual('hi');
+  });
+});
