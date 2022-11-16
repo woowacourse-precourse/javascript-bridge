f88834b44ea86fa402cec31d8df36fdd2e709876
Adult96 <djxor007@gmail.com>
2022-11-16 18:27:18 +0900

    test(InputTest.js): 다리 이동 유효성 검사 테스트 코드 작성

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -1,4 +1,5 @@
 const bridgeValidation = require('../src/validation/bridgeValidation');
+const movingValidation = require('../src/validation/movingValidation');
 
 describe('User Input 테스트', () => {
   test('다리 갯수 3~20까지의 숫자', () => {
@@ -7,5 +8,14 @@ describe('User Input 테스트', () => {
     expect(bridgeValidation('a')).toBeFalsy();
     expect(bridgeValidation('')).toBeFalsy();
     expect(bridgeValidation(' ')).toBeFalsy();
+  });
+
+  test('다리 이동 U/D의 문자 확인', () => {
+    expect(movingValidation('U')).toBeTruthy();
+    expect(movingValidation('D')).toBeTruthy();
+    expect(movingValidation(1)).toBeFalsy();
+    expect(movingValidation('a')).toBeFalsy();
+    expect(movingValidation('')).toBeFalsy();
+    expect(movingValidation(' ')).toBeFalsy();
   });
 });
