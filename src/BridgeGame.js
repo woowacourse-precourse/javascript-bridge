67a7b3d5f7c940d75d1edb440c879729dd3b8a26
Yoo Seung Guk <dbtmd878@naver.com>
2022-11-16 18:35:41 +0900

    feat(Functions): 위,아래 움직임을 받는 기능 추가 , 사용자의 움직임을 기록한 배열 추가 기능, 종료 기능 추가

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,20 +1,73 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Console = MissionUtils.Console;
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
+
+const InputView = require("./InputView");
+const OutputView = require("./OutputView");
+
+// inputView, outputView 사용 불가
 class BridgeGame {
-  /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  move() {}
+  #current_bridge = 0;
+  #bridge_map = [[], []];
+  #ANSWER_BRIDGE_ARRAY;
+
+  constructor(ANSWER_BRIDGE_ARRAY) {
+    this.#ANSWER_BRIDGE_ARRAY = ANSWER_BRIDGE_ARRAY;
+    console.log(this.#ANSWER_BRIDGE_ARRAY);
+  }
+
+  play() {
+    console.log(this.#bridge_map);
+    this.move();
+  }
+
+  async move() {
+    while (this.#current_bridge < this.#ANSWER_BRIDGE_ARRAY.length) {
+      const COMMAND = await InputView.readMoving();
+      this.chageCommandNumber(COMMAND);
+      this.#current_bridge += 1;
+    }
+    return this.endingPage();
+  }
+
+  async endingPage() {
+    const COMMAND = await InputView.readGameCommand();
+    if (COMMAND === "Q") Console.close();
+    if (COMMAND === "R") this.retry();
+  }
+
+  pushBridgeArray(commandNumber, restArrayNumber) {
+    if (
+      String(commandNumber) === this.#ANSWER_BRIDGE_ARRAY[this.#current_bridge]
+    ) {
+      this.#bridge_map[commandNumber].push("O");
+      this.#bridge_map[restArrayNumber].push(" ");
+      return OutputView.printMap(this.#bridge_map);
+    }
+    this.#bridge_map[commandNumber].push("X");
+    this.#bridge_map[restArrayNumber].push(" ");
+    return OutputView.printMap(this.#bridge_map);
+  }
+
+  chageCommandNumber(commandString) {
+    const COMMAND_NUMBER = commandString === "U" ? 1 : 0;
+    const REST_ARRAY_NUMBER = COMMAND_NUMBER === 1 ? 0 : 1;
+    this.pushBridgeArray(COMMAND_NUMBER, REST_ARRAY_NUMBER);
+    return;
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  retry() {}
+  retry() {
+    this.#bridge_map = [[], []];
+    this.#current_bridge = 0;
+    return this.play();
+  }
 }
 
 module.exports = BridgeGame;
