3cb4b461b039e6f56a77acf31914a14b9868acdc
jinhobyeong <jhb_0921@naver.com>
2022-11-16 21:00:15 +0900

    docs : 기능 요구 사항 및 프로그래밍 요구 사항 정의

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -4,8 +4,8 @@
 const BridgeMaker = {
   /**
    * @param {number} size 다리의 길이
-   * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
-   * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
+   * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수 int return
+   * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다. bridge
    */
   makeBridge(size, generateRandomNumber) {},
 };
