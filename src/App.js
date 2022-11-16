a582515de2f8e5aea4c0ed2d75cfc6a67c848832
olo90632951 <olo90632951@gmail.com>
2022-11-16 20:45:28 +0900

    feat: 다리의 길이를 입력 받는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+const InputView = require('./view/InputView');
 const OutputView = require('./view/OutputView');
 
 class App {
   play() {
     OutputView.printStart();
+    this.inputBridgeLength();
+  }
+
+  inputBridgeLength() {
+    InputView.readBridgeSize((bridgeLength) => {
+      console.log(bridgeLength);
+    });
   }
 }
 
