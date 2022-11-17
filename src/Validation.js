5a60ad5ec47a000676963ffa88953b093325ed06
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-11-17 14:14:50 +0900

    Feat : make validate fnc for restart and move input

--- src/Validation.js
+++ src/Validation.js
@@ -1,7 +1,24 @@
 const Validation = {
+  RESTART: "R",
+  QUIT: "Q",
   validateIsNumber(number) {
     if (/\D/.test(number)) {
       throw new Error("[ERROR] 숫자만 입력해야 합니다.");
+    }
+  },
+  validateBridgeRange(number) {
+    if (number < 3 || number > 20) {
+      throw new Error("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+    }
+  },
+  validateMoveInput(input) {
+    if (input !== "U" && input !== "D") {
+      throw new Error("[ERROR] U 혹은 D만 입력할 수 있습니다.");
+    }
+  },
+  validateRestartInput(input) {
+    if (input !== Validation.RESTART && input !== Validation.QUIT) {
+      throw new Error("[ERROR] R 혹은 Q만 입력할 수 있습니다.");
     }
   },
 };
