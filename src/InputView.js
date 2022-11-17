97214de96b43d415b1d72615eaf7b0d092d9a4fc
bassyu <yrkdgus@gmail.com>
2022-11-17 22:10:45 +0900

    refactor(InputView): 입력 메세지 상수 추출

--- src/InputView.js
+++ src/InputView.js
@@ -1,21 +1,30 @@
 const { Console } = require('@woowacourse/mission-utils');
 
 const InputView = {
+  INPUT_MESSAGE: {
+    bridgeSize: '다리의 길이를 입력해주세요.',
+    moving: '이동할 칸을 선택해주세요. (위: U, 아래: D)',
+    gameCommand: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)',
+  },
+
   readBridgeSize(handleBridgeSize) {
-    Console.readLine('다리의 길이를 입력해주세요.\n', (input) => {
+    Console.print(InputView.INPUT_MESSAGE.bridgeSize);
+    Console.readLine('', (input) => {
       const size = Number(input);
       handleBridgeSize(size);
     });
   },
 
   readMoving(handleMoving) {
-    Console.readLine('이동할 칸을 선택해주세요.\n', (input) => {
+    Console.print(InputView.INPUT_MESSAGE.moving);
+    Console.readLine('', (input) => {
       handleMoving(input);
     });
   },
 
   readGameCommand(handleGameCommand) {
-    Console.readLine('게임을 다시 시도할지 여부를 입력해주세요.\n', (input) => {
+    Console.print(InputView.INPUT_MESSAGE.gameCommand);
+    Console.readLine('', (input) => {
       handleGameCommand(input);
     });
   },
