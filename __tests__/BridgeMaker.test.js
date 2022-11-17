96b8a0d653e177bdc69d5dc677f7c9b2d7e49dab
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-11-17 19:34:05 +0900

    test(BridgeMaker): 다리의 길이를 입력 받아서 다리를 생성해주는 함수
    전달받는 첫 번째 인자가 숫자가 아니라면 예외를 발생한다.

--- __tests__/BridgeMaker.test.js
+++ __tests__/BridgeMaker.test.js
@@ -3,6 +3,8 @@ const MissionUtils = require('@woowacourse/mission-utils');
 const BridgeMaker = require('../src/BridgeMaker');
 const BridgeRandomNumberGenerator = require('../src/BridgeRandomNumberGenerator');
 
+const NUMBER_ERROR_TEXT = '[ERROR] 전달된 인수는 숫자 타입이 아닙니다.';
+
 afterAll(() => {
   MissionUtils.Console.close();
 });
@@ -27,5 +29,13 @@ describe('다리의 길이를 입력 받아서 다리를 생성해주는 함수 
     const EXPECTED = 3;
 
     expect(BridgeMaker.makeBridge(EXPECTED, generate).every((v) => typeof v === 'string')).toBeTruthy();
+  });
+
+  test('전달받는 첫 번째 인자가 숫자가 아니라면 예외를 발생한다.', () => {
+    const EXPECTED = NaN;
+
+    expect(() => {
+      BridgeMaker.makeBridge(EXPECTED, generate);
+    }).toThrow(NUMBER_ERROR_TEXT);
   });
 });
