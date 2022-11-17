4581b44c44e318470c14a7450e081281d40a6f50
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-11-17 15:05:48 +0900

    test(Validation): 숫자 예외 파악 함수 테스트
    인수가 NaN이라면 예외를 발생시킨다.

--- __tests__/Validation.test.js
+++ __tests__/Validation.test.js
@@ -1,9 +1,19 @@
 const Validation = require('../src/Validation');
 
 describe('숫자 예외 파악 함수 테스트', () => {
+  const NUMBER_ERROR_TEXT = '[ERROR] 전달된 인수는 숫자 타입이 아닙니다.';
+
   test('메소드 이름은 "number"로 정의된다.', () => {
     const METHOD_NAME = 'number';
 
     expect(Validation.number.name).toEqual(METHOD_NAME);
+  });
+
+  test('인수가 NaN이라면 예외를 발생시킨다.', () => {
+    const EXPECTED = NaN;
+
+    expect(() => {
+      Validation.number(EXPECTED);
+    }).toThrow(NUMBER_ERROR_TEXT);
   });
 });
