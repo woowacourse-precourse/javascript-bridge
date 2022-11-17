834722d56e8aafbae36783c54ad28d10702a64de
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-17 16:55:03 +0900

    feat: 다리 길이 number타입 여부 검사 메서드 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -16,10 +16,21 @@ class BridgeGame {
     if (this.isEmpty(size)) {
       throw new Error('[ERROR] 입력값이 비었습니다. 값을 입력해주세요');
     }
+
+    if (!this.hasOnlyNumber(size)) {
+      throw new Error('[ERROR] 숫자가 아닌 문자를 입력했습니다. 숫자만 입력해 주세요');
+    }
   }
 
   isEmpty(size) {
     return !size.length;
+  }
+
+  hasOnlyNumber(size) {
+    return size
+      .split('')
+      .map((eachLetter) => parseInt(eachLetter, 10))
+      .every((number) => Number.isInteger(number));
   }
 }
 
