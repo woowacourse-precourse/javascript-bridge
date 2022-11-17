ac784e7206442c261361035f60c1b5ae8dc3696c
vjvl95 <vjvl95@naver.com>
2022-11-17 17:19:21 +0900

    feat : 시작 메시지 출력

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,3 +1,5 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -15,6 +17,10 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printResult() {},
+
+  StartGame() {
+    MissionUtils.Console.print('다리 건너기 게임을 시작합니다.');
+  },
 };
 
 module.exports = OutputView;
