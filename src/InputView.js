3e12fa207e1dd84df6985eeefa9f4a7bb0e3fcae
jinseo <ghkdwlstj99@gmail.com>
2022-11-18 01:17:46 +0900

    refactor: readBridgeSize 로직 수정

--- src/InputView.js
+++ src/InputView.js
@@ -1,7 +1,6 @@
 const { Console } = require("@woowacourse/mission-utils");
 const { makeBridge } = require("./BridgeMaker");
-const BridgeMaker = require("./BridgeMaker");
-const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+const { generate } = require("./BridgeRandomNumberGenerator");
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -11,12 +10,11 @@ const InputView = {
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize(){
-    Console.readline("다리의 길이를 입력해주세요" , (num) => {
+    Console.readLine("다리의 길이를 입력해주세요" , (num) => {
       this.bridgeException(num);
-      const bridgeRandomNum = BridgeRandomNumberGenerator.generate();
-      const createBridge = makeBridge(num, bridgeRandomNum);
-      this.readMoving(createBridge);
-    })
+      this.createBridge = makeBridge(num, generate);
+      this.readMoving();
+    });
   },
 
   /**
