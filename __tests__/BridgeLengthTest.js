52fbdff8f796e14d773feb6c18b503f0368ad9b3
turtle601 <turtle601@naver.com>
2022-11-17 14:26:22 +0900

    test(BrideLength) 테스트 코드 작성
    - case1: 3 ~ 20 사이 숫자가 아니면 에러

---
+++ __tests__/BridgeLengthTest.js
@@ -0,0 +1,29 @@
+const BridgeLength = require('../src/domain/BridgeLength');
+
+describe('다리 길이 테스트', () => {
+  test('(다리 길이(오류) - 문자)', () => {
+    const input = 'a';
+    const bridgeLength = new BridgeLength(input);
+    expect(() => bridgeLength.getValidateData()).toThrow('[ERROR]');
+  });
+  test('(다리 길이(오류) - 3미만의 숫자)', () => {
+    const input = '1';
+    const bridgeLength = new BridgeLength(input);
+    expect(() => bridgeLength.getValidateData()).toThrow('[ERROR]');
+  });
+  test('(다리 길이(오류) - 20초과의 숫자)', () => {
+    const input = '21';
+    const bridgeLength = new BridgeLength(input);
+    expect(() => bridgeLength.getValidateData()).toThrow('[ERROR]');
+  });
+  test('(다리 길이(오류) - 문자 + 숫자(3 ~ 20))', () => {
+    const input = '20 ';
+    const bridgeLength = new BridgeLength(input);
+    expect(() => bridgeLength.getValidateData()).toThrow('[ERROR]');
+  });
+  test('(다리 길이(정상) - 숫자(3 ~ 20))', () => {
+    const input = '3';
+    const bridgeLength = new BridgeLength(input);
+    expect(bridgeLength.getValidateData()).toEqual(3);
+  });
+});
