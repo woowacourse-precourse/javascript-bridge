a873b91a3e971eb75a8ca62ede219a3b705ac921
JiSu Kim <85912592+jsk3342@users.noreply.github.com>
2022-11-18 01:56:52 +0900

    feat(move): 재시도 로직 구현 중

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,4 +1,5 @@
 const { makeUserBridge } = require("./BridgeMaker");
+const { readGameCommand } = require('./InputView');
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
@@ -8,7 +9,7 @@ class BridgeGame {
     this.mainBridge = mainBridge;
     this.bridgeSize = mainBridge[0].length;
     this.count = 1;
-    this.userBridge = [[],[]];
+    this.userBridge = [[], []];
   }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -16,6 +17,10 @@ class BridgeGame {
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   move(moveInput) {
+    if (this.count === this.bridgeSize) {
+      this.retry(this.mainBridge);
+      return;
+    }
     this.userBridge = makeUserBridge(moveInput, this.userBridge, this.mainBridge);
     this.count++;
   }
@@ -25,7 +30,9 @@ class BridgeGame {
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  retry() {}
+  retry(mainBridge) {
+    readGameCommand(mainBridge);
+  }
 }
 
 module.exports = BridgeGame;
