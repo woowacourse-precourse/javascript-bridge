28e206d0839aa5402588e5b79533f5e52ac4b5d3
Suyeon <msjgr2801@gmail.com>
2022-11-17 15:18:48 +0900

    Feat: 재시작 기능 구현
    - gameStart: playGame 메소드를 호출로 종료여부를 판단
    - 만약 종료되지 않았다면 retry를 호출해서 재시작, 종료여부를 한 번 더 판단
    - OutputView: 종료 시 printResult를 호출해서 게임 성공 여부와 시도 횟수를 출력

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -28,28 +28,52 @@ class BridgeGame {
   }
 
   gameStart() {
-    const userMoving = [];
+    let termination = false;
+    let tryCount = 1;
 
-    for (let round = 0; round < this.#bridgeSize; round++) {
-      this.move(userMoving, round);
+    while (!termination) {
+      termination = this.playGame(userMoving);
+      termination = !this.retry();
+      tryCount++;
     }
+
+    OutputView.printResult();
+  }
+
+  playGame() {
+    const userMoving = []; // TODO: 어떤걸 move로 쓰고 어떤 걸 moving으로 쓸지 정해야 함
+    for (let round = 0; round < this.#bridgeSize; round++) {
+      const isCorrectMoving = this.move(userMoving, round);
+      if (!isCorrectMoving) {
+        return false;
+      }
+    }
+
+    return true;
   }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move(userMoving, round) {
+  move(userMoving) {
     const moving = InputView.readMoving();
+    const isCorrectMoving = this.checkMoving(userMoving);
     userMoving.push(moving);
     OutputView.printMap(this.#bridgeStyle, userMoving);
 
-    if (userMoving[round] !== this.#bridgeStyle[round]) {
-      this.retry();
-    }
+    return isCorrectMoving;
   }
 
-  
+  checkMoving(userMoving) {
+    const currRound = userMoving.length - 1;
+    
+    if (this.#bridgeStyle[currRound] !== userMoving[currRound]) {
+      return false;
+    }
+
+    return true;
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
@@ -57,7 +81,9 @@ class BridgeGame {
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   retry() {
+    const isRetry = InputView.readGameCommand();
 
+    return isRetry === "R" ? true : false;
   }
 }
 
--- src/InputView.js
+++ src/InputView.js
@@ -57,7 +57,17 @@ const InputView = {
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+    let restartCommand = '';
+    
+    MissionUtils.Console.readLine(
+      "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n >",
+      answer => restartCommand = answer
+    )
+
+    return restartCommand;
+  },
+
 };
 
 module.exports = InputView;
