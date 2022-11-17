de4aaf41ecb16b92b0dbfc1b20289b7dccf112dd
unsnruu <unsnruu@gmail.com>
2022-11-18 03:26:37 +0900

    refactor: 분리된 메서드를 기존 메서드 내부에서 함수로 처리
    validation과 관련된 메서드를 분리하여 메서드로 만들었지만,
    해당 메서드가 InputView와 성격이 달라,
    기존의 InputView 메서드의 내부에서 함수로서 처리하였습니다.

--- src/InputView.js
+++ src/InputView.js
@@ -21,31 +21,24 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  validateMoveCommand(inputs) {
-    return Validation.validateCommand(inputs, ["U", "D"]);
-  },
   readMoving(current, next) {
     const message = "이동할 칸을 선택해주세요. (위: U, 아래: D)\n";
-    Console.readLine(
-      message,
-      withError(this.validateMoveCommand, current, next)
-    );
+    const validateMoveCommand = (inputs) =>
+      Validation.validateCommand(inputs, ["U", "D"]);
+
+    Console.readLine(message, withError(validateMoveCommand, current, next));
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  validateEndCommand(inputs) {
-    return Validation.validateCommand(inputs, ["R", "Q"]);
-  },
   readGameCommand(current, next) {
     const message =
       "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n";
-    Console.readLine(
-      this.validateEndCommand,
-      message,
-      withError(current, next)
-    );
+    const validateEndCommand = (inputs) =>
+      Validation.validateCommand(inputs, ["R", "Q"]);
+
+    Console.readLine(validateEndCommand, message, withError(current, next));
   },
 };
 
