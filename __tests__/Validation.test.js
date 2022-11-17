8d7b20e8c6ccec3436a8a39ce2e9facf304e6897
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-11-17 15:24:38 +0900

    test(Validation): 범위 파악 함수 테스트
    지정된 범위를 벗어나면 예외를 발생시킨다.

--- __tests__/Validation.test.js
+++ __tests__/Validation.test.js
@@ -1,8 +1,9 @@
 const Validation = require('../src/Validation');
 
-describe('숫자 예외 파악 함수 테스트', () => {
-  const NUMBER_ERROR_TEXT = '[ERROR] 전달된 인수는 숫자 타입이 아닙니다.';
+const NUMBER_ERROR_TEXT = '[ERROR] 전달된 인수는 숫자 타입이 아닙니다.';
+const RANGE_ERROR_TEXT = '[ERROR] 지정한 범위의 값이 아닙니다.';
 
+describe('숫자 예외 파악 함수 테스트', () => {
   test('메소드 이름은 "number"로 정의된다.', () => {
     const METHOD_NAME = 'number';
 
@@ -19,9 +20,21 @@ describe('숫자 예외 파악 함수 테스트', () => {
 });
 
 describe('범위 파악 함수 테스트', () => {
+  const START = 3;
+  const END = 20;
+  const checkRange = Validation.range(START, END);
+
   test('메소드 이름은 "range"로 정의된다.', () => {
     const METHOD_NAME = 'range';
 
     expect(Validation.range.name).toEqual(METHOD_NAME);
+  });
+
+  test('지정된 범위를 벗어나면 예외를 발생시킨다.', () => {
+    const EXPECTED = 2;
+
+    expect(() => {
+      checkRange(EXPECTED);
+    }).toThrow(RANGE_ERROR_TEXT);
   });
 });
