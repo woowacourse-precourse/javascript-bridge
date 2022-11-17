caccedec9b97078dd268d5d034f828973ea96c5b
dladncks1217 <dlaxodud1217@gmail.com>
2022-11-17 15:06:33 +0900

    feat(InputView): 입력 관련 동작 메서드들 추가

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,6 @@
+const { Console } = require('@woowacourse/mission-utils');
+const GAME_MESSAGE = require('./constants/constants');
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,17 +8,29 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine(GAME_MESSAGE.SET_BRIDGE_LENGTH, (userInput) => {
+      return Number(userInput);
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    Console.readLine(GAME_MESSAGE.MOVE_CHOICE, (userInput) => {
+      return userInput;
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+    Console.readLine(GAME_MESSAGE.GAME_RETRY, (userInput) => {
+      return userInput;
+    });
+  },
 };
 
 module.exports = InputView;
