b992c354f885f13e2e1b8efbe2c673ee00f28d27
saejinpark <saejinpark951210@gmail.com>
2022-11-17 19:16:32 +0900

    일일커밋

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const wConsole = MissionUtils.Console;
+
+
+const { readBridgeSize } = require("./InputView");
+
 class App {
-  play() {}
+  constructor() {
+    this.bridge = null;
+    this.moveLog = "";
+  }
+
+  getBridge() {
+    return this.bridge;
+  }
+
+  setBridge(bridge) {
+    this.bridge = bridge;
+  }
+
+  play() {
+    wConsole.print("다리 건너기 게임을 시작합니다.\n");
+    readBridgeSize(this);
+  }
 }
 
 module.exports = App;
+
+const app = new App();
+app.play();
--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,9 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+const BridgeMaker = require("./BridgeMaker");
+const wConsole = MissionUtils.Console;
+const { makeBridge } = BridgeMaker;
+const { generate } = BridgeRandomNumberGenerator;
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,7 +11,19 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize(app) {
+    wConsole.readLine("다리의 길이를 입력해주세요.", (line) => {
+      if (!/^\d+$/.test(line))
+        throw new Error("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+      const size = parseInt(line);
+      if (size < 3 || 20 < size)
+        throw new Error("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+
+      app.setBridge(makeBridge(size, generate));
+      wConsole.readLine();
+      console.log(app.getBridge());
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
