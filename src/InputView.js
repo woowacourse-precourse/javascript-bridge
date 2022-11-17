b01dd6897ddbbaeb5b5d68353417c53e275b5852
YMS <yms1789@naver.com>
2022-11-17 22:43:33 +0900

    Feat: 다리 길이 입력 기능 구현

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,7 @@
+const OutputView = require("./OutputView");
+const Console = require("@woowacourse/mission-utils");
+const { MESSAGE } = require("./constants");
+const { OutputView } = require("./constants");
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,12 +9,19 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+
+  readBridgeSize(bridgeLength) {
+    // 입력받고 난 다음 할 내용들 다 입출력하고
+    const outputView = new OutputView();
+    Console.readLine(MESSAGE.INPUT_BRIDGE_LENGTH_MESSAGE, (bridgeLength) => {
+      this.readMoving(bridgeLength);
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving(bridgeLength) {},
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
