d22f207125b3ebc5abc5ef14e427711e85323f9c
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-11-18 00:23:15 +0900

    feat(App): makeBridge 함수 적용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 const InputView = require("./InputView.js")
 const Validate = require("./Validate.js")
+const BridgeGame = require("./BridgeGame.js")
 class App {
+  constructor(){
+    this.game = new BridgeGame()
+  }
   play() {
     InputView.readBridgeSize(this.makeBridge.bind(this))
   }
   makeBridge(Length){
     Validate.BridgeLengthInput(Length)
+    this.game.makeBridge(Length)
   }
 }
 const app = new App();
