db66c58ffc329eeae6ea57d434caee24ccec8df8
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-17 19:18:53 +0900

    feat: Builder클래스 생성 및 다리 생성 관련 메서드 이동

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,43 +1,17 @@
-const { makeBridge } = require('./BridgeMaker.js');
-const { generate } = require('./BridgeRandomNumberGenerator.js');
-const { ERROR_MSG } = require('./constants/message.js');
-const { BRIDGE_SIZE_MIN_RANGE, BRIDGE_SIZE_MAX_RANGE } = require('./constants/condition.js');
+const Bulider = require('./Builder.js');
 
 class BridgeGame {
   #bridge;
 
   buildBridge(size) {
-    this.validateSize(size);
+    const builder = new Bulider();
+
+    builder.buildBridge(size);
   }
 
   move() {}
 
   retry() {}
-
-  validateSize(size) {
-    if (!size) throw new Error(ERROR_MSG.emptyInput);
-
-    if (!this.hasOnlyNumber(size)) throw new Error(ERROR_MSG.invalidInputType);
-
-    if (this.isStartedZero(size)) throw new Error(ERROR_MSG.isStartedZero);
-
-    if (!this.isValideBridgeSizeRange(size)) throw new Error(ERROR_MSG.invalidInputRange);
-  }
-
-  hasOnlyNumber(size) {
-    return size
-      .split('')
-      .map((eachLetter) => parseInt(eachLetter, 10))
-      .every((number) => Number.isInteger(number));
-  }
-
-  isStartedZero(size) {
-    return size.startsWith('0');
-  }
-
-  isValideBridgeSizeRange(size) {
-    return BRIDGE_SIZE_MIN_RANGE <= size && size <= BRIDGE_SIZE_MAX_RANGE;
-  }
 }
 
 module.exports = BridgeGame;
---
+++ src/Builder.js
@@ -0,0 +1,35 @@
+const { BRIDGE_SIZE_MIN_RANGE, BRIDGE_SIZE_MAX_RANGE } = require('./constants/condition.js');
+const { ERROR_MSG } = require('./constants/message.js');
+
+class Bulider {
+  buildBridge(size) {
+    this.validateSize(size);
+  }
+
+  validateSize(size) {
+    if (!size) throw new Error(ERROR_MSG.emptyInput);
+
+    if (!this.hasOnlyNumber(size)) throw new Error(ERROR_MSG.invalidInputType);
+
+    if (this.isStartedZero(size)) throw new Error(ERROR_MSG.isStartedZero);
+
+    if (!this.isValideBridgeSizeRange(size)) throw new Error(ERROR_MSG.invalidInputRange);
+  }
+
+  hasOnlyNumber(size) {
+    return size
+      .split('')
+      .map((eachLetter) => parseInt(eachLetter, 10))
+      .every((number) => Number.isInteger(number));
+  }
+
+  isStartedZero(size) {
+    return size.startsWith('0');
+  }
+
+  isValideBridgeSizeRange(size) {
+    return BRIDGE_SIZE_MIN_RANGE <= size && size <= BRIDGE_SIZE_MAX_RANGE;
+  }
+}
+
+module.exports = Bulider;
