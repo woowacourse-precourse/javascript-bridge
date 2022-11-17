4733dede1773680429443450c02e5754a3f3e52d
Chanwoong1 <jcw7468@gmail.com>
2022-11-17 23:27:37 +0900

    feat: 사용자에게 다리 길이 입력 받는 기능 구현

--- src/InputView.js
+++ src/InputView.js
@@ -1,21 +1,35 @@
+const { Console } = require("@woowacourse/mission-utils");
+const BridgeMaker = require("./BridgeMaker");
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+const ValidateBridgeSize = require("./ValidateBridgeSize");
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
+  bridge: null,
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine("다리의 길이를 입력해주세요.\n", (bridgeSize) => {
+      const validateBridgeSize = new ValidateBridgeSize(bridgeSize);
+      this.bridge = BridgeMaker
+        .makeBridge(validateBridgeSize.bridgeSize, BridgeRandomNumberGenerator);
+      this.readMoving();
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() { },
 };
 
 module.exports = InputView;
