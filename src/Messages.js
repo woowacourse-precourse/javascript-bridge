d31626004490722706b4bef6f26ee5cf4c086dcd
Narcoker <jykim9868@gmail.com>
2022-11-17 20:51:51 +0900

    Feat: ENTER_COMMAND 메세지 문구 추가

--- src/Messages.js
+++ src/Messages.js
@@ -1,7 +1,8 @@
 const MESSAGE = Object.freeze({
     START: "다리 건너기 게임을 시작합니다.\n",
     ENTER_BRIDGE_SIZE: "다리의 길이를 입력해주세요.\n",
-    ENTER_MOVE_TYPE: "이동할 칸을 선택해주세요. (위: U, 아래: D)\n"
+    ENTER_MOVE_TYPE: "이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
+    ENTER_COMMAND: "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n"
 });
 
 const ERROR = Object.freeze({
