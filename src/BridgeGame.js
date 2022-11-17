5103c4e6c3b06874d6d082787dc4c0690e4bc95b
dlwl98 <ff981113@hanyang.ac.kr>
2022-11-17 15:21:32 +0900

    feat(BridgeGame): map 만드는 로직 옮기기

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -3,11 +3,36 @@
  */
 class BridgeGame {
   /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+   * @param {string[]} bridge // 다리
+   * @param {string[]} inputs // 플레이어의 입력
+   * @return {string} // 출력할 map의 윗부분
    */
-  move() {}
+  makeUpperPart(bridge, inputs) {
+    const upperPart = inputs.map((e, i) => {
+      if (e !== 'U') return ' ';
+      if (e === bridge[i]) return 'O';
+      this.#isGameOver = true;
+      return 'X';
+    });
+
+    return upperPart.join(' | ');
+  }
+
+  /**
+   * @param {string[]} bridge // 다리
+   * @param {string[]} inputs // 플레이어의 입력
+   * @return {string} // 출력할 map의 아랫부분
+   */
+  makeLowerPart(bridge, inputs) {
+    const lowerPart = inputs.map((e, i) => {
+      if (e !== 'D') return ' ';
+      if (e === bridge[i]) return 'O';
+      this.#isGameOver = true;
+      return 'X';
+    });
+
+    return lowerPart.join(' | ');
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
