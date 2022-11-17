7b2636bb2c52fc33ea0cf0d4412f36174584965c
Dahye <06robin11@gmail.com>
2022-11-17 15:42:11 +0900

    fix(outputview): 그림에 O,X 출력안되는 버그 고침

--- src/OutputView.js
+++ src/OutputView.js
@@ -13,8 +13,8 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printMap(map) {
-    const upBridge = this.drawBridge(map, LETTER.up);
-    const downBridge = this.drawBridge(map, LETTER.down);
+    const upBridge = this.drawBridge(map, "up");
+    const downBridge = this.drawBridge(map, "down");
     Console.print(upBridge);
     Console.print(downBridge);
   },
@@ -23,10 +23,7 @@ const OutputView = {
     return `[ ${map
       .map((each) => {
         const [step, result] = each;
-        if (step === LETTER[UorD]) {
-          return result;
-        }
-        return " ";
+        return step === LETTER[UorD] ? result : " ";
       })
       .join(" | ")} ]`;
   },
