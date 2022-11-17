dea5b82a27aaec51c1189e3b256fbe214b73e8f6
hwanfront <kim015jh@gmail.com>
2022-11-17 17:56:47 +0900

    feat(OutputView): 출력을 위한 메서드 추가

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,9 +1,27 @@
+const { Console } = require('@woowacourse/mission-utils');
+
 const OutputView = {
-  printGameStart() {},
+  message(type) {
+    return {
+      GAME_START: '다리 건너기 게임을 시작합니다.\n',
+      RESULT: '최종 게임 결과\n',
+    }[type] ?? new Error('[ERROR] 존재하지 않는 타입입니다.');
+  },
 
-  printMap() {},
+  printGameStart() {
+    Console.print(this.message('GAME_START'));
+  },
 
-  printResult() {},
+  printMap(crossingBridge) {
+    Console.print(crossingBridge);
+  },
+
+  printResult({ crossingBridge, attempt, result }) {
+    Console.print(this.message('RESULT'));
+    this.printMap(crossingBridge);
+    Console.prin(result);
+    Console.prin(attempt.print());
+  },
 };
 
 module.exports = OutputView;
