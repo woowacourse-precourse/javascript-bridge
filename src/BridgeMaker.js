dca1981a3ca094efaeac0d06e80e5fcdcef5f9e7
MAGHC <windowsapple95@gmail.com>
2022-11-17 19:09:19 +0900

    feat: BridgeMaker.makeBridge 기능 구현

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,3 +1,5 @@
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
+const { BRINDGE_DIRECTION } = require('./Constant');
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
@@ -7,7 +9,19 @@ const BridgeMaker = Object.freeze({
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
-  makeBridge(size, generateRandomNumber) {},
+
+  makeBridge(size, generateRandomNumber = BridgeRandomNumberGenerator.generate) {
+    const bridge = Array(size)
+      .fill(null)
+      .map(() => {
+        const number = generateRandomNumber();
+        if (number === 0) return BRINDGE_DIRECTION.DOWN;
+        return BRINDGE_DIRECTION.UP;
+      });
+    console.log(bridge);
+  },
 });
 
 module.exports = BridgeMaker;
+
+console.log(BridgeMaker.makeBridge(20));
