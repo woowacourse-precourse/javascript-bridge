b7a28f91ce7db9b515b916c20104c832c20fd143
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-11-18 01:51:34 +0900

    test(BridgeGame): 테스트 케이스 일부 수정

--- __tests__/BridgeGame.test.js
+++ __tests__/BridgeGame.test.js
@@ -139,8 +139,7 @@ describe('사용자가 게임을 다시 시도할 때 사용하는 메서드 테
 
     bridgeGame.move();
     bridgeGame.move();
-    bridgeGame.retry();
 
-    expect(bridgeGame.findUserPosition()).toEqual(RECEIVED);
+    expect(bridgeGame.retry()).toEqual(RECEIVED);
   });
 });
