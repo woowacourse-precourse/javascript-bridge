7eacc436dcbd288e2ae578b32c419108b545093e
Mcdonalds Trump <whdmschd2@gmail.com>
2022-11-18 05:14:02 +0900

    feat: 출력 줄바꿈

--- src/utils/constans.js
+++ src/utils/constans.js
@@ -16,17 +16,17 @@ const BRIDGE = {
 };
 
 const INPUT_MESSAGE = {
-  BRIDGE_SIZE: "다리의 길이를 입력해주세요.",
-  MOVING_COMMAND: "이동할 칸을 선택해주세요. (위: U, 아래: D)",
+  BRIDGE_SIZE: "다리의 길이를 입력해주세요.\n",
+  MOVING_COMMAND: "\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
   GAME_COMMAND:
-    "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
+    "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
 };
 
 const OUTPUT_MESSAGE = {
-  GAME_START: "다리 건너기 게임을 시작합니다.",
-  GAME_END: "최종 게임 결과",
+  GAME_START: "다리 건너기 게임을 시작합니다.\n",
+  GAME_END: "\n최종 게임 결과",
   GAME_SUCCESS_STATUS: (isGameSuccess) =>
-    `게임 성공 여부: ${isGameSuccess ? "성공" : "실패"}`,
+    `\n게임 성공 여부: ${isGameSuccess ? "성공" : "실패"}`,
   GAME_TRY_COUNT: (tryCount) => `총 시도한 횟수: ${tryCount}`,
   BRIDGE_MAP: (bridgeMap) =>
     `${BRIDGE.MAP.START}${bridgeMap.join(BRIDGE.MAP.DIVIDING_LINE)}${
