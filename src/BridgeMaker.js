01a48a9648878f4d6f4bd144733f5645827c12eb
hwanfront <kim015jh@gmail.com>
2022-11-17 13:52:12 +0900

    feat(BridgeMaker): 랜덤 수를 통해 방향을 생성하는 메서드 추가

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,5 +1,9 @@
+const BridgeDirection = require('./domain/bridge/BridgeDirection');
+
 const BridgeMaker = {
-  makeBridge(size, generateRandomNumber) {},
+  makeBridge(size, generateRandomNumber) {
+    return Array.from({ length: size }, () => BridgeDirection.generate(generateRandomNumber));
+  },
 };
 
 module.exports = BridgeMaker;
