d341d66a6433919bef9d6c0abd5f8a0dcc0a6930
jeonbyeongmin <qudals7613@gmail.com>
2022-11-18 00:08:36 +0900

    feat: 게임 종료 기능 추가

--- src/utils/message.js
+++ src/utils/message.js
@@ -9,10 +9,9 @@ const INPUT_MESSAGE = {
 const OUTPUT_MESSAGE = {
   START_GAME: '다리 건너기 게임을 시작합니다.',
   RESULT: '최종 게임 결과',
-  SUCCESS_OR_FAILURE: '게임 성공 여부: ',
-  SUCCESS: '성공',
-  FAILURE: '실패',
-  NUMBER_OF_TRY: '총 시도한 횟수: ',
+  SUCCESS: '게임 성공 여부: 성공',
+  FAILURE: '게임 성공 여부: 실패',
+  NUMBER_OF_TRY: '총 시도한 횟수',
 };
 
 const ERROR_MESSAGE = {
