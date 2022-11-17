121128648e6028515ac747d680f4059669b698c9
Sijun <sionwer4@naver.com>
2022-11-17 21:16:11 +0900

    feat: input read game command add

--- src/InputView.js
+++ src/InputView.js
@@ -24,7 +24,11 @@ const InputView = {
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+    MissionUtils.Console.readLine("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)", (answer) => {
+      return answer;
+    });
+  },
 };
 
 module.exports = InputView;
