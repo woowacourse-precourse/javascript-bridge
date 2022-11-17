4a1b04e87c360e9ffab141d0a7b6158223f604aa
AnJaeSeong <dktmzh6@gmail.com>
2022-11-17 23:50:03 +0900

    feat(BridgeGameController): terminate()

--- src/BridgeGameController.js
+++ src/BridgeGameController.js
@@ -7,20 +7,32 @@ const OutputView = require("./OutputView");
  * 다리 건너기 게임 진행 로직 함수
  */
 class BridgeGameController {
+  static #SUCCESS_ARRIVAL = 2;
+  static #SUCCESS_ROUND = 1;
+  static #FAILURE_ROUND = 0;
+  static #QUIT = 0;
+  static #RETRY = 1;
+
   static makeNewBridgeGame(input) {
     return new BridgeGame(BridgeMaker.makeBridge(input, BridgeRandomNumberGenerator.generate));
   }
-
-  SUCCESS_ARRIVAL = 2;
-  SUCCESS_ROUND = 1;
-  FAILURE_ROUND = 0;
+  
   static startNewRound(bridgeGame, input) {
     const roundResult = bridgeGame.move(input);
     OutputView.printMap(bridgeGame.stateToString());
-    if(!roundResult) return this.FAILURE_ROUND;
-    else if (!bridgeGame.isArrived()) return this.SUCCESS_ROUND;
+    if(!roundResult) return this.#FAILURE_ROUND;
+    else if (!bridgeGame.isArrived()) return this.#SUCCESS_ROUND;
     OutputView.printResult(bridgeGame.stateToString(), bridgeGame.isArrived(), bridgeGame.getTry());
-    return this.SUCCESS_ARRIVAL;  
+    return this.#SUCCESS_ARRIVAL;  
+  }
+
+  static terminate(bridgeGame, input) {
+    if(input == 'R') {
+      bridgeGame.retry();
+      return this.#RETRY;
+    } 
+    OutputView.printResult(bridgeGame.stateToString(), bridgeGame.isArrived(), bridgeGame.getTry());
+    return this.#QUIT;
   }
 }
 
