f2c1ace653370149eae52eeef096214714808289
hwanfront <kim015jh@gmail.com>
2022-11-17 13:28:09 +0900

    feat(BridgeDirection): 랜덤 수를 통해 방향을 생성하는 메서드 추가

--- src/domain/bridge/BridgeDirection.js
+++ src/domain/bridge/BridgeDirection.js
@@ -1,10 +1,24 @@
-class BridgeDirection {
-  static validate() {
+const BridgeRandomNumberGenerator = require('../../BridgeRandomNumberGenerator');
 
+class BridgeDirection {
+  static #DIRECTION = Object.freeze({
+    [BridgeRandomNumberGenerator.RANDOM_LOWER_INCLUSIVE]: 'D',
+    [BridgeRandomNumberGenerator.RANDOM_UPPER_INCLUSIVE]: 'U',
+  });
+
+  static validate(number) {
+    if (number < BridgeRandomNumberGenerator.RANDOM_LOWER_INCLUSIVE) {
+      throw new Error('[ERROR] 랜덤 숫자가 범위에 포함되지 않습니다.');
+    }
+    if (number > BridgeRandomNumberGenerator.RANDOM_UPPER_INCLUSIVE) {
+      throw new Error('[ERROR] 랜덤 숫자가 범위에 포함되지 않습니다.');
+    }
   }
 
-  generate() {
-
+  static generate(generateRandomNumber) {
+    const number = generateRandomNumber();
+    BridgeDirection.validate(number);
+    return BridgeDirection.#DIRECTION[number];
   }
 }
 
