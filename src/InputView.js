bdb659dedc259ea20996f1e086371995c1149aef
Dahye <06robin11@gmail.com>
2022-11-17 13:51:29 +0900

    feat: 이동할 칸 입력+벨리데이트, 게임재시도 입력+벨리데이션

--- src/InputView.js
+++ src/InputView.js
@@ -20,12 +20,20 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    Console.readLine(INPUT_MESSAGE.chooseUpOrDown, (letter) => {
+      Validation.checkUorD(letter);
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+    Console.readLine(INPUT_MESSAGE.chooseToRetry, (letter) => {
+      Validation.checkRorQ(letter);
+    });
+  },
 };
 
 module.exports = InputView;
