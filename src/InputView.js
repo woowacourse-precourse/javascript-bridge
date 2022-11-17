6a70c1c112fa281e714a9e9feea5a77b968bbce5
devMingu <rooster100@naver.com>
2022-11-17 17:11:56 +0900

    refactor: 읽기 쉽도록 Console을 받아오는 방식 변경

--- src/InputView.js
+++ src/InputView.js
@@ -1,4 +1,4 @@
-const MissionUtils = require("@woowacourse/mission-utils");
+const {Console} = require("@woowacourse/mission-utils");
 const Message = require("./Message");
 
 /**
@@ -9,21 +9,23 @@ const InputView = {
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize() {
-    MissionUtils.Console.readLine(`${Message.INPUT_MESSAGE.BRIDGE_SIZE_TEXT}`, (size) => {})
+    Console.readLine(`${Message.INPUT_MESSAGE.BRIDGE_SIZE_TEXT}`, (size) => {
+
+    })
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving() {
-    MissionUtils.Console.readLine(`${Message.INPUT_MESSAGE.MOVING_TEXT}`, (move) => {})
+    Console.readLine(`${Message.INPUT_MESSAGE.MOVING_TEXT}`, (move) => {})
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand() {
-    MissionUtils.Console.readLine(`${Message.INPUT_MESSAGE.READ_GAME_COMMAND_TEXT}`, (command) => {})
+    Console.readLine(`${Message.INPUT_MESSAGE.READ_GAME_COMMAND_TEXT}`, (command) => {})
   },
 };
 module.exports = InputView;
