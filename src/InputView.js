8b7686142475f1bad3d9270b7df2f7157f8ff82a
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-11-18 01:43:45 +0900

    Repactor : InputView

--- src/InputView.js
+++ src/InputView.js
@@ -1,4 +1,6 @@
 const { Console } = require("@woowacourse/mission-utils");
+const { curry, errorHandler } = require("./libs");
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -10,25 +12,33 @@ const InputView = {
   BRIDGE_MOVE_QUESTION: "이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
   RESTART_QUESTION:
     "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n",
-
   /**
    ** 사용자가 다리 길이를 입력받는다.
    */
-  readBridgeSize(getBrigeSize) {
-    Console.readLine(InputView.BRIDGE_SIZE_QUESTION, getBrigeSize);
+  readBridgeSize(getBridgeSize) {
+    const errorHandledGetBrdigeSize = curry(errorHandler)(
+      InputView.readBridgeSize,
+      getBridgeSize
+    );
+    Console.readLine(InputView.BRIDGE_SIZE_QUESTION, errorHandledGetBrdigeSize);
   },
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving(move) {
-    Console.readLine(InputView.BRIDGE_MOVE_QUESTION, move);
+    const errorrHandledMove = curry(errorHandler)(InputView.readMoving, move);
+    Console.readLine(InputView.BRIDGE_MOVE_QUESTION, errorrHandledMove);
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand(retry) {
-    Console.readLine(InputView.RESTART_QUESTION, retry);
+    const errorHandledRetry = curry(errorHandler)(
+      InputView.readGameCommand,
+      retry
+    );
+    Console.readLine(InputView.RESTART_QUESTION, errorHandledRetry);
   },
 };
 
