b6773feb586dadb690dbcfabf1cb9b4ecfb15466
pakxe <pigkill40@naver.com>
2022-11-17 20:23:41 +0900

    chore(BridgeMaker): 프로퍼티명 수정

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,18 +1,11 @@
-const { BRIDGE } = require('./Constants');
-/**
- * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
- */
+const { BRIDGE } = require('./constants/Constants');
+
 const BridgeMaker = {
-  /**
-   * @param {number} size 다리의 길이
-   * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
-   * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
-   */
   makeBridge(size, generateRandomNumber) {
     const bridge = [];
     for (let i = 0; i < size; i++) {
       const step = Number(generateRandomNumber());
-      step ? bridge.push(BRIDGE.UP) : bridge.push(BRIDGE.DOWN);
+      step ? bridge.push(BRIDGE.up) : bridge.push(BRIDGE.down);
     }
     return bridge;
   },
