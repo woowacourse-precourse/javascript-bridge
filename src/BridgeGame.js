bd1e47e8e8bf0f992326f2fb5357ecf84d96b555
An Sol <apdlvmf486@gmail.com>
2022-11-17 16:05:28 +0900

    feat: 게임 종료 및 재시작 기능 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -12,7 +12,7 @@ class BridgeGame {
   #upperBridge = [];
   #lowerBridge = [];
   #gameRound = 0;
-  totalTrialNumber = 0;
+  numberOfTrials = 1;
 
   constructor(size) {
     this.#bridgeSize = size;
@@ -28,24 +28,30 @@ class BridgeGame {
     // 상수화 필요
     const COMMAND = await ViewController.input.moving();
     if (COMMAND === this.#bridge[this.#gameRound]) {
-      this.moveCorrectDirection(COMMAND);
+      this.handleCorrectMove(COMMAND);
     } else if (COMMAND !== this.#bridge[this.#gameRound]) {
-      this.moveWrongDirection(COMMAND);
+      this.handleWrongMove(COMMAND);
     }
   }
-
-  moveCorrectDirection(command) {
+  handleCorrectMove(command) {
+    //상수화 필요
     this.buildBridgeUsingInput(command, "O");
     ViewController.output.map(this.#upperBridge, this.#lowerBridge);
     this.#gameRound += 1;
+    if (this.#gameRound === this.#bridgeSize) {
+      ViewController.output.result(this.numberOfTrials, "성공", [this.#upperBridge, this.#lowerBridge]);
+      MissionUtils.Console.close();
+    }
     if (this.#gameRound < this.#bridgeSize) this.move();
   }
-
-  moveWrongDirection(command) {
+  handleWrongMove(command) {
     this.buildBridgeUsingInput(command, "X");
     ViewController.output.map(this.#upperBridge, this.#lowerBridge);
+    this.#upperBridge = [];
+    this.#lowerBridge = [];
+    this.#gameRound = 0;
+    this.retry();
   }
-
   buildBridgeUsingInput(direction, result) {
     // 상수화 필요
     if (direction === "U") {
@@ -63,7 +69,16 @@ class BridgeGame {
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  retry() {}
+  async retry() {
+    const COMMAND = await ViewController.input.gameCommand();
+    if (COMMAND === "R") {
+      this.numberOfTrials += 1;
+      this.move();
+    } else if (COMMAND === "Q") {
+      ViewController.output.result(this.numberOfTrials, "실패", [this.#upperBridge, this.#lowerBridge]);
+      MissionUtils.Console.close();
+    }
+  }
 }
 
 module.exports = BridgeGame;
--- src/InputView.js
+++ src/InputView.js
@@ -29,7 +29,13 @@ const InputView = {
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+    return new Promise((resolve, reject) => {
+      MissionUtils.Console.readLine("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n", (userInput) => {
+        resolve(returnGameCommand(userInput));
+      });
+    });
+  },
 };
 
 module.exports = InputView;
