1d882ef833a9f6ebe35fd0ec080338f03ed7c88c
Hoon <si932174@gmail.com>
2022-11-17 15:25:15 +0900

    feat : InputView : 다리길이 입력

--- src/InputView.js
+++ src/InputView.js
@@ -3,11 +3,14 @@ const { Console } = require('@woowacourse/mission-utils');
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
-  #bridgeSize
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine('다리의 길이를 입력해주세요.\n', bridgeSize => {
+      Console.print(bridgeSize);
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
