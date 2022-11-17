93dbad85d7e92f1ba8671fa39085d5afa3f032c9
Mcdonalds Trump <whdmschd2@gmail.com>
2022-11-18 01:06:54 +0900

    test: 사용자가 올바르게 이동했을 때 현재까지 건넌 다리 지도

---
+++ __tests__/BridgeGameTest.js
@@ -0,0 +1,23 @@
+const BridgeGame = require("../src/BridgeGame");
+
+describe("다리 건너기 게임을 관리하는 클래스 테스트", () => {
+  test("사용자가 올바르게 이동했을 때 현재까지 건넌 다리 지도", () => {
+    const playerChoiceMovingCommands = ["U", "D", "D", "D"];
+    const upsideBridgeMap = ["O", " ", " ", " "];
+    const downSideBridgeMap = [" ", "O", "O", "O"];
+
+    const bridgeGame = new BridgeGame();
+    bridgeGame.setBridgeShape(["U", "D", "D", "D", "U"]);
+
+    playerChoiceMovingCommands.forEach((playerChoiceMovingCommand) => {
+      bridgeGame.move(playerChoiceMovingCommand);
+    });
+
+    expect(bridgeGame.getCurrentBridgeMap().upsideBridgeMap).toEqual(
+      upsideBridgeMap
+    );
+    expect(bridgeGame.getCurrentBridgeMap().downsideBridgeMap).toEqual(
+      downSideBridgeMap
+    );
+  });
+});
