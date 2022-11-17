52959755073f87ef744f7269cff50ea66a84b82e
DongjoonShin <ehdwns1516@ajou.ac.kr>
2022-11-17 16:20:05 +0900

    feat: 플레이어가 게임을 종료할지 다시 시도할지 입력받는 기능 구현

--- src/InputView.js
+++ src/InputView.js
@@ -28,7 +28,13 @@ const InputView = {
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+    return new Promise((resolve) => {
+      Console.readLine('\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n', (command) => {
+        resolve(command);
+      });
+    });
+  },
 };
 
 module.exports = InputView;
