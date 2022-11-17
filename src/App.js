0b76516342ed96106cb02fd0a72710ebb1abedf7
olo90632951 <olo90632951@gmail.com>
2022-11-17 11:35:36 +0900

    feat: 게임을 재시작할 수 있는 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,24 +3,46 @@ const InputView = require('./view/InputView');
 const OutputView = require('./view/OutputView');
 
 class App {
+  #bridgeGame;
+
   play() {
     OutputView.printStart();
     this.inputBridgeSize();
   }
 
+  makeGame(length) {
+    this.#bridgeGame = new BridgeGame(Number(length));
+  }
+
   inputBridgeSize() {
     InputView.readBridgeSize((length) => {
-      this.bridgeGame = new BridgeGame(Number(length));
-      this.inputReadMoving();
+      this.makeGame(length);
+      this.inputMoving();
     });
   }
 
-  inputReadMoving() {
+  inputMoving() {
     InputView.readMoving((direction) => {
-      if (this.bridgeGame.move(direction)) {
-        this.inputReadMoving();
+      if (this.#bridgeGame.move(direction)) {
+        this.inputMoving();
+      }
+      this.inputRetry();
+    });
+  }
+
+  inputRetry() {
+    InputView.readGameCommand((command) => {
+      if (command === 'R') {
+        this.restart();
+      } else if (command === 'Q') {
+        console.log('종료');
       }
     });
+  }
+
+  restart() {
+    this.#bridgeGame.retry();
+    this.inputMoving();
   }
 }
 
