ee8835e45dd1c2674fcb86c1822ce37b0d84e098
su-lim <sujin.0xlim@gmail.com>
2022-11-18 02:05:18 +0900

    feat(printMap): 현재까지 이동한 다리상태 출력

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,3 +1,5 @@
+const { MissionUtils } = require("@woowacourse/mission-utils");
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -7,7 +9,20 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {},
+  reverse(direction) {
+    return direction === "U" ? "D" : "U";
+  },
+
+  printMap(bridgeMap, location, correct) {
+    const map = { U: "[", D: "[" };
+    for (let i = 0; i < location - 1; i++) {
+      map[bridgeMap[i]] += " O |";
+      map[reverse(bridgeMap[i])] += "   |";
+    }
+    map[bridgeMap[location - 1]] += correct ? " O ]" : "   ]";
+    map[reverse(bridgeMap[location - 1])] += correct ? "   ]" : " X ]";
+    MissionUtils.Console.print(map["U"] + "\n" + map["D"]);
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
