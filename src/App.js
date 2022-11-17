94feea05585c48bed352b9e0f6c309e542ab1484
hae-on <solbi2004@naver.com>
2022-11-17 20:57:38 +0900

    feat: 게임 시작 준비

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+const { Console } = require('@woowacourse/mission-utils');
+const { START_GAME } = require('./Constants');
+
 class App {
-  play() {}
+  play() {
+    Console.print(START_GAME);
+  }
 }
 
+const app = new App();
+app.play();
 module.exports = App;
