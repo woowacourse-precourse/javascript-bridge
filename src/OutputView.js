76f61b56af77d55d2bc61398eec1152fe89fc888
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2022-11-17 19:30:54 +0900

    feat: 입력받은 값과 랜덤배열의 첫값을 비교한 후 일치하면 [ O ] 출력해주는 기능 구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,3 +1,5 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -7,7 +9,19 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {},
+  printMap(userAnswer) {
+    const UP = [];
+    const DOWN = [];
+    if (userAnswer === 'U') {
+      UP.push(` O `);
+      DOWN.push(`   `);
+    }
+    if (userAnswer === 'D') {
+      UP.push(`   `);
+      DOWN.push(` O `);
+    }
+    MissionUtils.Console.print(`[${UP}]\n[${DOWN}]`);
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
