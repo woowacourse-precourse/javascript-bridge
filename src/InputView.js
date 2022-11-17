7a1df19a546687676bef45cfad06ce88b2dec0b2
unsnruu <unsnruu@gmail.com>
2022-11-17 20:26:52 +0900

    fix: readGameCommand에 next를 인자로 설정

--- src/InputView.js
+++ src/InputView.js
@@ -34,7 +34,7 @@ const InputView = {
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {
+  readGameCommand(next) {
     const message =
       "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
     Console.readLine(message, (command) => next(command));
