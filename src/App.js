8a3b86706e9ff65be953892ac5a67c1a237ecfbb
yeonsuBeak <soothbugger@gmail.com>
2022-11-17 14:48:53 +0900

    feat(bridge): 게임 시작 메세지 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const { Console } = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    this.printStart();
+  }
+
+  printStart() {
+    Console.print("다리 건너기 게임을 시작합니다.\n");
+  }
 }
 
 module.exports = App;
