115d4848de22c2ac712123989af26193dbb49f40
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-11-17 14:14:02 +0900

    Test : make test for move and restart input

--- __tests__/InputViewTest.js
+++ __tests__/InputViewTest.js
@@ -11,10 +11,28 @@ const mockQuestions = (answers) => {
 };
 
 describe("InputView 테스트", () => {
-  test("다리 사이즈 입력 유효 테스트", () => {
+  test("다리 사이즈 입력 숫자 테스트", () => {
     mockQuestions(["r"]);
     expect(() => {
       InputView.readBridgeSize();
+    }).toThrow("[ERROR]");
+  });
+  test("다리 사이즈 입력 범위 테스트", () => {
+    mockQuestions(["21"]);
+    expect(() => {
+      InputView.readBridgeSize();
+    }).toThrow("[ERROR]");
+  });
+  test("다리 건너는 방향 유효 테스트", () => {
+    mockQuestions(["R"]);
+    expect(() => {
+      InputView.readMoving();
+    }).toThrow("[ERROR]");
+  });
+  test("재시작 입력 유효 테스트", () => {
+    mockQuestions(["P"]);
+    expect(() => {
+      InputView.readGameCommand();
     }).toThrow("[ERROR]");
   });
 });
