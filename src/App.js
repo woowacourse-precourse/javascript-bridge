98be84dd3e2eb5e5261a5dc5a4d5b6932cda1aec
jjo-mi <whdal2636@naver.com>
2022-11-17 23:42:31 +0900

    feat: 1. 게임 시작 안내 문구 출력 및 메시지 상수처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+const { Console, Random } = require("@woowacourse/mission-utils");
+const { GUIDE_MESSAGE } = require("./Constant");
+
+
 class App {
-  play() {}
+  play() { 
+    this.printStart();
+  }
+
+
+  /** 1. 게임시작 안내 문구 출력 */
+  printStart() {
+    Console.print(GUIDE_MESSAGE.START);
+  }
+
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
---
+++ src/Constant.js
@@ -0,0 +1,21 @@
+const GUIDE_MESSAGE = {
+    START: "다리 건너기 게임을 시작합니다.",
+    INPUT_LENGTH: "다리의 길이를 입력해주세요.",
+    INPUT_MOVE: "이동할 칸을 선택해주세요. (위: U, 아래: D)",
+    INPUT_TRY: "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
+}
+
+const ERROR_MESSAGE = {
+    LENGTH: "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.",
+    MOVE: "[ERROR] 이동할 칸은 (위: U, 아래: D) 영어 대문자로만 입력해야 합니다."
+}
+
+const RESULT_MESSAGE = {
+    TITTLE: "최종 게임 결과",
+    RESULT_TABLE: " ",
+    IS_SUCCESS: "게임 성공 여부: ",
+    TRY_COUNT: "총 시도한 횟수: "
+}
+
+
+module.exports = { GUIDE_MESSAGE, ERROR_MESSAGE, RESULT_MESSAGE }
