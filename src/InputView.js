12acb2fdd21f8125fef058963653b4a299d4af21
hahagarden <88613455+hahagarden@users.noreply.github.com>
2022-11-17 15:25:52 +0900

    feat: 다리 길이 입력 예외처리

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,32 @@
+const ERROR_NOT_NUMBER = "[ERROR] 숫자를 입력해 주세요.";
+const ERROR_OUT_OF_RANGE = "[ERROR] 3~20 사이의 숫자를 입력해 주세요.";
+const ERROR_NOT_INTEGER = "[ERROR] 정수를 입력해 주세요.";
+
+const SIZE_START = 3;
+const SIZE_END = 20;
+
+/**
+ * @param {number} number
+ * 숫자 타입이 아니면 예외를 발생시킨다.
+ */
+function validateIsNumber(number) {
+  if (isNaN(number)) throw new Error(ERROR_NOT_NUMBER);
+}
+/**
+ * @param {number} number
+ * 범위 안의 숫자가 아니면 예외를 발생시킨다.
+ */
+function validateIsInRange(number) {
+  if ((number < SIZE_START) | (number > SIZE_END)) throw new Error(ERROR_OUT_OF_RANGE);
+}
+/**
+ * @param {number} number
+ * 정수형이 아니면 예외를 발생시킨다.
+ */
+function validateIsInteger(number) {
+  if (!Number.isInteger(number)) throw new Error(ERROR_NOT_INTEGER);
+}
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -16,6 +45,16 @@ const InputView = {
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand() {},
+
+  /**
+   * 입력받은 다리 길이를 검증하여 예외를 발생시킨다.
+   * @param {number} size
+   */
+  validateBridgeSize(size) {
+    validateIsNumber(size);
+    validateIsInRange(size);
+    validateIsInteger(size);
+  },
 };
 
 module.exports = InputView;
