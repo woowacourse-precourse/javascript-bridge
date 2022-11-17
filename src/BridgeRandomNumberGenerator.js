02160bc393b16e70fb174ab52857abb297331f60
letsjo <hyunoh.jo@gmail.com>
2022-11-17 21:08:18 +0900

    style(*): 제공된 소스파일의 쿼테이션 마크 수정

--- src/BridgeRandomNumberGenerator.js
+++ src/BridgeRandomNumberGenerator.js
@@ -1,12 +1,12 @@
-const MissionUtils = require("@woowacourse/mission-utils");
+const MissionUtils = require('@woowacourse/mission-utils');
 
 const BridgeRandomNumberGenerator = {
   RANDOM_LOWER_INCLUSIVE: 0,
   RANDOM_UPPER_INCLUSIVE: 1,
-  generate() {
+  generate () {
     return MissionUtils.Random.pickNumberInRange(
       BridgeRandomNumberGenerator.RANDOM_LOWER_INCLUSIVE,
-      BridgeRandomNumberGenerator.RANDOM_UPPER_INCLUSIVE
+      BridgeRandomNumberGenerator.RANDOM_UPPER_INCLUSIVE,
     );
   },
 };
