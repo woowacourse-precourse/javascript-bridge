9f38413be751d13195001804c48e4e0ea2e8d616
hahagarden <88613455+hahagarden@users.noreply.github.com>
2022-11-17 15:58:19 +0900

    test: 다리 길이 입력 받기

--- __tests__/InputViewTest.js
+++ __tests__/InputViewTest.js
@@ -1,4 +1,14 @@
+const { Console } = require("@woowacourse/mission-utils");
 const InputView = require("../src/InputView");
+
+const mockReadLine = (inputs) => {
+  Console.readLine = jest.fn();
+  inputs.reduce((acc, input) => {
+    return acc.mockImplementationOnce((_, callback) => {
+      callback(input);
+    });
+  }, Console.readLine);
+};
 
 describe("InputView 클래스 테스트", () => {
   test("입력받은 다리 길이의 예외상황 발생시 에러를 출력한다.", () => {
@@ -6,5 +16,12 @@ describe("InputView 클래스 테스트", () => {
     expect(() => InputView.validateBridgeSize(1)).toThrow();
     expect(() => InputView.validateBridgeSize(3.14)).toThrow();
     expect(() => InputView.validateBridgeSize(5)).not.toThrow();
+  });
+
+  test("유효한 다리의 길이가 입력될 때까지 예외를 발생시키고 유효한 값을 반환한다.", () => {
+    mockReadLine(["d", "1", "3.14", "5"]);
+    const spy = jest.spyOn(Console, "print");
+    expect(InputView.readBridgeSize()).resolves.toBe(5);
+    expect(spy).toHaveBeenCalledTimes(3);
   });
 });
