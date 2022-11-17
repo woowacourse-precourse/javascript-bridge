b777c5f53fa6042d4bbd4d749487c8b276640db9
bassyu <yrkdgus@gmail.com>
2022-11-17 20:37:27 +0900

    feat(BridgeGame): 시도 횟수를 저장하는 기능 추가

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,10 +1,12 @@
 class BridgeGame {
   #bridge;
   #map;
+  #try;
 
   constructor(bridge) {
     this.#bridge = bridge;
     this.#map = [];
+    this.#try = 1;
   }
 
   getBridge() {
@@ -15,6 +17,10 @@ class BridgeGame {
     return this.#map;
   }
 
+  getTry() {
+    return this.#try;
+  }
+
   move(moving) {
     const nextBridge = this.#bridge[this.#map.length];
     this.#map.push({
@@ -23,7 +29,10 @@ class BridgeGame {
     });
   }
 
-  retry() {}
+  retry() {
+    this.#map = [];
+    this.#try += 1;
+  }
 }
 
 module.exports = BridgeGame;
