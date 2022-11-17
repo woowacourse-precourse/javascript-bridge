3e2a4aef24b35147b7cfbf155f071d4354a71cf5
turtle601 <turtle601@naver.com>
2022-11-17 17:40:26 +0900

    fix(UpDownKeyTest) 방향키 테스트 코드 수정
    Changed:
    - toEqual 관련 테스트 값을 함수로 둔 것 수정

--- __tests__/UpDownKeyTest.js
+++ __tests__/UpDownKeyTest.js
@@ -26,13 +26,13 @@ describe('이동할 칸 입력 테스트', () => {
     const input = 'U';
     const upDowonKey = new UpDownKey(input);
 
-    expect(() => upDowonKey.getValidateData()).toEqual('U');
+    expect(upDowonKey.getValidateData()).toEqual('U');
   });
 
   test('(U or R 제외 문자 입력(정상) - "R") 입력 테스트', () => {
     const input = 'R';
     const upDowonKey = new UpDownKey(input);
 
-    expect(() => upDowonKey.getValidateData()).toEqual('R');
+    expect(upDowonKey.getValidateData()).toEqual('R');
   });
 });
