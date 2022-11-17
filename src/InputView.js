a2466262af1bbcf338a0dcd8c09263b4e82d1e2c
Yoo Seung Guk <dbtmd878@naver.com>
2022-11-18 01:27:03 +0900

    feat: Promise 메서드를 콜백 메서드로 변경

--- src/InputView.js
+++ src/InputView.js
@@ -11,42 +11,41 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {
-    return new Promise((resolve, reject) => {
-      Console.readLine("다리의 길이를 입력해주세요.\n", (bridgeLength) => {
-        resolve(bridgeLength);
-      });
+  readBridgeSize(callBack) {
+    Console.readLine("다리의 길이를 입력해주세요.\n", (bridgeLength) => {
+      return callBack(bridgeLength);
     });
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {
-    return new Promise((resolve, reject) => {
-      Console.readLine(
-        "이동할 칸을 선택해주세요. (위: U, 아래: D) \n",
-        (moveKey) => {
-          resolve(moveKey);
-        }
-      );
-    });
+  readMoving(callBack) {
+    Console.readLine(
+      "이동할 칸을 선택해주세요. (위: U, 아래: D) \n",
+      (moveKey) => {
+        return callBack(moveKey);
+      }
+    );
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
 
-  readGameCommand() {
-    return new Promise((resolve, reject) => {
-      Console.readLine(
-        "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q) \n",
-        (endKey) => {
-          resolve(endKey);
+  readGameCommand(stopCallBack, retryCallBack) {
+    Console.readLine(
+      "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q) \n",
+      (endKey) => {
+        if (endKey === "R") {
+          return retryCallBack();
         }
-      );
-    });
-  },
+        if (endKey === "Q") {
+          return stopCallBack();
+        }
+      }
+    );
+  }
 };
 
 module.exports = InputView;
