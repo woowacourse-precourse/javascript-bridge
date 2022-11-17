68881ba266ff5d5018b7391aca3282ee7b8e0357
turtle601 <turtle601@naver.com>
2022-11-18 00:04:37 +0900

    refactor(UpDownKeyTest) 테스트 코드 문자 수정
    Changed:
    - U or R -> U or D

--- __tests__/UpDownKeyTest.js
+++ __tests__/UpDownKeyTest.js
@@ -1,21 +1,21 @@
 const UpDownKey = require('../src/domain/UpDownKey');
 
 describe('이동할 칸 입력 테스트', () => {
-  test('(U or R 제외 문자 입력(오류) - "a") 입력 테스트', () => {
+  test('(U or D 제외 문자 입력(오류) - "a") 입력 테스트', () => {
     const input = 'a';
     const upDowonKey = new UpDownKey(input);
 
     expect(() => upDowonKey.getValidateData()).toThrow('[ERROR]');
   });
 
-  test('(U or R 제외 문자 입력(오류) - "1") 입력 테스트', () => {
+  test('(U or D 제외 문자 입력(오류) - "1") 입력 테스트', () => {
     const input = '1';
     const upDowonKey = new UpDownKey(input);
 
     expect(() => upDowonKey.getValidateData()).toThrow('[ERROR]');
   });
 
-  test('(U or R 제외 문자 입력(오류) - "u") 입력 테스트', () => {
+  test('(U or D 제외 문자 입력(오류) - "u") 입력 테스트', () => {
     const input = 'u';
     const upDowonKey = new UpDownKey(input);
 
