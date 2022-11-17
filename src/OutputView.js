d68a3e2c64ede85d0835bf15217ac57b14fa075f
Subin Jo <sbjo1202@naver.com>
2022-11-17 16:30:28 +0900

    refactor: 줄바꿈 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -6,7 +6,7 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const OutputView = {
 
   printStart() {
-    MissionUtils.Console.print('다리 건너기 게임을 시작합니다.');
+    MissionUtils.Console.print('다리 건너기 게임을 시작합니다.\n');
   },
   
   
