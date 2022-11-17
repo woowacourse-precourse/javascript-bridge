ccbe08e86a3b2bc7d70bb75ebb4f597cc92005ef
y00eunji <aine0227@naver.com>
2022-11-17 22:07:54 +0900

    feat: Print and Get bridge size input

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,11 +1,16 @@
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
+const { Console } = require("@woowacourse/mission-utils");
+const { GAME_MESSAGE } = require("../utils/Constants");
+
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize(callback) {
+    Console.readLine(GAME_MESSAGE.INPUT_BRIDGE_LENGTH, callback);
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
--- src/view/View.js
+++ src/view/View.js
@@ -1,9 +1,29 @@
 const InputView = require("./InputView");
 const OutputView = require("./OutputView");
+const BridgeGameController = require("../BridgeGameController");
+const BridgeSizeValidator = require("../validator/BridgeSizeValidator");
 
 class View {
+  #gameController;
+
+  constructor() {
+    this.#gameController = new BridgeGameController();
+  }
+
   startGame() {
     OutputView.startGame();
+    this.#inputBridgeSize();
+  }
+
+  #inputBridgeSize() {
+    InputView.readBridgeSize(this.#getBridgeSize());
+  }
+
+  #getBridgeSize() {
+    return (size) => {
+      BridgeSizeValidator.validate(size);
+      this.#gameController.inputBridge(size);
+    };
   }
 }
 
