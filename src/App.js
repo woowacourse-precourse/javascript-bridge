30b352865aa354b4f7de11582ebb5a371e823c91
Sungwk <73461032+Sungwk@users.noreply.github.com>
2022-11-17 18:10:34 +0900

    feat : 다리 길이 입력받기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 const OutputView = require('./OutputView');
+const BridgeGame = require('./BridgeGame');
+const InputView = require('./InputView');
 class App {
+  constructor() {
+    this.bridgeGame = new BridgeGame();
+  }
+
   play() {
     OutputView.playGame();
+    InputView.readBridgeSize();
   }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,5 @@
+const { Console } = require('@woowacourse/mission-utils');
+const { GAME_MESSAGE } = require('./Constants');
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,7 +7,11 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine(GAME_MESSAGE.INPUT_BRIDGE_LENGTH, (bridge) => {
+      Console.print('다리길이 : ' + bridge);
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
