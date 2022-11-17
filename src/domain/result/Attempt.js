609b26e6d4ac138c88088ac8b0f39620135b9f1d
hwanfront <kim015jh@gmail.com>
2022-11-17 16:00:33 +0900

    feat(Attempt): 시도 횟수 관련 메서드 추가
    추가 사항:
    - 시도 횟수를 늘리는 add 메서드
    - 시도 횟수의 출력 결과를 생성하는 print 메서드

--- src/domain/result/Attempt.js
+++ src/domain/result/Attempt.js
@@ -1,10 +1,16 @@
 class Attempt {
-  add() {
+  #count;
 
+  constructor() {
+    this.#count = 0;
+  }
+
+  add() {
+    this.#count += 1;
   }
 
   print() {
-
+    return `총 시도한 횟수: ${this.#count}`;
   }
 }
 
