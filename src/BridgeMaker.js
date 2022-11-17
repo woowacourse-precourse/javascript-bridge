80624281aae0f205d2fc173b922f181cea27f7e3
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-18 00:02:00 +0900

    fix(BridgeMaker): randomNumber의 자료형 Number => String 수정
    ApplicationTest의 미션유틸 mock함수 return 값이 String으로 되어있음.
    테스트 통과를 위한 자료형 수정.

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -3,9 +3,10 @@ const { DOWNSIDE_SYMBOL_NUM, DOWNSIDE_SYMBOL, UPSIDE_SYMBOL } = require('./const
 const BridgeMaker = {
   makeBridge(size, generateRandomNumber) {
     return Array.from({ length: size }, () => {
-      const randomNum = generateRandomNumber();
+      // ApplicationTest 테스트 코드 문자열로 주어짐... -->  ["1", "0", "0"];
+      const randomNum = String(generateRandomNumber());
 
-      if (randomNum === DOWNSIDE_SYMBOL_NUM) return DOWNSIDE_SYMBOL;
+      if (randomNum === String(DOWNSIDE_SYMBOL_NUM)) return DOWNSIDE_SYMBOL;
 
       return UPSIDE_SYMBOL;
     });
