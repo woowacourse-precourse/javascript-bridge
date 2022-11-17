7b9d70fa0f682abdfe844c2bb645e3b072505c57
hae-on <solbi2004@naver.com>
2022-11-17 19:11:34 +0900

    test: 플레이어 입력 문자 유효성 검사 테스트

--- __tests__/ValidateTest.js
+++ __tests__/ValidateTest.js
@@ -1,4 +1,4 @@
-const { validateBridgeSize } = require('../src/Validate');
+const { validateBridgeSize, checkMoveString } = require('../src/Validate');
 
 describe('다리 길이 입력에 대한 유효성 검사', () => {
   test('다리 길이 숫자인지 테스트', () => {
@@ -10,6 +10,12 @@ describe('다리 길이 입력에 대한 유효성 검사', () => {
   test('다리의 길이 3이상 20이하 범위 테스트', () => {
     expect(() => {
       validateBridgeSize('1');
+    }).toThrow('[ERROR]');
+  });
+
+  test('플레이어가 입력한 문자가 U 또는 D 인지 유효성 검사', () => {
+    expect(() => {
+      checkMoveString('u');
     }).toThrow('[ERROR]');
   });
 });
