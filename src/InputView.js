773d38a38fdb435f6928726b878f5ea849b79a3c
daal2quf <daal2quf@gmail.com>
2022-11-17 18:30:51 +0900

    feat(inputView): 유저가 이동할 칸을 입력받는 기능 추가
    add:
    readMoving(): 유저가 이동할 칸을 입력받고 입력을 반환

--- src/InputView.js
+++ src/InputView.js
@@ -3,13 +3,16 @@ const { Console } = require("@woowacourse/mission-utils");
 const InputView = {
 
   readBridgeSize() {
-    Console.readLine("다리의 길이를 입력해주세요.", (userInputBridgeLength) => {
+    Console.readLine('다리의 길이를 입력해주세요.\n', (userInputBridgeLength) => {
       return userInputBridgeLength;
     });
   },
 
-  readMoving() { },
-
+  readMoving() {
+    Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)\n', (userInputMoving) => {
+      return userInputMoving;
+    });
+  },
 
   readGameCommand() { },
 };
