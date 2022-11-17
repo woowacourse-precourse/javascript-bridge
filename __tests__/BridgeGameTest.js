bcde6df3d045229faec295a2f616df885e3a66ec
Mcdonalds Trump <whdmschd2@gmail.com>
2022-11-18 01:08:55 +0900

    test: 사용자가 이동할 수 없는 칸을 선택했을 때 현재까지 건넌 다리 지도

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -20,4 +20,24 @@ describe("다리 건너기 게임을 관리하는 클래스 테스트", () => {
       downSideBridgeMap
     );
   });
+
+  test("사용자가 이동할 수 없는 칸을 선택했을 때 현재까지 건넌 다리 지도", () => {
+    const playerChoiceMovingCommands = ["U", "D", "U"];
+    const upsideBridgeMap = ["O", " ", "X"];
+    const downSideBridgeMap = [" ", "O", " "];
+
+    const bridgeGame = new BridgeGame();
+    bridgeGame.setBridgeShape(["U", "D", "D", "D", "U"]);
+
+    playerChoiceMovingCommands.forEach((playerChoiceMovingCommand) => {
+      bridgeGame.move(playerChoiceMovingCommand);
+    });
+
+    expect(bridgeGame.getCurrentBridgeMap().upsideBridgeMap).toEqual(
+      upsideBridgeMap
+    );
+    expect(bridgeGame.getCurrentBridgeMap().downsideBridgeMap).toEqual(
+      downSideBridgeMap
+    );
+  });
 });
