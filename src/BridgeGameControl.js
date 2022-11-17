da3d2f8d241684acf2d96e94daacd39c80ba77c6
coolIlive <galilee155@naver.com>
2022-11-17 20:02:26 +0900

    Feat: 입력받은 길이만큼 다리 생성하는 기능 추가

--- src/BridgeGameControl.js
+++ src/BridgeGameControl.js
@@ -1,21 +1,30 @@
-// const { Console } = require('@woowacourse/mission-utils');
+const { Console } = require('@woowacourse/mission-utils');
 const InputView = require('./view/InputView');
 const OutputView = require('./view/OutputView');
 const BridgeSize = require('./available-check/BridgeSize');
+const BridgeMaker = require('./BridgeMaker');
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
 
 class BridgeGameControl {
-  size
+  size;
+  bridge;
 
   constructor() {
     this.bidgeSize = new BridgeSize();
-  }
+  };
   
   start() {
     InputView.readBridgeSize((size) => {
       this.bidgeSize.validate(size);
       this.size = size;
-      // Console.print('DJDKJFDKJALKDJDL');
+      this.makeBridge();
     });
+  };
+
+  makeBridge() {
+    this.bridge = BridgeMaker.makeBridge(this.size, BridgeRandomNumberGenerator.generate);
+    // Console.print('ddddddd')
+    // Console.print(this.bridge)
   };
 
 };
