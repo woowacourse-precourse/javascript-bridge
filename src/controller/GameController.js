6882c931b5b5aae684361c19cb63944d1716882d
bkdragon0228 <rlaqjarb2067@naver.com>
2022-11-17 21:45:21 +0900

    refactor : 다리 정보를 controller로 저장하기 위해 함수 수정
    다리를 GameController에 저장하기 위해 콜백을 InputView로 넘기는 방식으로
    변경했습니다.

--- src/controller/GameController.js
+++ src/controller/GameController.js
@@ -1,14 +1,36 @@
 const { Console } = require('@woowacourse/mission-utils');
 const BridgeGame = require('../model/BridgeGame');
 const InputView = require('../view/InputView');
+const BridgeRandomNumberGenerator = require('../BridgeRandomNumberGenerator');
+const BridgeMaker = require('../BridgeMaker');
 
 class GameController {
-  constructor() {
-    //
-  }
+  #bridge;
 
   start() {
     Console.print('다리 건너기 게임을 시작합니다.');
-    InputView.setting();
+    this.askBridge();
+  }
+
+  askBridge() {
+    InputView.readBridgeSize(this.setBridge.bind(this));
+  }
+
+  setBridge(size) {
+    this.#bridge = BridgeMaker.makeBridge(size, () =>
+      BridgeRandomNumberGenerator.generate()
+    );
+    console.log(this.#bridge);
+    this.askMoving();
+  }
+
+  askMoving() {
+    InputView.readMoving(this.setMoving.bind(this));
+  }
+
+  setMoving(next) {
+    //  이동하는 로직
   }
 }
+
+module.exports = GameController;
--- src/view/InputView.js
+++ src/view/InputView.js
@@ -4,34 +4,18 @@ const { Console } = require('@woowacourse/mission-utils');
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
-  bridgeSize: 0,
-
-  setting() {
-    this.readBridgeSize();
-  },
-
-  getValues() {
-    return { bridgelength: this.bridgelength };
-  },
-
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {
-    Console.readLine('다리의 길이를 입력해주세요.\n', (number) => {
-      this.validateBridgeSize(number);
-      this.bridgeSize = number;
-      this.readMoving();
-    });
+  readBridgeSize(callback) {
+    Console.readLine('다리의 길이를 입력해주세요.\n', callback);
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {
-    Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)\n', (next) => {
-      this.validateNext(next);
-    });
+  readMoving(callback) {
+    Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)\n', callback);
   },
 
   /**
