87ca3171059a2ce1719e040eae94fa2a9556ae3c
woong <inwoong100@gmail.com>
2022-11-17 21:36:07 +0900

    feat(outputView): outputView printMap의 메소드 분리(printLine, setMessage), 값 상수화

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,4 +1,5 @@
 const { Console } = require('@woowacourse/mission-utils');
+const { BRIDGE_ELEMENT, OUTPUT_MESSAGE } = require('./utils/Constant');
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -10,48 +11,37 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printMap(bridge, game) {
+    this.printLine(bridge, game, BRIDGE_ELEMENT.UP);
+    this.printLine(bridge, game, BRIDGE_ELEMENT.DOWN);
+  },
+
+  printLine(bridge, game, select) {
+    const message = this.setMessage(bridge, game, select);
+    Console.print(message);
+  },
+
+  setMessage(bridge, game, dependency) {
     const length = game.getLength();
-    let resultFirstLine = '[ ';
+
+    let message = OUTPUT_MESSAGE.START;
 
     for (let i = 0; i < length; i += 1) {
       const selectedElement = game.getSelected(i);
       const bridgeElement = bridge.getBridge(i);
 
-      if (selectedElement === '0') {
-        if (bridgeElement === '0') {
-          resultFirstLine += 'O ';
-        } else if (bridgeElement === '1') {
-          resultFirstLine += 'X ';
+      if (i !== 0) message += OUTPUT_MESSAGE.LINE;
+      if (selectedElement === dependency) {
+        if (bridgeElement === selectedElement) {
+          message += OUTPUT_MESSAGE.CORRECT;
+        } else if (bridgeElement !== selectedElement) {
+          message += OUTPUT_MESSAGE.INCORRECT;
         }
-      } else if (selectedElement === '1') {
-        resultFirstLine += '  ';
+      } else {
+        message += OUTPUT_MESSAGE.EMPTY;
       }
-      if (i !== length - 1) resultFirstLine += '| ';
     }
 
-    resultFirstLine += ']';
-
-    let resultSecondLine = '[ ';
-
-    for (let i = 0; i < length; i += 1) {
-      const selectedElement = game.getSelected(i);
-      const bridgeElement = bridge.getBridge(i);
-
-      if (selectedElement === '1') {
-        if (bridgeElement === '1') {
-          resultSecondLine += 'O ';
-        } else if (bridgeElement === '0') {
-          resultSecondLine += 'X ';
-        }
-      } else if (selectedElement === '0') {
-        resultSecondLine += '  ';
-      }
-      if (i !== length - 1) resultSecondLine += '| ';
-    }
-    resultSecondLine += ']';
-
-    Console.print(resultFirstLine);
-    Console.print(resultSecondLine);
+    return message + OUTPUT_MESSAGE.END;
   },
 
   /**
--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -8,4 +8,29 @@ const ERROR_MESSAGE = {
   LEVEL_INPUT: '[ERROR] 이동 값은 U 또는 D를 입력하세요',
 };
 
-module.exports = { MESSAGE, ERROR_MESSAGE };
+const INPUT_MESSAGE = {
+  UP: 'U',
+  DOWN: 'D',
+};
+
+const BRIDGE_ELEMENT = {
+  UP: '0',
+  DOWN: '1',
+};
+
+const OUTPUT_MESSAGE = {
+  CORRECT: 'O ',
+  INCORRECT: 'X ',
+  LINE: '| ',
+  EMPTY: '  ',
+  START: '[ ',
+  END: ']',
+};
+
+module.exports = {
+  MESSAGE,
+  ERROR_MESSAGE,
+  INPUT_MESSAGE,
+  BRIDGE_ELEMENT,
+  OUTPUT_MESSAGE,
+};
