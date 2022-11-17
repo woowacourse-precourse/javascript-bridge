c2db1ed83f0875db4144f3e896a5aaf532a9f2b2
khj0426 <706shin1728@naver.com>
2022-11-18 02:06:27 +0900

    feat:위아래 칸 입력받는 기능 추가

--- src/InputView.js
+++ src/InputView.js
@@ -3,12 +3,13 @@
  */
 const { Console } = require('@woowacourse/mission-utils');
 const { BRIDGE_INPUT } = require('./MESSAGES/InputMessage');
-const { RE_START_MESSAGE } = require('./MESSAGES/GameMessage');
+const { RE_START_MESSAGE, MOVE_MESSAGE } = require('./MESSAGES/GameMessage');
 
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
+
   readBridgeSize() {
     let bridgeSize;
     Console.readLine(BRIDGE_INPUT, (inputbridgeSize) => {
@@ -28,7 +29,13 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    let moving;
+    Console.readLine(MOVE_MESSAGE, (inputMoving) => {
+      moving = inputMoving;
+    });
+    return moving;
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
