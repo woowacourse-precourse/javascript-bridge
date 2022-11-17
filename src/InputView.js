8bda4aca9942263399a0c57b07c6ce8b4d619130
confidential-nt <youyoy689@gmail.com>
2022-11-17 17:13:41 +0900

    feat: 요구사항 3. User가 다리의 길이 입력할 수 있게 하기
    User가 다리의 길이를 입력할 수 있도록 하는 기능을 구현하였다.

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,5 @@
+const { Console } = require("@woowacourse/mission-utils");
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,7 +7,11 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine("다리의 길이를 입력해주세요.", (value) => {
+      console.log(value);
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
