5e143647cd77d03c77dae0294b05092fe50436b9
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-11-17 19:49:53 +0900

    test(BridgeGame): 숫자값 문자열 치환 기능 테스트
    메소드 이름은 "replaceString"로 정의된다.

---
+++ __tests__/BridgeGame.test.js
@@ -0,0 +1,9 @@
+const BridgeGame = require('../src/BridgeGame');
+
+describe('숫자값 문자열 치환 기능 테스트', () => {
+  test('메소드 이름은 "replaceString"로 정의된다.', () => {
+    const METHOD_NAME = 'replaceString';
+
+    expect(BridgeGame.replaceString.name).toEqual(METHOD_NAME);
+  });
+});
