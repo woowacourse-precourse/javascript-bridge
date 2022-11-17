75ea48ebffff3b39c1625fb24f3b70bdfd4c8866
guridaek <iue16@naver.com>
2022-11-17 14:37:15 +0900

    feat: 다리 이동 기능 추가

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -2,12 +2,16 @@
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  #bridge = [];
+  #movingLog = [];
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(direction) {
+    this.#movingLog.push(direction);
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
