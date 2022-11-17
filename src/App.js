469278a2aad708916af60b36d5b98ca960254f8b
DongjoonShin <ehdwns1516@ajou.ac.kr>
2022-11-17 16:13:12 +0900

    feat: 다리 길이를 입력받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 const { printStart } = require('./OutputView');
+const { readBridgeSize } = require('./InputView');
 
 class App {
   play() {
     printStart();
+    readBridgeSize().then((size) => {
+      console.log(size);
+    });
   }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,4 @@
+const { Console } = require('@woowacourse/mission-utils');
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,7 +6,13 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    return new Promise((resolve) => {
+      Console.readLine('\n다리의 길이를 입력해주세요.\n', (size) => {
+        resolve(size);
+      });
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
