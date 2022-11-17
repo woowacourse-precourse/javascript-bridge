6e964678155041258ca1c5d5603ebf578257e2bf
yuu <22yuu@naver.com>
2022-11-17 20:28:16 +0900

    chore: eslint 파일 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,24 @@ module.exports = {
     commonjs: true,
     es2021: true,
   },
-  extends: 'airbnb-base',
-  overrides: [
-  ],
+  extends: ["airbnb-base", "plugin:prettier/recommended"],
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaVersion: "latest",
   },
   rules: {
+    "max-depth": ["error", 2],
+    "max-lines-per-function": ["error", 10],
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+        bracketSpacing: true,
+        printWidth: 80,
+        semi: true,
+        trailingComma: "all",
+        tabWidth: 2,
+      },
+    ],
   },
 };
