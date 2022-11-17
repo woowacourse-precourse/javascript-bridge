124bbef2f96cdeeb4d63f2e4766ea33f2eb4e180
AnJaeSeong <dktmzh6@gmail.com>
2022-11-17 13:59:49 +0900

    feat(BridgeGameController): startNewRound()

--- src/BridgeGameController.js
+++ src/BridgeGameController.js
@@ -1,14 +1,27 @@
 const BridgeGame = require("./BridgeGame");
 const BridgeMaker = require("./BridgeMaker");
 const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+const OutputView = require("./OutputView");
 
 /**
  * 다리 건너기 게임 진행 로직 함수
  */
 class BridgeGameController {
-    static makeNewBridgeGame(input) {
-        return new BridgeGame(BridgeMaker.makeBridge(input, BridgeRandomNumberGenerator.generate));
-    }
+  static makeNewBridgeGame(input) {
+    return new BridgeGame(BridgeMaker.makeBridge(input, BridgeRandomNumberGenerator.generate));
+  }
+
+  SUCCESS_ARRIVAL = 2;
+  SUCCESS_ROUND = 1;
+  FAILURE_ROUND = 0;
+  static startNewRound(bridgeGame, input) {
+    const roundResult = bridgeGame.move(input);
+    OutputView.printMap(bridgeGame.stateToString());
+    if(!roundResult) return this.FAILURE_ROUND;
+    else if (!bridgeGame.isArrived()) return this.SUCCESS_ROUND;
+    OutputView.printResult(bridgeGame.stateToString(), bridgeGame.isArrived(), bridgeGame.getTry());
+    return this.SUCCESS_ARRIVAL;  
+  }
 }
 
 module.exports = BridgeGameController;
