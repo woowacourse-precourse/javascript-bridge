62446c04b97b209fb7e29a4fca896d7f2848ce32
jinseo <ghkdwlstj99@gmail.com>
2022-11-18 01:30:06 +0900

    feat: 이동 할 수 있는 칸을 선택한 경우 O로 표시하는 기능 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -2,13 +2,32 @@
  * 다리 건너기 게임을 관리하는 클래스
  */
  class BridgeGame {
+  constructor(){
+    this.userPickedArr = [];
+    this.userPickedUpOrDown = [[], []];
+  }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(createBridge, selectUpOrDown) {
+    this.userPickedArr.push(selectUpOrDown);
+    const compareLength = this.userPickedArr.length - 1;
+    this.moveIsCorrect(createBridge[compareLength], this.userPickedArr[compareLength]);
+  }
 
+
+  moveIsCorrect(createBridge, selectUpOrDown){
+    if(createBridge == selectUpOrDown && selectUpOrDown == "U"){
+      this.userPickedUpOrDown[0].push(" O ");
+      this.userPickedUpOrDown[1].push(" ");
+    }
+    if(createBridge == selectUpOrDown && selectUpOrDown == "D"){
+      this.userPickedUpOrDown[0].push(" ");
+      this.userPickedUpOrDown[1].push(" O ")
+    }
+  }
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
    * <p>
