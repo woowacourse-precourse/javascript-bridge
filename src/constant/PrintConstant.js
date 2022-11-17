5d1ed8389de55b815ff2c0848a95cf510e112769
Inae Choi <inaechoi89@gmail.com>
2022-11-17 20:54:28 +0900

    feat: add constant
    add message constant

---
+++ src/constant/PrintConstant.js
@@ -0,0 +1,11 @@
+const Message = {
+  START: '다리 건너기 게임을 시작합니다.',
+  INPUT_LENGTH: '다리의 길이를 입력해주세요.',
+  MOVE: '이동할 칸을 선택해주세요. (위: U, 아래: D)',
+  RETRY: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)',
+  FINAL: '최종 게임 결과',
+  RESULT: '게임 성공 여부: ',
+  TRY_COUNT: '총 시도한 횟수: ',
+};
+
+module.exports = Message;
