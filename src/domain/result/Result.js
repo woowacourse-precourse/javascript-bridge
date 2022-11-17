4621548ce0acf95c349da2fa8def5257cdb666a1
hwanfront <kim015jh@gmail.com>
2022-11-17 16:02:51 +0900

    feat(Attempt): 게임 성공 여부의 출력 결과를 생성하는 메서드 추가

--- src/domain/result/Result.js
+++ src/domain/result/Result.js
@@ -1,6 +1,15 @@
 class Result {
-  print() {
+  static #TYPE = {
+    success: '성공',
+    fail: '실패',
+  };
 
+  static print(type) {
+    return `게임 성공 여부: ${Result.#result(type)}`;
+  }
+
+  static #result(type) {
+    return Result.#TYPE[type] ?? new Error('[ERROR]');
   }
 }
 
