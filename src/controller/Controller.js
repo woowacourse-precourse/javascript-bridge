5d18b5afe046bdf1224e6dbb0ba39ab68bf43bac
violetwhenisawu <anashape710@gmail.com>
2022-11-18 04:23:19 +0900

    feat: 다리 길이를 입력받는 매서드 작성

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,9 +1,13 @@
+const InputView = require('../view/InputView');
 const OutputView = require('../view/OutputView');
 
 class Controller {
   start() {
     OutputView.printGameStart();
+    InputView.readBridgeSize(this.getBridgeSize.bind(this));
   }
+
+  getBridgeSize(size) {}
 }
 
 module.exports = Controller;
