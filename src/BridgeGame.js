634f32471720579f3bf8ec8b8bd4c63cc39b7a3c
Suyeon <msjgr2801@gmail.com>
2022-11-17 13:16:24 +0900

    Feat: 사용자가 이동할 때 다리 건너기의 결과를 보여주는 기능 구현
    - BridgeGame: gameStart 메소드에서 유저의 입력값을 저장해두고, 새 입력을 받을 때마다 건너기의 결과를 OutputView 객체로 출력
    - OutputView: printMap 메소드에서 line별로 값을 비교하여 다리 건너기 결과를 출력

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,8 +1,14 @@
-/**
- * 다리 건너기 게임을 관리하는 클래스
+/*
+  * 다리 건너기 게임을 관리하는 클래스
+    제공된 BridgeGame 클래스를 활용해 구현해야 한다.
+    BridgeGame에 필드(인스턴스 변수)를 추가할 수 있다.
+    BridgeGame의 파일 경로는 변경할 수 있다.
+    BridgeGame의 메서드의 이름은 변경할 수 없고, 인자는 필요에 따라 추가하거나 변경할 수 있다.
+    게임 진행을 위해 필요한 메서드를 추가 하거나 변경할 수 있다.
  */
 const BridgeMaker = require("./BridgeMaker");
 const InputView = require("./InputView");
+const OutputView = require("./OutputView");
 const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
 
 class BridgeGame {
@@ -22,8 +28,10 @@ class BridgeGame {
   }
 
   gameStart() {
+    const userMoving = [];
+
     for (let round = 0; round < this.#bridgeSize; round++) {
-      this.move();
+      this.move(userMoving, round);
     }
   }
   /**
@@ -31,16 +39,26 @@ class BridgeGame {
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {
+  move(userMoving, round) {
     const moving = InputView.readMoving();
+    userMoving.push(moving);
+    OutputView.printMap(this.#bridgeStyle, userMoving);
+
+    if (userMoving[round] !== this.#bridgeStyle[round]) {
+      this.retry();
+    }
   }
+
+  
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  retry() {}
+  retry() {
+
+  }
 }
 
 module.exports = BridgeGame;
--- src/OutputView.js
+++ src/OutputView.js
@@ -1,13 +1,77 @@
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
+const { MissionUtils } = require("@woowacourse/mission-utils");
+
+/*
+  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
+    제공된 OutputView 객체를 활용해 구현해야 한다.
+    OutputView의 파일 경로는 변경할 수 있다.
+    OutputView의 메서드의 이름은 변경할 수 없고, 인자는 필요에 따라 추가하거나 변경할 수 있다.
+    값 출력을 위해 필요한 메서드를 추가할 수 있다.
  */
 const OutputView = {
+  UP_MOVING: "U",
+  DOWN_MOVING: "D",
+  SUCCESS_SELECT_CASE: "[ O ]",
+  FAIL_SELECT_CASE: "[ X ]",
+  NOT_SELECT_CASE: "[   ]",
   /**
    * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {},
+  printMap(bridgeStyle, userMoving) {
+    for (let bridgeLine = 0; bridgeLine < userMoving.length; bridgeLine++) {
+      this.userMovingResult(bridgeStyle[bridgeLine], userMoving[bridgeLine]);
+    }
+  },
+
+  userMovingResult(bridgePosition, userPosition) {
+    if (bridgePosition === userPosition) {
+      this.selectCorrectBlock(userPosition);
+    } else { // TODO: 해당 else를 쓸 것인지 말 것인지 결정하기. 
+      this.selectUnCorrectBlock(userPosition);
+    }
+  },
+
+  selectCorrectBlock(userPosition) {
+    if (userPosition === this.UP_MOVING) {
+      this.selectUpperBlock(true);
+    }
+
+    if (userPosition === this.DOWN_MOVING) {
+      this.selectLowerBlock(true);
+    }
+
+  },
+
+  selectUnCorrectBlock(userPosition) {
+    if (userPosition === this.UP_MOVING) {
+      this.selectUpperBlock(false);
+    }
+
+    if (userPosition === this.DOWN_MOVING) {
+      this.selectLowerBlock(false);
+    }
+  },
+
+  selectUpperBlock(isCorrect) {
+    if (isCorrect) {
+      MissionUtils.Console.print(this.SUCCESS_SELECT_CASE);
+      MissionUtils.Console.print(this.NOT_SELECT_CASE);
+    } else {
+      MissionUtils.Console.print(this.FAIL_SELECT_CASE);
+      MissionUtils.Console.print(this.NOT_SELECT_CASE);
+    }
+  },
+
+  selectLowerBlock(isCorrect) {
+    if (isCorrect) {
+      MissionUtils.Console.print(this.NOT_SELECT_CASE);
+      MissionUtils.Console.print(this.SUCCESS_SELECT_CASE);
+    } else {
+      MissionUtils.Console.print(this.NOT_SELECT_CASE);
+      MissionUtils.Console.print(this.FAIL_SELECT_CASE);
+    }
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
