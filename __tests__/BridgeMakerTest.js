d02502761bd2c7777fe608bd13543243c178d9e4
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-11-17 19:01:13 +0900

    Test : fix bridgeMaker test

--- __tests__/BridgeMakerTest.js
+++ __tests__/BridgeMakerTest.js
@@ -1,13 +1,13 @@
 const BridgeMaker = require("../src/BridgeMaker");
 
-describe("BridgeMaker 함수", () => {
+describe.only("BridgeMaker 함수", () => {
   test("다리 생성 테스트", () => {
     const randomNumbers = ["1", "0", "0"];
     const mockGenerator = randomNumbers.reduce((acc, number) => {
       return acc.mockReturnValueOnce(number);
     }, jest.fn());
 
-    const bridge = BridgeMaker.makeBridge(3, mockGenerator);
+    const bridge = BridgeMaker.makeBridge("3", mockGenerator);
     expect(bridge).toEqual(["U", "D", "D"]);
   });
 });
