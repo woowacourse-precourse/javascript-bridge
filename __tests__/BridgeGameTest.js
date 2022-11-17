d50c9ee78d4fa18badd065614ec97fb868a489ad
dlwl98 <ff981113@hanyang.ac.kr>
2022-11-17 15:17:56 +0900

    test(BridgeGameTest): 다리 건너기 실패 케이스 작성

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -12,16 +12,52 @@ describe('다리 건너기 게임 테스트', () => {
 
   test('다리 건너기 테스트', () => {
     const resultMap = bridgeGame.move('U');
-    expect(resultMap.upperPart).toEqual('O');
+    expect(resultMap).toEqual({
+      upperPart: 'O',
+      lowerPart: ' ',
+      isGameOver: false,
+    });
   });
 
   test('다리 건너기 테스트', () => {
     const resultMap = bridgeGame.move('D');
-    expect(resultMap.upperPart).toEqual('O |  ');
+    expect(resultMap).toEqual({
+      upperPart: 'O |  ',
+      lowerPart: '  | O',
+      isGameOver: false,
+    });
   });
 
   test('다리 건너기 테스트', () => {
     const resultMap = bridgeGame.move('D');
     expect(resultMap.lowerPart).toEqual('  | O | X');
+    expect(resultMap).toEqual({
+      upperPart: 'O |   |  ',
+      lowerPart: '  | O | X',
+      isGameOver: true,
+    });
+  });
+});
+
+describe('다리 건너기 게임 테스트', () => {
+  mockRandoms(['1', '0', '1']);
+  const bridgeGame = new BridgeGame(3);
+
+  test('다리 건너기 테스트', () => {
+    const resultMap = bridgeGame.move('U');
+    expect(resultMap).toEqual({
+      upperPart: 'O',
+      lowerPart: ' ',
+      isGameOver: false,
+    });
+  });
+
+  test('다리 건너기 테스트', () => {
+    const resultMap = bridgeGame.move('U');
+    expect(resultMap).toEqual({
+      upperPart: 'O | X',
+      lowerPart: '  |  ',
+      isGameOver: true,
+    });
   });
 });
