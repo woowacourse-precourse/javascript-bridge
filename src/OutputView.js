94e3a2786d3282d0606692ac89dc4020f8f0851e
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-17 16:49:08 +0900

    fix(View): 이중 배열로 변경 된 유저의 이동 정보를 유저에게 출력

--- src/OutputView.js
+++ src/OutputView.js
@@ -9,12 +9,20 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
 
+  lineBreak() {
+    Console.print(``);
+  },
+
   paintToStadard(arr) {
     return arr.join(` | `).split(`,`);
   },
 
   printMap(bridgeGameResult) {
-    Console.print(`[ ${this.paintToStadard(bridgeGameResult)} ]`);
+    //[O,O,X]
+    Console.print(`[ ${this.paintToStadard(bridgeGameResult[0])} ]`);
+    Console.print(`[ ${this.paintToStadard(bridgeGameResult[1])} ]`);
+
+    this.lineBreak();
   },
 
   /**
@@ -24,11 +32,14 @@ const OutputView = {
    */
   printResult(bridgeGameResult, successOrFail, tryCount) {
     Console.print(`최종 게임 결과\n`);
+
     this.printMap(bridgeGameResult);
     Console.print(``);
 
     Console.print(`게임 성공 여부 : ${successOrFail}`);
     Console.print(`총 시도한 횟수: ${tryCount}`);
+    Console.close();
+    return;
   },
 };
 
