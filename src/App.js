74fe26b225c63bc3adcbcf65c80657d6830c3afc
wang-yurin <king_1994@naver.com>
2022-11-17 22:08:01 +0900

    feat(printStart): 게임 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+const OutputView = require("./OutputView");
 class App {
-  play() {}
+  play() {
+    OutputView.printStart();
+  }
 }
 
+const app = new App();
+app.play();
 module.exports = App;
--- src/OutputView.js
+++ src/OutputView.js
@@ -1,4 +1,10 @@
+const { Console } = require("@woowacourse/mission-utils");
+
 const OutputView = {
+  printStart() {
+    Console.print("다리 건너기 게임을 시작합니다.\n");
+  },
+
   printMap() {},
 
   printResult() {},
