cbc69f77166e89570cc631fb6382c8c0fa638da4
dlwl98 <ff981113@hanyang.ac.kr>
2022-11-17 21:44:51 +0900

    refactor(OutputView): printMap 메서드에 줄바꿈 출력 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -12,6 +12,7 @@ const OutputView = {
    */
   printMap(resultToString) {
     Console.print(resultToString);
+    Console.print('');
   },
 
   /**
@@ -22,7 +23,6 @@ const OutputView = {
   printResult(resultToString, gameStatus, count) {
     Console.print('최종 게임 결과');
     OutputView.printMap(resultToString);
-    Console.print('');
     Console.print(`게임 성공 여부: ${gameStatus === 1 ? '실패' : '성공'}`);
     Console.print(`총 시도한 횟수: ${count}`);
   },
