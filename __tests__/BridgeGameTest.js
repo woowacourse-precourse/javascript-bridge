9e63a404a2b8698f5eece9607811d9768004c947
Rki0 <pky00823@gmail.com>
2022-11-17 14:19:24 +0900

    test(BridgeGame): 메서드 인자 수정으로 인한 테스트 코드 수정
    해당 테스트를 수행하는 클래스와 메서드에 필요한 인자가 수정되었으므로,
    테스트 코드도 그에 알맞게 변경함.

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -2,7 +2,7 @@ const BridgeGame = require("../src/BridgeGame");
 
 describe("게임 진행 테스트", () => {
   test("플레이어의 움직임 테스트", () => {
-    const bridgeGame = new BridgeGame(["U", "D", "D"], "U");
-    expect(bridgeGame.move()).toBeTruthy();
+    const bridgeGame = new BridgeGame();
+    expect(bridgeGame.move(["U", "D", "D"], "U")).toBeTruthy();
   });
 });
