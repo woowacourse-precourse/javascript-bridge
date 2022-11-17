d726eba036b67aedaa8e9ed431e9a1cd808df5a1
kitez <duswl339@ajou.ac.kr>
2022-11-17 17:13:10 +0900

    feat: constant - MESSAGE 추가

---
+++ src/constant/message.js
@@ -0,0 +1,10 @@
+const MESSAGE = Object.freeze({
+    GAME_START : "다리 건너기 게임을 시작합니다.",
+    MOVE : "이동할 칸을 선택해주세요. (위: U, 아래: D)",
+    RE_GAME : "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
+    SUCCESS : "게임 성공 여부: 성공",
+    FAIL : "게임 성공 여부: 실패",
+    GAME_RESULT : "총 시도한 횟수: "
+});
+
+module.exports = MESSAGE;
