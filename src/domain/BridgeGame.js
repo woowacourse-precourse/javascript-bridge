5a5ace576a02a17fee483cde480e5c4f2b255641
hwanfront <kim015jh@gmail.com>
2022-11-17 16:15:33 +0900

    feat(BridgeGame): 게임 재시작 관련 메서드 추가

--- src/domain/BridgeGame.js
+++ src/domain/BridgeGame.js
@@ -1,9 +1,34 @@
+const Bridge = require('./bridge/Bridge');
+const { makeBridge } = require('../BridgeMaker');
+const { generate } = require('../BridgeRandomNumberGenerator');
+const CrossingBridge = require('./result/Crossingbridge');
+
 class BridgeGame {
-  move() {}
+  #bridge;
 
-  retry() {}
+  #crossingBridge;
 
-  quit() {}
+  execute(bridgeSize) {
+    const panels = makeBridge(bridgeSize, generate);
+    this.#bridge = new Bridge(panels);
+    this.start();
+  }
+
+  start() {
+    this.#crossingBridge = new CrossingBridge();
+  }
+
+  move(direction) {
+
+  }
+
+  retry() {
+    this.start();
+  }
+
+  quit() {
+
+  }
 }
 
 module.exports = BridgeGame;
