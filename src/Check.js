6fd5495b5a8dfeb37653287239ee93038ca0e50c
yeonsuBeak <soothbugger@gmail.com>
2022-11-17 15:14:19 +0900

    feat(bridge): 다리 길이가 범위를 벗어난 경우 예외 처리 기능 구현

--- src/Check.js
+++ src/Check.js
@@ -1,11 +1,21 @@
 const Check = {
   checkBrideSize(size) {
     this.checkNumber(size);
+    this.checkRange(size);
   },
 
   checkNumber(size) {
     if (isNaN(size)) {
       throw new Error("[ERROR] 다리 길이는 숫자여야 합니다.");
+    }
+  },
+
+  checkRange(size) {
+    const MINIMUM_SIZE = 3;
+    const MAXIMUM_SIZE = 20;
+
+    if (size < MINIMUM_SIZE || size > MAXIMUM_SIZE) {
+      throw new Error("[ERROR] 다리 길이는 3 이상 20 이하인 숫자여야 합니다.");
     }
   },
 };
