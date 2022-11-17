04116a0be7fbcda35729a06f9699730215bb0533
lsy20140 <lsy20140@naver.com>
2022-11-18 02:05:26 +0900

    feat: 생성할 다리의 길이를 입력 받는다.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 const {Console} = require("@woowacourse/mission-utils");
+const InputView = require("./InputView");
 
 class App {
 
   play() {
     Console.print('다리 건너기 게임을 시작합니다.');
 
+    const size = InputView.readBridgeSize();
   }
 }
 
