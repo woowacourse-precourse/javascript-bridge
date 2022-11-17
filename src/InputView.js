68f72bb224629111dd29c3ad39ec38497ce564f6
jiho1996 <jhp9270@naver.com>
2022-11-17 20:46:35 +0900

    feat : 다리 길이 입력 받기

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,4 @@
+const { readLine } = require("./utils");
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,7 +6,9 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    readLine("다리의 길이를 입력해주세요.", bridgeLengthFromUser => {});
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
