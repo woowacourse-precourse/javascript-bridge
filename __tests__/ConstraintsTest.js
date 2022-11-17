729714cc8752a0237452fe5c8c6cc93a18810f2b
Rki0 <pky00823@gmail.com>
2022-11-17 14:43:16 +0900

    test(Constraints): 게임 커맨드 예외처리 테스트 추가
    게임 커맨드에 올바르지 않은 값이 입력된 경우,
    예외처리가 진행되는지 테스트하는 코드 추가.

--- __tests__/ConstraintsTest.js
+++ __tests__/ConstraintsTest.js
@@ -1,4 +1,8 @@
-const { SizeConstraints, MoveConstraints } = require("../src/Constraints");
+const {
+  SizeConstraints,
+  MoveConstraints,
+  CommandConstraints,
+} = require("../src/Constraints");
 
 describe("제한 조건 테스트", () => {
   test("숫자가 아닌 다리 길이 입력값 테스트", () => {
@@ -27,5 +31,12 @@ describe("제한 조건 테스트", () => {
       const moveConstraints = new MoveConstraints("A");
       moveConstraints.checkInputUorD();
     }).toThrow("[ERROR] U 혹은 D만 입력 가능합니다.");
+  });
+
+  test("게임 커맨드는 R과 Q만 받을 수 있다.", () => {
+    expect(() => {
+      const commandConstraints = new CommandConstraints("U");
+      commandConstraints.checkInputRorQ();
+    }).toThrow("[ERROR] R 혹은 Q만 입력 가능합니다.");
   });
 });
