34fe060c510c83b05e9e15fd64ccd9412b6ed20a
Narcoker <jykim9868@gmail.com>
2022-11-17 18:58:04 +0900

    Feat: 잘못된 이동 값일 경우 재시도 기능 및 이동 경로 출력 기능 추가

--- src/InputView.js
+++ src/InputView.js
@@ -13,8 +13,7 @@ const InputView = {
   readBridgeSize(bridgeGame) {
     MissionUtils.Console.readLine(MESSAGE.ENTER_BRIDGE_SIZE, (size) => {
       if (!bridgeGame.vaildateBridgeSize(size)) {
-        this.readBridgeSize(bridgeGame);
-        return;
+        return this.readBridgeSize(bridgeGame);
       }
 
       bridgeGame.setBridge(new Bridge(size));
@@ -24,26 +23,33 @@ const InputView = {
 
   },
 
+
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving(bridgeGame) {
     MissionUtils.Console.readLine(MESSAGE.ENTER_MOVE_TYPE, (moveType) => {
+      if (!bridgeGame.validateMoveType(moveType))
+        return this.readMoving(bridgeGame)
+
       bridgeGame.move(moveType);
-      OutputView.printMap(bridgeGame, moveType);
+      OutputView.printMap(bridgeGame);
+
       let maxPosition = bridgeGame.getBridge().length - 1;
       let position = bridgeGame.getPosition();
-      if (position < maxPosition)
-        this.readMoving(bridgeGame);
-      if (position === maxPosition)
-        console.log('게임 끝');
+
+      position === maxPosition
+        ? console.log('게임 끝')
+        : this.readMoving(bridgeGame);
     })
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() { },
+  readGameCommand() {
+    console.log("다시시도");
+  },
 };
 
 module.exports = InputView;
