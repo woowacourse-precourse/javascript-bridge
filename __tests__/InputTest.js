6ba74fbad576c3982a9d80b913d2ad14b1497a25
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-17 12:02:42 +0900

    test: 다리 길이 입력값 테스트 작성 ( 숫자 확인 )

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -4,7 +4,11 @@ const ERROR_MESSAGES = require("../src/constants/ErrorMessages");
 describe("입력값 테스트", () => {
   const inputView = new InputView();
 
-  test("다리 길이 입력값 테스트", () => {
-    expect(inputView.readBridgeSize("ㅁ")).toThrow(ERROR_MESSAGES.NAN);
+  test("다리 길이 입력값 테스트 ( 문자 )", () => {
+    expect(inputView.readBridgeSize("a")).toThrow(ERROR_MESSAGES.NAN);
+  });
+
+  test("다리 길이 입력값 테스트 ( 특수문자 )", () => {
+    expect(inputView.readBridgeSize("@")).toThrow(ERROR_MESSAGES.NAN);
   });
 });
