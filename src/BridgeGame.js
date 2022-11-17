00e5b4bfdd89add95f15b27f3920ffdecbdc8898
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-17 16:29:59 +0900

    feat: 다리 길이 입력값 유무 검사 메서드 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,20 +1,26 @@
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
+const { makeBridge } = require('./BridgeMaker.js');
+const { generate } = require('./BridgeRandomNumberGenerator.js');
+
 class BridgeGame {
-  /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
+  #bridge;
+
+  buildBridge(size) {
+    this.validateSize(size);
+  }
+
   move() {}
 
-  /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
   retry() {}
+
+  validateSize(size) {
+    if (this.isEmpty(size)) {
+      throw new Error('[ERROR] 입력값이 비었습니다. 값을 입력해주세요');
+    }
+  }
+
+  isEmpty(size) {
+    return !size.length;
+  }
 }
 
 module.exports = BridgeGame;
--- src/InputView.js
+++ src/InputView.js
@@ -1,12 +1,17 @@
 const { Console } = require('@woowacourse/mission-utils');
 const { REQUEST_MSG } = require('./constants/message.js');
+const BridgeGame = require('./BridgeGame.js');
+
+const bridgeGame = new BridgeGame();
 
 const InputView = {
   requestBridgeSize() {
     Console.readLine(REQUEST_MSG.bridgeSize, InputView.readBridgeSize);
   },
 
-  readBridgeSize(size) {},
+  readBridgeSize(size) {
+    bridgeGame.buildBridge(size);
+  },
 
   readMoving() {},
 
