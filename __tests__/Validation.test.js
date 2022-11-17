85273bf3abcd49f64896e159b23d0e53df115bdf
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-11-17 15:09:00 +0900

    test(Validation): 범위 파악 함수 테스트
    메소드 이름은 "range"로 정의된다.

--- __tests__/Validation.test.js
+++ __tests__/Validation.test.js
@@ -17,3 +17,11 @@ describe('숫자 예외 파악 함수 테스트', () => {
     }).toThrow(NUMBER_ERROR_TEXT);
   });
 });
+
+describe('범위 파악 함수 테스트', () => {
+  test('메소드 이름은 "range"로 정의된다.', () => {
+    const METHOD_NAME = 'range';
+
+    expect(Validation.range.name).toEqual(METHOD_NAME);
+  });
+});
