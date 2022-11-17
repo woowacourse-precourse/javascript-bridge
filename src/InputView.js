15b3f68cda4b1d70e59d9a254d2dde742256c3ff
seoyeon <wjstjdus96@naver.com>
2022-11-17 20:31:06 +0900

    feat: 사용자가 이동할 칸 입력받는 기능

--- src/InputView.js
+++ src/InputView.js
@@ -3,6 +3,7 @@
  */
 const { Console, Random } = require("@woowacourse/mission-utils");
 const ENTER_BRIDGELENGTH = "다리의 길이를 입력해주세요.";
+const ENTER_MOVING = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
 
 const InputView = {
   /**
@@ -18,7 +19,12 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    Console.readLine(ENTER_MOVING, (answer) => {
+      console.log("선택한 이동할 칸: " + answer);
+      return answer;
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
