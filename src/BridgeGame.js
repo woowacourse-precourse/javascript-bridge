b5d409fff2537fac81d9e9c60b6c3d286e131780
Sim <zh025700@naver.com>
2022-11-17 19:27:37 +0900

    feat(InputView): InputView 함수 추가
    유저가 건널 다리 선택 입력 함수 추가

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -12,7 +12,9 @@ class BridgeGame {
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-
+  constructor(bridge) {
+    this.bridge = bridge;
+  }
   move() {}
 
   /**
--- src/InputView.js
+++ src/InputView.js
@@ -1,4 +1,5 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const BridgeGame = require("./BridgeGame");
 const BridgeMaker = require("./BridgeMaker");
 const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
 /**
@@ -6,7 +7,12 @@ const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
  */
 const BRIDGELENGTH_ERR_MESSAGE =
   "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+
+const UserSelect_ERR_MESSAGE = "[ERROR] 입력은 U 아니면 D여야 합니다.";
+
 const INPUT_BRIDGE_LEN_STR = "다리의 길이를 입력해주세요.\n";
+
+const INPUT_USER_GO = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
 
 const InputView = {
   /**
@@ -15,14 +21,21 @@ const InputView = {
   readBridgeSize() {
     MissionUtils.Console.readLine(INPUT_BRIDGE_LEN_STR, (bridgeLen) => {
       bridgeLenValidator(bridgeLen);
-      BridgeMaker.makeBridge(bridgeLen, BridgeRandomNumberGenerator.generate);
+      const bridgeGame = new BridgeGame(
+        BridgeMaker.makeBridge(bridgeLen, BridgeRandomNumberGenerator.generate)
+      );
+      this.readMoving(bridgeGame);
     });
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving(bridgeGame) {
+    MissionUtils.Console.readLine(INPUT_BRIDGE_LEN_STR, (selectBridge) => {
+      userMoveValidator(selectBridge);
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
@@ -45,6 +58,17 @@ const isIntNumber = (bridgeLen) => {
   if (isNaN(parseInt(bridgeLen)) || !Number.isInteger(parseFloat(bridgeLen))) {
     throw new Error(BRIDGELENGTH_ERR_MESSAGE);
   }
+};
+
+const userMoveValidator = (input) => {
+  isUandD(input);
+};
+
+const isUandD = (input) => {
+  if (input == "U" || input == "D") {
+    return;
+  }
+  throw new Error(UserSelect_ERR_MESSAGE);
 };
 
 module.exports = InputView;
