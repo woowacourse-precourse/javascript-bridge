7df3049dedfe28f14d575d6d71d61c0a6abe7380
pinkishincoloragain <smb1103@gmail.com>
2022-11-18 03:49:44 +0900

    refactor: OutputView 메소드가 history, bridge를 인자로 받지 않고  game 객체로 받는 것으로 변경

--- src/OutputView.js
+++ src/OutputView.js
@@ -12,15 +12,15 @@ const OutputView = {
   },
   /**
    * 현재까지 이동한 다리의 상태를 생성한다.
-   * @param {string[]} bridge 다리
-   * @param {boolean[]} history 각 이동의 성공 여부를 기록한 배열
+   * @param {BridgeGame} game 다리 건너기 게임
    */
-  makeMap(bridge, history) {
-    const map = { ups: [], downs: [] };
+  makeMap(game) {
+    const map = Array.from({ length: 2 }, () => Array(game.history.length).fill(MAP.EMPTY));
 
-    history.forEach((isSuccess, idx) => {
-      map.ups.push(bridge[idx] === MOVE.UP ? (isSuccess ? MAP.SUCCESS : MAP.FAIL) : MAP.EMPTY);
-      map.downs.push(bridge[idx] === MOVE.DOWN ? (isSuccess ? MAP.SUCCESS : MAP.FAIL) : MAP.EMPTY);
+    game.history.forEach((isSuccess, idx) => {
+      const bridgePosition = game.bridge[idx] === MOVE.UP ? 0 : 1;
+      if (isSuccess === true) map[bridgePosition][idx] = MAP.SUCCESS;
+      else map[1 - bridgePosition][idx] = MAP.FAIL;
     });
 
     return map;
@@ -28,29 +28,28 @@ const OutputView = {
 
   /**
    * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-   * @param {string[]} bridge 다리
-   * @param {boolean[]} history 각 이동의 성공 여부를 기록한 배열
+   * @param {BridgeGame} game 다리 건너기 게임
    */
-  printMap(bridge, history) {
-    const map = this.makeMap(bridge, history);
-    Object.values(map).forEach((row) => {
+  printMap(game) {
+    const map = this.makeMap(game);
+    map.forEach((row) => {
       Console.print(`${MAP.START}${row.join(MAP.DELIMITER)}${MAP.END}`);
     });
   },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-   * @param {string[]} bridge 다리
-   * @param {boolean[]} history 각 이동의 성공 여부를 기록한 배열
+   * @param {BridgeGame} game 다리 건너기 게임
    */
-  printResult(bridge, history) {
-    const isSuccess = history.every((isSuccess) => isSuccess);
+  printResult(game) {
+    const isSuccess = game.history.every((isSuccess) => isSuccess);
     const result = isSuccess ? PHRASE.SUCCESS : PHRASE.FAIL;
-    const totalTry = history.length;
+    const totalTry = game.resetCount + 1;
 
     Console.print(`${PHRASE.RESULT}`);
-    this.printMap(bridge, history);
-    Console.print(`${result}${PHRASE.TRY}${totalTry}`);
+    this.printMap(game);
+    Console.print(`${result}${PHRASE.TOTAL_TRY}${totalTry}`);
+    Console.close();
   },
 };
 
