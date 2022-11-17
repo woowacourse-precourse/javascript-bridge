fa765ab1578d0c22dd94acf6474264357b8f3b6f
chiman <rlaclghks123@naver.com>
2022-11-17 10:57:01 +0900

    feat: 이동할 칸을 입력받는 부분 구현
    1. 유효성 검사
    2. 에러시 throw문을 통해 에러를 던지고, 다시 이동할 칸을 입력받도록 try catch 사용

--- src/InputView.js
+++ src/InputView.js
@@ -11,19 +11,26 @@ const InputView = {
   readBridgeSize() {
     Console.readLine('\n다리의 길이를 입력해주세요.\n', (size) => {
       try {
-        Validate.validateSizeRange(size);
-        InputView.readMoving(size);
+        Validate.validateSizeRange(size) && InputView.readMoving(size);
       } catch (error) {
-        OutputView.printErrorMessage(error);
-        InputView.readBridgeSize();
+        OutputView.printErrorMessage(error) || InputView.readBridgeSize();
       }
+      InputView.readMoving(size);
     });
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving(size) {},
+  readMoving(size) {
+    Console.readLine('\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n', (movePosition) => {
+      try {
+        Validate.validateMovePosition(movePosition);
+      } catch (error) {
+        OutputView.printErrorMessage(error) || InputView.readMoving(size);
+      }
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
--- src/utils/Validate.js
+++ src/utils/Validate.js
@@ -1,7 +1,14 @@
 const Validate = {
   validateSizeRange(size) {
     if (size < 3 || size > 20 || !new RegExp('^[0-9]+$').test(size)) {
-      throw '[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.';
+      throw new Error('[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.');
+    }
+    return true;
+  },
+
+  validateMovePosition(movePosition) {
+    if (movePosition !== 'U' && movePosition !== 'D') {
+      throw new Error('[ERROR] 이동할 칸은 위 : U 또는 아래 : D인 문자로 입력해주세요.');
     }
     return true;
   },
