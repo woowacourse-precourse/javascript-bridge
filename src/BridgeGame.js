8d2f00ececd75a234984a345770168d2c28864be
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-17 17:58:18 +0900

    refactor: 테스트 케이스 통과를 위해 다리 만드는 함수 수정

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -18,14 +18,6 @@ class BridgeGame {
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   //여기서 반복문
-  wordConverse(word) {
-    if (word === "U") {
-      return 1;
-    }
-    if (word === "D") {
-      return 0;
-    }
-  }
 
   indexCountUp() {
     this.#indexCount = this.#indexCount + 1;
@@ -69,11 +61,11 @@ class BridgeGame {
   }
 
   move(safeBridge, userMove) {
-    if (safeBridge[this.#indexCount] !== this.wordConverse(userMove)) {
+    if (safeBridge[this.#indexCount] !== userMove) {
       this.fail(userMove);
       return false;
     }
-    if (safeBridge[this.#indexCount] === this.wordConverse(userMove)) {
+    if (safeBridge[this.#indexCount] === userMove) {
       this.pass(userMove);
       this.indexCountUp();
       return true;
--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -9,14 +9,19 @@ const BridgeMaker = {
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
   makeBridge(size, generateRandomNumber) {
-    let safeBridge = [];
+    let bridge = [];
     let i = 0;
     for (; i < size; i++) {
-      const number = generateRandomNumber();
+      const number = String(generateRandomNumber());
 
-      safeBridge.push(number);
+      if (number === "1") {
+        bridge.push("U");
+      }
+      if (number === "0") {
+        bridge.push("D");
+      }
     }
-    return safeBridge;
+    return bridge;
   },
 };
 
