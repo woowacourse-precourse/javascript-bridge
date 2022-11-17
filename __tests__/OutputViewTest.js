e011665d53e6d0a3135a691fbb34c93bb02ffd90
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-11-17 17:17:12 +0900

    Test : make printResult test

--- __tests__/OutputViewTest.js
+++ __tests__/OutputViewTest.js
@@ -18,6 +18,12 @@ const expectBridgeOrder = (received, upside, downside) => {
   expect(upsideIndex).toBeLessThan(downsideIndex);
 };
 
+const expectLogContains = (received, logs) => {
+  logs.forEach((log) => {
+    expect(received).toEqual(expect.stringContaining(log));
+  });
+};
+
 describe("출력 테스트", () => {
   test("다리 출력 테스트 1", () => {
     const logSpy = getLogSpy();
@@ -30,5 +36,32 @@ describe("출력 테스트", () => {
     OutputView.printMap(["U", "D", "U"], ["U", "D", "D"]);
     const log = getOutput(logSpy);
     expectBridgeOrder(log, "[ O |   |   ]", "[   | O | X ]");
+  });
+  describe("성공여부 테스트", () => {
+    test("실패 잘나오나 테스트", () => {
+      const logSpy = getLogSpy();
+      OutputView.printIsSuccess(["U", "D", "U"], ["U", "D", "D"]);
+      const log = getOutput(logSpy);
+      expectLogContains(log, ["게임 성공 여부: 실패"]);
+    });
+    test("성공 잘나오나 테스트", () => {
+      const logSpy = getLogSpy();
+      OutputView.printIsSuccess(["U", "D", "U"], ["U", "D", "U"]);
+      const log = getOutput(logSpy);
+      expectLogContains(log, ["게임 성공 여부: 성공"]);
+    });
+  });
+  test("결과 출력 테스트", () => {
+    const logSpy = getLogSpy();
+    OutputView.printResult(["U", "D", "U"], ["U", "D", "U"]);
+    const log = getOutput(logSpy);
+
+    expectLogContains(log, [
+      "최종 게임 결과",
+      "[ O |   | O ]",
+      "[   | O |   ]",
+      "게임 성공 여부: 성공",
+      "총 시도한 횟수: 1",
+    ]);
   });
 });
