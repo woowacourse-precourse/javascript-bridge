e821fb3174c4b84c3168114f8d7ffebfd0b78023
Rki0 <pky00823@gmail.com>
2022-11-17 14:50:07 +0900

    test(utils): 게임 커맨드 입력값 검증 테스트 추가
    게임 커맨드 입력값이 제한사항을 만족하지 못했을 경우,
    예외 처리가 되는지 테스트를 진행한다.

--- __tests__/utilsTest.js
+++ __tests__/utilsTest.js
@@ -1,4 +1,4 @@
-const { BridgeSize, MoveInput } = require("../src/utils");
+const { BridgeSize, MoveInput, CommandInput } = require("./Constraints");
 
 describe("제한 조건 테스트", () => {
   test("다리 길이 입력시 유효성 검사 진행 여부 테스트", () => {
@@ -18,5 +18,11 @@ describe("제한 조건 테스트", () => {
     expect(() => {
       new MoveInput("A");
     }).toThrow("[ERROR] U 혹은 D만 입력 가능합니다.");
+  });
+
+  test("게임 커맨드 유효성 검사 진행 여부 테스트", () => {
+    expect(() => {
+      new CommandInput("U");
+    }).toThrow("[ERROR] R 혹은 Q만 입력 가능합니다.");
   });
 });
