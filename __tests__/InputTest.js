ccdf8622130cdae790e75798274a342d8617c7a2
khj0426 <706shin1728@naver.com>
2022-11-18 00:15:07 +0900

    test:다리 길이 입력 테스트코드 수정

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -1,36 +1,40 @@
+const { BRIDGE_INPUT_ERROR } = require('../src/MESSAGES/InputMessage');
 const InputError = require('../src/InputErrorView');
-const BridgeInputError = require('../src/MESSAGES/InputMessage');
 
-const bridgeerr = new Error(BridgeInputError.BRIDGE_INPUT_ERROR);
 test('다리 입력 오류 테스트', () => {
-  inputlog = [
-    '1000',
-    '3',
-    '15',
-    '20',
+  const inputlog = [
+    '41123213123421',
+    '123',
     '2',
     '21',
     '',
-    '우아한 테크 코스',
-    '-1',
-    '+++++3333',
+    '-2',
+    '-5',
+    '문자열',
   ];
-  inputexpected = [
-    bridgeerr,
-    true,
-    true,
-    true,
-    true,
-    bridgeerr,
-    bridgeerr,
-    bridgeerr,
-    bridgeerr,
-    bridgeerr,
-  ];
-
-  inputlog.forEach((eachinput, index) => {
+  inputlog.forEach((eachlog) => {
     expect(() => {
-      InputError.bridgeSizeError(eachinput).toBe(inputexpected[index]);
+      InputError.bridgeSizeError(eachlog);
+    }).toThrow();
+  });
+});
+
+/*
+test('게임 재시작,종료 입력 유효 테스트', () => {
+  regameinputlog = ['R', 'Q', 'RR', 'C', 'Z', '', 'WOWOWOWOW'];
+  regameinputlogexpected = [
+    true,
+    false,
+    false,
+    regameerr,
+    regameerr,
+    regameerr,
+    regameerr,
+  ];
+  regameinputlog.forEach((eachinput, index) => {
+    expect(() => {
+      InputError.reGameError(eachinput).toBe(regameinputlogexpected[index]);
     });
   });
 });
+*/
