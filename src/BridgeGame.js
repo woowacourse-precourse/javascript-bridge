d6a07dbb1fc7b6905aeec7f039dcb81b6e95d897
evencoding <evencoding@gmail.com>
2022-11-18 00:00:26 +0900

    feat: 건널 수 있는 다리인지 체크하는 기능 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,23 +1,25 @@
-const GameController = require('./controllers/GameController');
-const PlayerController = require('./controllers/PlayerController');
+const { DIRECTION } = require('./constant');
+const Player = require('./Player');
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
-  #gameCtrl;
-  #playerCtrl;
+  #player;
 
   constructor() {
-    this.#gameCtrl = new GameController();
-    this.#playerCtrl = new PlayerController();
+    this.#player = new Player();
   }
-
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(direction, bridge) {
+    const directionNumber = DIRECTION[direction];
+    const nextBridgeNumber = bridge[this.#player.getCurPlace];
+
+    return directionNumber === nextBridgeNumber;
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
