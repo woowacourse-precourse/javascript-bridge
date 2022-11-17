fd059a2ca454a478494ba4381026d39cb5b2a4a7
Jeong seulho <wjdtmfgh@yu.ac.kr>
2022-11-18 00:46:41 +0900

    Feat(feature No.4): define printMap and method in OutputView

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,13 +1,55 @@
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
+const MissionUtils = require("@woowacourse/mission-utils");
+const BridgeGame = require("./BridgeGame");
+
 const OutputView = {
-	/**
-	 * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-	 * <p>
-	 * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-	 */
-	printMap() {},
+	CORRECT_BRIDGE_ELE: " O ",
+	WRONG_BRIDGE_ELE: " x ",
+	EMPTY_BRIDGE_ELE: "   ",
+
+	UPPER_BRIDGE: {
+		CORRECT: "UO",
+		WRONG: "UX",
+	},
+	LOWER_BRIDGE: {
+		CORRECT: "DO",
+		WRONG: "DX",
+	},
+
+	printMap(currentBridge) {
+		const upperArr = OutputView.getOneSideArr(
+			currentBridge,
+			OutputView.UPPER_BRIDGE,
+		);
+		const lowerArr = OutputView.getOneSideArr(
+			currentBridge,
+			OutputView.LOWER_BRIDGE,
+		);
+		const upperMap = OutputView.convertArrToMap(upperArr);
+		const lowerMap = OutputView.convertArrToMap(lowerArr);
+
+		MissionUtils.Console.print(upperMap + "\n" + lowerMap);
+	},
+
+	getOneSideArr(currentBridge, ONE_SIDE_BRIDGE) {
+		return currentBridge.map((bridgeEle) => {
+			if (bridgeEle === ONE_SIDE_BRIDGE.CORRECT) {
+				return OutputView.CORRECT_BRIDGE_ELE;
+			}
+			if (bridgeEle === ONE_SIDE_BRIDGE.WRONG) {
+				return OutputView.WRONG_BRIDGE_ELE;
+			}
+			return OutputView.EMPTY_BRIDGE_ELE;
+		});
+	},
+
+	// 정해진 출력형태로 바꾸어주는 함수
+	convertArrToMap(arr) {
+		const map = JSON.stringify(arr);
+
+		const quotationRegExp = new RegExp('"', "g");
+		const commaRegExp = new RegExp(",", "g");
+		return map.replace(quotationRegExp, "").replace(commaRegExp, "|");
+	},
 
 	/**
 	 * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
