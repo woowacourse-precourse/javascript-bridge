775449bae636d2fe405c90516e8e6f062c2012d7
dlwl98 <ff981113@hanyang.ac.kr>
2022-11-17 22:25:33 +0900

    refactor(Controller): private class 필드, 메서드로 변경

--- src/Controller.js
+++ src/Controller.js
@@ -3,47 +3,53 @@ const InputView = require('./InputView');
 const OutputView = require('./OutputView');
 
 class Controller {
+  #tryCount;
+  #bridgeGame;
+
+  constructor() {
+    this.#tryCount = 1;
+  }
+
   handleGameStart() {
     OutputView.printStart();
-    this.count = 1;
-    this.handleMakeBridge();
+    this.#handleMakeBridge();
   }
 
-  handleMakeBridge() {
+  #handleMakeBridge() {
     InputView.readBridgeSize((input) => {
-      this.bridgeGame = new BridgeGame(input);
+      this.#bridgeGame = new BridgeGame(input);
       OutputView.printLineBreak();
-      this.handleMove();
+      this.#handleMove();
     });
   }
 
-  handleMove() {
+  #handleMove() {
     InputView.readMoving((input) => {
-      const { resultToString, gameStatus } = this.bridgeGame.move(input);
+      const { resultToString, gameStatus } = this.#bridgeGame.move(input);
       OutputView.printMap(resultToString);
-      if (gameStatus === 2) this.handleGameEnd();
-      if (gameStatus === 1) this.handleGameOver();
-      if (gameStatus === 0) this.handleMove();
+      if (gameStatus === 2) this.#handleGameEnd();
+      if (gameStatus === 1) this.#handleGameOver();
+      if (gameStatus === 0) this.#handleMove();
     });
   }
 
-  handleGameOver() {
+  #handleGameOver() {
     InputView.readGameCommand((input) => {
-      if (input === 'R') this.handleGameRetry();
-      if (input === 'Q') this.handleGameEnd();
+      if (input === 'R') this.#handleGameRetry();
+      if (input === 'Q') this.#handleGameEnd();
     });
   }
 
-  handleGameRetry() {
-    this.bridgeGame.retry();
-    this.count += 1;
-    this.handleMove();
+  #handleGameRetry() {
+    this.#bridgeGame.retry();
+    this.#tryCount += 1;
+    this.#handleMove();
   }
 
-  handleGameEnd() {
-    const { resultToString } = this.bridgeGame.getResultMap();
-    const gameStatus = this.bridgeGame.getGameStatus();
-    OutputView.printResult(resultToString, gameStatus, this.count);
+  #handleGameEnd() {
+    const { resultToString } = this.#bridgeGame.getResultMap();
+    const gameStatus = this.#bridgeGame.getGameStatus();
+    OutputView.printResult(resultToString, gameStatus, this.#tryCount);
   }
 }
 
