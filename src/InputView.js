ac98920af40571cd142275acb0b9fed355cd8d6d
pinkishincoloragain <smb1103@gmail.com>
2022-11-18 03:35:37 +0900

    refactor: Input이 Promise를 리턴하게 변경

--- src/InputView.js
+++ src/InputView.js
@@ -1,5 +1,5 @@
 const { Console } = require('@woowacourse/mission-utils');
-const { PHRASE } = require('./config');
+const { RETRY, PHRASE } = require('./config');
 const { validateBridgeSize, validateMove, validateRestart } = require('./validate');
 
 /**
@@ -9,31 +9,37 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize(callback) {
-    Console.readLine(PHRASE.BRIDGE_LEN, (length) => {
-      validateBridgeSize(length);
-      callback(budget);
-    });
+  readBridgeSize() {
+    return new Promise((resolve) =>
+      Console.readLine(PHRASE.BRIDGE_LEN, (length) => {
+        validateBridgeSize(length);
+        resolve(length);
+      }),
+    );
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving(callback) {
-    Console.readLine(PHRASE.SELECT, (move) => {
-      validateMove(move);
-      callback(move);
-    });
+  readMoving() {
+    return new Promise((resolve) =>
+      Console.readLine(PHRASE.SELECT, (move) => {
+        validateMove(move);
+        resolve(move);
+      }),
+    );
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand(callback) {
-    Console.readLine(PHRASE.RESTART, (restart) => {
-      validateRestart(restart);
-      callback(restart);
-    });
+  readGameCommand() {
+    return new Promise((resolve) =>
+      Console.readLine(PHRASE.RESTART, (retry) => {
+        validateRestart(retry);
+        resolve(retry === RETRY.YES);
+      }),
+    );
   },
 };
 
