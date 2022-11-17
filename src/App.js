126d4d97436611c52da1b35cee7f5d6b6dc4b945
Cobocho <rlaalsrb1111@naver.com>
2022-11-18 00:16:49 +0900

    feat: 게임 종료 조건 판단 메서드 구현

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ const Validator = require('./Utils/Validator');
 const MissionUtils = require("@woowacourse/mission-utils");
 const OutputView = require('./OutputView');
 const InputView = require('./InputView');
+const { BRIDGE_REQUIREMENTS } = require('./constants');
 
 class App {
   play() {
@@ -29,10 +30,21 @@ class App {
       Validator.directionCheck(direction);
       this.game.move(direction);
       OutputView.printMap(this.game.course);
+      this.gameEndCheck();
     } catch(err) {
       MissionUtils.Console.print(err);
       this.moveSpace();
     }
+  }
+
+  gameEndCheck() {
+    const lastTrace = this.game.course[this.game.course.length - 1];
+    if([BRIDGE_REQUIREMENTS.LOWER_FAILED_CODE, BRIDGE_REQUIREMENTS.UPPER_FAILED_CODE].includes(lastTrace) ||
+      this.game.course.length === this.game.bridge.length) {
+      console.log('게임 종료');
+      return MissionUtils.Console.close();
+    }
+    this.moveSpace();
   }
 }
 
