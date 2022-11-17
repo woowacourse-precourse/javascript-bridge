cb90350005f876f343614181a6a09e272c4aaa97
Jeong seulho <wjdtmfgh@yu.ac.kr>
2022-11-17 23:28:19 +0900

    Test: add test code to check moving

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -9,4 +9,13 @@ describe("input 유효성 테스트", () => {
 			}).toThrow("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
 		},
 	);
+
+	test.each([["2"], ["21"], ["-1"], ["d"]])(
+		"moving이 D 또는 U가아니면 예외 발생",
+		(moving) => {
+			expect((moving) => {
+				Validation.validateMoving(moving);
+			}).toThrow("[ERROR] 이동할 칸은 대문자 D 또는 대문자 U 입니다.");
+		},
+	);
 });
