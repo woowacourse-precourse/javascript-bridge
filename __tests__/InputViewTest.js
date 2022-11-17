c898a933a1f2fe10676408955f5b57b1991d3b9a
hahagarden <88613455+hahagarden@users.noreply.github.com>
2022-11-18 05:00:21 +0900

    test: 이동할 칸 입력 받기

--- __tests__/InputViewTest.js
+++ __tests__/InputViewTest.js
@@ -10,11 +10,24 @@ const mockReadLine = (inputs) => {
   }, Console.readLine);
 };
 
+const spy = jest.spyOn(Console, "print");
+
 describe("InputView 객체 테스트", () => {
+  beforeEach(() => {
+    spy.mockClear();
+  });
+
   test("유효한 다리의 길이가 입력될 때까지 예외를 발생시키고 유효한 값을 반환한다.", () => {
     mockReadLine(["d", "1", "3.14", "5"]);
-    const spy = jest.spyOn(Console, "print");
+
     expect(InputView.readBridgeSize()).resolves.toBe(5);
     expect(spy).toHaveBeenCalledTimes(3);
+  });
+
+  test("유효한 이동할 칸 값이 입력될 때까지 예외를 발생시키고 유효한 값을 반환한다.", () => {
+    mockReadLine(["0", "1", "f", "u", "U"]);
+
+    expect(InputView.readMoving()).resolves.toBe("U");
+    expect(spy).toHaveBeenCalledTimes(4);
   });
 });
