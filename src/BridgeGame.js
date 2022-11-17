54994aead6de8801e59fb91f1c7704950b70e1aa
soohyun <bmy1320@gmail.com>
2022-11-17 17:10:31 +0900

    feat(BridgeGame): 사용자 칸 이동 메서드 기능 추가
    1. move 메서드

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -6,13 +6,23 @@ const { SPACE } = require("./utils/constants");
  * BridgeGame 클래스에서 InputView, OutputView 를 사용하지 않는다.
  */
 class BridgeGame {
-  constructor(stage, bridge) {}
+  #stage;
+
+  #bridge;
+
+  constructor(stage, bridge) {
+    this.#stage = stage;
+    this.#bridge = bridge;
+  }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move(nowStep, direction) {}
+  move(nowStep, direction) {
+    const result = this.#bridge[nowStep][SPACE[direction]];
+    OutputView.printMap(nowStep, this.#bridge, result);
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
