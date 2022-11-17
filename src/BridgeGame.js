4309c170b73535a6cbdac45b52edc29e7e5c6c92
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-17 17:26:34 +0900

    feat: 유효한 다리 길이인지 검사하는 메서드 추가

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,6 +1,7 @@
 const { makeBridge } = require('./BridgeMaker.js');
 const { generate } = require('./BridgeRandomNumberGenerator.js');
 const { ERROR_MSG } = require('./constants/message.js');
+const { BRIDGE_SIZE_MIN_RANGE, BRIDGE_SIZE_MAX_RANGE } = require('./constants/condition.js');
 
 class BridgeGame {
   #bridge;
@@ -25,6 +26,10 @@ class BridgeGame {
     if (this.isStartedZero(size)) {
       throw new Error(ERROR_MSG.isStartedZero);
     }
+
+    if (!this.isValideBridgeSizeRange(size)) {
+      throw new Error(ERROR_MSG.invalidInputRange);
+    }
   }
 
   isEmpty(size) {
@@ -40,6 +45,10 @@ class BridgeGame {
 
   isStartedZero(size) {
     return size.startsWith('0');
+  }
+
+  isValideBridgeSizeRange(size) {
+    return BRIDGE_SIZE_MIN_RANGE <= size && size <= BRIDGE_SIZE_MAX_RANGE;
   }
 }
 
