b48f65d5463f45232f40efb89870511ad0c46cbd
sungjihyun <jhsung23@gmail.com>
2022-11-17 21:16:52 +0900

    feat: 게임 시작 문구 출력 기능

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,7 +1,21 @@
+/*
+제공된 OutputView 객체를 활용해 구현해야 한다.
+OutputView의 `파일 경로`는 변경할 수 있다. 🙆‍♂️
+OutputView의 `메서드의 이름`은 변경할 수 없다. 🙅‍♀️
+`인자`는 필요에 따라 추가하거나 변경할 수 있다. 🙆‍♂️
+값 출력을 위해 필요한 `메서드`를 추가할 수 있다. 🙆‍♂️
+ */
+
+const { Console } = require('@woowacourse/mission-utils');
+
+const { GAME_START } = require('./utils/constants');
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 const OutputView = {
+  printStart() {
+    Console.print(GAME_START);
+  },
   /**
    * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
    * <p>
---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+const GAME_START = '다리 건너기 게임을 시작합니다.';
+const READ_MOVING = '이동할 칸을 선택해주세요. (위: U, 아래: D)';
+const READ_GAME_COMMAND = '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)';
+
+module.exports = {
+  GAME_START,
+  READ_MOVING,
+  READ_GAME_COMMAND,
+};
