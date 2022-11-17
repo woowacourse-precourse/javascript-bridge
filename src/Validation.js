560c7bf6521693f4b605696c06792611c6801660
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-11-18 03:04:16 +0900

    refactor(Validation): 숫자 예외 파악 함수

--- src/Validation.js
+++ src/Validation.js
@@ -2,8 +2,13 @@ const Validation = {
   VALIDATION_NUMBER_TEXT: '[ERROR] 전달된 인수는 숫자 타입이 아닙니다.',
   RANGE_ERROR_TEXT: '[ERROR] 지정한 범위의 값이 아닙니다.',
   FUNCTION_ERROR_TEXT: '[ERROR] 전달된 인수는 함수가 아닙니다.',
+
+  throwError(errorMessage) {
+    throw new Error(errorMessage);
+  },
+
   number(target) {
-    if (Number.isNaN(target)) {
+    if (Number.isNaN(target) || typeof target !== 'number') {
       throw new TypeError(Validation.VALIDATION_NUMBER_TEXT);
     }
   },
