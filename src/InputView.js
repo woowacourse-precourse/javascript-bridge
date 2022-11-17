34438d5004378691127e3f019439e01138c004af
Hchan2 <seongchan_@naver.com>
2022-11-17 18:47:14 +0900

    기본적인 구성

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,9 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const { BRIDGE_LENGTH, MOVE_SPACE, RE_OR_END } = require('./constant/inputMessage');
+const inputLengthCheck = require('./error/lengthInputError');
+const moveInputCheck = require('./error/moveInputCheck');
+const reOrEndCheck = require('./error/reOrEndCheck');
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,17 +11,29 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    MissionUtils.Console.readLine(BRIDGE_LENGTH, (length) => {
+      inputLengthCheck(length);
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    MissionUtils.Console.readLine(MOVE_SPACE, (move) => {
+      moveInputCheck(move);
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+    MissionUtils.Console.readLine(RE_OR_END, (decision) => {
+      reOrEndCheck(decision);
+    });
+  },
 };
 
 module.exports = InputView;
