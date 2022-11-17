7195660c4dae00f9d4a2984b0825964fa01abf8e
devMingu <rooster100@naver.com>
2022-11-17 17:08:28 +0900

    feat: 입력받을 수 있는 간단한 기능 구현

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,6 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Message = require("./Message");
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,17 +8,22 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    MissionUtils.Console.readLine(`${Message.INPUT_MESSAGE.BRIDGE_SIZE_TEXT}`, (size) => {})
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    MissionUtils.Console.readLine(`${Message.INPUT_MESSAGE.MOVING_TEXT}`, (move) => {})
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+    MissionUtils.Console.readLine(`${Message.INPUT_MESSAGE.READ_GAME_COMMAND_TEXT}`, (command) => {})
+  },
 };
-
 module.exports = InputView;
