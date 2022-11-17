4a850718c93f4691ef44d245bdfbf5a5855c883c
Chanwoong1 <jcw7468@gmail.com>
2022-11-18 00:33:16 +0900

    chore: 코드 위치 수정

--- src/ValidateBridgeSize.js
+++ src/ValidateBridgeSize.js
@@ -1,7 +1,15 @@
 class ValidateBridgeSize {
   constructor(bridgeSize) {
-    if (this.validate(bridgeSize) === false) throw new Error("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
     this.bridgeSize = bridgeSize;
+  }
+
+  get bridgeSize() {
+    return this._bridgeSize;
+  }
+
+  set bridgeSize(bridgeSize) {
+    if (this.validate(bridgeSize) === false) throw new Error("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+    this._bridgeSize = Number(bridgeSize);
   }
 
   validate(bridgeSize) {
@@ -18,15 +26,6 @@ class ValidateBridgeSize {
   isNumber = (input) => !isNaN(input);
 
   isRange = (input) => (input >= 3 && input <= 20);
-
-  get bridgeSize() {
-    return this._bridgeSize;
-  }
-
-  set bridgeSize(bridgeSize) {
-    if (this.validate === false) throw new Error("[ERROR]");
-    this._bridgeSize = Number(bridgeSize);
-  }
 }
 
 module.exports = ValidateBridgeSize;
