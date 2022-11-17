664be18c81b89a9a563f8e0223aa9e2d98ba261c
Chanwoong1 <jcw7468@gmail.com>
2022-11-17 23:19:27 +0900

    feat: 다리 길이 입력 유효성 판단 구현

---
+++ src/ValidateBridgeSize.js
@@ -0,0 +1,32 @@
+class ValidateBridgeSize {
+  constructor(bridgeSize) {
+    if (this.validate(bridgeSize) === false) throw new Error("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+    this.bridgeSize = bridgeSize;
+  }
+
+  validate(bridgeSize) {
+    return (
+      !this.isBlank(bridgeSize) &&
+      this.isNumber(bridgeSize) &&
+      this.isRange(bridgeSize) &&
+      Number.isInteger(Number(bridgeSize))
+    )
+  }
+
+  isBlank = (input) => !input;
+
+  isNumber = (input) => !isNaN(input);
+
+  isRange = (input) => (input >= 3 && input <= 20);
+
+  get bridgeSize() {
+    return this._bridgeSize;
+  }
+
+  set bridgeSize(bridgeSize) {
+    if (this.validate === false) throw new Error("[ERROR]");
+    this._bridgeSize = bridgeSize;
+  }
+}
+
+module.exports = ValidateBridgeSize;
