2d924e892d69606e0fa68cad77c749b0a8cfa764
jinseo <ghkdwlstj99@gmail.com>
2022-11-18 02:02:51 +0900

    feat: 다리 생성 후 플레이어가 이동할 칸을 입력받는 기능 구현

--- src/InputView.js
+++ src/InputView.js
@@ -1,6 +1,8 @@
 const { Console } = require("@woowacourse/mission-utils");
 const { makeBridge } = require("./BridgeMaker");
 const { generate } = require("./BridgeRandomNumberGenerator");
+const BridgeGame = require("./BridgeGame");
+const isAnswerOrWrong = new BridgeGame;;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -19,7 +21,12 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    Console.readLine("이동할 칸을 선택해주세요. (위: U, 아래: D", (selectUpOrDown) => {
+      isAnswerOrWrong.move(this.createBridge, selectUpOrDown);
+      this.readMoving();
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
