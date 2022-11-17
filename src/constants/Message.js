8966eb811aff0defab4192513de108c77b000b62
dragonfruitlemonade <tpghks9245@gmail.com>
2022-11-17 12:28:18 +0900

    feat(Message): 메세지 상수 추가

---
+++ src/constants/Message.js
@@ -0,0 +1,16 @@
+const GAME_RESULT = '최종 게임 결과';
+const GAME_WIN = '게임성공 여부: 성공';
+const GAME_FAIL = '게임성공 여부: 실패';
+const GAME_ATTEMPT = '총 시도한 횟수:';
+
+const OUTPUT_MESSAGE = {
+    GAME_RESULT,
+    GAME_WIN,
+    GAME_FAIL,
+    GAME_ATTEMPT,
+}
+
+
+module.exports = {
+    OUTPUT_MESSAGE,
+}
