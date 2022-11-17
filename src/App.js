5a8016bbaa2bf73347d15a752b6fb9f1bc7ed48b
Chanwoong1 <jcw7468@gmail.com>
2022-11-17 21:50:42 +0900

    feat: 게임 시작 문구 출력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+const { Console } = require("@woowacourse/mission-utils");
+const InputView = require("./InputView");
+
 class App {
-  play() {}
+  constructor() {
+  }
+
+  play() {
+    Console.print("다리 건너기 게임을 시작합니다.\n");
+    InputView.readBridgeSize();
+  }
 }
 
 module.exports = App;
+
+new App().play();
