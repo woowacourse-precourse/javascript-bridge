6eb78e7c5ba94a148fa930606f393f825948def9
Mcdonalds Trump <whdmschd2@gmail.com>
2022-11-18 04:35:59 +0900

    test: 이동할 칸 입력 유효성 검사

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -3,7 +3,7 @@ const Validation = require("../src/Validation");
 describe("입력값 유효성 검사 테스트", () => {
   test("다리 사이즈 입력 유효성 검사", () => {
     const inputBridgeSize = {
-      correct: ["3", "11", , "15", "20"],
+      correct: ["3", "11", "15", "20"],
       incorrect: ["abc", "   ", "", "1", "100"],
     };
 
@@ -12,6 +12,20 @@ describe("입력값 유효성 검사 테스트", () => {
     });
     inputBridgeSize.incorrect.forEach((bridgeSize) => {
       expect(() => Validation.bridgeSize(bridgeSize)).toThrow();
+    });
+  });
+
+  test("이동할 칸 입력 유효성 검사", () => {
+    const inputMovingCommand = {
+      correct: ["U", "D"],
+      incorrect: ["u", "   ", "", "d", "100"],
+    };
+
+    inputMovingCommand.correct.forEach((bridgeSize) => {
+      expect(Validation.movingCommand(bridgeSize)).toBeUndefined();
+    });
+    inputMovingCommand.incorrect.forEach((bridgeSize) => {
+      expect(() => Validation.movingCommand(bridgeSize)).toThrow();
     });
   });
 });
