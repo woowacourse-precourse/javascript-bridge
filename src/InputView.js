73eb0a3928aea60d04e3ba65f00612bb780b2ece
Dean <yjj052800@gmail.com>
2022-11-17 16:15:48 +0900

    feat: 이동할 칸을 입력받는 기능 추가

--- src/InputView.js
+++ src/InputView.js
@@ -1,4 +1,4 @@
-const MissionUtils = require('@woowacourse/mission-utils');
+const { Console } = require('@woowacourse/mission-utils');
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -7,7 +7,7 @@ const InputView = {
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize() {
-    MissionUtils.Console.readLine('다리의 길이를 입력해주세요.', (answer) => {
+    Console.readLine('다리의 길이를 입력해주세요.', (answer) => {
       console.log(`다리 길이 : ${answer}`);
     });
   },
@@ -15,12 +15,17 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)', (answer) => {
+      this.readMovingException(answer);
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand() {},
 };
+InputView.readMoving();
 
 module.exports = InputView;
