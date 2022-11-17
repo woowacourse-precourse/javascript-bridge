06a99681df03fd717445d4af613f2e8b804e5507
Sim <zh025700@naver.com>
2022-11-17 19:26:31 +0900

    test(InputViewTest): 유저 다리 선택 예외 테스트 추가

--- __tests__/InputViewTest.js
+++ __tests__/InputViewTest.js
@@ -1,6 +1,7 @@
 const InputView = require("../src/InputView");
 const BridgeRandomNumberGenerator = require("../src/BridgeRandomNumberGenerator");
 const MissionUtils = require("@woowacourse/mission-utils");
+const BridgeGame = require("../src/BridgeGame");
 
 const mockQuestions = (answer) => {
   MissionUtils.Console.readLine = jest.fn();
@@ -10,24 +11,31 @@ const mockQuestions = (answer) => {
 };
 
 describe("readBridgeSize 테스트", () => {
-  test("입력 예외 테스트: 범위를 넘는 수를 입력했을 때", () => {
+  test("다리길이 입력 예외 테스트: 범위를 넘는 수를 입력했을 때", () => {
     mockQuestions(1);
     expect(() => {
       InputView.readBridgeSize();
     }).toThrow("[ERROR]");
   });
 
-  test("입력 예외 테스트: 숫자가 아닌 문자를 입력했을 때", () => {
+  test("다리길이 입력 예외 테스트: 숫자가 아닌 문자를 입력했을 때", () => {
     mockQuestions("a");
     expect(() => {
       InputView.readBridgeSize();
     }).toThrow("[ERROR]");
   });
 
-  test("입력 예외 테스트: 실수를 입력했을 때", () => {
+  test("다리길이 입력 예외 테스트: 실수를 입력했을 때", () => {
     mockQuestions(6.6);
     expect(() => {
       InputView.readBridgeSize();
+    }).toThrow("[ERROR]");
+  });
+
+  test("유저 다리 선택 입력 예외 테스트: U와 D를 입력하지 않았을 때", () => {
+    mockQuestions("A");
+    expect(() => {
+      InputView.readMoving(new BridgeGame([1, 1, 1]));
     }).toThrow("[ERROR]");
   });
 });
