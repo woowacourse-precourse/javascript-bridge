726596738585ca2944c8cb97da74e00bd1a1df51
pakxe <pigkill40@naver.com>
2022-11-17 20:43:29 +0900

    feat(MovingCount): 걸음수를 한번 추가하는 메서드 작성

--- src/domain/MovingCount.js
+++ src/domain/MovingCount.js
@@ -1,4 +1,14 @@
 class MovingCount {
   #totalMovingCount;
   #currentMovingCount;
+
+  constructor() {
+    this.#totalMovingCount = 0;
+    this.#currentMovingCount = 0;
+  }
+
+  add1MovingCount() {
+    this.#totalMovingCount += 1;
+    this.#currentMovingCount += 1;
+  }
 }
