5923e1be09318ea6a0cee356421bddc58b5c0b70
jeonbyeongmin <qudals7613@gmail.com>
2022-11-17 16:08:19 +0900

    feat: 다리 생성 로직 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+const BridgeMaker = require('./BridgeMaker');
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
+const InputView = require('./InputView');
+const OutputView = require('./OutputView');
+
 class App {
-  play() {}
+  play() {
+    OutputView.printStart();
+    this.gameStart();
+  }
+
+  gameStart() {
+    InputView.readBridgeSize((bridgeSize) => {
+      const bridges = this.makeBridges(bridgeSize, 2);
+      console.log(bridges);
+    });
+  }
+
+  makeBridges(bridgeSize, numberOfRow) {
+    const generate = BridgeRandomNumberGenerator.generate;
+
+    let bridges = [];
+    for (let i = 0; i < numberOfRow; i++) {
+      const bridge = BridgeMaker.makeBridge(Number(bridgeSize), generate);
+      bridges.push(bridge);
+    }
+
+    return bridges;
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
