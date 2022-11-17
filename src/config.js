0bf597ef7b35491c90c0521a91195c54c4d83d82
pinkishincoloragain <smb1103@gmail.com>
2022-11-18 03:50:34 +0900

    style: PHRASE가 출력 형식에 맞게 \n 삽입

--- src/config.js
+++ src/config.js
@@ -24,11 +24,11 @@ const MAP = {
 
 const PHRASE = {
   START: '다리 건너기 게임을 시작합니다.\n',
-  BRIDGE_LEN: '다리의 길이를 입력해주세요.',
-  SELECT: '이동할 칸을 선택해주세요. (위: U, 아래: D)',
-  RESTART: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)',
+  BRIDGE_LEN: '다리의 길이를 입력해주세요.\n',
+  SELECT: '\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
+  RESTART: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
   RESULT: '\n최종 게임 결과',
-  SUCCESS: '게임 성공 여부: 성공\n',
+  SUCCESS: '\n게임 성공 여부: 성공\n',
   FAIL: '게임 성공 여부: 실패\n',
   TOTAL_TRY: '총 시도한 횟수: ',
 };
