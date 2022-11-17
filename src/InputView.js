902dd72c7d67bb484e9416cf8f25d823d3f0e634
dlwl98 <ff981113@hanyang.ac.kr>
2022-11-17 21:37:47 +0900

    refactor(InputView): try catch 문 재사용할 수 있게 분리
    change
    - try catch 문 errorHandler 메서드로 분리
    - jsdoc 작성

--- src/InputView.js
+++ src/InputView.js
@@ -6,19 +6,26 @@ const Validator = require('./Validator');
  */
 const InputView = {
   /**
-   * 다리의 길이를 입력받는다.
+   * @param {function(string): void} callback 입력받은 후 실행할 함수
    */
   readBridgeSize(callback) {
-    Console.readLine('다리의 길이를 입력해주세요.\n', (input) => {
-      try {
-        Validator.validateIsNumber(input);
-        Validator.validateInRange(3, 20, input);
-        callback(input);
-      } catch (err) {
-        Console.print(`\n${err.message}`);
-        this.readBridgeSize();
-      }
-    });
+    Console.readLine('다리의 길이를 입력해주세요.\n', (input) => this.errorHandler(
+      this.validateBridgeSize(input, callback),
+      () => this.readBridgeSize(callback),
+    ));
+  },
+
+  /**
+   * @param {string} input 플레이어의 입력
+   * @param {function(string): void} callback 입력받은 후 실행할 함수
+   * @return {function(): void} 에러핸들러에게 전달할 콜백함수
+   */
+  validateBridgeSize(input, callback) {
+    return () => {
+      Validator.validateIsNumber(input);
+      Validator.validateInRange(3, 20, input);
+      callback(input);
+    };
   },
 
   /**
@@ -30,6 +37,19 @@ const InputView = {
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand() {},
+
+  /**
+   * @param {function(): void} callback 입력 유효성을 검증하기 위한 콜백함수
+   * @param {function(): void} onError 에러 발생시 실행할 함수
+   */
+  errorHandler(callback, onError) {
+    try {
+      callback();
+    } catch (err) {
+      Console.print(`\n${err.message}`);
+      onError();
+    }
+  },
 };
 
 module.exports = InputView;
