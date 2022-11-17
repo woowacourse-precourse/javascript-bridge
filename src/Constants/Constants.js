3a0a9983beb59041a20a46da93baa814ec38bb2d
dhkham <khamdong@gmail.com>
2022-11-17 13:46:38 +0900

    feat(Constants): add messages to display

---
+++ src/Constants/Constants.js
@@ -0,0 +1,13 @@
+const MESSAGES = {
+  ENTER_SIZE: '다리 건너기 게임을 시작합니다. \n\n다리의 길이를 입력해주세요.\n',
+};
+
+const ERROR_MESSAGES = {
+  INVALID_SIZE: '다리의 길이는 3 이상의 20이하여야 합니다.',
+  INVALID_TYPE: '다리의 길이는 정수여야 합니다.',
+};
+
+module.exports = {
+  MESSAGES,
+  ERROR_MESSAGES,
+};
