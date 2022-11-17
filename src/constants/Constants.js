7018552ccb787cee96d4a716a7a12f4359ecfb63
pakxe <pigkill40@naver.com>
2022-11-17 20:36:25 +0900

    rename(Constants): length 메서드명과 겹치는 프로퍼티 명을 수정

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -14,9 +14,9 @@ const GAME = Object.freeze({
 
 const SENTENCE = Object.freeze({
   start: '다리 건너기 게임을 시작합니다.',
-  length: '다리의 길이를 입력해주세요.',
-  selectStep: `이동할 칸을 선택해주세요. (위: ${BRIDGE.UP}, 아래: ${BRIDGE.DOWN})`,
-  selectRestart: `게임을 다시 시도할지 여부를 입력해주세요. (재시도: ${GAME.RESTART}, 종료: ${GAME.QUIT})`,
+  bridgeLength: '다리의 길이를 입력해주세요.',
+  selectMoving: `이동할 칸을 선택해주세요. (위: ${BRIDGE.up}, 아래: ${BRIDGE.down})`,
+  selectRestart: `게임을 다시 시도할지 여부를 입력해주세요. (재시도: ${GAME.restart}, 종료: ${GAME.quit})`,
 });
 
 const RESULT = Object.freeze({
@@ -30,10 +30,10 @@ const RESULT = Object.freeze({
 
 const ERROR = Object.freeze({
   prefix: '[ERROR]',
-  bridgeLength: `다리 길이는 ${BRIDGE.MIN}부터 ${BRIDGE.MAX} 사이의 숫자여야 합니다.`,
+  bridgeLength: `다리 길이는 ${BRIDGE.min}부터 ${BRIDGE.max} 사이의 숫자여야 합니다.`,
   isNan: '다리 길이는 숫자로 입력해야 합니다.',
-  step: `다리 선택은 위: ${BRIDGE.UP}, 아래: ${BRIDGE.DOWN} 둘 중 하나를 입력해야 합니다.`,
-  restart: `재시도: ${GAME.RESTART}, 종료: ${GAME.QUIT} 둘 중 하나를 입력해야 합니다. `,
+  moving: `다리 선택은 위: ${BRIDGE.up}, 아래: ${BRIDGE.down} 둘 중 하나를 입력해야 합니다.`,
+  restart: `재시도: ${GAME.restart}, 종료: ${GAME.quit} 둘 중 하나를 입력해야 합니다. `,
 });
 
 module.exports = { BRIDGE, GAME, SENTENCE, RESULT, ERROR };
