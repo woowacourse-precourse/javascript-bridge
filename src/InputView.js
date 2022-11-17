2ec5a00ebe3afc0d088503f2241edac3fe435d03
guridaek <iue16@naver.com>
2022-11-17 12:25:26 +0900

    feat: 다리 길이 입력 기능 추가

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,5 @@
+const { Console } = require('@woowacourse/mission-utils');
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,7 +7,14 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    let size = 0;
+    Console.readLine('다리의 길이를 입력해주세요.', input => {
+      size = input;
+    });
+
+    return size;
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
