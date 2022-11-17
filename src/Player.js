295fb2ac5a2fa47f13b6d05b635adb732ef4a4cf
Rki0 <pky00823@gmail.com>
2022-11-17 09:26:42 +0900

    feat(Player): 진행 상황을 보여주기 위한 프로퍼티 추가
    state에 플레이어가 진행한 위 다리, 아래 다리의 상황을 기록한다.
    state에 대한 업데이트는 updateState 메서드를 사용한다.

--- src/Player.js
+++ src/Player.js
@@ -1,5 +1,27 @@
 const Player = {
   movingArr: [],
+  state: [[], []],
+  updateState(wantGo, isCorrect) {
+    if (wantGo === "U" && isCorrect) {
+      this.state[0].push(" O ");
+      this.state[1].push("   ");
+    }
+
+    if (wantGo === "U" && !isCorrect) {
+      this.state[0].push(" X ");
+      this.state[1].push("   ");
+    }
+
+    if (wantGo === "D" && isCorrect) {
+      this.state[0].push("   ");
+      this.state[1].push(" O ");
+    }
+
+    if (wantGo === "D" && !isCorrect) {
+      this.state[0].push("   ");
+      this.state[1].push(" X ");
+    }
+  },
 };
 
 module.exports = Player;
