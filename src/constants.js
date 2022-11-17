ea3e06df577026d7edd76eaaa66e245e6630b8fb
2jaebbang <72256238+2jaebbang@users.noreply.github.com>
2022-11-17 22:14:46 +0900

    feat: 상수값 기본 틀 추가

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const INPUT_MESSAGE = {};
+
+const PRINT_MESSAGE = {};
+
+const ERROR = '[ERROR]';
+
+const ERROR_MESSAGE = {};
+
+module.exports = {
+  INPUT_MESSAGE,
+  PRINT_MESSAGE,
+  ERROR_MESSAGE,
+};
