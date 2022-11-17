191e178924b4c00133e8b687a18fa55298b15d92
khj0426 <706shin1728@naver.com>
2022-11-17 14:59:36 +0900

    feat:다리 길이 상수화

---
+++ src/MESSAGES/InputMessage.js
@@ -0,0 +1,7 @@
+const INPUT_MESSAGE = {
+  BRIDGE_INPUT: '다리의 길이를 입력해주세요.',
+  BRIDGE_INPUT_ERROR: '다리 길이는 3부터 20사이의 숫자여야 합니다.',
+};
+
+Object.freeze(INPUT_MESSAGE);
+module.exports = INPUT_MESSAGE;
