0acfd6f12eb485b663a18ade5b9aec4206f3daaf
daal2quf <daal2quf@gmail.com>
2022-11-17 18:34:39 +0900

    feat(inputView): 유저가 재시작할지 게임을 종료할지 입력받는 기능 추가
    add:
    readRetryOrQuit(): 유저의 재시작 및 게임종료여부를 R or Q로 입력받고 입력을 반환

--- src/InputView.js
+++ src/InputView.js
@@ -14,7 +14,11 @@ const InputView = {
     });
   },
 
-  readGameCommand() { },
+  readRetryOrQuit() {
+    Console.readLine('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n', (userInputRetryOrQuit) => {
+      return userInputRetryOrQuit;
+    });
+  },
 };
 
 module.exports = InputView;
