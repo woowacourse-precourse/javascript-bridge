d6708097ad2a3aa350d0ba0e988ecadb5646aefd
Sungwk <73461032+Sungwk@users.noreply.github.com>
2022-11-17 17:05:35 +0900

    feat : 게임 메시지 상수화

---
+++ src/Constants.js
@@ -0,0 +1,8 @@
+const GAME_MESSAGE = {
+  PLAY_GAME: '다리 건너기 게임을 시작합니다.',
+  INPUT_BRIDGE_LENGTH: '다리의 길이를 입력해주세요.',
+  INPUT_UP_OR_DOWN: '이동할 칸을 선택해주세요. (위: U, 아래: D)',
+  ASK_REPLAY: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)',
+  RESULT: '게임 성공 여부: ',
+  NUMBERS_OF_PLAY: '총 시도한 횟수: ',
+};
