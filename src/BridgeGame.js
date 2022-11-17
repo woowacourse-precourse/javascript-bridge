b5236c9db1b2527d43ae8504e050f656d1c31fac
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-11-18 01:13:06 +0900

    feat(BridgeGame): 사용자가 시작전인지 확인하는 메서드
    초기값은 true를 반환한다.

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -4,6 +4,8 @@ const Validation = require('./Validation');
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  #userPosition = null;
+
   static replaceString(value) {
     Validation.number(value);
     const [TOP_NUMBER, TOP] = [0, 'D'];
@@ -15,12 +17,9 @@ class BridgeGame {
     return Validation.throwError(Validation.RANGE_ERROR_TEXT);
   }
 
-  /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  move() {}
+  isBeforeStart() {
+    return this.#userPosition === null;
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
