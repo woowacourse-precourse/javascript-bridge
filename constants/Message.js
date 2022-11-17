22f76f13f59b33d1bd03a0ec926a022be782a6d0
oio337a <oio337a@gmail.com>
2022-11-17 21:43:55 +0900

    feat : add message constants value

---
+++ constants/Message.js
@@ -0,0 +1,13 @@
+const MESSAGE = {
+  START: "다리 건너기 게임을 시작합니다.\n",
+  INPUT: "다리의 길이를 입력해주세요.\n",
+  MOVE: "이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
+  RETRY: "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n",
+  RESULT: "최종 게임 결과\n",
+  CONDITION: "게임 성공 여부: ",
+  TRYCOUNT: "총 시도한 횟수: ",
+  SUCCESS: "성공",
+  FAIL: "실패",
+};
+
+module.exports = MESSAGE;
