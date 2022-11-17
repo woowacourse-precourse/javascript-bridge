e70ddd26baddbe982a82457d04fe7deffe4b9492
sumin <csumin008255@gmail.com>
2022-11-17 18:55:29 +0900

    feat: [기능1] 게임 시작 메세지 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const Message = require("./Message");
+const OutputView = require("./OutputView");
+
 class App {
-  play() {}
+  play() {
+    OutputView.printGameStart(Message.GAME_START);
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
---
+++ src/Message.js
@@ -0,0 +1,5 @@
+const Message = {
+  GAME_START: "다리 건너기 게임을 시작합니다.",
+};
+
+module.exports = Message;
