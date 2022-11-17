5a9b94b1190c10f8406aabc4bf6863a85f30f2fc
dragonfruitlemonade <tpghks9245@gmail.com>
2022-11-17 12:15:33 +0900

    feat(inputView): readBridgeSize, readMoving, readGameCommand 함수 구현

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,22 +1,18 @@
 const { Console } = require("@woowacourse/mission-utils");
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+
 const InputView = {
-  /**
-   * 다리의 길이를 입력받는다.
-   */
-  readBridgeSize() {},
 
-  /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
-  readMoving() {},
+  readBridgeSize(bridgeLength) {
+    Console.readLine('다리의 길이를 입력해주세요.', bridgeLength);
+  },
 
-  /**
-   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-   */
-  readGameCommand() {},
+  readMoving(nextSpace) {
+    Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)', nextSpace);
+  },
+
+  readGameCommand(retryOrNot) {
+    Console.readLine('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)', retryOrNot);
+  },
 
 };
 
