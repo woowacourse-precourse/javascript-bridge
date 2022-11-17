a2ed9754a4319f7b7d5497623d93d03eab572b86
sh981013s <sh981013s@gmail.com>
2022-11-17 16:43:50 +0900

    chore(*): eslint, prettier configuration 파일 생성

---
+++ .prettier.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // 문자열은 큰 따옴표로 통일
+  singleQuote: false,
+  //코드 마지막에 세미콜른이 자동 생성
+  semi: true,
+  //탭의 사용을 금하고 스페이스바 사용으로 대체
+  useTabs: false,
+  // 들여쓰기 너비는 4칸
+  tabWidth: 4,
+  // 객체나 배열 키:값 뒤에 콤마 생성
+  trailingComma: "all",
+  // 코드 한줄이 maximum 80칸
+  printWidth: 80,
+};
