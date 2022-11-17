48eaa6bcdb4f928e0704d3033b17ff047b2fbd66
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-11-17 16:19:46 +0900

    Refactor: 매서드 정리 중복 코드 통합 및 변수명 정리

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,7 +1,6 @@
 const bridgeGame = require("../model/BridgeGame");
 const MissionUtils = require('@woowacourse/mission-utils');
 const { RESULTLINE, CONSOLELINE } = require("../utils/Constants");
-const inputView = require('./InputView')
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -12,21 +11,14 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printMap(answer, upOrdown) {
-    const returns = (bridgeGame.move(answer, upOrdown));
-    const cnt = returns[2];
-    MissionUtils.Console.print(RESULTLINE.LOG(returns[0]));
-    MissionUtils.Console.print(RESULTLINE.LOG(returns[1]));
-    //this.justFinishOrSuccess(answer, returns, cnt);
-    return returns;
+    const gameProgress = bridgeGame.move(answer, upOrdown);
+    MissionUtils.Console.print(RESULTLINE.LOG(gameProgress[0]));
+    MissionUtils.Console.print(RESULTLINE.LOG(gameProgress[1]));
+    return gameProgress;
   },
   
-  failOutputandNomoreGame(gameLog){
-    this.printFail(gameLog[0], gameLog[1]);
-    MissionUtils.Console.close();
-  },
-
-  SuccessOutput(gameLog){
-    this.printSuccess(gameLog[0], gameLog[1]);
+  finalSuccessFailure(gameLog, failorSuccess){
+    this.printResult(gameLog[0], gameLog[1], failorSuccess);
     MissionUtils.Console.close();
   },
   /**
@@ -34,23 +26,14 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResultLog(up_list, down_list) {
+
+  printResult(up_list, down_list, failorSuccess){
     MissionUtils.Console.print(CONSOLELINE.FIN_RESULT_LOG);
     MissionUtils.Console.print(RESULTLINE.LOG(up_list));
     MissionUtils.Console.print(RESULTLINE.LOG(down_list));
-  },
-
-  printFail(up_list, down_list){
-    this.printResultLog(up_list, down_list);
-    MissionUtils.Console.print(CONSOLELINE.FAIL_RESULT);
+    MissionUtils.Console.print(failorSuccess);
     MissionUtils.Console.print(RESULTLINE.COUNT(bridgeGame.restartcnt))
   },
-
-  printSuccess(up_list, down_list){
-    this.printResultLog(up_list, down_list);
-    MissionUtils.Console.print(CONSOLELINE.SUCCESS_RESULT);
-    MissionUtils.Console.print(RESULTLINE.COUNT(bridgeGame.restartcnt))
-  }
 };
 
 module.exports = OutputView;
