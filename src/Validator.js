39d837aca451c0992b775356d92a9c35e72a5d31
dlwl98 <ff981113@hanyang.ac.kr>
2022-11-17 23:14:52 +0900

    fix(Validator): input의 타입이 string인 것 수정

--- src/Validator.js
+++ src/Validator.js
@@ -1,6 +1,6 @@
 const Validator = {
   /**
-   * @param {number} input // 입력된 숫자
+   * @param {string} input // 입력된 문자열
    */
   validateIsNumber(input) {
     if (!input.trim() || Number.isNaN(Number(input))) {
@@ -11,10 +11,10 @@ const Validator = {
   /**
    * @param {number} start // 체크할 범위의 시작
    * @param {number} end // 체크할 범위의 끝
-   * @param {number} input // 입력된 숫자
+   * @param {string} input // 입력된 문자열
    */
   validateInRange(start, end, input) {
-    if (input < start || input > end) {
+    if (Number(input) < start || Number(input) > end) {
       throw new Error('[ERROR] 3 ~ 20 사이의 숫자만 입력 가능합니다.');
     }
   },
