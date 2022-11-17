60acdefd7090b524fc1ca6b8d782485f24dd06ac
kangyeongmin <kym6050@hanyang.ac.kr>
2022-11-17 15:05:57 +0900

    📌 feat(BridgeGame): 클래스 생성자 선언

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -7,6 +7,19 @@ class BridgeGame {
   #order;
   #trialNumber;
   #bridgeResult;
+
+  constructor(size) {
+    this.#bridge = BridgeMaker.makeBridge(
+      size,
+      BridgeRandomNumberGenerator.generate
+    );
+    this.#order = 0;
+    this.#trialNumber = 1;
+    this.#bridgeResult = {
+      upResult: [],
+      downResult: [],
+    };
+  }
 }
 
 module.exports = BridgeGame;
