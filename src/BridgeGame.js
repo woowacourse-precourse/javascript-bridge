a0fc9150d4f90f45a8e43bba1bcea3b1a0dfacda
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2022-11-18 01:56:53 +0900

    refactor: 정답을 맞춘 경우 [ O ]를 출력해주는 기능 수정

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -13,21 +13,13 @@ class BridgeGame {
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   move(userAnswer) {
-    return this.checkU(userAnswer) || this.checkD(userAnswer);
+    return this.correct(userAnswer);
   }
 
-  checkU(userAnswer) {
-    if (this.bridgeArray[0] === userAnswer && userAnswer === 'U') {
+  correct(userAnswer) {
+    if (this.bridgeArray[0] === userAnswer) {
       this.bridgeArray.shift();
-      OutputView.printMap(userAnswer, this.bridgeArray);
-      return this.bridgeArray;
-    }
-  }
-
-  checkD(userAnswer) {
-    if (this.bridgeArray[0] === userAnswer && userAnswer === 'D') {
-      this.bridgeArray.shift();
-      OutputView.printMap(userAnswer, this.bridgeArray);
+      OutputView.printMap(userAnswer);
       return this.bridgeArray;
     }
   }
--- src/OutputView.js
+++ src/OutputView.js
@@ -4,23 +4,24 @@ const MissionUtils = require('@woowacourse/mission-utils');
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 const OutputView = {
+  UP: [],
+  DOWN: [],
+
   /**
    * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printMap(userAnswer) {
-    const UP = [];
-    const DOWN = [];
     if (userAnswer === 'U') {
-      UP.push(` O `);
-      DOWN.push(`   `);
+      this.UP.push(' O ');
+      this.DOWN.push('   ');
     }
     if (userAnswer === 'D') {
-      UP.push(`   `);
-      DOWN.push(` O `);
+      this.UP.push('   ');
+      this.DOWN.push(' O ');
     }
-    MissionUtils.Console.print(`[${UP}]\n[${DOWN}]`);
+    MissionUtils.Console.print(`\n[${this.UP}]\n[${this.DOWN}]`);
   },
 
   /**
