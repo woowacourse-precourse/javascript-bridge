f94922288803e1bcb6f092be3bcf06941f14b33e
violetwhenisawu <anashape710@gmail.com>
2022-11-18 04:22:11 +0900

    style: 게임 메세지 출력 시 개행 추가

--- src/utils/gameMessage.js
+++ src/utils/gameMessage.js
@@ -1,15 +1,15 @@
 const GAME_MESSAGE = {
-  GAME_START: '다리 건너기 게임을 시작합니다.',
-  GAME_RESULT: '최종 게임 결과',
+  GAME_START: '다리 건너기 게임을 시작합니다.\n',
+  GAME_RESULT: '최종 게임 결과\n',
   GAME_STATUS: '게임 성공 여부: ',
   GAME_ATTEMPTS: '총 시도한 횟수: ',
 };
 
 const INPUT_MESSAGE = {
-  GET_BRIDGE_LENGTH: '다리의 길이를 입력해주세요.',
-  SELECT_MOVE: '이동할 칸을 선택해주세요. (위: U, 아래: D)',
+  GET_BRIDGE_LENGTH: '다리의 길이를 입력해주세요.\n',
+  SELECT_MOVE: '이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
   SELECT_RETRY:
-    '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)',
+    '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
 };
 
 const BRIDGE_COMPONENT = {
