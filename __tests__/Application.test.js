69251cdda51460ddd24dc02a228de8afaa2329b2
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-11-17 16:28:17 +0900

    test(Validation): 배열 생성 함수 테스트
    지정된 범위를 벗어나면 예외를 발생시킨다.

--- __tests__/Application.test.js
+++ __tests__/Application.test.js
@@ -1,6 +1,7 @@
 const Application = require('../src/Application');
 
 const NUMBER_ERROR_TEXT = '[ERROR] 전달된 인수는 숫자 타입이 아닙니다.';
+const RANGE_ERROR_TEXT = '[ERROR] 지정한 범위의 값이 아닙니다.';
 
 describe('숫자 변환 함수 테스트', () => {
   test('메소드 이름은 "convertNumber"로 정의된다.', () => {
@@ -50,4 +51,13 @@ describe('배열 생성 함수 테스트', () => {
       Application.createArray(EXPECTED1, EXPECTED2);
     }).toThrow();
   });
+
+  test.each(['2', '21'])(
+    '지정된 범위를 벗어나면 예외를 발생시킨다.',
+    (EXPECTED) => {
+      expect(() => {
+        Application.createArray(EXPECTED, mockFn);
+      }).toThrow(RANGE_ERROR_TEXT);
+    },
+  );
 });
