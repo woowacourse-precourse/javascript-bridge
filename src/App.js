0e7ba1b95584acfef244b5cc6adce3f406128e2e
vjvl95 <vjvl95@naver.com>
2022-11-17 17:20:53 +0900

    fact : 다리 이동하는 함수 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-class App {
-  play() {}
-}
+const BridgeMaker = require('./BridgeMaker');
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
+const MissionUtils = require('@woowacourse/mission-utils');
+const OutputView = require('./OutputView');
+const InputView = require('./InputView');
 
+class App {
+  play() {
+    OutputView.StartGame();
+    InputView.readBridgeSize();
+  }
+}
+const app = new App();
+app.play();
 module.exports = App;
--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,6 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const BridgeMaker = require('./BridgeMaker');
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,12 +8,22 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    MissionUtils.Console.readLine('다리의 길이를 입력해주세요.\n', (size) => {
+      const brige = BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate);
+      this.readMoving(brige);
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving(brige) {
+    MissionUtils.Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래:D)\n', (moving) => {
+      MissionUtils.Console.print(brige);
+      MissionUtils.Console.print(moving);
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
