cf0cbc605768b3cb76c2d539b82566c5818a476d
jisu-kim <kss2460@naver.com>
2022-11-17 19:59:42 +0900

    feat(InputView): 다리 입력 추가

--- src/InputView.js
+++ src/InputView.js
@@ -1,6 +1,6 @@
-import { Console } from "@woowacourse/mission-utils"
-import { makeBridge } from './BridgeMaker';
-import { generate } from './BridgeRandomNumberGenerator';
+const { Console } = require("@woowacourse/mission-utils");
+const { makeBridge } = require("./BridgeMaker");
+const { generate } = require("./BridgeRandomNumberGenerator");
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -10,28 +10,35 @@ const InputView = {
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize() {
-    Console.readLine('다리의 길이를 입력해주세요.', (bridgeSize) => {
-      makeBridge(bridgeSize, generate)
-    })
+    Console.readLine("다리의 길이를 입력해주세요.", (bridgeSize) => {
+      makeBridge(bridgeSize, generate);
+    });
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving() {
-    Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)', (maveInput) => {
-      move(maveInput)
-    })
+    Console.readLine(
+      "이동할 칸을 선택해주세요. (위: U, 아래: D)",
+      (maveInput) => {
+        move(maveInput);
+      }
+    );
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand() {
-    Console.readLine('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)', (retryInput) => {
-      retry(retryInput)
-    })
+    Console.readLine(
+      "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
+      (retryInput) => {
+        retry(retryInput);
+      }
+    );
   },
 };
+InputView.readBridgeSize();
 
 module.exports = InputView;
