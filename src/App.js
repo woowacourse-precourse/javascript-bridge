4aed19352454a4e90f270c8d7c539d5dba972a35
pinkishincoloragain <smb1103@gmail.com>
2022-11-18 03:43:42 +0900

    feat: 게임 시작 관리 메소드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+const OutputView = require('./OutputView.js');
+const InputView = require('./InputView.js');
+const BridgeGame = require('./BridgeGame.js');
+
 class App {
-  play() {}
+  async play() {
+    OutputView.printHi();
+
+    const bridgeSize = await InputView.readBridgeSize();
+    this.game = new BridgeGame(bridgeSize);
+    this.playOneStep();
+  }
+
+  playOneStep() {}
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
