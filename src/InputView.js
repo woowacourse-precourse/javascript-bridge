23cc3b873664e3769cc5005ec8b741bf769157e8
unsnruu <unsnruu@gmail.com>
2022-11-18 03:00:09 +0900

    refactor: withError 함수를 적용하여 반복되는 코드 추출

--- src/InputView.js
+++ src/InputView.js
@@ -1,43 +1,51 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const Validation = require("./Validation.js");
+const withError = require("./utils/withError.js");
 
 const { Console } = MissionUtils;
-
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
-   *
+   * @param {function} current
    * @param {function} next
    */
-  readBridgeSize(next) {
+  readBridgeSize(current, next) {
     const message = "다리의 길이를 입력해주세요.\n";
-    Console.readLine(message, (bridgeLength) => {
-      const validatedLength = Validation.validateBridgeLength(bridgeLength);
-      next(validatedLength);
-    });
+    const validateLength = Validation.validateBridgeLength.bind(Validation);
+    Console.readLine(message, withError(validateLength, current, next));
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving(next) {
+  validateMoveCommand(inputs) {
+    return Validation.validateCommand(inputs, ["U", "D"]);
+  },
+  readMoving(current, next) {
     const message = "이동할 칸을 선택해주세요. (위: U, 아래: D)\n";
-    Console.readLine(message, (command) => {
-      const validatedCommand = Validation.validateCommand(command, ["U", "D"]);
-      next(validatedCommand);
-    });
+    Console.readLine(
+      message,
+      withError(this.validateMoveCommand, current, next)
+    );
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand(next) {
+  validateEndCommand(inputs) {
+    return Validation.validateCommand(inputs, ["R", "Q"]);
+  },
+  readGameCommand(current, next) {
     const message =
       "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n";
-    Console.readLine(message, (command) => next(command));
+    Console.readLine(
+      this.validateEndCommand,
+      message,
+      withError(current, next)
+    );
   },
 };
 
