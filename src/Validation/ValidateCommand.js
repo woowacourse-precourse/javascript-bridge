f1e63fa49266a735609991f952e735823cd9c82a
박정규 <srk99279@naver.com>
2022-11-17 19:52:21 +0900

    refactor : 유효성 검사 통합

--- src/Validation/ValidateCommand.js
+++
@@ -1,9 +0,0 @@
-const { ERROR_MSG } = require("../constants");
-
-const ValidateCommand = (cmd) => {
-  if (cmd != "R" && cmd != "Q") {
-    throw new Error(ERROR_MSG.INPUT_RETRY_ERROR);
-  }
-};
-
-module.exports = ValidateCommand;
--- src/Validation/ValidateMoving.js
+++
@@ -1,9 +0,0 @@
-const { ERROR_MSG } = require("../constants");
-
-const ValidateMoving = (move) => {
-  if (move != "U" && move != "D") {
-    throw new Error(ERROR_MSG.INPUT_MOVING_ERROR);
-  }
-};
-
-module.exports = ValidateMoving;
