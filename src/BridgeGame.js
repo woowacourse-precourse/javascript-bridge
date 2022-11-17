e112762ca10db58b3bd1293b6125106731829162
dladncks1217 <dlaxodud1217@gmail.com>
2022-11-18 00:49:07 +0900

    feat(constructor, PrintMyBridge): 생성자 수정, print메서드 추가

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,3 +1,6 @@
+const { Console } = require('@woowacourse/mission-utils');
+const bridgeMaterialize = require('./utils/bridgeMaterialize');
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
@@ -5,17 +8,23 @@ class BridgeGame {
   #myBridge;
   #nowState;
 
-  constructor(bridgeMaker, makeRandomNumber) {
-    this.#myBridge = bridgeMaker(makeRandomNumber());
+  constructor(makeRandomNumber, bridgeMaker, size) {
+    this.#myBridge = bridgeMaker(size, makeRandomNumber);
     this.#nowState = 0;
+  }
+
+  myBridgeMaterialize() {
+    return bridgeMaterialize(this.#myBridge);
   }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move(readMoving) {
-    if (this.#myBridge[this.#nowState + 1] === readMoving()) return true;
+  async move(readMoving) {
+    const input = await readMoving();
+    Console.print(input);
+    if (this.#myBridge[this.#nowState] === input) return true;
     return false;
   }
 
@@ -27,6 +36,10 @@ class BridgeGame {
   retry(readGameCommand) {
     const result = readGameCommand();
     // 결과 이용해 값 유효 여부 확인 후 로직 전개
+  }
+
+  printMyBridge(printMap) {
+    printMap(this.#myBridge, this.#nowState);
   }
 }
 
