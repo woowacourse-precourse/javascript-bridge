6c8f5396b0bccb56507ee2c5446a16bb6da1791c
JiSu Kim <85912592+jsk3342@users.noreply.github.com>
2022-11-18 01:14:27 +0900

    feat(BridgeGame): constructor 추가, count활용

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -8,7 +8,7 @@ class BridgeGame {
     this.mainBridge = mainBridge;
     this.bridgeSize = mainBridge[0].length;
     this.count = 1;
-    this.userBridge = [];
+    this.userBridge = [[],[]];
   }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -16,7 +16,8 @@ class BridgeGame {
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   move(moveInput) {
-    makeUserBridge(moveInput, this.count, this.mainBridge);
+    this.userBridge = makeUserBridge(moveInput, this.userBridge, this.mainBridge);
+    this.count++;
   }
 
   /**
