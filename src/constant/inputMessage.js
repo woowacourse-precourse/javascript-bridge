e76c37a48b924e52f6173eb71df42430fc8281fe
Hchan2 <seongchan_@naver.com>
2022-11-17 18:46:52 +0900

    입력, 출력 메세지 상수 변수

--- src/constant/inputMessage.js
+++ src/constant/inputMessage.js
@@ -1,10 +1,8 @@
-const BRIDGE_START = '다리 건너기 게임을 시작합니다.';
 const BRIDGE_LENGTH = '다리의 길이를 입력해주세요.';
 const MOVE_SPACE = '이동할 칸을 선택해주세요. (위: U, 아래: D)';
 const RE_OR_END = '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)';
 
 module.exports = {
-    BRIDGE_START,
     BRIDGE_LENGTH,
     MOVE_SPACE,
     RE_OR_END
--- src/constant/outputMessage.js
+++ src/constant/outputMessage.js
@@ -1,7 +1,9 @@
 const GAME_RESULT = (result) => { return `게임 성공 여부: ${result}` };
 const TOTAL_ATTEMPT = (attemptNumber) => { return `총 시도한 횟수: ${attemptNumber}` };
+const BRIDGE_START = '다리 건너기 게임을 시작합니다.';
 
 module.exports = {
     GAME_RESULT,
-    TOTAL_ATTEMPT
+    TOTAL_ATTEMPT,
+    BRIDGE_START
 }
