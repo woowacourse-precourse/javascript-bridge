1c42afd74754d26dcf07b926fa54865c69624103
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-11-17 16:58:22 +0900

    feat(Validation): 함수 타입 체크 함수

--- src/Validation.js
+++ src/Validation.js
@@ -1,9 +1,16 @@
 const Validation = {
   VALIDATION_NUMBER_TEXT: '[ERROR] 전달된 인수는 숫자 타입이 아닙니다.',
   RANGE_ERROR_TEXT: '[ERROR] 지정한 범위의 값이 아닙니다.',
+  FUNCTION_ERROR_TEXT: '[ERROR] 전달된 인수는 함수가 아닙니다.',
   number(target) {
     if (Number.isNaN(target)) {
       throw new TypeError(Validation.VALIDATION_NUMBER_TEXT);
+    }
+  },
+
+  func(target) {
+    if (typeof target !== 'function') {
+      throw new TypeError(Validation.FUNCTION_ERROR_TEXT);
     }
   },
 
