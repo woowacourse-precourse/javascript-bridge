f2d996163475502748711d6b3433426c831e4aaa
hahagarden <88613455+hahagarden@users.noreply.github.com>
2022-11-18 05:01:23 +0900

    feat: 이동할 칸 입력 값과 정답 비교하여 결과 도출

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,13 +1,24 @@
+const { PASS, FAIL } = require("./Utils");
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  #bridge;
+  #position;
+  constructor(bridge) {
+    this.#bridge = bridge;
+    this.#position = 0;
+  }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(moving) {
+    if (moving === this.#bridge[this.#position++]) return PASS;
+    return FAIL;
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
