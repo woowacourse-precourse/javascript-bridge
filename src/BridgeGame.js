11c842f6c9df5908b99306f26389430dfecff587
dudgp128 <20181048@sungshin.ac.kr>
2022-11-18 00:35:48 +0900

    FEAT(BridgeGame.fillMap) : 위치값과 비교 결과를 활용해 맵 채우기

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -5,11 +5,24 @@ const InputView = require("../src/InputView");
  */
 class BridgeGame {
   #bridge;
+  #currentIdx;
   #resultMap;
 
   constructor(bridge) {
     this.#bridge = bridge;
+    this.#currentIdx = 0;
     this.#resultMap;
+  }
+
+  fillMap(position) {
+    const result = this.canMove(position);
+
+    this.#resultMap[position].push(result);
+    this.#resultMap[Number(!position)].push(" ");
+
+    OutputView.printMap(this.#resultMap);
+
+    result == "O" ? this.move() : this.stop();
   }
 
   canMove(position) {
@@ -27,7 +40,14 @@ class BridgeGame {
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move() {
+    this.#currentIdx += 1;
+    InputView.readMoving();
+  }
+
+  stop() {
+    InputView.readGameCommand();
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
