d4d0b40a5a29d117661fc21f6bb83d8267ec3193
daal2quf <daal2quf@gmail.com>
2022-11-17 17:57:11 +0900

    feat(inputView): 유저에게 다리 길이를 요청하는 기능 추가
    add:
    readBridgeSize() : 다리길이를 입력받아 유저입력을 반환

--- src/InputView.js
+++ src/InputView.js
@@ -1,21 +1,17 @@
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+const { Console } = require("@woowacourse/mission-utils");
+
 const InputView = {
-  /**
-   * 다리의 길이를 입력받는다.
-   */
-  readBridgeSize() {},
 
-  /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
-  readMoving() {},
+  readBridgeSize() {
+    Console.readLine("다리의 길이를 입력해주세요.", (userInputBridgeLength) => {
+      return userInputBridgeLength;
+    });
+  },
 
-  /**
-   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-   */
-  readGameCommand() {},
+  readMoving() { },
+
+
+  readGameCommand() { },
 };
 
 module.exports = InputView;
