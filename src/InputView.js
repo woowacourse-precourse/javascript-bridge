d95865493f249743fb6dff34ac7cc5bc7d1fd03a
Hoon <si932174@gmail.com>
2022-11-17 16:26:53 +0900

    feat : 이동방향 입력 후 생사여부 판단

--- src/InputView.js
+++ src/InputView.js
@@ -2,32 +2,37 @@ const { Console } = require('@woowacourse/mission-utils');
 const BridgeMaker = require('./BridgeMaker');
 const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
 const BridgeGame = require('./BridgeGame');
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
+  gameInfo: {
+    bridge: [],
+    currentPosition: 0,
+  },
   /**
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize() {
     Console.readLine('다리의 길이를 입력해주세요.\n', bridgeSize => {
-      const bridge = BridgeMaker.makeBridge(
+      this.gameInfo.bridge = BridgeMaker.makeBridge(
         bridgeSize,
         BridgeRandomNumberGenerator,
       );
-      this.readMoving(bridge);
+      this.readMoving();
     });
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving(bridge) {
+  readMoving() {
     Console.readLine(
       '이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
       direction => {
-        console.log(direction);
-        console.log(bridge);
+        const bridgeGame = new BridgeGame();
+        const result = bridgeGame.move(direction, this.gameInfo);
       },
     );
   },
