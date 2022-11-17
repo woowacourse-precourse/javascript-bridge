ccf1a10225ecb23e226a2697c384055a576374f5
hahagarden <88613455+hahagarden@users.noreply.github.com>
2022-11-18 05:03:24 +0900

    test: 도출된 결과 O,X로 출력하는 기능

--- __tests__/OutputViewTest.js
+++ __tests__/OutputViewTest.js
@@ -7,10 +7,24 @@ const getSpyOn = () => {
   return logSpy;
 };
 
-describe("OutputView 클래스 테스트", () => {
+const logSpy = getSpyOn();
+
+describe("OutputView 객체 테스트", () => {
+  beforeEach(() => {
+    logSpy.mockClear();
+  });
+
   test("게임 시작 안내 문구를 출력한다.", () => {
-    const logSpy = getSpyOn();
     OutputView.printStart();
     expect(logSpy).toHaveBeenCalledWith(MESSAGE_START);
+  });
+
+  test("현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.", () => {
+    OutputView.printMap("U", "O");
+    expect(logSpy).toHaveBeenCalledWith(`[ O ]\n[   ]`);
+    OutputView.printMap("U", "O");
+    expect(logSpy).toHaveBeenCalledWith(`[ O | O ]\n[   |   ]`);
+    OutputView.printMap("D", "X");
+    expect(logSpy).toHaveBeenCalledWith(`[ O | O |   ]\n[   |   | X ]`);
   });
 });
