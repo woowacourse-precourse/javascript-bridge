c25f86719709e1150a60a76fcb19971f4733d6ea
bassyu <yrkdgus@gmail.com>
2022-11-17 22:43:44 +0900

    feat(App): 모델 객체 초기화 추가
    - BridgeMaker로 랜덤 다리 생성
    - BridgeGame으로 모델 객체 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-class App {
-  play() {}
-}
+const BridgeGame = require('./BridgeGame');
+const BridgeMaker = require('./BridgeMaker');
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
+const InputView = require('./InputView');
+const OutputView = require('./OutputView');
 
+class App {
+  #model;
+
+  play() {
+    OutputView.printStart();
+    InputView.readBridgeSize(this.init.bind(this));
+  }
+
+  init(size) {
+    const bridge = BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate);
+    this.#model = new BridgeGame(bridge);
+  }
+}
 module.exports = App;
