2bd6a5990f7470f6f495fa02470ee40f1bbf8382
hwanfront <kim015jh@gmail.com>
2022-11-17 15:42:14 +0900

    feat(CrossingBridge): 다리를 건너온 정보를 추가하는 메서드 추가

--- src/domain/result/Crossingbridge.js
+++ src/domain/result/Crossingbridge.js
@@ -1,6 +1,44 @@
 class CrossingBridge {
-  add() {
+  static #MARK = {
+    success: 'O',
+    fail: 'X',
+    empty: ' ',
+  };
 
+  #up = [];
+
+  #down = [];
+
+  add({ direction, isSuccessed }) {
+    if (isSuccessed) {
+      this.#successedMark(direction);
+      return;
+    }
+
+    this.#failedMark(direction);
+  }
+
+  #successedMark(direction) {
+    if (direction === 'U') {
+      this.#marking({ up: CrossingBridge.#MARK.success });
+      return;
+    }
+
+    this.#marking({ down: CrossingBridge.#MARK.success });
+  }
+
+  #failedMark(direction) {
+    if (direction === 'U') {
+      this.#marking({ down: CrossingBridge.#MARK.fail });
+      return;
+    }
+
+    this.#marking({ up: CrossingBridge.#MARK.fail });
+  }
+
+  #marking({ up = CrossingBridge.#MARK.empty, down = CrossingBridge.#MARK.empty }) {
+    this.#up.push(up);
+    this.#down.push(down);
   }
 
   print() {
