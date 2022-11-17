9219634deac3cd8de78059ba09f7a61ea15dc59b
Yoo Seung Guk <dbtmd878@naver.com>
2022-11-18 01:22:54 +0900

    feat: Promise 메서드를 콜백 메서드로 변경, 정답이 틀린 경우의 기능 구현, 재시작 및 종료 기능 구현, 정답일 경우 O를 넣는 기능 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,73 +1,69 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-const Console = MissionUtils.Console;
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
-
 const InputView = require("./InputView");
 const OutputView = require("./OutputView");
-
+const Console = MissionUtils.Console;
 // inputView, outputView 사용 불가
 class BridgeGame {
   #current_bridge = 0;
+  #try_count = 1;
+  #game_state;
   #bridge_map = [[], []];
   #ANSWER_BRIDGE_ARRAY;
 
   constructor(ANSWER_BRIDGE_ARRAY) {
     this.#ANSWER_BRIDGE_ARRAY = ANSWER_BRIDGE_ARRAY;
-    console.log(this.#ANSWER_BRIDGE_ARRAY);
+    console.log(this.#ANSWER_BRIDGE_ARRAY, "construct");
   }
 
-  play() {
-    console.log(this.#bridge_map);
-    this.move();
+  move() {
+    console.log("hi");
+    this.#game_state = false;
+    InputView.readMoving(this.checkBridgeNumber);
   }
 
-  async move() {
-    while (this.#current_bridge < this.#ANSWER_BRIDGE_ARRAY.length) {
-      const COMMAND = await InputView.readMoving();
-      this.chageCommandNumber(COMMAND);
-      this.#current_bridge += 1;
+  checkBridgeNumber = (COMMAND) => {
+    const INDEX = this.#current_bridge;
+    if (COMMAND === this.#ANSWER_BRIDGE_ARRAY[INDEX]) {
+      return this.circlePush(COMMAND);
     }
-    return this.endingPage();
-  }
-
-  async endingPage() {
-    const COMMAND = await InputView.readGameCommand();
-    if (COMMAND === "Q") Console.close();
-    if (COMMAND === "R") this.retry();
-  }
-
-  pushBridgeArray(commandNumber, restArrayNumber) {
-    if (
-      String(commandNumber) === this.#ANSWER_BRIDGE_ARRAY[this.#current_bridge]
-    ) {
-      this.#bridge_map[commandNumber].push("O");
-      this.#bridge_map[restArrayNumber].push(" ");
-      return OutputView.printMap(this.#bridge_map);
-    }
-    this.#bridge_map[commandNumber].push("X");
-    this.#bridge_map[restArrayNumber].push(" ");
-    return OutputView.printMap(this.#bridge_map);
-  }
-
-  chageCommandNumber(commandString) {
-    const COMMAND_NUMBER = commandString === "U" ? 1 : 0;
+    return this.crossPush(COMMAND);
+  };
+  changeCommandNumber(COMMAND) {
+    const COMMAND_NUMBER = COMMAND === "U" ? 1 : 0;
     const REST_ARRAY_NUMBER = COMMAND_NUMBER === 1 ? 0 : 1;
-    this.pushBridgeArray(COMMAND_NUMBER, REST_ARRAY_NUMBER);
-    return;
+    return [COMMAND_NUMBER, REST_ARRAY_NUMBER];
   }
 
-  /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  retry() {
+  circlePush(COMMAND) {
+    const [COMMAND_NUMBER, REST_ARRAY_NUMBER] =
+      this.changeCommandNumber(COMMAND);
+    this.#bridge_map[COMMAND_NUMBER].push("O");
+    this.#bridge_map[REST_ARRAY_NUMBER].push(" ");
+    OutputView.printMap(this.#bridge_map);
+    return this.currentBridge();
+  }
+
+  crossPush(COMMAND) {
+    const [COMMAND_NUMBER, REST_ARRAY_NUMBER] =
+      this.changeCommandNumber(COMMAND);
+    this.#bridge_map[COMMAND_NUMBER].push("X");
+    this.#bridge_map[REST_ARRAY_NUMBER].push(" ");
+    OutputView.printMap(this.#bridge_map);
+    return this.failGame();
+  }
+  failGame() {
+    return InputView.readGameCommand(this.stop, this.retry);
+  }
+  retry = () => {
     this.#bridge_map = [[], []];
     this.#current_bridge = 0;
-    return this.play();
-  }
+    console.log("retry", this.#bridge_map, this.#current_bridge);
+    return this.move();
+  };
+  stop = () => {
+    OutputView.printResult(this.#bridge_map, this.#try_count, this.#game_state);
+    return Console.close();
+  };
 }
 
 module.exports = BridgeGame;
