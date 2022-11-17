5dc5c42e565064c2d70928b6a756383777ccf240
Suyeon <msjgr2801@gmail.com>
2022-11-17 10:39:21 +0900

    Feat: 사용자에게 위아래 중 건널 칸을 입력받는 기능 구현
    - App.js에서 BridgeGame의 gameStart를 호출하여 실행
    - gameStart는 다리의 개수만큼 사용자의 moving을 입력받음
    - InputView 의 readMoving 으로 사용자에게 이동방향을 받는 기능 구현

--- src/InputView.js
+++ src/InputView.js
@@ -1,7 +1,11 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
+/*
+  * 사용자로부터 입력을 받는 역할을 한다.
+    제공된 InputView 객체를 활용해 구현해야 한다.
+    InputView의 파일 경로는 변경할 수 있다.
+    InputView의 메서드의 인자는 변경할 수 있다.
+    사용자 값 입력을 위해 필요한 메서드를 추가할 수 있다.
  */
 const InputView = {
   /**
@@ -32,7 +36,16 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    let moving = '';
+
+    MissionUtils.Console.readLine(
+      "이동할 칸을 선택해주세요 \n >",
+      answer => moving = answer
+    )
+
+    return moving;
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
