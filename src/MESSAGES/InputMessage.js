c4e365fec6362a715b9b56dfdb4e86aa7126e736
khj0426 <706shin1728@naver.com>
2022-11-18 00:21:56 +0900

    feat:재시작 문자열 입력 오류 시 다시 입력받는 문자열 상수화

--- src/MESSAGES/InputMessage.js
+++ src/MESSAGES/InputMessage.js
@@ -1,6 +1,7 @@
 const INPUT_MESSAGE = {
   BRIDGE_INPUT: '다리의 길이를 입력해주세요.',
   BRIDGE_INPUT_ERROR: '다리 길이는 3부터 20사이의 숫자여야 합니다.',
+  REGAME_INPUT_ERROR: '재시도는 R,종료는 Q입니다.다시 입력해주세요',
 };
 
 Object.freeze(INPUT_MESSAGE);
