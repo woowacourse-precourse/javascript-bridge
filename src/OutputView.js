69ebc3da3124768bafe919ce398eb29a2795a0cb
guridaek <iue16@naver.com>
2022-11-17 16:01:04 +0900

    feat: 이동시 다리 출력 기능 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,3 +1,4 @@
+const { Console } = require('@woowacourse/mission-utils');
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -7,7 +8,25 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {},
+  printMap(bridge, movingLog) {
+    let map = '';
+    ['U', 'D'].forEach(direction => {
+      map += this.getLine(direction, bridge, movingLog);
+      map += '\n';
+    });
+
+    Console.print(map);
+  },
+
+  getLine(direction, bridge, movingLog) {
+    const line = Array(movingLog.length).fill(' ');
+    movingLog.forEach((value, idx) => {
+      if (value === direction && value === bridge[idx]) line[idx] = 'O';
+      if (value === direction && value !== bridge[idx]) line[idx] = 'X';
+    });
+
+    return `[ ${line.join(' | ')} ]`;
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
