eb8cc71b397ff9b4381d7eec4be84dcfa00c4b4b
Narcoker <jykim9868@gmail.com>
2022-11-17 23:34:46 +0900

    Refactor: 재시도 분기문 객체 내부 함수 사용

--- src/InputView.js
+++ src/InputView.js
@@ -1,8 +1,19 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const Bridge = require("./Bridge");
-const BridgeGame = require('./BridgeGame');
 const OutputView = require('./OutputView')
 const { MESSAGE } = require("./Messages");
+
+const restartType = {
+  Q() {
+    OutputView.printResult(bridgeGame);
+    return bridgeGame.exit();
+  },
+  R() {
+    bridgeGame.retry();
+    return this.readMoving(bridgeGame);
+  }
+}
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -19,7 +30,6 @@ const InputView = {
       console.log(bridgeGame.getBridge());
       this.readMoving(bridgeGame);
     })
-
   },
 
   /**
@@ -28,7 +38,7 @@ const InputView = {
   readMoving(bridgeGame) {
     MissionUtils.Console.readLine(MESSAGE.ENTER_MOVE_TYPE, (moveType) => {
       if (!bridgeGame.validateMoveType(moveType))
-        return this.readMoving(bridgeGame)
+        return this.readMoving(bridgeGame);
 
       bridgeGame.move(moveType);
       OutputView.printMap(bridgeGame);
@@ -36,34 +46,33 @@ const InputView = {
       if (bridgeGame.isFailMove(bridgeGame.getUpDownHistory()))
         return this.readGameCommand(bridgeGame);
 
-      let maxPosition = bridgeGame.getBridge().length - 1;
-      let position = bridgeGame.getPosition();
-      if (position === maxPosition) {
-        bridgeGame.setIsSuccess("성공");
-        OutputView.printResult(bridgeGame);
-        return bridgeGame.exit(); w
-      }
-      if (position !== maxPosition)
-        this.readMoving(bridgeGame);
+      return this.checkEndPosition(bridgeGame);
     })
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
+
   readGameCommand(bridgeGame) {
     MissionUtils.Console.readLine(MESSAGE.ENTER_COMMAND, (command) => {
       if (!bridgeGame.validateCommand(command))
         return this.readGameCommand(bridgeGame)
-      if (command === "Q") {
-        OutputView.printResult(bridgeGame);
-        return bridgeGame.exit();
-      }
-      if (command === "R") {
-        bridgeGame.retry();
-        return this.readMoving(bridgeGame);
-      }
+
+      restartType[command];
     });
+  },
+
+  checkEndPosition(bridgeGame) {
+    let maxPosition = bridgeGame.getBridge().length - 1;
+    let position = bridgeGame.getPosition();
+    if (position === maxPosition) {
+      bridgeGame.setIsSuccess("성공");
+      OutputView.printResult(bridgeGame);
+      return bridgeGame.exit();
+    }
+    if (position !== maxPosition)
+      this.readMoving(bridgeGame);
   }
 };
 
