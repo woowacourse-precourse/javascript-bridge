7ccb8e06d44394a27d43acfb65563e93bee1f7ae
Woong <jiyko7@kyonggi.ac.kr>
2022-11-18 12:57:43 +0900

    feat: 게임 시작 문구 출력
    feat: 게임 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 class App {
-  play() {}
+  play() {
+    const MISSIONUTILS = require("@woowacourse/mission-utils");
+
+    MISSIONUTILS.Console.print("다리 건너기 게임을 시작합니다.");
+  }
 }
 
 module.exports = App;
