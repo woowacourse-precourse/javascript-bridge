24772408e4ab5fddfc4383529a552ca5265ce06f
이건우 <lifeflavor@igeon-uui-MacBookPro.local>
2022-11-18 13:27:40 +0900

    feat(bridge)- 다리 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 const InputView = require("./InputView");
-const ArraySize = require("./modules/ArraySize");
 const BridgeMaker = require("./BridgeMaker");
 const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
 class App {
@@ -7,16 +6,9 @@ class App {
     // 다리의 길이 & 시도 횟수
     const size = InputView.readBridgeSize();
     let tryGame = 0;
-    // 다리의 길이만큼의 배열 생성
-    const ARRAY = new ArraySize(size);
-    let arrUp = ARRAY.getArr();
-    let arrDown = ARRAY.getArr();
-    // 0,1 무작위 값 설정
-    let randomArr = ARRAY.getArr().map(
-      (e) => (e = BridgeRandomNumberGenerator.generate())
-    );
     // 이동할 칸(U,D)을 입력한다.
     const upOrDown = InputView.readMoving(size);
+    BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate());
   }
 }
 
--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,3 +1,5 @@
+const ArraySize = require("./modules/ArraySize");
+
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
@@ -7,7 +9,19 @@ const BridgeMaker = {
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
-  makeBridge(size, generateRandomNumber) {},
+  makeBridge(size, generateRandomNumber) {
+    // 다리의 길이만큼의 배열 생성
+    const ARRAY = new ArraySize(size);
+    let arrUp = ARRAY.getArr();
+    let arrDown = ARRAY.getArr();
+    // 0,1 무작위 값 설정
+    let randomArr = ARRAY.getArr().map(
+      (element) => (element = generateRandomNumber())
+    );
+    //다리 생성
+    arrUp = randomArr.map((element) => (element === "1" ? "O" : " "));
+    arrDown = randomArr.map((element) => (element === "0" ? "O" : " "));
+  },
 };
 
 module.exports = BridgeMaker;
