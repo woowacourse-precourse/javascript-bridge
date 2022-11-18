2f90f36d3aa11e593f78503546615122f4c62edb
ramda <91455804+gnfnfnr@users.noreply.github.com>
2022-11-19 00:07:14 +0900

    feat: print and input movement

--- src/controller/BridgeProcess.js
+++ src/controller/BridgeProcess.js
@@ -1,9 +1,10 @@
 const InputView = require('../view/InputView');
 const OutputView = require('../view/OutputView');
 const { Console } = require('@woowacourse/mission-utils');
-const { PRINTBRIDGESIZE } = require('../view/Message');
-const BridgeMaker = require('../model/BridgeMaker');
+const { PRINTBRIDGESIZE, PRINTMOVEMENT } = require('../view/Message');
+const BridgeMaker = require('../BridgeMaker');
 const { generate } = require('../model/BridgeRandomNumberGenerator');
+const MovementVadlidation = require('../model/Movement');
 
 // 명심하기! controller는 데이터를 받거나 사용자에게 동작을 받으면 model 또는 view를 변경
 
@@ -19,13 +20,15 @@ class BridgeProcess {
   #inputBridgeSize() {
     Console.readLine(PRINTBRIDGESIZE, (bridgeSize) => {
       const bridgeSizeNumber = this.#inputView.readBridgeSize(bridgeSize);
-      bridgeSizeNumber ? this.#gen(bridgeSizeNumber) : this.#inputBridgeSize();
+      const match = BridgeMaker.makeBridge(bridgeSizeNumber, generate);
+      bridgeSizeNumber ? this.#inputMovement() : this.#inputBridgeSize();
     });
   }
 
-  #gen(bridgeSizeNumber) {
-    const tt = BridgeMaker.makeBridge(bridgeSizeNumber, generate);
-    console.log(tt);
+  #inputMovement() {
+    Console.readLine(PRINTMOVEMENT, (movement) => {
+      Console.print(movement);
+    });
   }
 }
 module.exports = BridgeProcess;
--- src/view/Message.js
+++ src/view/Message.js
@@ -1,4 +1,5 @@
 const PRINTSTARTGAME = '다리 건너기 게임을 시작합니다.';
 const PRINTBRIDGESIZE = '다리의 길이를 입력해주세요.\n';
+const PRINTMOVEMENT = '이동할 칸을 선택해주세요. (위: U, 아래: D)';
 
-module.exports = { PRINTSTARTGAME, PRINTBRIDGESIZE };
+module.exports = { PRINTSTARTGAME, PRINTBRIDGESIZE, PRINTMOVEMENT };
