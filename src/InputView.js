5dfe87f0b66f1d9475754a5cc3f3d9dd4a98649e
jiyujin <yujin1679@naver.com>
2022-11-18 21:58:03 +0900

    feat: 입력값 예외사항 처리하는 기능 추가

--- src/InputView.js
+++ src/InputView.js
@@ -2,26 +2,36 @@
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const { Console } = require("@woowacourse/mission-utils");
+const { validateBridgeSize, validateUserMove, validateUserEnd } = require("./InputModel");
 
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize(callback) {
-    Console.readLine('다리의 길이를 입력해주세요.\n', input => {
-      callback(Number(input));
+    Console.readLine('다리의 길이를 입력해주세요.\n', bridgeSize => {
+      validateBridgeSize(bridgeSize);
+      callback(Number(bridgeSize));
     });
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)', userMove => {
+      validateUserMove(userMove);
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+    Console.readLine('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)', userEnd => {
+      validateUserEnd(userEnd);
+    });
+  },
 };
 
 module.exports = InputView;
