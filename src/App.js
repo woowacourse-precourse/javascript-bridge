9cde09bd3adc0d0fe28fee7809103d8ce1bf4507
Jeong Hun Nam <njh3421@gmail.com>
2022-11-18 18:43:46 +0900

    feat : add starting code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+const InputView=require('./InputView')
+const BridgeMaker=require('./BridgeMaker')
+const BridgeRandomNumberGenerator=require('./BridgeRandomNumberGenerator')
+const OutputView=require('./OutputView')
+const BridgeGame=require('./BridgeGame')
+
 class App {
-  play() {}
+  constructor(){
+    let game=new BridgeGame()
+  }
+  play() {
+    InputView.printGameStart()
+    InputView.readBridgeSize()
+    // InputView.readMoving()
+  }
 }
+const app=new App()
+app.play()
 
 module.exports = App;
