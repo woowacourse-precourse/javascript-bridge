31ed84a5d80b864fcb081aba75d503dfa283d8b7
MAGHC <windowsapple95@gmail.com>
2022-11-18 20:24:14 +0900

    feat: BridgeMap.addCorrect 맞췄을때 map추가 기능 구현

--- src/BridgeMap.js
+++ src/BridgeMap.js
@@ -2,6 +2,19 @@ class BridgeMap {
   #FIRST_MAP_SOURCE = { CORRECT: ' O ', INCORRECT: ' X ', EMPTY: '   ' };
 
   #MAP_SOURCE = { CORRECT: '| O ', INCORRECT: '| X ', EMPTY: '|   ' };
+
+  static addCorrect(input, beforeMap, mapSource) {
+    // console.log(input, beforeMap, mapSource, 'addCorrect 확인용');
+    const [up, down] = beforeMap;
+    if (input === 'D') {
+      up.push(mapSource.EMPTY);
+      down.push(mapSource.CORRECT);
+    } else {
+      up.push(mapSource.CORRECT);
+      down.push(mapSource.EMPTY);
+    }
+    return [up, down];
+  }
 }
 
 module.exports = BridgeMap;
