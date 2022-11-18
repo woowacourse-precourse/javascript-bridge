9497b72bed085d99e73377658beef547b76f4855
thewronghand <penfreak77@gmail.com>
2022-11-19 08:29:32 +0900

    test: BridgeMaker 객체 내 makeBridge 메서드 테스트 구현
    makeBridge 메서드 사용 시 입력된 길이만큼의 배열이 생성되는지, 요소는 U와 D로 한정되는지 확인하는 테스트를 구현한다.

---
+++ __tests__/BridgeMakerTest.js
@@ -0,0 +1,16 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const BridgeMaker = require('../src/BridgeMaker');
+const BridgeRandomNumberGenerator = require('../src/BridgeRandomNumberGenerator');
+
+describe('다리 만들기 테스트', () => {
+  test('다리 생성 시 입력한 길이만큼의 다리 배열이 생성된다.', () => {
+    const testBridge = BridgeMaker.makeBridge(5,BridgeRandomNumberGenerator.generate);
+    expect(testBridge.length).toEqual(5);
+  });
+
+  test('다리 생성 시 랜덤으로 생성된 배열의 요소는 U와 D로 한정된다.', () => {
+    const testBridge = BridgeMaker.makeBridge(5,BridgeRandomNumberGenerator.generate);
+    const uniqueTestBridgeSet = new Set(testBridge);
+    expect(uniqueTestBridgeSet.size).toEqual(2);
+  })
+});
