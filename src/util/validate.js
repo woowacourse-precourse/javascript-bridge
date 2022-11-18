a948f3aadee1effc346006803d286532c586c635
SNUPI <joohaem@daum.net>
2022-11-18 11:51:22 +0900

    fix: modify validate condition

--- src/util/validate.js
+++ src/util/validate.js
@@ -1,7 +1,7 @@
 const { ERROR_MESSAGE } = require("./messages");
 
 function validateBrigeSize(size) {
-  if (Number.isInteger(size)) throw new Error(ERROR_MESSAGE.sizeInteger);
+  if (!Number.isInteger(size)) throw new Error(ERROR_MESSAGE.sizeInteger);
   if (size < 3 || size > 20) throw new Error(ERROR_MESSAGE.sizeRange);
 
   return true;
