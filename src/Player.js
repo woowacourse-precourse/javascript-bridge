a96a13270e20d8b446c0e9a1d1ea5c4e615216cb
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-18 16:46:01 +0900

    refactor(Player): 클래스 내부에서만 사용되는 메서드 private 처리

--- src/Player.js
+++ src/Player.js
@@ -5,20 +5,20 @@ class Player {
   #directions = [];
 
   addDirection(direction) {
-    this.validateDirection(direction);
+    this.#validateDirection(direction);
 
     this.#directions.push(direction);
   }
 
-  validateDirection(direction) {
+  #validateDirection(direction) {
     if (!direction) throw new Error(ERROR_MSG.emptyInput);
 
-    if (!this.isValidDirectionSymbol(direction)) {
+    if (!this.#isValidDirectionSymbol(direction)) {
       throw new Error(ERROR_MSG.invalidDirection);
     }
   }
 
-  isValidDirectionSymbol(direction) {
+  #isValidDirectionSymbol(direction) {
     return direction === DOWNSIDE_SYMBOL || direction === UPSIDE_SYMBOL;
   }
 }
