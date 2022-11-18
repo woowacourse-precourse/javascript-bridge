51165e23def0f85a8668919336dc2fdd29374b7b
dudgp128 <20181048@sungshin.ac.kr>
2022-11-18 18:03:28 +0900

    TEST(BridgeTest) : 수정된 함수명 반영, 잘못된 입력 수정

--- __tests__/BridgeTest.js
+++ __tests__/BridgeTest.js
@@ -1,8 +1,8 @@
 const BridgeGame = require("../src/BridgeGame");
 
 describe(" 다리 테스트 ", () => {
-  test("fillResultMap 테스트", () => {
-    const bridge = ["0", "1", "0"]; //D,U,D,D
+  test("fillMap 테스트", () => {
+    const bridge = ["D", "U", "D"];
     const bridgeGame = new BridgeGame(bridge);
     const inputs = ["D", "U", "U"];
     const results = [
@@ -17,8 +17,8 @@ describe(" 다리 테스트 ", () => {
       ],
     ];
     inputs.forEach((input, idx) => {
-      bridgeGame.transInputtoPosition(input);
-      expect(bridgeGame.getResultMap()).toEqual(results[idx]);
+      bridgeGame.canMove(input);
+      expect(bridgeGame.getBridgeMap()).toEqual(results[idx]);
     });
   });
 });
