eebd560de193fc7bd89bc1f74130a0cfcb20f6e2
turtle601 <turtle601@naver.com>
2022-11-18 09:39:25 +0900

    feat(BridgeLength) create 동작 추가
    <BridgeLength 클래스>
    - doAction() : 입력값 검증 후 데이터 저장
    - createBridgeData():
      1) 랜덤 다리 만들기
      2) 사용자 다리 만들기
      3) 시도 횟수 1 설정

--- src/service/domain/BridgeLength.js
+++ src/service/domain/BridgeLength.js
@@ -1,18 +1,58 @@
-const { REGEX, ERROR_MESSAGE } = require('../../utils/constants');
+const BridgeService = require('../BridgeService');
 
-class BridgeLength {
+const { REGEX, ERROR_MESSAGE, MODEL_KEY } = require('../../utils/constants');
+const { makeBridge } = require('../../utils/BridgeMaker');
+const BridgeRandomNumberGenerator = require('../../utils/BridgeRandomNumberGenerator');
+
+class BridgeLength extends BridgeService {
   #input;
 
   constructor(input) {
+    super();
+
     this.#input = input;
   }
 
-  getValidateData() {
+  #getValidateData() {
     if (!REGEX.bridgeLength.test(this.#input)) {
       throw new Error(ERROR_MESSAGE.bridgeLength);
     }
 
-    return Number(this.#input);
+    return this;
+  }
+
+  #createRandomBridgeData() {
+    this.setModelFor(
+      MODEL_KEY.randomBridge,
+      makeBridge(Number(this.#input), BridgeRandomNumberGenerator.generate)
+    );
+
+    return this;
+  }
+
+  #createAnwerBrideData() {
+    this.setModelFor(
+      MODEL_KEY.answerBridge,
+      Array.from({ length: this.#input }, () => null)
+    );
+
+    return this;
+  }
+
+  #createTryCountBridgeData() {
+    this.setModelFor(MODEL_KEY.tryCount, 1);
+
+    return this;
+  }
+
+  #createBridgeData() {
+    this.#createRandomBridgeData()
+      .#createAnwerBrideData()
+      .#createTryCountBridgeData();
+  }
+
+  doAction() {
+    this.#getValidateData().#createBridgeData();
   }
 }
 
