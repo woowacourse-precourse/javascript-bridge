74c79a08d25dc239a58c0f8e0bc07afdfd59ef3a
하지훈 <jnh03336@gmail.com>
2022-11-18 22:19:25 +0900

    feat(domain): 게임 실패 판단

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -22,4 +22,15 @@ describe('다리 게임 test', () => {
       expect(bridgeGame.isGameWin()).toBe(i === bridge.length - 1);
     }
   });
+
+  test('게임 실패 판단', () => {
+    const bridge = ['U', 'D', 'D', 'U', 'D'];
+    const choice = ['U', 'D', 'D', 'D'];
+    const bridgeGame = new BridgeGame(bridge);
+
+    for (let i = 0; i < choice.length; i += 1) {
+      bridgeGame.move(choice[i]);
+      expect(bridgeGame.isGameLose()).toBe(!(bridge[i] === choice[i]));
+    }
+  });
 });
