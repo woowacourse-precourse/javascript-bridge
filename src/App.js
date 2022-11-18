bd04c6333e88323332898e16db3ca00326c6effd
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2022-11-18 17:02:36 +0900

    feat(BridgeMaker): size를 입력받아 다리를 만드는 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const InputView = require("./InputView");
+const {generate} = require("./BridgeRandomNumberGenerator");
+const {makeBridge} = require("./BridgeMaker");
 const [Console, Random] = [MissionUtils.Console, MissionUtils.Random];
 
 
 class App {
 
   #bridgeSize;
+  #bridge;
 
   constructor(){
-    this.#bridgeSize =0;
+    this.#bridgeSize = 0;
+    this.#bridge = [];
   }
   
   async play() {
     InputView.gameStart();
     this.#bridgeSize = await InputView.readBridgeSize();
 
-    await Console.print(this.#bridgeSize);
-    Console.close();
   }
 
 }
--- src/InputView.js
+++ src/InputView.js
@@ -4,18 +4,18 @@ const [Console] = [MissionUtils.Console, MissionUtils.Random];
 
 const InputView = {
 
-  validateBridge(bridgeInput){
-    if(typeof bridgeInput !== "number") throw new Error("[ERROR] 다리길이는 숫자여야 합니다.");
-    if(!(3<=bridgeInput && bridgeInput<=20)) throw new Error("[ERROR] 다리길이는 3에서 20사이의 숫자여야 합니다.");
-  },
-
-  gameStart(){
+  gameStart() {
     Console.print("다리 건너기 게임을 시작합니다.");
   },
 
+  validateBridge(bridgeInput) {
+    if (typeof bridgeInput !== "number") throw new Error("[ERROR] 다리길이는 숫자여야 합니다.");
+    if (!(3 <= bridgeInput && bridgeInput <= 20)) throw new Error("[ERROR] 다리길이는 3에서 20사이의 숫자여야 합니다.");
+  },
+
   readBridgeSize() {
-    return new Promise((resolve,_) => {
-      Console.readLine("다리의 길이를 입력해주세요.\n", (inputString)=>{
+    return new Promise((resolve, _) => {
+      Console.readLine("다리의 길이를 입력해주세요.\n", (inputString) => {
         const inputNum = Number(inputString);
         this.validateBridge(inputNum);
         resolve(inputNum);
@@ -24,19 +24,16 @@ const InputView = {
     })
   },
 
-  getBridgeSize(){
-    return this.bridgeSize;
-  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() { },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() { },
 };
 
 module.exports = InputView;
