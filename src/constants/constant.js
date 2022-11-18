32a9d6831553fd84b02b92cbad13a202631ff442
pangkyu <baesense@naver.com>
2022-11-19 08:01:08 +0900

    Feat : constant.js 메세지 설정
    하드코딩된 값을 피하기 위해 미리 constant에 선언하여 호출하는 방식으로 구현 예정

---
+++ src/constants/constant.js
@@ -0,0 +1,9 @@
+const MESSAGE = {
+  BRIDGE_GAME_START: "다리 건너기 게임을 시작합니다.",
+  INPUT_BRIDGE_LENGTH: "다리의 길이를 입력해주세요.",
+  CHOOSE_MOVE_SPACE: "이동할 칸을 선택해주세요. (위: U, 아래: D)",
+  SELECT_RETRY:
+    "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
+};
+
+module.exports = { MESSAGE };
