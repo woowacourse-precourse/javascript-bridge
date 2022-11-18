b868534c34c31cc24b6ef68f6039f69f2c547dfd
saejinpark <saejinpark951210@gmail.com>
2022-11-18 21:50:21 +0900

    play

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const BridgeGame = require("./BridgeGame");
+const wConsole = MissionUtils.Console;
+
+const { readBridgeSize } = require("./InputView");
+
 class App {
-  play() {}
+  play() {
+    const bridgeGame = new BridgeGame();
+    wConsole.print("다리 건너기 게임을 시작합니다.\n");
+    readBridgeSize(bridgeGame);
+  }
 }
 
 module.exports = App;
