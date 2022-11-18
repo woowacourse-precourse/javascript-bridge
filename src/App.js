207cabea5b51abcb4502931d2910ed1afa82da33
MAGHC <windowsapple95@gmail.com>
2022-11-18 14:07:31 +0900

    feat: App.js 다리의길이 입력, 다리만들기 까지의 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
+const InputView = require('./InputView');
+const BridgeMaker = require('./BridgeMaker');
+const OutputView = require('./OutputView');
+const Validator = require('./Validator');
+
 class App {
-  play() {}
+  #appStatus = 1;
+
+  #bridgeAnswer;
+
+  play() {
+    OutputView.printStart();
+    this.progressApp(this.#appStatus);
+  }
+
+  progressApp(appStatus) {
+    if (appStatus === 1) return this.progressBridgeMake();
+    if (appStatus === 2) return BridgeMaker.makeBridge(this.#bridgeAnswer);
+  }
+
+  progressBridgeMake() {
+    InputView.readBridgeSize((answer) => {
+      if (Validator.checkBridgeInput(answer)) {
+        this.#bridgeAnswer = +answer;
+        this.#appStatus = 2;
+        console.log(this.#appStatus, typeof this.#bridgeAnswer, '확인');
+        this.progressApp(this.#appStatus);
+      }
+    });
+  }
 }
 
 module.exports = App;
+
+const app = new App();
+
+console.log(app.play());
+
+// 실패가 되면 메서드를 재실행시켜야됨
