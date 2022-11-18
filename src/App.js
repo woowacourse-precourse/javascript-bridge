f09945405cb3db54ebc3b392a5dbd35e320c43ea
guridaek <iue16@naver.com>
2022-11-18 15:08:21 +0900

    feat: 게임 초기화 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+const { Console } = require('@woowacourse/mission-utils');
+const BridgeGame = require('./BridgeGame.js');
+const InputView = require('./InputView.js');
+const Validation = require('./Validation.js');
+
 class App {
-  play() {}
+  #bridgeGame;
+
+  play() {
+    this.initBridgeGame();
+  }
+
+  initBridgeGame() {
+    try {
+      const bridgeSize = InputView.readBridgeSize();
+      Validation.validateBridgeSize(bridgeSize);
+      this.#bridgeGame = new BridgeGame(bridgeSize);
+      this.playBridgeGame();
+    } catch (err) {
+      Console.print(err);
+      this.initBridgeGame();
+    }
+  }
+
+  playBridgeGame() {}
+
+  checkBridgeGame() {}
+
+  requestRetryBridgeGame() {}
+
+  retryBridgeGame() {}
+
+  endBridgeGame() {}
 }
 
 module.exports = App;
