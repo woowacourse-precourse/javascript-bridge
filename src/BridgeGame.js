e4779f9f1a9e29a319898852aa091809a760f3ee
MAGHC <windowsapple95@gmail.com>
2022-11-18 15:44:23 +0900

    feat: BridgeGame move 기능 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -21,7 +21,17 @@ class BridgeGame {
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(input, bridge) {
+    const nowBridgeLength = this.#bridgeLengthStatus;
+    this.incrementNumberOfTry();
+    if (bridge[this.#bridgeLengthStatus] === input) {
+      this.incrementBridgeLengthStatus();
+      console.log(nowBridgeLength, this.#bridgeLengthStatus);
+      return [true, nowBridgeLength];
+    }
+    return [false, nowBridgeLength];
+  }
+
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
    * <p>
@@ -29,5 +39,12 @@ class BridgeGame {
    */
   retry() {}
 }
+
+const app = new BridgeGame();
+
+const mockBridge = ['D', 'U', 'D', 'U'];
+
+console.log(app.move('D', mockBridge));
+console.log(app.move('D', mockBridge));
 
 module.exports = BridgeGame;
