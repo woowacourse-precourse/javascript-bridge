6fe22b8a502f696f451e9b0638e72c85c6e005a7
bassyu <yrkdgus@gmail.com>
2022-11-18 14:22:39 +0900

    feat(InputView): 예외 처리 추가
    - 예외가 발생하면 오류 메세지 출력 후 다시 입력

--- src/InputView.js
+++ src/InputView.js
@@ -1,30 +1,35 @@
 const { Console } = require('@woowacourse/mission-utils');
+const OutputView = require('./OutputView');
 
 const InputView = {
   INPUT_MESSAGE: {
-    bridgeSize: '다리의 길이를 입력해주세요.',
-    moving: '이동할 칸을 선택해주세요. (위: U, 아래: D)',
-    gameCommand: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)',
+    bridgeSize: '\n다리의 길이를 입력해주세요.\n',
+    moving: '\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
+    gameCommand: '\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
   },
 
   readBridgeSize(handleBridgeSize) {
-    Console.print(InputView.INPUT_MESSAGE.bridgeSize);
-    Console.readLine('', (input) => {
+    Console.readLine(InputView.INPUT_MESSAGE.bridgeSize, (input) => {
+      try {
+        if (isNaN(input)) throw new Error('숫자를 입력해 주세요.');
+      } catch (e) {
+        OutputView.printError(e.message);
+        InputView.readBridgeSize(handleBridgeSize);
+      }
+
       const size = Number(input);
       handleBridgeSize(size);
     });
   },
 
   readMoving(handleMoving) {
-    Console.print(InputView.INPUT_MESSAGE.moving);
-    Console.readLine('', (input) => {
+    Console.readLine(InputView.INPUT_MESSAGE.moving, (input) => {
       handleMoving(input);
     });
   },
 
   readGameCommand(handleGameCommand) {
-    Console.print(InputView.INPUT_MESSAGE.gameCommand);
-    Console.readLine('', (input) => {
+    Console.readLine(InputView.INPUT_MESSAGE.gameCommand, (input) => {
       handleGameCommand(input);
     });
   },
