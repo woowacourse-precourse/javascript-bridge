37d23e327212200f002150bc1390755e609349c0
YMS <yms1789@naver.com>
2022-11-18 19:35:48 +0900

    Feat: 다리 상태 출력을 위한 입력 값 저장 변수 추가

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -7,11 +7,19 @@ class BridgeGame {
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
+  moveResult = [];
+  inputMoves = [];
   move(inputMove, callMove, bridge) {
-    const moveResult = bridge[callMove] === inputMove ? "O" : "X";
-    return [callMove + 1, moveResult];
+    if (bridge[callMove] === inputMove) {
+      this.moveResult.push("O");
+      this.inputMoves.push(inputMove);
+    }
+    if (bridge[callMove] !== inputMove) {
+      this.moveResult.push("X");
+      this.inputMoves.push(inputMove);
+    }
+    return [callMove + 1, this.moveResult, this.inputMoves];
   }
-
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
    * <p>
