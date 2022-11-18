c194fca386866ef4ad9fd0f1b6c97e217eef94a7
murpin <qw04011@gmail.com>
2022-11-18 15:59:19 +0900

    feat(BridgeGame/constructor): 다리게임 클래스 constructor 설정

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -2,6 +2,15 @@
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  constructor(state) {
+    this.nowPosition = 0;
+    this.tryCount = 0;
+    this.bridgeState = state;
+    this.nowBridge = {
+      upperBridge: [],
+      downBridge: [],
+    }
+  }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
