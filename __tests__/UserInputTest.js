657d67e809c79f2a57faefb9ff43ef573e7b75e3
Hwang Seong Taek <hst007@naver.com>
2022-11-18 13:42:40 +0900

    test(UserInputTest): 이동할 칸의 잘못된 입력 테스트

--- __tests__/UserInputTest.js
+++ __tests__/UserInputTest.js
@@ -13,4 +13,10 @@ describe('사용자의 입력 테스트', () => {
       InputView.validateBridgeSize(input);
     }).toThrow(errorMessage.OUT_RANGE_NUMBER);
   });
+
+  test.each([['+'], ['u'], ['d']])("이동할 칸의 입력이 'U' 또는 'D'가 아닐 경우 예외 발생" , (input) => {
+    expect(() => {
+      InputView.validateMovingDirection(input);
+    }).toThrow(errorMessage.MOVE_DIRECTION);
+  });
 });
