d37fd5ba61b564909c1fc57be16256163dd23ed4
kimeuns <dmstn2006@gmail.com>
2022-11-19 02:35:29 +0900

    feat: 재시작, 종료 값 유효성 검사 추가

--- src/Validator.js
+++ src/Validator.js
@@ -1,18 +1,27 @@
-const { BRIDGE, ERROR } = require("./constants");
+const { BRIDGE, ERROR, GAME, BRIDGE_MOVEMENT } = require("./constants");
 
 const Validator = {
   isValidBridgeSize(input) {
+    // eslint-disable-next-line no-restricted-globals
     if (isNaN(input)) {
       throw new Error(ERROR.BRIDGE_SIZE_TYPE);
     }
-    if (input < BRIDGE.SIZE_MIN || input > BRIDGE.SIZE_MAX) {
+    if (+input < BRIDGE.SIZE_MIN || +input > BRIDGE.SIZE_MAX) {
       throw new Error(ERROR.BRIDGE_SIZE_RANGE);
     }
     return true;
   },
+
   isValidMoving(input) {
-    if (input !== 'D' && input !== 'U') {
+    if (input !== BRIDGE_MOVEMENT.DOWN && input !== BRIDGE_MOVEMENT.UP) {
       throw new Error(ERROR.BRIDGE_MOVING);
+    }
+    return true;
+  },
+
+  isValidInput(input) {
+    if (input !== GAME.RETRY && input !== GAME.QUIT) {
+      throw new Error(ERROR.RETRY_END);
     }
     return true;
   },
