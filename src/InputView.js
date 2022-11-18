cd6670f6b003691954a7b702d9d8656e11fb014e
jiyujin <yujin1679@naver.com>
2022-11-18 21:37:34 +0900

    feat: 다리 길이를 입력하는 readBridgeSize 작성

--- src/InputView.js
+++ src/InputView.js
@@ -1,11 +1,17 @@
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
+const { Console } = require("@woowacourse/mission-utils");
+
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize(callback) {
+    Console.readLine('다리의 길이를 입력해주세요.\n', input => {
+      callback(Number(input));
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
