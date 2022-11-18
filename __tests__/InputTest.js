cc2c593b149db2258e1dd7b1250a0ee1a50acb50
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-18 15:09:35 +0900

    test: 다리길이 입력값 테스트 코드 수정

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -2,25 +2,33 @@ const InputView = require("../src/InputView");
 const ERROR_MESSAGES = require("../src/constants/ErrorMessages");
 
 describe("입력값 테스트", () => {
-  const inputView = new InputView();
-
   test("다리 길이 입력값 테스트 ( 문자 )", () => {
-    expect(inputView.readBridgeSize("a")).toThrow(ERROR_MESSAGES.NAN);
+    expect(() => {
+      InputView.validateBridgeSize("a");
+    }).toThrow(ERROR_MESSAGES.NAN);
   });
 
   test("다리 길이 입력값 테스트 ( 특수문자 )", () => {
-    expect(inputView.readBridgeSize("@")).toThrow(ERROR_MESSAGES.NAN);
+    expect(() => {
+      InputView.validateBridgeSize("@");
+    }).toThrow(ERROR_MESSAGES.NAN);
   });
 
   test("다리 길이 범위 테스트 (*3 ~ 20)", () => {
-    expect(inputView.readBridgeSize("2")).toThrow(ERROR_MESSAGES.BRIDGE_SIZE);
+    expect(() => {
+      InputView.validateBridgeSize("2");
+    }).toThrow(ERROR_MESSAGES.SIZE);
   });
 
   test("다리 길이 범위 테스트 (*3 ~ 20)", () => {
-    expect(inputView.readBridgeSize("21")).toThrow(ERROR_MESSAGES.BRIDGE_SIZE);
+    expect(() => {
+      InputView.validateBridgeSize("21");
+    }).toThrow(ERROR_MESSAGES.SIZE);
   });
 
   test("다리 길이 범위 테스트 (*3 ~ 20)", () => {
-    expect(inputView.readBridgeSize("3")).toBeTruthy();
+    expect(() => {
+      InputView.validateBridgeSize("3");
+    }).toBeTruthy();
   });
 });
