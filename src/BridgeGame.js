b7d131fe9a5e1486939b1adcb9f6e4b372a620f8
js43o <js43o@naver.com>
2022-11-18 22:13:17 +0900

    feat(BridgeGame): 생성자 및 필드 추가

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,19 +1,21 @@
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
+const { makeBridge } = require('./BridgeMaker');
+const { generate } = require('./BridgeRandomNumberGenerator');
+
 class BridgeGame {
-  /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
+  #bridge;
+  #position;
+  #attempts;
+  #failed;
+
+  constructor(length) {
+    this.#bridge = makeBridge(length, generate);
+    this.#position = 0;
+    this.#attempts = 0;
+    this.#failed = false;
+  }
+
   move() {}
 
-  /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
   retry() {}
 }
 
