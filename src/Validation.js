4ede75b82985053f9d31c6f2e26d594cc5f564b3
김세환 <sehwan@gimsehwan-ui-MacBookPro.local>
2022-11-18 14:08:07 +0900

    feat(Validation): 다리 길이 예외사항 검사하는 함수 구현

--- src/Validation.js
+++ src/Validation.js
@@ -1,5 +1,35 @@
-class Validation {
+const { Console } = require('@woowacourse/mission-utils');
 
+class Validation {
+  static bridgeLength(number) {
+    if (Validation.isEmpty(number)) {
+        Console.close();
+        throw new Error('[ERROR] 값을 입력해주세요.'); 
+    }
+
+    if (!Validation.isNumber(number)) {
+        Console.close();
+        throw new Error('[ERROR] 숫자를 입력해주세요.')
+    }
+
+    if (!Validation.numberNet(number)) {
+        Console.close();
+        throw new Error('[ERROR] 3과 20사이의 값을 입력해주세요.');
+    }
+  }
+
+  static isEmpty(number) {
+    return number.length === 0;
+  }
+
+  static numberNet(number) {
+    return number > 2 && number < 21;
+  }
+
+  static isNumber(number) {
+    const RegExp = /^[0-9]+$/;
+    return RegExp.test(number);
+  }
 }
 
 module.exports = Validation;
--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,5 +1,5 @@
-const { Console } = require("@woowacourse/mission-utils");
-const { OUTPUT_MESSAGE } = require("../constants/Message");
+const { Console } = require('@woowacourse/mission-utils');
+const { OUTPUT_MESSAGE } = require('../constants/Message');
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
