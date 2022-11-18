676643d9b00a533fc5f11fe8af09dd207fb63e47
박준서 <oponize@naver.com>
2022-11-18 18:39:38 +0900

    refactor(*): 테스트가 용이하게 외부에서 주입받도록 변경

--- src/Service/BridgeGameService.js
+++ src/Service/BridgeGameService.js
@@ -1,6 +1,3 @@
-const InputView = require("../View/InputView.js");
-const OutputView = require("../View/OutputView.js");
-const BridgeGameModel = require("../Model/BridgeGameModel.js");
 const { generate } = require("../BridgeRandomNumberGenerator.js");
 const { makeBridge } = require("../BridgeMaker.js");
 const { KEYWORD } = require("../constants/index.js");
@@ -9,10 +6,10 @@ const BridgeGameService = class {
   #inputView;
   #outputView;
   #bridgeGameModel;
-  constructor() {
-    this.#inputView = InputView;
-    this.#outputView = OutputView;
-    this.#bridgeGameModel = new BridgeGameModel();
+  constructor(inputView, outputView, bridgeGameModel) {
+    this.#inputView = inputView;
+    this.#outputView = outputView;
+    this.#bridgeGameModel = bridgeGameModel;
   }
 
   startGame() {}
