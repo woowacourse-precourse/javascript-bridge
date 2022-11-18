8635618168ebd7d0c1dc87ba8b47a7b17b737e72
AhnGeunTaek <dksrmsxor@gmail.com>
2022-11-18 22:38:11 +0900

    fix: 버그로 인해 현재 쓰이지 않는 상수 삭제

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,11 @@
 const INPUT_MESSAGE = {
-  BRIDGE_SIZE: '다리의 길이를 입력해주세요.',
+  BRIDGE_SIZE: '다리의 길이를 입력해주세요.\n',
   MOVING_COMMAND: '이동할 칸을 선택해주세요. (위: U, 아래: D)',
   GAME_COMMAND: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)',
 };
 
 const OUTPUT_MESSAGE = {
-  GAME_START: '다리 건너기 게임을 시작합니다.',
+  GAME_START: '다리 건너기 게임을 시작합니다.\n',
 };
 
 const GAME_COMMANDS = {
@@ -13,10 +13,4 @@ const GAME_COMMANDS = {
   GAME: { RETRY: 'R', QUIT: 'Q' },
 };
 
-const BRIDGE_SHAPE = {
-  START: '[ ',
-  END: ' ]',
-  DIVIDING_LINE: ' | ',
-};
-
-module.exports = { INPUT_MESSAGE, OUTPUT_MESSAGE, GAME_COMMANDS, BRIDGE_SHAPE };
+module.exports = { INPUT_MESSAGE, OUTPUT_MESSAGE, GAME_COMMANDS };
