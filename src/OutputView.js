3594b09af98c56ac4653f0e96a2cddc4739f4357
woo-jk <201702042@o.cnu.ac.kr>
2022-11-18 21:18:37 +0900

    feat: OutputView printResult 함수 구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,3 +1,4 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -14,7 +15,11 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() {},
+  printResult(success, attemptCount) {
+    const successText = success ? "성공" : "실패";
+    MissionUtils.Console.print(`게임 성공 여부: ${successText}`);
+    MissionUtils.Console.print(`총 시도한 횟수: ${attemptCount}`);
+  },
 };
 
 module.exports = OutputView;
