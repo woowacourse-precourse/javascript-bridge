e2950a783b613adbe7da43c0e0ae2b90fa75ca38
BB-choi <choibobae0809@gmail.com>
2022-11-18 16:58:52 +0900

    refactor(BridgeGame): 다리 생성 메서드 분리

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -10,9 +10,14 @@ const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
 class BridgeGame {
   bridgeStore;
 
-  constructor(welcomeMessage = '다리 건너기 게임을 시작합니다.', bridgeSizeMessage = '다리의 길이를 입력해주세요.') {
+  moveCount;
+
+  // TODO: 메세지들 많아짐, 정리
+  constructor(welcomeMessage = '다리 건너기 게임을 시작합니다.', bridgeSizeMessage = '다리의 길이를 입력해주세요.', movingMessage = '이동할 칸을 선택해주세요. (위: U, 아래: D)') {
     this.welcomeMessage = welcomeMessage;
     this.bridgeSizeMessage = bridgeSizeMessage;
+    this.movingMessage = movingMessage;
+    this.moveCount = 0;
   }
 
   /**
@@ -20,7 +25,9 @@ class BridgeGame {
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(command) {
+    // TODO: command확인
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
@@ -29,9 +36,14 @@ class BridgeGame {
    */
   retry() {}
 
-  runGame(bridgeSize) {
+  createBridge(bridgeSize) {
+    // TODO: bridge길이 확인
     const bridge = BridgeMaker.makeBridge(bridgeSize, BridgeRandomNumberGenerator.generate);
     this.bridgeStore = new BridgeStore(bridge);
+  }
+
+  runGame(bridgeSize) {
+    this.createBridge(bridgeSize);
   }
 
   init() {
