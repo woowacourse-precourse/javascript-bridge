9428d08100162fab324d035dd0cc9c59be23997c
kimeuns <dmstn2006@gmail.com>
2022-11-19 02:28:44 +0900

    feat: 게임을 재시도 여부를 물어보는 기능 추가

--- src/InputView.js
+++ src/InputView.js
@@ -21,13 +21,15 @@ const InputView = {
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving(callback) {
-    Console.readLine(INPUT.ASK_BRIDGE_SIZE, callback);
+    Console.readLine(INPUT.ASK_MOVEMENT, callback);
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand(callback) {
+    Console.readLine(INPUT.ASK_RETRY, callback);
+  },
 };
 
 module.exports = InputView;
