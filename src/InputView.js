b9f34937f0cbc96550989d327c81475c85445a7f
dudgp128 <20181048@sungshin.ac.kr>
2022-11-18 15:50:22 +0900

    REFACTORY(InputView.readMoving) : 콜백 함수 변경, 출력문 수정

--- src/InputView.js
+++ src/InputView.js
@@ -33,9 +33,12 @@ const InputView = {
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving() {
-    Console.readLine("이동할 칸을 선택해주세요.\n", (input) => {
-      this.Game.move(input);
-    });
+    Console.readLine(
+      "이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
+      (input) => {
+        this.Game.transInputtoPosition(input);
+      }
+    );
   },
 
   /**
