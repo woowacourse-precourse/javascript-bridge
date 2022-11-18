c14c4de58d887ce724cb954a79bfa267aca1d713
Rki0 <pky00823@gmail.com>
2022-11-18 10:49:23 +0900

    test(Player): 플레이어의 진행에 따른 다리 상태 테스트 추가
    플레이어가 선택하는 다리가 맞는지 틀린지를 저장하는 메서드를 테스트한다.

--- __tests__/PlayerTest.js
+++ __tests__/PlayerTest.js
@@ -1,8 +1,32 @@
 const Player = require("../src/Player");
 
 describe("플레이어 상태 테스트", () => {
-  test("위쪽 다리를 맞출 경우 테스트", () => {
+  test("위쪽 다리를 맞출 경우 상태 테스트", () => {
     Player.checkUpperBridgeCorrect("U", true);
     expect(Player.state).toStrictEqual([[" O "], ["   "]]);
+  });
+
+  test("위쪽 다리를 틀릴 경우 상태 테스트", () => {
+    Player.checkUpperBridgeWrong("U", false);
+    expect(Player.state).toStrictEqual([
+      [" O ", " X "],
+      ["   ", "   "],
+    ]);
+  });
+
+  test("아래 다리를 맞출 경우 상태 테스트", () => {
+    Player.checkLowerBridgeCorrect("D", true);
+    expect(Player.state).toStrictEqual([
+      [" O ", " X ", "   "],
+      ["   ", "   ", " O "],
+    ]);
+  });
+
+  test("아래 다리를 틀릴 경우 상태 테스트", () => {
+    Player.checkLowerBridgeWrong("D", false);
+    expect(Player.state).toStrictEqual([
+      [" O ", " X ", "   ", "   "],
+      ["   ", "   ", " O ", " X "],
+    ]);
   });
 });
