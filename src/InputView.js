b860b6e04f1d362eb6056e4c42f6f298e051f2f7
jisu-kim <kss2460@naver.com>
2022-11-18 18:05:29 +0900

    feat(readMoving): 재시작 로직 추가

--- src/InputView.js
+++ src/InputView.js
@@ -2,6 +2,7 @@ const { Console } = require("@woowacourse/mission-utils");
 const BridgeGame = require("./BridgeGame");
 const { makeBridge } = require("./BridgeMaker");
 const { generate } = require("./BridgeRandomNumberGenerator");
+const { printResult } = require("./OutputView");
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -14,7 +15,7 @@ const InputView = {
     let mainBridge = [];
     Console.readLine("다리의 길이를 입력해주세요.", (bridgeSize) => {
       mainBridge = makeBridge(bridgeSize, generate); //기준 다리
-      const bridgeGame = new BridgeGame(mainBridge);
+      const bridgeGame = new BridgeGame(mainBridge); //인스턴스 생성
       InputView.readMoving(mainBridge, bridgeGame); //움직임 입력 받기
     });
   },
@@ -25,10 +26,20 @@ const InputView = {
   readMoving(mainBridge, bridgeGame) {
     Console.readLine(
       "이동할 칸을 선택해주세요. (위: U, 아래: D)",
-      (maveInput) => {
-        console.log("mainBridge", mainBridge);
-        console.log("bridgeGame", bridgeGame);
-        bridgeGame.move(maveInput, InputView.readGameCommand);
+      (moveInput) => {
+        // console.log("mainBridge", mainBridge);
+        // console.log("bridgeGame", bridgeGame);
+        bridgeGame.move(moveInput); //클래스 내부에서 움직임
+
+        if (!bridgeGame.hasNext && bridgeGame.count === bridgeGame.bridgeSize) {
+          printResult("게임 성공 여부: 성공");
+          printResult(`총 시도한 횟수: ${bridgeGame.count}`);
+          return;
+        }
+        if (!bridgeGame.hasNext) {
+          InputView.readGameCommand(mainBridge);
+          return;
+        }
         InputView.readMoving(mainBridge, bridgeGame);
       }
     );
@@ -55,10 +66,6 @@ const InputView = {
         }
       }
     );
-  },
-
-  startGame() {
-    Console.print("다리 건너기 게임을 시작합니다.");
   },
 };
 
