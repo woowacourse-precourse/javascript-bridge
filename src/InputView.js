fa8508a463891074ea1a35418b208eea52f2a355
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-11-18 18:34:19 +0900

    feat($InputView): 사용자가 입력한 다리의 길이 예외사항 체크

--- src/InputView.js
+++ src/InputView.js
@@ -1,17 +1,50 @@
 const { Console } = require('@woowacourse/mission-utils');
 const BridgeMaker = require('./BridgeMaker');
 const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
+const {
+  IS_INPUT_NOT_NUMBER,
+  IS_INPUT_EMPTY,
+  IS_INPUT_NOT_IN_RANGE
+} = require('./constants/error/MakeBridgeError');
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
+  checkBridgeLengthInputNotNull(input) {
+    if (input.length === 0) {
+      throw new Error(IS_INPUT_EMPTY);
+    }
+    return true;
+  },
+
+  checkIsBridgeLengthInputNum(input) {
+    if (Number.isNaN(Number(input))) {
+      throw new Error(IS_INPUT_NOT_NUMBER);
+    }
+    return true;
+  },
+
+  checkBridgeLengthNumInRange(input) {
+    if (input < 3 || input > 20) {
+      throw new Error(IS_INPUT_NOT_IN_RANGE);
+    }
+    return true;
+  },
+
+  checkBridgeLengthInput(input) {
+    this.checkBridgeLengthInputNotNull(input);
+    this.checkIsBridgeLengthInputNum(input);
+    this.checkBridgeLengthNumInRange(input);
+  },
   /**
    * 다리의 길이를 입력받는다.
    */
+
   readBridgeSize() {
-    return Console.readLine('다리의 길이를 입력해주세요.\n', (length) => {
-      BridgeMaker.makeBridge(length, BridgeRandomNumberGenerator.generate);
+    return Console.readLine('다리의 길이를 입력해주세요.\n', (input) => {
+      this.checkBridgeLengthInput(input);
+      BridgeMaker.makeBridge(input, BridgeRandomNumberGenerator.generate);
     });
   },
 
