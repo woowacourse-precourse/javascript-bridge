ab6ceb445c6838f3965e141053ead624f0ff7a89
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2022-11-18 16:31:53 +0900

    feat(InputView) : gameStart, Bridge 입력부분추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const InputView = require("./InputView");
+const [Console, Random] = [MissionUtils.Console, MissionUtils.Random];
+
+
 class App {
-  play() {}
+
+  #bridgeSize;
+
+  constructor(){
+    this.#bridgeSize =0;
+  }
+  
+  async play() {
+    InputView.gameStart();
+    this.#bridgeSize = await InputView.readBridgeSize();
+
+    await Console.print(this.#bridgeSize);
+    Console.close();
+  }
+
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
--- src/InputView.js
+++ src/InputView.js
@@ -1,11 +1,32 @@
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+const MissionUtils = require("@woowacourse/mission-utils");
+const [Console] = [MissionUtils.Console, MissionUtils.Random];
+
+
 const InputView = {
-  /**
-   * 다리의 길이를 입력받는다.
-   */
-  readBridgeSize() {},
+
+  validateBridge(bridgeInput){
+    if(typeof bridgeInput !== "number") throw new Error("[ERROR] 다리길이는 숫자여야 합니다.");
+    if(!(3<=bridgeInput && bridgeInput<=20)) throw new Error("[ERROR] 다리길이는 3에서 20사이의 숫자여야 합니다.");
+  },
+
+  gameStart(){
+    Console.print("다리 건너기 게임을 시작합니다.");
+  },
+
+  readBridgeSize() {
+    return new Promise((resolve,_) => {
+      Console.readLine("다리의 길이를 입력해주세요.\n", (inputString)=>{
+        const inputNum = Number(inputString);
+        this.validateBridge(inputNum);
+        resolve(inputNum);
+        Console.close();
+      });
+    })
+  },
+
+  getBridgeSize(){
+    return this.bridgeSize;
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
