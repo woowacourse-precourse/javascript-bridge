6ab60d00c5956efca623ebe489a9e4d710ce39ab
inthhh <glass005533@gmail.com>
2022-11-18 21:07:09 +0900

    feat: InputView 입력 기능 추가

--- src/InputView.js
+++ src/InputView.js
@@ -1,21 +1,36 @@
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+const MissionUtils = require("@woowacourse/mission-utils");
+const Console = MissionUtils.Console;
+
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine("\n다리의 길이를 입력해주세요.\n", (inputBridgeSize) => {
+      //
+      Console.close();
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    Console.readLine("\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n", (inputMoveUpDown) => {
+      //
+      Console.close();
+    })
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+    Console.readLine("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n", (inputRetry) => {
+      //
+      Console.close();
+    })
+  },
 };
 
 module.exports = InputView;
