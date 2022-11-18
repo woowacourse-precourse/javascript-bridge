ced587116f7994b43390e3885806ab4c813d09ad
Deemou <4106243@naver.com>
2022-11-18 17:22:35 +0900

    feat(Validator): 다리 길이 유효성 검사 기능 추가

--- src/Validator.js
+++ src/Validator.js
@@ -3,8 +3,17 @@ const { ERROR_MESSAGE } = require('./constant/Error');
 const InvalidInputError = require('./error/InvalidInputError');
 
 class Validator {
+  static validateBridgeLength(length) {
+    if (this.isValidLength(length)) return;
+    this.throwException(InvalidInputError, ERROR_MESSAGE.BRIDGE_LENGTH);
+  }
+
   static throwException(Error, message) {
     throw new Error(message);
+  }
+
+  static isValidLength(length) {
+    return length >= BRIDGE.LENGTH_MIN && length <= BRIDGE.LENGTH_MAX;
   }
 }
 
