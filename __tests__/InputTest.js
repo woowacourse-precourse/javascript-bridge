6e1dbbb4b2d401f4dacdcea99533d2395af9b5f4
YeonsuBaek <soothbugger@gmail.com>
2022-11-18 11:25:06 +0900

    test(bridge): 잘못된 게임 진행 옵션을 선택한 경우 예외 처리 테스트 완료

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -48,4 +48,12 @@ describe("입력 테스트", () => {
       InputView.readMoving();
     }).toThrow("[ERROR] 이동할 칸은 U과 P만 입력할 수 있습니다.");
   });
+
+  test("게임 진행 옵션이 R이나 Q가 아닌 경우 예외 처리한다.", () => {
+    mockQuestions(["T"]);
+
+    expect(() => {
+      InputView.readGameCommand();
+    }).toThrow("[ERROR] 게임 진행 옵션은 R과 Q만 입력할 수 있습니다.");
+  });
 });
