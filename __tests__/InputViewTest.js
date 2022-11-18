125acd78b65b8cdf743a436bc084f97c0b2af0cf
imb96 <kimminje7267@naver.com>
2022-11-18 14:07:59 +0900

    test : 테스트 추가

--- __tests__/InputViewTest.js
+++ __tests__/InputViewTest.js
@@ -1,4 +1,4 @@
-const InputView = require("../src/InputView");
+const InputView = require("../src/View/InputView");
 describe("다리 길이 입력 예외 테스트", () => {
   test("다리 길이가 3보다 작거나 20보다 크면 예외가 발생한다.", () => {
     expect(() => {
@@ -9,6 +9,12 @@ describe("다리 길이 입력 예외 테스트", () => {
   test("다리 길이가 숫자가 아닐 경우 예외가 발생한다.", () => {
     expect(() => {
       InputView.bridgeSizeValidate("S");
+    }).toThrow("[ERROR]");
+  });
+
+  test("U 혹은 R 이 아닌 값을 입력할 경우 예외가 발생한다.", () => {
+    expect(() => {
+      InputView.moveValidate("y");
     }).toThrow("[ERROR]");
   });
 });
