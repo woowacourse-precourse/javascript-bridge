26bedf2b2bae08295e8dcbc15dc22910fcdda5a1
“somm12” <sominan8744@gmail.com>
2022-11-18 13:13:07 +0900

    feat: 다리 길이 입력받기 문구 출력 기능 구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -21,6 +21,10 @@ const OutputView = {
   printGameStart() {
     MissionUtils.Console.print("다리 건너기 게임을 시작합니다.\n");
   },
+
+  printPleaseInputBridgeLength() {
+    MissionUtils.Console.print("다리의 길이를 입력해주세요.\n");
+  },
 };
 
 module.exports = OutputView;
