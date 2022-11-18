01bd7a854e0efe05e8877d7e31cf696ac4410afa
SWARVY <swarvy0826@naver.com>
2022-11-19 01:38:24 +0900

    refactor : 객체명 직관적으로 변경, 및 비교로직 수정

---
+++ src/Utils/returnProcessedInput.js
@@ -0,0 +1,33 @@
+const returnProcessedInput = {
+  start(input, isPassed) {
+    const upAndDownArray = this.upAndDown(input, isPassed);
+    const splitUpAndDownArray = this.splitUpAndDown(upAndDownArray);
+    const result = [
+      [splitUpAndDownArray[0].join().replace(/,/gi, ' | ')],
+      [splitUpAndDownArray[1].join().replace(/,/gi, ' | ')],
+    ];
+    return result;
+  },
+
+  upAndDown(input, isPassed) {
+    const upAndDownArray = [];
+    input.forEach((value, index) => {
+      if (value === 'U' && index === input.length - 1 && !isPassed) upAndDownArray.push(['X', ' ']);
+      else if (value === 'U') upAndDownArray.push(['O', ' ']);
+      if (value === 'D' && index === input.length - 1 && !isPassed) upAndDownArray.push([' ', 'X']);
+      else if (value === 'D') upAndDownArray.push([' ', 'O']);
+    });
+    return upAndDownArray;
+  },
+
+  splitUpAndDown(array) {
+    const splitUpAndDownArray = [[], []];
+    array.forEach((value) => {
+      splitUpAndDownArray[0].push(value[0]);
+      splitUpAndDownArray[1].push(value[1]);
+    });
+    return splitUpAndDownArray;
+  },
+};
+
+module.exports = returnProcessedInput;
