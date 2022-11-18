b8f12a07bf8bd39c9df7c240308acef4518adb61
ramda <91455804+gnfnfnr@users.noreply.github.com>
2022-11-18 23:22:06 +0900

    test: confirm random numbers of bridge size

--- __tests__/BridgeSizeTest.js
+++ __tests__/BridgeSizeTest.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+const BridgeMaker = require('../src/model/BridgeMaker');
 const BridgeSize = require('../src/model/BridgeSize');
 
 describe('다리 길이 테스트', () => {
@@ -11,5 +13,15 @@ describe('다리 길이 테스트', () => {
     expect(() => {
       new BridgeSize(input).showValidateResult();
     }).toThrow('[ERROR]');
+  });
+
+  test('다리 생성 테스트', () => {
+    const randomNumbers = ['1', '0', '1', '1'];
+    const mockGenerator = randomNumbers.reduce((acc, number) => {
+      return acc.mockReturnValueOnce(number);
+    }, jest.fn());
+
+    const bridge = BridgeMaker.makeBridge(4, mockGenerator);
+    expect(bridge).toEqual(['1', '0', '1', '1']);
   });
 });
