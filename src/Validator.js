c3f6076bb7f6de82fb523a5e62881f27eccf7c02
evencoding <evencoding@gmail.com>
2022-11-18 17:58:36 +0900

    feat: 공백을 포함하면 에러를 발생시키는 기능 구현

--- src/Validator.js
+++ src/Validator.js
@@ -1,5 +1,9 @@
+const { Console } = require('@woowacourse/mission-utils');
+
 const Validator = {
-  size: () => {},
+  throwErrorIfHasBlank: (string) => {
+    if (string.includes(' ')) throw new Error('[ERROR]');
+  },
 };
 
 module.exports = Validator;
