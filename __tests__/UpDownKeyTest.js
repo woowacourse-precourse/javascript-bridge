5d46ff48dfa065fc955de42f887e13ee16c82ffd
turtle601 <turtle601@naver.com>
2022-11-18 10:30:22 +0900

    refactor(UpDownKeyTest) test함수 doAction으로 변경

--- __tests__/UpDownKeyTest.js
+++ __tests__/UpDownKeyTest.js
@@ -6,21 +6,21 @@ describe('이동할 칸 입력 테스트', () => {
     const input = 'a';
     const upDowonKey = new UpDownKey(input);
 
-    expect(() => upDowonKey.getValidateData()).toThrow('[ERROR]');
+    expect(() => upDowonKey.doAction()).toThrow('[ERROR]');
   });
 
   test('(U or D 제외 문자 입력(오류) - "1") 입력 테스트', () => {
     const input = '1';
     const upDowonKey = new UpDownKey(input);
 
-    expect(() => upDowonKey.getValidateData()).toThrow('[ERROR]');
+    expect(() => upDowonKey.doAction()).toThrow('[ERROR]');
   });
 
   test('(U or D 제외 문자 입력(오류) - "u") 입력 테스트', () => {
     const input = 'u';
     const upDowonKey = new UpDownKey(input);
 
-    expect(() => upDowonKey.getValidateData()).toThrow('[ERROR]');
+    expect(() => upDowonKey.doAction()).toThrow('[ERROR]');
   });
 
   test('(U or R 제외 문자 입력(정상) - "U") 입력 테스트', () => {
