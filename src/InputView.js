1715a9a3cb6409e500aa43854b96b72fbb051f52
MyungAe <ladiescheong@naver.com>
2022-11-18 14:38:13 +0900

    feat(InputView): 각 함수 간 작업이 끝날때 호출할 함수를 정의한 뒤 작업 내용을 구현
    - README의 3. 흐름의 큰 틀 잡기를 바탕으로 작성됨

--- src/InputView.js
+++ src/InputView.js
@@ -1,44 +1,45 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-const BridgeMaker = require("./BridgeMaker");
-const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+const BridgeGame = require("./BridgeGame");
 const Validate = require("./Validate");
+const OutputView = require("./OutputView");
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 const InputView = {
-  /**
-   * 다리의 길이를 입력받는다.
-   */
   readBridgeSize() {
     MissionUtils.Console.readLine("다리의 길이를 입력해주세요.\n", (input) => {
-      this.ValidateBridgeSize(input);
-      const bridgeNumbers = BridgeMaker.makeBridge(
-        parseInt(input),
-        BridgeRandomNumberGenerator.generate
-      );
-      this.readMoving(bridgeNumbers);
+      Validate.isNumber(input);
+      Validate.isBigger(input);
+      Validate.isLower(input);
+      const bridgeGame = new BridgeGame();
+      bridgeGame.initGame(parseInt(input));
+      this.readMoving(bridgeGame);
     });
   },
 
-  /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
-  readMoving() {},
+  readMoving(bridgeGame) {
+    MissionUtils.Console.readLine(
+      "이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
+      (input) => {
+        Validate.isCorrectMove(input);
+        const result = bridgeGame.move(input);
+        if (result === 1) return this.readGameCommand(bridgeGame);
+        if (result === 2) return OutputView.printResult();
+        this.readMoving(bridgeGame);
+      }
+    );
+  },
 
-  /**
-   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-   */
-  readGameCommand() {},
-
-  /**
-   * 사용자가 입력한 다리의 길이가 정상적으로 입력되었는지 검증한다.
-   * @param {string} input
-   */
-  ValidateBridgeSize(input) {
-    Validate.isNumber(input);
-    Validate.isBigger(input);
-    Validate.isLower(input);
+  readGameCommand(bridgeGame) {
+    MissionUtils.Console.readLine(
+      "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n",
+      (input) => {
+        Validate.isCorrectRetry(input);
+        if (input === "Q") MissionUtils.Console.close();
+        if (input === "R") {
+          bridgeGame.initSelectBridge();
+          this.readMoving(bridgeGame);
+        }
+      }
+    );
   },
 };
 
