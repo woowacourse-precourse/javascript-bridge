7e86ebeb1c9638d2b767519bc16051da687a605c
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-18 21:15:32 +0900

    test: 이동 입력값 테스트

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -1,7 +1,8 @@
 const InputView = require('../src/InputView');
 const ERROR_MESSAGES = require('../src/constants/ErrorMessages');
 
-describe('입력값 테스트', () => {
+describe('다리 길이 입력값 테스트', () => {
+  // Bridge Length
   test('다리 길이 입력값 테스트 ( 문자 )', () => {
     expect(() => {
       InputView.validateBridgeSize('a');
@@ -29,6 +30,26 @@ describe('입력값 테스트', () => {
   test('다리 길이 범위 테스트 (*3 ~ 20)', () => {
     expect(() => {
       InputView.validateBridgeSize('3');
+    }).toBeTruthy();
+  });
+});
+
+describe('사용자 이동 입력값 테스트', () => {
+  test('사용자 이동 입력값 테스트 ( 문자 )', () => {
+    expect(() => {
+      InputView.validateMoving('a');
+    }).toThrow(ERROR_MESSAGES.MOVING);
+  });
+
+  test('사용자 이동 입력값 테스트 ( 문자 )', () => {
+    expect(() => {
+      InputView.validateMoving('UD');
+    }).toThrow(ERROR_MESSAGES.MOVING);
+  });
+
+  test('사용자 이동 입력값 테스트 ( 문자 )', () => {
+    expect(() => {
+      InputView.validateMoving('U');
     }).toBeTruthy();
   });
 });
