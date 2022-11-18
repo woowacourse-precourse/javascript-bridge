0aea6d69a1b47953ee9a988ae3d6f4a09dcf9095
Jeong Hun Nam <njh3421@gmail.com>
2022-11-18 11:10:53 +0900

    feat : add input function

--- src/InputView.js
+++ src/InputView.js
@@ -1,10 +1,16 @@
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
+const MissionUtils = require("@woowacourse/mission-utils");
+
 const InputView = {
+  printGameStart(){
+    MissionUtils.Console.print('다리 건너기 게임을 시작합니다')
+  },
   /**
    * 다리의 길이를 입력받는다.
    */
+  
   readBridgeSize() {},
 
   /**
