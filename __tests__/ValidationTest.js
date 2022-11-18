d661920da0709f2e258f5fbb1d566d46b747aa70
hahagarden <88613455+hahagarden@users.noreply.github.com>
2022-11-18 20:10:21 +0900

    test: 재시도 여부 입력 예외처리

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,5 +1,6 @@
 const BridgeSizeValidation = require("../src/Validation/BridgeSizeValidation");
 const MovingValidation = require("../src/validation/MovingValidation");
+const GameCommandValidation = require("../src/validation/GameCommandValidation");
 
 describe("BridgeSizeValidation 객체 테스트", () => {
   test("입력받은 다리 길이의 유효성을 검증하여 예외를 발생시킨다.", () => {
@@ -15,5 +16,13 @@ describe("MovingValidation 객체 테스트", () => {
     expect(() => MovingValidation.validateMoving("u")).toThrow();
     expect(() => MovingValidation.validateMoving("D")).not.toThrow();
     expect(() => MovingValidation.validateMoving("2")).toThrow();
+  });
+});
+
+describe("GameCommandValidation 객체 테스트", () => {
+  test("입력받은 재시도 여부의 유효성을 검증하여 예외를 발생시킨다.", () => {
+    expect(() => GameCommandValidation.validateGameCommand("r")).toThrow();
+    expect(() => GameCommandValidation.validateGameCommand("Q")).not.toThrow();
+    expect(() => GameCommandValidation.validateGameCommand("2")).toThrow();
   });
 });
