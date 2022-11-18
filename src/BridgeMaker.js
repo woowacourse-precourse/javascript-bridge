5bfe6ba59be44541702ca8c1569b0018098573e7
한수지 <fatholic5@gmail.com>
2022-11-19 05:24:31 +0900

    :sparkles: feat: 다리 생성 기능 구현 완료 다리생성 기능은 다리의 길이와 어떠한 매개변수(다리의 정보가 될 변수)를 받아서 무작위 숫자 0또는 1을 다리 길이만큼 배정하여 1이면 U를, 0이면 D로 바꿔서 리턴하는 기능을 가진 메서드이다.

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,13 +1,30 @@
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+
+const UPSIDE_NUMBER = 1;
+const DOWNSIDE_NUMBER = 0;
+const UPSIDE_STRING = "U";
+const DOWNSIDE_STRING = "D";
+
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
 const BridgeMaker = {
+
   /**
    * @param {number} size 다리의 길이
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
-  makeBridge(size, generateRandomNumber) {},
+  makeBridge(size, generateRandomNumber) {
+    const bridgeNumbers = [];
+    for(let index = 0; index < size; index++) {
+      generateRandomNumber = BridgeRandomNumberGenerator.generate();
+      bridgeNumbers.push(generateRandomNumber);
+    }
+    return bridgeNumbers.map((value) => {
+      if(value === UPSIDE_NUMBER) return value = UPSIDE_STRING;
+      if(value === DOWNSIDE_NUMBER) return value = DOWNSIDE_STRING;
+    })
+  },
 };
-
 module.exports = BridgeMaker;
