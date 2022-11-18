8df2dbbd115acd8070d68c6b4ba894ae14429686
woo-jk <201702042@o.cnu.ac.kr>
2022-11-18 20:23:18 +0900

    feat: BridgeMaker 객체 makeBridge 구현

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -2,12 +2,22 @@
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
 const BridgeMaker = {
+  LOWER_BRIDGE_NUMBER: 0,
+  UPPER_BRIDGE_NUMBER: 1,
+  LOWER_BRIDGE_LETTER: "D",
+  UPPER_BRIDGE_LETTER: "U",
+
   /**
    * @param {number} size 다리의 길이
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
-  makeBridge(size, generateRandomNumber) {},
+  makeBridge(size, generateRandomNumber) {
+    return Array.from({ length: size }, () => generateRandomNumber()).map((number) => {
+      if (number === this.LOWER_BRIDGE_NUMBER) return this.LOWER_BRIDGE_LETTER;
+      if (number === this.UPPER_BRIDGE_NUMBER) return this.UPPER_BRIDGE_LETTER;
+    });
+  },
 };
 
 module.exports = BridgeMaker;
