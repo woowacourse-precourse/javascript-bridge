b73adc3be5ed2d921fbae3260496b991ccd9a7e3
yuu <22yuu@naver.com>
2022-11-19 03:18:57 +0900

    test: 재시도, 종료에 관한 예외처리 테스트

--- __tests__/InputTest.test.js
+++ __tests__/InputTest.test.js
@@ -1,4 +1,8 @@
-const { validateBridgeLength } = require('../src/Validation');
+const {
+  validateBridgeLength,
+  validateMovingValue,
+  validateEndValue
+} = require('../src/Validation');
 
 describe('사용자 입력 예외 테스트', () => {
   test('다리 길이 범위는 3이상 ~ 20 이하의 숫자여야 한다.', () => {
@@ -7,9 +11,31 @@ describe('사용자 입력 예외 테스트', () => {
     }).toThrow('[ERROR]');
   });
   test('라운드마다 요구되는 이동 입력은 대문자 "U", 대문자 "D" 중 하나의 문자여야 한다.', () => {
-    expect(() => {}).toThrow('[ERROR]');
+    expect(() => {
+      validateMovingValue('d');
+    }).toThrow('[ERROR]');
+    expect(() => {
+      validateMovingValue('u');
+    }).toThrow('[ERROR]');
+    expect(() => {
+      validateMovingValue('abc');
+    }).toThrow('[ERROR]');
+    expect(() => {
+      validateMovingValue(3);
+    }).toThrow('[ERROR]');
   });
-  test('게임 재시작 및 종료 여부 입력은 "R"(재시작)과 "Q"(종료) 중 하나의 문자여야 한다.', () => {
-    expect(() => {}).toThrow('[ERROR]');
+  test('게임 재시작 및 종료 여부 입력은 대문자 "R"(재시작)과 대문자 "Q"(종료) 중 하나의 문자여야 한다.', () => {
+    expect(() => {
+      validateEndValue('r');
+    }).toThrow('[ERROR]');
+    expect(() => {
+      validateEndValue('q');
+    }).toThrow('[ERROR]');
+    expect(() => {
+      validateEndValue('abc');
+    }).toThrow('[ERROR]');
+    expect(() => {
+      validateEndValue(1);
+    }).toThrow('[ERROR]');
   });
 });
