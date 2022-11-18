185e832c075f38972bfa268ac0d4f3f93f8227d7
hyunjin <leehj0110@kakao.com>
2022-11-19 01:37:03 +0900

    refactor: 게임 시작 문구 outputview로 이동

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-const { Console } = require("@woowacourse/mission-utils")
-const InputView = require("./InputView")
+const { Console } = require('@woowacourse/mission-utils');
+const InputView = require('./InputView');
+const BridgeMaker = require('./BridgeMaker');
+const BridgeGame = require('./BridgeGame');
+const OutputView = require('./OutputView');
 
 class App {
-  constructor() {}
+  bridge;
+  bridgeSize;
+  gameCommand;
+
+  constructor() {
+    const bridgeGame = new BridgeGame();
+
+    this.readBridgeSize = InputView.readBridgeSize.bind(this);
+    this.readMoving = InputView.readMoving.bind(this);
+    this.readGameCommand = InputView.readGameCommand.bind(this);
+  }
 
   play() {
-    Console.print("다리 건너기 게임을 시작합니다.\n")
-    InputView.readBridgeSize()
+    OutputView.printStart();
+
+    this.readBridgeSize(BridgeMaker.makeBridge);
+
+    // 다리 길이 입력후에 다리를 만들어야한다. 따라서 다리를 만들어주는 함수를 넣어줌
   }
 }
 
-new App().play()
+new App().play();
 
-module.exports = App
+module.exports = App;
