e5dcd6fb3f934a523733bb8f2b724f106e89899b
sungjihyun <jhsung23@gmail.com>
2022-11-18 12:51:00 +0900

    feat: 무작위 값을 이용해 다리 생성하기 기능

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,4 +1,13 @@
 /**
+ * 제공된 BridgeMaker 객체를 활용해 구현해야 한다.
+ * BridgeMaker에 `프로퍼티`를 추가할 수 없다. 🙅‍♀️
+ * BridgeMaker의 `파일 경로`는 변경할 수 없다. 🙅‍♀️
+ * BridgeMaker의 메서드의 `시그니처(인자, 이름)`와 `반환 타입`은 변경할 수 없다. 🙅‍♀️
+ */
+
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
+
+/**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
 const BridgeMaker = {
@@ -7,7 +16,12 @@ const BridgeMaker = {
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
-  makeBridge(size, generateRandomNumber) {},
+  makeBridge(size, generateRandomNumber) {
+    return new Array(size)
+      .fill(0)
+      .map(() => generateRandomNumber())
+      .map(number => (number === 1 ? 'U' : 'D'));
+  },
 };
 
 module.exports = BridgeMaker;
--- src/model/Bridge.js
+++ src/model/Bridge.js
@@ -1,3 +1,5 @@
+const BridgeMaker = require('../BridgeMaker');
+const BridgeRandomNumberGenerator = require('../BridgeRandomNumberGenerator');
 const { BRIDGE, ERROR } = require('../utils/constants');
 
 class Bridge {
@@ -7,8 +9,10 @@ class Bridge {
   constructor(size) {
     this.#validateSize(size);
     this.#size = +size;
+    this.#bridgeStructure = BridgeMaker.makeBridge(+size, BridgeRandomNumberGenerator.generate);
   }
 
+  //FIXME: 메서드 길이 10 초과
   #validateSize(size) {
     if (!/^[0-9]+$/.test(size)) {
       throw new Error(ERROR.bridge_size_error);
