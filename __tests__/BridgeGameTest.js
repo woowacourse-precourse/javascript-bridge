6567a99f71b6162a9c40e817cf75166c28c9d30a
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-11-18 18:42:24 +0900

    Test : repactor BridgeGameTest

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -4,20 +4,20 @@ describe("BridgeGame 테스트", () => {
   test("다리 길이 잘못된 사이즈 입력시 에러 테스트", () => {
     expect(() => {
       const bridgeGame = new BridgeGame();
-      bridgeGame.getBridgeSize("r");
+      bridgeGame.bridge("r");
     }).toThrow("[ERROR]");
   });
   describe("다리 길이 범위 외 숫자 입력시 에러 테스트", () => {
     test("20보다 큰 숫자 입력시 에러 테스트", () => {
       expect(() => {
         const bridgeGame = new BridgeGame();
-        bridgeGame.getBridgeSize("21");
+        bridgeGame.bridge("21");
       }).toThrow("[ERROR]");
     });
     test("3보다 큰 작은 숫자 입력시 에러 테스트", () => {
       expect(() => {
         const bridgeGame = new BridgeGame();
-        bridgeGame.getBridgeSize("2");
+        bridgeGame.bridge("2");
       }).toThrow("[ERROR]");
     });
   });
