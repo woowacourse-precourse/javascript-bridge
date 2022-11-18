9dd58ddf0db3e34228bfa1cc28ef5101cbc490c4
saejinpark <saejinpark951210@gmail.com>
2022-11-18 16:22:53 +0900

    Revert "일일커밋"
    This reverts commit b992c354f885f13e2e1b8efbe2c673ee00f28d27.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,5 @@
-const MissionUtils = require("@woowacourse/mission-utils");
-const wConsole = MissionUtils.Console;
-
-
-const { readBridgeSize } = require("./InputView");
-
 class App {
-  constructor() {
-    this.bridge = null;
-    this.moveLog = "";
-  }
-
-  getBridge() {
-    return this.bridge;
-  }
-
-  setBridge(bridge) {
-    this.bridge = bridge;
-  }
-
-  play() {
-    wConsole.print("다리 건너기 게임을 시작합니다.\n");
-    readBridgeSize(this);
-  }
+  play() {}
 }
 
 module.exports = App;
-
-const app = new App();
-app.play();
