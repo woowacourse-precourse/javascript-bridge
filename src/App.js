c481867d6068f8c87dc569ddb26740fd233b9db1
hahagarden <88613455+hahagarden@users.noreply.github.com>
2022-11-18 22:16:22 +0900

    feat: 다리 길이 입력받아 게임 초기화

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+const InputView = require("./InputView");
+const BridgeGame = require("./BridgeGame");
+const BridgeMaker = require("./BridgeMaker");
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+
 class App {
   #bridgeGame;
   #playCount;
   constructor() {
     this.#bridgeGame = null;
     this.#playCount = 0;
+  }
+
+  async initiate() {
+    const bridgeSize = await InputView.readBridgeSize();
+    this.#bridgeGame = new BridgeGame(
+      BridgeMaker.makeBridge(bridgeSize, BridgeRandomNumberGenerator.generate)
+    );
   }
 
   play() {}
