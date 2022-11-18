313bc1efa34f0bbeee158132841260c7134954d2
chiman <rlaclghks123@naver.com>
2022-11-18 11:05:10 +0900

    feat: 이동한경우 출력하는 기능 구현

--- src/InputView.js
+++ src/InputView.js
@@ -31,12 +31,23 @@ const InputView = {
       try {
         Validate.validateMovePosition(movePosition);
         const moveBridge = new BridgeGame(bridge).move(movePosition);
+        const bridgePositon = size - bridge.length;
+        OutputView.printMap(moveBridge, bridgePositon);
+        this.readMovingControler(moveBridge, size, bridge);
       } catch (error) {
         OutputView.printErrorMessage(error) || InputView.readMoving(bridge, size);
       }
     });
   },
 
+  readMovingControler(moveBridge, size, bridge) {
+    if (moveBridge[0] === 'X' || moveBridge[1] === 'X') {
+      InputView.readGameCommand();
+    }
+    if (bridge.length - 1 === 0) console.log('done');
+    bridge.shift();
+    InputView.readMoving(bridge, size);
+  },
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
--- src/OutputView.js
+++ src/OutputView.js
@@ -2,13 +2,27 @@ const { Console } = require('@woowacourse/mission-utils');
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
+let upBridge = '';
+let downBridge = '';
+
 const OutputView = {
   /**
    * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {},
+  printMap(moveBridge, bridgePositon) {
+    if (bridgePositon === 0) {
+      upBridge += moveBridge[0];
+      downBridge += moveBridge[1];
+    } else if (bridgePositon !== 0) {
+      upBridge += ` | ${moveBridge[0]}`;
+      downBridge += ` | ${moveBridge[1]}`;
+    }
+
+    Console.print(`[ ${upBridge} ]`);
+    Console.print(`[ ${downBridge} ]`);
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
