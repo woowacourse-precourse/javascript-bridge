56217334826d125674ec22a53ef21e16467c65d0
ReturnReason <reasonz@daum.net>
2022-11-19 03:48:06 +0900

    feat: 다리 건너기 게임 관련 상수 추가

---
+++ src/constants/bridgeGameInfo.js
@@ -0,0 +1,13 @@
+const BRIDGE_GAME = Object.freeze({
+  minSize: 3,
+  maxSize: 20,
+  leftSquareBracket: '[',
+  rightSquareBracket: ']',
+  verticalLine: '|',
+  up: 'U',
+  down: 'D',
+});
+
+module.exports = {
+  BRIDGE_GAME,
+};
