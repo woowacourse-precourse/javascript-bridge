e13ef7d46379040c791b8037226c097325191d26
Deemou <4106243@naver.com>
2022-11-18 21:22:26 +0900

    test(Validator): validateSpace() 테스트 케이스 추가

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -1,9 +1,17 @@
 const Validator = require('../src/Validator');
 
 describe('유효성 검사 테스트', () => {
-  test.each([-1, 0, 2, 21])('다리 길이가 3 이상 20 이하가 아닐 경우 예외가 발생한다.', (input) => {
-    expect((input) => {
-      Validator.validateBridgeLength(input);
+  test.each(['a', -1, 2, 21])(
+    '다리 길이가 3 이상 20 이하가 아닐 경우 예외가 발생한다.',
+    (input) => {
+      expect(() => {
+        Validator.validateBridgeLength(input);
+      }).toThrow('[ERROR]');
+    }
+  );
+  test.each(['u', 'd', 'A', 1])('이동할 칸이 U 나 D 가 아닌 경우 예외가 발생한다.', (input) => {
+    expect(() => {
+      Validator.validateSpace(input);
     }).toThrow('[ERROR]');
   });
 });
