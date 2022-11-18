4c28382c8fcfd226d21b0649e8c99989873b1a0e
Cobocho <rlaalsrb1111@naver.com>
2022-11-19 01:48:16 +0900

    test: 테스트 코드 전용 함수명 변경 및 테스트명 변경

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -3,8 +3,8 @@ const BridgeRandomNumberGenerator = require('../src/BridgeRandomNumberGenerator'
 
 
 describe("BridgeGame 테스트", () => {
-  test("다리 생성 테스트", () => {
-    function checkSpace(bridge) {
+  test("다리 길이 테스트", () => {
+    function checkSize(bridge) {
       const spaceType = [BridgeRandomNumberGenerator.RANDOM_LOWER_INCLUSIVE, BridgeRandomNumberGenerator.RANDOM_UPPER_INCLUSIVE];
       return bridge.filter(space => {
         spaceType.includes(space);
@@ -14,7 +14,7 @@ describe("BridgeGame 테스트", () => {
     sizes.forEach(size => {  
       const game = new BridgeGame(size);
       expect(game.bridge.length).toBe(size);
-      expect(checkSpace(game.bridge)).toBe(0);
+      expect(checkSize(game.bridge)).toBe(0);
     });
   });
 });
