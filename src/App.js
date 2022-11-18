437f55b79b46bf91b4d79e5929fd365a6a23ef53
Yoo Seung Guk <dbtmd878@naver.com>
2022-11-18 15:57:55 +0900

    feat: 에러 확인을 위한 메서드 추가(기능 미구현)

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+const MissionUtils = require("@woowacourse/mission-utils");
 const BridgeGame = require("./BridgeGame");
 const BridgeMaker = require("./BridgeMaker");
-const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
 const InputView = require("./InputView");
-const OutputView = require("./OutputView");
+const Console = MissionUtils.Console;
 
 class App {
-  #SIZE;
   play() {
-    return InputView.readBridgeSize(this.answerBridge);
+    Console.print("다리 건너기 게임을 시작합니다.\n");
+    return InputView.readBridgeSize(this.bridgeSizeValidate);
   }
 
-  answerBridge(size) {
+  bridgeSizeValidate = (bridgeSize) => {
+    const SIZE = bridgeSize;
+    return this.answerBridge(SIZE);
+  };
+
+  bridgeValidate(bridgeArray, answerBridgeArray) {
+    return this.bridgeErrorCheck.validateBridgeArray(
+      bridgeArray,
+      answerBridgeArray
+    );
+  }
+
+  answerBridge(bridgeSize) {
     const RANDOM_NUMBER_GENERATOR = BridgeRandomNumberGenerator.generate;
     const ANSWER_BRIDGE_ARRAY = BridgeMaker.makeBridge(
-      size,
+      bridgeSize,
       RANDOM_NUMBER_GENERATOR
     );
     const BRIDGE_GAME = new BridgeGame(ANSWER_BRIDGE_ARRAY);
     return BRIDGE_GAME.move();
-    // return this.start(ANSWER_BRIDGE_ARRAY);
   }
-
-  start(ANSWER_BRIDGE_ARRAY) {}
 }
 
-const app = new App();
-app.play();
 module.exports = App;
