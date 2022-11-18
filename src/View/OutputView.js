0627c85be669b225c9474426266d4399d9980b8a
d0422 <rlfehd2013@naver.com>
2022-11-18 16:33:09 +0900

    Refactor(OutputView):메시지문자열 상수형변수로 변경

--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -1,6 +1,11 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const MESSAGE = {
   START: '다리 건너기 게임을 시작합니다.',
+  SUCCESS: '성공',
+  FAIL: '실패',
+  GAME_RESULT: '\n최종 게임 결과',
+  IS_SUCCESS: '\n게임 성공 여부: ',
+  TRY_COUNT: '총 시도한 횟수: ',
 };
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -24,11 +29,11 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printResult(result, count, status) {
-    const RESULT = status ? '성공' : '실패';
-    MissionUtils.Console.print('최종 게임 결과');
+    const RESULT = status ? MESSAGE.SUCCESS : MESSAGE.FAIL;
+    MissionUtils.Console.print(MESSAGE.GAME_RESULT);
     MissionUtils.Console.print(result);
-    MissionUtils.Console.print(`게임 성공 여부: ${RESULT}`);
-    MissionUtils.Console.print(`총 시도한 횟수: ${count}`);
+    MissionUtils.Console.print(`${MESSAGE.IS_SUCCESS}${RESULT}`);
+    MissionUtils.Console.print(`${MESSAGE.TRY_COUNT}${count}`);
   },
   printError(error) {
     MissionUtils.Console.print(error);
