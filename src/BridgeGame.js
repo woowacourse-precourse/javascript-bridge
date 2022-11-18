2fe7c8ac9bb275112279ebecb388e7cf94f60cd6
inthhh <glass005533@gmail.com>
2022-11-18 23:00:40 +0900

    feat: BridgeGame 이동 메소드 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,3 +1,6 @@
+const InputView = require("./InputView");
+const OutputView = require("./OutputView");
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
@@ -7,7 +10,17 @@ class BridgeGame {
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(turnNumber, bridge, moveUpDown) {
+    if (bridge[turnNumber] == moveUpDown) {
+      OutputView.printMap(turnNumber, bridge, moveUpDown, true);
+      return bridge;
+    }
+    if (bridge[turnNumber] != moveUpDown) {
+      OutputView.printMap(turnNumber, bridge, moveUpDown, false);
+      InputView.readGameCommand();
+    }
+    return bridge;
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
