5c7dbc522ce674947aae3e0d6b949f2171a34c6c
Woong <jiyko7@kyonggi.ac.kr>
2022-11-18 17:53:45 +0900

    feat: 재시도 입력
    feat: 재시도 입력

--- src/InputView.js
+++ src/InputView.js
@@ -52,7 +52,12 @@ const InputView = {
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+   readGameCommand(count, bridge, bridgeMap) {
+    MISSIONUTILS.Console.readLine("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n", function(input) {
+      MISSIONUTILS.Console.print(input);
+
+    });
+  },
 };
 
 module.exports = InputView;
