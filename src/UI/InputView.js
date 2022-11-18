334ec21f8ab86e4baaea38f95bd54cb0f68f8ad7
dladncks1217 <dlaxodud1217@gmail.com>
2022-11-19 02:59:53 +0900

    refactor(InputView): readLine 비동기처리 문제로 인한 코드 전체 리팩토링

--- src/UI/InputView.js
+++ src/UI/InputView.js
@@ -8,30 +8,22 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize(func) {
-    Console.readLine(GAME_MESSAGE.SET_BRIDGE_LENGTH, func);
+  readBridgeSize(callback) {
+    Console.readLine(GAME_MESSAGE.SET_BRIDGE_LENGTH, callback);
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {
-    return new Promise((resolve) => {
-      Console.readLine(GAME_MESSAGE.MOVE_CHOICE, (userInput) => {
-        resolve(userInput);
-      });
-    });
+  readMoving(callback) {
+    Console.readLine(GAME_MESSAGE.MOVE_CHOICE, callback);
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {
-    return new Promise((resolve) => {
-      Console.readLine(GAME_MESSAGE.GAME_RETRY, (userInput) => {
-        resolve(userInput);
-      });
-    });
+  readGameCommand(callback) {
+    Console.readLine(GAME_MESSAGE.GAME_RETRY, callback);
   },
 };
 
