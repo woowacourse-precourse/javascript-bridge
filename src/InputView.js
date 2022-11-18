2ba54ce9e8847fabdfb1de5e9d53c447076986a2
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-18 15:11:08 +0900

    feat: 다리길이 입력값 검증 함수

--- src/InputView.js
+++ src/InputView.js
@@ -1,11 +1,20 @@
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
+const { Console } = require("@woowacourse/mission-utils");
+const ERROR_MESSAGES = require("./constants/ErrorMessages");
+const BREIDGE = require("./constants/constants");
+
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine("QUERY", (size) => {
+      this.validateBridgeSize(size);
+      console.log({ size });
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
@@ -16,6 +25,16 @@ const InputView = {
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand() {},
+
+  validateBridgeSize(size) {
+    size = Number(size);
+    if (!Number.isInteger(size)) {
+      throw new Error(ERROR_MESSAGES.NAN);
+    }
+    if (size < BREIDGE.MIN || size > BREIDGE.MAX) {
+      throw new Error(ERROR_MESSAGES.SIZE);
+    }
+  },
 };
 
 module.exports = InputView;
