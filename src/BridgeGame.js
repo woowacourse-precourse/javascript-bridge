df97d17902ccebbd48c57fb3320bba4c2c95a00d
saejinpark <saejinpark951210@gmail.com>
2022-11-18 22:02:25 +0900

    setBridge

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,7 +1,14 @@
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
-class BridgeGame {
+ class BridgeGame {
+  constructor() {
+    this.bridge = null;
+  }
+
+  setBridge(bridge) {
+    this.bridge = bridge;
+  }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
