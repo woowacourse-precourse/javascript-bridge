2e8dc1ae46c4a76f4280847f28a0bfeed2fc9665
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-11-18 15:46:28 +0900

    feat(NoticeMessage): RETRY_GAME 문구 추가

--- src/NoticeMessage.js
+++ src/NoticeMessage.js
@@ -4,7 +4,8 @@ const NOTICE_MESSAGE = {
     SELECT_MOVE : "이동할 칸을 선택해 주세요. (위: U, 아래: D)\n",
     START_PRINT : "[ ",
     NEXT_STEP : " | ",
-    END_PRINT : " ]"
+    END_PRINT : " ]",
+    RETRY_GAME : "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n"
    
 };
 
