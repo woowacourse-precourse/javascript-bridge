9e45c52b203fc14af068ae41dce8a37734c4849d
이건우 <lifeflavor@igeon-uui-MacBookPro.local>
2022-11-18 18:59:26 +0900

    feat(bridge)- @단위테스트 : "X"값 이후 게임이 중지 되는지

--- __tests__/GameStartTest.js
+++ __tests__/GameStartTest.js
@@ -12,7 +12,7 @@ describe("다리 O/X 판별 테스트", () => {
       [" ", " ", " "],
     ]);
   });
-  test(`arrUp : 'O', 'U' ,'U' || arrDown : " ", "X", " " `, () => {
+  test(`'X'값 이후 치환이 중지 됬는지 확인1`, () => {
     const GAMESTART = new GameStart(
       ["U", "D", "U"],
       ["U", "U", "U"],
@@ -21,6 +21,17 @@ describe("다리 O/X 판별 테스트", () => {
     expect(GAMESTART.getBrigeArr()).toMatchObject([
       ["O", "U", "U"],
       [" ", "X", " "],
+    ]);
+  });
+  test(`'X'값 이후 치환이 중지 됬는지 확인2`, () => {
+    const GAMESTART = new GameStart(
+      ["D", "D", "U"],
+      ["U", " ", " "],
+      [" ", "D", "D"]
+    );
+    expect(GAMESTART.getBrigeArr()).toMatchObject([
+      ["U", " ", " "],
+      ["X", "D", "D"],
     ]);
   });
   test(`arrUp : ' ', 'O' ,' ' || arrDown : "O", " ", "O" `, () => {
