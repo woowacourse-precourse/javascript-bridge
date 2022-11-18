569b2a1319bc7a5ef682eff4d9dbc6b95bf1f353
evencoding <evencoding@gmail.com>
2022-11-18 17:59:36 +0900

    feat(Validator): 다리 size에 대한 유효성 검사 기능 구현

--- src/Validator.js
+++ src/Validator.js
@@ -1,6 +1,17 @@
 const { Console } = require('@woowacourse/mission-utils');
 
 const Validator = {
+  sizeValidityCheck: (size) => {
+    const regex = /^\d+$/;
+    try {
+      Validator.throwErrorIfHasBlank(size);
+      if (!regex.test(size)) throw new Error('[ERROR]');
+      if (size < 3 || size > 20) throw new Error('[ERROR]');
+    } catch (error) {
+      Console.print(error);
+    }
+  },
+
   throwErrorIfHasBlank: (string) => {
     if (string.includes(' ')) throw new Error('[ERROR]');
   },
