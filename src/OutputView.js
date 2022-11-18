458320aa56f03444863d4e6b3f6f1a52bbfe3702
Gaeun-Jang <110662057+Gaeun-Jang@users.noreply.github.com>
2022-11-18 13:41:20 +0900

    feat: 게임 종료 기능 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -32,6 +32,7 @@ const OutputView = {
     this.printMap(bridgeGame.userBridge);
     Console.print(`게임 성공 여부: ${result}`);
     Console.print(`총 시도한 횟수: ${bridgeGame.attempts}`);
+    Console.close();
   },
 };
 
