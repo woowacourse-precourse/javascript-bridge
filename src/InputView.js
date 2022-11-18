1ffaa09df491e25141738cd2d1bb9d4897efe985
woo-jk <201702042@o.cnu.ac.kr>
2022-11-18 21:48:28 +0900

    feat: InputView 객체 readBridgeSize(), readMoving(), readGameCommand() 함수 구현

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,5 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,17 +7,35 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    return new Promise((resolve) => {
+      MissionUtils.Console.readLine("다리의 길이를 입력해주세요.\n", (input) => {
+        resolve(input);
+      });
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    return new Promise((resolve) => {
+      MissionUtils.Console.readLine("이동할 칸을 선택해주세요. (위: U, 아래: D)\n", (input) => {
+        resolve(input);
+      });
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+    return new Promise((resolve) => {
+      MissionUtils.Console.readLine("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n", (input) => {
+        resolve(input);
+      });
+    });
+  },
 };
 
 module.exports = InputView;
