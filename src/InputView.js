ea89b3de32b111f09fa57989abbfe77a7ca25bd8
Yebin Kim <kyb4312@gmail.com>
2022-11-18 21:56:43 +0900

    feat(InputView): implement a function to get bridge size from a player

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,4 @@
+const { Console } = require('@woowacourse/mission-utils');
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,17 +6,11 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
-
-  /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
-  readMoving() {},
-
-  /**
-   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-   */
-  readGameCommand() {},
+  readBridgeSize(startGame) {
+    Console.readLine('다리의 길이를 입력해주세요.', (bridgeLength) =>
+      startGame(bridgeLength)
+    );
+  },
 };
 
 module.exports = InputView;
