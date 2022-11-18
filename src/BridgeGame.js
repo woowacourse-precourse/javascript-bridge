b6471e99a68f7e3626f2e410a7d7957323ece6ab
Yoo Seung Guk <dbtmd878@naver.com>
2022-11-18 17:31:17 +0900

    feat: 에러 발생ㅅ 시 에러 문구 출력 및 재입력 기능 추가

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,4 +1,5 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const BridgeErrorCheck = require("./BridgeError");
 const CallView = require("./CallView");
 const Console = MissionUtils.Console;
 
@@ -11,24 +12,31 @@ class BridgeGame {
 
   constructor(ANSWER_BRIDGE_ARRAY) {
     this.CallView = new CallView();
+    this.errorCheck = new BridgeErrorCheck();
     this.#ANSWER_BRIDGE_ARRAY = ANSWER_BRIDGE_ARRAY;
   }
 
   move() {
     this.#game_state = false;
-    this.CallView.readMove(this.commandValidate);
+    this.CallView.readMove(this.checkCommand);
   }
 
-  commandValidate = (command) => {
-    const COMMAND = command;
-    return this.checkBridgeNumber(COMMAND);
-  };
-  checkBridgeNumber = (COMMAND) => {
-    const INDEX = this.#current_bridge;
-    if (COMMAND === this.#ANSWER_BRIDGE_ARRAY[INDEX]) {
-      return this.circlePush(COMMAND);
+  checkCommand = (command) => {
+    try {
+      this.errorCheck.validateCommand(command);
+      this.checkBridgeNumber(command);
+    } catch (e) {
+      this.CallView.justPrint(e);
+      this.move();
     }
-    return this.crossPush(COMMAND);
+    return;
+  };
+  checkBridgeNumber = (command) => {
+    const INDEX = this.#current_bridge;
+    if (command === this.#ANSWER_BRIDGE_ARRAY[INDEX]) {
+      return this.circlePush(command);
+    }
+    return this.crossPush(command);
   };
   changeCommandNumber(COMMAND) {
     const COMMAND_NUMBER = COMMAND === "U" ? 1 : 0;
@@ -59,7 +67,7 @@ class BridgeGame {
     this.#bridge_map[COMMAND_NUMBER].push("X");
     this.#bridge_map[REST_ARRAY_NUMBER].push(" ");
     this.CallView.currentMap(this.#bridge_map);
-    return this.failGame();
+    return this.validateMenuCommand();
   }
   complateGame() {
     this.#game_state = true;
@@ -70,15 +78,29 @@ class BridgeGame {
     );
     return Console.close();
   }
-  failGame() {
-    return this.CallView.readCommand(this.stop, this.retry);
-  }
+  failGame = (command) => {
+    try {
+      this.errorCheck.validateMenuCommand(command);
+      if (command === "R") this.retry();
+      if (command === "Q") this.stop();
+    } catch (e) {
+      this.CallView.justPrint(e);
+      this.validateMenuCommand();
+    }
+    return;
+  };
+
+  validateMenuCommand = () => {
+    return this.CallView.readCommand(this.failGame);
+  };
+
   retry = () => {
     this.#bridge_map = [[], []];
     this.#current_bridge = 0;
     this.#try_count += 1;
     return this.move();
   };
+
   stop = () => {
     this.CallView.resultPrint(
       this.#bridge_map,
