d849c180a6a4fb21929a34e2762c79adbea9cc51
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-11-18 20:22:34 +0900

    test(BridgeGame): 다리 생성 메서드 테스트
    숫자 3을 전달하면 길이가 3인 배열을 반환한다.

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,4 +1,10 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 const App = require('../src/App');
+
+afterAll(() => {
+  MissionUtils.Console.close();
+});
 
 describe('다리 생성 메서드 테스트', () => {
   test('메소드 이름은 "generateBridge"로 정의된다.', () => {
@@ -6,5 +12,13 @@ describe('다리 생성 메서드 테스트', () => {
     const METHOD_NAME = 'generateBridge';
 
     expect(app.generateBridge.name).toEqual(METHOD_NAME);
+  });
+
+  test('"3"을 전달하면 길이가 3인 배열을 반환한다.', () => {
+    const app = new App();
+    const EXPECTED = '3';
+    const RECEIVED = 3;
+
+    expect(app.generateBridge(EXPECTED)).toHaveLength(RECEIVED);
   });
 });
