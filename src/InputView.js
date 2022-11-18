6c97f6c60631533301942cbfdb0148185089b763
Jeong Hun Nam <njh3421@gmail.com>
2022-11-18 11:19:26 +0900

    feat : readBridgeSize f()

--- src/InputView.js
+++ src/InputView.js
@@ -5,18 +5,24 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 const InputView = {
   printGameStart(){
-    MissionUtils.Console.print('다리 건너기 게임을 시작합니다')
+    MissionUtils.Console.print('다리 건너기 게임을 시작합니다\n')
   },
   /**
    * 다리의 길이를 입력받는다.
    */
-  
-  readBridgeSize() {},
+
+  readBridgeSize() {
+    MissionUtils.Console.readLine('다리의 길이를 입력해주세요\n',(bridgeLength)=>{
+      this.readMoving(bridgeLength)
+    })
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving(bridgeLength) {
+    
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
