627e66baf513ce7e89da2cf25abfe1e3fc33610d
kimeuns <dmstn2006@gmail.com>
2022-11-19 02:27:05 +0900

    test: 입력에 따라 이동하는 결과값 테스트

--- __tests__/BrigeTest.js
+++ __tests__/BrigeTest.js
@@ -1,7 +1,8 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const BridgeMaker = require("../src/BridgeMaker");
 const InputView = require('../src/InputView');
-const App = require('../src/App')
+const App = require('../src/App');
+const BridgeGame = require("../src/BridgeGame");
 
 const mockQuestions = (answers) => {
   MissionUtils.Console.readLine = jest.fn();
@@ -32,7 +33,30 @@ describe("다리 건너기 테스트", () => {
     const app = new App();
 
     expect(() => {
-      app.setBridgeSize();
+      app.play();
     }).toThrow("[ERROR]");
+  });
+
+  test.each(["g",2,"u"])("이동할 칸에 'U', 'D'외 무엇도 입력할 수 없다", (input) => {
+    InputView.readMoving = jest
+      .fn()
+      .mockImplementationOnce((callback) => callback(input));
+
+    const app = new App();
+
+    expect(() => {
+      app.setBridge();
+    }).toThrow("[ERROR]");
+  });
+
+  test("이동 결과를 제대로 출력하는지", () => {
+    const bridge = ["U", "U", "D"];
+    const input = "U";
+    const output = [true, [1], [0]];
+
+    const bridgeGame = new BridgeGame();
+    bridgeGame.setBridge(bridge);
+
+    expect(bridgeGame.move(input)).toEqual(output);
   });
 });
