89b350e5a4cdbd8d732e9914f27b2e3dd8892966
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2022-11-19 02:05:56 +0900

    feat(BridgeGame): 다리를 움직이는 move 기능추가

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,13 +1,32 @@
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
+
+
 class BridgeGame {
-  /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  move() {}
+ 
+  #curIdx;
+  #success;
+  
+  constructor(){
+    this.#curIdx = 0;
+    this.#success = true;
+  }
+
+  getIdx(){
+    return this.#curIdx;
+  }
+
+  getSuccess(){
+    return this.#success;
+  }
+
+  move(dir, bridge) {
+    if(dir === bridge[this.#curIdx]){
+      this.#curIdx += 1;
+    }
+    else{
+      this.#curIdx +=1;
+      this.#success = false;
+    }
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
