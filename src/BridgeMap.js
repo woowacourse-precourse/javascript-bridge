3e45c9a9717b89ec5bd337247ecd40458a3bf4c5
MAGHC <windowsapple95@gmail.com>
2022-11-18 20:34:54 +0900

    feat: BridgeMap.addMap 처음이 아닐때 자원 추가 기능 구현

--- src/BridgeMap.js
+++ src/BridgeMap.js
@@ -3,6 +3,11 @@ class BridgeMap {
 
   #MAP_SOURCE = { CORRECT: '| O ', INCORRECT: '| X ', EMPTY: '|   ' };
 
+  addMap(boolean, input, beforeMap) {
+    if (boolean) return BridgeMap.addCorrect(input, beforeMap, this.#MAP_SOURCE);
+    if (!boolean) return BridgeMap.addIncorrect(input, beforeMap, this.#MAP_SOURCE);
+  }
+
   static addCorrect(input, beforeMap, mapSource) {
     // console.log(input, beforeMap, mapSource, 'addCorrect 확인용');
     const [up, down] = beforeMap;
