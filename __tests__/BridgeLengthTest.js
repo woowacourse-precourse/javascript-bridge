602f1d167fe71cf80fe81c659514ddc874e15af1
turtle601 <turtle601@naver.com>
2022-11-18 14:15:22 +0900

    refactor(BridgeLengthTest) BridgeService 내 함수로 테스트
    test:
    - bridgeService.start()

--- __tests__/BridgeLengthTest.js
+++ __tests__/BridgeLengthTest.js
@@ -1,38 +1,28 @@
-const BridgeLength = require('../src/service/domain/BridgeLength');
+const BridgeService = require('../src/service/BridgeService');
 const { MODEL_KEY } = require('../src/utils/constants');
 
 describe('다리 길이 테스트', () => {
   test('(다리 길이(오류) - 문자)', () => {
     const input = 'a';
-    const bridgeLength = new BridgeLength(input);
-    expect(() => bridgeLength.doAction()).toThrow('[ERROR]');
+    const bridgeLength = new BridgeService();
+    expect(() => bridgeLength.start(input)).toThrow('[ERROR]');
   });
 
   test('(다리 길이(오류) - 3미만의 숫자)', () => {
     const input = '1';
-    const bridgeLength = new BridgeLength(input);
-    expect(() => bridgeLength.doAction()).toThrow('[ERROR]');
+    const bridgeLength = new BridgeService(input);
+    expect(() => bridgeLength.start(input)).toThrow('[ERROR]');
   });
 
   test('(다리 길이(오류) - 20초과의 숫자)', () => {
     const input = '21';
-    const bridgeLength = new BridgeLength(input);
-    expect(() => bridgeLength.doAction()).toThrow('[ERROR]');
+    const bridgeLength = new BridgeService(input);
+    expect(() => bridgeLength.start(input)).toThrow('[ERROR]');
   });
 
   test('(다리 길이(오류) - 문자 + 숫자(3 ~ 20))', () => {
     const input = '20 ';
-    const bridgeLength = new BridgeLength(input);
-    expect(() => bridgeLength.doAction()).toThrow('[ERROR]');
-  });
-
-  test('(다리 길이(정상) - 숫자(3 ~ 20))', () => {
-    const input = '3';
-    const bridgeLength = new BridgeLength(input);
-
-    bridgeLength.doAction();
-
-    expect(bridgeLength.getModelFor(MODEL_KEY.userBridge)).toEqual([]);
-    expect(bridgeLength.getModelFor(MODEL_KEY.tryCount)).toEqual(1);
+    const bridgeLength = new BridgeService(input);
+    expect(() => bridgeLength.start(input)).toThrow('[ERROR]');
   });
 });
