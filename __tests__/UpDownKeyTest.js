b741eb032bc283f936f990e20d4386cf36675f50
turtle601 <turtle601@naver.com>
2022-11-18 14:19:22 +0900

    refactor(UpDownKeyTest) BridgeService 객체 내부 함수로 테스트
    test:
    - recordMove()

--- __tests__/UpDownKeyTest.js
+++ __tests__/UpDownKeyTest.js
@@ -1,3 +1,4 @@
+const BridgeService = require('../src/service/BridgeService');
 const UpDownKey = require('../src/service/domain/UpDownKey');
 const BridgeLength = require('../src/service/domain/BridgeLength');
 const { MODEL_KEY } = require('../src/utils/constants');
@@ -5,61 +6,34 @@ const { MODEL_KEY } = require('../src/utils/constants');
 describe('이동할 칸 입력 테스트', () => {
   test('(U or D 제외 문자 입력(오류) - "a") 입력 테스트', () => {
     const input = 'a';
-    const upDowonKey = new UpDownKey(input);
+    const bridgeService = new BridgeService();
 
-    expect(() => upDowonKey.doAction()).toThrow('[ERROR]');
+    expect(() => bridgeService.recordMove(input)).toThrow('[ERROR]');
   });
 
   test('(U or D 제외 문자 입력(오류) - "1") 입력 테스트', () => {
     const input = '1';
-    const upDowonKey = new UpDownKey(input);
+    const bridgeService = new BridgeService();
 
-    expect(() => upDowonKey.doAction()).toThrow('[ERROR]');
+    expect(() => bridgeService.recordMove(input)).toThrow('[ERROR]');
   });
 
   test('(U or D 제외 문자 입력(오류) - "u") 입력 테스트', () => {
     const input = 'u';
-    const upDowonKey = new UpDownKey(input);
+    const bridgeService = new BridgeService();
 
-    expect(() => upDowonKey.doAction()).toThrow('[ERROR]');
+    expect(() => bridgeService.recordMove(input)).toThrow('[ERROR]');
   });
 
-  test('(U or R 제외 문자 입력(정상) - "U") 입력 테스트', () => {
-    const input = 'U';
-    const upDowonKey = new UpDownKey(input);
-
-    upDowonKey.doAction();
-
-    const userBridge = upDowonKey.getModelFor(MODEL_KEY.userBridge);
-
-    expect(userBridge).toEqual(['U']);
-  });
-
-  test('(U or R 제외 문자 입력(정상) - "D") 입력 테스트', () => {
-    const input = 'D';
-    const upDowonKey = new UpDownKey(input);
-
-    upDowonKey.doAction();
-
-    const userBridge = upDowonKey.getModelFor(MODEL_KEY.userBridge);
-
-    expect(userBridge).toEqual(['D']);
-  });
-
-  test('(통합테스트(create - update)) 입력 테스트', () => {
+  test('(단위테스트(create - update)) 입력 테스트', () => {
     const inputLength = '3';
-    const bridgeLength = new BridgeLength(inputLength);
+    const inputKey = 'U';
 
-    bridgeLength.doAction();
+    const bridgeService = new BridgeService();
 
-    const input = 'U';
+    bridgeService.start(inputLength);
+    bridgeService.recordMove(inputKey);
 
-    const upDowonKey = new UpDownKey(input);
-
-    upDowonKey.doAction();
-
-    const userBridge = upDowonKey.getModelFor(MODEL_KEY.userBridge);
-
-    expect(userBridge).toEqual(['U']);
+    expect(bridgeService.getRepo(MODEL_KEY.userBridge)).toEqual(['U']);
   });
 });
