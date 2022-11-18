2e0c6b357447f8e1a0c15a62bd8cbf419252ed69
saram <yujun1018@naver.com>
2022-11-18 14:49:52 +0900

    feat: 다리 길이 입력 기능

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,7 @@
+const { Console } = require('@woowacourse/mission-utils');
+const BridgeMaker = require('./BridgeMaker');
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
+const { GAME } = require('./phrases');
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,7 +9,11 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine(GAME.BRIDGE_SIZE, (input) =>
+      BridgeMaker.makeBridge(Number(input), BridgeRandomNumberGenerator.generate())
+    );
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
--- src/phrases.js
+++ src/phrases.js
@@ -1,5 +1,6 @@
 const GAME = {
   START: '다리 건너기 게임을 시작합니다.',
+  BRIDGE_SIZE: '다리의 길이를 입력해주세요.',
 };
 
 module.exports = { GAME };
