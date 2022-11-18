cc68fc07fdf90d798be4d51a7c8d4bdce11dcb9f
hyunjin <leehj0110@kakao.com>
2022-11-19 02:13:05 +0900

    refactor: makeBridge

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -4,23 +4,16 @@ const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
 const BridgeMaker = {
-  UP: 'U',
-  DOWN: 'D',
   /**
    * @param {number} size 다리의 길이
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
   makeBridge(size, generateRandomNumber) {
-    const bridge = [];
-
-    for (let i = 0; i < size; i++) {
-      const randomNumber = generateRandomNumber();
-      if (randomNumber == '1') bridge.push(this.UP);
-      if (randomNumber == '2') bridge.push(this.DOWN);
-    }
-
-    return bridge;
+    return Array.from({ length: size }, generateRandomNumber).map((randomNumber, index, bridge) => {
+      if (randomNumber === 1) return 'U';
+      if (randomNumber === 0) return 'D';
+    });
   },
 };
 
