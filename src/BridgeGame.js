ecd701938bbcba995dc6dd6b4de18d0e66450fd0
jinseo <ghkdwlstj99@gmail.com>
2022-11-18 19:29:51 +0900

    refactor: attemptCount 값 변경

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,11 +1,11 @@
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
- const attemptCount = 0;
  class BridgeGame {
   constructor(){
     this.userPickedArr = [];
     this.userPickedUpOrDown = [[], []];
+    this.attemptCount = 1;
   }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
