cd184eb6ec2428ddff3855b1e448cf1a37ed8af7
ramda <91455804+gnfnfnr@users.noreply.github.com>
2022-11-18 23:05:00 +0900

    feat: generate  random numbers of bridge size

--- src/controller/BridgeProcess.js
+++ src/controller/BridgeProcess.js
@@ -2,7 +2,8 @@ const InputView = require('../view/InputView');
 const OutputView = require('../view/OutputView');
 const { Console } = require('@woowacourse/mission-utils');
 const { PRINTBRIDGESIZE } = require('../view/Message');
-const BridgeRandomNumberGenerator = require('../model/BridgeRandomNumberGenerator');
+const BridgeMaker = require('../model/BridgeMaker');
+const { generate } = require('../model/BridgeRandomNumberGenerator');
 
 // 명심하기! controller는 데이터를 받거나 사용자에게 동작을 받으면 model 또는 view를 변경
 
@@ -17,12 +18,14 @@ class BridgeProcess {
 
   #inputBridgeSize() {
     Console.readLine(PRINTBRIDGESIZE, (bridgeSize) => {
-      this.#inputView.readBridgeSize(bridgeSize) ? this.#gen() : this.#inputBridgeSize();
+      const bridgeSizeNumber = this.#inputView.readBridgeSize(bridgeSize);
+      bridgeSizeNumber ? this.#gen(bridgeSizeNumber) : this.#inputBridgeSize();
     });
   }
 
-  #gen() {
-    console.log(BridgeRandomNumberGenerator.generate());
+  #gen(bridgeSizeNumber) {
+    const tt = BridgeMaker.makeBridge(bridgeSizeNumber, generate);
+    console.log(tt);
   }
 }
 module.exports = BridgeProcess;
