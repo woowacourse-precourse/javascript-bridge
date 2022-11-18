f5f5faa2e388cec37431f8ff81f3463f7e633e9b
Rki0 <pky00823@gmail.com>
2022-11-18 10:00:31 +0900

    refactor(Player): 메서드 기능 분리
    하나의 메서드에서 실행되고 있는 연산을 쪼갰습니다.
    쪼개진 메서드들은 각각 하나의 일을 합니다.
    10줄 이하로 메서드 길이를 줄이기 위한 리팩토링이기도합니다.

--- src/Player.js
+++ src/Player.js
@@ -13,29 +13,48 @@ const Player = {
   gameSuccess: PlayerConstants.GMAE_FAIL,
 
   updateState(wantGo, isCorrect) {
+    this.calculateBridgeState(wantGo, isCorrect);
+    this.playerAns.push(isCorrect);
+
+    this.checkGameSuccess();
+  },
+
+  calculateBridgeState(wantGo, isCorrect) {
+    this.checkUpperBridgeCorrect(wantGo, isCorrect);
+
+    this.checkUpperBridgeWrong(wantGo, isCorrect);
+
+    this.checkLowerBridgeCorrect(wantGo, isCorrect);
+
+    this.checkLowerBridgeWrong(wantGo, isCorrect);
+  },
+
+  checkUpperBridgeCorrect(wantGo, isCorrect) {
     if (wantGo === PlayerInputConstants.UPPER_BRIDGE_STRING && isCorrect) {
       this.state[0].push(AnswerConstants.CORRECT_ANSWER);
       this.state[1].push(AnswerConstants.EMPTY_ANSWER);
     }
+  },
 
+  checkUpperBridgeWrong(wantGo, isCorrect) {
     if (wantGo === PlayerInputConstants.UPPER_BRIDGE_STRING && !isCorrect) {
       this.state[0].push(AnswerConstants.WRONG_ANSWER);
       this.state[1].push(AnswerConstants.EMPTY_ANSWER);
     }
+  },
 
+  checkLowerBridgeCorrect(wantGo, isCorrect) {
     if (wantGo === PlayerInputConstants.LOWER_BRIDGE_STRING && isCorrect) {
       this.state[0].push(AnswerConstants.EMPTY_ANSWER);
       this.state[1].push(AnswerConstants.CORRECT_ANSWER);
     }
+  },
 
+  checkLowerBridgeWrong(wantGo, isCorrect) {
     if (wantGo === PlayerInputConstants.LOWER_BRIDGE_STRING && !isCorrect) {
       this.state[0].push(AnswerConstants.EMPTY_ANSWER);
       this.state[1].push(AnswerConstants.WRONG_ANSWER);
     }
-
-    this.playerAns.push(isCorrect);
-    const success = this.checkGameSuccess();
-    return success;
   },
 
   updateSize(size) {
