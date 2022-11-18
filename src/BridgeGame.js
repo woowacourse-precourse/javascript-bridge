7b22677822bb010a7ff4a8612f9c449aecd47992
seoyeon <wjstjdus96@naver.com>
2022-11-18 18:15:02 +0900

    feat: 다리 이동 메서드 추가

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,18 +1,32 @@
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
+
 class BridgeGame {
+  constructor(bridge, userBridge) {
+    this.bridge = bridge;
+    this.userBridge = userBridge;
+  }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(moving) {
+    var idx = this.userBridge.length;
+
+    if (this.bridge[idx] == moving) {
+      this.userBridge.push(true);
+    } else if (this.bridge[idx] != moving) {
+      this.userBridge.push(false);
+    }
+
+    console.log(this.userBridge);
+
+    return this.userBridge;
+    //return userBridge ex) [O, O, O, X]
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   retry() {}
 }
