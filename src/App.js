cf0b399de7ca1e35dac45ded205219b6a4d8d04a
TaeeunKim <xodms0309@naver.com>
2022-11-18 22:02:17 +0900

    feat: 다리의 길이를 입력 받는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+const InputView = require('./InputView');
 const { Console } = require('@woowacourse/mission-utils');
+
 class App {
   play() {
     Console.print('다리 건너기 게임을 시작합니다.');
+    InputView.readBridgeSize();
   }
 }
 
--- src/InputView.js
+++ src/InputView.js
@@ -1,11 +1,21 @@
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+const { Console } = require('@woowacourse/mission-utils');
+const { validateBridge } = require('./utils/validate');
+const BridgeMaker = require('./BridgeMaker');
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine('다리의 길이를 입력해주세요.', (size) => {
+      try {
+        validateBridge(Number(size));
+        const bridge = BridgeMaker.makeBridge(Number(size), BridgeRandomNumberGenerator.generate());
+      } catch (e) {
+        this.readBridgeSize();
+      }
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
