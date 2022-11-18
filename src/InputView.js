e122a303c13b0718dba45f645c7b16557a140e20
inthhh <glass005533@gmail.com>
2022-11-19 00:32:32 +0900

    fix: InputView 게임진행 및 재시작 메소드 수정

--- src/InputView.js
+++ src/InputView.js
@@ -6,40 +6,33 @@ const OutputView = require("./OutputView");
 const Console = MissionUtils.Console;
 
 const InputView = {
-  /**
-   * 다리의 길이를 입력받는다.
-   */
-   readBridgeSize() {
+  
+  readBridgeSize() {
     Console.readLine("\n다리의 길이를 입력해주세요.\n", (inputBridgeSize) => {
       bridge = BridgeMaker.makeBridge(inputBridgeSize, BridgeRandomNumberGenerator.generate);
-      InputView.readMoving(bridge, 0);
-      // Console.close();
+      InputView.readMoving(bridge, 0, 1);
       return bridge;
     });
   },
 
-  /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
-  readMoving(bridge, turnNumber) {
+  readMoving(BRG, turnNumber) {
     const brgGame = new BridgeGame();
     Console.readLine("\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n", (inputMoveUpDown) => {
-      bridge = brgGame.move(turnNumber, bridge, inputMoveUpDown);
-      if (bridge.length > 0) this.readMoving(bridge, turnNumber + 1);
-      else Console.close();
+      let gameContinue = brgGame.move(turnNumber, BRG, inputMoveUpDown);
+      if (gameContinue > 0) this.readMoving(BRG, turnNumber + 1);
+      else {
+        brgGame.retry(turnNumber, BRG, inputMoveUpDown);
+        Console.close();
+      }
     })
+    return;
   },
 
-  /**
-   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-   */
   readGameCommand() {
     Console.readLine("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n", (inputRetry) => {
-      if (inputRetry == "R") return 0;
-      if (inputRetry == "Q") OutputView.printResult();
-      Console.close();
-    })
-    return 1;
+      if (inputRetry == "R") return true;
+      if (inputRetry == "Q") return false;
+    });
   },
 };
 
