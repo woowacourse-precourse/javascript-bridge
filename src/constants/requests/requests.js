6e750f8571aa136b43f4b1b61a7d8ad742574d86
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-11-18 18:47:58 +0900

    feat($requests): 게임 문구 상수로 분리

---
+++ src/constants/requests/requests.js
@@ -0,0 +1,4 @@
+const REQUEST_FOR_BRIDGE_LENGTH = '다리의 길이를 입력해주세요.\n';
+const REQUEST_FOR_MOVING = '\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n';
+
+module.exports = { REQUEST_FOR_BRIDGE_LENGTH, REQUEST_FOR_MOVING };
