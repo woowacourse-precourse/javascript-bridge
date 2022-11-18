dbe07e61cf432c0860d68c50d1adb7e4c8ffbc76
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-11-18 17:19:58 +0900

    feat: 이동할 칸 입력하는 기능 구현

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,11 +1,13 @@
 const BridgeGame = require('../BridgeGame');
+const WinningBridge = require('../WinningBridge');
 const InputView = require('../view/InputView');
 const OutputView = require('../view/OutputView');
 const { MESSAGE } = require('../utils/constants');
 
 class Controller {
   constructor() {
-    this.bridgeGame = new BridgeGame();
+    this.bridgeGame = new BridgeGame(this);
+    this.winningBridge = new WinningBridge(this);
   }
 
   // - 다리의 길이 입력 받는다.
@@ -24,6 +26,14 @@ class Controller {
   }
 
   // - 위, 아래 중 이동할 칸 입력 받는다.
+  inputMoving() {
+    InputView.readMoving(this.move.bind(this));
+  }
+
+  move(direction) {
+    this.bridgeGame.move(direction);
+  }
+
   // - 재시작 또는 종료 여부 입력 받는다.
 }
 
