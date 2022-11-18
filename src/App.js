866527f9e0186193e1f3f0cc0d166e7c703e0fce
guridaek <iue16@naver.com>
2022-11-18 15:13:29 +0900

    feat: 게임 진행 및 상태 체크 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 const { Console } = require('@woowacourse/mission-utils');
 const BridgeGame = require('./BridgeGame.js');
+const { GameState } = require('./Constant.js');
 const InputView = require('./InputView.js');
+const OutputView = require('./OutputView.js');
 const Validation = require('./Validation.js');
 
 class App {
@@ -22,9 +24,25 @@ class App {
     }
   }
 
-  playBridgeGame() {}
+  playBridgeGame() {
+    try {
+      const direction = InputView.readMoving();
+      Validation.validateMoving(direction);
+      this.#bridgeGame.move(direction);
+      OutputView.printMap();
+      this.checkBridgeGame();
+    } catch (err) {
+      Console.print(err);
+      this.playBridgeGame();
+    }
+  }
 
-  checkBridgeGame() {}
+  checkBridgeGame() {
+    const gameState = this.#bridgeGame.getGameState();
+    if (gameState === GameState.PLAYING) this.playBridgeGame();
+    if (gameState === GameState.GAME_OVER) this.requestRetryBridgeGame();
+    if (gameState === GameState.VICTORY) this.endBridgeGame();
+  }
 
   requestRetryBridgeGame() {}
 
