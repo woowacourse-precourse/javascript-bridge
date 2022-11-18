c9016f683246f505adb22046adb98ae0a02103dc
Inae Choi <inaechoi89@gmail.com>
2022-11-19 00:24:44 +0900

    fix: fix syntax error

--- src/constant/ErrorMessage.js
+++ src/constant/ErrorMessage.js
@@ -1,5 +1,7 @@
 const ERROR_MESSAGE = {
-  RANGE: '3~20 사이의 수를 입력해야 합니다',
-  INTEGER: '정수를 입력해야 합니다.',
-  NUMBER: '숫자를 입력해야 합니다.',
+  RANGE: '[ERROR] 3~20 사이의 수를 입력해야 합니다',
+  INTEGER: '[ERROR] 정수를 입력해야 합니다.',
+  NUMBER: '[ERROR] 숫자를 입력해야 합니다.',
 };
+
+module.exports = ERROR_MESSAGE;
