4611ca43c5b2959b761226491a9735563a2680c1
ReturnReason <reasonz@daum.net>
2022-11-19 03:47:25 +0900

    feat: 미션 유틸 헬퍼 함수 추가

---
+++ src/utils/missionUtil.js
@@ -0,0 +1,19 @@
+const { Console } = require('@woowacourse/mission-utils');
+
+const print = (message) => {
+  return Console.print(message);
+};
+
+const readLine = (query, callback) => {
+  return Console.readLine(query, callback);
+};
+
+const close = () => {
+  return Console.close();
+};
+
+module.exports = {
+  print,
+  readLine,
+  close,
+};
