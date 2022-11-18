2fab0c0b844b9ae95aa67fe21bef4533fb99e263
osdoonhyun <skw1996@naver.com>
2022-11-18 16:51:01 +0900

    feat(App, OutputView): 게임 시작 문구 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+const OutputView = require("./OutputView");
+
 class App {
-  play() {}
+  play() {
+    OutputView.printGameStartPhrase();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
