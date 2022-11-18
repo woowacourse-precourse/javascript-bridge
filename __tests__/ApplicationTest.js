f033b23018eb2a719f05a8a5698f2bd94ce7ab4a
saejinpark <saejinpark951210@gmail.com>
2022-11-18 22:33:47 +0900

    테스트 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -81,7 +81,46 @@ describe("다리 건너기 테스트", () => {
     expectBridgeOrder(log, "[ O |   | O ]", "[   | O |   ]");
   });
 
+  test("기능 테스트", () => {
+    const logSpy = getLogSpy();
+    mockRandoms(["1", "0", "1"]);
+    mockQuestions(["3", "U", "D", "D", "Q"]);
+
+    const app = new App();
+    app.play();
+
+    const log = getOutput(logSpy);
+    expectLogContains(log, [
+      "최종 게임 결과",
+      "[ O |   |   ]",
+      "[   | O | X ]",
+      "게임 성공 여부: 실패",
+      "총 시도한 횟수: 1",
+    ]);
+    expectBridgeOrder(log, "[ O |   |   ]", "[   | O | X ]");
+  });
+  
+  test("기능 테스트", () => {
+    const logSpy = getLogSpy();
+    mockRandoms(["1", "0", "1"]);
+    mockQuestions(["3", "U", "D", "D", "R", "U", "D", "U"]);
+
+    const app = new App();
+    app.play();
+
+    const log = getOutput(logSpy);
+    expectLogContains(log, [
+      "최종 게임 결과",
+      "[ O |   | O ]",
+      "[   | O |   ]",
+      "게임 성공 여부: 성공",
+      "총 시도한 횟수: 2",
+    ]);
+    expectBridgeOrder(log, "[ O |   | O ]", "[   | O |   ]");
+  });
+
   test("예외 테스트", () => {
     runException(["a"]);
+    runException(["3", "a"]);
   });
 });
--- src/App.js
+++ src/App.js
@@ -13,3 +13,6 @@ class App {
 }
 
 module.exports = App;
+
+const app = new App();
+app.play();
