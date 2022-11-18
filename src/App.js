37dc215a1aa5fabe62d16878e3adb78cc6dcfb18
YMS <yms1789@naver.com>
2022-11-18 21:02:08 +0900

    Feat: App.js에 기능 import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const InputView = require("./InputView");
+const OutputView = require("./OutputView");
+const { Console } = require("@woowacourse/mission-utils");
+const { MESSAGE } = require("./constants");
 class App {
-  play() {}
+  play() {
+    Console.print(MESSAGE.START_MESSAGE);
+
+    InputView.readBridgeSize();
+  }
 }
 
+const app = new App();
+app.play();
 module.exports = App;
