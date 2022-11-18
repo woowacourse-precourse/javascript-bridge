68b0403a9bd7a8914402987972160d1eb24669d6
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-18 21:16:01 +0900

    feat: 이동 에러메세지 추가

--- src/constants/ErrorMessages.js
+++ src/constants/ErrorMessages.js
@@ -1,6 +1,7 @@
 const ERROR_MESSAGES = {
-  NAN: "[ERROR] 숫자를 입력해 주세요.",
-  SIZE: "[ERROR] 다리 길이는 3 ~ 20 입니다.",
+  NAN: '[ERROR] 숫자를 입력해 주세요.',
+  SIZE: '[ERROR] 다리 길이는 3 ~ 20 입니다.',
+  MOVING: "[ERROR] 'U' 혹은 'D'를 입력하세요.",
 };
 
 module.exports = ERROR_MESSAGES;
