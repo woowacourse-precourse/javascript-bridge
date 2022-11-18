26fc914ac5a58f29c157439153090d31b2311243
jjo-mi <whdal2636@naver.com>
2022-11-18 14:14:39 +0900

    fix : 메시지 상수 수정(한줄띄어쓰기 추가)

--- src/Constant.js
+++ src/Constant.js
@@ -1,8 +1,8 @@
 const GUIDE_MESSAGE = {
-    START: "다리 건너기 게임을 시작합니다.",
-    INPUT_LENGTH: "다리의 길이를 입력해주세요.",
-    INPUT_MOVE: "이동할 칸을 선택해주세요. (위: U, 아래: D)",
-    INPUT_TRY: "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
+    START: "다리 건너기 게임을 시작합니다. \n",
+    INPUT_LENGTH: "다리의 길이를 입력해주세요. \n",
+    INPUT_MOVE: "이동할 칸을 선택해주세요. (위: U, 아래: D) \n",
+    INPUT_TRY: "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q) \n",
 }
 
 const ERROR_MESSAGE = {
@@ -11,7 +11,7 @@ const ERROR_MESSAGE = {
 }
 
 const RESULT_MESSAGE = {
-    TITTLE: "최종 게임 결과",
+    TITTLE: "최종 게임 결과 \n",
     RESULT_TABLE: " ",
     IS_SUCCESS: "게임 성공 여부: ",
     TRY_COUNT: "총 시도한 횟수: "
