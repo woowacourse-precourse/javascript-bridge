b3f6478461e713ea7d44cc37958b1c1160f66e80
Tory <88531407+1two13@users.noreply.github.com>
2022-11-18 16:26:49 +0900

    feat(BridgeGame): 총 시도한 횟수 증가시키기

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -4,6 +4,7 @@
 class BridgeGame {
   constructor() {
     this.round = 0;
+    this.count = 0;
   }
 
   /**
@@ -23,6 +24,12 @@ class BridgeGame {
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   retry() {}
+
+  countTry() {
+    this.count += 1;
+
+    return this.count;
+  }
 }
 
 module.exports = BridgeGame;
