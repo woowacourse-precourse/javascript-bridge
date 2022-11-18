4b6fe8cf46a4d303763011a8cdd4dccee5806ea6
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-11-18 16:40:57 +0900

    docs: 다리 생성하는 기능 구현 방법 수정

--- src/WinningBridge.js
+++ src/WinningBridge.js
@@ -1,15 +1,27 @@
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
 const Validation = require('./utils/Validation');
+const BridgeMaker = require('./BridgeMaker');
 
 class WinningBridge {
   #size;
+  #winningBridge;
 
   constructor(size) {
     this.#size = this.validate(size);
+    // this.WinningBridge();
   }
 
   validate(size) {
     Validation.checkType(size);
     Validation.checkRange(size);
+  }
+
+  makeWinningBridge(size) {
+    const bridge = BridgeMaker.makeBridge(size, () =>
+      BridgeRandomNumberGenerator.generate()
+    );
+    console.log(bridge);
+    // console.log(this.#winningBridge);
   }
 }
 
