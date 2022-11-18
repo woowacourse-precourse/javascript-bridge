ac3b17358ff1d9dbcfea598db425c99acc3191b0
turtle601 <turtle601@naver.com>
2022-11-18 09:58:10 +0900

    test(UpDownKeyTest) update 동작 테스트 추가
    Add:
    - case: 'U'키를 누를 때 userBridge 배열에 추가되는 지

--- __tests__/UpDownKeyTest.js
+++ __tests__/UpDownKeyTest.js
@@ -1,4 +1,5 @@
 const UpDownKey = require('../src/service/domain/UpDownKey');
+const { MODEL_KEY } = require('../src/utils/constants');
 
 describe('이동할 칸 입력 테스트', () => {
   test('(U or D 제외 문자 입력(오류) - "a") 입력 테스트', () => {
@@ -26,13 +27,21 @@ describe('이동할 칸 입력 테스트', () => {
     const input = 'U';
     const upDowonKey = new UpDownKey(input);
 
-    expect(upDowonKey.getValidateData()).toEqual('U');
+    upDowonKey.doAction();
+
+    const userBridge = upDowonKey.getModelFor(MODEL_KEY.userBridge);
+
+    expect(userBridge).toEqual(['U']);
   });
 
   test('(U or R 제외 문자 입력(정상) - "D") 입력 테스트', () => {
     const input = 'D';
     const upDowonKey = new UpDownKey(input);
 
-    expect(upDowonKey.getValidateData()).toEqual('D');
+    upDowonKey.doAction();
+
+    const userBridge = upDowonKey.getModelFor(MODEL_KEY.userBridge);
+
+    expect(userBridge).toEqual(['D']);
   });
 });
