31f933356f0cd5becbe32bfa7d19dbd8ceb8de7e
dladncks1217 <dlaxodud1217@gmail.com>
2022-11-18 15:58:45 +0900

    feat(constants): 상수 일부 추가 및 변경

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,7 +4,7 @@ const GAME_MESSAGE = {
   MOVE_CHOICE: '이동할 칸을 선택해주세요. (위: U, 아래: D)',
   GAME_RETRY: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)',
   GAME_RESULT: '최종 게임 결과',
-  GAME_CLEAR: '게임 성공 여부:',
+  GAME_CLEAR: (string) => `게임 성공 여부: ${string}`,
   TRY_COUNT: (count) => `총 시도한 횟수: ${count}`,
 };
 
