0f67493bb975517816908c7c34f9ffdf6e57a21f
Yebin Kim <kyb4312@gmail.com>
2022-11-18 23:23:57 +0900

    feat(App): check if the player wants to retry and if so, undo a step and move on

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ const InputView = require('./InputView');
 const OutputView = require('./OutputView');
 const BridgeGame = require('./BridgeGame');
 
+const RETRY_OR_NOT = { R: true, Q: false };
+
 class App {
   #bridgeGame = new BridgeGame();
 
@@ -34,6 +36,13 @@ class App {
 
   askRetry() {
     InputView.readGameCommand(this.checkRetry);
+  }
+
+  checkRetry(command) {
+    if (RETRY_OR_NOT[command]) {
+      this.#bridgeGame.undoOneStep();
+      return this.askMove();
+    }
   }
 }
 
