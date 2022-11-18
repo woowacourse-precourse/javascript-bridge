1eb5dc4c8601d3b750eb9d567d3d762c2650b5f1
Sijun <sionwer4@naver.com>
2022-11-18 15:16:39 +0900

    fix: input value return error fix

--- src/InputView.js
+++ src/InputView.js
@@ -7,27 +7,33 @@ const InputView = {
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize() {
+    let input;
     MissionUtils.Console.readLine("다리의 길이를 입력해주세요.", (answer) => {
-      return answer;
+      input = answer;
     });
+    return input;
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving() {
+    let input;
     MissionUtils.Console.readLine("이동할 칸을 선택해주세요. (위: U, 아래: D)", (answer) => {
-      return answer;
+      input = answer;
     });
+    return input
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand() {
+    let input;
     MissionUtils.Console.readLine("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)", (answer) => {
-      return answer;
+      input = answer;
     });
+    return input;
   },
 };
 
