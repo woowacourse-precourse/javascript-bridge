c6f9678f57de7c88d0daeb5f6136e39a6a374deb
park junwoo <always1997@kakao.com>
2022-11-18 17:43:42 +0900

    feat: 건널수있는지 여부에 따른 'O' 'X' 표시 기능 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,16 +1,58 @@
+const BridgeMaker = require('./BridgeMaker');
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  #bridge
+  #upBridge
+  #downBridge
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move(command,stage) {
+  constructor(bridge) {
+    this.#bridge = bridge;
 
   }
 
+  move(command,idx) {
+    if(bridge[idx] === command) {
+      return this.passableBridge(command);
+    }
+    return this.unPassableBridge(command);
+  }
+
+  passableBridge(command) {
+    switch(command) {
+      case 'U' : {
+        this.#upBridge.push('O');
+        this.#downBridge.push(' ');
+        break;
+      }
+      case 'D' : {
+        this.#upBridge.push(' ');
+        this.#downBridge.push('O');
+        break;
+      }  
+    }
+  }
+  unPassableBridge(command) {
+    switch(command) {
+      case 'U' : {
+        this.#upBridge.push('X');
+        this.#downBridge.push(' ');
+        break;
+      }
+      case 'D' : {
+        this.#upBridge.push(' ');
+        this.#downBridge.push('X');
+        break;
+      }  
+    }
+    //gameOver()
+  }
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
    * <p>
