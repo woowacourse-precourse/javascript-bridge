0aec5c6c40a8b742d479aecea7e6ff81db9eb87b
Yoo Seung Guk <dbtmd878@naver.com>
2022-11-18 16:01:12 +0900

    style: 띄어쓰기 추가

--- src/InputView.js
+++ src/InputView.js
@@ -1,5 +1,4 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-const BridgeMaker = require("./BridgeMaker");
 const Console = MissionUtils.Console;
 
 /**
@@ -16,26 +15,24 @@ const InputView = {
       return callBack(bridgeLength);
     });
   },
-
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving(callBack) {
     Console.readLine(
-      "이동할 칸을 선택해주세요. (위: U, 아래: D) \n",
+      "\n이동할 칸을 선택해주세요. (위: U, 아래: D) \n",
       (moveKey) => {
         return callBack(moveKey);
       }
     );
   },
-
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
 
   readGameCommand(stopCallBack, retryCallBack) {
     Console.readLine(
-      "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q) \n",
+      "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q) \n",
       (endKey) => {
         if (endKey === "R") {
           return retryCallBack();
