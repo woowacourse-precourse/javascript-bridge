8b498f3af95318deee9cf8776ee1d1e33c0fee20
inthhh <glass005533@gmail.com>
2022-11-19 00:34:27 +0900

    feat: OutputView 최종결과 출력 메소드 구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,41 +1,44 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const Console = MissionUtils.Console;
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
+
 const OutputView = {
-  /**
-   * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-   * <p>
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
+
   printMap(nowLength, bridge, moveUpDown, tf) {
     let upList = [];
     let downList = [];
-    for (let move = 0; move < nowLength+1; ++move) {
-      if (bridge[move] == "D") {
+    for (let n = 0; n < nowLength+1; ++n) {
+      if (bridge[n] == "D") {
         upList.push(" ");
         downList.push("O");
       }
-      if (bridge[move] == "U") {
+      if (bridge[n] == "U") {
         upList.push("O");
         downList.push(" ");
       }
     }
     if (!tf) {
-      if (moveUpDown == "U") downList[nowLength] = "X";
-      if (moveUpDown == "D") upList[nowLength] = "X";
+      if (moveUpDown == "U") {
+        upList[nowLength] = "X";
+        downList[nowLength] = " ";
+      }
+      if (moveUpDown == "D") {
+        downList[nowLength] = "X";
+        upList = " ";
+      }
     }
     Console.print("[ " + upList.join(" | ") + " ]");
     Console.print("[ " + downList.join(" | ") + " ]");
+    return;
   },
 
-  /**
-   * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-   * <p>
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  printResult() {},
+  printResult(gameLength, bridge, moveUpDown, tf) {
+    Console.print("\n최종 게임 결과");
+    this.printMap(gameLength, bridge, moveUpDown, tf);
+    if (tf) Console.print("\n게임 성공 여부: 성공");
+    if (!tf) Console.print("\n게임 성공 여부: 실패");
+    Console.print("총 시도한 횟수: (tryCount 구하기)");
+    return;
+  },
 };
 
 module.exports = OutputView;
