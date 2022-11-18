3830c459905f3f33537c56ff53dad317e35587c6
JiSu Kim <85912592+jsk3342@users.noreply.github.com>
2022-11-18 22:02:18 +0900

    feat(validateInput): 메시지 변경

--- src/Validator.js
+++ src/Validator.js
@@ -1,9 +1,11 @@
 const Validator = {
   validateInput(number) {
     if (3 > number || number > 20) {
-      throw new Error("[ERROR] 3 이상 20 이하의 숫자만 입력 가능합니다.");
+      throw "[ERROR] 3 이상 20 이하의 숫자만 입력 가능합니다.";
     }
   },
 };
 
 module.exports = Validator;
+
+
