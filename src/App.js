9963bba80137a3d55c2f5c36347c179e1ad890a5
gyeongza <gyeongza@naver.com>
2022-11-18 22:22:54 +0900

    feat : 게임 시작 메시지 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const { Console } = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    this.bridgeGameStart();
+  }
+
+  bridgeGameStart() {
+    Console.print("다리 건너기 게임을 시작합니다.\n");
+  }
 }
 
 module.exports = App;
