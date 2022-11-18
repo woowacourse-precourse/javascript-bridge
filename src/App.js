1fc4428d5de3c4bd311bc6d5936f26123b5f6506
ReturnReason <reasonz@daum.net>
2022-11-19 03:53:25 +0900

    feat: 다리 건너기 게임 시작 문구를 출력하는 메서드 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+const OutputView = require('./view/OutputView');
+
 class App {
-  play() {}
+  play() {
+    OutputView.printStart();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
