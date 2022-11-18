e8007be98264efcf6ba080e16119d30265beb86b
millifail <hwangbird99@gmail.com>
2022-11-18 17:38:57 +0900

    feat(bridge-generator): generate bridge
     - 입력받은 다리길이를 이용해서 랜덤하게 U와 D를 다리에 넣어준다
     - 다리 생성 테스트 통과

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 const MissionUtils = require('@woowacourse/mission-utils');
-const inputVIew = require('./InputView');
+const InputVIew = require('./InputView');
+const BridgeMaker = require('./BridgeMaker');
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
 
 class App {
   play() {
     MissionUtils.Console.print('다리 건너기 게임을 시작합니다.');
-    const BRIDGE_SIZE = inputVIew.readBridgeSize();
-    console.log(BRIDGE_SIZE);
+    const BRIDGE_SIZE = InputVIew.readBridgeSize();
+    const BRIDGE = BridgeMaker.makeBridge(
+      BRIDGE_SIZE,
+      BridgeRandomNumberGenerator.generate()
+    );
+    console.log('briddddddddddddddge', BRIDGE);
   }
 }
 
--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -7,7 +7,21 @@ const BridgeMaker = {
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
-  makeBridge(size, generateRandomNumber) {},
+  makeBridge(size, generateRandomNumber) {
+    const up_and_down = [];
+    console.log('asbdc');
+    let num;
+    for (let i = 0; i < size; i++) {
+      num = Number(generateRandomNumber());
+      console.log(num);
+      if (num === 0) {
+        up_and_down.push('D');
+      } else {
+        up_and_down.push('U');
+      }
+    }
+    return up_and_down;
+  },
 };
 
 module.exports = BridgeMaker;
