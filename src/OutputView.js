3284de962dcb36eb08924d9ab9d596fe3177ea98
kangyeongmin <kym6050@hanyang.ac.kr>
2022-11-18 19:28:01 +0900

    ♻️ refactor(OutputView): 메소드 분리, 코드 리펙토링

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,11 +1,12 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const { VALUE } = require("./constant");
 
 const OutputView = {
   printStart() {
     MissionUtils.Console.print("다리 건너기 게임을 시작합니다.\n");
   },
 
-  printMap(bridge) {
+  printBridge(bridge) {
     let blockString = "";
     bridge.forEach((sign, index) => {
       blockString += bridge.length - 1 === index ? ` ${sign}` : ` ${sign} |`;
@@ -13,11 +14,16 @@ const OutputView = {
     MissionUtils.Console.print(`[${blockString} ]`);
   },
 
-  printResult(bridgeGame, isSuccess) {
-    const result = isSuccess ? "성공" : "실페";
+  printMap(bridgeResult) {
+    this.printBridge(bridgeResult.upResult);
+    this.printBridge(bridgeResult.downResult);
+  },
+
+  printResult(bridgeGame) {
+    const result = bridgeGame.getIsSuccess() ? VALUE.SUCCESS : VALUE.FAIL;
+
     MissionUtils.Console.print("\n최종 게임 결과");
-    this.printMap(bridgeGame.getBridgeResult().upResult);
-    this.printMap(bridgeGame.getBridgeResult().downResult);
+    this.printMap(bridgeGame.getBridgeResult());
     MissionUtils.Console.print("\n게임 성공 여부: " + result);
     MissionUtils.Console.print(
       "총 시도한 횟수: " + bridgeGame.getTrialNumber()
