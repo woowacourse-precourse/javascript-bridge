b95dcb4980fb100bcdad58fe400d64390e123d71
ReturnReason <reasonz@daum.net>
2022-11-19 04:40:35 +0900

    feat: BridgeGame 클래스의 인스턴스가 생성될 때 다리게임 맵이 생성되도록 코드 추가

--- src/domain/BridgeGame.js
+++ src/domain/BridgeGame.js
@@ -1,13 +1,25 @@
+const BridgeMaker = require('../BridgeMaker');
+const BridgeRandomNumberGenerator = require('../BridgeRandomNumberGenerator');
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  #position = 0;
+
+  constructor(size) {
+    this.bridgeMap = BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate);
+    console.log(this.bridgeMap);
+  }
+
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move() {
+    this.#position += 1;
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
