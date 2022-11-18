6a025cf2667c1bf76bd4da041d1f3fdf6f9fefc7
AhnGeunTaek <dksrmsxor@gmail.com>
2022-11-18 16:38:13 +0900

    feat: 게임 진행 상황 안내 문구 -> 상수 처리 constants.js 생성

---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+const INPUT_MESSAGE = {
+  BRIDGE_SIZE: '다리의 길이를 입력해주세요.',
+  USER_MOVEMENT: '이동할 칸을 선택해주세요. (위: U, 아래: D)',
+  GAME_COMMAND: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)',
+};
+
+const OUTPUT_MESSAGE = {
+  GAME_START: '다리 건너기 게임을 시작합니다.',
+};
+
+module.exports = { INPUT_MESSAGE, OUTPUT_MESSAGE };
