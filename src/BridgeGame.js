89a7a67bbc59188e55f539bfc6bb8a52aa883cca
chiman <rlaclghks123@naver.com>
2022-11-18 19:34:07 +0900

    fix: 불필요한 필드변수 제거 및 불필요한 else if 제거
    1. 함수에 메세지를 보내 불필요한 필드변수를 제거
    2. else if 대신 if로 대체

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -5,11 +5,9 @@ const BridgeState = require('./BridgeState');
  */
 class BridgeGame {
   #bridge;
-  #crossResult;
 
   constructor(bridge) {
     this.#bridge = bridge;
-    this.#crossResult = [];
   }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -17,41 +15,44 @@ class BridgeGame {
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   move(userBridge) {
+    const crossBridgeResult = [];
     userBridge.forEach((position, i) => {
       if (position === this.#bridge[i] && position === 'U') {
-        return this.#crossResult.push(['O', ' ']);
-      } else if (position === this.#bridge[i] && position === 'D') {
-        return this.#crossResult.push([' ', 'O']);
-      } else if (position === 'U') return this.#crossResult.push(['X', ' ']);
-      return this.#crossResult.push([' ', 'X']);
+        return crossBridgeResult.push(['O', ' ']);
+      }
+      if (position === this.#bridge[i] && position === 'D') {
+        return crossBridgeResult.push([' ', 'O']);
+      }
+      if (position === 'U') return crossBridgeResult.push(['X', ' ']);
+      return crossBridgeResult.push([' ', 'X']);
     });
-    return this.#crossResult;
+    return crossBridgeResult;
   }
 
-  draw() {
+  draw(moveBridge) {
     let upBridge = '';
     let downBridge = '';
 
-    this.#crossResult.forEach((position) => {
-      upBridge = this.drawUpBridge(position, upBridge);
-      downBridge = this.drawDownBridge(position, downBridge);
+    moveBridge.forEach((position) => {
+      upBridge = this.drawUpBridge(position, upBridge, moveBridge);
+      downBridge = this.drawDownBridge(position, downBridge, moveBridge);
     });
     return [upBridge, downBridge];
   }
 
-  drawUpBridge(position, upBridge) {
-    if (position === this.#crossResult[0]) {
+  drawUpBridge(position, upBridge, moveBridge) {
+    if (position === moveBridge[0]) {
       upBridge += position[0];
-    } else if (position !== this.#crossResult[0]) {
+    } else if (position !== moveBridge[0]) {
       upBridge += ` | ${position[0]}`;
     }
     return upBridge;
   }
 
-  drawDownBridge(position, downBridge) {
-    if (position === this.#crossResult[0]) {
+  drawDownBridge(position, downBridge, moveBridge) {
+    if (position === moveBridge[0]) {
       downBridge += position[1];
-    } else if (position !== this.#crossResult[0]) {
+    } else if (position !== moveBridge[0]) {
       downBridge += ` | ${position[1]}`;
     }
     return downBridge;
