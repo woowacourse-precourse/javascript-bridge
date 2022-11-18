5e34e80c2c0de95cbbb2c304d12661d6459027db
Junseo <oponize@naver.com>
2022-11-18 14:10:31 +0900

    feat(InputView): 다리의 길이를 입력받는 기능 구현

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -1,3 +1,5 @@
+const { read, print } = require("../utils/Io.js");
+const { INPUT } = require("../constants/index.js");
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,7 +7,17 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize(callback) {
+    const { READ_BRIDGE } = INPUT;
+    read(READ_BRIDGE, (size) => {
+      try {
+        callback(size);
+      } catch (error) {
+        print(error);
+        this.readBridgeSize(callback);
+      }
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
@@ -19,3 +31,8 @@ const InputView = {
 };
 
 module.exports = InputView;
+
+// ? 제공된 InputView 객체를 활용해 구현해야 한다.
+// ? InputView의 파일 경로는 변경할 수 있다.
+// ? InputView의 메서드의 인자는 변경할 수 있다.
+// ? 사용자 값 입력을 위해 필요한 메서드를 추가할 수 있다.
