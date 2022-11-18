d9ee42f1afd9c47e242035e24ad1eecfffeed7c2
Tory <88531407+1two13@users.noreply.github.com>
2022-11-18 09:30:43 +0900

    feat(Messages): 메세지들 static으로 관리

---
+++ src/Messages.js
@@ -0,0 +1,9 @@
+class Messages {
+  static START_GAME = '다리 건너기 게임을 시작합니다.\n';
+  static INPUT_BRIDGE_SIZE = '다리의 길이를 입력해주세요.\n';
+  static INPUT_UP_OR_DOWN = '이동할 칸을 선택해주세요. (위: U, 아래: D)\n';
+  static INPUT_RESTART_OR_END = '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n';
+  static END_GAME = '최종 게임 결과';
+}
+
+module.exports = Object.freeze(Messages);
