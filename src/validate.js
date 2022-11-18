7344c2d2bad59d91dc68d307c84f52c2cd598a10
pinkishincoloragain <smb1103@gmail.com>
2022-11-18 17:27:29 +0900

    bug: bridgeSize를 number 형태로 비교하지 않는 에러 수정

--- src/validate.js
+++ src/validate.js
@@ -18,7 +18,6 @@ const validateMove = (move) => {
 };
 
 const validateRestart = (restart) => {
-  console.log("Restart is=" +restart);
   const isRestart = restart === RETRY.YES || restart === RETRY.NO;
 
   if (isRestart === false) throw Error(ERRORS.INVALID_RESTART_TYPE);
