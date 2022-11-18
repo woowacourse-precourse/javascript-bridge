3864a7712660f2b8258ef94ec4e07fa843e9c598
gyeongza <gyeongza@naver.com>
2022-11-19 01:16:59 +0900

    feat : (예외처리) 숫자의 범위가 아닌 경우 예외 처리 추가

--- src/InputCheck.js
+++ src/InputCheck.js
@@ -1,6 +1,7 @@
 const InputCheck = {
   checkBridgeSize(inputBridgeSize) {
     this.isNumber(inputBridgeSize);
+    this.isCorrectRange(inputBridgeSize)
   },
 
   isNumber(inputBridgeSize) {
@@ -8,6 +9,12 @@ const InputCheck = {
       throw new Error("[ERROR] 숫자만 입력해 주세요.");
     }
   },
+
+  isCorrectRange(inputBridgeSize) {
+    if (inputBridgeSize < 3 || inputBridgeSize > 20) {
+      throw new Error("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+    }
+  }
 };
 
 module.exports = InputCheck;
