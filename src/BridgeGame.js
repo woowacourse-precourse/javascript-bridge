776d41bda3fc5dae2a47afb7727b41508977bb9c
Jeong seulho <wjdtmfgh@yu.ac.kr>
2022-11-18 19:00:30 +0900

    Refactor: using #currentPos and simplify code

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -2,65 +2,43 @@ const BridgeMaker = require('./BridgeMaker');
 
 class BridgeGame {
 	#bridge;
+	#currentPos;
 
 	constructor(bridgeSize, generateRandomNumber) {
 		this.#bridge = BridgeMaker.makeBridge(bridgeSize, generateRandomNumber);
+		this.#currentPos = 0;
 		console.log(this.#bridge);
 	}
 
 	move(moving) {
-		const yetCrossBridge = this.updateYetcrossBridge(moving);
-		const inverse = true;
-		const prevCrossedBridge = this.delPrevBridgeEle(inverse);
-		this.#bridge = [...prevCrossedBridge, ...yetCrossBridge];
-	}
+		const isCorrect = this.#bridge[this.#currentPos] === moving;
 
-	updateYetcrossBridge(moving) {
-		let yetCrossBridge = this.delPrevBridgeEle();
-		const isCorrect = yetCrossBridge[0] === moving;
 		// UO : 지나온 다리가 U이며, 정답이었던 경우
 		// UX : 지나온 다리가 U이며, 틀린 경우
 		// DO : 지나온 다리가 D이며, 정답이었던 경우
 		// DX : 지나온 다리가 D이며, 틀린 경우
 		isCorrect
-			? (yetCrossBridge[0] = moving + 'O')
-			: (yetCrossBridge[0] = moving + 'X');
-		return yetCrossBridge;
-	}
-
-	delPrevBridgeEle(inverse = false) {
-		const filterBridgeEle = this.#bridge.filter((bridgeEleInfo) =>
-			inverse
-				? !(bridgeEleInfo === 'D' || bridgeEleInfo === 'U')
-				: bridgeEleInfo === 'D' || bridgeEleInfo === 'U',
-		);
-
-		return filterBridgeEle;
+			? (this.#bridge[this.#currentPos] = moving + 'O')
+			: (this.#bridge[this.#currentPos] = moving + 'X');
+		this.#currentPos++;
 	}
 
 	getPrevCrossedBridge() {
-		const inverse = true;
-		const prevCrossedBridge = this.delPrevBridgeEle(inverse);
-		return prevCrossedBridge;
+		return this.#bridge.slice(0, this.#currentPos);
 	}
 
 	retry() {
-		const prevCrossedBridge = this.backMove();
-		const yetCrossBridge = this.delPrevBridgeEle();
-		this.#bridge = [...prevCrossedBridge, ...yetCrossBridge];
+		this.backMove();
 	}
 
 	backMove() {
-		const inverse = true;
-		let prevCrossedBridge = this.delPrevBridgeEle(inverse);
-		const currentBridgeEle = prevCrossedBridge.pop();
-		if (currentBridgeEle === 'UX') {
-			prevCrossedBridge.push('D');
+		this.#currentPos--;
+		if (this.#bridge[this.#currentPos] === 'UX') {
+			this.#bridge[this.#currentPos] = 'D';
 		}
-		if (currentBridgeEle === 'DX') {
-			prevCrossedBridge.push('U');
+		if (this.#bridge[this.#currentPos] === 'DX') {
+			this.#bridge[this.#currentPos] = 'U';
 		}
-		return prevCrossedBridge;
 	}
 }
 
