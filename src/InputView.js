d67e4f398fd8e427117df3c8bb9256bf7424f2f2
inthhh <glass005533@gmail.com>
2022-11-18 22:59:39 +0900

    feat: InputView 반복입력 구현

--- src/InputView.js
+++ src/InputView.js
@@ -2,16 +2,18 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const BridgeGame = require("./BridgeGame");
 const BridgeMaker = require("./BridgeMaker");
 const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+const OutputView = require("./OutputView");
 const Console = MissionUtils.Console;
 
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {
+   readBridgeSize() {
     Console.readLine("\n다리의 길이를 입력해주세요.\n", (inputBridgeSize) => {
-      let bridge = BridgeMaker.makeBridge(inputBridgeSize, BridgeRandomNumberGenerator.generate);
-      Console.close();
+      bridge = BridgeMaker.makeBridge(inputBridgeSize, BridgeRandomNumberGenerator.generate);
+      InputView.readMoving(bridge, 0);
+      // Console.close();
       return bridge;
     });
   },
@@ -19,10 +21,12 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving(bridge) {
+  readMoving(bridge, turnNumber) {
+    const brgGame = new BridgeGame();
     Console.readLine("\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n", (inputMoveUpDown) => {
-      BridgeGame.move(bridge, inputMoveUpDown);
-      Console.close();
+      bridge = brgGame.move(turnNumber, bridge, inputMoveUpDown);
+      if (bridge.length > 0) this.readMoving(bridge, turnNumber + 1);
+      else Console.close();
     })
   },
 
@@ -31,9 +35,11 @@ const InputView = {
    */
   readGameCommand() {
     Console.readLine("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n", (inputRetry) => {
-      //
+      if (inputRetry == "R") return 0;
+      if (inputRetry == "Q") OutputView.printResult();
       Console.close();
     })
+    return 1;
   },
 };
 
