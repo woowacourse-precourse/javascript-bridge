28fd17581dd7d0f1c720fd7019b2d1eb0edc9e96
ukkodeveloper <ukkodeveloper@gmail.com>
2022-11-18 15:54:41 +0900

    feat: 출력 메세지 목록 작성

---
+++ src/message.js
@@ -0,0 +1,6 @@
+const MESSAGE = Object.freeze({
+  start: '다리 건너기 게임을 시작합니다.',
+  askBridgeLength: '다리의 길이를 입력해주세요.',
+  askMoveDirection: '이동할 칸을 선택해주세요. (위: U, 아래: D)',
+  askRestart: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)',
+});
