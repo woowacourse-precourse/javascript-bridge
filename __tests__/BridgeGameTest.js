760b0dce9dc2b87a5c0468e87f974f7c8094f6d2
chiman <rlaclghks123@naver.com>
2022-11-18 11:41:05 +0900

    test: BridgeGame 이동시 결과값 테스트
    1. 사용자의 입력값이 U로 일치 할 경우
    2. 사용자가 입력값이 D로  일치 할 경우
    3. 일치하지 않을 경우

---
+++ __tests__/BridgeGameTest.js
@@ -0,0 +1,31 @@
+const BridgeGame = require('../src/BridgeGame');
+
+describe('Bridge Game Test', () => {
+  describe('사용자의 입력값이 일치할 경우', () => {
+    test('사용자의 입력값이 U로 일치할 경우 Test', () => {
+      const bridgeGame = new BridgeGame(['U', 'D', 'U']);
+      const result = bridgeGame.move('U');
+      expect(result).toEqual(['O', ' ']);
+    });
+
+    test('사용자의 입력값이 D로 일치할 경우 Test', () => {
+      const bridgeGame = new BridgeGame(['D', 'D', 'U']);
+      const result = bridgeGame.move('D');
+      expect(result).toEqual([' ', 'O']);
+    });
+  });
+
+  describe('사용자의 입력값이 일치하지 않을 경우', () => {
+    test('사용자의 입력값이 일치하지 않을 경우 Test', () => {
+      const bridgeGame = new BridgeGame(['D', 'D', 'U']);
+      const result = bridgeGame.move('U');
+      expect(result).toEqual(['X', ' ']);
+    });
+
+    test('사용자의 입력값이 일치하지 않을 경우 Test', () => {
+      const bridgeGame = new BridgeGame(['U', 'D', 'U']);
+      const result = bridgeGame.move('D');
+      expect(result).toEqual(['X', ' ']);
+    });
+  });
+});
