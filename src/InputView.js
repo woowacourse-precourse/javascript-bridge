2e121e29181968444348bf456ee9bd1fe8d77033
oio337a <oio337a@gmail.com>
2022-11-18 17:41:04 +0900

    feat : check validate bridge input  size

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,6 @@
+const { Console } = require("@woowacourse/mission-utils");
+const { MESSAGE } = require("../constants/Message");
+const ValidateInput = require("./ValidateInput");
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  * InputView 에서만 MissionUtils의
@@ -7,7 +10,15 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine(MESSAGE.READ_BRIDGE_SIZE, (size) => {
+      try {
+        ValidateInput.validate(size);
+      } catch (e) {
+        this.readBridgeSize();
+      }
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
