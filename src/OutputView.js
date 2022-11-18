1044b1af35eef81cdf9e8756a3939a237590710c
minsoo <als494@naver.com>
2022-11-18 19:38:22 +0900

    feat: 방향 입력받고 출력

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,3 +1,5 @@
+const { Console } = require("@woowacourse/mission-utils");
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -7,7 +9,39 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {},
+  printMap(currentBridge, check) {
+    let upBridge = [];
+    let downBridge = [];
+    upBridge, downBridge = this.pushBridge(currentBridge, upBridge, downBridge);
+    this.lastCheck(upBridge, downBridge, check);
+    Console.print(`[ ${upBridge.join(" | ")} ]`);
+    Console.print(`[ ${downBridge.join(" | ")} ]`);
+  },
+
+  pushBridge(currentBridge, upBridge, downBridge) {
+    for (let i=0; i < currentBridge.length; i++) {
+      if (currentBridge[i] === "U") {
+        upBridge.push("O");
+        downBridge.push(" ");
+      }
+      if (currentBridge[i] === "D") {
+        upBridge.push(" ");
+        downBridge.push("O");
+      }
+    }
+    return upBridge, downBridge
+  },
+
+  lastCheck(upBridge, downBridge, check) {
+    if (check === "X" && upBridge[upBridge.length - 1] === "O") {
+      upBridge.pop();
+      upBridge.push("X");
+    }
+    if (check === "X" && downBridge[downBridge.length - 1] === "O") {
+      downBridge.pop();
+      downBridge.push("X");
+    }
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
