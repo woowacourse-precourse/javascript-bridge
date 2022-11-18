e8aef1bb2e31f74490e90f1f866f2a906132fe88
Leejin-Yang <ejin1996@gmail.com>
2022-11-18 21:51:51 +0900

    test: 입력 유효성 검증 테스트 추가 작성

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -1,22 +1,3 @@
-const BridgeGame = require('../src/models/BridgeGame');
+// const BridgeGame = require('../src/models/BridgeGame');
 
-describe('다리 건너기 게임 클래스 테스트', () => {
-  test.each([[2], [-1], [21]])('다리 길이가 3미만 20초과인 경우 예외 처리', (size) => {
-    const bridgeGame = new BridgeGame();
-
-    expect(() => {
-      bridgeGame.setBridge(size);
-    }).toThrow('[ERROR]');
-  });
-
-  test.each([[''], [' '], ['a'], ['2']])(
-    '이동 입력 값이 U 또는 D가 아닌 경우 예외 처리',
-    (input) => {
-      const bridgeGame = new BridgeGame();
-
-      expect(() => {
-        bridgeGame.move(input);
-      }).toThrow('[ERROR]');
-    },
-  );
-});
+describe('다리 건너기 게임 클래스 테스트', () => {});
--- __tests__/InputValidatorTest.js
+++ __tests__/InputValidatorTest.js
@@ -1,7 +1,7 @@
 const InputValidator = require('../src/utils/InputValidator');
 
 describe('입력 유효성 검사 테스트', () => {
-  test('빈 값 유효성 검증.', () => {
+  test('빈 값 유효성 검증', () => {
     const input = '';
 
     expect(() => {
@@ -9,7 +9,7 @@ describe('입력 유효성 검사 테스트', () => {
     }).toThrow('[ERROR]');
   });
 
-  test('공백 포함 유효성 검증.', () => {
+  test('공백 포함 유효성 검증', () => {
     const input = ' 1';
 
     expect(() => {
@@ -17,11 +17,38 @@ describe('입력 유효성 검사 테스트', () => {
     }).toThrow('[ERROR]');
   });
 
-  test('숫자 유효성 검증.', () => {
+  test('숫자 유효성 검증', () => {
     const input = 'a';
 
     expect(() => {
       InputValidator.validateNumber(input);
     }).toThrow('[ERROR]');
   });
+
+  test.each([[''], [' 1'], ['a']])('다리 길이 값이 숫자가 아닌 경우 예외 처리', (size) => {
+    expect(() => {
+      InputValidator.validateSize(size);
+    }).toThrow('[ERROR]');
+  });
+
+  test.each([['2'], ['-1'], ['21']])('다리 길이 값이 3미만 20초과인 경우 예외 처리', (size) => {
+    expect(() => {
+      InputValidator.validateSize(size);
+    }).toThrow('[ERROR]');
+  });
+
+  test.each([[''], [' '], ['a'], ['2']])('이동 값이 U 또는 D가 아닌 경우 예외 처리', (input) => {
+    expect(() => {
+      InputValidator.validateMovingCommand(input);
+    }).toThrow('[ERROR]');
+  });
+
+  test.each([[''], [' '], ['a'], ['2']])(
+    '게임 커맨드 값이 R 또는 Q가 아닌 경우 예외 처리',
+    (input) => {
+      expect(() => {
+        InputValidator.validateGameCommand(input);
+      }).toThrow('[ERROR]');
+    },
+  );
 });
