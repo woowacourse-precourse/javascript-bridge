eb09f53c967b4417de18faea90f73ff1bdcd4324
minjaeKim <smallkdb@gmail.com>
2022-11-18 17:42:25 +0900

    fix(BridgeMarker): 배열 생성시 넘겨주는 인자의 타입을 Number로 수정

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,5 +1,3 @@
-const { generate } = require('./BridgeRandomNumberGenerator');
-
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
@@ -10,7 +8,7 @@ const BridgeMaker = {
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
   makeBridge(size, generateRandomNumber) {
-    return Array(size)
+    return Array(Number(size))
       .fill(0)
       .map(() => {
         const number = String(generateRandomNumber());
