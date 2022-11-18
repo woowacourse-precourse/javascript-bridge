fbed2f8cb1e40a8c28883ed131a70a838de956dd
han0224 <dbn0224@gmail.com>
2022-11-18 16:54:39 +0900

    Refactor: let -> const로 변경
    inputView의 입력받은 정보를 저장하는 변수를 let이 아닌 const로 선언

--- src/InputView.js
+++ src/InputView.js
@@ -18,7 +18,7 @@ const InputView = {
    * 다리의 길이를 입력받는다.
    */
   async readBridgeSize() {
-    let birdgeSize = Number(await this.inputMethod(INPUT_MSG.BRIDGESIZE));
+    const birdgeSize = Number(await this.inputMethod(INPUT_MSG.BRIDGESIZE));
     try {
       if (!isNumber(birdgeSize) || !(birdgeSize >= 3 && birdgeSize <= 20))
         throw error(ERROR_MSG.INPUT_BRIDGE);
@@ -33,7 +33,7 @@ const InputView = {
    * 사용자가 이동할 칸을 입력받는다.
    */
   async readMoving() {
-    let moving = await this.inputMethod(INPUT_MSG.MOVING);
+    const moving = await this.inputMethod(INPUT_MSG.MOVING);
     try {
       if (moving !== "U" && moving !== "D") {
         throw error(ERROR_MSG.INPUT_MOVING);
