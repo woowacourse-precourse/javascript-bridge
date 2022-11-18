63b7893f4b6e274a1079cc9d1de9495675fc7503
NaveOWO <pianowell96@gmail.com>
2022-11-19 03:06:51 +0900

    [ feat ] 사용자가 입력한 칸이 건널 수 있는지 판단하는 기능

--- src/InputView.js
+++ src/InputView.js
@@ -1,5 +1,6 @@
 const { Console } = require("@woowacourse/mission-utils");
 const BridgeMaker = require("./BridgeMaker");
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -9,13 +10,20 @@ const InputView = {
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize() {
-    Console.readLine("다리의 길이를 입력해주세요.", (size) => {});
+    Console.readLine("다리의 길이를 입력해주세요.", (size) => {
+      BridgeRandomNumberGenerator.createRandomLocation(size);
+    });
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    Console.readLine(
+      "이동할 칸을 선택해주세요. (위: U, 아래: D)",
+      (direction) => {}
+    );
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
