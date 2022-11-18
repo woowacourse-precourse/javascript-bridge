2067e2a70f683c473530e0e2460d79e84d02a245
Yoo Seung Guk <dbtmd878@naver.com>
2022-11-18 16:01:29 +0900

    style: 띄어쓰기 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -23,10 +23,10 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printResult(map, tryCount, gameState) {
-    Console.print("최종 게임 결과");
+    Console.print("\n최종 게임 결과");
     this.printMap(map);
     const ANSWER = gameState === true ? "성공" : "실패";
-    Console.print(`게임 성공 여부: ${ANSWER}`);
+    Console.print(`\n게임 성공 여부: ${ANSWER}`);
     Console.print(`총 시도한 횟수: ${tryCount}`);
     return;
   },
