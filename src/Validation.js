79240751fa1887b7f3bbb6ecd801c072ccb7eb52
minjaeKim <smallkdb@gmail.com>
2022-11-19 06:48:47 +0900

    refactor(Validation): 클래스 생성, 에러 메시지 를 보여주는 메서드 추가
    - 각 메서드의 이름을 변경하였습니다.
    - 에러 메시지를 보여주는 showErrorMessage메서드를 추가하였습니다.

--- src/Validation.js
+++ src/Validation.js
@@ -1,16 +1,40 @@
-const isValidateBridgeSize = (size) => {
-  const regExp = /^[3-9]{1}$|^1{1}[0-9]{1}$|^2{1}0{1}$/;
-  return regExp.test(size);
-};
+const { Console } = require('@woowacourse/mission-utils');
+const { printNewLine } = require('./OutputView');
 
-const isValidateRoundInput = (input) => {
-  const regExp = /U|D/;
-  return regExp.test(input);
-};
+class Validation {
+  static showErrorMessage(message) {
+    try {
+      throw new Error(message);
+    } catch (e) {
+      Console.print(e.message);
+      return false;
+    }
+  }
 
-const isValidateRetryInput = (input) => {
-  const regExp = /R|Q/;
-  return regExp.test(input);
-};
+  static checkBridgeSize(size) {
+    const regExp = /^[3-9]{1}$|^1{1}[0-9]{1}$|^2{1}0{1}$/;
+    if (!regExp.test(size)) {
+      printNewLine();
+      return Validation.showErrorMessage('[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.');
+    }
+    return true;
+  }
 
-module.exports = { isValidateBridgeSize, isValidateRoundInput, isValidateRetryInput };
+  static checkRoundInput(input) {
+    const regExp = /U|D/;
+    if (!regExp.test(input)) {
+      printNewLine();
+      Validation.showErrorMessage('[ERROR] 입력값은 문자 "U"이거나 "D"여야 합니다.');
+    }
+  }
+
+  static checkRetryInput(input) {
+    const regExp = /R|Q/;
+    if (!regExp.test(input)) {
+      printNewLine();
+      Validation.showErrorMessage('[ERROR] 입력값은 문자 "R"이거나 "Q"여야 합니다.');
+    }
+  }
+}
+
+module.exports = Validation;
