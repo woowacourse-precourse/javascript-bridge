76554d38e1d67195e7dcd73db36f1d934655e0dc
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-11-18 15:56:23 +0900

    feat(Validate, ErrorMessage): RetryInput 함수 추가

--- src/ErrorMessage.js
+++ src/ErrorMessage.js
@@ -1,7 +1,9 @@
 const NOTICE_MESSAGE = {
     BRIDGE_LENGTH_ERROR : "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.",
     BRIDGE_INPUT_ERROR : "[ERROR] 다리 길이는 숫자만 입력해야 합니다.",
-    BRIDGE_MOVE_ERROR : "[ERROR] 입력은 U 또는 D만 가능합니다."
+    BRIDGE_MOVE_ERROR : "[ERROR] 입력은 U 또는 D만 가능합니다.",
+    BRIDGE_RETRY_ERROR : "[ERROR] 입력은 R 또는 Q만 가능합니다."
+
 };
 
 module.exports = NOTICE_MESSAGE;
--- src/Validate.js
+++ src/Validate.js
@@ -12,6 +12,11 @@ const Validate = {
         if(input != "U" && input != "D"){
             throw Error.BRIDGE_MOVE_ERROR
         }
+    },
+    RetryInput(input){
+        if(input != "R" && input != "Q"){
+            throw Error.BRIDGE_RETRY_ERROR
+        }
     }
   };
   
