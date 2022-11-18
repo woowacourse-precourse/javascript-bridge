b22e63680a80d8f54807dc6091fdfc677bf37426
saejinpark <saejinpark951210@gmail.com>
2022-11-18 22:06:31 +0900

    move, insertMoveMap

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,20 +1,43 @@
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
- class BridgeGame {
+class BridgeGame {
   constructor() {
     this.bridge = null;
+    this.moveMap = new Map([
+      ["U", ""],
+      ["D", ""],
+    ]);
   }
 
   setBridge(bridge) {
     this.bridge = bridge;
   }
+
+  insertMoveMap(square, nowIndex) {
+    const match = this.bridge[nowIndex] === square ? "O" : "X";
+    this.moveMap.set(square, this.moveMap.get(square) + match);
+    const other = square === "U" ? "D" : "U";
+    this.moveMap.set(other, this.moveMap.get(other) + " ");
+  }
+  
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(square) {
+    const nowIndex = this.moveMap.get("U").length;
+    this.insertMoveMap(square, nowIndex);
+    printMap(this.moveMap);
+    if (
+      this.moveMap.get("U")[nowIndex] === "X" ||
+      this.moveMap.get("D")[nowIndex] === "X"
+    )
+      readGameCommand(this);
+    else if (this.bridge.length === nowIndex + 1) printResult(this, true);
+    else readMoving(this);
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
