9781e9917efc56030fffdafe78747d9ad735bfb8
AhnGeunTaek <dksrmsxor@gmail.com>
2022-11-18 23:41:34 +0900

    feat: 입력한 다리 길이의 범위를 검증하는 기능 추가

--- src/Validation.js
+++ src/Validation.js
@@ -1,11 +1,20 @@
 const Validation = {
   validateBridgeSize(bridgeSize) {
     this.validateIsNumber(bridgeSize);
+    this.validateIsRange(bridgeSize);
   },
 
   validateIsNumber(bridgeSize) {
     if (isNaN(bridgeSize)) {
       throw new Error('[ERROR] 다리의 길이는 숫자만 입력해야 합니다.');
+    }
+  },
+
+  validateIsRange(bridgeSize) {
+    const MIN_SIZE = 3;
+    const MAX_SIZE = 20;
+    if (bridgeSize < MIN_SIZE || bridgeSize > MAX_SIZE) {
+      throw new Error('[ERROR] 다리의 길이는 3 이상 20 이하여야 합니다.');
     }
   },
 };
