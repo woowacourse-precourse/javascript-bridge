e9230f1528b270dd36cce78b2d646e00994cd3fc
Sijun <sionwer4@naver.com>
2022-11-18 18:05:15 +0900

    feat: bridge game move function add

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,3 +1,4 @@
+const InputView = require("./InputView");
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
@@ -7,14 +8,25 @@ class BridgeGame {
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(bridge) {
+    for(let i = 0; i < bridge.length; i++){
+      const moving = InputView.readMoving();
+      const result = this.oneMoveCheck(moving, bridge[i]);
+      if(!result) this.retry();
+    }
+  }
 
+  oneMoveCheck(moving, space){
+    if(moving === space) return true;
+    return false;
+  }
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  retry() {}
+  retry() {
+  }
 }
 
 module.exports = BridgeGame;
