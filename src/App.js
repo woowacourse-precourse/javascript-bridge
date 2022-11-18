7751732134ba479a4807307fb14bdd247420571c
Sukyeong Park <zztnrudzz13@naver.com>
2022-11-18 16:45:20 +0900

    fix(App): remove bridgesize variable

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
 const { readBridgeSize } = require('./InputView');
 
 class App {
-  #bridgeSize;
   play() {
     readBridgeSize((size) => {
-      this.#bridgeSize = size;
       const bridge = makeBridge(size, BridgeRandomNumberGenerator.generate);
       const bridgeGame = new BridgeGame(bridge);
       bridgeGame.start();
