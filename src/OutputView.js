39f85c6deb289b2228faf2115f996a869c39fe37
vinivin153 <vinivin153@naver.com>
2022-11-19 02:20:55 +0900

    feat: (printError) 에러 출력 하는 기능 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,3 +1,4 @@
+const { Console } = require('@woowacourse/mission-utils');
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -15,6 +16,10 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printResult() {},
+
+  printError(e) {
+    Console.print(e);
+  },
 };
 
 module.exports = OutputView;
