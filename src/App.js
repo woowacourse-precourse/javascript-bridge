5246922135242a9f85410b9f079cc3fe7a12da2d
millifail <hwangbird99@gmail.com>
2022-11-18 15:51:45 +0900

    feat(bridge-generator):input length
    - 다리 길이를 입력받고 길이가 3~20의 숫자가 아니라면 예외처리한다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const inputVIew = require('./InputView');
+
 class App {
-  play() {}
+  play() {
+    MissionUtils.Console.print('다리 건너기 게임을 시작합니다.');
+    const BRIDGE_SIZE = inputVIew.readBridgeSize();
+    console.log(BRIDGE_SIZE);
+  }
 }
 
 module.exports = App;
--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,4 @@
+const MissionUtils = require('@woowacourse/mission-utils');
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,7 +6,19 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    let length;
+    MissionUtils.Console.readLine(
+      '다리의 길이를 입력해주세요.',
+      (length_answer) => {
+        length = Number(length_answer);
+      }
+    );
+    if (length < 3 || length > 20) {
+      throw new Error('[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.');
+    }
+    return length;
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
