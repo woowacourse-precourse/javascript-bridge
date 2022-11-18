8821f3d1cd6521992fb15391d0e569c2583bf0d3
jinseo <ghkdwlstj99@gmail.com>
2022-11-19 00:14:37 +0900

    refactor: printStart 로직 수정

--- src/OutputView.js
+++ src/OutputView.js
@@ -5,7 +5,7 @@ const { Console } = require("@woowacourse/mission-utils");
  const OutputView = {
 
   printStart(){
-    return Console.print("다리 건너기 게임을 시작합니다.");
+    Console.print("다리 건너기 게임을 시작합니다.");
   },
   /**
    * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
