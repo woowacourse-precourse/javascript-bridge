3cbf804887956b99cdcbf8a4c34d8f55e5aa55b0
GunHoi <lgh509@kyonggi.ac.kr>
2022-11-18 23:57:44 +0900

    feat - 1 : 입력 받은 다리길이 예외처리(범위를 벗어난 경우)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 const OutputView = require("./OutputView.js");
 const InputView = require("./InputView.js");
+const ValidCheck = require("./ValidCheck.js");
 class App {
   play() {
     OutputView.printGameStart();
     const BRIDGE_SIZE = InputView.readBridgeSize();
+    ValidCheck.bridgeIsInRange(BRIDGE_SIZE);
   }
 }
 
---
+++ src/ValidCheck.js
@@ -0,0 +1,14 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const ValidCheck = {
+  bridgeIsInRange(bridgeSize){
+    try{
+      if (bridgeSize<3 || bridgeSize>20) {
+        throw new Error();
+      }
+    }catch(e){
+      MissionUtils.Console.print("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+    }
+  }
+};
+
+module.exports = ValidCheck;
