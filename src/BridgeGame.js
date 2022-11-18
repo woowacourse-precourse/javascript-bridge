e3d515f6542d79a38f744483fb7a037e5ff48330
dudgp128 <20181048@sungshin.ac.kr>
2022-11-18 17:51:06 +0900

    FEAT(BridgeGame.getGameState) : 게임 상태값 반환하기 ( O, X, 성공) 클래스 내 gameState 필드 추가

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -6,12 +6,14 @@ class BridgeGame {
   #currentIdx;
   #bridgeMap;
   #retryCount;
+  #gameState;
 
   constructor(bridge) {
     this.#bridge = bridge;
     this.#currentIdx = 0;
     this.#bridgeMap = [[], []]; // 0 ≒ D , 1 ≒ U
     this.#retryCount = 1;
+    this.#gameState = "";
   }
 
   transInputtoPosition(input) {
@@ -38,15 +40,16 @@ class BridgeGame {
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   move() {
-    if (!this.#currentIdx == this.#bridge.length) {
+    if (this.#currentIdx === this.#bridge.length)
+      return (this.#gameState = "성공");
+    else {
       this.#currentIdx += 1;
-      return "O";
+      return (this.#gameState = "O");
     }
-    return "성공";
   }
 
   stop() {
-    return "X";
+    return (this.#gameState = "X");
   }
 
   getBridgeMap() {
@@ -55,6 +58,10 @@ class BridgeGame {
 
   getRetryCount() {
     return this.#retryCount;
+  }
+
+  getGameState() {
+    return this.#gameState;
   }
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
