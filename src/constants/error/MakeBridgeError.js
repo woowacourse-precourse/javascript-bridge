72179dacb84b7c934b1d4acf0ba69fed007279e1
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-11-18 17:56:26 +0900

    feat($MakeBridgeError): 다리의 길이를 입력받을 때 예외사항 문구 상수 분리

---
+++ src/constants/error/MakeBridgeError.js
@@ -0,0 +1,11 @@
+const IS_LENGTH_NUMBER = '[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.';
+const IS_LENGTH_NOT_NUMBER = '[ERROR] 다리 길이는 숫자여야 합니다.';
+const IS_LENGTH_EMPTY = '[ERROR] 다리 길이는 입력해주세요.';
+const IS_LENGTH_IN_RANGE = '[ERROR] 다리의 길이는 3이상 20이하입니다.';
+
+module.exports = {
+  IS_LENGTH_NUMBER,
+  IS_LENGTH_NOT_NUMBER,
+  IS_LENGTH_EMPTY,
+  IS_LENGTH_IN_RANGE
+};
