e6c8f1bf2011436e1034c1ec45399b42b82a8dcb
ReturnReason <reasonz@daum.net>
2022-11-19 04:34:38 +0900

    feat: 사용자로부터 이동하고자 하는 칸을 입력 받는 메서드 추가

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,5 +1,6 @@
 const { INPUT_MESSAGES } = require('../constants/messages');
 const { readLine } = require('../utils/missionUtil');
+const BridgeGame = require('../domain/BridgeGame');
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -11,7 +12,8 @@ const InputView = {
   readBridgeSize() {
     const { bridgeSize } = INPUT_MESSAGES;
     const getUserInput = (answer) => {
-      console.log(answer);
+      const bridgeGame = new BridgeGame(answer);
+      this.readMoving(bridgeGame);
     };
     readLine(bridgeSize, getUserInput);
   },
@@ -19,7 +21,13 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving(bridgeGame) {
+    const { moveSpace } = INPUT_MESSAGES;
+    const getUserInput = (answer) => {
+      bridgeGame.move(answer);
+    };
+    readLine(moveSpace, getUserInput);
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
