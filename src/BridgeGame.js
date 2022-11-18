0734485e75105873a2e8c897625c64e3f3e2b451
jiho1996 <jhp9270@naver.com>
2022-11-18 16:05:40 +0900

    refactor : 다리 생성 기능 분리해뒀던 것을 game모듈에 통합

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -16,20 +16,26 @@ class BridgeGame {
 
   makeBridge(bridgeSize) {
     this.#validPath = BridgeMaker.makeBridge(bridgeSize);
+    // 유효성 검사
   }
 
+  getMoveDirectionFromUser() {
+    InputView.readMoving();
+  }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
+  move(validState) {
+    // 위쪽이고 옳은 길 이면? 틀린 길 이면? 아래쪽이고 옳은 길이면? 틀린 길 이면?'
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
   retry() {}
 }
 
