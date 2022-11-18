d6ac14071c44c89831b2f6a9ad172ea93d273661
saejinpark <saejinpark951210@gmail.com>
2022-11-18 21:58:54 +0900

    readBridgeSize, readBridgeSizeReadLineHandler

--- src/InputView.js
+++ src/InputView.js
@@ -1,11 +1,35 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const BridgeMaker = require("./BridgeMaker");
+const wConsole = MissionUtils.Console;
+const { makeBridge } = BridgeMaker;
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+const { generate } = BridgeRandomNumberGenerator;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
-const InputView = {
+ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSizeReadLineHandler(line, bridgeGame) {
+    try {
+      const size = parseInt(line);
+      if (!/^\d+$/.test(line)|| isNaN(line) || size < 3 || 20 < size)
+        throw new Error("[ERROR] 입력은  3부터 20 사이 숫자여야 합니다.");
+      bridgeGame.setBridge(makeBridge(size, generate));
+      InputView.readMoving(bridgeGame);
+    } catch (error) {
+      wConsole.print(error);
+      InputView.readBridgeSize(bridgeGame);
+    }
+  },
+
+  readBridgeSize(bridgeGame) {
+    wConsole.readLine("다리의 길이를 입력해주세요.\n", (line) => {
+      InputView.readBridgeSizeReadLineHandler(line, bridgeGame);
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
