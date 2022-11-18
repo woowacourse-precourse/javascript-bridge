bb8db510fdbe2afc2608c91f8328ea91b305bf1d
pangkyu <baesense@naver.com>
2022-11-19 08:50:10 +0900

    Feat : 게임 시작 문구를 출력하는 기능

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,7 +1,13 @@
+const { Console } = require("@woowacourse/mission-utils");
+const { MESSAGE } = require("./constants/Constant");
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 const OutputView = {
+  printStart() {
+    Console.print(MESSAGE.BRIDGE_GAME_START);
+  },
   /**
    * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
    * <p>
