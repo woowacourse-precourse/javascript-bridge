6db29621c1fd79f307644af067aa7a45420d948f
hahagarden <88613455+hahagarden@users.noreply.github.com>
2022-11-18 21:57:43 +0900

    feat: 게임 시도한 횟수 추가하여 저장하는 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 class App {
+  #bridgeGame;
+  #playCount;
+  constructor() {
+    this.#bridgeGame = null;
+    this.#playCount = 0;
+  }
+
   play() {}
+
+  increasePlayCount() {
+    this.#playCount++;
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
