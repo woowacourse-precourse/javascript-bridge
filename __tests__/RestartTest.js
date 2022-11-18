5e3fdbb7c902c60ed93f275db8b4f0ebf3bb86e2
이건우 <lifeflavor@igeon-uui-MacBookPro.local>
2022-11-18 21:39:01 +0900

    feat(bridge)- @단위테스트 : 재시작 후 다리의 길이 확인

--- __tests__/RestartTest.js
+++ __tests__/RestartTest.js
@@ -15,4 +15,13 @@ describe("R or Q 가 입력 받았을 경우 확인", () => {
     app.restart(false);
     expect(app.tryGame).toBe(tryGame);
   });
+  test(" 재식작 size 동일 테스트 ", () => {
+    const app = new App();
+    app.size = 3;
+    app.play();
+    const size1 = app.size;
+    app.restart(true);
+    const size2 = app.size;
+    expect(size1).toBe(size2);
+  });
 });
