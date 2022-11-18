4525b600933ad2cd5f4c76f96e117d57a7ce00bd
YMS <yms1789@naver.com>
2022-11-18 19:34:01 +0900

    Feat: 다리를 건넌만큼 출력하는 기능 추가

--- src/InputView.js
+++ src/InputView.js
@@ -1,17 +1,20 @@
-const { OutputView } = require("./OutputView");
+const OutputView = require("./OutputView");
 const { Console } = require("@woowacourse/mission-utils");
 const { MESSAGE } = require("./constants");
 const Validation = require("./Validation");
 const BridgeMaker = require("./BridgeMaker");
 const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
 const BridgeGame = require("./BridgeGame");
+const Controller = require("./Controller");
+
+let callMove = 0;
+
+const validation = new Validation();
+const brigeGame = new BridgeGame();
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
-let callMove = 0;
-const validation = new Validation();
-const brigeGame = new BridgeGame();
 
 const InputView = {
   /**
@@ -23,7 +26,13 @@ const InputView = {
     Console.readLine(MESSAGE.INPUT_BRIDGE_LENGTH_MESSAGE, (bridgeLength) => {
       try {
         validation.validateBridgeLength(bridgeLength);
-        this.readMoving(bridgeLength);
+        const bridge = BridgeMaker.makeBridge(
+          bridgeLength,
+          BridgeRandomNumberGenerator.generate
+        );
+
+        console.log(bridge);
+        this.readMoving(bridge);
       } catch (error) {
         Console.print(error);
         this.readBridgeSize();
@@ -34,31 +43,44 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving(bridgeLength) {
-    const bridge = BridgeMaker.makeBridge(
-      bridgeLength,
-      BridgeRandomNumberGenerator.generate
-    );
+  readMoving(bridge) {
     Console.readLine(MESSAGE.INPUT_MOVE_MESSAGE, (inputMove) => {
       try {
         validation.validateMove(inputMove);
-        [callMove, moveResult] = brigeGame.move(inputMove, callMove, bridge);
-        moveResult === "X"
-          ? this.readGameCommand()
-          : callMove === bridgeLength
-          ? OutputView.printResult()
-          : this.readMoving(bridgeLength);
+        [callMove, moveResult, inputMoves] = brigeGame.move(
+          inputMove,
+          callMove,
+          bridge
+        );
+        console.log(callMove, moveResult);
+        OutputView.printMap(inputMoves, callMove, moveResult);
+        this.checkMove(callMove, moveResult, bridge);
       } catch (error) {
         Console.print(error);
-        this.readMoving(bridgeLength);
+        this.readMoving(bridge);
       }
     });
+  },
+
+  checkMove(callMove, moveResult, bridge) {
+    if (callMove === bridge.length) {
+      OutputView.printResult();
+    }
+    if (moveResult[callMove - 1] === "X") {
+      this.readGameCommand();
+    }
+    if (moveResult[callMove - 1] === "O" && callMove < bridge.length) {
+      this.readMoving(bridge);
+    }
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+    Console.readLine(MESSAGE.INPUT_RESTART_MESSAGE, (input) => {
+    });
+  },
 };
 
 module.exports = InputView;
--- src/OutputView.js
+++ src/OutputView.js
@@ -1,3 +1,5 @@
+const { Console } = require("@woowacourse/mission-utils");
+const { MESSAGE } = require("./constants");
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -7,7 +9,41 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {},
+  printMap(inputMoves, callMove, moveResult) {
+    callMove <= 1
+      ? this.makeMap(inputMoves, moveResult)
+      : this.makeMaps(inputMoves, callMove, moveResult);
+  },
+  makeMap(inputMoves, moveResult) {
+    let upBridge = "[ ";
+    let downBridge = "[ ";
+    if (inputMoves[0] === "U") {
+      upBridge += moveResult[0] + " ]";
+      downBridge += "  ]";
+    }
+    if (inputMoves[0] === "D") {
+      downBridge += moveResult[0] + " ]";
+      upBridge += "  ]";
+    }
+    Console.print(`${upBridge}\n${downBridge}`);
+  },
+  makeMaps(inputMoves, callMove, moveResult) {
+    let upBridge = [];
+    let downBridge = [];
+    for (let i = 0; i < callMove; i++) {
+      if (inputMoves[i] === "U") {
+        upBridge.push(moveResult[i]);
+        downBridge.push(" ");
+      }
+      if (inputMoves[i] === "D") {
+        downBridge.push(moveResult[i]);
+        upBridge.push(" ");
+      }
+    }
+    upBridge = upBridge.join(" | ");
+    downBridge = downBridge.join(" | ");
+    Console.print(`${"[ " + upBridge + " ]"}\n${"[ " + downBridge + " ]"}`);
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
