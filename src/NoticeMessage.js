1484025a701828890cc33c1efb8e5d31af7866d6
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-11-18 15:16:34 +0900

    feat(NoticeMessage, OutputView): 넘어온 배열 print에 적합하게 string으로 저장 후 return 기능 구현

--- src/NoticeMessage.js
+++ src/NoticeMessage.js
@@ -1,7 +1,10 @@
 const NOTICE_MESSAGE = {
     START_GAME : "다리 건너기 게임을 시작합니다.\n",
     INPUT_BRIDGE_LENGTH : "다리의 길이를 입력해주세요.\n",
-    SELECT_MOVE : "이동할 칸을 선택해 주세요. (위: U, 아래: D)\n"
+    SELECT_MOVE : "이동할 칸을 선택해 주세요. (위: U, 아래: D)\n",
+    START_PRINT : "[ ",
+    NEXT_STEP : " | ",
+    END_PRINT : " ]\n"
    
 };
 
--- src/OutputView.js
+++ src/OutputView.js
@@ -1,14 +1,28 @@
-const BridgeGame = require("./BridgeGame.js")
+const MissionUtils = require("@woowacourse/mission-utils");
+const Notice = require("./NoticeMessage.js")
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 const OutputView = {
+  makePrintable(side){
+    let printSide = ""
+    printSide += Notice.START_PRINT
+    for(let i = 0; i<side.length; i++){
+      if (i == side.length-1){
+        printSide += side[i] + Notice.END_PRINT
+        return printSide
+      }
+      printSide += side[i] + Notice.NEXT_STEP
+    }
+  },
   /**
    * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printMap(upside, downside) {
+    const printupside = this.makePrintable(upside)
+    const printdownside = this.makePrintable(downside)
   },
 
   /**
