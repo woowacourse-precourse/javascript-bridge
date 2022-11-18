2682da3aac8971911b16e37c987f02f51e6d1fb9
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2022-11-18 22:45:05 +0900

    feat: 다리 길이를 입력받기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 const { Console } = require("@woowacourse/mission-utils");
+const InputView = require("./InputView");
 
 class App {
   play() {
@@ -7,6 +8,7 @@ class App {
 
   start() {
     Console.print("다리 건너기 게임을 시작합니다.\n");
+    InputView.readBridgeSize();
   }
 }
 
