1285bcd5bc278d02820de55bb40b908d0413596e
sungjihyun <jhsung23@gmail.com>
2022-11-18 09:33:57 +0900

    feat: 다리의 길이를 입력받는 기능

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,11 @@
+/*
+ 제공된 InputView 객체를 활용해 구현해야 한다.
+ InputView의 `파일 경로`는 변경할 수 있다. 🙆‍♂️
+ InputView의 메서드의 `인자`는 변경할 수 있다. 🙆‍♂️
+ 사용자 값 입력을 위해 필요한 `메서드`를 추가할 수 있다. 🙆‍♂️
+ */
+const { Console } = require('@woowacourse/mission-utils');
+const { READ_BRIDGE_SIZE } = require('./utils/constants');
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,7 +13,9 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine(READ_BRIDGE_SIZE, () => {});
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,11 @@
 const GAME_START = '다리 건너기 게임을 시작합니다.';
+const READ_BRIDGE_SIZE = '다리의 길이를 입력해주세요.';
 const READ_MOVING = '이동할 칸을 선택해주세요. (위: U, 아래: D)';
 const READ_GAME_COMMAND = '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)';
 
 module.exports = {
   GAME_START,
+  READ_BRIDGE_SIZE,
   READ_MOVING,
   READ_GAME_COMMAND,
 };
