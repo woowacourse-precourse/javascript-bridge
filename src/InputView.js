207094dfcc5407d7aa9b1246b0f0eadea87f6a70
Jeong Hun Nam <njh3421@gmail.com>
2022-11-18 11:34:40 +0900

    feat : add readMoving function

--- src/InputView.js
+++ src/InputView.js
@@ -21,7 +21,9 @@ const InputView = {
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving(bridgeLength) {
-    
+    MissionUtils.Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)\n',(userSpace)=>{
+      return userSpace
+    })
   },
 
   /**
