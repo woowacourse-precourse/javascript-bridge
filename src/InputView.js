25b77979786a32623f2925ffd035b9a61104f556
minjaeKim <smallkdb@gmail.com>
2022-11-18 17:38:39 +0900

    feat(InputView): 입력 값을 받는 InputView의 메서드 구현
    - 다리 길이를 입력받는 readBridge
    - 사용자가 이동하는 칸을 입력받는 readMoving
    - 사용자 게임 재시도 여부를 입력받는 readCommand

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,4 @@
+const { Console } = require('@woowacourse/mission-utils');
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,17 +6,25 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize(callback) {
+    Console.readLine('다리의 길이를 입력해주세요.\n', (size) => callback(size));
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving(callback) {
+    Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)\n', (square) => callback(square));
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand(callback) {
+    Console.readLine('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)', (input) =>
+      callback(input)
+    );
+  },
 };
 
 module.exports = InputView;
