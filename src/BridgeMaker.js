2ec140446ad72a692e5452ac407d5d76c8d60259
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-18 17:46:39 +0900

    fix: 다리 생성 함수 논리 오류 수정

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,6 +1,8 @@
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
+const BRIDGE = require('./constants/constants');
+
 const BridgeMaker = {
   /**
    * @param {number} size 다리의 길이
@@ -8,16 +10,12 @@ const BridgeMaker = {
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
   makeBridge(size, generateRandomNumber) {
-    const upper = this.makeUpper(size, generateRandomNumber);
-    const lower = this.makeLower(upper);
-
-    return [upper, lower];
-  },
-  makeUpper(size, generateRandomNumber) {
-    return Array(size).fill(0).map(generateRandomNumber);
-  },
-  makeLower(upper) {
-    return upper.map((piece) => (piece + 1) % 2);
+    return Array(size)
+      .fill(0)
+      .map(() => {
+        const random = generateRandomNumber();
+        return random === 0 ? BRIDGE.DOWN_CHAR : BRIDGE.UP_CHAR;
+      });
   },
 };
 
