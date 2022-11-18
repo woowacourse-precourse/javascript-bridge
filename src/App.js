e7b15251e0bf8df8206981fc2bc47da5dcfa9fd2
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2022-11-19 02:06:35 +0900

    feat(App) : bridge move까지 app에서 main 로직 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const InputView = require("./InputView");
+const OutputView = require("./OutputView");
+const BridgeGame = require("./BridgeGame");
 const {generate} = require("./BridgeRandomNumberGenerator");
 const {makeBridge} = require("./BridgeMaker");
 const [Console, Random] = [MissionUtils.Console, MissionUtils.Random];
@@ -14,19 +16,28 @@ class App {
   constructor(){
     this.#bridgeSize = 0;
     this.#bridge = [];
-    this.#move = "s";
+    this.#move = "";
   }
   
   async play() {
-
     InputView.gameStart();
     this.#bridgeSize = await InputView.readBridgeSize();
     this.#bridge = makeBridge(this.#bridgeSize, generate);
     Console.print(this.#bridge);
-    this.#move = await InputView.readMoving();
-
+    await this.moveBridge();
   }
 
+  async moveBridge(){
+    const bridgeGame = new BridgeGame();
+    while((bridgeGame.getIdx()<this.#bridgeSize) && (bridgeGame.getFlag())){
+      this.#move = await InputView.readMoving();
+      bridgeGame.move(this.#move, this.#bridge);
+      
+
+
+
+    }
+  }
 }
 
 const app = new App();
