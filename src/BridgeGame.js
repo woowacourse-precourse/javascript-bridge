0dd96457349b9e69fa2ba1e35c2112660a6fae95
Yebin Kim <kyb4312@gmail.com>
2022-11-18 22:56:06 +0900

    feat(BridgeGame): implement a function for retrying

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -79,6 +79,16 @@ class BridgeGame {
     this.#bridgeMap.D.pop();
     this.#stepCount -= 1;
   }
+
+  /**
+   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
+   * <p>
+   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+   */
+  retry() {
+    this.#status.numberOfAttempts += 1;
+    this.#status.isFinished = false;
+  }
 }
 
 module.exports = BridgeGame;
