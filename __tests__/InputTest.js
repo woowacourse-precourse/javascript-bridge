cbddc95be8956c5f8578e352c298248bb47e4d55
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-18 17:35:47 +0900

    style: double Quote -> single Quote

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -1,34 +1,34 @@
-const InputView = require("../src/InputView");
-const ERROR_MESSAGES = require("../src/constants/ErrorMessages");
+const InputView = require('../src/InputView');
+const ERROR_MESSAGES = require('../src/constants/ErrorMessages');
 
-describe("입력값 테스트", () => {
-  test("다리 길이 입력값 테스트 ( 문자 )", () => {
+describe('입력값 테스트', () => {
+  test('다리 길이 입력값 테스트 ( 문자 )', () => {
     expect(() => {
-      InputView.validateBridgeSize("a");
+      InputView.validateBridgeSize('a');
     }).toThrow(ERROR_MESSAGES.NAN);
   });
 
-  test("다리 길이 입력값 테스트 ( 특수문자 )", () => {
+  test('다리 길이 입력값 테스트 ( 특수문자 )', () => {
     expect(() => {
-      InputView.validateBridgeSize("@");
+      InputView.validateBridgeSize('@');
     }).toThrow(ERROR_MESSAGES.NAN);
   });
 
-  test("다리 길이 범위 테스트 (*3 ~ 20)", () => {
+  test('다리 길이 범위 테스트 (*3 ~ 20)', () => {
     expect(() => {
-      InputView.validateBridgeSize("2");
+      InputView.validateBridgeSize('2');
     }).toThrow(ERROR_MESSAGES.SIZE);
   });
 
-  test("다리 길이 범위 테스트 (*3 ~ 20)", () => {
+  test('다리 길이 범위 테스트 (*3 ~ 20)', () => {
     expect(() => {
-      InputView.validateBridgeSize("21");
+      InputView.validateBridgeSize('21');
     }).toThrow(ERROR_MESSAGES.SIZE);
   });
 
-  test("다리 길이 범위 테스트 (*3 ~ 20)", () => {
+  test('다리 길이 범위 테스트 (*3 ~ 20)', () => {
     expect(() => {
-      InputView.validateBridgeSize("3");
+      InputView.validateBridgeSize('3');
     }).toBeTruthy();
   });
 });
