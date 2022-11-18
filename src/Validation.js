57c4e91a286923578edfd326f9250ff5ca188dd1
kwh97 <zzzang0512@naver.com>
2022-11-18 15:03:47 +0900

    feat: 다리 길이 입력값이 숫자인지 체크하는 기능 구현

--- src/Validation.js
+++ src/Validation.js
@@ -1,13 +1,23 @@
 class Validation {
   static checkBridgeLength(bridgeLen) {
+
     if (Validation.isEmpty(bridgeLen)) {
       throw new Error("[ERROR] 잘못된 입력값 입니다.");
+    }
+
+    if (Validation.isNumber(bridgeLen)) {
+      throw new Error("[ERROR] 숫자가 아닌 값을 입력하였습니다. 3 이상 25 이하의 숫자를 입력해주세요.");
     }
   }
 
   static isEmpty(input) {
     return input.length === 0;
   }
+
+  static isNumber(input ) {
+    return isNaN(input);
+  }
 }
+
 
 module.exports = Validation;
