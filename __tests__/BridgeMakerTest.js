ee588fd5942c873d8f4f31957a92847a81723ca0
daal2quf <daal2quf@gmail.com>
2022-11-18 17:21:33 +0900

    test(bridgeMakerTest): randomNumberToUpAndDown() 작동 테스트
    add:
    afterEach(): 각각의 테스트 후에 console을 종료함
    test1: randomNumberToUpAndDown()의 argument로 0 이 들어왔을때 'D'로 반환
    test2: randomNumberToUpAndDown()의 argument로 0, 1  배열이 들어왔을때 배열'D', 'U'으로 반환

--- __tests__/BridgeMakerTest.js
+++ __tests__/BridgeMakerTest.js
@@ -1,7 +1,12 @@
-const BridgeMaker = require("../src/BridgeMaker");
+const { Console } = require("@woowacourse/mission-utils");
+const { BridgeMaker } = require("../src/BridgeMaker");
 const BridgeRandomNumberGenerator = require("../src/BridgeRandomNumberGenerator");
 
-describe('BridgeMaker 객체의 makeBridge 메서드 테스트', () => {
+afterEach(() => {
+  Console.close();
+});
+
+describe('BridgeMaker 객체의 makeBridge() 메서드 테스트', () => {
   test('입력한 사이즈 만큼의 다리 개수가 만들어지는지 확인', () => {
     const bridge = BridgeMaker.makeBridge(3, BridgeRandomNumberGenerator.generate);
     const bridgeLength = bridge.length;
@@ -11,5 +16,18 @@ describe('BridgeMaker 객체의 makeBridge 메서드 테스트', () => {
     const longBridge = BridgeMaker.makeBridge(15, BridgeRandomNumberGenerator.generate);
     const notDuplicated = Array.from([...new Set(longBridge)]);
     expect([["U", "D"], ["D", "U"]]).toContainEqual(notDuplicated);
+  });
+});
+
+describe('BridgeMaker 객체의 randomNumberToUpAndDown() 메서드 테스트', () => {
+  test('키값으로 0이 들어왔을떄 "D" 반환하는지확인', () => {
+    const randomNumber = 0;
+    const stringUOrD = BridgeMaker.randomNumberToUpAndDown(randomNumber);
+    expect(stringUOrD).toEqual('D');
+  });
+  test('키값으로 0이 들어왔을떄 "D", 1이 들어왔을때 "U"를 반환하는지 확인', () => {
+    const randomNumber = [0, 1];
+    const stringUOrD = randomNumber.map((x) => BridgeMaker.randomNumberToUpAndDown(x));
+    expect(stringUOrD).toEqual(['D', 'U']);
   });
 });
