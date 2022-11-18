f156c2e3034c99e3d89dec2d5ce974296192c637
saejinpark <saejinpark951210@gmail.com>
2022-11-18 22:03:46 +0900

    readMoving, readMovingReadLineHandler

--- src/InputView.js
+++ src/InputView.js
@@ -8,14 +8,14 @@ const { generate } = BridgeRandomNumberGenerator;
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
- const InputView = {
+const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
   readBridgeSizeReadLineHandler(line, bridgeGame) {
     try {
       const size = parseInt(line);
-      if (!/^\d+$/.test(line)|| isNaN(line) || size < 3 || 20 < size)
+      if (!/^\d+$/.test(line) || isNaN(line) || size < 3 || 20 < size)
         throw new Error("[ERROR] 입력은  3부터 20 사이 숫자여야 합니다.");
       bridgeGame.setBridge(makeBridge(size, generate));
       InputView.readMoving(bridgeGame);
@@ -34,7 +34,22 @@ const { generate } = BridgeRandomNumberGenerator;
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMovingReadLineHandler(line, bridgeGame) {
+    try {
+      if (["U", "D"].includes(line)) bridgeGame.move(line);
+      else throw new Error("[ERROR] 이동할 칸은 U또는 D입니다.");
+    } catch (error) {
+      wConsole.print(error);
+      InputView.readMoving(bridgeGame);
+    }
+  },
+
+  readMoving(bridgeGame) {
+    wConsole.readLine(
+      "\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
+      (line) => InputView.readMovingReadLineHandler(line, bridgeGame)
+    );
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
