d5af4f886dcec60f68d1a96080ab5228c5276cbd
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-11-18 16:11:15 +0900

    feat($InputView): 다리의 길이를 입력받는 메소드 구현

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,7 @@
+const { Console } = require('@woowacourse/mission-utils');
+const BridgeMaker = require('./BridgeMaker');
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,7 +9,11 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    return Console.readLine('다리의 길이를 입력해주세요.\n', (length) => {
+      BridgeMaker.makeBridge(length, BridgeRandomNumberGenerator.generate);
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
