c8c2e00043e825b42665843d3dfbb65e80e754d9
jiyujin <yujin1679@naver.com>
2022-11-18 17:16:13 +0900

    feat: ErrorMessages.js -> 에러 메세지 작성

---
+++ src/ErrorMessages.js
@@ -0,0 +1,9 @@
+const WRONG_INPUT_BRIDGE_SIZE = '[ERROR] 다리 길이는 3 이상 20 이하의 숫자를 입력해야 합니다.';
+const WRONG_INPUT_BRIDGE_MOVE = '[ERROR] 이동할 칸은 U 와 D 중 하나의 문자를 입력해야 합니다.';
+const WRONG_INPUT_GAME_OVER = '[ERROR] 게임 재시작/종료 여부는 R 과 Q 중 하나의 문자를 입력해야 합니다.';
+
+module.exports = {
+  WRONG_INPUT_BRIDGE_SIZE,
+  WRONG_INPUT_BRIDGE_MOVE,
+  WRONG_INPUT_GAME_OVER
+};
