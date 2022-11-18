d1efdf08eafc6b588d6f13f58a92c1b9ce1a8c60
jinseo <ghkdwlstj99@gmail.com>
2022-11-18 19:57:04 +0900

    feat: 재시작 / 종료 입력값에 따라 진행되는 기능 구현

--- src/InputView.js
+++ src/InputView.js
@@ -39,7 +39,15 @@ const InputView = {
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand() {
-    Console.readLine("게임을 다시 시도할지 여부를 입력해주세요. (재시도:R, 종료:Q)", (Value) => {
+    Console.readLine("게임을 다시 시도할지 여부를 입력해주세요. (재시도:R, 종료:Q)", (value) => {
+      if(value == 'R'){
+        bridgeGame.retry();
+        this.readBridgeSize();
+      }
+      if(value == 'Q'){
+        OutputView.printResult();
+        Console.close();
+      }
     })
   },
 };
