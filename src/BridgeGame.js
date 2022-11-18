eab35b5e59a8ec1ee8fefda1d5ea5dc8e79afc0f
Yebin Kim <kyb4312@gmail.com>
2022-11-18 19:08:13 +0900

    feat(BridgeGame): implement functions used when the player moves

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -29,6 +29,45 @@ class BridgeGame {
       isFinished: false,
     };
   }
+
+  /**
+   * 사용자가 칸을 이동할 때 사용하는 메서드
+   * <p>
+   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+   */
+  move(playerMoving) {
+    // TODO: validation of player moving
+    const isCorrect = this.isCorrectStep(playerMoving);
+    this.updateBridgeMap(playerMoving, isCorrect);
+    this.updateGameStatus(isCorrect);
+  }
+
+  isCorrectStep(playerMoving) {
+    if (this.#bridgeAnswer[this.#correctStep] === playerMoving) {
+      return true;
+    }
+    return false;
+  }
+
+  updateBridgeMap(playerMoving, isCorrect) {
+    if (isCorrect) {
+      this.#bridgeMap[playerMoving][this.#correctStep] = 'O';
+    } else {
+      this.#bridgeMap[playerMoving][this.#correctStep] = 'X';
+    }
+  }
+
+  updateGameStatus(isCorrect) {
+    if (isCorrect) {
+      this.#correctStep += 1;
+      if (this.#correctStep === this.#bridgeAnswer.length) {
+        this.#gameStatus.isSuccess = true;
+        this.#gameStatus.isFinished = true;
+      }
+    } else {
+      this.#gameStatus.isFinished = true;
+    }
+  }
 }
 
 module.exports = BridgeGame;
