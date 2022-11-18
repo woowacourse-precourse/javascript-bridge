377d8782885040d80f5fce3300278d5c74da6749
bassyu <yrkdgus@gmail.com>
2022-11-18 13:40:59 +0900

    feat(OutputView): printMap 구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,24 +1,45 @@
 const { Console } = require('@woowacourse/mission-utils');
 
 const OutputView = {
+  START_MESSAGE: '다리 건너기 게임을 시작합니다.',
   RESULT_MESSAGE: {
+    head: '\n최종 게임 결과',
     true: '성공',
     false: '실패',
   },
+  MAP_RESULT: {
+    true: 'O',
+    false: 'X',
+  },
 
   printStart() {
-    Console.print('다리 건너기 게임을 시작합니다.\n');
+    Console.print(OutputView.START_MESSAGE);
   },
 
-  printMap(map) {
-    Console.print(map);
+  printMap(moveList) {
+    OutputView.printLocalMap(moveList, 'U');
+    OutputView.printLocalMap(moveList, 'D');
   },
 
-  printResult(map, tryCount) {
-    const result = map[map.length - 1].result;
-    Console.print('최종 게임 결과');
-    OutputView.printMap(map);
-    Console.print(`\n게임 성공 여부: ${OutputView.RESULT_MESSAGE[result]}`);
+  printLocalMap(moveList, location) {
+    const map = [];
+    moveList.forEach((move) => {
+      if (move.moving === location) {
+        map.push(OutputView.MAP_RESULT[move.result]);
+      } else {
+        map.push(' ');
+      }
+    });
+
+    Console.print(`[ ${map.join(' | ')} ]`);
+  },
+
+  printResult(moveList, tryCount) {
+    const lastMoveResult = moveList[moveList.length - 1].result;
+
+    Console.print(OutputView.RESULT_MESSAGE.head);
+    OutputView.printMap(moveList);
+    Console.print(`\n게임 성공 여부: ${OutputView.RESULT_MESSAGE[lastMoveResult]}`);
     Console.print(`총 시도한 횟수: ${tryCount}`);
     Console.close();
   },
