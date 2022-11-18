47ae66f3eb321a6367487d9f2848d98b775bf326
MyungAe <ladiescheong@naver.com>
2022-11-18 14:44:06 +0900

    feat(OutputView): 맵 출력과 결과 출력, 게임 시작 등을 알 수 있도록 임시 코드 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -9,14 +9,19 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {},
+  printMap(selectBridge, isUserDie) {
+    console.log(selectBridge, isUserDie);
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() {},
+  printResult() {
+    console.log("game is end");
+    MissionUtils.Console.close();
+  },
 
   /**
    *
