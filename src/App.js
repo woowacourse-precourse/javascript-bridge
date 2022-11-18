a9344ccfbe6114e9bd00a5378aff9db6f83bec9b
dladncks1217 <dlaxodud1217@gmail.com>
2022-11-19 02:56:30 +0900

    refactor(App): input 비동기처리 문제로 인한 코드 전체 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-const Game = require('./Game');
+const { Console } = require('@woowacourse/mission-utils');
+const BridgeGame = require('./BridgeGame');
+const BridgeMaker = require('./BridgeMaker');
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
+const { GAME_MESSAGE } = require('./constants/constants');
+const makePrintBridge = require('./utils/makePrintBridge');
+const { printResult, printMap } = require('./UI/OutputView');
+const {
+  readBridgeSize,
+  readMoving,
+  readGameCommand,
+} = require('./UI/InputView');
+const InputView = require('./UI/InputView');
+const OutputView = require('./UI/OutputView');
+
+function check(input) {
+  return input;
+}
 
 class App {
-  constructor() {
-    this.game = new Game();
-  }
+  #myBridge;
+  #myBridgeLength;
+
+  constructor() {}
+
   play() {
-    this.game.start();
+    OutputView.gameStart();
+    this.getBridgeLength();
+  }
+
+  getBridgeLength() {
+    InputView.readBridgeSize((input) => {
+      this.#myBridgeLength = input;
+      Console.print(this.#myBridgeLength);
+
+      return this.makeMyBridge();
+    });
+  }
+
+  makeMyBridge() {
+    // 다리 객체 생성! -> 다리만드는함수, 랜덤숫자넣어주는함수, 다리 길이 넣어주기
+    this.#myBridge = new BridgeGame(
+      BridgeRandomNumberGenerator.generate,
+      BridgeMaker.makeBridge,
+      this.#myBridgeLength
+    );
   }
 }
 
