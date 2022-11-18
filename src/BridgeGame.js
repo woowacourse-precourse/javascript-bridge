61e7473ad69d3e9389a335d71645f2689fc1384b
MAGHC <windowsapple95@gmail.com>
2022-11-18 15:43:51 +0900

    feat: BridgeGame 다리길이를 저장하고 늘리는 기능 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -2,7 +2,7 @@
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
-  #nowBridgeLength;
+  #bridgeLengthStatus = 0;
 
   #numberOftry = 0;
 
@@ -11,13 +11,17 @@ class BridgeGame {
     console.log(this.#numberOftry);
   }
 
+  incrementBridgeLengthStatus() {
+    this.#bridgeLengthStatus += 1;
+    console.log(this.#bridgeLengthStatus, '브릿지스테이터스');
+  }
+
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   move() {}
-
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
    * <p>
@@ -25,9 +29,5 @@ class BridgeGame {
    */
   retry() {}
 }
-
-const app = new BridgeGame();
-
-console.log(app.incrementNumberOfTry());
 
 module.exports = BridgeGame;
