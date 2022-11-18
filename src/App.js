30a7ea5ad0484d87da212272540a94d78ac1c46f
HBSPS <wiyeong79@naver.com>
2022-11-18 17:45:55 +0900

    feat: 사용자 입력 테스트용 코드 작성
    call 방식으로 연쇄 작용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+const InputView = require('./InputView');
+
 class App {
-  play() {}
+  play() {
+    InputView.readBridgeSize(this.func1, this.func2, this.func3);
+  }
+
+  func1(input) {
+    console.log(input);
+  }
+
+  func2(input) {
+    console.log(input);
+  }
+
+  func3(input) {
+    console.log(input);
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,5 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,17 +7,36 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize(func1, func2, func3) {
+    MissionUtils.Console.readLine('test1', (input) => {
+      func1.call(this, input);
+      this.readMoving(func2, func3);
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving(func2, func3) {
+    MissionUtils.Console.readLine('test2', (input) => {
+      func2.call(this, input);
+      this.readGameCommand(func3);
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand(func3) {
+    MissionUtils.Console.readLine('test3', (input) => {
+      func3.call(this, input);
+      this.gameEnd();
+    });
+  },
+
+  gameEnd() {
+    MissionUtils.Console.close();
+  },
 };
 
 module.exports = InputView;
