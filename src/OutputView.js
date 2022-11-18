e3eeb42c307d7e8ff355723f58c73a1708a0e4c1
Yebin Kim <kyb4312@gmail.com>
2022-11-18 17:40:12 +0900

    feat(OutputView): implement a funtion to print the result of a game

--- src/OutputView.js
+++ src/OutputView.js
@@ -27,6 +27,19 @@ const OutputView = {
     mapString = `${mapString.slice(0, -1)}]`;
     return mapString;
   },
+
+  /**
+   * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
+   * <p>
+   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+   */
+  printResult({ upperRow, lowerRow }, isSuccess, numberOfAttempts) {
+    Console.print('최종 게임 결과');
+    this.printMap({ upperRow, lowerRow });
+    Console.print('');
+    Console.print(`게임 성공 여부: ${isSuccess}`);
+    Console.print(`총 시도한 횟수: ${numberOfAttempts}`);
+  },
 };
 
 module.exports = OutputView;
