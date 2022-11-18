64b2495bce7361177d062131a025d06c5bd17b7c
SWARVY <swarvy0826@naver.com>
2022-11-19 01:37:45 +0900

    feat : App.js 시작기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+const BridgeGame = require('./BridgeGame');
+
 class App {
-  play() {}
+  play() {
+    const bridgeGame = new BridgeGame();
+  }
 }
 
+const app = new App();
+app.play();
 module.exports = App;
