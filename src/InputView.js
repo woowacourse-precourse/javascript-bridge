97f4346a2b9c2372697983d3c64fab78bc2bb487
saram <yujun1018@naver.com>
2022-11-18 15:43:29 +0900

    feat: 이동 칸 입력 기능

--- src/InputView.js
+++ src/InputView.js
@@ -1,24 +1,21 @@
 const { Console } = require('@woowacourse/mission-utils');
+const { GAME } = require('./phrases');
+const BridgeGame = require('./BridgeGame');
 const BridgeMaker = require('./BridgeMaker');
 const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
-const { GAME } = require('./phrases');
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+
 const InputView = {
-  /**
-   * 다리의 길이를 입력받는다.
-   */
   readBridgeSize() {
     Console.readLine(GAME.BRIDGE_SIZE, (input) =>
       BridgeMaker.makeBridge(Number(input), BridgeRandomNumberGenerator.generate())
     );
   },
 
-  /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
-  readMoving() {},
+  readMoving() {
+    Console.readLine(GAME.MOVING, (input) => {
+      game.move(input);
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
--- src/phrases.js
+++ src/phrases.js
@@ -1,6 +1,7 @@
 const GAME = {
   START: '다리 건너기 게임을 시작합니다.',
   BRIDGE_SIZE: '다리의 길이를 입력해주세요.',
+  MOVING: '이동할 칸을 선택해주세요. (위: U, 아래: D)',
 };
 
 const ERROR = {
