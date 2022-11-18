e7e3f0982690545adaec0c6d69fb881bd600e767
hyunjin <leehj0110@kakao.com>
2022-11-19 02:14:20 +0900

    refactor: makeBridge 사용하지 않는 인자 제거

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -10,7 +10,7 @@ const BridgeMaker = {
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
   makeBridge(size, generateRandomNumber) {
-    return Array.from({ length: size }, generateRandomNumber).map((randomNumber, index, bridge) => {
+    return Array.from({ length: size }, generateRandomNumber).map((randomNumber) => {
       if (randomNumber === 1) return 'U';
       if (randomNumber === 0) return 'D';
     });
