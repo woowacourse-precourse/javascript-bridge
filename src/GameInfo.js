0082634f06bdd83c994c8b6b8655e92077a27fe5
Chanwoong1 <jcw7468@gmail.com>
2022-11-18 20:47:00 +0900

    feat: 다리 건너기 게임에 대한 정보를 담은 객체 생성

---
+++ src/GameInfo.js
@@ -0,0 +1,17 @@
+const GameInfo = {
+  upper: "U",
+
+  lower: "D",
+
+  gameStat: [],
+
+  bridge: null,
+
+  indexingArray: ["U", "D"],
+
+  moveBridge: [[], []],
+
+  position: -1,
+}
+
+module.exports = GameInfo;
