1ea48acb6d4c5ee97f9e040a64e8b05dcc765e3b
park junwoo <always1997@kakao.com>
2022-11-18 15:06:26 +0900

    fix: 다리 생성기능 리턴값 요구사항에 맞도록 변경

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -8,11 +8,7 @@ const BridgeMaker = {
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
   makeBridge(size, generateRandomNumber) {
-    const bridge = new Map();
-    bridge.set('U',Array.from({length: size}, () => generateRandomNumber()));
-    bridge.set('D',Array.from({length: size}, () => generateRandomNumber()));
-
-    return bridge;
+    return Array.from({length: size}, () => generateRandomNumber() ? 'U' : 'D');
   },
 };
 
