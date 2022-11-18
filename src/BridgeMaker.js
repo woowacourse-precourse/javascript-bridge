ee03f65aa3c931098474aa57554a989d10e1c42c
Jeong Hun Nam <njh3421@gmail.com>
2022-11-18 18:43:21 +0900

    feat : add bridgeMaker logic

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,13 +1,27 @@
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
+const InputView=require('./InputView')
+const BridgeRandomNumberGenerator=require('./BridgeRandomNumberGenerator')
+const OutputView=require('./OutputView')
+
 const BridgeMaker = {
   /**
    * @param {number} size 다리의 길이
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
-  makeBridge(size, generateRandomNumber) {},
+
+  makeBridge(size, generateRandomNumber) {
+    let bridgeArray=[]
+    console.log(Number(size),'사이즈')
+    for (let i = 0; i < Number(size); i++) {
+      const number=generateRandomNumber()
+      bridgeArray.push(number)
+    }
+    console.log(bridgeArray);
+    return bridgeArray
+  }
 };
 
 module.exports = BridgeMaker;
--- src/InputView.js
+++ src/InputView.js
@@ -2,6 +2,8 @@
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const MissionUtils = require("@woowacourse/mission-utils");
+const BridgeMaker=require('./BridgeMaker');
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
 
 const InputView = {
   printGameStart(){
@@ -10,31 +12,35 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-
   readBridgeSize() {
-    MissionUtils.Console.readLine('다리의 길이를 입력해주세요\n',(bridgeLength)=>{
+    MissionUtils.Console.readLine('다리의 길이를 입력해주세요\n', (bridgeLength)=>{
       this.checkBridgeSizeInput(bridgeLength)
     })
   },
-
   checkBridgeSizeInput(bridgeLength){
     if(bridgeLength<3 || bridgeLength>20) throw "[ERROR] range error occured"
-    if(bridgeLength.match(/[a-zA-z]/g)|| bridgeLength.match([/ㄱ-ㅎ|ㅏ-ㅣ|가-힣/g])) throw "[ERROR] The string can not be accepted"
+    if(bridgeLength.match(/[a-zA-z]/g) || bridgeLength.match(/[ㄱ-ㅎ가-힣]/g)) throw "[ERROR] The string can not be accepted"
     if(bridgeLength.match([/[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/g])) throw "[ERROR] The string can not be accepted"
     if(bridgeLength<0) throw "[ERROR] The Negative number can't be accepted"
-    return bridgeLength
+    this.randomBridge(bridgeLength)
+    // return bridgeLength
+  },
+  randomBridge(bridgeLength) {
+    const number=BridgeRandomNumberGenerator.generate
+    const bridgeArray=BridgeMaker.makeBridge(bridgeLength,number)
+    this.readMoving(bridgeArray)
   },
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {
+  readMoving(bridgeArray) {
     MissionUtils.Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)\n',(userSpace)=>{
       this.checkMovingInput(userSpace)
     })
   },
   checkMovingInput(userSpace){
-    if(userSpace!=='U'||userSpace!=='D') throw "[ERROR] Only U,D accepted"
-    return checkMovingInput
+    if(userSpace!=='U'&& userSpace!=='D') throw "[ERROR] Only U,D accepted"
+    return userSpace
   },
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
