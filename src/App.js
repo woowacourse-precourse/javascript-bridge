36564991bc1c0868e51c52d39c30ea4991f030db
Yebin Kim <kyb4312@gmail.com>
2022-11-18 21:55:21 +0900

    feat(App): start and set up the game

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+const InputView = require('./InputView');
+const OutputView = require('./OutputView');
+const BridgeGame = require('./BridgeGame');
+
 class App {
-  play() {}
+  #bridgeGame = new BridgeGame();
+
+  play() {
+    OutputView.printStartMessage();
+    InputView.readBridgeSize(this.startGame);
+  }
+
+  startGame(bridgeLength) {
+    this.#bridgeGame.setUp(bridgeLength);
+  }
 }
 
 module.exports = App;
