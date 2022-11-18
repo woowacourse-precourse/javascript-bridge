b2f6344da6fa5c26ed8f1eb20107bf9cb2d94c9c
inthhh <glass005533@gmail.com>
2022-11-19 00:31:07 +0900

    feat: BridgeGame 재시작 메소드 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,33 +1,29 @@
 const InputView = require("./InputView");
 const OutputView = require("./OutputView");
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
 class BridgeGame {
-  /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
+
   move(turnNumber, bridge, moveUpDown) {
     if (bridge[turnNumber] == moveUpDown) {
       OutputView.printMap(turnNumber, bridge, moveUpDown, true);
-      return bridge;
+      if (turnNumber == bridge.length - 1) {
+        OutputView.printResult(turnNumber, bridge, moveUpDown, true);
+        return 0;
+      }
+      return 1;
     }
     if (bridge[turnNumber] != moveUpDown) {
       OutputView.printMap(turnNumber, bridge, moveUpDown, false);
-      InputView.readGameCommand();
+      return 0;
     }
-    return bridge;
   }
 
-  /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  retry() {}
+  retry(nowLength, brg, moveUpDown) {
+    let isRetry = InputView.readGameCommand();
+    if (isRetry) InputView.readMoving(brg, 0);
+    if (!isRetry) OutputView.printResult(nowLength, brg, moveUpDown,false);
+    return;
+  }
 }
 
 module.exports = BridgeGame;
