1c56257abf9a6efa9ddf1e65aa07b630dcdbf835
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-18 17:46:12 +0900

    fix: 다리 생성 테스트 변경 ( 다리 길이, U,D 포함)

--- __tests__/BridgeTest.js
+++ __tests__/BridgeTest.js
@@ -4,17 +4,15 @@ const BridgeRandomNumberGenerator = require('../src/BridgeRandomNumberGenerator'
 describe('Bridge 테스트', () => {
   const MOCK = {
     SIZE: 6,
-    UPPER: 1,
-    LOWER: 0,
+    UP: 'U',
+    DOWN: 'D',
   };
   const generator = BridgeRandomNumberGenerator.generate;
 
-  test('다리 테스트', () => {
-    const [upper, lower] = BridgeMaker.makeBridge(MOCK.SIZE, generator);
-    const sum = upper.reduce((acc, curr, idx) => acc + (curr + lower[idx]), 0);
+  test('다리 생성 테스트', () => {
+    const bridge = BridgeMaker.makeBridge(MOCK.SIZE, generator);
 
-    expect(upper).toHaveLength(MOCK.SIZE);
-    expect(upper).not.toContain(2);
-    expect(sum).toBe(MOCK.SIZE);
+    expect(bridge).toHaveLength(MOCK.SIZE);
+    expect(bridge).toContain(MOCK.UP || MOCK.DOWN);
   });
 });
