53e594e57768c77811295bebf840fcfeca920b8b
jisu-kim <kss2460@naver.com>
2022-11-18 18:48:52 +0900

    feat(retry): 재시도 로직 이전 및 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -8,7 +8,9 @@ class BridgeGame {
     this.mainBridge = mainBridge;
     this.bridgeSize = mainBridge[0].length;
     this.count = 0;
+    this.retrycount = 1;
     this.hasNext = true;
+    this.finish = false;
     this.userBridge = [[], []];
   }
   /**
@@ -22,10 +24,8 @@ class BridgeGame {
       this.userBridge,
       this.mainBridge
     );
-    console.log(this.mainBridge);
-    console.log(this.userBridge);
     this.count++;
-    this.isNext();
+    this.retry();
   }
 
   /**
@@ -33,16 +33,20 @@ class BridgeGame {
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  retry(readGameCommand, mainBridge) {
-    readGameCommand(mainBridge);
-  }
-
-  isNext() {
+  retry() {
     if (
-      this.userBridge.flat().indexOf("X") !== -1 ||
+      this.userBridge.flat().indexOf("X") === -1 &&
       this.bridgeSize === this.count
-    )
+    ) {
       this.hasNext = false;
+      this.finish = true;
+      return;
+    }
+
+    if (this.userBridge.flat().indexOf("X") !== -1) {
+      this.hasNext = false;
+      this.retrycount++;
+    }
   }
 }
 
