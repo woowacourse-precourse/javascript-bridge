b9ce0598c51e68d98affa2915b98fd4a3bf0c716
yunho <Yunho7058@github.com>
2022-11-18 18:32:48 +0900

    Feat: 시작 문구 및 사용자에게 다리길이 받기 기능 구현

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,5 +1,9 @@
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
+ 제공된 BridgeMaker 객체를 활용해 구현해야 한다.
+BridgeMaker에 프로퍼티를 추가할 수 없다.
+BridgeMaker의 파일 경로는 변경할 수 없다.
+BridgeMaker의 메서드의 시그니처(인자, 이름)와 반환 타입은 변경할 수 없다.
  */
 const BridgeMaker = {
   /**
@@ -7,7 +11,11 @@ const BridgeMaker = {
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
-  makeBridge(size, generateRandomNumber) {},
+  makeBridge(size, generateRandomNumber) {
+    //여기에 다리의 길이와 맞는 다리위치 인자로 받아 1,0에 따른 U, D 리턴 해주기
+    //사용자가 입력한 번호와, 여기서 만들어져있는 함수 보내기
+    //그리고 1,0에 따라 U나 P리턴
+  },
 };
 
 module.exports = BridgeMaker;
