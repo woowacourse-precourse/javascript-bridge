bde94d4cca02b3fe12ec4d5d1eb989c74a3b046e
kangyeongmin <kym6050@hanyang.ac.kr>
2022-11-18 19:30:59 +0900

    ♻️ refactor(App): App play 코드 리펙토링

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-const OutputView = require("./OutputView");
+const MissionUtils = require("@woowacourse/mission-utils");
+const BridgeGame = require("./BridgeGame");
 const InputView = require("./InputView");
+const OutputView = require("./OutputView");
+const { REGEX } = require("./constant");
 
 class App {
+  #bridgeGame;
+
   play() {
-    OutputView.printStart();
-    InputView.readBridgeSize();
+    this.#bridgeGame = new BridgeGame();
+    this.controller();
   }
 }
 
