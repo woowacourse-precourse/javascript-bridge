41656118bc356624915f2403e180d630a4556236
hahagarden <88613455+hahagarden@users.noreply.github.com>
2022-11-18 20:10:08 +0900

    feat: 재시도 여부 입력 예외처리

--- src/Utils.js
+++ src/Utils.js
@@ -3,5 +3,9 @@ const DOWN = "D";
 const PASS = "O";
 const FAIL = "X";
 const EMPTY = " ";
+const RETRY = "R";
+const QUIT = "Q";
 
-module.exports = { UP, DOWN, PASS, FAIL, EMPTY };
+const ERROR_NOT_UPPER_CASE = "[ERROR] 대문자로 입력해 주세요.";
+
+module.exports = { UP, DOWN, PASS, FAIL, EMPTY, RETRY, QUIT, ERROR_NOT_UPPER_CASE };
