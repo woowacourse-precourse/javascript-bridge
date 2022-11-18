c857d596e282a39b751d1255a4dd8410b0ac502d
Deemou <4106243@naver.com>
2022-11-18 17:23:46 +0900

    feat(Validator): 이동할 칸 유효성 검사 기능 추가

--- src/Validator.js
+++ src/Validator.js
@@ -8,12 +8,22 @@ class Validator {
     this.throwException(InvalidInputError, ERROR_MESSAGE.BRIDGE_LENGTH);
   }
 
+  static validateSpace(space) {
+    if (this.isValidSpace(space)) return;
+    this.throwException(InvalidInputError, ERROR_MESSAGE.SPACE);
+  }
+
   static throwException(Error, message) {
     throw new Error(message);
   }
 
   static isValidLength(length) {
     return length >= BRIDGE.LENGTH_MIN && length <= BRIDGE.LENGTH_MAX;
+  }
+
+  static isValidSpace(space) {
+    const spaceList = [BRIDGE.UP, BRIDGE.DOWN];
+    return spaceList.includes(space);
   }
 }
 
