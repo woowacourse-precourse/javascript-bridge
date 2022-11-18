66f21c64c409c6c8940f727043925efc238d6048
Jongco <narira0922@gmail.com>
2022-11-18 22:55:21 +0900

    feat: error message 생성

---
+++ utils/constant.js
@@ -0,0 +1,7 @@
+const ERROR = Object.freeze({
+  NOT_A_NUMBER: "[ERROR] 숫자가 아닙니다.",
+  UP_DOWN_COMMAND: "[ERROR] 'U'와 'D'만 입력 가능합니다.",
+  RETRY_COMMAND: "[ERROR] 'R'과 'Q'만 입력 가능합니다.",
+});
+
+module.exports = ERROR;
