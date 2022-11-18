cb41b2afbd3c1d3ff48d9f13e0720bc81b416070
jungsmins1 <jungsmins00@gmail.com>
2022-11-18 21:42:29 +0900

    feat: 사용자에게 다리의 길이를 입력받는 메서드 추가

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,8 @@
+const BridgeMaker = require("./BridgeMaker");
+const BridgeRandomNumberGenerator =  require("./BridgeRandomNumberGenerator");
+const MissionUtils = require("@woowacourse/mission-utils");
+const Validate = require("./Validate");
+const { INPUT_MESSAGE } = require("./Utils");
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,7 +10,12 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    MissionUtils.Console.readLine(INPUT_MESSAGE.INPUT_BRIDGE_LENGTH, (size) => {
+      return BridgeMaker.makeBridge(Validate.validateBridgeLength(size), 
+        BridgeRandomNumberGenerator.generate());
+    })
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
