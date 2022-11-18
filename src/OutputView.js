8733b613a7d6b6c5fb595af8de041667dac53d02
kimeuns <dmstn2006@gmail.com>
2022-11-19 02:34:47 +0900

    feat: 최종 결과를 출력하는 기능 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -48,7 +48,17 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() {},
+  printResult() {
+    Console.print(GAME.END_RESULT);
+  },
+
+  printSucessOrFail(isSuccess) {
+    Console.print(`${GAME.RESULT}${isSuccess ? "성공" : "실패"}`);
+  },
+
+  printTryCount(gameCount) {
+    Console.print(`${GAME.TRY_COUNT}${gameCount}`);
+  },
 };
 
 module.exports = OutputView;
