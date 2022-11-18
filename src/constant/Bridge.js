e205f03e9bbc243b56d5602b7167e4b5122f16a4
Deemou <4106243@naver.com>
2022-11-18 15:48:13 +0900

    chore(constant): Bridge 관련 상수 추가

---
+++ src/constant/Bridge.js
@@ -0,0 +1,35 @@
+const BRIDGE = Object.freeze({
+  LENGTH_MIN: 3,
+  LENGTH_MAX: 20,
+  UP: 'U',
+  DOWN: 'D'
+});
+const PASS_RESULT = Object.freeze({
+  CORRECT: 'O',
+  WRONG: 'X',
+  DELIMITER: ' | ',
+  START: '[ ',
+  END: ' ]'
+});
+const COMMAND = Object.freeze({
+  RESTART: 'R',
+  QUIT: 'Q'
+});
+const MESSAGE = Object.freeze({
+  START: '다리 건너기 게임을 시작합니다.',
+  INPUT_BRIDGE_LENGTH: '\n다리 길이를 입력해주세요.\n',
+  INPUT_SPACE: '\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
+  INPUT_COMMAND: '\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
+  END: '\n최종 게임 결과',
+  GAME_RESULT: '\n게임 성공 여부:',
+  SUCCESS: '성공',
+  FAIL: '실패',
+  TRY: '총 시도한 횟수:'
+});
+
+module.exports = {
+  BRIDGE,
+  PASS_RESULT,
+  COMMAND,
+  MESSAGE
+};
