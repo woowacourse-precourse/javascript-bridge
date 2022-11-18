ed101b55dc7fc336304f8809ce643dd3cbf77bf7
dladncks1217 <dlaxodud1217@gmail.com>
2022-11-18 19:27:55 +0900

    refactor(constants): 상수 문자열 일부 수정

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,8 +1,9 @@
 const GAME_MESSAGE = {
-  GAME_START: '다리 건너기 게임을 시작합니다.',
-  SET_BRIDGE_LENGTH: '다리의 길이를 입력해주세요.',
-  MOVE_CHOICE: '이동할 칸을 선택해주세요. (위: U, 아래: D)',
-  GAME_RETRY: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)',
+  GAME_START: '다리 건너기 게임을 시작합니다.\n',
+  SET_BRIDGE_LENGTH: '다리의 길이를 입력해주세요.\n',
+  MOVE_CHOICE: '이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
+  GAME_RETRY:
+    '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
   GAME_RESULT: '최종 게임 결과',
   GAME_CLEAR: (string) => `게임 성공 여부: ${string}`,
   TRY_COUNT: (count) => `총 시도한 횟수: ${count}`,
