9181761e9fa5cf6ddac612960e00f602a3c444a2
doheeMac <thsehgml37@korea.ac.kr>
2022-11-18 11:23:34 +0900

    feat: 다리 길이 입력 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+const InputView = require("./View/InputView.js");
+
 class App {
-  play() {}
+  play() {
+    const input = InputView.readBridgeSize();
+    console.log("다리 길이는 ", input);
+  }
 }
 
 module.exports = App;
---
+++ src/View/InputView.js
@@ -0,0 +1,38 @@
+/**
+ * 사용자로부터 입력을 받는 역할을 한다.
+ * 제공된 InputView 객체를 활용해 구현해야 한다.
+ * InputView의 파일 경로는 변경할 수 있다.
+ * InputView의 메서드의 인자는 변경할 수 있다.
+ * 사용자 값 입력을 위해 필요한 메서드를 추가할 수 있다.
+ */
+
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const BridgeMaker = require;
+
+const InputView = {
+  /**
+   * 다리의 길이를 입력받는다.
+   */
+  readBridgeSize() {
+    MissionUtils.Console.readLine(
+      "다리의 길이를 입력해주세요.\n",
+      (bridgeLen) => {
+        const BRIDGE_LEN = len;
+        // bridgeLen입력값을 Controller에서 검증한 후 --> BridegMaker로 다리를 만들어서 Model이용 --> Model에서 bridge길이 및 만들어진 다리 정보를 저장
+      }
+    );
+  },
+
+  /**
+   * 사용자가 이동할 칸을 입력받는다.
+   */
+  readMoving() {},
+
+  /**
+   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+   */
+  readGameCommand() {},
+};
+
+module.exports = InputView;
