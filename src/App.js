b23a7f056b62444639e9fb1bbc1f1da553c945c4
oio337a <oio337a@gmail.com>
2022-11-18 13:20:31 +0900

    feat : print bridge game start

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+const inputView = require("./InputView");
+const outputView = require("./OutputView");
+
 class App {
+  constructor() {
+    outputView.printGameStart();
+  }
   play() {}
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
