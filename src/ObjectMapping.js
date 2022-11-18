558b1a8b5d353cf9f5acd96164ac8026cb22752c
MyungAe <ladiescheong@naver.com>
2022-11-18 14:16:49 +0900

    refactor(ObjectMapping): 새 객체 매핑이 필요해 객체로 감싸줌, 추후 추가 예정

--- src/ObjectMapping.js
+++ src/ObjectMapping.js
@@ -1,10 +1,8 @@
-/**
- * 입력된 값을 U 또는 D로 바꾸어주는 역할을 한다.
- */
-
-const BridgeLineMap = {
-  0: "D",
-  1: "U",
+const ObjectMapping = {
+  BridgeLineMap: {
+    0: "D",
+    1: "U",
+  },
 };
 
-module.exports = BridgeLineMap;
+module.exports = ObjectMapping;
