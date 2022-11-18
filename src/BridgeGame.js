f1b3af85975664bab58adc2f0d29f504ea97e531
js43o <js43o@naver.com>
2022-11-18 22:19:48 +0900

    feat(BridgeGame): 다리 이동 함수 추가

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -10,11 +10,18 @@ class BridgeGame {
   constructor(length) {
     this.#bridge = makeBridge(length, generate);
     this.#position = 0;
-    this.#attempts = 0;
+    this.#attempts = 1;
     this.#failed = false;
   }
 
-  move() {}
+  move(direction) {
+    if (this.#bridge[this.#position] === direction) {
+      this.#position += 1;
+      return this.#position < this.#bridge.length;
+    }
+    this.#failed = true;
+    return false;
+  }
 
   retry() {}
 }
