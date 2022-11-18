c7ffc4e97b70d0ea6791632e0c05f051520782fc
dudgp128 <20181048@sungshin.ac.kr>
2022-11-18 14:45:09 +0900

    입력받은 문자와 다리를 비교하여 정상적으로 ResultMap을 채우는지 테스트

--- __tests__/BridgeTest.js
+++ __tests__/BridgeTest.js
@@ -1,14 +1,24 @@
 const BridgeGame = require("../src/BridgeGame");
 
 describe(" 다리 테스트 ", () => {
-  test("move 테스트", () => {
-    const bridge = ["0", "1", "0", "0"]; //D,U,D,D
+  test("fillResultMap 테스트", () => {
+    const bridge = ["0", "1", "0"]; //D,U,D,D
     const bridgeGame = new BridgeGame(bridge);
-    const states = ["D", "U", "U", "D"];
-    const result = ["O", "O", "X", "O"];
-
-    states.forEach((state, idx) => {
-      expect(bridgeGame.move(state)).toEqual(result[idx]);
+    const inputs = ["D", "U", "U"];
+    const results = [
+      [["O"], [" "]],
+      [
+        ["O", " "],
+        [" ", "O"],
+      ],
+      [
+        ["O", " ", " "],
+        [" ", "O", "X"],
+      ],
+    ];
+    inputs.forEach((input, idx) => {
+      bridgeGame.transInputtoPosition(input);
+      expect(bridgeGame.getResultMap()).toEqual(results[idx]);
     });
   });
 });
