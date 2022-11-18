d15a04f153afb39cc2e51c421fa647e3830cb662
vinivin153 <vinivin153@naver.com>
2022-11-19 03:02:30 +0900

    feat: (readBridgeSize) 다리 길이 입력받는 기능 추가

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,9 @@
+const { Console } = require('@woowacourse/mission-utils');
+const { MESSAGE_INPUT } = require('./utils/constant');
+const { makeBridge } = require('./BridgeMaker');
+const { generate } = require('./BridgeRandomNumberGenerator');
+const { bridgeSizeValidator } = require('./Validator');
+const OutputView = require('./OutputView');
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,7 +11,17 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    try {
+      Console.readLine(MESSAGE_INPUT.BRIDGE_SIZE, (size) => {
+        bridgeSizeValidator.isBridgeSizeValid(size);
+        const bridge = makeBridge(size, generate);
+      });
+    } catch (e) {
+      OutputView.printError(e);
+      this.readBridgeSize();
+    }
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
