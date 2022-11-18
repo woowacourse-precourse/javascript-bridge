778694b81eed2a953d42a8e8b268c5943acf9c05
murpin <qw04011@gmail.com>
2022-11-18 11:56:18 +0900

    feat(App/play): 입력받는 기능 게임 실행부에 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+const { readBridgeSize } = require('./InputView');
+const { Console } = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  play() {
+    Console.print('다리 건너기 게임을 시작합니다.\n');
+    readBridgeSize();
+  }
 }
 
 module.exports = App;
