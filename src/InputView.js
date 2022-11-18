0a7228f833326ae6cfcfbba80600cc4ed97bf046
AhnGeunTaek <dksrmsxor@gmail.com>
2022-11-18 17:21:05 +0900

    feat: 사용자 입력값 받기 기능 추가

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,6 @@
+const INPUT_MESSAGE = require('./utils/constants');
+const { Console } = require('@woowacourse/mission-utils');
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,17 +8,23 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine(INPUT_MESSAGE.BRIDGE_SIZE, (bridgeSize) => {});
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    Console.readLine(INPUT_MESSAGE.MOVING_COMMAND, (movingCommand) => {});
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+    Console.readLine(INPUT_MESSAGE.GAME_COMMAND, (gameCommand) => {});
+  },
 };
 
 module.exports = InputView;
