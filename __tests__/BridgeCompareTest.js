efdd9923ff450b2cb1e3896d78dd5ac728e77465
khj0426 <706shin1728@naver.com>
2022-11-18 20:30:48 +0900

    test:출력이 올바르게 나오는지 확인 테스트

--- __tests__/BridgeCompareTest.js
+++ __tests__/BridgeCompareTest.js
@@ -1,4 +1,5 @@
 const BridgeGame = require('../src/BridgeGame');
+const BridgeMessage = require('../src/MESSAGES/BridgeMessage');
 
 const bridgegame = new BridgeGame();
 
@@ -7,4 +8,17 @@ test('사용자가 입력한 칸이 위이고, 이용가능한 다리가 위인 
   expect(bridgegame.move('U', 'D')).toBe(false);
   expect(bridgegame.move('D', 'U')).toBe(false);
   expect(bridgegame.move('D', 'D')).toBe(true);
+});
+
+test('사용자가 입력한 칸과 이용가능한 다리에 따른 출력 형식 테스트', () => {
+  expect(bridgegame.moveCaseAction('U', 'U')).toBe(BridgeMessage.UPUP_MESSAGE);
+  expect(bridgegame.moveCaseAction('U', 'D')).toBe(
+    BridgeMessage.UPDOWN_MESSAGE
+  );
+  expect(bridgegame.moveCaseAction('D', 'D')).toBe(
+    BridgeMessage.DOWNDOWN_MESSAGE
+  );
+  expect(bridgegame.moveCaseAction('D', 'U')).toBe(
+    BridgeMessage.DOWNUP_MESSAGE
+  );
 });
