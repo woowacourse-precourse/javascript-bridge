0a43b33a85192e6fe2e86c7f0a712791afe725a8
Jeong Hun Nam <njh3421@gmail.com>
2022-11-18 11:58:16 +0900

    feat(InputView) : add readGameCommand f(),error

--- src/InputView.js
+++ src/InputView.js
@@ -39,7 +39,15 @@ const InputView = {
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+    MissionUtils.Console.readLine("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",(gameInput)=>{
+      this.checkReadGameInput(gameInput)
+    })
+  },
+  checkReadGameInput(gameInput){
+    if(gameInput!=='R' || gameInput!=='Q') throw "[ERROR] Only R,Q accepted"
+    return gameInput
+  }
 };
 
 module.exports = InputView;
