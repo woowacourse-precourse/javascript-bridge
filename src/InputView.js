d0bbe164d1c9e6fdfc813c237cb22b7ca5f54fbd
murpin <qw04011@gmail.com>
2022-11-18 16:07:47 +0900

    feat(InputView/readGameCommand): 게임에서 옵션을 입력받고 반환하는 기능

--- src/InputView.js
+++ src/InputView.js
@@ -30,7 +30,13 @@ const InputView = {
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+    let gameOption;
+    Console.readLine("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",(option) => {
+      gameOption = option;
+    })
+    return gameOption;
+  },
 };
 
 module.exports = InputView;
