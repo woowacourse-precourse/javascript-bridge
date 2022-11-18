414317c60df06c01b0b1b2414cf116185f045b4b
khj0426 <706shin1728@naver.com>
2022-11-19 08:40:11 +0900

    feat:랜덤 숫자가 1이면 U를, 0이면 D를 반환하는 기능 추가

--- src/MESSAGES/NumberMessage.js
+++ src/MESSAGES/NumberMessage.js
@@ -1,6 +1,12 @@
 const NUMBER_MESSAGE = {
   BRIDGE_START: 3,
   BRIDGE_END: 20,
+  flagBridge(randomnum) {
+    if (randomnum === '1') {
+      return 'U';
+    }
+    return 'D';
+  },
 };
 
 Object.freeze(NUMBER_MESSAGE);
