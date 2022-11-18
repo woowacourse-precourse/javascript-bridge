91536a1b32998d7a88e7872a35043a9c23522d28
TaeeunKim <xodms0309@naver.com>
2022-11-18 20:34:54 +0900

    feat: 게임 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+const { Console } = require('@woowacourse/mission-utils');
 class App {
-  play() {}
+  play() {
+    Console.print('다리 건너기 게임을 시작합니다.');
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
