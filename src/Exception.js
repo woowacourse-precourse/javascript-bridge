27acfaa7065a3dfc4f5e9245f24537924ca639d2
jinseo <ghkdwlstj99@gmail.com>
2022-11-19 23:44:43 +0900

    feat: 다리 길이가 숫자인지 검사하는 메서드 구현

--- src/Exception.js
+++ src/Exception.js
@@ -7,11 +7,18 @@ class CheckBridgeSizeException{
 
     validate(size){
         this.checkBridgeSize(size);
+        this.checkBridgeSize(size);
     }
 
     checkBridgeSize(size){
         if(size > 20 || size < 3){
             throw new Error("[ERROR] 다리의 길이는 3초과 20미만이여야 합니다.");
+        }
+    }
+
+    checkBridgeNum(size){
+        if(isNaN(size)){
+            throw new Error("[ERROR] 숫자를 입력해주세요.")
         }
     }
 }
