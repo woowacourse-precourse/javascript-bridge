7ab85d1f57f8478610e2a08d20b69945d88cf7a9
turtle601 <turtle601@naver.com>
2022-11-19 19:29:44 +0900

    refactor(BridgeInputTest) input 입력값 에러 체크 테스트 작성

--- __tests__/BridgeInputTest.js
+++ __tests__/BridgeInputTest.js
@@ -1,28 +1,127 @@
-const BridgeService = require('../src/service/BridgeService');
-const { MODEL_KEY } = require('../src/utils/constants');
+const Input = require('../src/controller/validate/Input');
 
-describe('다리 길이 테스트', () => {
+const BridgeLengthException = require('../src/controller/validate/BridgeLengthException');
+const BridgeUpDownException = require('../src/controller/validate/BridgeUpDownException');
+const BridgeRestartExitException = require('../src/controller/validate/BridgeRestartExitException');
+
+describe('입력한 다리 길이 테스트 - BridgeLengthException', () => {
   test('(다리 길이(오류) - 문자)', () => {
     const input = 'a';
-    const bridgeLength = new BridgeService();
-    expect(() => bridgeLength.start(input)).toThrow('[ERROR]');
+
+    expect(() => Input.getValidate(new BridgeLengthException(input))).toThrow(
+      '[ERROR]'
+    );
   });
 
   test('(다리 길이(오류) - 3미만의 숫자)', () => {
     const input = '1';
-    const bridgeLength = new BridgeService(input);
-    expect(() => bridgeLength.start(input)).toThrow('[ERROR]');
+
+    expect(() => Input.getValidate(new BridgeLengthException(input))).toThrow(
+      '[ERROR]'
+    );
   });
 
   test('(다리 길이(오류) - 20초과의 숫자)', () => {
     const input = '21';
-    const bridgeLength = new BridgeService(input);
-    expect(() => bridgeLength.start(input)).toThrow('[ERROR]');
+
+    expect(() => Input.getValidate(new BridgeLengthException(input))).toThrow(
+      '[ERROR]'
+    );
   });
 
   test('(다리 길이(오류) - 문자 + 숫자(3 ~ 20))', () => {
-    const input = '20 ';
-    const bridgeLength = new BridgeService(input);
-    expect(() => bridgeLength.start(input)).toThrow('[ERROR]');
+    const input = '1 ';
+
+    expect(() => Input.getValidate(new BridgeLengthException(input))).toThrow(
+      '[ERROR]'
+    );
+  });
+
+  test('(다리 길이(정상) - 숫자(3 ~ 20))', () => {
+    const input = '3';
+
+    expect(Input.getValidate(new BridgeLengthException(input))).toEqual('3');
+  });
+});
+
+describe('입력한 UpDownKey 테스트 - BridgeUpDownException', () => {
+  test('(Q or R 제외 문자 입력(오류) - "a") 입력 테스트', () => {
+    const input = 'a';
+
+    expect(() => Input.getValidate(new BridgeUpDownException(input))).toThrow(
+      '[ERROR]'
+    );
+  });
+
+  test('(U or D 제외 문자 입력(오류) - "1") 입력 테스트', () => {
+    const input = '1';
+
+    expect(() => Input.getValidate(new BridgeUpDownException(input))).toThrow(
+      '[ERROR]'
+    );
+  });
+
+  test('(U or D 제외 문자 입력(오류) - "u") 입력 테스트', () => {
+    const input = 'u';
+
+    expect(() => Input.getValidate(new BridgeUpDownException(input))).toThrow(
+      '[ERROR]'
+    );
+  });
+
+  test('(U or D 제외 문자 입력(오류) - "U ") 입력 테스트', () => {
+    const input = 'U ';
+
+    expect(() => Input.getValidate(new BridgeUpDownException(input))).toThrow(
+      '[ERROR]'
+    );
+  });
+
+  test('(U or D 문자 입력(정상) - "U") 입력 테스트', () => {
+    const input = 'U';
+
+    expect(Input.getValidate(new BridgeUpDownException(input))).toEqual('U');
+  });
+});
+
+describe('입력한 RestartExitKey 테스트 - BridgeRestartExitException', () => {
+  test('(Q or R 제외 문자 입력(오류) - "a") 입력 테스트', () => {
+    const input = 'a';
+
+    expect(() =>
+      Input.getValidate(new BridgeRestartExitException(input))
+    ).toThrow('[ERROR]');
+  });
+
+  test('(Q or R 제외 문자 입력(오류) - "1") 입력 테스트', () => {
+    const input = '1';
+
+    expect(() =>
+      Input.getValidate(new BridgeRestartExitException(input))
+    ).toThrow('[ERROR]');
+  });
+
+  test('(Q or R 제외 문자 입력(오류) - "u") 입력 테스트', () => {
+    const input = 'u';
+
+    expect(() =>
+      Input.getValidate(new BridgeRestartExitException(input))
+    ).toThrow('[ERROR]');
+  });
+
+  test('(Q or R 제외 문자 입력(오류) - "U ") 입력 테스트', () => {
+    const input = 'U ';
+
+    expect(() =>
+      Input.getValidate(new BridgeRestartExitException(input))
+    ).toThrow('[ERROR]');
+  });
+
+  test('(Q or R 문자 입력(정상) - "U") 입력 테스트', () => {
+    const input = 'Q';
+
+    expect(Input.getValidate(new BridgeRestartExitException(input))).toEqual(
+      'Q'
+    );
   });
 });
