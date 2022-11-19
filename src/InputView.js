3915e5ec8b6afe0a4e3c171a171b7b39d70d21e2
Sangmin Lee <gink.ss00@gmail.com>
2022-11-19 21:14:39 +0900

    feat: 다리 길이 입력 받는 메소드 구현
    다리의 길이를 입력 받고 예외 사항이 있다면 예외 처리한 후 다시 입력 받는다.

--- src/InputView.js
+++ src/InputView.js
@@ -1,11 +1,20 @@
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
+const { Console } = require('@woowacourse/mission-utils');
+const { MESSAGE, ERROR_MESSAGE } = require('./constant/constant');
+
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine(MESSAGE.INPUT_LENGTH, (length) => {
+      this.validate(length);
+
+      return length;
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
@@ -16,6 +25,27 @@ const InputView = {
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand() {},
+
+  validate(length) {
+    const CHECK_NUMBER = /^[0-9]*$/;
+    const CHECK_START_NUMBER = /^0/;
+    try {
+      if (!CHECK_NUMBER.test(length)) {
+        throw new Error(ERROR_MESSAGE.INVALID_LENGTH_NUMBER);
+      }
+
+      if (CHECK_START_NUMBER.test(length)) {
+        throw new Error(ERROR_MESSAGE.START_ZERO);
+      }
+
+      if (parseInt(length, 10) < 3 || parseInt(length, 10) > 20) {
+        throw new Error(ERROR_MESSAGE.INVALID_LENGTH_RANGE);
+      }
+    } catch (error) {
+      Console.print(error);
+      this.readBridgeSize();
+    }
+  },
 };
 
 module.exports = InputView;
