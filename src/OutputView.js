928ffe3471eb76d1dbcf30880d5c960cb6c328c5
wontae chang <cwt629@naver.com>
2022-11-19 22:03:51 +0900

    feat: print one line of the map

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,4 +1,5 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const BridgeGame = require("./BridgeGame");
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -23,6 +24,18 @@ const OutputView = {
    */
   printStartMessage() {
     MissionUtils.Console.print("다리 건너기 게임을 시작합니다.\n");
+  },
+
+  /**
+   * 특정 줄의 map을 출력하는 함수
+   * @param {BridgeGame} bridgeGame 브릿지게임 객체
+   * @param {string} side 위나 아래를 뜻하는 문자(U or D)
+   */
+  printOneSideMap(bridgeGame, side) {
+    const COMPARISON_RESULT = bridgeGame.getComparisonResultArray(side);
+    const MAP_TO_PRINT = "[ " + COMPARISON_RESULT.join(" | ") + " ]";
+
+    MissionUtils.Console.print(MAP_TO_PRINT);
   }
 };
 
