ca20900176542465df453594fcb73a074b8f2a65
d0422 <rlfehd2013@naver.com>
2022-11-20 01:43:23 +0900

    Test(ControlTest):게임 재시작 테스트 코드 추가

---
+++ __tests__/ControlTest.js
@@ -0,0 +1,14 @@
+const ControlValidation = require('../src/Validation/ControlValidation');
+
+describe('게임 재시작 테스트', () => {
+  test('R 또는 Q가 아닌경우', () => {
+    expect(() => {
+      ControlValidation('W');
+    }).toThrow('[ERROR]');
+  });
+  test('소문자인 경우', () => {
+    expect(() => {
+      ControlValidation('r');
+    }).toThrow('[ERROR]');
+  });
+});
