dc02bc5823788e5af57269bc5b898efa6cc08747
inthhh <glass005533@gmail.com>
2022-11-20 01:10:47 +0900

    refactor: InputView 메소드 분리 및 전역변수 추가

--- src/InputView.js
+++ src/InputView.js
@@ -6,18 +6,26 @@ const OutputView = require("./OutputView");
 const Console = MissionUtils.Console;
 
 const InputView = {
-  readBridgeSize(tryCount) {
+  gameContinue: 0,
+  bridge: [],
+  tryCount: 1,
+
+  readBridgeSize() {
     Console.readLine("\n다리의 길이를 입력해주세요.\n", (inputBridgeSize) => {
-      try {
-        this.bridgeSizeException(inputBridgeSize);
-      } catch (e) {
-        Console.print(e);
-        if (e) this.readBridgeSize();
-      }
-      bridge = BridgeMaker.makeBridge(inputBridgeSize, BridgeRandomNumberGenerator.generate);
-      this.readMoving(bridge, 0, tryCount);
-      return bridge;
+      this.bridgeSizeExceptionCatch(inputBridgeSize);
+      this.bridge = BridgeMaker.makeBridge(inputBridgeSize, BridgeRandomNumberGenerator.generate);
+      this.readMoving(0);
+      return;
     });
+  },
+
+  bridgeSizeExceptionCatch(inputBridgeSize) {
+    try {
+      this.bridgeSizeException(inputBridgeSize);
+    } catch (e) {
+      Console.print(e);
+      this.readBridgeSize();
+    }
   },
 
   bridgeSizeException(inputBridgeSize) {
@@ -25,52 +33,74 @@ const InputView = {
     if (inputBridgeSize < 3 || inputBridgeSize > 20) throw "[ERROR] 3이상 20이하의 수만 입력할 수 있습니다.";
   },
 
-  readMoving(bridge, turnNumber, tryCount) {
+  readMoving(turnNumber) {
     Console.readLine("\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n", (inputMoveUpDown) => {
-      try {
-        this.movingException(inputMoveUpDown);
-      } catch (e) {
-        Console.print(e);
-        if (e) this.readMoving(bridge, turnNumber, tryCount);
-      }
+      this.movingExceptionCatch(inputMoveUpDown, turnNumber);
       const brgGame = new BridgeGame();
-      let gameContinue = brgGame.move(turnNumber, bridge, inputMoveUpDown);
-      if (gameContinue > 0) {
-        OutputView.printMap(turnNumber, bridge, inputMoveUpDown, true);
-        this.readMoving(bridge, turnNumber + 1, tryCount);
-      }
-      if (gameContinue < 0) {
-        OutputView.printMap(turnNumber, bridge, inputMoveUpDown, false);
-        this.readGameCommand(turnNumber, bridge, inputMoveUpDown, tryCount);
-      }
-      if (gameContinue == 0) {
-        OutputView.printResult(turnNumber, bridge, inputMoveUpDown, true, tryCount);
-        Console.close();
-      }
+      this.gameContinue = brgGame.move(turnNumber, this.bridge, inputMoveUpDown);
+      this.isGameContinue(turnNumber, inputMoveUpDown);
     });
-    return;
+  },
+
+  isGameContinue(turnNumber, inputMoveUpDown) {
+    if (this.gameContinue > 0) this.successContinueGame(turnNumber, inputMoveUpDown);
+    if (this.gameContinue < 0) this.inputWrongGame(turnNumber, inputMoveUpDown);
+    if (this.gameContinue == 0) this.failGameOver(turnNumber, inputMoveUpDown);
+  },
+
+  successContinueGame(turnNumber, inputMoveUpDown) {
+    OutputView.printMap(turnNumber, this.bridge, inputMoveUpDown, true);
+    this.readMoving(turnNumber + 1);
+  },
+
+  inputWrongGame(turnNumber, inputMoveUpDown) {
+    OutputView.printMap(turnNumber, this.bridge, inputMoveUpDown, false);
+    this.readGameCommand(turnNumber, inputMoveUpDown);
+  },
+
+  failGameOver(turnNumber, inputMoveUpDown) {
+    OutputView.printResult(turnNumber, this.bridge, inputMoveUpDown, true, this.tryCount);
+    Console.close();
+  },
+
+  movingExceptionCatch(inputMoveUpDown, turnNumber) {
+    try {
+      this.movingException(inputMoveUpDown);
+    } catch (e) {
+      Console.print(e);
+      this.readMoving(turnNumber);
+    }
   },
 
   movingException(inputMoveUpDown) {
     if (inputMoveUpDown != "U" && inputMoveUpDown != "D") throw "[ERROR] U 혹은 D만 입력할 수 있습니다.";
   },
 
-  readGameCommand(turnNumber, bridge, inputMoveUpDown, tryCount) {
+  readGameCommand(turnNumber, inputMoveUpDown) {
     Console.readLine("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n", (inputRetry) => {
-      try {
-        this.retryException(inputRetry);
-      } catch (e) {
-        Console.print(e);
-        if (e) this.readGameCommand(turnNumber, bridge, inputMoveUpDown, tryCount);
-      }
+      this.retryExceptionCatch(inputRetry, turnNumber, inputMoveUpDown);
       const brgGame = new BridgeGame();
       let isRetry = brgGame.retry(inputRetry);
-      if (isRetry == 1) this.readMoving(bridge, 0, tryCount + 1);
-      if (isRetry == 0) {
-        OutputView.printResult(turnNumber, bridge, inputMoveUpDown, false, tryCount);
-        Console.close();
-      }
+      this.isRetryGame(isRetry, turnNumber, inputMoveUpDown);
+      if (isRetry == 0) Console.close();
     });
+  },
+
+  isRetryGame(isRetry, turnNumber, inputMoveUpDown) {
+    if (isRetry == 1) {
+      this.tryCount++;
+      this.readMoving(0);
+    }
+    if (isRetry == 0) OutputView.printResult(turnNumber, this.bridge, inputMoveUpDown, false, this.tryCount);
+  },
+
+  retryExceptionCatch(inputRetry, turnNumber, inputMoveUpDown) {
+    try {
+      this.retryException(inputRetry);
+    } catch (e) {
+      Console.print(e);
+      if (e) this.readGameCommand(turnNumber, inputMoveUpDown);
+    }
   },
 
   retryException(inputRetry) {
