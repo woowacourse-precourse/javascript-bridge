3b3022e3449200102ef11bd03413a8b4a16d5c66
yaena1223 <yaena1223@dgu.ac.kr>
2022-11-20 04:59:48 +0900

    feat: add readBridgeSize function

--- src/InputView.js
+++ src/InputView.js
@@ -1,11 +1,17 @@
+const { Console } = require('@woowacourse/mission-utils');
+const { INPUT_MESSAGE } = require('./Constant');
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
+// InputView 에서만 MissionUtils의 Console.readLine() 을 이용해 사용자의 입력을 받을 수 있다.
 const InputView = {
-  /**
-   * 다리의 길이를 입력받는다.
-   */
-  readBridgeSize() {},
+  readBridgeSize() {
+    return new Promise(resolve => {
+      Console.readLine(INPUT_MESSAGE.BRIDGE_SIZE, size => {
+        resolve(size);
+      });
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
