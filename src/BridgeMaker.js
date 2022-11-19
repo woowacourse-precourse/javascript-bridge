65176dfb860583ce84ee4f6154ae12bb9d5f098e
Circlewee <pnj1118@naver.com>
2022-11-19 20:45:17 +0900

    feat(BridgeMaker): 다리 생성 로직 (#8)
    1. generateRandomNumber로 난수를 생성해 size만큼의 길이로 다리를 생성
    2. 테스트 코드 작성

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -5,9 +5,17 @@ const BridgeMaker = {
   /**
    * @param {number} size 다리의 길이
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
-   * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
+   * @returns {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
-  makeBridge(size, generateRandomNumber) {},
+  makeBridge(size, generateRandomNumber) {
+    const emptyArray = Array.from({ length: size });
+    const bridge = emptyArray.map(() => {
+      const randomNumber = generateRandomNumber();
+      return randomNumber === 0 ? 'D' : 'U';
+    });
+
+    return bridge;
+  },
 };
 
 module.exports = BridgeMaker;
