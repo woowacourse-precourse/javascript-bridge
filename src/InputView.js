23d92b418bb61ef8c4d0b4f07eec7c9cf5a9430f
durumi99 <kms990615@naver.com>
2022-11-19 12:30:04 +0900

    feat: 사용자가 이동할 칸을 입력받는 기능 추가

--- src/InputView.js
+++ src/InputView.js
@@ -16,7 +16,12 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    MissionUtils.Console.readLine(MESSAGE.INPUTMOVINGLOCATION, (input) => {
+      this.validateMove(input);
+      return input;
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
