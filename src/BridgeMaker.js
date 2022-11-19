ac800e35c4324dce5e3f385c06f6acac8e8862ec
Choi siun <siun0331@gmail.com>
2022-11-19 17:17:20 +0900

    feat: 다리 생성하는 함수 구현
    makeBridge의 파라미터 generatoeRandomNumber에는 BridgeRandomNumberGenerator.generate가 들어간다.
    생성된 변수를 조건에 따라 문자로 치환한 후 뱅려에 담아 문자열 배열로 리턴한다.

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,13 +1,20 @@
-/**
- * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
- */
 const BridgeMaker = {
   /**
    * @param {number} size 다리의 길이
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
-  makeBridge(size, generateRandomNumber) {},
+  makeBridge(size, generateRandomNumber) {
+    const bridgeInfo = [];
+    for (let index = 0; index < size; index += 1) {
+      bridgeInfo.push(this.directionConverter(generateRandomNumber()));
+    }
+    return bridgeInfo;
+  },
+
+  directionConverter(generatedNumber) {
+    return generatedNumber === 0 ? 'D' : 'U';
+  },
 };
 
 module.exports = BridgeMaker;
