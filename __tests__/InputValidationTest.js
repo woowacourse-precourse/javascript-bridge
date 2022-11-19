e4e747493132f93db35c00223efea8021e34d262
daal2quf <daal2quf@gmail.com>
2022-11-19 21:53:55 +0900

    test(inputValidationTest): ofMove() 기능 테스트
    add:
    ofMove(): user moving input 값이 'U'와 'D'가 아닐때 throw 확인

--- __tests__/InputValidationTest.js
+++ __tests__/InputValidationTest.js
@@ -2,6 +2,15 @@ const InputVaildation = require("../src/InputValidation");
 
 describe('InputValidation 클래스의 ofBridgeLength() 테스트', () => {
   test.each([[1], ['a'], [21], [2], ['aa'], ['2a'], [0]])('3~20 범위 외의 값이 들어왔을때 throw', (input) => {
-    expect(() => { InputVaildation.ofBridgeLength(input); }).toThrow();
+    expect(() => {
+      InputVaildation.ofBridgeLength(input);
+    }).toThrow();
+  });
+});
+describe('InputValidation 클래스의 ofMove() 테스트', () => {
+  test.each([[1], ['u'], ['aa'], ['UU'], [0], ['I']])('유저의 moving input이 "U"와 "D"가 아닐때 throw', (input) => {
+    expect(() => {
+      InputVaildation.ofMove(input);
+    }).toThrow();
   });
 }); 
