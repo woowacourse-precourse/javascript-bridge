5549146d2f384135910a2805b54f725a2fa7df3a
mjmj0217 <mjmj0217@naver.com>
2022-11-19 18:27:23 +0900

    feat(InputView): 재시작 여부 입력 기능 추가

--- src/InputView.js
+++ src/InputView.js
@@ -29,7 +29,11 @@ const InputView = {
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand(makeDecision) {
+    Console.readLine(gameConst.process.INPUT_RESART_MESSAGE + "\n", (cmd) => {
+      makeDecision(cmd);
+    });
+  },
 };
 
 module.exports = InputView;
