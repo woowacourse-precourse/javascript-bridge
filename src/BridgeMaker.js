ce7f7d09f4b1a5861d6516fd632220bc216d7045
한수지 <fatholic5@gmail.com>
2022-11-19 09:22:13 +0900

    :recycle: refactor: 인자로 받은 무작위 값이 메서드 내에서 변경되지않도록 수정 다리의 랜덤 값을 생성할때, 지정된 랜덤 값을 받을 수 있도록 호출 방식을 변경함 랜덤 숫자값을 문자열로 변경할때, 같은 반복문이 처리할 수 있도록 변경하여 반복문의 중복사용을 줄임

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,7 +1,5 @@
-const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
-
-const UPSIDE_NUMBER = 1;
-const DOWNSIDE_NUMBER = 0;
+const UPSIDE_NUMBER = "1";
+const DOWNSIDE_NUMBER = "0";
 const UPSIDE_STRING = "U";
 const DOWNSIDE_STRING = "D";
 
@@ -15,16 +13,17 @@ const BridgeMaker = {
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
+
   makeBridge(size, generateRandomNumber) {
     const bridgeNumbers = [];
+    const bridgeStrings =[];
     for(let index = 0; index < size; index++) {
-      generateRandomNumber = BridgeRandomNumberGenerator.generate();
-      bridgeNumbers.push(generateRandomNumber);
+      const number = generateRandomNumber();
+      bridgeNumbers.push(number);
+      if(bridgeNumbers[index] === UPSIDE_NUMBER) bridgeStrings.push(UPSIDE_STRING);
+      if(bridgeNumbers[index] === DOWNSIDE_NUMBER) bridgeStrings.push(DOWNSIDE_STRING);
     }
-    return bridgeNumbers.map((value) => {
-      if(value === UPSIDE_NUMBER) return value = UPSIDE_STRING;
-      if(value === DOWNSIDE_NUMBER) return value = DOWNSIDE_STRING;
-    })
-  },
+    return bridgeStrings;
+  }
 };
 module.exports = BridgeMaker;
