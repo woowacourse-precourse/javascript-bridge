eaa7c6c3f0e1d9e1673ed7c4591ab87c106d74ab
sujungYu <su_moment@naver.com>
2022-11-19 18:26:05 +0900

    feat(GameCommandValidator) : 입력 받은 재시도 여부 유효성 검사 기능 구현

---
+++ src/validator/GameCommandValidator.js
@@ -0,0 +1,27 @@
+class GameCommandValidator {
+  constructor(moving) {
+    this.validate(moving);
+  }
+
+  validate(moving) {
+    if (this.isNull(moving)) throw new Error('[ERROR] 값을 입력해야 합니다.');
+    if (!this.isAlphabet(moving)) throw new Error('[ERROR] 영어로 입력해야 합니다.');
+    if (this.isNotRightRange(moving)) throw new Error('[ERROR] Q or R 를 입력해야 합니다.');
+    return moving;
+  }
+
+  isNull(moving) {
+    return moving == '';
+  }
+
+  isAlphabet(moving) {
+    const AlphabetTest = /^[a-zA-Z]*$/;
+    return AlphabetTest.test(moving);
+  }
+
+  isNotRightRange(moving) {
+    const range = /[^QR]{1,}/;
+    return range.test(moving);
+  }
+}
+module.exports = GameCommandValidator;
