fff6be1e0b0113e269929324cf8c9774c71c1e2f
2yunseong <dbsdltjd3701@naver.com>
2022-11-19 23:15:13 +0900

    feat(InputValidator): 이동할 칸이 유효한지 판단하는 기능

--- src/InputVaildator.js
+++ src/InputVaildator.js
@@ -1,9 +1,15 @@
+const command = require('./util/command');
+
 const InputVaildator = {
   isVaildLength(input) {
     if (!Number.isInteger(+input)) {
       return false;
     }
     return +input > 0;
+  },
+
+  isVaildStep(input) {
+    return input === command.UP || input === command.DOWN;
   },
 };
 
