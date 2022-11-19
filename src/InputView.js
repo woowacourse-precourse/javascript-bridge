da29a0410440f0a73f599fe2d9337a825442a3b7
ShimSungBo <shimdb96@gmail.com>
2022-11-19 09:34:50 +0900

    feat:(bridge) 사용자에게 이동할 칸 입력값 받기 구현

--- src/InputView.js
+++ src/InputView.js
@@ -14,13 +14,21 @@ const InputView = {
     Console.readLine("다리의 길이를 입력해주세요.\n", (size) => {
       Validation.isVaildBridgeSize(size);
       BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate);
+      this.readMoving();
     });
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    Console.readLine(
+      "\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
+      (block) => {
+        Console.print(block);
+      }
+    );
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
