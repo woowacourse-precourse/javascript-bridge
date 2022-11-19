9aff313650e410747c0853287b9964b61907295c
Jin1won <wlsdnjs16521@gmail.com>
2022-11-19 17:12:33 +0900

    refactor: validate의 isEmptyInput 함수 이름 수정

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -2,12 +2,12 @@ const { ERROR_MESSAGE, BRIDGE_VALUE } = require("../constants");
 const AppError = require("../errors/AppError");
 
 const isBridgeLengthValid = (value) => {
-  if (isEmpty) throw new AppError(ERROR_MESSAGE.EMPTY_ERROR);
+  if (isEmptyInput(value)) throw new AppError(ERROR_MESSAGE.EMPTY_ERROR);
   if (!isNumberType(value)) throw new AppError(ERROR_MESSAGE.TYPE_ERROR);
   if (!isRangeValid(value)) throw new AppError(ERROR_MESSAGE.RANGE_ERROR);
 };
 
-const isEmpty = (value) => !value;
+const isEmptyInput = (value) => !value;
 
 const isNumberType = (value) => !Number.isNaN(Number(value));
 
