53d27057c9aafb2f138b4113090daaa91cb0cf13
LEEJW1953 <jiwonlee1953@naver.com>
2022-11-19 23:11:27 +0900

    test: 다리 생성 테스트 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -62,6 +62,18 @@ describe("다리 건너기 테스트", () => {
     expect(bridge).toEqual(["U", "D", "D"]);
   });
 
+
+    test("다리 생성 테스트2", () => {
+    const randomNumbers = ["1", "0", "0", "1", "0"];
+    const mockGenerator = randomNumbers.reduce((acc, number) => {
+      return acc.mockReturnValueOnce(number);
+    }, jest.fn());
+
+    const bridge = BridgeMaker.makeBridge(5, mockGenerator);
+    expect(bridge).toEqual(["U", "D", "D", "U", "D"]);
+  });
+
+
   test("기능 테스트", () => {
     const logSpy = getLogSpy();
     mockRandoms(["1", "0", "1"]);
