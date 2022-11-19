ef58f1162eb6342e419ee4d874fbc08d4b163ee7
froggy1014 <clcl6084@gmail.com>
2022-11-19 19:55:24 +0900

    feat[Model]: 이동한 칸에 대한 맵 그리는 기능
    1. checkAnswer() 메소드의 반환값과 사용자 입력값을 받아 2차 배열로 맵을 반환

--- src/model/BridgeModel.js
+++ src/model/BridgeModel.js
@@ -1,5 +1,6 @@
 const BridgeMaker = require('../BridgeMaker');
 const BridgeRandomNumberGenerator = require('../BridgeRandomNumberGenerator');
+const { DIRECTION } = require('../constants');
 const Bridge = require('../Bridge');
 
 class BridgeModel {
@@ -8,11 +9,13 @@ class BridgeModel {
     this.tryCount = 1;
     this.moveCount = 0;
     this.isRight = false;
+    this.map = [[],[]];
   }
 
   updateState(direction, isRight) {
     this.moveCount++;
     this.isRight = isRight;
+    this.paintMap(direction, isRight);
   }
 
   buildBridge(length) {
@@ -24,8 +27,20 @@ class BridgeModel {
 
   stepForward(direction) {
     const [isRight, isDone] = this.bridge.checkAnswer(this.moveCount, direction);
-    updateState(direction, isRight);
+    this.updateState(direction, isRight);
+    console.log(this.map);
     return [isRight, isDone];
+  }
+
+  paintMap(direction, isRight) {
+    this.map.forEach((path) => path.push(' '));
+    this.map[DIRECTION[direction]].pop();
+    switch(isRight){
+      case(true):
+        return this.map[DIRECTION[direction]].push('O');
+      default:
+        return this.map[DIRECTION[direction]].push('X');    
+    }
   }
 
 }
