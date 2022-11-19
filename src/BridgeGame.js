e7d4d5f7cfb17be4e94da80c19f7670eb4fff020
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-11-20 06:00:54 +0900

    ✨ [feat]: 플레이어가 선택한 칸이 다리의 칸과 같은 방향인지 판별하는 메서드 추가

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,17 +1,33 @@
 const InputView = require("./View/InputView");
+const MissionUtils = require("@woowacourse/mission-utils");
+const OutputView = require("./View/OutputView");
+const { GAME } = require("./Constants");
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
-  constructor(bridge) {
-    this.bridge = bridge;
+  constructor(computerBridge) {
+    this.computerBridge = computerBridge;
+    this.playerUpperBridge = [];
+    this.playerLowerBridge = [];
   }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(direction) {
+    const canCross = this.isSameDirection(direction);
+
+    this.isSuccess = canCross ? GAME.SUCCESS : GAME.FAIL;
+  }
+
+  isSameDirection(direction) {
+    const currPosition = this.playerUpperBridge.length;
+
+    return this.computerBridge[currPosition] === direction;
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
