fc15ae25b2bf13db3c4a0c900afa71108c4b146b
SangDon <adoim@naver.com>
2022-11-20 00:59:44 +0900

    Refactor: 출력 클래스 분리

--- src/view/Printer.js
+++ src/view/Printer.js
@@ -1,11 +1,12 @@
 const OutputView = require("../console/OutputView");
 const Message = require("../lib/Message");
+const MissionUtils = require("@woowacourse/mission-utils");
 
 class Printer {
-  #bridgeGame;
+  #bridgeGame2;
 
-  constructor(bridgeGame) {
-    this.#bridgeGame = bridgeGame;
+  constructor(bridgeGame2) {
+    this.#bridgeGame2 = bridgeGame2;
   }
 
   printEndResult(bridges, gameState) {
@@ -17,4 +18,12 @@ class Printer {
   sayHello() {
     OutputView.printLine(Message.GAME_START);
   }
+
+  printBridge(bridges) {
+    // const [upBridge, downBridge] = bridges;
+    // OutputView.printMap(upBridge, downBridge);
+    OutputView.printMap(...bridges);
+  }
 }
+
+module.exports = Printer;
