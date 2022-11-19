b0e4053eb4bc5b33bd640a0c29254c65a1cb21a9
yeongi <cbg5255@gmail.com>
2022-11-19 23:03:45 +0900

    feat: Player 기능 구현

--- src/Bridge.Domain/Player.js
+++ src/Bridge.Domain/Player.js
@@ -1,3 +1,25 @@
-class Player {}
+class Player {
+  #bridgePostion;
+  #gameTryCount;
+
+  constructor() {
+    this.#bridgePostion = 0;
+    this.#gameTryCount = 1;
+  }
+
+  getCurrentPositionAndMovePlayer() {
+    return this.#bridgePostion++;
+  }
+
+  bridgeGameRetry() {
+    this.#bridgePostion = 0;
+    this.#gameTryCount++;
+    return this;
+  }
+
+  getBridgeGameTryCount() {
+    return this.#gameTryCount;
+  }
+}
 
 exports.module = Player;
