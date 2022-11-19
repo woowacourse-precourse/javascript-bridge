ea851d56648f0ecec4377f8a66db4a7edbb22886
Chanwoong1 <jcw7468@gmail.com>
2022-11-19 20:47:05 +0900

    chore: OutputView 객체에서 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-const { Console } = require("@woowacourse/mission-utils");
 const InputView = require("./InputView");
+const OutputView = require("./OutputView");
 
 class App {
   constructor() {
   }
 
   play() {
-    Console.print("다리 건너기 게임을 시작합니다.\n");
+    OutputView.startGame();
     InputView.readBridgeSize();
   }
 }
