87b6fe150e15c00afbf6596448e360df6aaebb5d
Choi siun <siun0331@gmail.com>
2022-11-19 16:14:58 +0900

    style: 공통 상수 세팅

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+module.exports = Object.freeze({
+  START_MESSAGE: '다리 건너기 게임을 시작합니다.\n',
+  MESSAGE_INPUT_BRIDGE_LENGTH: '\n다리의 길이를 입력해주세요.\n',
+  MESSAGE_NEXT_MOVING_INPUT: '\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
+  MESSAGE_RETRY: '\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
+  MESSAGE_RESULT: '\n최종 게임 결과\n',
+  MESSAGE_SUCCESS_BOOLEAN: '게임 성공 여부: ',
+  MESSAGE_TRY_COUNT: '총 시도한 횟수: ',
+});
