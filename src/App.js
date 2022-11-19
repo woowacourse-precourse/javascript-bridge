afb6ed94148fae35832c5e20a4fdd6beb9164548
durumi99 <kms990615@naver.com>
2022-11-19 12:59:22 +0900

    feat: 게임을 시작하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const OutputView = require('./OutputView');
+
 class App {
-  play() {}
+  play() {
+    OutputView.printInit();
+    this.initGame();
+  }
+
+  initGame() {
+
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
