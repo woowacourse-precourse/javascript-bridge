f21860b7dcf229cd535689ba5cfdbfdd71460d3e
leeuihyun <meta212121@naver.com>
2022-11-19 14:27:45 +0900

    feat : 다리의 길이를 입력받는 메서드에 틀 작성

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,5 @@
+const { Console } = require('@woowacourse/mission-utils');
+const { MESSAGE } = require('./lib/constants');
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,7 +7,12 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine(MESSAGE.INPUT_BRIDGE_SIZE, (size) => {
+      try {
+      } catch (error) {}
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
