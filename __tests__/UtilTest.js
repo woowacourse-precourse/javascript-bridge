4e18c249ca1c23711806e484ff07592403499ce4
Yeom-JinHo <pwlsghq@naver.com>
2022-11-19 22:19:28 +0900

    test : 이동할 칸 선택

--- __tests__/UtilTest.js
+++ __tests__/UtilTest.js
@@ -1,5 +1,5 @@
-const { ERROR_MSG } = require('../src/constants');
-const { validateBridgeSize } = require('../src/Util');
+const { ERROR_MSG, POSITIONS } = require('../src/constants');
+const { validateBridgeSize, validatePosition } = require('../src/Util');
 
 describe('유틸 테스트', () => {
   test('다리 길이 유효성', () => {
@@ -11,6 +11,17 @@ describe('유틸 테스트', () => {
     const validSize = [3, 5, 6, 9, 19];
     validSize.forEach(size => {
       expect(validateBridgeSize(size)).toEqual(true);
+    });
+  });
+
+  test('이동할 칸 선택 유효성', () => {
+    const invalidPosition = [1, 2, 'u', 'd', 'UP', 'DOWN'];
+    invalidPosition.forEach(position => {
+      expect(validatePosition(position)).toEqual(false);
+    });
+
+    POSITIONS.forEach(position => {
+      expect(validatePosition(position)).toEqual(true);
     });
   });
 });
--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,13 @@
 const MIN_BRIDGE_SIZE = 3;
 const MAX_BRIDGE_SIZE = 20;
+const POSITIONS = ['D', 'U'];
 
 const OUTPUT_MSG = Object.freeze({
   start: '다리 건너기 게임을 시작합니다.'
 });
 const QUESTIONS = Object.freeze({
-  bridgeSize: '다리의 길이를 입력해주세요.'
+  bridgeSize: '다리의 길이를 입력해주세요.',
+  movePosition: '이동할 칸을 선택해주세요. (위: U, 아래: D)'
 });
 const ERROR_MSG = Object.freeze({
   invalidBridgeSize: `[ERROR] 다리 길이는 ${MIN_BRIDGE_SIZE} 이상 ${MAX_BRIDGE_SIZE}이하의 숫자를 입력해주세요`
@@ -16,5 +18,6 @@ module.exports = {
   QUESTIONS,
   ERROR_MSG,
   MAX_BRIDGE_SIZE,
-  MIN_BRIDGE_SIZE
+  MIN_BRIDGE_SIZE,
+  POSITIONS
 };
