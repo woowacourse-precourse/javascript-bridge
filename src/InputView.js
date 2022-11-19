f78cd870f49d015dbd1c8543f2d2010f5589333e
2yunseong <dbsdltjd3701@naver.com>
2022-11-20 04:52:50 +0900

    feat(InputView): 사용자 명령어 입력을 받아오는 기능

--- src/InputView.js
+++ src/InputView.js
@@ -28,7 +28,11 @@ const InputView = {
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand(callback, message = '') {
+    MissionUtils.Console.readLine(message, (answer) => {
+      callback(answer);
+    });
+  },
 };
 
 module.exports = InputView;
