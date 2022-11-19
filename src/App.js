729d3c2dcca8956eca819e7ee0d03a3556ade90f
Circlewee <pnj1118@naver.com>
2022-11-19 16:28:50 +0900

    feat(App): 다리 길이 입력 (#7)
    1. 시작 안내 메시지 출력
    2. readBridgeSize를 통해 입력을 받고 프로퍼티에 담음

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+const BridgeGame = require('./BridgeGame');
+const Validation = require('./Validation');
+const InputView = require('./InputView');
+const OutputView = require('./OutputView');
+const GameMessage = require('./constants/GameMessage');
+
 class App {
-  play() {}
+  /** @type {BridgeGame} */
+  #GameClient = new BridgeGame();
+
+  /** @type {number} */
+  #bridgeLength;
+
+  /**
+   * @param {string} input
+   */
+  #setBridgeLength(input) {
+    Validation.Bridge(input);
+    this.#bridgeLength = Number(input);
+  }
+
+  play() {
+    OutputView.printMessage(GameMessage.WELCOME);
+    InputView.readBridgeSize(this.#setBridgeLength.bind(this));
+  }
 }
 
 module.exports = App;
