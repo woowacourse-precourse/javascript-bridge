4c98f9cfecc6157d47482a95c4baaacad2058a59
Buyoung Chung <sunarol@naver.com>
2022-11-19 16:22:39 +0900

    test: size input exception test
    use test.each

--- __tests__/MyTest.js
+++ __tests__/MyTest.js
@@ -1,5 +1,10 @@
 const BridgeMaker = require("../src/BridgeMaker");
 const BridgeRandomNumberGenerator = require("../src/BridgeRandomNumberGenerator");
+const GameController = require("../src/Controller/GameController");
+
+const ExceptionHandler = require("../src/utils/ExceptionHandler");
+
+const gameController = new GameController();
 
 describe("다리 건너기 테스트", () => {
   test("다리 생성 길이 테스트", () => {
@@ -15,4 +20,15 @@ describe("다리 건너기 테스트", () => {
 
     expect(BridgeMaker.makeBridge(size, upDownMocks)).toEqual(["U", "D"]);
   });
+
+  test.each([["2"], ["21"]])("다리 길이 입력 범위에 대한 예외 처리", (input) => {
+    const size = input;
+    const validateTest = (size) => {
+      gameController.constructBridgeGameAndSaveAnswerBridge(size);
+    };
+    expect(() => {
+      validateTest(size);
+    }).toThrow("[ERROR] 3부터 20 사이의 숫자를 입력해주세요.");
+  });
 });
+//  "[ERROR] 3부터 20 사이의 숫자를 입력해주세요."
