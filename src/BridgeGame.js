358769095a216c1c135ceb694b79464c372a6b2c
tkdrb12 <tkdrb15@gmail.com>
2022-11-19 15:16:07 +0900

    feat : 플레이어가 칸을 이동하는 기능 추가

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,20 +1,38 @@
+const { generate } = require("./BridgeRandomNumberGenerator");
+const { makeBridge } = require("./BridgeMaker");
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  #player
+  #bridgeShape
+  #playerMoving
+
+  constructor(size){
+    this.#bridgeShape = makeBridge(size, generate);
+    this.#playerLocation = -1;
+  }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(playerMoving) {
+    if(bridgeShape.length > this.#playerLocation){
+      this.#playerLocation++;
+      this.#playerMoving = playerMoving;
+    }
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  retry() {}
+  retry() {
+
+  }
 }
 
 module.exports = BridgeGame;
