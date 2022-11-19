ce9e720a4b78dd89da3c7dcf7cd02c0a03908525
ShimSungBo <shimdb96@gmail.com>
2022-11-19 14:23:44 +0900

    feat:(bridge) 사용자의 입력값에 따라서 다리를 건넌다.
    1. 입력 된 값에 "X"가 포함되어 있으면 readGameCommand로 이동한다.
    2. 입력 된 값에 "X"가 없으면 계속해서 게임을 진행한다.

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -30,6 +30,7 @@ class BridgeGame {
     }
 
     OutputView.printMap(movingList);
+    return movingList;
   }
 
   /**
--- src/InputView.js
+++ src/InputView.js
@@ -32,7 +32,11 @@ const InputView = {
       (moving) => {
         Validation.isVaildMoving(moving);
         const bridgeGame = new BridgeGame();
-        bridgeGame.move(moving, bridge, moveList);
+        const checkContinue = bridgeGame.move(moving, bridge, moveList);
+        if (checkContinue[0].includes("X") || checkContinue[1].includes("X")) {
+          return this.readGameCommand();
+        }
+        return this.readMoving(bridge, checkContinue);
       }
     );
   },
@@ -40,7 +44,14 @@ const InputView = {
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+    Console.readLine(
+      "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n",
+      (line) => {
+        // 재시도 및 종료 상황 구현하기
+      }
+    );
+  },
 };
 
 module.exports = InputView;
