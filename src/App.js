ecc1b4f25077d8a83dfa88e6a47ba7ceb905e90e
2yunseong <dbsdltjd3701@naver.com>
2022-11-19 23:55:33 +0900

    feat(App): 다리 정보를 설정하는 기능

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+const BridgeMaker = require('./BridgeMaker');
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
 const InputValidator = require('./InputValidator');
 const InputView = require('./InputView');
 const OutputView = require('./OutputView');
@@ -5,7 +7,9 @@ const OutputView = require('./OutputView');
 class App {
   constructor() {
     this.bridgeSize;
+    this.bridges;
   }
+
   play() {
     InputView.readBridgeSize(this.handleInputLength.bind(this));
   }
@@ -15,11 +19,20 @@ class App {
       if (!InputValidator.isValidLength(input)) {
         throw new Error('[ERROR] : 유효한 길이가 아닙니다.');
       }
-      this.bridgeSize = +input;
+      this.initBridges(+input);
+      InputView.readMoving(this.handleInputStep.bind(this));
     } catch (error) {
       OutputView.printMessage(error.message);
       InputView.readBridgeSize(this.handleInputLength.bind(this));
     }
+  }
+
+  initBridges(size) {
+    this.bridgeSize = size;
+    this.bridges = BridgeMaker.makeBridge(
+      this.bridgeSize,
+      BridgeRandomNumberGenerator.generate
+    );
   }
 }
 
