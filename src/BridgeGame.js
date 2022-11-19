fca1342cae5e8958f5b73346a970520f8684a0a7
daal2quf <daal2quf@gmail.com>
2022-11-19 19:17:08 +0900

    feat(bridgeGame): 메서드분리 및 잘못 인지한 게임규칙으로 인한 코드변경
    add:
    generateOfBridgeGamePassword(): 멤버변수로 인스턴스 생성시 할당하던 다리게임정답을 분리
    stackOfUserMovingInput(): 유저가 입력한 moving 값을 userInput에 쌓음

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,15 +1,21 @@
 const BridgeMaker = require("./BridgeMaker");
 const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
-const InputView = require("./InputView");
-
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
   #password;
-  constructor(bridgeLength) {
-    this.#password = BridgeMaker.makeBridge(bridgeLength, BridgeRandomNumberGenerator.generate);
+  constructor() {
+    this.#password;
     this.userInput = [];
+  }
+
+  generateOfBridgeGamePassword(bridgeLength) {
+    this.#password = BridgeMaker.makeBridge(bridgeLength, BridgeRandomNumberGenerator.generate);
+  }
+
+  stackOfUserMovingInput(userMovingInput) {
+    this.userInput.push(userMovingInput);
   }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -26,9 +32,6 @@ class BridgeGame {
   retry() {
   }
 
-  setBeginningMapToUserInput() {
-    this.userInput.push(this.#password[0]);
-  }
 }
 
 module.exports = BridgeGame;
