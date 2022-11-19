fcf6f5951e4484738e3cf4863e284db31124ddc2
wzrabbit <curious.wzrabbit@gmail.com>
2022-11-20 01:10:49 +0900

    feat(inputview): 이동 방향 입력받는 기능 구현

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -5,6 +5,10 @@ const InputView = {
   readBridgeSize(callback) {
     Console.readLine(MESSAGE.ASK_BRIDGE_LENGTH, callback);
   },
+
+  readMoving(callback) {
+    Console.readLine(MESSAGE.ASK_SELECT_MOVE, callback);
+  },
 };
 
 module.exports = InputView;
