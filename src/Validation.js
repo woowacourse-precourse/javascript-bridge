faf0cec6806625c8d297923f88fd2992cbfa8bee
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2022-11-19 14:26:32 +0900

    feat: 다리 길이가 3 이상 20 이하가 아닌 경우 예외 처리 후 예외 메세지 출력 및 다시 입력받기

--- src/Validation.js
+++ src/Validation.js
@@ -10,6 +10,17 @@ const Validation = {
       Console.print(error.message);
       return true;
     }
+
+    try {
+      if (!(inputValue >= 3 && inputValue <= 20)) {
+        throw new Error(
+          "[ERROR] 다리 길이는 3 이상 20 이하의 숫자를 입력해야 합니다."
+        );
+      }
+    } catch (error) {
+      Console.print(error.message);
+      return true;
+    }
   },
 };
 
