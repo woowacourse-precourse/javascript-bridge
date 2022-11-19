86714503563a40f2a74e23d8e99d93c43fb3d411
areumsheep <areumsheep@gmail.com>
2022-11-20 00:22:09 +0900

    feat(move): 사용자가 입력한 값과 랜덤으로 생성된 값으로 다리 생성

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -7,12 +7,37 @@ class BridgeGame {
     U: 1,
   };
 
+  #randomBridge = [];
+  #userBridge = [];
+
+  setRandomBridge(bridge) {
+    this.#randomBridge = bridge;
+  }
+
+  setUserBlock(block) {
+    this.#userBridge.push(block);
+  }
+
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move() {
+    return this.#userBridge.reduce(
+      (acc, block, index) => {
+        if (this.#randomBridge[index] === 'D') {
+          acc[0].push(this.#randomBridge[index] === block ? 'O' : 'X');
+          acc[1].push(' ');
+        } else {
+          acc[1].push(this.#randomBridge[index] === block ? 'O' : 'X');
+          acc[0].push(' ');
+        }
+        return acc;
+      },
+      [[], []],
+    );
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
--- src/InputView.js
+++ src/InputView.js
@@ -3,6 +3,11 @@ const BridgeRandomNumberGenerator = require('./utils/BridgeRandomNumberGenerator
 const MESSAGES = require('./constants/Messages');
 
 const BridgeMaker = require('./BridgeMaker');
+const BridgeGame = require('./BridgeGame');
+
+const OutputView = require('./OutputView');
+
+const bridgeGame = new BridgeGame();
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -15,15 +20,23 @@ const InputView = {
     Console.readLine(MESSAGES.INPUT_BRIDGE_LENGTH, (length) => {
       // TODO length validation check
       const bridge = BridgeMaker.makeBridge(length, BridgeRandomNumberGenerator.generate);
+      bridgeGame.setRandomBridge(bridge);
+
+      this.readMoving();
     });
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving(bridge) {
+  readMoving() {
     Console.readLine(MESSAGES.INPUT_MOVE_BLOCK, (block) => {
       // TODO block validation check
+      bridgeGame.setUserBlock(block);
+      const formattedBridges = bridgeGame.move();
+      OutputView.printMap(formattedBridges);
+
+      this.readMoving();
     });
   },
 
