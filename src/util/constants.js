6c7e8d9cf69a680c6e483a6427d49515d878278d
dim <dim@student.42seoul.kr>
2022-11-19 23:59:18 +0900

    Feat: 프린트 메세지 상수화

---
+++ src/util/constants.js
@@ -0,0 +1,10 @@
+const PRINT_MESSAGE = {
+  START: '다리 건너기 게임을 시작합니다.',
+  BRIDGE_LENGTH: '다리의 길이를 입력해주세요.',
+  BRIDGE_TO_MOVE: '이동할 칸을 선택해주세요. (위: U, 아래: D)',
+  RETRY: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)',
+  GAME_RESULT: `게임 성공 여부: ${result}`,
+  TRY_COUNT: `총 시도한 횟수: ${tryCount}`,
+};
+
+module.exports = { PRINT_MESSAGE };
