b5db0a5d4a9660583f4a540f0ffdb51f6b8e9593
하지훈 <jnh03336@gmail.com>
2022-11-19 12:19:03 +0900

    feat(view): 게임 결과 출력

--- src/OutputView.js
+++ src/OutputView.js
@@ -50,12 +50,21 @@ const OutputView = {
     MissionUtils.Console.print(bridgeView);
   },
 
+  RESULT_VIEW: (gameResult, tryCount) =>
+    `게임 성공 여부: ${
+      gameResult ? '성공' : '실패'
+    }\n총 시도한 횟수: ${tryCount}`,
+
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-   * <p>
+   * @param {boolean} gameResult 여태까지의 움직임 점보를 받는다.
+   * @param {number} tryCount
+   * @return {void} 출력
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() {},
+  printResult(gameResult, tryCount) {
+    MissionUtils.Console.print(this.RESULT_VIEW(gameResult, tryCount));
+  },
 };
 
 module.exports = OutputView;
