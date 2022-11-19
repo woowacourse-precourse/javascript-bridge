0816dbfa469c53dd39f8daf443fa1f37423b4656
pakxe <pigkill40@naver.com>
2022-11-19 21:49:59 +0900

    feat(OutputView): 이동 여부에 따라서 O, X로 다리 출력 메서드 작성

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,5 +1,5 @@
 const { Console } = require('@woowacourse/mission-utils');
-const { SENTENCE } = require('../constants/Constants');
+const { SENTENCE, BRIDGE, RESULT } = require('../constants/Constants');
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -8,13 +8,49 @@ const OutputView = {
   printStart() {
     Console.print(SENTENCE.START);
   },
-  /**
-   * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-   * <p>
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  printMap() {},
 
+  printMap(partialBridgeMap, lastMoving) {
+    const partialBridgeLength = partialBridgeMap.length;
+    const printMap = this.initPrintBridgeMap(partialBridgeLength); // ' '로 초기화된 다리 배열
+    const lastDirection = partialBridgeMap[partialBridgeLength - 1];
+
+    partialBridgeMap.reduce(this.checkDirection, printMap);
+
+    if (!lastMoving)
+      this.amendLastBridge(printMap, lastDirection, partialBridgeLength);
+
+    const { openBracket, closeBracket } = RESULT;
+
+    printMap.forEach((direction) =>
+      Console.print(
+        `${openBracket} ${direction.join(RESULT.divider)} ${closeBracket}`
+      )
+    );
+  },
+
+  checkDirection(printMap, side, i) {
+    const [up, down] = printMap;
+    if (side === BRIDGE.up) up[i] = RESULT.correct;
+    if (side === BRIDGE.down) down[i] = RESULT.correct;
+    return printMap;
+  },
+
+  amendLastBridge([up, down], lastSide, partialBridgeLength) {
+    if (lastSide === BRIDGE.up) {
+      up[partialBridgeLength - 1] = RESULT.blank;
+      down[partialBridgeLength - 1] = RESULT.incorrect;
+    }
+    if (lastSide === BRIDGE.down) {
+      down[partialBridgeLength - 1] = RESULT.blank;
+      up[partialBridgeLength - 1] = RESULT.incorrect;
+    }
+  },
+
+  initPrintBridgeMap(partialBridgeLength) {
+    return Array.from(Array(BRIDGE.width), () =>
+      Array(partialBridgeLength).fill(RESULT.blank)
+    );
+  },
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
    * <p>
