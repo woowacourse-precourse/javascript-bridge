da7a2212839a52156a90f5ebbbf7bf69c85f0203
semnil5202 <semnil5202@gmail.com>
2022-11-19 19:29:20 +0900

    feat: 입력받은 다리의 길이만큼 건널 수 있는 다리의 위치를 생성하는 기능

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -7,7 +7,22 @@ const BridgeMaker = {
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
-  makeBridge(size, generateRandomNumber) {},
+  makeBridge(size, generateRandomNumber) {
+    const bridge = [];
+    for (let i = 0; i < size; i++) {
+      const answer = generateRandomNumber();
+      const crossable = this.whereCrossable(answer);
+      bridge.push(crossable);
+    }
+    return bridge;
+  },
+
+  whereCrossable(answer) {
+    const UP = 'U';
+    const DOWN = 'D';
+    if (answer === 0) return DOWN;
+    return UP;
+  },
 };
 
 module.exports = BridgeMaker;
