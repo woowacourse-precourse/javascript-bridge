3e9be3c9af5f5d2e182aaa872508e9cfa4774392
inthhh <glass005533@gmail.com>
2022-11-20 01:14:48 +0900

    refactor: BridgeGame 코드 정리

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,14 +1,10 @@
 class BridgeGame {
   move(turnNumber, bridge, moveUpDown) {
     if (bridge[turnNumber] == moveUpDown) {
-      if (turnNumber == bridge.length - 1) {
-        return 0;
-      }
+      if (turnNumber == bridge.length - 1) return 0;
       return 1;
     }
-    if (bridge[turnNumber] != moveUpDown) {
-      return -1;
-    }
+    if (bridge[turnNumber] != moveUpDown) return -1;
   }
 
   retry(isRetry) {
