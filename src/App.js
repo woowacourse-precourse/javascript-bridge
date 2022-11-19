19689cfb98b5896b839527def0b8b3f40634336b
jjo-mi <whdal2636@naver.com>
2022-11-19 23:07:56 +0900

    feat: 4. 이동할 칸 사용자 입력 및  출력(입력값 유효성 검사, 재시작)

--- src/App.js
+++ src/App.js
@@ -13,8 +13,27 @@ class App {
         throw new Error(ERROR_MESSAGE.LENGTH);
       }
       const bridgeArr = bridgeMaker.makeBridge(size, bridgeRandomNumberGenerator.generate);
-    });
 
+      this.move();
+    });
+  }
+
+  /** 4-1. 이동할 칸 입력 및 입력값 */
+  move() {
+    inputView.readMoving((movingInput) => {
+      this.validMovingInput(movingInput);
+    });
+  }
+  /** 4-2. 이동할 칸 입력값 유효성 및 에러시 입력 재시작 */
+  validMovingInput(movingInput) {
+    if(movingInput !== "U" && movingInput !== "D") {
+      try {
+        throw new Error(ERROR_MESSAGE.MOVE);
+      } catch(e) {
+        Console.print(e.message);
+        this.move();
+      }
+    }
   }
 
 }
--- src/InputView.js
+++ src/InputView.js
@@ -15,10 +15,10 @@ const InputView = {
     Console.readLine(GUIDE_MESSAGE.INPUT_LENGTH, callback);
   },
 
-  /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
-  readMoving() {},
+  /** 4. 사용자에게 이동할 칸 입력 및 값 출력 */
+  readMoving(callback) {
+    Console.readLine(GUIDE_MESSAGE.INPUT_MOVE, callback);
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
