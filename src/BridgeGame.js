16d022b22829058ed72830a4f107ad498e3505df
sujungYu <su_moment@naver.com>
2022-11-19 17:55:45 +0900

    feat(BridgeGame) : 입력 받은 다리 길이 예외 처리 기능 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,7 +1,32 @@
+const { Console } = require('@woowacourse/mission-utils');
+const InputView = require('./InputView');
+const BridgeSizeValidator = require('./validator/BridgeSizeValidator');
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
+
 class BridgeGame {
+  #bridgeSize;
+  constructor() {}
+
+  start() {
+    Console.print('다리 건너기 게임을 시작합니다.\n');
+    this.inputBridgeSize();
+  }
+
+  inputBridgeSize() {
+    InputView.readBridgeSize(this.validateBridgeSize.bind(this));
+  }
+
+  validateBridgeSize(bridgeSize) {
+    try {
+      new BridgeSizeValidator(bridgeSize);
+      this.#bridgeSize = bridgeSize;
+    } catch (error) {
+      Console.print(error);
+      this.inputBridgeSize();
+    }
+  }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
