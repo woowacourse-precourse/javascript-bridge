3760860997cf2b7a25c71ae87a7de6e1030b8385
hyunjin <leehj0110@kakao.com>
2022-11-19 19:08:42 +0900

    feat: Result 클래스

--- src/models/Result.js
+++ src/models/Result.js
@@ -1,5 +1,43 @@
+const { printMap, printResult } = require('../views/OutputView');
+
 class Result {
-  print() {}
+  result;
+  canMoveForward;
+
+  constructor(bridge, index, canMoveForward) {
+    this.canMoveForward = canMoveForward;
+    this.result = this.makeLine(bridge, 'U', index);
+    this.result += this.addSuffix(bridge.getBridgeFragmentByIndex(index), canMoveForward, 'U');
+    this.result += '\n';
+    this.result += this.makeLine(bridge, 'D', index);
+    this.result += this.addSuffix(bridge.getBridgeFragmentByIndex(index), canMoveForward, 'D');
+  }
+
+  makeLine(bridge, rightDirection, index) {
+    let string = '[';
+
+    for (let i = 0; i < index; i++) {
+      const direction = bridge.getBridgeFragmentByIndex(i);
+      if (direction === rightDirection) string += ' O ' + '|';
+      else string += '   ' + '|';
+    }
+
+    return string;
+  }
+
+  addSuffix(lastFragment, canMoveForward, rightDirection) {
+    if (canMoveForward) {
+      if (rightDirection === lastFragment) return ' O ]';
+      return '   ]';
+    }
+
+    if (rightDirection === lastFragment) return ' X ]';
+    return '   ]';
+  }
+
+  print() {
+    printMap(this.result);
+  }
 }
 
 module.exports = Result;
