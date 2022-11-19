a0cb8551c29a633ca3cc65fb0939d918ffdc66f8
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-11-19 19:11:28 +0900

    ✨ [feat]: 다리의 길이를 입력받는 callback 함수

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+const InputView = require("./View/InputView");
+const OutputView = require("./View/OutputView");
+const { makeBridge } = require("./BridgeMaker");
+const BridgeGame = require("./BridgeGame");
+const { generate } = require("./BridgeRandomNumberGenerator");
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  constructor() {
+    this.bridgeGame = null;
+  }
+
+  play() {
+    OutputView.printStart();
+    this.requestBridgeSize();
+  }
+
+  requestBridgeSize() {
+    InputView.readBridgeSize((bridgeSize) => {
+      const bridge = makeBridge(bridgeSize, generate);
+      this.bridgeGame = new BridgeGame(bridge);
+
+      MissionUtils.Console.print(bridge);
+      this.requestMoving();
+    });
+  }
+
+  requestMoving() {
+    InputView.readMoving((input) => {
+      
+    })
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
