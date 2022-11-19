58a818445ea16701da8037390b43187f54dedbf9
하지훈 <jnh03336@gmail.com>
2022-11-19 12:13:51 +0900

    feat(view): 다리 출력

---
+++ __tests__/OutputViewTest.js
@@ -0,0 +1,31 @@
+const OutputView = require('../src/OutputView');
+
+describe('Ouput View 테스트', () => {
+  test('다리 한 줄을 조건에 따라 생성', () => {
+    const selections = [true, false, true, true, false];
+    const movesPossible = [true, true, true, true, true];
+    expect(OutputView.makeOneLinefBridgeView(selections, movesPossible)).toBe(
+      '[ O |   | O | O |   ]'
+    );
+
+    const selections2 = [false, false, true, true, true];
+    const movesPossible2 = [true, true, true, true, false];
+    expect(OutputView.makeOneLinefBridgeView(selections2, movesPossible2)).toBe(
+      '[   |   | O | O | X ]'
+    );
+  });
+
+  test('조건에 따라 다리 생성', () => {
+    const moveStatus = ['U', 'U', 'D', 'D', 'U'];
+    const movesPossible = [true, true, true, true, true];
+    expect(OutputView.makeBridgeView(moveStatus, movesPossible)).toBe(
+      '[ O | O |   |   | O ]\n[   |   | O | O |   ]'
+    );
+
+    const moveStatus2 = ['D', 'U', 'D', 'D', 'U'];
+    const movesPossible2 = [true, true, true, true, false];
+    expect(OutputView.makeBridgeView(moveStatus2, movesPossible2)).toBe(
+      '[   | O |   |   | X ]\n[ O |   | O | O |   ]'
+    );
+  });
+});
--- src/OutputView.js
+++ src/OutputView.js
@@ -1,13 +1,54 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 const OutputView = {
   /**
    * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-   * <p>
+   * @param {boolean[]} selections 여태까지의 움직임 점보를 받는다.
+   * @param {boolean[]} movePossible 각각의 움직임이 가능한지 받는다.
+   * @return {string} 출력
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {},
+  makeOneLinefBridgeView(selections, movesPossible) {
+    const chars = [];
+    for (let i = 0; i < selections.length; i += 1) {
+      if (!selections[i]) chars.push(' ');
+      if (selections[i] && movesPossible[i]) chars.push('O');
+      if (selections[i] && !movesPossible[i]) chars.push('X');
+    }
+    return `[ ${chars.join(' | ')} ]`;
+  },
+
+  /**
+   * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
+   * @param {('U' | 'D')[]} moveStatus 여태까지의 움직임 점보를 받는다.
+   * @param {boolean[]} movesPossible 각각의 움직임이 가능한지 받는다.
+   * @return {string} 출력
+   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+   */
+  makeBridgeView(moveStatus, movesPossible) {
+    return `${this.makeOneLinefBridgeView(
+      moveStatus.map((elem) => elem === 'U'),
+      movesPossible
+    )}\n${this.makeOneLinefBridgeView(
+      moveStatus.map((elem) => elem === 'D'),
+      movesPossible
+    )}`;
+  },
+
+  /**
+   * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
+   * @param {('U' | 'D')[]} moveStatus 여태까지의 움직임 점보를 받는다.
+   * @param {boolean[]} movesPossible 각각의 움직임이 가능한지 받는다.
+   * @return {void} 출력
+   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+   */
+  printMap(moveStatus, movesPossible) {
+    const bridgeView = this.makeBridgeView(moveStatus, movesPossible);
+    MissionUtils.Console.print(bridgeView);
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
