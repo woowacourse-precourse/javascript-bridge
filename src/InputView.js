1ff0f9a36c93313306d4a2fd320b668839201f05
TeusLee <xodn1311@gmail.com>
2022-11-19 19:45:26 +0900

    complete INPUTVIEW to play all game sequence

--- src/InputView.js
+++ src/InputView.js
@@ -17,27 +17,26 @@ const InputView = {
       }
       MissionUtils.Console.print(`${bridge_len}`);
       Game_Bridge = BridgeMaker.makeBridge(bridge_len, randnum_gen)             
-      this.readMoving(Game_Bridge, 0)
+      this.readMoving(Game_Bridge, 0, 0)
     });
   },
 
   /**
   * 사용자가 이동할 칸을 입력받는다.
   */
-  readMoving(Game_Bridge, curr_loc) {
+  readMoving(Game_Bridge, curr_loc, retry_cnt) {
     MissionUtils.Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)', (move_dir) => {
       MissionUtils.Console.print(`${move_dir}`);      
       OutputView.printMap(Game_Bridge, move_dir, curr_loc)
       if (Game_Bridge[curr_loc] !== move_dir){
         //다리를 맞추는데 실패한 경우
-        this.readGameCommand(Game_Bridge)
+        this.readGameCommand(Game_Bridge, curr_loc, retry_cnt)
       } else if (Game_Bridge[curr_loc] === move_dir) {
-        //다리를 맞추는데 성공한 경우
-        curr_loc += 1
-        if (curr_loc === Game_Bridge.length){
-          this.readGameCommand()
+        //다리를 맞추는데 성공한 경우         
+        if (curr_loc+1 === Game_Bridge.length){
+          OutputView.printResult(Game_Bridge, curr_loc, retry_cnt)
         } else {
-          this.readMoving(Game_Bridge, curr_loc)
+          this.readMoving(Game_Bridge, curr_loc+1, retry_cnt)
         }
       }      
     });
@@ -46,10 +45,14 @@ const InputView = {
   /**
   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
   */
-  readGameCommand(Game_Bridge) {    
+  readGameCommand(Game_Bridge, final_loc, retry_cnt) {    
     MissionUtils.Console.readLine('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)', (answer) => {
+      if (answer !== "R"&& answer !== "Q"){
+        throw new Error(`[ERROR] 게임 재시작 중 잘못된 입력이 되었습니다.`)
+      }
       MissionUtils.Console.print(`${answer}`);
-      answer === "R" ? this.readMoving(Game_Bridge, 0) : MissionUtils.Console.close()      
+      //true === 게임 재시작, False === 게임 끝
+      answer === "R" ? this.readMoving(Game_Bridge, 0, retry_cnt+1) : OutputView.printResult(Game_Bridge, final_loc, retry_cnt)
     });
   },
 };
--- src/OutputView.js
+++ src/OutputView.js
@@ -11,15 +11,20 @@ const print_history = (print_obj, cur_loc, bridge) => {
   return print_obj
 }
 
-const print_last = (print_obj, cur_loc, bridge, input) => {
-  if (bridge[cur_loc] === input) {
-    print_obj[bridge[cur_loc]] += " O ]"
-    print_obj[Object.keys(print_obj).filter((e)=>e!==bridge[cur_loc])[0]] += "   ]"
-  } else if (bridge[cur_loc] !== input) {
-    print_obj[bridge[cur_loc]] += " X ]"
-    print_obj[Object.keys(print_obj).filter((e)=>e!==bridge[cur_loc])[0]] += "   ]"
+const print_last = (print_obj, bridge_cur_loc, input) => {
+  if (bridge_cur_loc === input) {
+    print_obj[bridge_cur_loc] += " O ]"
+    print_obj[Object.keys(print_obj).filter((e)=>e!==bridge_cur_loc)[0]] += "   ]"
+  } else if (bridge_cur_loc !== input) {
+    print_obj[bridge_cur_loc] += " X ]"
+    print_obj[Object.keys(print_obj).filter((e)=>e!==bridge_cur_loc)[0]] += "   ]"
   }
   return print_obj
+}
+
+const final_output = (retry_cnt) => {
+  MissionUtils.Console.print(`총 시도한 횟수: ${retry_cnt}`);  
+  MissionUtils.Console.close() 
 }
 
 const OutputView = {
@@ -29,9 +34,10 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printMap(bridge, input, cur_loc) {
+    console.log(bridge, input, cur_loc)
     print_obj = {"U" : "[", "D" : "["}    
     print_obj = print_history(print_obj, cur_loc, bridge)
-    print_obj = print_last(print_obj, cur_loc, bridge, input)
+    print_obj = print_last(print_obj, bridge[cur_loc], input)
     MissionUtils.Console.print(print_obj["U"]);
     MissionUtils.Console.print(print_obj["D"]);    
   },
@@ -41,7 +47,17 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() {},
+  printResult(bridge, final_loc, retry_cnt) {
+    MissionUtils.Console.print(`최종 게임 결과`);       
+    if (bridge.length === (final_loc+1)){      
+      this.printMap(bridge, bridge.slice(-1)[0], final_loc)
+      MissionUtils.Console.print(`게임 성공 여부: 성공`);
+    } else if (bridge.lenght !== (final_loc-1)){
+      this.printMap(bridge, bridge[final_loc] === "D"? "U" : "D", final_loc)
+      MissionUtils.Console.print(`게임 성공 여부: 실패`);
+    }    
+    final_output(retry_cnt)
+  },
 };
 
 module.exports = OutputView;
