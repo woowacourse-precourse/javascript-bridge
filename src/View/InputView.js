64c9dc98cbed8a579ce590a1f9ba937899290808
2je0 <bulljy@naver.com>
2022-11-19 21:14:41 +0900

    ✨ feat(inputview): 이동할 칸을 입력받는 기능 구현

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -20,7 +20,9 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving(callback) {
+    Console.readLine(MESSAGE.ASK_MOVING, (moving) => callback(moving));
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
