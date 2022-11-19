a08af93eabee1207af2fe4095ef6c9b71fb91964
2yunseong <dbsdltjd3701@naver.com>
2022-11-20 04:57:21 +0900

    feat(InputView): 사용자 안내 문구 출력 기능

--- src/InputView.js
+++ src/InputView.js
@@ -1,5 +1,5 @@
 const MissionUtils = require('@woowacourse/mission-utils');
-
+const OutputView = require('./OutputView');
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  * + 파일 경로 변경 가능
@@ -11,6 +11,7 @@ const InputView = {
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize(callback, message = '') {
+    OutputView.printMessage('다리의 길이를 입력해주세요.');
     MissionUtils.Console.readLine(message, (answer) => {
       callback(answer);
     });
@@ -20,6 +21,7 @@ const InputView = {
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving(callback, message = '') {
+    OutputView.printMessage('이동할 칸을 선택해주세요. (위: U, 아래: D)');
     MissionUtils.Console.readLine(message, (answer) => {
       callback(answer);
     });
@@ -29,6 +31,9 @@ const InputView = {
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand(callback, message = '') {
+    OutputView.printMessage(
+      '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)'
+    );
     MissionUtils.Console.readLine(message, (answer) => {
       callback(answer);
     });
