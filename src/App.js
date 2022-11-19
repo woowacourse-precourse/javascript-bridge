755c10c1b8b9275d9b7f8135aefaf875372c25be
2yunseong <dbsdltjd3701@naver.com>
2022-11-19 23:46:14 +0900

    feat(App): 다리 길이를 입력 받는 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+const InputValidator = require('./InputValidator');
+const InputView = require('./InputView');
+const OutputView = require('./OutputView');
+
 class App {
-  play() {}
+  constructor() {
+    this.bridgeSize;
+  }
+  play() {
+    InputView.readBridgeSize(this.handleInputLength.bind(this));
+  }
+
+  handleInputLength(input) {
+    try {
+      if (!InputValidator.isValidLength(input)) {
+        throw new Error('[ERROR] : 유효한 길이가 아닙니다.');
+      }
+      this.bridgeSize = +input;
+    } catch (error) {
+      OutputView.printMessage(error.message);
+      InputView.readBridgeSize(this.handleInputLength.bind(this));
+    }
+  }
 }
 
 module.exports = App;
--- src/InputView.js
+++ src/InputView.js
@@ -1,11 +1,20 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
+ * + 파일 경로 변경 가능
+ * + 메서드 인자 변경 가능
+ * + 메서드 추가 가능
  */
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize(callback, message) {
+    MissionUtils.Console.readLine(message, (answer) => {
+      callback(answer);
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
