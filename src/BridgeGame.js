2a9d4d012341ae165bf37984c224ff49be5e05d3
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-11-20 06:10:10 +0900

    ✨ [feat]: 게임을 재시작하는 메서드 추가

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -11,6 +11,12 @@ class BridgeGame {
     this.computerBridge = computerBridge;
     this.playerUpperBridge = [];
     this.playerLowerBridge = [];
+    this.attempsCount = GAME.ATTEMPTS_COUNT_INIT;
+  }
+
+  init() {
+    this.playerUpperBridge = [];
+    this.playerLowerBridge = [];
   }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -53,7 +59,10 @@ class BridgeGame {
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  retry() {}
+  retry() {
+    this.init();
+    this.attempsCount++;
+  }
 }
 
 module.exports = BridgeGame;
