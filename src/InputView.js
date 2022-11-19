285b7c80bf90d963f78212938383bffbd45719fa
cocoa <jwv0323@kau.kr>
2022-11-19 21:28:46 +0900

    feat: 다리의 길이를 입력받는 메서드 구현

--- src/InputView.js
+++ src/InputView.js
@@ -1,11 +1,31 @@
+const { Console } = require('@woowacourse/mission-utils');
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
+  BRIDGE_SIZE: '\n다리의 길이를 입력해주세요.\n',
+
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine(this.BRIDGE_SIZE, (number) => {
+      this.validateBridgeSize(number);
+    });
+  },
+
+  // 숫자는 문자열로 들어온다.
+  validateBridgeSize(number) {
+    // 문자열에서 0-9까지 검사함
+    if (/[^0-9]/g.test(number)) {
+      throw new Error('[ERROR] 다리 길이는 숫자로 입력해야합니다.');
+    }
+
+    if (Number(number) < 3 || Number(number) > 20) {
+      throw new Error('[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.');
+    }
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
--- src/OutputView.js
+++ src/OutputView.js
@@ -4,10 +4,10 @@ const { Console } = require('@woowacourse/mission-utils');
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 const OutputView = {
-  GAME_START_PHRASE: '다리 건너기 게임을 시작합니다.\n',
+  START_PHRASE: '다리 건너기 게임을 시작합니다.\n',
   
   printStart() {
-    Console.print(this.GAME_START_PHRASE);
+    Console.print(this.START_PHRASE);
   },
 
   /**
