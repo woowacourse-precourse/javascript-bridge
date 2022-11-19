03869bbc095ce6e51c9fd06afa1e49a2d7666673
kwh97 <zzzang0512@naver.com>
2022-11-19 21:51:12 +0900

    test: 이동할 칸 입력값에 대한 유효성을 검사하는 테스트코드 구현

--- __tests__/InputValidationTest.js
+++ __tests__/InputValidationTest.js
@@ -1,7 +1,13 @@
 const Validation = require('../src/Validation');
 
-describe("다리 길이 입력값 예외 테스트", () => {
-  test.each([[""], ["ds"], [26]])("다리 길이 입력값 대한 예외 처리", (input) => {
+describe("사용자 입력값 예외 테스트", () => {
+  test.each([[""], ["ds"], [26]])("다리 길이 입력값에 대한 예외 테스트", (input) => {
     expect(() => Validation.checkBridgeLength(input)).toThrow(); 
   });
+
+  test.each([[""], [1], ["u"], ["d"], ["UD"]])("이동할 칸 입력값에 대한 예외 테스트", (input) => {
+    expect(() => Validation.checkSpace(input)).toThrow(); 
+  });
+
 });
+
