395b535be51ae54062c4266a973784f9e4bcf5bc
sh981013s <sh981013s@gmail.com>
2022-11-19 16:07:33 +0900

    feat(UserModel): 최종경로 필드 및 getter 메서드 구현

--- src/model/UserModel.js
+++ src/model/UserModel.js
@@ -1,8 +1,10 @@
 class UserModel {
   #tryCount;
+  #userMove;
 
   constructor() {
     this.#tryCount = 0;
+    this.#userMove = [];
   }
 
   /**
@@ -16,6 +18,14 @@ class UserModel {
   // 유저의 시도 횟수를 1 증가시킨다.
   increaseTryCount() {
     this.#tryCount = this.#tryCount + 1;
+  }
+
+  /**
+   * 유저가 이동한 경로를 반환한다.
+   * @return {string[]} [유저 이동 경로]
+   */
+  getUserMove() {
+    return this.#userMove;
   }
 }
 
