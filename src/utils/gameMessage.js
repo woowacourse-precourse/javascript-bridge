86b05799cf6ba80be18254e7a2966579dd628d6a
violetwhenisawu <anashape710@gmail.com>
2022-11-19 21:29:14 +0900

    style: 게임 다시 시도 출력 메세지 마지막에도 개행 추가

--- src/utils/gameMessage.js
+++ src/utils/gameMessage.js
@@ -9,7 +9,7 @@ const INPUT_MESSAGE = {
   GET_BRIDGE_LENGTH: '다리의 길이를 입력해주세요.\n',
   SELECT_MOVE: '\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
   SELECT_RETRY:
-    '\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)',
+    '\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
 };
 
 const BRIDGE_COMPONENT = {
