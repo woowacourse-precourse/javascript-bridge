65d7292134cb27ff0bf1304d39305f0ac74d4f44
sunub <bsc5672@gmail.com>
2022-11-20 02:25:51 +0900

    feat : 사용자를 위 혹은 아래로 이동시키는 기능과 게임을 다시 시작하기 위한 기능의 추가
    사용자가 위로 이동하거나 아래로 이동할 때 필요한 기능들을 추가하였다.
    1. 전달 받은 사용자의 값이 정답인지 오답인지 판단한다.
    2. 판단한 값을 토대로 O 또는 X를 산출하고 전달한다.
    크게 두 가지 기능을 수행하게끔 작성하였다.
    재시도 할 경우에는 사용자가 이동 값을 다시금 입력할 수 있게 끔 기능을
    추가하였다.

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,20 +1,105 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+// const Index = require("./index");
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
+
 class BridgeGame {
+  constructor() {
+    this.bridgeString = {
+      start(text) {
+        return `[ ${text} `;
+      },
+      middle(text) {
+        return `| ${text} `;
+      },
+    };
+  }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(data) {
+    const bridge = data["bridge"];
+    const callCount = data["callCount"];
+
+    if (bridge[callCount] === data["currentAnswer"]) {
+      if (data["currentAnswer"] === "U") {
+        if (data["callCount"] === 0) {
+          data["upperBridge"] += this.bridgeString.start("O");
+          data["lowerBridge"] += this.bridgeString.start(" ");
+          data["callCount"] += 1;
+          data["status"] = "continue";
+          return data;
+        }
+        data["upperBridge"] += this.bridgeString.middle("O");
+        data["lowerBridge"] += this.bridgeString.middle(" ");
+        data["callCount"] += 1;
+        data["status"] = "continue";
+        return data;
+      } else if (data["currentAnswer"] === "D") {
+        if (data["callCount"] === 0) {
+          data["upperBridge"] += this.bridgeString.start(" ");
+          data["lowerBridge"] += this.bridgeString.start("O");
+          data["callCount"] += 1;
+          data["status"] = "continue";
+          return data;
+        } else {
+          data["upperBridge"] += this.bridgeString.middle(" ");
+          data["lowerBridge"] += this.bridgeString.middle("O");
+          data["callCount"] += 1;
+          data["status"] = "continue";
+          return data;
+        }
+      }
+    } else {
+      if (data["currentAnswer"] === "U") {
+        if (data["callCount"] === 0) {
+          data["upperBridge"] += this.bridgeString.start("X");
+          data["lowerBridge"] += this.bridgeString.start(" ");
+          data["status"] = "fail";
+          return data;
+        }
+        data["upperBridge"] += this.bridgeString.middle("X");
+        data["lowerBridge"] += this.bridgeString.middle(" ");
+        data["status"] = "fail";
+        return data;
+      }
+      if (data["currentAnswer"] === "D") {
+        if (data["callCount"] === 0) {
+          data["upperBridge"] += this.bridgeString.start(" ");
+          data["lowerBridge"] += this.bridgeString.start("X");
+          data["status"] = "fail";
+          return Inputthis.data;
+        }
+        data["upperBridge"] += this.bridgeString.middle(" ");
+        data["lowerBridge"] += this.bridgeString.middle("X");
+        data["status"] = "fail";
+        return data;
+      }
+    }
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  retry() {}
+  retry(data) {
+    if (data["currentAnswer"] === "R") {
+      data["status"] = "retry";
+      data["callCount"] = 0;
+      data["upperBridge"] = "";
+      data["lowerBridge"] = "";
+
+      return data;
+    }
+    if (data["currentAnswer"] === "Q") {
+      data["status"] = "quit";
+      return data;
+    }
+  }
 }
 
 module.exports = BridgeGame;
