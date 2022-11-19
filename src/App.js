7a57cff87434ccc5ceb0e10a7eef7290f9e555da
cham-min <hymn9613@gmail.com>
2022-11-19 11:37:25 +0900

    refactor(App, OutputView): fix App.js Console to use use OutputView.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-const { Console } = require("@woowacourse/mission-utils");
+const OutputView = require("./OutputView");
 
 class App {
   play() {
-    Console.print("다리 건너기 게임을 시작합니다.\n");
+    OutputView.printStart();
   }
 }
 
