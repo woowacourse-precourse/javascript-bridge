935d9a7b90c6d2046b684049150f506473d72274
한수지 <fatholic5@gmail.com>
2022-11-19 20:49:21 +0900

    :sparkles: feat: (InputView) 사용자에게 다리 길이를 입력받는 기능추가 우아한테크코스 라이브러리 Console API 연결 상수 메세지 모듈을 연결하여 다리 길이 입력시 사용될 메세지를 받음 다리길이 입력시 콜백함수를 인자로 받아, App 컨트롤러와 연결할 수 있게 구현함

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,6 @@
+const{ Console } = require("@woowacourse/mission-utils");
+const{ INPUT } = require("./constants/FixMessages");
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,7 +8,9 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize(callback) {
+    Console.readLine(`${INPUT.SIZE}\n`, callback);
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
