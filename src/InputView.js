8481e1628cb9d3d74d46c5af4008b1793c0f92aa
TeusLee <xodn1311@gmail.com>
2022-11-19 12:48:53 +0900

    Implement InputView.js

--- src/InputView.js
+++ src/InputView.js
@@ -1,21 +1,37 @@
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
+const MissionUtils = require("@woowacourse/mission-utils");
 const InputView = {
   /**
-   * 다리의 길이를 입력받는다.
-   */
-  readBridgeSize() {},
+  * 다리의 길이를 입력받는다.
+  */
+  readBridgeSize() {
+    MissionUtils.Console.readLine('다리의 길이를 입력해주세요.', (answer) => {
+      MissionUtils.Console.print(`${answer}`);      
+      this.readMoving()
+    });
+  },
 
   /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
-  readMoving() {},
+  * 사용자가 이동할 칸을 입력받는다.
+  */
+  readMoving() {
+    MissionUtils.Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)', (answer) => {
+      MissionUtils.Console.print(`${answer}`);
+      this.readGameCommand()
+    });
+  },
 
   /**
-   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-   */
-  readGameCommand() {},
+  * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+  */
+  readGameCommand() {    
+    MissionUtils.Console.readLine('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)', (answer) => {
+      MissionUtils.Console.print(`${answer}`);
+      MissionUtils.Console.close();
+    });
+  },
 };
 
 module.exports = InputView;
