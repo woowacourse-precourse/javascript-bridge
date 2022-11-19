91d7c4b80868adc3e6485bb5df62248d99deced9
oyhoyhk <oyhoyhk@naver.com>
2022-11-19 19:49:19 +0900

    [기능] 다리 길이 입력 받는 기능 및 예외 처리
    다리 길이 입력 값이 숫자가 아니거나 3보다 작거나 20보다 크면 에러 처리
    조건에 맞는 값이 들어오면 전달한 App객체에 저장한다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const InputView = require('./InputView');
+const OutputView = require('./OutputView');
+
 class App {
-  play() {}
+	constructor() {
+		this.bridgeLength = 0;
+		this.bridgeInfo = null;
+	}
+	play() {
+		OutputView.printStart();
+		InputView.readBridgeSize(this);
+		console.log(this);
+	}
 }
 
+new App().play();
 module.exports = App;
--- src/InputView.js
+++ src/InputView.js
@@ -1,21 +1,27 @@
+const { Console } = require('@woowacourse/mission-utils');
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
-  /**
-   * 다리의 길이를 입력받는다.
-   */
-  readBridgeSize() {},
+	/**
+	 * 다리의 길이를 입력받는다.
+	 */
+	readBridgeSize(info) {
+		Console.readLine('다리 길이를 입력해주세요.\n', line => {
+			if (isNaN(line) || Number(line) < 3 || Number(line > 20)) throw new Error('[Error] 다리 길이 입력 값이 잘못 되었습니다.');
+			info.bridgeLength = Number(line);
+		});
+	},
 
-  /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
-  readMoving() {},
+	/**
+	 * 사용자가 이동할 칸을 입력받는다.
+	 */
+	readMoving() {},
 
-  /**
-   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-   */
-  readGameCommand() {},
+	/**
+	 * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+	 */
+	readGameCommand() {},
 };
 
 module.exports = InputView;
