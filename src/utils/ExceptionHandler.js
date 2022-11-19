58c3eae7688869ba8e6251a6bc8d9de6998d746b
Buyoung Chung <sunarol@naver.com>
2022-11-19 14:21:17 +0900

    feat: Add isNaN exception and size range exception

---
+++ src/utils/ExceptionHandler.js
@@ -0,0 +1,15 @@
+const Constant = require("./Constant");
+
+const ExceptionHandler = {
+  isNotaNumber(input) {
+    if (isNaN(input)) throw new Error("[ERROR] 숫자를 입력해주세요.");
+  },
+
+  validateSizeInput(size) {
+    const intSize = parseInt(size);
+    if (intSize < Constant.MIN_SIZE && intSize > Constant.MAX_SIZE)
+      throw new Error("[ERROR] 3부터 20 사이의 숫자를 입력해주세요.");
+  },
+};
+
+module.exports = ExceptionHandler;
