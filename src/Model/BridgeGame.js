db4e938a4e9e01fa11f89ca7e56fddb3b79a2a54
Buyoung Chung <sunarol@naver.com>
2022-11-19 13:53:12 +0900

    refactor: remove function saveAnswerBridge

--- src/Model/BridgeGame.js
+++ src/Model/BridgeGame.js
@@ -12,10 +12,6 @@ class BridgeGame {
     this.bridges = new Bridges();
     this.answerBridge = [];
   }
-
-  saveAnswerBridge(answerBridge) {
-    this.answerBridge = answerBridge;
-  }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
