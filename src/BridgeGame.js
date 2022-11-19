4a30d61ac93044b388ec7bb80c75ccfb93efcff1
tkdrb12 <tkdrb15@gmail.com>
2022-11-19 15:50:26 +0900

    feat : 게임 재시도를 위해 플레이어 위치를 초기화하는 기능 추가

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -5,7 +5,7 @@ const { makeBridge } = require("./BridgeMaker");
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
-  #player
+  #playerLocation
   #bridgeShape
   #playerMoving
 
@@ -31,7 +31,7 @@ class BridgeGame {
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   retry() {
-
+    this.#playerLocation = -1;
   }
 
   isFinish() {
