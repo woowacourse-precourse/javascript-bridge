e815b06cbe857c1f743b0718bc71ac82ddc7bb42
Choi siun <siun0331@gmail.com>
2022-11-20 00:44:14 +0900

    feat: InputView와 BridgeGame 상호작용 연결

--- src/InputView.js
+++ src/InputView.js
@@ -1,26 +1,32 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const { MESSAGE_INPUT_BRIDGE_LENGTH, MESSAGE_NEXT_MOVING_INPUT, MESSAGE_RETRY } = require('./constants');
+const BridgeMaker = require('./BridgeMaker');
+const { generate } = require('./BridgeRandomNumberGenerator');
+const BridgeGame = require('./BridgeGame');
 
 const { Console } = MissionUtils;
-
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+const GAME_MANAGER = new BridgeGame();
 const InputView = {
   readBridgeSize() {
     Console.readLine(MESSAGE_INPUT_BRIDGE_LENGTH, (length) => {
+      BridgeMaker.makeBridge(length, generate);
+      console.log(GAME_MANAGER.getBridge());
+      InputView.readMoving();
     });
   },
 
   readMoving() {
     Console.readLine(MESSAGE_NEXT_MOVING_INPUT, (direction) => {
-
+      GAME_MANAGER.move(direction)
+        ? console.log('move!') : this.readGameCommand();
     });
   },
 
   readGameCommand() {
     Console.readLine(MESSAGE_RETRY, (command) => {
-
+      if (GAME_MANAGER.retry(command)) {
+        this.readMoving();
+      }
     });
   },
 };
