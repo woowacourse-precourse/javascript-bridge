1a8b38408a9c982c6f612cb1646cdb585abfbeaf
wnw <fkaus65@naver.com>
2022-11-19 20:10:11 +0900

    feat(InputView.js): 입력을 위한 InputView 객체 구현
    아래 함수는 모두 콜백함수를 매개변수로 받는다
    
    readBridgeSize: 다리 길이 입력받는 함수
    readMoving: 이동 방향 입력받는 함수
    readGameCommand: 게임의 재시작 여부 입력받는 함수

---
+++ constants/number.js
@@ -0,0 +1,6 @@
+const NUMBER = Object.freeze({
+  ZERO: 0,
+  ONE: 1,
+});
+
+module.exports = NUMBER;
--- src/View/InputView.js
+++ src/View/InputView.js
@@ -1,13 +1,22 @@
-//  사용자로부터 입력을 받는 역할을 한다.
+const { Console } = require('@woowacourse/mission-utils');
+const SYSTEM_MESSAGE = require('../../constants/system message');
+
+// 사용자로부터 입력을 받는 역할을 한다.
 const InputView = {
-  //  다리의 길이를 입력받는다.
-  readBridgeSize() {},
+  // 다리의 길이를 입력받는다.
+  readBridgeSize(callback) {
+    Console.readLine(SYSTEM_MESSAGE.INPUT_SIZE, callback);
+  },
 
-  //  사용자가 이동할 칸을 입력받는다.
-  readMoving() {},
+  // 사용자가 이동할 칸을 입력받는다.
+  readMoving(callback) {
+    Console.readLine(SYSTEM_MESSAGE.INPUT_MOVE, callback);
+  },
 
-  //  사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-  readGameCommand() {},
+  // 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+  readGameCommand(callback) {
+    Console.readLine(SYSTEM_MESSAGE.RESTART, callback);
+  },
 };
 
 module.exports = InputView;
