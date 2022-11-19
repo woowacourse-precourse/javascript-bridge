c37fb1d3039113778f10c515848d2414c7403e9f
bassyu <yrkdgus@gmail.com>
2022-11-19 13:18:59 +0900

    refactor(Validator): 상수 추출

--- src/Validator.js
+++ src/Validator.js
@@ -1,22 +1,28 @@
 const Validator = {
+  NUMBER_STRING: '0123456789',
+  MIN_SIZE: 3,
+  MAX_SIZE: 20,
+  MOVING_STRING: 'UD',
+  GAME_COMMAND_STRING: 'RQ',
+
   checkBridgeSize(input) {
-    if ('0123456789'.includes(input) === false) {
+    if (Validator.NUMBER_STRING.includes(input) === false) {
       throw new Error('다리 길이는 정수만 입력해야 합니다.');
     }
     const size = Number(input);
-    if (size < 3 || size > 20) {
-      throw new Error('다리 길이는 3부터 20 사이의 숫자여야 합니다.');
+    if (size < Validator.MIN_SIZE || size > Validator.MAX_SIZE) {
+      throw new Error(`다리 길이는 3 부터 20 사이의 숫자여야 합니다.`);
     }
   },
 
   checkMoving(input) {
-    if ('UD'.includes(input) === false) {
+    if (Validator.MOVING_STRING.includes(input) === false) {
       throw new Error('U(위 칸)와 D(아래 칸) 중 하나의 문자만 입력할 수 있습니다.');
     }
   },
 
   checkGameCommand(input) {
-    if ('RQ'.includes(input) === false) {
+    if (Validator.GAME_COMMAND_STRING.includes(input) === false) {
       throw new Error('R(재시작)과 Q(종료) 중 하나의 문자만 입력할 수 있습니다.');
     }
   },
