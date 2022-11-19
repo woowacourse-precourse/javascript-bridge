cc698122997dddbf3b8f8659c27297f6ad8c8d2d
kangju2000 <kangju2000@naver.com>
2022-11-19 16:31:12 +0900

    test(ValidatorTest): Validator 테스트 코드에서 잘못된 값인 경우 에러 던지도록 변경 - 기능 구현 목록 오타 수정 및 constants 폴더의 error, message 파일 추가

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -1,26 +1,28 @@
+const Validator = require('../src/Validator');
+
 describe('예외 처리 테스트', () => {
   test('다리 길이 입력 시 3~20의 숫자를 입력한 경우 true', () => {
     const bridgeSize = 10;
     expect(Validator.validateBridgeSize(bridgeSize)).toBeTruthy();
   });
-  test('다리 길이 입력 시 3~20의 숫자가 아닌 경우 false', () => {
+  test('다리 길이 입력 시 3~20의 숫자가 아닌 경우 에러 던지기', () => {
     const bridgeSize = 2;
-    expect(Validator.validateBridgeSize(bridgeSize)).toBeFalsy();
+    expect(Validator.validateBridgeSize(bridgeSize)).toThrow('[ERROR]');
   });
-  test("이동할 칸 입력 시 'U' 또는 'R' 중 하나를 입력한 경우 true", () => {
+  test("이동할 칸 입력 시 'U' 또는 'D' 중 하나를 입력한 경우 true", () => {
     const moving = 'U';
     expect(Validator.validateMoving(moving)).toBeTruthy();
   });
-  test("이동할 칸 입력 시 'U' 또는 'R' 중 하나를 입력하지 않은 경우 false", () => {
+  test("이동할 칸 입력 시 'U' 또는 'D' 중 하나를 입력하지 않은 경우 에러 던지기", () => {
     const moving = 'A';
-    expect(Validator.validateMoving(moving)).toBeFalsy();
+    expect(Validator.validateMoving(moving)).toThrow('[ERROR]');
   });
   test("게임 재시작/종료 여부 입력 시 'R' 또는 'Q' 중 하나를 입력한 경우 true", () => {
     const gameCommand = 'R';
     expect(Validator.validateGameCommand(gameCommand)).toBeTruthy();
   });
-  test("게임 재시작/종료 여부 입력 시 'R' 또는 'Q' 중 하나를 입력하지 않은 경우 false", () => {
+  test("게임 재시작/종료 여부 입력 시 'R' 또는 'Q' 중 하나를 입력하지 않은 경우 에러 던지기", () => {
     const gameCommand = 'U';
-    expect(Validator.validateGameCommand(gameCommand)).toBeFalsy();
+    expect(Validator.validateGameCommand(gameCommand)).toThrow('[ERROR]');
   });
 });
