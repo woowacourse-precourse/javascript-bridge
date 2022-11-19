6aab3a93b6068cd6cbf257ffe5361b76650187a4
jisu-kim <kss2460@naver.com>
2022-11-19 14:06:00 +0900

    feat(makeBridge): 요청 형식에 맞게 출력 데이터 변환

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,3 +1,5 @@
+const { generate } = require("./BridgeRandomNumberGenerator");
+
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
@@ -8,19 +10,17 @@ const BridgeMaker = {
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
   makeBridge(size, generateRandomNumber) {
-    const [bridgeTop, bridgeBottom] = [[], []];
+    const bridge = [];
     for (let i = 0; i < size; i++) {
       const number = generateRandomNumber();
       if (number === 1) {
-        bridgeTop.push("U");
-        bridgeBottom.push("X");
+        bridge.push("U");
       }
       if (number === 0) {
-        bridgeTop.push("X");
-        bridgeBottom.push("D");
+        bridge.push("D");
       }
     }
-    return [bridgeTop, bridgeBottom];
+    return bridge;
   },
 
   makeUserBridge(moveInput, userBridge, mainBridge) {
