db60f1e0112b9d860c698d66e2beebb5e22ebdd0
vinivin153 <vinivin153@naver.com>
2022-11-19 21:17:09 +0900

    refactor: (readBridgeSize) callback 함수 반환 형태로 변경

--- src/InputView.js
+++ src/InputView.js
@@ -2,7 +2,7 @@ const { Console } = require('@woowacourse/mission-utils');
 const { MESSAGE_INPUT } = require('./utils/constant');
 const { makeBridge } = require('./BridgeMaker');
 const { generate } = require('./BridgeRandomNumberGenerator');
-const { bridgeSizeValidator } = require('./Validator');
+const { bridgeSizeValidator, directionValidator } = require('./Validator');
 const OutputView = require('./OutputView');
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -11,22 +11,16 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {
-    try {
-      Console.readLine(MESSAGE_INPUT.BRIDGE_SIZE, (size) => {
-        bridgeSizeValidator.isBridgeSizeValid(size);
-        const bridge = makeBridge(size, generate);
-      });
-    } catch (e) {
-      OutputView.printError(e);
-      this.readBridgeSize();
-    }
+  readBridgeSize(callback) {
+    Console.readLine(MESSAGE_INPUT.BRIDGE_SIZE, callback);
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving(callback) {
+    Console.readLine(MESSAGE_INPUT.MOVING, callback);
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
