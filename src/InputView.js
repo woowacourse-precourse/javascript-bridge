cfabe094c26d6b78592e185dccd03e9a5921bede
yeonkyoung <keep.ykk@gmail.com>
2022-11-20 03:27:45 +0900

    featL add readMoving

--- src/InputView.js
+++ src/InputView.js
@@ -16,7 +16,11 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    MissionUtils.Console.readLine(message.MOVE_MESSAGE, (answer) => {
+      console.log(answer);
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
