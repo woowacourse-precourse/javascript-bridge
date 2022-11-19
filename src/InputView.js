7e317d63de005e67e5bece9d5af9ac98824fa4c5
areumsheep <areumsheep@gmail.com>
2022-11-19 20:32:42 +0900

    feat(InputView): 다리 길이 사용자 입력 기능 추가
    - 메세지는 상수화하여 저장

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,6 @@
+const { Console } = require('@woowacourse/mission-utils');
+const MESSAGES = require('./constants/Messages');
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,7 +8,11 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine(MESSAGES.INPUT_BRIDGE_LENGTH, (length) => {
+      // TODO length validation check
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
---
+++ src/constants/Messages.js
@@ -0,0 +1,5 @@
+const MESSAGES = Object.freeze({
+  INPUT_BRIDGE_LENGTH: '다리의 길이를 입력해주세요.\n'
+})
+
+module.exports = MESSAGES;
