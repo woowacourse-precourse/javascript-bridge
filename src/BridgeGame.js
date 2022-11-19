d1862479a70fab21a6aaec5dd25e8cf372acf999
NaveOWO <pianowell96@gmail.com>
2022-11-19 13:14:00 +0900

    [ feat ] 사용자가 입력한 칸이 건널 수 있는지 판단하는 기능

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -7,7 +7,26 @@ class BridgeGame {
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(direction, location, cnt) {
+    if (direction === "U") {
+      if (location[cnt] === 1) {
+        //printBridge
+        console.log("good");
+        return true;
+      } else {
+        return false;
+      }
+    }
+    if (direction === "D") {
+      if (location[cnt] === 1) {
+        return false;
+      } else {
+        //printBridge
+        console.log("good");
+        return true;
+      }
+    }
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
--- src/InputView.js
+++ src/InputView.js
@@ -1,7 +1,8 @@
 const { Console } = require("@woowacourse/mission-utils");
+const BridgeGame = require("./BridgeGame");
 const BridgeMaker = require("./BridgeMaker");
 const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
-
+const game = new BridgeGame();
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,19 +10,37 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
+
   readBridgeSize() {
     Console.readLine("다리의 길이를 입력해주세요.", (size) => {
-      BridgeRandomNumberGenerator.createRandomLocation(size);
+      const locations = BridgeRandomNumberGenerator.createRandomLocation(size);
+      InputView.readMoving(locations, size);
     });
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {
+  cnt: 0,
+
+  readMoving(locations, size) {
+    this.cnt += 1;
+
     Console.readLine(
       "이동할 칸을 선택해주세요. (위: U, 아래: D)",
-      (direction) => {}
+      (direction) => {
+        if (!game.move(direction, locations, this.cnt)) {
+          console.log("finish");
+          return;
+        }
+        if (this.cnt === size) {
+          console.log("finish");
+          Console.close();
+          return;
+        }
+        console.log(this.cnt);
+        this.readMoving(locations, size);
+      }
     );
   },
 
