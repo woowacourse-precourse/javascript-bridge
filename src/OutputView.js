018881f773e29eb8d973641f2af10bf1e0b1e0f8
daal2quf <daal2quf@gmail.com>
2022-11-20 04:58:35 +0900

    feat(outputView): 에러 메세지 출력 기능 추가
    add:
    printWrongInputOfBridgeLength(): 다리 길이 입력 오류에 대한 메세지
    printWorngInputOfMoving(): 다리 이동 입력 오류에 대한 메세지

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,4 +1,4 @@
-const { Console } = require("@woowacourse/mission-utils");
+const { Console } = require('@woowacourse/mission-utils');
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -9,18 +9,26 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() { },
+  printMap() {},
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() { },
+  printResult() {},
 
   printGameStartMsg() {
     Console.print('다리 건너기 게임을 시작합니다.');
-  }
+  },
+
+  printWrongInputOfBridgeLength() {
+    Console.print('[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.');
+  },
+
+  printWorngInputOfMoving() {
+    Console.print('[ERROR] 다리 이동 입력은 "U"와 "D"만 가능합니다');
+  },
 };
 
 module.exports = OutputView;
