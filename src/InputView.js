ac30ec6d018c8fd20cd96926ca3a5255a6ee80f7
NaveOWO <pianowell96@gmail.com>
2022-11-20 00:15:12 +0900

    [ fix ] 사용자가 입력한 횟수가 size가 되었을 때 return 되지 않는 이슈수정

--- src/InputView.js
+++ src/InputView.js
@@ -2,7 +2,9 @@ const { Console } = require("@woowacourse/mission-utils");
 const BridgeGame = require("./BridgeGame");
 const BridgeMaker = require("./BridgeMaker");
 const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+const { printMap } = require("./OutputView");
 const game = new BridgeGame();
+const OutputView = require("./OutputView");
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -14,34 +16,34 @@ const InputView = {
   readBridgeSize() {
     Console.readLine("다리의 길이를 입력해주세요.", (size) => {
       const locations = BridgeRandomNumberGenerator.createRandomLocation(size);
-      InputView.readMoving(locations, size);
+      InputView.readMoving(locations, Number(size));
     });
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  cnt: 0,
+  step: 0,
 
   readMoving(locations, size) {
-    this.cnt += 1;
-
-    Console.readLine(
-      "이동할 칸을 선택해주세요. (위: U, 아래: D)",
-      (direction) => {
-        if (!game.move(direction, locations, this.cnt)) {
-          console.log("finish");
-          return;
-        }
-        if (this.cnt === size) {
-          console.log("finish");
-          Console.close();
-          return;
-        }
-        console.log(this.cnt);
+    console.log(locations);
+    Console.readLine("이동할 칸을 선택해주세요. (위: U, 아래: D)", (answer) => {
+      if (answer === "U" && locations[this.step] === 1) {
+        console.log("success");
+        this.step += 1;
+        if (this.step === size) return;
         this.readMoving(locations, size);
+        return;
       }
-    );
+      if (answer === "D" && locations[this.step] === 0) {
+        console.log("success");
+        this.step += 1;
+        if (this.step === size) return;
+        this.readMoving(locations, size);
+        return;
+      }
+      console.log("wrong");
+    });
   },
 
   /**
