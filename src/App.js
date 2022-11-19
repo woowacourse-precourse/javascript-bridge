dcadbf3467e325025fc13495bca63750448da74c
“somm12” <sominan8744@gmail.com>
2022-11-19 14:21:27 +0900

    test: 다리 사이즈 입력 예외처리 테스트 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 const OutputView = require("./OutputView");
 const InputView = require("./InputView");
-// const BridgeGame = require("./BridgeGame");
+const BridgeGame = require("./BridgeGame");
 // const bridgeGame = new BridgeGame();
 class App {
   play() {
