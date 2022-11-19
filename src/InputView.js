36d26b26e97e5d8a425d02bea74b9b81a3c5e02f
Deemou <4106243@naver.com>
2022-11-19 18:55:59 +0900

    feat(InputView): 값을 입력받는 기능 추가

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,5 @@
+const { Console } = require('@woowacourse/mission-utils');
+const { MESSAGE } = require('./constant/Bridge');
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,17 +7,23 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize(callback) {
+    Console.readLine(MESSAGE.INPUT_BRIDGE_SIZE, callback);
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving(callback) {
+    Console.readLine(MESSAGE.INPUT_SPACE, callback);
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand(callback) {
+    Console.readLine(MESSAGE.INPUT_COMMAND, callback);
+  }
 };
 
 module.exports = InputView;
