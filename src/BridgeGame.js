71382cfa2bca724abd084f2fe93dacfb87eb8144
wontae chang <cwt629@naver.com>
2022-11-19 17:37:59 +0900

    feat: make base of BridgeGame class

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -2,19 +2,27 @@
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  #path
+
+  constructor(path) {
+    this.#path = path; // 실제 다리
+    this.passed = []; // 현재까지 지나온 길
+    this.count = 1; // 시도 횟수
+  }
+
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move() { }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  retry() {}
+  retry() { }
 }
 
 module.exports = BridgeGame;
