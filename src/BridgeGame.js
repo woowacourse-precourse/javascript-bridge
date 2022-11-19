6777d7186adb9bfabea2311ca16b85213e3f3718
AnJaeSeong <dktmzh6@gmail.com>
2022-11-20 01:33:39 +0900

    refactor(BridgeGame): change current history machanism
    현재 플레이어 위치와 도착여부로 정보를 저장하던 방식에서
    현재까지 이동 기록을 배열로 저장하는 방식으로 변경
    - curr: number -> array
    - move(): curr 증가 -> curr 배열에 현재 입력값 push
    - getLastRoundResult(): 마지막 시도의 성공 여부 반환
    - isArrived(): isAlive값 참조 방식 -> curr배열의 길이와 마지막 시도 성공여부 참조 방식
    - stateToString(): upper와 down를 분리하여 합치는 방식으로 변경
    - retry(): curr 배열을 초기화

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,71 +1,89 @@
 /**
- * 다리 건너기 게임을 관리하는 클래스
+ * 다리 건너기 게임의 정보를 저장한다.
  */
 class BridgeGame {
   #bridge     // 생성된 다리 정보
-  #curr       // 현재 플레이어 다리 위치
-  #isAlive    // 현재 플레이어 생존 여부
+  #curr       // 현재까지 기록
   #try        // 현재까지 시도 횟수
-
 
   constructor(bridge) {
     console.log(bridge);
     this.#bridge = bridge;
-    this.#curr = 0;
-    this.#isAlive = true;
+    this.#curr = new Array();
     this.#try = 1;
   }
+  
   /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * @param {char} input 건너기를 시도한 방향 (U or D)
-   * @return {boolean} 건너기 시도 성공시 true, 실패시 false
+   * 입력받은 방향에 맞게 curr를 갱신
+   * @param {char} input 현재 라운드의 이동 방향 문자 
    */
   move(input) {
-    const result = this.#bridge[this.#curr] == input;
-    this.#curr += 1;
-    this.#isAlive = result;
-    return result;
+    this.#curr.push(input);
   }
 
   /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * 현재 상태 정보를 초기화
-   * 시도 횟수 + 1
+   * 현재까지 기록의 마지막 성공 여부를 반환
+   * @returns {boolean} 마지막 라운드 성공 여부
    */
-  retry() {
-    this.#curr = 0;
-    this.#try += 1;
+  getLastRoundResult() {
+    return this.#bridge[this.#curr.length-1] == this.#curr[this.#curr.length-1];
   }
 
   /**
    * 다리 끝에 도착 여부 반환
-   * @return {boolean} bridge의 길이와 curr의 길이가 같으면서 플레이어 생존시 true, 아니면 false
+   * @return {boolean} bridge의 길이와 curr의 길이가 같고, 마지막 시도가 성공이면 true, 아니면 false
    */
   isArrived() {
-    return this.#bridge.length == this.#curr && this.#isAlive;
+    return this.#bridge.length == this.#curr.length && this.getLastRoundResult();
   }
 
-  stateToString() {
+  /**
+   * 현재까지 위쪽 다리 기록을 문자열로 반환
+   * @returns {string} 위쪽 다리 기록
+   */
+  stateUpperToString() {
     let upper = '[';
-    let down = '[';
-    for(let i=0; i<this.#curr-1; i++) {
-      upper += this.#bridge[i] == 'U' ? ' O ' : '   ';
-      down += this.#bridge[i] == 'U' ? '   ' : ' O ';
-      upper += '|';
-      down += '|';
+    for(let i=0; i<this.#curr.length-1; i++) {
+      upper += this.#curr[i] == 'U' ? ' O |' : '   |';
     }
-    if (this.#isAlive) {
-      upper += this.#bridge[this.#curr-1] == 'U' ? ' O ' : '   ';
-      down += this.#bridge[this.#curr-1] == 'U' ? '   ' : ' O ';
-    }else{
-      upper += this.#bridge[this.#curr-1] == 'U' ? '   ' : ' X ';
-      down += this.#bridge[this.#curr-1] == 'U' ? ' X ' : '   ';
-    }
-    upper += ']';
-    down += ']';
-    return [upper, down];
+    const mark = this.getLastRoundResult() ? ' O ]' : ' X ]';
+    upper += this.#curr[this.#curr.length-1] == 'U' ? mark : '   ]';
+    return upper;
   }
 
+  /**
+   * 현재까지 아래쪽 다리 기록을 문자열로 반환
+   * @returns {string} 아래쪽 다리 기록
+   */
+  stateDownToString() {
+    let down = '[';
+    for(let i=0; i<this.#curr.length-1; i++) {
+      down += this.#curr[i] == 'D' ? ' O |' : '   |';
+    }
+    const mark = this.getLastRoundResult() ? ' O ]' : ' X ]';
+    down += this.#curr[this.#curr.length-1] == 'D' ? mark : '   ]';
+    return down;
+  }
+
+  /**
+   * 현재까지 위/아래 다리 기록을 반환 
+   * @returns {array[string]} [위쪽 다리 기록, 아래쪽 다리 기록] 문자열 배열
+   */
+  stateToString() {
+    return [this.stateUpperToString(), this.stateDownToString()];
+  }
+
+  /**
+   * 게임 재시작시 현재까지 기록을 초기화하고 시도 횟수 1 증가
+   */
+  retry() {
+    this.#curr = new Array();
+    this.#try += 1;
+  }
+
+  /**
+   * @returns {number} 현재까지 시도 횟수 반환
+   */
   getTry() {
     return this.#try;
   }
