7ecb64c4f55845415f5328b7706befc911068864
Hchan2 <seongchan_@naver.com>
2022-11-19 20:23:19 +0900

    상수 변수 추가

--- src/constant/outputMessage.js
+++ src/constant/outputMessage.js
@@ -1,9 +1,15 @@
 const GAME_RESULT = (result) => { return `게임 성공 여부: ${result}` };
 const TOTAL_ATTEMPT = (attemptNumber) => { return `총 시도한 횟수: ${attemptNumber}` };
 const BRIDGE_START = '다리 건너기 게임을 시작합니다.';
+const SUCCESS_GAME = '게임 성공 여부: 성공'
+const FAIL_GAME = '게임 성공 여부: 실패'
+const GAME_COUNT = (count) => { return `총 시도한 횟수: ${count}`};
 
 module.exports = {
     GAME_RESULT,
     TOTAL_ATTEMPT,
-    BRIDGE_START
+    BRIDGE_START,
+    SUCCESS_GAME,
+    FAIL_GAME,
+    GAME_COUNT
 }
