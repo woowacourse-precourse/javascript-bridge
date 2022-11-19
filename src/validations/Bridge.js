2e79ba2dcea93ff0ea9e06586325e6366faae980
hyunjin <leehj0110@kakao.com>
2022-11-19 14:36:33 +0900

    refactor: 다리 사이즈 조건 추가

--- src/validations/Bridge.js
+++ src/validations/Bridge.js
@@ -2,7 +2,8 @@ const CustomError = require('../CustomError');
 
 const bridgeValidation = {
   isValidSize(birdgeSize) {
-    if (isNaN(Number(birdgeSize))) throw new CustomError('다리 길이는 숫자여야 합니다.');
+    if (isNaN(birdgeSize)) throw new CustomError('다리 길이는 숫자여야 합니다.');
+    if (birdgeSize < 3 || birdgeSize > 20) throw new CustomError('다리 길이는 3이상 20이하여야 합니다.');
   },
 };
 
