97def4742fdd10b2d63a5df783626e2fc6b11ddc
Narcoker <jykim9868@gmail.com>
2022-11-19 17:03:42 +0900

    Feat: GAME_RESULT 개행 삭제

--- src/Messages.js
+++ src/Messages.js
@@ -6,7 +6,7 @@ const MESSAGE = Object.freeze({
     ENTER_BRIDGE_SIZE: "다리의 길이를 입력해주세요.\n",
     ENTER_MOVE_TYPE: "이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
     ENTER_COMMAND: "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n",
-    GAME_RESULT: "최종 게임 결과\n",
+    GAME_RESULT: "최종 게임 결과",
     GAME_SUCCESS_STATE: "게임 성공 여부: ",
     GAME_TRY_COUNT: "총 시도한 횟수: "
 });
