89347b4f4f5fc84f1338c3f1ec3e25f9ad978888
kangju2000 <kangju2000@naver.com>
2022-11-19 19:00:56 +0900

    test(BridgeGameTest): BridgeGame 테스트 코드 일부 수정 - 게임 시도 횟수 테스트 추가

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -2,29 +2,7 @@ const BridgeGame = require('../src/BridgeGame');
 const ERROR = require('../src/constants/error');
 
 describe('BridgeGame 테스트', () => {
-  test('위로 이동', () => {
-    const bridge = ['U', 'D', 'U'];
-    const moving = 'U';
-    const bridgeGame = new BridgeGame(bridge);
-
-    expect(() => {
-      bridgeGame.move(moving);
-      bridgeGame.getMovingBridge();
-    }).toEqual(['U']);
-  });
-
-  test('아래로 이동', () => {
-    const bridge = ['D', 'D', 'U'];
-    const moving = 'D';
-    const bridgeGame = new BridgeGame(bridge);
-
-    expect(() => {
-      bridgeGame.move(moving);
-      bridgeGame.getMovingBridge();
-    }).toEqual(['D']);
-  });
-
-  test('이동했는지 확인', () => {
+  test('이동 성공 테스트', () => {
     const bridge = ['U', 'D', 'U'];
     const movings = ['U', 'D', 'U'];
     const bridgeGame = new BridgeGame(bridge);
@@ -32,21 +10,10 @@ describe('BridgeGame 테스트', () => {
       bridgeGame.move(moving);
     });
 
-    expect(() => bridgeGame.isMove()).toBeTruthy();
+    expect(bridgeGame.isSuccessMove()).toBeTruthy();
   });
 
-  test('생성된 다리와 건넌 다리의 길이가 일치하는지 확인', () => {
-    const bridge = ['U', 'D', 'U'];
-    const movings = ['U', 'D', 'U'];
-    const bridgeGame = new BridgeGame(bridge);
-    movings.forEach((moving) => {
-      bridgeGame.move(moving);
-    });
-
-    expect(() => bridgeGame.isSameBridgeSize()).toBeTruthy();
-  });
-
-  test('게임 실패 확인', () => {
+  test('이동 실패 테스트', () => {
     const bridge = ['U', 'D', 'U'];
     const movings = ['U', 'D', 'D'];
     const bridgeGame = new BridgeGame(bridge);
@@ -54,10 +21,31 @@ describe('BridgeGame 테스트', () => {
       bridgeGame.move(moving);
     });
 
-    expect(() => bridgeGame.isMove()).toBeFalsy();
+    expect(bridgeGame.isSuccessMove()).toBeFalsy();
   });
 
-  test('이동 문자를 잘못 입력 시 에러', () => {
+  test('생성된 다리와 건넌 다리의 길이가 일치 테스트', () => {
+    const bridge = ['U', 'D', 'U'];
+    const movings = ['U', 'D', 'U'];
+    const bridgeGame = new BridgeGame(bridge);
+    movings.forEach((moving) => {
+      bridgeGame.move(moving);
+    });
+
+    expect(bridgeGame.isSameBridgeSize()).toBeTruthy();
+  });
+
+  test('게임 시도 횟수 테스트', () => {
+    const bridge = ['U', 'D', 'U'];
+    const bridgeGame = new BridgeGame(bridge);
+    bridgeGame.retry();
+    bridgeGame.retry();
+    bridgeGame.retry();
+
+    expect(bridgeGame.getAttempt()).toBe(4);
+  });
+
+  test('이동 문자가 잘못된 값일 시 에러 던저기', () => {
     const bridge = ['U', 'D', 'U'];
     const moving = 'Q';
     const bridgeGame = new BridgeGame(bridge);
