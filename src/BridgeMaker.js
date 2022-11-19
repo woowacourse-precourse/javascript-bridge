920e85ca40af147bd148c64258c5cfd2034f0861
sunub <bsc5672@gmail.com>
2022-11-20 02:30:12 +0900

    feat : 입력 받은 사이즈 값에 맞춰 사다리를 만드는 기능을 추가
    InputView에서 사다리의 크기를 전달 받고 전달 받은 사다리의 크기 값을
    토대로 사다리를 만든다.
    - 입력 받은 사다리의 크기 만큼의 랜덤한 숫자를 생성한다.
    - 생성한 랜덤 숫자를 U 또는 D의 값으로 변환 시켜 반환한다.
    이 두 가지를 수행하는 기능을 추가 하였다.

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -7,7 +7,20 @@ const BridgeMaker = {
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
-  makeBridge(size, generateRandomNumber) {},
+  makeBridge(size, generateRandomNumber) {
+    const bridge = new Array(size).fill(null);
+    const upAndDown = {
+      0: "D",
+      1: "U",
+    };
+
+    for (let i = 0; i < bridge.length; i++) {
+      const num = generateRandomNumber();
+      bridge[i] = upAndDown[`${num}`];
+    }
+
+    return bridge;
+  },
 };
 
 module.exports = BridgeMaker;
