2cbb42daba5bc5990729c27de279eee362a500a6
violetwhenisawu <anashape710@gmail.com>
2022-11-19 20:33:02 +0900

    style: 이동할 칸을 선택할때 사용하는 게임 메세지 개행 추가

--- src/utils/gameMessage.js
+++ src/utils/gameMessage.js
@@ -7,7 +7,7 @@ const GAME_MESSAGE = {
 
 const INPUT_MESSAGE = {
   GET_BRIDGE_LENGTH: '다리의 길이를 입력해주세요.\n',
-  SELECT_MOVE: '이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
+  SELECT_MOVE: '\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
   SELECT_RETRY:
     '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
 };
