d0400898474694e35fcfd4aabee0b6562a795f33
unsnruu <unsnruu@gmail.com>
2022-11-20 01:33:04 +0900

    test: isInRange에 대한 테스트 작성

---
+++ __tests__/ValidationTest.js
@@ -0,0 +1,20 @@
+const Validation = require("../src/Validation");
+
+describe("검증 메서드에 대한 테스트를 구현합니다.", () => {
+  test.each([
+    [1, 10, 100],
+    [1, 2, 3],
+  ])("범위 값을 벗어난 경우 false를 반환합니다.", (start, end, value) => {
+    expect(Validation.isInRange(start, end, value)).toBe(false);
+  });
+  test.each([
+    [1, 1, 1],
+    [1, 10, 5],
+  ])("범위 값 내에 있을 경우 true를 반환합니다.", (start, end, value) => {
+    expect(Validation.isInRange(start, end, value)).toBe(true);
+  });
+  test("범위 값의 설정이 잘못된 경우 에러를 반환합니다.", () => {
+    const [start, end, value] = [10, 1, 5];
+    expect(Validation.isInRange(start, end, value)).toThrow();
+  });
+});
