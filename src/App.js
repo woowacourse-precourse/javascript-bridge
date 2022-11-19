82779191bd5a100474745de8c7113e1f293cef3c
jjo-mi <whdal2636@naver.com>
2022-11-19 19:26:12 +0900

    feat: 3. 입력받은 길이 만큼 다리 생성해서 U,D형태 배열 만들기

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ const { Console, Random } = require("@woowacourse/mission-utils");
 const { GUIDE_MESSAGE, ERROR_MESSAGE } = require("./Constant");
 const outputView = require("./OutputView");
 const inputView = require("./InputView");
-
+const bridgeMaker = require("./BridgeMaker");
+const bridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
 
 class App {
   play() {
@@ -11,7 +12,9 @@ class App {
       if(size < 2 || size > 20) {
         throw new Error(ERROR_MESSAGE.LENGTH);
       }
+      const bridgeArr = bridgeMaker.makeBridge(size, bridgeRandomNumberGenerator.generate);
     });
+
   }
 
 }
