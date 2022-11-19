4be125a858178c7dd50718a67836671d347f5c23
leeharyung <lhr4884@naver.com>
2022-11-19 22:02:54 +0900

    feat: add bridge maker

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,13 +1,28 @@
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
+const BRIDGE = {
+	UP: 'U',
+	DOWN: 'D',
+	CORRECT: 'O',
+	WRONG: 'X',
+	RANDOM_DOWN: 0,
+	RANDOM_UP: 1,
+};
+
 const BridgeMaker = {
-  /**
-   * @param {number} size 다리의 길이
-   * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
-   * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
-   */
-  makeBridge(size, generateRandomNumber) {},
+	/**
+	 * @param {number} size 다리의 길이
+	 * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
+	 * @return {string} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
+	 */
+	makeBridge: (size, generateRandomNumber) => {
+		return Array.from({ length: size }).map(() => {
+			return generateRandomNumber() === BRIDGE.RANDOM_DOWN
+				? BRIDGE.DOWN
+				: BRIDGE.UP;
+		});
+	},
 };
 
 module.exports = BridgeMaker;
