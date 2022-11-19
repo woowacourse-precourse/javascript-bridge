e8568e4af7fedd5e02d1943d2ff9f2bfae080704
dev-redo <mis05041@naver.com>
2022-11-19 17:44:35 +0900

    feat(InputView, OutputView): InputView, OutputView 클래스를 인터페이스화

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,21 +1,32 @@
+const { ERROR_MESSAGE } = require('../constants');
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
+  input() {
+    throw new Error(ERROR_MESSAGE.interface_class);
+  },
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    throw new Error(ERROR_MESSAGE.interface_class);
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    throw new Error(ERROR_MESSAGE.interface_class);
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+    throw new Error(ERROR_MESSAGE.interface_class);
+  },
 };
 
 module.exports = InputView;
--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,20 +1,28 @@
+const { ERROR_MESSAGE } = require('../constants');
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 const OutputView = {
+  output() {
+    throw new Error(ERROR_MESSAGE.interface_class);
+  },
   /**
    * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {},
+  printMap() {
+    throw new Error(ERROR_MESSAGE.interface_class);
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() {},
+  printResult() {
+    throw new Error(ERROR_MESSAGE.interface_class);
+  },
 };
 
 module.exports = OutputView;
