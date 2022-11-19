0e1aa85f37d6b7d763e2adad987623aad1513273
thewronghand <penfreak77@gmail.com>
2022-11-20 07:09:58 +0900

    test: InputView 기능 테스트 내용 수정
    InputViewTest가 InputView 내의 세부 기능에 대해서만 테스트하도록 테스트 내용을 수정한다.

--- __tests__/InputViewTest.js
+++ __tests__/InputViewTest.js
@@ -1,71 +1,33 @@
-const MissionUtils = require('@woowacourse/mission-utils');
 const InputView = require('../src/InputView');
-const App = require('../src/App');
-const { MESSAGE } = require('../src/constants/messages');
 
 describe('InputView 기능 테스트', () => {
   test('다리 길이 입력 시 유효하지 않은 값을 입력하면 예외가 발생한다', () => {
-    runBridgeSizeException(['string']);
-    runBridgeSizeException(['105']);
-    runBridgeSizeException(['1']);
+    try {
+      InputView.handleBridgeSizeException('45');
+    } catch (error) {
+      expect(error).toEqual(
+        '[ERROR] 다리 길이는 3부터 20 사이의 자연수여야 합니다.'
+      );
+    }
   });
 
   test('이동 방향 입력 시 유효하지 않은 값을 입력하면 예외가 발생한다.', () => {
-    const logSpy = getLogSpy();
-    mockQuestions(['3', 'Up']);
-    const app = new App();
-    app.play();
-    expectLogContains(getOutput(logSpy), ['[ERROR]']);
+    try {
+      InputView.handleMovingException('Up');
+    } catch (error) {
+      expect(error).toEqual(
+        '[ERROR] 이동할 칸은 영문 대문자 U나 D만 입력할 수 있습니다.'
+      );
+    }
   });
 
   test('게임 커맨드 입력 시 유효하지 않은 값을 입력하면 예외가 발생한다', () => {
-    const logSpy = getLogSpy();
-    mockRandoms(['1', '0', '1']);
-    mockQuestions(['3', 'U', 'U', 'Quit']);
-    const app = new App();
-    app.play();
-    expectLogContains(getOutput(logSpy), ['[ERROR]']);
+    try {
+      InputView.handleGameCommandException('Quit');
+    } catch (error) {
+      expect(error).toEqual(
+        '[ERROR] 종료 및 재시작 명령어는 영문 대문자 R이나 Q만 입력할 수 있습니다.'
+      );
+    }
   });
 });
-
-const mockQuestions = (answers) => {
-  MissionUtils.Console.readLine = jest.fn();
-  answers.reduce((acc, input) => {
-    return acc.mockImplementationOnce((_, callback) => {
-      callback(input);
-    });
-  }, MissionUtils.Console.readLine);
-};
-
-const mockRandoms = (numbers) => {
-  MissionUtils.Random.pickNumberInRange = jest.fn();
-  numbers.reduce((acc, number) => {
-    return acc.mockReturnValueOnce(number);
-  }, MissionUtils.Random.pickNumberInRange);
-};
-
-const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
-  logSpy.mockClear();
-  return logSpy;
-};
-
-const expectLogContains = (received, logs) => {
-  logs.forEach((log) => {
-    expect(received).toEqual(expect.stringContaining(log));
-  });
-};
-
-const getOutput = (logSpy) => {
-  return [...logSpy.mock.calls].join('');
-};
-
-const runBridgeSizeException = (inputs) => {
-  mockQuestions(inputs);
-  const logSpy = getLogSpy();
-  const app = new App();
-
-  app.play();
-
-  expectLogContains(getOutput(logSpy), ['[ERROR]']);
-};
