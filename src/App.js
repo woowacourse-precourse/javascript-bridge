0d12a7b91d83c4396453762ad557acd35c9a39d3
하송희 <99heesong@naver.com>
2022-11-19 20:38:29 +0900

    기능추가: 다리 건너기 게임 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+const { Console } = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    Console.print("다리 건너기 게임을 시작합니다.\n");
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
