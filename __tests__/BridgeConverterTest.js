400c824ea29b2dbb62dab267e4562c7a5c486ee8
unsnruu <unsnruu@gmail.com>
2022-11-20 03:53:49 +0900

    test: writeBridgeD/U에 대한 테스트 작성

--- __tests__/BridgeConverterTest.js
+++ __tests__/BridgeConverterTest.js
@@ -7,4 +7,36 @@ describe("다리 배열을 문자열로 바꾸는 BridgeConverter 테스트", ()
       [" ", " ", " "],
     ]);
   });
+  test("사용자가 U를 입력하고 정답이 U라면 입력값으로 받은 색인의 bridge에 O표시", () => {
+    const bridge = BridgeConverter.makeEmptyBridge(3);
+    BridgeConverter.writeBridgeU("U", 0, bridge);
+    expect(bridge).toEqual([
+      ["O", " ", " "],
+      [" ", " ", " "],
+    ]);
+  });
+  test("사용자가 D를 입력하고 정답이 U라면 입력값으로 받은 색인의 bridge에 X표시", () => {
+    const bridge = BridgeConverter.makeEmptyBridge(3);
+    BridgeConverter.writeBridgeU("D", 0, bridge);
+    expect(bridge).toEqual([
+      ["X", " ", " "],
+      [" ", " ", " "],
+    ]);
+  });
+  test("사용자가 D를 입력하고 정답이 D라면 입력값으로 받은 색인의 bridge에 O표시", () => {
+    const bridge = BridgeConverter.makeEmptyBridge(3);
+    BridgeConverter.writeBridgeD("D", 0, bridge);
+    expect(bridge).toEqual([
+      [" ", " ", " "],
+      ["O", " ", " "],
+    ]);
+  });
+  test("사용자가 U를 입력하고 정답이 D라면 입력값으로 받은 색인의 bridge에 X표시", () => {
+    const bridge = BridgeConverter.makeEmptyBridge(3);
+    BridgeConverter.writeBridgeD("U", 0, bridge);
+    expect(bridge).toEqual([
+      [" ", " ", " "],
+      ["X", " ", " "],
+    ]);
+  });
 });
