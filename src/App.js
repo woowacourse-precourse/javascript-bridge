6ac1bb0e64411307b9847bce3b9258ee2e33136b
daal2quf <daal2quf@gmail.com>
2022-11-19 14:12:32 +0900

    feat(app): 유저에게 다리길이를 입력받아 bridgeGame 인스턴스 생성
    add:
    InputView.readBridgeSize(): 유저에게 다리를 입력받고 argument로 this.gamestart 메서드를 넘겨줌

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+const BridgeGame = require("./BridgeGame");
+const InputView = require("./InputView");
 const OutputView = require("./OutputView");
 
 class App {
   play() {
     OutputView.printGameStartMsg();
+    InputView.readBridgeSize.call(this, this.gameStart);
+  }
+  gameStart(userInputBridgeLength) {
+    const bridgeGame = new BridgeGame(userInputBridgeLength);
+  }
+
+  gamese() {
+
   }
 }
 
--- src/InputView.js
+++ src/InputView.js
@@ -2,22 +2,16 @@ const { Console } = require("@woowacourse/mission-utils");
 
 const InputView = {
 
-  readBridgeSize() {
-    Console.readLine('다리의 길이를 입력해주세요.\n', (userInputBridgeLength) => {
-      return userInputBridgeLength;
-    });
+  readBridgeSize(callback) {
+    Console.readLine('다리의 길이를 입력해주세요.\n', callback);
   },
 
-  readMoving() {
-    Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)\n', (userInputMoving) => {
-      return userInputMoving;
-    });
+  readMoving(callback) {
+    Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)\n', callback);
   },
 
-  readRetryOrQuit() {
-    Console.readLine('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n', (userInputRetryOrQuit) => {
-      return userInputRetryOrQuit;
-    });
+  readRetryOrQuit(callback) {
+    Console.readLine('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n', callback);
   },
 };
 
