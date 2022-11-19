8dd05bcebb6e46bd843933c926bbd5cab6cb6f63
dladncks1217 <dlaxodud1217@gmail.com>
2022-11-20 02:51:31 +0900

    test(makePrintBridge): 출력될 다리 제작 유틸함수 테스트 추가

--- __tests__/UtilTest.js
+++ __tests__/UtilTest.js
@@ -1,7 +1,8 @@
 const bridgeMaterialize = require('../src/utils/bridgeMaterialize');
+const makePrintBridge = require('../src/utils/makePrintBridge');
 
 describe('유틸 함수 동작 테스트', () => {
-  test('다리 구체화 함수 테스트', () => {
+  test('다리 구체화 유틸함수 테스트', () => {
     expect(bridgeMaterialize(['U', 'D', 'D'])).toEqual([
       ['O', ' ', ' '],
       [' ', 'O', 'O'],
@@ -9,6 +10,64 @@ describe('유틸 함수 동작 테스트', () => {
     expect(bridgeMaterialize(['U', 'D', 'U'])).toEqual([
       ['O', ' ', 'O'],
       [' ', 'O', ' '],
+    ]);
+  });
+
+  test('출력될 다리 제작 유틸함수 테스트', () => {
+    expect(
+      makePrintBridge(
+        [
+          ['O', ' ', 'O'],
+          [' ', 'O', ' '],
+        ],
+        2,
+        false
+      )
+    ).toEqual([
+      ['O', ' ', ' '],
+      [' ', 'O', 'X'],
+    ]);
+
+    expect(
+      makePrintBridge(
+        [
+          ['O', ' ', 'O'],
+          [' ', 'O', ' '],
+        ],
+        2,
+        true
+      )
+    ).toEqual([
+      ['O', ' ', 'O'],
+      [' ', 'O', ' '],
+    ]);
+
+    expect(
+      makePrintBridge(
+        [
+          ['O', ' ', 'O', 'O'],
+          [' ', 'O', ' ', ' '],
+        ],
+        3,
+        false
+      )
+    ).toEqual([
+      ['O', ' ', 'O', ' '],
+      [' ', 'O', ' ', 'X'],
+    ]);
+
+    expect(
+      makePrintBridge(
+        [
+          ['O', ' ', 'O', 'O'],
+          [' ', 'O', ' ', ' '],
+        ],
+        3,
+        true
+      )
+    ).toEqual([
+      ['O', ' ', 'O', 'O'],
+      [' ', 'O', ' ', ' '],
     ]);
   });
 });
