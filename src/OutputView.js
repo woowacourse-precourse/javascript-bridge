c1b31e5332032b0ead2481a751a9ea6588fd4a26
durumi99 <kms990615@naver.com>
2022-11-19 12:58:52 +0900

    feat: 게임 시작 문구를 출력하는 기능 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,7 +1,12 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const { MESSAGE } = require('./constant/Constant');
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 const OutputView = {
+  printInit() {
+    MissionUtils.Console.print(MESSAGE.INIT);
+  },
   /**
    * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
    * <p>
@@ -15,6 +20,7 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printResult() {},
+
 };
 
 module.exports = OutputView;
