ac2093e4aa7b0293d7db08076e2617ef2f612ba3
Deemou <4106243@naver.com>
2022-11-19 15:27:26 +0900

    feat(BridgeMaker): 다리를 생성하는 기능 추가

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,3 +1,4 @@
+const { BRIDGE } = require('./constant/Bridge');
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
@@ -7,7 +8,14 @@ const BridgeMaker = {
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
-  makeBridge(size, generateRandomNumber) {},
+  makeBridge(size, generateRandomNumber) {
+    const bridge = [];
+    for (let i = 0; i < size; i++) {
+      const space = generateRandomNumber() == 0 ? BRIDGE.DOWN : BRIDGE.UP;
+      bridge.push(space);
+    }
+    return bridge;
+  }
 };
 
 module.exports = BridgeMaker;
