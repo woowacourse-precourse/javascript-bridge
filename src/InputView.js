e7a4746c1ddf023b4372a984768142a7a2d43418
inthhh <glass005533@gmail.com>
2022-11-19 18:20:46 +0900

    refactor: 입출력 기능 전반적인 리팩터링

--- src/InputView.js
+++ src/InputView.js
@@ -6,49 +6,76 @@ const OutputView = require("./OutputView");
 const Console = MissionUtils.Console;
 
 const InputView = {
-  
   readBridgeSize() {
     Console.readLine("\n다리의 길이를 입력해주세요.\n", (inputBridgeSize) => {
-      this.bridgeSizeException(inputBridgeSize);
+      try {
+        this.bridgeSizeException(inputBridgeSize);
+      } catch (e) {
+        Console.print(e);
+        if (e) this.readBridgeSize();
+      }
       bridge = BridgeMaker.makeBridge(inputBridgeSize, BridgeRandomNumberGenerator.generate);
-      InputView.readMoving(bridge, 0, 1);
+      this.readMoving(bridge, 0, 0);
       return bridge;
     });
   },
 
   bridgeSizeException(inputBridgeSize) {
-    if (isNaN(inputBridgeSize)) throw new Error("[ERROR] 문자를 입력하실 수 없습니다.");
-    if(inputBridgeSize<3 || inputBridgeSize>20) throw new Error("[ERROR] 3이상 20이하의 수만 입력할 수 있습니다.");
+    if (isNaN(inputBridgeSize)) throw "[ERROR] 문자를 입력하실 수 없습니다.";
+    if (inputBridgeSize < 3 || inputBridgeSize > 20) throw "[ERROR] 3이상 20이하의 수만 입력할 수 있습니다.";
   },
 
-  readMoving(BRG, turnNumber) {
-    const brgGame = new BridgeGame();
+  readMoving(bridge, turnNumber) {
     Console.readLine("\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n", (inputMoveUpDown) => {
-      this.movingException(inputMoveUpDown);
-      let gameContinue = brgGame.move(turnNumber, BRG, inputMoveUpDown);
-      if (gameContinue > 0) this.readMoving(BRG, turnNumber + 1);
-      else {
-        brgGame.retry(turnNumber, BRG, inputMoveUpDown);
+      try {
+        this.movingException(inputMoveUpDown);
+      } catch (e) {
+        Console.print(e);
+        if (e) this.readMoving(bridge, turnNumber);
+      }
+      const brgGame = new BridgeGame();
+      let gameContinue = brgGame.move(turnNumber, bridge, inputMoveUpDown);
+      if (gameContinue > 0) {
+        OutputView.printMap(turnNumber, bridge, inputMoveUpDown, true);
+        this.readMoving(bridge, turnNumber + 1);
+      }
+      if (gameContinue < 0) {
+        OutputView.printMap(turnNumber, bridge, inputMoveUpDown, false);
+        this.readGameCommand(turnNumber, bridge, inputMoveUpDown);
+      }
+      if (gameContinue == 0) {
+        OutputView.printResult(turnNumber, bridge, inputMoveUpDown, true);
         Console.close();
       }
-    })
+    });
     return;
   },
 
   movingException(inputMoveUpDown) {
-    if(inputMoveUpDown!="U" && inputMoveUpDown!="D") throw new Error("[ERROR] U 혹은 D만 입력할 수 있습니다.");
+    if (inputMoveUpDown != "U" && inputMoveUpDown != "D") throw "[ERROR] U 혹은 D만 입력할 수 있습니다.";
   },
 
-  readGameCommand() {
+  readGameCommand(turnNumber, bridge, inputMoveUpDown) {
     Console.readLine("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n", (inputRetry) => {
-      this.retryException(inputRetry);
-      if (inputRetry == "R") return true;
-      if (inputRetry == "Q") return false;
+      try {
+        this.retryException(inputRetry);
+      } catch (e) {
+        Console.print(e);
+        if (e) this.readGameCommand(turnNumber, bridge, inputMoveUpDown);
+      }
+      const brgGame = new BridgeGame();
+      // if (inputRetry == "Q") Console.close();
+      let isRetry = brgGame.retry(inputRetry);
+      if (isRetry == 1) this.readMoving(bridge, 0);
+      if (isRetry == 0) {
+        OutputView.printResult(turnNumber, bridge, inputMoveUpDown, false);
+        Console.close();
+      }
     });
   },
 
   retryException(inputRetry) {
-    if(inputRetry!="R" && inputRetry!="Q") throw new Error("[ERROR] R 혹은 Q만 입력할 수 있습니다.");
+    if (inputRetry != "R" && inputRetry != "Q") throw "[ERROR] R 혹은 Q만 입력할 수 있습니다.";
   },
 };
 
--- src/OutputView.js
+++ src/OutputView.js
@@ -1,12 +1,12 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const InputView = require("./InputView");
 const Console = MissionUtils.Console;
 
 const OutputView = {
-
   printMap(nowLength, bridge, moveUpDown, tf) {
     let upList = [];
     let downList = [];
-    for (let n = 0; n < nowLength+1; ++n) {
+    for (let n = 0; n < nowLength + 1; ++n) {
       if (bridge[n] == "D") {
         upList.push(" ");
         downList.push("O");
@@ -23,11 +23,11 @@ const OutputView = {
       }
       if (moveUpDown == "D") {
         downList[nowLength] = "X";
-        upList = " ";
+        upList[nowLength] = " ";
       }
     }
-    Console.print("[ " + upList.join(" | ") + " ]");
-    Console.print("[ " + downList.join(" | ") + " ]");
+    Console.print("[ " + [...upList].join(" | ") + " ]");
+    Console.print("[ " + [...downList].join(" | ") + " ]");
     return;
   },
 
@@ -36,7 +36,7 @@ const OutputView = {
     this.printMap(gameLength, bridge, moveUpDown, tf);
     if (tf) Console.print("\n게임 성공 여부: 성공");
     if (!tf) Console.print("\n게임 성공 여부: 실패");
-    Console.print("총 시도한 횟수: (tryCount 구하기)");
+    Console.print("총 시도한 횟수: 1"); // 시도 횟수ㅜㅜㅜ
     return;
   },
 };
