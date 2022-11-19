ac2ba6598916a67e0b868a2e5c163a8e0349f7ec
sunub <bsc5672@gmail.com>
2022-11-20 02:41:22 +0900

    feat : 사다리 게임을 수행하기 위한 App추가
    사용자가 App을 통하여 play를 입력하였을 경우 게임을 바로 수행할 수
    있게끔 기능을 추가 하였고 미션을 추가 하기 위해 필요한 사용자의 데이터에
    대하여 추가 하였다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+const InputView = require("./InputView");
+
 class App {
-  play() {}
+  #data = {
+    callCount: 0,
+    upperBridge: "",
+    lowerBridge: "",
+    currentAnswer: "",
+    bridge: [],
+    status: "",
+    try: 1,
+  };
+  play() {
+    InputView.readBridgeSize(this.#data);
+  }
 }
 
 module.exports = App;
+
+const a = new App();
+a.play();
