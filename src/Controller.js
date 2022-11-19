6621f58b0a02af4505d1b2da66f8f5da7e41f069
Jiwoo Park <jiwoo9446@naver.com>
2022-11-20 01:24:26 +0900

    feat: 랜덤 숫자를 이용한 다리 만들기 메서드 구현

--- src/Controller.js
+++ src/Controller.js
@@ -1,8 +1,11 @@
 const InputView = require("./InputView");
-const Validation = require("./utils/validation.js");
+const Validation = require("./utils/validation");
 const { Console } = require("@woowacourse/mission-utils");
+const BridgeMaker = require("./BridgeMaker");
+const BridgeMachine = require("./BridgeRandomNumberGenerator");
 
 class Controller {
+  #bridge;
   constructor() {
     this.validation = new Validation();
   }
@@ -17,6 +20,7 @@ class Controller {
   bridgeSizeForm(length) {
     try {
       this.validation.isValidLength(length);
+      this.#bridge = BridgeMaker.makeBridge(length, BridgeMachine.generate);
     } catch (error) {
       Console.print(error);
     }
