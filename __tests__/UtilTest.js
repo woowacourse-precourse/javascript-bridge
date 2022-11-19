2e4731c0db8adafe910de2dfcf9454e986bfe2d1
dladncks1217 <dlaxodud1217@gmail.com>
2022-11-20 03:14:03 +0900

    test(validCheck.moveInput): 위치 이동 입력값 검증 유틸함수 테스트 추가

--- __tests__/UtilTest.js
+++ __tests__/UtilTest.js
@@ -77,4 +77,12 @@ describe('유틸 함수 동작 테스트', () => {
     expect(validCheck.bridgeLength('21')).toBe(false);
     expect(validCheck.bridgeLength('a')).toBe(false);
   });
+
+  test('위치 이동 입력값 검증 함수', () => {
+    expect(validCheck.moveInput('U')).toBe(true);
+    expect(validCheck.moveInput('D')).toBe(true);
+    expect(validCheck.moveInput('1')).toBe(false);
+    expect(validCheck.moveInput('u')).toBe(false);
+    expect(validCheck.moveInput('d')).toBe(false);
+  });
 });
