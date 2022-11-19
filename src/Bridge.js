e8ec6609446dd74f6b1dde37e6d3956b9ed21451
nangkyeonglim <nangkyeonglim@gmail.com>
2022-11-19 15:00:48 +0900

    feat(Bridge): 다리의 끝인지 확인하는 함수 구현

--- src/Bridge.js
+++ src/Bridge.js
@@ -9,6 +9,10 @@ class Bridge {
   isCurrentDirection(direction) {
     return this.#directions === direction;
   }
+
+  isBridgeEnd() {
+    return this.#currentDirection === this.#directions.length - 1;
+  }
 }
 
 module.exports = Bridge;
