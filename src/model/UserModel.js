89ba9ee15f3f6e3196fda05b3c897b239a9a65b5
sh981013s <sh981013s@gmail.com>
2022-11-19 14:53:12 +0900

    comment(UserModel): 시도 횟수 getter, 증가 메서드 주석 작성

--- src/model/UserModel.js
+++ src/model/UserModel.js
@@ -5,10 +5,15 @@ class UserModel {
     this.#tryCount = 0;
   }
 
+  /**
+   * 유저의 시도 횟수를 반환한다.
+   * @return {number} [유저의 시도 횟수]
+   */
   getTryCount() {
     return this.#tryCount;
   }
 
+  // 유저의 시도 횟수를 1 증가시킨다.
   increaseTryCount() {
     this.#tryCount = this.#tryCount + 1;
   }
