cadd8799fc4d9c3cebd12f44ae414f095c96c168
sh981013s <sh981013s@gmail.com>
2022-11-19 20:46:20 +0900

    feat(BridgeController): 유저 다리 길이 입력 후 검증 메서드, 로직 구현

--- src/controller/BridgeController.js
+++ src/controller/BridgeController.js
@@ -5,6 +5,24 @@ class BridgeController {
     this.mainController = mainController;
     this.bridgeModel = new BridgeModel();
   }
+
+  /**
+   * 다리 길이에 대한 input 을 검증한다.
+   * @param bridgeLengthInput {string} [다리 길이 input]
+   */
+  validateBridgeLengthInput(bridgeLengthInput) {
+    if (!GENERAL_CONSTANTS.BRIDGE_LENGTH_REGEX.test(bridgeLengthInput)) {
+      throw new Error(BRIDGE_INPUT_MESSAGES.INPUT_ERROR);
+    }
+  }
+
+  /**
+   * 유저에게 다리 길이를 입력 받은 후 순서에 맞게 실행한다.
+   * @param bridgeLengthInput {string} [다리 길이 input]
+   */
+  onBridgeSizeInput(bridgeLengthInput) {
+    this.validateBridgeLengthInput(bridgeLengthInput);
+  }
 }
 
 module.exports = BridgeController;
