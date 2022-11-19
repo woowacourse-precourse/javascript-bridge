696f8cfcdacd0af739aac496e24cbc818c273b46
AhnGeunTaek <dksrmsxor@gmail.com>
2022-11-19 23:37:36 +0900

    test: 다리 길이 입력값에 대한 예외 테스트 추가

--- __tests__/InputViewTest.js
+++ __tests__/InputViewTest.js
@@ -11,10 +11,26 @@ const mockQuestions = (answers) => {
 };
 
 describe("InputView 테스트", () => {
-  test("다리 사이즈 입력 유효 테스트", () => {
-    mockQuestions(["r"]);
+  test("다리 길이 입력값 유효 테스트", () => {
+    mockQuestions(["g"]);
     expect(() => {
       InputView.readBridgeSize();
-    }).toThrow("[ERROR]");
+    }).toThrow("[ERROR] 다리의 길이는 숫자만 입력해야 합니다.");
+  });
+
+  test("다리 길이가 3보다 작은 숫자인 경우 예외 처리한다.", () => {
+    mockQuestions(["2"]);
+
+    expect(() => {
+      InputView.readBridgeSize();
+    }).toThrow("[ERROR] 다리의 길이는 3 이상 20 이하여야 합니다.");
+  });
+
+  test("다리 길이가 20보다 큰 숫자인 경우 예외 처리한다.", () => {
+    mockQuestions(["21"]);
+
+    expect(() => {
+      InputView.readBridgeSize();
+    }).toThrow("[ERROR] 다리의 길이는 3 이상 20 이하여야 합니다.");
   });
 });
