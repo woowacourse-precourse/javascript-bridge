a1882ed4ee6b01010022a779696ea12b93dbc629
Jin1won <wlsdnjs16521@gmail.com>
2022-11-19 15:57:39 +0900

    feat: 입력한 값이 공백인 경우 예외 처리한다.

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,9 +1,12 @@
 const { ERROR_MESSAGE, BRIDGE_VALUE } = require("../constants");
 
 const isBridgeLengthValid = (value) => {
+  if (isEmpty) throw Error(ERROR_MESSAGE.EMPTY_ERROR);
   if (!isNumberType(value)) throw Error(ERROR_MESSAGE.TYPE_ERROR);
   if (!isRangeValid(value)) throw Error(ERROR_MESSAGE.RANGE_ERROR);
 };
+
+const isEmpty = (value) => !value;
 
 const isNumberType = (value) => !Number.isNaN(Number(value));
 
