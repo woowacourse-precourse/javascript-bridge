7d55e812ef928fec562d5a4bfef6922bd092a2d2
vinivin153 <vinivin153@naver.com>
2022-11-19 16:20:40 +0900

    feat: 이동할 칸 선택  예외 테스트

--- __tests__/ExceptionTest.js
+++ __tests__/ExceptionTest.js
@@ -1,23 +1,34 @@
-const { bridgeSizeValidator } = require('../src/Validator');
+const { bridgeSizeValidator, directionValidator } = require('../src/Validator');
 
-describe('다리 길이 예외 테스트', () => {
+describe('예외 테스트', () => {
   test.each([
-    [['03'], false],
-    [['a'], false],
-    [[' 3'], false],
-    [['3.3'], false],
-    [[' '], false],
-    [['18'], true],
+    ['03', false],
+    ['a', false],
+    [' 3', false],
+    ['3.3', false],
+    [' ', false],
+    ['18', true],
   ])('숫자 예외 테스트(자연수만)', (input, result) => {
     expect(bridgeSizeValidator.isNumber(input)).toEqual(result);
   });
 
   test.each([
-    [['1'], false],
-    [['21'], false],
-    [['3'], true],
-    [['20'], true],
+    ['1', false],
+    ['21', false],
+    ['3', true],
+    ['20', true],
   ])('범위 예외 테스트', (input, result) => {
     expect(bridgeSizeValidator.isValidRange(Number(input))).toEqual(result);
+  });
+
+  test.each([
+    ['U', true],
+    ['D', true],
+    ['32', false],
+    ['', false],
+    [' D', false],
+    ['DD', false],
+  ])('이동할 칸 선택 예외 테스트', (input, result) => {
+    expect(directionValidator.isUpOrDown(input)).toEqual(result);
   });
 });
