5bd2bfa2e87d6574eef3ff986d3ab1c54fa649cf
Adult96 <djxor007@gmail.com>
2022-11-19 23:22:59 +0900

    remove(BridgeMakerTest): 다리 생성 함수 테스트 중복되어 삭제

--- __tests__/BridgeMakerTest.js
+++
@@ -1,9 +0,0 @@
-const BridgeMaker = require('../src/BridgeMaker');
-
-describe('BridgeMaker 테스트', () => {
-  test('랜덤 숫자 생성하여 U/D로 변환', () => {
-    expect(BridgeMaker.makeBridge(3, () => 1)).toEqual(
-      expect.arrayContaining(['U', 'U', 'U'])
-    );
-  });
-});
