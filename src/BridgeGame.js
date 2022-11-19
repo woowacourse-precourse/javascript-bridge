b8a67c702534988c1325c36212c2fd83281f7573
kwh97 <zzzang0512@naver.com>
2022-11-19 19:41:33 +0900

    feat: 사용자에게 이동할 칸 입력을 요청하는 기능 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -7,10 +7,14 @@ const BridgeMaker = require("./BridgeMaker");
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  #bridge
+
   set() {
     InputView.readBridgeSize(INPUT_QUESTION.bridgeLen, (bridgeLength) => {
       Validation.checkBridgeLength(bridgeLength);
       const bridge = BridgeMaker.makeBridge(bridgeLength, BridgeRandomNumberGenerator.generate);
+      this.#bridge = bridge;
+      this.move();
     });
   }
   /**
@@ -18,7 +22,11 @@ class BridgeGame {
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move() {
+    InputView.readMoving('이동할 칸을 선택해주세요.', (space) => {
+
+    });
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
--- src/InputView.js
+++ src/InputView.js
@@ -13,7 +13,9 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving(question, callback) {
+    Console.readLine(question, callback);
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
