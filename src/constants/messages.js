3a46857e219542aa0c55d1a70e8cf1179e42daa1
sh981013s <sh981013s@gmail.com>
2022-11-19 17:23:29 +0900

    feat(messages): 다리길이 입력, 에러 메세지 상수화

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,12 +1,7 @@
-const BRIDGE_GAME_MESSAGES = Object.freeze({
+const BRIDGE_INPUT_MESSAGES = Object.freeze({
   OPENING: "다리 건너기 게임을 시작합니다.\n",
-  LENGTH_INPUT: "다리의 길이를 입력해주세요.\n",
-  MOVE_INPUT: "이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
-  RESTART_INPUT:
-    "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n",
-  FINAL_RESULT: "최종 게임 결과\n",
-  SUCCESS_STATUS: "게임 성공 여부: ",
-  TRY_COUNT: "총 시도한 횟수: ",
+  INPUT: "다리의 길이를 입력해주세요.\n",
+  INPUT_ERROR: "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.\n",
 });
 
-module.exports = BRIDGE_GAME_MESSAGES;
+module.exports = { BRIDGE_INPUT_MESSAGES };
