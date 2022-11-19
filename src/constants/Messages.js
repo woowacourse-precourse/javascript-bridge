c84f753d61e8d7f0faa810f6d1c13cbb0aba6497
sh981013s <sh981013s@gmail.com>
2022-11-20 02:16:26 +0900

    feat(Messages): 이동할 칸 메세지 상수 객체 구현

--- src/constants/Messages.js
+++ src/constants/Messages.js
@@ -1,7 +1,12 @@
 const BRIDGE_INPUT_MESSAGES = Object.freeze({
   OPENING: "다리 건너기 게임을 시작합니다.\n",
   INPUT: "다리의 길이를 입력해주세요.\n",
-  INPUT_ERROR: "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.\n",
+  INPUT_ERROR: "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.",
 });
 
-module.exports = { BRIDGE_INPUT_MESSAGES };
+const USER_MOVE_MESSAGES = Object.freeze({
+  INPUT: "\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
+  INPUT_ERROR: "[ERROR] 이동할 칸은 U(위) 와 D(아래)만 입력 가능합니다.",
+});
+
+module.exports = { BRIDGE_INPUT_MESSAGES, USER_MOVE_MESSAGES };
