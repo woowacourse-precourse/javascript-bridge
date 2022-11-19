ca953fb2cfaf1fc86511852c2165b9ded8e54c39
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-19 20:55:48 +0900

    feat(TemplateMaker): 파일 생성 및 템플릿용 심볼 상수 추가

---
+++ src/utils/TemplateMaker.js
@@ -0,0 +1,10 @@
+const { UPSIDE_SYMBOL, DOWNSIDE_SYMBOL } = require('../constants/condition.js');
+
+const SYMBOLS = {
+  start: '[',
+  end: ']',
+  fail: 'X',
+  success: 'O',
+  blank: ' ',
+  separator: ' | ',
+};
