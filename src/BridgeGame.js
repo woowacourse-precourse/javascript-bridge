acb92da4cd4947a51f6168c5394307fd2dba539b
chocoro99 <76997927+chocoro99@users.noreply.github.com>
2022-11-19 21:28:24 +0900

    feat : 다리 결과 배열에 이동 결과 값 "O"나 "X" 또는 "공백" 추가

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -9,6 +9,7 @@ const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
 class BridgeGame {
   constructor() {
     this.bridge = [];
+    this.bridgeResult = [[], []];
     this.size = 0;
     this.moving = "";
   }
@@ -35,13 +36,30 @@ class BridgeGame {
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move() {
+    this.readMove();
+    this.pushXOrO(i);
+    this.blankPush();
+  }
 
   readMove() {
     OutputView.pickMoveSentence();
     this.moving = InputView.readMoving();
   }
 
+  pushXOrO(i) {
+    if (this.moving === "U") this.bridgeResult[0].push(this.returnXOrO(i));
+    if (this.moving === "D") this.bridgeResult[1].push(this.returnXOrO(i));
+  }
+
+  blankPush() {
+    if (this.bridgeResult[0].length > this.bridgeResult[1].length) {
+      this.bridgeResult[1].push(" ");
+    }
+    if (this.bridgeResult[0].length < this.bridgeResult[1].length) {
+      this.bridgeResult[0].push(" ");
+    }
+  }
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
    * <p>
