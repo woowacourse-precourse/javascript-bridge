b71ada6c73ba2c81c72a0da78d0b719fbf8a2ad5
nangkyeonglim <nangkyeonglim@gmail.com>
2022-11-19 14:58:42 +0900

    feat(BridgeGame): 다리를 건널 수 있는지 확인하는 기능 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,7 +1,14 @@
+const Bridge = require("./Bridge");
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  #bridge;
+
+  constructor() {
+    this.bridge = new Bridge();
+  }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
@@ -15,6 +22,11 @@ class BridgeGame {
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   retry() {}
+
+  isAnswer(direction) {
+    if (this.bridge.isCurrentDirection(direction)) return true;
+    return false;
+  }
 }
 
 module.exports = BridgeGame;
