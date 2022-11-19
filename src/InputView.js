cf667ce85a1db527b8e7b64aee42c36ff5743972
thewronghand <penfreak77@gmail.com>
2022-11-19 10:28:58 +0900

    feat: 사용자 이동방향 입력 메서드 readMoving 구현
    사용자로부터 이동 방향을 입력받아 유효성을 판별하고 bridgeGame 내에서 move 메서드를 실행하는 로직을 구현한다.

--- src/InputView.js
+++ src/InputView.js
@@ -1,5 +1,6 @@
 const BridgeGame = require ('./BridgeGame');
 const { MESSAGE } = require('./constants/messages');
+const Validate = require('./utils/validation');
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -17,7 +18,14 @@ const { MESSAGE } = require('./constants/messages');
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+   readMoving(game) {
+    MissionUtils.Console.readLine(
+      MESSAGE.READ_USER_INPUT_MOVE,
+      (input) => {
+        this.handleMovingException(input, game);
+      }
+    );
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
@@ -32,6 +40,18 @@ const { MESSAGE } = require('./constants/messages');
     } catch (error) {
       MissionUtils.Console.print(MESSAGE.ERROR.USER_INPUT_BRIDGE_SIZE_INVALID);
       this.readBridgeSize();
+    }
+  },
+
+  handleMovingException(move, game) {
+    try {
+      Validate.userInputMove(move);
+      const moveResult = game.move(move);
+      OutputView.printMap(moveResult[0], moveResult[1]);
+      this.handleGameStatus(moveResult[2], game);
+    } catch (error) {
+      MissionUtils.Console.print(MESSAGE.ERROR.USER_INPUT_MOVING_KEY_INVALID);
+      this.readMoving(game);
     }
   },
 };
