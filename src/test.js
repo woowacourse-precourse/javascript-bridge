dce869d4a7c96918fa77f1c056943df51273a4b9
TeusLee <xodn1311@gmail.com>
2022-11-19 13:24:25 +0900

    Implement BridgeMaker.js

---
+++ src/test.js
@@ -0,0 +1,24 @@
+const randnum_gen = require("../src/BridgeRandomNumberGenerator");
+const MissionUtils = require("@woowacourse/mission-utils");
+/**
+ * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
+ */
+ const BridgeMaker = {
+    /**
+     * @param {number} size 다리의 길이
+     * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
+     * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
+     */
+    makeBridge(size, generateRandomNumber) {
+      bridge = []
+      for (let i = 0; i < size; i++){
+        temp = generateRandomNumber.generate()
+        MissionUtils.Console.close();
+        temp = temp === 1 ? "U" : "D"
+        bridge.push(temp)      
+      }
+      return bridge          
+    },
+  };
+
+BridgeMaker.makeBridge(8, randnum_gen)
