9500d4812f05d1a894f2cfeb9628357ff3be585c
Adult96 <djxor007@gmail.com>
2022-11-19 21:14:55 +0900

    feat(BridgeGame): 이동 변수값 증가 하는 기능 추가

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -30,9 +30,15 @@ class BridgeGame {
 
   progress(input) {
     if (input === 'U' || input === 'D') {
+      return this.progressMove(input);
     }
     if (input === 'R' || input === 'Q') {
     }
+  }
+
+  progressMove(input) {
+    const moveCount = this.#moveCount;
+    this.increamentMoveCount();
   }
 
   /**
@@ -48,6 +54,10 @@ class BridgeGame {
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   retry() {}
+
+  increamentMoveCount() {
+    return (this.#moveCount += 1);
+  }
 
   setBridgeList(list) {
     return (this.#bridgeList = list);
