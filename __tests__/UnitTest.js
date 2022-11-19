8980489f7113ab1ba5aace37da6dbae086109eda
2yunseong <dbsdltjd3701@naver.com>
2022-11-19 22:37:16 +0900

    test(BridgeMaker): 다리를 생성하는 기능 테스트

--- __tests__/UnitTest.js
+++ __tests__/UnitTest.js
@@ -1,9 +1,29 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const BridgeMaker = require('../src/BridgeMaker');
+const BridgeRandomNumberGenerator = require('../src/BridgeRandomNumberGenerator');
 const InputVaildator = require('../src/InputVaildator');
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+};
+
 describe('Vaildator 단위 테스트', () => {
   test('입력한 길이가 유효한지 판단하는 기능', () => {
     expect(InputVaildator.isVaildLength('5')).toEqual(true);
     expect(InputVaildator.isVaildLength('35.1')).toEqual(false);
     expect(InputVaildator.isVaildLength('-1')).toEqual(false);
     expect(InputVaildator.isVaildLength('0')).toEqual(false);
+  });
+});
+
+describe('BridgeMaker 단위 테스트', () => {
+  test('길이만큼 랜덤한 다리를 생성하는 기능', () => {
+    mockRandoms([0, 1, 1, 0, 0, 1]);
+    expect(
+      BridgeMaker.makeBridge(6, BridgeRandomNumberGenerator.generate)
+    ).toEqual(['D', 'U', 'U', 'D', 'D', 'U']);
   });
 });
