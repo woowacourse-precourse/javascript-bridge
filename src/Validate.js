cc581a99103447c80134ec3c90c58c536cacc560
한수지 <fatholic5@gmail.com>
2022-11-20 01:34:09 +0900

    :sparkles: feat: (Validate) 이동시 입력값을 인자로 받아 예외시 throw 오류 메세지를 호출하는 기능 추가 입력값이 U가 아닐 때 오류를 호출한다. 입력값이 D가 아닐 때 오류를 호출한다.

--- src/Validate.js
+++ src/Validate.js
@@ -1,4 +1,4 @@
-const{ SIZE } = require("./constants/Values");
+const{ SIZE , MOVING } = require("./constants/Values");
 const{ ERROR } = require("./constants/FixMessages");
 
 
@@ -7,6 +7,10 @@ class Validate {
   validateBridgeSize(size) {
     if(isNaN(size)) throw new Error(ERROR.INPUT_BRIDGE_SIZE);
     if(size < SIZE.MIN_NUMBER || size > SIZE.MAX_NUMBER) throw new Error(ERROR.INPUT_BRIDGE_SIZE);
+  }
+
+  validateMove(moving) {
+    if(moving != MOVING.UPSIDE_STRING && moving != MOVING.DOWNSIDE_STRING) throw new Error(ERROR.INPUT_MOVING);
   }
 }
 module.exports = Validate;
