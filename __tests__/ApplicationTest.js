4747bf9699d1c217b4cfc761a88cfa3498b78821
haesa <xdhaesa@gmail.com>
2022-11-19 16:30:48 +0900

    test(Application.js): randomNumbers 숫자로 변경
    BridgeRandomNumberGenerator에서 랜덤 숫자는 String이 아닌 Number로 반환되기 때문

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -53,7 +53,7 @@ const expectBridgeOrder = (received, upside, downside) => {
 
 describe('다리 건너기 테스트', () => {
   test('다리 생성 테스트', () => {
-    const randomNumbers = ['1', '0', '0'];
+    const randomNumbers = [1, 0, 0];
     const mockGenerator = randomNumbers.reduce((acc, number) => {
       return acc.mockReturnValueOnce(number);
     }, jest.fn());
