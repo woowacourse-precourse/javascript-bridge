80c7aa080cd71bf38c76d939b6103e6d486b2ca0
GC-Park <kuyt3122@naver.com>
2022-11-19 16:22:26 +0900

    feat:utils폴더 생성 후 필요한 상수 목록 추가

---
+++ src/utils/constant.js
@@ -0,0 +1,22 @@
+const GAME_START_STRING ="다리 건너기 게임을 시작합니다."
+const BRIDGE_NUMBER_INPUT ="다리의 길이를 입력해주세요."
+const PLAYER_MOVE_INPUT ="이동할 칸을 선택해주세요. (위: U, 아래: D)"
+const RESTART_OR_QUIT_INPUT ="게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"
+const GAME_RESULT_STRING ="최종 게임 결과"
+const GAME_SUCCESS_OR_NOT ="게임 성공 여부: "
+const TOTAL_NUMBER_OF_ATTEMPTS ="총 시도한 횟수: "
+
+const RANGE_BRIDGE_NUMBER_ERROR ="[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다."
+const PLAYER_INPUT_ERROR ="[ERROR] 플레이어 입력 값이 U나 D가 아닙니다."
+
+module.exports = {
+  GAME_START_STRING,
+  BRIDGE_NUMBER_INPUT,
+  PLAYER_MOVE_INPUT,
+  RESTART_OR_QUIT_INPUT,
+  GAME_RESULT_STRING,
+  GAME_SUCCESS_OR_NOT,
+  TOTAL_NUMBER_OF_ATTEMPTS,
+  RANGE_BRIDGE_NUMBER_ERROR,
+  PLAYER_INPUT_ERROR
+}
