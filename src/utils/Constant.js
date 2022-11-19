c92a870c304234897a5d1d50a4ba2d45bd6fcb94
Buyoung Chung <sunarol@naver.com>
2022-11-19 22:53:50 +0900

    feat: Add WRONG,CORRECT,NOT_CHOSEN,LEFT_BRACKET,BAR

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -5,6 +5,11 @@ const Constant = {
   INCORECT: " X ",
   MIN_SIZE: 3,
   MAX_SIZE: 20,
+  WRONG: " X ]",
+  CORRECT: " O ]",
+  NOT_CHOSEN: "   ]",
+  LEFT_BRACKET: "[",
+  BAR: "|",
 };
 
 module.exports = Constant;
