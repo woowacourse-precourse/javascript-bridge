f53b23b065608a53fe195c17cb4bb0737e349908
bassyu <yrkdgus@gmail.com>
2022-11-19 21:43:17 +0900

    refactor(App): 메서드 캡슐화

--- src/App.js
+++ src/App.js
@@ -10,40 +10,40 @@ class App {
 
   play() {
     OutputView.printStart();
-    InputView.readBridgeSize(this.handleBridgeSize.bind(this));
+    InputView.readBridgeSize(this.#handleSize.bind(this));
   }
 
-  handleBridgeSize(size) {
+  #handleSize(size) {
     const bridge = BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate);
     this.#model = new BridgeGame(bridge);
-    InputView.readMoving(this.handleMoving.bind(this));
+    InputView.readMoving(this.#handleMoving.bind(this));
   }
 
-  handleMoving(moving) {
+  #handleMoving(moving) {
     this.#model.move(moving);
     OutputView.printMap(this.#model.getMoveList());
-    this.handleResult();
+    this.#decideNext();
   }
 
-  handleResult() {
+  #decideNext() {
     const moveList = this.#model.getMoveList();
-    const lastMoveResult = moveList[moveList.length - 1].result;
+    const result = moveList[moveList.length - 1].result;
 
-    if (lastMoveResult === true && moveList.length === this.#model.getBridge().length) {
+    if (result === false) {
+      InputView.readGameCommand(this.#handleGameCommand.bind(this));
+    } else if (moveList.length === this.#model.getBridge().length) {
       OutputView.printResult(moveList, this.#model.getTryCount());
-    } else if (lastMoveResult === false) {
-      InputView.readGameCommand(this.handleGameCommand.bind(this));
     } else {
-      InputView.readMoving(this.handleMoving.bind(this));
+      InputView.readMoving(this.#handleMoving.bind(this));
     }
   }
 
-  handleGameCommand(gameCommand) {
-    if (gameCommand === GAME_COMMAND_STRING.quit) {
+  #handleGameCommand(gameCommand) {
+    if (gameCommand === GAME_COMMAND_STRING.retry) {
       OutputView.printResult(this.#model.getMoveList(), this.#model.getTryCount());
     } else {
       this.#model.retry();
-      InputView.readMoving(this.handleMoving.bind(this));
+      InputView.readMoving(this.#handleMoving.bind(this));
     }
   }
 }
