d165586076fba19f29cf755b645912cb10b3470b
한수지 <fatholic5@gmail.com>
2022-11-19 21:27:12 +0900

    :sparkles: feat: (App) 다리길이 입력,판별 요청 과 입력재실행 요청 기능 구현 입력 요청을 할때 인자로 오류발생시 입력을 재실행 할수 있는 콜백함수를 보냄 BridgeGame에 입력받는 다리 사이즈값을 연결 다리 길이 입력값이 오류일 경우 OutputView에 메세지를 요청하고 다리길이를 재입력 받을수 있도록 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 const OutputView = require("./OutputView");
+const InputView = require("./InputView");
+const BridgeGame = require("./BridgeGame");
 
 class App {
+
+  constructor() {
+    this.brideGame = new BridgeGame();
+  }
+
   play() {
     OutputView.printStart();
+    this.requestBridgeSize();
   }
+
+  requestBridgeSize() {
+    InputView.readBridgeSize(this.tryBuildBridge.bind(this));
+  }
+
+  tryBuildBridge(size) {
+    try {
+      this.brideGame.ready(size);
+    } catch(error) {
+      this.retryRequestBridgeSize();
+    }
+  }
+
+  retryRequestBridgeSize() {
+    OutputView.printValidateSizeError();
+    this.requestBridgeSize();
+  }
+
 }
 
 const app = new App();
