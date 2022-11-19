c64e7b4ead94c6e39198646f170afa7bc901f132
wontae chang <cwt629@naver.com>
2022-11-19 23:18:46 +0900

    feat: proceed after getting move input

--- src/App.js
+++ src/App.js
@@ -16,18 +16,20 @@ class App {
     const PATH = BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate);
     this.game = new BridgeGame(PATH);
 
-    // TODO: 이후 과정을 파라메터로 넘겨주기
-    InputView.readMoving();
+    InputView.readMoving(this.proceedAfterMove);
   }
 
   proceedAfterMove(direction) {
     this.game.move(direction);
     OutputView.printMap(this.game);
 
-
+    // TODO: 게임 재시작 및 종료 이후 과정을 파라미터에 넘겨주자.
+    (this.game.isGoingCorrect()) ? this.handleCorrectCase() : InputView.readGameCommand();
   }
 
-
+  handleCorrectCase() {
+    (this.game.crossedAll()) ? this.finish() : InputView.readMoving(this.proceedAfterMove);
+  }
 
   finish() {
     OutputView.printResult(this.game);
