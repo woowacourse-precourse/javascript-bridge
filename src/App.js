1adcb8ca73e2476d522d5a705b14f37f26c418bf
장경은 <jang_kyung_eun@gyeong-eun-ui-MacBookAir.local>
2022-11-20 01:07:28 +0900

    feat: 게임 시작 문구 출력하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Console = MissionUtils.Console;
+const Random = MissionUtils.Random;
+
 class App {
-  play() {}
+  GameStart() {
+    Console.print("다리 건너기 게임을 시작합니다.");
+  }
+  play() {
+    this.GameStart();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
