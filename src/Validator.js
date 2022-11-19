a11e7c43c496b6fe54e18de84296f2e960eaff1d
jisu-kim <kss2460@naver.com>
2022-11-19 22:38:13 +0900

    feat(validateRetryInput): 재시도 예외처리

--- src/Validator.js
+++ src/Validator.js
@@ -1,13 +1,21 @@
 const Validator = {
   validateInput(number) {
-    if (3 > number || number > 20) {
+    const check = /^[0-9]+$/;
+    console.log(number);
+    if (3 > number || number > 20 || !check.test(number)) {
       throw "[ERROR] 3 이상 20 이하의 숫자만 입력 가능합니다.";
     }
   },
 
   validateMoveInput(move) {
-    if (move !== "U" || move === "D") {
+    if (move !== "U" && move !== "D") {
       throw "[ERROR] 입력값은 U 또는 D 만 가능합니다.";
+    }
+  },
+
+  validateRetryInput(retryInput) {
+    if (move !== "R" && move !== "Q") {
+      throw "[ERROR] 입력값은 R 또는 Q 만 가능합니다.";
     }
   },
 };
