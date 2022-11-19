0379f2b94d29cfcc635f0c09bcedbde09ab5a512
doheeMac <thsehgml37@korea.ac.kr>
2022-11-19 15:19:28 +0900

    refractor: 전체 구조 재편

--- src/App.js
+++ src/App.js
@@ -6,10 +6,27 @@ class App {
   }
   play() {
     this.createGame();
+    this.processingGame();
   }
 
   createGame() {
     this.game.gameStartPoint();
+  }
+
+  processingGame() {
+    const recentPlayCount = this.game.getPlayCount();
+
+    for (
+      let playCounter = 0;
+      playCounter < recentPlayCount + 1;
+      playCounter++
+    ) {
+      this.game.increasePlayCount();
+      if(게임실행 및 결과 알려주기){ 
+        // 게임이 완전히 종료됩니다 --> 게임 종료 여부 안 물어봄
+        break;
+      }
+    }
   }
 }
 
