49cf2c13d382c631749360a84e2d2de5d6fc886e
thewronghand <penfreak77@gmail.com>
2022-11-19 22:51:52 +0900

    feat: printResult 기능 및 하위 기능 구현
    게임 성공 여부, 최종 이동 결과, 시도 횟수를 출력하고 게임을 종료하는 printResult 기능을 구현한다.

--- src/OutputView.js
+++ src/OutputView.js
@@ -26,7 +26,39 @@ const { VALUE } = require('./constants/values');
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() {},
+   printResult(game) {
+    let getWin = game.getGameWin();
+    let getTrialCount = game.getTrialCount();
+    let getUpper = game.getBridgeUpper();
+    let getLower = game.getBridgeLower();
+    MissionUtils.Console.print(MESSAGE.GAME_RESULT_PRINT);
+    this.printMap(getUpper, getLower);
+    this.printGameSuccessFail(getWin, getTrialCount);
+  },
+
+  printGameSuccessFail(getWin, getTrialCount) {
+    if (getWin === true) {
+      this.printGameSuccess(getTrialCount);
+      return;
+    }
+    this.printGameFail(getTrialCount);
+  },
+
+  printGameSuccess(getTrialCount) {
+    MissionUtils.Console.print(MESSAGE.GAME_SUCCESS);
+    MissionUtils.Console.print(
+      `${MESSAGE.TOTAL_TRIAL_NUMBERS}` + `${getTrialCount}`
+    );
+    MissionUtils.Console.close();
+  },
+
+  printGameFail(getTrialCount) {
+    MissionUtils.Console.print(MESSAGE.GAME_FAILURE);
+    MissionUtils.Console.print(
+      `${MESSAGE.TOTAL_TRIAL_NUMBERS}` + `${getTrialCount}`
+    );
+    MissionUtils.Console.close();
+  }
 };
 
 module.exports = OutputView;
