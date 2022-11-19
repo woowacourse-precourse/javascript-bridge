9828e95ffcde18caa0129fd70fd21bb6137e408d
2je0 <bulljy@naver.com>
2022-11-19 22:48:55 +0900

    ✨ feat(bridgegame): 사용자 다음 다리로 이동하는 기능 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -28,10 +28,12 @@ class BridgeGame {
 
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+   * @param {string} movement 이동 방향
    */
-  move() {}
+  move(movement) {
+    Validator.movingValidate(movement);
+    this.#userPath.push(movement);
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
