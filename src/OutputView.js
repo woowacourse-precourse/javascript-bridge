276866cc2fc98c0fbd2092b0903e27da7a1392b9
MAGHC <windowsapple95@gmail.com>
2022-11-19 18:06:02 +0900

    feat: OutputView.printResult 기능 구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,6 +1,6 @@
 const Utils = require('./Utils');
 
-const mockMap = [[' O '], [' X ']];
+const mockMap = [['[ O |   | O ]'], ['[   | O |   ]']];
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -26,9 +26,16 @@ const OutputView = Object.freeze({
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() {},
+  printResult(map, tryOfNUmber, success) {
+    const isSuccess = success ? '성공' : '실패';
+    Utils.print('최종 게임 결과');
+    OutputView.printMap(map);
+    Utils.print(`게임 성공 여부: ${isSuccess}\n총 시도한 횟수: ${tryOfNUmber}`);
+  },
 });
 
 module.exports = OutputView;
 
 console.log(OutputView.printMap(mockMap));
+
+console.log(OutputView.printResult(mockMap, 5, true));
