2bc0fad002df5c93750b550491740c48e41c90ac
Chanwoong1 <jcw7468@gmail.com>
2022-11-19 20:59:33 +0900

    chore: 숫자 비교 대신 문자 비교 사용

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -2,10 +2,10 @@
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
 const BridgeMaker = {
-  RANDOM_LOWER_BLOCK: 0,
-  RANDOM_UPPER_BLOCK: 1,
-  LOWER_BLOCK: "D",
-  UPPER_BLOCK: "U",
+  RANDOM_LOWER_BLOCK: '0',
+  RANDOM_UPPER_BLOCK: '1',
+  LOWER_BLOCK: 'D',
+  UPPER_BLOCK: 'U',
   /**
    * @param {number} size 다리의 길이
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
@@ -15,7 +15,7 @@ const BridgeMaker = {
     return new Array(size)
       .fill(0)
       .map(() => {
-        return (generateRandomNumber.generate() === this.RANDOM_UPPER_BLOCK) ?
+        return (String(generateRandomNumber()) === this.RANDOM_UPPER_BLOCK) ?
           this.UPPER_BLOCK : this.LOWER_BLOCK;
       });
   },
