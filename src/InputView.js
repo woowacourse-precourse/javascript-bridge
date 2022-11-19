82505b0159769a62b7302d4d6d82db134c547a9a
jiyujin <yujin1679@naver.com>
2022-11-20 08:18:24 +0900

    feat: 사용자가 잘못된 값을 입력했을 경우 에러를 띄우고 다시 입력받도록 하는 기능 추가

--- src/InputView.js
+++ src/InputView.js
@@ -3,36 +3,45 @@
  */
 const { Console } = require("@woowacourse/mission-utils");
 const { validateBridgeSize, validateUserMove, validateUserEnd } = require("./InputModel");
+const OutputView = require("./OutputView");
 
 const InputView = {
+  getUserInputRecursive(prompt, callback, validator) {
+    Console.readLine(prompt, input => {
+      try {
+        validator(input);
+        callback(input);
+      } catch (error) {
+        OutputView.printError(error);
+        this.getUserInputRecursive(prompt, callback, validator);
+      }
+    });
+  },
   /**
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize(callback) {
-    Console.readLine('다리의 길이를 입력해주세요.\n', bridgeSize => {
-      validateBridgeSize(bridgeSize);
-      callback(Number(bridgeSize));
-    });
+    this.getUserInputRecursive('다리의 길이를 입력해주세요.\n', input => {
+      callback(Number(input));
+    }, validateBridgeSize);
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving(callback) {
-    Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)\n', userMove => {
-      validateUserMove(userMove);
-      callback(userMove);
-    });
+    this.getUserInputRecursive('이동할 칸을 선택해주세요. (위: U, 아래: D)\n', input => {
+      callback(input);
+    }, validateUserMove);
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand(callback) {
-    Console.readLine('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n', userEnd => {
-      validateUserEnd(userEnd);
-      callback(userEnd);
-    });
+    this.getUserInputRecursive('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n', input => {
+      callback(input);
+    }, validateUserEnd);
   },
 };
 
