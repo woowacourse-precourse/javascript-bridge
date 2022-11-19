f77b37c6d121be2ebe1c971594982b6fdb53fa34
sujungYu <su_moment@naver.com>
2022-11-19 18:41:47 +0900

    feat(App) : 다리 건너기 게임 실행

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+const BridgeGame = require('./BridgeGame');
 class App {
-  play() {}
+  play() {
+    const bridgeGame = new BridgeGame();
+    bridgeGame.play();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
