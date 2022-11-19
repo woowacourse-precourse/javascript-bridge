ea0acebb0576eae5424d019623a78026b1f4f74f
GC-Park <kuyt3122@naver.com>
2022-11-19 17:38:31 +0900

    feat:라운드마다 플레이어가 이동할 칸을 입력하는 기능 추가

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,5 +1,5 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-const { BRIDGE_NUMBER_INPUT } = require("../utils/constant")
+const { BRIDGE_NUMBER_INPUT, PLAYER_MOVE_INPUT } = require("../utils/constant")
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,13 +9,17 @@ const InputView = {
    */
   readBridgeSize() {
     MissionUtils.Console.readLine(BRIDGE_NUMBER_INPUT, (answer) => {
+      this.readMoving();
     })
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    MissionUtils.Console.readLine(PLAYER_MOVE_INPUT, (answer) => {
+    })
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
