b8a39d0285e125d563593a3d3440d5c29847e218
wonkyum-kim <dodo12120@naver.com>
2022-11-19 15:25:55 +0900

    test(__tests__): add BridgeGame tests
    - Test if an input is not 'U' or 'D'.

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -1,11 +1,21 @@
 const BridgeGame = require('../src/BridgeGame');
 
-describe('BridgeGameTest', () => {
+describe('BridgeGameTest1', () => {
   test('다리 길이 입력 예외 테스트', () => {
     const BRIDGE_SIZE_INPUT = ['1', '21', 'a'];
+    const BRIDGE_GAME = new BridgeGame();
     expect(() => {
-      const BRIDGE_GAME = new BridgeGame();
       BRIDGE_SIZE_INPUT.forEach((input) => { BRIDGE_GAME.validateBridgeSize(input); });
     }).toThrow('[ERROR] 유효하지 않은 다리 길이입니다.');
+  });
+});
+
+describe('BridgeGameTest2', () => {
+  test('다리 길이 입력 예외 테스트', () => {
+    const BRIDGE_MOVE_INPUT = ['3', 'u', 'UD'];
+    const BRIDGE_GAME = new BridgeGame();
+    expect(() => {
+      BRIDGE_MOVE_INPUT.forEach((input) => { BRIDGE_GAME.validateBridgeMove(input); });
+    }).toThrow('[ERROR] U 또는 D를 입력하세요.');
   });
 });
