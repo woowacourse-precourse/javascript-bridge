3e37a8778d0b81da72a2a846140ee803355599bc
inthhh <glass005533@gmail.com>
2022-11-19 18:18:06 +0900

    refactor: BridgeGame에서 입출력 메소드 사용 제거

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,28 +1,19 @@
-const InputView = require("./InputView");
-const OutputView = require("./OutputView");
-
 class BridgeGame {
-
   move(turnNumber, bridge, moveUpDown) {
     if (bridge[turnNumber] == moveUpDown) {
-      OutputView.printMap(turnNumber, bridge, moveUpDown, true);
       if (turnNumber == bridge.length - 1) {
-        OutputView.printResult(turnNumber, bridge, moveUpDown, true);
         return 0;
       }
       return 1;
     }
     if (bridge[turnNumber] != moveUpDown) {
-      OutputView.printMap(turnNumber, bridge, moveUpDown, false);
-      return 0;
+      return -1;
     }
   }
 
-  retry(nowLength, brg, moveUpDown) {
-    let isRetry = InputView.readGameCommand();
-    if (isRetry) InputView.readMoving(brg, 0);
-    if (!isRetry) OutputView.printResult(nowLength, brg, moveUpDown,false);
-    return;
+  retry(isRetry) {
+    if (isRetry == "R") return 1;
+    if (isRetry == "Q") return 0;
   }
 }
 
