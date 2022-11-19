79dbc61f966a17dc43a9c597be23ebead6e9cbe4
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2022-11-19 15:56:39 +0900

    feat: 사용자의 입력값에 따라 다리를 출력 해줄수 있도록 하는 기능 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -4,30 +4,17 @@ const OutputView = require('./OutputView');
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
-  constructor(array) {
-    this.bridgeArray = array;
-  }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   move(userAnswer) {
-    return this.correct(userAnswer) || this.incorrect(userAnswer);
+    OutputView.printMap(userAnswer);
   }
 
-  correct(userAnswer) {
-    if (this.bridgeArray[0] === userAnswer) {
-      this.bridgeArray.shift();
-      OutputView.printMap(userAnswer);
-      return this.bridgeArray;
-    }
-  }
-
-  incorrect(userAnswer) {
-    if (this.bridgeArray[0] !== userAnswer) {
-      OutputView.printIncorrect(userAnswer);
-    }
+  fail(userAnswer) {
+    OutputView.printIncorrect(userAnswer);
   }
 
   /**
