fc3d7f48afb5792bcc1f6fa900a183e047b97a39
unsnruu <unsnruu@gmail.com>
2022-11-20 01:41:51 +0900

    test: 오류 검증을 위한 테스트 로직 수정

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -13,8 +13,11 @@ describe("검증 메서드에 대한 테스트를 구현합니다.", () => {
   ])("범위 값 내에 있을 경우 true를 반환합니다.", (start, end, value) => {
     expect(Validation.isInRange(start, end, value)).toBe(true);
   });
+  //The function that throws an exception needs to be invoked within a wrapping function otherwise the toThrow assertion will fail.
   test("범위 값의 설정이 잘못된 경우 에러를 반환합니다.", () => {
     const [start, end, value] = [10, 1, 5];
-    expect(Validation.isInRange(start, end, value)).toThrow();
+    expect(() => Validation.isInRange(start, end, value)).toThrow(
+      "[ERROR] end는 start보다 항상 크거나 같아야 합니다."
+    );
   });
 });
