d93e2f84b453f6ba73616337a2cafc566eacde53
leeuihyun <meta212121@naver.com>
2022-11-19 17:12:47 +0900

    feat : App에서 입력 시작을 할 수 있게함

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
+const InputView = require('./InputView');
 const OutputView = require('./OutputView');
 
 class App {
   play() {
     OutputView.printStart();
+    InputView.readBridgeSize();
   }
 }
-
-const app = new App();
-app.play();
 
 module.exports = App;
