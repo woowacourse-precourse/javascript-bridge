35ed87fd1bd899a4fdaa9e4b04bf2fa8ab5ac2de
2yunseong <dbsdltjd3701@naver.com>
2022-11-19 21:54:09 +0900

    feat(OutputView): 게임시작 문구를 출력하는 기능

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,3 +1,5 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -7,6 +9,11 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
+  printMessage(message) {
+    MissionUtils.Console.print(message);
+    return this;
+  },
+
   printMap() {},
 
   /**
---
+++ src/util/message.js
@@ -0,0 +1,5 @@
+const message = {
+  GAME_START: '다리 건너기 게임을 시작합니다.',
+};
+
+module.exports = message;
