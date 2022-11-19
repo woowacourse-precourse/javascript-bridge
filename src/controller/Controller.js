cff1626ff276c314911877744727c527c89eb740
violetwhenisawu <anashape710@gmail.com>
2022-11-19 13:29:37 +0900

    feat: 유저가 이동할 칸 입력받기, 입력 값 유효성 검사 기능 추가

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,8 +1,18 @@
 const InputView = require('../view/InputView');
 const OutputView = require('../view/OutputView');
 const validator = require('../utils/validator');
+const BridgeMaker = require('../BridgeMaker');
+const BridgeRandomNumberGenerator = require('../model/BridgeRandomNumberGenerator');
 
 class Controller {
+  #bridgeWay;
+  #userSelect;
+
+  constructor() {
+    this.#bridgeWay;
+    this.#userSelect;
+  }
+
   start() {
     OutputView.printGameStart();
     this.inputBridgeSize();
@@ -18,9 +28,25 @@ class Controller {
     } catch {
       this.inputBridgeSize();
     }
+    this.#bridgeWay = BridgeMaker.makeBridge(
+      size,
+      BridgeRandomNumberGenerator.generate
+    );
+    this.inputBridgeWay();
   }
 
-  getBridgeWay() {}
+  inputBridgeWay() {
+    InputView.readMoving(this.getBridgeWay.bind(this));
+  }
+
+  getUserMoving(select) {
+    try {
+      validator.checkUserMovingInput(select);
+    } catch {
+      this.inputBridgeWay();
+    }
+    this.#userSelect = select;
+  }
 }
 
 module.exports = Controller;
--- src/utils/validator.js
+++ src/utils/validator.js
@@ -5,12 +5,21 @@ const validator = {
     throw Console.print('[ERROR] 입력한 다리 길이가 올바르지 않습니다.');
   },
 
+  checkUserMovingInput(input) {
+    if (this.checkIsUpDown(input)) return;
+    throw Console.print('[ERROR] 입력한 문자가 올바르지 않습니다.');
+  },
+
   checkIsNum(input) {
     return /^[0-9]*$/g.test(input);
   },
 
   checkNumRange(input) {
     return input >= 3 && input <= 20;
+  },
+
+  checkIsUpDown(input) {
+    return input === 'U' || input === 'D';
   },
 };
 
