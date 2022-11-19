c8f4a7a0ac3bc25e4f9f59409835a4853fb5bc07
hyunjin <leehj0110@kakao.com>
2022-11-19 19:09:28 +0900

    refactor: result 객체를 사용한 BridgeGame 리팩터링

--- src/controllers/BridgeGame.js
+++ src/controllers/BridgeGame.js
@@ -9,6 +9,7 @@ class BridgeGame {
   try;
   index;
   answer;
+  result;
 
   constructor(size) {
     this.bridge = new Bridge(size);
@@ -16,7 +17,7 @@ class BridgeGame {
     this.index = 0;
     this.answer = [];
 
-    console.log(this.bridge);
+    console.log(this.bridge); // ! remove
   }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -24,24 +25,13 @@ class BridgeGame {
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   move(input) {
-    // if (this.bridge.canMoveForward(this.index, input)) {
-    //   answer.push(true);
-    // }
-
     const canMoveForward = this.bridge.canMoveForward(this.index, input);
+    
     this.result = new Result(this.bridge, this.index, canMoveForward);
-    // this.result.print();
-    console.log('결과 프린트');
+    this.result.print();
     this.index += 1;
 
     return canMoveForward;
-    // if (this.bridge.canMoveForward(this.index, input)) {
-    //   this.index += 1;
-    //   OutputView.printMap(this.bridge.bridgeArr, this.index, true);
-    //   return true;
-    // }
-    // OutputView.printMap(this.bridge.bridgeArr, this.index, false);
-    // return false;
   }
 
   /**
