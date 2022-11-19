630df10618baf2803e7eeb3fe9d2bce470da0c6a
MyungAe <ladiescheong@naver.com>
2022-11-19 17:40:12 +0900

    test(Validate): 검증 기능 변경으로 인해 테스트 코드 구현 방법 수정

--- __tests__/ValidateTest.js
+++ __tests__/ValidateTest.js
@@ -2,15 +2,15 @@ const Validate = require("../src/Validate");
 
 describe("Validate Object Test", () => {
   test("숫자가 아닌 값을 넣으면 에러를 반환한다.", () => {
-    expect(() => Validate.isNumber("1a")).toThrow("[ERROR]");
+    expect(Validate.isNumber("1a")).toBe(false);
   });
 
   test("입력된 값이 20을 넘는 경우 에러를 반환한다.", () => {
-    expect(() => Validate.isBigger("21")).toThrow("[ERROR]");
+    expect(Validate.isLowerThanMaxLength("21")).toBe(false);
   });
 
   test("입력된 값이 3보다 작은 경우 에러를 반환한다.", () => {
-    expect(() => Validate.isLower("2")).toThrow("[ERROR]");
+    expect(Validate.isBiggerThanMinLength("2")).toBe(false);
   });
 
   test("입력된 값이 U 또는 D가 아닌 경우 에러를 반환한다.", () => {
