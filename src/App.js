90f9d34f81a5667912fd3fbd42bed40d532a2cbf
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-19 11:43:22 +0900

    refactor(App,InputView): InputView에서 담당하던 게임 흐름 제어를 App이 담당하도록 개선
    기존 InputView에서 BridgeGame을 초기화하여 게임 흐름을 제어하고 있었음.
    UI로직과 비즈니스 로직이 섞여있어 구현에 여러움을 겪음.
    App에서 게임 흐름 제어를 담당하게 함으로써 UI/비즈니스 로직이 분리 되었음.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-const { printMsg } = require('./views/OutputView.js');
-const { requestBridgeSize } = require('./views/InputView.js');
 const { GAME_MSG } = require('./constants/message.js');
+const { printMsg } = require('./views/OutputView.js');
+const InputView = require('./views/InputView.js');
+const BridgeGame = require('./BridgeGame.js');
 
 class App {
+  constructor() {
+    this.bridgeGame = new BridgeGame();
+  }
+
   play() {
     printMsg(GAME_MSG.start);
-    requestBridgeSize();
+    this.requestBridgeSize();
+  }
+
+  requestBridgeSize() {
+    InputView.readBridgeSize(this.tryBuildBridge.bind(this));
+  }
+
+  tryBuildBridge(size) {
+    try {
+      this.bridgeGame.build(size);
+
+      this.requestMovingDirection();
+    } catch ({ message }) {
+      printMsg(message);
+      this.requestBridgeSize();
+    }
+  }
+
+  requestMovingDirection() {
+    InputView.readMoving(this.tryMovePlayer.bind(this));
+  }
+
+  tryMovePlayer(movingDirection) {
+    try {
+      this.bridgeGame.move(movingDirection);
+    } catch ({ message }) {
+      printMsg(message);
+      this.requestMovingDirection();
+    }
   }
 }
 
--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,37 +1,13 @@
 const { Console } = require('@woowacourse/mission-utils');
 const { REQUEST_MSG } = require('../constants/message.js');
-const { printMsg } = require('./OutputView.js');
-const BridgeGame = require('../BridgeGame.js');
-
-const bridgeGame = new BridgeGame();
 
 const InputView = {
-  requestBridgeSize() {
-    Console.readLine(REQUEST_MSG.bridgeSize, InputView.readBridgeSize);
+  readBridgeSize(callback) {
+    Console.readLine(REQUEST_MSG.bridgeSize, callback);
   },
 
-  readBridgeSize(size) {
-    try {
-      bridgeGame.build(size);
-
-      InputView.requestMoving();
-    } catch ({ message }) {
-      printMsg(message);
-      InputView.requestBridgeSize();
-    }
-  },
-
-  requestMoving() {
-    Console.readLine(REQUEST_MSG.movingDirection, InputView.readMoving);
-  },
-
-  readMoving(movingDirection) {
-    try {
-      bridgeGame.move(movingDirection);
-    } catch ({ message }) {
-      printMsg(message);
-      InputView.requestMoving();
-    }
+  readMoving(callback) {
+    Console.readLine(REQUEST_MSG.movingDirection, callback);
   },
 
   readGameCommand() {},
