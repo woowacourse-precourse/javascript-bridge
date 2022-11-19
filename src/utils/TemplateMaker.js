24bea0e574ea06f7c04abbaa4e87e667b4728215
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-19 20:58:22 +0900

    feat(TemplateMaker): 비교 결과로 템플릿 생성하는 기능 구현

--- src/utils/TemplateMaker.js
+++ src/utils/TemplateMaker.js
@@ -8,3 +8,29 @@ const SYMBOLS = {
   blank: ' ',
   separator: ' | ',
 };
+
+class TemplateMaker {
+  static getLogTemplates(logs) {
+    const upsideTemplate = TemplateMaker.#makeLogTemplate(logs, UPSIDE_SYMBOL);
+    const downsideTemplate = TemplateMaker.#makeLogTemplate(logs, DOWNSIDE_SYMBOL);
+
+    return [upsideTemplate, downsideTemplate];
+  }
+
+  static #makeLogTemplate(logs, directionSymbol) {
+    const template = logs
+      .map(({ isCrossable, movingDirection }) => {
+        const isSymbolSide = movingDirection === directionSymbol;
+
+        if (isCrossable && isSymbolSide) return SYMBOLS.success;
+        if (!isCrossable && isSymbolSide) return SYMBOLS.fail;
+
+        return SYMBOLS.blank;
+      })
+      .join(SYMBOLS.separator);
+
+    return `${SYMBOLS.start} ${template} ${SYMBOLS.end}`;
+  }
+}
+
+module.exports = TemplateMaker;
