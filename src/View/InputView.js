38096032bb6f5de56f22ffcc58ba23d82605edd3
2je0 <bulljy@naver.com>
2022-11-19 21:17:14 +0900

    ✨ feat(inputview): 재시도 여부를 입력받는 기능 구현

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -27,7 +27,9 @@ const InputView = {
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand(callback) {
+    Console.readLine(MESSAGE.ASK_GAME_COMMAND, (command) => callback(command));
+  },
 };
 
 module.exports = InputView;
