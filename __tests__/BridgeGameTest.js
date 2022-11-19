a9f71747e6635970ccc8c7b1a89279bcb7c58747
daal2quf <daal2quf@gmail.com>
2022-11-19 19:38:27 +0900

    test(bridgeGameTest): stackOfUserMovingInput() 기능 추가 및 기존테스트 삭제
    delete:
    기존 test1: 정답의 0번째 인덱스를 userinput에 넣어줄 필요가없어 메서드삭제로 인한 테스트 삭제
    add:
    test1: 객체 멤버변수에 입력받은 user moving input이 잘 들어가는지 확인

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -5,12 +5,12 @@ afterEach(() => {
   Console.close();
 });
 
-describe('BridgeGame 클래스의 setBeginningMapToUserInput() 메서드 테스트', () => {
-  test('생성된 password의 첫번째 다리정답이 userinput 배열에 들어가는지 확인', () => {
-    const bridge = new BridgeGame(3);
-    bridge.setBeginningMapToUserInput();
-    const result = ['U', 'D'];
-    const beginningUserInput = bridge.userInput;
-    expect(result).toContain(...beginningUserInput);
+describe('BridgeGame 클래스의 stackOfUserMovingInput() 메서드 테스트', () => {
+  test('this.userInput 멤버변수에 쌓아줄 입력값이 잘 들어가는지 확인', () => {
+    const bridge = new BridgeGame();
+    const userInput = 'D';
+    bridge.stackOfUserMovingInput(userInput);
+    const result = 'D';
+    expect(...bridge.userInput).toContain(result);
   });
 });
