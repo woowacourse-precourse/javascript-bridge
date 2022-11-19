b0de6037d5fdf899a9cbda5f13cec545cd586bd6
SWARVY <swarvy0826@naver.com>
2022-11-19 12:39:21 +0900

    refactor : returnProcessedInput 상수분리, else 로직 제거

--- src/Utils/returnProcessedInput.js
+++ src/Utils/returnProcessedInput.js
@@ -1,21 +1,25 @@
+const { INPUT_CHAR } = require('../Constants/InputValues');
+const { STATUS } = require('../Constants/BridgeStatus');
+
 const returnProcessedInput = {
   getProcessedInput(input, isPassed) {
     const upAndDownArray = this.upAndDown(input, isPassed);
     const splitUpAndDownArray = this.splitUpAndDown(upAndDownArray);
     const result = [
-      [splitUpAndDownArray[0].join().replace(/,/gi, ' | ')],
-      [splitUpAndDownArray[1].join().replace(/,/gi, ' | ')],
+      [splitUpAndDownArray[0].join().replace(/,/gi, STATUS.seperator)],
+      [splitUpAndDownArray[1].join().replace(/,/gi, STATUS.seperator)],
     ];
     return result;
   },
 
-  upAndDown(input, isPassed) {
+  upAndDown(input, pass) {
     const upAndDownArray = [];
+    const current = input.length - 1;
     input.forEach((value, index) => {
-      if (value === 'U' && index === input.length - 1 && !isPassed) upAndDownArray.push(['X', ' ']);
-      else if (value === 'U') upAndDownArray.push(['O', ' ']);
-      if (value === 'D' && index === input.length - 1 && !isPassed) upAndDownArray.push([' ', 'X']);
-      else if (value === 'D') upAndDownArray.push([' ', 'O']);
+      if (value === INPUT_CHAR.up && index === current && !pass) upAndDownArray.push([STATUS.fail, STATUS.pass]);
+      if (value === INPUT_CHAR.up && pass) upAndDownArray.push([STATUS.correct, STATUS.pass]);
+      if (value === INPUT_CHAR.down && index === current && !pass) upAndDownArray.push([STATUS.pass, STATUS.fail]);
+      if (value === INPUT_CHAR.down && pass) upAndDownArray.push([STATUS.pass, STATUS.correct]);
     });
     return upAndDownArray;
   },
