bae3b197ea718563a72494957746ec66e0083b1c
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-11-19 15:38:27 +0900

    feat(App): 다리 생성 메서드

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+const BridgeGame = require('./BridgeGame');
+const BridgeMaker = require('./BridgeMaker');
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
+
 class App {
-  play() {}
+  #bridgeGame;
+
+  constructor() {
+    this.#bridgeGame = new BridgeGame();
+  }
+
+  generateBridge(size) {
+    const newBridge = BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate);
+
+    this.#bridgeGame.setBridge(newBridge);
+
+    return newBridge;
+  }
 }
 
 module.exports = App;
