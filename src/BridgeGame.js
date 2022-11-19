a3957e15a298d59f012c625cfc62f39c971623db
chocoro99 <76997927+chocoro99@users.noreply.github.com>
2022-11-19 21:21:34 +0900

    feat : "이동할 칸을 선택해주세요. (위: U, 아래: D)" 출력하기

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -16,6 +16,7 @@ class BridgeGame {
     OutputView.startSentence();
     OutputView.lengthBridgeSentence();
     this.bridgeSize();
+    this.readMove();
   }
 
   bridgeSize() {
@@ -34,6 +35,10 @@ class BridgeGame {
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   move() {}
+
+  readMove() {
+    OutputView.pickMoveSentence();
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
