f6f3d239533d9232fbe45e74a5a012036b802752
sh981013s <sh981013s@gmail.com>
2022-11-19 16:08:14 +0900

    feat(UserModel): 이동경로 추가 및 초기화 메서드 구현

--- src/model/UserModel.js
+++ src/model/UserModel.js
@@ -27,6 +27,19 @@ class UserModel {
   getUserMove() {
     return this.#userMove;
   }
+
+  /**
+   * 유저가 이동한 칸을 userMove 배열에 추가한다.
+   * @param userMove  {string} [유저가 이동한 칸]
+   */
+  appendUserMove(userMove) {
+    this.#userMove.push(userMove);
+  }
+
+  // 유저 이동경로를 초기화한다.
+  resetUserMove() {
+    this.#userMove = [];
+  }
 }
 
 module.exports = UserModel;
