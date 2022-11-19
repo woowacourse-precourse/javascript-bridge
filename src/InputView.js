afd1e0303cb729d2464c83f8fa1956b3a9a44b1d
sunub <bsc5672@gmail.com>
2022-11-20 02:17:09 +0900

    feat : 사용자의 값을 입력받는 3가지 도메인 로직 기능 추가
    사용자와 상호작용이 필요한 로직에 대한 기능들을 추가하였다.
    1. 사용자에게 다리의 크기를 입력 받는다.
     - 입력 받은 데이터로 다리를 생성 후 전달
    2. 사용자에게 어디로 이동할지에 대한 값을 입력 받는다.
     - 1에서 전달 받은 다리 값을 기반으로 결과에 대한 분석 및 전달
    3. 사용자에게 재시작이나 종료할지 여부에 대한값을 입력 받는다.
     - 2에서 전달 받은 값과 3에서 입력 받은 값을 비교 하여 결과를 산출
     위와 같은 기능들을 수행하기 위한 기능들을 추가 하였다.

--- src/InputView.js
+++ src/InputView.js
@@ -1,21 +1,100 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const BridgeMaker = require("./BridgeMaker.js");
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+const BridgeGame = require("./BridgeGame");
+const OutputView = require("./OutputView");
+const Error = require("./Error");
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
+const bridgeGame = new BridgeGame();
+const error = new Error();
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize(data) {
+    MissionUtils.Console.readLine("다리의 길이를 입력해주세요.\n", (answer) => {
+      try {
+        error.bridgeSizeError(answer);
+
+        data["bridge"] = BridgeMaker.makeBridge(
+          Number(answer),
+          BridgeRandomNumberGenerator.generate
+        );
+        this.readMoving(data);
+      } catch (error) {
+        MissionUtils.Console.print(error);
+        this.readBridgeSize();
+      }
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving(data) {
+    MissionUtils.Console.readLine(
+      "이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
+      (answer) => {
+        try {
+          error.movingError(answer);
+
+          data["currentAnswer"] = answer;
+          data = bridgeGame.move(data);
+          OutputView.printMap(data);
+          if (data["status"] === "continue") {
+            if (data["callCount"] === data["bridge"].length) {
+              data["status"] = "성공";
+              return OutputView.printResult(data);
+            }
+            this.readMoving(data);
+          }
+          if (data["status"] === "fail") {
+            this.readGameCommand(data);
+          }
+        } catch (error) {
+          MissionUtils.Console.print(error);
+          data["currentAnswer"] = "";
+          this.readMoving(data);
+        }
+      }
+    );
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand(data) {
+    MissionUtils.Console.readLine(
+      "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n",
+      (answer) => {
+        try {
+          error.gameCommandError(answer);
+
+          data["currentAnswer"] = answer;
+          bridgeGame.retry(data);
+
+          this.checkStatus(data);
+        } catch (error) {
+          MissionUtils.Console.print(error);
+          data["currentAnswer"] = "";
+          this.readGameCommand(data);
+        }
+      }
+    );
+  },
+
+  checkStatus(data) {
+    if (data["status"] === "retry") {
+      data["try"] += 1;
+      return this.readMoving(data);
+    }
+    if (data["status"] === "quit") {
+      data["status"] = "실패";
+      return OutputView.printResult(data);
+    }
+  },
 };
 
 module.exports = InputView;
