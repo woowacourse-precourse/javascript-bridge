e3bf88594d901dbe60cc15679735f89380167aec
AnJaeSeong <dktmzh6@gmail.com>
2022-11-19 19:37:55 +0900

    fix(InputView): delete validation logic
    InputValidate 모듈 생성후 InpuView의 필요없어진 validate 코드 삭제

--- src/InputView.js
+++ src/InputView.js
@@ -1,6 +1,4 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-const BridgeGameController = require("./BridgeGameController");
-const OutputView = require("./OutputView");
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -32,12 +30,6 @@ const InputView = {
       roundRetryBranch(input)
     });
   },
-
-  validateBridgeLength(input) {
-    if(!input.match(/^[0-9]+$/g)) throw new Error("[ERROR] 다리 길이는 숫자(정수)를 입력해야 합니다.");
-    else if(parseInt(input)>20 || parseInt(input)<3) throw new Error("[ERROR] 다리 길이는 3이상 20이하의 수를 입력해야 합니다.");
-    return input;
-  }
 };
 
 module.exports = InputView;
