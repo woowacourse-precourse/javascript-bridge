ba043bb24bdc3de3cfdb5470a2cb1ed62ac4cd53
Buyoung Chung <sunarol@naver.com>
2022-11-19 13:31:59 +0900

    feat: output greeting and input readBridgeSize
    start of the game

---
+++ src/View/InputView.js
@@ -0,0 +1,26 @@
+const { Console } = require("@woowacourse/mission-utils");
+const Message = require("../utils/Message");
+
+/**
+ * 사용자로부터 입력을 받는 역할을 한다.
+ */
+const InputView = {
+  /**
+   * 다리의 길이를 입력받는다.
+   */
+  readBridgeSize(callback) {
+    Console.readLine(Message.ASK_BRIDGE_SIZE, callback);
+  },
+
+  /**
+   * 사용자가 이동할 칸을 입력받는다.
+   */
+  readMoving() {},
+
+  /**
+   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+   */
+  readGameCommand() {},
+};
+
+module.exports = InputView;
