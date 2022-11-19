a2820326e2039bbe8a8f5ed34fe6cb3f1eea573f
dev-redo <mis05041@naver.com>
2022-11-19 19:26:24 +0900

    feat(ConsoleInputView, ConsoleOutputView): Console input, output 메서드 구현

--- src/view/console/ConsoleInputView.js
+++ src/view/console/ConsoleInputView.js
@@ -1,5 +1,10 @@
+const { Console } = require('@woowacourse/mission-utils');
 const InputView = require('../InputView');
 
-const ConsoleInputView = class extends InputView {};
+const ConsoleInputView = class extends InputView {
+  input(message, callback) {
+    Console.readLine(message, callback);
+  }
+};
 
 module.exports = ConsoleInputView;
--- src/view/console/ConsoleOutputView.js
+++ src/view/console/ConsoleOutputView.js
@@ -1,5 +1,10 @@
+const { Console } = require('@woowacourse/mission-utils');
 const OutputView = require('../OutputView');
 
-const ConsoleOutputView = class extends OutputView {};
+const ConsoleOutputView = class extends OutputView {
+  output(message) {
+    Console.print(message);
+  }
+};
 
 module.exports = ConsoleOutputView;
