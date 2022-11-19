7f08b36089ecfc6aba717c86a4539d898a99b866
turtle601 <turtle601@naver.com>
2022-11-20 00:07:43 +0900

    refactor(Component) Componennt 클래스 제거

--- src/view/core/Component.js
+++
@@ -1,9 +0,0 @@
-class Component {
-  #OVERIDING_ERROR = '[ERROR] YOU SHOULD DECLARE "render" method';
-
-  render() {
-    throw new Error(this.#OVERIDING_ERROR);
-  }
-}
-
-module.exports = Component;
