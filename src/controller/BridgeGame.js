0445de7f708b93257ec470b84fa7745bf084b79b
pakxe <pigkill40@naver.com>
2022-11-20 05:30:37 +0900

    feat(BridgeGame): 다리 게임 초안 작성

--- src/controller/BridgeGame.js
+++ src/controller/BridgeGame.js
@@ -1,20 +1,85 @@
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
-class BridgeGame {
-  /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  move() {}
+const OutputView = require('../view/OutputView');
+const InputView = require('../view/InputView');
+const Bridge = require('../domain/Bridge');
+const MovingCount = require('../domain/MovingCount');
+const CurrentLocation = require('../domain/CurrentLocation');
+const { GAME } = require('../constants/Constants');
+const { Console } = require('@woowacourse/mission-utils');
 
-  /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  retry() {}
+class BridgeGame {
+  #bridge;
+  #totalMovingCount;
+  #currentLocation;
+  #partialBridgeMap;
+  #result;
+
+  constructor() {
+    this.#bridge;
+    this.#totalMovingCount = new MovingCount();
+    this.#currentLocation = new CurrentLocation();
+    this.#partialBridgeMap;
+    OutputView.printStart();
+  }
+
+  start() {
+    try {
+      InputView.readBridgeLength(this.#initBridge.bind(this));
+    } catch (err) {
+      Console.print(err);
+      this.start();
+    }
+  }
+
+  #initBridge(length) {
+    this.#bridge = new Bridge(length);
+    this.#move();
+  }
+
+  #move() {
+    this.#isEndOfBridge()
+      ? this.#end()
+      : InputView.readMoving(this.#moveResult.bind(this));
+  }
+
+  #moveResult(direction) {
+    this.#currentLocation.moveOneStep();
+    const location = this.#currentLocation.getCurrentLocation();
+
+    this.#result = this.#bridge.isMovable(direction, location);
+    this.#partialBridgeMap = this.#bridge.getPartialBridgeMap(location);
+
+    OutputView.printMap(this.#partialBridgeMap, this.#result);
+
+    this.#result
+      ? this.#move()
+      : InputView.readGameCommand(this.#isRetry.bind(this));
+  }
+
+  #isRetry(gameCommand) {
+    this.#totalMovingCount.plusTotalMovingCount();
+    if (gameCommand === GAME.restart) {
+      this.#currentLocation.resetCurrentLocation();
+      this.#move();
+    }
+    if (gameCommand === GAME.quit) this.#end();
+  }
+
+  #end() {
+    this.#totalMovingCount.plusTotalMovingCount();
+    OutputView.printResult(
+      this.#partialBridgeMap,
+      this.#result,
+      this.#totalMovingCount.getTotalMovingCount()
+    );
+    Console.close();
+  }
+
+  #isEndOfBridge() {
+    return (
+      this.#bridge.getBridgeLength() ===
+      this.#currentLocation.getCurrentLocation() + 1
+    );
+  }
 }
 
 module.exports = BridgeGame;
