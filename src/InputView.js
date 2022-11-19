4121396b297d3b80b0468636cd2db5b27d4a06a7
tkdrb12 <tkdrb15@gmail.com>
2022-11-19 17:41:58 +0900

    feat : 사용자가 이동할 칸을 입력받는 기능 추가

--- src/InputView.js
+++ src/InputView.js
@@ -1,7 +1,5 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const { Console } = MissionUtils;
-const BridgeGame = require("./BridgeGame");
-
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -10,29 +8,23 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize(App) {
-    let size;
-    Console.readLine("다리의 길이를 입력해주세요.", (input) => {
-      size = input;
-    });
-    return App;
+  readBridgeSize(callback) {
+    Console.readLine("다리의 길이를 입력해주세요.", callback);
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {
-    let move;
-    Console.readLine("이동할 칸을 입력해주세요.", (input) => {
-      const move = input;
-    });
-    return move;
+  readMoving(callback) {
+    Console.readLine("이동할 칸을 선택해주세요. (위: U, 아래: D)", callback);
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+    
+  },
 };
 
 module.exports = InputView;
