1be750b1f3cf8417809daab246edf082f06399b9
hae-on <solbi2004@naver.com>
2022-11-19 11:20:44 +0900

    test: 다리 이동 시 리턴 값 테스트

--- __tests__/BridgeTest.js
+++ __tests__/BridgeTest.js
@@ -1,4 +1,5 @@
 const BridgeMaker = require('../src/BridgeMaker');
+const BridgeGame = require('../src/BridgeGame');
 
 describe('다리 테스트', () => {
   test('다리 생성 테스트', () => {
@@ -9,5 +10,19 @@ describe('다리 테스트', () => {
 
     const bridge = BridgeMaker.makeBridge(3, mockGenerator);
     expect(bridge).toEqual(['U', 'D', 'U']);
+  });
+
+  test('다리 이동 일치할 때 리턴값 테스트', () => {
+    const bridgeGame = new BridgeGame(0, [], []);
+    const bridge = ['U', 'D', 'D'];
+    const userMove = 'U';
+    expect(bridgeGame.move(bridge, userMove)).toBeTruthy();
+  });
+
+  test('다리 이동 불일치할 때 리턴값 테스트', () => {
+    const bridgeGame = new BridgeGame(0, [], []);
+    const bridge = ['U', 'D', 'D'];
+    const userMove = 'D';
+    expect(bridgeGame.move(bridge, userMove)).toBeFalsy();
   });
 });
