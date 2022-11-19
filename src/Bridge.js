72934f339106bfa30e622989389d7f99fec62cba
nangkyeonglim <nangkyeonglim@gmail.com>
2022-11-19 14:57:49 +0900

    feat(Bridge): 해당 방향이 현재 방향인지 확인하는 함수 구현

--- src/Bridge.js
+++ src/Bridge.js
@@ -1,9 +1,14 @@
 class Bridge {
   #directions;
+  #currentDirection;
 
   constructor(directions) {
     this.#directions = directions;
   } 
+
+  isCurrentDirection(direction) {
+    return this.#directions === direction;
+  }
 }
 
 module.exports = Bridge;
