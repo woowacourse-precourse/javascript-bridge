5a085a7e082f3c6f8042c7418f5a7d4501e36609
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-11-20 05:44:04 +0900

    ✨ [feat]: 현재까지 이동한 다리를 출력하는 기능

--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -1,26 +1,34 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-const { MESSAGE } = require("../Constants");
+const { MESSAGE, UNIT } = require("../Constants");
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 const OutputView = {
   printStart() {
-    MissionUtils.Console.print(MESSAGE.START);
+    MissionUtils.Console.print(MESSAGE.GAME_START);
   },
   /**
    * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {},
+  printMap(upperBridge, lowerBridge) {
+    MissionUtils.Console.print(
+      UNIT.START + upperBridge.join(UNIT.DIVISION) + UNIT.END
+    );
+    MissionUtils.Console.print(
+      UNIT.START + lowerBridge.join(UNIT.DIVISION) + UNIT.END
+    );
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() {},
+  printResult(gameResult, tryingCount) {
+  },
 };
 
 module.exports = OutputView;
