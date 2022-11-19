d1e2c1d7e91d8f83cc724ce32d26f6bd344923fa
Jin1won <wlsdnjs16521@gmail.com>
2022-11-19 15:51:15 +0900

    feat: 3 이상 20이하의 값이 아닌 경우 예외 처리한다.

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,8 @@
+const BRIDGE_VALUE = Object.freeze({
+  MIN: 3,
+  MAX: 20,
+});
+
 const GAME_MESSAGE = Object.freeze({
   START: "다리 건너기 게임을 시작합니다.",
   INPUT_BRIDGE_LENGTH: "다리의 길이를 입력해주세요.",
@@ -5,6 +10,7 @@ const GAME_MESSAGE = Object.freeze({
 
 const ERROR_MESSAGE = Object.freeze({
   TYPE_ERROR: "[ERROR] 숫자를 입력해주세요.",
+  RANGE_ERROR: "[ERROR] 3~20 범위의 값만 입력해주세요.",
 });
 
-module.exports = { GAME_MESSAGE, ERROR_MESSAGE };
+module.exports = { BRIDGE_VALUE, GAME_MESSAGE, ERROR_MESSAGE };
--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,9 +1,13 @@
-const { ERROR_MESSAGE } = require("../constants");
+const { ERROR_MESSAGE, BRIDGE_VALUE } = require("../constants");
 
 const isBridgeLengthValid = (value) => {
   if (!isNumberType(value)) throw Error(ERROR_MESSAGE.TYPE_ERROR);
+  if (!isRangeValid(value)) throw Error(ERROR_MESSAGE.RANGE_ERROR);
 };
 
 const isNumberType = (value) => !Number.isNaN(Number(value));
+
+const isRangeValid = (value) =>
+  BRIDGE_VALUE.MIN <= value && value <= BRIDGE_VALUE.MAX;
 
 module.exports = { isBridgeLengthValid };
