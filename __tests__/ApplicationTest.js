e0b9cc86d1badbe04a0962a538cf195c174b1e45
jeonjeunghoon <jeonjeunghoon@gmail.com>
2022-11-19 18:54:33 +0900

    test: 기능, 예외 테스트 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -45,6 +45,7 @@ const expectLogContains = (received, logs) => {
 };
 
 const expectBridgeOrder = (received, upside, downside) => {
+  console.log(received);
   const upsideIndex = received.indexOf(upside);
   const downsideIndex = received.indexOf(downside);
 
@@ -63,25 +64,118 @@ describe('다리 건너기 테스트', () => {
   });
 
   test('기능 테스트', () => {
-    const logSpy = getLogSpy();
-    mockRandoms(['1', '0', '1']);
-    mockQuestions(['3', 'U', 'D', 'U']);
+    const randoms = [
+      ['1', '0', '1'],
+      ['1', '1', '1'],
+      ['0', '1', '1'],
+      ['0', '0', '1'],
+      ['0', '0', '0'],
+    ];
+    const inputs = [
+      ['3', 'U', 'D', 'U'],
+      ['3', 'U', 'U', 'U'],
+      ['3', 'D', 'U', 'U'],
+      ['3', 'D', 'D', 'U'],
+      ['3', 'D', 'D', 'D'],
+    ];
+    const results = [
+      [
+        '최종 게임 결과',
+        '[ O |   | O ]',
+        '[   | O |   ]',
+        '게임 성공 여부: 성공',
+        '총 시도한 횟수: 1',
+      ],
+      [
+        '최종 게임 결과',
+        '[ O | O | O ]',
+        '[   |   |   ]',
+        '게임 성공 여부: 성공',
+        '총 시도한 횟수: 1',
+      ],
+      [
+        '최종 게임 결과',
+        '[   | O | O ]',
+        '[ O |   |   ]',
+        '게임 성공 여부: 성공',
+        '총 시도한 횟수: 1',
+      ],
+      [
+        '최종 게임 결과',
+        '[   |   | O ]',
+        '[ O | O |   ]',
+        '게임 성공 여부: 성공',
+        '총 시도한 횟수: 1',
+      ],
+      [
+        '최종 게임 결과',
+        '[   |   |   ]',
+        '[ O | O | O ]',
+        '게임 성공 여부: 성공',
+        '총 시도한 횟수: 1',
+      ],
+    ];
+    const orders = [
+      ['[ O |   | O ]', '[   | O |   ]'],
+      ['[ O | O | O ]', '[   |   |   ]'],
+      ['[   | O | O ]', '[ O |   |   ]'],
+      ['[   |   | O ]', '[ O | O |   ]'],
+      ['[   |   |   ]', '[ O | O | O ]'],
+    ];
 
     const app = new App();
-    app.play();
 
-    const log = getOutput(logSpy);
-    expectLogContains(log, [
-      '최종 게임 결과',
-      '[ O |   | O ]',
-      '[   | O |   ]',
-      '게임 성공 여부: 성공',
-      '총 시도한 횟수: 1',
-    ]);
-    expectBridgeOrder(log, '[ O |   | O ]', '[   | O |   ]');
+    randoms.forEach((number, i) => {
+      const logSpy = getLogSpy();
+      mockRandoms(number);
+      mockQuestions(inputs[i]);
+      app.play();
+      const log = getOutput(logSpy);
+      expectLogContains(log, results[i]);
+      expectBridgeOrder(log, orders[i][0], orders[i][1]);
+    });
   });
 
-  test('예외 테스트', () => {
-    runException(['a']);
+  test('다리 길이 입력 예외 테스트', () => {
+    const inputs = [
+      ['a'],
+      ['1'],
+      ['2'],
+      ['21'],
+      ['22'],
+      ['23'],
+      ['ㅁ'],
+      [';'],
+      [''],
+      [0],
+      [true],
+      [false],
+    ];
+
+    inputs.forEach(input => runException(input));
+  });
+
+  test('이동 방향 입력 예외 테스트', () => {
+    const inputs = [
+      ['3', 1],
+      ['3', 'u'],
+      ['3', 'ㅕ'],
+      ['3', 'd'],
+      ['3', 'ㅇ'],
+    ];
+
+    inputs.forEach(input => runException(input));
+  });
+
+  test('재시작 입력 예외 테스트', () => {
+    mockRandoms(['1', '0', '1']);
+    const inputs = [
+      ['3', 'D', 'r'],
+      ['3', 'D', 'ㄱ'],
+      ['3', 'D', 'q'],
+      ['3', 'D', 'ㅂ'],
+    ];
+
+    inputs.forEach(input => runException(input));
   });
 });
