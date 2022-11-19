de95d16e304b1030265ec19f108f1a5255110b8f
turtle601 <turtle601@naver.com>
2022-11-19 11:50:51 +0900

    feat(constant) BridgeRestart 관련 enum 객체 생성

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,13 @@
 const REGEX = Object.freeze({
   bridgeLength: /^[3-9]{1}$|^1{1}[0-9]{1}$|^20$/,
-  upDownKey: /^U$|^D$/
+  upDownKey: /^U$|^D$/,
+  restartExitKey: /^Q$|^R$/
 });
 
 const ERROR_MESSAGE = Object.freeze({
   bridgeLength: '[ERROR] 3 ~ 20 이하의 다리 길이를 입력하세요',
-  upDownKey: '[ERROR] U 또는 D 문자만 입력 가능합니다.'
+  upDownKey: '[ERROR] U 또는 D 문자만 입력 가능합니다.',
+  restartExitKey: '[ERROR] Q 또는 R 문자만 입력 가능합니다.'
 });
 
 const MODEL_KEY = Object.freeze({
