2a0ae850751d5bebd7346ea3aab352d219a5f22a
Hchan2 <seongchan_@naver.com>
2022-11-19 20:24:38 +0900

    사용자 입력 시 Return 추가

--- src/InputView.js
+++ src/InputView.js
@@ -12,27 +12,39 @@ const InputView = {
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize() {
+    let bridgeLength = 0;
     MissionUtils.Console.readLine(BRIDGE_LENGTH, (length) => {
       inputLengthCheck(length);
+      bridgeLength = length;
     });
+
+    return Number(bridgeLength);
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving() {
+    let moveInput = '';
     MissionUtils.Console.readLine(MOVE_SPACE, (move) => {
       moveInputCheck(move);
+      moveInput = move;
     });
+
+    return moveInput;
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand() {
+    let gameRestartCheck = '';
     MissionUtils.Console.readLine(RE_OR_END, (decision) => {
       reOrEndCheck(decision);
+      gameRestartCheck = decision;
     });
+
+    return gameRestartCheck;
   },
 };
 
