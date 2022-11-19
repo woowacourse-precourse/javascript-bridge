6a247647def1aadf3ed6d7a73a07d6f6271ce24c
daal2quf <daal2quf@gmail.com>
2022-11-19 19:35:07 +0900

    revert(inputView): InputView의 메서드들이 callback을 argument로 받게 회귀
    reason:
    callback으로 메서드를 넘겨줄때 binding해야하는 이유와 방법을 깨달음

--- src/InputView.js
+++ src/InputView.js
@@ -1,10 +1,12 @@
 const { Console } = require("@woowacourse/mission-utils");
+const BridgeGame = require("./BridgeGame");
 
 const InputView = {
 
   readBridgeSize(callback) {
     Console.readLine('다리의 길이를 입력해주세요.\n', callback);
   },
+
 
   readMoving(callback) {
     Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)\n', callback);
