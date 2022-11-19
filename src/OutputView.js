efb36ba636972813d8eb71d36818756ca1d18cae
wontae chang <cwt629@naver.com>
2022-11-19 22:10:15 +0900

    feat: print final result

--- src/OutputView.js
+++ src/OutputView.js
@@ -20,7 +20,14 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() { },
+  printResult(bridgeGame) {
+    MissionUtils.Console.print("최종 게임 결과");
+    this.printMap(bridgeGame);
+
+    const GAME_RESULT_WORD = (bridgeGame.crossedAll()) ? "성공" : "실패";
+    MissionUtils.Console.print(`게임 성공 여부: ${GAME_RESULT_WORD}`);
+    MissionUtils.Console.print(`총 시도한 횟수: ${bridgeGame.count}`);
+  },
 
   /**
    * 게임 시작 메시지를 출력하는 함수
