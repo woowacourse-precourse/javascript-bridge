08c681f694d3ee4656f07f25c47fadab2419206f
강인영 <kiy@gang-in-yeong-ui-MacBookAir.local>
2022-11-19 21:15:43 +0900

    feat: Read bridge size and Make bridge

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const { Console } = require("@woowacourse/mission-utils");
+const InputView  = require("./InputView");
+const OutputView = require("./OutputView");
+const BridgeMaker = require("./BridgeMaker");
+
 class App {
-  play() {}
+
+  play() {
+    Console.print('다리 건너기 게임을 시작합니다.\n\n');  
+    InputView.readBridgeSize();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
--- src/InputView.js
+++ src/InputView.js
@@ -1,21 +1,35 @@
+const { Console } = require("@woowacourse/mission-utils");
+const BridgeMaker = require("./BridgeMaker");
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
+        
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine('다리의 길이를 입력해주세요.\n', (length) => {
+      //console.log("입력한 다리 길이는...",length);
+      const bridgeList = BridgeMaker.makeBridge(length);
+      console.log(bridgeList);
+    })
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+
+  },
 };
 
 module.exports = InputView;
