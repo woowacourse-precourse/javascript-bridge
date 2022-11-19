b01f1333d935a85c01337019a302aeccf12f263c
ReturnReason <reasonz@daum.net>
2022-11-19 18:48:13 +0900

    feat: 유저 클래스 추가

--- src/model/User.js
+++ src/model/User.js
@@ -1,5 +1,23 @@
 class User {
   #location;
+  #tryCount;
+
+  constructor() {
+    this.#location = 0;
+    this.#tryCount = 1;
+  }
+
+  setLocation() {
+    this.#location += 1;
+  }
+
+  getLocation() {
+    return this.#location;
+  }
+
+  setTryCount() {
+    this.#tryCount += 1;
+  }
 }
 
 module.exports = User;
