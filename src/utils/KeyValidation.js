79146b4da680a2d6912c20d46e1ead2eb219cb9a
pakxe <pigkill40@naver.com>
2022-11-20 02:57:10 +0900

    feat(keyValidation): 이동 방향과 재시작 여부의 유효성을 판별하는 메서드가 비슷하므로 하나로 합침

---
+++ src/utils/KeyValidation.js
@@ -0,0 +1,20 @@
+const { ERROR, BRIDGE, GAME } = require('../constants/Constants');
+
+const movingValidation = (movingDirection) => {
+  const { up, down } = BRIDGE;
+  const errorMessage = ERROR.moving;
+  keyValidation(up, down, movingDirection, errorMessage);
+};
+
+const gameCommandValidation = (gameCommand) => {
+  const { restart, quit } = GAME;
+  const errorMessage = ERROR.restart;
+  keyValidation(restart, quit, gameCommand, errorMessage);
+};
+
+const keyValidation = (firstKey, secondKey, pressedKey, errorMessage) => {
+  if (pressedKey !== firstKey && pressedKey !== secondKey)
+    throw new Error(`${ERROR.prefix} ${errorMessage}`);
+};
+
+module.exports = { movingValidation, gameCommandValidation };
--- src/utils/MovingValidation.js
+++
@@ -1,8 +0,0 @@
-const { ERROR, BRIDGE } = require('../constants/Constants');
-
-const MovingValidation = (moving) => {
-  if (moving !== BRIDGE.up && moving !== BRIDGE.down)
-    throw new Error(`${ERROR.prefix} ${ERROR.moving}`);
-};
-
-module.exports = { MovingValidation };
