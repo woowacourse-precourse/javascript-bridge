40c1e28a549134256c7d36c766534e5f49501bdc
woo-jk <201702042@o.cnu.ac.kr>
2022-11-19 23:30:36 +0900

    feat: Player 클래스 retry 함수와 isMovableNextStep 함수 구현

--- src/Player.js
+++ src/Player.js
@@ -1,11 +1,22 @@
 class Player {
   #position;
+  #tryCount;
 
   constructor() {
     this.#position = 0;
+    this.#tryCount = 1;
   }
 
   move() {
-    this.#position++;
+    this.#position += 1;
+  }
+
+  retry() {
+    this.#position = 0;
+    this.#tryCount += 1;
+  }
+
+  isMovableNextStep(position, bridge) {
+    return bridge.getPassableBridge(this.#position + 1) === position;
   }
 }
