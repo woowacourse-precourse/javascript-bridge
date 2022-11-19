95aff1f7a8ac80b4b330b16220059da1db0c5f7b
jiho1996 <jhp9270@naver.com>
2022-11-19 17:16:09 +0900

    feat : 다리 길이 입력 예외처리 - 3이상 20이하 제한

--- src/utils/BridgeLengthValidator.js
+++ src/utils/BridgeLengthValidator.js
@@ -1,11 +1,15 @@
 class BridgeLengthValidate {
   static validate(number) {
     if (this.#isNumber(number)) throw new Error("[ERROR] 정수만 입력해주세요.");
+    if (this.#isValidRange(number)) throw new Error("[ERROR] 3이상 20이하 숫자만 입력해주세요");
   }
 
   static #isNumber(number) {
     const isNumber = /^[0-9]+$/;
     return !isNumber.test(number);
+  }
+  static #isValidRange(number) {
+    return number > 20 || number < 3;
   }
 }
 module.exports = BridgeLengthValidate;
