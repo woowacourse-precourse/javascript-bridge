73624854fa36b9171bfaa93b9ddb68eab9134003
yaena1223 <yaena1223@dgu.ac.kr>
2022-11-20 05:22:56 +0900

    feat: add ERROR Message

--- src/Constant.js
+++ src/Constant.js
@@ -4,6 +4,7 @@ const INPUT_MESSAGE = {
   READ_GAME_COMMAND:
     '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
 };
+
 const START_MESSAGE = '다리 건너기 게임을 시작합니다.\n';
 
 const OUTPUT_MESSAGE = {
@@ -12,4 +13,16 @@ const OUTPUT_MESSAGE = {
   FAIL: '게임 성공 여부: 실패',
   COUNT_TRY: '총 시도한 횟수: ',
 };
-module.exports = { START_MESSAGE, INPUT_MESSAGE, OUTPUT_MESSAGE };
+
+const ERROR_MESSAGE = {
+  SIZE_ERROR: '[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.',
+  MOVE_ERROR: '[ERROR] 이동할 칸은 U 또는 D여야 합니다.',
+  RESTART_ERROR: '[ERROR] 게임 재시작 여부는 R 또는 Q여야 합니다.',
+};
+
+module.exports = {
+  START_MESSAGE,
+  INPUT_MESSAGE,
+  OUTPUT_MESSAGE,
+  ERROR_MESSAGE,
+};
