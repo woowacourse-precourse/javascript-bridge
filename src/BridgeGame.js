0bdcb026367e61a948d19eddce6584084125cacd
daal2quf <daal2quf@gmail.com>
2022-11-19 14:28:54 +0900

    feat(bridgeGame): 게임 시작시 초기 맵 값을 셋팅
    add:
    setBeginningMapToUserInput(): 생성된 다리인 #password의 0번째 인덱스는 초기 맵셋팅으로 userinput에 넣어줌

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -7,9 +7,9 @@ const InputView = require("./InputView");
  */
 class BridgeGame {
   #password;
-  #userInput = [];
   constructor(bridgeLength) {
     this.#password = BridgeMaker.makeBridge(bridgeLength, BridgeRandomNumberGenerator.generate);
+    this.userInput = [];
   }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -24,6 +24,10 @@ class BridgeGame {
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   retry() {
+  }
+
+  setBeginningMapToUserInput() {
+    this.userInput.push(this.#password[0]);
   }
 }
 
