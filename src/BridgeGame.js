cdde6d761b412dc7b9867ca9cf8e6aa6ae6bc9c9
kangju2000 <kangju2000@naver.com>
2022-11-19 19:01:20 +0900

    feat(BridgeGame): BridgeGame 클래스 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,20 +1,38 @@
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
-class BridgeGame {
-  /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  move() {}
+const Validator = require('./Validator');
 
-  /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  retry() {}
+class BridgeGame {
+  #bridge;
+  #movingBridge;
+  #attempt;
+
+  constructor(bridge) {
+    this.#bridge = bridge;
+    this.#movingBridge = [];
+    this.#attempt = 1;
+  }
+
+  move(moving) {
+    Validator.validateMoving(moving);
+    this.#movingBridge.push(moving);
+  }
+
+  retry() {
+    this.#movingBridge = [];
+    this.#attempt += 1;
+  }
+
+  isSuccessMove() {
+    const index = this.#movingBridge.length - 1;
+    return this.#movingBridge[index] === this.#bridge[index];
+  }
+
+  isSameBridgeSize() {
+    return this.#bridge.length === this.#movingBridge.length;
+  }
+
+  getAttempt() {
+    return this.#attempt;
+  }
 }
 
 module.exports = BridgeGame;
