d8219161d34a937c32ebe90d5f0b7ca38cbade18
yaena1223 <yaena1223@dgu.ac.kr>
2022-11-20 05:23:29 +0900

    feat: add validation of Bridge Size

--- src/InputView.js
+++ src/InputView.js
@@ -1,13 +1,19 @@
 const { Console } = require('@woowacourse/mission-utils');
-const { INPUT_MESSAGE } = require('./Constant');
+const { INPUT_MESSAGE, ERROR_MESSAGE } = require('./Constant');
 
 const InputView = {
   readBridgeSize() {
     return new Promise(resolve => {
       Console.readLine(INPUT_MESSAGE.BRIDGE_SIZE, size => {
-        resolve(size);
+        const VALIDATION = this.validateBridgeSize(size);
+        resolve(VALIDATION);
       });
     });
+  },
+
+  validateBridgeSize(size) {
+    if (size >= 3 && size <= 20) return size;
+    throw new Error(ERROR_MESSAGE.SIZE_ERROR);
   },
 
   readMoving() {
