289f0629116146b12630f268288af72dcc482a18
tkdrb12 <tkdrb15@gmail.com>
2022-11-19 17:39:25 +0900

    feat : 다리 길이를 입력받는 기능 추가

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,5 +1,5 @@
 const { generate } = require("./BridgeRandomNumberGenerator");
-const { makeBridge } = require("./BridgeMaker");
+const { makeBridge } = require("./BridgeMaker.js");
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,8 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const { Console } = MissionUtils;
+const BridgeGame = require("./BridgeGame");
+
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,12 +10,24 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize(App) {
+    let size;
+    Console.readLine("다리의 길이를 입력해주세요.", (input) => {
+      size = input;
+    });
+    return App;
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    let move;
+    Console.readLine("이동할 칸을 입력해주세요.", (input) => {
+      const move = input;
+    });
+    return move;
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
