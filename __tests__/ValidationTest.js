445e752f9b8ff9987dd8f327b857c9d5f695e28d
haesa <xdhaesa@gmail.com>
2022-11-20 02:16:55 +0900

    test(ValidationTest.js): 이동할 칸에 대한 입력 예외 테스트

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,4 +1,5 @@
 const Validation = require('../src/Validation');
+const { ERROR_MESSAGE } = require('../src/Constants');
 
 describe('다리 길이 예외 테스트', () => {
   test('다리 길이가 양의 정수가 아니면 예외가 발생한다.', () => {
@@ -11,5 +12,13 @@ describe('다리 길이 예외 테스트', () => {
     expect(() => {
       Validation.bridgeSize(21);
     }).toThrow('[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.');
+  });
+});
+
+describe('이동할 칸에 대한 입력 예외 테스트', () => {
+  test('입력이 U 또는 D가 아니면 예외가 발생한다.', () => {
+    expect(() => {
+      Validation.moving('A');
+    }).toThrow(ERROR_MESSAGE.MOVING);
   });
 });
