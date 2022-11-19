22c71a06826b58aaccdd8804a3e33f52306e4979
pinkishincoloragain <smb1103@gmail.com>
2022-11-20 00:12:59 +0900

    feat: BridgeGame 테스트 구현

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -81,6 +81,25 @@ describe("다리 건너기 테스트", () => {
     expectBridgeOrder(log, "[ O |   | O ]", "[   | O |   ]");
   });
 
+  test("BridgeGame 테스트", () => {
+    const logSpy = getLogSpy();
+    mockRandoms(["0", "0", "0", "1", "1", "1"]);
+    mockQuestions(["6", "D", "D", "D", "U", "U", "U"]);
+
+    const app = new App();
+    app.play();
+
+    const log = getOutput(logSpy);
+    expectLogContains(log, [
+      "최종 게임 결과",
+      "[   |   |   | O | O | O ]",
+      "[ O | O | O |   |   |   ]",
+      "게임 성공 여부: 성공",
+      "총 시도한 횟수: 1",
+    ]);
+    expectBridgeOrder(log, "[   |   |   | O | O | O ]", "[ O | O | O |   |   |   ]");
+  });
+
   test("예외 테스트", () => {
     runException(["a"]);
   });
