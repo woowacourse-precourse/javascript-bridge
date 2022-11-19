e382c050e6bc1bcdc32bffcb11181282fa0a9933
durumi99 <kms990615@naver.com>
2022-11-19 14:40:11 +0900

    feat: 게임을 시작하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 const OutputView = require('./OutputView');
+const InputView = require('./InputView');
+const BridgeGame = require('./BridgeGame');
 
 class App {
+
   play() {
     OutputView.printInit();
-    this.initGame();
+    InputView.readBridgeSize(this.initGame);
   }
 
-  initGame() {
-
+  initGame(bridgeLength) {
+    new BridgeGame(bridgeLength);
   }
+
 }
 
 const app = new App();
