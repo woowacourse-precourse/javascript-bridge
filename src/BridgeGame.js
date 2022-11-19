ab3bf6f7634f352322e133a02d4bfc1da2986c0f
Adult96 <djxor007@gmail.com>
2022-11-19 21:46:04 +0900

    feat(BridgeGame): 이동한 다리 만큼 설정하는 메서드 추가

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -39,12 +39,15 @@ class BridgeGame {
   progressMove(input) {
     const moveCount = this.#moveCount;
     this.increamentMoveCount();
-    this.compareInput(input, moveCount);
+    this.compareMoveInput(input, moveCount);
   }
 
-  compareInput(input, index) {
+  compareMoveInput(input, index) {
     if (this.#bridgeList[index] === input) {
+      return this.setMapList(input, 'O');
     }
+
+    return this.setMapList(input, 'X');
   }
 
   /**
@@ -60,6 +63,20 @@ class BridgeGame {
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   retry() {}
+
+  setMapList(input, stingOX) {
+    if (input === 'U') {
+      return this.addMapList(stingOX, ' ');
+    }
+    if (input === 'D') {
+      return this.addMapList(' ', stingOX);
+    }
+  }
+
+  addMapList(up, down) {
+    this.#mapList.up.push(up);
+    this.#mapList.down.push(down);
+  }
 
   increamentMoveCount() {
     return (this.#moveCount += 1);
