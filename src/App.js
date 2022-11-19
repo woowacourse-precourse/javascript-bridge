1c04f41db08541521b77e9040f809b6a05980812
wontae chang <cwt629@naver.com>
2022-11-19 23:01:34 +0900

    feat: make postprocess of getting bridge size input

--- src/App.js
+++ src/App.js
@@ -5,10 +5,18 @@ const BridgeMaker = require("./BridgeMaker");
 const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
 
 class App {
+  constructor() {
+    this.game = null;
+  }
+
   play() { }
 
   processAfterGettingBridgeSize(size) {
     const PATH = BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate);
+    this.game = new BridgeGame(PATH);
+
+    // TODO: 이후 과정을 파라메터로 넘겨주기
+    InputView.readMoving();
   }
 }
 
