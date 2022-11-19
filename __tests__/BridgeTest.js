10426a54f87392271d6bad9e7b0a0bc4937dbc2a
woo-jk <201702042@o.cnu.ac.kr>
2022-11-19 16:37:40 +0900

    test: Bridge 클래스 건널 수 있는지 확인하는 checkPassable 함수 테스트 코드 작성

--- __tests__/BridgeTest.js
+++ __tests__/BridgeTest.js
@@ -16,4 +16,16 @@ describe("다리 클래스 테스트", () => {
       new Bridge(path);
     }).toThrow("[ERROR] 다리의 길이는 3 이상 20 이하의 숫자만 입력 가능합니다.");
   });
+
+  test("해당 위치의 칸을 건너지 못할 때", () => {
+    const bridge = new Bridge(["U", "U", "D", "D"]);
+    const secondIndexIsUpPosition = bridge.checkPassable("U", 2);
+    expect(secondIndexIsUpPosition).toBeFalsy();
+  });
+
+  test("해당 위치의 칸을 건널 수 있을 때", () => {
+    const bridge = new Bridge(["U", "U", "D", "D"]);
+    const secondIndexIsUpPosition = bridge.checkPassable("D", 2);
+    expect(secondIndexIsUpPosition).toBeTruthy();
+  });
 });
