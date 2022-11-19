5fa3f9d886c699dae2c7df06a6b1e73b42567f45
Tory <88531407+1two13@users.noreply.github.com>
2022-11-19 20:22:14 +0900

    style(Messages): 에러 메세지에 '\n' 추가

--- src/Messages.js
+++ src/Messages.js
@@ -5,9 +5,9 @@ class Messages {
   static INPUT_RESTART_OR_END = '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n';
   static END_GAME = '최종 게임 결과';
 
-  static BRIDGE_SIZE_ERROR = '[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.';
-  static MOVING_ERROR = '[ERROR] 이동할 칸("U" 또는 "D")을 입력해주세요.';
-  static GAME_COMMAND_ERROR = '[ERROR] 재시작 또는 종료("R" 또는 "Q")를 입력해주세요.';
+  static BRIDGE_SIZE_ERROR = '\n[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.';
+  static MOVING_ERROR = '\n[ERROR] 이동할 칸("U" 또는 "D")을 입력해주세요.';
+  static GAME_COMMAND_ERROR = '\n[ERROR] 재시작 또는 종료("R" 또는 "Q")를 입력해주세요.';
 }
 
 module.exports = Object.freeze(Messages);
