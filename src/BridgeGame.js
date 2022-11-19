f5fbf4669550cccc28f95aa0e5da86b8b44f409d
Deemou <4106243@naver.com>
2022-11-19 18:47:23 +0900

    feat(BridgeGame): #count 추가, 생성자 수정

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,3 +1,4 @@
+const { BRIDGE } = require('./constant/Bridge');
 const BridgeMaker = require('./BridgeMaker');
 const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
 /**
@@ -5,15 +6,16 @@ const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
  */
 class BridgeGame {
   #bridge;
+  #count;
   constructer(size) {
     this.bridge = BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator);
+    this.count = 0;
   }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  //다리, 현재 칸, 이동할 칸
   move() {}
 
   /**
