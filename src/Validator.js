ec10ab59c6fd77d1d63793d75ffcdffaaa02587e
jisu-kim <kss2460@naver.com>
2022-11-19 21:37:58 +0900

    feat(validateMoveInput): 움직임 입력 예외처리

--- src/Validator.js
+++ src/Validator.js
@@ -4,8 +4,12 @@ const Validator = {
       throw "[ERROR] 3 이상 20 이하의 숫자만 입력 가능합니다.";
     }
   },
+
+  validateMoveInput(move) {
+    if (move !== "U" || move === "D") {
+      throw "[ERROR] 입력값은 U 또는 D 만 가능합니다.";
+    }
+  },
 };
 
 module.exports = Validator;
-
-
