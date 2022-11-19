1c99ed38548849b34c993ea91165ac473e0d4237
thewronghand <penfreak77@gmail.com>
2022-11-19 10:23:39 +0900

    feat: 사용자 다리 길이 입력 메서드 readBridgeSize 구현
    사용자로부터 다리 길이를 입력받아 유효성을 판별하고, 해당 입력값으로 bridgeGame을 실행하는 로직을 구현한다.

--- src/InputView.js
+++ src/InputView.js
@@ -1,11 +1,18 @@
+const BridgeGame = require ('./BridgeGame');
+const { MESSAGE } = require('./constants/messages');
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
-const InputView = {
+ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    MissionUtils.Console.readLine(MESSAGE.READ_USER_INPUT_COMMAND, (input) => {
+      this.handleBridgeSizeException(input);
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
@@ -16,6 +23,17 @@ const InputView = {
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand() {},
+
+  handleBridgeSizeException(size) {
+    try {
+      Validate.userInputSize(size);
+      const bridgeGame = new BridgeGame(size);
+      this.readMoving(bridgeGame);
+    } catch (error) {
+      MissionUtils.Console.print(MESSAGE.ERROR.USER_INPUT_BRIDGE_SIZE_INVALID);
+      this.readBridgeSize();
+    }
+  },
 };
 
 module.exports = InputView;
