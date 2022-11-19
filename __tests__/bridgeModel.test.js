73833fb7f69ebb8c3bea1ed7bdd9d6cd6887e259
ukkodeveloper <ukkodeveloper@gmail.com>
2022-11-19 16:11:47 +0900

    test: bridgeModel move 상태확인  테스트 추가

--- __tests__/bridgeModel.test.js
+++ __tests__/bridgeModel.test.js
@@ -1,10 +1,11 @@
 const BridgeModel = require('../src/BridgeModel');
 
 describe('move', () => {
-  const bridgeModel = new BridgeModel(['U', 'U', 'D']);
+  const bridgeModel = new BridgeModel(['U', 'U', 'D', 'D']);
   test('첫 번째 이동(U): this.trails 에 {U, O} 추가', () => {
     bridgeModel.move('U');
     expect(bridgeModel.trials).toEqual([{ direction: 'U', result: 'O' }]);
+    expect(bridgeModel.status).toBe('진행중');
   });
 
   test('두 번째 이동(D): this.trails 에 {D, X} 추가', () => {
@@ -13,14 +14,16 @@ describe('move', () => {
       { direction: 'U', result: 'O' },
       { direction: 'D', result: 'X' },
     ]);
+    expect(bridgeModel.status).toBe('실패');
   });
 
   test('세 번째 이동(D): this.trails 에 {D, X} 추가', () => {
-    bridgeModel.move('U');
+    bridgeModel.move('D');
     expect(bridgeModel.trials).toEqual([
       { direction: 'U', result: 'O' },
       { direction: 'D', result: 'X' },
-      { direction: 'U', result: 'X' },
+      { direction: 'D', result: 'O' },
     ]);
+    expect(bridgeModel.status).toBe('진행중');
   });
 });
