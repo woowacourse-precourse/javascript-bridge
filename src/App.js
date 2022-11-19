a1023caa25026ad0e6663ac755490ec1cfd17cb5
한수지 <fatholic5@gmail.com>
2022-11-20 02:26:12 +0900

    :sparkles: feat: (App) 이동 정보 입력을 호출할 순서를 추가, 오류 발생시 재실행 구현 모델(BridgeGame)에서 데이터를 저장하고 처리할 수 있도록 호출하는 방식으로 구현함 사용자가 이동정보시 잘못된 값을 입력하면 재실행 할 수 있도록 만듬 게임시작 후 다리 건너기 게임의 다리정보가 생성된 다음에 이동값을 받을수 있도록 순서를 연결함

--- src/App.js
+++ src/App.js
@@ -17,17 +17,35 @@ class App {
     InputView.readBridgeSize(this.tryBuildBridge.bind(this));
   }
 
+  requestMoving() {
+    InputView.readMoving(this.tryBuildMoving.bind(this));
+  }
+
   tryBuildBridge(size) {
     try {
       this.brideGame.ready(size);
+      this.requestMoving();
     } catch(error) {
       this.retryRequestBridgeSize();
+    }
+  }
+
+  tryBuildMoving(moving) {
+    try {
+      this.brideGame.move(moving);
+    } catch(error) {
+      this.retryRequestMoving();
     }
   }
 
   retryRequestBridgeSize() {
     OutputView.printValidateSizeError();
     this.requestBridgeSize();
+  }
+
+  retryRequestMoving() {
+    OutputView.printValidateStringError();
+    this.requestMoving();
   }
 
 }
