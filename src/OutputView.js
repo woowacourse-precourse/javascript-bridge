dd21b172f2323dc7fb6eabb4e95bc6f4c4b28c41
shackstack <kms1061@naver.com>
2022-11-19 17:36:42 +0900

    refactor(OutputView): 출력물 벡틱구조로 변경

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,13 +1,6 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 const OutputView = {
-  printMap(moving, boolean) {
-    let upstairs = this.makeBridge(moving, boolean, "U");
-    let downstairs = this.makeBridge(moving, boolean, "D");
-
-    MissionUtils.Console.print("[" + upstairs.join("|") + "]");
-    MissionUtils.Console.print("[" + downstairs.join("|") + "]" + "\n");
-  },
   makeBridge(moving, boolean, string) {
     let answer = moving.map((direction, index) => {
       if (direction == string && index === moving.length - 1) {
@@ -21,14 +14,20 @@ const OutputView = {
     return answer;
   },
 
+  printMap(moving, boolean) {
+    let upstairs = this.makeBridge(moving, boolean, "U");
+    let downstairs = this.makeBridge(moving, boolean, "D");
+
+    MissionUtils.Console.print(`[${upstairs.join("|")}]`);
+    MissionUtils.Console.print(`[${downstairs.join("|")}]` + "\n");
+  },
+
   printResult(moving, totalTry, success) {
     MissionUtils.Console.print("최종 게임 결과");
     this.printMap(moving, success);
 
-    MissionUtils.Console.print(
-      "게임 성공 여부: " + (success ? "성공" : "실패")
-    );
-    MissionUtils.Console.print("총 시도한 횟수: " + totalTry);
+    MissionUtils.Console.print(`게임 성공 여부: ${success ? "성공" : "실패"}`);
+    MissionUtils.Console.print(`총 시도한 횟수: ${totalTry}`);
   },
 };
 
