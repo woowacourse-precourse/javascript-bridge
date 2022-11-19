6ece849750ede23a01f62b5804387158cee55168
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-11-19 19:53:50 +0900

    Test : erase only

--- __tests__/BridgeMakerTest.js
+++ __tests__/BridgeMakerTest.js
@@ -1,6 +1,6 @@
 const BridgeMaker = require("../src/BridgeMaker");
 
-describe.only("BridgeMaker 함수", () => {
+describe("BridgeMaker 함수", () => {
   test("다리 생성 테스트", () => {
     const randomNumbers = ["1", "0", "0"];
     const mockGenerator = randomNumbers.reduce((acc, number) => {
