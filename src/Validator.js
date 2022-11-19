703e8f372225bf9e924d09e840ac2e480de677ae
2je0 <bulljy@naver.com>
2022-11-19 22:23:52 +0900

    ✨ feat(validator): 다리 길이 입력 검증 기능 추가

---
+++ src/Validator.js
@@ -0,0 +1,17 @@
+const { BRIDGE_SIZE, MESSAGE } = require('./Constants');
+
+const Validator = {
+  /**
+   * 다리 길이의 유효성을 검증
+   * @param {string} size input으로 들어온 다리의 길이
+   */
+  bridgeSizeValidate(size) {
+    const sizeNum = +size;
+    const sizeIsInteger = Number.isInteger(sizeNum);
+    const sizeIsValid = sizeNum >= BRIDGE_SIZE.LOWER_INCLUSIVE
+     && sizeNum <= BRIDGE_SIZE.UPPER_INCLUSIVE;
+    if (!sizeIsInteger) throw new Error(MESSAGE.ERROR_NOT_A_NUMBER);
+    if (!sizeIsValid) throw new Error(MESSAGE.ERROR_OUT_OF_RANGE);
+  },
+};
+module.exports = Validator;
