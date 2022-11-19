6cebc42dbdde7e85407b055158484fbd6522b189
yan9vvoojin <yan9vvoojin@naver.com>
2022-11-19 13:08:49 +0900

    feat(BridgeGame): 입력받은 칸으로 이동하는 기능 추가

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -6,6 +6,7 @@ const BridgeMaker = require("./BridgeMaker");
  */
 class BridgeGame {
   #bridge;
+  #movings = [];
 
   constructor(size) {
     this.validateBridgeSize(size);
@@ -47,7 +48,10 @@ class BridgeGame {
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(moving) {
+    this.validateMoving(moving);
+    this.#movings.push(moving);
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
