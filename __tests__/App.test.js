bfbb7eebbbeeb55c7e456663618296271179e968
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-11-19 15:41:10 +0900

    test(App): 다리 생성 메서드 테스트
    인자가 숫자로 변환이 불가능하면 예외를 발생한다.

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -2,6 +2,8 @@ const MissionUtils = require('@woowacourse/mission-utils');
 
 const App = require('../src/App');
 
+const NUMBER_ERROR_TEXT = '[ERROR] 전달된 인수는 숫자 타입이 아닙니다.';
+
 afterAll(() => {
   MissionUtils.Console.close();
 });
@@ -20,5 +22,14 @@ describe('다리 생성 메서드 테스트', () => {
     const RECEIVED = 3;
 
     expect(app.generateBridge(EXPECTED)).toHaveLength(RECEIVED);
+  });
+
+  test('인자가 숫자로 변환이 불가능하면 예외를 발생한다.', () => {
+    const app = new App();
+    const EXPECTED = NaN;
+
+    expect(() => {
+      app.generateBridge(EXPECTED);
+    }).toThrow(NUMBER_ERROR_TEXT);
   });
 });
