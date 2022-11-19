43616f4158b8e153fc93d35a217e2cfa03c42efa
wjs5025 <wjs_5025@naver.com>
2022-11-19 23:50:06 +0900

    feat : 예외 메시지 추가 (COUNT_EXCEPTION)

--- src/constraints/constarints.js
+++ src/constraints/constarints.js
@@ -10,22 +10,23 @@ const MESSAGES = {
       true: "게임 성공 여부: 성공",
       false: "게임 성공 여부 : 실패",
     },
-    EXCEPTIONS: {
-      BRIDGE: {
-        TYPE_EXCEPTION: "[ERROR] 다리 길이의 입력 값은 정수여야 합니다.",
-        RANGE_EXCEPTION: "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.",
-      },
-      RANDOM: {
-        VALUE_EXCEPTION: "[ERROR] 무작위 값이 0 또는 1이 아닙니다.",
-      },
-      MOVE: {
-        LENGTH_EXCEPTION: "[ERROR] 한 개의 문자만 입력해주세요.",
-        VALUE_EXCEPTION: "[ERROR] 입력된 문자가 U 또는 D가 아닙니다.",
-      },
-      RETRY: {
-        LENGTH_EXCEPTION: "[ERROR] 한 개의 문자만 입력해주세요.",
-        VALUE_EXCEPTION: "[ERROR] 입력된 문자가 R 또는 Q가 아닙니다.",
-      },
+  },
+  EXCEPTIONS: {
+    BRIDGE: {
+      COUNT_EXCEPTION: "[ERROR] 다리 길이의 입력 값을 1개만 입력해주세요.",
+      TYPE_EXCEPTION: "[ERROR] 다리 길이의 입력 값은 정수여야 합니다.",
+      RANGE_EXCEPTION: "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.",
+    },
+    RANDOM: {
+      VALUE_EXCEPTION: "[ERROR] 무작위 값이 0 또는 1이 아닙니다.",
+    },
+    MOVE: {
+      LENGTH_EXCEPTION: "[ERROR] 한 개의 문자만 입력해주세요.",
+      VALUE_EXCEPTION: "[ERROR] 입력된 문자가 U 또는 D가 아닙니다.",
+    },
+    RETRY: {
+      LENGTH_EXCEPTION: "[ERROR] 한 개의 문자만 입력해주세요.",
+      VALUE_EXCEPTION: "[ERROR] 입력된 문자가 R 또는 Q가 아닙니다.",
     },
   },
 };
