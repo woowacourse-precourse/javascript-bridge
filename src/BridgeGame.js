b991332de99197c68451bcc889c3c20d428a4711
hae-on <solbi2004@naver.com>
2022-11-19 10:43:13 +0900

    feat: 다리 이동에 따라 기록 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,3 +1,4 @@
+const { UP, DOWN } = require('./Constants');
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
@@ -7,7 +8,49 @@ class BridgeGame {
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  constructor() {
+    this.step = 0;
+    this.upBridge = [];
+    this.downBridge = [];
+  }
+
+  countStep() {
+    this.step += 1;
+  }
+
+  correct(userMove) {
+    if (userMove === UP) {
+      this.upBridge.push('O');
+      this.downBridge.push(' ');
+    }
+    if (userMove === DOWN) {
+      this.upBridge.push(' ');
+      this.downBridge.push('O');
+    }
+  }
+
+  wrong(userMove) {
+    if (userMove === UP) {
+      this.upBridge.push('X');
+      this.downBridge.push(' ');
+    }
+    if (userMove === DOWN) {
+      this.upBridge.push(' ');
+      this.downBridge.push('X');
+    }
+  }
+
+  move(bridge, userMove) {
+    if (bridge[this.step] === userMove) {
+      this.countStep();
+      this.correct(userMove);
+      return true;
+    }
+    if (bridge[this.step] !== userMove) {
+      this.wrong(userMove);
+      return false;
+    }
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
@@ -17,4 +60,6 @@ class BridgeGame {
   retry() {}
 }
 
+const bridgeGame = new BridgeGame();
+bridgeGame.move(['U', 'D', 'D'], 'D');
 module.exports = BridgeGame;
