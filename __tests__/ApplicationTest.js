666d27871088a92f4e2f02ce947a939782273882
wjs5025 <wjs_5025@naver.com>
2022-11-19 15:44:42 +0900

    chore : 디렉토리 구조 변경

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,6 +1,6 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const App = require("../src/App");
-const BridgeMaker = require("../src/BridgeMaker");
+const BridgeMaker = require("../src/models/BridgeMaker");
 
 const mockQuestions = (answers) => {
   MissionUtils.Console.readLine = jest.fn();
