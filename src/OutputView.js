f6a8cb52cd10a2ac7915ece46d75f36e70e7829a
mjmj0217 <mjmj0217@naver.com>
2022-11-19 22:42:47 +0900

    feat(OutputView): 게임 결과 출력 메소드 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -20,11 +20,23 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() {},
+  printResult(upBridge, downBridge, success) {
+    Console.print(gameConst.result.message.RESULT_MESSAGE);
+    Console.print(upBridge.join(" "));
+    Console.print(downBridge.join(" "));
+    
+    Console.print("");
+    
+    const STATUS = success ? gameConst.result.value.SUCCESS : gameConst.result.value.FAIL;
+    const TRIAL_CNT = Math.floor((upBridge.length-2)/2+1);
+    Console.print(gameConst.result.message.SUCCESS_MESSAGE + " " + STATUS);
+    Console.print(gameConst.result.message.TRIAL_MESSAGE + " " + TRIAL_CNT);
+  },
 
   printStartMessage() {
     Console.print(gameConst.process.START_MESSAGE);
   },
 };
+OutputView.printResult(['[','O','|','O','|','O',']'],['[','O','|','O','|','O',']'],false);
 
 module.exports = OutputView;
