b984d563464bac19be9c08ffcaeda17f23823d2e
osdoonhyun <skw1996@naver.com>
2022-11-19 18:56:00 +0900

    feat(InputView,Validation): 이동할 칸 입력 받는 기능 & 예외처리 구현

--- src/Validation.js
+++ src/Validation.js
@@ -27,6 +27,36 @@ const Validation = {
   isValidSizeType(input) {
     return !isNaN(input);
   },
+  /**
+   * 플레이어가 게임 중 입력한 이동할 칸 값이 형식에 어긋날 경우 예외를 발생시킵니다.
+   * @param {string} playerInput - 플레이어의 입력
+   */
+  validateMoving(input) {
+    if (!this.isValidMovingType(input)) {
+      throw new Error("[ERROR] 이동할 칸은 알파벳을 입력해야 합니다.");
+    }
+
+    if (!this.isValidMovingLength(input)) {
+      throw new Error("[ERROR 이동할 칸은 한 개의 문자열을 입력해야 합니다.");
+    }
+
+    if (!this.isValidMoving(input)) {
+      throw new Error("[ERROR] 이동할 칸은 U 또는 D를 입력해야 합니다.");
+    }
+  },
+
+  isValidMovingType(input) {
+    const regex = /^[a-z|A-Z]+$/;
+    return regex.test(input);
+  },
+
+  isValidMovingLength(input) {
+    return input.length === 1;
+  },
+
+  isValidMoving(input) {
+    return input === "U" || input === "D";
+  },
 };
 
 module.exports = Validation;
