04aed59cdd8b6039c1dfe41410a2543af930d9fc
hyunjin <leehj0110@kakao.com>
2022-11-19 16:13:50 +0900

    feat: GameControl Validation

--- src/validations/GameControl.js
+++ src/validations/GameControl.js
@@ -1,5 +1,7 @@
-const GameControl = {
-  validation: (input) => {
+const GameControlValidation = {
+  validate: (input) => {
     if (input !== 'R' && input !== 'Q') throw new CustomError('게임 재시작/종료 여부는 R 또는 Q만 입력할 수 있습니다.');
   },
 };
+
+module.exports = GameControlValidation;
