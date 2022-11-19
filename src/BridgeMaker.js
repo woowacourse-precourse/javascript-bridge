58542e016b1daf9599700de1fd7c5f9d74fa45c3
Hchan2 <seongchan_@naver.com>
2022-11-19 20:24:03 +0900

    다리 Up, Down 생성

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,6 +1,3 @@
-const MissionUtils = require("@woowacourse/mission-utils");
-const { BRIDGE_START } = require('./constant/outputMessage');
-
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
@@ -11,7 +8,18 @@ const BridgeMaker = {
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
   makeBridge(size, generateRandomNumber) {
-    MissionUtils.Console.print(BRIDGE_START);
+    let makeStringBridge = [];
+    for (let i = 0; i < size; i++) {
+      const randomNumber = generateRandomNumber;
+      if (randomNumber === 1) {
+        makeStringBridge.push('U');
+      }
+      if (randomNumber === 0) {
+        makeStringBridge.push('D');
+      }
+    }
+    
+    return makeStringBridge;
   },
 };
 
