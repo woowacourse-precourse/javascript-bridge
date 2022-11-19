9bbf4f98efb0779b22cecda70eccbf8ca02866ac
MAGHC <windowsapple95@gmail.com>
2022-11-19 19:51:28 +0900

    feat: App.js 다리의 움직일 값을 입력 받고 이동하여 지도 만들기까지의 진행 기능 구현

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ const InputView = require('./InputView');
 const BridgeMaker = require('./BridgeMaker');
 const OutputView = require('./OutputView');
 const Validator = require('./Validator');
+const BridgeGame = require('./BridgeGame');
+const BridgeMap = require('./BridgeMap');
+const Utils = require('./Utils');
 
 class App {
   #appStatus = 1;
 
   #bridgeAnswer;
+
+  #bridge;
+
+  #brdigeGame = new BridgeGame();
+
+  #moveAnswer;
+
+  #bridgeMap = new BridgeMap();
+
+  #moveStatement;
+
+  #gameStaus;
 
   play() {
     OutputView.printStart();
@@ -15,7 +30,25 @@ class App {
 
   progressApp(appStatus) {
     if (appStatus === 1) return this.progressBridgeMake();
-    if (appStatus === 2) return BridgeMaker.makeBridge(this.#bridgeAnswer);
+    if (appStatus === 2) {
+      this.#appStatus = 3;
+      this.#bridge = BridgeMaker.makeBridge(this.#bridgeAnswer);
+      return this.progressBridgeMove();
+    }
+    if (appStatus === 3) return this.progressBridgeMove();
+    if (appStatus === 4) {
+      this.#moveStatement = this.#brdigeGame.move(this.#moveAnswer, this.#bridge);
+      console.log(this.#moveStatement);
+      if (this.#moveStatement) {
+        this.#bridgeMap.handleMap(this.#moveStatement, this.#bridge);
+        return this.progressBridgeMove();
+      }
+      if (!this.#moveStatement) {
+        this.#bridgeMap.handleMap(this.#moveStatement, this.#bridge);
+        this.#appStatus = 5;
+        Utils.close();
+      }
+    }
   }
 
   progressBridgeMake() {
@@ -24,6 +57,20 @@ class App {
         Validator.checkBridgeInput(answer);
         this.#bridgeAnswer = +answer;
         this.#appStatus = 2;
+      } catch (e) {
+        console.log(e, '예외발생');
+      } finally {
+        this.progressApp(this.#appStatus);
+      }
+    });
+  }
+
+  progressBridgeMove() {
+    InputView.readMoving((answer) => {
+      try {
+        Validator.confirmOfCondition(answer, 'move');
+        this.#moveAnswer = answer;
+        this.#appStatus = 4;
       } catch (e) {
         console.log(e, '예외발생');
       } finally {
