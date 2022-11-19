1b891a4560403d637496d5351568dcaf009eba88
jaewoogwak <jaewoo010207@gmail.com>
2022-11-19 15:44:01 +0900

    feat(BridgeMaker): 유효성 검사 로직 Model 내부로 이동

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,4 +1,5 @@
 const { createTokens } = require('./BridgeGame');
+const Validator = require('./Validator');
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -10,11 +11,14 @@ const BridgeMaker = {
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
   makeBridge(size, generateRandomNumber) {
+    Validator.chekcBridgeSizeValue(size);
+
     const tokens = createTokens(size, generateRandomNumber);
     const bridge = tokens.reduce((acc, cur) => {
-      acc.push(cur === 1 ? 'U' : 'D');
+      acc.push(parseInt(cur, 10) === 1 ? 'U' : 'D');
       return acc;
     }, []);
+
     return bridge;
   },
 };
