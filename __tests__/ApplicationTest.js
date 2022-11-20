681b90f889a5c3d81e3f8cb0fd51ca7e580ad95a
bkdragon0228 <rlaqjarb2067@naver.com>
2022-11-21 01:06:00 +0900

    test : bridge 도메인에 대한 단위 테스트

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -53,80 +53,7 @@ const expectBridgeOrder = (received, upside, downside) => {
   expect(upsideIndex).toBeLessThan(downsideIndex);
 };
 
-// describe('다리 건너기 테스트', () => {
-//   test('다리 생성 테스트', () => {
-//     const randomNumbers = ['1', '0', '0'];
-//     const mockGenerator = randomNumbers.reduce((acc, number) => {
-//       return acc.mockReturnValueOnce(number);
-//     }, jest.fn());
-
-//     const bridge = BridgeMaker.makeBridge(3, mockGenerator);
-//     expect(bridge).toEqual(['U', 'D', 'D']);
-//   });
-
-//   test('기능 테스트', () => {
-//     const logSpy = getLogSpy();
-//     mockRandoms(['1', '0', '1']);
-//     mockQuestions(['3', 'U', 'D', 'U']);
-
-//     const app = new App();
-//     app.play();
-
-//     const log = getOutput(logSpy);
-//     expectLogContains(log, [
-//       '최종 게임 결과',
-//       '[ O |   | O ]',
-//       '[   | O |   ]',
-//       '게임 성공 여부: 성공',
-//       '총 시도한 횟수: 1',
-//     ]);
-//     expectBridgeOrder(log, '[ O |   | O ]', '[   | O |   ]');
-//   });
-
-//   test('예외 테스트', () => {
-//     runException(['a']);
-//   });
-// });
-
-describe('유효성 TEST', () => {
-  test('다리 개수 유효성 테스트', () => {
-    InputView.readBridgeSize = jest
-      .fn()
-      .mockImplementation((callback) => callback(500));
-
-    const game = new GameController();
-
-    expect(() => {
-      game.askBridge();
-    }).toThrow('[ERROR]');
-  });
-
-  test('다음 칸 이동 입력 유효성 테스트', () => {
-    InputView.readMoving = jest
-      .fn()
-      .mockImplementation((callback) => callback('k'));
-
-    const game = new GameController();
-
-    expect(() => {
-      game.askMoving();
-    }).toThrow('[ERROR]');
-  });
-
-  test('재시작/종료 여부 입력 유효성 테스트', () => {
-    InputView.readGameCommand = jest
-      .fn()
-      .mockImplementation((callback) => callback('k'));
-
-    const game = new GameController();
-
-    expect(() => {
-      game.askGameCommand();
-    }).toThrow('[ERROR]');
-  });
-});
-
-describe.only('기능 TEST', () => {
+describe('다리 건너기 테스트', () => {
   test('다리 생성 테스트', () => {
     const randomNumbers = ['1', '0', '0'];
     const mockGenerator = randomNumbers.reduce((acc, number) => {
@@ -137,15 +64,26 @@ describe.only('기능 TEST', () => {
     expect(bridge).toEqual(['U', 'D', 'D']);
   });
 
-  test('다리 이동 테스트', () => {
+  test('기능 테스트', () => {
     const logSpy = getLogSpy();
-    mockRandoms(['1', '1', '1']);
-    mockQuestions(['3', 'U', 'U', 'U']);
+    mockRandoms(['1', '0', '1']);
+    mockQuestions(['3', 'U', 'D', 'U']);
 
     const app = new App();
     app.play();
 
     const log = getOutput(logSpy);
-    expectBridgeOrder(log, '[ O | O | O ]', '[   |   |   ]');
+    expectLogContains(log, [
+      '최종 게임 결과',
+      '[ O |   | O ]',
+      '[   | O |   ]',
+      '게임 성공 여부: 성공',
+      '총 시도한 횟수: 1',
+    ]);
+    expectBridgeOrder(log, '[ O |   | O ]', '[   | O |   ]');
+  });
+
+  test('예외 테스트', () => {
+    runException(['a']);
   });
 });
---
+++ __tests__/BridgeTest.js
@@ -0,0 +1,20 @@
+const BridgeGame = require('../src/model/BridgeGame');
+const Bridge = require('../src/model/Bridge');
+
+describe.only('Bridge Test', () => {
+  test.each(['500', '1', 'a'])('다리 개수 유효성 테스트', (input) => {
+    expect(() => {
+      const game = new BridgeGame();
+      game.setBridge(input);
+    }).toThrow('[ERROR]');
+  });
+
+  test.each(['3', '5', '10'])(
+    '다리 길이가 맞게 생성 되는지 테스트',
+    (input) => {
+      const bridge = new Bridge(input);
+
+      expect(bridge.getBridge().length).toEqual(Number(input));
+    }
+  );
+});
