b8499f69d54408b47770a5b9fcc6f880ad1713d5
nalsae <nalsae0207@naver.com>
2022-11-21 02:25:56 +0900

    feat: Create an object about messages of information
    게임 안내 관련 메시지 추가

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,5 +1,12 @@
-const ERROR_MESSAGES = {
-  BRIDGE_SIZE: '[ERROR] 다리 길이는 3 ~ 20 사이의 숫자로 입력해야 합니다.',
+const INFO_MESSAGES = {
+  BRIDGE_SIZE: '다리의 길이를 입력해주세요.\n',
+  MOVING: '\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
 };
 
-module.exports = ERROR_MESSAGES;
+const ERROR_MESSAGES = {
+  WRONG_BRIDGE_SIZE:
+    '[ERROR] 다리 길이는 3 ~ 20 사이의 숫자로 입력해야 합니다.',
+  WRONG_MOVEMENT: '[ERROR] 이동할 칸은 "U" 혹은 "D"로 입력해야 합니다.',
+};
+
+module.exports = { INFO_MESSAGES, ERROR_MESSAGES };
