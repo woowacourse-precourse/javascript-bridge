bf6e89195cab3eca9bf6024aa48fdf3f14c2975f
Yesong <to06109@naver.com>
2022-11-21 05:06:53 +0900

    feat(makeBridge): 0과 1 중 무작위 값 생성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
 const command = require('./utils/message');
 const OutputView = require('./OutputView');
 const InputView = require('./InputView');
+const BridgeMaker = require('./BridgeMaker');
 class App {
   #bridgeSize;
+  #bridge;
 
   async play() {
     OutputView.printCommand(command.START);
     this.#bridgeSize = await InputView.readBridgeSize();
+    this.#bridge = BridgeMaker.makeBridge(
+      this.#bridgeSize,
+      BridgeRandomNumberGenerator.generate
+    );
   }
 }
 
