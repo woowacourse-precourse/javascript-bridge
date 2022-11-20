3a6de0552ab26f6dd3bfb6f770af0616b6744583
mjmj0217 <mjmj0217@naver.com>
2022-11-20 19:46:23 +0900

    feat(BridgeGame): 게임 클래스 초기화

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,7 +1,21 @@
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
+
 class BridgeGame {
+  realBridge;
+  upBridge;
+  downBridge;
+  curr;
+
+  constructor() {
+    this.upBridge = [];
+    this.downBridge = [];
+    this.curr = 0;
+  }
+
+  makeRealBridge() {}
+
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
