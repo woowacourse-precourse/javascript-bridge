4a38dca62078a316ae7075386566f0e2aee5c448
1 <wrasf175@naver.com>
2022-11-20 23:51:08 +0900

    test(BridgeMakerTest): 다리 생성기능 단위 테스트 작성

--- __tests__/BridgeMakerTest.js
+++ __tests__/BridgeMakerTest.js
@@ -1,5 +1,12 @@
 const BridgeMaker = require('../src/BridgeMaker');
 
 describe('다리 생성 기능 단위 테스트', () => {
-
+    it('사용자의 입력값 만큼 다리 길이의 사이즈를 정한다',()=>{
+        const bridge = BridgeMaker.makeBridgeTest(15, 1);
+        expect(bridge.length).toBe(15);
+    })
+    it('사용자의 입력값 만큼 다리 길이의 사이즈를 정한다',()=>{
+        const bridge = BridgeMaker.makeBridgeTest(3, 1);
+        expect(bridge).toBe([1,1,1]);
+    })
 });
