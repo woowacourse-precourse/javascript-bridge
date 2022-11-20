2782dab4430dd5bbf731dbbee71c720875a3f24e
HWI <dlwognl99@naver.com>
2022-11-20 14:09:53 +0900

    feat: 게임 시작 메세지 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+const { Console } = require("@woowacourse/mission-utils");
+const { OUTPUT_MESSAGE } = require("./constants/message");
+
 class App {
-  play() {}
+  play() {
+    Console.print(OUTPUT_MESSAGE.START);
+  }
 }
 
 module.exports = App;
