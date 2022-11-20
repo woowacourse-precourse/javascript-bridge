132ef6d4cc75426aee54d177fdd52cae5407a54e
Suyoung <jsy8492@gmail.com>
2022-11-20 19:38:42 +0900

    feat(BridgeValidator): 게임 재시작 시 입력되는 게임 명령에 대한 유효성 검사 기능 구현

--- src/BridgeValidator.js
+++ src/BridgeValidator.js
@@ -64,6 +64,28 @@ const BridgeValidator = {
   isInvalidMoving(moving) {
     return !['U', 'D'].includes(moving);
   },
+
+  /**
+   * 사용자가 입력한 게임 명령에 대한 유효성을 검사하는 메서드
+   * @param {string} gameCommand
+   * @throws 유효하지 않은 게임 명령인 경우 error를 throw한다.
+   */
+  validateGameCommand(gameCommand) {
+    if (this.isInvalidGameCommand(gameCommand)) {
+      throw new Error(
+        '[ERROR] 유효하지 않은 명령입니다. 재시작을 원하면 R, 종료를 원하면 Q를 입력해 주세요.',
+      );
+    }
+  },
+
+  /**
+   * 사용자가 입력한 게임 명령이 유효하지 않은 명령인지 판단하는 메서드
+   * @param {string} gameCommand
+   * @returns {boolean} 유효하지 않은 게임 명령인 경우 true, 아니면 false를 반환한다.
+   */
+  isInvalidGameCommand(gameCommand) {
+    return !['R', 'Q'].includes(gameCommand);
+  },
 };
 
 module.exports = BridgeValidator;
