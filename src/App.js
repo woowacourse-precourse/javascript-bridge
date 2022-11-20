797ff1091db41f1517b4cfde2bf1cde42af746fc
d-charlie-kim <dokkim@student.42seoul.kr>
2022-11-20 22:25:52 +0900

    Feat: 다리 길이 입력을 받는다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+const InputView = require('./InputView');
+const Validator = require('./Validator');
+
 class App {
-  play() {}
+  play() {
+    InputView.readBridgeSize(this.settingBridge);
+  }
+  
+  settingBridge(size) {
+    Validator.checkBridgeSize(size);
+    InputView.readMoving(this.playingBridge);
+  }
+
+  playingBridge() {
+    InputView.readGameCommand(this.endingBridge);
+  }
+
+  endingBridge() {
+
+  }
 }
 
 module.exports = App;
+
+const app = new App();
+app.play();
--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,5 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,17 +7,25 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize(settingBridge) {
+    MissionUtils.Console.readLine("다리의 길이를 입력해주세요.\n", (size) => {
+      settingBridge(size);
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+
+  },
 };
 
 module.exports = InputView;
