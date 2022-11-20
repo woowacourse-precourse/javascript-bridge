7d1c3a3a73b51f411a4b29e1261e4804052c095a
dudgp128 <20181048@sungshin.ac.kr>
2022-11-21 01:55:49 +0900

    FEAT(BridgeGame.canMove) : 기능 분리 - 입력값 검증

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -19,18 +19,24 @@ class BridgeGame {
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
+  canMove(input) {
+    console.log(this.#bridge);
+    if (!["U", "D"].includes(input)) throw "[ERROR]";
+    if (input === this.#bridge[this.#currentIdx]) return true;
+    return false;
+  }
+
   move(input) {
-    if (input === this.#bridge[this.#currentIdx]) {
-      this.fillMap(input, "O");
+    if (input == this.#bridge[this.#currentIdx]) {
       this.#currentIdx += 1;
       return this.#currentIdx == this.#bridge.length ? "성공" : "O";
     } else {
-      this.fillMap(input, "X");
       return "X";
     }
   }
 
-  fillMap(position, str) {
+  fillMap(position) {
+    const str = this.canMove(position) ? "O" : "X";
     const positionNumber = position == "U" ? 1 : 0;
     this.#bridgeMap[positionNumber].push(str);
     this.#bridgeMap[Number(!positionNumber)].push(" ");
