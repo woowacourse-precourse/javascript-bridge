8b42a056ece21ce27a500382242b157d670b2046
Jaehyeon <jaehyeonp74@gmail.com>
2022-11-20 17:37:48 +0900

    feat: Model 클래스 setComputerBridgeArr 구현
    - 컴퓨터가 만든 다리 위치를 담은 배열을 저장하는 기능

--- src/Model.js
+++ src/Model.js
@@ -1,3 +1,21 @@
-class Model {}
+class Model {
+  #userUpBridgeArr;
+
+  #userDownBridgeArr;
+
+  #computerBridgeArr;
+
+  #accumulatedOX;
+
+  #totalTryCount;
+
+  constructor() {
+    this.#totalTryCount = 0;
+  }
+
+  setComputerBridgeArr(arr) {
+    this.#computerBridgeArr = arr;
+  }
+}
 
 module.exports = Model;
