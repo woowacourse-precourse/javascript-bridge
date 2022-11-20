76b73e48bf2df5c4d629d47501089792e0ac3ec5
HWI <dlwognl99@naver.com>
2022-11-20 17:15:24 +0900

    feat: 다리 생성, 이동 관련 상수 추가

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,9 +1,20 @@
 const MIN = 3;
 const MAX = 20;
+const LOWER_ZONE_NUMBER = 0;
+const UPPER_ZONE_NUMBER = 1;
+const LOWER_ZONE = "D";
+const UPPER_ZONE = "U";
 
 const LENGTH = {
   MIN,
   MAX,
 };
 
-module.exports = { LENGTH };
+const BRIDGE = {
+  LOWER_ZONE_NUMBER,
+  UPPER_ZONE_NUMBER,
+  LOWER_ZONE,
+  UPPER_ZONE,
+};
+
+module.exports = { LENGTH, BRIDGE };
