2c352149a705c190fad5bca2dbf4a1449fe42045
jaewoogwak <jaewoo010207@gmail.com>
2022-11-20 22:57:10 +0900

    refactor(meesage): 프로퍼티 네이밍 변경

--- src/Constants/message.js
+++ src/Constants/message.js
@@ -1,5 +1,5 @@
 const INPUT_MESSAGE = Object.freeze({
-  bridge_length: '다리의 길이를 입력해주세요.\n',
+  bridge_size: '다리의 길이를 입력해주세요.\n',
   move: '이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
   retry: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
 });
