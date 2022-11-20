e55678f300dd0a1c940787d5cdf13528b98777e8
woo-jk <201702042@o.cnu.ac.kr>
2022-11-21 01:26:15 +0900

    refactor: 다리 길이 검증하는 로직 InputValidate 객체로 이동

--- __tests__/BridgeTest.js
+++ __tests__/BridgeTest.js
@@ -2,22 +2,6 @@ const Bridge = require("../src/Bridge");
 const Player = require("../src/Player");
 
 describe("다리 클래스 테스트", () => {
-  test("다리 길이가 20 초과일 때 예외 처리", () => {
-    const path = Array.from({ length: 21 }, () => "U");
-
-    expect(() => {
-      new Bridge(path);
-    }).toThrow("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
-  });
-
-  test("다리 길이가 3 미만일 때 예외 처리", () => {
-    const path = ["U", "U"];
-
-    expect(() => {
-      new Bridge(path);
-    }).toThrow("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
-  });
-
   test("플레이어의 다음 칸 방향 얻기", () => {
     const path = ["U", "U", "D", "D", "U"];
 
