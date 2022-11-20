a7584fe7a40899960168a53f5a2bd14a5416385c
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-20 12:13:04 +0900

    fix(Test): test.each로 테스트 할 때 input 매개변수를 넘겨주지 못한 오류 수정

--- __tests__/MyTest.js
+++ __tests__/MyTest.js
@@ -1,29 +1,25 @@
 const { ERROR, INPUT, OUTPUT } = require("../src/data/Constants");
 const Validation = require("../src/Validation");
-const App = require("../src/App");
+
 const BridgeMaker = require("../src/BridgeMaker");
 const BridgeRandomNumberGenerator = require("../src/BridgeRandomNumberGenerator");
 const BridgeGame = require("../src/BridgeGame");
-const { checkRetry } = require("../src/Validation");
+const { checkBridgeLength } = require("../src/Validation");
 
 describe(`bridgeSize 입력값 타당성 테스트`, () => {
   test.each([["0"], ["-1"], ["21"]])(
-    `유저가 3~20 사이의 숫자를 입력했는지 확인`,
+    `유저가 3이상 20이하의 숫자를 입력했는지 확인`,
     (input) => {
-      expect((input) => {
-        Validation.checkBridgeLength(input);
-      }).toThrow();
+      expect(() => checkBridgeLength(input)).toThrow();
     }
   );
 
   test.each([["a"], ["a1"], ["#3"], ["10."]])(
     `유저가 숫자만 입력 했는지 확인`,
-    () => {
-      (input) => {
-        expect((input) => {
-          Validation.checkBridgeLength(input);
-        }).toThrow();
-      };
+    (input) => {
+      expect(() => {
+        Validation.checkBridgeLength(input);
+      }).toThrow();
     }
   );
 });
@@ -70,28 +66,25 @@ describe(`userMove 입력값 타당성 테스트`, () => {
   test.each([["u"], ["d"]])(
     `알파벳이 UpperCase가 아닐 때 에러발생`,
     (input) => {
-      expect((input) => {
+      expect(() => {
         Validation.checkMove(input);
       }).toThrow();
     }
   );
 
-  test.each([["#d"], ["a"], ["1d"], ["3U"], ["123"]])(
+  test.each([["#d"], ["a"], ["1d"], ["123"], ["U%"], ["D#"]])(
     `U 또는 D 가 아닌 다른 걸 입력했을 때 에러발생`,
     (input) => {
-      expect((input) => {
+      expect(() => {
         Validation.checkMove(input);
       }).toThrow();
     }
   );
-  test.each([["DD"], ["UU"], ["Ud"], ["U3"], ["U%"]])(
-    `2글자 이상을 입력했을 때 에러발생`,
-    (input) => {
-      expect((input) => {
-        Validation.checkMove(input);
-      }).toThrow();
-    }
-  );
+  test.each([["DD"], ["UU"]])(`2글자 이상을 입력했을 때 에러발생`, (input) => {
+    expect(() => {
+      Validation.checkMove(input);
+    }).toThrow();
+  });
 });
 
 describe(`유저 이동 결과 판단 및 결과를 배열에 저장 테스트`, () => {
@@ -140,25 +133,25 @@ describe(`재시작 입력값 타당성 테스트`, () => {
   test.each([["r"], ["q"]])(
     `알파벳이 UpperCase가 아닐 때 에러발생`,
     (input) => {
-      expect((input) => {
+      expect(() => {
         Validation.checkRetry(input);
       }).toThrow();
     }
   );
 
-  test.each([["3R"], ["a"], ["#"], ["Q."]])(
+  test.each([["a"], ["#"], ["Q."], ["R3"]])(
     `R 또는 Q가 아닌 다른 걸 입력했을 때 에러발생`,
     (input) => {
-      expect((input) => {
+      expect(() => {
         Validation.checkRetry(input);
       }).toThrow();
     }
   );
 
-  test.each([["RR"], ["R3"], ["QQ"], ["QR"]])(
+  test.each([["RR"], ["QQ"], ["QR"], ["RQ"]])(
     `2글자 이상을 입력했을 때 에러발생`,
     (input) => {
-      expect((input) => {
+      expect(() => {
         Validation.checkRetry(input);
       }).toThrow();
     }
