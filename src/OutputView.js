50c55c3dd3933ec56da221a82ac63bd2b08e1786
Hchan2 <seongchan_@naver.com>
2022-11-20 22:04:06 +0900

    Console 출력 문자열로 수정

--- src/OutputView.js
+++ src/OutputView.js
@@ -11,8 +11,8 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printMap(bridgeArray) {
-    MissionUtils.Console.print(bridgeArray[0].join(' | '));
-    MissionUtils.Console.print(bridgeArray[1].join(' | '));
+    MissionUtils.Console.print(`[${bridgeArray[0].join(' | ')}]`);
+    MissionUtils.Console.print(`[${bridgeArray[1].join(' | ')}]`);
   },
 
   /**
