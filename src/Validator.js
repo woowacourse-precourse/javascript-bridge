40c696fe9e2cf527465c228187a88737008f6d50
jisu-kim <kss2460@naver.com>
2022-11-20 15:47:38 +0900

    feat(isRepeat): try catch문 분리

--- src/Validator.js
+++ src/Validator.js
@@ -1,3 +1,5 @@
+const { readMoving } = require("./InputView");
+
 const Validator = {
   validateInput(number) {
     const check = /^[0-9]+$/;
@@ -16,6 +18,16 @@ const Validator = {
   validateRetryInput(retryInput) {
     if (retryInput !== "R" && retryInput !== "Q") {
       throw "[ERROR] 입력값은 R 또는 Q 만 가능합니다.";
+    }
+  },
+
+  isRepeat(moveInput, mainBridge, bridgeGame) {
+    try {
+      Validator.validateMoveInput(moveInput);
+    } catch (error) {
+      Console.print(error);
+      readMoving(mainBridge, bridgeGame);
+      return;
     }
   },
 };
