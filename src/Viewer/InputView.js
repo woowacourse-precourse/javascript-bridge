fa19a83d4aa097d242790fc76a2178cb15c824b5
woong <inwoong100@gmail.com>
2022-11-21 00:06:34 +0900

    refactor(Viewer): Viewer 함수들에 대한 type JSDoc 작성

--- src/Viewer/InputView.js
+++ src/Viewer/InputView.js
@@ -1,4 +1,5 @@
 const { Console } = require('@woowacourse/mission-utils');
+const GameController = require('../Controller/GameController');
 const { MESSAGE } = require('../Utils/Constant');
 
 /**
@@ -7,6 +8,7 @@ const { MESSAGE } = require('../Utils/Constant');
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
+   * @param {GameController} controller
    */
   readBridgeSize(controller) {
     Console.readLine(MESSAGE.READ_BRIDGE_SIZE, (input) => {
@@ -16,6 +18,7 @@ const InputView = {
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
+   * @param {GameController} controller
    */
   readMoving(controller) {
     Console.readLine(MESSAGE.READ_MOVE_LEVEL, (input) => {
@@ -25,6 +28,7 @@ const InputView = {
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+   * @param {GameController} controller
    */
   readGameCommand(controller) {
     Console.readLine(MESSAGE.GAME_RETRY, (input) => {
--- src/Viewer/OutputView.js
+++ src/Viewer/OutputView.js
@@ -1,4 +1,5 @@
 const { Console } = require('@woowacourse/mission-utils');
+const BridgeGame = require('../BridgeGame');
 const { OUTPUT_MESSAGE } = require('../Utils/Constant');
 
 /**
@@ -9,6 +10,7 @@ const OutputView = {
    * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+   * @param {BridgeGame} game
    */
   printMap(game) {
     const resultMap = game.result;
@@ -16,30 +18,51 @@ const OutputView = {
     Console.print(this.getPrintLine(1, resultMap));
   },
 
+  /**
+   *
+   * @param {number} line
+   * @param {boolean|undefined[]} resultMap
+   * @returns {string}
+   */
   getPrintLine(line, resultMap) {
     let messageLine = OUTPUT_MESSAGE.START;
-    for (let levelCnt = 0; levelCnt < resultMap[line].length; levelCnt += 1) {
-      messageLine += this.getMessageElement(line, resultMap, levelCnt);
-      if (!this.isLastLine(levelCnt, resultMap[line].length)) {
+    for (let level = 0; level < resultMap[line].length; level += 1) {
+      messageLine += this.getMessageElement(line, resultMap, level);
+      if (!this.isLastLine(level, resultMap[line].length)) {
         messageLine += OUTPUT_MESSAGE.LINE;
       }
     }
     return messageLine + OUTPUT_MESSAGE.END;
   },
 
-  getMessageElement(line, resultMap, levelCnt) {
-    if (resultMap[line][levelCnt] === true) return OUTPUT_MESSAGE.CORRECT;
-    if (resultMap[line][levelCnt] === false) return OUTPUT_MESSAGE.INCORRECT;
+  /**
+   *
+   * @param {number} line
+   * @param {boolean|undefined[]} resultMap
+   * @param {number} level
+   * @returns
+   */
+  getMessageElement(line, resultMap, level) {
+    if (resultMap[line][level] === true) return OUTPUT_MESSAGE.CORRECT;
+    if (resultMap[line][level] === false) return OUTPUT_MESSAGE.INCORRECT;
     return OUTPUT_MESSAGE.EMPTY;
   },
 
-  isLastLine(levelCnt, resultMapLength) {
-    return levelCnt === resultMapLength - 1;
+  /**
+   *
+   * @param {number} level
+   * @param {number} resultMapLength
+   * @returns {boolean}
+   */
+  isLastLine(level, resultMapLength) {
+    return level === resultMapLength - 1;
   },
+
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+   * @param {BridgeGame} game
    */
   printResult(game) {
     Console.print(OUTPUT_MESSAGE.GAME_RESULT);
