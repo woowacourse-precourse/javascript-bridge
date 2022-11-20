8e2cd61debb73b212a95ce211c6ae64edde7b7ff
Sangjun-man <molung@icloud.com>
2022-11-20 16:57:59 +0900

    refactor(utils) : 유틸 함수 디렉토리 위치 변경
    src/utils안의 유틸함수를 src/utils/func으로 위치 변경했습니다.

--- src/utils/func/getVarType.js
+++ src/utils/func/getVarType.js
@@ -1,4 +1,4 @@
-const varTypeConst = require("./const/varType");
+const varTypeConst = require("../const/varType");
 function getVarType(param) {
   return Array.isArray(param) ? varTypeConst.array : typeof param;
 }
