4ff0f24c27721830ef0817bfc1da06f0a9db20f6
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2022-11-21 00:59:15 +0900

    feat : 게임 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
+
   play() {
+   this.gameStart()
+  }
+
+  gameStart() {
     MissionUtils.Console.print('다리 건너기 게임을 시작합니다.');
   }
 }
