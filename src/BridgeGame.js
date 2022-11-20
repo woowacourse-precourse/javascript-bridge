43bd03fafbe3db89b1cff4698a733733a0a5e920
WaiNaat <qoli0703@gmail.com>
2022-11-20 20:34:15 +0900

    feat(BridgeGame): add move feature
    움직임 결과에 따라서 게임 성공, 움직임 성공, 게임 실패 결과 반환

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,3 +1,5 @@
+const { GameConfig } = require('./Config');
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
@@ -16,7 +18,17 @@ class BridgeGame {
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(direction) {
+    const result = this.#bridge.moveSuccess(this.#position, direction);
+    if (result) this.#position += 1;
+    return this.gameStatus(result);
+  }
+
+  gameStatus(lastMoveResult) {
+    if (this.#position === this.#bridge.length()) return GameConfig.STATUS_SUCCESS;
+    if (!lastMoveResult) return GameConfig.STATUS_FAIL;
+    return GameConfig.STATUS_PLAY;
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
