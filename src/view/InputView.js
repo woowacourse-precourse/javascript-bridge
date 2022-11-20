09d47c0e59f68a7d7c1f4a280f0093c749ab3111
seongminn <berry9901@gmail.com>
2022-11-21 01:06:31 +0900

    fix(InputView): 테스트 코드 수행을 위해 코드 단순화(추후 리팩토링 예정)

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,4 +1,5 @@
 const { INPUT_MESSAGE } = require('../utils/constant');
+const { Console } = require('@woowacourse/mission-utils');
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -8,44 +9,21 @@ const InputView = {
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize(callback) {
-    this.getInput(
-      INPUT_MESSAGE.BRIDGE_LENGTH,
-      callback,
-      this.readBridgeSize(callback).bind(this)
-    );
+    Console.readLine(INPUT_MESSAGE.BRIDGE_LENGTH, callback);
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving(callback) {
-    this.getInput(
-      INPUT_MESSAGE.MOVING,
-      callback,
-      this.readMoving(callback).bind(this)
-    );
+    Console.readLine(INPUT_MESSAGE.MOVING, callback);
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand(callback) {
-    this.getInput(
-      INPUT_MESSAGE.RETRY,
-      callback,
-      this.readGameCommand(callback).bind(this)
-    );
-  },
-
-  getInput(message, callback, reDo) {
-    Console.readLine(message, (input) => {
-      try {
-        callback(input);
-      } catch (err) {
-        Console.print(err.message);
-        reDo(callback);
-      }
-    });
+    Console.readLine(INPUT_MESSAGE.RETRY, callback);
   },
 };
 
