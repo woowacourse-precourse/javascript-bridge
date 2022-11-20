4f5d89c64285a8716c6a6acec686276f40681261
c0c0pang <coc1789@naver.com>
2022-11-20 18:38:11 +0900

    feat: 사용자가 이동할 칸을 입력받는 기능 구현

--- src/InputView.js
+++ src/InputView.js
@@ -17,7 +17,14 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+   move: "",
+  readMoving() {
+    MissionUtils.Console.readLine(
+      "이동할 칸을 선택해주세요. (위: U, 아래: D)\n",(move)=>{
+        this.move = move;
+      }
+    );
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
