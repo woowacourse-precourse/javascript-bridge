cf109c9c7ce2219653fd0cd81d6bff1167633b10
vjvl95 <vjvl95@naver.com>
2022-11-20 21:27:00 +0900

    fix : OutputView 주석삭제

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,29 +1,17 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const { OUTPUT_MESSAGES } = require('./common/messages');
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
 const OutputView = {
-  /**
-   * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-   * <p>
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
   printMap(uparray, downarray) {
     MissionUtils.Console.print('[ ' + uparray.join(' | ') + ' ]');
     MissionUtils.Console.print('[ ' + downarray.join(' | ') + ' ]');
   },
 
-  /**
-   * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-   * <p>
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
   printResult(result) {
-    const [uparray, downarray, count] = result;
-    this.printMap(uparray, downarray);
-    MissionUtils.Console.print(OUTPUT_MESSAGES.OUTPUT_FINALCOUNT(count));
+    const [Player, Bridge] = result;
+
+    this.printMap(Bridge.getUpBridge(), Bridge.getDownBridge());
+    MissionUtils.Console.print(OUTPUT_MESSAGES.OUTPUT_FINALCOUNT(Player.getCount()));
   },
 
   StartGame() {
