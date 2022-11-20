d8931ee7c1fac6800f6bcfbee414fa8f1643da1e
turtle601 <turtle601@naver.com>
2022-11-20 14:22:34 +0900

    refactor(InputView) Controller 싱글톤 객체 적용
    Add:
    - InputView 각 함수마다 데이터를 저장하는 controller 로직을 추가

--- src/InputView.js
+++ src/InputView.js
@@ -4,6 +4,8 @@ const BridgeLengthException = require('./controller/validate/BridgeLengthExcepti
 const BridgeUpDownException = require('./controller/validate/BridgeUpDownException');
 const BridgeRestartExitException = require('./controller/validate/BridgeRestartExitException');
 
+const controller = require('./controller/BridgeController');
+
 const InputView = {
   isValidate(exceptInstance) {
     return exceptInstance.isValidate();
@@ -13,12 +15,16 @@ const InputView = {
     if (!InputView.isValidate(new BridgeLengthException(input))) {
       Console.close();
     }
+
+    controller.inputBridgeLength(input);
   },
 
   readMoving(input) {
     if (!InputView.isValidate(new BridgeUpDownException(input))) {
       Console.close();
     }
+
+    controller.inputBridgeUpDown(input);
   },
 
   readGameCommand(input) {
