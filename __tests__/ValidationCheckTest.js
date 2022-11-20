f263e9720086c3c2b3513ae04fc9f6cf41df062d
Sangjun-man <molung@icloud.com>
2022-11-20 16:47:05 +0900

    feat(ValidationCheck) : 숫자 타입 확인해주는 기능 추가
    값 입력 시 문자열이 공백으로 입력되는 경우를 체크해주기 위해, 숫자 타입을 체크해주는 기능을 추가했습니다.

--- __tests__/ValidationCheckTest.js
+++ __tests__/ValidationCheckTest.js
@@ -3,24 +3,51 @@ const varType = require("../src/utils/const/varType");
 
 const Validate = new ValidationCheck();
 
-describe("1. 값의 타입이 정확한가? ", () => {
-  const testCase_1_fail = [
+describe("1. 숫자 타입을 정확히 확인해 주는가?", () => {
+  const testCase_fail = [
+    { testId: "1-1", param: "" },
+    { testId: "1-2", param: "12_23" },
+    { testId: "1-3", param: [] },
+    { testId: "1-4", param: null },
+    { testId: "1-5", param: "12 3" },
+  ];
+  test.each(testCase_fail)(
+    "$testId. 숫자값을 받지 않았을 때, $param",
+    ({ param }) => {
+      expect(() => Validate.isNumber(param)).toThrow();
+    }
+  );
+  const testCase_pass = [
+    { testId: "1-4", param: 123 },
+    { testId: "1-5", param: "123" },
+    { testId: "1-6", param: " 123 " },
+  ];
+  test.each(testCase_pass)(
+    "$testId. 숫자값을 받았을 때, $param",
+    ({ param }) => {
+      expect(Validate.isNumber(param)).toBe(true);
+    }
+  );
+});
+
+describe("2. 값의 타입을 정확히 확인해 주는가? ", () => {
+  const testCase_fail = [
     { testId: "1-1", param: [varType.string, 1] },
     { testId: "1-2", param: [varType.array, { a: 1, b: 2 }] },
     { testId: "1-3", param: [varType.number, "hello"] },
   ];
-  test.each(testCase_1_fail)(
+  test.each(testCase_fail)(
     "$testId. 값의 타입이 정확하지 않을 때, $param",
     ({ param }) => {
       expect(() => Validate.isExactVarType(...param)).toThrow();
     }
   );
 
-  const testCase_1_pass = [
+  const testCase_pass = [
     { testId: "1-4", param: [varType.string, "thisIsString"] },
     { testId: "1-5", param: [varType.array, [1, 2, 3]] },
   ];
-  test.each(testCase_1_pass)(
+  test.each(testCase_pass)(
     "$testId. 값의 타입이 정확할 때, $param",
     ({ param }) => {
       expect(Validate.isExactVarType(...param)).toBe(true);
@@ -28,23 +55,23 @@ describe("1. 값의 타입이 정확한가? ", () => {
   );
 });
 
-describe("2. 숫자가 해당 범위안에 있는 값인가?", () => {
-  const testCase_2_fail = [
+describe("3. 숫자가 해당 범위안에 있지 잘 확인해 주는가?", () => {
+  const testCase_fail = [
     { testId: "2-1", param: [[3, 20], 2] },
     { testId: "2-2", param: [[3, 20], 201] },
   ];
-  test.each(testCase_2_fail)(
+  test.each(testCase_fail)(
     "$testId. 숫자가 해당 범위 안에 없을 때, $param",
     ({ param }) => {
       expect(() => Validate.isNumberIntheRange(...param)).toThrow();
     }
   );
 
-  const testCase_2_pass = [
+  const testCase_pass = [
     { testId: "2-3", param: [[2, 30], 10] },
     { testId: "2-4", param: [[3, 20], 20] },
   ];
-  test.each(testCase_2_pass)(
+  test.each(testCase_pass)(
     "$testId. 숫자가 해당 범위 안에 들어있을 때, $param",
     ({ param }) => {
       expect(Validate.isNumberIntheRange(...param)).toBe(true);
@@ -52,20 +79,20 @@ describe("2. 숫자가 해당 범위안에 있는 값인가?", () => {
   );
 });
 
-describe("3. 문자열이 해당 목록 안에 들어있는 값인가?,", () => {
-  const testCase_3_fail = [
+describe("4. 문자열이 해당 목록 안에 들어있는지 잘 확인해 주는가?,", () => {
+  const testCase_fail = [
     { testId: "3-1", param: [["U", "D"], "d"] },
     { testId: "3-2", param: [["U", "D"], "F"] },
   ];
 
-  test.each(testCase_3_fail)(
+  test.each(testCase_fail)(
     "$testId 문자열이 해당 목록 안에 들어있지 않을 때 , $param",
     ({ param }) => {
       expect(() => Validate.isStringIntheList(...param)).toThrow();
     }
   );
-  const testCase_3_pass = [{ testId: "3-3", param: [["U", "D"], "U"] }];
-  test.each(testCase_3_pass)(
+  const testCase_pass = [{ testId: "3-3", param: [["U", "D"], "U"] }];
+  test.each(testCase_pass)(
     "$testId 문자열이 해당 목록 안에 들어있을 때 , $param",
     ({ param }) => {
       expect(Validate.isStringIntheList(...param)).toBe(true);
@@ -73,8 +100,8 @@ describe("3. 문자열이 해당 목록 안에 들어있는 값인가?,", () => 
   );
 });
 
-describe("4. 객체안의 프로퍼티 목록과, 프로퍼티 밸류의 값이 정확한가?", () => {
-  const testCase_4_fail_notCorrectProperty = [
+describe("5. 객체안의 프로퍼티 목록과, 프로퍼티 밸류의 값이 정확한지 잘 확인해 주는가?", () => {
+  const testCase_fail_notCorrectProperty = [
     {
       testId: "4-1",
       param: [
@@ -91,14 +118,14 @@ describe("4. 객체안의 프로퍼티 목록과, 프로퍼티 밸류의 값이 
     },
   ];
 
-  test.each(testCase_4_fail_notCorrectProperty)(
+  test.each(testCase_fail_notCorrectProperty)(
     "$testId 객체안의 프로퍼티 목록이 일치 하지 않을 때, $param",
     ({ param }) => {
       expect(() => Validate.isExactObjectStructure(...param)).toThrow();
     }
   );
 
-  const testCase_4_fail_notSameType = [
+  const testCase_fail_notSameType = [
     {
       testId: "4-3",
       param: [
@@ -114,14 +141,14 @@ describe("4. 객체안의 프로퍼티 목록과, 프로퍼티 밸류의 값이 
       ],
     },
   ];
-  test.each(testCase_4_fail_notSameType)(
+  test.each(testCase_fail_notSameType)(
     "$testId  객체안의 프로퍼티 목록은 일치하지만, 값의 타입이 다를 때, $param",
     ({ param }) => {
       expect(() => Validate.isExactObjectStructure(...param)).toThrow();
     }
   );
 
-  const testCase_4_pass = [
+  const testCase_pass = [
     {
       testId: "4-5",
       param: [
@@ -130,7 +157,7 @@ describe("4. 객체안의 프로퍼티 목록과, 프로퍼티 밸류의 값이 
       ],
     },
   ];
-  test.each(testCase_4_pass)(
+  test.each(testCase_pass)(
     "$testId 객체안의 프로퍼티 목록과 값의 타입이 모두 일치할 때 ,$param",
     ({ param }) => {
       expect(Validate.isExactObjectStructure(...param)).toBe(true);
--- src/VaildationCheck.js
+++ src/VaildationCheck.js
@@ -1,62 +1,70 @@
 const ErrorWithPrifix = require("./CustomError");
 const varTypeConst = require("./utils/const/varType");
 const ErrorMessage = require("./utils/const/errorMessage");
-const getVarType = require("./utils/getVarType");
+const getVarType = require("./utils/func/getVarType");
 
 class ValidationCheck {
+  isNumber(target) {
+    if (!(typeof target === "number" || typeof target === "string"))
+      throw new ErrorWithPrifix(ErrorMessage.notNumber);
+    if (target === "") throw new ErrorWithPrifix(ErrorMessage.notNumber);
+    if (Number.isNaN(Number(target)))
+      throw new ErrorWithPrifix(ErrorMessage.notNumber);
+    return true;
+  }
   isExactVarType(varType, target) {
     if (typeof varType !== "string")
-      throw ErrorWithPrifix(ErrorMessage.varTypeIsNotString);
+      throw new ErrorWithPrifix(ErrorMessage.varTypeIsNotString);
 
     const targetVarType = Array.isArray(target)
       ? varTypeConst.array
       : typeof target;
 
     if (varType !== targetVarType)
-      throw ErrorWithPrifix(ErrorMessage.varTypeIsNotExactForTarget);
+      throw new ErrorWithPrifix(ErrorMessage.varTypeIsNotExactForTarget);
 
     return true;
   }
 
   isNumberIntheRange(numRange, targetNumber) {
     if (typeof targetNumber !== "number")
-      throw ErrorWithPrifix(ErrorMessage.notNumber);
+      throw new ErrorWithPrifix(ErrorMessage.notNumber);
     if (!Array.isArray(numRange) || numRange.length !== 2)
-      throw ErrorWithPrifix(ErrorMessage.numRangeIsNotNumberArray);
+      throw new ErrorWithPrifix(ErrorMessage.numRangeIsNotNumberArray);
     if (typeof numRange[0] !== "number" || typeof numRange[1] !== "number")
-      throw ErrorWithPrifix(ErrorMessage.notNumber);
+      throw new ErrorWithPrifix(ErrorMessage.notNumber);
     if (numRange[0] > numRange[1])
-      throw ErrorWithPrifix(ErrorMessage.notCorrectRange);
+      throw new ErrorWithPrifix(ErrorMessage.notCorrectRange);
     if (targetNumber < numRange[0] || targetNumber > numRange[1])
-      throw ErrorWithPrifix(ErrorMessage.notInRange);
+      throw new ErrorWithPrifix(ErrorMessage.notInRange);
     return true;
   }
 
   isStringIntheList(stringList, targetString) {
     if (!Array.isArray(stringList))
-      throw ErrorWithPrifix(ErrorMessage.notArray);
+      throw new ErrorWithPrifix(ErrorMessage.notArray);
     if (typeof targetString !== "string")
-      throw ErrorWithPrifix(ErrorMessage.notString);
+      throw new ErrorWithPrifix(ErrorMessage.notString);
     for (let item of stringList) {
       if (typeof item !== "string")
-        throw ErrorWithPrifix(ErrorMessage.notString);
+        throw new ErrorWithPrifix(ErrorMessage.notString);
       if (item === targetString) return true;
     }
 
-    throw ErrorWithPrifix(ErrorMessage.stringIsNotInList);
+    throw new ErrorWithPrifix(ErrorMessage.stringIsNotInList);
   }
   isExactObjectStructure(object, targetObject) {
     const creteriaKeys = Object.keys(object);
     const compareKeys = Object.keys(targetObject);
 
     if (creteriaKeys.length !== compareKeys.length)
-      throw ErrorWithPrifix(ErrorMessage.notSamePropertyCounts);
+      throw new ErrorWithPrifix(ErrorMessage.notSamePropertyCounts);
 
     Object.keys(object).forEach((keys) => {
       if (!targetObject.hasOwnProperty(keys))
-        throw ErrorWithPrifix(ErrorMessage.notHasProperty);
+        throw new ErrorWithPrifix(ErrorMessage.notHasProperty);
       if (getVarType(object[keys]) !== getVarType(targetObject[keys]))
-        throw ErrorWithPrifix(ErrorMessage.varTypeIsNotExactForTarget);
+        throw new ErrorWithPrifix(ErrorMessage.varTypeIsNotExactForTarget);
     });
 
     return true;
