16370f0e4d1f2f4f83562f7af1578d46ef68ff0a
sohyun <park.sohyun0430@gmail.com>
2022-11-20 23:59:51 +0900

    feat: 게임 시작 문구 출력
    outputView에 printGuide(message) 함수 구현 : 매개변수로 받은 메시지를 출력
    App.js play 함수 안에 게임 시작 문구 출력
    constants.js 파일 : 출력문구,입력 문구 등 상수 저장하는 파일

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+const OutputView = require('./OutputView');
+const { START_GAME_MSG } = require('./constants');
+
 class App {
-  play() {}
+  play() {
+    OutputView.printGuide(START_GAME_MSG);
+  }
 }
 
+const app = new App();
+app.play();
 module.exports = App;
---
+++ src/constants.js
@@ -0,0 +1,5 @@
+const START_GAME_MSG = '다리 건너기 게임을 시작합니다.\n';
+
+module.exports = {
+  START_GAME_MSG,
+};
