ee975551b21a9f670d44ace713e555ab2a28d0c4
wjs5025 <wjs_5025@naver.com>
2022-11-21 02:26:41 +0900

    feat : readMoving() 구현 (플레이어 이동)

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,3 +1,4 @@
+const { readMoving } = require("./views/InputView");
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  * 주의 : 변경 없이 그대로 사용할 것.
@@ -8,11 +9,12 @@ const BridgeMaker = {
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
-  makeBridge(size, generateRandomNumber) {
+  makeBridge(size, generateRandomNumber, readMoving, game) {
     let bridge = [];
     while (bridge.length < size) {
       generateRandomNumber() ? bridge.push("U") : bridge.push("D");
     }
+    return readMoving(game, bridge);
   },
 };
 
--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,27 +1,37 @@
 const { Console } = require("@woowacourse/mission-utils");
-const { makeBridge } = require("../BridgeMaker");
 const { MESSAGES } = require("../constraints/constarints");
 const { generate } = require("../utils/BridgeRandomNumberGenerator");
+const { makeBridge } = require("../BridgeMaker");
 const { validateLength } = require("../utils/validators/validators");
+const { printResult, printMap } = require("./OutputView");
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
+
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {
-    Console.readLine(MESSAGES.READ_BRIDGE_SIZE, (bridgeSize) => {
-      Console.close();
-      if (validateLength(bridgeSize))
-        Console.print(makeBridge(bridgeSize, generate));
+  readBridgeSize(game, readMoving) {
+    Console.readLine(MESSAGES.READ_BRIDGE_SIZE, (res) => {
+      if (validateLength(res)) {
+        return makeBridge(res, generate, readMoving, game);
+      }
     });
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving(game, bridge) {
+    console.log("다리", bridge); // 나중에 지울것
+    if (game.done) return printResult(game);
+    Console.readLine(MESSAGES.MOVE, (input) => {
+      game.move(input, bridge);
+      printMap(game, bridge);
+      InputView.readMoving(game, bridge);
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
