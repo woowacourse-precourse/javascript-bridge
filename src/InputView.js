fc819027e2aec412b45944631e919170a8fd54ec
eunsung <esk114477@gmail.com>
2022-11-21 00:47:02 +0900

    feat : 이동할 칸 선택 기능 추가

--- src/InputView.js
+++ src/InputView.js
@@ -1,4 +1,6 @@
 const Console = require("@woowacourse/mission-utils");
+const {BridgeMaker, makeBridge} = require("./BridgeMaker.js");
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator.js");
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -9,14 +11,19 @@ const InputView = {
    */
   readBridgeSize(bridgeGame) {
     Console.readLine("다리의 길이를 입력해주세요.\n", (length) => {
-      
+      bridgeGame.init(makeBridge(length,BridgeRandomNumberGenerator.generate()));
+      this.readMoving(bridgeGame);
     })
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving(bridgeGame) {
+    Console.readLine("\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n", (str) => {
+      if(["U","D"].includes(str)) bridgeGame.move(str);
+    })
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
