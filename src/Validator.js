be49b8d3c60ca67f15c1ca120058d5b0e2b4dbe8
Choi siun <siun0331@gmail.com>
2022-11-20 18:00:48 +0900

    feat: 예외처리 모듈 생성

---
+++ src/Validator.js
@@ -0,0 +1,30 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
+const { Console } = MissionUtils;
+const Validator = {
+  moveCommandValidator(command) {
+    if ((command !== 'U') && (command !== 'D')) {
+      Console.print(('[ERROR] 올바르지 않은 커멘드 입니다.'));
+      return false;
+    }
+    return true;
+  },
+
+  bridgeLengthValidator(length) {
+    if (!(length >= 3 && length <= 20)) {
+      Console.print('[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.');
+      return false;
+    }
+    return true;
+  },
+
+  restartCommandValidator(command) {
+    if ((command !== 'R') && (command !== 'Q')) {
+      Console.print('[ERROR] 올바르지 않은 커멘드 입니다.');
+      return false;
+    }
+    return true;
+  },
+};
+
+module.exports = Validator;
