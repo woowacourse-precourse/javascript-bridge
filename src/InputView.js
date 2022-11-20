d6e9901403a205254666249c8d209c086295f491
seoye0ng <101791501+seoye0ng@users.noreply.github.com>
2022-11-20 11:33:59 +0900

    feat : 다리 길이 입력 예외처리 구현

--- src/InputView.js
+++ src/InputView.js
@@ -3,13 +3,28 @@ const MissionUtils = require('@woowacourse/mission-utils');
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
+  MINIMUM_BRIDGE_LENGTH: 3,
+  MAXIMUM_BRIDGE_LENGTH: 20,
   /**
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize() {
     Console.readLine('다리의 길이를 입력해주세요.', (number) => {
+      this.numberValidate(number);
       return number;
     });
+  },
+  // 다리 길이 예외처리
+  numberValidate(number) {
+    if (isNaN(number)) {
+      throw new Error('[ERROR] 숫자를 입력해야 합니다.');
+    }
+    if (
+      !InputView.MINIMUM_BRIDGE_LENGTH <= number &&
+      number <= InputView.MAXIMUM_BRIDGE_LENGTH
+    ) {
+      throw new Error('[ERROR] 3 ~ 20 범위 안의 숫자를 입력해야 합니다.');
+    }
   },
 
   /**
