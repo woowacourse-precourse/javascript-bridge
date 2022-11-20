8501266deacbc233f9269fa72076f3971c9a3722
장경은 <jang_kyung_eun@gyeong-eun-ui-MacBookAir.local>
2022-11-21 00:10:59 +0900

    feat: 입력받은 다리 길이만큼 다리를 만드는 기능

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,4 +1,5 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
 const Console = MissionUtils.Console;
 const Random = MissionUtils.Random;
 /**
@@ -11,8 +12,12 @@ const BridgeMaker = {
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
   makeBridge(size, generateRandomNumber) {
-    Console.readLine('다리의 길이를 입력해주세요.\n', (size) => {
-      size = size;
+    Console.readLine('다리의 길이를 입력해주세요.\n', (num) => {
+      size = num;
+      let bridge = Array(parseInt(size));
+      for (let i = 0; i < size; i++) {
+        bridge[i] = BridgeRandomNumberGenerator.generate();
+      }
     })
   },
 };
