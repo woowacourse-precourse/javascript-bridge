d444e155e5b2093b42f50644488faba5023f68a0
wjs5025 <wjs_5025@naver.com>
2022-11-21 06:47:23 +0900

    test : 테스트 코드 추가 및 분리(정상테스트, 예외테스트)

--- __tests__/ExceptionTest.js
+++ __tests__/ExceptionTest.js
@@ -28,6 +28,16 @@ const getOutput = (logSpy) => {
   return [...logSpy.mock.calls].join("");
 };
 
+const runException = (inputs) => {
+  mockQuestions(inputs);
+  const logSpy = getLogSpy();
+  const app = new App();
+
+  app.play();
+
+  expectLogContains(getOutput(logSpy), ["[ERROR]"]);
+};
+
 const expectLogContains = (received, logs) => {
   logs.forEach((log) => {
     expect(received).toEqual(expect.stringContaining(log));
@@ -37,37 +47,11 @@ const expectLogContains = (received, logs) => {
 const expectBridgeOrder = (received, upside, downside) => {
   const upsideIndex = received.indexOf(upside);
   const downsideIndex = received.indexOf(downside);
-
   expect(upsideIndex).toBeLessThan(downsideIndex);
 };
 
-describe("다리 건너기 테스트", () => {
-  test("다리 생성 테스트", () => {
-    const randomNumbers = ["1", "0", "0"];
-    const mockGenerator = randomNumbers.reduce((acc, number) => {
-      return acc.mockReturnValueOnce(number);
-    }, jest.fn());
-
-    const bridge = BridgeMaker.makeBridge(3, mockGenerator);
-    expect(bridge).toEqual(["U", "D", "D"]);
-  });
-
-  test("기능 테스트", () => {
-    const logSpy = getLogSpy();
-    mockRandoms(["1", "0", "1"]);
-    mockQuestions(["3", "U", "D", "U"]);
-
-    const app = new App();
-    app.play();
-
-    const log = getOutput(logSpy);
-    expectLogContains(log, [
-      "최종 게임 결과",
-      "[ O |   | O ]",
-      "[   | O |   ]",
-      "게임 성공 여부: 성공",
-      "총 시도한 횟수: 1",
-    ]);
-    expectBridgeOrder(log, "[ O |   | O ]", "[   | O |   ]");
+describe("예외 테스트", () => {
+  test("예외 테스트 1", () => {
+    runException(["a"]);
   });
 });
