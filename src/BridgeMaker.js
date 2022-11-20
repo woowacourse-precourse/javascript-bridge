faeb20d372e3dad3fbce22bf85642504b7c219a5
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2022-11-21 01:33:39 +0900

    feat : BridgeMaker 기능 추가

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -7,7 +7,20 @@ const BridgeMaker = {
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
-  makeBridge(size, generateRandomNumber) {},
+  makeBridge(size, generateRandomNumber) {
+    let bridge = []
+    for(i = 0; i < size; i++) {
+      const number = generateRandomNumber(i);
+      if(number === '0') {
+        bridge.push('D');
+      } 
+      if(number === '1') {
+        bridge.push('U');
+      };
+    }
+    console.log(bridge)
+    return bridge
+  },
 };
 
 module.exports = BridgeMaker;
