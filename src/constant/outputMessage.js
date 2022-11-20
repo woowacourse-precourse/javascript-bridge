c21892c2ac65a7efe2313ab1550199b8a332032a
Hchan2 <seongchan_@naver.com>
2022-11-20 21:53:49 +0900

    불필요한 상수 제거

--- src/constant/outputMessage.js
+++ src/constant/outputMessage.js
@@ -1,13 +1,9 @@
 const GAME_RESULT = (result) => { return `게임 성공 여부: ${result}` };
 const TOTAL_ATTEMPT = (attemptNumber) => { return `총 시도한 횟수: ${attemptNumber}` };
 const BRIDGE_START = '다리 건너기 게임을 시작합니다.';
-const SUCCESS_GAME = '게임 성공 여부: 성공'
-const FAIL_GAME = '게임 성공 여부: 실패'
 
 module.exports = {
     GAME_RESULT,
     TOTAL_ATTEMPT,
-    BRIDGE_START,
-    SUCCESS_GAME,
-    FAIL_GAME,
+    BRIDGE_START
 }
