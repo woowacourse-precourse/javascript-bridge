0d3a16d2c854e07d36c01e13aa3003a1e8562ddf
Jaehyeon <jaehyeonp74@gmail.com>
2022-11-21 05:30:42 +0900

    feat: InputView의 readMoving 메소드 구현
    - 재귀를 이용해서 model의 다리길이만큼 시행

--- src/InputView.js
+++ src/InputView.js
@@ -1,4 +1,5 @@
 const { Console } = require('@woowacourse/mission-utils');
+const OutputView = require('./OutputView');
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -13,7 +14,15 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving(callback, model, index) {
+    const bridgeSize = model.getBridgeSize();
+    if (index === bridgeSize) return OutputView.printResult();
+    Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D) \n', (input) => {
+      callback(input, index);
+      return this.readMoving(callback, model, index + 1);
+    });
+    return true;
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
