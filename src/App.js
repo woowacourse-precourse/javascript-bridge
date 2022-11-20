f52e9eef63d184a09e23d330c2e21b30df6b2636
sohyun <park.sohyun0430@gmail.com>
2022-11-21 01:05:26 +0900

    feat: isValidBridgeSize 함수 구현
    입력받은 다리 길이 유효한 값인지 확인

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+const { Console } = require('@woowacourse/mission-utils');
 const OutputView = require('./OutputView');
-const { START_GAME_MSG } = require('./constants');
+const InputView = require('./InputView');
+const {
+  START_GAME_MSG,
+  BRIDGE_SIZE_ERROR,
+  MIN_BRIDGE_SIZE,
+  MAX_BRIDGE_SIZE,
+} = require('./constants');
 
 class App {
   play() {
     OutputView.printGuide(START_GAME_MSG);
+  }
+
+  isValidBridgeSize(bridgeSize) {
+    if (
+      isNaN(bridgeSize) ||
+      bridgeSize < MIN_BRIDGE_SIZE ||
+      bridgeSize > MAX_BRIDGE_SIZE ||
+      bridgeSize === ''
+    ) {
+      throw new Error(BRIDGE_SIZE_ERROR);
+    }
   }
 }
 
