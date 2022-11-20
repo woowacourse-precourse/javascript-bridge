482472af18d71128e75c860f1bc057f48e573ed1
Sangmin Lee <gink.ss00@gmail.com>
2022-11-20 13:45:04 +0900

    feat: 입력 확인값 상수화
    확인 하는 값을 하드코딩하지 않기 위해 상수화 하였다.

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -1,7 +1,7 @@
 const MESSAGE = {
   START: '다리 건너기 게임을 시작합니다.\n',
   INPUT_LENGTH: '다리의 길이를 입력해주세요.\n',
-  INPUT_UP_DOWN: '이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
+  INPUT_UP_DOWN: '\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
   INPUT_CHECK_TRY: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
   FINAL_RESULT: '최종 게임 결과\n',
   SUCCESS_OR_FAIL: '\n게임 성공 여부: ',
@@ -25,6 +25,10 @@ const BRIDGE = {
   START: '[ ',
   SEPARATE: ' | ',
   END: ' ]',
+  NUMBER_UP: 1,
+  NUMBER_DOWN: 0,
+  STRING_UP: 'U',
+  STRING_DOWN: 'D',
 };
 
 const REGEXP = {
