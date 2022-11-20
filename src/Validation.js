d8acce271803e4b54f09d733d8e756c7aa6841e8
seunghoria <102285042+seunghoria@users.noreply.github.com>
2022-11-20 23:29:45 +0900

    feat: 다리 이동 입력값 유효성 검증 기능 구현

--- src/Validation.js
+++ src/Validation.js
@@ -3,5 +3,9 @@ class Validation {
     if (size < 3 || size > 20) throw Error;
     if (isNaN(size)) throw Error;
   }
+
+  static checkDirectionInput(direction) {
+    if (direction === "U" || direction === "D") throw new Error();
+  }
 }
 module.exports = Validation;
