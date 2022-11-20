49ec7120ca8fa5055c9579f8c05aff47f5bf090a
Rki0 <pky00823@gmail.com>
2022-11-20 20:20:15 +0900

    refactor(ValidationTest): 테스트 통합
    하나의 클래스가 여러 입력값에 대한 테스트를 진행해야하므로,
    반복적으로 코드를 작성하는 것을 지양하고, 하나의 테스트에 통합한다.

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -5,11 +5,14 @@ const {
 } = require("../src/Validation");
 
 describe("제한 조건 검증 테스트", () => {
-  test("다리 길이 입력시 유효성 검사 진행 여부 테스트", () => {
-    expect(() => {
-      new BridgeSizeValidation("3 ");
-    }).toThrow("[ERROR] 숫자만 입력 가능합니다.");
-  });
+  test.each(["3 ", "1", "04"])(
+    "다리 길이 입력시 유효성 검사 진행 여부 테스트",
+    (input) => {
+      expect((input) => {
+        new BridgeSizeValidation(input);
+      }).toThrow();
+    }
+  );
 
   test("움직임 입력값 유효성 검사 진행 여부 테스트", () => {
     expect(() => {
