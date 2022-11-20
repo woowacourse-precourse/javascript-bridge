2944e7a985b3db91290d033d74a41eb57bef6eff
Suyoung <jsy8492@gmail.com>
2022-11-20 20:09:18 +0900

    refactor(BridgeSizeValidator): 다리 길이에 대한 입력값의 유효성 검사 기능을 별도 객체로 분리

---
+++ src/BridgeSizeValidator.js
@@ -0,0 +1,47 @@
+/**
+ * 사용자가 입력한 다리 길이의 유효성을 검사하는 역할을 한다.
+ */
+const BridgeSizeValidator = {
+  MIN_BRIDGE_SIZE: 3,
+  MAX_BRIDGE_SIZE: 20,
+
+  /**
+   * 사용자가 입력한 다리 길이의 값에 대한 유효성을 검사하는 메서드
+   * @param {number} bridgeSize
+   * @throws 숫자가 아닌 문자가 포함된 경우 error를 throw한다.
+   * @throws 다리 길이 범위 이외의 값인 경우 error를 throw한다.
+   */
+  validateBridgeSize(bridgeSizeInput) {
+    if (this.isNotNumber(bridgeSizeInput)) {
+      throw new Error('[ERROR] 숫자가 아닌 문자가 입력되었습니다.');
+    }
+
+    if (this.isOutOfBound(parseInt(bridgeSizeInput, 10))) {
+      throw new Error(
+        `[ERROR] 다리 길이는 ${this.MIN_BRIDGE_SIZE} 이상 ${this.MAX_BRIDGE_SIZE} 이하의 숫자여야 합니다.`,
+      );
+    }
+  },
+
+  /**
+   * 사용자가 입력한 값에 숫자가 아닌 문자가 포함되는지 판단하는 메서드
+   * @param {string} inputValue 사용자로부터 입력받은 값
+   * @returns {boolean} 숫자가 아닌 문자가 포함된 경우 true, 아니면 false를 반환한다.
+   */
+  isNotNumber(inputValue) {
+    return !/^\d+$/.test(inputValue);
+  },
+
+  /**
+   * 사용자가 입력한 값이 다리 길이 범위 이외의 값인지 판단하는 메서드
+   * @param {number} bridgeSize 사용자로부터 입력받은 다리의 길이
+   * @returns {boolean} 다리 길이 범위 이외의 값이면 true, 아니면 false를 반환한다.
+   */
+  isOutOfBound(bridgeSize) {
+    return (
+      bridgeSize < this.MIN_BRIDGE_SIZE || bridgeSize > this.MAX_BRIDGE_SIZE
+    );
+  },
+};
+
+module.exports = BridgeSizeValidator;
--- src/BridgeValidator.js
+++ src/BridgeValidator.js
@@ -2,47 +2,6 @@
  * 다리 건너기 게임 진행을 위해 입력받은 값의 유효성을 검사하는 역할을 한다.
  */
 const BridgeValidator = {
-  MIN_BRIDGE_SIZE: 3,
-  MAX_BRIDGE_SIZE: 20,
-
-  /**
-   * 사용자가 입력한 다리 길이의 값에 대한 유효성을 검사하는 메서드
-   * @param {number} bridgeSize
-   * @throws 숫자가 아닌 문자가 포함된 경우 error를 throw한다.
-   * @throws 다리 길이 범위 이외의 값인 경우 error를 throw한다.
-   */
-  validateBridgeSize(bridgeSizeInput) {
-    if (this.isNotNumber(bridgeSizeInput)) {
-      throw new Error(
-        '[ERROR] 다리 길이를 위한 값에 숫자가 아닌 문자가 입력되었습니다.',
-      );
-    }
-
-    if (this.isOutOfBound(parseInt(bridgeSizeInput, 10))) {
-      throw new Error('[ERROR] 다리 길이는 3 이상 20 이하의 숫자여야 합니다.');
-    }
-  },
-
-  /**
-   * 사용자가 입력한 값에 숫자가 아닌 문자가 포함되는지 판단하는 메서드
-   * @param {string} inputValue 사용자로부터 입력받은 값
-   * @returns {boolean} 숫자가 아닌 문자가 포함된 경우 true, 아니면 false를 반환한다.
-   */
-  isNotNumber(inputValue) {
-    return !/^\d+$/.test(inputValue);
-  },
-
-  /**
-   * 사용자가 입력한 값이 다리 길이 범위 이외의 값인지 판단하는 메서드
-   * @param {number} bridgeSize 사용자로부터 입력받은 다리의 길이
-   * @returns {boolean} 다리 길이 범위 이외의 값이면 true, 아니면 false를 반환한다.
-   */
-  isOutOfBound(bridgeSize) {
-    return (
-      bridgeSize < this.MIN_BRIDGE_SIZE || bridgeSize > this.MAX_BRIDGE_SIZE
-    );
-  },
-
   /**
    * 사용자가 입력한 이동할 칸에 대한 유효성을 검사하는 메서드
    * @param {string} moving
