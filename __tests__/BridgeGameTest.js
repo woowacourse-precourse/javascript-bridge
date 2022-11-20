c35c5f0d514ddec98885cae2af5252501e9c360d
rovxxmjxx <miju3545@gmail.com>
2022-11-20 20:27:19 +0900

    test(BridgeGameTest): + 테스트 코드 추가

---
+++ __tests__/BridgeGameTest.js
@@ -0,0 +1,62 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const BridgeGame = require('../src/BridgeGame');
+const { MESSAGE, ERROR } = require('../src/constants');
+
+const mockQuestions = (answers) => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce((acc, input) => {
+    return acc.mockImplementationOnce((_, callback) => {
+      callback(input);
+    });
+  }, MissionUtils.Console.readLine);
+};
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
+
+const getOutput = (logSpy) => {
+  return [...logSpy.mock.calls].join('');
+};
+
+const runException = (inputs) => {
+  mockQuestions(inputs);
+  const logSpy = getLogSpy();
+  const game = new BridgeGame();
+};
+
+const expectLogContains = (received, logs) => {
+  logs.forEach((log) => {
+    expect(received).toEqual(expect.stringContaining(log));
+  });
+};
+
+const expectBridgeOrder = (received, upside, downside) => {
+  const upsideIndex = received.indexOf(upside);
+  const downsideIndex = received.indexOf(downside);
+
+  expect(upsideIndex).toBeLessThan(downsideIndex);
+};
+
+describe('플레이어 입력값의 유효성 검사', () => {
+  test('게임 시작 메시지를 출력한다.', () => {
+    const logSpy = getLogSpy();
+
+    const game = new BridgeGame();
+    game.proceed();
+
+    const log = getOutput(logSpy);
+    game.proceed().then(() => {
+      expectLogContains(log, [MESSAGE.ENTRY]);
+    });
+  });
+});
