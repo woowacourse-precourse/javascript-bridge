207949ad10ee5f9f3f8b544384ba04a139f199b1
wonkyung <mwk0725@gmail.com>
2022-11-20 16:40:59 +0900

    feat: 다리 길이 입력 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 const { Console } = require('@woowacourse/mission-utils');
 
+const InputView = require('./InputView');
+const BridgeMaker = require('./BridgeMaker');
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
+
 class App {
   play() {
     this.startGame();
@@ -7,6 +11,19 @@ class App {
 
   startGame() {
     Console.print('다리 건너기 게임을 시작합니다.');
+    this.getBridgeSize();
+  }
+
+  getBridgeSize() {
+    InputView.readBridgeSize(this.createBridge.bind(this));
+  }
+
+  createBridge(size) {
+    const bridge = BridgeMaker.makeBridge(
+      size,
+      BridgeRandomNumberGenerator.generate,
+    );
+    this.bridgeGame.setBridge(bridge);
   }
 }
 
