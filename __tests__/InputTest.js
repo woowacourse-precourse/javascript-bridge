62952c56002bc541cc38316f44f162cd8a6ec49c
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2022-11-20 10:01:43 +0900

    test: 다리 길이 입력 테스트

---
+++ __tests__/InputTest.js
@@ -0,0 +1,55 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const InputView = require("../src/InputView");
+
+const mockQuestions = (answers) => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce((acc, input) => {
+    return acc.mockImplementationOnce((_, callback) => {
+      callback(input);
+    });
+  }, MissionUtils.Console.readLine);
+};
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
+const getOutput = (logSpy) => {
+  return [...logSpy.mock.calls].join("");
+};
+
+const runException = (inputs) => {
+  mockQuestions(inputs);
+  const logSpy = getLogSpy();
+
+  InputView.readBridgeSize();
+
+  expectLogContains(getOutput(logSpy), ["[ERROR]"]);
+};
+
+const expectLogContains = (received, logs) => {
+  logs.forEach((log) => {
+    expect(received).toEqual(expect.stringContaining(log));
+  });
+};
+
+describe("입력 테스트", () => {
+  test("다리 길이가 숫자가 아닌 경우 예외 처리한다.", () => {
+    runException(["d"]);
+  });
+  test("다리 길이가 3 이상 20 이하가 아닌 경우 예외 처리한다", () => {
+    runException(["2"]);
+  });
+  test("다리 길이가 3 이상 20 이하가 아닌 경우 예외 처리한다", () => {
+    runException(["21"]);
+  });
+});
