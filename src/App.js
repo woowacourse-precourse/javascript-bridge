096c2affbc5c111de121cc20aa3ce7321f6c75ff
wjs5025 <wjs_5025@naver.com>
2022-11-21 02:27:22 +0900

    feat : 게임 시작 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-const { readBridgeSize } = require("./views/InputView");
-const { generate } = require("./utils/BridgeRandomNumberGenerator");
-const { makeBridge } = require("./BridgeMaker");
-class App {
-  constructor() {}
+const { MESSAGES } = require("./constraints/constarints");
+const { Console } = require("@woowacourse/mission-utils");
+const { readBridgeSize, readMoving } = require("./views/InputView");
+const BridgeGame = require("./BridgeGame");
 
+class App {
   play() {
-    readBridgeSize();
+    // 시작 메시지 출력
+    Console.print(MESSAGES.START);
+
+    // 게임 생성
+    let game = new BridgeGame();
+
+    // 다리 생성 및 다리와 게임의 연결
+    readBridgeSize(game, readMoving);
   }
 }
 
