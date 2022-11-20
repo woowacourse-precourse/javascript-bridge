5823b650763ded830fecf3d72fc8721423d28160
turtle601 <turtle601@naver.com>
2022-11-21 04:10:01 +0900

    fix(BridgeInputTest) console -> throw로 변경

--- __tests__/BridgeInputTest.js
+++ __tests__/BridgeInputTest.js
@@ -6,26 +6,26 @@ const BridgeRestartExitException = require('../src/controller/validate/BridgeRes
 
 describe('입력한 다리 길이 테스트 - BridgeLengthException', () => {
   test.each([['a'], ['1'], ['21'], ['1 ']])('다리 길이가 3 ~ 20 이외의 숫자에 대한 예외처리', (input) => {
-    expect(InputView.isValidate(new BridgeLengthException(input))).toBeFalsy();
+    expect(() => InputView.validate(new BridgeLengthException(input))).toThrow('[ERROR]');
   })
 
   test('(다리 길이(정상) - 숫자(3 ~ 20))', () => {
     const input = '3';
 
-    expect(InputView.isValidate(new BridgeLengthException(input))).toBeTruthy();
+    expect(() => InputView.validate(new BridgeLengthException(input))).not.toThrow();
   });
 });
 
 describe('입력한 UpDownKey 테스트 - BridgeUpDownException', () => {
 
   test.each([['a'], ['1'], ['u'], ['U ']])('방향키(U, D)에 대한 예외처리', (input) => {
-    expect(InputView.isValidate(new BridgeUpDownException(input))).toBeFalsy();
+    expect(() => InputView.validate(new BridgeUpDownException(input))).toThrow('[ERROR]');
   })
 
   test('(U or D 문자 입력(정상) - "U") 입력 테스트', () => {
     const input = 'U';
 
-    expect(InputView.isValidate(new BridgeUpDownException(input))).toBeTruthy();
+    expect(() => InputView.validate(new BridgeUpDownException(input))).not.toThrow();
   });
 });
 
@@ -33,12 +33,12 @@ describe('입력한 RestartExitKey 테스트 - BridgeRestartExitException', () =
 
 
   test.each([['a'], ['1'], ['q'], ['Q ']])('재시작/종료(Q, R)에 대한 예외처리', (input) => {
-    expect(InputView.isValidate(new BridgeRestartExitException(input))).toBeFalsy();
+    expect(() => InputView.validate(new BridgeRestartExitException(input))).toThrow('[ERROR]');
   })
 
   test('(Q or R 문자 입력(정상) - "U") 입력 테스트', () => {
     const input = 'Q';
 
-    expect(InputView.isValidate(new BridgeRestartExitException(input))).toBeTruthy();
+    expect(() => InputView.validate(new BridgeRestartExitException(input))).not.toThrow();
   });
 });
