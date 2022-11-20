b0945ebeec0e55664868add86796d9d354508296
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2022-11-21 00:48:52 +0900

    feat(InputView) 에러처리부분 throw -> reject로 변경

--- src/InputView.js
+++ src/InputView.js
@@ -8,43 +8,43 @@ const InputView = {
     Console.print("다리 건너기 게임을 시작합니다.");
   },
 
-  validateBridge(bridgeInput) {
-    if (Number.isNaN(bridgeInput)) throw new Error("[ERROR] 다리길이는 숫자여야 합니다.");
-    if (!(3 <= bridgeInput && bridgeInput <= 20)) throw new Error("[ERROR] 다리길이는 3에서 20사이의 숫자여야 합니다.");
+  validateBridge(bridgeInput, reject) {
+    if (Number.isNaN(bridgeInput)) reject(new Error("[ERROR] 다리길이는 숫자여야 합니다."));
+    if (!(3 <= bridgeInput && bridgeInput <= 20)) reject(new Error("[ERROR] 다리길이는 3에서 20사이의 숫자여야 합니다."));
   },
 
   readBridgeSize() {
-    return new Promise((resolve, _) => {
+    return new Promise((resolve, reject) => {
       Console.readLine("다리의 길이를 입력해주세요.\n", (inputString) => {
         const inputNum = parseInt(inputString);
-        this.validateBridge(inputNum);
+        this.validateBridge(inputNum, reject);
         resolve(inputNum);
       });
     })
   },
 
   validateMove(move){
-    if(!(move === "U" || move ==="D")) throw new Error("[ERROR] 이동할 칸을 잘못 입력하셨습니다.");  
+    if(!(move === "U" || move ==="D")) reject(new Error("[ERROR] 이동할 칸을 잘못 입력하셨습니다."));  
   },
 
 
   readMoving() {
-    return new Promise((resolve, _) => {
+    return new Promise((resolve, reject) => {
       Console.readLine("\n이동할 칸을 선택해주세요. (위:U, 아래:D)\n", (inputMove) => {
-        this.validateMove(inputMove);
+        this.validateMove(inputMove, reject);
         resolve(inputMove);
       });
     })
   },
 
   validateStart(start){
-    if(!(start === "R" || start ==="Q")) throw new Error("[ERROR] 다시시도 여부를 잘못 입력하셨습니다.");  
+    if(!(start === "R" || start ==="Q")) reject(new Error("[ERROR] 다시시도 여부를 잘못 입력하셨습니다."));  
   },
 
   readGameCommand(){
-    return new Promise((resolve, _) => {
+    return new Promise((resolve, reject) => {
       Console.readLine("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n", (restart) => {
-        this.validateStart(restart);
+        this.validateStart(restart, reject);
         resolve(restart);
       });
     })
