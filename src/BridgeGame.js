5226d2873a56ef9e88e4b3bc4458dd9d73c922b1
WaiNaat <qoli0703@gmail.com>
2022-11-20 20:11:17 +0900

    feat(BridgeGame): add constructor

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -2,6 +2,15 @@
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  #bridge;
+
+  #position;
+
+  constructor(bridge) {
+    this.#bridge = bridge;
+    this.#position = 0;
+  }
+
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
