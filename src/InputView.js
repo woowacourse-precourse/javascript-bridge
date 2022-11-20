bab23619a7690be9f961da656202f814b5fe54b9
turtle601 <turtle601@naver.com>
2022-11-21 04:11:44 +0900

    refactor(InputView) validate함수 생성
    Changed:
    - getValidate -> validate

--- src/InputView.js
+++ src/InputView.js
@@ -1,5 +1,3 @@
-const { Console } = require('@woowacourse/mission-utils');
-
 const BridgeLengthException = require('./controller/validate/BridgeLengthException');
 const BridgeUpDownException = require('./controller/validate/BridgeUpDownException');
 const BridgeRestartExitException = require('./controller/validate/BridgeRestartExitException');
@@ -7,30 +5,24 @@ const BridgeRestartExitException = require('./controller/validate/BridgeRestartE
 const controller = require('./controller/BridgeController');
 
 const InputView = {
-  isValidate(exceptInstance) {
-    return exceptInstance.isValidate();
+  validate(exceptInstance) {
+    exceptInstance.isValidate();
   },
 
   readBridgeSize(input) {
-    if (!InputView.isValidate(new BridgeLengthException(input))) {
-      Console.close();
-    }
+    InputView.validate(new BridgeLengthException(input));
 
     controller.inputBridgeLength(input);
   },
 
   readMoving(input) {
-    if (!InputView.isValidate(new BridgeUpDownException(input))) {
-      Console.close();
-    }
+    InputView.validate(new BridgeUpDownException(input));
 
     controller.inputBridgeUpDown(input);
   },
 
   readGameCommand(input) {
-    if (!InputView.isValidate(new BridgeRestartExitException(input))) {
-      Console.close();
-    }
+    InputView.validate(new BridgeRestartExitException(input));
   }
 };
 
