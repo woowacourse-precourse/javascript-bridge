74873bcfe7d434db6b5bd441cdc32a73f9c0c60c
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2022-11-20 15:58:04 +0900

    test: add test function for validate input range

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -2,7 +2,11 @@ const validation = require('../src/Validations')
 const ERROR = require('../src/Error')
 describe("예외처리 테스트", () => {
     test("입력 범위 예외처리 테스트", () => {
-        const input = 4
+        const range = [3,20]
+        expect(validation.validateRange(range, 2)).toThrow(ERROR.numberNotInRange)
+        expect(validation.validateRange(range, 3)).toBe(undefined)
+        expect(validation.validateRange(range, 20)).toBe(undefined)
+        expect(validation.validateRange(range, 21)).toThrow(ERROR.numberNotInRange)
     });
     test("문자 예외처리 테스트", () => {
 
