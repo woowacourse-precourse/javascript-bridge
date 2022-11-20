70bcd2f0fc3f16a1f18078f6f13b01b427869303
woo-jk <201702042@o.cnu.ac.kr>
2022-11-20 14:12:23 +0900

    refactor: 다리의 길이 검증 Bridge 클래스에서 처리하도록 수정

--- src/Bridge.js
+++ src/Bridge.js
@@ -2,7 +2,14 @@ class Bridge {
   #bridge;
 
   constructor(bridge) {
+    this.#validate(bridge);
     this.#bridge = bridge;
+  }
+
+  #validate(bridge) {
+    if (bridge.length < 3 || bridge.length > 20) {
+      throw new Error("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+    }
   }
 
   isMovable(position, direction) {
