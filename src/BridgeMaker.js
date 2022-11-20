f73b83d488a49f201f58a7c14adc6449cdc4ba63
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-11-21 01:41:59 +0900

    refactoring($BridgeMaker): makeBridge 리팩토링

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,23 +1,19 @@
 const { POSITION } = require('./enums');
 
 const UP_NUMBER = 1;
-
-/**
- * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
- */
 const BridgeMaker = {
   /**
    * @param {number} size 다리의 길이
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
-  makeBridge: (size, generateRandomNumber) => {
+
+  makeBridge: (size, generateRandomNumber) =>
     [...Array(size)].map(() => {
       const randomNumber = generateRandomNumber();
       const isUpNumber = Number(randomNumber) === UP_NUMBER;
       return isUpNumber ? POSITION.UP : POSITION.DOWN;
-    });
-  },
+    }),
 };
 
 module.exports = BridgeMaker;
