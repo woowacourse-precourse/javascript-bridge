781e3e886f93767f0854773ed2d5fc606e60cb96
GunHoi <lgh509@kyonggi.ac.kr>
2022-11-20 21:45:10 +0900

    feat -2 : 건널 수 있는 칸 무작위 설정해서 다리 생성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 const OutputView = require("./OutputView.js");
-const RecallUntilCorrect = require("./RecallUntilCorrect");
+const RecallUntilCorrect = require("./RecallUntilCorrect.js");
+const BridgeMaker = require("./BridgeMaker.js");
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
 class App {
   play() {
     OutputView.printGameStart();
     const BRIDGE_SIZE = RecallUntilCorrect.recallReadBridgeSize(true);
+    const BRIDGE_MAP = BridgeMaker.makeBridge(BRIDGE_SIZE,BridgeRandomNumberGenerator.generate);
+    console.log(BRIDGE_MAP);
   }
 }
 
--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -7,7 +7,19 @@ const BridgeMaker = {
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
-  makeBridge(size, generateRandomNumber) {},
+  makeBridge(size, generateRandomNumber) {
+    let bridgeArray = [];
+    for(let count=0;count<size;count++){
+      let randomNumber = generateRandomNumber();
+      if(randomNumber==="1"){
+        bridgeArray.push("U");
+      }
+      if(randomNumber==="0"){
+        bridgeArray.push("D");
+      }
+    }
+    return bridgeArray;
+  },
 };
 
 module.exports = BridgeMaker;
