023f95f0ccf443497ddc2a8a99cc080d6b6ea076
kangju2000 <kangju2000@naver.com>
2022-11-20 23:12:46 +0900

    test(BridgeGameStatisticsTest, BridgeGameTest): 테스트 코드 수정

--- __tests__/BridgeGameStatisticsTest.js
+++ __tests__/BridgeGameStatisticsTest.js
@@ -3,7 +3,8 @@ const BridgeGameStatistics = require('../src/Model/BridgeGameStatistics');
 describe('BridgeGameStatistics 테스트', () => {
   test('시도한 횟수를 3번 증가', () => {
     const bridgeSize = 5;
-    const statistics = new BridgeGameStatistics(bridgeSize);
+    const statistics = new BridgeGameStatistics();
+    statistics.setBridgeSize(bridgeSize);
     statistics.increaseAttempt();
     statistics.increaseAttempt();
     statistics.increaseAttempt();
@@ -12,11 +13,12 @@ describe('BridgeGameStatistics 테스트', () => {
 
   test('다리 끝까지 도달했는지 확인(게임 성공)', () => {
     const bridgeSize = 5;
-    const statistics = new BridgeGameStatistics(bridgeSize);
+    const statistics = new BridgeGameStatistics();
+    statistics.setBridgeSize(bridgeSize);
     for (let i = 0; i < bridgeSize; i++) {
       statistics.increaseMoveCount();
     }
 
-    expect(statistics.isGameWin()).toBeTruthy();
+    expect(statistics.isGameSuccess()).toBeTruthy();
   });
 });
--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -5,8 +5,9 @@ const SETTING = require('../src/constants/gameSetting');
 describe('BridgeGame 테스트', () => {
   test('이동할 수 있는지 확인', () => {
     const bridge = ['U', 'D', 'U'];
-    const bridgeGame = new BridgeGame(bridge);
-    bridgeGame.start();
+    const bridgeGame = new BridgeGame();
+    bridgeGame.setBridgeGame(bridge);
+    bridgeGame.try();
 
     expect(bridgeGame.canMove(SETTING.MOVING_UP)).toBeTruthy();
     expect(bridgeGame.canMove(SETTING.MOVING_DOWN)).toBeFalsy();
@@ -15,25 +16,18 @@ describe('BridgeGame 테스트', () => {
   test('게임 성공 테스트', () => {
     const bridge = ['U', 'D', 'U'];
     const movings = ['U', 'D', 'U'];
-    const bridgeGame = new BridgeGame(bridge);
-    bridgeGame.start();
+    const bridgeGame = new BridgeGame();
+    bridgeGame.setBridgeGame(bridge);
+    bridgeGame.try();
     movings.forEach((moving) => {
-      bridgeGame.move(moving);
+      bridgeGame.move(moving, bridgeGame.canMove(moving));
     });
 
-    expect(bridgeGame.isGameWin()).toBeTruthy();
+    expect(bridgeGame.isGameSuccess()).toBeTruthy();
     expect(bridgeGame.getBridgeMap()).toEqual([
       [SETTING.CAN_MOVE, SETTING.DONT_MOVE, SETTING.CAN_MOVE],
       [SETTING.DONT_MOVE, SETTING.CAN_MOVE, SETTING.DONT_MOVE],
     ]);
     expect(bridgeGame.getAttempt()).toBe(1);
-  });
-
-  test('이동 문자가 잘못된 값일 시 에러 던저기', () => {
-    const bridge = ['U', 'D', 'U'];
-    const moving = 'Q';
-    const bridgeGame = new BridgeGame(bridge);
-
-    expect(() => bridgeGame.move(moving)).toThrow(ERROR.MOVING);
   });
 });
