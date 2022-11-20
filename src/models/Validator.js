9d9f411e794e09f381514d83bcc7615d30fa4c8d
nangkyeonglim <nangkyeonglim@gmail.com>
2022-11-20 23:20:47 +0900

    fix(Validator): 유효성 검사 로직 수정

--- src/models/Validator.js
+++ src/models/Validator.js
@@ -1,22 +1,22 @@
 class Validator {
   checkBridgeLengthInput(bridgeLength) {
     if (this.isNotNumber(bridgeLength)) {
-      throw new Error('[ERROR] 다리 길이는 숫자만 입력 가능합니다.');
+      throw new Error('[ERROR] 다리 길이는 숫자만 입력 가능합니다.\n');
     }
     if (this.isNotRangeOfBridgeLength(bridgeLength)) {
-      throw new Error('[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.');
+      throw new Error('[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.\n');
     }
   }
 
   checkDirectionInput(direction) {
     if (this.isNotValidDirectionInput(direction)) {
-      throw new Error('[ERROR] 이동할 칸은 위: U, 아래: D 만 입력 가능합니다.')
+      throw new Error('[ERROR] 이동할 칸은 위: U, 아래: D 만 입력 가능합니다.\n')
     }
   }
 
   checkRetryInput(input) {
     if (this.isNotValidRetryInput(input)) {
-      throw new Error('[ERROR] 재시도: R, 종료: Q 만 입력 가능합니다.')
+      throw new Error('[ERROR] 재시도: R, 종료: Q 만 입력 가능합니다.\n')
     }
   }
 
@@ -30,11 +30,11 @@ class Validator {
   }
 
   isNotValidDirectionInput(direction) {
-    return direction !== 'U' || direction !== 'D';
+    return direction !== 'U' && direction !== 'D';
   }
 
   isNotValidRetryInput(input) {
-    return input !== 'R' || input !== 'Q';
+    return input !== 'R' && input !== 'Q';
   }
 }
 
