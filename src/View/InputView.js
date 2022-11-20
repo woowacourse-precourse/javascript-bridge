9041c7be4c8eadea5912d2a15d427dee1f177430
imb96 <kimminje7267@naver.com>
2022-11-20 19:53:31 +0900

    refactor : 예외처리 코드 수정

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -5,13 +5,8 @@ const {
 const Random = require('../Utils/BridgeRandomNumberGenerator');
 const BridgeMaker = require('../BridgeMaker');
 const OutputView = require('./OutputView');
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+
 const InputView = {
-  /**
-   * 다리의 길이를 입력받는다.
-   */
   size: 0,
 
   readBridgeSize() {
@@ -19,7 +14,7 @@ const InputView = {
       this.size = +answer;
       try {
         this.bridgeSizeValidate(answer);
-      } catch (err) {
+      } catch {
         MissionUtils.Console.print(ERROR.BRIDGE_LENGTH);
         this.readBridgeSize();
       }
@@ -38,32 +33,31 @@ const InputView = {
     return size;
   },
 
-  /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
   readMoving(bridge, size) {
     MissionUtils.Console.readLine(MESSAGE.INPUT_MOVE, (answer) => {
       const move = answer;
       try {
         this.moveValidate(answer);
-      } catch (err) {
+        OutputView.printMap(bridge, size, move);
+      } catch {
         MissionUtils.Console.print(ERROR.MOVE);
         this.readMoving(bridge, size);
       }
-      OutputView.printMap(bridge, size, move);
     });
   },
 
   moveValidate(move) {
     if (![BUTTON.UP, BUTTON.DOWN].includes(move)) {
-      throw ERROR.RETRY_OR_QUIT;
+      throw ERROR.MOVE;
     }
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+    MissionUtils.Console.readLine(MESSAGE.INPUT_RETRY_OR_QUIT, (answer) => answer);
+  },
 };
 
 module.exports = InputView;
--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -1,21 +1,17 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const { MESSAGE, ERROR, BUTTON } = require('../Utils/constant');
 const BridgeGame = require('../BridgeGame');
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
+
 const OutputView = {
   isSucces: MESSAGE.SUCCESS,
   count: 1,
   size: 0,
+  upsideLast: '',
+  downSideLast: '',
   printStart() {
     MissionUtils.Console.print(MESSAGE.GAME_START);
   },
-  /**
-   * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-   * <p>
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
+
   printMap(bridge, size, move) {
     this.size = size;
     const bridgeGame = new BridgeGame(bridge, size, move);
@@ -24,68 +20,65 @@ const OutputView = {
   },
 
   checkBridge(bridgeGame) {
-    if (
-      (bridgeGame.realBridge[0].length === this.size
-        && bridgeGame.realBridge[0][bridgeGame.realBridge[0].length - 1]
-          === 'O')
-      || (bridgeGame.realBridge[0].length === this.size
-        && bridgeGame.realBridge[1][bridgeGame.realBridge[1].length - 1] === 'O')
-    ) {
+    if (this.isFinish(bridgeGame)) {
       return this.printResult(bridgeGame);
     }
+    this.printUpO(bridgeGame);
+  },
 
+  isFinish(bridgeGame) {
+    if ((bridgeGame.realBridge[0].length === this.size
+      && bridgeGame.realBridge[0][bridgeGame.realBridge[0].length - 1]
+        === 'O')
+    || (bridgeGame.realBridge[0].length === this.size
+      && bridgeGame.realBridge[1][bridgeGame.realBridge[1].length - 1] === 'O')
+    ) {
+      return true;
+    }
+  },
+
+  printUpO(bridgeGame) {
     if (bridgeGame.realBridge[0][bridgeGame.realBridge[0].length - 1] === 'O') {
-      let move = '';
-      MissionUtils.Console.readLine(MESSAGE.INPUT_MOVE, (answer) => {
-        move = answer;
-        bridgeGame.moveIsU(move);
-        bridgeGame.realBridge.map((x) => MissionUtils.Console.print(`[ ${x.join(' | ')} ]`));
-        this.checkBridge(bridgeGame, bridgeGame.realBridge);
-      });
+      this.printBridge(bridgeGame);
     }
+    this.printDownO(bridgeGame);
+  },
 
+  printDownO(bridgeGame) {
     if (bridgeGame.realBridge[1][bridgeGame.realBridge[1].length - 1] === 'O') {
-      let move = '';
-      MissionUtils.Console.readLine(MESSAGE.INPUT_MOVE, (answer) => {
-        move = answer;
-        bridgeGame.moveIsU(move);
-        bridgeGame.realBridge.map((x) => MissionUtils.Console.print(`[ ${x.join(' | ')} ]`));
-        this.checkBridge(bridgeGame, bridgeGame.realBridge);
-      });
+      this.printBridge(bridgeGame);
     }
+    this.printUpX(bridgeGame);
+  },
 
+  printUpX(bridgeGame) {
     if (bridgeGame.realBridge[0][bridgeGame.realBridge[0].length - 1] === 'X') {
       MissionUtils.Console.readLine(MESSAGE.INPUT_RETRY_OR_QUIT, (answer) => {
-        this.retryOrQuitValidate(bridgeGame, answer);
+        try {
+          this.retryOrQuitValidate(bridgeGame, answer);
+        } catch {
+          MissionUtils.Console.print(ERROR.RETRY_OR_QUIT);
+          this.printUpX(bridgeGame);
+        }
         if (answer === 'R') {
-          this.count += 1;
-          let move = '';
-          MissionUtils.Console.readLine(MESSAGE.INPUT_MOVE, (inputMove) => {
-            move = inputMove;
-            bridgeGame.retry(move);
-            bridgeGame.realBridge.map((x) => {
-              MissionUtils.Console.print(`[ ${x.join(' | ')} ]`);
-            });
-            this.checkBridge(bridgeGame, bridgeGame.realBridge);
-          });
+          this.printRetryBridge(bridgeGame);
         }
       });
     }
+    this.printDownX(bridgeGame);
+  },
 
+  printDownX(bridgeGame) {
     if (bridgeGame.realBridge[1][bridgeGame.realBridge[1].length - 1] === 'X') {
       MissionUtils.Console.readLine(MESSAGE.INPUT_RETRY_OR_QUIT, (answer) => {
-        this.retryOrQuitValidate(bridgeGame, answer);
+        try {
+          this.retryOrQuitValidate(bridgeGame, answer);
+        } catch {
+          MissionUtils.Console.print(ERROR.RETRY_OR_QUIT);
+          this.printUpO(bridgeGame);
+        }
         if (answer === 'R') {
-          this.count += 1;
-          let move = '';
-          MissionUtils.Console.readLine(MESSAGE.INPUT_MOVE, (inputMove) => {
-            move = inputMove;
-            bridgeGame.retry(move);
-            bridgeGame.realBridge.map((x) => {
-              MissionUtils.Console.print(`[ ${x.join(' | ')} ]`);
-            });
-            this.checkBridge(bridgeGame, bridgeGame.realBridge);
-          });
+          this.printRetryBridge(bridgeGame);
         }
       });
     }
@@ -106,16 +99,29 @@ const OutputView = {
       throw ERROR.MOVE;
     }
   },
-  /**
-   * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-   * <p>
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
+
+  printBridge(bridgeGame) {
+    MissionUtils.Console.readLine(MESSAGE.INPUT_MOVE, (answer) => {
+      const move = answer;
+      bridgeGame.moveIsU(move);
+      bridgeGame.realBridge.map((x) => MissionUtils.Console.print(`[ ${x.join(' | ')} ]`));
+      this.checkBridge(bridgeGame);
+    });
+  },
+
+  printRetryBridge(bridgeGame) {
+    this.count += 1;
+    MissionUtils.Console.readLine(MESSAGE.INPUT_MOVE, (inputMove) => {
+      const move = inputMove;
+      bridgeGame.retry(move);
+      bridgeGame.realBridge.map((x) => MissionUtils.Console.print(`[ ${x.join(' | ')} ]`));
+      this.checkBridge(bridgeGame);
+    });
+  },
+
   printResult(bridgeGame) {
     MissionUtils.Console.print(MESSAGE.RESULT);
-    bridgeGame.realBridge.map((x) => {
-      MissionUtils.Console.print(`[ ${x.join(' | ')} ]`);
-    });
+    bridgeGame.realBridge.map((x) => MissionUtils.Console.print(`[ ${x.join(' | ')} ]`));
     MissionUtils.Console.print(MESSAGE.isSucces(this.isSucces));
     MissionUtils.Console.print(MESSAGE.printCount(this.count));
     MissionUtils.Console.close();
