e6f51887ccdfdce232f7ff9c06ed1a476c9ca693
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-20 15:14:54 +0900

    :sparkles: feat : 오류 메시지 상수 정의

---
+++ src/utils/constants/ErrorMessage.js
@@ -0,0 +1,13 @@
+const ErrorMessage = Object.freeze({
+  input_number: '숫자를 입력해주세요.',
+  range_number: '3부터 20 사이의 숫자를 입력해주세요.',
+  valid_move: 'U 또는 D를 입력해주세요.',
+  valid_command: 'Q 또는 R을 입력해주세요.'
+});
+
+const ERROR_CODE = '[ERROR]'
+
+module.exports = {
+  ErrorMessage,
+  ERROR_CODE
+}
