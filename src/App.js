c33f2b4cf9fea84a120a62c9c9836452b5b8e23d
xownsdl0 <xownsdl0@naver.com>
2022-11-21 00:17:04 +0900

    feat: 입력한 방향과 다리의 방향의 일치여부 판별하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ const InputView = require('../src/InputView')
 const BridgeGame = require('../src/BridgeGame');
 
 class App {
-  play() {
-    const bridgeGame = new BridgeGame()
+  #bridgeGame = new BridgeGame()
 
+  play() {
     InputView.readBridgeSize(size => {
-      bridgeGame.makeBridge(size)
+      this.#bridgeGame.makeBridge(size)
       this.getUserMove()
     })
   }
 
   getUserMove() {
-    InputView.readMoving(move => {})
+    InputView.readMoving(move => {
+      this.#bridgeGame.moveCompare(move)
+      this.getUserMove()
+    })
   }
 }
 
--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -3,17 +3,43 @@ const randomNumber = require('../src/controller/GenerateRandomNumber')
 
 class BridgeGame {
   #bridge
+  #collectInputResult = {
+    moveIndex: 0,
+    bridgeMap: [[], []],
+  }
 
   makeBridge(size) {
     this.#bridge = BridgeMaker.makeBridge(size, randomNumber)
   }
-  /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  move() {}
 
+  moveCompare(move) {
+    const index = this.#bridge[this.#collectInputResult.moveIndex]
+    this.isPass(move, index)
+    this.isNonPass(move, index)
+    this.#collectInputResult.moveIndex += 1
+  }
+
+  isPass(move, index) {
+    if (move === index && move === 'U') {
+      this.#collectInputResult.bridgeMap[0].push('O') 
+      this.#collectInputResult.bridgeMap[1].push(' ')
+    }
+    if (move === index && move === 'D') {
+      this.#collectInputResult.bridgeMap[0].push(' ') 
+      this.#collectInputResult.bridgeMap[1].push('O')
+    }
+  }
+
+  isNonPass(move, index) {
+    if (move !== index && move === 'U') {
+      this.#collectInputResult.bridgeMap[0].push('X') 
+      this.#collectInputResult.bridgeMap[1].push(' ')
+    }
+    if (move !== index && move === 'D') {
+      this.#collectInputResult.bridgeMap[0].push(' ') 
+      this.#collectInputResult.bridgeMap[1].push('X')
+    }
+  }
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
    * <p>
