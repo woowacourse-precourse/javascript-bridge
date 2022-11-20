0ef99c93d22a87bfc5a7dfbc63943c38d76f38b7
yaena1223 <yaena1223@dgu.ac.kr>
2022-11-21 05:20:28 +0900

    feat: repeat input & move

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ const InputView = require('./InputView');
 const OutputView = require('./OutputView');
 const BridgeMaker = require('./BridgeMaker');
 const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
+const BridgeGame = require('./BridgeGame');
 class App {
   async play() {
     OutputView.printStart();
@@ -9,6 +10,25 @@ class App {
     const generateRandomNumber = BridgeRandomNumberGenerator.generate;
     const RESULT = BridgeMaker.makeBridge(SIZE, generateRandomNumber);
     console.log(RESULT);
+    const GAME = new BridgeGame(RESULT);
+    this.repeatMove(SIZE, GAME);
+  }
+
+  //모두 이동하거나, 실패하기 전까지 반복.
+  async repeatMove(size, GAME) {
+    let CNT = 0;
+    while (CNT < size) {
+      const MOVE = await InputView.readMoving();
+      const RESULT = GAME.move(CNT, MOVE);
+      OutputView.printResult();
+      CNT += 1;
+      if (RESULT === 'X') this.selectRestart();
+    }
+  }
+
+  //실패하면 Restart여부 선택
+  async selectRestart() {
+    const RESTART_OR_END = await InputView.readGameCommand();
   }
 }
 
--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,13 +1,23 @@
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
+//  - [ ] BridgeGame에서는 InputView, OutputView 사용 불가능
+
 class BridgeGame {
+  #answer;
+
+  constructor(answer) {
+    this.#answer = answer;
+  }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(index, input) {
+    if (this.#answer[index] === input) return 'O';
+    return 'X';
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
