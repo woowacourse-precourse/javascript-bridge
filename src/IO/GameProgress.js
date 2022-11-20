e1feb4bba06b6d26659c3c5c118cefe645b7875f
wonkyum-kim <dodo12120@naver.com>
2022-11-20 12:15:38 +0900

    refactor(GameProgress): change printGameStart
    - Make a constant that will be printed out.

--- src/IO/GameProgress.js
+++ src/IO/GameProgress.js
@@ -1,8 +1,14 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 
+/* ************************************************ *
+ * GameProgress: 게임 진행을 위한 문장을 출력하는 객체 *
+ * ************************************************ *
+ */
+
 const GameProgress = {
   printGameStart() {
-    MissionUtils.Console.print('다리 건너기 게임을 시작합니다.\n');
+    const BRIDGE_GAME_START = '다리 건너기 게임을 시작합니다.\n';
+    MissionUtils.Console.print(BRIDGE_GAME_START);
   },
 
   printErrorMessage(err) {
