9f12a267834358c58311754562f9fa881f43d337
woo-jk <201702042@o.cnu.ac.kr>
2022-11-20 18:30:52 +0900

    feat: Player 클래스 현재 위치 가져오는 getCurrentPosition() 함수, 지도 가져오는 getMap() 함수 구현

--- src/Player.js
+++ src/Player.js
@@ -10,4 +10,44 @@ class Player {
   move(direction) {
     this.#bridgePath.push(direction);
   }
+
+  out(direction) {
+    this.#out = direction;
+  }
+
+  getCurrentPosition() {
+    return this.#bridgePath.length;
+  }
+
+  #getBridgeArray() {
+    const upperBridge = this.#bridge.map((direction) => (direction === "U" ? "O" : " "));
+    const lowerBridge = this.#bridge.map((direction) => (direction === "D" ? "O" : " "));
+    return [upperBridge, lowerBridge];
+  }
+
+  #addOutMark(upperBridge, lowerBridge) {
+    if (this.#out === "U") {
+      upperBridge.push("X");
+      lowerBridge.push(" ");
+    }
+    if (this.#out === "D") {
+      upperBridge.push(" ");
+      lowerBridge.push("X");
+    }
+  }
+
+  #toString(upperBridge, lowerBridge) {
+    const upperBridgeString = `[ ${upperBridge.join(" | ")} ]`;
+    const lowerBridgeString = `[ ${lowerBridge.join(" | ")} ]`;
+
+    return [upperBridgeString, lowerBridgeString];
+  }
+
+  getMap() {
+    const [upperBridge, lowerBridge] = this.#getBridgeArray();
+    if (this.#out) {
+      this.#addOutMark(upperBridge, lowerBridge);
+    }
+    return this.#toString(upperBridge, lowerBridge);
+  }
 }
