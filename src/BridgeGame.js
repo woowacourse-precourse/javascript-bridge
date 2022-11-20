53f88ee96071561dd333235372959f2769f11b99
wjs5025 <wjs_5025@naver.com>
2022-11-21 02:26:08 +0900

    feat : BridgeGame 필드 추가 및 move() 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,13 +1,38 @@
 /**
- * 다리 건너기 게임을 관리하는 클래스
+ * 다리 건너기 게임을 관리하는 클래스,
+ * inputView, OutputView 사용 금지
  */
 class BridgeGame {
+  constructor() {
+    this.bridge;
+    this.done = false;
+    this.succeed = false;
+    this.playerLocation = 0;
+    this.progress = [[], []]; // playerLocation[0] = up, playerLocation[1] = down
+  }
+
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(input, bridge) {
+    if (bridge[this.playerLocation] === input) {
+      this.playerLocation += 1;
+      this.updateProgress(this.progress, input);
+    }
+  }
+
+  updateProgress(progress, input) {
+    if (input === "U") {
+      progress[0].push("O");
+      progress[1].push(" ");
+    } else {
+      progress[0].push(" ");
+      progress[1].push("O");
+    }
+    return progress;
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
