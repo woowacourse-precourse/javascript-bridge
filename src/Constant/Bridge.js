8f81972ef888251bd901d586ab6d920c8514a9cc
murpin <qw04011@gmail.com>
2022-11-21 01:21:30 +0900

    feat(Constant): 폴더를 통한 상수 관리

---
+++ src/Constant/Bridge.js
@@ -0,0 +1,13 @@
+export const BRIDGE_GAME = {
+  move_correct: 'O',
+  move_fail: 'X',
+  move_up: 'U',
+  move_down: 'D',
+  quit_game: 'Q',
+  retry_game: 'R',
+}
+
+export const BRIDGE_GAME_RESULT = {
+  success: "성공",
+  fail: "실패",
+}
---
+++ src/Constant/PrintSentence.js
@@ -0,0 +1,15 @@
+export const BRIDGE_GAME_SENTENCE = {
+  start: "다리 건너기 게임을 시작합니다.\n",
+}
+
+export const BRIDGE_GAME_INPUT = {
+  input_bridge_size: "다리의 길이를 입력해주세요.",
+  input_move_direction: "이동할 칸을 선택해주세요. (위: U, 아래: D)",
+  input_game_command: "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
+}
+
+export const BRIDGE_GAME_OUTPUT = {
+  final_result: "최종 게임 결과\n",
+  game_successful: "게임 성공 여부",
+  game_try_count: "총 시도한 횟수",
+}
