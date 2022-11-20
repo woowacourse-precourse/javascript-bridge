8a17b621c3d67320b5e258584ca6210bde46f511
wontae chang <cwt629@naver.com>
2022-11-20 13:14:15 +0900

    style: add remarks on the functions in App and complement some other remarks

--- src/App.js
+++ src/App.js
@@ -15,6 +15,10 @@ class App {
     InputView.readBridgeSize(this.proceedAfterGettingBridgeSize);
   }
 
+  /**
+   * 다리 길이 입력을 받은 후의 과정
+   * @param {number} size 입력된 다리 길이
+   */
   proceedAfterGettingBridgeSize = (size) => {
     const PATH = BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate);
     this.game = new BridgeGame(PATH);
@@ -22,6 +26,10 @@ class App {
     InputView.readMoving(this.proceedAfterMove);
   }
 
+  /**
+   * 이동 입력을 받은 후의 과정
+   * @param {string} direction 입력된 이동
+   */
   proceedAfterMove = (direction) => {
     this.game.move(direction);
     OutputView.printMap(this.game);
@@ -29,10 +37,17 @@ class App {
     (this.game.isGoingCorrect()) ? this.handleCorrectCase() : InputView.readGameCommand(this.handleGameCommand);
   }
 
+  /**
+   * 올바르게 다리를 건넌 후의 과정
+   */
   handleCorrectCase = () => {
     (this.game.crossedAll()) ? this.finish() : InputView.readMoving(this.proceedAfterMove);
   }
 
+  /**
+   * 게임 재시작 및 종료 과정
+   * @param {string} command 입력된 커맨드
+   */
   handleGameCommand = (command) => {
     switch (command) {
       case "R":
@@ -46,6 +61,9 @@ class App {
     }
   }
 
+  /**
+   * 게임 종료 과정
+   */
   finish = () => {
     OutputView.printResult(this.game);
     MissionUtils.Console.close();
--- src/InputView.js
+++ src/InputView.js
@@ -5,7 +5,7 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
-   * @param {function} proceed 제대로 된 입력을 받으면 진행할 함수
+   * @param {function()} proceed 제대로 된 입력을 받으면 진행할 함수
    */
   readBridgeSize(proceed) {
     MissionUtils.Console.readLine("다리의 길이를 입력해주세요.\n", (input) => {
@@ -16,7 +16,7 @@ const InputView = {
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
-   * @param {function} proceed 제대로 된 입력을 받으면 진행할 함수
+   * @param {function()} proceed 제대로 된 입력을 받으면 진행할 함수
    */
   readMoving(proceed) {
     MissionUtils.Console.readLine("이동할 칸을 선택해주세요. (위: U, 아래: D)\n", (input) => {
@@ -27,7 +27,7 @@ const InputView = {
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-   * @param {function} proceed 제대로 된 입력을 받으면 진행할 함수
+   * @param {function()} proceed 제대로 된 입력을 받으면 진행할 함수
    */
   readGameCommand(proceed) {
     MissionUtils.Console.readLine("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n", (input) => {
@@ -48,7 +48,7 @@ const InputView = {
 
   /**
    * 다리 길이 입력이 올바르지 않을 때 수행하는 함수
-   * @param {function} proceed 다리 길이가 올바른 입력일때 수행하는 다음 함수
+   * @param {function()} proceed 다리 길이가 올바른 입력일때 수행하는 다음 함수
    */
   handleBridgeSizeException(proceed) {
     // TODO: 에러 메시지 출력은 input view가 아닌 다른 클래스에서 수행할 수 있지 않을까? (ex. ErrorView)
@@ -67,7 +67,7 @@ const InputView = {
 
   /**
    * 올바르지 않은 이동 입력에 대해 처리하는 함수
-   * @param {function} proceed 올바른 이동 입력에 대해 수행하는 함수
+   * @param {function()} proceed 올바른 이동 입력에 대해 수행하는 함수
    */
   handleMoveException(proceed) {
     MissionUtils.Console.print("[ERROR] 이동은 U나 D 중 하나의 명령어로만 가능합니다.");
@@ -85,7 +85,7 @@ const InputView = {
 
   /**
    * 올바르지 않은 게임 재시도 및 종료 커맨드에 대해 처리하는 함수
-   * @param {function} proceed 올바른 커맨드에 대해 수행하는 함수
+   * @param {function()} proceed 올바른 커맨드에 대해 수행하는 함수
    */
   handleGameCommandException(proceed) {
     MissionUtils.Console.print("[ERROR] 명령어는 R이나 Q 중 하나만 가능합니다.");
