1eeda49174705e873f5d345c79ab8afb55c09e21
BaeDongWoo <ehddn5410@naver.com>
2022-11-20 17:18:03 +0900

    feat: 재시작 여부를 입력받는 기능

--- src/InputView.js
+++ src/InputView.js
@@ -1,30 +1,21 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+const MESSAGE=require('../src/constant');
 const InputView = {
-  /**
-   * 다리의 길이를 입력받는다.
-   */
   readBridgeSize() {
-    MissionUtils.Console.readLine("다리의 길이를 입력해주세요.",(bridgeLength)=>{
+    MissionUtils.Console.readLine(MESSAGE.BRIDGE_SIZE,(bridgeLength)=>{
 
     })
   },
-
-  /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
   readMoving() {
-    MissionUtils.Console.readLine("이동할 칸을 선택해주세요. (위: U, 아래: D)",(move)=>{
+    MissionUtils.Console.readLine(MESSAGE.MOVE_MESSAGE,(move)=>{
       
     })
   },
-
-  /**
-   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-   */
-  readGameCommand() {},
+  readGameCommand() {
+    MissionUtils.Console.readLine(MESSAGE.RESTAR_MESSAGE,(restart)=>{
+      
+    })
+  },
 };
 
 module.exports = InputView;
