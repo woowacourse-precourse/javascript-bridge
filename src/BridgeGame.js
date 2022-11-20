00228e11cbc5cdeddecce44cd2c9ff33024e2f47
unsnruu <unsnruu@gmail.com>
2022-11-20 12:19:07 +0900

    style: 불필요한 주석 제거 및 위치 변경

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -6,15 +6,6 @@ const Bridge = require("./Bridge.js");
 
 const { Console } = MissionUtils;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- * 1. 필드를 추가할 수 있다.
- * 2. 파일 경로를 변경할 수 있다.
- * 3. 메서드의 이름은 변경할 수 없다.
- * 4. 인자는 필요에 따라 추가하거나 변경할 수 있다.
- * 5. 메서드를 추가하거나 변경할 수 있다.
- */
-
 class BridgeGame {
   #bridge;
   #inputs;
@@ -34,10 +25,6 @@ class BridgeGame {
       this.move();
     });
   }
-  /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
   move() {
     const current = this.move.bind(this);
 
@@ -65,10 +52,7 @@ class BridgeGame {
     }
     this.#doWhenRightAnswer();
   }
-  /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
+
   retry() {
     const current = this.retry.bind(this);
     InputView.readGameCommand(current, (command) => {
@@ -96,3 +80,12 @@ class BridgeGame {
 }
 
 module.exports = BridgeGame;
+
+/**
+ * 다리 건너기 게임을 관리하는 클래스
+ * 1. 필드를 추가할 수 있다.
+ * 2. 파일 경로를 변경할 수 있다.
+ * 3. 메서드의 이름은 변경할 수 없다.
+ * 4. 인자는 필요에 따라 추가하거나 변경할 수 있다.
+ * 5. 메서드를 추가하거나 변경할 수 있다.
+ */
