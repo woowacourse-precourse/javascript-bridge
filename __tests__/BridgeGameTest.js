f8bd29b40932fee3e009650e2ff569d8f6852427
js43o <js43o@naver.com>
2022-11-20 19:48:40 +0900

    test: BridgeGame 클래스 테스트 코드 추가

---
+++ __tests__/BridgeGameTest.js
@@ -0,0 +1,82 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const BridgeGame = require('../src/BridgeGame');
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+};
+
+describe('BridgeGame 클래스 테스트', () => {
+  const randoms = ['1', '0', '1'];
+
+  test('move: 다리 중간에서 틀린 경우 false 반환', () => {
+    mockRandoms(randoms);
+    const bridgeGame = new BridgeGame(randoms.length);
+
+    expect(bridgeGame.move('U')).toEqual(true);
+    expect(bridgeGame.move('U')).toEqual(false);
+  });
+
+  test('move: 다리 끝까지 이동한 경우 false 반환', () => {
+    mockRandoms(randoms);
+    const bridgeGame = new BridgeGame(randoms.length);
+
+    expect(bridgeGame.move('U')).toEqual(true);
+    expect(bridgeGame.move('D')).toEqual(true);
+    expect(bridgeGame.move('U')).toEqual(false);
+  });
+
+  test('isFailed: 다리 중간에서 틀린 경우 true 반환', () => {
+    mockRandoms(randoms);
+    const bridgeGame = new BridgeGame(randoms.length);
+
+    bridgeGame.move('U');
+    expect(bridgeGame.isFailed()).toEqual(false);
+
+    bridgeGame.move('U');
+    expect(bridgeGame.isFailed()).toEqual(true);
+  });
+
+  test('isFinished: 다리 끝까지 이동한 경우 true 반환', () => {
+    mockRandoms(randoms);
+    const bridgeGame = new BridgeGame(randoms.length);
+
+    bridgeGame.move('U');
+    expect(bridgeGame.isFinished()).toEqual(false);
+
+    bridgeGame.move('D');
+    expect(bridgeGame.isFinished()).toEqual(false);
+
+    bridgeGame.move('U');
+    expect(bridgeGame.isFinished()).toEqual(true);
+  });
+
+  test('getSingleResultArray: 다리 배열을 위/아래 각각 변환', () => {
+    const commands = ['U', 'D', 'U'];
+    const upper = ['O', ' ', 'O'];
+    const lower = [' ', 'O', ' '];
+
+    const bridgeGame = new BridgeGame(0);
+
+    expect(bridgeGame.getSingleResultArray('U', commands)).toEqual(upper);
+    expect(bridgeGame.getSingleResultArray('D', commands)).toEqual(lower);
+  });
+
+  test('getFailedMarkedResultArrays: 실패 지점을 표시', () => {
+    const commands = ['U', 'D', 'D'];
+    const upper = ['O', ' ', 'O'];
+    const lower = [' ', 'O', ' '];
+
+    mockRandoms(randoms);
+    const bridgeGame = new BridgeGame(randoms.length);
+
+    commands.forEach((direction) => bridgeGame.move(direction));
+
+    expect(bridgeGame.getFailedMarkedResultArrays({ upper, lower })).toEqual({
+      upper: ['O', ' ', ' '],
+      lower: [' ', 'O', 'X'],
+    });
+  });
+});
