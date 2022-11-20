9a2aa37a2e7fb5971ce237cfab70aafd07a34d0d
eunsung <esk114477@gmail.com>
2022-11-21 00:21:27 +0900

    feat : 다리 길이 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const Console = require("@woowacourse/mission-utils");
+const BridgeGame = require("./BridgeGame.js");
+const InputView = require("./InputView");
+
 class App {
-  play() {}
+  play() {
+    const bridgeGame = new BridgeGame();
+    Console.print("다리 건너기 게임을 시작합니다.\n");
+    InputView.readBridgeSize(bridgeGame);
+  }
 }
 
 module.exports = App;
