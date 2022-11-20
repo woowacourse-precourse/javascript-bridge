85ee92d99998167a5009901c4d8d6e7ff1197fb0
inthhh <glass005533@gmail.com>
2022-11-20 16:37:49 +0900

    refactor: OutputView 메소드 분리 및 전역변수 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -3,37 +3,47 @@ const InputView = require("./InputView");
 const Console = MissionUtils.Console;
 
 const OutputView = {
-  printMap(nowLength, bridge, moveUpDown, tf) {
-    let upList = [];
-    let downList = [];
-    for (let n = 0; n < nowLength + 1; ++n) {
-      if (bridge[n] == "D") {
-        upList.push(" ");
-        downList.push("O");
-      }
-      if (bridge[n] == "U") {
-        upList.push("O");
-        downList.push(" ");
-      }
+  upList: [],
+  downList: [],
+
+  makeBridgeMap(nowLength, bridge, moveUpDown, tf) {
+    this.upList = [];
+    this.downList = [];
+    for (let n = 0; n < nowLength + 1; ++n) this.makeMapJustO(bridge, n);
+    if (!tf) this.makeMapWithX(nowLength, moveUpDown);
+    this.printMap();
+  },
+
+  makeMapJustO(bridge, n) {
+    if (bridge[n] == "D") {
+      this.upList.push(" ");
+      this.downList.push("O");
     }
-    if (!tf) {
-      if (moveUpDown == "U") {
-        upList[nowLength] = "X";
-        downList[nowLength] = " ";
-      }
-      if (moveUpDown == "D") {
-        downList[nowLength] = "X";
-        upList[nowLength] = " ";
-      }
+    if (bridge[n] == "U") {
+      this.upList.push("O");
+      this.downList.push(" ");
     }
-    Console.print("[ " + [...upList].join(" | ") + " ]");
-    Console.print("[ " + [...downList].join(" | ") + " ]");
-    return;
+  },
+
+  makeMapWithX(nowLength, moveUpDown) {
+    if (moveUpDown == "U") {
+      this.upList[nowLength] = "X";
+      this.downList[nowLength] = " ";
+    }
+    if (moveUpDown == "D") {
+      this.downList[nowLength] = "X";
+      this.upList[nowLength] = " ";
+    }
+  },
+
+  printMap() {
+    Console.print("[ " + [...this.upList].join(" | ") + " ]");
+    Console.print("[ " + [...this.downList].join(" | ") + " ]");
   },
 
   printResult(gameLength, bridge, moveUpDown, tf, tryCount) {
     Console.print("\n최종 게임 결과");
-    this.printMap(gameLength, bridge, moveUpDown, tf);
+    this.makeBridgeMap(gameLength, bridge, moveUpDown, tf);
     if (tf) Console.print("\n게임 성공 여부: 성공");
     if (!tf) Console.print("\n게임 성공 여부: 실패");
     Console.print("총 시도한 횟수: " + tryCount);
