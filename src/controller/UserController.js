984ee340ff851c19c87aa911b937788d4a3abb1d
sh981013s <sh981013s@gmail.com>
2022-11-20 17:23:18 +0900

    comment(UserController): 메서드 주석 작성

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -6,10 +6,15 @@ class UserController {
     this.userModel = new UserModel();
   }
 
+  /**
+   * 유저의 시도 횟수 반환 연결 메서드
+   * @return {number} [유저 시도 횟수]
+   */
   getTryCount() {
     return this.userModel.getTryCount();
   }
 
+  // 유저의 시도 횟수 증가 메서드
   increaseTryCount() {
     this.userModel.increaseTryCount();
   }
