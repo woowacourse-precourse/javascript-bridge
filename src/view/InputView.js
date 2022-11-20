7de5aca85d81e84dea203b0c0712876407104353
s0ojin <onepst@hanyang.ac.kr>
2022-11-20 21:58:39 +0900

    feat(InputView.js) : 다리길이를 입력받는 'readBridgeSize'메서드 작성

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,3 +1,7 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const Console = MissionUtils.Console;
+const { GAME_MESSAGE } = require('../utils/constants');
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,7 +9,12 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine(GAME_MESSAGE.BRIDGE_SIZE, answer => {
+      //TODO:유효성검사 - 숫자인가? 3~20 사이인가?
+      return answer;
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
