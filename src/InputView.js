73776de73d223f08711f377d3b3c8e0df4546836
curomame <curomame@naver.com>
2022-11-21 00:41:11 +0900

    ➕ feat - <InputView> [readGameCommand] add
    사용자가 게임을 다시 시도할지 입력받은 후 callback 함수를 실행시키는 함수 구현

--- src/InputView.js
+++ src/InputView.js
@@ -22,8 +22,11 @@ const InputView = {
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+   * @param {callback} function 사용자의 응답을 받은 후 실행될 함수
    */
-  readGameCommand() {},
+  readGameCommand(callback) {
+    MissionUtils.Console.readLine("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n", callback);
+  },
 };
 
 module.exports = InputView;
