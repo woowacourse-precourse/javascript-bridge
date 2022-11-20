21043d8d272ea884294099214566e3e8eb19d7ae
cho seonghoon <chsida655@gmail.com>
2022-11-21 02:24:09 +0900

    feat: 사용자 input 받는 메서드 정의

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,5 @@
+const { Console, Random } = require("@woowacourse/mission-utils");
+const OutputView = require("./OutputView.js");
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,12 +7,25 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize(bridgeGame) {
+    Console.print("다리 건너기 게임을 시작합니다.\n");
+    Console.readLine("다리의 길이를 입력해주세요.\n", (input) => {
+      bridgeGame.makeBridge(parseInt(input));
+    }); //예외 처리(범위, 숫자)
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving(bridgeGame) {
+    Console.readLine(
+      "이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
+      (input) => {
+        bridgeGame.moveInput.push(input);
+        OutputView.printMap(bridgeGame.bridgeArray, bridgeGame.moveInput);
+      } //readGameCommand 실행 여부 판단하고
+    );
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
