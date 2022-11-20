b2c9db6829fd6badb940fcc9ec7470d14b58d1d6
evencoding <evencoding@gmail.com>
2022-11-21 00:19:14 +0900

    test(ValidatorTest): 방향에 대한 예외 테스트

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -31,3 +31,23 @@ describe('다리 길이에 대한 예외 테스트', () => {
     });
   });
 });
+
+describe('방향에 대한 예외 테스트', () => {
+  test('문자에 공백이 포함되어 있다면 예외가 발생한다.', () => {
+    const inputValues = [' ', ' 3', '12 ', '3 4'];
+    inputValues.forEach((inputValue) => {
+      expect(() => Validator.throwErrorIfHasBlank(inputValue)).toThrow(
+        '[ERROR]'
+      );
+    });
+  });
+
+  test('대문자 U 혹은 D 가 아니라면 예외가 발생한다.', () => {
+    const inputValues = ['u', 'd', 'UD', 'DU'];
+    inputValues.forEach((inputValue) => {
+      expect(() => Validator.directionValidityCheck(inputValue)).toThrow(
+        '[ERROR]'
+      );
+    });
+  });
+});
