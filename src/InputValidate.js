4d23021c86a58dae9b46e4458244f599e97056a7
woo-jk <201702042@o.cnu.ac.kr>
2022-11-20 11:14:46 +0900

    feat: InputValidate 재시작 명령어 입력 검증 구현

--- src/InputValidate.js
+++ src/InputValidate.js
@@ -18,7 +18,13 @@ const InputValidate = {
 
   checkMovingDirection(direction) {
     if (direction !== "U" && direction !== "D") {
-      throw new Error("[ERROR] 이동할 칸은 U 혹은 D여야 합니다.");
+      throw new Error("[ERROR] 이동할 칸은 U 혹은 D 여야 합니다.");
+    }
+  },
+
+  checkRetryOrQuitCommand(command) {
+    if (command !== "R" && command !== "Q") {
+      throw new Error("[ERROR] 입력 명령어는 R 혹은 Q 여야 합니다.");
     }
   },
 };
