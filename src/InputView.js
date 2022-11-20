7517f1ce766235cdd68d4f4c96ee69bb776763b9
yuna Kim <yuna000804@gmail.com>
2022-11-20 12:11:44 +0900

    feat : 다리 길이 숫자 입력

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,5 @@
+const {START_GAME, USER_INPUT} = require('./Messages');
+const { Random, Console } = require("@woowacourse/mission-utils");
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,7 +7,11 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine(USER_INPUT.ENTER_LENGTH, (length) => {
+      console.log(length);
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
