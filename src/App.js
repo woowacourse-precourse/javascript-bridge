81983709cc7833b27fa2f9807312c1739f00136d
HWI <dlwognl99@naver.com>
2022-11-20 14:23:41 +0900

    feat: 다리 길이 입력 기능 구현 refactor: 게임 시작 메세지 출력 기능 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 const { Console } = require("@woowacourse/mission-utils");
 const { OUTPUT_MESSAGE } = require("./constants/message");
+const InputView = require("./InputView");
 
 class App {
   play() {
+    this.printGameStart();
+    InputView.readBridgeSize();
+  }
+  printGameStart() {
     Console.print(OUTPUT_MESSAGE.START);
   }
 }
