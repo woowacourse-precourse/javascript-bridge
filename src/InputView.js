cb0417e3a55c1ed3fe77cac6407ca55dda230df7
Sangjun-man <molung@icloud.com>
2022-11-20 16:59:08 +0900

    feat(InputView) : 다리 길이 입력 기능 구현
    다리 길이 입력 기능을 구현했습니다.

--- src/InputView.js
+++ src/InputView.js
@@ -1,21 +1,46 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const InputMessage = require("./utils/const/inputViewMessage");
+const VaildationCheck = require("./VaildationCheck");
+const varTypeConst = require("./utils/const/varType");
+const ErrorWithPrifix = require("./CustomError");
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
+  validate: new VaildationCheck(),
+  inputContainer: null,
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    this.inputHelper(InputMessage.readBridgeSize);
+    const bridgeSize = Number(this.inputContainer);
+    this.validate.isNumber(bridgeSize);
+    this.validate.isNumberIntheRange([3, 20], bridgeSize);
+    return bridgeSize;
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    return true;
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+    return true;
+  },
+
+  readBridgeSizeValidate() {},
+
+  inputHelper(message) {
+    MissionUtils.Console.readLine(message, (input) => {
+      this.inputContainer = input;
+    });
+  },
 };
 
 module.exports = InputView;
---
+++ src/utils/const/inputViewMessage.js
@@ -0,0 +1,9 @@
+const INPUT_READ_BRIDGE_SIZE = `다리의 길이를 입력해주세요.`;
+const INPUT_READ_MOVING = `다리의 길이를 입력해주세요.`;
+const INPUT_READ_GAME_COMMAND = `게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)`;
+
+module.exports = {
+  readBridgeSize: INPUT_READ_BRIDGE_SIZE,
+  readMoving: INPUT_READ_MOVING,
+  readGamdCommand: INPUT_READ_GAME_COMMAND,
+};
