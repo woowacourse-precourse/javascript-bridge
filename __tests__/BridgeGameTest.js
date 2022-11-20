3beb86e26d6919895c3361d1a52333bc985ce379
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-11-20 17:43:35 +0900

    ✅ [test]: BridgeGame 클래스 테스트 작성 - 사용자가 칸을 이동하는 경우 - 사용자가 게임을 재시작하는 경우

---
+++ __tests__/BridgeGameTest.js
@@ -0,0 +1,27 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const { ERROR } = require("../src/Constants");
+const App = require("../src/Controller/App");
+const BridgeGame = require("../src/Model/BridgeGame");
+
+describe("BridgeGame 클래스 테스트", () => {
+  test("사용자가 칸을 이동하는 경우", () => {
+    const bridgeGame = new BridgeGame(["U", "U", "D"]);
+    let [canCross, upperBridge, lowerBridge] = bridgeGame.move("U");
+
+    expect(canCross).toBe(true);
+    expect(upperBridge).toEqual(["O"]);
+    expect(lowerBridge).toEqual([" "]);
+
+    [canCross, upperBridge, lowerBridge] = bridgeGame.move("D");
+
+    expect(canCross).toBe(false);
+    expect(upperBridge).toEqual(["O", " "]);
+    expect(lowerBridge).toEqual([" ", "X"]);
+  });
+
+  test("사용자가 게임을 재시작하는 경우", () => {
+    const bridgeGame = new BridgeGame(["U", "U", "D"]);
+    bridgeGame.retry();
+    expect(bridgeGame.attempsCount).toEqual(2);
+  });
+});
