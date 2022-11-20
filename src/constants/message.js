9cd266316c1c131794a3f9a4df8a6dbf6cd83a6a
somin <smhs12@naver.com>
2022-11-21 01:42:52 +0900

    feat: 메시지 상수 줄바꿈 수정

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,6 +1,6 @@
 const INPUT_MESSAGE = Object.freeze({
   BRIDGE_LENGTH: "다리의 길이를 입력해주세요.\n",
-  MOVE: "이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
+  MOVE: "\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
   RETRY: "게임을 다시 시도할지 여부를 입력해주세요.(재시도: R, 종료: Q)\n",
 });
 
@@ -11,4 +11,4 @@ const OUTPUT_MESSAGE = Object.freeze({
   ATTEMPT_COUNT: (count) => `총 시도한 횟수: ${count}`,
 });
 
-const ERROR_MESSAGE = Object.freeze({});
+module.exports = { INPUT_MESSAGE, OUTPUT_MESSAGE };
