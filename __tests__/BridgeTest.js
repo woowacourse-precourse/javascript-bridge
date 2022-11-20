9de152521f8369bbc77bc7d93c6a253792be7c9f
woo-jk <201702042@o.cnu.ac.kr>
2022-11-20 23:45:09 +0900

    test: Bridge 클래스 플레이어가 다리에 끝에 섰는지 확인하는 테스트 코드 작성

--- __tests__/BridgeTest.js
+++ __tests__/BridgeTest.js
@@ -18,7 +18,7 @@ describe("다리 클래스 테스트", () => {
     }).toThrow("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
   });
 
-  test("다리의 다음 칸 방향 얻기", () => {
+  test("플레이어의 다음 칸 방향 얻기", () => {
     const path = ["U", "U", "D", "D", "U"];
 
     const bridge = new Bridge(path);
@@ -33,5 +33,21 @@ describe("다리 클래스 테스트", () => {
     expect(directionFirst).toEqual("U");
     expect(directionSecond).toEqual("U");
     expect(directionThird).toEqual("D");
+  });
+
+  test("플레이어가 다리의 끝에 섰는지 확인", () => {
+    const path = ["U", "U", "U"];
+
+    const bridge = new Bridge(path);
+    const player = new Player();
+
+    player.move("U");
+    player.move("U");
+    const twiceMove = bridge.isLastPosition(player);
+    player.move("U");
+    const threeTimesMove = bridge.isLastPosition(player);
+
+    expect(twiceMove).toBeFalsy();
+    expect(threeTimesMove).toBeTruthy();
   });
 });
