82c1a123bfeee426bfe897e2679afb76f8da9061
unknown <sjjoseph106@gmail.com>
2022-11-20 20:21:16 +0900

    test: checkMovingValid 테스트 코드 추가

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -1,6 +1,6 @@
 const Validator = require("../src/Validator");
 
-describe("Validator 테스트", () => {
+describe("checkBridgeSizeValid 테스트", () => {
   test("다리 길이가 숫자가 아니라면 예외가 발생한다.", () => {
     expect(() => {
       Validator.checkBridgeSizeValid("삼");
@@ -14,6 +14,14 @@ describe("Validator 테스트", () => {
   test("다리 길이가 20보다 크다면 예외가 발생한다.", () => {
     expect(() => {
       Validator.checkBridgeSizeValid(21);
+    }).toThrow("[ERROR]");
+  });
+});
+
+describe("checkMovingValid 테스트", () => {
+  test("플레이어가 이동할 칸이 U 또는 R이 아니라면 예외가 발생한다.", () => {
+    expect(() => {
+      Validator.checkMovingValid("u");
     }).toThrow("[ERROR]");
   });
 });
