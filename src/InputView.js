a1339538ea0a6f02baba9abaf1c357f518feb7cf
Sangjun-man <molung@icloud.com>
2022-11-20 17:49:30 +0900

    refactor(InputView) : 인풋값 리턴방식 수정
    입력값을 그대로 리턴해주는 inputHelper 함수의 값 제공 방식을 수정했습니다
    - 변경된 내용
      기존 방식 :  inputHelper 함수가 this.inputContainer을 수정하면 메소드들이 그 값을 참조해서 사용하는 방식
      변경 방식 : inputHelper 함수 내에서 this.inputContainer 참조 후 그 값 리턴, this.inputContainer 다시 null로 초기화
    - this.inputContainer는 null값을 항상 유지하므로 혹시모를 사이드 이펙트 방지

--- src/InputView.js
+++ src/InputView.js
@@ -11,8 +11,7 @@ const InputView = {
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize() {
-    this.inputHelper(InputMessage.readBridgeSize);
-    const bridgeSize = Number(this.inputContainer);
+    const bridgeSize = Number(this.inputHelper(InputMessage.readBridgeSize));
     this.validate.isNumber(bridgeSize);
     this.validate.isNumberIntheRange([3, 20], bridgeSize);
     return bridgeSize;
@@ -22,8 +21,7 @@ const InputView = {
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving() {
-    this.inputHelper(InputMessage.readMoving);
-    const move = this.inputContainer;
+    const move = this.inputHelper(InputMessage.readMoving);
     this.validate.isStringIntheList(["U", "D"], move);
     return move;
   },
@@ -32,8 +30,7 @@ const InputView = {
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand() {
-    this.inputHelper(InputMessage.readGamdCommand);
-    const command = this.inputContainer;
+    const command = this.inputHelper(InputMessage.readGamdCommand);
     this.validate.isStringIntheList(["R", "Q"], command);
     return command;
   },
@@ -42,6 +39,9 @@ const InputView = {
     MissionUtils.Console.readLine(message, (input) => {
       this.inputContainer = input;
     });
+    const buffer = this.inputContainer;
+    this.inputContainer = null;
+    return buffer;
   },
 };
 
