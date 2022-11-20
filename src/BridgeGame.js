43fcc14952278992e767a1b678a4e6a40877790d
JSH99 <tjgus9966@naver.com>
2022-11-20 22:06:25 +0900

    feat(retry): 재시도 여부 기능 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -64,7 +64,16 @@ class BridgeGame {
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  retry() {}
+  retry(gameCommandInput) {
+    if (gameCommandInput === 'Q') {
+      return false;
+    }
+    return true;
+  }
+
+  checkGameSuccess() {}
+
+  checkTryCount() {}
 }
 
 module.exports = BridgeGame;
