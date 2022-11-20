2088e9339e2a7bd8a78051b6b7d438992deec3ec
nerdchanii <nerdchanii@gmail.com>
2022-11-21 01:06:13 +0900

    feat(usecase): 게임이 끝났거나, 중간에 실패했는지 판단하는  인터렉터 추가

---
+++ src/domain/usecases/JudgeInteractor.js
@@ -0,0 +1,28 @@
+const BridgeInteractor = require('./BridgeInteractor');
+
+/**
+ * this class depends on the Interactor interface
+ * - bridgeInteractor
+ * - userInteractor
+ */
+class JudgeInteractor {
+  #bridge;
+  #user;
+
+  constructor(userInteractor, bridgeInteractor) {
+    this.#user = userInteractor;
+    this.#bridge = bridgeInteractor;
+  }
+
+  judge() {
+    const userLocation = this.#user.getLocation();
+    const BridgeSpace = this.#bridge.getSpace(userLocation);
+    return BridgeSpace === this.#user.getCurrentDirection();
+  }
+
+  isSucceed() {
+    return this.#bridge.getSize() === this.#user.getLocation();
+  }
+}
+
+module.exports = JudgeInteractor;
