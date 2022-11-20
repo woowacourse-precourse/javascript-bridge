5b9edda3007e2ecca5169b826a8c4b29746cc792
Chanwoong1 <jcw7468@gmail.com>
2022-11-20 20:40:42 +0900

    refactor: inputErrorProcess 클래스 생성으로 인한 코드 리펙토링

---
+++ src/domain/InputErrorProcess.js
@@ -0,0 +1,21 @@
+const { ERROR_MESSAGES } = require("../constants/constant");
+const OutputView = require("../ui/OutputView");
+const GameInfo = require("./GameInfo");
+
+class InputErrorProcess {
+  constructor() {
+
+  }
+
+  inputErrorProcess(validClass, inputValue, objectCode) {
+    try {
+      GameInfo[objectCode] = new validClass(inputValue)[objectCode];
+    } catch {
+      OutputView.printMessage(ERROR_MESSAGES[objectCode]);
+      return false;
+    }
+    return true;
+  }
+}
+
+module.exports = InputErrorProcess;
