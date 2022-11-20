007e2d8cfc67b27da701687fc79fd4b0b0c5720d
useon <yoosk4986@naver.com>
2022-11-21 05:03:35 +0900

    feat: 사용자가 이동할 때마다 실행 결과를 출력한다

--- src/BridgeGameController.js
+++ src/BridgeGameController.js
@@ -1,16 +1,18 @@
 const BridgeGame = require('./BridgeGame');
 const BridgeMaker = require('./BridgeMaker');
 const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
+const OutputView = require('./OutputView');
 
 const BridgeGameController = {
   getSize(value) {
     this.bridgeSize = Number(value);
-    this.bridgeMap = [];
+    this.upper = [];
+    this.lower = [];
     this.getRightBlocks();
   },
   getMoving(block, index) {
     this.chooseBlock = block;
-    this.mapData(index);
+    this.getMove(index);
     return this.isPass;
   },
   getRightBlocks() {
@@ -19,18 +21,37 @@ const BridgeGameController = {
       BridgeRandomNumberGenerator
     );
   },
-  mapData(index) {
-    this.getMove(index);
-    if (this.isPass) this.bridgeMap.push('O');
-    if (this.isPass === false) {
-      this.bridgeMap.push('X');
-    }
-  },
   getMove(index) {
     this.isPass = new BridgeGame().move(
       this.chooseBlock,
       this.rightBlocks[index]
     );
+    if (this.isPass) this.passData();
+    if (this.isPass === false) this.failData();
+    this.mapOutputData();
+  },
+  passData() {
+    if (this.isPass && this.chooseBlock === 'U') {
+      this.upper.push('O');
+      this.lower.push(' ');
+    }
+    if (this.isPass && this.chooseBlock === 'D') {
+      this.upper.push(' ');
+      this.lower.push('O');
+    }
+  },
+  failData() {
+    if (this.isPass === false && this.chooseBlock === 'U') {
+      this.upper.push('X');
+      this.lower.push(' ');
+    }
+    if (this.isPass == false && this.chooseBlock === 'D') {
+      this.upper.push(' ');
+      this.lower.push('X');
+    }
+  },
+  mapOutputData() {
+    OutputView.printMap(this.upper, this.lower);
   },
 };
 
