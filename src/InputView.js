da76983b226e4199f96389b1570f3a454bbd5ce4
pione3r <jhyong0626@gmail.com>
2022-11-20 18:50:31 +0900

    feat: add readBridgeSize function and validateBridgeSize function
    - readBridgeSize function
      - read user input about bridge size
      - try validate function
      - set bridgeSize property
    - validateBridgeSize function
      - if size over 20 or under 3 thrwo error

--- src/InputView.js
+++ src/InputView.js
@@ -1,11 +1,29 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
+  bridgeSize: 0,
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    MissionUtils.Console.readLine("다리의 길이를 입력해주세요.", (answer) => {
+      try {
+        this.validateBridgeSize(answer);
+      } catch (error) {
+        MissionUtils.Console.print(error);
+        this.readBridgeSize();
+      }
+      this.bridgeSize = answer;
+    });
+  },
+
+  validateBridgeSize(size) {
+    if (size < 3 || size > 20)
+      throw new Error("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
