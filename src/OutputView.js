25d4f9186f3cae2c832558f2eacd031394dba7d4
JSH99 <tjgus9966@naver.com>
2022-11-20 23:03:01 +0900

    feat(printResult): 최종 게임 결과 출력 기능 구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -21,7 +21,13 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() {},
+  printResult(currentBridge) {
+    MissionUtils.Console.print('최종 게임 결과\n');
+    let upBridge = currentBridge[0].join(' | ');
+    let downBridge = currentBridge[1].join(' | ');
+    MissionUtils.Console.print(`[ ${upBridge} ]\n`);
+    MissionUtils.Console.print(`[ ${downBridge} ]`);
+  },
 
   printGameSuccess(gameSuccess) {
     const result = gameSuccess ? '성공' : '실패';
