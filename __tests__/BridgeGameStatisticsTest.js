360ed9ab0118c020351529dd876e04aeb015b555
kangju2000 <kangju2000@naver.com>
2022-11-20 15:51:47 +0900

    test(BridgeGameStatisticsTest): BridgeGameStatistics 테스트 코드 작성

---
+++ __tests__/BridgeGameStatisticsTest.js
@@ -0,0 +1,22 @@
+const BridgeGameStatistics = require('../src/Model/BridgeGameStatistics');
+
+describe('BridgeGameStatistics 테스트', () => {
+  test('시도한 횟수를 3번 증가', () => {
+    const bridgeSize = 5;
+    const statistics = new BridgeGameStatistics(bridgeSize);
+    statistics.increaseAttempt();
+    statistics.increaseAttempt();
+    statistics.increaseAttempt();
+    expect(statistics.getAttempt()).toBe(3);
+  });
+
+  test('다리 끝까지 도달했는지 확인(게임 성공)', () => {
+    const bridgeSize = 5;
+    const statistics = new BridgeGameStatistics(bridgeSize);
+    for (let i = 0; i < bridgeSize; i++) {
+      statistics.increaseMoveCount();
+    }
+
+    expect(statistics.isGameWin()).toBeTruthy();
+  });
+});
