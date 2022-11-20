67966daa60e9b2d24fa898b769f2738404dd5fea
tkdrb12 <tkdrb15@gmail.com>
2022-11-20 22:20:24 +0900

    feat : 게임을 제어하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const { Console } = MissionUtils;
+const BridgeGame = require("./BridgeGame.js");
+const InputView = require("./InputView.js");
+
 class App {
-  play() {}
+  #bridgeGame;
+
+  play() {
+    Console.print('다리 건너기 게임을 시작합니다.\n');
+    this.start();
+  }
+
+  start(){
+    InputView.readBridgeSize((size)=>{
+      this.#bridgeGame = new BridgeGame(size);
+      this.selectMove();
+    });
+  }
+
+  selectMove(){
+    InputView.readMoving((playerMoving)=>{
+      this.#bridgeGame.move(playerMoving);
+    });
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
