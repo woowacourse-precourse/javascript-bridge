de054f5b1b77e6830177b893b4cf877ce2c08583
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-20 15:15:56 +0900

    :sparkles: feat : MVC 패턴으로 클래스 사용하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+const BridgeGame = require('./Controller/BridgeGame');
+const Model = require('./Model');
+const View = require('./View');
+
 class App {
-  play() {}
+  model;
+
+  view;
+
+  constructor() {
+    this.model = new Model();
+    this.view = new View();
+    this.controller = new BridgeGame(this.model, this.view);
+  }
+
+  play() {
+    this.controller.start();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
