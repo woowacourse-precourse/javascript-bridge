fa7b047825d7f439f774e2c362f1f640155c712d
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-11-20 20:56:10 +0900

    refactor: 이동할 칸을 입력 받는 값에 대한 예외 처리 테스트에서 반복되는 코드 함수화

--- __tests__/CurrBridgeTest.js
+++ __tests__/CurrBridgeTest.js
@@ -1,20 +1,25 @@
 const CurrBridge = require('../src/CurrBridge.js');
 
+const validateDirection = (input) => {
+  const currBridge = new CurrBridge();
+  return currBridge.validate(input);
+};
+
 describe('CurrBridge 클래스 테스트', () => {
   test('예외 테스트 : 입력값이 문자가 아닌 경우 예외 처리한다', () => {
-    expect(() => new CurrBridge('17')).toThrow(
+    expect(() => validateDirection('17')).toThrow(
       '[ERROR] 숫자를 제외한 문자를 입력해주세요.'
     );
   });
 
   test('예외 테스트 : 입력값이 U 또는 D가 아닌 경우 예외 처리한다', () => {
-    expect(() => new CurrBridge('R')).toThrow(
-      '[ERROR] U (위칸) 와 D (아래칸) 중에서만 입력해주세요.'
+    expect(() => validateDirection('R')).toThrow(
+      '[ERROR] U (위칸) 와 D (아래칸) 중에서만 이동할 칸을 정해 입력해주세요.'
     );
   });
 
   test('예외 테스트 : 입력값이 대문자가 아닌 소문자(u 또는 d)인 경우 예외 처리한다', () => {
-    expect(() => new CurrBridge('u')).toThrow(
+    expect(() => validateDirection('u')).toThrow(
       '[ERROR] 소문자가 아닌 대문자를 입력해주세요.'
     );
   });
