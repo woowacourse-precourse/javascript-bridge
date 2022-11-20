1d31d77fd9dc9754ceffd12cc16ddcb8209574d3
Sukyeong Park <zztnrudzz13@naver.com>
2022-11-20 22:48:18 +0900

    feat(Constants): add constants for the strings used when the game ends

--- src/Constants.js
+++ src/Constants.js
@@ -16,9 +16,17 @@ const INPUT = {
 };
 
 const ERROR = {
+  prefix: '[ERROR] ',
   bridgeSizeException: '다리 길이는 3부터 20 사이의 숫자여야 합니다.',
   movingException: 'U(위 칸)만 D(아래 칸)만 입력해 주세요.',
   gameCommandException: 'R(재시도)과 Q(종료)만 입력해 주세요.',
+};
+
+const GAME = {
+  end: '최종 게임 결과',
+  success: '게임 성공 여부: 성공',
+  fail: '게임 성공 여부: 실패',
+  try: '총 시도한 횟수: ',
 };
 
 module.exports = {
@@ -26,4 +34,5 @@ module.exports = {
   MOVE_INDEX,
   INPUT,
   ERROR,
+  GAME,
 };
