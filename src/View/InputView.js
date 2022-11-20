e204068370744fde7221a30fb48ddb979dccb032
kangju2000 <kangju2000@naver.com>
2022-11-20 19:19:23 +0900

    feat(InputView): InputView 객체 구현

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -1,21 +1,27 @@
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+const { Console } = require('@woowacourse/mission-utils');
+const MESSAGE = require('../constants/message');
+
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize(callback) {
+    Console.readLine(MESSAGE.INPUT.BRIDGE_SIZE, (bridgeSize) => callback(bridgeSize));
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving(callback) {
+    Console.readLine(MESSAGE.INPUT.MOVING, (moving) => callback(moving));
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand(callback) {
+    Console.readLine(MESSAGE.INPUT.GAME_COMMAND, (gameCommand) => callback(gameCommand));
+  },
 };
 
 module.exports = InputView;
