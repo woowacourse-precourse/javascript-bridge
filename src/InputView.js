acc942a5377d40e76b1956aa15b20e1d6f075bf4
jongwonp <jw032876@gmail.com>
2022-11-20 13:54:27 +0900

    refactor: InputView 파일 리팩토링

--- src/InputView.js
+++ src/InputView.js
@@ -5,16 +5,14 @@ const BridgeMaker = require('./BridgeMaker');
 const makeBridge = BridgeMaker.makeBridge;
 const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
 const generate = BridgeRandomNumberGenerator.generate;
+const validator = require('./validators');
 
 const InputView = {
   readBridgeSize() {
     Console.readLine('다리의 길이를 입력해 주세요.', (num) => {
       try {
-        is3To20(num);
-        const bridge = makeBridge(num, generate);
-        const bridgeGame = new BridgeGame(bridge);
-
-        this.readMoving(bridgeGame);
+        validator.is3To20(num);
+        this.readMoving(setBridgeGame(num));
       } catch (error) {
         Console.print(error);
         InputView.readBridgeSize();
@@ -25,22 +23,8 @@ const InputView = {
   readMoving(bridgeGame) {
     Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)', (input) => {
       try {
-        isUorD(input);
-
-        const numberOfTry = bridgeGame.getNumberOfTry();
-        const [UBlock, DBlock] = bridgeGame.move(input);
-        OutputView.printMap(UBlock, DBlock);
-
-        if (UBlock.includes('X') || DBlock.includes('X')) {
-          this.readGameCommand(bridgeGame, [UBlock, DBlock], numberOfTry);
-        }
-
-        if (!bridgeGame.isFinish()) {
-          this.readMoving(bridgeGame);
-        }
-        if (bridgeGame.isFinish()) {
-          OutputView.printResult('성공', [UBlock, DBlock], numberOfTry);
-        }
+        validator.isUorD(input);
+        moveAndCheck(input, bridgeGame);
       } catch (error) {
         Console.print(error);
         InputView.readMoving(bridgeGame);
@@ -48,48 +32,56 @@ const InputView = {
     });
   },
 
-  readGameCommand(bridgeGame, blocks, numberOfTry) {
-    Console.readLine(
-      '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)',
-      (input) => {
-        try {
-          isRorQ(input);
-          if (input === 'R') {
-            bridgeGame.retry();
-            this.readMoving(bridgeGame);
-          }
-          if (input === 'Q') {
-            OutputView.printResult('실패', blocks, numberOfTry);
-          }
-        } catch (error) {
-          Console.print(error);
-          InputView.readGameCommand(bridgeGame, blocks, numberOfTry);
-        }
+  readGameCommand(bridgeGame, blocks) {
+    Console.readLine('재시도는 R을, 종료는 Q를 입력해주세요.', (input) => {
+      try {
+        validator.isRorQ(input);
+        retryOrQuit(input, bridgeGame, blocks);
+      } catch (error) {
+        Console.print(error);
+        InputView.readGameCommand(bridgeGame, blocks);
       }
-    );
+    });
   },
 };
 
-const is3To20 = (input) => {
-  const number = Number(input);
-  if (isNaN(number) === false && number >= 3 && number <= 20) {
-    return true;
-  }
-  throw '[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.';
+const setBridgeGame = (num) => {
+  const bridge = makeBridge(num, generate);
+  const bridgeGame = new BridgeGame(bridge);
+  return bridgeGame;
 };
 
-const isUorD = (input) => {
-  if (input === 'U' || input === 'D') {
-    return true;
-  }
-  throw '[ERROR] 이동할 칸은 위는 "U", 아래는 "D" 입니다.';
+const moveAndCheck = (input, bridgeGame) => {
+  const [UBlock, DBlock] = bridgeGame.move(input);
+
+  OutputView.printMap(UBlock, DBlock);
+  isFailed(bridgeGame, [UBlock, DBlock]);
+  moveOrFinish(bridgeGame, [UBlock, DBlock]);
 };
 
-const isRorQ = (input) => {
-  if (input === 'R' || input === 'Q') {
-    return true;
+const moveOrFinish = (bridgeGame, blocks) => {
+  if (!bridgeGame.isFinish()) {
+    InputView.readMoving(bridgeGame);
   }
-  throw '[ERROR] 재시도는 "R", 종료는 "Q"입니다.';
+  if (bridgeGame.isFinish()) {
+    OutputView.printResult('성공', blocks, bridgeGame.getNumberOfTry());
+  }
+};
+
+const isFailed = (bridgeGame, blocks) => {
+  if (blocks[0].includes('X') || blocks[1].includes('X')) {
+    InputView.readGameCommand(bridgeGame, blocks);
+  }
+};
+
+const retryOrQuit = (input, bridgeGame, blocks) => {
+  if (input === 'R') {
+    bridgeGame.retry();
+    InputView.readMoving(bridgeGame);
+  }
+  if (input === 'Q') {
+    OutputView.printResult('실패', blocks, bridgeGame.getNumberOfTry());
+  }
 };
 
 module.exports = InputView;
