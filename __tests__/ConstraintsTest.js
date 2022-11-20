16f3196898bbf02e1086dd059badc4aa36e62b22
Rki0 <pky00823@gmail.com>
2022-11-20 20:19:18 +0900

    refactor(ConstraintsTest): 테스트 통합
     하나의 클래스에서 여러 입력값에 대한 테스트를 진행한다.
    비슷한 코드가 반복되므로 이를 하나의 테스트로 통합하여 진행한다.

--- __tests__/ConstraintsTest.js
+++ __tests__/ConstraintsTest.js
@@ -5,25 +5,13 @@ const {
 } = require("../src/Constraints");
 
 describe("제한 조건 테스트", () => {
-  test("숫자가 아닌 다리 길이 입력값 테스트", () => {
-    expect(() => {
-      const sizeConstraints = new SizeConstraints("3 ");
+  test.each(["3 ", "1", "04"])("다리 길이 입력값 예외 처리", (input) => {
+    expect((input) => {
+      const sizeConstraints = new SizeConstraints(input);
       sizeConstraints.checkOnlyNumber();
-    }).toThrow("[ERROR] 숫자만 입력 가능합니다.");
-  });
-
-  test("범위 외 다리 길이 입력값 테스트", () => {
-    expect(() => {
-      const sizeConstraints = new SizeConstraints("1");
       sizeConstraints.checkNumberRange();
-    }).toThrow("[ERROR] 다리 길이는 3 이상 20 이하만 가능합니다.");
-  });
-
-  test("0으로 시작되는 다리 길이 입력값 테스트", () => {
-    expect(() => {
-      const sizeConstraints = new SizeConstraints("04");
       sizeConstraints.checkStartZero();
-    }).toThrow("[ERROR] 0으로 시작하는 숫자는 입력할 수 없습니다.");
+    }).toThrow();
   });
 
   test("움직임 입력에 U나 D가 아닌 입력값 테스트", () => {
