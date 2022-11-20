228fe1f3e2b9e84518e20df7fc5ae640387825b8
chasj0326@naver.com <62418379+chasj0326@users.noreply.github.com>
2022-11-20 22:55:46 +0900

    feat: 다리 길이 입력 구현 (App.js, InputView.js)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 const OutputView = require("./OutputView");
+const InputView = require("./InputView");
 
 class App {
   start() {
     OutputView.printStart();
+    InputView.readBridgeSize(this);
+  }
+  init(bridgeSize){
+    console.log(bridgeSize);//test
   }
   play() {
     this.start();
@@ -10,3 +15,7 @@ class App {
 }
 
 module.exports = App;
+
+//test app
+const app = new App();
+app.play();
