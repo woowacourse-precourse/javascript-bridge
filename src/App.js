e7ee4d372e09dc3c28302254552ab6b717a26edf
Vo9Ming <kimminj2017@gmail.com>
2022-11-20 19:53:13 +0900

    다리 길이를 사용자에게 입력 받기 위해 readBridgeSize() 함수와 예외 처리를 위한 checkSize()함수를 만들어 처리한다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const InputView = require("./InputView");
+const OutputView = require("./OutputView");
+
 class App {
-  play() {}
+  play() {
+    OutputView.printStart();
+    InputView.readBridgeSize();
+  }
 }
 
+const app = new App();
+console.log(app.play());
 module.exports = App;
--- src/InputView.js
+++ src/InputView.js
@@ -1,21 +1,37 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
+  constructor() {
+    this.size = 0;
+  },
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    MissionUtils.Console.readLine("다리의 길이를 입력해주세요.", (length) => {
+      this.size = length;
+      this.checkSize(this.size);
+    });
+  },
 
+  checkSize(size) {
+    if (size < 3 || size > 20) {
+      throw new Error("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+    }
+    return size;
+  },
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  //readMoving() {},
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  //readGameCommand() {},
 };
 
 module.exports = InputView;
