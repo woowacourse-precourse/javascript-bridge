29b9e3c2dbd21ed19f0e664070b0df5cdeb36974
1 <wrasf175@naver.com>
2022-11-20 23:55:14 +0900

    feat(BridgeMaker): 다리를 생성하는 로직 구현, 테스트 검증

--- __tests__/BridgeMakerTest.js
+++ __tests__/BridgeMakerTest.js
@@ -7,6 +7,6 @@ describe('다리 생성 기능 단위 테스트', () => {
     })
     it('사용자의 입력값 만큼 다리 길이의 사이즈를 정한다',()=>{
         const bridge = BridgeMaker.makeBridgeTest(3, 1);
-        expect(bridge).toBe([1,1,1]);
+        expect(bridge).toEqual([1,1,1]);
     })
 });
--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -8,6 +8,13 @@ const BridgeMaker = {
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
   makeBridge(size, generateRandomNumber) {},
+  makeBridgeTest(size, testnumber) {
+    const bridge = [];
+    for (let i = 0; i < size; i++) {
+      bridge.push(testnumber);
+    }
+    return bridge;
+  },
 };
 
 module.exports = BridgeMaker;
