836baee04e3db5c1973c8005fca7954a30b5d455
dhkham <khamdong@gmail.com>
2022-11-21 00:44:42 +0900

    feat(BridgeMaker): feat(BridgeMaker): make bridge of designated size
    - randomly generate 0 or 1 and convert it to "D" or "U"

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -7,7 +7,14 @@ const BridgeMaker = {
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
-  makeBridge(size, generateRandomNumber) {},
+  makeBridge(size, generateRandomNumber) {
+    const bridge = [];
+    for (let i = 0; i < size; i += 1) {
+      const number = generateRandomNumber(); // 0 or 1
+      bridge.push(number.toString()); // ["1", "0", "0"]
+    }
+    return bridge.map(number => (number === "1" ? "U" : "D")); // bridge = ["1", "0", "0"] -> ["U", "D", "D"]
+  },
 };
 
 module.exports = BridgeMaker;
