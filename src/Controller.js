b7c13fbb6acb38d9b6abe9f820d4b6f10164482b
nuheajiohc <jaehun341012@gmail.com>
2022-11-20 10:54:37 +0900

    refactor: 이동할 칸 입력 기능 리팩토링

--- src/Controller.js
+++ src/Controller.js
@@ -1,7 +1,7 @@
 const { printMessage } = require('./Utils.js');
 const BridgeSize = require('./Validate/BridgeSize.js');
+const SafeBridge = require('./Validate/SafeBridge.js');
 const InputView = require('./View/InputView.js');
-const OutputView = require('./View/OutputView.js');
 
 class Controller {
   constructor() {}
@@ -16,9 +16,26 @@ class Controller {
   checkReInputSize(bridgeSize) {
     try {
       bridgeSize.validate();
+      this.inputMoving();
     } catch (errorMessage) {
       printMessage(errorMessage);
       this.InputBridgeSize();
+    }
+  }
+
+  inputMoving() {
+    InputView.readMoving((command) => {
+      const safeBridge = new SafeBridge(command);
+      this.checkReInputMoving(safeBridge);
+    });
+  }
+
+  checkReInputMoving(safeBridge) {
+    try {
+      safeBridge.validate();
+    } catch (errorMessage) {
+      printMessage(errorMessage);
+      this.inputMoving();
     }
   }
 }
