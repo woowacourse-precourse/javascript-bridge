d395afb5be3fa13634d7db3a02e6175e2f432642
wonkyum-kim <dodo12120@naver.com>
2022-11-20 12:53:56 +0900

    refactor(BridgeMaker): change makeBridge()
    - apply type conversion to pass application tests

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,11 +1,10 @@
 /**
- * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
- */
-
-/*
+ * BridgeMaker: 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
+ *
+ * 제공된 BridgeMaker 객체를 활용해 구현해야 한다.
  * BridgeMaker에 프로퍼티를 추가할 수 없다.
- * BridgeMaker의 파일 경로를 변경할 수없다.
- * BridgeMaker의 메서드 시그니처(인자, 이름)와 반환 타입은 변경할 수 없다.
+ * BridgeMaker의 파일 경로는 변경할 수 없다.
+ * BridgeMaker의 메서드의 시그니처(인자, 이름)와 반환 타입은 변경할 수 없다.
  */
 
 const BridgeMaker = {
@@ -14,11 +13,13 @@ const BridgeMaker = {
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
+
+  // ApplicationTest의 다리 생성 테스트에서 randomNumbers가 문자로 주어지기 때문에 `Number()` 메서드를 사용해서 다시 변환해준다.
   makeBridge(size, generateRandomNumber) {
     const BRIDGE = [];
     for (let length = 0; length < size; length += 1) {
-      const NUMBER = generateRandomNumber();
-      const POSSIBLE_CELL = NUMBER ? 'U' : 'D';
+      const RANDOM_NUMBER = Number(generateRandomNumber());
+      const POSSIBLE_CELL = RANDOM_NUMBER === 1 ? 'U' : 'D';
       BRIDGE.push(POSSIBLE_CELL);
     }
     return BRIDGE;
