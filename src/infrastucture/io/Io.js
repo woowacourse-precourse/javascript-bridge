cc5796ba0d2f686f08ec10d8e45fa1b93afccb03
nerdchanii <nerdchanii@gmail.com>
2022-11-21 01:13:31 +0900

    style(io): eslint 적용

--- src/infrastucture/io/Io.js
+++ src/infrastucture/io/Io.js
@@ -1,12 +1,11 @@
 const { Console } = require('@woowacourse/mission-utils');
 
 const Io = Object.freeze({
-  input(message, callback){
+  input(message, callback) {
     Console.readLine(message, callback);
   },
-  output(message){
+  output(message) {
     Console.print(message);
-  }
-})
+  },
 
 module.exports = Io;
