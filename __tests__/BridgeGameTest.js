d8d5accaa169d5a47630f8a8e1dc46697ecb9542
ReturnReason <reasonz@daum.net>
2022-11-20 20:13:03 +0900

    test(BridgeGame): makeBridgeMap 메서드 테스트

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -7,4 +7,21 @@ describe('BridgeGame 클래스 테스트', () => {
 
     expect(bridgeGame.makeBridgeMap(mapSize).length).toBe(mapSize);
   });
+
+  test('⭐ 게임 맵 테스트 : makeBridgeMap 메서드 테스트 U와 D로 이루어진 게임 맵을 반환합니다. ', () => {
+    const bridgeGame = new BridgeGame();
+    const bridgeMap = [
+      ['U', 'U', 'D'],
+      ['U', 'U', 'U'],
+      ['D', 'D', 'D'],
+      ['D', 'U', 'D'],
+    ];
+    const mapSize = 3;
+
+    const mockFn = jest.fn(bridgeGame.makeBridgeMap);
+    bridgeMap.forEach((map, idx) => {
+      mockFn.mockReturnValue(map);
+      expect(mockFn(mapSize)).toEqual(bridgeMap[idx]);
+    });
+  });
 });
