585a6c5a1dc72073245f2a93c025a4e9dabdc05d
OrangeVinyl <hap_hazard@naver.com>
2022-11-21 05:22:20 +0900

    fix: U/D 입력 예외사항 오류 해결

--- src/validate/CheckUD.js
+++ src/validate/CheckUD.js
@@ -2,8 +2,19 @@
 const { ERROR } = require('../util/Constant');
 
 class CheckUD {
-  validate(size) {
-    if (size.match(/[^UD]/g) !== null) {
+  validate(ud) {
+    this.checkUorD(ud);
+    this.checkOneCharacter(ud);
+  }
+
+  checkUorD(ud) {
+    if (ud.match(/[^UD]/g) !== null) {
+      throw new Error(ERROR.UPDOWN_WRONG);
+    }
+  }
+
+  checkOneCharacter(ud) {
+    if (ud.length > 1) {
       throw new Error(ERROR.UPDOWN_WRONG);
     }
   }
