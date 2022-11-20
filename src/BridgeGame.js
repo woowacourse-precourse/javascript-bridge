b8bee9df07d5b7a8a7daa70bbb7abed1832b271e
rovxxmjxx <miju3545@gmail.com>
2022-11-20 20:58:56 +0900

    feat(BridgeGame): 다리 생성

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,14 +1,21 @@
 const OutputView = require('./OutputView');
 const { MESSAGE } = require('./constants');
 const InputView = require('./InputView');
+const { generate: generateRandomNumber } = require('./BridgeRandomNumberGenerator');
+const BridgeMaker = require('./BridgeMaker');
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 
 class BridgeGame {
+  constructor() {
+    this.bridge = [];
+  }
+
   async proceed() {
     OutputView.printMessage(MESSAGE.ENTRY);
+    this.makeBridge();
   }
 
   async getBridgeSize() {
@@ -18,6 +25,12 @@ class BridgeGame {
       OutputView.printMessage(error.message);
       return this.getBridgeSize();
     }
+  }
+
+  async makeBridge() {
+    const size = await this.getBridgeSize();
+    const bridge = BridgeMaker.makeBridge(size, generateRandomNumber);
+    this.bridge = bridge;
   }
 }
 
