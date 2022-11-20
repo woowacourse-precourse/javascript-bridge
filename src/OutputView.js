3a2b8de5b6cd170cb399a2a7803f167769ae6ccf
JSH99 <tjgus9966@naver.com>
2022-11-20 22:54:39 +0900

    feat(printMap): 현재 다리 상태 출력 기능 구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -9,7 +9,12 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {},
+  printMap(currentBridge) {
+    let upBridge = currentBridge[0].join(' | ');
+    let downBridge = currentBridge[1].join(' | ');
+    MissionUtils.Console.print(`[ ${upBridge} ]\n`);
+    MissionUtils.Console.print(`[ ${downBridge} ]`);
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
