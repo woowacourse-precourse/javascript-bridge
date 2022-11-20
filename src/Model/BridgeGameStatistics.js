be4f01b21fef5d99950ea92f6322f5c0dee6b607
kangju2000 <kangju2000@naver.com>
2022-11-20 15:58:16 +0900

    feat(BridgeGameStatistics): BridgeGameStatistics 클래스 구현

---
+++ src/Model/BridgeGameStatistics.js
@@ -0,0 +1,33 @@
+class BridgeStatistics {
+  #bridgeSize;
+  #moveCount;
+  #attempt;
+
+  constructor(bridgeSize) {
+    this.#bridgeSize = bridgeSize;
+    this.#moveCount = 0;
+    this.#attempt = 0;
+  }
+
+  increaseAttempt() {
+    this.#attempt += 1;
+  }
+
+  increaseMoveCount() {
+    this.#moveCount += 1;
+  }
+
+  getAttempt() {
+    return this.#attempt;
+  }
+
+  isGameWin() {
+    return this.#bridgeSize === this.#moveCount;
+  }
+
+  resetMoveCount() {
+    this.#moveCount = 0;
+  }
+}
+
+module.exports = BridgeStatistics;
