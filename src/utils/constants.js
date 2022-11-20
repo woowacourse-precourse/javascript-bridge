d6a15c6875170fbbc09572ecdafd1092e42fd506
pp449 <mma7710@naver.com>
2022-11-20 18:01:49 +0900

    refactor: 가독성을 위해 MESSAGE 출력 내용에 줄바꿈 하나 추가

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 const MESSAGE = Object.freeze({
   GAMESTART: '다리 건너기 게임을 시작합니다.\n',
   SETBRIDGESIZE: '다리의 길이를 입력해주세요.\n',
-  WHERETOMOVE: '이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
+  WHERETOMOVE: '\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
 });
 
 module.exports = MESSAGE;
