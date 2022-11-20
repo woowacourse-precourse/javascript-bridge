2909da5a3f7df1c582b252223aee4a13e8a11eda
2pandi <coder.2pandi@gmail.com>
2022-11-20 21:05:36 +0900

    feat:(BridgeLength): add checkNaturality method

--- src/BridgeLength.js
+++ src/BridgeLength.js
@@ -3,12 +3,19 @@ class BridgeLength {
 
   constructor(number) {
     this.validate(number);
+    this.checkNaturality();
   }
 
   validate(number) {
     const isNumber = Number(number);
     if (!isNumber) throw "[ERROR] 다리 길이는 숫자여야 합니다.";
     this.#number = isNumber;
+  }
+
+  checkNaturality() {
+    const number = this.#number;
+    if (Math.floor(number) !== number)
+      throw "[ERROR] 다리 길이는 자연수여야 합니다.";
   }
 }
 
