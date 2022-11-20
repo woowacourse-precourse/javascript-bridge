9c1b8bd838b06d3daf60e1780ca0dc593aee1f2c
wonkyum-kim <dodo12120@naver.com>
2022-11-20 13:17:12 +0900

    style(IO): edit some comments

--- src/IO/OutputView.js
+++ src/IO/OutputView.js
@@ -1,8 +1,9 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 
 /**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
+ * OutputView: 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
+
 const OutputView = {
 
   upperBridge: '',
