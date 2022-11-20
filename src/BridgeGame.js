a69887d2635ba2c0d46cf3142b3fd7b0e55370ba
pp449 <mma7710@naver.com>
2022-11-20 17:59:31 +0900

    feat: 입력받은 이동할 칸이 U, D가 아닌경우 에러를 발생

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -6,16 +6,36 @@ const MESSAGE = require('./utils/constants');
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  #history = [];
+  #attempts;
+
+  constructor() {
+    this.#attempts = 0;
+  }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {
+  move(bridge) {
     Console.readLine(MESSAGE.WHERETOMOVE, (direction) => {
       InputView.readMoving(direction);
+      this.#checkMoveCorrectly(direction, bridge);
     });
   }
+
+  #checkMoveCorrectly(direction, bridge) {
+    if (bridge[this.#history.length] === direction)
+      this.#moveCorretly(direction, bridge);
+    else this.#moveInCorrectly();
+  }
+
+  #moveCorretly(direction, bridge) {
+    this.#history.push(direction);
+    console.log(this.#history);
+  }
+
+  #moveInCorrectly() {}
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
