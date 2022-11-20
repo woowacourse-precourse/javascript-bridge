b8fe023b5af3624a4b4064dc7bdea988d2425225
HWI <dlwognl99@naver.com>
2022-11-21 00:50:04 +0900

    fix: 유효성 검사 메서드 및 테스트 코드 에러 수정

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -17,42 +17,52 @@ const getLogSpy = () => {
   logSpy.mockClear();
   return logSpy;
 };
+const getOutput = (logSpy) => {
+  return [...logSpy.mock.calls].join("");
+};
+
+const runException = (inputs) => {
+  mockQuestions(inputs);
+  const logSpy = getLogSpy();
+  InputView.readBridgeSize();
+  expectLogContains(getOutput(logSpy), ["[ERROR]"]);
+};
+const expectLogContains = (received, logs) => {
+  logs.forEach((log) => {
+    expect(received).toEqual(expect.stringContaining(log));
+  });
+};
+const runExceptionMoving = (inputs) => {
+  mockQuestions(inputs);
+  const logSpy = getLogSpy();
+  InputView.readMoving();
+  expectLogContains(getOutput(logSpy), ["[ERROR]"]);
+};
+const runExceptionCommand = (inputs) => {
+  mockQuestions(inputs);
+  const logSpy = getLogSpy();
+  InputView.readGameCommand();
+  expectLogContains(getOutput(logSpy), ["[ERROR]"]);
+};
 
 describe("입력 테스트", () => {
-  test("다리 길이 확인", () => {
-    const logSpy = getLogSpy();
-    mockQuestions(["3"]);
-    InputView.readBridgeSize();
-    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining("3"));
-  });
-
   test("다리 길이 입력이 숫자가 아닌 경우 예외 처리한다.", () => {
-    expect(() => {
-      Validation.isOnlyNumber("a");
-    }).toThrow(ERROR_MESSAGE.NUMBER);
+    runException(["a"]);
   });
 
   test("다리 길이 입력 숫자가 3보다 작은 경우 예외 처리한다.", () => {
-    expect(() => {
-      Validation.isInRange("1");
-    }).toThrow(ERROR_MESSAGE.RANGE);
+    runException(["1"]);
   });
 
   test("다리 길이 입력 숫자가 20보다 큰 경우 예외 처리한다.", () => {
-    expect(() => {
-      Validation.isInRange("21");
-    }).toThrow(ERROR_MESSAGE.RANGE);
+    runException(["25"]);
   });
 
   test("이동 관련 유저의 입력이 대문자 U, D 가 아닌 경우 예외 처리한다.", () => {
-    expect(() => {
-      Validation.checkUserMoveInput("A");
-    }).toThrow(ERROR_MESSAGE.MOVE_INPUT);
+    runExceptionMoving(["A"]);
   });
 
   test("재시작 입력 관련 유저의 입력이 대문자 R, Q 가 아닌 경우 예외 처리한다.", () => {
-    expect(() => {
-      Validation.checkCommandInput("a");
-    }).toThrow(ERROR_MESSAGE.COMMAND);
+    runExceptionCommand(["a"]);
   });
 });
--- src/Validation.js
+++ src/Validation.js
@@ -1,27 +1,54 @@
+const { Console } = require("@woowacourse/mission-utils");
 const { LENGTH, BRIDGE, COMMAND } = require("./constants/data");
 const { ERROR_MESSAGE } = require("./constants/message");
 
 const Validation = {
   checkBridgeLength(length) {
-    this.isOnlyNumber(length);
-    this.isInRange(length);
+    const number = this.isOnlyNumber(length);
+    const range = this.isInRange(length);
+    if (number || range) {
+      return true;
+    }
+    return false;
   },
   isOnlyNumber(length) {
-    if (isNaN(length)) throw new Error(ERROR_MESSAGE.NUMBER);
+    try {
+      if (isNaN(length)) throw new Error(ERROR_MESSAGE.NUMBER);
+    } catch (e) {
+      Console.print(e.message);
+      return true;
+    }
   },
   isInRange(length) {
-    if (length < LENGTH.MIN || length > LENGTH.MAX)
-      throw new Error(ERROR_MESSAGE.RANGE);
+    try {
+      if (length < LENGTH.MIN || length > LENGTH.MAX)
+        throw new Error(ERROR_MESSAGE.RANGE);
+    } catch (e) {
+      Console.print(e.message);
+      return true;
+    }
   },
 
   checkUserMoveInput(userInput) {
-    if (userInput !== BRIDGE.LOWER_ZONE && userInput !== BRIDGE.UPPER_ZONE)
-      throw new Error(ERROR_MESSAGE.MOVE_INPUT);
+    try {
+      if (userInput !== BRIDGE.LOWER_ZONE && userInput !== BRIDGE.UPPER_ZONE)
+        throw new Error(ERROR_MESSAGE.MOVE_INPUT);
+    } catch (e) {
+      Console.print(e.message);
+      return true;
+    }
+    return false;
   },
 
   checkCommandInput(commandInput) {
-    if (commandInput !== COMMAND.RESTART && commandInput !== COMMAND.END)
-      throw new Error(ERROR_MESSAGE.COMMAND);
+    try {
+      if (commandInput !== COMMAND.RESTART && commandInput !== COMMAND.END)
+        throw new Error(ERROR_MESSAGE.COMMAND);
+    } catch (e) {
+      Console.print(e.message);
+      return true;
+    }
+    return false;
   },
 };
 
