a3c21a263b0e9da2f8b59bdf80456d8706de4038
seongminn <berry9901@gmail.com>
2022-11-20 16:18:56 +0900

    test(BridgeLengthTest): 다리 길이 입력 테스트 코드 추가

---
+++ __tests__/BridgeLengthTest.js
@@ -0,0 +1,14 @@
+const BridgeGameController = require('../src/controller/BridgeGameController');
+
+describe('다리 길이 입력 테스트', () => {
+  const gameController = new BridgeGameController();
+
+  test.each([['1'], ['1, 3'], ['a'], ['30'], ['3 '], ['1 7']])(
+    '3~20까지의 숫자 입력 테스트',
+    () => {
+      expect((input) => {
+        gameController.setBridge(input);
+      }).toThrow('[ERROR]');
+    }
+  );
+});
