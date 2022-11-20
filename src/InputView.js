bc1f7b8d259b4bb9eb9721ed713fd9f05eb9e7ff
vjvl95 <vjvl95@naver.com>
2022-11-20 23:54:24 +0900

    refactor : 게임 종료 확인 함수 추가

--- src/InputView.js
+++ src/InputView.js
@@ -4,6 +4,7 @@ const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
 const BridgeGame = require('./BridgeGame');
 const Check = require('./Check');
 const { INPUT_MESSAGES } = require('./common/messages');
+
 const InputView = {
   readBridgeSize() {
     MissionUtils.Console.readLine(`${INPUT_MESSAGES.INPUT_BRIDGESIZE}\n`, (size) => {
@@ -16,18 +17,28 @@ const InputView = {
   readMoving(gamePlay) {
     const playerInput = (input) => {
       Check.isVaildInput(input);
-      const isGameOver = gamePlay.move(input);
-      if (isGameOver) {
-        this.readGameCommand(gamePlay);
-      }
+      gamePlay.move(input);
+      if (this.checkGameOver(gamePlay) === 0) return;
       return this.readMoving(gamePlay);
     };
     MissionUtils.Console.readLine(`${INPUT_MESSAGES.INPUT_MOVE}`, playerInput);
   },
 
+  checkGameOver(gamePlay) {
+    if (gamePlay.getIsWinnging()) {
+      MissionUtils.Console.close();
+      return 0;
+    }
+    if (gamePlay.getIsGameOver()) {
+      this.readGameCommand(gamePlay);
+    }
+  },
+
   readGameCommand(gamePlay) {
     MissionUtils.Console.readLine(`${INPUT_MESSAGES.INPUT_RETRY}`, (answer) => {
-      if (Check.CheckRestartGame(answer, gamePlay)) this.readMoving(gamePlay);
+      if (Check.checkRestartGame(answer, gamePlay)) {
+        this.readMoving(gamePlay);
+      }
     });
   },
 };
