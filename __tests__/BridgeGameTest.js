2a67891a82f4b33c5204efd80ce51674e2df6deb
cys4585 <choizerowater@gmail.com>
2022-11-20 14:15:21 +0900

    test: 판정 테스트 (이동한 칸이 이동 가능한 칸인지, 끝까지 도착했는지)

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -2,8 +2,8 @@
 const BridgeGame = require("../src/BridgeGame");
 
 describe("BridgeGame 테스트", () => {
-  const bridgeGame = new BridgeGame();
   test("move 이동 테스트", () => {
+    const bridgeGame = new BridgeGame();
     bridgeGame.move("U");
     expect(bridgeGame.getMovedSpace()).toStrictEqual(["U"]);
     bridgeGame.move("U");
@@ -13,4 +13,27 @@ describe("BridgeGame 테스트", () => {
     bridgeGame.move("U");
     expect(bridgeGame.getMovedSpace()).toStrictEqual(["U", "U", "D", "U"]);
   });
+
+  test("판정 테스트 (이동한 칸이 이동가능한 칸인지)", () => {
+    const bridgeGame = new BridgeGame(["U", "D", "U"]);
+    expect(bridgeGame.move("U")).toEqual(true);
+    expect(bridgeGame.move("U")).toEqual(false);
+    expect(bridgeGame.move("D")).toEqual(false);
+  })
+
+  test("끝까지 도착했는지 테스트 (성공 테스트)", () => {
+    const bridgeGame = new BridgeGame(["U", "D", "U"]);
+    bridgeGame.move("U");
+    bridgeGame.move("D");
+    bridgeGame.move("U");
+    expect(bridgeGame.isArrive()).toEqual(true);
+  })
+  
+  test("끝까지 도착 못했는지 테스트 (실패 테스트)", () => {
+    const bridgeGame = new BridgeGame(["U", "D", "U"]);
+    bridgeGame.move("U");
+    bridgeGame.move("D");
+    bridgeGame.move("D");
+    expect(bridgeGame.isArrive()).toEqual(false);
+  })
 });
