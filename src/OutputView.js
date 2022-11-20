b8f5cd422734e71364cb75c852dac5a7bf445e52
Choi siun <siun0331@gmail.com>
2022-11-20 16:19:17 +0900

    feat: O X 표시기능 생성

--- src/OutputView.js
+++ src/OutputView.js
@@ -3,24 +3,44 @@ const MissionUtils = require('@woowacourse/mission-utils');
 const { Console } = MissionUtils;
 const { MESSAGE_SUCCESS_BOOLEAN, MESSAGE_TRY_COUNT } = require('./constants');
 
-const OutputView = {
-  /**
-   * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-   * <p>
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  printMap() {},
+let upArea = [];
+let downArea = [];
 
-  /**
-   * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-   * <p>
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
+const OutputView = {
+  initResult() {
+    upArea = [];
+    downArea = [];
+  },
+
+  printMap(command, bridgeInformation) {
+    OutputView.drawMap(command, bridgeInformation);
+    Console.print(upArea);
+    Console.print(downArea);
+  },
+
+  drawMap(command, bridgeInformation) {
+    const CORRECT = (command === bridgeInformation);
+    OutputView.drawUpArea(CORRECT, command);
+    OutputView.drawDownArea(CORRECT, command);
+    return true;
+  },
+
+  drawUpArea(correct, command) {
+    if (correct) upArea.push(command === 'U' ? 'O' : ' ');
+    else upArea.push(command === 'U' ? 'X' : ' ');
+  },
+
+  drawDownArea(correct, command) {
+    if (correct) downArea.push(command === 'D' ? 'O' : ' ');
+    else downArea.push(command === 'D' ? 'X' : ' ');
+  },
+
   printResult(gameComplete, gameTryCount) {
     const RESULT = gameComplete ? '성공' : '실패';
     Console.print(`${MESSAGE_SUCCESS_BOOLEAN}${RESULT}`);
     Console.print(`${MESSAGE_TRY_COUNT}${gameTryCount}`);
   },
+
 };
 
 module.exports = OutputView;
