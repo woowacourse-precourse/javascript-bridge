60a4714f5b876eb257e2bc525df1ff6446a03a95
Jaehyeon <jaehyeonp74@gmail.com>
2022-11-20 19:38:48 +0900

    feat: Controller 클래스 inputBridgeSize 메소드 구현
    - input 값을 유효성 검사를 통과시킨 후 다리에관한 정보를 담은 배열을 model에 저장하는 역할

--- src/Controller.js
+++ src/Controller.js
@@ -1,16 +1,28 @@
 const Model = require('./Model');
 const InputView = require('./InputView');
 const OutputView = require('./OutputView');
+const Validator = require('./Validator');
+const BridgeMaker = require('./BridgeMaker');
+const { generate } = require('./BridgeRandomNumberGenerator');
 
 class Controller {
   constructor() {
     this.model = new Model();
-    this.inputView = new InputView();
-    this.outputView = new OutputView();
   }
 
   startGame() {
-    this.outputView.printGameStart();
+    OutputView.printGameStart();
+    this.inputBridgeSize();
+  }
+
+  inputBridgeSize() {
+    const readBridgeSizeCallback = (input) => {
+      Validator.validateNumber(input);
+      Validator.validateNumberInRange(input);
+      const num = parseInt(input, 10);
+      this.model.setComputerBridgeArr(BridgeMaker.makeBridge(num, generate));
+    };
+    InputView.readBridgeSize(readBridgeSizeCallback);
   }
 }
 module.exports = Controller;
