933173e01394a180d5284eca42a077d946da979f
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-11-20 16:29:51 +0900

    test(BridgeGame): 범위 추출 메서드 잘못된 테스트 케이스 수정

--- __tests__/BridgeGame.test.js
+++ __tests__/BridgeGame.test.js
@@ -586,8 +586,8 @@ describe('빈 다리 로그 생성 메서드 테스트', () => {
 });
 
 describe('범위 추출 메서드 테스트', () => {
-  const U = [['O'], [' '], ['O']];
-  const D = [[' '], ['O'], [' ']];
+  const U = ['O', ' ', 'O'];
+  const D = [' ', 'O', ' '];
 
   test('메소드 이름은 "extractFromZeroTo"로 정의된다.', () => {
     const METHOD_NAME = 'extractFromZeroTo';
@@ -610,7 +610,7 @@ describe('범위 추출 메서드 테스트', () => {
   });
 
   test('3을 전달하면 [[ "O", " ", "O" ], [ " ", "O", " " ]]을 반환한다.', () => {
-    const EXPECTED = 2;
+    const EXPECTED = 3;
     const RECEIVED = [['O', ' ', 'O'], [' ', 'O', ' ']];
 
     expect(BridgeGame.extractFromZeroTo([U, D], EXPECTED)).toEqual(RECEIVED);
