195bcd6a838ded88d49c808422b2bb3baac0f7d3
nangkyeonglim <nangkyeonglim@gmail.com>
2022-11-20 22:36:28 +0900

    feat(Validator): 다리의 길이 범위가 넘어가는지 확인하는 메소드 구현

--- src/models/Validator.js
+++ src/models/Validator.js
@@ -15,6 +15,10 @@ class Validator {
     const check = /^[0-9]+$/; 
     return !check.test(num);
   }
+
+  isNotRangeOfBridgeLength(bridgeLength) {
+    return bridgeLength < 3 || bridgeLength > 20; 
+  }
 }
 
 module.exports = Validator;
