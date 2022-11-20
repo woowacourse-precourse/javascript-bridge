498eba2a04b2cde30b53877a2732ad16af04f91d
kknyapple <kknyapple@gmail.com>
2022-11-20 21:19:05 +0900

    refactor: 함수의 길이가 10라인을 넘지 안도록 함

--- src/InputView.js
+++ src/InputView.js
@@ -2,59 +2,55 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 const OutputView = require("./OutputView");
 const { checkBridgeSize, checkMovement, checkRestart } = require("./validate");
+const { TEXT } = require("./constant");
 
 const InputView = {
   readBridgeSize(bridge, bridgeGame) {
-    MissionUtils.Console.readLine("다리의 길이를 입력해주세요.\n", (size) => {
+    MissionUtils.Console.readLine(TEXT.BRIDGE_SIZE, (size) => {
       if (checkBridgeSize(size)) return this.readBridgeSize(bridge, bridgeGame);
 
       bridge.setBridge(size);
-      console.log(bridge.getBridge());
       this.readMoving(bridge, bridgeGame);
-
       this.readGameCommand();
     });
   },
 
+  isCrossing(bridge, bridgeGame) {
+    if (bridgeGame.getCross()) {
+      if (bridgeGame.getSuccess()) OutputView.printResult(bridgeGame);
+      else this.readMoving(bridge, bridgeGame);
+    }
+
+    if (!bridgeGame.getCross()) this.readGameCommand(bridge, bridgeGame);
+  },
+
   readMoving(bridge, bridgeGame) {
-    MissionUtils.Console.readLine(
-      "이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
-      (movement) => {
-        if (checkMovement(movement)) return this.readMoving(bridge, bridgeGame);
+    MissionUtils.Console.readLine(TEXT.MOVEMENT, (movement) => {
+      if (checkMovement(movement)) return this.readMoving(bridge, bridgeGame);
 
-        bridgeGame.move(movement, bridge);
-        OutputView.printMap(
-          bridgeGame.getUpResult(),
-          bridgeGame.getDownResult()
-        );
+      bridgeGame.move(movement, bridge);
+      OutputView.printMap(bridgeGame.getUpResult(), bridgeGame.getDownResult());
 
-        if (bridgeGame.getCross()) {
-          if (bridgeGame.getSuccess()) {
-            OutputView.printResult(bridgeGame);
-          } else {
-            this.readMoving(bridge, bridgeGame);
-          }
-        } else {
-          this.readGameCommand(bridge, bridgeGame);
-        }
-      }
-    );
+      this.isCrossing(bridge, bridgeGame);
+    });
+  },
+
+  doRestart(restart, bridge, bridgeGame) {
+    bridgeGame.retry(restart);
+    this.readMoving(bridge, bridgeGame);
+  },
+
+  doQuit(bridgeGame) {
+    OutputView.printResult(bridgeGame);
   },
 
   readGameCommand(bridge, bridgeGame) {
-    MissionUtils.Console.readLine(
-      "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n",
-      (restart) => {
-        if (checkRestart(restart)) this.readGameCommand(bridge, bridgeGame);
+    MissionUtils.Console.readLine(TEXT.RETRY, (restart) => {
+      if (checkRestart(restart)) this.readGameCommand(bridge, bridgeGame);
 
-        if (restart === "R") {
-          bridgeGame.retry(restart);
-          this.readMoving(bridge, bridgeGame);
-        } else if (restart === "Q") {
-          OutputView.printResult(bridgeGame);
-        }
-      }
-    );
+      if (restart === "R") this.doRestart(restart, bridge, bridgeGame);
+      if (restart === "Q") this.doQuit(bridgeGame);
+    });
   },
 };
 
