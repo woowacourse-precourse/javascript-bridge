9db2b77f6a81ca38f9623b16dbd5ee85b7f3ee41
yong <tkddbs1644@gmail.com>
2022-11-20 23:56:39 +0900

    feat(BridgeMaker): 다리를 생성하는 기능 구현

---
+++ __tests__/BridgeMakerTest.js
@@ -0,0 +1,29 @@
+const BridgeMaker = require("../src/BridgeMaker");
+
+describe("다리 생성 기능 테스트", () => {
+  test("다리를 형성할 숫자를 U, D로 반환", () => {
+    expect(BridgeMaker.convertNumberToBridge(0)).toEqual("D");
+    expect(BridgeMaker.convertNumberToBridge(1)).toEqual("U");
+  });
+
+  test("다리를 형성할 숫자가 0이나 1이 아니라면 예외 발생", () => {
+    expect(() => BridgeMaker.convertNumberToBridge(3)).toThrow("[ERROR]");
+    expect(() => BridgeMaker.convertNumberToBridge("a")).toThrow("[ERROR]");
+  });
+
+  test("숫자 배열을 다리 형태로 반환", () => {
+    const TEST_CASE = [
+      {
+        numbers: [1, 0, 0],
+        result: ["U", "D", "D"],
+      },
+      {
+        numbers: [1, 1, 0, 0, 1],
+        result: ["U", "U", "D", "D", "U"],
+      },
+    ];
+    TEST_CASE.forEach(({ numbers, result }) => {
+      expect(BridgeMaker.makeBridgeByNumbers(numbers)).toEqual(result);
+    });
+  });
+});
--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -7,7 +7,26 @@ const BridgeMaker = {
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
-  makeBridge(size, generateRandomNumber) {},
+  makeBridge(size, generateRandomNumber) {
+    const randomNumbers = Array.from({ length: size }, generateRandomNumber);
+    return this.makeBridgeByNumbers(randomNumbers);
+  },
+
+  makeBridgeByNumbers(numbers) {
+    return numbers.map(this.convertNumberToBridge.bind(this));
+  },
+
+  convertNumberToBridge(number) {
+    const numberToBridgeMap = {
+      0: "D",
+      1: "U",
+    };
+    return numberToBridgeMap[number] || this.throwRangeError();
+  },
+
+  throwRangeError() {
+    throw new Error("[ERROR] 다리를 만들기 위한 숫자는 0이나 1이어야 합니다.");
+  },
 };
 
 module.exports = BridgeMaker;
