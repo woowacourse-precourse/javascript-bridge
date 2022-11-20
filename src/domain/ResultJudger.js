494cf15847396bfbcf346cc50893552fbc38cc5d
wzrabbit <curious.wzrabbit@gmail.com>
2022-11-20 17:51:05 +0900

    feat(resultjudger): 라운드의 결과를 판별하여 반환하는 기능 구현

--- src/domain/ResultJudger.js
+++ src/domain/ResultJudger.js
@@ -1,6 +1,18 @@
 const { MOVE_RESULT } = require('../constant/Constant');
 
 class ResultJudger {
+  getRoundResult(bridge, bridgeCrossCount, movingDirection) {
+    if (this.#isPlayerFail(bridge, bridgeCrossCount, movingDirection)) {
+      return MOVE_RESULT.FAIL;
+    }
+
+    if (this.#isLastRound(bridge, bridgeCrossCount)) {
+      return MOVE_RESULT.CLEAR;
+    }
+
+    return MOVE_RESULT.OK;
+  }
+
   #isPlayerFail(bridge, bridgeCrossCount, movingDirection) {
     return bridge[bridgeCrossCount - 1] !== movingDirection;
   }
