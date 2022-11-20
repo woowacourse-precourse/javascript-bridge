4bf19667ed0fe1605834b7f3b3883142b2e3cce6
feb-dain <feb.dain@gmail.com>
2022-11-20 15:54:38 +0900

    feat: accept length of bridge from user

--- src/UI/InputView.js
+++ src/UI/InputView.js
@@ -1,3 +1,7 @@
+const { GAME_MESSAGE } = require("../Utils/Constants");
+const { BRIDGE_LENGTH } = GAME_MESSAGE;
+const { readLine } = require("../Utils/MissionUtils");
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,7 +9,11 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize(generateBridge) {
+    readLine(BRIDGE_LENGTH, (size) => {
+      generateBridge(size);
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
