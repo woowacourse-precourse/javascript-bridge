5067f51de7322d9609edf9b440c3d7332ca803d8
wjs5025 <wjs_5025@naver.com>
2022-11-21 02:31:55 +0900

    feat : JSDOC 주석 추가

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -7,9 +7,11 @@ const BridgeMaker = {
   /**
    * @param {number} size 다리의 길이
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
-   * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
+   * @param {function()} readMoving 사용자가 이동할 칸을 입력받는 함수
+   * @param {BridgeGame} game 현재 진행 중인 게임(인스턴스)
    */
   makeBridge(size, generateRandomNumber, readMoving, game) {
+    // 생성된 다리
     let bridge = [];
     while (bridge.length < size) {
       generateRandomNumber() ? bridge.push("U") : bridge.push("D");
