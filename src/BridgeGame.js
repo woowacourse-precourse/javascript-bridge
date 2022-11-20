c64ba835725bc3a3133ddd48480f23aed02ef01b
somin <smhs12@naver.com>
2022-11-21 01:49:24 +0900

    feat: 이동 방향 입력 값 유효성 검사 기능, 유효성 통과 실패시 재입력 기능 추가

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -7,6 +7,7 @@ const BridgeValidator = require("./utils/BridgeValidator");
 const { BRIDGE_LENGTH } = require("./constants/gameState");
 const BridgeMaker = require("./BridgeMaker");
 const { generate } = require("./BridgeRandomNumberGenerator");
+const DirectionValidator = require("./utils/DirectionValidator");
 
 class BridgeGame {
   #bridge;
@@ -36,7 +37,18 @@ class BridgeGame {
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(direction) {
+    this.validateDirection(direction);
+  }
+
+  validateDirection(direction) {
+    try {
+      DirectionValidator.validateDirection(direction);
+    } catch (e) {
+      OutputView.printError(e);
+      return InputView.readMoving();
+    }
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
