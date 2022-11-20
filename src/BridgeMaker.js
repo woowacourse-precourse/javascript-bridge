5a7a3a6196bb4304e221922b4781ec18253f4792
dudgp128 <20181048@sungshin.ac.kr>
2022-11-20 13:41:33 +0900

    REFACTORY(isValidBridgeSize) : 함수 옮기기
    InputView.isValidBridgeSize -> BridgeMaker.isValidBridgeSize

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -7,6 +7,13 @@ const BridgeMaker = {
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
+
+  isValidBridgeSize(input) {
+    const validInRange = input >= 3 && input <= 20;
+    if (validInRange) return input % 1 === 0;
+    throw "[ERROR] 3과 20사이의 자연수가 아닙니다.";
+  },
+
   makeBridge(size, generateRandomNumber) {
     return new Array(size).fill(undefined).map((e) => {
       return Number(generateRandomNumber()) == 1 ? "U" : "D";
--- src/InputView.js
+++ src/InputView.js
@@ -11,25 +11,21 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  Game: "",
+  Game: undefined,
+
   readBridgeSize() {
     Console.readLine("다리의 길이를 입력해주세요.\n", (input) => {
-      if (!this.isValidIBridgeSize(input))
-        throw "[ERROR] 3과 20사이의 자연수가 아닙니다.";
-      const bridge = BridgeMaker.makeBridge(
-        Number(input),
-        BridgeRandomNumberGenerator.generate
-      );
-      this.Game = new BridgeGame(bridge);
-      this.readMoving();
+      if (BridgeMaker.isValidBridgeSize(input)) {
+        const bridge = BridgeMaker.makeBridge(
+          Number(input),
+          BridgeRandomNumberGenerator.generate
+        );
+        this.Game = new BridgeGame(bridge);
+        this.readMoving();
+      }
     });
   },
 
-  isValidIBridgeSize(input) {
-    const validInRange = input >= 3 && input <= 20;
-    if (validInRange) return input % 1 === 0;
-    return false;
-  },
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
