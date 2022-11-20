38e2181837b89c9e9c2ed61326b2160ea671d8e5
yong <tkddbs1644@gmail.com>
2022-11-21 00:34:47 +0900

    feat(App): 기능 구현 완료를 위한 뼈대 메서드 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+const InputView = require("./View/InputView");
 const OutputView = require("./View/OutputView");
 
 class App {
@@ -7,6 +8,28 @@ class App {
 
   showGreeting() {
     OutputView.printGreeting();
+  }
+
+  showInputBridgeNumber() {
+    InputView.readBridgeSize((bridgeSize) => {
+      // TODO
+      // 입력받은 다리크기로 BridgeGame 을 생성한다.
+      this.showInputMoveCommand();
+    });
+  }
+
+  showInputMoveCommand() {
+    InputView.readGameCommand((command) => {
+      // TODO
+      // 다리를 건널 수 있다면 다리를 건넌다.
+      // 다리를 건널 수 없다면 this.showInputRetry()를 호출한다.
+    });
+  }
+
+  showInputRetry() {
+    InputView.readGameCommand((command) => {
+      // command에 따라서 게임을 재시작하거나 종료한다.
+    });
   }
 }
 
