26325fdd8db0eb7d9ca5f9e5c4caf1b452fb938f
evencoding <evencoding@gmail.com>
2022-11-20 19:16:29 +0900

    refactor(InputView): 콜백함수를 bind를 사용해 호출하도록 수정

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -4,60 +4,54 @@ const BridgeController = require('../controller/BridgeController');
 const Validator = require('../Validator');
 
 const InputView = {
-  controller: new BridgeController(),
+  bridgeCtrl: new BridgeController(),
 
   readBridgeSize() {
-    Console.readLine(MESSAGE.ASK_BRIDGE_SIZE, (size) => {
-      this.sizeCallback(size);
-    });
+    Console.readLine(MESSAGE.ASK_BRIDGE_SIZE, this.sizeCallback.bind(this));
   },
 
   sizeCallback(size) {
     try {
       Validator.sizeValidityCheck(size);
-      this.controller.handleSize(size);
+      this.bridgeCtrl.handleSize(size);
       this.readMoving();
     } catch ({ message }) {
-      this.controller.printMessage(message);
+      this.bridgeCtrl.printMessage(message);
       this.readBridgeSize();
     }
   },
 
   readMoving() {
-    Console.readLine(MESSAGE.ASK_SELECT_MOVE_POINT, (direction) => {
-      this.moveCallback(direction);
-    });
+    Console.readLine(MESSAGE.ASK_MOVE_POINT, this.moveCallback.bind(this));
   },
 
   moveCallback(direction) {
     try {
       Validator.directionValidityCheck(direction);
-      const isCorrectDirection = this.controller.handleDirection(direction);
+      const isCorrectDirection = this.bridgeCtrl.handleDirection(direction);
       isCorrectDirection ? this.doesGameOver() : this.readGameCommand();
     } catch ({ message }) {
-      this.controller.printMessage(message);
+      this.bridgeCtrl.printMessage(message);
       this.readMoving();
     }
   },
 
   doesGameOver() {
-    const doesUserWin = this.controller.doesUserWin();
-    doesUserWin ? this.controller.gameOver(doesUserWin) : this.readMoving();
+    const doesUserWin = this.bridgeCtrl.doesUserWin();
+    doesUserWin ? this.bridgeCtrl.gameOver(doesUserWin) : this.readMoving();
   },
 
   readGameCommand() {
-    Console.readLine(MESSAGE.ASK_RETRY, (command) => {
-      this.commandCallback(command);
-    });
+    Console.readLine(MESSAGE.ASK_RETRY, this.commandCallback.bind(this));
   },
 
   commandCallback(command) {
     try {
       Validator.commandValidityCheck(command);
-      const sholudRetry = this.controller.handleCommand(command);
-      sholudRetry ? this.readMoving() : this.controller.gameOver();
+      const sholudRetry = this.bridgeCtrl.handleCommand(command);
+      sholudRetry ? this.readMoving() : this.bridgeCtrl.gameOver();
     } catch ({ message }) {
-      this.controller.printMessage(message);
+      this.bridgeCtrl.printMessage(message);
       this.readGameCommand();
     }
   },
