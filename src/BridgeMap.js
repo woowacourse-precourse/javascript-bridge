ad519a9597d65315a8e4b6e7ae8ca7bee621f34f
Sukyeong Park <zztnrudzz13@naver.com>
2022-11-20 16:15:23 +0900

    fix(BridgeMap): remove unnecessary else statement

--- src/BridgeMap.js
+++ src/BridgeMap.js
@@ -1,16 +1,12 @@
 const { MOVE_INDEX } = require('./Constants');
 
-// FIXME: change logic of bridgemap generate function
 const BridgeMap = {
   map: [[], []],
   generate(answer, userMove) {
     this.map = [[], []];
     userMove.forEach((step, index) => {
-      if (answer[index] === step) {
-        this.map[MOVE_INDEX[step]].push('O');
-      } else {
-        this.map[MOVE_INDEX[step]].push('X');
-      }
+      if (answer[index] === step) this.map[MOVE_INDEX[step]].push('O');
+      if (answer[index] !== step) this.map[MOVE_INDEX[step]].push('X');
       this.map[1 - MOVE_INDEX[step]].push(' ');
     });
   },
