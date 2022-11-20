c1e8d85c88ae785e2807ca20079a348e88a8c048
feb-dain <feb.dain@gmail.com>
2022-11-20 16:25:37 +0900

    feat: create bridge

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,3 +1,6 @@
+const { APPROPRIATE_INPUT } = require("./Utils/Constants");
+const { UP, DOWN } = APPROPRIATE_INPUT;
+
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
@@ -7,7 +10,15 @@ const BridgeMaker = {
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
-  makeBridge(size, generateRandomNumber) {},
+  makeBridge(size, generateRandomNumber) {
+    const bridge = [];
+    while (size) {
+      size -= 1;
+      bridge.push(Number(generateRandomNumber()) ? UP : DOWN);
+    }
+
+    return bridge;
+  },
 };
 
 module.exports = BridgeMaker;
