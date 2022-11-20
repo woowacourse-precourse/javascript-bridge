2c319bbec5eee41efff10c7d106b4511a863d505
WaiNaat <qoli0703@gmail.com>
2022-11-20 23:37:11 +0900

    feat(InputView): add moving direction input feature

--- src/InputView.js
+++ src/InputView.js
@@ -27,7 +27,11 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    const input = InputView.readInput().trim();
+    Validator.direction(input);
+    return input;
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
