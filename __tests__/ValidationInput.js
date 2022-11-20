193d8de428a5bb56053dff3f41186e06d8b0fd35
ramda <91455804+gnfnfnr@users.noreply.github.com>
2022-11-21 00:09:09 +0900

    test: change validaion import style

--- __tests__/ValidationInput.js
+++ __tests__/ValidationInput.js
@@ -1,19 +1,18 @@
 /* eslint-disable */
+const Validation = require('../src/model/Validaion');
 const BridgeMaker = require('../src/BridgeMaker');
-const ObjectToValidate = require('../src/model/ObjectToValidate');
+const { BRIDGESIZE_KEY, MOVEMENT_KEY, COMMAND_KEY } = require('../src/constant/Key');
 
 describe('다리 길이 테스트', () => {
   test.each(['2', '0', '-123'])('범위 미만의 금액에 대한 예외처리', (input) => {
     expect(() => {
-      const infomation = ObjectToValidate.bridgeSize;
-      infomation.checkValidation(input, infomation.minimum, infomation.maximum);
+      new Validation(BRIDGESIZE_KEY.CHECK_VALIDATION, input).showResult();
     }).toThrow('[ERROR]');
   });
 
   test.each([' ', '삼이일', 'three', '1e3', '@#$'])('숫자 이외에 대한 예외처리', (input) => {
     expect(() => {
-      const infomation = ObjectToValidate.bridgeSize;
-      infomation.checkValidation(input, infomation.minimum, infomation.maximum);
+      new Validation(BRIDGESIZE_KEY.CHECK_VALIDATION, input).showResult();
     }).toThrow('[ERROR]');
   });
 
@@ -31,8 +30,7 @@ describe('다리 길이 테스트', () => {
 describe('움직임 테스트', () => {
   test.each(['u', 'd', '34', '4', 'go'])('범위 미만의 금액에 대한 예외처리', (input) => {
     expect(() => {
-      const infomation = ObjectToValidate.movement;
-      infomation.checkValidation(input, [infomation.up, infomation.down]);
+      new Validation(MOVEMENT_KEY.CHECK_VALIDATION, input).showResult();
     }).toThrow('[ERROR]');
   });
 });
@@ -40,8 +38,7 @@ describe('움직임 테스트', () => {
 describe('명령어 테스트', () => {
   test.each(['r', 'q', 'qq', '0', '1'])('범위 미만의 금액에 대한 예외처리', (input) => {
     expect(() => {
-      const infomation = ObjectToValidate.progress;
-      infomation.checkValidation(input, [infomation.quit, infomation.restart]);
+      new Validation(COMMAND_KEY.CHECK_VALIDATION, input).showResult();
     }).toThrow('[ERROR]');
   });
 });
