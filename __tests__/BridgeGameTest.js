5d6edb40aa51eb99ff5f9d0ab268cd920e6ab26c
daal2quf <daal2quf@gmail.com>
2022-11-21 02:53:02 +0900

    test(bridgeGameTest): bridgeCloseConverter() 기능 테스트
    add:
    test1: bridgeCloseConverter() argument로 '|' 넣었을때 ']' 변경 확인
    test2: 마지막값이 '|' 일때만 바뀌는지 확인
    test3: expect.not.arrayCoutaining으로 값이 안들어있을때 테스트가 실패해야 성공하는 test.failing 실험

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -1,5 +1,5 @@
-const { Console } = require("@woowacourse/mission-utils");
-const BridgeGame = require("../src/BridgeGame");
+const { Console } = require('@woowacourse/mission-utils');
+const BridgeGame = require('../src/BridgeGame');
 
 afterEach(() => {
   Console.close();
@@ -9,8 +9,41 @@ describe('BridgeGame 클래스의 stackOfUserMovingInput() 메서드 테스트',
   test('this.userInput 멤버변수에 쌓아줄 입력값이 잘 들어가는지 확인', () => {
     const bridge = new BridgeGame();
     const userInput = 'D';
-    bridge.stackOfUserMovingInput(userInput);
+    bridge.move(userInput);
     const result = 'D';
     expect(...bridge.userInput).toContain(result);
   });
+});
+
+describe('bridgeGame 클래스의 bridgeCloseConverter() 메서드 테스트', () => {
+  test('배열의 마지막값이 "|" 일때 "]" 로 바뀌는지 확인', () => {
+    const bridgeGame = new BridgeGame();
+    const lastString = ['|'];
+    const convertedLastString = [']'];
+    expect(bridgeGame.bridgeCloseConverter(lastString)).toEqual(
+      convertedLastString,
+    );
+  });
+  test.each([['a'], ['@'], [']'], [2]])(
+    '배열의 마지막값이 "|"가 아닐때 "]" 로 바뀌지 않는지 확인',
+    input => {
+      const bridgeGame = new BridgeGame();
+      const lastString = input;
+      const convertedLastString = [']'];
+      expect(bridgeGame.bridgeCloseConverter(lastString)).toEqual(
+        expect.not.arrayContaining(convertedLastString),
+      );
+    },
+  );
+  test.failing.each([['|']])(
+    '옳은값을 넣었을때 테스트가 실패하는지 test.failing 테스트',
+    input => {
+      const bridgeGame = new BridgeGame();
+      const lastString = input;
+      const convertedLastString = [']'];
+      expect(bridgeGame.bridgeCloseConverter(lastString)).toEqual(
+        expect.not.arrayContaining(convertedLastString),
+      );
+    },
+  );
 });
