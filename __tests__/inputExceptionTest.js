541489695ce1ad81fa41c2be6a2a5b651acc2f48
WaiNaat <qoli0703@gmail.com>
2022-11-20 23:21:14 +0900

    test: change test code for input test
    참고: 8b74429850e8955244f823b84935578ab0361e0f
    이제 재입력 과정이 없으므로
    - 예외 발생 시 바로 throw를 이용한 확인 가능
    - 연속된 입력에 대한 처리 테스트는 App에서 진행해야 함

--- __tests__/inputExceptionTest.js
+++ __tests__/inputExceptionTest.js
@@ -37,20 +37,18 @@ describe('다리 길이 입력 테스트', () => {
     expect(InputView.readBridgeSize()).toEqual(Number(input[0]));
   });
 
-  test.each([[['woowacourse', '3']], [['0x13', '3']], [['123b', '3']]])('예외: 입력값이 숫자가 아님', (input) => {
-    runException(input);
+  test.each([[['woowacourse']], [['0x13']], [['123b']]])('예외: 입력값이 숫자가 아님', (input) => {
+    mockQuestions(input);
+    expect(InputView.readBridgeSize).toThrow('[ERROR]');
   });
 
-  test.each([[['-1', '3']], [['12.345', '3']], [['0', '3']]])('예외: 입력값이 자연수가 아님', (input) => {
-    runException(input);
+  test.each([[['-1']], [['12.345']], [['0']]])('예외: 입력값이 자연수가 아님', (input) => {
+    mockQuestions(input);
+    expect(InputView.readBridgeSize).toThrow('[ERROR]');
   });
 
-  test.each([[['2', '3']], [['21', '3']], [['121687', '3']]])('예외: 입력값 범위', (input) => {
-    runException(input);
-  });
-
-  test('연속된 잘못된 입력값에 대한 처리', () => {
-    mockQuestions(['0x13', '12.345', '121687', '-1', '다리', '15']);
-    expect(InputView.readBridgeSize()).toEqual(15);
+  test.each([[['2']], [['21']], [['121687']]])('예외: 입력값 범위', (input) => {
+    mockQuestions(input);
+    expect(InputView.readBridgeSize).toThrow('[ERROR]');
   });
 });
