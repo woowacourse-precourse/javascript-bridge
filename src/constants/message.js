48ef62a2b0b1d2ffda103584cae161dd808f7fbf
somin <smhs12@naver.com>
2022-11-20 17:48:47 +0900

    feat: 게임 진행 메시지 상수 설정

---
+++ src/constants/message.js
@@ -0,0 +1,6 @@
+const INPUT_MESSAGE = Object.freeze({
+  START: "다리 건너기 게임을 시작합니다.\n",
+  BRIDGE_LENGTH: "다리의 길이를 입력해주세요.\n",
+  MOVE: "이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
+  RETRY: "게임을 다시 시도할지 여부를 입력해주세요.(재시도: R, 종료: Q)\n",
+});
