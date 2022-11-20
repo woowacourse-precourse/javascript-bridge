12735be0620cfb6edd5baaeb5e4ed9b9e6e1f669
WaiNaat <qoli0703@gmail.com>
2022-11-20 21:48:56 +0900

    feat(Validator): throw if bridge size is not natural number

--- src/Validator.js
+++ src/Validator.js
@@ -2,7 +2,7 @@ const { BridgeConfig, GameConfig, Message } = require('./Config');
 
 const Validator = {
   bridgeSize(number) {
-    if (number < BridgeConfig.MIN_LENGTH || number > BridgeConfig.MAX_LENGTH) {
+    if (number % 1 !== 0 || number < BridgeConfig.MIN_LENGTH || number > BridgeConfig.MAX_LENGTH) {
       throw new RangeError(Message.ERROR_BRIDGE_LENGTH);
     }
   },
