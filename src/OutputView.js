ab6656f67b21f01dc5ca322522d72e3bc9d491d2
cocoa <jwv0323@kau.kr>
2022-11-20 16:03:04 +0900

    feat: printResult 출력문 구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -4,10 +4,10 @@ const { Console } = require('@woowacourse/mission-utils');
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 const OutputView = {
-  START_PHRASE: '다리 건너기 게임을 시작합니다.\n',
+  START: '다리 건너기 게임을 시작합니다.\n',
   
   printStart() {
-    Console.print(this.START_PHRASE);
+    Console.print(this.START);
   },
 
   /**
@@ -24,7 +24,11 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() {},
+  printResult() {
+    Console.print(`최종 게임 결과`);
+    Console.print(`게임 성공 여부: `);
+    Console.print(`총 시도한 횟수: `);
+  },
 };
 
 module.exports = OutputView;
