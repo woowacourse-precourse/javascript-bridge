c22692fcedc5fad1627b0f8c6415b23b3162270b
woo-jk <201702042@o.cnu.ac.kr>
2022-11-20 12:28:44 +0900

    fix: InputView 객체 Promise 전부 삭제, 콜백 함수 사용 방식으로 변경

--- src/InputView.js
+++ src/InputView.js
@@ -7,34 +7,21 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {
-    return new Promise((resolve) => {
-      MissionUtils.Console.readLine("다리의 길이를 입력해주세요.\n", (input) => {
-        resolve(input);
-      });
-    });
+  readBridgeSize(callback) {
+    MissionUtils.Console.readLine("다리의 길이를 입력해주세요.\n", callback);
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {
-    return new Promise((resolve) => {
-      MissionUtils.Console.readLine("이동할 칸을 선택해주세요. (위: U, 아래: D)\n", (input) => {
-        resolve(input);
-      });
-    });
+  readMoving(callback) {
+    MissionUtils.Console.readLine("이동할 칸을 선택해주세요. (위: U, 아래: D)\n", callback);
   },
-
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {
-    return new Promise((resolve) => {
-      MissionUtils.Console.readLine("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n", (input) => {
-        resolve(input);
-      });
-    });
+  readGameCommand(callback) {
+    MissionUtils.Console.readLine("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n", callback);
   },
 };
 
