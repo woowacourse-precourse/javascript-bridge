2d09fdba3fa04885696d990cd1e5600d8d8b0f06
Sukyeong Park <zztnrudzz13@naver.com>
2022-11-20 16:34:42 +0900

    refactor(InputView): change readline strings into constants

--- src/InputView.js
+++ src/InputView.js
@@ -1,4 +1,5 @@
 const MissionUtils = require('@woowacourse/mission-utils');
+const { INPUT } = require('./Constants');
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -8,27 +9,21 @@ const InputView = {
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize(callback) {
-    MissionUtils.Console.readLine('다리의 길이를 입력해주세요.\n', callback);
+    MissionUtils.Console.readLine(INPUT.bridgeSize, callback);
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving(callback) {
-    MissionUtils.Console.readLine(
-      '이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
-      callback,
-    );
+    MissionUtils.Console.readLine(INPUT.moving, callback);
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand(callback) {
-    MissionUtils.Console.readLine(
-      '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
-      callback,
-    );
+    MissionUtils.Console.readLine(INPUT.gameCommand, callback);
   },
 };
 
