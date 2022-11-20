fc2c66a4c5312d163cd73e79fe75014930e45ce3
JangHoonSeok <orirori512@naver.com>
2022-11-21 07:16:12 +0900

    ✨ feat(constants): Error 메세지 상수 추가

--- src/constants/Messages.js
+++ src/constants/Messages.js
@@ -6,5 +6,8 @@ const REQUEST_MESSAGE = Object.freeze({
   SELECT_MOVE: "이동할 칸을 선택해주세요. (위: U, 아래: D)",
   RESTART: "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
 });
+const ERROR_MESSAGE = Object.freeze({
+  BRIDGE_LENGTH_ERROR: "[ERROR]: 다리 길이는 3~20사이의 숫자를 입력해야합니다.",
+});
 
-module.exports(PRINT_MESSAGE, REQUEST_MESSAGE);
+module.exports = { PRINT_MESSAGE, REQUEST_MESSAGE, ERROR_MESSAGE };
