3d1646553c05fd363f0e9f1e200c6f2cd81f00d9
Sukyeong Park <zztnrudzz13@naver.com>
2022-11-20 22:49:43 +0900

    refactor(OutputView): use constants for the strings

--- src/OutputView.js
+++ src/OutputView.js
@@ -25,7 +25,7 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printResult(isSuccess, playCount) {
-    MissionUtils.Console.print('최종 게임 결과');
+    MissionUtils.Console.print(GAME.end);
     OutputView.printMap();
     if (isSuccess) MissionUtils.Console.print(GAME.success);
     if (!isSuccess) MissionUtils.Console.print(GAME.fail);
