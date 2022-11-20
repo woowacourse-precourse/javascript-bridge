ced22b2f1d2078eac8b76b8f924bdec8d19cfd11
HWI <dlwognl99@naver.com>
2022-11-20 14:22:08 +0900

    feat: 다리 길이 입력 메세지 상수 추가

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,7 +1,12 @@
 const START = "다리 건너기 게임을 시작합니다.";
+const LENGTH = "다리의 길이를 입력해주세요\n.";
 
 const OUTPUT_MESSAGE = {
   START,
 };
 
-module.exports = { OUTPUT_MESSAGE };
+const INPUT_MESSAGE = {
+  LENGTH,
+};
+
+module.exports = { OUTPUT_MESSAGE, INPUT_MESSAGE };
