b23c1705d4b913ed007f2aed0d8816bfcaa92fec
nerdchanii <nerdchanii@gmail.com>
2022-11-21 01:17:50 +0900

    style(*): eslint 적용

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,5 +1,5 @@
-const Io = require("../infrastucture/io/Io");
-const MESSAGE = require("./view.constants");
+const Io = require('../infrastucture/io/Io');
+const MESSAGE = require('./view.constants');
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
