ca1e6ad85fd404a69198158cd680e95d549b3c9c
khj0426 <706shin1728@naver.com>
2022-11-21 00:37:45 +0900

    feat:에러 메시지 상수화

--- src/MESSAGES/InputMessage.js
+++ src/MESSAGES/InputMessage.js
@@ -1,7 +1,8 @@
 const INPUT_MESSAGE = {
   BRIDGE_INPUT: '다리의 길이를 입력해주세요.',
-  BRIDGE_INPUT_ERROR: '다리 길이는 3부터 20사이의 숫자여야 합니다.',
-  REGAME_INPUT_ERROR: '재시도는 R,종료는 Q입니다.다시 입력해주세요',
+  BRIDGE_INPUT_ERROR: '[ERROR] 다리 길이는 3부터 20사이의 숫자여야 합니다.',
+  REGAME_INPUT_ERROR: '[ERROR] 재시도는 R,종료는 Q입니다.다시 입력해주세요',
+  MOVE_INPUT_ERROR: '[ERROR] 위는 U,아래는 D입니다. 다시 입력해주세요',
 };
 
 Object.freeze(INPUT_MESSAGE);
