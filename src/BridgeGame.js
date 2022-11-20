48277fcf8ac88e2a62edd36d316e834c5e15ff6d
cham-min <hymn9613@gmail.com>
2022-11-21 02:33:34 +0900

    feat(BridgeGame): add retry method

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -5,6 +5,7 @@ class BridgeGame {
   #bridge;
   #move = [];
   #result = [[], []];
+  #count;
 
   constructor(bridge) {
     this.#bridge = bridge;
@@ -40,7 +41,10 @@ class BridgeGame {
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  retry() {}
+  retry() {
+    this.#count += 1;
+    this.#move = [];
+  }
 }
 
 module.exports = BridgeGame;
