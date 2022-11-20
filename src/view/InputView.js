23627f153c2196169bd7233bc1870107797a223b
unknown <chn0202@gmail.com>
2022-11-20 13:54:34 +0900

    style(view): view폴더 생성

---
+++ src/view/InputView.js
@@ -0,0 +1,96 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const BridgeGame = require("../BridgeGame");
+const BridgeMaker = require("../BridgeMaker");
+const BridgeRandomNumberGenerator = require("../BridgeRandomNumberGenerator");
+const OutputView = require("./OutputView");
+const ExceptionCheck = require("../ExceptionCheck");
+/**
+ * 사용자로부터 입력을 받는 역할을 한다.
+ */
+const InputView = {
+  BRIDGE_GAME: new BridgeGame(),
+  LOWER_INCLUSIVE: 3,
+  UPPER_INCLUSIVE: 20,
+  UP: [],
+  DOWN: [],
+  IDX: 0,
+  TRY: 1,
+  /**
+   * 다리의 길이를 입력받는다.
+   */
+  readBridgeSize() {
+    MissionUtils.Console.readLine(
+      "다리의 길이를 입력해주세요.",
+      (bridgeSize) => {
+        try {
+          ExceptionCheck.checkBridgeSize(Number(bridgeSize));
+          OutputView.printBridgeSize(bridgeSize);
+          const bridge = BridgeMaker.makeBridge(
+            Number(bridgeSize),
+            BridgeRandomNumberGenerator.generate
+          );
+          this.readMoving(bridge);
+        } catch (err) {
+          MissionUtils.Console.print(err);
+          this.readBridgeSize();
+        }
+      }
+    );
+  },
+
+  /**
+   * 사용자가 이동할 칸을 입력받는다.
+   */
+  readMoving(bridge) {
+    if (this.IDX === bridge.length) return;
+    MissionUtils.Console.readLine(
+      "이동할 칸을 선택해주세요. (위: U, 아래: D)",
+      (moveSpace) => {
+        try {
+          this.checkMove(moveSpace);
+          OutputView.printSpace(moveSpace);
+
+          const { up, down, compareResult, idx } = this.BRIDGE_GAME.move(
+            bridge,
+            moveSpace
+          );
+          this.IDX = idx;
+          OutputView.printMap(up, down);
+          if (compareResult === "O") {
+            this.readMoving(bridge);
+            if (idx === bridge.length)
+              OutputView.printResult({ up, down }, this.TRY, compareResult);
+          } else {
+            this.readGameCommand(bridge, [up], [down]);
+          }
+        } catch (err) {
+          console.log(err);
+          this.readMoving(bridge);
+        }
+      }
+    );
+  },
+  checkMove(playerMove) {
+    if (playerMove === "U" || playerMove === "D") return;
+    else throw new Error("player이동은 U 또는 D만 입력해야 합니다.");
+  },
+
+  /**
+   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+   */
+  readGameCommand(bridge, up, down) {
+    MissionUtils.Console.readLine(
+      "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
+      (string) => {
+        if (string === "R") {
+          this.TRY = this.TRY + 1;
+          this.readMoving(bridge);
+        } else {
+          OutputView.printResult({ up, down }, this.TRY, compareResult);
+        }
+      }
+    );
+  },
+};
+
+module.exports = InputView;
