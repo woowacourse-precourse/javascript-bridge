c4dce8b82110e39f790390120c1a513706a10549
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-20 15:24:13 +0900

    :sparkles: feat : 다리 생성을 위한 함수 작성

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,3 +1,5 @@
+const { MOVE_UP_DOWN } = require('./utils/constants/GameSystem');
+
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
@@ -7,7 +9,13 @@ const BridgeMaker = {
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
-  makeBridge(size, generateRandomNumber) {},
+  makeBridge(size, generateRandomNumber) {
+    const array = Array.from({length: size}, () => {
+      const number = generateRandomNumber();
+      return MOVE_UP_DOWN[number];
+    })
+    return array;
+  },
 };
 
 module.exports = BridgeMaker;
