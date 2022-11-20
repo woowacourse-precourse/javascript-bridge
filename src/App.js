93a80e7f1a9d6d7b4a0fa027c601d81d87a12579
wisdomin121@naver.com <wisdomin121@naver.com>
2022-11-20 16:39:24 +0900

    ✨ [FEAT] : 게임 시작 문구 출력하기 (src/App.js)
    App에서 게임 시작에 대한 문구를 출력한다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+const { Console } = require("@woowacourse/mission-utils");
+
+const GAME_START_SENTENCE = '다리 건너기 게임을 시작합니다. \n'
+
 class App {
-  play() {}
+  play() {
+    Console.print(GAME_START_SENTENCE);
+  }
 }
 
 module.exports = App;
