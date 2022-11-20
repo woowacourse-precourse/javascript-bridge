5d793868ba1aac44dd34fc28b0b3b539676d5ad5
Choi siun <siun0331@gmail.com>
2022-11-20 18:01:12 +0900

    feat: Input에 예외처리 적용

--- src/InputView.js
+++ src/InputView.js
@@ -1,22 +1,27 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const { MESSAGE_INPUT_BRIDGE_LENGTH, MESSAGE_NEXT_MOVING_INPUT, MESSAGE_RETRY } = require('./constants');
 const BridgeMaker = require('./BridgeMaker');
+const Validator = require('./Validator');
+
+const { moveCommandValidator, bridgeLengthValidator, restartCommandValidator } = Validator;
 const { generate } = require('./BridgeRandomNumberGenerator');
 const BridgeGame = require('./BridgeGame');
 
 const { Console } = MissionUtils;
 const GAME_MANAGER = new BridgeGame();
+
 const InputView = {
   readBridgeSize() {
     Console.readLine(MESSAGE_INPUT_BRIDGE_LENGTH, (length) => {
+      if (!bridgeLengthValidator(length)) this.readBridgeSize();
       GAME_MANAGER.setBridge(BridgeMaker.makeBridge(length, generate));
-      Console.print(GAME_MANAGER.getBridge());
       InputView.readMoving();
     });
   },
 
   readMoving() {
     Console.readLine(MESSAGE_NEXT_MOVING_INPUT, (direction) => {
+      if (!moveCommandValidator(direction)) this.readMoving();
       GAME_MANAGER.move(direction)
         ? this.readMoving() : this.readGameCommand();
     });
@@ -25,6 +30,7 @@ const InputView = {
   readGameCommand() {
     if (GAME_MANAGER.getGameComplete()) return;
     Console.readLine(MESSAGE_RETRY, (command) => {
+      if (!restartCommandValidator(command)) this.readGameCommand();
       if (GAME_MANAGER.retry(command)) {
         this.readMoving();
       } else {
