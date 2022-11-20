d4a098334e32ade6017badfdaedef9cee4586ef2
ReturnReason <reasonz@daum.net>
2022-11-20 16:06:29 +0900

    test(Validator): 사용자가 입력한 다리 길이가 3 ~ 20 사이 숫자가 아닌 경우 예외처리 테스트

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -9,4 +9,15 @@ describe('🌈 Validator 클래스 테스트 ', () => {
       validator.emptyInput(userInput);
     }).toThrow('emptyInput');
   });
+
+  test('⚡ 사용자가 입력한 다리 길이가 3 ~ 20사이 숫자가 아닌 경우 예외 처리됩니다.', () => {
+    const validator = new Validator();
+    const userInput = ['0', '123', '-1', '99999'];
+    expect(() => {
+      userInput.forEach((input) => {
+        validator.outOfRange(input);
+        console.log(input);
+      });
+    }).toThrow('invalidRange');
+  });
 });
