1b7c07ddf2f42bd802611e87b0cdeba9d60754da
woo-jk <201702042@o.cnu.ac.kr>
2022-11-20 19:07:42 +0900

    test: Player 클래스 지도 가져오는 함수 테스트 코드 작성

--- __tests__/PlayerTest.js
+++ __tests__/PlayerTest.js
@@ -21,4 +21,27 @@ describe("플레이어 클래스 테스트", () => {
     const playerPosition = player.getCurrentPosition();
     expect(playerPosition).toEqual(5);
   });
+
+  test("플레이어가 두 번 이동했다면 플레이어의 위치는 2", () => {
+    const player = new Player();
+    player.move("U");
+    player.move("D");
+
+    const [upperBridge, lowerBridge] = player.getMap();
+    expect(upperBridge).toEqual("[ O |   ]");
+    expect(lowerBridge).toEqual("[   | O ]");
+  });
+
+  test("플레이어가 두 번 이동했다면 플레이어의 위치는 2", () => {
+    const player = new Player();
+    player.move("U");
+    player.move("D");
+    player.move("U");
+    player.move("U");
+    player.move("U");
+
+    const [upperBridge, lowerBridge] = player.getMap();
+    expect(upperBridge).toEqual("[ O |   | O | O | O ]");
+    expect(lowerBridge).toEqual("[   | O |   |   |   ]");
+  });
 });
