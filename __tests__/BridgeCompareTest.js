673899efcb50ba5ba7f9a0e74696e82c4010424b
khj0426 <706shin1728@naver.com>
2022-11-21 00:39:13 +0900

    test:사용자가 입력한 칸, 이용가능한 다리 비교 테스트가 에러반환하는지 확인

--- __tests__/BridgeCompareTest.js
+++ __tests__/BridgeCompareTest.js
@@ -12,13 +12,13 @@ test('사용자가 입력한 칸이 위이고, 이용가능한 다리가 위인 
 
 test('사용자가 입력한 칸과 이용가능한 다리에 따른 출력 형식 테스트', () => {
   expect(bridgegame.moveCaseAction('U', 'U')).toBe(BridgeMessage.UPUP_MESSAGE);
-  expect(bridgegame.moveCaseAction('U', 'D')).toBe(
-    BridgeMessage.UPDOWN_MESSAGE
-  );
+  expect(() => {
+    bridgegame.moveCaseAction('U', 'D');
+  }).toThrow();
   expect(bridgegame.moveCaseAction('D', 'D')).toBe(
     BridgeMessage.DOWNDOWN_MESSAGE
   );
-  expect(bridgegame.moveCaseAction('D', 'U')).toBe(
-    BridgeMessage.DOWNUP_MESSAGE
-  );
+  expect(() => {
+    bridgegame.moveCaseAction('D', 'U');
+  }).toThrow();
 });
