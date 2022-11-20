319962fb4cb27c918ea7bc8617b56fe1215595c2
leeharyung <lhr4884@naver.com>
2022-11-21 04:55:20 +0900

    refactor: app에서 UI로직 담당하도록 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,52 @@
+const { getInputs, readMoving, closeConsole } = require('./View/InputView');
+const {
+	checkBridgeSize,
+	checkMoveInput,
+	checkGameCommand,
+} = require('./util/validationInput');
+const { printStart } = require('./View/OutputView');
+const BridgeGame = require('./BridgeGame');
+
 class App {
-  play() {}
+	bridgeGame;
+
+	play() {
+		printStart();
+		getInputs.bind(this)([this.createBridge, this.move, this.controlGame]);
+	}
+
+	createBridge(input) {
+		checkBridgeSize(input);
+		this.bridgeGame = new BridgeGame(input);
+	}
+
+	move(input) {
+		checkMoveInput(input);
+		const MOVABLE = this.bridgeGame.move(input);
+		const IS_END = this.bridgeGame.isCrossed();
+		if (IS_END) {
+			this.gameEnd();
+		}
+		return [MOVABLE, IS_END];
+	}
+
+	controlGame(input) {
+		checkGameCommand(input);
+		if (input === 'R') {
+			this.bridgeGame.retry();
+			readMoving.call(this, [this.move, this.controlGame]);
+		} else {
+			this.gameEnd();
+		}
+	}
+
+	gameEnd() {
+		this.bridgeGame.printResult();
+		closeConsole();
+	}
 }
+
+const a = new App();
+a.play();
 
 module.exports = App;
