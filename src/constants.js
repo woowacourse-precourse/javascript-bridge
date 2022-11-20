18b3390f02592d2f4806b9d954e205ce7270333a
d-charlie-kim <dokkim@student.42seoul.kr>
2022-11-20 23:13:38 +0900

    Style: 출력 문장들 constant로 관리

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+
+const TEXT = {
+	GAME_START_MSG: '다리 건너기 게임을 시작합니다.\n',
+
+	INPUT_BRIDGE_SIZE_MSG: '다리의 길이를 입력해주세요.\n',
+	INPUT_MOVING_MSG: '이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
+	INPUT_ENDING_MSG: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
+
+	RESULT_MSG: '최종 게임 결과\n',
+	SUCCESS_OR_FAIL_MSG: '게임 성공 여부: ',
+	TRY_COUNT_MSG: '총 시도한 횟수: ',
+};
+
+module.exports(TEXT);
