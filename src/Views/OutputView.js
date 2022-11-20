69e61db297a61c7118b0436a8090f8294ce68d50
Pengoose <73521518+pengooseDev@users.noreply.github.com>
2022-11-21 03:11:48 +0900

    feat: 다리를 건너는 결과를 출력한다

--- src/Views/OutputView.js
+++ src/Views/OutputView.js
@@ -15,13 +15,30 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printMap(progressData) {
-    const blueprint = createBlueprint(progressData.length);
-    const upRow = [...blueprint];
-    const downRow = [...blueprint];
+    const progressMap = this.createMap(progressData);
+    Console.print(progressMap.up);
+    Console.print(progressMap.down);
+  },
 
-    const resultMap = [[...upRow], [...downRow]];
-    Console.print(progressData);
-    Console.printO(resultMap);
+  createMap(progressData) {
+    const upRow = this.drawMap(progressData, 'U');
+    const downRow = this.drawMap(progressData, 'D');
+    const upString = `[${upRow.join('|')}]`;
+    const downString = `[${downRow.join('|')}]`;
+
+    return { up: upString, down: downString };
+  },
+
+  drawMap(progressData, position) {
+    const blueprint = createBlueprint(progressData.length);
+    const progressMap = [...blueprint].map((_, index) => {
+      const data = progressData[index];
+      if (data.select === position) return '   ';
+
+      return data.alive ? ' O ' : ' X ';
+    });
+
+    return progressMap;
   },
 
   /**
