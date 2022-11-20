590c64ccf6c8c3279ae61fd6888e8ea49a60c82a
Sukyeong Park <zztnrudzz13@naver.com>
2022-11-20 22:49:03 +0900

    refactor(OutputView): use constants for the string

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,5 +1,6 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const BridgeMap = require('./BridgeMap');
+const { ERROR, GAME } = require('./Constants');
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -24,17 +25,16 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printResult(isSuccess, playCount) {
-    // FIXME: change strings into constants
     MissionUtils.Console.print('최종 게임 결과');
     OutputView.printMap();
-    if (isSuccess) MissionUtils.Console.print('게임 성공 여부: 성공');
-    if (!isSuccess) MissionUtils.Console.print('게임 성공 여부: 실패');
-    MissionUtils.Console.print(`총 시도한 횟수: ${playCount}`);
+    if (isSuccess) MissionUtils.Console.print(GAME.success);
+    if (!isSuccess) MissionUtils.Console.print(GAME.fail);
+    MissionUtils.Console.print(`${GAME.try}${playCount}`);
     MissionUtils.Console.close();
   },
 
   printError(message) {
-    MissionUtils.Console.print(`[ERROR] ${message}`);
+    MissionUtils.Console.print(`${ERROR.prefix}${message}`);
   },
 };
 
