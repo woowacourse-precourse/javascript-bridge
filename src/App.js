b217599b59b98a47a5cc96cb8d244845185c2de3
cho seonghoon <chsida655@gmail.com>
2022-11-21 02:12:49 +0900

    feat: BridgeGame Class 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+const InputView = require("./InputView.js");
+const { Console } = require("@woowacourse/mission-utils");
+const BridgeMaker = require("./BridgeMaker.js");
+const BridgeGame = require("./BridgeGame.js");
+const OutputView = require("./OutputView.js");
 class App {
-  play() {}
+  play() {
+    const bridgeGame = new BridgeGame();
+    InputView.readBridgeSize(bridgeGame);
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
