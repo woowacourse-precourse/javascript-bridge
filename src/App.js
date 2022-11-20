c3b939cda5e2fe2c005ed2756012c10035cf1bf4
GunHoi <lgh509@kyonggi.ac.kr>
2022-11-20 23:14:18 +0900

    feat -3: 플레이어 이동받아오는 부분을 BridgeGame 객체에서 처리할 수 있도록 변경

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 const OutputView = require("./OutputView.js");
 const RecallUntilCorrect = require("./RecallUntilCorrect.js");
 const BridgeMaker = require("./BridgeMaker.js");
+const BridgeGame = require("./BridgeGame.js");
 const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
 class App {
   play() {
     OutputView.printGameStart();
     const BRIDGE_SIZE = RecallUntilCorrect.recallReadBridgeSize(true);
     const BRIDGE_MAP = BridgeMaker.makeBridge(BRIDGE_SIZE,BridgeRandomNumberGenerator.generate);
-    const moving = RecallUntilCorrect.recallReadMoving(true);
+    const player = new BridgeGame(BRIDGE_SIZE, BRIDGE_MAP);
   }
 }
 
