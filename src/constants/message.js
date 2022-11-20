cc624860571b2dab0db5d7dc7386ff625cda2f9f
HWI <dlwognl99@naver.com>
2022-11-20 14:31:39 +0900

    feat: 숫자만 입력 가능 에러 메세지 상수 추가

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,5 +1,6 @@
 const START = "다리 건너기 게임을 시작합니다.";
 const LENGTH = "다리의 길이를 입력해주세요\n.";
+const NUMBER = "[ERROR] 숫자만 입력할 수 있습니다.";
 
 const OUTPUT_MESSAGE = {
   START,
@@ -9,4 +10,7 @@ const INPUT_MESSAGE = {
   LENGTH,
 };
 
-module.exports = { OUTPUT_MESSAGE, INPUT_MESSAGE };
+const ERROR_MESSAGE = {
+  NUMBER,
+};
+module.exports = { OUTPUT_MESSAGE, INPUT_MESSAGE, ERROR_MESSAGE };
