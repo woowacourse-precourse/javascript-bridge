233931f1b63ec249e8d4d188b5a19db37f8fd1d5
Hanbeul You <109324631+hanbeulYou@users.noreply.github.com>
2022-11-20 12:12:43 +0900

    test: 입력 예외 및 종료 테스트 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -126,7 +126,23 @@ describe("다리 건너기 테스트", () => {
     expectBridgeOrder(log, "[ O |   | O ]", "[   | O |   ]");
   });
 
+
   test("입력 예외 테스트2", () => {
+    const logSpy = getLogSpy();
+    mockRandoms(["1", "0", "1"]);
+    mockQuestions(["a", "3", "U", "D", "U"])
+
+    const app = new App();
+    app.play();
+
+    const log = getOutput(logSpy);
+    expectLogContains(log, [
+      "[ERROR]",
+    ]);
+    expectBridgeOrder(log, "[ O |   | O ]", "[   | O |   ]");
+  });
+
+  test("실패 후 종료 테스트1", () => {
     const logSpy = getLogSpy();
     mockRandoms(["1", "0", "1"]);
     mockQuestions(["3", "D", "Q"]);
@@ -143,6 +159,25 @@ describe("다리 건너기 테스트", () => {
       "총 시도한 횟수: 1",
     ]);
     expectBridgeOrder(log, "[  ]", "[ X ]");
+  });
+
+  test("실패 후 종료 테스트2", () => {
+    const logSpy = getLogSpy();
+    mockRandoms(["1", "0", "1"]);
+    mockQuestions(["3", "U", "U", "Q"]);
+
+    const app = new App();
+    app.play();
+
+    const log = getOutput(logSpy);
+    expectLogContains(log, [
+      "최종 게임 결과",
+      "[ O | X ]",
+      "[   |   ]",
+      "게임 성공 여부: 실패",
+      "총 시도한 횟수: 1",
+    ]);
+    expectBridgeOrder(log, "[ O | X ]", "[   |   ]");
   });
 
   test("다리 길이 예외 테스트", () => {
