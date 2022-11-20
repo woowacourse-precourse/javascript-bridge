7e6f5cb7b4a4fccd4898b77cbf1cc52e530f70ec
moseung <tmdah900@naver.com>
2022-11-20 18:42:47 +0900

    ♻️ refactor : BridgeMaker 리팩토링 -ApplicationTest에 맞춰지도록 코드 수정

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,6 +1,8 @@
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
+const { BRIDGE_DETAIL } = require("./constant/Constant");
+
 const BridgeMaker = {
   /**
    * @param {number} size 다리의 길이
@@ -8,9 +10,13 @@ const BridgeMaker = {
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
   makeBridge(size, generateRandomNumber) {
-    const bridgeArray = Array.from({ length: size }).map(() =>
-      generateRandomNumber()
-    );
+    const bridgeArray = Array.from({ length: size }).map((number, index) => {
+      const RandomNumber = Number(generateRandomNumber());
+      console.log(RandomNumber);
+      if (RandomNumber === BRIDGE_DETAIL.UP.NUMBER)
+        return BRIDGE_DETAIL.UP.COMMAND;
+      return BRIDGE_DETAIL.DOWN.COMMAND;
+    });
     return bridgeArray;
   },
 };
