7d9ec91aea44d2ba83b60f3c8fa0288f4f387ca1
JangHoonSeok <orirori512@naver.com>
2022-11-21 07:17:53 +0900

    ✨ feat(Validation): 3이상 20이하인 수가 아니면 에러처리 하는 함수 구현

---
+++ src/Validation.js
@@ -0,0 +1,11 @@
+const { ERROR_MESSAGE } = require("./constants/Messages.js");
+const Validation = {
+  BRIDGE_LENGTH_REGEX: /^[3-9]{1}$|^1{1}[0-9]{1}$|20/,
+  // 3이상 20이하의 숫자가 아니면 Error
+  checkBridgeLengthInput(input) {
+    if (!this.BRIDGE_LENGTH_REGEX.test(input))
+      throw new Error(ERROR_MESSAGE.BRIDGE_LENGTH_ERROR);
+  },
+};
+
+module.exports = Validation;
