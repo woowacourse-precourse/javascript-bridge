8bf1b8fa134335e88e68ec690e09ac5afc8cb9f4
curomame <curomame@naver.com>
2022-11-21 00:39:21 +0900

    ➕ feat - <InputView> [readMoving] add
    사용자가 이동할 칸을 입력받아 callback 함수를 실행시키는 함수 구현

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,5 @@
+const { MissionUtils } = require("@woowacourse/mission-utils");
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -12,8 +14,11 @@ const InputView = {
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
+   * * @param {callback} function 사용자가 이동할 칸을 입력받은 후 실행될 함수
    */
-  readMoving() {},
+  readMoving(callback) {
+    MissionUtils.Console.readLine("이동할 칸을 선택해주세요. (위: U, 아래: D)\n", callback);
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
