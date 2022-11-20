d99e61ecc546c36d0b19fb038bcfbf004c98f6c4
jisu-kim <kss2460@naver.com>
2022-11-20 17:21:20 +0900

    feat(validateInputCatch): input try문 분리

--- src/Validator.js
+++ src/Validator.js
@@ -1,9 +1,9 @@
 const { readMoving } = require("./InputView");
+const { Console } = require("@woowacourse/mission-utils");
 
 const Validator = {
   validateInput(number) {
     const check = /^[0-9]+$/;
-    console.log(number);
     if (3 > number || number > 20 || !check.test(number)) {
       throw "[ERROR] 3 이상 20 이하의 숫자만 입력 가능합니다.";
     }
@@ -18,6 +18,15 @@ const Validator = {
   validateRetryInput(retryInput) {
     if (retryInput !== "R" && retryInput !== "Q") {
       throw "[ERROR] 입력값은 R 또는 Q 만 가능합니다.";
+    }
+  },
+
+  validateInputCatch(bridgeSize) {
+    try {
+      Validator.validateInput(bridgeSize);
+    } catch (error) {
+      Console.print(error);
+      return true;
     }
   },
 
