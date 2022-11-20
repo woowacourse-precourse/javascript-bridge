a1113d2a06209244f5e3ba2562c4ed9f6ccf6d03
JSH99 <tjgus9966@naver.com>
2022-11-20 20:22:56 +0900

    feat(isCorrect): 입력한 다리가 건널 수 있는 다리인지 체크하는 기능 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -2,12 +2,23 @@
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  #bridgeAnswer;
+  #current = 0;
+
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   move() {}
+
+  isCorrect(movingInput) {
+    if (movingInput === this.#bridgeAnswer[this.#current]) {
+      this.#index++;
+      return true;
+    } 
+    return false;
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
