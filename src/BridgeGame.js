248e5026d63edb6f2549e7a62b02ca8c4d69297a
Circlewee <pnj1118@naver.com>
2022-11-20 22:19:57 +0900

    feat(BridgeGame): move 메소드 정의 (#9)
    1. 입력받은 방향으로 게임 정보를 수정

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,12 +1,11 @@
-const BridgeMaker = require('./BridgeMaker');
-const { generate } = require('./BridgeRandomNumberGenerator');
+const BridgeGameStatus = require('./BridgeGameStatus');
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
-  /** @type {string[]} */
-  #bridge;
+  /** @type {BridgeGameStatus} */
+  #BridgeGameStatus;
 
   /**
    * 게임 시작 초기 설정 메서드
@@ -14,15 +13,25 @@ class BridgeGame {
    * @param {number} size
    */
   init(size) {
-    this.#bridge = BridgeMaker.makeBridge(size, generate);
+    this.#BridgeGameStatus = new BridgeGameStatus(size);
   }
 
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+   *
+   * @param {string} direction
    */
-  move() {}
+  move(direction) {
+    const currentStatus = this.#BridgeGameStatus.getGameStatus();
+    const movedStatus = {
+      moveCount: currentStatus.moveCount + 1,
+      movedRoutes: [...currentStatus.movedRoutes, direction],
+    };
+
+    this.#BridgeGameStatus.setGameStatus(movedStatus);
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
