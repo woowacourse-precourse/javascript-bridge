5d40515a782e372c4a42ee1b91413e90a4f75069
nerdchanii <nerdchanii@gmail.com>
2022-11-20 15:18:21 +0900

    refeactor(constants): 파일명 오타 수정

---
+++ src/domain/constants.js
@@ -0,0 +1,13 @@
+const RULE = Object.freeze({
+  // rule of the game
+  BRIDGE: {
+    MIN_LENGTH: 3,
+    MAX_LENGTH: 20,
+  },  
+  BEHAVIOR:{
+    UP: 'U',
+    DOWN: 'D'
+  }
+});
+
+module.exports = RULE;
