3e1c0e42f7513ff34645ef0c571152d2140064e2
seongminn <berry9901@gmail.com>
2022-11-20 15:14:21 +0900

    refactor(InputView): 반복되는 코드 한번에 관리

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -8,40 +8,42 @@ const InputView = {
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize(callback) {
-    Console.readLine(INPUT_MESSAGE.BRIDGE_LENGTH, (input) => {
-      try {
-        callback(input);
-      } catch (err) {
-        Console.print(err.message);
-        this.readBridgeSize(callback).bind(this);
-      }
-    });
+    this.getInput(
+      INPUT_MESSAGE.BRIDGE_LENGTH,
+      callback,
+      this.readBridgeSize(callback).bind(this)
+    );
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving(callback) {
-    Console.readLine(INPUT_MESSAGE.MOVING, (input) => {
-      try {
-        callback(input);
-      } catch (err) {
-        Console.print(err.message);
-        this.readMoving(callback).bind(this);
-      }
-    });
+    this.getInput(
+      INPUT_MESSAGE.MOVING,
+      callback,
+      this.readMoving(callback).bind(this)
+    );
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand(callback) {
-    Console.readLine(INPUT_MESSAGE.RETRY, (input) => {
+    this.getInput(
+      INPUT_MESSAGE.RETRY,
+      callback,
+      this.readGameCommand(callback).bind(this)
+    );
+  },
+
+  getInput(message, callback, reDo) {
+    Console.readLine(message, (input) => {
       try {
         callback(input);
       } catch (err) {
         Console.print(err.message);
-        this.readGameCommand(callback).bind(this);
+        reDo(callback);
       }
     });
   },
