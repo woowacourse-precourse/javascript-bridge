3a55fd3aa371d54b7b68e98706912d55cf65e184
BBIYAC <kiy7833@gmail.com>
2022-11-20 18:44:35 +0900

    fix(readMoving): fix exception handling code in two more turn

--- src/InputView.js
+++ src/InputView.js
@@ -1,5 +1,5 @@
 const { Console } = require("@woowacourse/mission-utils");
-const { MESSAGE_PROCESS, MESSAGE_ERROR, BRIDGE_SIZE_RANGE, MOVING } = require('./Constants');
+const { MESSAGE_PROCESS, MESSAGE_ERROR, BRIDGE_SIZE_RANGE, MOVING, GAME_COMMAND } = require('./Constants');
 const OutputView = require('./OutputView');
 const BridgeMaker = require('./BridgeMaker');
 const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
@@ -8,50 +8,46 @@ const bridgeGame = new BridgeGame();
 
 const InputView = {
   readBridgeSize() {
-    Console.readLine(MESSAGE_PROCESS.INPUT_BRIDGE_LENGTH, this.setBridgeSize.bind(this));
-  },
-
-  setBridgeSize(bridgeSize) {
-    this.validateBridgeSize(bridgeSize);
-    bridgeGame.setBridge(BridgeMaker.makeBridge(bridgeSize, BridgeRandomNumberGenerator));
-    this.readMoving();
+    Console.readLine(MESSAGE_PROCESS.INPUT_BRIDGE_LENGTH, this.validateBridgeSize.bind(this));
   },
 
   validateBridgeSize(bridgeSize) {
     try {
-      if (bridgeSize.trim().length === 0) throw MESSAGE_ERROR.BRIDGE_NOT_EMPTY;
       if (/[^0-9]/.test(bridgeSize)) throw MESSAGE_ERROR.BRIDGE_ONLY_NUMBER;
       if (Number(bridgeSize) < BRIDGE_SIZE_RANGE.MIN || Number(bridgeSize) > BRIDGE_SIZE_RANGE.MAX) {
         throw MESSAGE_ERROR.BRIDGE_OUT_OF_RANGE;
       }
+      this.setBridgeSize(bridgeSize);
     } catch(e) {
       OutputView.printError(e);
       this.readBridgeSize();
     }
   },
 
-  readMoving() {
-    Console.readLine(MESSAGE_PROCESS.INPUT_MOVING, this.setMoving.bind(this));
+  setBridgeSize(bridgeSize) {
+    bridgeGame.setBridge(BridgeMaker.makeBridge(bridgeSize, BridgeRandomNumberGenerator));
+    this.readMoving();
   },
 
-  setMoving(moving) {
-    this.valitateMoving(moving);
-    if (moving.length > 0) {
-      bridgeGame.move(moving);
-      this.printMoveResult();
-    }
+  readMoving() {
+    Console.readLine(MESSAGE_PROCESS.INPUT_MOVING, this.valitateMoving.bind(this));
   },
 
   valitateMoving(moving) {
     try {
-      if (moving.trim().length === 0) throw MESSAGE_ERROR.MOVING_NOT_EMPTY;
       if (moving.length !== 1) throw MESSAGE_ERROR.MOVING_ONLY_CHAR;
       if (moving !== MOVING.UP && moving !== MOVING.DOWN ) throw MESSAGE_ERROR.MOVING_ONLY_U_OR_D;
+      this.setMoving(moving);
     } catch(e) {
       OutputView.printError(e);
       this.readMoving();
     }
-    return true;
+  },
+
+  setMoving(moving) {
+    bridgeGame.move(moving);
+    this.printMoveResult();
+
   },
 
   printMoveResult() {
@@ -62,7 +58,20 @@ const InputView = {
   },
 
   readGameCommand() {
-    Console.readLine(MESSAGE_PROCESS.INPUT_GAME_COMMAND, this.setGameCommand.bind(this));
+    Console.readLine(MESSAGE_PROCESS.INPUT_GAME_COMMAND, this.valitateGameCommand.bind(this));
+  },
+
+  valitateGameCommand(command) {
+    try {
+      if (command.length !== 1) throw MESSAGE_ERROR.GAME_COMMAND_ONLY_CHAR;
+      if (command !== GAME_COMMAND.RETRY && command !== GAME_COMMAND.QUIT ) {
+        throw MESSAGE_ERROR.GAME_COMMAND_ONLY_R_OR_Q;
+      }
+      this.setGameCommand(command);
+    } catch(e) {
+      OutputView.printError(e);
+      this.readGameCommand();
+    }
   },
 
   setGameCommand(command) {
@@ -71,19 +80,6 @@ const InputView = {
       this.readMoving();
     }
     if (command === 'Q') OutputView.printResult(bridgeGame.getGameResult());
-  },
-
-  valitateGameCommand(command) {
-    try {
-      if (command.trim().length === 0) throw MESSAGE_ERROR.GAME_COMMAND_NOT_EMPTY;
-      if (command.length !== 1) throw MESSAGE_ERROR.GAME_COMMAND_ONLY_CHAR;
-      if (command !== GAME_COMMAND.RETRY && command !== GAME_COMMAND.QUIT ) {
-        throw MESSAGE_ERROR.GAME_COMMAND_ONLY_R_OR_Q;
-      }
-    } catch(e) {
-      OutputView.printError(e);
-      this.readGameCommand();
-    }
   }
 };
 
