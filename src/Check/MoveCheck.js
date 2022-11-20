6dbb55338ad7c84400d1d6da142b03c42f8f06fd
dhkham <khamdong@gmail.com>
2022-11-21 00:57:53 +0900

    feat(MoveCheck): throw error if movement input is invalid
    - check if input is "U" or "D"

---
+++ src/Check/MoveCheck.js
@@ -0,0 +1,19 @@
+const { ERROR_MESSAGES } = require('../Constants/Constants');
+
+class MoveCheck {
+  #userInput;
+
+  constructor(userInput) {
+    this.#userInput = userInput;
+    this.checkInputChar(userInput);
+  }
+
+  checkInputChar(userInput) {
+    if (!userInput.match(/^[UD]$/)) {
+        throw new Error(ERROR_MESSAGES.INVALID_CHAR_UD);
+    }
+  }
+}
+
+// InputCheckTest.js 에서 검증하기!
+module.exports = MoveCheck;
