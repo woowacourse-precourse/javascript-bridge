7f3d24343e4c2ca38c5426ae72ea71ff3028afbf
woo-jk <201702042@o.cnu.ac.kr>
2022-11-20 22:52:15 +0900

    refactor: 새로 생성된 Player 클래스에 맞게 Bridge 클래스 변경

--- src/Bridge.js
+++ src/Bridge.js
@@ -12,31 +12,12 @@ class Bridge {
     }
   }
 
-  isMovable(position, direction) {
-    return this.#bridge[position] === direction;
+  isLastPosition(player) {
+    return this.#bridge.length === player.getCurrentPosition();
   }
 
-  isLastStep(position) {
-    return this.#bridge.length === position;
-  }
-
-  getPassedMap(position) {
-    const upperBridge = this.#bridge.slice(0, position).map((direction) => (direction === "U" ? "O" : " "));
-    const lowerBridge = this.#bridge.slice(0, position).map((direction) => (direction === "D" ? "O" : " "));
-    return [upperBridge, lowerBridge];
-  }
-
-  getFailMap(position, direction) {
-    const [upperBridge, lowerBridge] = this.getPassedMap(position);
-    if (direction === "U") {
-      upperBridge.push("X");
-      lowerBridge.push(" ");
-    }
-    if (direction === "D") {
-      upperBridge.push(" ");
-      lowerBridge.push("X");
-    }
-    return [upperBridge, lowerBridge];
+  getNextDirection(player) {
+    return this.#bridge[player.getCurrentPosition()];
   }
 }
 
