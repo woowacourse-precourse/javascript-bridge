ab9c0cecd6189ff1630306ff5ee970b7acfac159
Circlewee <pnj1118@naver.com>
2022-11-20 22:22:08 +0900

    test(BridgeGameStatus): 게임 초기 설정 테스트 (#9)
    1. U와 D로 이루어진 정답다리를 생성하는지 테스트
    2. 게임 정보가 원하는 대로 초기화되었는지 테스트

---
+++ __tests__/BridgeGameStatusTest.js
@@ -0,0 +1,21 @@
+const BridgeGameStatus = require('../src/BridgeGameStatus');
+
+describe('다리 게임 초기 설정 테스트', () => {
+  const size = 5;
+  const bridgeGameStatus = new BridgeGameStatus(size);
+
+  test('U와 D로 이루어진 정답 다리를 생성한다.', () => {
+    const bridge = bridgeGameStatus.getBridge();
+
+    bridge.forEach((value) => {
+      expect(['D', 'U']).toContain(value);
+    });
+  });
+
+  test('원하는 초기 값으로 게임 상태가 설정된다.', () => {
+    const gameStatus = bridgeGameStatus.getGameStatus();
+    const initValue = { moveCount: 0, current: [] };
+
+    expect(gameStatus).toEqual(initValue);
+  });
+});
