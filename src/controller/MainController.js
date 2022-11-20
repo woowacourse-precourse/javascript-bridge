7f10253e730ad9ca9f547141864d6d2fc37438f0
sh981013s <sh981013s@gmail.com>
2022-11-20 17:18:03 +0900

    comment(MainController): 메서드에 주석 작성

--- src/controller/MainController.js
+++ src/controller/MainController.js
@@ -9,18 +9,30 @@ class MainController {
     this.userController = new UserController(this);
   }
 
+  /**
+   * 다리 길이 입력 후 실행 될 연결 메서드
+   * @param bridgeLengthInput {string} [다리 길이 input]
+   * @param mainController {object} [메인 컨트롤러]
+   */
   onBridgeSizeInput(bridgeLengthInput, mainController) {
     mainController.bridgeController.onBridgeSizeInput(bridgeLengthInput);
   }
 
+  /**
+   * 유저 이동 입력 후 생행 될 연결 메서드
+   * @param userMovingInput {string} [유저 이동 input]
+   * @param mainController [메인 컨트롤러]
+   */
   onUserMovingInput(userMovingInput, mainController) {
     mainController.userController.onUserMovingInput(userMovingInput);
   }
 
+  // 유저 이동 입력 연결 메서드
   readUserMovingInput() {
     InputView.readMoving(this.onUserMovingInput, this);
   }
 
+  // 게임 초기 실행 메서드
   init() {
     this.userController.increaseTryCount();
     if (this.userController.getTryCount() === 1) {
