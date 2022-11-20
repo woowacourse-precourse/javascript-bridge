ccef7907bc1f094b50ced0d18b739785985f4555
yan9vvoojin <yan9vvoojin@naver.com>
2022-11-20 12:44:11 +0900

    feat(App): 사용자가 다리 길이를 입력하면 처리하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+const { Console } = require("@woowacourse/mission-utils");
+const BridgeGame = require("./BridgeGame");
+const InputView = require("./InputView");
 const OutputView = require("./OutputView");
 
 class App {
+  #bridgeGame;
+
   play() {
     OutputView.printStart();
+    InputView.readBridgeSize(this.tryMakeBridge.bind(this));
+  }
+
+  tryMakeBridge(input) {
+    const size = Number(input);
+    try {
+      this.#bridgeGame = new BridgeGame(size);
+      InputView.readMoving(/** */);
+    } catch ({ message }) {
+      Console.print(message);
+      InputView.readBridgeSize(this.tryMakeBridge.bind(this));
+    }
   }
 }
 
