81e41fa7089bcf6e35483110de62e052e03dbdf8
Jiwoo Park <jiwoo9446@naver.com>
2022-11-20 21:17:18 +0900

    feat: 입력값에 따른 재시도 또는 강제종료 메서드 구현

--- src/Controller.js
+++ src/Controller.js
@@ -87,7 +87,7 @@ class Controller {
   commandForm(input) {
     try {
       this.validation.isRestartOrQuit(input);
-      Console.print(input);
+      this.RetryOrQuitGame(input);
     } catch (error) {
       Console.print(error);
     }
@@ -102,6 +102,16 @@ class Controller {
     OutputView.printMap(this.#currentBridge);
     OutputView.printResult(this.#try, false);
     Console.close();
+  }
+
+  RetryOrQuitGame(input) {
+    if (input === "R") {
+      this.retryGame();
+    }
+    if (input === "Q") {
+      this.quitGame();
+    }
+    this.#try += 1;
   }
 }
 
