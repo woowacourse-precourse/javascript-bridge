a72a4399718b4b4a639931b47f91f7d9745e50c3
Woong <jiyko7@kyonggi.ac.kr>
2022-11-21 00:08:37 +0900

    test: 테스트 추가
    test: 테스트 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -85,3 +85,78 @@ describe("다리 건너기 테스트", () => {
     runException(["a"]);
   });
 });
+
+describe("다리 테스트", () => {
+  test("생성 테스트", () => {
+    const randomNumbers = ["0", "1", "1", "0"];
+    const mockGenerator = randomNumbers.reduce((acc, number) => {
+      return acc.mockReturnValueOnce(number);
+    }, jest.fn());
+
+    const bridge = BridgeMaker.makeBridge(4, mockGenerator);
+    expect(bridge).toEqual(["D", "U", "U", "D"]);
+  });
+
+  test("이동 예외 테스트", () => {
+    const logSpy = getLogSpy();
+    mockRandoms(["1", "1", "0", "1"]);
+    mockQuestions(["4", "P"]);
+
+    const app = new App();
+    app.play();
+
+    expectLogContains(getOutput(logSpy), ["[ERROR]"]);
+  });
+
+  test("재시도 예외 테스트", () => {
+    const logSpy = getLogSpy();
+    mockRandoms(["1", "1", "0", "1"]);
+    mockQuestions(["4", "U", "D", "X"]);
+
+    const app = new App();
+    app.play();
+
+    expectLogContains(getOutput(logSpy), ["[ERROR]"]);
+  });
+
+
+  test("실패 테스트", () => {
+    const logSpy = getLogSpy();
+    mockRandoms(["1", "0", "1", "1"]);
+    mockQuestions(["4", "U", "D", "D", "Q"]);
+
+    const app = new App();
+    app.play();
+
+    const log = getOutput(logSpy);
+    expectLogContains(log, [
+      "최종 게임 결과",
+      "[ O |   |   ]",
+      "[   | O | X ]",
+      "게임 성공 여부: 실패",
+      "총 시도한 횟수: 1",
+    ]);
+    expectBridgeOrder(log, "[ O |   |   ]", "[   | O | X ]");
+  });
+
+  test("횟수 테스트", () => {
+    const logSpy = getLogSpy();
+    mockRandoms(["1", "0", "1", "1"]);
+    mockQuestions(["4", "D", "R", "U", "D", "U", "U"]);
+
+    const app = new App();
+    app.play();
+
+    const log = getOutput(logSpy);
+    expectLogContains(log, [
+      "최종 게임 결과",
+      "[ O |   | O | O ]",
+      "[   | O |   |   ]",
+      "게임 성공 여부: 성공",
+      "총 시도한 횟수: 2",
+    ]);
+    expectBridgeOrder(log, "[ O |   | O | O ]", "[   | O |   |   ]");
+  });
+
+  
+});
