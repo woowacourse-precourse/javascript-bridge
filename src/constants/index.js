d4657ce7726376b6bccc00cc5134bb13ae6f0ae8
unknown <sjjoseph106@gmail.com>
2022-11-20 22:03:40 +0900

    refactor: 상수 객체 동결

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,26 +1,26 @@
-const SIZE = {
+const SIZE = Object.freeze({
   MIN: 3,
   MAX: 20,
-};
+});
 
-const WAY = {
+const WAY = Object.freeze({
   UP: "U",
   DOWN: "D",
-};
+});
 
-const MESSAGE = {
+const MESSAGE = Object.freeze({
   START_GAME: "다리 건너기 게임을 시작합니다.",
   INPUT_SIZE: "다리의 길이를 입력해주세요.",
   INPUT_MOVING: `이동할 칸을 선택해주세요. (위: ${WAY.UP}, 아래: ${WAY.DOWN})`,
-};
+});
 
 const PREFIX = "[ERROR]";
 
-const ERROR = {
+const ERROR = Object.freeze({
   SIZE_TYPE: `${PREFIX} 다리 길이는 숫자를 입력해야 합니다.`,
   SIZE_RANGE: `${PREFIX} 다리 길이는 ${SIZE.MIN}부터  ${SIZE.MAX} 사이의 숫자여야 합니다.`,
   INVALID_MOVING: `${PREFIX} ${WAY.UP} 또는 ${WAY.DOWN} 중 하나를 입력해야 합니다.`,
-};
+});
 
 module.exports = {
   SIZE,
