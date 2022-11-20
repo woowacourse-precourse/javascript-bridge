c35de104e832b0731c02f2ebe23d8bda7283a4b9
yong <tkddbs1644@gmail.com>
2022-11-21 02:16:41 +0900

    feat(BridgeGame): 다리를 건너는 기능 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -18,19 +18,36 @@ class BridgeGame {
     this.#totalTrial = 1;
   }
 
+  isFinished() {
+    return this.#position >= this.#bridge.length;
+  }
+
+  isMatchCommand(command) {
+    return command === this.#bridge[this.#position];
+  }
+
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(command) {
+    if (this.isFinished()) {
+      return;
+    }
+    if (this.isMatchCommand(command)) {
+      this.#position += 1;
+      return;
+    }
+    return;
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  retry() {}
+  retry(command) {}
 }
 
 new BridgeGame(3);
