f86c3bea1f7687c657bc87bf5abeba7ecb871be8
WaiNaat <qoli0703@gmail.com>
2022-11-20 19:52:27 +0900

    test: add move success decision test code
    해당 기능: e7625777c3ed3fc597237d550c45749892bd6251

---
+++ __tests__/BridgeTest.js
@@ -0,0 +1,26 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const BridgeRandomNumberGenerator = require('../src/BridgeRandomNumberGenerator');
+const BridgeMaker = require('../src/BridgeMaker');
+const Bridge = require('../src/Bridge');
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce(
+    (acc, number) => acc.mockReturnValueOnce(number),
+    MissionUtils.Random.pickNumberInRange,
+  );
+};
+
+describe('Bridge 기능 테스트', () => {
+  test('이동 성공 여부 판단', () => {
+    mockRandoms([1, 0, 1, 0]);
+
+    const bridgeArray = BridgeMaker.makeBridge(4, BridgeRandomNumberGenerator.generate);
+    const bridge = new Bridge(bridgeArray);
+    const result = [];
+
+    result.push(bridge.moveSuccess(1, 'U'));
+    result.push(bridge.moveSuccess(1, 'D'));
+    expect(result).toEqual([false, true]);
+  });
+});
