0717782940874a441ef169bcaf23202aad629dc8
Gaeun-Jang <110662057+Gaeun-Jang@users.noreply.github.com>
2022-11-20 17:19:53 +0900

    test: 예외 테스트 코드 리팩터링

--- __tests__/ValidateTest.js
+++ __tests__/ValidateTest.js
@@ -1,47 +1,15 @@
 const Validate = require('../src/Validate');
 
-describe('다리 길이 예외 테스트', () => {
-  test('3 이상 20 이하가 아닌 경우', () => {
-    expect(Validate.checkBridgeSize(1)).toBe(false);
+describe('예외 테스트', () => {
+  test.each([1, NaN])('다리 길이 예외 테스트', (input) => {
+    expect(Validate.checkBridgeSize(input)).toBeFalsy();
   });
 
-  test('숫자가 아닌 경우', () => {
-    expect(Validate.checkBridgeSize(NaN)).toBe(false);
-  });
-});
-
-describe('이동 키 예외 테스트', () => {
-  test('소문자가 들어올 경우', () => {
-    expect(Validate.checkMovingKey('u')).toBe(false);
+  test.each(['u', 'A', '!', ' '])('이동 키 예외 테스트', (input) => {
+    expect(Validate.checkMovingKey(input)).toBeFalsy();
   });
 
-  test('U 또는 D가 아닌 경우', () => {
-    expect(Validate.checkMovingKey('A')).toBe(false);
-  });
-
-  test('특수문자가 들어온 경우', () => {
-    expect(Validate.checkMovingKey('!')).toBe(false);
-  });
-
-  test('공백이 들어온 경우', () => {
-    expect(Validate.checkMovingKey(' ')).toBe(false);
-  });
-});
-
-describe('명령 키 예외 테스트', () => {
-  test('소문자가 들어올 경우', () => {
-    expect(Validate.checkCommandKey('r')).toBe(false);
-  });
-
-  test('R 또는 Q가 아닌 경우', () => {
-    expect(Validate.checkCommandKey('A')).toBe(false);
-  });
-
-  test('특수문자가 들어온 경우', () => {
-    expect(Validate.checkCommandKey('!')).toBe(false);
-  });
-
-  test('공백이 들어온 경우', () => {
-    expect(Validate.checkCommandKey(' ')).toBe(false);
+  test.each(['r', 'A', '!', ' '])('명령 키 예외 테스트', (input) => {
+    expect(Validate.checkCommandKey(input)).toBeFalsy();
   });
 });
