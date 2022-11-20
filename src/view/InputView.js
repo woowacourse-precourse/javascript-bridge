fad11d8006a17e25bf1c4de392e30863fa13e54e
dlwl98 <ff981113@hanyang.ac.kr>
2022-11-21 02:05:44 +0900

    refactor(InputView): 외부에서 호출할 필요 없는 메서드들 객체 밖으로 분리

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,6 +1,46 @@
 const { Console } = require('@woowacourse/mission-utils');
 const Validator = require('../utils/Validator');
 const { BRIDGE, GAME_COMMAND } = require('../constants');
+
+/**
+ * @param {string} input 플레이어의 입력
+ * @param {function(string): void} callback 입력받은 후 실행할 함수
+ * @return {function(): void} 에러핸들러에게 전달할 콜백함수
+ */
+function validateBridgeSize(input, callback) {
+  return () => {
+    Validator.validateIsNumber(input);
+    Validator.validateInRange(BRIDGE.MIN_LENGTH, BRIDGE.MAX_LENGTH, input);
+    callback(input);
+  };
+}
+
+/**
+ * @param {string} input 플레이어의 입력
+ * @param {string[]} allowedSet 허용되는 입력들의 집합
+ * @param {function(string): void} callback 입력받은 후 실행할 함수
+ * @return {function(): void} 에러핸들러에게 전달할 콜백함수
+ */
+function validateCommand(input, allowedSet, callback) {
+  return () => {
+    Validator.validateIncludes(allowedSet, input);
+    callback(input);
+  };
+}
+
+/**
+ * @param {function(): void} callback 입력 유효성을 검증하기 위한 콜백함수
+ * @param {function(): void} onError 에러 발생시 실행할 함수
+ */
+function errorHandler(callback, onError) {
+  try {
+    callback();
+  } catch (err) {
+    Console.print(`\n${err.message}`);
+    onError();
+  }
+}
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -10,23 +50,10 @@ const InputView = {
    */
   readBridgeSize(callback) {
     Console.print('다리의 길이를 입력해주세요.');
-    Console.readLine('', (input) => this.errorHandler(
-      this.validateBridgeSize(input, callback),
+    Console.readLine('', (input) => errorHandler(
+      validateBridgeSize(input, callback),
       () => this.readBridgeSize(callback),
     ));
-  },
-
-  /**
-   * @param {string} input 플레이어의 입력
-   * @param {function(string): void} callback 입력받은 후 실행할 함수
-   * @return {function(): void} 에러핸들러에게 전달할 콜백함수
-   */
-  validateBridgeSize(input, callback) {
-    return () => {
-      Validator.validateIsNumber(input);
-      Validator.validateInRange(BRIDGE.MIN_LENGTH, BRIDGE.MAX_LENGTH, input);
-      callback(input);
-    };
   },
 
   /**
@@ -34,8 +61,8 @@ const InputView = {
    */
   readMoving(callback) {
     Console.print(`이동할 칸을 선택해주세요. (위: ${BRIDGE.ABOVE}, 아래: ${BRIDGE.BELOW})`);
-    Console.readLine('', (input) => this.errorHandler(
-      this.validateCommand(input, [BRIDGE.ABOVE, BRIDGE.BELOW], callback),
+    Console.readLine('', (input) => errorHandler(
+      validateCommand(input, [BRIDGE.ABOVE, BRIDGE.BELOW], callback),
       () => this.readMoving(callback),
     ));
   },
@@ -45,36 +72,10 @@ const InputView = {
    */
   readGameCommand(callback) {
     Console.print(`게임을 다시 시도할지 여부를 입력해주세요. (재시도: ${GAME_COMMAND.RETRY}, 종료: ${GAME_COMMAND.QUIT})`);
-    Console.readLine('', (input) => this.errorHandler(
-      this.validateCommand(input, [GAME_COMMAND.RETRY, GAME_COMMAND.QUIT], callback),
+    Console.readLine('', (input) => errorHandler(
+      validateCommand(input, [GAME_COMMAND.RETRY, GAME_COMMAND.QUIT], callback),
       () => this.readGameCommand(callback),
     ));
-  },
-
-  /**
-   * @param {string} input 플레이어의 입력
-   * @param {string[]} allowedSet 허용되는 입력들의 집합
-   * @param {function(string): void} callback 입력받은 후 실행할 함수
-   * @return {function(): void} 에러핸들러에게 전달할 콜백함수
-   */
-  validateCommand(input, allowedSet, callback) {
-    return () => {
-      Validator.validateIncludes(allowedSet, input);
-      callback(input);
-    };
-  },
-
-  /**
-   * @param {function(): void} callback 입력 유효성을 검증하기 위한 콜백함수
-   * @param {function(): void} onError 에러 발생시 실행할 함수
-   */
-  errorHandler(callback, onError) {
-    try {
-      callback();
-    } catch (err) {
-      Console.print(`\n${err.message}`);
-      onError();
-    }
   },
 };
 
