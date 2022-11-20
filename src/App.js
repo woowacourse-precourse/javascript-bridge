3f8308dceee2303d2a215a8e80867ce0646889ba
woo-jk <201702042@o.cnu.ac.kr>
2022-11-20 13:53:11 +0900

    feat: App 클래스 retrtOrNotInput 함수 구현

--- src/App.js
+++ src/App.js
@@ -57,6 +57,38 @@ class App {
       this.retryOrNotInput();
     }
   }
+
+  retryOrNotInput() {
+    InputView.readGameCommand(this.retryOrNot.bind(this));
+  }
+
+  retryOrNot(command) {
+    try {
+      InputValidate.checkRetryOrQuitCommand(command);
+    } catch (error) {
+      OutputView.printErrorMessage(error.message);
+      this.retryOrNotInput();
+    }
+    if (command === "R") {
+      this.#bridgeGame.retry();
+      this.moveInput();
+    }
+    if (command === "Q") {
+      this.success();
+    }
+  }
+
+  fail(direction) {
+    const [map, tryCount] = this.#bridgeGame.getFailResult(direction);
+    OutputView.printResult(this.#bridgeGame.isSuccess(), map, tryCount);
+    MissionUtils.Console.close();
+  }
+
+  success() {
+    const [map, tryCount] = this.#bridgeGame.getResult();
+    OutputView.printResult(this.#bridgeGame.isSuccess(), map, tryCount);
+    MissionUtils.Console.close();
+  }
 }
 
 const app = new App();
