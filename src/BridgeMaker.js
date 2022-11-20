15fa8b74bd67c0d0fbc75261150133aa3124aed6
Deemou <4106243@naver.com>
2022-11-20 22:31:53 +0900

    fix(BridgeMaker): 함수 호출 문제 해결

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -11,7 +11,7 @@ const BridgeMaker = {
   makeBridge(size, generateRandomNumber) {
     const bridge = [];
     for (let i = 0; i < size; i++) {
-      const space = generateRandomNumber() == 0 ? BRIDGE.DOWN : BRIDGE.UP;
+      const space = generateRandomNumber == BRIDGE.LOWER ? BRIDGE.DOWN : BRIDGE.UP;
       bridge.push(space);
     }
     return bridge;
