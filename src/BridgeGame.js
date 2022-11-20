d272da5f5467b9ff0fb9effa6d5a9b5d6f8eb3df
daal2quf <daal2quf@gmail.com>
2022-11-20 21:58:02 +0900

    feat(bridgeGame): 유저가 이동한 다리를 출력하기 위한 이동다리 만드는 기능 추가)
    add:
    drawingBridge(): 유저가 이동한 배열과 다리 게임 정답인 password를 비교해 다리를 만듬
    bridgeCloseConverter(): 유저가 이동한 다리가 만들고 다리 배열의 마지막이 '|'일때 ']'로 변경

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,17 +1,19 @@
-const BridgeMaker = require("./BridgeMaker");
-const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+const BridgeMaker = require('./BridgeMaker');
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
   #password;
   constructor() {
-    this.#password;
     this.userInput = [];
   }
 
   generateOfBridgeGamePassword(bridgeLength) {
-    this.#password = BridgeMaker.makeBridge(bridgeLength, BridgeRandomNumberGenerator.generate);
+    this.#password = BridgeMaker.makeBridge(
+      bridgeLength,
+      BridgeRandomNumberGenerator.generate,
+    );
   }
 
   stackOfUserMovingInput(userMovingInput) {
@@ -22,21 +24,57 @@ class BridgeGame {
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {
-  }
+  move() {}
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  retry() {
-  }
+  retry() {}
 
   checkUserMovingInput() {
-    if (this.userInput[this.userInput.length - 1] === this.#password[this.userInput.length - 1]) {
-      return true
+    if (
+      this.userInput[this.userInput.length - 1] ===
+      this.#password[this.userInput.length - 1]
+    ) {
+      return true;
     }
-    return false
+    return false;
+  }
+
+  drawingBridge(userMoveInputCollection, password) {
+    const upside = ['['];
+    const downside = ['['];
+    userMoveInputCollection.forEach((movingInput, index) => {
+      if (movingInput === password[index]) {
+        if (movingInput === 'U') {
+          upside.push('O', '|');
+          downside.push(' ', '|');
+        } else if (movingInput === 'D') {
+          upside.push(' ', '|');
+          downside.push('O', '|');
+        }
+      } else if (movingInput !== password[index]) {
+        if (movingInput === 'U') {
+          upside.push('X', '|');
+          downside.push(' ', '|');
+        } else if (movingInput === 'D') {
+          upside.push(' ', '|');
+          downside.push('X', '|');
+        }
+      }
+    });
+    return [
+      BridgeGame.bridgeCloseConverter(upside).join(' '),
+      BridgeGame.bridgeCloseConverter(downside).join(' '),
+    ];
+  }
+
+  static bridgeCloseConverter(drawingBridge) {
+    if (drawingBridge[drawingBridge.length - 1] === '|') {
+      drawingBridge[drawingBridge.length - 1] = ']';
+    }
+    return drawingBridge;
   }
 }
 
