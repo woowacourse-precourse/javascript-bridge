0b89578121adf9d16166397bdb58a0d532a29223
yaena1223 <yaena1223@dgu.ac.kr>
2022-11-21 07:24:21 +0900

    feat: print result when success

--- src/App.js
+++ src/App.js
@@ -3,38 +3,43 @@ const OutputView = require('./OutputView');
 const BridgeMaker = require('./BridgeMaker');
 const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
 const BridgeGame = require('./BridgeGame');
+const { Console } = require('@woowacourse/mission-utils');
+
 class App {
   async play() {
     OutputView.printStart();
-    const SIZE = await InputView.readBridgeSize();
+    const SIZE = parseInt(await InputView.readBridgeSize());
     const generateRandomNumber = BridgeRandomNumberGenerator.generate;
     const ANSWER = BridgeMaker.makeBridge(SIZE, generateRandomNumber);
-    console.log(ANSWER);
     const GAME = new BridgeGame(ANSWER);
     this.repeatMove(SIZE, GAME);
   }
 
   //모두 이동하거나, 실패하기 전까지 반복.
   async repeatMove(size, GAME) {
-    let CNT = 0;
+    let [CNT, RESULT] = [0, []];
     while (CNT < size) {
       const MOVE = await InputView.readMoving();
-      const RESULT = GAME.move(CNT, MOVE);
+      RESULT = GAME.move(CNT, MOVE);
       OutputView.printMap(RESULT);
       if (RESULT[CNT][1] === 'X') this.selectRestart(size, GAME);
       CNT += 1;
     }
+    OutputView.printResult(RESULT, GAME.getCnt());
+    Console.close();
   }
 
   //실패하면 Restart여부 선택
   async selectRestart(size, GAME) {
     const RESTART_OR_END = await InputView.readGameCommand();
-    const RESULT = GAME.getResult();
-    const CNT = GAME.getCnt();
+    const [RESULT, CNT] = [GAME.getResult(), GAME.getCnt()];
     if (RESTART_OR_END === 'R') {
       GAME.retry();
       this.repeatMove(size, GAME);
-    } else OutputView.printResult(RESULT, CNT);
+    } else {
+      OutputView.printResult(RESULT, CNT);
+      Console.close();
+    }
   }
 }
 
