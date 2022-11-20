9913df7ec5c6758299068aac39ec02184d9289d6
nalsae <nalsae0207@naver.com>
2022-11-21 02:26:42 +0900

    feat: Implement a method to validate an input of movement
    이동할 칸의 입력 값을 검사하는 로직 추가

--- src/Validator.js
+++ src/Validator.js
@@ -1,4 +1,5 @@
 const ERROR_MESSAGES = require('./utils/messages');
+const { WRONG_BRIDGE_SIZE, WRONG_MOVEMENT } = ERROR_MESSAGES;
 
 const Validator = {
   bridgeSize(size) {
@@ -8,7 +9,11 @@ const Validator = {
       +size < 3 ||
       +size > 20
     )
-      throw new Error(ERROR_MESSAGES.BRIDGE_SIZE);
+      throw new Error(WRONG_BRIDGE_SIZE);
+  },
+
+  moving(movement) {
+    if (movement !== 'U' && movement !== 'D') throw new Error(WRONG_MOVEMENT);
   },
 };
 
