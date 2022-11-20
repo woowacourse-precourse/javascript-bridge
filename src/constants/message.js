b5b70b25301e1bdbe0a946a39103a449e97c0a0f
HWI <dlwognl99@naver.com>
2022-11-20 14:37:22 +0900

    feat: 다리 길이 입력 범위 에러 메세지 상수 추가

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,6 +1,7 @@
 const START = "다리 건너기 게임을 시작합니다.";
 const LENGTH = "다리의 길이를 입력해주세요\n.";
 const NUMBER = "[ERROR] 숫자만 입력할 수 있습니다.";
+const RANGE = "[ERROR] 다리 길이의 입력은 3부터 20 사이의 숫자여야 합니다.";
 
 const OUTPUT_MESSAGE = {
   START,
@@ -12,5 +13,6 @@ const INPUT_MESSAGE = {
 
 const ERROR_MESSAGE = {
   NUMBER,
+  RANGE,
 };
 module.exports = { OUTPUT_MESSAGE, INPUT_MESSAGE, ERROR_MESSAGE };
