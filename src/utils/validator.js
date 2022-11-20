4394017b6ca80cd680af6ef72433ef00da54a2f4
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-11-20 15:14:34 +0900

    fix: 예외 발생시 재시작을 위해 Console.close 제거

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -10,16 +10,14 @@ const isCollectBridgeLength = (input) => {
 const isValidateMoveInput = (number) => {
   const moveInputRegex = /^[U|D]$/g;
   if (!number.match(moveInputRegex)) {
-    Console.close();
-    throw new Error('U 또는 D 문자만 입력이 가능합니다.');
+    throw new Error('[ERROR] U 또는 D 문자만 입력이 가능합니다.');
   }
 };
 
 const isValidateRetryInput = (number) => {
   const moveInputRegex = /^[R|Q]$/g;
   if (!number.match(moveInputRegex)) {
-    Console.close();
-    throw new Error('R 또는 Q 문자만 입력이 가능합니다.');
+    throw new Error('[ERROR] R 또는 Q 문자만 입력이 가능합니다.');
   }
 };
 
