717095140f92f53611fcb0fd004d82261f5fa876
Deemou <4106243@naver.com>
2022-11-20 22:35:04 +0900

    feat(BridgeGame): #currentPosition 추가, 생성자 변경, 오탈자 수정

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -7,11 +7,13 @@ const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
 class BridgeGame {
   #bridge;
   #tryCount;
+  #currentPosition;
   #map;
-  constructer(size) {
+  constructor(size) {
     this.#bridge = BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator);
+    this.#currentPosition = 0;
     this.#tryCount = 1;
-    this.#map = [];
+    this.#map = [[], []];
   }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
