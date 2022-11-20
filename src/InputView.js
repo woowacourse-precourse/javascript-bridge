3494c8fa4a3a830f2f05f3b44039ec30d2af848f
jhyns <tockiib@gmail.com>
2022-11-20 16:59:12 +0900

    feat: Add InputView without callback
    InputView에서 입력받는 부분 추가
    콜백함수는 추후 추가 예정

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,7 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const BridgeMaker = require('./BridgeMaker');
+const MESSAGES = require('./Constants');
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,17 +9,44 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    MissionUtils.Console.readLine(MESSAGES.INPUT_BRIDGE_LENGTH, (sizeInput) => {
+      this.generateBridge(sizeInput);
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    MissionUtils.Console.readLine(MESSAGES.INPUT_MOVE, (moving) => {});
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+    MissionUtils.Console.readLine(MESSAGES.INPUT_RETRY, (command) => {});
+  },
+
+  validateBridgeSize(size) {
+    if (isNaN(size)) {
+      throw new Error(MESSAGES.ERROR.BRIDGE_LENGTH_NAN);
+    }
+    if (!Number.isInteger(size)) {
+      throw new Error(MESSAGES.ERROR.BRIDGE_LENGTH_NOT_INTEGER);
+    }
+    if (size < 3 || size > 20) {
+      throw new Error(MESSAGES.ERROR.BRIDGE_LENGTH_RANGE);
+    }
+  },
+
+  generateBridge(input) {
+    const size = Number(input);
+
+    this.validateBridgeSize(size);
+    BridgeMaker.makeBridge(size);
+  },
 };
 
 module.exports = InputView;
