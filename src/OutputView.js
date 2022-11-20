1eb7cb3e82727e2dbe1a9eb14b3a1f7fef5f07cc
Suyoung <jsy8492@gmail.com>
2022-11-20 20:29:20 +0900

    refactor(OutputView): 다리의 상태를 출력하는 기능을 printMap 메서드를 활용하도록 수정

--- src/OutputView.js
+++ src/OutputView.js
@@ -17,12 +17,9 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult({ map, isClear, totalTryCount }) {
-    const [upperMap, lowerMap] = map;
-
+  printResult({ bridgeMap, isClear, totalTryCount }) {
     Console.print('최종 게임 결과');
-    Console.print(`[ ${upperMap.join(' | ')} ]`);
-    Console.print(`[ ${lowerMap.join(' | ')} ]`);
+    this.printMap(bridgeMap);
     Console.print(`게임 성공 여부: ${isClear === true ? '성공' : '실패'}`);
     Console.print(`총 시도한 횟수: ${totalTryCount}`);
     Console.close();
