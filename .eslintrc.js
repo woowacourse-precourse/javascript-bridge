da5420fd4f2c95c1cbdeadf44a3b25977251b029
Quartz <skyu.dev@gmail.com>
2022-11-20 13:29:38 +0900

    chore: jest eslint 플러그인 설치

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,19 @@ module.exports = {
     commonjs: true,
     es2021: true,
   },
-  extends: ["airbnb", "airbnb/hooks"],
+  extends: ["airbnb", "airbnb/hooks", "plugin:jest-extended/all"],
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",
   },
+
   rules: {
     quotes: ["off", "single"],
     "class-methods-use-this": "off",
     "arrow-body-style": "off",
     "max-depth": ["error", 2],
     "max-lines-per-function": ["error", 10],
+    "jest-extended/prefer-to-be-true": "warn",
+    "jest-extended/prefer-to-be-false": "error",
   },
 };
--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ const BridgeGame = require("./presenter/BridgeGame");
 
 class App {
   play() {
-    // const game = new BridgeGame();
-    // game.start();
     const bridgeGamePresenter = new BridgeGame();
     bridgeGamePresenter.start();
   }
 }
 
-const app = new App();
-app.play();
+// const app = new App();
+// app.play();
 
 module.exports = App;
