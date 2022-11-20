462198ae2616137c1bfcb76df3b13cc62f10da01
evencoding <evencoding@gmail.com>
2022-11-21 00:20:30 +0900

    test(ValidatorTest): beforeEach로 각 테스트 전에 공백에 대한 예외 테스트를 하는 것으로 중복 제거

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -1,22 +1,15 @@
 const Validator = require('../src/Validator');
 
-test('문자에 공백이 포함되어 있다면 예외가 발생한다.', () => {
+const blackTest = test('문자에 공백이 포함되어 있다면 예외가 발생한다.', () => {
   const inputValues = [' ', ' 3', '12 ', '3 4'];
   inputValues.forEach((inputValue) => {
     expect(() => Validator.throwErrorIfHasBlank(inputValue)).toThrow('[ERROR]');
   });
 });
 
-describe('다리 길이에 대한 예외 테스트', () => {
-  test('문자에 공백이 포함되어 있다면 예외가 발생한다.', () => {
-    const inputValues = [' ', ' 3', '12 ', '3 4'];
-    inputValues.forEach((inputValue) => {
-      expect(() => Validator.throwErrorIfHasBlank(inputValue)).toThrow(
-        '[ERROR]'
-      );
-    });
-  });
+beforeEach(() => blackTest);
 
+describe('다리 길이에 대한 예외 테스트', () => {
   test('숫자가 아니라면 예외가 발생한다.', () => {
     const inputValues = ['잉', '#', 'number', '1e3'];
     inputValues.forEach((inputValue) => {
@@ -33,15 +26,6 @@ describe('다리 길이에 대한 예외 테스트', () => {
 });
 
 describe('방향에 대한 예외 테스트', () => {
-  test('문자에 공백이 포함되어 있다면 예외가 발생한다.', () => {
-    const inputValues = [' ', ' 3', '12 ', '3 4'];
-    inputValues.forEach((inputValue) => {
-      expect(() => Validator.throwErrorIfHasBlank(inputValue)).toThrow(
-        '[ERROR]'
-      );
-    });
-  });
-
   test('대문자 U 혹은 D 가 아니라면 예외가 발생한다.', () => {
     const inputValues = ['u', 'd', 'UD', 'DU'];
     inputValues.forEach((inputValue) => {
@@ -53,15 +37,6 @@ describe('방향에 대한 예외 테스트', () => {
 });
 
 describe('재시작 커맨드에 대한 예외 테스트', () => {
-  test('문자에 공백이 포함되어 있다면 예외가 발생한다.', () => {
-    const inputValues = [' ', ' 3', '12 ', '3 4'];
-    inputValues.forEach((inputValue) => {
-      expect(() => Validator.throwErrorIfHasBlank(inputValue)).toThrow(
-        '[ERROR]'
-      );
-    });
-  });
-
   test('대문자 R 혹은 Q 가 아니라면 예외가 발생한다.', () => {
     const inputValues = ['r', 'q', 'RQ', 'RQ'];
     inputValues.forEach((inputValue) => {
