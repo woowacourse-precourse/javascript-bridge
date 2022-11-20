1524183ac3998c516cfe378d29fa088eb83c1cfa
MAGHC <windowsapple95@gmail.com>
2022-11-20 18:10:49 +0900

    refactor: Constant 안쓰는 상수삭제및 이동 , OutputView 상수 적용

--- src/Constant.js
+++ src/Constant.js
@@ -1,21 +1,8 @@
-const GAME_RESULTS_MESSAGES = Object.freeze({
-  SUCCESS: '성공',
-  FAIL: '실패',
-  TRY: '총 시도한 횟수',
-});
-
 const BRINDGE_DIRECTION = Object.freeze({
   UP: 'U',
   DOWN: 'D',
 });
 
-const VIEW_FIGURE = Object.freeze({
-  O: 'O',
-  X: 'X',
-});
-
 module.exports = {
-  VIEW_FIGURE,
   BRINDGE_DIRECTION,
-  GAME_RESULTS_MESSAGES,
 };
--- src/OutputView.js
+++ src/OutputView.js
@@ -1,12 +1,17 @@
 const Utils = require('./Utils');
 
-const mockMap = [['[ O |   | O ]'], ['[   | O |   ]']];
-
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 const OutputView = Object.freeze({
-  GAME_OUTPUT_MESSAGES: { START: '다리 건너기 게임을 시작합니다.' },
+  GAME_OUTPUT_MESSAGES: {
+    START: '다리 건너기 게임을 시작합니다.',
+    GAME_RESULT: '최종 게임 결과',
+    GAME_SUCCESS: '게임 성공 여부:',
+    SUCCESS: '성공',
+    FAIL: '실패',
+    TRY: '총 시도한 횟수:',
+  },
 
   printStart() {
     Utils.print(this.GAME_OUTPUT_MESSAGES.START);
@@ -27,10 +32,12 @@ const OutputView = Object.freeze({
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printResult(map, tryOfNUmber, success) {
-    const isSuccess = success ? '성공' : '실패';
-    Utils.print('최종 게임 결과');
+    const isSuccess = success ? this.GAME_OUTPUT_MESSAGES.SUCCESS : this.GAME_OUTPUT_MESSAGES.FAIL;
+    Utils.print(this.GAME_OUTPUT_MESSAGES.GAME_RESULT);
     OutputView.printMap(map);
-    Utils.print(`게임 성공 여부: ${isSuccess}\n총 시도한 횟수: ${tryOfNUmber}`);
+    Utils.print(
+      `${this.GAME_OUTPUT_MESSAGES.GAME_SUCCESS} ${isSuccess}\n${this.GAME_OUTPUT_MESSAGES.TRY} ${tryOfNUmber}`,
+    );
   },
 });
 
