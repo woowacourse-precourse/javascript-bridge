747c5f00cea8a28f3b5200fa8b39af8bf62b974c
feb-dain <feb.dain@gmail.com>
2022-11-20 15:29:25 +0900

    feat: print opening message

--- src/UI/OutputView.js
+++ src/UI/OutputView.js
@@ -1,7 +1,14 @@
+const { GAME_MESSAGE } = require("../Utils/Constants");
+const { START } = GAME_MESSAGE;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 const OutputView = {
+  printStart() {
+    print(START);
+  },
+
   /**
    * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
    * <p>
