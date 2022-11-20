1bde4c7803ef0b94794f86c174a745053efdef8a
1 <wrasf175@naver.com>
2022-11-21 00:25:05 +0900

    test(BridgeMaker): 다리 길이 배열 원소 테스트 검증

--- __tests__/BridgeMakerTest.js
+++ __tests__/BridgeMakerTest.js
@@ -1,12 +1,10 @@
 const BridgeMaker = require('../src/BridgeMaker');
+const BridgeRandomNumberGenerator = require('../src/BridgeRandomNumberGenerator');
 
 describe('다리 생성 기능 단위 테스트', () => {
-    it('사용자의 입력값 만큼 다리 길이의 사이즈를 정한다',()=>{
-        const bridge = BridgeMaker.makeBridgeTest(15, 1);
-        expect(bridge.length).toBe(15);
-    })
-    it('사용자의 입력값 만큼 다리 길이의 사이즈를 정한다',()=>{
-        const bridge = BridgeMaker.makeBridgeTest(3, 1);
-        expect(bridge).toEqual([1,1,1]);
+
+    it('사용자 입력값을 크기로 갖는 배열을 생성한다.',()=>{
+        const bridge = BridgeMaker.makeBridge(3, BridgeRandomNumberGenerator.generate);
+        expect(bridge.length).toBe(3);
     })
 });
