4ecdaa464eb4feca2b29cc7d68627359840dff0d
sunny <huiseon37@gmail.com>
2022-11-21 16:52:43 +0900

    feat: 게임 재시도 입력 함수 구현

--- src/InputView.js
+++ src/InputView.js
@@ -1,7 +1,6 @@
 const { Console } = require("@woowacourse/mission-utils");
 const { INPUT_MESSAGE } = require("./constants/inputMessage");
 
-
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -27,7 +26,11 @@ const InputView = {
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+    Console.readLine(INPUT_MESSAGE.READ_GAME_COMMAND,(gameCommand) => {
+      
+    });
+  },
 };
 
 module.exports = InputView;
