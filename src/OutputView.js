01197f3fdb7b002e732128bdb92aa1632f4479b0
curomame <curomame@naver.com>
2022-11-21 23:47:17 +0900

    ➕ feat - <OutputView> [printResult] add
    게임의 최종 결과를 정해진 형식에 맞춰 출력하는 함수 구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -14,10 +14,17 @@ const OutputView = {
   
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-   * <p>
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+   * @param {[string[],string[]]} checkedMap 현재까지 이동한 다리의 상태에 대한 리스트 내용
+   * @param {boolean} result 게임의 성공 여부를 true/false로 받는다
+   * @param {number} tryCount 게임을 총 시도한 횟수를 받는다
    */
-  printResult() {
+  printResult(checkedMap,result,tryCount) {
+    MissionUtils.Console.print("최종 게임 결과");
+    OutputView.printMap(checkedMap);
+    MissionUtils.Console.print("");
+    MissionUtils.Console.print(`게임 성공 여부: ${result ? "성공" : "실패" }`);
+    MissionUtils.Console.print(`총 시도한 횟수: ${tryCount}`);
+    MissionUtils.Console.close();
   },
 
     /**
