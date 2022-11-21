07032118d35cc52e6861316e030877294d0e2284
jjo-mi <whdal2636@naver.com>
2022-11-21 23:42:09 +0900

    feat: 6. 현재 결과값 출력을 위한 형태

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,6 +1,5 @@
-const { Console, Random } = require("@woowacourse/mission-utils");
-const { GUIDE_MESSAGE, ERROR_MESSAGE, RESULT_MESSAGE } = require("./Constant");
-const app = require("./App");
+const { Console } = require("@woowacourse/mission-utils");
+const { GUIDE_MESSAGE, RESULT_MESSAGE } = require("./Constant");
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -16,20 +15,14 @@ const OutputView = {
     Console.print(GUIDE_MESSAGE.INPUT_TRY);
   },
 
-
   /**
    * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {
-    let upBridgeReultArr = []; // o,x,공백으로 들어와야함
-    let downBridgeReultArr = [];
-    if(upBridgeReultArr > 1) {
-      return upBridgeReultArr.join(" | ")
-    }
-
-    Console.print("[ " + upBridgeReultArr + " ]")
+  printMap(upBridgeReultArr, downBridgeReultArr) {
+    Console.print("[ " + upBridgeReultArr.join(" | ") + " ]");
+    Console.print("[ " + downBridgeReultArr.join(" | ") + " ]");
   },
 
   /**
@@ -37,13 +30,11 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() {
+  printResult(bridgeResult) {
     Console.print(RESULT_MESSAGE.TITTLE)
-
-    this.printMap();
-
-    Console.print(RESULT_MESSAGE.IS_SUCCESS,)//성공여부
-    Console.print(RESULT_MESSAGE.TRY_COUNT,)//성공 횟수
+    this.printMap(bridgeResult.upBridgeReultArr, bridgeResult.downBridgeReultArr);
+    Console.print(RESULT_MESSAGE.IS_SUCCESS + bridgeResult.gameResult);
+    Console.print(RESULT_MESSAGE.TRY_COUNT + bridgeResult.tryCount);
 
     Console.close();
   }
