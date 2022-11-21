159587e5348a5feba6fea1163b9dc1c53196e754
Jin-uu <78341764+Jin-uu@users.noreply.github.com>
2022-11-22 00:50:26 +0900

    feat : 플레이어 이동 기능 및 이동 성공 여부 판정 기능 추가

--- src/GameManager.js
+++ src/GameManager.js
@@ -1,14 +1,16 @@
 const InputView = require('./InputView');
 const OutputView = require('./OutputView');
 const Validator = require('./Validator');
+const BridgeGame = require('./BridgeGame');
 const BridgeMaker = require('./BridgeMaker');
 const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
 const { MESSAGE } = require('./const.js');
 
 
 class GameManager {
-    constructor() {
 
+    constructor() {
+        this.Game = new BridgeGame();
     }
 
     startGame() {
@@ -21,13 +23,32 @@ class GameManager {
             try {
                 Validator.validateBridgeSize(bridgeSize);
                 let bridge = BridgeMaker.makeBridge(bridgeSize, BridgeRandomNumberGenerator.generate);
-                // to get move bridge
+                this.getMoveDirection(bridge, 0)
             } catch(error) {
                 OutputView.displayMessage(error.message)
                 this.getInputBridgeLength();
             }
         })
     }
+
+    getMoveDirection(bridge, location) {
+        InputView.readMoving((moveDirection) => {
+            try {
+                Validator.validateMoveDirection(moveDirection);
+                this.afterMove(bridge,location,this.Game.move(bridge,location,moveDirection));
+            } catch(error) {
+                OutputView.displayMessage(error.message);
+                this.getMoveDirection(bridge);
+            }            
+        })
+    }
+
+    afterMove(bridge, location, isSuccess) {
+        OutputView.printMap(bridge, location+1, isSuccess);
+        if(isSuccess) return this.getMoveDirection(bridge, location+1);
+        // to retry;
+    }
+
 
 
 }
--- src/const.js
+++ src/const.js
@@ -33,6 +33,7 @@ const QUERY = {
 const ERROR = {
     HEADER : "[ERROR] ",
     BRIDGE_LENGTH : "다리의 길이는 3이상 20이하의 정수입니다.",
+    MOVE_DIRECTION : "이동은 (위: "+BRIDGE.INPUT_RANGE[0]+", 아래: "+BRIDGE.INPUT_RANGE[1]+") 로만 가능합니다.",
 
 }
 
