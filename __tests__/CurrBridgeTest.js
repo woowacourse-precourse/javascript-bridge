0825e9610e080c7f371fa7538af27286ef3fd879
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-11-21 22:19:21 +0900

    test: CurrBridge 클래스의 메서드 기능 테스트 추가

--- __tests__/CurrBridgeTest.js
+++ __tests__/CurrBridgeTest.js
@@ -6,6 +6,36 @@ const validateDirection = (input) => {
 };
 
 describe('CurrBridge 클래스 테스트', () => {
+  test('move 메서드 기능 테스트 : 입력 받은 칸이 건널 수 있는 칸이라면 O 표시하고, 건널 수 없는 칸이라면 X 표시한다', () => {
+    const currBridge = new CurrBridge();
+    currBridge.move((direction = 'U'), (CAN_MOVE = true));
+    currBridge.move((direction = 'D'), (CAN_MOVE = false));
+
+    expect(currBridge.getUpperBridge()).toEqual(['O', ' ']);
+    expect(currBridge.getLowerBridge()).toEqual([' ', 'X']);
+  });
+
+  test('move 메서드 기능 테스트 : 입력 받은 칸이 건널 수 있는 칸이라면 O 표시하고, 건널 수 없는 칸이라면 X 표시한다', () => {
+    const currBridge = new CurrBridge();
+    currBridge.move((direction = 'D'), (CAN_MOVE = true));
+    currBridge.move((direction = 'D'), (CAN_MOVE = true));
+    currBridge.move((direction = 'D'), (CAN_MOVE = true));
+    currBridge.move((direction = 'U'), (CAN_MOVE = false));
+
+    expect(currBridge.getUpperBridge()).toEqual([' ', ' ', ' ', 'X']);
+    expect(currBridge.getLowerBridge()).toEqual(['O', 'O', 'O', ' ']);
+  });
+
+  test('delete 메서드 기능 테스트 : 가장 최근 건너간 다리의 칸 삭제한다', () => {
+    const currBridge = new CurrBridge();
+    currBridge.move((direction = 'D'), (CAN_MOVE = true));
+    currBridge.move((direction = 'U'), (CAN_MOVE = false));
+    currBridge.delete();
+
+    expect(currBridge.getUpperBridge()).toEqual([' ']);
+    expect(currBridge.getLowerBridge()).toEqual(['O']);
+  });
+
   test('예외 테스트 : 입력값이 문자가 아닌 경우 예외 처리한다', () => {
     expect(() => validateDirection('17')).toThrow(
       '[ERROR] 숫자를 제외한 문자를 입력해주세요.'
