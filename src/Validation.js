fd3eb749db75f37db5dbd49070e857fcfc74bce3
Dahye <06robin11@gmail.com>
2022-11-21 13:43:26 +0900

    refactor(Validation):조건문함수화해 이름에 의미내포

--- src/Validation.js
+++ src/Validation.js
@@ -2,25 +2,37 @@ const { LETTER, ERROR_MESSAGE, NUMBER_RANGE } = require("./constant");
 
 const Validation = {
   validateBridgeNumber(number) {
-    if (
-      number < NUMBER_RANGE.min ||
-      number > NUMBER_RANGE.max ||
-      !Number.isInteger(number)
-    ) {
+    if (!Validation.isNumberInRange(number)) {
       throw ERROR_MESSAGE.notInRange;
     }
   },
 
+  isNumberInRange(number) {
+    return (
+      number >= NUMBER_RANGE.min &&
+      number <= NUMBER_RANGE.max &&
+      Number.isInteger(number)
+    );
+  },
+
   validateMoveInput(letter) {
-    if (letter !== LETTER.up && letter !== LETTER.down) {
+    if (!Validation.isLetterUorD(letter)) {
       throw ERROR_MESSAGE.notUorD;
     }
   },
 
+  isLetterUorD(letter) {
+    return letter === LETTER.up || letter === LETTER.down;
+  },
+
   validateCommandInput(letter) {
-    if (letter !== LETTER.retry && letter !== LETTER.quit) {
+    if (!Validation.isLetterRorQ(letter)) {
       throw ERROR_MESSAGE.notRorQ;
     }
+  },
+
+  isLetterRorQ(letter) {
+    return letter === LETTER.retry || letter === LETTER.quit;
   },
 };
 
