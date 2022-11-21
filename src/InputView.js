1cae7081b709d03daf746b7d9987547b35a3bcc0
Jinu <jinu200131@gmail.com>
2022-11-21 22:09:08 +0900

    feat : InputView callback 추가

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,5 @@
+const {Console} = require('@woowacourse/mission-utils');
+const { QUERY } = require('./const.js')
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  * 파일 경로 변경 가능
@@ -8,17 +10,23 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize(callback) {
+    Console.readLine(QUERY.BRIDGE_LENGTH,callback);
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving(callback) {
+    Console.readLine(QUERY.MOVE_DIRECTION,callback);
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand(callback) {
+    Console.readLine(QUERY.GAME_RESTART,callback);
+  },
 };
 
 module.exports = InputView;
