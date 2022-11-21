3fd7df196cfde68fe706f52dd338be59fc5a3649
Minyoung Lee <mminy62@gmail.com>
2022-11-21 16:50:32 +0900

    feat: [feat1] 게임 시작 및 input 값 입력 받기
    게임 시작(outputView) 및 input 값(inputView) 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const InputView = require("../src/InputView");
+const OutputView = require("../src/OutputView");
+
+
 class App {
-  play() {}
+  
+  play() {
+    OutputView.printStart();
+    InputView.readBridgeSize();
+
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
--- src/InputView.js
+++ src/InputView.js
@@ -1,21 +1,41 @@
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
+const MissionUtils = require("@woowacourse/mission-utils");
+const BridgeMaker = require("../src/BridgeMaker");
+const BridgeRandomNumberGenerator = require("../src/BridgeRandomNumberGenerator");
+ 
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    MissionUtils.Console.readLine('다리의 길이를 입력해주세요.\n', (bridgeSize) => {
+      console.log(bridgeSize);
+      this.readMoving();
+    });
+
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    MissionUtils.Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)\n', (userMove) => {
+      console.log("userMove: ", userMove); //이동할 칸 입력해서 에러 받은 후에 
+      
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+    MissionUtils.Console.readLine('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n', (restartInput) => {
+      console.log("restart: ", restartInput);
+    });
+
+  },
 };
 
 module.exports = InputView;
