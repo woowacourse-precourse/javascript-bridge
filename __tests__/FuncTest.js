4507d1c5ea2f31b2f11c96f68bbe495d71e62fe2
jiheon788 <jiheon788@ajou.ac.kr>
2022-11-21 15:16:10 +0900

    test: 다리 생성 테스트

---
+++ __tests__/FuncTest.js
@@ -0,0 +1,13 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const App = require("../src/App");
+const BridgeGame = require("../src/BridgeGame");
+
+describe("BridgeGame 테스트", () => {
+  test("기능 테스트", () => {
+    const bridgeGame = new BridgeGame();
+    const size = 5
+    bridgeGame.setBridge(size)
+
+    expect(bridgeGame.getBride().length).toEqual(size);
+  });
+});
