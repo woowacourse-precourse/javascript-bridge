f21706e0f24345763401fbdf752c6e71a83e4d4d
choieastsea <choieastsea@gmail.com>
2022-11-22 02:13:36 +0900

    fix: printMap 테스트 위한 수정
    \b를 삽입하여 문자열을 구성하는 것은 jest의 StringContaining을 통과하지 못함을 주의!

--- src/ui/OutputView.js
+++ src/ui/OutputView.js
@@ -1,6 +1,13 @@
 const { Console } = require('@woowacourse/mission-utils');
-const { CMM_START, CMM_RESULT, CMM_IF_SUCCESS, CMM_SHOW_TRY } = require('../constant/Comment');
-const { BRIDGE_END, BRIDGE_START, UP, DOWN, BRIDGE_O, BRIDGE_X, BRIDGE_NONE } = require('../constant/constants');
+const {
+  CMM_START,
+  CMM_RESULT,
+  CMM_IF_SUCCESS,
+  CMM_SHOW_TRY,
+  CMM_SUCCESS,
+  CMM_FAILURE,
+} = require('../constant/Comment');
+const { UP, DOWN, BRIDGE_O, BRIDGE_X, BRIDGE_NONE } = require('../constant/constants');
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -12,21 +19,18 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printMap(isAnswerList, userInputString) {
-    let upperStr = BRIDGE_START;
-    let lowerStr = BRIDGE_START;
+    let upperBridge = [];
+    let lowerBridge = [];
     for (let i = 0; i < userInputString.length; i++) {
       if (userInputString[i] === UP) {
-        upperStr += isAnswerList[i] ? BRIDGE_O : BRIDGE_X;
-        lowerStr += BRIDGE_NONE;
+        upperBridge.push(isAnswerList[i] ? BRIDGE_O : BRIDGE_X);
+        lowerBridge.push(BRIDGE_NONE);
       } else if (userInputString[i] === DOWN) {
-        lowerStr += isAnswerList[i] ? BRIDGE_O : BRIDGE_X;
-        upperStr += BRIDGE_NONE;
+        lowerBridge.push(isAnswerList[i] ? BRIDGE_O : BRIDGE_X);
+        upperBridge.push(BRIDGE_NONE);
       }
     }
-    upperStr += BRIDGE_END;
-    lowerStr += BRIDGE_END;
-    Console.print(upperStr);
-    Console.print(lowerStr);
+    this.printBridge(upperBridge, lowerBridge);
   },
 
   /**
@@ -48,6 +52,7 @@ const OutputView = {
   printStart() {
     Console.print(CMM_START);
   },
+
   modifyCurStr(char) {
     switch (char) {
       case 'o':
@@ -58,12 +63,19 @@ const OutputView = {
         return ' X |';
     }
   },
+
   checkSuccess(isAnswerList) {
     const lastInd = isAnswerList.length - 1;
     if (isAnswerList[lastInd] === true) {
-      return '성공';
+      return CMM_SUCCESS;
     }
-    return '실패';
+    return CMM_FAILURE;
+  },
+  printBridge(upperBridge, lowerBridge) {
+    let upperString = `[${upperBridge.join('|')}]`;
+    let lowerString = `[${lowerBridge.join('|')}]`;
+    Console.print(upperString);
+    Console.print(lowerString);
   },
 };
 
