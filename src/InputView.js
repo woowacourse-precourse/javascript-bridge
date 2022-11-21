5a197d8b44f0613c31016fc5650e816c372441d9
JSH99 <tjgus9966@naver.com>
2022-11-22 04:30:42 +0900

    fix(InputView): Validation 객체 생성 코드 추가

--- src/InputView.js
+++ src/InputView.js
@@ -9,10 +9,11 @@ const InputView = {
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize() {
+    const validation = new Validation();
     let bridgeSize;
     MissionUtils.Console.readLine("다리의 길이를 입력해주세요.", (bridgeSizeInput) => {
       bridgeSize = Number(bridgeSizeInput);
-      Validation.checkSizeInputValidation(bridgeSize);
+      validation.checkSizeInputValidation(bridgeSize);
     });
     return bridgeSize;
   },
@@ -21,10 +22,11 @@ const InputView = {
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving() {
+    const validation = new Validation();
     let moving;
     MissionUtils.Console.readLine("이동할 칸을 선택해주세요. (위: U, 아래: D)", (movingInput) => {
       moving = movingInput;
-      Validation.checkMovingInputValidation(moving);
+      validation.checkMovingInputValidation(moving);
     });
     return moving;
   },
@@ -33,10 +35,11 @@ const InputView = {
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand() {
+    const validation = new Validation();
     let gameCommand; 
     MissionUtils.Console.readLine("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)", (gameCommandInput) => {
       gameCommand = gameCommandInput;
-      Validation.checkRetryInputValidation(gameCommand);
+      validation.checkRetryInputValidation(gameCommand);
     });
     return gameCommand;
   },
