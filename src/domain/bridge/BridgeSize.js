11abc7fe38c7d855a6120b55e1efd484116751ab
hwanfront <kim015jh@gmail.com>
2022-11-21 17:25:04 +0900

    refactor(exception): 다리 길이에 대한 예외처리 분리

--- src/domain/bridge/BridgeSize.js
+++ src/domain/bridge/BridgeSize.js
@@ -1,3 +1,5 @@
+const BridgeSizeException = require('../../exception/BridgeSizeException');
+
 class BridgeSize {
   static #RULES = {
     min: 3,
@@ -5,12 +7,12 @@ class BridgeSize {
   };
 
   static validate(size) {
-    if (size < this.#RULES.min) {
-      throw new Error('[ERROR] 다리의 길이는 3이상 20이하입니다.');
+    if (size < BridgeSize.#RULES.min) {
+      throw new BridgeSizeException(BridgeSize.#RULES);
     }
 
-    if (size > this.#RULES.max) {
-      throw new Error('[ERROR] 다리의 길이는 3이상 20이하입니다.');
+    if (size > BridgeSize.#RULES.max) {
+      throw new BridgeSizeException(BridgeSize.#RULES);
     }
   }
 }
