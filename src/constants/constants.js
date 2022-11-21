f7aa9f3f351836531bb64d39cad94ecec679868b
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-21 19:36:45 +0900

    feat: 게임 문구 상수로

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -5,4 +5,17 @@ const BRIDGE = {
   DOWN_CHAR: 'D',
 };
 
-module.exports = BRIDGE;
+const QUERY = {
+  START: '다리 건너기 게임을 시작합니다.\n',
+  SIZE: '다리의 길이를 입력해주세요.\n',
+  MOVE: '이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
+  RETRY: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
+  END: '최종 게임 결과\n',
+  RESULT: '성공',
+  TRY: '총 시도한 횟수:',
+};
+
+module.exports = {
+  BRIDGE,
+  QUERY,
+};
