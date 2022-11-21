da340f026b77418896ebdd87bfe137969ffae415
Jinu <jinu200131@gmail.com>
2022-11-21 22:09:25 +0900

    feat : 상수 추가

--- src/const.js
+++ src/const.js
@@ -3,16 +3,18 @@ const BRIDGE ={
     LENGTH_RANGE_RIGHT : 20,
     INPUT_RANGE : ["U", "D"],
     LEFT_SIDE : "[",
+    EMPTY : "   ",
+    DELIMITER : "|",
     RIGHT_SIDE : "]",
     MOVE_SUCCESS : " O ",
     MOVE_FAIL : " X "
 };
 
 const MESSAGE = {
-    GAME_START : "다리 건너기 게임을 시작합니다.\n",
-    FINAL_RESULT : "최종 게임 결과\n",
+    GAME_START : "다리 건너기 게임을 시작합니다.",
+    FINAL_RESULT : "최종 게임 결과",
     IS_WIN : "게임 성공 여부: ",
-    WIN : "성공\n",
+    WIN : "성공",
     LOSS : "실패",
     ATTEMPT_TIMES : "총 시도한 횟수: ",
 }
@@ -28,7 +30,9 @@ const QUERY = {
     GAME_RESTART : "게임을 다시 시도할지 여부를 입력해주세요. (재시도: "+USER_ANSWER.RETRY+", 종료: "+USER_ANSWER.QUIT+")\n",
 }
 
-const ERRROR = {
+const ERROR = {
     HEADER : "[ERROR] ",
 
 }
+
+module.exports = { BRIDGE, MESSAGE, USER_ANSWER, QUERY, ERROR };
