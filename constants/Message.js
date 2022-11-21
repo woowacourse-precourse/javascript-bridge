b5872f6cdde928640de04c7f8809a111cf29fbe2
dongyeopca <ckehdduq95@gmail.com>
2022-11-21 13:09:38 +0900

    style(Message): 줄바꿈 문자 수정

--- constants/Message.js
+++ constants/Message.js
@@ -1,7 +1,7 @@
 const MESSAGE = {
   ANNOUNCE: {
-    START: '다리 건너기 게임을 시작합니다.\n',
-    INPUT_SIZE: '다리의 길이를 입력해주세요.\n',
+    START: '다리 건너기 게임을 시작합니다.',
+    INPUT_SIZE: '\n다리의 길이를 입력해주세요.\n',
     INPUT_MOVE: '\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
     INPUT_RETRY: '\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
   },
