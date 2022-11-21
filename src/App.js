0eeed18d06a296dd88db14e4fd480da235d8b57f
jjo-mi <whdal2636@naver.com>
2022-11-21 19:11:11 +0900

    fix: 4. App.js movingInput에 받기(for문삭제)

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ const inputView = require("./InputView");
 const bridgeMaker = require("./BridgeMaker");
 const bridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
 const { vaildSizeInput } = require("./InputView");
-const BridgeGame = require("./BridgeGame");
-// const bridgeGame = require("./BridgeGame");
+
 
 class App {
   async play() {
@@ -22,16 +21,10 @@ class App {
     }
 
     const bridgeArr = bridgeMaker.makeBridge(size, bridgeRandomNumberGenerator.generate);
-    console.log('###bridgeArr:', bridgeArr);
-
 
     const movingInput = await this.inputMoving(bridgeArr);
-    console.log('###movingInput:', movingInput);
   
-
     const bridgeGame = new BridgeGame(bridgeArr, size);
-    const comparisonResult = bridgeGame.move(movingInput);
-    // console.log('###'comparisonResult);
 
   }
 
@@ -47,33 +40,15 @@ class App {
     return size;
   }
 
-  /**4-1. 이동할 칸 movingInput에 받음 수정중 */
+  /**4-1. 이동할 칸 movingInput에 받기*/
    async inputMoving(bridgeArr) {
     let movingInput;
     await inputView.readMoving()
     .then(value => {
       movingInput = value;
-    })
+    }).catch(error => Console.print(error.message));
     return movingInput;
   }
-
-
-  // /** 4-1. 이동할 칸 입력 및 입력값 */
-  // async move(bridgeArr) {
-  //   const movingInput = [];
-  //   for(let i = 0; i < bridgeArr.length; i++) {
-  //     await inputView.readMoving()
-  //       .then(value => {
-  //         //중간결과 출력
-  //         movingInput.push(value);
-  //       }).catch(error => {
-  //         Console.print(error.message);
-  //         --i;
-  //       });
-  //   }
-  //   console.log('###movingInputArr:', movingInput);
-  //   return movingInput;
-  // }
 
 }
 
