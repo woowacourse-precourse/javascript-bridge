53a2fd78c0ed15c2ebf9bc957baaa709e33cdefd
Sungwk <73461032+Sungwk@users.noreply.github.com>
2022-11-21 23:36:05 +0900

    test : 재시도 여부 입력값 테스트케이스 추가

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -14,11 +14,21 @@ describe('다리 길이 입력값 테스트', () => {
     });
   });
 });
+
 describe('다리 이동 입력값 테스트', () => {
   test('입력값이 U/D 가 아닌 경우', () => {
     const inputValues = ['a', 'b', '\n', '1', 'u'];
     inputValues.forEach((inputValue) => {
       expect(Validation.validateDirection(inputValue)).toBeFalsy();
+    });
+  });
+});
+
+describe('재시도 여부 입력값 테스트', () => {
+  test('입력값이 U/D 가 아닌 경우', () => {
+    const inputValues = ['a', 'b', '\n', '1', 'r', 'q'];
+    inputValues.forEach((inputValue) => {
+      expect(Validation.validateRetryComment(inputValue)).toBeFalsy();
     });
   });
 });
