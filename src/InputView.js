43e8a47aa2649ba5f68f83365ee66cc3d4e82180
minh0518 <rubics@naver.com>
2022-11-21 16:39:56 +0900

    feat : 다리 길이 입력 구현 docs : 이번주 주의사항 문장 수정

--- src/InputView.js
+++ src/InputView.js
@@ -1,11 +1,18 @@
+const { Console } = require('@woowacourse/mission-utils')
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
+
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine('다리의 길이를 입력해주세요.',(input)=>{
+      
+    })
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
