26b84f88d79d4063e97531492963d86855b386da
Yesong <to06109@naver.com>
2022-11-22 06:05:28 +0900

    feat(App): 모든 입력에서 잘못된 값 입력 시 검증 후 입력 다시받게 함

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App {
 
   constructor() {
     this.#tryNum = 1;
+    this.bridgeIndex = 0;
   }
 
   play() {
@@ -36,31 +37,48 @@ class App {
       BridgeRandomNumberGenerator.generate
     );
     this.#bridgeGame = new BridgeGame(this.#bridge);
-    this.doGame();
+    this.getMove();
   }
 
-  async doGame() {
-    let brigeIndex = 0;
-    while (brigeIndex < this.#bridgeSize) {
-      const moveInput = await InputView.readMoving();
-      this.#curBridge = this.#bridgeGame.makeCurBridge(moveInput, brigeIndex);
-      OutputView.printMap(this.#curBridge);
-      if (!this.#bridgeGame.move(moveInput, brigeIndex)) {
-        this.doRetry();
-        return;
-      }
-      brigeIndex += 1;
+  async getMove() {
+    const moveInput = await InputView.readMoving();
+    this.doGame(moveInput);
+  }
+
+  doGame(moveInput) {
+    if (!this.validateMove(moveInput)) return this.getMove();
+    // 현재까지 건넌 다리 만들기
+    this.#curBridge = this.#bridgeGame.makeCurBridge(
+      moveInput,
+      this.bridgeIndex
+    );
+    OutputView.printMap(this.#curBridge);
+    // 건널 수 없다면
+    if (!this.#bridgeGame.move(moveInput, this.bridgeIndex)) {
+      return this.getRetry();
     }
+    // 다리를 끝까지 건너지 않았다면
+    if (this.bridgeIndex !== this.#bridgeSize - 1) {
+      this.bridgeIndex += 1;
+      return this.getMove();
+    }
+
     OutputView.printResult('성공', this.#curBridge, this.#tryNum);
     this.end();
   }
 
-  async doRetry() {
+  async getRetry() {
     const retryInput = await InputView.readGameCommand();
+    this.doRetry(retryInput);
+  }
+
+  doRetry(retryInput) {
+    if (!this.validateRetry(retryInput)) return this.getRetry();
     if (retryInput === 'R') {
+      this.bridgeIndex = 0;
       this.#tryNum += 1;
       this.#bridgeGame.retry();
-      this.doGame();
+      this.getMove();
     }
     if (retryInput === 'Q') {
       OutputView.printResult('실패', this.#curBridge, this.#tryNum);
@@ -71,6 +89,26 @@ class App {
   validateBridgeSize() {
     try {
       Validate.size(parseInt(this.#bridgeSize));
+      return true;
+    } catch (e) {
+      OutputView.printError(e.message);
+      return false;
+    }
+  }
+
+  validateMove(moveInput) {
+    try {
+      Validate.move(moveInput);
+      return true;
+    } catch (e) {
+      OutputView.printError(e.message);
+      return false;
+    }
+  }
+
+  validateRetry(retryInput) {
+    try {
+      Validate.retry(retryInput);
       return true;
     } catch (e) {
       OutputView.printError(e.message);
