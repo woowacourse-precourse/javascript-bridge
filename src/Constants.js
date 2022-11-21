a6f1c438e5f11092ddcf25b045ec3a0908c56541
imhynni <omelas329@gmail.com>
2022-11-22 02:27:17 +0900

    feat: 잘못된 값 입력하면 다시 입력 받기

--- src/Constants.js
+++ src/Constants.js
@@ -1,10 +1,10 @@
 module.exports = {
   INPUT_ERROR_MESSAGE: {
-    NAN_ERROR: "[ERROR] 숫자를 입력해주세요.",
-    RANGE_ERROR: "[ERROR] 3 이상 20 이하의 수를 입력해주세요.",
+    NAN_ERROR: "[ERROR] 숫자를 입력해주세요.\n",
+    RANGE_ERROR: "[ERROR] 3 이상 20 이하의 수를 입력해주세요.\n",
 
-    DIRECTION_ERROR: "[ERROR] U 또는 D를 입력해주세요.",
-    COMMEND_ERROR: "[ERROR] R 또는 Q를 입력해주세요.",
+    DIRECTION_ERROR: "[ERROR] U 또는 D를 입력해주세요.\n",
+    COMMEND_ERROR: "[ERROR] R 또는 Q를 입력해주세요.\n",
   },
 
   GAME_STATE: {
