00c6d4d597fb809dcbaad5d3a5acac7215a4d2d4
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-11-21 15:12:07 +0900

    feat(UI): 현재까지 이동한 다리의 상태 출력

--- src/OutputView.js
+++ src/OutputView.js
@@ -27,7 +27,15 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {},
+  printMap(uBridge, dBridge) {
+    const framework = (bridge) => {
+      const SEPARATOR = ' | ';
+
+      return `[ ${bridge.join(SEPARATOR)} ]`;
+    };
+
+    [uBridge, dBridge].forEach((bridge) => this.print(framework(bridge)));
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
