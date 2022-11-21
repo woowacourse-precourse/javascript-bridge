7147565c7e35163e01e47db1b34624d326388bb9
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2022-11-22 03:01:18 +0900

    fix(OutputView) : 게임결과 저장부 class로 분리

--- src/OutputView.js
+++ src/OutputView.js
@@ -4,39 +4,38 @@ const {GAME_MESSAGE, RESULT,DIRECTION,BRIDGE, JOINER} = require("./util/Constant
 
 const OutputView = {
 
-  upper: [],
+  // upper: [],
 
-  downer: [],
+  // downer: [],
 
-  addValue(upperValue, downerValue) {
-    this.upper.push(upperValue);
-    this.downer.push(downerValue);
+  // addValue(upperValue, downerValue) {
+  //   this.upper.push(upperValue);
+  //   this.downer.push(downerValue);
+  // },
+
+
+  // pushResult(dir, success) {
+  //   if (success) {
+  //     if (dir === DIRECTION.UP) this.addValue(BRIDGE.CAN_MOVE, BRIDGE.BLANK);
+  //     if (dir === DIRECTION.DOWN) this.addValue(BRIDGE.BLANK, BRIDGE.CAN_MOVE);
+  //   }
+  //   else {
+  //     if (dir === DIRECTION.UP) this.addValue(BRIDGE.CANNOT_MOVE, BRIDGE.BLANK);
+  //     else this.addValue(BRIDGE.BLANK, BRIDGE.CANNOT_MOVE);
+  //   }
+  // },
+
+
+  printMap(bridgeGame) {
+    const {upperString, downerString} = bridgeGame.gameResult();
+    Console.print(upperString);
+    Console.print(downerString);
   },
 
 
-  pushResult(dir, success) {
-    if (success) {
-      if (dir === DIRECTION.UP) this.addValue(BRIDGE.CAN_MOVE, BRIDGE.BLANK);
-      if (dir === DIRECTION.DOWN) this.addValue(BRIDGE.BLANK, BRIDGE.CAN_MOVE);
-    }
-    else {
-      if (dir === DIRECTION.UP) this.addValue(BRIDGE.CANNOT_MOVE, BRIDGE.BLANK);
-      else this.addValue(BRIDGE.BLANK, BRIDGE.CANNOT_MOVE);
-    }
-  },
-
-
-  printMap() {
-    const [upperStr, downStr] = [JOINER.START + this.upper.join(JOINER.SEPARATOR) + JOINER.END,
-       JOINER.START + this.downer.join(JOINER.SEPARATOR) + JOINER.END];
-    Console.print(upperStr);
-    Console.print(downStr);
-  },
-
-
-  printResult(gameCount, success) {
+  printResult(gameCount, success, bridgeGame) {
     Console.print(GAME_MESSAGE.RESULT);
-    this.printMap();
+    this.printMap(bridgeGame);
     const result = success ? RESULT.SUCCESS : RESULT.FAIL;
     Console.print(`${GAME_MESSAGE.ISSUCCESS} ${result}`);
     Console.print(`${GAME_MESSAGE.TOTALCOUNT} ${gameCount}`);
