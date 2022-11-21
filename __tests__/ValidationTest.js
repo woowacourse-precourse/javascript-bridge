dd646136120da97096e5433ea3ce619acd8f5383
dragonfruitlemonade <tpghks9245@gmail.com>
2022-11-21 21:04:01 +0900

    feat(ValidationTest): retry 게임오버시 입력값이 올바르지 않을 경우 검사 함수 추가

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,102 +1,99 @@
-const Validation = require("../src/Validation");
+const Validation = require('../src/Validation');
 
-describe("Validation 클래스 테스트", () => {
-  test("bridgeLength 입력 값이 비었을 경우", () => {
-    const input = "";
+describe('Validation 클래스 테스트', () => {
+  test('bridgeLength 입력 값이 비었을 경우', () => {
+    const input = '';
 
     expect(() => Validation.bridgeLength(input)).toThrow('[ERROR] 값을 입력해주세요.');
   });
 
-  test("bridgeLength 입력 값이 숫자가 아닐경우", () => {
-    const input = "가";
+  test('bridgeLength 입력 값이 숫자가 아닐경우', () => {
+    const input = '가';
 
     expect(() => Validation.bridgeLength(input)).toThrow('[ERROR] 숫자를 입력해주세요.');
   });
 
-  test("bridgeLength 입력 값이 숫자가 아닐경우", () => {
-    const input = "number";
+  test('bridgeLength 입력 값이 숫자가 아닐경우', () => {
+    const input = 'number';
 
     expect(() => Validation.bridgeLength(input)).toThrow('[ERROR] 숫자를 입력해주세요.');
   });
 
-  test("bridgeLength 입력 값이 3~20 사이가 아닐 경우", () => {
-    const input = "2";
+  test('bridgeLength 입력 값이 3~20 사이가 아닐 경우', () => {
+    const input = '2';
 
     expect(() => Validation.bridgeLength(input)).toThrow('[ERROR] 3과 20사이의 값을 입력해주세요.');
   });
 
-  test("bridgeLength 입력 값이 3~20 사이가 아닐 경우", () => {
-    const input = "21";
+  test('bridgeLength 입력 값이 3~20 사이가 아닐 경우', () => {
+    const input = '21';
 
     expect(() => Validation.bridgeLength(input)).toThrow('[ERROR] 3과 20사이의 값을 입력해주세요.');
   });
 
-  test("bridge 이동 값이 비었을 경우", () => {
-    const input = "";
+  test('bridge 이동 값이 비었을 경우', () => {
+    const input = '';
 
     expect(() => Validation.nextStep(input)).toThrow('[ERROR] 값을 입력해주세요.');
   });
   
-  test("bridge 이동 값이 여러개일 경우", () => {
-    const input = "UU";
+  test('bridge 이동 값이 여러개일 경우', () => {
+    const input = 'UU';
 
     expect(() => Validation.nextStep(input)).toThrow('[ERROR] 1개의 값을 입력해주세요.');
   });
 
-  test("bridge 이동 값이 여러개일 경우", () => {
-    const input = "DD";
+  test('bridge 이동 값이 여러개일 경우', () => {
+    const input = 'DD';
 
     expect(() => Validation.nextStep(input)).toThrow('[ERROR] 1개의 값을 입력해주세요.');
   });
 
-  test("bridge 이동 값이 여러개일 경우", () => {
-    const input = "UD";
+  test('bridge 이동 값이 여러개일 경우', () => {
+    const input = 'UD';
 
     expect(() => Validation.nextStep(input)).toThrow('[ERROR] 1개의 값을 입력해주세요.');
   });
 
-  test("bridge 이동 값이 올바르지 않을 경우", () => {
-    const input = "u";
+  test('bridge 이동 값이 올바르지 않을 경우', () => {
+    const input = 'q';
 
     expect(() => Validation.nextStep(input)).toThrow('[ERROR] U 또는 D를 입력해주세요.');
   });
 
-  test("bridge 이동 값이 올바르지 않을 경우", () => {
-    const input = "d";
+  test('bridge 이동 값이 올바르지 않을 경우', () => {
+    const input = 'j';
 
     expect(() => Validation.nextStep(input)).toThrow('[ERROR] U 또는 D를 입력해주세요.');
   });
 
-  test("게임오버시 입력 값이 없을 경우", () => {
-    const input = "";
+  test('게임오버시 입력 값이 없을 경우', () => {
+    const input = '';
 
     expect(() => Validation.retry(input)).toThrow('[ERROR] 값을 입력해주세요.');
   });
 
-  test("게임오버시 여러개의 값을 입력했을 경우", () => {
-    const input = "RQ";
+  test('게임오버시 여러개의 값을 입력했을 경우', () => {
+    const input = 'RQ';
 
-    expect(() => Validation.retry(input)).toThrow("[ERROR] 1개의 값을 입력해주세요.");
+    expect(() => Validation.retry(input)).toThrow('[ERROR] 1개의 값을 입력해주세요.');
   });
 
-  test("게임오버시 여러개의 값을 입력했을 경우", () => {
-    const input = "QR";
+  test('게임오버시 여러개의 값을 입력했을 경우', () => {
+    const input = 'QR';
 
-    expect(() => Validation.retry(input)).toThrow("[ERROR] 1개의 값을 입력해주세요.");
+    expect(() => Validation.retry(input)).toThrow('[ERROR] 1개의 값을 입력해주세요.');
   });
 
-//   test("bridge 이동 값이 올바르지 않을 경우", () => {
-//     const input = "d";
+  test('게임 오버시 입력값이 올바르지 않을 경우', () => {
+    const input = 'w';
 
-//     expect(() => Validation.retry(input)).toThrow('[ERROR] U 또는 D를 입력해주세요.');
-//   });
+    expect(() => Validation.retry(input)).toThrow('[ERROR] R 또는 Q를 입력해주세요.');
+  });
 
-//   test("bridge 이동 값이 올바르지 않을 경우", () => {
-//     const input = "d";
+  test('게임 오버시 입력값이 올바르지 않을 경우', () => {
+    const input = 's';
 
-//     expect(() => Validation.retry(input)).toThrow('[ERROR] U 또는 D를 입력해주세요.');
-//   });
-
-
-
+    expect(() => Validation.retry(input)).toThrow('[ERROR] R 또는 Q를 입력해주세요.');
+  });
 });
