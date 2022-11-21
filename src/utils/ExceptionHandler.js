9b4f1b15e9ed6ba832e77f201af9da47aeb30b72
Buyoung Chung <sunarol@naver.com>
2022-11-21 18:06:39 +0900

    refactor: exceptions throw error string
    this will be printed by Controller's try/catch

--- src/utils/ExceptionHandler.js
+++ src/utils/ExceptionHandler.js
@@ -1,37 +1,29 @@
 const Constant = require("./Constant");
-const { Console } = require("@woowacourse/mission-utils");
+
 const ExceptionHandler = {
   isNotaNumber(input) {
     if (isNaN(input)) {
-      Console.print("[ERROR] 숫자를 입력해주세요.");
-      return false;
+      throw "\n[ERROR] 숫자를 입력해주세요.\n";
     }
-    return true;
   },
 
   validateSizeInput(size) {
     const intSize = parseInt(size);
     if (intSize < Constant.MIN_SIZE || intSize > Constant.MAX_SIZE) {
-      Console.print("[ERROR] 3부터 20 사이의 숫자를 입력해주세요.");
-      return false;
+      throw "\n[ERROR] 3부터 20 사이의 숫자를 입력해주세요.\n";
     }
-    return true;
   },
 
   validateGameInput(input) {
     if (input !== Constant.UP && input !== Constant.DOWN) {
-      Console.print("[ERROR] 정확히 입력해주세요.");
-      return false;
+      throw "\n[ERROR] 대문자 U, D를 정확히 입력해주세요.\n";
     }
-    return true;
   },
 
   validateRetryInput(input) {
     if (input !== Constant.QUIT && input !== Constant.RETRY) {
-      Console.print("[ERROR] 정확히 입력해주세요.");
-      return false;
+      throw "\n[ERROR] 대문자 Q, R을 정확히 입력해주세요.\n";
     }
-    return true;
   },
 };
 
