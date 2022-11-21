e51d67463e54736ccbb56b27a7b537c61bcd9461
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-21 17:45:56 +0900

    refactor: 검증 함수 분리

--- src/InputView.js
+++ src/InputView.js
@@ -4,42 +4,37 @@
 // InputView 에서만 MissionUtils의 Console.readLine() 을 이용해 사용자의 입력을 받을 수 있다.
 const { Console } = require('@woowacourse/mission-utils');
 const ERROR_MESSAGES = require('./constants/ErrorMessages');
-const BRIDGE = require('./constants/constants');
+const { BRIDGE, QUERY } = require('./constants/constants');
+const BridgeMaker = require('./BridgeMaker');
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
+const InputValidator = require('./validator/InputValidator');
 
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize() {
-    Console.readLine('QUERY', (size) => {
-      this.validateBridgeSize(size);
+    Console.readLine(QUERY.SIZE, (size) => {
+      InputValidator.validateBridgeSize(size);
+      BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate);
+      readMoving();
     });
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    Console.readLine(QUERY.MOVE, (char) => {
+      InputValidator.validateMoving(char);
+      // O, X 표시
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand() {},
-
-  validateBridgeSize(size) {
-    if (!this.isNumber(size)) {
-      throw new Error(ERROR_MESSAGES.NAN);
-    }
-    if (this.outOfRange(size)) {
-      throw new Error(ERROR_MESSAGES.SIZE);
-    }
-  },
-  isNumber(size) {
-    return Number.isInteger(size);
-  },
-  outOfRange(size) {
-    return Number(size) < BRIDGE.MIN || Number(size) > BRIDGE.MAX;
-  },
 };
 
 module.exports = InputView;
