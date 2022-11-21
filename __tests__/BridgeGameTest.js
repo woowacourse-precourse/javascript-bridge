02c3e5edfcec875bc9c9cc3a5f435142bb244265
1 <wrasf175@naver.com>
2022-11-21 14:02:51 +0900

    test(BridgeGame): retry 메서드 단위 테스트 작성

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -8,4 +8,10 @@ describe('다리 건너기 게임 단위 테스트', () => {
         expect(test1).toEqual([1]);
         expect(test2).toEqual([0]);
     });
+    it('재시작시 사용자의 입력값 초기화',()=>{
+        const bridgeGame = new BridgeGame();
+        bridgeGame.move('U', [0,1,1]);
+        const test = bridgeGame.retry();
+        expect(test).toEqual([]);
+    })
 });
