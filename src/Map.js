cd80f64ea8b3638779384ab6fa2a7056069ad34e
oseunghyun <ohsh1477@gmail.com>
2022-11-22 00:48:18 +0900

    refactor(Map): 지도 생성 메서드 분리

--- src/Map.js
+++ src/Map.js
@@ -9,36 +9,55 @@ class Map {
 
   make(bridge) {
     let bridgeMap = [[], []];
-    for (let i = 0; i < bridge.length; i += 1) {
-      if (bridge[i] === 'U') {
-        bridgeMap[0][i] = 'X';
-        bridgeMap[1][i] = 'O';
-      }
-      if (bridge[i] === 'D') {
-        bridgeMap[0][i] = 'O';
-        bridgeMap[1][i] = 'X';
-      }
-    }
+    bridgeMap[0] = this.makeUpperBridge(bridge);
+    bridgeMap[1] = this.makeLowerBridge(bridge);
     this.#map = bridgeMap;
   }
 
+  makeUpperBridge(bridge) {
+    let upperBridge = [];
+    for (let i = 0; i < bridge.length; i += 1) {
+      if (bridge[i] === 'U') upperBridge[i] = 'X';
+      if (bridge[i] === 'D') upperBridge[i] = 'O';
+    }
+    return upperBridge;
+  }
+
+  makeLowerBridge(bridge) {
+    let lowerBridge = [];
+    for (let i = 0; i < bridge.length; i += 1) {
+      if (bridge[i] === 'U') lowerBridge[i] = 'O';
+      if (bridge[i] === 'D') lowerBridge[i] = 'X';
+    }
+    return lowerBridge;
+  }
+
   makeNowMap(go, step) {
-    let nowMap = [[], []];
-    nowMap[0] = this.#map[0].slice(0, step + 1);
-    nowMap[1] = this.#map[1].slice(0, step + 1);
+    let nowMap = this.sliceMapFromZeroToNowStep(step);
     for (let k = 0; k < nowMap[0].length - 1; k += 1) {
       if (nowMap[0][k] === 'X') nowMap[0][k] = ' ';
       if (nowMap[1][k] === 'X') nowMap[1][k] = ' ';
     }
-    if (go == 'O') {
+    nowMap = this.markUnpassedStep(nowMap, go, step);
+    return nowMap;
+  }
+
+  sliceMapFromZeroToNowStep(step) {
+    let map = [[], []];
+    map[0] = this.#map[0].slice(0, step + 1);
+    map[1] = this.#map[1].slice(0, step + 1);
+    return map;
+  }
+
+  markUnpassedStep(nowMap, go, step) {
+    if (go === 'O') {
       if (nowMap[0][step] === 'X') nowMap[0][step] = ' ';
       if (nowMap[1][step] === 'X') nowMap[1][step] = ' ';
     }
-    if (go == 'X') {
+    if (go === 'X') {
       if (nowMap[0][step] === 'O') nowMap[0][step] = ' ';
       if (nowMap[1][step] === 'O') nowMap[1][step] = ' ';
     }
-    // OutputView.printMap(nowMap);
     return nowMap;
   }
 
