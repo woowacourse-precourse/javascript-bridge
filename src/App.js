429cb2b673b611282c68ca565c73869cc91e7c9e
KINHYEONJI <hyeonji0718@gmail.com>
2022-11-21 22:37:41 +0900

    fix(App) : this가 실행 위치에 따라 바뀌어 있는 문제 해결
    - makeBridge를 바인딩

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 const InputView = require("./InputView");
 const OutputView = require("./OutputView");
+const BridgeMaker = require("./BridgeMaker");
 
 class App {
   constructor() {
     this.inputView = InputView;
     this.outputView = OutputView;
+    this.bridgeMaker = BridgeMaker;
   }
 
   play() {
     this.outputView.print("다리 건너기 게임을 시작합니다.");
     this.inputView.readBridgeSize(
-      this.bridgeMaker.makeBridge(this.bridgeMaker),
+      this.bridgeMaker.makeBridge.bind(this.bridgeMaker),
       "다리의 길이를 입력해주세요. "
     );
   }
