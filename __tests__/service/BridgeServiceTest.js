d66740e3e79180408d222fed3d2af5cb5e457954
turtle601 <turtle601@naver.com>
2022-11-22 00:42:57 +0900

    refactor(BridgeServiceTest) test.each 함수 사용

--- __tests__/service/BridgeServiceTest.js
+++ __tests__/service/BridgeServiceTest.js
@@ -12,102 +12,88 @@ const mockRandoms = (numbers) => {
   );
 };
 
+const startService = (bridgeLength) => {
+  const randoms = [1, 1, 1];
+  mockRandoms(randoms);
+
+  const bridgeService = new BridgeService();
+  bridgeService.start(bridgeLength);
+
+  return bridgeService;
+};
+
+const makeInput = ({ input, moveResult, gameResult }) => {
+  return {
+    input,
+    moveResult,
+    gameResult,
+    service: startService('3')
+  };
+};
+
 describe('랜덤 브릿지와 유저 브릿지 비교 테스트', () => {
-  test('(결과:실패 - 유저브릿지["U","D"], 랜덤브릿지["U","U","U"],)) 입력 테스트', () => {
-    const randoms = [1, 1, 1];
-    mockRandoms(randoms);
+  test.each([
+    [
+      makeInput({
+        input: ['U', 'D'],
+        moveResult: [
+          ['O', ' '],
+          [' ', 'X']
+        ],
+        gameResult: {
+          result: GAME_RESULT_STATE.fail,
+          tryCount: 1
+        }
+      })
+    ],
+    [
+      makeInput({
+        input: ['U', 'U', 'D'],
+        moveResult: [
+          ['O', 'O', ' '],
+          [' ', ' ', 'X']
+        ],
+        gameResult: {
+          result: GAME_RESULT_STATE.fail,
+          tryCount: 1
+        }
+      })
+    ],
+    [
+      makeInput({
+        input: ['U', 'U'],
+        moveResult: [
+          ['O', 'O'],
+          [' ', ' ']
+        ],
+        gameResult: {
+          result: GAME_RESULT_STATE.try,
+          tryCount: 1
+        }
+      })
+    ],
+    [
+      makeInput({
+        input: ['U', 'U', 'U'],
+        moveResult: [
+          ['O', 'O', 'O'],
+          [' ', ' ', ' ']
+        ],
+        gameResult: {
+          result: GAME_RESULT_STATE.success,
+          tryCount: 1
+        }
+      })
+    ]
+  ])(
+    '다리 길이 입력 시 시작데이터가 잘 만들어지는 지 확인',
+    ({ input, gameResult, moveResult, service }) => {
+      input.forEach((updown) => {
+        service.recordMove(updown);
+      });
 
-    const bridgeService = new BridgeService();
-
-    bridgeService.start('3');
-    bridgeService.recordMove('U');
-    bridgeService.recordMove('D');
-
-    const moveResult = bridgeService.getMoveResult();
-    const gameResult = bridgeService.getGameResult();
-
-    expect(moveResult).toEqual([
-      ['O', ' '],
-      [' ', 'X']
-    ]);
-    expect(gameResult).toEqual({
-      result: GAME_RESULT_STATE.fail,
-      tryCount: 1
-    });
-  });
-
-  test('(결과:실패 - 유저브릿지["U","U","D"], 랜덤브릿지["U","U","U"],)) 입력 테스트', () => {
-    const randoms = [1, 1, 1];
-    mockRandoms(randoms);
-
-    const bridgeService = new BridgeService();
-
-    bridgeService.start('3');
-
-    bridgeService.recordMove('U');
-    bridgeService.recordMove('U');
-    bridgeService.recordMove('D');
-
-    const moveResult = bridgeService.getMoveResult();
-    const gameResult = bridgeService.getGameResult();
-
-    expect(moveResult).toEqual([
-      ['O', 'O', ' '],
-      [' ', ' ', 'X']
-    ]);
-
-    expect(gameResult).toEqual({
-      result: GAME_RESULT_STATE.fail,
-      tryCount: 1
-    });
-  });
-
-  test('(결과:계속 시도 - 유저브릿지["U","U"], 랜덤브릿지["U","U","U"],)) 입력 테스트', () => {
-    const randoms = [1, 1, 1];
-    mockRandoms(randoms);
-
-    const bridgeService = new BridgeService();
-
-    bridgeService.start('3');
-    bridgeService.recordMove('U');
-    bridgeService.recordMove('U');
-
-    const moveResult = bridgeService.getMoveResult();
-    const gameResult = bridgeService.getGameResult();
-
-    expect(moveResult).toEqual([
-      ['O', 'O'],
-      [' ', ' ']
-    ]);
-
-    expect(gameResult).toEqual({
-      result: GAME_RESULT_STATE.try,
-      tryCount: 1
-    });
-  });
-
-  test('(결과:성공 - 유저브릿지["U","U","U"], 랜덤브릿지["U","U","U"],)) 입력 테스트', () => {
-    const randoms = [1, 1, 1];
-    mockRandoms(randoms);
-
-    const bridgeService = new BridgeService();
-
-    bridgeService.start('3');
-    bridgeService.recordMove('U');
-    bridgeService.recordMove('U');
-    bridgeService.recordMove('U');
-
-    const moveResult = bridgeService.getMoveResult();
-    const gameResult = bridgeService.getGameResult();
-
-    expect(moveResult).toEqual([
-      ['O', 'O', 'O'],
-      [' ', ' ', ' ']
-    ]);
-
-    expect(gameResult).toEqual({
-      result: GAME_RESULT_STATE.success,
-      tryCount: 1
-    });
-  });
+      expect(service.getMoveResult()).toEqual(moveResult);
+      expect(service.getGameResult()).toEqual(gameResult);
+    }
+  );
 });
