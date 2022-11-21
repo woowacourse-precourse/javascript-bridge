5f5adbbf60bde7f1d4ffa4b5763a4a0687271a2b
장경은 <jang_kyung_eun@gyeong-eun-ui-MacBookAir.local>
2022-11-22 00:54:18 +0900

    feat: 최종 게임 결과를 출력하는 기능 구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,6 +1,7 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const Console = MissionUtils.Console;
 const Random = MissionUtils.Random;
+let tryNum = 1;
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -11,6 +12,7 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printMap(bridge, count, upDown) {
+    let success = 0;
     let xNum = 0;
     let upMark = '';
     let downMark = '';
@@ -49,11 +51,12 @@ const OutputView = {
     Console.print(printArr.join(''));
 
     if (xNum != 0) {
-      this.readGameCommand(bridge, count);
+      this.readGameCommand(bridge, count, upDown, success);
     } else {
       count++;
       if (count == bridge.length) {
-        this.printResult();
+        success++;
+        this.printResult(bridge, count, upDown, success);
       } else {
         this.readMoving(bridge, count);
       }
@@ -72,13 +75,14 @@ const OutputView = {
     });
   },
   
-  readGameCommand(bridge, count) {
-    count = 0;
+  readGameCommand(bridge, count, upDown, success) {
     Console.readLine('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n', (retry) => {
       if (retry == 'R') {
+        tryNum++;
+        count = 0;
         this.readMoving(bridge, count);
       } else {
-        this.printResult();
+        this.printResult(bridge, count, upDown, success);
       }
     });
   },
@@ -87,7 +91,52 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() {},
+  printResult(bridge, count, upDown, success) {
+    Console.print('최종 게임 결과');
+    if (success == 1) {
+      count--;
+    }
+    let upMark = '';
+    let downMark = '';
+    let printArr = ['['];
+    for (let i = 0; i <= count; i++) {
+      upMark = ' ';
+      downMark = ' ';
+      if ((upDown == 0 && bridge[count] == upDown && count == i) || (bridge[i] == 0 && i < count)) {
+        upMark = 'O';
+      } else if (upDown == 0 && bridge[count] != upDown && count == i) {
+        upMark = 'X';
+      }
+      printArr.push(` ${upMark} `);
+      printArr.push('|');
+    }
+    printArr.pop();
+    printArr.push(']');
+    Console.print(printArr.join(''));
+
+    printArr = ['['];
+    for (let i = 0; i <= count; i++) {
+      upMark = ' ';
+      downMark = ' ';
+      if ((upDown == 1 && bridge[count] == upDown && count == i) || (bridge[i] == 1 && i < count)) {
+        upMark = 'O';
+      } else if (upDown == 1 && bridge[count] != upDown&& count == i) {
+        upMark = 'X';
+      }
+      printArr.push(` ${upMark} `);
+      printArr.push('|');
+    }
+    printArr.pop();
+    printArr.push(']');
+    Console.print(printArr.join(''));
+    if (success == 0) {
+      Console.print('\n게임 성공 여부: 실패');
+    } else {
+      Console.print('\n게임 성공 여부: 성공');
+    }
+    Console.print(`총 시도한 횟수: ${tryNum}`);
+    Console.close();
+  },
 };
 
 module.exports = OutputView;
