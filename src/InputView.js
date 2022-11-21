f61e3f9379fe92e3b6e3782b991c02f0489bfcc4
wlthdud <thdudzi@naver.com>
2022-11-21 15:28:30 +0900

    feat: 다리길이 입력받기 예외처리 기능 추가

--- src/InputView.js
+++ src/InputView.js
@@ -9,6 +9,7 @@ const InputView = {
    */
   readBridgeSize() {
     MissionUtils.Console.readline('다리의 길이를 입력해주세요.', (size) => {
+      this.exceptionOfReadBridgeSize(size);
     });
   },
 
@@ -24,6 +25,19 @@ const InputView = {
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand() {},
+
+
+  ////🐹예외처리🐹////
+  /**
+   * 다리길이 입력받기 예외처리 
+   */
+  exceptionOfReadBridgeSize(){
+    if(size < 3 || size > 20){
+      throw new Error('[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.');
+    }
+  }
+
+
 };
 
 module.exports = InputView;
