8b8a30e811b1c0de854eecd2e91771d286d82046
Yesong <to06109@naver.com>
2022-11-21 17:51:00 +0900

    fix(makeBridge): 출력값을 스트링 배열 타입으로 변경

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -8,11 +8,11 @@ const BridgeMaker = {
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
   makeBridge(size, generateRandomNumber) {
-    let bridge = '';
+    let bridge = [];
     for (let i = 0; i < size; i++) {
       const randomNum = generateRandomNumber();
-      if (randomNum === 0) bridge += 'D';
-      if (randomNum === 1) bridge += 'U';
+      if (randomNum === 0) bridge.push('D');
+      if (randomNum === 1) bridge.push('U');
     }
     return bridge;
   },
