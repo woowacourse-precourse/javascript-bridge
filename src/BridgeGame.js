5329336e605426d5957671a05bc98ab0f3c1edc2
sojinjang <jsojin6119@gmail.com>
2022-11-22 01:27:47 +0900

    :sparkles: feat(model): 다리 건너기 성공 여부 판별하기

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,25 +1,24 @@
 const { generate } = require("./BridgeRandomNumberGenerator");
 const { makeBridge } = require("./BridgeMaker");
+const InputView = require("./InputView");
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
+
+// InputView, Outputview 사용하면 안됨
+
 class BridgeGame {
   #bridgeArr;
 
   constructor(length) {
     this.#bridgeArr = makeBridge(length, generate);
-  }
-  /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  move() {
-    // U를 선택한 경우 -> 정답 다리 리스트 해당 칸이 U인 경우 통과, D인 경우 실패
-    // D를 선택한 경우 -> 정답 다리 리스트 해당 칸이 D인 경우 통과, U인 경우 실패
-    // 통과 혹은 실패 여부 UI 로직에 넘겨주기
+    this.stepArr = [];
   }
 
+  move(inputDirection) {
+    this.judgeSuccess(this.getNumOfSteps(), inputDirection);
+    this.updateProgressOfGame(inputDirection);
+  }
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
    * <p>
@@ -27,6 +26,17 @@ class BridgeGame {
    */
   retry() {
     // 처음에 만든 정답 다리 리스트, 총 시도 횟수 기억한채로 iii.으로 다시 돌아감
+  }
+  updateProgressOfGame(inputDirection) {
+    this.stepArr.push(inputDirection);
+    if (this.stepArr.length === this.#bridgeArr.length) this.isCleared = true;
+  }
+  getNumOfSteps() {
+    return this.stepArr.length;
+  }
+
+  judgeSuccess(index, inputDirection) {
+    this.isSuccess = this.#bridgeArr[index] === inputDirection;
   }
 }
 
