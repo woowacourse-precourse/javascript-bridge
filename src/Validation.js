12c4f73ea4e04d90dda2e2629736c46919b2a286
imhynni <omelas329@gmail.com>
2022-11-22 00:30:31 +0900

    feat: 이동할 칸이 'U'나 'D'가 아닌 경우 예외 발생

--- src/Validation.js
+++ src/Validation.js
@@ -1,17 +1,24 @@
 const { INPUT_ERROR_MESSAGE } = require("./Constants");
 
-const checkNaN = (value) => {
-  const check = /^[0-9]+$/;
-  if (!check.test(value)) return true;
+const Validation = {
+  checkNaN(value) {
+    const check = /^[0-9]+$/;
+    if (!check.test(value)) return true;
+  },
+
+  checkRange(value) {
+    if (value < 3 || value > 20) return true;
+  },
+
+  checkBridgeSize(size) {
+    if (this.checkNaN(size)) throw INPUT_ERROR_MESSAGE.NAN_ERROR;
+    if (this.checkRange(parseInt(size))) throw INPUT_ERROR_MESSAGE.RANGE_ERROR;
+  },
+
+  checkDirection(direction) {
+    if (!["U", "D"].includes(direction))
+      throw INPUT_ERROR_MESSAGE.DIRECTION_ERROR;
+  },
 };
 
-const checkRange = (value) => {
-  if (value < 3 || value > 20) return true;
-};
-
-const checkBridgeSize = (size) => {
-  if (checkNaN(size)) throw INPUT_ERROR_MESSAGE.NAN_ERROR;
-  if (checkRange(parseInt(size))) throw INPUT_ERROR_MESSAGE.RANGE_ERROR;
-};
-
-module.exports = { checkBridgeSize: checkBridgeSize };
+module.exports = Validation;
