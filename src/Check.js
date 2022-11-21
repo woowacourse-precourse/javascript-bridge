345455e488093c0651d62c91f3079aeccbb768da
vjvl95 <vjvl95@naver.com>
2022-11-21 14:11:02 +0900

    fact : 다리 길이 입력시 숫자만 가능하도록 예외처리

--- src/Check.js
+++ src/Check.js
@@ -21,6 +21,10 @@ const Check = {
     if (isFinshed) return this.checkIsFinshed(Player, Bridge);
   },
 
+  isVaildSize(size) {
+    if (isNaN(size)) throw new Error('[ERROR] 숫자만 입력이 가능합니다');
+  },
+
   isVaildInput(input) {
     if (input === USER_VALID_INPUT.U || input === USER_VALID_INPUT.D) return;
     throw new Error(ERROR_MESSAGES.ERROR_INVAILD_INPUT(USER_VALID_INPUT.U, USER_VALID_INPUT.D));
