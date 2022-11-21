d6ebcd1199e0c8b505bfb848d7a52794b7cf12be
Yesong <to06109@naver.com>
2022-11-21 20:36:11 +0900

    feat(move): 이동한 칸을 건널 수 있는지 확인

--- src/App.js
+++ src/App.js
@@ -3,18 +3,32 @@ const command = require('./utils/message');
 const OutputView = require('./OutputView');
 const InputView = require('./InputView');
 const BridgeMaker = require('./BridgeMaker');
+const BridgeGame = require('./BridgeGame');
+
 class App {
   #bridgeSize;
   #bridge;
 
-  async play() {
+  play() {
     OutputView.printCommand(command.START);
+    this.#setBridge();
+  }
+
+  async #setBridge() {
     this.#bridgeSize = await InputView.readBridgeSize();
     this.#bridge = BridgeMaker.makeBridge(
       this.#bridgeSize,
       BridgeRandomNumberGenerator.generate
     );
-    const move = await InputView.readMoving();
+    this.#doGame();
+  }
+
+  async #doGame() {
+    const bridgeGame = new BridgeGame(this.#bridge);
+    for (let brigeIndex = 0; brigeIndex < this.#bridgeSize; brigeIndex++) {
+      const moveInput = await InputView.readMoving();
+      bridgeGame.move(moveInput, brigeIndex);
+    }
   }
 }
 
