2b65febcdaa22e8dfb150bee7a645ffba0e8331c
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-11-21 18:37:19 +0900

    refactoring($requests): 파일명 변경

--- src/constants/requests/requests.js
+++
@@ -1,4 +0,0 @@
-const REQUEST_FOR_BRIDGE_LENGTH = '다리의 길이를 입력해주세요.\n';
-const REQUEST_FOR_MOVING = '\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n';
-
-module.exports = { REQUEST_FOR_BRIDGE_LENGTH, REQUEST_FOR_MOVING };
