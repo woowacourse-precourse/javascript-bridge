85f9f9d895015a7b85a6016c6eb945643da47e00
seoye0ng <101791501+seoye0ng@users.noreply.github.com>
2022-11-22 01:24:44 +0900

    feat : 이동할 칸 입력 값이 두 다리 중 어느 곳인지 확인하고 이동할 다리의 상태를 리턴하는 기능 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -14,7 +14,24 @@ class BridgeGame {
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move() {
+    const matching = this.match();
+    const userStep = matching[0];
+    const matchMap = matching[1];
+    let upBridge = [];
+    let downBridge = [];
+    for (let i = 0; i < userStep.length; i++) {
+      if (userStep[i] === 'U' && (matchMap[i] === 'O' || matchMap[i] === 'X')) {
+        upBridge.push(matchMap[i]);
+        downBridge.push(' ');
+      }
+      if (userStep[i] === 'D' && (matchMap[i] === 'O' || matchMap[i] === 'X')) {
+        downBridge.push(matchMap[i]);
+        upBridge.push(' ');
+      }
+    }
+    return [upBridge, downBridge];
+  }
 
   // 일단 O인지 X인지 거르기
   match(bridge, userMoving) {
