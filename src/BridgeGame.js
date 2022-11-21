e1ddb122ee21c0fad5cef43c90fbd31d8919bc06
kwh97 <zzzang0512@naver.com>
2022-11-21 17:36:33 +0900

    refactor: InputView를 BridgeGame.js 클래스 내에서 사용하지 않도록 분리 기존 set() 함수 기능을  start() 와 getBridge() 함수가 대체하도록 코드 수정

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,39 +1,31 @@
-const { INPUT_QUESTION } = require("./utils/Constants");
-const InputView = require("./InputView");
 const Validation = require("./Validation");
-const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
-const BridgeMaker = require("./BridgeMaker");
+const GameManager = require("./GameManager");
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
-  #bridge
+  #bridges
 
-  set() {
-    InputView.readBridgeSize(INPUT_QUESTION.bridgeLen, (bridgeLength) => {
-      Validation.checkBridgeLength(bridgeLength);
-      const bridge = BridgeMaker.makeBridge(bridgeLength, BridgeRandomNumberGenerator.generate);
-      this.#bridge = bridge;
-      this.move();
-    });
+  constructor() {
+    this.gameManager = new GameManager();
   }
-  /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
+
+  start() {
+    this.gameManager.inputBridgeSize(this.getBridge.bind(this));
+  }
+
+  getBridge(bridge) {
+    this.#bridges = bridge;
+  }  
+  
   move() {
     InputView.readMoving('이동할 칸을 선택해주세요.', (space) => {
       Validation.checkSpace(space);
     });
   }
 
-  /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  retry() {}
+
 }
 
 module.exports = BridgeGame;
