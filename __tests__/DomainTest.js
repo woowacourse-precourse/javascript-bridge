e3f61c7d983b10ccac31caaa7d0bb9246f4b29fb
ilgon0110 <moses011010@gmail.com>
2022-11-21 19:24:48 +0900

    test: 게임 결과 출력 테스트 완료 (#test 10, 11)
    다리를 다 건너서 게임이 끝난 경우 성공, 아닌 경우 실패로 처리했다.
    총 시도한 횟수를 계산했다.

--- __tests__/DomainTest.js
+++ __tests__/DomainTest.js
@@ -5,6 +5,7 @@ const InputView = require('../src/Views/InputView');
 const BridgeMaker = require('../src/BridgeMaker');
 const GameView = require('../src/Views/GameView');
 const OutputView = require('../src/Views/OutputView');
+const App = require('../src/App');
 
 const mockQuestions = (answers) => {
   MissionUtils.Console.readLine = jest.fn();
@@ -25,7 +26,6 @@ const mockRandoms = (numbers) => {
 };
 
 const getLogSpy = () => {
-  console.log('getLogSpy');
   const logSpy = jest.spyOn(MissionUtils.Console, 'print');
   logSpy.mockClear();
   return logSpy;
@@ -69,7 +69,7 @@ describe('1. 다리 생성하기', () => {
 });
 
 describe('3. 플레이어가 이동할 칸 선택하기', () => {
-  test.skip.each([[['Up']], [['down']], [['△']]])(
+  test.each([[['Up']], [['down']], [['△']]])(
     '사용자가 대문자 U나 D를 입력했는지 확인했다.',
     (input) => {
       const model = new Model();
@@ -81,7 +81,7 @@ describe('3. 플레이어가 이동할 칸 선택하기', () => {
       );
     },
   );
-  test('다리 출력 테스트', () => {
+  test.skip('다리 출력 테스트', () => {
     const logSpy = getLogSpy();
     const model = new Model();
     const gameView = new GameView(new InputView(), new OutputView());
@@ -110,7 +110,7 @@ describe('4. 잘못된 칸을 밟았을 때', () => {
       );
     },
   );
-  test('R을 눌러 재시작하고 다리 무사히 건넌 경우', () => {
+  test.skip('R을 눌러 재시작하고 다리 무사히 건넌 경우', () => {
     const logSpy = getLogSpy();
     const model = new Model();
     const gameView = new GameView(new InputView(), new OutputView());
@@ -118,7 +118,6 @@ describe('4. 잘못된 칸을 밟았을 때', () => {
     mockQuestions(['3', 'U', 'D', 'U', 'R', 'U', 'D', 'D']);
     mockRandoms(['1', '0', '0']);
 
-    bridgeGame.getBridgeSize();
     bridgeGame.start();
     const log = getOutput(logSpy);
 
@@ -127,6 +126,35 @@ describe('4. 잘못된 칸을 밟았을 때', () => {
       '[   | O |   ]',
       '[ O |   |   ]',
       '[   | O | O ]',
+    ]);
+    expectBridgeOrder(
+      log,
+      '[ O |   | X ]',
+      '[   | O |   ]',
+      '[ O |   |   ]',
+      '[   | O | O ]',
+    );
+  });
+});
+
+describe('6. 게임이 종료됐을 경우', () => {
+  test('R을 눌러 재시작하고 다리 무사히 건넌 경우 게임 결과까지 출력', () => {
+    const logSpy = getLogSpy();
+    mockQuestions(['3', 'U', 'D', 'U', 'R', 'U', 'D', 'D']);
+    mockRandoms(['1', '0', '0']);
+
+    const app = new App();
+    app.play();
+    const log = getOutput(logSpy);
+
+    expectLogContains(log, [
+      '[ O |   | X ]',
+      '[   | O |   ]',
+      '최종 게임 결과',
+      '[ O |   |   ]',
+      '[   | O | O ]',
+      '게임 성공 여부: 성공',
+      '총 시도한 횟수: 2',
     ]);
     expectBridgeOrder(
       log,
