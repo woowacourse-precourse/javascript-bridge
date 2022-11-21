a67f5b879eb4c04fffa6def7731bc87048d9d5a3
seoye0ng <101791501+seoye0ng@users.noreply.github.com>
2022-11-22 01:21:26 +0900

    feat : 이동할 칸 입력 값과 생성된 다리 배열값과 같은 값이 들어있는지 비교하는 기능 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,13 +1,35 @@
+const InputView = require('./InputView');
+const BridgeMaker = require('./BridgeMaker');
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  count = 1;
+  result = '성공';
+  //bridge = BridgeMaker.makeBridge();
+
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   move() {}
+
+  // 일단 O인지 X인지 거르기
+  match(bridge, userMoving) {
+    let matchMap = [];
+    let userStep = [];
+    userStep.push(userMoving);
+    for (let i = 0; i < userStep.length; i++) {
+      if (bridge[i] === userStep[i]) {
+        matchMap.push('O');
+      }
+      matchMap.push('X');
+      this.notMatched();
+    }
+    return [userStep, matchMap];
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
