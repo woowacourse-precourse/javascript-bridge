1b379ce77239f89461524a58a0dc2768db4cefed
kitez <duswl339@ajou.ac.kr>
2022-11-21 13:41:16 +0900

    refactor: test code for문으로 수정
    + 테스트코드도 코드다.

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,30 +1,30 @@
 const {validateBridgeLength, validationMove} = require("../src/Validation");
 
 describe("validation 다리 길이 입력 test", () => {
-    test("validation length function test(1) 범위내의 숫자 입력", () => {
-        expect(validateBridgeLength(10)).toEqual(true);
-    });
+    test("validation length function test", () => {
+        // (1) 범위 내의 숫자 입력 true
+        // (2) 범위 외의 숫자 입력 true
+        // (3) 문자열 입력 false
+       const input = [10,40,'a'];
+       const output = [true,false,false];
 
-    test("validation length function test(2) 범위 외의 숫자 입력", () => {
-        expect(validateBridgeLength(40)).toEqual(false);
-    });
-
-    test("validation length function test(2) 문자 입력", () => {
-        expect(validateBridgeLength('a')).toEqual(false);
+       for(let i = 0 ; i < input.length ; i++){
+           expect(validateBridgeLength(input[i])).toEqual(output[i]);
+       }
     });
 });
 
 
 describe("validation user 이동 입력 test", () => {
-    test("validation move function test(1) U입력", () => {
-        expect(validationMove('U')).toEqual(true);
-    });
+    test("validation move function test", () => {
+        // (1) U 입력 true
+        // (2) D 입력 true
+        // (3) 숫자 입력 false
+        const input = ['U', 'D', 3];
+        const output = [true,true,false];
 
-    test("validation move function test(2) D입력", () => {
-        expect(validationMove('D')).toEqual(true);
-    });
-
-    test("validation move function test(3) 숫자 입력", () => {
-        expect(validationMove(2)).toEqual(false);
+        for(let i = 0 ; i < input.length ; i++){
+            expect(validationMove(input[i])).toEqual(output[i]);
+        }
     });
 });
