3e50e0b28da909915aa813e8be0f72e2bf7ff767
mjmj0217 <mjmj0217@naver.com>
2022-11-21 20:25:39 +0900

    feat(const): 게임 상수 추가

--- src/const.js
+++ src/const.js
@@ -1,10 +1,17 @@
+const cmd = {
+  UP:"U",
+  DOWN : "D",
+  RETRY : "R",
+  QUIT : "Q",
+}
+
 const gameConst = {
   process: {
     START_MESSAGE: "다리 건너기 게임을 시작합니다.",
     INPUT_LENGTH_MESSAGE: "다리의 길이를 입력해주세요.",
-    INPUT_CHOOSE_MESSAGE: "이동할 칸을 선택해주세요. (위: U, 아래: D)",
+    INPUT_CHOOSE_MESSAGE: `이동할 칸을 선택해주세요. (위: ${cmd.UP}, 아래: ${cmd.DOWN})`,
     INPUT_RESART_MESSAGE:
-      "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
+      `게임을 다시 시도할지 여부를 입력해주세요. (재시도: ${cmd.RETRY}, 종료: ${cmd.QUIT})`,
   },
   result: {
     message: {
@@ -17,5 +24,17 @@ const gameConst = {
       SUCCESS: "성공",
     },
   },
+  sign:{
+    O_SIGN:"O",
+    X_SIGN:"X",
+    BLANK_SIGN:" "
+  },
+  error:{
+    BRIDGE_ERROR :"[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.",
+    MOVE_COMMAND_ERROR : `[ERROR] ${cmd.UP} 또는 ${cmd.DOWN}를 입력해주세요.`,
+    RETRY_COMMAND_ERROR : `[ERROR] ${cmd.QUIT} 또는 ${cmd.RETRY}를 입력해주세요.`
+  },
+  cmd
 };
+
 module.exports = gameConst;
