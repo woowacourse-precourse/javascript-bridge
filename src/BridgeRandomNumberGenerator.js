72ef512780b23b08e00b7c8b6b3c64f0d178ade2
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-21 13:16:10 +0900

    feat(BridgeRandomNumberGenerator): generate 메서드 구현

--- src/BridgeRandomNumberGenerator.js
+++ src/BridgeRandomNumberGenerator.js
@@ -1,14 +1,15 @@
-const MissionUtils = require("@woowacourse/mission-utils");
+const {Random} = require('@woowacourse/mission-utils');
 
-const BridgeRandomNumberGenerator = {
-  RANDOM_LOWER_INCLUSIVE: 0,
-  RANDOM_UPPER_INCLUSIVE: 1,
+class BridgeRandomNumberGenerator {
+  static RANDOM_LOWER_INCLUSIVE = 0;
+  static RANDOM_UPPER_INCLUSIVE = 1;
+
   generate() {
-    return MissionUtils.Random.pickNumberInRange(
+    return Random.pickNumberInRange(
       BridgeRandomNumberGenerator.RANDOM_LOWER_INCLUSIVE,
       BridgeRandomNumberGenerator.RANDOM_UPPER_INCLUSIVE
     );
-  },
-};
+  }
+}
 
 module.exports = BridgeRandomNumberGenerator;
