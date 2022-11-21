9127aa5fc8c92ba697effeb8297dbfb41eaa49b6
Jin-uu <78341764+Jin-uu@users.noreply.github.com>
2022-11-22 00:16:31 +0900

    feat : 다리 이동 방향 검증 테스트 구현

--- __tests__/Validator.test.js
+++ __tests__/Validator.test.js
@@ -4,7 +4,12 @@ const {Console} = require("@woowacourse/mission-utils");
 
 
 
-const bridgeLengthArray = [-1, 0, "5", 2, 21, 999];
+const bridgeLengthArray = [-1, 0, 2, 21, 999];
 test.each(bridgeLengthArray)("다리 길이 Validator 테스트", (bridgeLength) => {
     expect(() => Validator.validateBridgeSize(bridgeLength)).toThrow()
+})
+
+const moveDirectionArray = [1, "A", "UD", undefined];
+test.each(bridgeLengthArray)("다리 이동 방향 Validator 테스트", (moveDirection) => {
+    expect(() => Validator.validateMoveDirection(moveDirection)).toThrow()
 })
