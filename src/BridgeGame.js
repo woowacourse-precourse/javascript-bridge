6a9d5646040e1130ffb1b609368431f75beec0f9
nalsae <nalsae0207@naver.com>
2022-11-22 01:16:39 +0900

    feat: Implement a method to retry game
    게임을 재시작하는 메서드 구현
    - bridge를 제외한 게임 관련 상태 초기화 후 이동할 칸 입력 값 받는 메서드
      호출

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,4 +1,4 @@
-const BridgeMap = require("./BridgeMap");
+const BridgeMap = require('./BridgeMap');
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
@@ -7,6 +7,7 @@ class BridgeGame {
   static bridge;
   static movingCount = 0;
   static currentState = [];
+  static tryCount = 0;
 
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -19,12 +20,13 @@ class BridgeGame {
     this.movingCount += 1;
     this.currentState = [
       ...this.currentState,
-      [moving, movingResult ? "O" : "X"],
+      [moving, movingResult ? 'O' : 'X'],
     ];
 
     const mapInfo = {
       gameState: this.currentState,
       originBridgeSize: this.bridge.length,
+      tryCount: this.tryCount,
     };
 
     BridgeMap.createMap(mapInfo, readFuncs);
@@ -35,7 +37,13 @@ class BridgeGame {
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  static retry() {}
+  static retry(readMoving) {
+    this.movingCount = 0;
+    this.currentState = [];
+    this.tryCount += 1;
+
+    readMoving();
+  }
 }
 
 module.exports = BridgeGame;
