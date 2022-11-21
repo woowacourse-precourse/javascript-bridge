dd0b7c2771152cb505c83737946bbf56c84af223
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-11-22 06:22:20 +0900

    feat(method): add hasCrossedCompletely
    Add hasCrossedCompletely method to BridgeGame
    - Return true if user crossed the bridge completely
    - Return false otherwise

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -46,6 +46,10 @@ class BridgeGame {
     return lastSymbol === O;
   }
 
+  hasCrossedCompletely() {
+    return this.#bridge.length === this.#results.length;
+  }
+
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
