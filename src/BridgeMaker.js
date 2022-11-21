ba93bcb2887ee5e8a6c012b67a45ddd551c7a636
Mcdonalds Trump <whdmschd2@gmail.com>
2022-11-22 01:18:10 +0900

    refactor: 다리 생성 함수 Array.from으로 수정

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -10,14 +10,9 @@ const BridgeMaker = {
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
   makeBridge(size, generateRandomNumber) {
-    const bridgeShape = [];
-    for (let makedBridgeSize = 1; makedBridgeSize <= size; makedBridgeSize++) {
-      const shape = generateRandomNumber()
-        ? COMMAND.MOVING.UP
-        : COMMAND.MOVING.DOWN;
-      bridgeShape.push(shape);
-    }
-    return bridgeShape;
+    return Array.from({ length: size }, () => {
+      return generateRandomNumber() ? COMMAND.MOVING.UP : COMMAND.MOVING.DOWN;
+    });
   },
 };
 
