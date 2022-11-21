8f085c0d9169c8fb875655c2a54cc62fe8db0c87
SeungHun Park <tmdgns1126@naver.com>
2022-11-21 17:46:42 +0900

    feat: 다리 칸 생성 로직 구현

--- src/InputView.js
+++ src/InputView.js
@@ -1,4 +1,6 @@
 const { Console } = require("@woowacourse/mission-utils");
+const BridgeMaker = require("./BridgeMaker");
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -10,6 +12,11 @@ const InputView = {
   readBridgeSize() {
     Console.readLine("다리의 길이를 입력해주세요.\n", (input) => {
       const bridgeLength = Number(input);
+      const bridge = BridgeMaker.makeBridge(
+        bridgeLength,
+        BridgeRandomNumberGenerator.generate
+      );
+      Console.close();
     });
   },
 
