25a3f110ff171daa51b3f5ad948e8a51c8a8f0b8
solo5star <solo_5@naver.com>
2022-11-22 08:46:48 +0900

    feat(bridge-basic): 다리를 무작위로 생성하는 기능 구현

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+const Tile = {
+  /** @type {string[]} */
+  ALL_TILES: [this.DOWN, this.UP],
+  SPAWNABLE_TILES: [this.DOWN, this.UP],
+
+  UP: 'U',
+  DOWN: 'D',
+};
+
+module.exports = {
+  Tile,
+};
