f71c1661791558af15878475d724b1fabab84a6b
ashleysyheo <ashleysyheo@gmail.com>
2022-11-21 15:11:02 +0900

    feat: start bridge game

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const InputView = require('./InputView');
+const { CONSOLE_MESSAGE } = require('./utils/constants');
+
 class App {
-  play() {}
+  play() {
+    MissionUtils.Console.print(CONSOLE_MESSAGE.startGame);
+    InputView.readBridgeSize();
+  }
 }
 
 module.exports = App;
