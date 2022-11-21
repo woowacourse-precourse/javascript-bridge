440d7719f4d067aade84f69f9b916f333ade8b52
vinivin153 <vinivin153@naver.com>
2022-11-21 23:48:56 +0900

    feat: 이동경로 업데이트 기능 추가

--- src/Player.js
+++ src/Player.js
@@ -1,8 +1,23 @@
 class Player {
   #path;
+  #currentLocation;
 
   constructor() {
     this.#path = { upside: [], downside: [] };
     this.#currentLocation = 0;
   }
+
+  updatePath(direction, mark) {
+    if (direction === 'U') {
+      this.#path.upside.push(mark);
+      this.#path.downside.push('   ');
+    } else {
+      this.#path.upside.push('   ');
+      this.#path.downside.push(mark);
+    }
+
+    this.#currentLocation += 1;
+  }
 }
+
+module.exports = Player;
