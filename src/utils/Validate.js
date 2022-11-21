47957f102ff0d10b009f6b568dce81558f62cc70
shackstack <kms1061@naver.com>
2022-11-21 15:59:52 +0900

    fix(Validate): 에러메세지 오류 수정

--- src/utils/Validate.js
+++ src/utils/Validate.js
@@ -3,20 +3,20 @@ const { ERROR_MESSAGE } = require("../constants/Message");
 const Validate = {
   validateSize(size) {
     if (isNaN(size)) {
-      throw ERROR_MESSAGE.BRIDGE_SIZE.NOT_A_NUMBER;
+      throw new Error(ERROR_MESSAGE.BRIDGE_SIZE.NOT_A_NUMBER);
     }
     if (size < 3 || size > 20) {
-      throw ERROR_MESSAGE.BRIDGE_SIZE.INVALID_RANGE;
+      throw new Error(ERROR_MESSAGE.BRIDGE_SIZE.INVALID_RANGE);
     }
   },
   validateMoving(moving) {
     if (moving !== "U" && moving !== "D") {
-      throw ERROR_MESSAGE.MOVING;
+      throw new Error(ERROR_MESSAGE.MOVING);
     }
   },
   validateCommand(command) {
     if (command !== "R" && command !== "Q") {
-      throw ERROR_MESSAGE.COMMAND;
+      throw new Error(ERROR_MESSAGE.COMMAND);
     }
   },
 };
