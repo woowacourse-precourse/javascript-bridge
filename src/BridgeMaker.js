2e265f4f733e9c8c0f73e2caa7e76c0c5630d9c5
Minhye Kang <alsgp9898@naver.com>
2022-11-21 15:43:43 +0900

    feat: 다리를 만드는 기능 추가

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -7,7 +7,18 @@ const BridgeMaker = {
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
-  makeBridge(size, generateRandomNumber) {},
+  makeBridge(size, generateRandomNumber) {
+    let bridgeArray = Array(Number(size)).fill('');
+    bridgeArray = bridgeArray.map((item) => item + generateRandomNumber());
+    
+    return bridgeArray.map((zeroOrOne) =>
+      zeroOrOne.replace(zeroOrOne, replaceString(zeroOrOne))
+    );
+  },
 };
 
+const replaceString = (zeroOrOne) => {
+    if (zeroOrOne === '1') return 'U';
+    if (zeroOrOne === '0') return 'D';
+}
 module.exports = BridgeMaker;
--- src/InputHandling.js
+++ src/InputHandling.js
@@ -1,8 +1,12 @@
 const { Console } = require('@woowacourse/mission-utils');
+const { makeBridge } = require('./BridgeMaker');
+const { generate } = require('./BridgeRandomNumberGenerator');
 const InputView = require('./InputView');
 const Validation = require('./Validation');
 
 class InputHandling {
+  #answerBridgeArray
+
   play() {
     InputView.readBridgeSize(this.handleBridgeSize.bind(this));
   }
@@ -10,6 +14,7 @@ class InputHandling {
   handleBridgeSize(size) {
     try {
       Validation.checkBridgeSize(size);
+      this.#answerBridgeArray = makeBridge(size, generate);
     } catch (error) {
       Console.print(error);
       InputView.readBridgeSize(this.handleBridgeSize.bind(this));
