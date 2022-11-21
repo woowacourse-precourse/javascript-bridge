54a2c8064a961e73ba1f0d80531adfee0f62f3ee
dragonfruitlemonade <tpghks9245@gmail.com>
2022-11-21 20:47:41 +0900

    feat(ValidationTest): 다리 입력값 숫자인지 테스트

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -7,17 +7,17 @@ describe("Validation 클래스 테스트", () => {
     expect(() => Validation.bridgeLength(input)).toThrow('[ERROR] 값을 입력해주세요.');
   });
 
-//   test("다리 길이 입력 값이 비었을 경우", () => {
-//     const input = "";
+  test("다리 길이 입력 값이 숫자가 아닐경우", () => {
+    const input = "가";
 
-//     expect(() => Validation.bridgeLength(input)).toThrow('[ERROR] 값을 입력해주세요.');
-//   });
+    expect(() => Validation.bridgeLength(input)).toThrow('[ERROR] 숫자를 입력해주세요.');
+  });
 
-//   test("다리 길이 입력 값이 비었을 경우", () => {
-//     const input = "";
+  test("다리 길이 입력 값이 숫자가 아닐경우", () => {
+    const input = "number";
 
-//     expect(() => Validation.bridgeLength(input)).toThrow('[ERROR] 값을 입력해주세요.');
-//   });
+    expect(() => Validation.bridgeLength(input)).toThrow('[ERROR] 숫자를 입력해주세요.');
+  });
 
 //   test("다리 길이 입력 값이 비었을 경우", () => {
 //     const input = "";
