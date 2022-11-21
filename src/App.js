29f8faea488acd601bb1f7dd3d3373522a982c13
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-11-22 01:44:38 +0900

    feat(method): add createGame
    Add createGame method to App
    - Create new BridgeGame instance with bridge
    - Run game

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 const { printStartingMessage } = require("./OutputView");
 const { readBridgeSize } = require("./InputView");
 const { Console } = require("@woowacourse/mission-utils");
+const BridgeGame = require("./BridgeGame");
 
 class App {
   play() {
     printStartingMessage();
     readBridgeSize(this);
+  }
+
+  createGame(bridge) {
+    const bridgeGame = new BridgeGame(bridge);
+    this.runGame(bridgeGame);
   }
 
   terminate() {
