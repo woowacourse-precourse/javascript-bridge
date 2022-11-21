c9b02627b122030681c20138aad54be847b9cde6
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-11-21 22:41:22 +0900

    feat(App): 게임 시작 안내 문구 메서드

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+const InputView = require('./InputView');
+const OutputView = require('./OutputView');
 const BridgeGame = require('./BridgeGame');
 const BridgeMaker = require('./BridgeMaker');
 const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
@@ -5,16 +7,20 @@ const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
 class App {
   #bridgeGame;
 
+  #input;
+
+  #output;
+
   constructor() {
     this.#bridgeGame = new BridgeGame();
+    this.#input = InputView;
+    this.#output = OutputView;
   }
 
-  generateBridge(size) {
-    const newBridge = BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate);
+  printStartMessage() {
+    const message = OutputView.message.START;
 
-    this.#bridgeGame.setBridge(newBridge);
-
-    return newBridge;
+    this.#output.print(message);
   }
 }
 
