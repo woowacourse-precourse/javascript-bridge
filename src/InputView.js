3a0772e9754b55d9b172fbc224f65bf843ab4d91
Jaehyeon <jaehyeonp74@gmail.com>
2022-11-21 23:12:02 +0900

    feat: 이동 칸 입력 시 예외 값을 입력할 때 다시 입력 받는 기능
    - 에러 메세지 출력 후 다시 입력을 받음

--- src/InputView.js
+++ src/InputView.js
@@ -25,6 +25,10 @@ const InputView = {
     const bridgeSize = model.getBridgeSize();
     if (index === bridgeSize) return OutputView.printResult();
     Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D) \n', (input) => {
+      if (!Validator.validateUpDown(input)) {
+        Console.print('[ERROR] U 또는 D 중 한 문자만 입력해주세요.\n');
+        return this.readMoving(callback, model, index);
+      }
       callback(input, index);
       return this.readMoving(callback, model, index + 1);
     });
