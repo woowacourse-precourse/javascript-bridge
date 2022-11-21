5b3894c168caed5a5678ad41dbb93a9e97d0933c
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2022-11-21 17:11:27 +0900

    style: add space after [ERROR] tag

--- src/Error.js
+++ src/Error.js
@@ -1,8 +1,8 @@
 const ERROR = Object.freeze({
-    numberNotInRange: '[ERROR]3~20사이의 숫자를 입력',
-    notNumber: '[ERROR]숫자가 아님',
-    notPlayCommand: '[ERROR]U 또는 D 중 하나를 입력해야 함',
-    notGameCommand: '[ERROR]R 또는 Q 중 하나를 입력해야 함'
+  numberNotInRange: "[ERROR] 3~20사이의 숫자를 입력",
+  notNumber: "[ERROR] 숫자가 아님",
+  notPlayCommand: "[ERROR] U 또는 D 중 하나를 입력해야 함",
+  notGameCommand: "[ERROR] R 또는 Q 중 하나를 입력해야 함",
 });
 
 module.exports = ERROR;
