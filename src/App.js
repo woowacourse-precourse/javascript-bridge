9e68a4b4bd6092056be14abc199f58478e376b0a
hxezin <leehxejin@gmail.com>
2022-11-22 00:15:40 +0900

    feat: 게임 시작 시 다리의 길이를 숫자로 입력받음

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import BridgeGame from "./BridgeGame";
+import { readBridgeSize } from "./InputView";
+const { Console } = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    Console.print("다리 건너기 게임을 시작합니다.\n");
+    const bridgeGame = new BridgeGame();
+    readBridgeSize(bridgeGame);
+  }
 }
 
 module.exports = App;
--- src/InputView.js
+++ src/InputView.js
@@ -1,11 +1,30 @@
+const { Console } = require("@woowacourse/mission-utils");
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
+  validateBridgeSize(value) {
+    const bridgeSizeReg = /^[3-9]{1}$|^[1]{1}[0-9]{1}$|20$/;
+    if (!bridgeSizeReg.test(value)) {
+      throw new Error(
+        "[ERROR] 다리의 길이는 3 이상 20 이하의 숫자여야 합니다."
+      );
+    }
+  },
+
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine("다리의 길이를 입력해주세요.", (answer) => {
+      try {
+        this.validateBridgeSize(Number(answer));
+      } catch {
+        this.readBridgeSize();
+      }
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
