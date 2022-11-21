674419a7652354421c49b351168b22c24f1349d5
jisu-kim <kss2460@naver.com>
2022-11-21 19:45:58 +0900

    test(Validator): 예외 처리 테스트 추가

--- __tests__/Validator.test.js
+++ __tests__/Validator.test.js
@@ -11,6 +11,26 @@ describe("예외 처리 테스트", () => {
         expect(Validator.validateInputCatch(size)).toBe(true)
     });
 
+    test("validateisRepeat 예외 테스트 정상", () => {
+        const moveInput = "U";
+        expect(Validator.validateisRepeat(moveInput)).toBe(undefined)
+    });
+
+    test("validateisRepeat 예외 테스트 실패", () => {
+        const moveInput = '우테코';
+        expect(Validator.validateisRepeat(moveInput)).toBe(true)
+    });
+
+    test("validateRetryInputCatch 예외 테스트 정상", () => {
+        const retryInput = "R";
+        expect(Validator.validateRetryInputCatch(retryInput)).toBe(undefined)
+    });
+
+    test("validateRetryInputCatch 예외 테스트 실패", () => {
+        const retryInput = '우테코';
+        expect(Validator.validateRetryInputCatch(retryInput)).toBe(true)
+    });
+
 });
 
 
