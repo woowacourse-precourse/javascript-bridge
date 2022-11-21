8e6c3d657527ca7aaaef2609087efdacac0e7fbe
hwanfront <kim015jh@gmail.com>
2022-11-21 17:49:54 +0900

    refactor(exception): 필드를 상수로 변경

--- src/exception/BridgeRandomNumberException.js
+++ src/exception/BridgeRandomNumberException.js
@@ -1,7 +1,7 @@
 const Exception = require('./Exception');
 
 class BridgeRandomNumberException extends Exception {
-  #ERROR_MESSAGE = '랜덤 숫자가 범위에 포함되지 않습니다.';
+  static #ERROR_MESSAGE = '랜덤 숫자가 범위에 포함되지 않습니다.';
 
   constructor() {
     super(BridgeRandomNumberException.#ERROR_MESSAGE);
