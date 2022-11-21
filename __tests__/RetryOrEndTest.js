485d5fabb53a1a597f10f110f39cb0ddbf974cb5
Subin Jo <sbjo1202@naver.com>
2022-11-22 00:48:36 +0900

    test: 재시작 / 종료 테스트

---
+++ __tests__/RetryOrEndTest.js
@@ -0,0 +1,88 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const App = require("../src/App");
+const BridgeMaker = require("../src/BridgeMaker");
+
+const mockQuestions = (answers) => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce((acc, input) => {
+    return acc.mockImplementationOnce((_, callback) => {
+      callback(input);
+    });
+  }, MissionUtils.Console.readLine);
+};
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
+const getOutput = (logSpy) => {
+  return [...logSpy.mock.calls].join("");
+};
+
+const runException = (inputs) => {
+  mockQuestions(inputs);
+  const logSpy = getLogSpy();
+  const app = new App();
+
+  app.play();
+
+  expectLogContains(getOutput(logSpy), ["[ERROR]"]);
+};
+
+const expectLogContains = (received, logs) => {
+  logs.forEach((log) => {
+    expect(received).toEqual(expect.stringContaining(log));
+  });
+};
+
+const expectBridgeOrder = (received, upside, downside) => {
+  const upsideIndex = received.indexOf(upside);
+  const downsideIndex = received.indexOf(downside);
+
+  expect(upsideIndex).toBeLessThan(downsideIndex);
+};
+
+describe("재시작 또는 종료 테스트", () => {
+  test("2번 실패 후 성공하는 경우", () => {
+    const logSpy = getLogSpy();
+    mockRandoms([1, 0, 1]);
+    mockQuestions(["3", "U", "D", "D", "R", "D", "R", "U", "D", "U"]);
+
+    const app = new App();
+    app.play();
+
+    const log = getOutput(logSpy);
+    expectLogContains(log, [
+      "게임 성공 여부: 성공",
+      "총 시도한 횟수: 3",
+    ]);
+    
+    expectBridgeOrder(log, "[ O |   | O ]", "[   | O |   ]");
+  });
+
+  test("1번 실패 후 종료하는 경우", () => {
+    const logSpy = getLogSpy();
+    mockRandoms([1, 0, 1]);
+    mockQuestions(["3", "U", "U", "Q"]);
+
+    const app = new App();
+    app.play();
+
+    const log = getOutput(logSpy);
+    expectLogContains(log, [
+      "게임 성공 여부: 실패",
+      "총 시도한 횟수: 1",
+    ]);
+    
+    expectBridgeOrder(log, "[ O | X ]", "[   |   ]");
+  });
+});
