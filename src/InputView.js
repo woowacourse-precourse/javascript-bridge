3c372ef941b149a501cc0419da5292dd324a2a4d
sujungYu <su_moment@naver.com>
2022-11-21 19:38:25 +0900

    refactor(InputView) : 선언된 상수명으로 수정

--- src/InputView.js
+++ src/InputView.js
@@ -1,5 +1,5 @@
 const { Console } = require('@woowacourse/mission-utils');
-// const BridgeSizeValidator = require('./validator/BridgeSizeValidator');
+const { INPUT_MESSAGE } = require('./util/Constant');
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,21 +8,21 @@ const InputView = {
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize(input) {
-    Console.readLine('다리의 길이를 입력해주세요.\n', input);
+    Console.readLine(INPUT_MESSAGE.BRIDGE_SIZE, input);
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving(input) {
-    Console.readLine('이동할 칸을 선택해 주세요. (위: U, 아래:D)\n', input);
+    Console.readLine(INPUT_MESSAGE.MOVE, input);
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand(input) {
-    Console.readLine('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n', input);
+    Console.readLine(INPUT_MESSAGE.COMMAND, input);
   },
 };
 
