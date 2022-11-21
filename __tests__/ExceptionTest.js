be45d0386fc3164c0400d60586e79f75e1730b05
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-11-22 02:39:39 +0900

    test(ExceptionTest): 기능 1. 다리 길이 입력받기 예외처리 테스트 케이스 추가

---
+++ __tests__/ExceptionTest.js
@@ -0,0 +1,23 @@
+const BridgeController = require('../src/BridgeController');
+const InputView = require('../src/InputView');
+const OutputView = require('../src/OutputView');
+
+const views = {
+    inputView: InputView,
+    outputView: OutputView
+  }
+
+describe('1. 다리 길이 입력에 대한 예외처리', () => {
+    const controller = new BridgeController(views);
+    test.each([
+        ',',
+        '1ㄷ',
+        '40',
+        ' ',
+        'abc'
+    ])('다리 길이의 입력값 %s일 때 에러를 반환한다.', (input) => {
+        expect(() => { 
+             controller.bridgeSizeException(input)
+        } ).toThrow();
+    })
+})
