5f290791265a529dc19bae18791947c584417b5c
Raehyeon Jeong <raehy19@gmail.com>
2022-11-21 18:51:10 +0900

    feat(src/inputView.js) : 사용자에게 다리 길이 입력받고 예외처리 메소드

--- src/InputView.js
+++ src/InputView.js
@@ -1,11 +1,33 @@
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+const MissionUtils = require('@woowacourse/mission-utils');
+
+const MSG = {
+  BRIDGE_SIZE: '다리의 길이를 입력해주세요.\n',
+  BRIDGE_SIZE_ERR: '[ERROR] 다리 길이는 3부터 20 사이의 정수 여야 합니다.\n',
+};
+
 const InputView = {
-  /**
-   * 다리의 길이를 입력받는다.
-   */
-  readBridgeSize() {},
+  readBridgeSize(setBridgeSize) {
+    MissionUtils.Console.readLine(MSG.BRIDGE_SIZE, (input) => {
+      try {
+        this.validateBridgeSize(input);
+      } catch (e) {
+        MissionUtils.Console.print(e);
+        this.readBridgeSize(setBridgeSize);
+      }
+      setBridgeSize(input);
+    });
+  },
+  validateBridgeSize(input) {
+    const inputNum = Number(input);
+    if (
+      isNaN(inputNum) ||
+      !Number.isInteger(inputNum) ||
+      inputNum < 3 ||
+      inputNum > 20
+    ) {
+      throw new Error(MSG.BRIDGE_SIZE_ERR);
+    }
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
