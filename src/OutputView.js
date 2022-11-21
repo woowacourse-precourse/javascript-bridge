93e75b2c509c13f0742234840123dfb1c3cd5500
Steadystudy <sgmin206@naver.com>
2022-11-21 17:41:49 +0900

    feat: 결과 변환해서 출력하기

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,5 +1,11 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
+ * - 제공된 `OutputView` 객체를 활용해 구현해야 한다.
+- `OutputView`의 파일 경로는 변경할 수 있다.
+- `OutputView`의 메서드의 이름은 변경할 수 없고, 인자는 필요에 따라 추가하거나 변경할 수 있다.
+- 값 출력을 위해 필요한 메서드를 추가할 수 있다.
  */
 const OutputView = {
   /**
@@ -7,7 +13,11 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {},
+  printMap(map) {
+    const [up, down] = OutputView.convertMap(map);
+    MissionUtils.Console.print(up);
+    MissionUtils.Console.print(down);
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
@@ -15,6 +25,13 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printResult() {},
+
+  convertMap(map) {
+    const { U, D } = map;
+    const up = `[ ${U.join(' | ')} ]`;
+    const down = `[ ${D.join(' | ')} ]`;
+    return [up, down];
+  },
 };
 
 module.exports = OutputView;
