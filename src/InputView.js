0ba57738b52a5cce53ad81a8629aa131d8c6f992
jiyun1006 <jiyun4251@gmail.com>
2022-11-21 22:46:47 +0900

    feat : 다리의 길이를 입력받고, 사용자가 이동할 칸을 입력받는 단계.
    다리의 길이를 입력받고 그 길이를 토대로 만든 다리를 만듦.
    이후, 그 다리와 이동할 칸을 가지고, 출력 기능을 만들어야 함.

--- src/InputView.js
+++ src/InputView.js
@@ -1,16 +1,27 @@
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+const MissionUtils = require("@woowacourse/mission-utils");
+const Script = require("./Script");
+const BridgeMaker = require("./BridgeMaker");
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
-
+  readBridgeSize() {
+    MissionUtils.Console.readLine(`${Script.BRIDGELENGTHINPUT}\n`, (answer) => {
+      const ans = BridgeMaker.makeBridge(answer, BridgeRandomNumberGenerator);
+      InputView.readMoving();
+      return ans;
+    })
+  },
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    MissionUtils.Console.readLine(`\n${Script.CHOICEBRIDGE}\n`, (answer) => {
+      
+    })
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
