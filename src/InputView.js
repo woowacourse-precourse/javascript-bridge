e7f5cd832041f6664af24b82c3037712ce8c234d
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-11-21 21:44:18 +0900

    feat(InputView): 입력받기 기능(다리 길이, 이동할 칸) 구현

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,9 @@
+const { Console } = require("@woowacourse/mission-utils");
+const BridgeGame = require("./BridgeGame");
+const BridgeMaker = require("./BridgeMaker");
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+const { eventEmitter } = require("./EventEmitter");
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,12 +11,35 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine("다리의 길이를 입력해주세요.\n", (bridgeLength) => {
+      if (isNaN(bridgeLength)) {
+        Console.print("[ERROR] 다리의 길이는 숫자입니다");
+        InputView.readBridgeSize();
+        return;
+      }
+
+      if (bridgeLength < 3 || bridgeLength > 20) {
+        Console.print("[ERROR] 다리의 길이는 3이상, 20이하입니다");
+        InputView.readBridgeSize();
+        return;
+      }
+
+      eventEmitter.emit("readBridgeSize", bridgeLength);
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    Console.readLine(
+      "이동할 칸을 선택해주세요. (위:U, 아래:D)\n",
+      (direction) => {
+        eventEmitter.emit("readMoving", direction);
+      }
+    );
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
