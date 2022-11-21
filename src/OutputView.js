35d46ab9a47785df00db1b894298ba5a9720d0eb
dongyeopca <ckehdduq95@gmail.com>
2022-11-21 13:06:21 +0900

    feat(OutputView): 결과 출력 - 사용자가 이동할 때마다 다리 건너기 결과를 출력

--- src/OutputView.js
+++ src/OutputView.js
@@ -13,7 +13,45 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap(bridgegame) {},
+  printMap(bridgegame) {
+    const [up, down] = this.makeMap(bridgegame);
+    Console.print(`[${up}]`);
+    Console.print(`[${down}]`);
+  },
+
+  makeMap(bridgegame) {
+    let up = '';
+    let down = '';
+    for (const history of bridgegame.getHistory()) {
+      if (up.length == 0 || down.length == 0) {
+        [up, down] = this.firstDraw(history);
+        continue;
+      }
+      [up, down] = this.addDraw(up, down, history);
+    }
+    return [up, down];
+  },
+
+  firstDraw(history) {
+    const value = history[0],
+      result = history[1];
+    if (value == 'U') {
+      if (result) return [' O ', '   '];
+      return [' X ', '   '];
+    }
+    if (result) return ['   ', ' O '];
+    return ['   ', ' X '];
+  },
+
+  addDraw(up, down, history) {
+    const value = history[0],
+      result = history[1];
+    if (value == 'U' && result) (up += '| O '), (down += '|   ');
+    if (value == 'U' && !result) (up += '| X '), (down += '|   ');
+    if (value == 'D' && result) (up += '|   '), (down += '| O ');
+    if (value == 'D' && !result) (up += '|   '), (down += '| X ');
+    return [up, down];
+  },
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
    * <p>
