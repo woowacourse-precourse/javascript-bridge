12b0f6194c0b4b85e126f21fc32297bd80b78bec
nalsae <nalsae0207@naver.com>
2022-11-22 00:47:01 +0900

    feat: Implement a method to print result
    결과를 출력하는 메서드 구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,11 +1,12 @@
-const { Console } = require("@woowacourse/mission-utils");
+const { Console } = require('@woowacourse/mission-utils');
+const { INFO_MESSAGES } = require('./utils/messages');
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 const OutputView = {
   printStartMessage() {
-    Console.print("다리 건너기 게임을 시작합니다.\n");
+    Console.print('다리 건너기 게임을 시작합니다.\n');
   },
 
   /**
@@ -14,17 +15,16 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printMap(gameInfo, readFuncs) {
-    const { map, lastResult, originBridgeSize, currentBridgeSize } = gameInfo;
+    const { map, lastResult, originBridgeSize, currentBridgeSize, tryCount } =
+      gameInfo;
     const { readMoving, readGameCommand } = readFuncs;
+
     Console.print(map);
 
-    if (lastResult === "X") readGameCommand();
-
-    if (lastResult === "O" && originBridgeSize !== currentBridgeSize) {
-      readMoving();
-    } else {
-      this.printResult();
-    }
+    lastResult === 'X' && readGameCommand(map, this.printResult.bind(this));
+    lastResult === 'O' && originBridgeSize !== currentBridgeSize
+      ? readMoving()
+      : this.printResult(map, true, tryCount);
   },
 
   /**
@@ -32,7 +32,23 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() {},
+  printResult(map, isSuccess, tryCount) {
+    const { RESULT_TITLE, SUCCESS_TITLE, TRY_TITLE } = INFO_MESSAGES;
+
+    const gameResult = `${RESULT_TITLE}${map}\n`;
+    const successResult = `${SUCCESS_TITLE}${isSuccess ? '성공' : '실패'}`;
+    const tryResult = `${TRY_TITLE}${tryCount}`;
+
+    const resultMessage = `${gameResult}${successResult}${tryResult}`;
+
+    Console.print(resultMessage);
+
+    this.endGame();
+  },
+
+  endGame() {
+    Console.close();
+  },
 };
 
 module.exports = OutputView;
