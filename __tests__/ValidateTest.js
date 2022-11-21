29c901fcb70a20a8a2421cdd768e4b351b5b4ee3
d-charlie-kim <dokkim@student.42seoul.kr>
2022-11-21 16:46:38 +0900

    Test: 다리 생성 테스트

--- __tests__/ValidateTest.js
+++ __tests__/ValidateTest.js
@@ -1,4 +1,5 @@
 const Validator = require('../src/Validator');
+const BridgeMaker = require('../src/BridgeMaker');
 
 describe('다리 생성 테스트', () => {
 	test('다리 사이즈 유효성 테스트', () => {
@@ -8,5 +9,25 @@ describe('다리 생성 테스트', () => {
 				Validator.checkBridgeSize(cases);
 			}).toThrow('[ERROR]');
 		});
-	})
+	});
+
+	test('짧은 옳은 다리 생성 확인 테스트', () => {
+    const randomNumbers = [1, 0, 0];
+    const mockGenerator = randomNumbers.reduce((acc, number) => {
+      return acc.mockReturnValueOnce(number);
+    }, jest.fn());
+
+    const bridge = BridgeMaker.makeBridge(3, mockGenerator);
+    expect(bridge).toEqual(['U', 'D', 'D']);
+  });
+
+	test('긴 옳은 다리 생성 확인 테스트', () => {
+    const randomNumbers = [0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0];
+    const mockGenerator = randomNumbers.reduce((acc, number) => {
+      return acc.mockReturnValueOnce(number);
+    }, jest.fn());
+
+    const bridge = BridgeMaker.makeBridge(12, mockGenerator);
+    expect(bridge).toEqual(['D', 'D', 'D', 'U', 'U', 'U', 'U', 'U', 'U', 'D', 'D', 'D']);
+  });
 });
