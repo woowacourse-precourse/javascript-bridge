840b87026c3ce592228a6166d648df2ed7744131
vjvl95 <vjvl95@naver.com>
2022-11-21 13:10:45 +0900

    feat : 최종 게임 결과 출력

--- src/OutputView.js
+++ src/OutputView.js
@@ -4,13 +4,14 @@ const { OUTPUT_MESSAGES } = require('./common/messages');
 const OutputView = {
   printMap(uparray, downarray) {
     MissionUtils.Console.print('[ ' + uparray.join(' | ') + ' ]');
-    MissionUtils.Console.print('[ ' + downarray.join(' | ') + ' ]');
+    MissionUtils.Console.print('[ ' + downarray.join(' | ') + ' ]\n');
   },
 
   printResult(result) {
     const [Player, Bridge] = result;
-
+    MissionUtils.Console.print(OUTPUT_MESSAGES.OUTPUT_FINSHED);
     this.printMap(Bridge.getUpBridge(), Bridge.getDownBridge());
+    MissionUtils.Console.print(OUTPUT_MESSAGES.OUTPUT_ISWINNING(Player.getIsWinnging()));
     MissionUtils.Console.print(OUTPUT_MESSAGES.OUTPUT_FINALCOUNT(Player.getCount()));
   },
 
