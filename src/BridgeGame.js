4ecb1597b1d8d42eeb8c989dda01fe943713e7e3
jhyns <tockiib@gmail.com>
2022-11-22 00:14:53 +0900

    feat: Add methods for BridgeGame
    BridgeGame 클래스를 위한 메소드 추가

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,58 +1,50 @@
 const BridgeMaker = require('./BridgeMaker');
-const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
+const { MAP } = require('./Constants');
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
   #bridge;
-  #choice;
+  #choices;
   #count;
 
   constructor() {
     this.#bridge = [];
-    this.#choice = [];
-    this.#count = 0;
+    this.#choices = [];
+    this.#count = 1;
   }
 
   #checkChoice() {
-    const length = this.#choice.length;
-    const lastBridge = this.#bridge[length - 1];
-    const lastChoice = this.#choice[length - 1];
+    const length = this.#choices.length;
 
-    return lastBridge === lastChoice;
+    return this.#bridge[length - 1] === this.#choices[length - 1];
   }
 
-  makeBridge(size) {
-    this.#bridge = BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator);
+  checkClear() {
+    return this.#bridge.join('') === this.#choices.join('');
   }
 
-  /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
   move(choice) {
-    this.#choice.push(choice);
+    this.#choices.push(choice);
     return this.#checkChoice();
   }
 
-  /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
   retry() {
-    this.#choice = [];
+    this.#choices = [];
     this.#count += 1;
   }
 
-  getBridgeLength() {
-    return this.#bridge.length;
+  setBridge(bridge) {
+    this.#bridge = bridge;
   }
 
-  getChoiceLength() {
-    return this.#choice.length;
+  getBridge() {
+    return this.#bridge;
+  }
+
+  getChoices() {
+    return this.#choices;
   }
 
   getCount() {
