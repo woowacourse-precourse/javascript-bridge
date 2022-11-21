f7af5fb021ba9438ca965aa8e5d6ec5921b02c44
xownsdl0 <xownsdl0@naver.com>
2022-11-22 01:47:11 +0900

    feat: ErrorMessage, 게임 재시작 또는 종료 예외처리 문구 추가

--- src/error/ErrorMessage.js
+++ src/error/ErrorMessage.js
@@ -1,6 +1,7 @@
 const ERROR_MESSAGE = {
   RANGE_ERROR: '[ERROR] 3 ~ 20 사이의 숫자만 입력해주세요.',
-  INPUT_MOVE_ERROR: '[ERROR] 대문자 U 또는 D만 입력해주세요.'
+  INPUT_MOVE_ERROR: '[ERROR] 대문자 U 또는 D만 입력해주세요.',
+  INPUT_RETRY_ERROR: '[ERROR] 대문자 R 또는 Q만 입력해주세요.'
 }
 
 module.exports = ERROR_MESSAGE
