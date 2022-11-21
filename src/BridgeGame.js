18f75e8536ea358f05646ca3bf37a2b2ee86c2d6
Ryan-Dia <cjfdnjs158@naver.com>
2022-11-21 14:45:13 +0900

    refactor(BridgeGame):move()메서드 분리
    20줄이 넘어가는	 move()메서드 분리 및 효율화 작업

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -5,14 +5,6 @@ const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
-  /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-
-  RESTART = 1;
-
   #BridgeData = {
     size: null,
     tryCount: 1,
@@ -23,6 +15,75 @@ class BridgeGame {
     state: null,
     prebuilt: null,
   };
+
+  /**
+   * 사용자가 칸을 이동할 때 사용하는 메서드
+   * <p>
+   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+   */
+
+  move(upOrDown, state) {
+    this.divideSpace();
+    if (state) return this.pass(state, upOrDown);
+    return this.fail(state, upOrDown);
+  }
+
+  /**
+   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
+   * <p>
+   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+   */
+
+  retry() {
+    this.#BridgeData.up = '';
+    this.#BridgeData.down = '';
+    this.#BridgeData.tryOrder = 0;
+    this.#BridgeData.tryCount += 1;
+  }
+
+  static end() {
+    Console.close();
+  }
+
+  pass(state, upOrDown) {
+    if (upOrDown === 'U') {
+      this.moveToUp(state);
+      return [upOrDown, this.#BridgeData.up, this.#BridgeData.down];
+    }
+    this.moveToDown(state);
+    return [upOrDown, this.#BridgeData.up, this.#BridgeData.down];
+  }
+
+  fail(state, upOrDown) {
+    if (upOrDown === 'U') {
+      this.moveToUp(state);
+      return [upOrDown, this.#BridgeData.up, this.#BridgeData.down];
+    }
+    this.moveToDown(state);
+    return [upOrDown, this.#BridgeData.up, this.#BridgeData.down];
+  }
+
+  moveToUp(state) {
+    const passOrFail = state ? 'O' : 'X';
+    this.#BridgeData.up += ` ${passOrFail} `;
+    this.#BridgeData.down += '   ';
+  }
+
+  moveToDown(state) {
+    const passOrFail = state ? 'O' : 'X';
+    this.#BridgeData.up += '   ';
+    this.#BridgeData.down += ` ${passOrFail} `;
+  }
+
+  static isRetry(input) {
+    const RETRY = 1;
+    if (input === RETRY) {
+    }
+  }
+
+  saveSize(input) {
+    this.#BridgeData.size = Number(input);
+  }
 
   isAllPass() {
     return this.#BridgeData.tryOrder === this.#BridgeData.size;
@@ -54,63 +115,11 @@ class BridgeGame {
     return this.#BridgeData;
   }
 
-  move(upOrDown, state) {
+  divideSpace() {
     if (this.#BridgeData.up.length) {
       this.#BridgeData.up += '|';
       this.#BridgeData.down += '|';
     }
-
-    if (state) {
-      if (upOrDown === 'U') {
-        this.#BridgeData.up += ' O ';
-        this.#BridgeData.down += '   ';
-        return [upOrDown, this.#BridgeData.up, this.#BridgeData.down];
-      }
-      if (upOrDown === 'D') {
-        this.#BridgeData.up += '   ';
-        this.#BridgeData.down += ' O ';
-        return [upOrDown, this.#BridgeData.up, this.#BridgeData.down];
-      }
-    }
-    if (!state) {
-      if (upOrDown === 'U') {
-        this.#BridgeData.up += ' X ';
-        this.#BridgeData.down += '   ';
-        return [upOrDown, this.#BridgeData.up, this.#BridgeData.down];
-      }
-      if (upOrDown === 'D') {
-        this.#BridgeData.up += '   ';
-        this.#BridgeData.down += ' X ';
-        return [upOrDown, this.#BridgeData.up, this.#BridgeData.down];
-      }
-    }
-  }
-
-  /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-
-  static isRetry(input) {
-    const RETRY = 1;
-    if (input === RETRY) {
-    }
-  }
-
-  saveSize(input) {
-    this.#BridgeData.size = Number(input);
-  }
-
-  retry() {
-    this.#BridgeData.up = '';
-    this.#BridgeData.down = '';
-    this.#BridgeData.tryOrder = 0;
-    this.#BridgeData.tryCount += 1;
-  }
-
-  static end() {
-    Console.close();
   }
 }
 
