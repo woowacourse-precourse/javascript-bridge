c3c2cf2fd84130094a6a947e9ff0f1d2653a76be
sunny <huiseon37@gmail.com>
2022-11-21 16:50:46 +0900

    feat: 이동할 칸 입력 함수 구현

--- src/InputView.js
+++ src/InputView.js
@@ -12,13 +12,17 @@ const InputView = {
   readBridgeSize() {
     Console.readLine(INPUT_MESSAGE.READ_BRIDGE_SIZE,(bridgeSize) => {
       
-    })
+    });
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    Console.readLine(INPUT_MESSAGE.READ_MOVING,(moving) => {
+      
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
