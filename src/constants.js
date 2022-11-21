fdc305adcf17c404c8728e32640fb499147eb434
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-11-22 07:52:53 +0900

    chore: adjust blank lines

--- src/constants.js
+++ src/constants.js
@@ -2,9 +2,9 @@ module.exports = {
   NOTICE: Object.freeze({
     START_GAME: "다리 건너기 게임을 시작합니다.\n",
     INPUT_SIZE: "다리의 길이를 입력해주세요.\n",
-    INPUT_DIRECTION: "이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
-    INPUT_RETRY: "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n",
-    END_GAME: "최종 게임 결과",
+    INPUT_DIRECTION: "\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
+    INPUT_RETRY: "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n",
+    END_GAME: "\n최종 게임 결과",
   }),
 
   ERROR: Object.freeze({
