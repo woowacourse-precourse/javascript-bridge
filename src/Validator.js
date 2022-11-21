9b4feeadd4654d3e06754c7e09a5bc48d24977a8
BB-choi <choibobae0809@gmail.com>
2022-11-21 16:54:33 +0900

    refactor(Validator): 불필요한 return문 삭제
    메서드 길이 10라인 기준 충족

--- src/Validator.js
+++ src/Validator.js
@@ -21,8 +21,6 @@ class Validator {
     if (input !== input.trim()) {
       throw new Error(`${this.ERROR_MESSAGE_HEADER} 입력에 공백이 포함되어 있습니다.`);
     }
-
-    return true;
   };
 
   isValidNumber = (input) => {
@@ -33,8 +31,6 @@ class Validator {
     if (!Validator.#isInteger(input)) {
       throw new Error(`${this.ERROR_MESSAGE_HEADER} 올바른 숫자(정수)를 입력하세요.`);
     }
-
-    return true;
   };
 }
 
