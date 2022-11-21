caa048684562d9106c766bfce3754e4c9f4c615d
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-11-22 04:24:43 +0900

    ✅ [test]: type 오류 수정

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,5 +1,4 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-const { ERROR_MESSAGE } = require("../src/Utils/Constants");
 const App = require("../src/Controller/App");
 const BridgeMaker = require("../src/Model/BridgeMaker");
 
@@ -36,7 +35,7 @@ const runException = (inputs) => {
 
   app.play();
 
-  expectLogContains(getOutput(logSpy), [ERROR_MESSAGE.BRIDGE_SIZE_IS_NAN]);
+  expectLogContains(getOutput(logSpy), ["[ERROR]"]);
 };
 
 const expectLogContains = (received, logs) => {
@@ -54,7 +53,7 @@ const expectBridgeOrder = (received, upside, downside) => {
 
 describe("다리 건너기 테스트", () => {
   test("다리 생성 테스트", () => {
-    const randomNumbers = ["1", "0", "0"];
+    const randomNumbers = [1, 0, 0];
     const mockGenerator = randomNumbers.reduce((acc, number) => {
       return acc.mockReturnValueOnce(number);
     }, jest.fn());
@@ -65,7 +64,7 @@ describe("다리 건너기 테스트", () => {
 
   test("기능 테스트", () => {
     const logSpy = getLogSpy();
-    mockRandoms(["1", "0", "1"]);
+    mockRandoms([1, 0, 1]);
     mockQuestions(["3", "U", "D", "U"]);
 
     const app = new App();
