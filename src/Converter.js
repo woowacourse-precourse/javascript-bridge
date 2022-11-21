372d3cee45959bdfdf0ba07e1465cc3afab9bfc0
안호진 <asdv94@naver.com>
2022-11-21 22:48:57 +0900

    feat(converter): 다리 결과 변환 skeleton 코드 추가

--- src/Converter.js
+++ src/Converter.js
@@ -1,8 +1,11 @@
 const { BRIDGE_MSG } = require('./Constant');
+
 const Converter = {
   convertToUpDown(number) {
     return +number === 1 ? BRIDGE_MSG.upward : BRIDGE_MSG.downward;
   },
+
+  convertToBridgeMap() {},
 };
 
 module.exports = Converter;
