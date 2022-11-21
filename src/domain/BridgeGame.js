d2f03fb6e91858a27a00daa5c56f3e5b47047d82
wzrabbit <curious.wzrabbit@gmail.com>
2022-11-21 10:59:01 +0900

    feat(bridgegame): 다리 이동 기능 구현

---
+++ src/domain/BridgeGame.js
@@ -0,0 +1,35 @@
+const Validator = require('../Validator');
+const BridgeMaker = require('../BridgeMaker');
+const BridgeRandomNumberGenerator = require('../BridgeRandomNumberGenerator');
+const ResultJudger = require('./ResultJudger');
+const BridgeDrawer = require('./BridgeDrawer');
+
+class BridgeGame {
+  #gameStatus = {};
+  #bridgeDrawer;
+  #resultJudger;
+
+  constructor(bridgeSize) {
+    Validator.errorIfBridgeSizeInvalid(bridgeSize);
+    this.#gameStatus.bridge = BridgeMaker.makeBridge(
+      bridgeSize,
+      BridgeRandomNumberGenerator.generate
+    );
+    this.#gameStatus.bridgeCrossCount = 0;
+    this.#gameStatus.attempt = 1;
+    this.#bridgeDrawer = new BridgeDrawer();
+    this.#resultJudger = new ResultJudger();
+  }
+
+  move(movingDirection) {
+    this.#gameStatus.movingDirection = movingDirection;
+    this.#gameStatus.bridgeCrossCount += 1;
+    this.#gameStatus.roundResult = this.#resultJudger.getRoundResult(
+      this.#gameStatus.bridge,
+      this.#gameStatus.bridgeCrossCount,
+      this.#gameStatus.movingDirection
+    );
+  }
+}
+
+module.exports = BridgeGame;
