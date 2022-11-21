4e56910e0f7511f78e6b8c9d4bcbd06f1c959277
vinivin153 <vinivin153@naver.com>
2022-11-21 23:43:25 +0900

    feat: 변수 설정 및 초기화

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,19 +1,25 @@
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
+const BridgeGameHandler = require('./BridgeGameHandler');
+const Player = require('./Player');
+const { makeBridge } = require('./BridgeMaker');
+const { generate } = require('./BridgeRandomNumberGenerator');
+
 class BridgeGame {
-  /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
+  #bridge;
+  #player;
+  #attempts;
+
+  constructor() {
+    this.#bridge;
+    this.#player = new Player();
+    this.#attempts = 1;
+  }
+
+  setBridge(size) {
+    this.#bridge = makeBridge(size, generate);
+  }
+
   move() {}
 
-  /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
   retry() {}
 }
 
