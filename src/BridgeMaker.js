caf5b2e49abe37b4e8bf9dc4c3b081534fe1a65c
Tory <88531407+1two13@users.noreply.github.com>
2022-11-21 22:17:32 +0900

    refactor(BridgeMaker): else-if문 else문으로 변경

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -4,7 +4,7 @@ const BridgeMaker = {
     for (let i = 1; i <= size; i++) {
       const random = String(generateRandomNumber());
       if (random === '1') bridge.push('U');
-      else if (random === '0') bridge.push('D');
+      else bridge.push('D');
     }
 
     return bridge;
