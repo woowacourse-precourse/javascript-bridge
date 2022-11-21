6f76339796fd497f0a543313fb1913e9137b78e3
d0422 <rlfehd2013@naver.com>
2022-11-21 22:51:12 +0900

    Refactor(ControlValidation): R, Q 문자열 상수변수로 변환

--- src/Validation/ControlValidation.js
+++ src/Validation/ControlValidation.js
@@ -5,7 +5,7 @@ module.exports = function controlValidate(input) {
   if (input !== RETRY && input !== QUIT) {
     throw new CustomError(
       ERROR_NAME.CONTROL,
-      '게임 재시작/종료 여부는 R 또는 Q만 입력할 수 있습니다.'
+      `게임 재시작/종료 여부는 ${RETRY} 또는 ${QUIT}만 입력할 수 있습니다.`
     );
   }
 };
