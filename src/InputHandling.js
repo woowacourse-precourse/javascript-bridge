90dd2c44d137c0b6e32086aedb0d028adec521d8
Minhye Kang <alsgp9898@naver.com>
2022-11-21 14:47:06 +0900

    feat(InputHandling): 잘못된 값을 입력하면 에러를 발생시키고 다시 입력받는 기능 추가

--- src/InputHandling.js
+++ src/InputHandling.js
@@ -1,12 +1,19 @@
+const { Console } = require('@woowacourse/mission-utils');
 const InputView = require('./InputView');
-const Validation = require('./Validation')
+const Validation = require('./Validation');
+
 class InputHandling {
   play() {
     InputView.readBridgeSize(this.handleBridgeSize.bind(this));
   }
 
   handleBridgeSize(size) {
-    Validation.checkBridgeSize(size);
+    try {
+      Validation.checkBridgeSize(size);
+    } catch (error) {
+      Console.print(error);
+      InputView.readBridgeSize(this.handleBridgeSize.bind(this));
+    }
   }
 }
 
