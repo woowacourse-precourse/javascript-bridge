4aa3ae00e96e1d1600babf73de427ce8299774d2
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-22 03:14:37 +0900

    test(BridgeGameTest): retry 메서드 테스트 케이스 추가 및 테스트 완료

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -24,6 +24,32 @@ describe('BridgeGame 클래스 테스트', () => {
     });
   });
 
+  describe('retry 메서드 테스트', () => {
+    test('방향 입력 이후 retry 메서드가 실행되면 저장된 로그가 초기화 되어야 한다.', () => {
+      const bridge = new Bridge(['U', 'D', 'U']);
+      const bridgeGame = new BridgeGame(bridge);
+
+      bridgeGame.move('U');
+      bridgeGame.retry();
+
+      const result = bridgeGame.getMovementLogs();
+
+      expect(result).toHaveLength(0);
+    });
+
+    test('첫 시도 이후 retry 메서드가 실행되면 시도 횟수 1에서 2로 증가해야 한다.', () => {
+      const bridge = new Bridge(['U', 'D', 'U']);
+      const bridgeGame = new BridgeGame(bridge);
+
+      bridgeGame.move('U');
+      bridgeGame.retry();
+
+      const result = bridgeGame.getTryCount();
+
+      expect(result).toBe(2);
+    });
+  });
+
   describe('isLatestMoveSucceeded 메서드 테스트', () => {
     test('생성된 다리와 방향 입력이 일치하면, isLatestMoveSucceeded 메서드는 true를 return한다.', () => {
       const bridge = new Bridge(['U', 'D', 'U']);
