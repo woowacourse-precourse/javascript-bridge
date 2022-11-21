2d262ee45dae4b5e22c715e0297652a02615bed4
jhyns <tockiib@gmail.com>
2022-11-21 22:31:02 +0900

    feat: Add constants
    출력에 쓰일 상수 추가

--- src/Constants.js
+++ src/Constants.js
@@ -9,9 +9,10 @@ const ERROR = Object.freeze({
 
 const MESSAGES = Object.freeze({
   GAME_START: '다리 건너기 게임을 시작합니다.',
-  INPUT_BRIGDE_LENGTH: '다리의 길이를 입력해주세요.',
-  INPUT_MOVE: '이동할 칸을 선택해주세요. (위: U, 아래: D)',
-  INPUT_RETRY: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)',
+  INPUT_BRIDGE_LENGTH: '다리의 길이를 입력해주세요.\n',
+  INPUT_MOVE: '이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
+  INPUT_RETRY:
+    '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
   FINAL_RESULT: '최종 게임 결과',
   GAME_RESULT: '게임 성공 여부: ',
   GAME_SUCCESS: '성공',
@@ -19,7 +20,19 @@ const MESSAGES = Object.freeze({
   TOTAL_TRY: '총 시도한 횟수: ',
 });
 
+const MAP = Object.freeze({
+  START: '[ ',
+  END: ' ]',
+  DIVIDER: ' | ',
+  SUCCESS: 'O',
+  FAIL: 'X',
+  EMPTY: ' ',
+  UP: 'U',
+  DOWN: 'D',
+});
+
 module.exports = {
   ERROR,
   MESSAGES,
+  MAP,
 };
