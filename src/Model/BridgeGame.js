c01cc29544aed70478c5c67ca3d4e3ab6a360077
youzysu <111998760+youzysu@users.noreply.github.com>
2022-11-21 18:05:28 +0900

    feat: 사용자가 입력한 값이 이동할 수 있는 칸인지 판별하는 기능

--- src/Model/BridgeGame.js
+++ src/Model/BridgeGame.js
@@ -46,6 +46,11 @@ class BridgeGame {
     return this.#lowerTrack;
   }
 
+  keepGoing() {
+    const lastPath = [this.#upperTrack.pop(), this.#lowerTrack.pop()];
+    return lastPath.includes(PATH.RIGHT) ? true : false;
+  }
+
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
    * <p>
