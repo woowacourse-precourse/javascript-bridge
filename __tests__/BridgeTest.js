06d755b12753a76a97aed0f03e08a6b815082388
sohyun <park.sohyun0430@gmail.com>
2022-11-22 02:22:20 +0900

    test: move 테스트 코드 작성
    이동값이 U,D 아닌 경우 throw error 테스트 코드 작성

--- __tests__/BridgeTest.js
+++ __tests__/BridgeTest.js
@@ -1,13 +1,23 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const App = require('../src/App');
-const { BRIDGE_SIZE_ERROR } = require('../src/constants');
+const BridgeGame = require('../src/BridgeGame');
 
 describe('다리 길이 입력값 유효성 검사', () => {
   test('다리 길이는 3이상 20이하의 숫자이다.', () => {
     const app = new App();
-    const testBridgeSize = ['', 2, 'ab', '100', 100];
+    const testBridgeSize = ['', 2, 'ab', '100', 100, '03'];
     testBridgeSize.forEach((element) => {
       expect(() => app.isValidBridgeSize(element)).toThrow();
+    });
+  });
+});
+
+describe('move 유효성 검사', () => {
+  test('입력이 U,D가 아닌 경우 throw error.', () => {
+    const bridgeGame = new BridgeGame();
+    const testInput = ['r', '100', 33, 'ㅎㅎ', ' '];
+    testInput.forEach((element) => {
+      expect(() => bridgeGame.move(element)).toThrow();
     });
   });
 });
