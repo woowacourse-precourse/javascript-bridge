7a44c721e2049eed02e817960745791034e57a7d
vjvl95 <vjvl95@naver.com>
2022-11-21 14:11:37 +0900

    fix : 요구사항에 맞도록 문구 수정

--- src/common/messages.js
+++ src/common/messages.js
@@ -1,7 +1,7 @@
 const INPUT_MESSAGES = {
   INPUT_BRIDGESIZE: '다리의 길이를 입력해주세요.\n',
-  INPUT_MOVE: '이동할 칸을 선택해주세요. (위: U, 아래:D)\n',
-  INPUT_RETRY: '게임을 종료합니다.게임을 재시도하려면 R, 종료하려면 Q를 눌러주세요.',
+  INPUT_MOVE: '이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
+  INPUT_RETRY: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
 };
 
 const USER_VALID_INPUT = {
@@ -18,10 +18,11 @@ const BRIDGE_PRINT_WORD = {
 };
 
 const OUTPUT_MESSAGES = {
+  OUTPUT_STARTGAME: '다리 건너기 게임을 시작합니다.\n',
+
   OUTPUT_FINALCOUNT(count) {
-    return `최종 시도 횟수 : ${count}`;
+    return `총 시도한 횟수: ${count}`;
   },
-  OUTPUT_STARTGAME: '다리 건너기 게임을 시작합니다.',
   OUTPUT_ISWINNING(isWinning) {
     return `게임 성공 여부: ${isWinning ? '성공' : '실패'}`;
   },
@@ -30,7 +31,7 @@ const OUTPUT_MESSAGES = {
 
 const ERROR_MESSAGES = {
   ERROR_INVAILD_INPUT(word1, word2) {
-    return `${word1} 와 ${word2} 둘 중에 하나의 문자를 입력하세요.`;
+    return `[ERROR] ${word1} 와 ${word2} 둘 중에 하나의 문자를 입력하세요.`;
   },
 };
 
