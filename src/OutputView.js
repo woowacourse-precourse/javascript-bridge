4014612f1bbee13b46beb7f1f9f553de19b16c6f
ashleysyheo <ashleysyheo@gmail.com>
2022-11-21 23:52:53 +0900

    fix: bridge game scope

--- src/OutputView.js
+++ src/OutputView.js
@@ -6,20 +6,20 @@ const { RESULT_MESSAGE } = require('./utils/constants');
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 const OutputView = {
+  bridgeGame: new BridgeGame(),
   /**
    * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printMap(bridgeValue, moveDirection) {
-    const bridgeGame = new BridgeGame();
-    const bridgeGameProgress = bridgeGame.move(bridgeValue, moveDirection);
+    const bridgeGameProgress = this.bridgeGame.move(bridgeValue, moveDirection);
 
     bridgeGameProgress.forEach((progress) => {
       MissionUtils.Console.print(RESULT_MESSAGE.bridgeLog(progress));
     });
   },
-  
+
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
    * <p>
