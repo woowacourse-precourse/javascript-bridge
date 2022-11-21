db9bdfd3988ac34fb8fb6ba10f96fafb97e9ab6b
Buyoung Chung <sunarol@naver.com>
2022-11-21 20:59:27 +0900

    feat: Add property currentSize,failed and edit function move
    currentSize is the length of user's bridge
    failed is boolean for retry or quit

--- src/Model/BridgeGame.js
+++ src/Model/BridgeGame.js
@@ -1,32 +1,35 @@
 const Bridges = require("./Bridges");
-const BridgeMaker = require("../BridgeMaker");
+const Constant = require("../utils/Constant");
+const { LEFT_BRACKET } = require("../utils/Constant");
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
   #size;
+  #currentSize;
+  #failed;
 
   constructor(size) {
     this.#size = size;
+    this.#currentSize = 0;
+
+    this.#failed = false;
 
     this.bridges = new Bridges();
     this.answerBridge = [];
   }
+
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
+   * @param {string} input 사용자가 입력하는 "U" 혹은 "D"
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(input) {
+    if (this.#currentSize === 0) this.createBridges(input);
+    else this.extendBridge(input);
 
-  buildBridge() {}
-
-  /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  retry() {}
+    this.#currentSize++;
+  }
 }
 
 module.exports = BridgeGame;
