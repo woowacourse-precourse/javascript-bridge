620ca61bf6197e82fa320db6c889d1097065d138
moonelysian <dandyblack@naver.com>
2022-11-21 20:18:17 +0900

    feat(App): initBridge 작성
    - 다리 길이를 입력받는다
    - 다리를 랜덤으로 만든다

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 const BridgeGame = require('./BridgeGame.js');
+const InputView = require('./InputView.js');
+const { makeBridge } = require('./BridgeMaker.js');
+const { generate } = require('./BridgeRandomNumberGenerator.js');
+
 class App {
   #game
 
   constructor() {
     this.#game = new BridgeGame();
+  }
+
+  async initBridge() {
+    const bridgeSize = await InputView.readBridgeSize();
+    const bridge = makeBridge(bridgeSize, generate);
+    return bridge;
   }
 
   play() {
