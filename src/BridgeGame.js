416663ffc92e76472c150199736c99a985ba18a4
DongWooKim97 <nanaukim@naver.com>
2022-11-22 00:39:29 +0900

    feat: 정보 클래스에 이동 루트를 넘겨 로직에 대한 결과메시지를 받아오는 메소드 추가

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -12,9 +12,38 @@ class BridgeGame {
     this.#info = new BridgeInformation(size);
   }
 
-  move() {}
+  changeState(direction) {
+    this.#movingRoute.push(direction);
+    this.#inputIndex += 1;
+  }
+
+  move(direction) {
+    this.changeState(direction);
+    this.#info.makeRouteMap(direction, this.#inputIndex);
+    this.checkStatus();
+  }
+
+  checkStatus() {
+    this.getRouteMap();
+    this.checkWrongInput();
+    this.checkSuccess();
+  }
 
   retry() {}
+
+  getMatchSize() {
+    return this.#info.checkMatchLength(this.#movingRoute.length);
+  }
+
+  checkWrongInput() {
+    return this.#info.getWrongRoute();
+  }
+
+  checkSuccess() {
+    if (this.#info.successStatus(this.#movingRoute.length)) {
+      this.#info.finishGame(this.#tryCount, SIGN.SUCCESS);
+    }
+  }
 }
 
 module.exports = BridgeGame;
