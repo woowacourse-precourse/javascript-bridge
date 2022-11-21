6226d4ad9b38bb757da50f54184b46d70f06a09e
chiman <rlaclghks123@naver.com>
2022-11-21 14:00:23 +0900

    fix: bridge 생성 하는 기능 재수정

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -8,13 +8,11 @@ const BridgeMaker = {
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
   makeBridge(size, generateRandomNumber) {
-    return Array(size)
-      .fill(0)
-      .map(() => {
-        const number = generateRandomNumber();
-        if (number === 0 || number === '0') return 'D';
-        return 'U';
-      });
+    return Array.from({ length: size }, () => {
+      const number = generateRandomNumber();
+      if (number === 0 || number === '0') return 'D';
+      return 'U';
+    });
   },
 };
 
