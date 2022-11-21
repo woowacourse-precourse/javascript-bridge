1528d880f9fda4109f530b483d544961b55c24b8
chsua <csj1919@naver.com>
2022-11-21 15:42:51 +0900

    feat: 다리길이 받기~ U/D 입력요청(App.js)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+const {readBridgeSize, readMoving, readGameCommand} = require("./InputView");
+const {gameStart,printMap, printResult, printError} = require("./OutputView");
+const BridgeGame = require("./BridgeGame") ;
+
 class App {
-  play() {}
+  /**
+   * 게임 play
+   */
+   play() {
+    gameStart() ;
+    readBridgeSize( (input) => {
+      this.bridgeGame.makeRandomBridge(input);
+      this.movingBridge() ;
+    }) ;
+  }
+
+  /**
+   * User에게 UD을 받아서 전진하기 ~ 끝까지
+   */
+  movingBridge() {
+      readMoving(this.readMovingCallback)
+  }
+
+  readMovingCallback = (input) => {
+    
+  }
+
 }
 
 module.exports = App;
