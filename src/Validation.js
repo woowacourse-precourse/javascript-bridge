368382053e71b80da8438202cde0b3fbea589fc6
dragonfruitlemonade <tpghks9245@gmail.com>
2022-11-21 22:49:45 +0900

    style(Validation): 줄바꿈 정리

--- src/Validation.js
+++ src/Validation.js
@@ -2,53 +2,23 @@ const { Console } = require('@woowacourse/mission-utils');
 
 class Validation {
   static bridgeLength(number) {
-    if (Validation.isEmpty(number)) {
-        throw new Error('[ERROR] 값을 입력해주세요.'); 
-    }
-
-    if (!Validation.isNumber(number)) {
-        throw new Error('[ERROR] 숫자를 입력해주세요.')
-    }
-
-    if (!Validation.numberNet(number)) {
-        throw new Error('[ERROR] 3과 20사이의 값을 입력해주세요.');
-    }
+    if (Validation.isEmpty(number)) throw new Error('[ERROR] 값을 입력해주세요.'); 
+    if (!Validation.isNumber(number)) throw new Error('[ERROR] 숫자를 입력해주세요.')
+    if (!Validation.numberNet(number)) throw new Error('[ERROR] 3과 20사이의 값을 입력해주세요.');
   }
 
   static nextStep(value) {
-    if (Validation.isStringEmpty(value)) {
-        throw new Error('[ERROR] 값을 입력해주세요.');
-    }
-
-    if (!Validation.oneValue(value)) {
-        throw new Error('[ERROR] 1개의 값을 입력해주세요.');
-    }
-
-    if (Validation.nextStepLowerCase(value)) {
-      throw new Error('[ERROR] 소문자로 입력하셨습니다. 대문자로 입력해주세요.')
-    }
-
-    if (!Validation.nextStepValue(value)) {
-        throw new Error('[ERROR] U 또는 D를 입력해주세요.');
-    }
+    if (Validation.isStringEmpty(value)) throw new Error('[ERROR] 값을 입력해주세요.');
+    if (!Validation.oneValue(value)) throw new Error('[ERROR] 1개의 값을 입력해주세요.');
+    if (Validation.nextStepLowerCase(value)) throw new Error('[ERROR] 소문자로 입력하셨습니다. 대문자로 입력해주세요.')
+    if (!Validation.nextStepValue(value)) throw new Error('[ERROR] U 또는 D를 입력해주세요.');
   }
 
   static retry(value) {
-    if (Validation.isStringEmpty(value)) {
-        throw new Error('[ERROR] 값을 입력해주세요.');
-    }
-
-    if (!Validation.oneValue(value)) {
-        throw new Error('[ERROR] 1개의 값을 입력해주세요.');
-    }
-
-    if (Validation.retryLowerCase(value)) {
-      throw new Error('[ERROR] 소문자로 입력하셨습니다. 대문자로 입력해주세요.')
-    }
-
-    if (!Validation.retryOrNotValue(value)) {
-      throw new Error('[ERROR] R 또는 Q를 입력해주세요.');
-    }
+    if (Validation.isStringEmpty(value)) throw new Error('[ERROR] 값을 입력해주세요.');
+    if (!Validation.oneValue(value)) throw new Error('[ERROR] 1개의 값을 입력해주세요.');
+    if (Validation.retryLowerCase(value)) throw new Error('[ERROR] 소문자로 입력하셨습니다. 대문자로 입력해주세요.')
+    if (!Validation.retryOrNotValue(value)) throw new Error('[ERROR] R 또는 Q를 입력해주세요.');
   }
 
   static nextStepLowerCase(value) {
