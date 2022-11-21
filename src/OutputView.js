750ba1a62846caef797dfd1811f9c808b9dbda62
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2022-11-22 03:03:00 +0900

    fix(OutputView) : 분리후 주석제거

--- src/OutputView.js
+++ src/OutputView.js
@@ -3,29 +3,7 @@ const Console = MissionUtils.Console;
 const {GAME_MESSAGE, RESULT,DIRECTION,BRIDGE, JOINER} = require("./util/Constant");
 
 const OutputView = {
-
-  // upper: [],
-
-  // downer: [],
-
-  // addValue(upperValue, downerValue) {
-  //   this.upper.push(upperValue);
-  //   this.downer.push(downerValue);
-  // },
-
-
-  // pushResult(dir, success) {
-  //   if (success) {
-  //     if (dir === DIRECTION.UP) this.addValue(BRIDGE.CAN_MOVE, BRIDGE.BLANK);
-  //     if (dir === DIRECTION.DOWN) this.addValue(BRIDGE.BLANK, BRIDGE.CAN_MOVE);
-  //   }
-  //   else {
-  //     if (dir === DIRECTION.UP) this.addValue(BRIDGE.CANNOT_MOVE, BRIDGE.BLANK);
-  //     else this.addValue(BRIDGE.BLANK, BRIDGE.CANNOT_MOVE);
-  //   }
-  // },
-
-
+  
   printMap(bridgeGame) {
     const {upperString, downerString} = bridgeGame.gameResult();
     Console.print(upperString);
