ee5845f2db3e83aed1f71dda20b6b3e4fa4f1a49
Steadystudy <sgmin206@naver.com>
2022-11-21 17:23:44 +0900

    feat: 다리 길이 입력받고 게임 만들기

--- src/InputView.js
+++ src/InputView.js
@@ -1,11 +1,28 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const BridgeMaker = require('./BridgeMaker');
+const BridgeGame = require('./BridgeGame');
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
+ * - 제공된 `InputView` 객체를 활용해 구현해야 한다.
+- `InputView`의 파일 경로는 변경할 수 있다.
+- `InputView`의 메서드의 인자는 변경할 수 있다.
+- 사용자 값 입력을 위해 필요한 메서드를 추가할 수 있다.
  */
+let game;
+
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    MissionUtils.Console.readLine('다리 길이 입력', (size) => {
+      const bridge = BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate);
+      game = new BridgeGame(bridge);
+      InputView.readMoving();
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
