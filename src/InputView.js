62fa814c24c9aebeb4bf15e973e938993ed34ae9
Minju Park <project.mizzu@gmail.com>
2022-11-21 20:09:00 +0900

    Feat : 사용자로 부터 생성할 다리 길이 입력 받기
    - Console.readLine을 통해 사용자로 부터 생성할 다리 길이를 입력 받는다.

--- src/InputView.js
+++ src/InputView.js
@@ -1,11 +1,20 @@
+const { Console } = require('@woowacourse/mission-utils');
+const MESSAGE = require('./constants/BridgeGameMessage');
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
+  gameStart() {
+    Console.print(MESSAGE.PROCESS.GAME_START);
+  },
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine(MESSAGE.PROCESS.ENTER_BRIDGE_LENGTH, (length) => {
+      Console.print(`다리길이 ${length}`);
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
@@ -17,5 +26,7 @@ const InputView = {
    */
   readGameCommand() {},
 };
+
+Console.print(InputView.readBridgeSize());
 
 module.exports = InputView;
--- src/constants/BridgeGameMessage.js
+++ src/constants/BridgeGameMessage.js
@@ -1,20 +1,26 @@
-const { BRIDGE_INFO, GAME_STATUS, ERROR_TAG } = require('./Setting');
+const {
+  BRIDGE_INFO,
+  GAME_STATUS,
+  ERROR_TAG,
+  NEW_LINE,
+} = require('./BridgeGameSetting');
 
-const GAME_STATUS_MESSAGE = Object.freeze({
-  GAME_START: '다리 건너기 게임을 시작합니다.',
-  ENTER_BRIDGE_LENGTH: '다리의 길이를 입력해주세요.',
-  SELECT_UP_DOWN: `이동할 칸을 선택해주세요. (위: ${BRIDGE_INFO.SELECT_UP}, 아래: ${BRIDGE_INFO.SELECT_DOWN})`,
-  SELECT_RESTART_OR_QUIT: `게임을 다시 시도할지 여부를 입력해주세요. (재시도: ${GAME_STATUS.GAME_RESTART}, 종료: ${GAME_STATUS.GAME_QUIT})`,
-  GAME_RESULT: '최종 게임 결과',
-  SUCCESS_OR_FAILURE: `게임 성공 여부: `,
-  NUMBER_OF_ATTEMPTS: `총 시도한 횟수: `,
-  GAME_FINISH: '게임종료',
-});
+const MESSAGE = {
+  PROCESS: {
+    GAME_START: `다리 건너기 게임을 시작합니다.${NEW_LINE}`,
+    ENTER_BRIDGE_LENGTH: '다리의 길이를 입력해주세요.',
+    SELECT_UP_DOWN: `이동할 칸을 선택해주세요. (위: ${BRIDGE_INFO.SELECT_UP}, 아래: ${BRIDGE_INFO.SELECT_DOWN})`,
+    SELECT_RESTART_OR_QUIT: `게임을 다시 시도할지 여부를 입력해주세요. (재시도: ${GAME_STATUS.GAME_RESTART}, 종료: ${GAME_STATUS.GAME_QUIT})`,
+    GAME_RESULT: '최종 게임 결과',
+    SUCCESS_OR_FAILURE: `게임 성공 여부: `,
+    NUMBER_OF_ATTEMPTS: `총 시도한 횟수: `,
+    GAME_FINISH: '게임종료',
+  },
+  ERROR: {
+    INCORRECT_BRIDGE_LENGTH: `${ERROR_TAG} ${BRIDGE_INFO.MIN_LENGTH}이상 ${BRIDGE_INFO.MAX_LENGTH}이하의 숫자만 입력 가능합니다.`,
+    INCORRECT_BRIDGE_SELECT: `${ERROR_TAG} 알파벳 ${BRIDGE_INFO.SELECT_UP}또는 ${BRIDGE_INFO.SELECT_DOWN}만 입력 가능합니다.`,
+    INCORRECT_GAME_STATUS: `${ERROR_TAG} 알파벳 ${GAME_STATUS.GAME_RESTART}또는 ${GAME_STATUS.GAME_QUIT}만 입력 가능합니다.`,
+  },
+};
 
-const ERROR_MESSAGE = Object.freeze({
-  INCORRECT_BRIDGE_LENGTH: `${ERROR_TAG} ${BRIDGE_INFO.MIN_LENGTH}이상 ${BRIDGE_INFO.MAX_LENGTH}이하의 숫자만 입력 가능합니다.`,
-  INCORRECT_BRIDGE_SELECT: `${ERROR_TAG} 알파벳 ${BRIDGE_INFO.SELECT_UP}또는 ${BRIDGE_INFO.SELECT_DOWN}만 입력 가능합니다.`,
-  INCORRECT_GAME_STATUS: `${ERROR_TAG} 알파벳 ${GAME_STATUS.GAME_RESTART}또는 ${GAME_STATUS.GAME_QUIT}만 입력 가능합니다.`,
-});
-
-module.exports = { GAME_STATUS_MESSAGE, ERROR_MESSAGE };
+module.exports = MESSAGE;
