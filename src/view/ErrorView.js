c3f77ec710afec4cf471a35c02396729beef5953
Narcoker <jykim9868@gmail.com>
2022-11-21 17:25:43 +0900

    Comment: 설명 주석 추가

--- src/view/ErrorView.js
+++ src/view/ErrorView.js
@@ -1,6 +1,13 @@
 const MissionUtils = require('@woowacourse/mission-utils')
 
+/**
+ * 에러를 출력하는 역할을 한다.
+ */
 const ErrorView = {
+    /**
+     * 에러를 출력한다.
+     * @param {string} message 
+     */
     printError(message) {
         MissionUtils.Console.print(message);
     }
