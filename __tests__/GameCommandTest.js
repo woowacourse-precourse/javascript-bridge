6790b2653888a2ba21b4f3150dccbaa09884ebc0
Leejin-Yang <ejin1996@gmail.com>
2022-11-21 16:28:50 +0900

    test: 게임, 이동 커맨드 테스트 추가 작성

--- __tests__/GameCommandTest.js
+++ __tests__/GameCommandTest.js
@@ -9,4 +9,24 @@ describe('커맨드 클래스 테스트', () => {
       }).toThrow('[ERROR] 재시도 여부 입력값은 R 또는 Q여야 합니다.');
     },
   );
+
+  test.each([
+    ['R', true],
+    ['Q', false],
+  ])('입력값이 R인지 확인', (command, expected) => {
+    const movingCommand = new GameCommand(command);
+    const result = movingCommand.isRetry();
+
+    expect(result).toEqual(expected);
+  });
+
+  test.each([
+    ['R', false],
+    ['Q', true],
+  ])('입력값이 Q인지 확인', (command, expected) => {
+    const movingCommand = new GameCommand(command);
+    const result = movingCommand.isQuit();
+
+    expect(result).toEqual(expected);
+  });
 });
--- __tests__/MovingCommandTest.js
+++ __tests__/MovingCommandTest.js
@@ -8,6 +8,26 @@ describe('이동 커맨드 클래스 테스트', () => {
   });
 
   test.each([
+    ['U', true],
+    ['D', false],
+  ])('입력값이 U인지 확인', (command, expected) => {
+    const movingCommand = new MovingCommand(command);
+    const result = movingCommand.isUpside();
+
+    expect(result).toEqual(expected);
+  });
+
+  test.each([
+    ['U', false],
+    ['D', true],
+  ])('입력값이 D인지 확인', (command, expected) => {
+    const movingCommand = new MovingCommand(command);
+    const result = movingCommand.isDownside();
+
+    expect(result).toEqual(expected);
+  });
+
+  test.each([
     ['U', 'U', true],
     ['U', 'D', false],
   ])('다리를 건넜는지 확인', (command, current, expected) => {
