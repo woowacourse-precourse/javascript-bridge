6001f03dbefd720d891297af580eb72f2cdf73f9
Suyoung <jsy8492@gmail.com>
2022-11-21 14:22:38 +0900

    test(BridgeGameCommandValidatorTest): 테스트 케이스 수정 및 추가

--- __tests__/BridgeGameCommandValidatorTest.js
+++ __tests__/BridgeGameCommandValidatorTest.js
@@ -1,7 +1,7 @@
 const BridgeGameCommandValidator = require('../src/validators/BridgeGameCommandValidator');
 
 describe('사용자가 입력한 게임 명령의 유효성을 검사하는 객체 테스트', () => {
-  test.each([['DD'], ['UU'], ['A'], ['ABC']])(
+  test.each([['RR'], ['QQ'], ['A'], ['ABC'], ['']])(
     '유효하지 않은 게임 명령인 경우 예외 처리',
     input => {
       expect(input => BridgeGameCommandValidator.validate(input)).toThrow(
