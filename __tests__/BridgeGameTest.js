2c98632f7bec122fe129eda2ff3e215ad50b0188
chiman <rlaclghks123@naver.com>
2022-11-21 19:56:02 +0900

    test: MVC 패턴으로 수정하면서 controller관련 Test 작성
    1. size  테스트
    2. movemoment 테스트
    3. reTryAndQuit 테스트

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -1,55 +1,115 @@
+const BridgeController = require('../src/controller/BridgeController');
 const BridgeGame = require('../src/model/BridgeGame');
+const { Console } = require('@woowacourse/mission-utils');
+const OutputView = require('../src/OutputView');
 
-describe('Bridge Game Move Test', () => {
+describe('bridgeController Test', () => {
+  let bridgeController;
   let bridgeGame;
+
   beforeEach(() => {
+    bridgeController = new BridgeController();
     bridgeGame = new BridgeGame();
   });
 
-  test('사용자가 입력한 개수와, 다리의 총 개수가 일치하는지 Test', () => {
-    const bridge = ['U', 'D', 'U'];
-    bridgeGame.updateBridge(bridge);
+  test('잘못된 size 입력값이 입력될때 재요청 기능이 다시 실행되는지 Test', () => {
+    const spyFn = jest.spyOn(bridgeController, 'requestBridgeSize');
+    bridgeController.controlBridgeSize('a');
 
-    bridgeGame.selectMovemomentPosition('U');
-    bridgeGame.selectMovemomentPosition('D');
-    bridgeGame.selectMovemomentPosition('U');
-
-    const result = bridgeGame.isSuccess();
-    expect(result).toBeTruthy();
+    expect(spyFn).toHaveBeenCalledTimes(1);
   });
 
-  test('사용자가 선택한 값으로 잘 이동했는지 Test', () => {
-    const bridge = ['U', 'D', 'U', 'D'];
-
-    bridgeGame.updateBridge(bridge);
-
-    bridgeGame.selectMovemomentPosition('U');
-    bridgeGame.selectMovemomentPosition('D');
-    bridgeGame.selectMovemomentPosition('D');
-    bridgeGame.selectMovemomentPosition('U');
-    const result = bridgeGame.move();
-    expect(result).toEqual([
-      ['O', ' '],
-      [' ', 'O'],
-      [' ', 'X'],
-      ['X', ' '],
-    ]);
-  });
-
-  describe('retry test', () => {
-    test('재시작시 사용자가 입력한값이 초기화가 되는지 Test', () => {
+  describe('movemoment Test', () => {
+    test('사용자가 이동한 다리의 위치 Test', () => {
       bridgeGame.selectMovemomentPosition('U');
       bridgeGame.selectMovemomentPosition('D');
-      bridgeGame.retry();
       const result = bridgeGame.getUserBridge();
-      expect(result).toEqual([]);
+
+      expect(result).toEqual(['U', 'D']);
     });
 
-    test('재시작한 횟수 Test', () => {
-      bridgeGame.retry();
-      bridgeGame.retry();
-      const result = bridgeGame.getNumberOfAttempts();
-      expect(result).toBe(3);
+    test('사용자가 이동한 위치의 성공 여부 Test', () => {
+      bridgeGame.updateBridge(['U', 'D', 'D', 'U']);
+
+      bridgeGame.selectMovemomentPosition('U');
+      bridgeGame.selectMovemomentPosition('U');
+      bridgeGame.selectMovemomentPosition('D');
+      bridgeGame.selectMovemomentPosition('D');
+
+      const result = bridgeGame.move();
+
+      expect(result).toEqual([
+        ['O', ' '],
+        ['X', ' '],
+        [' ', 'O'],
+        [' ', 'X'],
+      ]);
+    });
+
+    test('이동에 대한 입력값이 잘못된 값으로 입력될때 재요청 기능이 다시 실행되는지 Test', () => {
+      const spyFn = jest.spyOn(bridgeController, 'requestBridgeMovemoment');
+      bridgeController.controlMovemoment('u');
+
+      expect(spyFn).toHaveBeenCalledTimes(1);
+    });
+
+    test('이동시 draw가 잘 되는지 Test', () => {
+      bridgeGame.updateBridge(['U', 'D', 'U']);
+
+      bridgeGame.selectMovemomentPosition('U');
+      const moveBridge = bridgeGame.move();
+      const drawBridge = bridgeController.draw(moveBridge);
+      expect(drawBridge).toEqual({ upBridge: 'O', downBridge: ' ' });
+    });
+
+    test('재시작 또는 종료 요청시 사용자가 선택한 값이 건널수 없는 다리인경우 재요청 기능이 작동하는지 Test', () => {
+      bridgeGame.updateBridge(['U', 'D', 'U']);
+
+      bridgeGame.selectMovemomentPosition('D');
+
+      const moveBridge = bridgeGame.move();
+      const drawBridge = bridgeController.draw(moveBridge);
+      const spyFn = jest.spyOn(bridgeController, 'requestGameCommand');
+      bridgeController.controlNextStep(drawBridge);
+
+      expect(spyFn).toHaveBeenCalledTimes(1);
+    });
+
+    test('사용자가 지정한 size만큼 이동했다면 종료 기능이 작동하는지 Test', () => {
+      bridgeGame.updateBridge(['U', 'D', 'U']);
+      bridgeGame.selectMovemomentPosition('U');
+      bridgeGame.selectMovemomentPosition('D');
+      bridgeGame.selectMovemomentPosition('U');
+
+      const moveBridge = bridgeGame.move();
+      const drawBridge = bridgeController.draw(moveBridge);
+
+      const spyFn = jest.spyOn(OutputView, 'printResult');
+      bridgeController.controlNextStep(drawBridge);
+      expect(spyFn).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('재시작 또는 종료 요청시 Test', () => {
+    test('재요청에 대한 값으로 R 입력시 이동할 칸 요청 기능이 작동 하는지', () => {
+      const spyFn = jest.spyOn(bridgeController, 'requestBridgeMovemoment');
+      bridgeController.controlGameCommand('R');
+
+      expect(spyFn).toHaveBeenCalledTimes(1);
+    });
+
+    test('재요청에 대한 값으로 Q 입력시 종료가 되는지', () => {
+      const spyFn = jest.spyOn(Console, 'close');
+      bridgeController.controlGameCommand('Q');
+
+      expect(spyFn).toHaveBeenCalledTimes(1);
+    });
+
+    test('재요청시 잘못된 입력값이 입력될때 재요청 기능이 다시 실행되는지 Test', () => {
+      const spyFn = jest.spyOn(bridgeController, 'requestGameCommand');
+      bridgeController.controlGameCommand('q');
+
+      expect(spyFn).toHaveBeenCalledTimes(1);
     });
   });
 });
