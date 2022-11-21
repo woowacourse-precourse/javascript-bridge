ab8a8641c1da476906f07297029ad71fd13e7ea1
h2002y <h2002y@naver.com>
2022-11-22 00:23:59 +0900

    fix: Add return true at each checkBridgeSize sub func

--- src/Validation.js
+++ src/Validation.js
@@ -19,20 +19,23 @@ const Validation = {
             Console.print("[ERROR] 다리 길이는 숫자여야 합니다.\n");
             return false;
         }
+        return true;
     },
 
     isBridgeSizeValue(size) {
-        if(Number(size) < 3 || 20 < Number(size)) {
+        if((Number(size) < 3) || (20 < Number(size))) {
             Console.print("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.\n");
             return false;
         }
+        return true;
     },
 
     isBridgeSizeInt(size) {
-        if(Number(size) % 1 !== 0) {
+        if((Number(size) % 1) !== 0) {
             Console.print("[ERROR] 다리 길이는 정수여야 합니다.\n");
             return false;
         }
+        return true;
     }
 }
 
