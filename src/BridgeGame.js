86d8062c7b6e62459ef1cb7ae7518322c13a01a9
HeoJiye <heoze1031@gmail.com>
2022-11-22 01:07:50 +0900

    feat: BridgeGame 생성/재시작  시 필드 초기화

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -4,6 +4,11 @@
 class BridgeGame {
   #bridge;
   #path;
+  
+  constructor() {
+    this.#bridge = [];
+    this.#path = [];
+  }
 
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -28,7 +33,7 @@ class BridgeGame {
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   retry() {
-
+    this.#path = [];
   }
 
   setBridge(bridge) {
