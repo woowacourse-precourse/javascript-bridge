9503a69745221ac2bcec91c84b1ffa1bee89c5d7
동동이 <drdragon777@naver.com>
2022-11-21 17:18:23 +0900

    feat - add createGame 메소드

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-const OutputView = require("./OutputView");
+const BridgeGame = require("./model/BridgeGame");
+const InputView = require("./view/InputView");
+const OutputView = require("./view/OutputView");
 
 class App {
+  game;
+
   play() {
     OutputView.printStart();
+    InputView.readBridgeSize.bind(this)(this.createGame);
+  }
+
+  createGame(input) {
+    this.game = new BridgeGame(input);
   }
 }
 
