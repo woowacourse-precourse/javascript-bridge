15e3a8a65ec46ef7d343ab48611bb1e33b4dba5b
Deemou <4106243@naver.com>
2022-11-21 09:47:42 +0900

    chore(constant): Bridge 관련 상수 업데이트

--- src/constant/Bridge.js
+++ src/constant/Bridge.js
@@ -2,14 +2,17 @@ const BRIDGE = Object.freeze({
   SIZE_MIN: 3,
   SIZE_MAX: 20,
   UP: 'U',
-  DOWN: 'D'
+  DOWN: 'D',
+  UPPER: 1,
+  LOWER: 0
 });
-const PASS_RESULT = Object.freeze({
-  CORRECT: 'O',
-  WRONG: 'X',
-  DELIMITER: ' | ',
-  START: '[ ',
-  END: ' ]'
+const MAP = Object.freeze({
+  PASS: ' O ',
+  NONPASS: ' X ',
+  DELIMITER: '|',
+  START: '[',
+  END: ']',
+  BLANK: '   '
 });
 const COMMAND = Object.freeze({
   RESTART: 'R',
@@ -21,15 +24,15 @@ const MESSAGE = Object.freeze({
   INPUT_SPACE: '\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
   INPUT_COMMAND: '\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
   END: '\n최종 게임 결과',
-  GAME_RESULT: '\n게임 성공 여부:',
+  GAME_RESULT: '\n게임 성공 여부: ',
   SUCCESS: '성공',
   FAIL: '실패',
-  TRY: '총 시도한 횟수:'
+  TRY: '총 시도한 횟수: '
 });
 
 module.exports = {
   BRIDGE,
-  PASS_RESULT,
+  MAP,
   COMMAND,
   MESSAGE
 };
