ca6a14b07d7262d21bc89d8ccbce982e73013f1e
안호진 <asdv94@naver.com>
2022-11-21 22:48:57 +0900

    feat(inputview): 입력 기능 구현

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,6 @@
+const { Console } = require('@woowacourse/mission-utils');
+const { GAME_MSG, COMMON } = require('./Constant');
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,17 +8,26 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize(cb) {
+    Console.readLine(GAME_MSG.enterLength + COMMON.newLine, cb);
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving(cb) {
+    Console.readLine(GAME_MSG.enterToMove + COMMON.newLine, (input) => {
+      Console.print();
+      cb(input);
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand(cb) {
+    Console.readLine(GAME_MSG.enterRetry + COMMON.newLine, cb);
+  },
 };
 
 module.exports = InputView;
