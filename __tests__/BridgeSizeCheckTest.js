c54f0c28f763c889c69e630f2017d1cfda4a87fb
dhkham <khamdong@gmail.com>
2022-11-21 20:28:10 +0900

    style(BridgeSizeCheckTest): format document with prettier

--- __tests__/BridgeSizeCheckTest.js
+++ __tests__/BridgeSizeCheckTest.js
@@ -5,7 +5,10 @@ describe("다리 사이즈 입력 테스트", () => {
     expect(() => new BridgeSizeCheck(input)).not.toThrow("[ERROR]");
   });
 
-  test.each([3.3, "a", 2, 21, -1])("다리 사이즈 비정상 입력 (소수, 문자, 3-20 이외 정수)", input => {
-    expect(() => new BridgeSizeCheck(input)).toThrow("[ERROR]");
-  });
+  test.each([3.3, "a", 2, 21, -1])(
+    "다리 사이즈 비정상 입력 (소수, 문자, 3-20 이외 정수)",
+    input => {
+      expect(() => new BridgeSizeCheck(input)).toThrow("[ERROR]");
+    },
+  );
 });
