33e5beea4e11883ab152cff61fbea0e167c8f636
wontae chang <cwt629@naver.com>
2022-11-21 18:09:44 +0900

    refactor: make multiple move instructions to be performed in one function in BridgeGameTest

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -1,11 +1,20 @@
 const BridgeGame = require("../src/BridgeGame");
 
+/**
+ * 여러 커맨드로 move시키는 함수
+ * @param {BridgeGame} GAME 브릿지 게임 객체
+ * @param {string[]} commands 커맨드 여러 개를 담은 배열
+ */
+const moveMultipleCommands = (GAME, commands) => {
+    for (let command of commands) {
+        GAME.move(command);
+    }
+}
+
 describe("move 테스트", () => {
     test("이동 반영 테스트", () => {
         const GAME = new BridgeGame(["U", "D", "D"]);
-        GAME.move("U");
-        GAME.move("D");
-        GAME.move("U");
+        moveMultipleCommands(GAME, ["U", "D", "U"]);
 
         expect(GAME.passed).toEqual(["U", "D", "U"]);
     })
@@ -19,17 +28,13 @@ describe("move 테스트", () => {
 
     test("crossedAll 참 테스트", () => {
         const GAME = new BridgeGame(["U", "D", "D"]);
-        GAME.move("U");
-        GAME.move("D");
-        GAME.move("D");
+        moveMultipleCommands(GAME, ["U", "D", "D"]);
         expect(GAME.crossedAll()).toBe(true);
     })
 
     test("crossedAll 거짓 테스트", () => {
         const GAME = new BridgeGame(["U", "D", "D"]);
-        GAME.move("U");
-        GAME.move("D");
-        GAME.move("U");
+        moveMultipleCommands(GAME, ["U", "D", "U"]);
         expect(GAME.crossedAll()).toBe(false);
     })
 })
@@ -48,18 +53,14 @@ describe("retry 테스트", () => {
     })
 
     test("경로 불변 테스트", () => {
-        GAME.move("U");
-        GAME.move("D");
-        GAME.move("D");
+        moveMultipleCommands(GAME, ["U", "D", "D"]);
         expect(GAME.crossedAll()).toBe(true);
     })
 })
 
 describe("문자열 표시 테스트", () => {
     const GAME = new BridgeGame(["U", "D", "D"]);
-    GAME.move("U");
-    GAME.move("D");
-    GAME.move("U");
+    moveMultipleCommands(GAME, ["U", "D", "U"]);
 
     test("윗줄 테스트", () => {
         const RESULT = GAME.getComparisonResultArray("U");
