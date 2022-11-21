5884c8da5989a7000bd7d71964b48ddc27012395
chsua <csj1919@naver.com>
2022-11-21 15:21:41 +0900

    feat: R/Q 입력받는 함수(InputView.js)

--- src/InputView.js
+++ src/InputView.js
@@ -34,9 +34,19 @@ const MissionUtils = require("@woowacourse/mission-utils");
   },
 
   /**
-   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는 함수 + 예최처리
    */
-  readGameCommand() {},
+  readGameCommand(callback) {
+    MissionUtils.Console.readLine("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)", (input) => {
+      try {
+        if ((input != "R")&&(input != "Q")) throw new Error("[ERROR] R/Q으로 입력 바랍니다.")
+        callback(input)
+      } catch (err) {
+        MissionUtils.Console.print(err.message) ;
+        InputView.readGameCommand(callback) ;
+      }
+    }) 
+  },
 };
 
 module.exports = InputView;
