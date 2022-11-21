86ab652cbec5feb065c8bd0f53eed033eea02261
freemoon99 <soloist132@gmail.com>
2022-11-21 19:47:08 +0900

    feat: 게임을 다시 시작할지 입력받는 기능

--- src/InputView.js
+++ src/InputView.js
@@ -18,7 +18,7 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {
+  readMoving(bridegame) {
     MissionUtils.Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)', (userInput) => {
       bridegame.move(userInput);
     });
@@ -27,7 +27,11 @@ const InputView = {
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+    MissionUtils.Console.readLine('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)', (command) => {
+      bridegame.restartInput(command);
+    });
+  },
 };
 
 module.exports = InputView;
