ce15213055911346e3895e0a978e42e3e8d8ff50
nalsae <nalsae0207@naver.com>
2022-11-21 10:56:01 +0900

    feat: Implement a method to return moving result
    사용자의 입력 값에 따라 움직인 칸의 결과를 반환하는 메서드 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -2,19 +2,28 @@
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  static bridge;
+  static movingCount = 0;
+
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  static move(moving) {
+    const movingResult = this.bridge[this.movingCount] === moving;
+
+    this.movingCount += 1;
+
+    return movingResult;
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  retry() {}
+  static retry() {}
 }
 
 module.exports = BridgeGame;
