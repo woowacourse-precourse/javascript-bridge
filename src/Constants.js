216bd6c3dcf32bd3a6b72e8fb7e4a4eea45f4bc2
Minhye Kang <alsgp9898@naver.com>
2022-11-21 16:49:12 +0900

    feat: 이동할 칸 유효성 검사 코드 추가

--- src/Constants.js
+++ src/Constants.js
@@ -1,6 +1,7 @@
 const ERROR = {
   NOT_NUMBER: '[ERROR] 숫자를 입력해야 합니다.',
   NOT_THREE_TO_TWENTY: '[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.',
+  NOT_U_OR_D: '[ERROR] 이동할 칸은 U나 D만 입력할 수 있습니다.',
 }
 
 module.exports = {ERROR};
--- src/Validation.js
+++ src/Validation.js
@@ -1,10 +1,15 @@
-const {ERROR} = require('./Constants');
+const { ERROR } = require('./Constants');
 
 const Validation = {
   checkBridgeSize(size) {
     if (/[^\d]+/g.test(size)) throw new Error(ERROR.NOT_NUMBER);
     if (size < 3 || size > 20) throw new Error(ERROR.NOT_THREE_TO_TWENTY);
   },
+
+  checkMovingValue(direction) {
+    if (/[^UD]/g.test(direction) || direction.length !== 1) 
+      throw new Error(ERROR.NOT_U_OR_D);
+  }
 };
 
 module.exports = Validation;
