c09d90e8c1c3b6b166e0b15ec289f9bc2497d2da
DongWooKim97 <nanaukim@naver.com>
2022-11-22 01:31:52 +0900

    style: 에러 메시지 출력 방식 수정(하드코딩 -> ERROR_MESSAGES객체)

--- src/Validate.js
+++ src/Validate.js
@@ -1,25 +1,26 @@
+const { ERROR_MESSAGES } = require("./Constants");
 const Validate = {
   checkSizeInputType(inputNumber) {
     if (Number.isNaN(Number(inputNumber))) {
-      throw new Error("[ERROR] 다리 길이는 숫자로 입력해야 합니다.");
+      throw new Error(ERROR_MESSAGES.SIZE_INPUT_TYPE);
     }
   },
 
   checkSizeInputRange(inputNumber) {
     if (Number(inputNumber) < 3 || Number(inputNumber) > 20) {
-      throw new Error("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+      throw new Error(ERROR_MESSAGES.SIZE_INPUT_RANGE);
     }
   },
 
   checkDirectionInputType(inputDirection) {
     if (inputDirection !== "U" && inputDirection !== "D") {
-      throw new Error("[ERROR] 이동 경로는 U 아님 D로 입력해야 합니다.");
+      throw new Error(ERROR_MESSAGES.DIRECTION_INPUT_TYPE);
     }
   },
 
   checkRetryCommandType(inputOpinion) {
     if (inputOpinion !== "R" && inputOpinion !== "Q") {
-      throw new Error("[ERROR] 재시작은 R, 게임 종료는 Q를 눌러주세요.");
+      throw new Error(ERROR_MESSAGES.RETRY_COMMAND_TYPE);
     }
   },
 };
