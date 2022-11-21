11286d3329507292f2b158e8153a728683b4e53a
GunHoi <lgh509@kyonggi.ac.kr>
2022-11-21 18:11:54 +0900

    fix -4 : O,X 받아오는 함수 비동기처리

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -17,16 +17,16 @@ const OutputView = require("./OutputView.js");
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {
+  async move() {
     let upMap = "", downMap ="";
     for(let moveCount=0;moveCount<this.#bridgeSize;moveCount++){
       const moving = RecallUntilCorrect.recallReadMoving(true);
       if(moving===this.#bridgeMap[moveCount]){  //움직이려고 하는 곳이 이동할 수 있는 곳이면 O표시 후 맵을 보여주고 계속진행
-        const Map = this.isUpOrDown(moving, true);
+        const Map = await this.isUpOrDown(moving, true);
         upMap += Map[0], downMap += Map[1];
         OutputView.printMap(upMap.slice(0,-1), downMap.slice(0,-1));
       }else{  //움직이려고 하는 곳이 이동할 수 없는 곳이면 X표시 후 맵을 보여주고 재시작 여부를 묻는다.
-        const Map = this.isUpOrDown(moving, false);
+        const Map = await this.isUpOrDown(moving, false);
         upMap += Map[0], downMap += Map[1];
         OutputView.printMap(upMap.slice(0,-1), downMap.slice(0,-1));
         this.isRetryOrQuit();
