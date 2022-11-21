b3b1a143ae7eba5e07cc89349af27924a7fe142c
marugy <gyulife7301@naver.com>
2022-11-22 08:12:57 +0900

    feat(OutputView): printStart() 게임 시작을 출력하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const InputView = require("../src/InputView");
+const OutputView = require("../src/OutputView");
 class App {
   play() {
+    OutputView.printStart();
     MissionUtils.Console.print(InputView.readBridgeSize());
   }
 }
