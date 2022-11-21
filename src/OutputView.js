58b2ac9a3fca388b843f2d411e12ec768b3f2e96
JaeyeonHong <39540473+HongJaeyeon@users.noreply.github.com>
2022-11-21 17:50:20 +0900

    feat: OutputView 맵 출력, 결과 출력 기능 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,20 +1,22 @@
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
-const OutputView = {
-  /**
-   * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-   * <p>
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  printMap() {},
+const MissionUtils = require("@woowacourse/mission-utils");
+const { OutputMessage } = require("./constant/Constants");
 
-  /**
-   * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-   * <p>
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  printResult() {},
+const OutputView = {
+  printStartMessage(){
+    MissionUtils.Console.print(OutputMessage.START);
+  },
+
+  printMap(upBridge, downBridge) {
+    MissionUtils.Console.print(OutputMessage.FORMAT(upBridge));
+    MissionUtils.Console.print(OutputMessage.FORMAT(downBridge));
+  },
+
+  printResult(upBridge, downBridge, isSuccess, tryNumber) {
+    MissionUtils.Console.print(OutputMessage.RESULT_TITLE);
+    this.printMap(upBridge, downBridge);
+    MissionUtils.Console.print(OutputMessage.RESULT(isSuccess));
+    MissionUtils.Console.print(OutputMessage.TRY_NUMBER(tryNumber));
+  },
 };
 
 module.exports = OutputView;
