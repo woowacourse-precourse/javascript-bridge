ecb3b7ef615580d82f0113ea11fe0f8eb0c8c690
pakxe <pigkill40@naver.com>
2022-11-21 19:19:17 +0900

    refactor(App): view, domain 객체를 컨트롤러에 전달

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 const BridgeGame = require('./controller/BridgeGame');
+const View = require('./view/ViewInterface');
+const Domain = require('./domain/DomainInterface');
 
 class App {
+  constructor() {
+    this.view = View;
+    this.domain = Domain;
+  }
+
   play() {
-    const bridgeGame = new BridgeGame();
+    const bridgeGame = new BridgeGame(this.view, this.domain);
     bridgeGame.start();
   }
 }
