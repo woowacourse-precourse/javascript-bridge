e802d60fd0b5a2cfd92ccc327fdee139a8517c87
jhyns <tockiib@gmail.com>
2022-11-22 01:07:08 +0900

    feat: Add makeBridge for BridgeMaker
    BridgeMaker의 makeBridge 추가

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -10,12 +10,9 @@ const BridgeMaker = {
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
   makeBridge(size, generateRandomNumber) {
-    const bridge = Array.from({ length: size }, () => {
-      const randomNumber = Number(generateRandomNumber());
-
-      Validate.bridgeGenerateNumber(randomNumber);
-      return randomNumber === 1 ? 'U' : 'D';
-    });
+    const bridge = Array.from({ length: size }, () =>
+      generateRandomNumber() === 1 ? 'U' : 'D'
+    );
 
     return bridge;
   },
