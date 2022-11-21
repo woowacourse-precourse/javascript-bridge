bcc9ebbfe2835ded7819dbd04e662c99f60d2a2f
devMingu <rooster100@naver.com>
2022-11-21 12:14:55 +0900

    feat: play기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const { Console } = require("@woowacourse/mission-utils");
+const InputView = require("./InputView");
+const Message = require("./Message");
+
 class App {
-  play() {}
+  play() {
+    Console.print(`${Message.START_MESSAGE.START_TEXT}\n`);1
+    InputView.readBridgeSize();
+  }
 }
+
+const game = new App();
+game.play();
 
 module.exports = App;
