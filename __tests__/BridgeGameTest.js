f42e2e02862e9d094815b2f2536fdbdbf92df874
Deemou <4106243@naver.com>
2022-11-21 15:52:20 +0900

    test(BridgeGame): isClear() 테스트 케이스 추가

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -28,7 +28,7 @@ describe('BridgeGame 클래스 테스트', () => {
     expect(map).toEqual(expectedOutput);
   });
 
-  test('다리를 무사히 건넜는지 확인한다', () => {
+  test('다리를 무사히 건넜는지 확인한다.', () => {
     mockRandoms([1, 0, 0]);
     //bridge = ['U','D','D']
 
@@ -41,5 +41,50 @@ describe('BridgeGame 클래스 테스트', () => {
       const output = game.isPass();
       expect(output).toEqual(expectedOutput[index]);
     });
+  });
+
+  test('다리를 전부 건넜는지 확인한다. case1', () => {
+    mockRandoms([1, 0, 0]);
+    //bridge = ['U','D','D']
+
+    const inputs = [BRIDGE.UP, BRIDGE.DOWN, BRIDGE.DOWN];
+    const expectedOutput = true;
+    const game = new BridgeGame(3);
+
+    inputs.forEach((input) => {
+      game.move(input);
+    });
+    const output = game.isClear();
+    expect(output).toEqual(expectedOutput);
+  });
+
+  test('다리를 전부 건넜는지 확인한다. case2', () => {
+    mockRandoms([1, 0, 0]);
+    //bridge = ['U','D','D']
+
+    const inputs = [BRIDGE.UP, BRIDGE.DOWN, BRIDGE.UP];
+    const expectedOutput = false;
+    const game = new BridgeGame(3);
+
+    inputs.forEach((input) => {
+      game.move(input);
+    });
+    const output = game.isClear();
+    expect(output).toEqual(expectedOutput);
+  });
+
+  test('다리를 전부 건넜는지 확인한다. case3', () => {
+    mockRandoms([1, 0, 0]);
+    //bridge = ['U','D','D']
+
+    const inputs = [BRIDGE.UP, BRIDGE.DOWN];
+    const expectedOutput = false;
+    const game = new BridgeGame(3);
+
+    inputs.forEach((input) => {
+      game.move(input);
+    });
+    const output = game.isClear();
+    expect(output).toEqual(expectedOutput);
   });
 });
