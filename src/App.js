ced53db4071603a8f1a60567b485101530372d88
marugy <gyulife7301@naver.com>
2022-11-22 08:32:11 +0900

    feat(App): validateSize() size 오류를 검토하고 잘못입력한 경우 다시 입력받는 기능 추가

--- src/App.js
+++ src/App.js
@@ -4,7 +4,28 @@ const OutputView = require("../src/OutputView");
 class App {
   play() {
     OutputView.printStart();
-    MissionUtils.Console.print(InputView.readBridgeSize());
+    let size = InputView.readBridgeSize();
+    size = this.validateSize(size);
+
+    MissionUtils.Console.print("pass");
+  }
+
+  validateSize(size) {
+    try {
+      if (isNaN(Number(size))) {
+        MissionUtils.Console.print("[ERROR] 숫자를 입력해야합니다.");
+        throw new Error("[ERROR] 숫자를 입력해야합니다.");
+      }
+      if (size < 3 && 20 < size) {
+        MissionUtils.Console.print(
+          "[ERROR] 3이상 20이하의 size를 입력해여합니다."
+        );
+        throw new Error("[ERROR] 3이상 20이하의 size를 입력해여합니다.");
+      }
+      return size;
+    } catch {
+      return InputView.readBridgeSize();
+    }
   }
 }
 
