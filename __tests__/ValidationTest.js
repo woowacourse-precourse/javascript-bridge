4e06e5547279f32e5469b875cd17f7c91e562ca8
dragonfruitlemonade <tpghks9245@gmail.com>
2022-11-21 20:55:14 +0900

    feat(ValidationTest): nextStep 입력값이 빈값인지 검사, 값이 여러개일 경우 검사 함수 추가

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,53 +1,59 @@
 const Validation = require("../src/Validation");
 
 describe("Validation 클래스 테스트", () => {
-  test("다리 길이 입력 값이 비었을 경우", () => {
+  test("bridgeLength 입력 값이 비었을 경우", () => {
     const input = "";
 
     expect(() => Validation.bridgeLength(input)).toThrow('[ERROR] 값을 입력해주세요.');
   });
 
-  test("다리 길이 입력 값이 숫자가 아닐경우", () => {
+  test("bridgeLength 입력 값이 숫자가 아닐경우", () => {
     const input = "가";
 
     expect(() => Validation.bridgeLength(input)).toThrow('[ERROR] 숫자를 입력해주세요.');
   });
 
-  test("다리 길이 입력 값이 비었을 경우", () => {
+  test("bridgeLength 입력 값이 숫자가 아닐경우", () => {
     const input = "number";
 
     expect(() => Validation.bridgeLength(input)).toThrow('[ERROR] 숫자를 입력해주세요.');
   });
 
-  test("다리 길이 입력 값이 3~20 사이가 아닐 경우", () => {
+  test("bridgeLength 입력 값이 3~20 사이가 아닐 경우", () => {
     const input = "2";
 
     expect(() => Validation.bridgeLength(input)).toThrow('[ERROR] 3과 20사이의 값을 입력해주세요.');
   });
 
-  test("다리 길이 입력 값이 3~20 사이가 아닐 경우", () => {
+  test("bridgeLength 입력 값이 3~20 사이가 아닐 경우", () => {
     const input = "21";
 
     expect(() => Validation.bridgeLength(input)).toThrow('[ERROR] 3과 20사이의 값을 입력해주세요.');
   });
 
-//   test("다리 길이 입력 값이 비었을 경우", () => {
-//     const input = "";
+  test("bridge 이동 값이 비었을 경우", () => {
+    const input = "";
 
-//     expect(() => Validation.bridgeLength(input)).toThrow('[ERROR] 값을 입력해주세요.');
-//   });
+    expect(() => Validation.nextStep(input)).toThrow('[ERROR] 값을 입력해주세요.');
+  });
   
-//   test("다리 길이 입력 값이 비었을 경우", () => {
-//     const input = "";
+  test("bridge 이동 값이 여러개일 경우", () => {
+    const input = "UU";
 
-//     expect(() => Validation.bridgeLength(input)).toThrow('[ERROR] 값을 입력해주세요.');
-//   });
+    expect(() => Validation.nextStep(input)).toThrow('[ERROR] 1개의 값을 입력해주세요.');
+  });
 
-//   test("다리 길이 입력 값이 비었을 경우", () => {
-//     const input = "";
+  test("bridge 이동 값이 여러개일 경우", () => {
+    const input = "DD";
 
-//     expect(() => Validation.bridgeLength(input)).toThrow('[ERROR] 값을 입력해주세요.');
-//   });
+    expect(() => Validation.nextStep(input)).toThrow('[ERROR] 1개의 값을 입력해주세요.');
+  });
+
+  test("bridge 이동 값이 여러개일 경우", () => {
+    const input = "UD";
+
+    expect(() => Validation.nextStep(input)).toThrow('[ERROR] 1개의 값을 입력해주세요.');
+  });
 
 //   test("다리 길이 입력 값이 비었을 경우", () => {
 //     const input = "";
