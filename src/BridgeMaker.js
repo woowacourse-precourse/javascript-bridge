87e8607d3e79e450f056420b3954aa34336ca837
nuheajiohc <jaehun341012@gmail.com>
2022-11-21 14:55:15 +0900

    chore: 메인 브랜치 최신 커밋에 따른 코드 수정

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -11,7 +11,7 @@ const BridgeMaker = {
   makeBridge(size, generateRandomNumber) {
     const safeBridgeList = [];
     for (let count = 0; count < size; count++) {
-      const direction = Number(generateRandomNumber());
+      const direction = generateRandomNumber();
       direction ? safeBridgeList.push(COMMAND.UP) : safeBridgeList.push(COMMAND.DOWN);
     }
     return safeBridgeList;
