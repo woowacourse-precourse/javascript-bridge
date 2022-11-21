ace4603dd90a101407fba731bbb49073fc37e1ad
Hannah Ahn <ahnlook90@gmail.com>
2022-11-21 09:03:39 +0900

    test: add bridge maker tester

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,16 +1,4 @@
-const { Console } = require("@woowacourse/mission-utils");
-// 제공된 BridgeMaker 객체를 활용해 구현해야 한다.
-// BridgeMaker에 프로퍼티를 추가할 수 없다.
-// BridgeMaker의 파일 경로는 변경할 수 없다.
-// BridgeMaker의 메서드의 시그니처(인자, 이름)와 반환 타입은 변경할 수 없다.
-
-/**
- * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
- */
-
-// 다리 칸을 생성하기 위한 Random 값은 아래와 같이 추출한다.
-// const number = generateRandomNumber();
-// BridgeRandomNumberGenerator
+const { Console } = require('@woowacourse/mission-utils')
 
 const BridgeMaker = {
   /**
@@ -21,27 +9,28 @@ const BridgeMaker = {
   makeBridge(size, generateRandomNumber) {
     let numbers = []
 
-    while (size <= 0) {
+    while (size > 0) {
       const number = generateRandomNumber()
       numbers.push(number)
+      size--
     }
 
-    const bridge = formatBridge(numbers)
+    const bridge = this.formatBridge(numbers)
     return bridge
   },
 
   formatBridge(numbers) {
-    let bridge = []
-
-    if (numbers) {
-      numbers.push('U')
-    }
-    if (!numbers) {
-      numbers.push('D')
-    }
+    let bridge = numbers.map((number) => {
+      if (number) {
+        return 'U'
+      }
+      if (!number) {
+        return 'D'
+      }
+    })
 
     return bridge
-  }
-};
+  },
+}
 
-module.exports = BridgeMaker;
+module.exports = BridgeMaker
