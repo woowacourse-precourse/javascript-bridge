d571739dc678eb0f63904562f76187c0b1109c47
yong <tkddbs1644@gmail.com>
2022-11-22 01:31:14 +0900

    test(Progress): 변수 이름, 반환 시그니처 변경

--- __tests__/ProgressTest.js
+++ __tests__/ProgressTest.js
@@ -1,21 +1,27 @@
 const Progress = require("../src/Progress");
 
 describe("Progress 테스트", () => {
+  test("command가 U나 D가 아니라면 에러 발생", () => {
+    const progress = new Progress();
+    expect(() => progress.success("Z")).toThrow();
+    expect(() => progress.fail("Z")).toThrow();
+  });
+
   test("첫번째 다리 건너기 성공", () => {
     const progress = new Progress();
     progress.success("U");
-    const { firstBridge, secondBridge } = progress.desc();
-    expect(firstBridge).toEqual("[ O ]");
-    expect(secondBridge).toEqual("[   ]");
+    const [upBridges, downBridges] = progress.desc();
+    expect(upBridges).toEqual(["O"]);
+    expect(downBridges).toEqual([" "]);
   });
 
   test("두번째 다리 건너기 성공", () => {
     const progress = new Progress();
     progress.success("U");
     progress.success("D");
-    const { firstBridge, secondBridge } = progress.desc();
-    expect(firstBridge).toEqual("[ O |   ]");
-    expect(secondBridge).toEqual("[   | O ]");
+    const [upBridges, downBridges] = progress.desc();
+    expect(upBridges).toEqual(["O", " "]);
+    expect(downBridges).toEqual([" ", "O"]);
   });
 
   test("세번째 다리 건너기 실패", () => {
@@ -23,8 +29,8 @@ describe("Progress 테스트", () => {
     progress.success("U");
     progress.success("D");
     progress.fail("U");
-    const { firstBridge, secondBridge } = progress.desc();
-    expect(firstBridge).toEqual("[ O |   | X ]");
-    expect(secondBridge).toEqual("[   | O |   ]");
+    const [upBridges, downBridges] = progress.desc();
+    expect(upBridges).toEqual(["O", " ", "X"]);
+    expect(downBridges).toEqual([" ", "O", " "]);
   });
 });
