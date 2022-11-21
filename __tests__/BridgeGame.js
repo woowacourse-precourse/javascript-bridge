92dcbdceeb2623a6b67612722aa18467f3adfd8c
Mcdonalds Trump <whdmschd2@gmail.com>
2022-11-21 22:01:20 +0900

    test: 다리를 끝까지 올바르게 건넜는지 테스트

--- __tests__/BridgeGame.js
+++ __tests__/BridgeGame.js
@@ -1,7 +1,7 @@
 const BridgeGame = require("../src/BridgeGame");
 
-describe("현재까지 건넌 다리 지도 테스트", () => {
-  test("사용자가 올바르게 이동했을 때", () => {
+describe("BridgeGame class 테스트", () => {
+  test("사용자가 올바르게 이동했을 때 현재까지 건넌 다리 지도", () => {
     const playerChoiceMovingCommands = ["U", "D", "D", "D"];
     const upsideBridgeMap = ["O", " ", " ", " "];
     const downSideBridgeMap = [" ", "O", "O", "O"];
@@ -21,7 +21,7 @@ describe("현재까지 건넌 다리 지도 테스트", () => {
     );
   });
 
-  test("사용자가 이동할 수 없는 칸을 선택했을 때", () => {
+  test("사용자가 이동할 수 없는 칸을 선택했을 때 현재까지 건넌 다리 지도", () => {
     const playerChoiceMovingCommands = ["U", "D", "U"];
     const upsideBridgeMap = ["O", " ", "X"];
     const downSideBridgeMap = [" ", "O", " "];
@@ -39,5 +39,17 @@ describe("현재까지 건넌 다리 지도 테스트", () => {
     expect(bridgeGame.getCurrentBridgeMap().downsideBridgeMap).toEqual(
       downSideBridgeMap
     );
+  });
+
+  test("다리를 끝까지 올바르게 건넜는지 테스트", () => {
+    const bridgeShape = ["U", "D", "D", "D", "U"];
+    const bridgeGame = new BridgeGame();
+    bridgeGame.setBridgeShape(bridgeShape);
+
+    bridgeShape.forEach((playerChoiceMovingCommand) => {
+      bridgeGame.move(playerChoiceMovingCommand);
+    });
+
+    expect(bridgeGame.isGameSuccess()).toBeTruthy();
   });
 });
