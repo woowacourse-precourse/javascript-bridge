3845ee6c6f5b6d9c24a58490317320c20f8d8418
kitez <duswl339@ajou.ac.kr>
2022-11-21 19:53:29 +0900

    feat: 게임 진행하면서 현재까지 입력된 값을 기준으로 다리 건넌 결과 출력하는 함수 작성
    + printMap() : printMapLine()함수를 호출하여 다리 위아래를 한번에 출력
    + printMapLine() : 다리 한 줄을 출력

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,4 +1,7 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const Const = require('../src/constant/constant');
+const message = require('../src/constant/message');
+
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -9,7 +12,22 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {},
+  printMap(bridge, current, input) {
+    MissionUtils.Console.print('['+this.printMapLine(bridge, current, input, Const.MOVE[0])+']');
+    MissionUtils.Console.print('['+this.printMapLine(bridge, current, input, Const.MOVE[1])+']');
+  },
+
+  printMapLine(bridge, current, input, flag){
+    let result = '';
+    for(let i = 0 ; i < current -1 ;i++){
+      if(bridge[i] === flag) {result += ' O |';}
+      else{result += '   |'}
+    }
+    if(input === flag && bridge[current-1] === flag) {result += ' O ';}
+    else if(input === flag && bridge[current-1] !== flag) {result += ' X ';}
+    else{result += '   '}
+    return result.length === 0 ? '   ' : result;
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
