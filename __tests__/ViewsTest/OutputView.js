2eb31a077d4259ffbc9013753856689f98b8ae8e
KES <aprilsmaple@gmail.com>
2022-11-22 02:00:15 +0900

    test: View 폴더 분리 및 OutputView 시작문구 테스트 코드 작성

---
+++ __tests__/ViewsTest/OutputView.js
@@ -0,0 +1,18 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const OutputView = require('../../src/views/OutputView');
+const MESSAGES = require('../../src/constants/Messages');
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
+
+const logSpy = getLogSpy();
+
+describe('출력 테스트', () => {
+  test('시작 문구 출력', () => {
+    OutputView.printStartMessage();
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(MESSAGES.start));
+  });
+});
