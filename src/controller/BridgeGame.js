fef5637bc33f80a04add9b0592e37a2973648cb4
dev-redo <mis05041@naver.com>
2022-11-21 16:26:34 +0900

    refactor(BridgeGame, BridgeMap): 클래스 내부에서만 사용하는 메서드를 프라이빗 메서드화

--- src/controller/BridgeGame.js
+++ src/controller/BridgeGame.js
@@ -9,83 +9,73 @@ const BridgeGame = class extends GameCtrl {
 
   start() {
     this.view.printGameStart();
-    this.gameProcess();
+    this.#gameProcess();
   }
 
-  gameProcess() {
+  #gameProcess() {
     this.view.readBridgeSize((bridgeSize) => {
       try {
         bridgeSize = parseInt(bridgeSize);
         this.model.validateBridgeSize(bridgeSize);
         this.model.createBridge(bridgeSize);
-        this.getUserCommand();
+        this.#getUserCommand();
       } catch (error) {
         this.view.printErrorMessage(error.message);
-        this.gameProcess(bridgeSize);
+        this.#gameProcess(bridgeSize);
       }
     });
   }
 
-  getUserCommand() {
+  #getUserCommand() {
     this.view.readMoving((command) => {
       try {
         this.model.validateBridgeCommand(command);
-        this.move(command);
+        this.#move(command);
       } catch (error) {
         this.view.printErrorMessage(error.message);
-        this.getUserCommand();
+        this.#getUserCommand();
       }
     });
   }
 
-  // 1. 입력받기 ✅
-  // 2. 이번 command를 this.model.commandList에 push ✅
-  // 3. map과 bridge를 비교 후 bridgeMap + 현재 칸 통과 여부 얻기 -> BridgeMap 클래스 ✅
-  // 4. map 출력하기 = this.view.printMap(bridgeMap) ✅
-
-  // 5. 현재 칸 통과 실패시 -> 게임을 재시작하거나 종료 여부 묻기
-  //    -> askToReplayGame 실행 ✅
-  move(command) {
+  #move(command) {
     this.model.addCommandToList = command;
     const { isPassed, bridgeMap } = this.model.getMovedResult;
     this.view.printMap(bridgeMap);
 
     const isGameSuccess = this.model.getIsGameSuccess(bridgeMap);
 
-    if (!isPassed) return this.askToReplayGame({ bridgeMap, isGameSuccess });
-    if (isGameSuccess) return this.end({ bridgeMap, isGameSuccess });
+    if (!isPassed) return this.#askToReplayGame({ bridgeMap, isGameSuccess });
+    if (isGameSuccess) return this.#end({ bridgeMap, isGameSuccess });
 
-    return this.getUserCommand();
+    return this.#getUserCommand();
   }
 
-  // 게임을 재시작 할 것인지 여부를 묻기
-  askToReplayGame({ bridgeMap, isGameSuccess }) {
+  #askToReplayGame({ bridgeMap, isGameSuccess }) {
     this.view.readGameCommand((replayCommand) => {
       try {
         this.model.validateBridgeReplayCommand(replayCommand);
-        this.quitOrRetryByCommand({ replayCommand, bridgeMap, isGameSuccess });
+        this.#quitOrRetryByCommand({ replayCommand, bridgeMap, isGameSuccess });
       } catch (error) {
         this.view.printErrorMessage(error.message);
-        this.askToReplayGame({ bridgeMap, isGameSuccess });
+        this.#askToReplayGame({ bridgeMap, isGameSuccess });
       }
     });
   }
 
-  quitOrRetryByCommand({ replayCommand, bridgeMap, isGameSuccess }) {
-    if (replayCommand === 'R') return this.retry();
-    return this.end({ bridgeMap, isGameSuccess });
+  #quitOrRetryByCommand({ replayCommand, bridgeMap, isGameSuccess }) {
+    if (replayCommand === 'R') return this.#retry();
+    return this.#end({ bridgeMap, isGameSuccess });
   }
 
-  // 게임 종료 = Q
-  end({ bridgeMap, isGameSuccess }) {
+  #end({ bridgeMap, isGameSuccess }) {
     const result = this.model.makeBridgeGameResult({ bridgeMap, isGameSuccess });
     this.view.printResult(result);
   }
 
-  // 사용자가 게임을 다시 시도할 때 사용하는 메서드 = R
-  retry() {
+  #retry() {
     this.model.setStateToReplay();
-    this.getUserCommand();
+    this.#getUserCommand();
   }
 };
 
--- src/model/bridge/BridgeMap.js
+++ src/model/bridge/BridgeMap.js
@@ -1,18 +1,5 @@
-const { Console } = require('@woowacourse/mission-utils');
 const { BRIDGE_MAP } = require('../../constants');
 
-// 1. initBridgeMap: ✅
-// bridge를 이용해 유저의 command와 bridge의 값이 동일하다면 O를 체크한 map ✅
-// 2. markIfLatestCommandFailed: ✅
-// 1번을 통해 init한 bridgeMap에서 유저가 마지막에 밟은 칸이 통과인지 불통인지 체크 ✅
-//    @통과 조건 = 마지막에 밟은 칸과 해당 index의 bridge 값이 동일
-//    - 통과라면 isPassed = true
-//    - 불통이라면 X 표시 후 isPassed = false
-// 3. convertBridgeMapToPrint: ✅
-// 만든 bridgeMap를 출력 형식에 맞게 convert
-// @return: {isPassed, bridgeMap} ✅
-//     - isPassed: 게임 실패 flag 변수 = 이번에 이동한 유저의 입력값이 이동할 수 있는 칸인지 여부
-//     - bridgeMap: 지금까지 유저의 bridgeMap
 const BridgeMap = class {
   #bridge;
   #position;
@@ -28,14 +15,14 @@ const BridgeMap = class {
   }
 
   getBridgeMap() {
-    this.initBridgeMap();
-    this.markIfLatestCommandFailed();
-    this.convertBridgeMapToPrint();
+    this.#initBridgeMap();
+    this.#markIfLatestCommandFailed();
+    this.#convertBridgeMapToPrint();
 
     return { isPassed: this.#isPassed, bridgeMap: this.#bridgeMap };
   }
 
-  initBridgeMap() {
+  #initBridgeMap() {
     const directions = [BRIDGE_MAP.up_direction, BRIDGE_MAP.down_direction];
 
     this.#bridgeMap = directions.reduce((bridgeMap, currDirection) => {
@@ -49,7 +36,7 @@ const BridgeMap = class {
     }, {});
   }
 
-  markIfLatestCommandFailed() {
+  #markIfLatestCommandFailed() {
     const latestDirectionIndex = this.#position - 1;
     const latestBridgeDirection = this.#bridge[latestDirectionIndex];
     const latestUserDirection = this.#commandList[latestDirectionIndex];
@@ -61,7 +48,7 @@ const BridgeMap = class {
     this.#bridgeMap[latestUserDirection][latestDirectionIndex] = BRIDGE_MAP.fail_space;
   }
 
-  convertBridgeMapToPrint() {
+  #convertBridgeMapToPrint() {
     const convertedBridgeMap = Object.values(this.#bridgeMap).reduce((bridgeMap, currDirection) => {
       bridgeMap.push(`[${currDirection.join('|')}]`);
       return bridgeMap;
