17c28577be646e777b000f5da28adb1927866ddc
moonelysian <dandyblack@naver.com>
2022-11-21 14:43:46 +0900

    fix: 에러 발생 시 재입력 받는 조건 반영
    - error를 던지고 프로그램이 꺼지지않고 에러를 출력하고 재입력을 받을수 있게 변경

--- src/InputView.js
+++ src/InputView.js
@@ -1,37 +1,46 @@
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
-const { MOVEMENT, COMMAND, BRIDGE_SIZE } = require('./constructor.js');
-const { input, throwError } = require('./utill.js');
+const { MOVEMENT, COMMAND, BRIDGE_SIZE, ERROR_MESSAGE } = require('./constructor.js');
+const { input, handleError } = require('./utill.js');
 
 const InputView = {
   async readBridgeSize() {
-    const answer = await input('다리의 길이를 입력해주세요\n');
-    if (isNaN(answer)) {
-      return throwError(`숫자를 입력해주세요.`);
-    } 
-    
-    const bridgeSize = parseInt(answer);
-    if (bridgeSize < BRIDGE_SIZE.MIN || bridgeSize > BRIDGE_SIZE.MAX) {
-      return throwError(`${MIN_BRIDGE_SIZE} ~ ${MAX_BRIDGE_SIZE}까지 수를 입력해주세요.`);
+    const answer = await input(`다리의 길이를 입력해주세요. (${BRIDGE_SIZE.MIN}~${BRIDGE_SIZE.MAX} 사이)\n`);
+    try {
+      if (isNaN(answer)) throw Error(ERROR_MESSAGE.NOT_NUMBER);
+      const bridgeSize = parseInt(answer);
+      if (bridgeSize < BRIDGE_SIZE.MIN || bridgeSize > BRIDGE_SIZE.MAX) {
+        throw Error(ERROR_MESSAGE.OUT_OF_RANGE);
+      }
+      return bridgeSize;
+    } catch (e) {
+      handleError(e.message, this.readBridgeSize);
     }
-    return bridgeSize;
   },
 
   async readMoving() {
     const answer = await input('이동할 칸을 선택해주세요. (위: U, 아래: D)\n');
-    if (answer !== MOVEMENT.UP && answer !== MOVEMENT.DOWN) {
-      return throwError('잘못된 입력입니다.');
+    try {
+      if (answer !== MOVEMENT.UP && answer !== MOVEMENT.DOWN) {
+        throw Error(ERROR_MESSAGE.WRONG_INPUT);
+      }
+      return answer;
+    } catch (e) {
+      handleError(e.message, this.readMoving);
     }
-    return answer;
   },
 
   async readGameCommand() {
     const answer = await input('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n');
-    if (answer !== COMMAND.RETRY && answer !== COMMAND.QUIT) {
-      return throwError('잘못된 입력입니다.')
+    try {
+      if (answer !== COMMAND.RETRY && answer !== COMMAND.QUIT) {
+        throw Error(ERROR_MESSAGE.WRONG_INPUT);
+      }
+      return answer;
+    } catch (e) {
+      handleError(e.message, this.readGameCommand);
     }
-    return answer;
   },
 };
 
