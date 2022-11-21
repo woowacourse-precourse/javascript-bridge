47e73245d741f0b57d0c83577127562034085ac8
chasj0326@naver.com <62418379+chasj0326@users.noreply.github.com>
2022-11-21 21:36:35 +0900

    refactor: 클래스 필드 private 로 변경 (BridgeGame.js)

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,33 +1,37 @@
 class BridgeGame {
+  #bridge;
+  #userBridge;
+  
   constructor(bridge, userBridge){
-    this.bridge = bridge;
-    this.userBridge = userBridge;
+    this.#bridge = bridge;
+    this.#userBridge = userBridge;
   }
   
   move(moving) {
-    this.userBridge.moved.push(moving);
-    return this.userBridge.moved;
+    this.#userBridge.moved.push(moving);
+    return this.#userBridge.moved;
   }
 
   status(){
-    const moveBy = this.userBridge.moved.length-1;
-    if(this.userBridge.moved[moveBy]!==this.bridge[moveBy]){
+    const moveBy = this.#userBridge.moved.length-1;
+    if(this.#userBridge.moved[moveBy]!==this.#bridge[moveBy]){
       return 0; // CAN NOT MOVE
     }
-    if(moveBy === this.bridge.length-1){
+    if(moveBy === this.#bridge.length-1){
       return 2; // MOVE COMPLETE
     }
     return 1; // CAN MOVE
   }
 
   retry() {
-    this.userBridge.attempts += 1;
-    this.userBridge.moved = [];
+    this.#userBridge.attempts += 1;
+    this.#userBridge.moved = [];
   }
 
   get(){
-    return this.userBridge;
+    return this.#userBridge;
   }
+
 }
 
 module.exports = BridgeGame;
