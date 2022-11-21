70a0b7d0caff39214e810afb77d27231c79f212b
Jaehyeon <jaehyeonp74@gmail.com>
2022-11-21 23:43:21 +0900

    refactor: inputMoving 메소드 리팩토링
    - 게임 재시도 여부를 담당하는 입력 로직을 다리 모양 그리는 로직 이후로 설정

--- src/Controller.js
+++ src/Controller.js
@@ -1,7 +1,6 @@
 const Model = require('./Model');
 const InputView = require('./InputView');
 const OutputView = require('./OutputView');
-const Validator = require('./Validator');
 const BridgeMaker = require('./BridgeMaker');
 const { generate } = require('./BridgeRandomNumberGenerator');
 const BridgeGame = require('./BridgeGame');
@@ -28,14 +27,14 @@ class Controller {
 
   inputMoving() {
     const callback = (input, index) => {
-      Validator.validateUpDown(input);
+      const bridgeSize = this.model.getBridgeSize();
       const computerBridgeArr = this.model.getComputerBridgeArr();
       const OX = input === computerBridgeArr[index] ? ' O ' : ' X ';
-      if (OX === ' X ') return InputView.readGameCommand(this.inputGameCommandCallback);
       BridgeGame.move(input, OX, this.model);
-      return OutputView.printMap(this.model);
+      OutputView.printMap(this.model);
+      if (OX === ' X ') return this.inputGameCommand();
     };
-    InputView.readMoving(callback, this.model, 0);
+    InputView.readMoving(callback, 0, this.model);
   }
 }
 module.exports = Controller;
