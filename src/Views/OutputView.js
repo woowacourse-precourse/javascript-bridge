dadaef7e48d2a6f7ff5eb6535ab843cd8d71688b
oseunghyun <ohsh1477@gmail.com>
2022-11-21 23:32:13 +0900

    feat(OutputView): printError 메소드 추가

--- src/Views/OutputView.js
+++ src/Views/OutputView.js
@@ -37,6 +37,10 @@ const OutputView = {
   printGameStart() {
     Console.print('다리 건너기 게임을 시작합니다.\n');
   },
+
+  printError(message) {
+    Console.print(`[ERROR] ${message}`);
+  },
 };
 
 module.exports = OutputView;
