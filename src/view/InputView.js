84434a7473a59594b6d4390ec4274f2baf86f2f2
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-11-21 11:06:32 +0900

    refactor: input validate 검사를 inputView 에서 하도록 변경

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,4 +1,5 @@
 const { Console } = require('@woowacourse/mission-utils');
+const { isCollectBridgeLength, isValidateMoveInput, isValidateRetryInput } = require('../utils/validator.js');
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -7,22 +8,58 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize(createBridgeByUser) {
-    Console.readLine('다리의 길이를 입력해주세요.\n', createBridgeByUser);
+  readBridgeSize(callback) {
+    Console.readLine('다리의 길이를 입력해주세요.\n', (userInput) => {
+      try {
+        isCollectBridgeLength(userInput);
+        callback(userInput);
+      } catch (error) {
+        this.readErrorBridgeSize(error, callback);
+      }
+    });
+  },
+
+  readErrorBridgeSize(error, callback) {
+    Console.print(error.message);
+    this.readBridgeSize(callback);
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving(movingByUser) {
-    Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)\n', movingByUser);
+  readMoving(callback) {
+    Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)\n', (userInput) => {
+      try {
+        isValidateMoveInput(userInput);
+        callback(userInput);
+      } catch (error) {
+        this.readErrorMoving(error, callback);
+      }
+    });
+  },
+
+  readErrorMoving(error, callback) {
+    Console.print(error.message);
+    this.readMoving(callback);
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand(askWantRetry) {
-    Console.readLine('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n', askWantRetry);
+  readGameCommand(callback) {
+    Console.readLine('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n', (userInput) => {
+      try {
+        isValidateRetryInput(userInput);
+        callback(userInput);
+      } catch (error) {
+        this.readErrorGameCommand(error, callback);
+      }
+    });
+  },
+
+  readErrorGameCommand(error, callback) {
+    Console.print(error.message);
+    this.readGameCommand(callback);
   },
 };
 
