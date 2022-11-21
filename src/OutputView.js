5b0b3236cc1414fddea0e275e160bdbd2d5781d7
nalsae <nalsae0207@naver.com>
2022-11-21 20:14:17 +0900

    feat: Implement a method to print map
    생성한 map을 출력하는 메서드 구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,11 +1,12 @@
-const { Console } = require('@woowacourse/mission-utils');
+const { Console } = require("@woowacourse/mission-utils");
+const InputView = require("./InputView");
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 const OutputView = {
   printStartMessage() {
-    Console.print('다리 건너기 게임을 시작합니다.\n');
+    Console.print("다리 건너기 게임을 시작합니다.\n");
   },
 
   /**
@@ -13,7 +14,17 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {},
+  printMap({ map, lastResult, originBridgeSize, currentBridgeSize }) {
+    Console.print(map);
+
+    if (lastResult === "X") InputView.readGameCommand();
+
+    if (lastResult === "O" && originBridgeSize !== currentBridgeSize) {
+      InputView.readMoving();
+    } else {
+      this.printResult();
+    }
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
