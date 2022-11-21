a885ca732c2c0d1a2950b0c3f584db4da3d016fa
pakxe <pigkill40@naver.com>
2022-11-22 02:30:00 +0900

    test(ProcessTest): 올바르게 게임이 종료되는지 테스트 작성 완료

--- __tests__/controller/ProcessTest.js
+++ __tests__/controller/ProcessTest.js
@@ -6,7 +6,7 @@ const {
   getOutput,
 } = require('../ApplicationTest');
 const App = require('../../src/App');
-const { SENTENCE, ERROR } = require('../../src/constants/Constants');
+const { SENTENCE, ERROR, RESULT } = require('../../src/constants/Constants');
 
 describe('과정 테스트', () => {
   test('잘못된 값 입력시 다시 입력을 받는지 테스트', () => {
@@ -24,6 +24,24 @@ describe('과정 테스트', () => {
       `${ERROR.restart}`,
       `${SENTENCE.start}`,
       '총 시도한 횟수: 2',
+    ]);
+  });
+
+  test('올바르게 게임이 종료되는지 테스트', () => {
+    const logSpy = getLogSpy();
+    mockRandoms([1, 0, 1]);
+    mockQuestions(['3', 'U', 'U', 'Q']);
+
+    const app = new App();
+    app.play();
+
+    const log = getOutput(logSpy);
+    expectLogContains(log, [
+      '[ O | X ]',
+      '[   |   ]',
+      `${RESULT.final}`,
+      `${RESULT.isClear}실패`,
+      `${RESULT.tryCount}1`,
     ]);
   });
 });
