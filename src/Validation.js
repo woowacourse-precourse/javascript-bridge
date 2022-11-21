e945df090a45222cb57e152a8d266342a6710431
Mcdonalds Trump <whdmschd2@gmail.com>
2022-11-21 20:12:30 +0900

    refactor: Validation class로 변경

--- src/Validation.js
+++ src/Validation.js
@@ -2,45 +2,43 @@ const { Console } = require("@woowacourse/mission-utils");
 const { COMMAND, BRIDGE } = require("./constants/game");
 const { ERROR_MESSAGE } = require("./constants/message");
 
-const Validation = {
-  bridgeSize: (bridgeSize) => {
-    if (
-      Number.isNaN(bridgeSize) ||
-      !Validation.isCorrectBrigeSize(bridgeSize)
-    ) {
+class Validation {
+  static bridgeSize(bridgeSize) {
+    if (Number.isNaN(bridgeSize) || !this.#isCorrectBrigeSize(bridgeSize)) {
       throw Console.print(ERROR_MESSAGE.BRIDGE_SIZE);
     }
-  },
-  movingCommand: (movingCommand) => {
-    if (!Validation.isCorrectMovingCommand(movingCommand)) {
+  }
+
+  static movingCommand(movingCommand) {
+    if (!this.#isCorrectMovingCommand(movingCommand)) {
       throw Console.print(ERROR_MESSAGE.MOVING_COMMAND);
     }
-  },
+  }
 
-  gameCommand: (gameCommand) => {
-    if (!Validation.isCorrectGameCommand(gameCommand)) {
+  static gameCommand(gameCommand) {
+    if (!this.#isCorrectGameCommand(gameCommand)) {
       throw Console.print(ERROR_MESSAGE.GAME_COMMAND);
     }
-  },
+  }
 
-  isCorrectBrigeSize(bridgeSize) {
+  static #isCorrectBrigeSize(bridgeSize) {
     return (
       BRIDGE.SIZE.MINIMUN <= bridgeSize && bridgeSize <= BRIDGE.SIZE.MAXIMUM
     );
-  },
+  }
 
-  isCorrectMovingCommand(movingCommand) {
+  static #isCorrectMovingCommand(movingCommand) {
     return (
       movingCommand === COMMAND.MOVING.UP ||
       movingCommand === COMMAND.MOVING.DOWN
     );
-  },
+  }
 
-  isCorrectGameCommand(gameCommand) {
+  static #isCorrectGameCommand(gameCommand) {
     return (
       gameCommand === COMMAND.GAME.RETRY || gameCommand === COMMAND.GAME.QUIT
     );
-  },
-};
+  }
+}
 
 module.exports = Validation;
