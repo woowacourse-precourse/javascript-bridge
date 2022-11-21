1d0bdfd5d91b58a1bf8ce109854df8032a75d07f
d-charlie-kim <dokkim@student.42seoul.kr>
2022-11-21 16:47:29 +0900

    Fix: Jest 정상 종료가 안되는 부분 때문에 console.close 삭제

--- src/Validator.js
+++ src/Validator.js
@@ -4,14 +4,12 @@ const numRegex = /[^0-9]/;
 const Validator = {
 	isNum(size) {
 		if (numRegex.test(size)) {
-			MissionUtils.Console.close();
 			throw new Error('[ERROR] 다리 길이는 숫자여야 합니다.');
 		}
 	},
 	
 	isInRange(size) {
 		if (size < 3 || size > 20) {
-			MissionUtils.Console.close();
 			throw new Error('[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.');
 		}
 	},
