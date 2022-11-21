42fe258a70d6c4ef414d80ef9461fba10b4bd8d2
pakxe <pigkill40@naver.com>
2022-11-21 19:23:50 +0900

    rename(TotalMovingCount): 가독성이 좋은 객체명으로 수정

---
+++ src/domain/TotalMovingCount.js
@@ -0,0 +1,17 @@
+const TotalMovingCount = class {
+  #totalMovingCount;
+
+  constructor() {
+    this.#totalMovingCount = 0;
+  }
+
+  plusTotalMovingCount() {
+    this.#totalMovingCount += 1;
+  }
+
+  getTotalMovingCount() {
+    return this.#totalMovingCount;
+  }
+};
+
+module.exports = TotalMovingCount;
