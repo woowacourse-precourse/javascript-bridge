f6cacd3a34abc3d2b60dae7ae5c646deaf0919dc
동동이 <drdragon777@naver.com>
2022-11-21 17:18:35 +0900

    feat - add input constants

---
+++ src/constants/input.constants.js
@@ -0,0 +1,6 @@
+const INPUT = {
+  BRIDGE_SIZE: "다리의 길이를 입력해주세요.\n",
+  MOVE: "이동할 칸을 입력해주세요. (위: U, 아래: D)\n",
+  GAME_END: "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n",
+};
+module.exports = INPUT;
