07dd05a2b0f3f7925d21027096571d1dd2d1fff8
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-11-21 12:46:05 +0900

    test(Applicationtest): 다리 이동 예외 테스트 케이스 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,7 +1,7 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const App = require("../src/App");
 const BridgeMaker = require("../src/BridgeMaker");
-
+const {MoveInput, RetryInput} = require("../src/Validate.js")
 const mockQuestions = (answers) => {
   MissionUtils.Console.readLine = jest.fn();
   answers.reduce((acc, input) => {
@@ -91,5 +91,30 @@ describe("다리 건너기 테스트", () => {
 
   test("다리 길이 예외 테스트", () => {
     runException([101]);
+  });
+
+  test("다리 길이 예외 테스트", () => {
+    runException([101]);
+  });
+  
+  test("다리 이동 예외 테스트", () => {
+    expect(() => {
+      MoveInput('Z');
+    }).toThrow('[ERROR]');
+    expect(() => {
+      MoveInput('Q');
+    }).toThrow('[ERROR]');
+    expect(() => {
+      MoveInput('u');
+    }).toThrow('[ERROR]');
+    expect(() => {
+      MoveInput('d');
+    }).toThrow('[ERROR]');
+    expect(() => {
+      MoveInput('ZZZ');
+    }).toThrow('[ERROR]');
+    expect(() => {
+      MoveInput(15);
+    }).toThrow('[ERROR]');
   });
 });
