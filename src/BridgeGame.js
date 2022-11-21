087db9def89667407e52dc52d8170873d8b084e7
rayark1 <max980203@snu.ac.kr>
2022-11-21 21:01:39 +0900

    feat(src/BridgeGame.js): 다리 건너기 게임을 관리하는 클래스의 move, retry 메소드 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -7,14 +7,24 @@ class BridgeGame {
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(currentposition, direction, bridge) {
+    if (direction === "U" && bridge[currentposition] === "U") {
+      return currentposition + 1;
+    }
+    if (direction === "D" && bridge[currentposition] === "D") {
+      return currentposition + 1;
+    }
+    return -1;
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  retry() {}
+  retry() {
+    return 0;
+  }
 }
 
 module.exports = BridgeGame;
