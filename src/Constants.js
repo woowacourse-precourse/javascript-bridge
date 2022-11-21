cd58f7a7c024151490cfd37a9f99c5c6db80893d
DongWooKim97 <nanaukim@naver.com>
2022-11-21 23:13:56 +0900

    feat: 자주 사용되는 상수값들 관리하는 객체 생성(Constants)

---
+++ src/Constants.js
@@ -0,0 +1,8 @@
+const GAME_MESSAGES = Object.freeze({
+  START_GAME: "다리 건너기 게임을 시작합니다.\n",
+  ASK_TO_BRIDGE_LENGTH: "다리의 길이를 입력해주세요.\n",
+  ASK_TO_MOVE_BLOCKS: "\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
+  ASK_OPINION_FOR_RESTART: "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n",
+});
+
+module.exports = { GAME_MESSAGES };
