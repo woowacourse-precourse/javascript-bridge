b5aaeb367fece7d106772dc3c471c5f1e40ce09c
rabBit64 <gojh47@gmail.com>
2022-11-21 13:54:39 +0900

    feat: [기능6] 실패할 경우 종료 혹은 재시작

--- src/InputView.js
+++ src/InputView.js
@@ -36,7 +36,8 @@ const InputView = {
       if(bridgeGame.move(bridge,steps)){
         OutputView.printMap(bridge,steps);
       }else{
-
+        OutputView.printMap(bridge,steps);
+        this.readGameCommand(attempt);
       }
     });
   },
@@ -44,8 +45,13 @@ const InputView = {
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {
-    
+  readGameCommand(attempt) {
+    Console.readLine('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)', (retry) => {
+      const inputCheck = new InputCheck();
+      inputCheck.retryCheck(retry);
+      const bridgeGame = new BridgeGame();
+      bridgeGame.retry(retry,attempt);
+    })
   },
 };
 module.exports = InputView;
