4bec7a769f6f08926c602b49975937bc66a0da5e
Sangjun-man <molung@icloud.com>
2022-11-22 01:46:39 +0900

    feat(BridgeGame) : 초기 게임 설정 기능 구현
    다리 게임을 시작할 때 필요한 값을 생성하는 기능을 구현했습니다.

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,13 +1,44 @@
+const BridgeMaker = require("./BridgeMaker");
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+const MovementStatus = require("./model/MovementStatus");
+const ValidationCheck = require("./VaildationCheck");
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  #bridge;
+  #movementStatus;
+  #gameState;
+
+  constructor(size) {
+    ValidationCheck.isNumberIntheRange([3, 20], size);
+    this.#bridge = BridgeMaker.makeBridge(
+      size,
+      BridgeRandomNumberGenerator.generate
+    );
+    this.#movementStatus = new MovementStatus();
+    this.#gameState = true;
+  }
+
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(moveInput) {
+    //이동 체크
+    const currentRound = this.currentStatus.round;
+    const { upperResult, lowerResult, gameState } = this.roundCheck(
+      moveInput,
+      currentRound
+    );
+    //이동 상태 변경
+    this.#movementStatusUpdate({ upperResult, lowerResult });
+    this.#gameState = gameState;
+    //현재 상태 리턴,
+    return { status: this.currentStatus, gameState };
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
@@ -15,6 +46,13 @@ class BridgeGame {
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   retry() {}
+
+  get currentStatus() {
+    return { ...this.#movementStatus };
+  }
+  get gameState() {
+    return this.#gameState;
+  }
 }
 
 module.exports = BridgeGame;
