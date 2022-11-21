9ea5d62eb5770ca62fb258ab3e0a56e6be1e18bd
wang-yurin <king_1994@naver.com>
2022-11-21 23:40:43 +0900

    refactor: 변수명 길이 축소

--- src/Constraint.js
+++ src/Constraint.js
@@ -1,8 +1,4 @@
-const ERROR_NOT_ONLY_NUMBER = "[ERROR] 숫자만 입력 가능합니다.";
-const ERROR_NOT_IN_RANGE = "[ERROR] 다리 길이는 3 이상 20 이하만 가능합니다.";
-const ERROR_NOT_START_ZERO =
-  "[ERROR] 0으로 시작하는 숫자는 입력할 수 없습니다.";
-
+const { ERROR } = require("./constant/Constants");
 class SizeConstraint {
   #size;
 
@@ -12,7 +8,7 @@ class SizeConstraint {
 
   checkStartZero() {
     if (this.#size[0] === "0") {
-      throw new Error(ERROR_NOT_START_ZERO);
+      throw ERROR.ERROR_NOT_START_ZERO;
     }
   }
 
@@ -20,7 +16,7 @@ class SizeConstraint {
     const REGEX = /^\d+$/;
 
     if (!REGEX.test(this.#size)) {
-      throw new Error(ERROR_NOT_ONLY_NUMBER);
+      throw ERROR.ERROR_NOT_ONLY_NUMBER;
     }
   }
 
@@ -28,11 +24,10 @@ class SizeConstraint {
     const numberSize = Number(this.#size);
 
     if (numberSize < 3 || numberSize > 20) {
-      throw new Error(ERROR_NOT_IN_RANGE);
+      throw ERROR.ERROR_NOT_IN_RANGE;
     }
   }
 }
-
 class MoveConstraint {
   #move;
 
@@ -40,13 +35,12 @@ class MoveConstraint {
     this.#move = move;
   }
 
-  checkInputValue() {
+  checkInputUD() {
     if (this.#move !== "U" && this.#move !== "D") {
-      throw new Error("[ERROR] U 또는 D만 입력 가능합니다.");
+      throw ERROR.ERROR_NOT_ONLY_U_OR_D;
     }
   }
 }
-
 class CommandConstraint {
   #command;
 
@@ -56,7 +50,7 @@ class CommandConstraint {
 
   checkInputRQ() {
     if (this.#command !== "R" && this.#command !== "Q") {
-      throw new Error("[ERROR] R 또는 Q만 입력 가능합니다.");
+      throw ERROR.ERROR_NOT_ONLY_R_OR_Q;
     }
   }
 }
