0e40bdb49059cd33be6a3e7542b7c3c173d5c4b6
nalsae <nalsae0207@naver.com>
2022-11-21 10:33:25 +0900

    feat: Implement a method to read moving
    사용자의 입력 값으로 이동할 칸을 받아 처리하는 메서드 구현
    - 입력 값의 유효성 검사
    - 입력 값을 바탕으로 결과를 계산하는 메서드 호출

--- src/InputView.js
+++ src/InputView.js
@@ -1,7 +1,10 @@
-const { Console } = require('@woowacourse/mission-utils');
-const Validator = require('./Validator');
-const { generate } = require('./BridgeRandomNumberGenerator');
-const BridgeMaker = require('./BridgeMaker');
+const { Console } = require("@woowacourse/mission-utils");
+const Validator = require("./Validator");
+const { generate } = require("./BridgeRandomNumberGenerator");
+const { makeBridge } = require("./BridgeMaker");
+
+const { INFO_MESSAGES } = require("./utils/messages");
+const BridgeGame = require("./BridgeGame");
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -11,11 +14,13 @@ const InputView = {
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize() {
-    Console.readLine('다리의 길이를 입력해주세요.\n', (userInput) => {
+    Console.readLine(INFO_MESSAGES.BRIDGE_SIZE, (userInput) => {
       try {
         Validator.bridgeSize(+userInput);
 
-        return BridgeMaker.makeBridge(+userInput, generate);
+        BridgeGame.bridge = makeBridge(+userInput, generate);
+
+        this.readMoving();
       } catch (error) {
         Console.print(error.message);
 
@@ -27,7 +32,19 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    Console.readLine(INFO_MESSAGES.MOVING, (userInput) => {
+      try {
+        Validator.moving(userInput);
+
+        BridgeGame.move(userInput);
+      } catch (error) {
+        Console.print(error.message);
+
+        this.readMoving();
+      }
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
