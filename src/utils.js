ab30e9e208be15cb9d223ac4ef56cbff430a254c
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-11-22 06:32:58 +0900

    feat(method): add takeNextStep
    Add takeNextStep object to utils
    - Contain two methods, true and false
    
    Add true to takeNextStep
    - Run when user moved to correct direction
    - Decide to stop or continue
    
    Add false to takeNextStep
    - Run when user moved to wrong direction
    - Ask user to retry or not

--- src/utils.js
+++ src/utils.js
@@ -5,4 +5,13 @@ module.exports = {
     U: (matchSymbol) => [matchSymbol, BLANK_SPACE],
     D: (matchSymbol) => [BLANK_SPACE, matchSymbol],
   },
+
+  takeNextStep: {
+    true: ({ app, bridgeGame, readAgain }) => {
+      bridgeGame.endStep(app, readAgain);
+    },
+    false: ({ app }) => {
+      app.askRetry();
+    },
+  },
 };
