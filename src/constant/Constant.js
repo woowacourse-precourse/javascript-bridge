708a5e3ec0423d3b43d5f7e1837981c9a6399089
wzrabbit <curious.wzrabbit@gmail.com>
2022-11-21 10:01:27 +0900

    feat(constant): 메시지 줄바꿈 문자 변경, 에러 메시지 추가
    - 문제 예시 출력과 일치하도록 ASK_BRIDGE_LENGTH, ASK_SELECT_MOVE, ASK_RETRY에 줄바꿈 문자 추가
    - 잘못된 다리 길이, 잘못된 이동 커맨드, 잘못된 재시작 커맨드에 대한 에러 메시지 추가

--- src/constant/Constant.js
+++ src/constant/Constant.js
@@ -1,8 +1,14 @@
 const MESSAGE = Object.freeze({
   GAME_START: '다리 건너기 게임을 시작합니다.',
-  ASK_BRIDGE_LENGTH: '다리의 길이를 입력해주세요.\n',
-  ASK_SELECT_MOVE: '이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
-  ASK_RETRY: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
+  ASK_BRIDGE_LENGTH: '\n다리의 길이를 입력해주세요.\n',
+  ASK_SELECT_MOVE: '\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
+  ASK_RETRY: '\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
+});
+
+const ERROR = Object.freeze({
+  INVALID_BRIDGE_SIZE: '[ERROR] 다리의 길이는 3 이상 20 이하의 자연수여야 합니다.\n',
+  INVALID_MOVING: '[ERROR] 이동할 칸은 반드시 U 또는 D 여야 합니다.\n',
+  INVALID_RETRY_COMMAND: '[ERROR] 재시작 여부에 대한 입력값은 반드시 R 또는 Q 여야 합니다.\n',
 });
 
 const COMMAND = Object.freeze({
@@ -32,6 +38,7 @@ const MOVE_RESULT = Object.freeze({
 
 module.exports = Object.freeze({
   MESSAGE,
+  ERROR,
   COMMAND,
   REGEX,
   BRIDGE,
