b10251d0c03b3652d8cd05bfbc5a3eb6010cc2b2
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-11-21 11:00:36 +0900

    refactor: printMap 메소드의 파라미터를 stepResult 로 변경

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -13,9 +13,9 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap(bridge) {
-    Console.print(`[${bridge.data.upperBridge}]`);
-    Console.print(`[${bridge.data.lowerBridge}]`);
+  printMap(stepResult) {
+    Console.print(`[${stepResult.upperBridge}]`);
+    Console.print(`[${stepResult.lowerBridge}]`);
   },
 
   /**
