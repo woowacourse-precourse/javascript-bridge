cc8ee74192e7b8fa96054406d5bc625601163256
nalsae <nalsae0207@naver.com>
2022-11-22 02:13:34 +0900

    feat: Create a file for constants
    상수를 관리하기 위한 파일 생성
    - 출력할 다리, 방향, 재시도 여부 관련 상수 추가

---
+++ src/utils/constants.js
@@ -0,0 +1,18 @@
+const BRIDGE = {
+  INITIAL: '[ ',
+  NO_RESULT: '  ',
+  END: ']',
+  NOT_END: '| ',
+};
+
+const DIRECTION = {
+  UP: 'U',
+  DOWN: 'D',
+};
+
+const INPUT = {
+  RETRY: 'R',
+  END: 'Q',
+};
+
+module.exports = { BRIDGE, DIRECTION, INPUT };
