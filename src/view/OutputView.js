f61f742ae5ca67f629576784d2a6dc736428441f
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-11-21 14:16:00 +0900

    refactor: OutputView printResult 메소드 수정

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -24,22 +24,11 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printResult(isSuccess, retryCount, bridge) {
+    const gameResult = isSuccess ? '성공' : '실패';
     Console.print('최종 게임 결과');
     this.printMap(bridge);
-    if (isSuccess) return this.printSuccessCase(retryCount);
-    return this.printFailCase(retryCount);
-  },
-
-  printSuccessCase(retryCount) {
     Console.print('');
-    Console.print('게임 성공 여부: 성공');
-    Console.print(`총 시도한 횟수: ${retryCount} `);
-    Console.close();
-  },
-
-  printFailCase(retryCount) {
-    Console.print('');
-    Console.print('게임 성공 여부: 성공');
+    Console.print(`게임 성공 여부: ${gameResult} `);
     Console.print(`총 시도한 횟수: ${retryCount} `);
     Console.close();
   },
