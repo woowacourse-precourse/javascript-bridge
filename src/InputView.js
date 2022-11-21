b9f0b92ecd63b853516de6dd27d71dc7bf24e3a9
nailen1 <nietsnie1@gmail.com>
2022-11-21 22:13:19 +0900

    style: 미션 가이드 주석 삭제

--- src/InputView.js
+++ src/InputView.js
@@ -2,13 +2,9 @@ const { Console } = require('@woowacourse/mission-utils');
 const OutputView = require('./OutputView');
 const BridgeUnit = require('./BridgeUnit');
 const BridgeGame = require('./BridgeGame');
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+
 const InputView = {
-  /**
-   * 다리의 길이를 입력받는다.
-   */
+
   readBridgeSize() {
     Console.readLine('다리의 길이를 입력해주세요' + '\n', (input) => {
       let bridgeGame = new BridgeGame(Number(input));
@@ -29,9 +25,6 @@ const InputView = {
     }
   },
 
-  /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
   readMoving(bridgeGame, bridge) {
     Console.readLine('\n' + '이동할 칸을 선택해주세요. (위: U, 아래: D)' + '\n', (UorD) => {
       this.tryMoving(UorD, bridgeGame, bridge);
@@ -67,9 +60,7 @@ const InputView = {
     if (bridgeGame.lose()) this.readGameCommand(bridgeGame, bridge);
     if (bridgeGame.move()) this.readMoving(bridgeGame, bridge);
   },
-  /**
-   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-   */
+
   readGameCommand(bridgeGame, bridge) {
     Console.readLine('\n' + '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)' + '\n', (RorQ) => {
       this.tryGameCommand(RorQ, bridgeGame, bridge);
@@ -88,6 +79,7 @@ const InputView = {
       this.readGameCommand(bridgeGame, bridge);
     }
   }
+
 };
 
 module.exports = InputView;
--- src/OutputView.js
+++ src/OutputView.js
@@ -1,18 +1,12 @@
 const { Console } = require('@woowacourse/mission-utils');
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
+
 const OutputView = {
+
   print(string) {
     Console.print(string);
   },
 
-  /**
-   * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-   * <p>
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
   printMap(bridgeGame, bridge) {
     let upMarks = [];
     let downMarks = [];
@@ -24,11 +18,6 @@ const OutputView = {
     this.print(downMarks.join('').replaceAll('][', '|'));
   },
 
-  /**
-   * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-   * <p>
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
   printResult(bridgeGame) {
     this.print(`게임 성공 여부: ${bridgeGame.result}`);
     this.print(`총 시도한 횟수: ${bridgeGame.trials}`);
@@ -40,6 +29,7 @@ const OutputView = {
     this.printResult(bridgeGame);
     Console.close();
   }
+
 };
 
 module.exports = OutputView;
