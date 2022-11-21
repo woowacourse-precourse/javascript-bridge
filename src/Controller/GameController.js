2fff8004a0ec0ae7365d5ad94ca34287d4110c89
Buyoung Chung <sunarol@naver.com>
2022-11-22 00:06:35 +0900

    feat: construct BridgeGame and make answerBridge
    if exception handler throw an error print it
    and restartGame

--- src/Controller/GameController.js
+++ src/Controller/GameController.js
@@ -39,6 +39,36 @@ class GameController {
   greeting() {
     OutputView.printGreeting();
   }
+
+  /**
+   * try/catch 구문을 사용해 예외 처리를 한다.
+   * 에러 문구를 출력하고 다시 실행하거나 게임을 그대로 진행한다.
+   * 입력받은 다리의 길이를 BridgeGame의 생성자에 전달하면서 생성한다.
+   * 그리고 다리의 길이를 BridgeMaker에 전달해 정답 다리를 생성하고 BridgeGame에 전달한다.
+   * 위 로직을 다 수행한 후 다음 로직을 수행할 첫 메소드를 호출한다.
+   * @param {string} size 사용자가 입력한 다리의 길이이다.
+   */
+  constructBridgeGameAndSaveAnswerBridge(size) {
+    try {
+      ExceptionHandler.isNotaNumber(size);
+      ExceptionHandler.validateSizeInput(size);
+
+      this.constructBridgeGame(size);
+      this.createAndSaveAnswerBridge(size);
+    } catch (err) {
+      OutputView.printError(err);
+      this.restartGame();
+    }
+  }
+
+  constructBridgeGame(size) {
+    this.BridgeGame = new BridgeGame(parseInt(size));
+  }
+
+  createAndSaveAnswerBridge(size) {
+    this.BridgeGame.answerBridge = BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate);
+    this.startMove();
+  }
 }
 
 module.exports = GameController;
