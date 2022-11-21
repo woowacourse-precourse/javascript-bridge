518cf19e7656abacaa415e8f43534e258810f2a4
kangyeongmin <kym6050@hanyang.ac.kr>
2022-11-21 09:05:13 +0900

    📌 feat(constant): 상수 추가

--- src/constant/value.js
+++ src/constant/value.js
@@ -4,9 +4,14 @@ const DOWN = "D";
 const SIGN_O = "O";
 const SIGN_X = "X";
 
+const SUCCESS = "성공";
+const FAIL = "실패";
+
 module.exports = {
   UP,
   DOWN,
   SIGN_O,
   SIGN_X,
+  SUCCESS,
+  FAIL,
 };
