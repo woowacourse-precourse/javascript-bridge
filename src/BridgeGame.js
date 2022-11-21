6fd9b8ad99ae336e04251434d9c01bee334836ba
Jinu <jinu200131@gmail.com>
2022-11-21 15:41:27 +0900

    docs : 요구사항 주석에 추가

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,5 +1,7 @@
 /**
  * 다리 건너기 게임을 관리하는 클래스
+ * 파일 경로 변경 금지
+ * InputView, OutputView 사용 금지
  */
 class BridgeGame {
   /**
--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,8 +1,11 @@
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
+ * 프로퍼티 추가 금지
+ * 파일 경로 변경 금지
  */
 const BridgeMaker = {
   /**
+   * 인자, 이름, return type 변경 금지
    * @param {number} size 다리의 길이
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
