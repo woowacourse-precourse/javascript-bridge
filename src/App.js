9f4666be6a900826b8c859af12b1bf5f766cfc3e
wlthdud <thdudzi@naver.com>
2022-11-21 16:23:41 +0900

    test: 테스트코드 오류 수정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 const { printGameStart } = require("../src/OutputView");
+const { readBridgeSize } = require("./InputView");
 //const OutputView = require("../src/OutputView");
 
 class App {
   play() {
     printGameStart();
+    readBridgeSize();
   }
 }
 
