b73c2ec9b831e8523e101634625a929e4e3caf25
wang-yurin <king_1994@naver.com>
2022-11-21 20:07:09 +0900

    feat(stateUpdate): 다리 길이 출력하는 기능 구현 중

--- src/Player.js
+++ src/Player.js
@@ -1,5 +1,24 @@
 const Player = {
   movingArray: [],
+  state: [[], []],
+  stateUpdate(move, correct) {
+    if (move === "U" && correct) {
+      this.state[0].push(" O ");
+      this.state[1].push("   ");
+    }
+    if (move === "U" && !correct) {
+      this.state[0].push(" X ");
+      this.state[1].push("   ");
+    }
+    if (move === "D" && correct) {
+      this.state[0].push("   ");
+      this.state[1].push(" O ");
+    }
+    if (move === "D" && !correct) {
+      this.state[0].push("   ");
+      this.state[1].push(" X ");
+    }
+  },
 };
 
 module.exports = Player;
