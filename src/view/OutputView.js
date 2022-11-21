439b9e3462f852c17fbbe164691f3a9e37f9aa79
pakxe <pigkill40@naver.com>
2022-11-21 20:55:12 +0900

    chore(OutputView): 주석 제거

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,9 +1,6 @@
 const { Console } = require('@woowacourse/mission-utils');
 const { SENTENCE, BRIDGE, RESULT, GAME } = require('../constants/Constants');
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
 const OutputView = {
   printStart() {
     Console.print(SENTENCE.start);
