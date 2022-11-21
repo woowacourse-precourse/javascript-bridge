fed64f069e6a2e4d44d831a4b4f58bb816c1bab4
sohyun <park.sohyun0430@gmail.com>
2022-11-21 17:39:51 +0900

    feat: printMap  구현
    moveInputArray 를 인자로 받아서, 현재까지의 bridge map을 출력하는 함수

--- src/OutputView.js
+++ src/OutputView.js
@@ -8,7 +8,46 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {},
+  printMap(moveInputArray) {
+    // moveInputArray = [{isRightDirect:true, moveInput},{},{}]
+    const resultMap = [];
+    let upsideBridge = '';
+    let downsideBridge = '';
+    for (let idx = 0; idx < 2; idx += 1) {
+      // idx==0 이면 위칸
+      // idx==1 이면 아래칸
+      if (idx === 0) {
+        // 위칸
+        const upsideBlock = moveInputArray
+          .map(({ isRightDirect, moveInput }) => {
+            if (moveInput === 'U') {
+              if (isRightDirect) return 'O';
+              return 'X';
+            }
+            return ' ';
+          })
+          .join(' | ');
+        upsideBridge = `[ ${upsideBlock} ]`;
+        resultMap.push(upsideBridge);
+      }
+      if (idx === 1) {
+        const downSideBlock = moveInputArray
+          .map(({ isRightDirect, moveInput }) => {
+            if (moveInput === 'D') {
+              if (isRightDirect) return 'O';
+              return 'X';
+            }
+            return ' ';
+          })
+          .join(' | ');
+        downsideBridge = `[ ${downSideBlock} ]`;
+        resultMap.push(downsideBridge);
+      }
+    }
+    resultMap.forEach((bridge) => {
+      Console.print(bridge);
+    });
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
