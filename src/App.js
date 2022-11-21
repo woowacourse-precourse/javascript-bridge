0be9bddb46c7d4b554ccb467d014b91369344ad8
seungki <sysh9498@gmail.com>
2022-11-21 17:19:02 +0900

    feat: 시작 메세지 출력 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+const Const = require('./Const');
+const MissionUtils = require('@woowacourse/mission-utils');
+const { Console } = MissionUtils;
 class App {
   play() {}
+
+  printStartMessage() {
+    Console.print(Const.START_MESSAGE);
+  }
 }
 
 module.exports = App;
