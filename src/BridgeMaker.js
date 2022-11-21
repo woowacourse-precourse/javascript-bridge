103579184c026effbcd719afdce45e87a51b7ac0
gyeongza <gyeongza@naver.com>
2022-11-21 15:37:23 +0900

    fix : 테스트코드 타입 변경 및 다리 생성 번호 타입 수정

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -5,17 +5,17 @@ const BridgeMaker = {
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
   makeBridge(size, generateRandomNumber) {
-    const RANDOM_LOWER_NUMBER = "0";
-    const RANDOM_UPPER_NUMBER = "1";
+    const RANDOM_LOWER_NUMBER = 0;
+    const RANDOM_UPPER_NUMBER = 1;
     const LOWER_BLOCK = "D";
     const UPPER_BLOCK = "U";
     let bridge = [];
 
     for (let index = 0; index < size; index++) {
       const number = generateRandomNumber();
-      if (number.toString() === RANDOM_LOWER_NUMBER) {
+      if (number === RANDOM_LOWER_NUMBER) {
         bridge.push(LOWER_BLOCK);
-      } else if (number.toString() === RANDOM_UPPER_NUMBER) {
+      } else if (number === RANDOM_UPPER_NUMBER) {
         bridge.push(UPPER_BLOCK);
       }
     }
