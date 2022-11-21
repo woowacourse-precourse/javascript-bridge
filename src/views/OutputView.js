4cabebb894c667d43a502f243be7d9ebc9449d87
Ryong-E <think8867@gmail.com>
2022-11-21 19:04:27 +0900

    refactor(OutputView): 변환 기능 분리로 불필요 한 로직 삭제

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,5 +1,6 @@
 const { Console } = require('@woowacourse/mission-utils');
 const CONSTANT = require('../constant');
+const Conversion = require('../utils/conversion');
 
 const { BUILD_BRIDGE_ERROR, MOVE_INPUT_ERROR, GAME_COMMAND_ERROR } = CONSTANT.ERROR_MESSAGE;
 const { START_MESSAGE } = CONSTANT.NORMAL_MESSAGE;
@@ -8,11 +9,6 @@ const { FAIL, SUCCESS, SUCCESS_WHETHER, TOTAL_ATTEMPT, FINAL_RESULT } = CONSTANT
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 const OutputView = {
-  /**
-   * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-   * <p>
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
   printStartMessage() {
     Console.print(START_MESSAGE);
   },
@@ -29,25 +25,16 @@ const OutputView = {
     Console.print(GAME_COMMAND_ERROR);
   },
 
+  /**
+   * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
+   * <p>
+   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+   */
   printMap(results) {
     results.forEach((result) => {
-      Console.print(this.convertResult(result));
+      Console.print(Conversion.convertResult(result));
     });
     Console.print('');
-  },
-
-  Sortation(result, index) {
-    if (index !== 0) return `| ${result} `;
-
-    return ` ${result} `;
-  },
-
-  convertResult(compareResult) {
-    let result = '';
-    compareResult.forEach((status, index) => {
-      result += this.Sortation(status, index);
-    });
-    return `[${result}]`.replace(/N/g, ' ');
   },
 
   /**
