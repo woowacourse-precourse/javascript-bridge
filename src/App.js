c67a27ea26fcb1d3c385a84d1e0c68a98d856421
HojinAn <asdv94@naver.com>
2022-11-21 22:48:58 +0900

    feat(bridgegame): 다리 게임 구현 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,70 @@
+const BridgeGame = require('./BridgeGame');
+const { GAME_RESULT } = require('./common/Constant');
+const InputView = require('./io/InputView');
+const OutputView = require('./io/OutputView');
+const {
+  validateMove,
+  validateRetry,
+  validateNumber,
+  validateNumberRange,
+} = require('./utils/Validator');
+
 class App {
-  play() {}
+  play() {
+    OutputView.printGameStart();
+    this.askSize();
+  }
+
+  askSize() {
+    InputView.readBridgeSize((input) => {
+      try {
+        validateNumber(input);
+        validateNumberRange(input);
+        this.game = new BridgeGame(+input);
+        this.askMove();
+      } catch (e) {
+        OutputView.printError(e);
+        this.askSize();
+      }
+    });
+  }
+
+  askMove() {
+    InputView.readMoving((input) => {
+      try {
+        validateMove(input);
+        this.game.move(input);
+        OutputView.printMap(this.game.movingState);
+        if (this.game.checkGameOver())
+          return this.game.judgeGameSuccess() ? this.end() : this.askRestart();
+      } catch (e) {
+        OutputView.printError(e);
+      }
+      this.askMove();
+    });
+  }
+
+  askRestart() {
+    InputView.readGameCommand((input) => {
+      try {
+        validateRetry(input);
+        if (input === GAME_RESULT.retry)
+          return this.game.retry(), this.askMove();
+        this.end();
+      } catch (e) {
+        OutputView.printError(e);
+        this.askRestart();
+      }
+    });
+  }
+
+  end() {
+    OutputView.printResult(
+      this.game.movingState,
+      this.game.judgeGameSuccess(),
+      this.game.tryCnt
+    );
+  }
 }
 
 module.exports = App;
--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,6 +1,7 @@
 const { makeBridge } = require('./BridgeMaker');
 const { generate } = require('./BridgeRandomNumberGenerator');
 const Checker = require('./utils/Checker');
+const Judge = require('./utils/Judge');
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
@@ -45,6 +46,14 @@ class BridgeGame {
    */
   countTry() {
     this.tryCnt++;
+  }
+
+  checkGameOver() {
+    return Checker.checkGameOver(this.movingState, this.#bridge);
+  }
+
+  judgeGameSuccess() {
+    return Judge.judgeGameSuccess(this.movingState, this.#bridge);
   }
 }
 
