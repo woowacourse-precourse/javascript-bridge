1b34ca1d1507c7efee5d316625eec1dc421ad11b
sying012 <syi369@naver.com>
2022-11-21 22:32:11 +0900

    docs : 기능 목록 작성

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -3,6 +3,7 @@
  */
 const BridgeMaker = {
   /**
+   * 프로퍼티를 추가할 수 없다.
    * @param {number} size 다리의 길이
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
