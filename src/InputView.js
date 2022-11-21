0d3535d8c7ffadebfa35196362af5b81c3124499
dongyeopca <ckehdduq95@gmail.com>
2022-11-21 13:01:19 +0900

    feat(InputView): 유저 입력 - 재시작 혹은 종료 입력(readGameCommand) 및 콜백 로직

--- src/InputView.js
+++ src/InputView.js
@@ -43,7 +43,16 @@ const InputView = {
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand(bridgegame) {
-    Console.readLine(MESSAGE.ANNOUNCE.INPUT_RETRY, (answer) => {});
+    Console.readLine(MESSAGE.ANNOUNCE.INPUT_RETRY, (answer) => {
+      try {
+        Validate.validateReadGameCommand(answer);
+        if (answer == 'Q') return OutputView.printResult(bridgegame);
+        bridgegame.retry();
+        return this.readMoving(bridgegame);
+      } catch (error) {
+        OutputView.printError(error, this.readGameCommand.bind(InputView), bridgegame);
+      }
+    });
   },
 };
 
