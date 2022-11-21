f5f493ca233a26263602ce136261c1e2a7b04b58
Ryong-E <think8867@gmail.com>
2022-11-21 18:52:22 +0900

    feat(referee,OutputView): 비교 후 결과 출력 기능 구현
    - referee 비교 결과를 가져와 출력
    - OutputView 비교결과를 출력형식에 맞게 변환하여 출력

--- src/controllers/referee.js
+++ src/controllers/referee.js
@@ -1,4 +1,5 @@
 const InputView = require('../views/InputView');
+const OutputView = require('../views/OutputView');
 const BridgeGame = require('./BridgeGame');
 
 class Referee {
@@ -14,7 +15,33 @@ class Referee {
   }
 
   start() {
-    this.bridgeGame.move();
+    this.bridgeGame.move(() => this.resultAnalysis());
+  }
+
+  resultAnalysis() {
+    const compareResult = this.bridgeGame.bridge.getCompareResult();
+    OutputView.printMap(compareResult);
+    this.judgement(compareResult);
+  }
+
+  judgement(compareResult) {
+    if (Referee.#isOver(compareResult)) {
+      InputView.readGameCommand();
+    }
+    if (this.bridgeGame.bridge.bridge.length === compareResult[0].length) {
+      OutputView.printResult();
+    }
+  }
+
+  static #isOver(compareResult) {
+    const upStage = compareResult[0];
+    const downStage = compareResult[1];
+
+    for (let index = 0; index < upStage.length; index += 1) {
+      if (upStage[index] === 'X' || downStage[index] === 'X') return true;
+    }
+
+    return false;
   }
 }
 module.exports = Referee;
