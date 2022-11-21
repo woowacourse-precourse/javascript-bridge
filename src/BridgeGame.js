1ee0631ab82612d0b7b8bbdd4a8df694d09a3927
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-11-21 14:44:19 +0900

    feat($BridgeGame): constructor 에 게임 시작 문구 출력 추가

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,20 +1,91 @@
+const Validator = require('./Validator');
+const BridgeMaker = require('./BridgeMaker');
+const InputView = require('./InputView');
+const OutputView = require('./OutputView');
+
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
+const { GAME_STATUS, FINAL_COMMAND_GROUP, POSITION } = require('./enums');
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
-class BridgeGame {
-  /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  move() {}
 
-  /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  retry() {}
+class BridgeGame {
+  #status = GAME_STATUS.WAITING;
+
+  #currentLocation = 0;
+
+  #bridge = null;
+
+  constructor() {
+    OutputView.printStart();
+  }
+
+  get isPlaying() {
+    return this.#status === GAME_STATUS.PLAYING;
+  }
+
+  get isFailed() {
+    return this.#status === GAME_STATUS.FAILED;
+  }
+
+  get isSuccess() {
+    return this.#status === GAME_STATUS.SUCCEEDED;
+  }
+
+  start(bridgeSize) {
+    this.#status = GAME_STATUS.PLAYING;
+    this.#currentLocation = 0;
+    this.#bridge = BridgeMaker.makeBridge(bridgeSize, BridgeRandomNumberGenerator.generate);
+  }
+
+  move(position) {
+    const isCorrect = this.#bridge[this.#currentLocation] === position;
+    if (!isCorrect) {
+      this.#status = GAME_STATUS.FAILED;
+      return;
+    }
+    this.#currentLocation += 1;
+    if (this.#currentLocation === this.#bridge.length) {
+      this.#status = GAME_STATUS.SUCCEEDED;
+    }
+  }
+
+  retry() {
+    this.#status = GAME_STATUS.PLAYING;
+    this.#currentLocation = 0;
+  }
+
+  end() {
+    this.#status = GAME_STATUS.WAITING;
+  }
+
+  printCurrent() {
+    console.log(this.#bridge);
+    console.log(this.#currentLocation);
+  }
+
+  printResult() {
+    //
+  }
+
+  static async readBridgeSizeInput() {
+    const input = await InputView.readBridgeSize();
+    Validator.validateBridgeSizeInput(input);
+    return input;
+  }
+
+  static async readMoveInput() {
+    const input = await InputView.readMoving();
+    Validator.validateMoveInput(input);
+    return input;
+  }
+
+  static async readIsRetry() {
+    const input = await InputView.readGameCommand();
+    Validator.validateRetryInput(input);
+    return input === FINAL_COMMAND_GROUP.RETRY;
+  }
 }
 
 module.exports = BridgeGame;
