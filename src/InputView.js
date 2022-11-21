8f8a9caea4ed0d17df558f64e8234877dc9a7053
wang-yurin <king_1994@naver.com>
2022-11-21 22:31:02 +0900

    style: 주석 제거

--- src/InputView.js
+++ src/InputView.js
@@ -20,9 +20,6 @@ const InputView = {
     });
   },
 
-  /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
   readMoving() {
     Console.readLine("이동할 칸을 선택해주세요. (위: U, 아래: D)\n", (move) => {
       new MoveInput(move);
@@ -35,9 +32,6 @@ const InputView = {
     });
   },
 
-  /**
-   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-   */
   readGameCommand() {
     Console.readLine(
       "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n",
