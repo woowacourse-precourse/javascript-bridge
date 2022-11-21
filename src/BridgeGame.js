973f00534fc950de4ebb43128aed69de4817ee17
Ryan-Dia <cjfdnjs158@naver.com>
2022-11-21 19:23:11 +0900

    B refactor(BridgeGame): @member 및 @returns 주석 추가
    처음 코드를 접한 사람들과 미래 유지보수를 위해 추가

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -5,6 +5,16 @@ const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  /**
+   * @member { number } size - 다리 길이
+   * @member { number } tryCount - 시도한 횟수
+   * @member { number } tryOrder - 대조하는 순서
+   * @member { string } up - 위쪽 다리 map
+   * @member { string } down - 아래쪽 다리 map
+   * @member { string } upOrDown - 위쪽인지 아래쪽인지 택 1
+   * @member { number } state - 이동할 수 있는 칸이면 1 이동할 수 없는 칸이면 0
+   * @member { string[] } prebuilt - 미리 만들어진 다리
+   */
   #BridgeData = {
     size: null,
     tryCount: 1,
@@ -20,8 +30,8 @@ class BridgeGame {
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+   * @returns { string[] } 입력값에 맞는 다리를 그린뒤 다리 위쪽과 아래쪽을 각각 return 한다.
    */
-
   move(upOrDown, state) {
     this.divideSpace();
     if (state) return this.pass(state, upOrDown);
