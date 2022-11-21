ebd9cc3ec618ad10aee8e9d2dd15c90f0b02bc61
하지훈 <jnh03336@gmail.com>
2022-11-21 16:03:14 +0900

    refactor(view): 인풋뷰 상수 메세지 분리

---
+++ src/view/InputMessages.js
@@ -0,0 +1,7 @@
+const { UP, DOWN, RETRY, QUIT } = require('../contants/Options');
+
+module.exports = {
+  ASK_BRIDGE_SIZE: '다리의 길이를 입력해주세요.',
+  ASK_MOVE: `이동할 칸을 선택해주세요. (위: ${UP}, 아래: ${DOWN})`,
+  ASK_RETRY: `게임을 다시 시도할지 여부를 입력해주세요. (재시도: ${RETRY}, 종료: ${QUIT})`,
+};
--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,7 +1,6 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const InputTypeValidator = require('./InputTypeValidator');
-const { UP, DOWN, RETRY, QUIT } = require('../contants/Options');
-
+const { ASK_BRIDGE_SIZE, ASK_MOVE, ASK_RETRY } = require('./InputMessages');
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -10,7 +9,6 @@ const InputView = {
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize(next) {
-    const ASK_BRIDGE_SIZE = '다리의 길이를 입력해주세요.';
     MissionUtils.Console.readLine(ASK_BRIDGE_SIZE, (input) => {
       InputTypeValidator.isInteger(input);
       next(input);
@@ -21,7 +19,6 @@ const InputView = {
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving(next) {
-    const ASK_MOVE = `이동할 칸을 선택해주세요. (위: ${UP}, 아래: ${DOWN})`;
     MissionUtils.Console.readLine(ASK_MOVE, (input) => {
       InputTypeValidator.isChar(input);
       next(input);
@@ -32,7 +29,6 @@ const InputView = {
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand(next) {
-    const ASK_RETRY = `게임을 다시 시도할지 여부를 입력해주세요. (재시도: ${RETRY}, 종료: ${QUIT})`;
     MissionUtils.Console.readLine(ASK_RETRY, (input) => {
       InputTypeValidator.isChar(input);
       next(input);
