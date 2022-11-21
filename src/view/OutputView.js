308101090a30f78afb55419fc68a94bc926b11f7
sh981013s <sh981013s@gmail.com>
2022-11-21 20:49:52 +0900

    feat(OutputView): 이동한 다리 상태 결과 출력 메서드 구현

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -6,12 +6,18 @@ const OutputView = {
   printOpening() {
     Console.print(BRIDGE_INPUT_MESSAGES.OPENING);
   },
+
   /**
    * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-   * <p>
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+   * @param up {string[]} 위쪽 다리 성공여부
+   * @param down {string[]} 아래쪽 다리 성공여부
    */
-  printMap() {},
+  printMap({ up, down }) {
+    const upLineToPrint = up.join(" | ");
+    const downLineToPrint = down.join(" | ");
+    Console.print(`[ ${upLineToPrint} ]`);
+    Console.print(`[ ${downLineToPrint} ]`);
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
