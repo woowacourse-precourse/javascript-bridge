4e95ff597edb1803d4356a3eb2b9b2d4e953d44b
Leejin-Yang <ejin1996@gmail.com>
2022-11-21 23:09:07 +0900

    test: 다리 건너기 게임 상태 클래스 테스트 추가 작성

--- __tests__/BridgeGameStatusTest.js
+++ __tests__/BridgeGameStatusTest.js
@@ -31,6 +31,59 @@ describe('다리 건너기 게임 상태 클래스 테스트', () => {
     expect(result).toEqual(1);
   });
 
+  test('마지막 위치와 현재 위치가 다르면 false', () => {
+    const bridgeGameStatus = new BridgeGameStatus();
+    bridgeGameStatus.increaseLocation();
+    bridgeGameStatus.increaseLocation();
+    bridgeGameStatus.increaseLocation();
+    const result = bridgeGameStatus.isLastLocation(4);
+
+    expect(result).toEqual(false);
+  });
+
+  test('마지막 위치와 현재 위치가 같으면 true', () => {
+    const bridgeGameStatus = new BridgeGameStatus();
+    bridgeGameStatus.increaseLocation();
+    bridgeGameStatus.increaseLocation();
+    bridgeGameStatus.increaseLocation();
+    const result = bridgeGameStatus.isLastLocation(3);
+
+    expect(result).toEqual(true);
+  });
+
+  test('마지막 위치와 현재 위치가 같고 건너지 못했다면 false', () => {
+    const bridgeGameStatus = new BridgeGameStatus();
+    bridgeGameStatus.increaseLocation();
+    bridgeGameStatus.increaseLocation();
+    bridgeGameStatus.increaseLocation();
+    const result = bridgeGameStatus.isWin(3, false);
+
+    expect(result).toEqual(false);
+  });
+
+  test('마지막 위치와 현재 위치가 같고 건넜다면 true', () => {
+    const bridgeGameStatus = new BridgeGameStatus();
+    bridgeGameStatus.increaseLocation();
+    bridgeGameStatus.increaseLocation();
+    bridgeGameStatus.increaseLocation();
+    const result = bridgeGameStatus.isWin(3, true);
+
+    expect(result).toEqual(true);
+  });
+
+  test.each([[true], [false]])(
+    '마지막 위치와 현재 위치가 다르면 건너기 성공 유무와 상관없이 false',
+    (isCrossed) => {
+      const bridgeGameStatus = new BridgeGameStatus();
+      bridgeGameStatus.increaseLocation();
+      bridgeGameStatus.increaseLocation();
+      bridgeGameStatus.increaseLocation();
+      const result = bridgeGameStatus.isWin(4, isCrossed);
+
+      expect(result).toEqual(false);
+    },
+  );
+
   test('시도 횟수 초기화', () => {
     const bridgeGameStatus = new BridgeGameStatus();
     bridgeGameStatus.increaseLocation();
--- __tests__/BridgeGameTest.js
+++
@@ -1,3 +0,0 @@
-// const BridgeGame = require('../src/models/BridgeGame');
-
-describe('다리 건너기 게임 클래스 테스트', () => {});
