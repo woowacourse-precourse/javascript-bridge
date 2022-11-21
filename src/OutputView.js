92d8cca8b101a5c1ac635aac9817f715c8a85ed3
aram <snsdl0905@naver.com>
2022-11-22 01:58:01 +0900

    refactor: 출력 형태 수정

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,4 +1,4 @@
-const MissionUtils = require("@woowacourse/mission-utils");
+const { Console } = require("@woowacourse/mission-utils");
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -20,18 +20,20 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-   printResult(result, count) {
+   printResult(map, result, count) {
+    Console.print('최종 게임 결과');
+    this.printMap(map);
     let message = "실패";
     if (result === true){
       message = "성공";
     }
-    console.log("게임 성공 여부: ", message);
-    console.log("총 시도한 횟수: ", count);
+    Console.print(`게임 성공 여부: ${message}`);
+    Console.print(`총 시도한 횟수: ${count}`);
     Console.close();
   },
 
   printStart(){
-    console.log(`다리 건너기 게임을 시작합니다.\n`);
+    Console.print(`다리 건너기 게임을 시작합니다.\n`);
   },
 };
 
