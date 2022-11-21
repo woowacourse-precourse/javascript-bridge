de6d2d4b38b9c14672eae062d3cab374b5a7290b
wontae chang <cwt629@naver.com>
2022-11-21 18:16:45 +0900

    style: customize remarks on BridgeGame

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -12,8 +12,7 @@ class BridgeGame {
 
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+   * @param {string} command 사용자가 입력한 이동 칸
    */
   move(command) {
     this.passed.push(command);
@@ -21,8 +20,6 @@ class BridgeGame {
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   retry() {
     this.passed = [];
