76e0cb1af40f5c20e52b4e3164719ee7ed47fd42
hjy0951 <hjy0951@gmail.com>
2022-11-22 01:34:21 +0900

    feat: 사용자 입력 구현

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,12 @@
+import Console from "@woowacourse/mission-utils";
+import BridgeGame from "./BridgeGame";
+import {
+  validateBridgeLength,
+  validateDirection,
+  validateCommand,
+} from "./Exceptions";
+import OutputView from "./OutputView";
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,17 +14,50 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine("다리의 길이를 입력해주세요.\n", function (length) {
+      validateBridgeLength(length);
+      const bridgeGame = new BridgeGame(length);
+      this.readMoving(bridgeGame);
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving(bridgeGame) {
+    Console.readLine(
+      "이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
+      function (direction) {
+        validateDirection(direction);
+        const isFinish = bridgeGame.move(direction);
+        if (isFinish) {
+          OutputView.printResult();
+          this.readGameCommand(bridgeGame);
+        }
+      }
+    );
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand(bridgeGame) {
+    Console.readLine(
+      "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n",
+      function (command) {
+        validateCommand(command);
+        this.executeCommand(command, bridgeGame);
+      }
+    );
+  },
+
+  executeCommand(command, bridgeGame) {
+    if (command === "R") {
+      bridgeGame.retry();
+      this.readBridgeSize();
+    } else if (command === "Q") Console.close();
+  },
 };
 
 module.exports = InputView;
