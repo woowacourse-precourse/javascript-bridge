3c24992356ad7fb730fcb8ca4a2f3930717973d3
daal2quf <daal2quf@gmail.com>
2022-11-21 22:58:39 +0900

    feat(bridgeGame): 재시작 기능 변경 및 기존 재시작 기능 삭제
    add:
    retry(): 유저의 입력이 'R'일때 유저 move 입력을 초기화하고 시도횟수를 +1 해줌
    delete:
    userChoiceRetry(): retry 사용으로 불필요해짐
    change:
    기능 작동 순서순으로 보기 편하게 메서드 위치 최적화

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -8,37 +8,20 @@ class BridgeGame {
   constructor() {
     this.#password;
     this.userInput = [];
+    this.tryCount = 1;
   }
 
   generateOfBridgeGamePassword(bridgeLength) {
     this.#password = BridgeMaker.makeBridge(bridgeLength, BridgeRandomNumberGenerator.generate);
   }
 
-  /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
   move(userMovingInput) {
     this.userInput.push(userMovingInput);
   }
-  /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  retry() {}
 
   drawingBridge() {
     const bridge = this.readyForDrawingBridge(this.userInput, this.#password);
     return this.bridgeToStringConverter(bridge);
-  }
-
-  bridgeToStringConverter(vanilaBridge) {
-    const convertedBridge = vanilaBridge.map(side => {
-      return '[' + side.join('|') + ']';
-    });
-    return convertedBridge;
   }
 
   readyForDrawingBridge(userMoveCollection, password) {
@@ -69,6 +52,13 @@ class BridgeGame {
     return [upside, downside];
   }
 
+  bridgeToStringConverter(vanilaBridge) {
+    const convertedBridge = vanilaBridge.map(side => {
+      return '[' + side.join('|') + ']';
+    });
+    return convertedBridge;
+  }
+
   gameSuccessStatus() {
     return JSON.stringify(this.userInput) === JSON.stringify(this.#password);
   }
@@ -77,8 +67,13 @@ class BridgeGame {
     return this.userInput[this.userInput.length - 1] === this.#password[this.userInput.length - 1];
   }
 
-  userChoiceRetry(userInput) {
-    return userInput === 'R';
+  retry(userChoice) {
+    if (userChoice === 'R') {
+      this.userInput = [];
+      this.tryCount += 1;
+      return true;
+    }
+    return false;
   }
 }
 module.exports = BridgeGame;
