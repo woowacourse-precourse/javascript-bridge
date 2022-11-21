01d4ff262c5f7977d1d1e8738945496d05e68a8e
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-11-21 16:52:06 +0900

    test: 사용자에게 U 또는 D 를 입력받는다.

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,9 +1,21 @@
-const { isCollectBridgeLength } = require('../src/utils/bridgeGameValidator.js');
+const { isCollectBridgeLength, isValidateMoveInput } = require('../src/utils/bridgeGameValidator.js');
 
-describe('다리길이 입력 테스트', () => {
+describe('입력 유효성 검사 테스트', () => {
   test.each([['1'], ['-11'], ['어머'], [''], ['/n']])('3이상 20이하가 아닌 수를 입력했을 때', (input) => {
     expect(() => {
       isCollectBridgeLength(input);
     }).toThrow('[ERROR]');
+  });
+
+  test.each([['1'], ['-11'], ['어머'], [''], ['/n'], ['UU'], ['DD']])('U 과 D가 아닌 값을 입력했을 때', (input) => {
+    expect(() => {
+      isValidateMoveInput(input);
+    }).toThrow('[ERROR]');
+  });
+
+  test.each([['U'], ['D']])('U 과 D가 아닌 값을 입력했을 때', (input) => {
+    expect(() => {
+      isValidateMoveInput(input);
+    }).not.toThrow('[ERROR]');
   });
 });
