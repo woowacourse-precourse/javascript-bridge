cf98f83fba3e9e5a248d6b804d8f11cc0cb1b95a
동동이 <drdragon777@naver.com>
2022-11-21 10:21:05 +0900

    feat - add initial readBridgeSize

--- src/InputView.js
+++ src/InputView.js
@@ -1,11 +1,26 @@
+const { Console } = require("@woowacourse/mission-utils");
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+const BridgeMaker = require("./BridgeMaker");
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
+  BRIDGE_SIZE: "다리의 길이를 입력해주세요.\n",
+  MOVE: "이동할 칸을 입력해주세요. (위: U, 아래: D)\n",
+  GAME_END: "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n",
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine(BRIDGE_SIZE, (input) => {
+      this.bridgeSize = Number(input);
+      this.bridge = BridgeMaker.makeBridge(
+        this.bridgeSize,
+        BridgeRandomNumberGenerator.generate
+      );
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
