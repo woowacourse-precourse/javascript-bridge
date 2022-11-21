b7fdd5774ff0d442f010a8659555092c63ed2c15
JangHoonSeok <orirori512@naver.com>
2022-11-22 01:01:14 +0900

    ✨ feat(BridgeGameController): 게임 진행(move)까지 구현

--- src/Controller/BridgeGameController.js
+++ src/Controller/BridgeGameController.js
@@ -9,39 +9,36 @@ class BridgeGameController {
   #bridge_size;
   #random_bridge;
   start() {
-    OutputView.printStartMessage();
-    this.#bridge_size = InputView.readBridgeSize();
-    this.generateBridge(this.#bridge_size);
+    OutputView.printStartMessage(); // 시작 문구 출력
+    this.#bridge_size = InputView.readBridgeSize(); // 다리 사이즈 입력
+    this.generateBridge(this.#bridge_size); // 랜덤 다리 생성
   }
 
   generateBridge(size) {
+    // 랜덤 다리 정보 Model에 저장.
     const randomBridge = BridgeMaker.makeBridge(
       size,
       BridgeRandomNumberGenerator.generate()
     );
     BridgeGame.setBridge(randomBridge);
     this.#random_bridge = BridgeGame.getBridge();
+    this.inputMoveDirection();
   }
 
   inputMoveDirection() {
-    return InputView.readMoving();
+    const cmd = InputView.readMoving();
+    this.move(cmd);
   }
 
+  gameProgress() {}
   /**
    *
    * @param {char} cmd 'U', 'D' 둘 중 하나로 들어옴.
    */
-  move(step) {
-    const cmd = this.inputMoveDirection();
-    const currentBridge = this.#random_bridge[step];
-    if (currentBridge === BRIDGE_DIRECTION.DOWN) {
-      Bridge.upBridge.push(currentBridge === cmd ? "O" : "X");
-      Bridge.downBridge.push(" ");
-    }
-    if (currentBridge === BRIDGE_DIRECTION.UP) {
-      Bridge.downBridge.push(currentBridge === cmd ? "O" : "X");
-      Bridge.upBridge.push(" ");
-    }
+  move(cmd, step) {
+    BridgeGame.move(cmd, step);
+    OutputView.printMap();
+    this.inputMoveDirection();
   }
 }
 
