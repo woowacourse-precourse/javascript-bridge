3eb40f3ce62b75d4eacf7c083a1037bb22cb91a8
sojinjang <jsojin6119@gmail.com>
2022-11-22 04:03:43 +0900

    :lipstick: style(view): 안내 메시지 줄간격 수정

--- src/constants/InfoMessages.js
+++ src/constants/InfoMessages.js
@@ -1,8 +1,8 @@
 class InfoMessages {
-  static OPENING_REMARKS = "다리 건너기 게임을 시작합니다.";
-  static ENTER_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
-  static ENTER_MOVEMENT_DIRECTION = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
-  static RESTART_OR_QUIT = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+  static OPENING_REMARKS = "다리 건너기 게임을 시작합니다.\n";
+  static ENTER_BRIDGE_SIZE = "다리의 길이를 입력해주세요.\n";
+  static ENTER_MOVEMENT_DIRECTION = "이동할 칸을 선택해주세요. (위: U, 아래: D)\n";
+  static RESTART_OR_QUIT = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n";
 
   static GAME_RESULTS = "최종 게임 결과";
   static GAME_FAILED = "게임 성공 여부: 실패";
