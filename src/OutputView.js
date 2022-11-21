903601cfbc0625c320c4d0bae93bec5c6bfb195e
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-11-21 21:34:17 +0900

    style: change object name
    Change object name
    - MESSAGE -> NOTICE

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,5 +1,5 @@
 const { Console } = require("@woowacourse/mission-utils");
-const { MESSAGE } = require("./constants");
+const { NOTICE } = require("./constants");
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -9,7 +9,7 @@ const OutputView = {
    * 게임 시작을 알리는 문구를 출력한다.
    */
   printStartingMessage() {
-    Console.print(MESSAGE.START_GAME);
+    Console.print(NOTICE.START_GAME);
   },
   /**
    * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
