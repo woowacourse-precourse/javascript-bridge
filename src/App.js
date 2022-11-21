709c55a34e9a16dd59a0b36572dcdc67aed9230e
catca <kdg96g@gmail.com>
2022-11-21 23:01:58 +0900

    feat: 다리 사이즈 입력받는 메소드 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 const BridgeGame = require("./BridgeGame");
+const BridgeMaker = require("./BridgeMaker");
+const InputView = require("./InputView");
 const OutputView = require("./OutputView");
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
 
 class App {
   constructor() {
-    this.game = new BridgeGame();
+    this.bridgeGame = new BridgeGame();
   }
   play() {
     OutputView.printStart();
+    this.setBridge();
   }
+
+  setBridge = () => {
+    InputView.readBridgeSize((size) => {
+      this.bridgeGame.setBridge(size);
+    });
+  };
 }
 
 const app = new App();
