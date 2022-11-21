b93921fe8b36245f15c0b5323d68f3fbc01e57a9
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-11-21 23:13:21 +0900

    feat(method): modify App
    Add line to play method
    - Use readBridgeSize method
    
    Add terminate method to App
    - Close console to terminate process

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 const { printStartingMessage } = require("./OutputView");
+const { readBridgeSize } = require("./InputView");
+const { Console } = require("@woowacourse/mission-utils");
 
 class App {
   play() {
     printStartingMessage();
+    readBridgeSize(this);
+  }
+
+  terminate() {
+    Console.close();
   }
 }
 
