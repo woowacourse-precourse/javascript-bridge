cddc5f663291d51ceb4d392699e5c8882e6164cf
Deemou <4106243@naver.com>
2022-11-21 10:57:20 +0900

    feat(OutputView): 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력하는 기능 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,3 +1,5 @@
+const { Console } = require('@woowacourse/mission-utils');
+const { BRIDGE, MAP } = require('./constant/Bridge');
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -7,14 +9,19 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {},
+  printMap(map) {
+    const upper = MAP.START + map[BRIDGE.UPPER].join(MAP.DELIMITER) + MAP.END;
+    const lower = MAP.START + map[BRIDGE.LOWER].join(MAP.DELIMITER) + MAP.END;
+    Console.print(upper);
+    Console.print(lower);
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() {},
+  printResult() {}
 };
 
 module.exports = OutputView;
