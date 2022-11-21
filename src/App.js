8b632c27b6e32f35608731b0ea3b7151f26f0a29
Hyungeol94 <55428818+Hyungeol94@users.noreply.github.com>
2022-11-21 18:23:08 +0900

    feat: 게임 시작 멘트 출력 및 다리 길이 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+const BridgeMaker = require("./BridgeMaker");
+const InputView = require("./InputView");
+console.log(InputView);
+
 class App {
-  play() {}
+  play() {
+    const MissionUtils = require("@woowacourse/mission-utils");
+    MissionUtils.Console.print("다리 건너기 게임을 시작합니다.");
+    InputView.readBridgeSize();
+    //BridgeMaker.makeBridge(InputView.readBridgeSize(), generateRandomNumber);
+    
+
+  }
 }
 
 module.exports = App;
--- src/InputView.js
+++ src/InputView.js
@@ -1,11 +1,23 @@
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
+
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+
+  readBridgeSize() {
+    const MissionUtils = require("@woowacourse/mission-utils");
+    var bridgeSize;
+    MissionUtils.Console.print('다리의 길이를 입력해주세요.');
+    MissionUtils.Console.readLine('다리의 길이를 입력해주세요.', (answer) => {
+      bridgeSize = answer;
+      MissionUtils.Console.print(answer);
+      if (this.checkBridgeSize(bridgeSize)) throw '[ERROR] wrong size';
+    });
+    return bridgeSize;
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
@@ -16,6 +28,12 @@ const InputView = {
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand() {},
+
+  checkBridgeSize(bridgeSize){
+    if (isNaN(bridgeSize)) return true;
+    if ((bridgeSize<3)||(bridgeSize>20)) return true;
+    if (bridgeSize%1 != 0) return true;
+  }
 };
 
 module.exports = InputView;
