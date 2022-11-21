424d3a0fc1adbb9fc6658d65ab84041280c250f0
DongWooKim97 <nanaukim@naver.com>
2022-11-21 23:23:02 +0900

    feat: 유저로부터 다리 사이즈를 입력받는 기능 추가

--- src/InputView.js
+++ src/InputView.js
@@ -1,20 +1,14 @@
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+const { Console } = require("@woowacourse/mission-utils");
+const { GAME_MESSAGES } = require("./Constants");
 const InputView = {
-  /**
-   * 다리의 길이를 입력받는다.
-   */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine(GAME_MESSAGES.ASK_TO_BRIDGE_LENGTH, (size) => {
+      Console.print(size);
+    });
+  },
 
-  /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
   readMoving() {},
 
-  /**
-   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-   */
   readGameCommand() {},
 };
 
