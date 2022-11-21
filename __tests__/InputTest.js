0a8ab9b4c0a65c8ad8828c41c0ba5e7be506f217
Jeong Hun Nam <njh3421@gmail.com>
2022-11-22 00:30:03 +0900

    chore : change file name

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -1,31 +1,31 @@
-const InputView=require('../src/InputView');
+const Input=require('../src/Input')
 
-describe("InputView 테스트",()=>{
+describe("Input 테스트",()=>{
   test("입력이 3미만, 20초과일때 에러를 발생시킨다.",()=>{
     expect(()=>{
-      new InputView.readBridgeSize([0,1,2,23,50,130,2200,213121])
+      Input.checkBride([0,1,2,23,50,130,2200,213121])
     }).toThrow("[ERROR] range error occured")
   })
   test("문자일때 에러가 발생한다.",()=>{
     expect(()=>{
-      new InputView.readBridgeSize(['a','b','ㅁ','ㅅ','+','*'])
+      Input.checkBride(['a','b','ㅁ','ㅅ','+','*'])
     }).toThrow("[ERROR] The string can not be accepted")
   })
   test("음수일때 에러가 발생한다.",()=>{
     expect(()=>{
-      new InputView.readBridgeSize([-1,-2,-4,-40,-20,-10])
+      Input.checkBride([-1,-2,-4,-40,-20,-10])
     }).toThrow("[ERROR] The Negative number can't be accepted")
   })
 
   test("U,D가 아닐 때 에러가 발생한다.",()=>{
     expect(()=>{
-      new InputView.readMoving([1,'a','ㅈ','-',-1,'+'])
+      Input.checkMovingInput(['a','ㅈ','-',-1,'+'])
     }).toThrow("[ERROR] Only U,D accepted")
   })
 
   test("R,Q가 아닐 때 에러가 발생한다.",()=>{
     expect(()=>{
-      new InputView.readGameCommand([1,'a','ㅈ','-',-1,'+'])
+      Input.checkReadGameInput([1,'a','ㅈ','-',-1,'+'])
     }).toThrow("[ERROR] Only R,Q accepted")
   })
 })
