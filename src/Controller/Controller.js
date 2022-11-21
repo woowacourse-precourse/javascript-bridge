cddd4e4a66e4ee50a9f421ce0c6b135c54a4f93d
ming0830 <minsun0830@naver.com>
2022-11-22 01:27:12 +0900

    feat : 시작 안내문구 출력(OutputView)와 constroller 연결

---
+++ src/Controller/Controller.js
@@ -0,0 +1,13 @@
+const OutputView = require("c:/Users/minsu/Documents/javascript-bridge/src/View/OutputView")
+
+class Controller {
+  // constructor(){
+  //   this.outputView = new OutputView();
+  // }
+
+  start(){
+    OutputView.printStart();
+  }
+}
+
+module.exports = Controller;
