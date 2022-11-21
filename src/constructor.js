b67a4ba417446aceeeabd1b9c5e64610fe3af527
moonelysian <dandyblack@naver.com>
2022-11-21 14:46:13 +0900

    feat: ERROR_MESSAGE 추가
    - 에러메세지 상수화

--- src/constructor.js
+++ src/constructor.js
@@ -18,9 +18,16 @@ const BRIDGE_POSITION = {
   DOWN: 0,
 }
 
+const ERROR_MESSAGE = {
+  NOT_NUMBER: '숫자를 입력해주세요.',
+  OUT_OF_RANGE: `${BRIDGE_SIZE.MIN}~${BRIDGE_SIZE.MAX}까지 수를 입력해주세요.`,
+  WRONG_INPUT: '잘못된 입력입니다. 지정된 문자만 입력해주세요.',
+}
+
 module.exports = {
   MOVEMENT,
   COMMAND,
   BRIDGE_SIZE,
   BRIDGE_POSITION,
+  ERROR_MESSAGE,
 }
