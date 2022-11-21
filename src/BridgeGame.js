b97be03f8c19427341b0c5ee26bfaea28b5681a5
d-charlie-kim <dokkim@student.42seoul.kr>
2022-11-21 19:37:19 +0900

    Feat: 입력 받은 이동할 칸의 값을 확인한다

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,13 +1,35 @@
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
+const BridgeMaker = require('./BridgeMaker');
+const { Utils } = require('./Utils');
+
 class BridgeGame {
+  #bridge;
+  #tryCount;
+  #step;
+
+  constructor(size) {
+    this.#tryCount = 1;
+    this.#step = 0;
+		this.#bridge = BridgeMaker.makeBridge(size, Utils.generateRandomNumber);
+  }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  isRightStep(move) {
+    if (this.#bridge[this.#step - 1] === move) {
+      return true;
+    }
+    return false;
+  }
+
+  move(move) {
+    this.#step += 1;
+    if (this.isRightStep(move)) {
+      return true;
+    }
+    return false;
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
