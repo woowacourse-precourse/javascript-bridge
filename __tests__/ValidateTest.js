b2453b1d66e68421693e2db974abd80ec2194409
chiman <rlaclghks123@naver.com>
2022-11-22 00:21:22 +0900

    test: validateTest 테스트 기능별로 분리

--- __tests__/ValidateTest.js
+++ __tests__/ValidateTest.js
@@ -1,32 +1,25 @@
 const Validate = require('../src/utils/Validate');
 
 describe('예외 테스트', () => {
-  test.each(['2', '0', '-3', '25'])('다리의 길이 예외 테스트', (input) => {
-    expect(() => {
-      Validate.validateSizeRange(input);
-    }).toThrow('[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.');
-  });
-
-  test.each(['u', 'd', '3', '0', 'UU', 'dD'])('이동할 칸에 대한 입력값 예외 테스트', (input) => {
-    expect(() => {
-      Validate.validateMovePosition(input);
-    }).toThrow('[ERROR] 이동할 칸은 위 : U 또는 아래 : D인 문자로 입력해주세요.');
-  });
-
-  test.each(['r', 'q', '3', 'Rr', 'qQ', 'Q.', ' Q'])(
-    '재시작 혹은 종료에 대한 입력값 예외 테스트',
-    (input) => {
+  describe('size Test', () => {
+    test.each(['2', '0', '-3', '25'])('다리의 길이 예외 테스트', (input) => {
       expect(() => {
-        Validate.validateRetryOfQuit(input);
-      }).toThrow('[ERROR] 재시도는 R, 종료는 Q인 문자로 입력해주세요.');
-    }
-  );
+        Validate.validateSizeRange(input);
+      }).toThrow('[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.');
+    });
 
-  describe('올바른 값이 들어갔을때 잘 동작하는지 Test', () => {
     test.each(['3', '6', '10', '16', '19', '20'])('다리의 길이 입력값 통과 테스트', (input) => {
       expect(() => {
         Validate.validateSizeRange(input);
       }).not.toThrow();
+    });
+  });
+
+  describe('movemoment Test', () => {
+    test.each(['u', 'd', '3', '0', 'UU', 'dD'])('이동할 칸에 대한 입력값 예외 테스트', (input) => {
+      expect(() => {
+        Validate.validateMovePosition(input);
+      }).toThrow('[ERROR] 이동할 칸은 위 : U 또는 아래 : D인 문자로 입력해주세요.');
     });
 
     test.each(['U', 'D'])('이동할 칸 입력값 통과 테스트', (input) => {
@@ -34,6 +27,17 @@ describe('예외 테스트', () => {
         Validate.validateMovePosition(input);
       }).not.toThrow();
     });
+  });
+
+  describe('재시작 혹은 종료에 대한 테스트 Test', () => {
+    test.each(['r', 'q', '3', 'Rr', 'qQ', 'Q.', ' Q'])(
+      '재시작 혹은 종료에 대한 입력값 예외 테스트',
+      (input) => {
+        expect(() => {
+          Validate.validateRetryOfQuit(input);
+        }).toThrow('[ERROR] 재시도는 R, 종료는 Q인 문자로 입력해주세요.');
+      }
+    );
 
     test.each(['R', 'Q'])('재시도 혹은 종료에 대한 입력값 통과 테스트', (input) => {
       expect(() => {
