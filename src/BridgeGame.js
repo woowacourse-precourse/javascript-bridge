bdb1d18f2d654064074bb529f17435ca55212101
moonelysian <dandyblack@naver.com>
2022-11-22 00:53:55 +0900

    refac: BridgeGame에 private 함수 추가해서 리팩토링
    - checkMoveable: 맞게 움직였는지 끝자리인지 확인
    - checkMarking: 사용자가 움직인 곳이 어디인지
    - updateMap: map 모양틀 만듦

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -17,30 +17,34 @@ class BridgeGame {
     this.#map = { 1: [], 0: [] };
   }
 
-
-  /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  move(movement) {
+  #checkMoveable(movement) {
     const isCorrect = this.#bridge[this.#currentIndex] === movement;
     const isEnd = this.#currentIndex === this.#bridge.length - 1;
-    const markedPosition = movement === MOVEMENT.UP ? BRIDGE_POSITION.UP : BRIDGE_POSITION.DOWN;
-    const unmarkedPosition = movement === MOVEMENT.UP ? BRIDGE_POSITION.DOWN : BRIDGE_POSITION.UP;
-    
+    return { isCorrect, isEnd }
+  }
+
+  #checkMarking(movement) {
+    if (movement === MOVEMENT.UP) {
+      return { markedPosition: BRIDGE_POSITION.UP, unmarkedPosition: BRIDGE_POSITION.DOWN };
+    }
+    return { markedPosition: BRIDGE_POSITION.DOWN, unmarkedPosition: BRIDGE_POSITION.UP };
+  }
+
+  #updateMap(isCorrect, marked, unmarked) {
+    this.#map[marked].push(isCorrect ? MARK.CORRECT : MARK.WRONG);
+    this.#map[unmarked].push(MARK.EMPTY);
+  }
+
+  move(movement) {
+    const { isCorrect, isEnd } = this.#checkMoveable(movement);
     if (isCorrect) this.#currentIndex += 1;
-    this.#map[markedPosition].push(isCorrect ? MARK.CORRECT : MARK.WRONG);
-    this.#map[unmarkedPosition].push(MARK.EMPTY);
     
+    const { markedPosition, unmarkedPosition } = this.#checkMarking(movement);
+    this.#updateMap(isCorrect, markedPosition, unmarkedPosition);
+
     return { isCorrect, isGameEnd: isEnd && isCorrect, count: this.#count, map: this.#map,  }
   }
 
-  /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
   retry() {
     this.#currentIndex = 0;
     this.#count += 1;
