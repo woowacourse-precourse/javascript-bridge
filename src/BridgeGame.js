59e4f1c46913cd59623967cf7be99f964ac873a6
eunsung <esk114477@gmail.com>
2022-11-21 21:11:58 +0900

    feat : 재시작 기능 수정 및 게임카운트 증가 기능 추가

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -7,7 +7,7 @@ const InputView = require("./InputView.js");
 class BridgeGame {
   #bridge = [];
   #count = 0;
-  #gameCount = 0;
+  #gameCount = 1;
   #moveBridge = new Map([
     ["U", ""],
     ["D", ""],
@@ -20,6 +20,9 @@ class BridgeGame {
   getMoveBridge(){
     return this.#moveBridge;
   }
+  getGameCount(){
+    this.#gameCount++;
+  }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
@@ -30,6 +33,8 @@ class BridgeGame {
     this.checkMove(str, state);
     OutputView.printMap(this.#moveBridge);
     if(this.#moveBridge.get("U")[state] === "X" || this.#moveBridge.get("D")[state] === "X") InputView.readGameCommand(this);
+    else if(this.#bridge.length === state + 1) OutputView.printResult(this, true);
+    else InputView.readMoving(this);
   }
 
   checkMove(str, state){
@@ -45,11 +50,12 @@ class BridgeGame {
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   retry(str) {
-    if(str == "R"){
-      this.#gameCount += 1;
-      return true;
-    }
-    if(str == "Q") return false;
+    this.getGameCount();
+    this.#moveBridge = new Map([
+      ["U", ""],
+      ["D", ""],
+    ]);
+    InputView.readMoving(this);
   }
 }
 
