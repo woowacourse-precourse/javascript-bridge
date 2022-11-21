48d90bfdd0fad5b6edc59f3c6b3563c66916d39f
seolyeongbae <peixueying@gmail.com>
2022-11-22 02:00:22 +0900

    feat: input bridge length and make bridgeGame

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 const { MissionUtils } = require("@woowacourse/mission-utils");
+const BridgeGame = require("./BridgeGame");
+const BridgeMaker = require("./BridgeMaker");
+const InputView = require("./InputView");
 
 class App {
+  bridge;
+
   print(message) {
     MissionUtils.Console.print(message);
     MissionUtils.Console.close();
@@ -8,6 +13,9 @@ class App {
 
   play() {
     this.print(`다리 건너기 게임을 시작합니다.`);
+    InputView.readBridgeSize((bridgeLength) => {
+      this.bridge = new BridgeGame(brigeLength);
+    });
   }
 }
 
