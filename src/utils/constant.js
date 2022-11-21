146608e083a98a2ee891ae6bfcc40b1e4fa51fa7
hozzijeong <ghwls0905@naver.com>
2022-11-21 19:43:07 +0900

    Feat: 상수 값 관련 객체 파일 작성

---
+++ src/utils/constant.js
@@ -0,0 +1,37 @@
+const DEFAULT = Object.freeze({
+  ZERO: 0,
+  EMPTY_ARRAY: [],
+  ONE: 1,
+  TRUE: true,
+  FALSE: false,
+  MIN_BRIDGE_NUM: 3,
+  MAX_BRIDGE_NUM: 20,
+  UP: "U",
+  DOWN: "D",
+  RETRY: "R",
+  QUIT: "Q",
+  END: "E",
+  MOVE: "M",
+});
+
+const OUTPUT_VIEW_VALUE = Object.freeze({
+  FIRST_EMPTY: "   ",
+  FIRST_WRONG: " X ",
+  FIRST_RIGHT: " O ",
+  EMPTY: "|  ",
+  RIGHT: "| O ",
+  WRONG: "| X ",
+});
+
+const ERROR = Object.freeze({
+  BRIDGE_IS_NUMBER_ERROR: "[ERROR] 숫자만 입력 가능합니다.",
+  BRIDGE_RANGE_ERROR: "[ERROR] 3 ~ 20사이의 숫자만 입력 가능합니다.",
+  DIRECTION_INPUT_ERROR: "[ERROR] U 또는 D만 입력 가능합니다.",
+  CONTINUE_INPUT_ERROR: "[ERROR] R 또는 Q만 입력 가능합니다.",
+});
+
+module.exports = {
+  DEFAULT,
+  OUTPUT_VIEW_VALUE,
+  ERROR,
+};
