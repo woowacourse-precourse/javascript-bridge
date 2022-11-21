31adffa69df8d58943512aea2d0cd658060ee311
ReturnReason <reasonz@daum.net>
2022-11-21 23:13:45 +0900

    test(GameMap): 만들어진 게임 맵이 반환되는 메서드 테스트

--- __tests__/GameMapTest.js
+++ __tests__/GameMapTest.js
@@ -7,4 +7,11 @@ describe('🗺 GameMap 클래스 테스트', () => {
 
     expect(gameMap.getMapLength()).toBe(3);
   });
+
+  test('⭐ 만들어진 게임 맵 카피본이 반환됩니다.', () => {
+    const gameMap = new GameMap();
+    gameMap.setBridgeGameMap(['U', 'U', 'D']);
+
+    expect(gameMap.getGameMap()).toEqual(['U', 'U', 'D']);
+  });
 });
