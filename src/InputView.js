8e4c0fc354184a40e269e56297bc233a21e53ed3
HeoJiye <heoze1031@gmail.com>
2022-11-21 15:55:36 +0900

    refector: InputView 객체 구현 순서 변경

--- src/InputView.js
+++ src/InputView.js
@@ -4,12 +4,18 @@ const Vaildation = require("./Vaildation");
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
+  MINIMUM_BRIDGE_SIZE: 3,
+  MAXIMUM_BRIDGE_SIZE: 20,
+
+  INPUT_UP_MOVING: 'U',
+  INPUT_DOWN_MOVING: 'D',
+
+  INPUT_RESTART: 'R',
+  INPUT_QUIT: 'Q',
+
   /**
    * 다리의 길이를 입력받는다.
    */
-  MINIMUM_BRIDGE_SIZE: 3,
-  MAXIMUM_BRIDGE_SIZE: 20,
-  
   readBridgeSize( callback ) {
     MissionUtils.Console.readLine('다리의 길이를 입력해주세요.', (answer) => {
       const size = Number(answer);
@@ -22,9 +28,6 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  INPUT_UP_MOVING: 'U',
-  INPUT_DOWN_MOVING: 'D',
-
   readMoving( callback ) {
     MissionUtils.Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)', (answer) => {
       Vaildation.one_of_candidates(answer, [this.INPUT_UP_MOVING, this.INPUT_DOWN_MOVING]);
@@ -36,9 +39,6 @@ const InputView = {
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  INPUT_RESTART: 'R',
-  INPUT_QUIT: 'Q',
-
   readGameCommand( callback ) {
     MissionUtils.Console.readLine('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)', (answer) => {
       Vaildation.one_of_candidates(answer, [this.INPUT_RESTART, this.INPUT_QUIT]);
