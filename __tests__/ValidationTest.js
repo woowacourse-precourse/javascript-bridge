6949d3565fecf6f4245275dc34284cc3df2fd00d
dim <dim@student.42seoul.kr>
2022-11-21 18:20:52 +0900

    Test: 입력받은 이동할 칸 인풋 예외처리 테스트

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,16 +1,28 @@
 const InputView = require('../src/Views/InputView');
 
 describe('예외처리 테스트', () => {
-  test.each(['1a', '8e8', 'a'])('숫자가 아닌 입력값 테스트', (input) => {
-    expect(() => {
-      InputView.validateBridgeSize(input);
-    }).toThrow('[ERROR]');
-  });
+  test.each(['1a', '8e8', 'a'])(
+    '다리 사이즈 숫자가 아닌 입력값 테스트',
+    (input) => {
+      expect(() => {
+        InputView.validateBridgeSize(input);
+      }).toThrow('[ERROR]');
+    }
+  );
 
-  test('숫자 범위 예외 테스트', () => {
+  test('다리 사이즈 숫자 범위 예외 테스트', () => {
     const size = '2';
     expect(() => {
       InputView.validateBridgeSize(size);
     }).toThrow('[ERROR]');
   });
+
+  test.each(['u', 'ud', 'D2'])(
+    '다리 사이즈 숫자가 아닌 입력값 테스트',
+    (input) => {
+      expect(() => {
+        InputView.validateMove(input);
+      }).toThrow('[ERROR]');
+    }
+  );
 });
