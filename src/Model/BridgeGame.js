bdbe6a25b2d55658272896ae44a96897f8373b92
ming0830 <minsun0830@naver.com>
2022-11-22 06:40:10 +0900

    feat : 사용자가 칸을 이동할때, 입력값이 정답리스트와 같으면 true, 틀리면 false를 리턴하는 기능

--- src/Model/BridgeGame.js
+++ src/Model/BridgeGame.js
@@ -3,16 +3,21 @@ const {makeBridge} = require('./BridgeMaker');
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  constructor(){
+    this.gameCount = 0;
+  }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move(moveKeyword,answer) {
-    if(moveKeyword === answer[0]) {
-      answer.shift();
-      return answer;
+
+  move(moveInput,answer) {
+    if(moveInput === answer[this.gameCount]){
+      this.gameCount++
+      return true
     }
+    if(moveInput !== answer[this.gameCount]) return false
   }
 
   /**
