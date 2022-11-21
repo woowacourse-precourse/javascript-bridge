596bd08a24221e0da8f83ed5eab66b52d4a1405b
2je0 <bulljy@naver.com>
2022-11-21 23:26:05 +0900

    ✨ feat(app): 다리 길이를 입력 받는 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
-class App {
-  play() {}
-}
+const OutputView = require('./View/OutputView');
+const InputView = require('./View/InputView');
+const BridgeGame = require('./BridgeGame');
 
+class App {
+  #brideGame;
+
+  play() {
+    OutputView.printStart();
+    this.#readBridgeSizeStage();
+  }
+
+  #readBridgeSizeStage() {
+    InputView.readBridgeSize(this.#readBridgeSizeCallback.bind(this));
+  }
+
+  #readBridgeSizeCallback(size) {
+    try {
+      this.#brideGame = new BridgeGame(size);
+    } catch (error) {
+      OutputView.print(error.message);
+      this.#readBridgeSizeStage();
+    }
+  }
+}
 module.exports = App;
