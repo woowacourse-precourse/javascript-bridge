442e9221c8dec9b4e62d6cd0582de2e2aeb7cff5
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-11-21 14:34:19 +0900

    refactor: GameManager 클래스를 App 클래스로 통합
    - App에서는 GameManager 클래스를 호출하는 일만 하고 있어서 둘을 하나로 통합처리

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-const GameManager = require('./controller/GameManager');
+const BridgeGame = require('./controller/BridgeGame');
+const Bridge = require('./model/Bridge');
+const { readBridgeSize, readMoving } = require('./view/InputView');
+const { printGameStart } = require('./view/OutputView');
 
 class App {
+  #bridge;
+
+  #bridgeGame;
+
   play() {
-    new GameManager();
+    this.#bridgeGame = new BridgeGame();
+    printGameStart();
+    readBridgeSize(this);
+  }
+
+  getBridge() {
+    return this.#bridge;
+  }
+
+  getBridgeGame() {
+    return this.#bridgeGame;
+  }
+
+  makeBridge(size) {
+    this.#bridge = new Bridge(size);
+    readMoving(this, 0);
   }
 }
 
