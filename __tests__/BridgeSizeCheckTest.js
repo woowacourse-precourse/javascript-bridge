28dede7adba51cfb78c5dee52d22cd38ea720a13
dhkham <khamdong@gmail.com>
2022-11-21 19:57:26 +0900

    test(BridgeSizeCheckTest): add test to check both upper and lower boundary value(3 and 20 respectively)

--- __tests__/BridgeSizeCheckTest.js
+++ __tests__/BridgeSizeCheckTest.js
@@ -2,7 +2,7 @@ const BridgeSizeCheck = require("../src/Check/BridgeSizeCheck");
 
 describe("다리 사이즈 입력 테스트", () => {
   test("다리 사이즈 정수 입력 시 통과", () => {
-    expect(() => new BridgeSizeCheck(3)).not.toThrow("[ERROR]");
+    expect(() => new BridgeSizeCheck(7)).not.toThrow("[ERROR]");
   });
 
   test("다리 사이즈 소수 입력 시 에러", () => {
@@ -15,6 +15,10 @@ describe("다리 사이즈 입력 테스트", () => {
 
   test("다리 사이즈 3-20 범위 충족 시 통과", () => {
     expect(() => new BridgeSizeCheck(3)).not.toThrow("[ERROR]");
+  });
+
+  test("다리 사이즈 3-20 범위 충족 시 통과", () => {
+    expect(() => new BridgeSizeCheck(20)).not.toThrow("[ERROR]");
   });
 
   test("다리 사이즈 3-20 범위 벗어날 시 에러", () => {
