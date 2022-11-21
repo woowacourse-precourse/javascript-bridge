658fd03e870a364525ec2c36f7c7cc719732ab0f
c0c0pang <coc1789@naver.com>
2022-11-22 04:20:08 +0900

    test: 테스트 케이스들 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -84,4 +84,44 @@ describe("다리 건너기 테스트", () => {
   test("예외 테스트", () => {
     runException(["a"]);
   });
+  test("이동하는 칸 테스트", () => {
+    runException(["3","1","U"]);
+  });
+  test("게임 종료 테스트", () => {
+    const logSpy = getLogSpy();
+    mockRandoms(["1", "0", "1"]);
+    mockQuestions(["3", "U", "U", "Q"]);
+
+    const app = new App();
+    app.play();
+
+    const log = getOutput(logSpy);
+    expectLogContains(log, [
+      "최종 게임 결과",
+      "[ O | X ]",
+      "[   |   ]",
+      "게임 성공 여부: 실패",
+      "총 시도한 횟수: 1",
+    ]);
+    expectBridgeOrder(log, "[ O | X ]", "[   |   ]");
+  });
+
+  test("게임 재시작 테스트", () => {
+    const logSpy = getLogSpy();
+    mockRandoms(["1", "0", "1"]);
+    mockQuestions(["3", "U", "U", "R","U","D","U"]);
+
+    const app = new App();
+    app.play();
+
+    const log = getOutput(logSpy);
+    expectLogContains(log, [
+      "최종 게임 결과",
+      "[ O |   | O ]",
+      "[   | O |   ]",
+      "게임 성공 여부: 성공",
+      "총 시도한 횟수: 2",
+    ]);
+    expectBridgeOrder(log,"[ O |   | O ]", "[   | O |   ]");
+  });
 });
