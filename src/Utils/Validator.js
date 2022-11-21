f68f05187a8caa2865c8f31accc8b747e2dd04f0
d-charlie-kim <dokkim@student.42seoul.kr>
2022-11-22 02:39:32 +0900

    Fix: new Error로 긴 출력물 나오는 것 단순 Throw로 수정

--- src/Utils/Validator.js
+++ src/Utils/Validator.js
@@ -3,13 +3,13 @@ const numRegex = /[^0-9]/;
 const Validator = {
 	isNum(size) {
 		if (numRegex.test(size)) {
-			throw new Error('[ERROR] 다리 길이는 숫자여야 합니다.');
+			throw ('[ERROR] 다리 길이는 숫자여야 합니다.');
 		}
 	},	
 	
 	isInRange(size) {
 		if (size < 3 || size > 20) {
-			throw new Error('[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.');
+			throw ('[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.');
 		}
 	},
 
@@ -20,13 +20,13 @@ const Validator = {
 
 	checkMoving(move) {
 		if (move !== 'U' && move !== 'D') {
-			throw new Error('[ERROR] 이동은 \'위(U)\' 혹은 \'아래(D)\'로만 할 수 있습니다.');
+			throw ('[ERROR] 이동은 \'위(U)\' 혹은 \'아래(D)\'로만 할 수 있습니다.');
 		}
 	},
 
 	checkGameCommand(command) {
 		if (command !== 'R' && command !== 'Q') {
-			throw new Error('[ERROR] 재시작하려면 \'R\' 종료하려면 \'Q\'를 입력해야 합니다.');
+			throw ('[ERROR] 재시작하려면 \'R\' 종료하려면 \'Q\'를 입력해야 합니다.');
 		}
 	}
 }
