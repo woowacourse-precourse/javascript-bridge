8f07511fe22a8c9b16c90aa876025dca2c9e9ed8
jjjjjeongwon <jjjjjwon21@gmail.com>
2022-11-22 06:17:19 +0900

    feat(InputView): 플레이어로부터 이동할 칸을 받는다

--- src/InputView.js
+++ src/InputView.js
@@ -19,6 +19,7 @@ const InputView = {
 
         const bridge = makeBridge(length, generate);
         bridgeGame.setBridge(bridge);
+        bridgeGame.move(bridgeGame)
       } catch (error) {
         Console.print(error.message);
         this.readBridgeSize(bridgeGame);
@@ -29,7 +30,22 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving(bridgeGame, idx = 0) {
+    const bridge = bridgeGame.getBridge();
+
+    if (idx !== bridge.length) {
+      Console.print(GAME_MESSAGE.UPDOWN_INPUT);
+      Console.readLine('', (string) => {
+        try {
+          InputValidator.isUpDown(string);
+
+        } catch (error) {
+          Console.print(error.message);
+          this.readMoving(bridgeGame, idx);
+        }
+      });
+    }
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
