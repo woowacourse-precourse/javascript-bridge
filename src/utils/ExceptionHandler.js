2aa0796313500a5798bada2b8968398575395f85
Buyoung Chung <sunarol@naver.com>
2022-11-21 15:39:50 +0900

    refactor: exceptions print error message and return boolean

--- src/utils/ExceptionHandler.js
+++ src/utils/ExceptionHandler.js
@@ -1,14 +1,37 @@
 const Constant = require("./Constant");
-
+const { Console } = require("@woowacourse/mission-utils");
 const ExceptionHandler = {
   isNotaNumber(input) {
-    if (isNaN(input)) throw new Error("[ERROR] 숫자를 입력해주세요.");
+    if (isNaN(input)) {
+      Console.print("[ERROR] 숫자를 입력해주세요.");
+      return false;
+    }
+    return true;
   },
 
   validateSizeInput(size) {
     const intSize = parseInt(size);
-    if (intSize < Constant.MIN_SIZE || intSize > Constant.MAX_SIZE)
-      throw new Error("[ERROR] 3부터 20 사이의 숫자를 입력해주세요.");
+    if (intSize < Constant.MIN_SIZE || intSize > Constant.MAX_SIZE) {
+      Console.print("[ERROR] 3부터 20 사이의 숫자를 입력해주세요.");
+      return false;
+    }
+    return true;
+  },
+
+  validateGameInput(input) {
+    if (input !== Constant.UP && input !== Constant.DOWN) {
+      Console.print("[ERROR] 정확히 입력해주세요.");
+      return false;
+    }
+    return true;
+  },
+
+  validateRetryInput(input) {
+    if (input !== Constant.QUIT && input !== Constant.RETRY) {
+      Console.print("[ERROR] 정확히 입력해주세요.");
+      return false;
+    }
+    return true;
   },
 };
 
