c487fccf0c7366613441355283a63a5bc1858a79
cofla <cofla159@naver.com>
2022-11-21 13:24:11 +0900

    feat: 다리 길이 유효성 검사

---
+++ src/ValidateInput.js
@@ -0,0 +1,27 @@
+/**
+ * 입력값 유효성 검사
+ */
+const ErrorHandler = require("./ErrorHandler");
+const ValidateInput = {
+  /**
+   * 다리의 길이가 3~20의 숫자인지 검사
+   */
+  bridgeSize(size) {
+    if (isNaN(size)) return ErrorHandler.bridgeSize();
+    size = Number(size);
+    if (size < 3 || size > 20) return ErrorHandler.bridgeSize();
+    return true;
+  },
+
+  /**
+   * 이동할 칸이 U/D인지 검사
+   */
+  moving() {},
+
+  /**
+   * 재시도 여부가 R/Q인지 검사
+   */
+  gameCommand() {},
+};
+
+module.exports = ValidateInput;
