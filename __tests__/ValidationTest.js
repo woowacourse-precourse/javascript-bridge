a202154af5054858516ddf06b143b289470374e5
Jin1won <wlsdnjs16521@gmail.com>
2022-11-22 00:06:56 +0900

    test: isCommandValid 함수 테스트코드 작성

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,4 +1,8 @@
-const { isBridgeLengthValid, isMoveValid } = require("../src/utils/index");
+const {
+  isBridgeLengthValid,
+  isMoveValid,
+  isCommandValid,
+} = require("../src/utils/index");
 
 describe("isBridgeLengthValid 함수 테스트", () => {
   test("공백을 입력하면 예외가 발생한다.", () => {
@@ -31,5 +35,19 @@ describe("isMoveValid 함수 테스트", () => {
     expect(() => {
       isMoveValid("a");
     }).toThrow("[ERROR] U 또는 D 값만 입력해주세요.");
+  });
+});
+
+describe("isCommandValid 함수 테스트", () => {
+  test("공백을 입력하면 예외가 발생한다.", () => {
+    expect(() => {
+      isCommandValid("");
+    }).toThrow("[ERROR] 공백이 아닌 값을 입력해주세요.");
+  });
+
+  test("옵션에 해당하지 않은 알파벳을 입력하면 예외가 발생한다.", () => {
+    expect(() => {
+      isCommandValid("a");
+    }).toThrow("[ERROR] R 또는 Q 값만 입력해주세요.");
   });
 });
