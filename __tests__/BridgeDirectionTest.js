afdcb0c7be8e84bbe5f6fedabdd9381d1e22ab92
hwanfront <kim015jh@gmail.com>
2022-11-21 17:56:21 +0900

    test(BridgeDirection): 다리 방향에 대한 예외처리 테스트 추가

--- __tests__/BridgeDirectionTest.js
+++ __tests__/BridgeDirectionTest.js
@@ -1,24 +1,35 @@
 const BridgeRandomNumberGenerator = require('../src/BridgeRandomNumberGenerator');
 const BridgeDirection = require('../src/domain/bridge/BridgeDirection');
+const BridgeDirectionException = require('../src/exception/BridgeDirectionException');
+const BridgeRandomNumberException = require('../src/exception/BridgeRandomNumberException');
 
 const generateNumber = (number) => () => number;
 
 describe('BridgeDirection 클래스 테스트', () => {
-  test('callback 함수로 BridgeRandomNumberGenerator.generate()인 경우', () => {
+  test('다리를 생성하는 메서드가 callback 함수로 BridgeRandomNumberGenerator.generate()인 경우', () => {
     const directions = ['D', 'U'];
     const direction = BridgeDirection.generate(BridgeRandomNumberGenerator.generate);
     expect(directions).toContain(direction);
   });
 
-  test.each([[0], [1]])('callback 함수의 리턴 값이 0 또는 1인 경우', (number) => {
+  test.each([[0], [1]])('다리를 생성하는 메서드가 callback 함수의 리턴 값이 0 또는 1인 경우', (number) => {
     const directions = ['D', 'U'];
     const direction = BridgeDirection.generate(generateNumber(number));
     expect(directions).toContain(direction);
   });
 
-  test('callback 함수의 리턴 값이 0 또는 1이 아닌 경우 예외처리', () => {
+  test('다리를 생성하는 메서드가 callback 함수의 리턴 값이 0 또는 1이 아닌 경우 예외처리', () => {
     expect(() => {
       BridgeDirection.generate(generateNumber(2));
-    }).toThrow('[ERROR]');
+    }).toThrow(BridgeRandomNumberException);
   });
+
+  test.each([["d"], ["u"], ["R"], [1]])(
+    '방향이 조건에 부합하지 않는 경우 예외처리',
+    (direction) => {
+      expect(() => {
+        BridgeDirection.validate(direction);
+      }).toThrow(BridgeDirectionException);
+    },
+  );
 });
