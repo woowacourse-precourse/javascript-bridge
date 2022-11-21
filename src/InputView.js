6faf76728b3cecdf184f862d98e93e382e1857eb
seoye0ng <101791501+seoye0ng@users.noreply.github.com>
2022-11-22 02:24:54 +0900

    fix : 입력값 예외처리 오류 수정

--- src/InputView.js
+++ src/InputView.js
@@ -9,7 +9,7 @@ const InputView = {
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize() {
-    Console.readLine('다리의 길이를 입력해주세요.', (number) => {
+    MissionUtils.Console.readLine('다리의 길이를 입력해주세요.', (number) => {
       this.numberValidate(number);
       return number;
     });
@@ -20,8 +20,8 @@ const InputView = {
       throw new Error('[ERROR] 숫자를 입력해야 합니다.');
     }
     if (
-      !InputView.MINIMUM_BRIDGE_LENGTH <= number &&
-      number <= InputView.MAXIMUM_BRIDGE_LENGTH
+      InputView.MINIMUM_BRIDGE_LENGTH > number ||
+      number > InputView.MAXIMUM_BRIDGE_LENGTH
     ) {
       throw new Error('[ERROR] 3 ~ 20 범위 안의 숫자를 입력해야 합니다.');
     }
@@ -31,7 +31,7 @@ const InputView = {
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving() {
-    Console.readLine(
+    MissionUtils.Console.readLine(
       '이동할 칸을 선택해주세요. (위: U, 아래: D)',
       (alphabet) => {
         this.alphabetValidate(alphabet);
@@ -41,9 +41,6 @@ const InputView = {
   },
   // 이동할 칸 예외처리
   alphabetValidate(alphabet) {
-    if (typeof alphabet !== 'string') {
-      throw new Error('[ERROR] 문자를 입력해야 합니다.');
-    }
     if (alphabet !== 'U' || alphabet !== 'D') {
       throw new Error('[ERROR] U나 D를 입력해야 합니다.');
     }
@@ -53,7 +50,7 @@ const InputView = {
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand() {
-    Console.readLine(
+    MissionUtils.Console.readLine(
       '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)',
       (answer) => {
         this.answerValidate(answer);
@@ -63,9 +60,6 @@ const InputView = {
   },
   // 재시작/종료 예외처리
   answerValidate(alphabet) {
-    if (typeof alphabet !== 'string') {
-      throw new Error('[ERROR] 문자를 입력해야 합니다.');
-    }
     if (alphabet !== 'R' || alphabet !== 'Q') {
       throw new Error('[ERROR] R나 Q를 입력해야 합니다.');
     }
