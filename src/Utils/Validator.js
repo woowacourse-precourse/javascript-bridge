7733267af23a7eb6f01fc499032ce8bac5ff0f21
feb-dain <feb.dain@gmail.com>
2022-11-21 20:40:02 +0900

    feat: throw exception if there's blank

--- src/Utils/Validator.js
+++ src/Utils/Validator.js
@@ -3,7 +3,8 @@ const {
   BRIDGE_LENGTH_LIMIT,
   APPROPRIATE_INPUT,
 } = require("./Constants");
-const { NOT_APPROPRIATE_LENGTH, WRONG_MOVEMENT, WRONG_ENDING } = ERROR_MESSAGE;
+const { NOT_APPROPRIATE_LENGTH, WRONG_MOVEMENT, WRONG_ENDING_COMMAND, BLANK } =
+  ERROR_MESSAGE;
 const { UP, DOWN, RESTART, QUIT } = APPROPRIATE_INPUT;
 
 class Validator {
@@ -11,17 +12,26 @@ class Validator {
     if (!BRIDGE_LENGTH_LIMIT.includes(Number(bridgeLength))) {
       throw new Error(NOT_APPROPRIATE_LENGTH);
     }
+    if (String(bridgeLength) !== String(bridgeLength).replace(/ /g, "")) {
+      throw new Error(BLANK);
+    }
   }
 
   userMovement(upOrDown) {
+    if (upOrDown !== upOrDown.replace(/ /g, "")) {
+      throw new Error(BLANK);
+    }
     if (upOrDown !== UP && upOrDown !== DOWN) {
       throw new Error(WRONG_MOVEMENT);
     }
   }
 
   ending(restartOrQuit) {
+    if (restartOrQuit !== restartOrQuit.replace(/ /g, "")) {
+      throw new Error(BLANK);
+    }
     if (restartOrQuit !== RESTART && restartOrQuit !== QUIT) {
-      throw new Error(WRONG_ENDING);
+      throw new Error(WRONG_ENDING_COMMAND);
     }
   }
 }
