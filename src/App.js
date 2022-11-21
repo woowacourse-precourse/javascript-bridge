0d8d9a8ad4abccdaa81006cb8acff5424ddfaa24
catca <kdg96g@gmail.com>
2022-11-21 22:24:10 +0900

    feat: 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+const BridgeGame = require("./BridgeGame");
+const OutputView = require("./OutputView");
+
 class App {
-  play() {}
+  constructor() {
+    this.game = new BridgeGame();
+  }
+  play() {
+    OutputView.printStart();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
