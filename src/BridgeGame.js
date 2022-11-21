9840c29f86ccbdd20a031377c6ca658e92ef99b9
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-11-21 18:23:33 +0900

    feat($BridgeGame): 게임 결과 처리

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -4,7 +4,7 @@ const InputView = require('./InputView');
 const OutputView = require('./OutputView');
 
 const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
-const { GAME_STATUS, FINAL_COMMAND_GROUP, POSITION } = require('./enums');
+const { GAME_STATUS, FINAL_COMMAND_GROUP, POSITION, MAP_MARK } = require('./enums');
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
@@ -16,6 +16,10 @@ class BridgeGame {
   #currentLocation = 0;
 
   #bridge = null;
+
+  #map = null;
+
+  #tryCount = 0;
 
   constructor() {
     OutputView.printStart();
@@ -37,36 +41,46 @@ class BridgeGame {
     this.#status = GAME_STATUS.PLAYING;
     this.#currentLocation = 0;
     this.#bridge = BridgeMaker.makeBridge(bridgeSize, BridgeRandomNumberGenerator.generate);
+    this.#map = Array.from({ length: 2 }, () => Array(this.#bridge.length).fill(' '));
   }
 
   move(position) {
-    const isCorrect = this.#bridge[this.#currentLocation] === position;
+    this.#currentLocation += 1;
+    const isCorrect = this.#bridge[this.#currentLocation - 1] === position;
     if (!isCorrect) {
       this.#status = GAME_STATUS.FAILED;
+      this.#setCurrentMap(position, MAP_MARK.WRONG);
       return;
     }
-    this.#currentLocation += 1;
-    if (this.#currentLocation === this.#bridge.length) {
-      this.#status = GAME_STATUS.SUCCEEDED;
-    }
+    this.#setCurrentMap(position, MAP_MARK.CORRECT);
+    if (this.#currentLocation === this.#bridge.length) this.#status = GAME_STATUS.SUCCEEDED;
   }
 
   retry() {
+    this.#tryCount += 1;
     this.#status = GAME_STATUS.PLAYING;
     this.#currentLocation = 0;
+    this.#map = Array.from({ length: 2 }, () => Array(this.#bridge.length).fill(' '));
   }
 
   end() {
+    this.#tryCount += 1;
+    OutputView.printEnd();
+    OutputView.printMap(this.#map, this.#currentLocation);
+    OutputView.printResult(this.isSuccess, this.#tryCount);
     this.#status = GAME_STATUS.WAITING;
   }
 
   printCurrent() {
-    console.log(this.#bridge);
-    console.log(this.#currentLocation);
+    OutputView.printMap(this.#map, this.#currentLocation);
   }
 
-  printResult() {
-    //
+  #setCurrentMap(position, mark) {
+    if (position === POSITION.UP) {
+      this.#map[0][this.#currentLocation - 1] = mark;
+      return;
+    }
+    this.#map[1][this.#currentLocation - 1] = mark;
   }
 
   static async readBridgeSizeInput() {
