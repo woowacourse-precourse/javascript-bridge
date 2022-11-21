64982621a62ac51f910c079d05d16adda3b6e93f
jiho-bae <jiho@dgu.ac.kr>
2022-11-21 18:17:30 +0900

    Feat: inputView.init 메서드에서 사용자로부터 입력받기를 시작한다.

--- src/InputView.js
+++ src/InputView.js
@@ -1,16 +1,33 @@
+const { Console } = require('@woowacourse/mission-utils');
+const { MSG } = require('./libs/constant');
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
   /**
+   * 사용자로부터 입력받기를 시작한다.
+   */
+  init() {
+    Console.print(MSG.startGame);
+    this.readBridgeSize();
+  },
+
+  /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine(MSG.inputBridgeSize, (bridgeSize) => {
+      this.readMoving();
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    Console.readLine(MSG.inputMoveDirection, (moveDirection) => {});
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
---
+++ src/libs/constant.js
@@ -0,0 +1,8 @@
+const MSG = {
+  startGame: '다리 건너기 게임을 시작합니다.\n',
+  inputBridgeSize: '다리의 길이를 입력해주세요.\n',
+  inputMoveDirection: '이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
+  inputRestartOrQuitGame: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
+};
+
+module.exports = { MSG };
