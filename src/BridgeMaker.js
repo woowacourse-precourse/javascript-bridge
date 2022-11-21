7be2223a45d60069ef9b7a85293eb47cbf40924c
yeongseokim <1903020@ewhain.net>
2022-11-21 22:32:23 +0900

    fix(BridgeMaker): Application test를 통과하도록 수정
    generateRandomNumber에 의한 값이 숫자가 아니라 문자일 때도 로직이
    작동하도록 parseInt 처리

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,3 +1,5 @@
+const { VALID_CHAR } = require("./constants");
+
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
@@ -8,10 +10,10 @@ const BridgeMaker = {
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
   makeBridge(size, generateRandomNumber) {
-    let correctBridge = '';
+    let correctBridge = [];
     for (let i = 0; i < size; i++) {
       const step = generateRandomNumber();
-      step ? correctBridge += "U" : correctBridge += "D"
+      parseInt(step) ? correctBridge.push(VALID_CHAR.UP) : correctBridge.push(VALID_CHAR.DOWN);
     }
     return correctBridge;
   },
