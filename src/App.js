67917f2903c9cbd9c7518f025bfe89033cfeb44b
moonelysian <dandyblack@naver.com>
2022-11-21 20:16:25 +0900

    feat(App): play 함수 로직 작성
    - 다리를 만들고
    - BridgeGame에 bridge를 셋팅해주고
    - 다리를 건넌다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+const BridgeGame = require('./BridgeGame.js');
 class App {
-  play() {}
+  #game
+
+  constructor() {
+    this.#game = new BridgeGame();
+  }
+
+  play() {
+    (async() => {
+        const bridge = await this.initBridge();
+        this.#game.setBridge(bridge);
+        await this.runGame();
+    })();
+  }
 }
 
 module.exports = App;
