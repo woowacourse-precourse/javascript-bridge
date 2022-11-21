d415c47434f6644e6d9731089a0b6da21c9f0482
Buyoung Chung <sunarol@naver.com>
2022-11-21 20:04:41 +0900

    feat: class Bridges has private properties and get methods
    initialize its properties when the user called retry

--- src/Model/Bridges.js
+++ src/Model/Bridges.js
@@ -1,9 +1,32 @@
+const Constant = require("../utils/Constant");
 class Bridges {
   #upBridge;
   #downBridge;
   constructor() {
+    this.#upBridge;
+    this.#downBridge;
+  }
+
+  initializeBridges() {
     this.#upBridge = "";
     this.#downBridge = "";
+  }
+
+  saveBridges(bridges) {
+    this.#upBridge = bridges[0];
+    this.#downBridge = bridges[1];
+  }
+
+  getAllBridges() {
+    return [this.#upBridge, this.#downBridge];
+  }
+
+  getUpBridge() {
+    return this.#upBridge;
+  }
+
+  getDownBridge() {
+    return this.#downBridge;
   }
 }
 
