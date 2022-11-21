89a7c61bee45e3b8ca70442fb92d629d5370ad97
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-11-22 00:59:08 +0900

    refactoring($utils): 안 쓰는 파일 삭제

--- src/utils/index.js
+++
@@ -1,9 +0,0 @@
-const { Console } = require('@woowacourse/mission-utils');
-
-const read = (query, callback) => Console.readLine(query, callback);
-
-const print = (message) => Console.print(message);
-
-const close = () => Console.close();
-
-module.exports = { read, print, close };
