f711e24b8d767374dc3d965c978671212c97ec21
curomame <curomame@naver.com>
2022-11-22 00:13:52 +0900

    ➕ feat - <App> [endGame] add
    게임을 끝내고 결과를 출력하는 메서드 구현

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 class App {
   #size;
   #tryCount;
@@ -7,7 +8,21 @@ class App {
     this.#tryCount = 0;
   }
 
-  play() {}
+  play() {
+  }
+
+  /**
+   * 게임을 끝내고 결과를 출력한다.
+   * @param {class} bridgeGame 다리 건너기 게임의 인스턴스
+   * @param {boolean} result 최종 게임의 성공 여부
+   */
+  endGame(bridgeGame, result) {
+    printResult(
+      returnCheckedMap(bridgeGame.bridge, bridgeGame.tempPosition, result),
+      result,
+      this.#tryCount
+    );
+  }
 }
 
 module.exports = App;
