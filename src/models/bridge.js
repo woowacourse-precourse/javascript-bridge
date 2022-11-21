d88266a53f158586a09dfdd29be2997c3736b96a
Ryong-E <think8867@gmail.com>
2022-11-21 18:49:38 +0900

    feat(Bridge): 건널 수 있는 다리인지 확인 기능 구현
    - 생성된 다리와 유저가 이동한 칸을 비교하여 성공하면 O 실패하면 X 선택하지않은 곳은 N 추가

--- src/models/bridge.js
+++ src/models/bridge.js
@@ -2,9 +2,55 @@ const BridgeMaker = require('../BridgeMaker');
 const BridgeRandomNumberGenerator = require('../BridgeRandomNumberGenerator');
 
 class Bridge {
+  constructor() {
+    this.locationNumber = 0;
+    this.compareResult = [[], []];
+  }
+
+  getCompareResult() {
+    return this.compareResult;
+  }
+
   makeBridge(size) {
     Bridge.#bridgeSizeValidate(size);
     this.bridge = BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate);
+  }
+
+  compareSpace(value) {
+    Bridge.#moveInputValidate(value);
+    this.sameCheck(value);
+    this.locationNumber += 1;
+  }
+
+  sameCheck(value) {
+    if (this.bridge[this.locationNumber] === value) {
+      this.#correct(value);
+      return;
+    }
+
+    this.#incorrect(value);
+  }
+
+  #incorrect(value) {
+    if (value === 'U') {
+      this.compareResult[0].push('X');
+      this.compareResult[1].push('N');
+      return;
+    }
+
+    this.compareResult[0].push('N');
+    this.compareResult[1].push('X');
+  }
+
+  #correct(value) {
+    if (value === 'U') {
+      this.compareResult[0].push('O');
+      this.compareResult[1].push('N');
+      return;
+    }
+
+    this.compareResult[0].push('N');
+    this.compareResult[1].push('O');
   }
 
   static #bridgeSizeValidate(size) {
@@ -14,6 +60,12 @@ class Bridge {
     if (!(size >= '0' && size <= '9')) throw new Error('0~9숫자만 입력가능합니다');
 
     if (size[0] === '0') throw new Error('옳지않은 입력입니다');
+  }
+
+  static #moveInputValidate(input) {
+    if (input.length !== 1) throw new Error('U,D만 입력가능합니다');
+
+    if (!(input === 'U' || input === 'D')) throw new Error('U,D만 입력가능합니다.');
   }
 }
 
