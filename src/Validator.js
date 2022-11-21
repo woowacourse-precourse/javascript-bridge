8d9b886511ca19ee8af5944bd9e9f9ef199b206e
rayark1 <max980203@snu.ac.kr>
2022-11-21 22:45:18 +0900

    feat(src/Validator.js): bridgeSize 예외 케이스 추가

--- src/Validator.js
+++ src/Validator.js
@@ -1,10 +1,15 @@
 const Validator = {
   ERROR_MESSEAGE_MUST_IN_RANGE:
     '[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.',
-  
-    validateBridgeSize(bridgeSize) {
+  ERROR_MESSEAGE_MUST_BE_NUMBER:
+    '[ERROR] 다리 길이는 숫자여야 합니다.',
+
+  validateBridgeSize(bridgeSize) {
     if (bridgeSize >= 3 || bridgeSize <= 20) {
       throw new Error(Validator.ERROR_MESSEAGE_MUST_IN_RANGE);
+    }
+    if (typeof bridgeSize !== 'number') {
+      throw new Error(Validator.ERROR_MESSEAGE_MUST_BE_NUMBER);
     }
   },
 };
