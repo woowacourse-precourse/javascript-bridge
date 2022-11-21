c88d4d4924c463bf401e21145c8e357d2478c8d6
Sangjun-man <molung@icloud.com>
2022-11-22 01:41:37 +0900

    test(BridgeGame) : BridgeGame 테스트 코드 작성
    다리 건너기 게임의 메인 로직에 대한 테스트 코드를 작성했습니다.

--- src/model/GameStatus.js
+++ src/model/GameStatus.js
@@ -2,7 +2,7 @@
  * 게임 결과에 대한 인스턴스를 만들기 위한 클래스입니다.
  * 게임 종료 후 게임 결과를 출력하기 위해 사용합니다.
  */
-class GameResult {
+class GameStatus {
   constructor() {
     /** @type {boolean} 게임 종료 후 성공 여부를 나타냅니다.*/
     this.success = false;
@@ -11,4 +11,4 @@ class GameResult {
   }
 }
 
-module.exports = GameResult;
+module.exports = GameStatus;
