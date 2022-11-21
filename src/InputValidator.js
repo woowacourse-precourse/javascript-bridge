e876b7d635bb982478228ff8b934d04888626da4
hyeongdong <khd941018@gmail.com>
2022-11-22 05:00:56 +0900

    feat(InputValidator) : 이동할 칸 입력값 유효성 확인하는 기능 구현

--- src/InputValidator.js
+++ src/InputValidator.js
@@ -1,9 +1,14 @@
 const InputValidator = {
-  checkBridgeSize() {},
+  checkBridgeSize(bridgeSize) {},
 
-  checkMoving() {},
+  checkMoving(direction) {
+    if (direction === "U" || direction === "D") {
+      return;
+    }
+    throw new Error("[ERROR] 이동할 칸은 U나 D만 입력하셔야 합니다.");
+  },
 
-  checkGameCommand() {},
+  checkGameCommand(command) {},
 };
 
 module.exports = InputValidator;
