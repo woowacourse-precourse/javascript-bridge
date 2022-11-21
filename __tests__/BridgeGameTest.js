efb6a361369075649caaeca0110152cd3efc4fbc
Deemou <4106243@naver.com>
2022-11-21 21:14:24 +0900

    chore(*): 주석 삭제

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -12,7 +12,6 @@ const mockRandoms = (numbers) => {
 describe('BridgeGame 클래스 테스트', () => {
   test('다리를 건너고 현재까지 다리를 건넌 상태를 저장한다.', () => {
     mockRandoms([1, 0, 0]);
-    //bridge = ['U','D','D']
 
     const inputs = [BRIDGE.UP, BRIDGE.DOWN, BRIDGE.UP];
     const upper = [MAP.PASS, MAP.BLANK, MAP.NONPASS];
@@ -30,7 +29,6 @@ describe('BridgeGame 클래스 테스트', () => {
 
   test('다리를 무사히 건넜는지 확인한다.', () => {
     mockRandoms([1, 0, 0]);
-    //bridge = ['U','D','D']
 
     const inputs = [BRIDGE.UP, BRIDGE.DOWN];
     const expectedOutput = [true, false];
@@ -45,7 +43,6 @@ describe('BridgeGame 클래스 테스트', () => {
 
   test('다리를 전부 건넜는지 확인한다. case1', () => {
     mockRandoms([1, 0, 0]);
-    //bridge = ['U','D','D']
 
     const inputs = [BRIDGE.UP, BRIDGE.DOWN, BRIDGE.DOWN];
     const expectedOutput = true;
@@ -60,7 +57,6 @@ describe('BridgeGame 클래스 테스트', () => {
 
   test('다리를 전부 건넜는지 확인한다. case2', () => {
     mockRandoms([1, 0, 0]);
-    //bridge = ['U','D','D']
 
     const inputs = [BRIDGE.UP, BRIDGE.DOWN, BRIDGE.UP];
     const expectedOutput = false;
@@ -75,7 +71,6 @@ describe('BridgeGame 클래스 테스트', () => {
 
   test('다리를 전부 건넜는지 확인한다. case3', () => {
     mockRandoms([1, 0, 0]);
-    //bridge = ['U','D','D']
 
     const inputs = [BRIDGE.UP, BRIDGE.DOWN];
     const expectedOutput = false;
@@ -90,7 +85,6 @@ describe('BridgeGame 클래스 테스트', () => {
 
   test('재시도를 위한 설정을 한다.', () => {
     mockRandoms([1, 0, 0]);
-    //bridge = ['U','D','D']
 
     const inputs = [BRIDGE.UP, BRIDGE.DOWN, BRIDGE.UP];
     const upper = [MAP.PASS];
--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,9 +1,7 @@
 const { BRIDGE, MAP } = require('./constant/Bridge');
 const BridgeMaker = require('./BridgeMaker');
 const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
+
 class BridgeGame {
   #bridge;
   #tryCount;
@@ -24,11 +22,6 @@ class BridgeGame {
     return this.#map;
   }
 
-  /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
   move(space) {
     this.#currentPosition += 1;
     const passResult = space === this.#bridge[this.#currentPosition] ? MAP.PASS : MAP.NONPASS;
@@ -41,11 +34,6 @@ class BridgeGame {
     }
   }
 
-  /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
   retry() {
     this.#tryCount += 1;
     this.#currentPosition = -1;
