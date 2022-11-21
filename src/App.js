5a01265e3e395cd4556eb4a41e2d8c6319cc5ea5
pakxe <pigkill40@naver.com>
2022-11-21 20:52:25 +0900

    fix(App): new 키워드와 함께 선언해 각 객체 초기화

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ const View = require('./view/ViewInterface');
 const Domain = require('./domain/DomainInterface');
 
 class App {
+  #view;
+  #domain;
+  #bridgeGame;
+
   constructor() {
-    this.view = View;
-    this.domain = Domain;
+    this.#view = new View();
+    this.#domain = new Domain();
+    this.#bridgeGame = new BridgeGame(this.#view, this.#domain);
   }
 
   play() {
-    const bridgeGame = new BridgeGame(this.view, this.domain);
-    bridgeGame.start();
+    this.#bridgeGame.start();
   }
 }
 
