a54c63d0b2a8d0d006d899409ef5951b68a48ce8
hjy0951 <hjy0951@gmail.com>
2022-11-22 02:02:03 +0900

    feat: App 클래스에서 게임 시작

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Console from "@woowacourse/mission-utils";
+import BridgeMaker from "./BridgeMaker";
+import InputView from "./InputView";
+
 class App {
-  play() {}
+  play() {
+    Console.print("다리 건너기 게임을 시작합니다.\n");
+    InputView.readBridgeSize();
+  }
 }
 
 module.exports = App;
