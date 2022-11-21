9a525b22efe85e02ddd8039d3bd1adf591f7a57d
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-11-22 00:52:48 +0900

    docs: class별 제약사항 주석 임시 작성
    Add
      클래스 별 구현시 제약사항을 주석으로 추가(추후 삭제 예정)

--- src/InputView.js
+++ src/InputView.js
@@ -1,6 +1,14 @@
+const MissonUtils = require('@woowacourse/mission-utils');
+
 /**
- * 사용자로부터 입력을 받는 역할을 한다.
+ * 제약사항
+ * 1. InputView 에서만 MissionUtils의 Console.readLine() 을 이용해 사용자의 입력을 받을 수 있다.
+ * 2. 제공된 InputView 객체를 활용해 구현해야 한다.
+ * 3. InputView의 파일 경로는 변경할 수 있다.
+ * 4. InputView의 메서드의 인자는 변경할 수 있다.
+ * 5. 사용자 값 입력을 위해 필요한 메서드를 추가할 수 있다.
  */
+
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
