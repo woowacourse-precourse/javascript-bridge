14f582320fbea83c480d4b98731603a249e060d3
pangkyu <baesense@naver.com>
2022-11-21 10:59:32 +0900

    Feat : 입력된 다리 길이에 따라 건널 수 있는 공간 랜덤하게 뽑는 기능
    입력된 다리 길이에 따라 건널 수 있는 공간(숫자) 랜덤으로 뽑는 기능 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -4,10 +4,6 @@ const { Console } = require("@woowacourse/mission-utils");
 const { MESSAGE } = require("./constants/Constant");
 const Validate = require("./Validate");
 
-// const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
-// const number = BridgeRandomNumberGenerator.generate();
-// console.log(number);
-
 /**
  * 다리 건너기 게임을 관리하는 클래스
  * 필드(인스턴스 변수)를 추가할 수 있음
@@ -16,26 +12,16 @@ const Validate = require("./Validate");
  * 메소드 추가/변경할 수 있음
  */
 class BridgeGame {
-  start() {
-    console.log("start");
-    // Console.readLine(MESSAGE.INPUT_BRIDGE_LENGTH, (inputBridgeSize) => {
-    //   Validate.isNumber(inputBridgeSize);
-    //   Validate.checkLength(inputBridgeSize);
-    //   this.bridgeSize = inputBridgeSize;
-    //   this.move();
-    // });
-  }
-
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {
-    Console.readLine(MESSAGE.CHOOSE_MOVE_SPACE, (userInput) => {
-      Validate.checkMovingKey(userInput);
-      console.log(MESSAGE.CHOOSE_MOVE_SPACE);
-    });
+  move(size) {
+    console.log(size);
+    // Console.readLine(MESSAGE.CHOOSE_MOVE_SPACE, (userInput) => {
+    //   Validate.checkMovingKey(userInput);
+    // });
   }
 
   /**
--- src/InputView.js
+++ src/InputView.js
@@ -1,6 +1,8 @@
 const { Console } = require("@woowacourse/mission-utils");
 const { MESSAGE } = require("./constants/Constant");
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
 const Validate = require("./Validate");
+const BridgeMaker = require("./BridgeMaker");
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -9,19 +11,27 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {
+  readBridgeSize(bridgeGame) {
     Console.readLine(MESSAGE.INPUT_BRIDGE_LENGTH, (inputBridgeSize) => {
       Validate.isNumber(inputBridgeSize);
       Validate.checkLength(inputBridgeSize);
 
-      // console.log(inputBridgeSize);
+      BridgeMaker.makeBridge(
+        inputBridgeSize,
+        BridgeRandomNumberGenerator.generate
+      );
+
+      // this.readMoving();
     });
   },
-
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    Console.readLine(MESSAGE.CHOOSE_MOVE_SPACE, (userInput) => {
+      Validate.checkMovingKey(userInput);
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
