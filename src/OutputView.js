4f76ffbe7362d9a0d08afba245691549b021c958
jhyns <tockiib@gmail.com>
2022-11-22 01:03:34 +0900

    feat: Add printMap for OutputView
    OutputView 클래스의 printMap 메소드 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,3 +1,6 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const { MESSAGES, MAP } = require('./Constants');
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -7,14 +10,43 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {},
+  printMap(game) {
+    const bridge = game.getBridge();
+    const choices = game.getChoices();
+
+    [MAP.UP, MAP.DOWN].forEach((direction) => {
+      MissionUtils.Console.print(
+        `${MAP.START}${this.getLine(bridge, choices, direction)}${MAP.END}`
+      );
+    });
+    MissionUtils.Console.print('');
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() {},
+  printResult(game) {},
+
+  getPrintCharacter(selectedDirection, success) {
+    if (!selectedDirection) {
+      return MAP.EMPTY;
+    }
+    return success ? MAP.SUCCESS : MAP.FAIL;
+  },
+
+  getLine(bridge, choices, direction) {
+    let line = '';
+
+    choices.forEach((choice, index) => {
+      const selectedDirection = choice === direction;
+      const success = choice === bridge[index];
+
+      line += this.getPrintCharacter(selectedDirection, success) + MAP.DIVIDER;
+    });
+    return line.slice(0, line.length - 3);
+  },
 };
 
 module.exports = OutputView;
