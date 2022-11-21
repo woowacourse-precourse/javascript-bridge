8948e14984f4edba0009a977c85e3b0a058e6a13
dhkham <khamdong@gmail.com>
2022-11-21 20:23:49 +0900

    refactor(MoveCheckTest): specify types of invalid input

--- __tests__/MoveCheckTest.js
+++ __tests__/MoveCheckTest.js
@@ -5,7 +5,7 @@ describe("다리 이동 입력 테스트", () => {
     expect(() => new MoveCheck(input)).not.toThrow("[ERROR]");
   });
 
-  test.each(["2.7", "a", "가"])("다리 이동 정상 입력 (U 혹은 D)", input => {
+  test.each(["2.7", "a", "가"])("다리 이동 비정상 입력 (2.7, a, 가)", input => {
     expect(() => new MoveCheck(input)).toThrow("[ERROR]");
   });
 });
--- __tests__/RetryQuitCheckTest.js
+++ __tests__/RetryQuitCheckTest.js
@@ -5,7 +5,7 @@ describe("게임 재시도 혹은 종료 입력 테스트", () => {
     expect(() => new RetryQuitCheck(input)).not.toThrow("[ERROR]");
   });
 
-  test.each(["2.7", "a", "가"])("게임 재시도 혹은 종료 비정상 입력(2.7, a, 가)", input => {
+  test.each(["2.7", "a", "가"])("게임 재시도 혹은 종료 비정상 입력 (2.7, a, 가)", input => {
     expect(() => new RetryQuitCheck(input)).toThrow("[ERROR]");
   });
 });
