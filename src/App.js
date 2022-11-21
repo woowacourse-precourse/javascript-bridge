9b46dc39e6a6dfbe6a69f91814caff4f8ff78551
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2022-11-21 19:51:08 +0900

    fix(App) : 비동기 동작방식 InputView에서 콜백함수로 변경

--- src/App.js
+++ src/App.js
@@ -20,61 +20,9 @@ class App {
   greeting(){
     Console.print("다리 건너기 게임을 시작합니다.");
   }
-
-  // async startGame(bridgeGame) {
-  //   await this.moveBridge(bridgeGame);
-  //   const [isSuccess, gameCount] = [bridgeGame.getSuccess(), bridgeGame.getCount()];
-  //   isSuccess ? OutputView.printResult(gameCount, isSuccess) : await this.restartGame(bridgeGame);
-  // }
-
-  // async moveBridge(bridgeGame) {
-  //   while ((bridgeGame.getIdx() < this.#bridgeSize) && (bridgeGame.getSuccess())) {
-  //     await this.readMovingInput();
-  //     bridgeGame.move(this.#move, this.#bridge);
-  //     OutputView.pushResult(this.#move, bridgeGame.getSuccess());
-  //     OutputView.printMap();
-  //   }
-  // }
-
-  // async readMovingInput() {
-  //   try {
-  //     this.#move = await InputView.readMoving();
-  //   } catch (err) {
-  //     Console.print(err.message);
-  //     await this.readMovingInput();
-  //   }
-  // }
-
-  // async restartGame(bridgeGame) {
-  //   await this.readCommandInput() 
-  //   if (this.#restart === "Q") {
-  //     const [isSuccess, gameCount] = [bridgeGame.getSuccess(), bridgeGame.getCount()];
-  //     OutputView.printResult(gameCount, isSuccess);
-  //   }
-  //   else if (this.#restart === "R") {
-  //     this.initGame(bridgeGame);
-  //     this.startGame(bridgeGame);
-  //   }
-  // }
-
-  // async readCommandInput() {
-  //   try {
-  //     this.#restart = await InputView.readGameCommand();
-  //   } catch (err) {
-  //     Console.print(err.message);
-  //     await this.readCommandInput();
-  //   }
-  // }
-
-  // initGame(bridgeGame) {
-  //   bridgeGame.retry();
-  //   OutputView.upper.length = 0;
-  //   OutputView.downer.length = 0;
-  // }
-
 }
 
-const app = new App();
-app.play();
+// const app = new App();
+// app.play();
 
 module.exports = App;
