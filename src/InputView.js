64771e24e896f15b2acb46470e813d3e72d2f1c6
chsua <csj1919@naver.com>
2022-11-21 15:20:16 +0900

    feat: 다리길이 입력받는 함수(InputView.js)

--- src/InputView.js
+++ src/InputView.js
@@ -1,11 +1,22 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
-const InputView = {
+ const InputView = {
   /**
-   * 다리의 길이를 입력받는다.
+   * 다리의 길이를 입력받는 함수 + 예외처리
    */
-  readBridgeSize() {},
+  readBridgeSize(callback) {
+    MissionUtils.Console.readLine("다리의 길이를 입력해주세요.", (input) => {
+      try {
+        if ((input >= 3)&&(input <= 20)) callback(input) ;
+        else throw new Error("[ERROR] 다리의 길이는 3이상 20이하입니다.")
+      } catch (err) {
+        MissionUtils.Console.print(err.message) ;
+        InputView.readBridgeSize(callback) ; 
+      }
+    })
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
