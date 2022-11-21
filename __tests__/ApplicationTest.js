19f62217bcf6b6138e9654347f6992499de32bfc
Yoo Seung Guk <dbtmd878@naver.com>
2022-11-21 19:25:26 +0900

    test: 테스트 코드 수정

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -53,7 +53,7 @@ const expectBridgeOrder = (received, upside, downside) => {
 
 describe("다리 건너기 테스트", () => {
   test("다리 생성 테스트", () => {
-    const randomNumbers = ["1", "0", "0"];
+    const randomNumbers = [1, 0, 0];
     const mockGenerator = randomNumbers.reduce((acc, number) => {
       return acc.mockReturnValueOnce(number);
     }, jest.fn());
@@ -64,7 +64,7 @@ describe("다리 건너기 테스트", () => {
 
   test("기능 테스트", () => {
     const logSpy = getLogSpy();
-    mockRandoms(["1", "0", "1"]);
+    mockRandoms([1, 0, 1]);
     mockQuestions(["3", "U", "D", "U"]);
 
     const app = new App();
@@ -76,12 +76,15 @@ describe("다리 건너기 테스트", () => {
       "[ O |   | O ]",
       "[   | O |   ]",
       "게임 성공 여부: 성공",
-      "총 시도한 횟수: 1",
+      "총 시도한 횟수: 1"
     ]);
     expectBridgeOrder(log, "[ O |   | O ]", "[   | O |   ]");
   });
 
-  test("예외 테스트", () => {
+  test("예외 테스트1", () => {
     runException(["a"]);
+  });
+  test("예외 테스트2", () => {
+    runException(["1", "2", "1"]);
   });
 });
