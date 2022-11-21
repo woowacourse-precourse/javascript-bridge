19d03e8b1ec61488c0fa8b20fc27097b91743886
Buyoung Chung <sunarol@naver.com>
2022-11-22 00:05:39 +0900

    feat: Add startGame and restartGame
    startGame will be entry point of this game

--- src/Controller/GameController.js
+++ src/Controller/GameController.js
@@ -14,6 +14,31 @@ class GameController {
     this.BridgeGame;
     this.#tries = 1;
   }
+
+  /**
+   * 게임을 시작하는 메소드이다.
+   * InputView의 메소드를 호출하여 해당 메소드의 콜백으로 로직을 수행할 메소드를 전달한다.
+   */
+  startGame() {
+    this.greeting();
+    InputView.readBridgeSize(this.startGameCallback.bind(this));
+  }
+
+  /**
+   * 사용자가 입력을 잘못했을 때 해당 부분부터 게임을 재개하는 메소드이다.
+   * InputView의 메소드를 호출하여 해당 메소드의 콜백으로 로직을 수행할 메소드를 전달한다.
+   */
+  restartGame() {
+    InputView.readBridgeSize(this.startGameCallback.bind(this));
+  }
+
+  startGameCallback(input) {
+    this.constructBridgeGameAndSaveAnswerBridge(input);
+  }
+
+  greeting() {
+    OutputView.printGreeting();
+  }
 }
 
 module.exports = GameController;
