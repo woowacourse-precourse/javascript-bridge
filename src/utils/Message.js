3a7d7aa1932cb91481b55101c9a7b30514f66904
ksg2388 <ksg2388@naver.com>
2022-11-21 19:06:09 +0900

    feat(InputView): 다리 이동 메시지 입력 받기 추가

--- src/utils/Message.js
+++ src/utils/Message.js
@@ -2,12 +2,14 @@ const MESSAGE = {
   io: {
     start: '다리 건너기 게임을 시작합니다.\n',
     bridgeLength: '다리의 길이를 입력해주세요.\n',
+    move: '\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
   },
   error: {
     bridge: {
       type: '숫자만 입력 가능합니다.',
       length: '다리 길이는 3부터 20 사이의 숫자여야 합니다.',
     },
+    move: 'U또는 D만 입력 가능합니다.',
   },
 };
 
