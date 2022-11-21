6de696486d82a1a07a867adf3ebef8baed53e96e
afds4567 <afds4567@gmail.com>
2022-11-21 20:39:16 +0900

    feat: 다리 위치 정보 생성 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+const BridgeMaker = require('./BridgeMaker');
 const InputView = require('./InputView');
 const OutputView = require('./OutputView');
 const { validateReadBridgeSize } = require('./Validate');
+const { generate } = require('./BridgeRandomNumberGenerator');
 
 class App {
   play() {
     OutputView.printStart();
-    InputView.readBridgeSize(validateReadBridgeSize);
+    InputView.readBridgeSize(this.onReadBridgeSize);
+  }
+
+  onReadBridgeSize(size) {
+    validateReadBridgeSize(size);
+    const bridge = BridgeMaker.makeBridge(size, generate);
+    console.log(bridge);
   }
 }
 
