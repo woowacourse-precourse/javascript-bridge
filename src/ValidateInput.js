368e5382645c5e1b332fdcebeceb62b44d088e73
cofla <cofla159@naver.com>
2022-11-21 13:32:38 +0900

    feat: 다리 길이 입력값 유효성 검사

--- src/ValidateInput.js
+++ src/ValidateInput.js
@@ -1,15 +1,14 @@
 /**
  * 입력값 유효성 검사
  */
-const ErrorHandler = require("./ErrorHandler");
 const ValidateInput = {
   /**
    * 다리의 길이가 3~20의 숫자인지 검사
    */
   bridgeSize(size) {
-    if (isNaN(size)) return ErrorHandler.bridgeSize();
+    if (isNaN(size)) return false;
     size = Number(size);
-    if (size < 3 || size > 20) return ErrorHandler.bridgeSize();
+    if (size < 3 || size > 20) return false;
     return true;
   },
 
