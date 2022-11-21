95d4f3af7deab9827f5099b8348c041ee509cd08
Buyoung Chung <sunarol@naver.com>
2022-11-21 15:38:04 +0900

    feat: seperate game success message and tries

--- src/utils/Message.js
+++ src/utils/Message.js
@@ -3,13 +3,17 @@ const Message = {
   ASK_BRIDGE_SIZE: "다리의 길이를 입력해주세요.\n",
   ASK_UPDOWN: "\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
   ASK_REPLAY_CLOSE: "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n",
-  RESULT: "최종 게임 결과",
+  RESULT: "\n최종 게임 결과",
 
-  returnResultMessage(bool, count) {
+  returnResultMessage(bool) {
     if (bool) {
-      return [`게임 성공 여부 : 실패`, `총 시도한 횟수 : ${count}`];
+      return `\n게임 성공 여부: 실패`;
     }
-    return [`게임 성공 여부 : 성공`, `총 시도한 횟수 : ${count}`];
+    return `\n게임 성공 여부: 성공`;
+  },
+
+  returnTries(tries) {
+    return `총 시도한 횟수: ${tries}`;
   },
 };
 
