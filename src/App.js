be68c20c9ec4e073eb3b7b45006082cc17df4b32
Deemou <4106243@naver.com>
2022-11-21 16:58:41 +0900

    feat(App): 다리 길이를 입력받고 처리하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -6,8 +6,17 @@ const OutputView = require('./OutputView');
 const Validator = require('./Validator');
 
 class App {
+  #game;
+
   play() {
     Console.print(MESSAGE.START);
+    InputView.readBridgeSize(this.proceedStepOne.bind(this));
+  }
+
+  proceedStepOne(input) {
+    Validator.validateBridgeSize(input);
+    const game = new BridgeGame(input);
+    this.#game = game;
   }
 }
 
