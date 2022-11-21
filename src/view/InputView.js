07b1e5cc78f1c74e56d3aa0e24b790fcd5240ca4
동동이 <drdragon777@naver.com>
2022-11-21 22:33:52 +0900

    feat - readMoving 및 readGameCommand 메소드 구현

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,5 +1,5 @@
 const { Console } = require("@woowacourse/mission-utils");
-const INPUT = require("../constants/input.constants");
+const { MSG } = require("../constants/input.constants");
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -10,7 +10,7 @@ const InputView = {
    */
   readBridgeSize(callbackArr) {
     const [createGame, ...rest] = callbackArr;
-    Console.readLine(INPUT.BRIDGE_SIZE, (input) => {
+    Console.readLine(MSG.BRIDGE_SIZE, (input) => {
       createGame.call(this, input);
       InputView.readMoving.call(this, rest);
     });
@@ -20,16 +20,24 @@ const InputView = {
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving(callbackArr) {
-    const [movePlayer, ...rest] = callbackArr;
-    Console.readLine(INPUT.MOVE, (input) => {
+    const [movePlayer, getGameState, ...rest] = callbackArr;
+    Console.readLine(MSG.MOVE, (input) => {
       movePlayer.call(this, input);
+      const { isPossible, isEnd } = getGameState.call(this);
+      if (!isEnd && isPossible) InputView.readMoving.bind(this)(callbackArr);
+      if (!isPossible) InputView.readGameCommand.call(this, rest);
     });
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand(callbackArr) {
+    const [gameEndControl] = callbackArr;
+    Console.readLine(MSG.GAME_END, (input) => {
+      gameEndControl.call(this, input);
+    });
+  },
 
   close() {
     Console.close();
