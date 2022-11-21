e49ad94ff55b8352c5db724a246ce4489671e5c8
Vo9Ming <kimminj2017@gmail.com>
2022-11-21 23:11:57 +0900

    readMoving(), checkSign()함수를 통해 사용자에게 이동할 칸(위 칸 - U, 아래 칸 -D)을 입력받은 후 그 값이 올바른 값인지 확인한다.

--- __tests__/BridgeTest.js
+++
@@ -1,9 +0,0 @@
-const InputView = require("../src/InputView");
-
-describe("다리 게임 테스트", () => {
-  test("다리 길이 생성 테스트", () => {
-    expect(() => {
-      InputView.checkSize(1);
-    }).toThrow("[ERROR]");
-  });
-});
--- src/InputView.js
+++ src/InputView.js
@@ -1,35 +1,51 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
-  constructor() {
-    this.size = 0;
-  },
   /**
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize() {
-    MissionUtils.Console.readLine("다리의 길이를 입력해주세요.", (length) => {
-      this.size = length;
-      this.checkSize(this.size);
-    });
+    MissionUtils.Console.readLine(
+      "다리의 길이를 입력해주세요. \n",
+      (length) => {
+        const size = parseInt(length);
+        this.checkSize(size);
+        this.readMoving();
+      }
+    );
   },
 
   checkSize(size) {
-    if (size < 3 || size > 20) {
+    if (size < 3 || size > 20 || size !== Number(size)) {
       MissionUtils.Console.print(
         "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다."
       );
       this.readBridgeSize();
     }
-    return size;
   },
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  //readMoving() {},
+  readMoving() {
+    MissionUtils.Console.readLine(
+      "이동할 칸을 선택해주세요. (위: U, 아래: D) \n",
+      (location) => {
+        //console.log(location);
+        this.checkSign(location);
+      }
+    );
+  },
+
+  checkSign(location) {
+    if (location !== "U" && location !== "D") {
+      MissionUtils.Console.print(
+        "[ERROR] 올바른 값이 아닙니다. U 또는 D로 답변해주세요."
+      );
+      this.readMoving();
+    }
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
