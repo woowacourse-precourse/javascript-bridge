9fa9156dd5f2e8ac02a061c8d0fcdd26ca89ca98
seungki <sysh9498@gmail.com>
2022-11-22 00:05:23 +0900

    feat: 최종 게임 결과 출력 기능

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,5 +1,6 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const { Console } = MissionUtils;
+const ConstValue = require('./ConstValue');
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -10,8 +11,7 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printMap(bridgeArray) {
-    Console.print(bridgeArray[0]);
-    Console.print(bridgeArray[1]);
+    Console.print(bridgeArray[0] + '\n' + bridgeArray[1]);
   },
 
   /**
@@ -19,7 +19,12 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() {},
+  printResult(bridgeArray, tryCount) {
+    Console.print('최종 게임 결과\n' + bridgeArray[0] + '\n' + bridgeArray[1]);
+
+    Console.print('게임 성공 여부: 성?');
+    Console.print('총 시도한 횟수: ' + tryCount);
+  },
 
   printStartMessage() {
     Console.print(ConstValue.START_MESSAGE);
