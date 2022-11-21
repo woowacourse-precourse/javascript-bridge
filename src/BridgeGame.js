8653e110564100fbe1e425f1d7d78ccc350c866d
chsua <csj1919@naver.com>
2022-11-21 14:51:28 +0900

    feat: 숫자를 받아 다리 만드는 함수(BridgeGame.js)

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,7 +1,25 @@
+const { generate } = require("./BridgeRandomNumberGenerator");
+const { makeBridge } = require("./BridgeMaker");
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
-class BridgeGame {
+ class BridgeGame {
+
+  constructor() {
+    this.currSpot = 0 ;
+    this.tryCount = 1 ;
+    this.bridge ;
+  }
+
+  /**
+   * User에게 숫자를 받아 정답다리를 만드는 함수
+   * @param {number} num User에게 받은 다리의 길이
+   */
+   makeRandomBridge(num){
+    this.num = num ;
+    this.bridge =  makeBridge(num, generate) ; //ex. ["U","D","D"]
+  }
+
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
