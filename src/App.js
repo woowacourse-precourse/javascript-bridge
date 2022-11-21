cf1f1a611544a863c0724e5c5043b22e352f8035
h2002y <h2002y@naver.com>
2022-11-21 17:00:57 +0900

    feat: Add setBridge func & prepareGame func in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+const BridgeGame = require("./BridgeGame");
+const InputView = require("./InputView");
 const OutputView = require("./OutputView");
+
 
 class App {
   play() {
     OutputView.printGameStart();
+    const BridgeGame = new BridgeGame();
+    this.prepareGame(BridgeGame);
+  }
+
+  prepareGame(BridgeGame) {
+    this.inputBridgeSize(BridgeGame);
+    this.setBridge(BridgeGame);
+  }
+
+  inputBridgeSize(BridgeGame) {
+    InputView.readBridgeSize(BridgeGame);
+  }
+
+  setBridge(BridgeGame) {
+    BridgeGame.setBridge();
   }
 }
 
