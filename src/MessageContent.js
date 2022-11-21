17d7bc4549834ed6c22618cf0b815a4628ad5978
xownsdl0 <xownsdl0@naver.com>
2022-11-21 23:10:45 +0900

    style: MessageContent내 문구 줄바꿈 수정

--- src/MessageContent.js
+++ src/MessageContent.js
@@ -1,9 +1,9 @@
 const MESSAGE = {
   BRIDGE_SIZE: '다리의 길이를 입력해주세요.\n',
   MOVE: '\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
-  END_GAME: '\n최종 게임 결과\n',
-  SUCCESSS: '게임 성공 여부: 성공\n',
-  FAILURE: '\n게임 성공 여부: 실패\n',
+  END_GAME: '\n최종 게임 결과',
+  SUCCESSS: '게임 성공 여부: 성공',
+  FAILURE: '게임 성공 여부: 실패',
   TRY: '총 시도한 횟수: ',
 }
 
