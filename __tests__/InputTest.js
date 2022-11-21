f164b292dcee524ee9ebb80f98b8c1c442f2f044
hailey-hy <lillyine.hy@gmail.com>
2022-11-21 20:36:50 +0900

    test(__test__\InputTest): 다리 이동 입력값 예외 처리 테스트 작성

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -3,8 +3,17 @@ const Validator = require('../src/utils/Validator');
 describe("입력값 테스트", () => {
     test("다리 길이로 3~20 사이가 아닌 값을 입력하면 예외 처리한다.", () => {
         expect(() => {
-            Validator.validateBridgeSize(30)}).toThrow(
-            '[ERROR] 잘못된 입력입니다.'
+            Validator.validateBridgeSize(30)
+        }).toThrow(
+            '[ERROR] 3 ~ 20 사이 숫자만 입력할 수 있습니다.'
         );
-    })
+    });
+
+    test("다리 이동시 U나 D가 아닌 값을 입력하면 예외 처리한다.", () => {
+        expect(() => {
+            Validator.validateMoving(3)
+        }).toThrow(
+            '[ERROR] U 또는 D만 입력할 수 있습니다.'
+        );
+    });
 });
