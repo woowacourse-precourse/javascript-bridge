43c34e12198b9a6f230dd7a602a6d9b7a0b2b4ea
jangsumin <jsm0726@naver.com>
2022-11-21 12:56:50 +0900

    feat: make a new bridge game and confirm bridge throughout console

--- src/BridgeGameController.js
+++ src/BridgeGameController.js
@@ -1,9 +1,13 @@
+const BridgeGame = require("./BridgeGame");
 const BridgeMaker = require("./BridgeMaker");
 const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
 
 class BridgeGameController {
+  #bridgeGame;
+
   makeBridge(size) {
     const bridge = BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate);
+    this.#bridgeGame = new BridgeGame(bridge);
   }
 }
 
