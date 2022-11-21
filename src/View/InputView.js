4864e96f46db0c8770c75ce46bf47c7d90c9d3c1
oio337a <oio337a@gmail.com>
2022-11-21 21:15:31 +0900

    feat : add print message

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -1,5 +1,6 @@
 const { Console } = require('@woowacourse/mission-utils');
 const { MESSAGE } = require('../../constants/Message');
+const { printMessage } = require('./OutputView');
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
