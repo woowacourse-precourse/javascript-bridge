73f3c4453ad0f3da3fa01dc8456b434a28e89064
jiyun1006 <jiyun4251@gmail.com>
2022-11-21 23:53:21 +0900

    feat : Promise를 이용한 콜백함수 수정.
    이전에 콜백함수 안에 계속해서 갇히는 형태의 함수에서
    Promise를 이용해서 return값들을 제대로 받을 수 있게 됨.

--- src/InputView.js
+++ src/InputView.js
@@ -2,26 +2,32 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const Script = require("./Script");
 const BridgeMaker = require("./BridgeMaker");
 const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+const OutputView = require("./OutputView");
+const START = 1;
 
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize() {
-    MissionUtils.Console.readLine(`${Script.BRIDGELENGTHINPUT}\n`, (answer) => {
-      const ans = BridgeMaker.makeBridge(answer, BridgeRandomNumberGenerator);
-      InputView.readMoving();
-      return ans;
+    return new Promise(resolve => {
+      MissionUtils.Console.readLine(`${Script.BRIDGELENGTHINPUT}\n`, (answer) => {
+        const bridge = BridgeMaker.makeBridge(answer, BridgeRandomNumberGenerator);
+        resolve(bridge);
+      })
     })
   },
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving() {
-    MissionUtils.Console.readLine(`\n${Script.CHOICEBRIDGE}\n`, (answer) => {
-      
+    return new Promise(resolve => {
+      MissionUtils.Console.readLine(`\n${Script.CHOICEBRIDGE}\n`, (movingDirection) => {
+        resolve(movingDirection);
+      })
     })
   },
+  
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
