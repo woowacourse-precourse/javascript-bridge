d2a7612f3b3c4ab9b2ef42e2a2c9480c303c42a7
jongwonp <jw032876@gmail.com>
2022-11-21 19:52:07 +0900

    fix: 바뀐 예제 테스트 코드에 맞게 다시 일치연산자 사용

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -8,8 +8,8 @@ const BridgeMaker = {
     const bridge = [];
     for (let i = 0; i < size; i++) {
       const randomNumber = generateRandomNumber();
-      if (randomNumber == 0) bridge.push('D');
-      if (randomNumber == 1) bridge.push('U');
+      if (randomNumber === 0) bridge.push('D');
+      if (randomNumber === 1) bridge.push('U');
     }
     return bridge;
   },
