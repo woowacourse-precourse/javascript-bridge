246267bd64ccca9eded2d8dff53b488b6ee07863
Jin1won <wlsdnjs16521@gmail.com>
2022-11-22 00:05:51 +0900

    test: isMoveValid 함수 테스트코드 작성

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,4 +1,4 @@
-const { isBridgeLengthValid } = require("../src/utils/index");
+const { isBridgeLengthValid, isMoveValid } = require("../src/utils/index");
 
 describe("isBridgeLengthValid 함수 테스트", () => {
   test("공백을 입력하면 예외가 발생한다.", () => {
@@ -17,5 +17,19 @@ describe("isBridgeLengthValid 함수 테스트", () => {
     expect(() => {
       isBridgeLengthValid(" ");
     }).toThrow("[ERROR] 3~20 범위의 값만 입력해주세요.");
+  });
+});
+
+describe("isMoveValid 함수 테스트", () => {
+  test("공백을 입력하면 예외가 발생한다.", () => {
+    expect(() => {
+      isMoveValid("");
+    }).toThrow("[ERROR] 공백이 아닌 값을 입력해주세요.");
+  });
+
+  test("옵션에 해당하지 않은 알파벳을 입력하면 예외가 발생한다.", () => {
+    expect(() => {
+      isMoveValid("a");
+    }).toThrow("[ERROR] U 또는 D 값만 입력해주세요.");
   });
 });
