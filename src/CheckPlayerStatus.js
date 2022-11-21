b2ac7886a90cdffd2ae2f696be91211c33ffbbb5
SWARVY <swarvy0826@naver.com>
2022-11-22 02:02:41 +0900

    refactor : 조건문 캡슐화

--- src/CheckPlayerStatus.js
+++ src/CheckPlayerStatus.js
@@ -6,16 +6,14 @@ const CheckPlayerStatus = {
    * 이동이 성공했는지 판별하는 메서드
    */
   isPlayerPassed(move, stage) {
-    if (move !== stage) return false;
-    return true;
+    return move === stage;
   },
 
   /**
    * 클리어에 성공했는지 판별하는 메서드
    */
   isPlayerCleared(endStage, currentStage, isPassed) {
-    if (endStage === currentStage && isPassed) return true;
-    return false;
+    return endStage === currentStage && isPassed;
   },
 };
 
