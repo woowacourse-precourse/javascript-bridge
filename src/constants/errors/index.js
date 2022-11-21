790536264d6ba97d2d40d19e42efb0a162dd59fe
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-11-21 13:19:03 +0900

    feat($errors): 재시작 입력 값 유효성 검사 상수 추

--- src/constants/errors/index.js
+++ src/constants/errors/index.js
@@ -1,4 +1,9 @@
 const IS_SIZE_INPUT_ERROR = '[ERROR] 다리의 길이는 3 이상 20 이하의 숫자여야 합니다.';
 const IS_MOVING_INPUT_ERROR = '[ERROR] U 또는 D를 입력해주세요.';
+const IS_RETRY_INPUT_ERROR = '[ERROR] R 또는 Q를 입력해주세요.';
 
-module.exports = { IS_SIZE_INPUT_ERROR, IS_MOVING_INPUT_ERROR };
+module.exports = {
+  IS_SIZE_INPUT_ERROR,
+  IS_MOVING_INPUT_ERROR,
+  IS_RETRY_INPUT_ERROR,
+};
