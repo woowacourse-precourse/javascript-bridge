afede801b17af488af392b25fd3d1e665fddd8e3
ukkodeveloper <ukkodeveloper@gmail.com>
2022-11-22 08:30:47 +0900

    refactor: Validator 예외 검증시 공백은 제외하고 검사하는 것으로 수정

--- src/Validator.js
+++ src/Validator.js
@@ -1,11 +1,11 @@
 class Validator {
-  REGEXES = new Map([
+  REGEX_MAP = new Map([
     ['bridgeSize', /^([3-9]|1[0-9]|20)$/],
     ['moving', /^[UD]$/],
     ['gameCommand', /^[RQ]$/],
   ]);
 
-  ERROR_MESSAGES = new Map([
+  ERROR_MESSAGE_MAP = new Map([
     ['bridgeSize', '[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.'],
     ['moving', `[ERROR] 다리 이동에 대한 입력은 'U'와 'D'만 가능합니다.`],
     ['gameCommand', `[ERROR] 게임 재시도 및 종료에 대한 입력은 'R'과 'Q'만 가능합니다.`],
@@ -13,14 +13,20 @@ class Validator {
   ]);
 
   check(category) {
-    if (!this.REGEXES.has(category)) {
-      throw new Error(this.ERROR_MESSAGES.get(category), `(${category})`);
+    if (!this.REGEX_MAP.has(category)) {
+      throw new Error(this.ERROR_MESSAGE_MAP.get(category), `(${category})`);
     }
 
     return (input) => {
-      const regex = this.REGEXES.get(category);
-      if (!regex.test(input)) throw new Error(this.ERROR_MESSAGES.get(category));
+      const testSubject = this.eraseWhiteSpace(input);
+      const regex = this.REGEX_MAP.get(category);
+      if (!regex.test(testSubject)) throw new Error(this.ERROR_MESSAGE_MAP.get(category));
     };
+  }
+
+  eraseWhiteSpace(input) {
+    let result = input;
+    return result.replace(' ', '');
   }
 
   checkBridgeSize = this.check('bridgeSize');
