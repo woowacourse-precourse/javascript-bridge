ec157ff2cee856fbf3eec945fea436968895bd15
moseung <tmdah900@naver.com>
2022-11-21 20:37:33 +0900

    ✅ test : 기능4 테스트구현 -기능4 구현에 따른 관련 클래스들 테스트 구현

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -1,4 +1,4 @@
-// const BridgeGame = require("../src/BridgeGame");
+const BridgeGame = require("../src/BridgeGame");
 const Validation = require("../src/utils/Validation");
 const Constant = require("../src/constant/Constant");
 const { ERROR_MESSAGE } = Constant;
@@ -10,5 +10,27 @@ describe("도메인 기능(BridgeGame) 유닛테스트", () => {
     expect(() => Validation.isBridgeWords(userInput)).toThrow(
       ERROR_MESSAGE.IS_BRIDGE_WORDS
     );
+  });
+
+  test("BridgeGame move method테스트, 값을 제대로 넣으면 true를 리턴", () => {
+    const bridgeGame = new BridgeGame();
+    bridgeGame.bridge = ["U", "D", "U"];
+    expect(bridgeGame.move("U")).toBe(true);
+  });
+
+  test("BridgeGame move method테스트, 잘못된 값일시 false를 리턴", () => {
+    const bridgeGame = new BridgeGame();
+    bridgeGame.bridge = ["U", "D", "U"];
+    expect(bridgeGame.move("D")).toBe(false);
+  });
+
+  test("BridgeGame retry method테스트", () => {
+    const bridgeGame = new BridgeGame();
+    expect(bridgeGame.retry("R")).toBe("R");
+  });
+
+  test("BridgeGame retry method테스트", () => {
+    const bridgeGame = new BridgeGame();
+    expect(bridgeGame.retry("Q")).toBe("Q");
   });
 });
