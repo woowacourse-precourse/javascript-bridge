75ffaa5c0f284ce6ec34421932c655445fc2571b
cys4585 <choizerowater@gmail.com>
2022-11-21 13:55:19 +0900

    test: 테스트 코드 수정 (move함수 리턴 값 삭제, 해당 기능 별도로 함수화)

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -16,9 +16,12 @@ describe("BridgeGame 테스트", () => {
 
   test("판정 테스트 (이동한 칸이 이동가능한 칸인지)", () => {
     const bridgeGame = new BridgeGame(["U", "D", "U"]);
-    expect(bridgeGame.move("U")).toEqual(true);
-    expect(bridgeGame.move("U")).toEqual(false);
-    expect(bridgeGame.move("D")).toEqual(false);
+    bridgeGame.move("U")
+    expect(bridgeGame.isSuccessMoved()).toEqual(true);
+    bridgeGame.move("U")
+    expect(bridgeGame.isSuccessMoved()).toEqual(false);
+    bridgeGame.move("D")
+    expect(bridgeGame.isSuccessMoved()).toEqual(false);
   })
 
   test("끝까지 도착했는지 테스트 (성공 테스트)", () => {
