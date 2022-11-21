a3455590d25952b7ccb4f8984ce3960e7fae5587
ashleysyheo <ashleysyheo@gmail.com>
2022-11-21 22:23:14 +0900

    feat: move in bridge game

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,13 +1,33 @@
+const { PARAMETERS } = require('./utils/constants');
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  constructor(bridge) {
+    this.init(bridge);
+    this.gameCount = 1;
+  }
+
+  init(bridge) {
+    this.top = [];
+    this.bottom = [];
+    this.bridge = bridge;
+    this.moveCount = 0;
+  }
+
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(moveDirection) {
+    if (this.bridge[this.moveCount] === moveDirection) {
+      // check direction: top or bottom and deliver movable mark 
+    } else {
+      // check direction: top or bottom and deliver immovable mark 
+    }
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
