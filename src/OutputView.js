35effc1427e35866b87c935fd34795f366156e01
sojinjang <jsojin6119@gmail.com>
2022-11-22 04:02:28 +0900

    :sparkles: feat(view): 게임 종료 문구 보여주기

--- src/OutputView.js
+++ src/OutputView.js
@@ -2,9 +2,6 @@ const { Console } = require("@woowacourse/mission-utils");
 const InfoMessages = require("./constants/InfoMessages.js");
 const Values = require("./constants/Values.js");
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
 const OutputView = {
   printGameStart() {
     Console.print(InfoMessages.OPENING_REMARKS);
@@ -18,16 +15,17 @@ const OutputView = {
     Console.print(Values.BLANK);
   },
 
-  /**
-   * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-   * <p>
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  printResult() {
+  printResult(stepObj, isSuccess, numOfTrials) {
     Console.print(InfoMessages.GAME_RESULTS);
-    Console.print(InfoMessages.GAME_FAILED);
-    Console.print(InfoMessages.GAME_SUCCESS);
-    Console.print(InfoMessages.TOTAL_ATTEMPTS);
+    this.printMap(stepObj);
+    this.showSuccessOrFailure(isSuccess);
+    Console.print(InfoMessages.TOTAL_ATTEMPTS + numOfTrials);
+    Console.close();
+  },
+
+  showSuccessOrFailure(isSuccess) {
+    if (isSuccess) return Console.print(InfoMessages.GAME_SUCCESS);
+    return Console.print(InfoMessages.GAME_FAILED);
   },
 };
 
