e49c077e0f553109462f6d6fe5b307d727eefe4c
NaveOWO <pianowell96@gmail.com>
2022-11-22 00:55:20 +0900

    [ refactor ] 다리 생성 로직 & 출력로직 분리

--- src/OutputView.js
+++ src/OutputView.js
@@ -10,26 +10,45 @@ const OutputView = {
    */
 
   printMap(currentBridge) {
-    const bridgeMap = ["[", "["];
-    for (let i = 0; i < currentBridge.length; i++) {
-      if (currentBridge[i][0] === "U") {
-        bridgeMap[0] += ` ${currentBridge[i][1]} |`;
-        bridgeMap[1] += "   |";
-      }
-      if (currentBridge[i][0] === "D") {
-        bridgeMap[0] += "   |";
-        bridgeMap[1] += ` ${currentBridge[i][1]} |`;
-      }
-    }
-
-    bridgeMap[0] = bridgeMap[0].substring(0, bridgeMap[0].length - 1);
-    bridgeMap[1] = bridgeMap[1].substring(0, bridgeMap[1].length - 1);
-
-    bridgeMap[0] += "]";
-    bridgeMap[1] += "]";
+    const bridgeMap = this.createBridgeMap(currentBridge);
+    this.transformBridgeMap(bridgeMap);
 
     Console.print(bridgeMap[0]);
     Console.print(bridgeMap[1]);
+  },
+
+  createBridgeMap(currentBridge) {
+    let bridgeMap = ["[", "["];
+    for (let i = 0; i < currentBridge.length; i++) {
+      this.createSinglBridge(currentBridge, bridgeMap, i);
+    }
+    return bridgeMap;
+  },
+
+  createSinglBridge(currentBridge, bridgeMap, index) {
+    if (currentBridge[index][0] === "U") {
+      bridgeMap[0] += ` ${currentBridge[index][1]} |`;
+      bridgeMap[1] += "   |";
+    }
+    if (currentBridge[index][0] === "D") {
+      bridgeMap[0] += "   |";
+      bridgeMap[1] += ` ${currentBridge[index][1]} |`;
+    }
+  },
+
+  sliceLastString(targetStr) {
+    return targetStr.substring(0, targetStr.length - 1);
+  },
+
+  addLastCloseMark(targetStr) {
+    return (targetStr += "]");
+  },
+
+  transformBridgeMap(bridgeMap) {
+    bridgeMap[0] = this.sliceLastString(bridgeMap[0]);
+    bridgeMap[1] = this.sliceLastString(bridgeMap[1]);
+    bridgeMap[0] = this.addLastCloseMark(bridgeMap[0]);
+    bridgeMap[1] = this.addLastCloseMark(bridgeMap[1]);
   },
 
   /**
@@ -37,17 +56,19 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult(state, round, currentBridge) {
-    Console.print("최종 게임 결과");
-    if (state === "성공") {
-      this.printMap(currentBridge);
-    }
-    if (state === "실패") {
-      this.printMap(currentBridge);
-    }
+  printResult(state, bridge) {
+    this.printFianlResult(bridge);
+    this.printGameSummury(state, bridge);
+  },
 
+  printFianlResult(bridge) {
+    Console.print("최종 게임 결과");
+    this.printMap(bridge.getCurrentBridge());
+  },
+
+  printGameSummury(state, bridge) {
     Console.print(`게임 성공 여부: ${state}`);
-    Console.print(`총 시도한 횟수: ${round}`);
+    Console.print(`총 시도한 횟수: ${bridge.getRound()}`);
   },
 };
 
