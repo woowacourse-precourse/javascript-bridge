ca2fc7ba022b64e668678c625cb47db1bd5b1520
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-21 09:46:35 +0900

    test(BuilderTest): 테스트 파일 삭제 및 해당 테스트 케이스 ValidationTest로 이동
    Builder 클래스를 삭제함에 따라 테스트 케이스도 이동

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -4,6 +4,8 @@ const {
   DOWNSIDE_SYMBOL,
   QUIT_COMMAND,
   RESTART_COMMAND,
+  BRIDGE_SIZE_MAX_RANGE,
+  BRIDGE_SIZE_MIN_RANGE,
 } = require('../src/constants/condition.js');
 const { ERROR_MSG } = require('../src/constants/message.js');
 
@@ -46,5 +48,45 @@ describe('Validation 클래스 테스트', () => {
         }).not.toThrow();
       }
     );
+  });
+
+  describe('validateSize 메서드 테스트', () => {
+    test('입력이 빈 값인 경우, 에러가 발생한다.', () => {
+      const input = '';
+
+      expect(() => {
+        Validation.validateSize(input);
+      }).toThrow(ERROR_MSG.emptyInput);
+    });
+
+    test.each([['   '], ['  1'], ['asd'], ['일'], ['!']])(
+      '입력에 숫자가 아닌 값이 있을 경우, 에러가 발생한다.',
+      (input) => {
+        expect(() => {
+          Validation.validateSize(input);
+        }).toThrow(ERROR_MSG.invalidInputType);
+      }
+    );
+
+    test.each([['012'], ['0012']])('입력이 0으로 시작할 경우, 에러가 발생한다.', (input) => {
+      expect(() => {
+        Validation.validateSize(input);
+      }).toThrow(ERROR_MSG.isStartedZero);
+    });
+
+    test.each([['1'], ['100']])(
+      `입력이 ${BRIDGE_SIZE_MIN_RANGE} ~ ${BRIDGE_SIZE_MAX_RANGE} 사이 숫자가 아닐 경우, 에러가 발생한다.`,
+      (input) => {
+        expect(() => {
+          Validation.validateSize(input);
+        }).toThrow(ERROR_MSG.invalidInputRange);
+      }
+    );
+
+    test.each([['10'], ['14']])('입력이 유효한 경우 에러가 발생하지 않는다.', (input) => {
+      expect(() => {
+        Validation.validateSize(input);
+      }).not.toThrow();
+    });
   });
 });
