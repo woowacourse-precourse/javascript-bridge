4b12fb54e3182f3061953f7e119ae739e04a3bd6
vjvl95 <vjvl95@naver.com>
2022-11-21 14:10:39 +0900

    fix : 메소드 간 공백 추가

--- src/Bridge.js
+++ src/Bridge.js
@@ -5,12 +5,15 @@ class Bridge {
   pushUpBridge(word) {
     this.#upBridge.push(word);
   }
+
   pushDownBridge(word) {
     this.#downBridge.push(word);
   }
+
   getUpBridge() {
     return this.#upBridge;
   }
+
   getDownBridge() {
     return this.#downBridge;
   }
