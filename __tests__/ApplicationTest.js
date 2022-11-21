cdd389b5f447f88d70fcf7f18c580959eb4f1c8a
mochang2 <qjsrodksro@gmail.com>
2022-11-21 14:14:40 +0900

    refactor: 여러 테스트에서 사용되는 util 분리

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,55 +1,13 @@
-const MissionUtils = require('@woowacourse/mission-utils')
-const App = require('../src/App')
 const BridgeMaker = require('../src/BridgeMaker')
-
-const mockQuestions = (answers) => {
-  MissionUtils.Console.readLine = jest.fn()
-  answers.reduce((acc, input) => {
-    return acc.mockImplementationOnce((_, callback) => {
-      callback(input)
-    })
-  }, MissionUtils.Console.readLine)
-}
-
-const mockRandoms = (numbers) => {
-  MissionUtils.Random.pickNumberInRange = jest.fn()
-  numbers.reduce((acc, number) => {
-    return acc.mockReturnValueOnce(number)
-  }, MissionUtils.Random.pickNumberInRange)
-}
-
-const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, 'print')
-  logSpy.mockClear()
-  return logSpy
-}
-
-const getOutput = (logSpy) => {
-  return [...logSpy.mock.calls].join('')
-}
-
-const runException = (inputs) => {
-  mockQuestions(inputs)
-  const logSpy = getLogSpy()
-  const app = new App()
-
-  app.play()
-
-  expectLogContains(getOutput(logSpy), ['[ERROR]'])
-}
-
-const expectLogContains = (received, logs) => {
-  logs.forEach((log) => {
-    expect(received).toEqual(expect.stringContaining(log))
-  })
-}
-
-const expectBridgeOrder = (received, upside, downside) => {
-  const upsideIndex = received.indexOf(upside)
-  const downsideIndex = received.indexOf(downside)
-
-  expect(upsideIndex).toBeLessThan(downsideIndex)
-}
+const {
+  mockQuestions,
+  mockRandoms,
+  getLogSpy,
+  getOutput,
+  runException,
+  expectBridgeOrder,
+  expectLogContains,
+} = require('./lib/utils')
 
 describe('다리 건너기 테스트', () => {
   test('다리 생성 테스트', () => {
