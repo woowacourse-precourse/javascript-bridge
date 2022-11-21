b60209263bcca60843b24423bdc27608d23ccea8
KINHYEONJI <hyeonji0718@gmail.com>
2022-11-21 23:35:05 +0900

    fix(App) : 생성된 다리로 bridgeGame을 생성하기 위해 함수 playCallback 생성
    - 필드에 bridge, bridgeGame 추가
    - playCallback에서 생성된 다리를 bridgeGame 생성자 인자로 하여 bridgeGame 생성

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 const InputView = require("./InputView");
 const OutputView = require("./OutputView");
 const BridgeMaker = require("./BridgeMaker");
+const BridgeGame = require("./BridgeGame");
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
+  #bridge;
+  #bridgeGame;
+
   constructor() {
     this.inputView = InputView;
     this.outputView = OutputView;
     this.bridgeMaker = BridgeMaker;
   }
 
+  get bridgeGame() {
+    return this.#bridgeGame;
+  }
+
   play() {
     this.outputView.print("다리 건너기 게임을 시작합니다.");
     this.inputView.readBridgeSize(
-      this.bridgeMaker.makeBridge.bind(this.bridgeMaker),
+      this.playCallback.bind(this),
       "다리의 길이를 입력해주세요. "
     );
+  }
+
+  playCallback(bridgeSize) {
+    this.#bridge = this.bridgeMaker.makeBridge(
+      bridgeSize,
+      BridgeRandomNumberGenerator.generate
+    );
+    this.#bridgeGame = new BridgeGame(this.#bridge);
   }
 }
 
