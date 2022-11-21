97bd813d4b5849c0edc9cc4c0e5c7cd7478ad9d9
HeoJiye <heoze1031@gmail.com>
2022-11-21 15:25:44 +0900

    feat: 다리 길이 입력 값이 숫자가 아닌 경우 오류 처리 추가

--- src/InputView.js
+++ src/InputView.js
@@ -1,14 +1,16 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const Vaildation = require("./Vaildation");
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
-   */
+   */  
   readBridgeSize( callback ) {
     MissionUtils.Console.readLine('다리의 길이를 입력해주세요.', (answer) => {
       const size = Number(answer);
+      vaildation_bridgeSize(size);
       
       callback(size);
     });
@@ -17,12 +19,16 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() { },
+  readMoving() {},
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand() {},
 };
+
+function vaildation_bridgeSize(size) {
+  Vaildation.isNumber(size);
+}
 
 module.exports = InputView;
---
+++ src/Vaildation.js
@@ -0,0 +1,9 @@
+const Vaildation =  {
+    isNumber(input) {
+        if (isNaN(money)) {
+            throw new Error(`[ERROR] 입력값 ${input}은 숫자가 아닙니다.`);
+        }
+    }
+}
+
+module.exports = Vaildation;
