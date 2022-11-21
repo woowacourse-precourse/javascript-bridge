4961270cfc543256b7c9cd041eb66c3cbf8adb3d
안호진 <asdv94@naver.com>
2022-11-21 22:48:57 +0900

    test(validator): 숫자 범위 유효성 검사 테스트 작성

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -1,5 +1,7 @@
 const { ERROR_MSG } = require('../src/Constant');
 const Validator = require('../src/Validator');
+const { validateNumber, validateNumberRange } = Validator;
+const { prefix, isNotANumber, isOutOfRange } = ERROR_MSG;
 
 describe('유효성 검사 테스트', () => {
   test('숫자 이외의 값이 들어오면 예외가 발생한다.', () => {
@@ -7,13 +9,31 @@ describe('유효성 검사 테스트', () => {
     const validInput2 = 30;
     const invalidInput = /\D+/;
     expect(() => {
-      Validator.validateNumber(validInput1);
+      validateNumber(validInput1);
     }).not.toThrow();
     expect(() => {
-      Validator.validateNumber(validInput2);
+      validateNumber(validInput2);
     }).not.toThrow();
     expect(() => {
-      Validator.validateNumber(invalidInput);
-    }).toThrow(ERROR_MSG.prefix + ERROR_MSG.isNotANumber);
+      validateNumber(invalidInput);
+    }).toThrow(prefix + isNotANumber);
+  });
+  test('3 미만, 20 초과 값이 들어오면 예외가 발생한다.', () => {
+    const validNumber1 = 3;
+    const validNumber2 = 20;
+    const invalidNumber1 = 2;
+    const invalidNumber2 = 21;
+    expect(() => {
+      validateNumberRange(validNumber1);
+    }).not.toThrow();
+    expect(() => {
+      validateNumberRange(validNumber2);
+    }).not.toThrow();
+    expect(() => {
+      validateNumberRange(invalidNumber1);
+    }).toThrow(prefix + isOutOfRange);
+    expect(() => {
+      validateNumberRange(invalidNumber2);
+    }).toThrow(prefix + isOutOfRange);
   });
 });
