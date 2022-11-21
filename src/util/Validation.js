f8002502db5771b91efb69c2973c2e97932c571c
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2022-11-21 19:18:00 +0900

    feat(Validation.js) : input값 검증로직 분리

---
+++ src/util/Validation.js
@@ -0,0 +1,30 @@
+
+const validateBridge = (bridgeInput) => {
+    try {
+        if (Number.isNaN(bridgeInput)) throw new Error("[ERROR] 다리길이는 숫자여야 합니다.");
+        if (!(3 <= bridgeInput && bridgeInput <= 20)) throw new Error("[ERROR] 다리길이는 3에서 20사이의 숫자여야 합니다.");
+    } catch (err) {
+        Console.print(err.message);
+        return true;
+    }
+}
+
+const validateMove = (move) => {
+    try {
+        if (!(move === "U" || move === "D")) throw new Error("[ERROR] 이동할 칸을 잘못 입력하셨습니다.");
+    } catch (err) {
+        Console.print(err.message);
+        return true;
+    }
+}
+
+const validateStart = (start) => {
+    try {
+        if (!(start === "R" || start === "Q")) throw new Error("[ERROR] 다시시도 여부를 잘못 입력하셨습니다.");
+    } catch (err) {
+        Console.print(err.message);
+        return true;
+    }
+}
+
+module.exports = {validateBridge, validateMove, validateStart};
