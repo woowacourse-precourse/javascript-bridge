148022c4f06bef06bed8cabd46ca4d7cae73d6c5
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2022-11-21 17:06:22 +0900

    feat(BridgeGame): add for generate bridge

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,7 +1,12 @@
+const BridgeBoard = require("./BridgeBoard");
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  #bridgeBoard;
+  constructor() {
+    this.#bridgeBoard = new BridgeBoard();
+  }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
@@ -15,6 +20,9 @@ class BridgeGame {
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   retry() {}
+  play(size) {
+    this.#bridgeBoard.makeBoard(size);
+  }
 }
 
 module.exports = BridgeGame;
