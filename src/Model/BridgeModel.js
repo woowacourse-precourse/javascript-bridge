a7878d89e3b297cc33b7ac4061e3dfd1f6e4019f
wonkyum-kim <dodo12120@naver.com>
2022-11-21 23:01:47 +0900

    feat(BridgeModel): make new BridgeModel.js
    - BridgeModel has some constants for BridgeGame

---
+++ src/Model/BridgeModel.js
@@ -0,0 +1,10 @@
+const BridgeModel = {
+  ErrorMessages: [
+    '\n[ERROR] 유효하지 않은 다리 길이입니다.',
+    '\n[ERROR] U 또는 D를 입력하세요.',
+    '\n[ERROR] R 또는 Q를 입력하세요.'],
+
+  bridge: null,
+};
+
+module.exports = BridgeModel;
