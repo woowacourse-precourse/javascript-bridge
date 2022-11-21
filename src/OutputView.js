9de826a7a307e85ce86971e642929fa091ec96ab
Jeong Hun Nam <njh3421@gmail.com>
2022-11-21 11:50:20 +0900

    feat : add initailizeArray()

--- src/OutputView.js
+++ src/OutputView.js
@@ -6,18 +6,18 @@ const bridge=new BridegGame();
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
-let bridgePrinterAbove=[]
-let bridgePrinterBelow=[]
+bridge.bridgePrinterAbove=[]
+bridge.bridgePrinterBelow=[]
+let bridgePrinterAbove=bridge.bridgePrinterAbove
+let bridgePrinterBelow=bridge.bridgePrinterBelow
 const OutputView = {
   /**
    * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap(userSpace,bridgeArray,count) {
-    // count를 추가해주면서 idx를 추가
-    const correctValue=this.printMapHelper(userSpace,bridgeArray,count)
-    // console.log(userSpace,'Outputview',bridgeArray,count);
+  printMap(userSpace,bridgeArray,restart) {
+    const correctValue=this.printMapHelper(userSpace,bridgeArray,restart)
     return correctValue
   },
   printMapHelper(userSpace,bridgeArray){
@@ -41,6 +41,10 @@ const OutputView = {
       bridgePrinterBelow.push('empty')
       return 'X'
     }
+  },
+  initializeArray(){
+    bridgePrinterAbove=[]
+    bridgePrinterBelow=[]
   },
   printArrays(bridgePrinterAbove,bridgePrinterBelow){
     let bridgeAbove=''
