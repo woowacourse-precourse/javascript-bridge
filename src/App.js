f27a74a608ace2aa6905663566e1dc4aee8d986e
WaiNaat <qoli0703@gmail.com>
2022-11-21 19:09:05 +0900

    feat(App): add method for requiring bridge size
    정해진 횟수 이전까지는 잘못된 입력을 받으면 재입력 요구,
    정해진 횟수를 채우면 throw - 나중에 play() 등에서 처리 필요.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,33 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const {
+  BridgeConfig,
+  GameConfig,
+  AppConfig,
+  Message,
+} = require('./Config');
+const InputView = require('./InputView');
+const OutputView = require('./OutputView');
+
 class App {
+  static requestBridgeSize() {
+    let errorCount;
+    let input = false;
+    for (errorCount = -1; !input && errorCount < AppConfig.MAX_ERROR_PATIENCE; errorCount += 1) {
+      input = App.#tryInput(InputView.readBridgeSize);
+    }
+    if (errorCount === AppConfig.MAX_ERROR_PATIENCE) throw new Error(Message.ERROR_TOO_MANY);
+    return input;
+  }
+
+  static #tryInput(inputFunction) {
+    try {
+      return inputFunction();
+    } catch (error) {
+      OutputView.printErrorLog(error);
+      return false;
+    }
+  }
+
   play() {}
 }
 
