03ea041a3f5f65051393ae7508365236d12749ef
yeongseokim <1903020@ewhain.net>
2022-11-21 23:18:50 +0900

    feat(InputView): 게임 재시도 여부 입력 받기 기능 구현

--- src/InputView.js
+++ src/InputView.js
@@ -30,7 +30,13 @@ const InputView = {
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() { },
+  readGameCommand(bridgeGame) {
+    Console.readLine("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n", (gameCommand) => {
+      ErrorChecker.checkValidChar(VALID_CHAR.REPLAY, VALID_CHAR.QUIT, gameCommand);
+      if (gameCommand === VALID_CHAR.REPLAY) bridgeGame.retry();
+      else bridgeGame.quit();
+    });
+  },
 };
 
 module.exports = InputView;
