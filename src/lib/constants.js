385da48edd2a8a7d6259fc420441cab4dca33a68
mochang2 <qjsrodksro@gmail.com>
2022-11-21 14:14:40 +0900

    chore: 게임 전반적으로 사용되는 상수 선언

---
+++ src/lib/constants.js
@@ -0,0 +1,16 @@
+const status = {
+  OFF: 0,
+  READ_SIZE: 1,
+  READ_MOVEMENT: 2,
+  READ_COMMAND: 3,
+}
+
+const step = {
+  D: 0, // Down
+  U: 1, // Up
+}
+
+module.exports = {
+  status,
+  step,
+}
