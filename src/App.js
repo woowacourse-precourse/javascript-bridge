8a2298969e3ed523151a6e7f476e52556ac4f374
marugy <gyulife7301@naver.com>
2022-11-22 08:39:47 +0900

    feat(BridgeGame): 생성자 추가

--- src/App.js
+++ src/App.js
@@ -4,26 +4,28 @@ const OutputView = require("../src/OutputView");
 class App {
   play() {
     OutputView.printStart();
-    let size = InputView.readBridgeSize();
-    size = this.validateSize(size);
+    let size = this.makeSize();
 
     MissionUtils.Console.print("pass");
+  }
+
+  makeSize() {
+    let size = InputView.readBridgeSize();
+    size = this.validateSize(size);
+    return size;
   }
 
   validateSize(size) {
     try {
       if (isNaN(Number(size))) {
-        MissionUtils.Console.print("[ERROR] 숫자를 입력해야합니다.");
         throw new Error("[ERROR] 숫자를 입력해야합니다.");
       }
       if (size < 3 && 20 < size) {
-        MissionUtils.Console.print(
-          "[ERROR] 3이상 20이하의 size를 입력해여합니다."
-        );
         throw new Error("[ERROR] 3이상 20이하의 size를 입력해여합니다.");
       }
       return size;
-    } catch {
+    } catch (e) {
+      MissionUtils.Console.print(e.message);
       return InputView.readBridgeSize();
     }
   }
