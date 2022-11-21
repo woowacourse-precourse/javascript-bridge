f6ce495d17e2df1df0c5a37a1d89de02339480a4
BB-choi <choibobae0809@gmail.com>
2022-11-21 14:44:42 +0900

    feat(Validator): 입력 검증 위한 Validator 구현

---
+++ src/Validator.js
@@ -0,0 +1,41 @@
+const ERROR_HEADER = '[ERROR]';
+
+class Validator {
+  constructor(ERROR_MESSAGE_HEADER = ERROR_HEADER) {
+    this.ERROR_MESSAGE_HEADER = ERROR_MESSAGE_HEADER;
+  }
+
+  static #isNumber(value) {
+    return !Number.isNaN(Number(value));
+  }
+
+  static #isInteger(value) {
+    return Number(value) === parseInt(value, 10);
+  }
+
+  isValidInput = (input) => {
+    if (!input.length) {
+      throw new Error(`${this.ERROR_MESSAGE_HEADER} 아무것도 입력하지 않았습니다.`);
+    }
+
+    if (input !== input.trim()) {
+      throw new Error(`${this.ERROR_MESSAGE_HEADER} 입력에 공백이 포함되어 있습니다.`);
+    }
+
+    return true;
+  };
+
+  isValidNumber = (input) => {
+    if (!(Validator.#isNumber(input))) {
+      throw new Error(`${this.ERROR_MESSAGE_HEADER} 숫자(양수)만 입력할 수 있습니다.`);
+    }
+
+    if (!Validator.#isInteger(input)) {
+      throw new Error(`${this.ERROR_MESSAGE_HEADER} 올바른 숫자(정수)를 입력하세요.`);
+    }
+
+    return true;
+  };
+}
+
+module.exports = Validator;
