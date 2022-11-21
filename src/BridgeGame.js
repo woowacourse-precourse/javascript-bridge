8d9286a105110146364be1404e0a7c71894e5b38
Dae-une <ko301201@naver.com>
2022-11-21 16:10:27 +0900

    feat: 성공,실패 를 구분하여 출력

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -2,8 +2,11 @@
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
-  constructor(numbers) {
+  constructor() {
     this.randomBridge;
+    this.upperBridge = [];
+    this.lowerBridge = [];
+    this.count = 1;
   }
 
   makeBridge(number) {
@@ -14,7 +17,39 @@ class BridgeGame {
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(command) {
+    if (this.randomBridge[this.count] === command) {
+      switch (command) {
+        case "U":
+          this.upperBridge.push("O");
+          this.lowerBridge.push(" ");
+          break;
+        case "D":
+          this.upperBridge.push(" ");
+          this.lowerBridge.push("O");
+          break;
+      }
+    } else {
+      switch (command) {
+        case "U":
+          this.upperBridge.push("X");
+          this.lowerBridge.push(" ");
+          break;
+        case "D":
+          this.upperBridge.push(" ");
+          this.lowerBridge.push("X");
+          break;
+      }
+    }
+    this.count++;
+  }
+
+  isSuccess() {
+    if (this.lowerBridge[this.count - 1] === "X" || this.upperBridge[this.count - 1] === "X") {
+      return false;
+    }
+    return true;
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
--- src/InputView.js
+++ src/InputView.js
@@ -1,13 +1,14 @@
 const { Console } = require("@woowacourse/mission-utils");
 const { makeBridge } = require("./BridgeMaker");
 const { generate } = require("./BridgeRandomNumberGenerator");
+const OutPutView = require("./OutputView");
 const BridgeGame = require("./BridgeGame");
 const bridgeGame = new BridgeGame();
 
 const MESSAGE = {
   start: "다리 건너기 게임을 시작합니다.\n",
   getLength: "다리의 길이를 입력해주세요.\n",
-  move: "이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
+  move: "\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
   end: "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n",
 };
 
@@ -19,18 +20,20 @@ const TYPE = {
 
 const ERROR_MESSAGE = {
   outOfRange: "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.",
+  moveCommand: "[ERROR] 이동은 'U' 와 'D'로 가능합니다. ",
 };
 
 const InputView = {
-  validate(number, type) {
+  validate(command, type) {
     switch (type) {
       case "LENGTH":
-        if (number >= 3 && number <= 20) {
+        if (command >= 3 && command <= 20) {
           return;
         } else {
           throw new Error(ERROR_MESSAGE.outOfRange);
         }
       case "MOVE":
+        if (command !== "U" && command !== "D") throw new Error(ERROR_MESSAGE.moveCommand);
         break;
       case "END":
         break;
@@ -43,13 +46,21 @@ const InputView = {
       this.validate(number, TYPE.length);
       const randomBridge = makeBridge(Number(number), () => generate());
       bridgeGame.makeBridge(randomBridge);
+      this.readMoving();
     });
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    Console.readLine(MESSAGE.move, (command) => {
+      this.validate(command, TYPE.move);
+      bridgeGame.move(command);
+      OutPutView.printMap(bridgeGame.upperBridge, bridgeGame.lowerBridge);
+      bridgeGame.isSuccess() ? this.readMoving() : this.readGameCommand();
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
