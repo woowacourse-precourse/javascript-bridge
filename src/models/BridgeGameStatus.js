cc9b41e40c01f3b218432111ea3f291ae1b55713
Leejin-Yang <ejin1996@gmail.com>
2022-11-21 22:14:40 +0900

    refactor: 다리 건너기 게임 상태 클래스 필드 수정

--- src/models/BridgeGameStatus.js
+++ src/models/BridgeGameStatus.js
@@ -1,34 +1,22 @@
 /**
- * 게임 상태 타입 정의
- * @typedef {Object} BridgeGameStatusState
- * @property {number} location
- * @property {number} tryCount
- */
-
-/**
  * 다리 건너기 게임 상태 클래스
  * @class
  */
 class BridgeGameStatus {
-  static initLocation = -1;
+  static #INITIAL_LOCATION = -1;
 
-  static initTryCount = 0;
+  static #INITIAL_TRY_COUNT = 0;
 
-  /**
-   * 게임 상태 타입
-   * @type {BridgeGameStatusState}
-   */
-  #state = {
-    location: BridgeGameStatus.initLocation,
-    tryCount: BridgeGameStatus.initTryCount,
-  };
+  #location = BridgeGameStatus.#INITIAL_LOCATION;
+
+  #tryCount = BridgeGameStatus.#INITIAL_TRY_COUNT;
 
   /**
    * 위치 가져올 때 사용하는 메서드
    * @returns {number}
    */
   getLocation() {
-    return this.#state.location;
+    return this.#location;
   }
 
   /**
@@ -36,28 +24,28 @@ class BridgeGameStatus {
    * @returns {number}
    */
   getTryCount() {
-    return this.#state.tryCount;
+    return this.#tryCount;
   }
 
   /**
    * 위치 증가할 때 사용하는 메서드
    */
   increaseLocation() {
-    this.#state.location += 1;
+    this.#location += 1;
   }
 
   /**
    * 시도 횟수 증가할 때 사용하는 메서드
    */
   increaseTryCount() {
-    this.#state.tryCount += 1;
+    this.#tryCount += 1;
   }
 
   /**
    * 위치 초기화 시 사용하는 메서드
    */
   resetLocation() {
-    this.#state.location = BridgeGameStatus.initLocation;
+    this.#location = BridgeGameStatus.#INITIAL_LOCATION;
   }
 }
 
