a98e99d6abd4d2525d4938ffcbbda04629b13223
d-charlie-kim <dokkim@student.42seoul.kr>
2022-11-21 16:35:44 +0900

    Style: 출력 유지보수 향상을 위해 constants 활용

--- src/InputView.js
+++ src/InputView.js
@@ -1,14 +1,12 @@
 const MissionUtils = require('@woowacourse/mission-utils');
+const { MESSAGE } = require('./constants');
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
-  /**
-   * 다리의 길이를 입력받는다.
-   */
   readBridgeSize(settingBridge) {
-    MissionUtils.Console.readLine("다리의 길이를 입력해주세요.\n", (size) => {
+    MissionUtils.Console.readLine(MESSAGE.INPUT_BRIDGE_SIZE, (size) => {
       settingBridge(size);
     });
   },
