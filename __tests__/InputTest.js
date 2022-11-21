2dcdcbf9f97d7b8fb6cdeb1b3f74824987569e94
Sungwk <73461032+Sungwk@users.noreply.github.com>
2022-11-21 23:34:00 +0900

    test : 다리 길이 입력값 테스트케이스 추가

---
+++ __tests__/InputTest.js
@@ -0,0 +1,16 @@
+const Validation = require('../src/Validation');
+
+describe('다리 길이 입력값 테스트', () => {
+  test('숫자가 아닌 값을 입력한 경우', () => {
+    const inputValues = ['a', 'b', '\n'];
+    inputValues.forEach((inputValue) => {
+      expect(Validation.validateBridgeSize(inputValue)).toBeFalsy();
+    });
+  });
+  test('3~20 사이의 값이 아닌 값을 입력한 경우', () => {
+    const inputValues = ['0', '1', '-1', '21', '100'];
+    inputValues.forEach((inputValue) => {
+      expect(Validation.validateBridgeSize(inputValue)).toBeFalsy();
+    });
+  });
+});
