30890bbc12eb20c0f085ed5dd7827b2ca53c720a
KINHYEONJI <hyeonji0718@gmail.com>
2022-11-22 02:52:09 +0900

    feat(OutputView) : 게임 결과를 출력하는 함수 printResult 구현
    - 최종 게임 결과 출력
    - 게임 성공 여부 출력
    - 총 시도 횟수 출력

--- src/OutputView.js
+++ src/OutputView.js
@@ -19,12 +19,17 @@ const OutputView = {
         " ]"
     );
   },
-  /**
-   * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-   * <p>
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  printResult() {},
+
+  printResult(nowBridge, isSuccess, playCount) {
+    MissionUtils.Console.print(
+      "최종 게임 결과\n" +
+        this.printMap(nowBridge) +
+        "\n게임 성공 여부: " +
+        isSuccess +
+        "\n총 시도된 횟수: " +
+        playCount
+    );
+  },
 };
 
 module.exports = OutputView;
