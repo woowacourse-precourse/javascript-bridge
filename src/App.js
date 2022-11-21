a895bd9bf07f3c35452f7bf577e81647b42ef827
jangsumin <jsm0726@naver.com>
2022-11-21 11:45:36 +0900

    feat: print game start comment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const OutputView = require("./OutputView");
+
 class App {
-  play() {}
+
+  play() {
+    OutputView.printStart(); // 게임 시작 문구 출력
+  }
 }
+
+let app = new App();
+app.play();
 
 module.exports = App;
