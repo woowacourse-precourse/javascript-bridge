080cd4db303168a543f0fab8ff10df7480798b2b
Jaehyeon <jaehyeonp74@gmail.com>
2022-11-21 22:41:07 +0900

    refactor: Validator 클래서 메서드 리턴 값 boolean으로 변경
    - 예외를 발생하면 입력을 다시 못하고 종료되기 때문

--- src/Validator.js
+++ src/Validator.js
@@ -1,29 +1,33 @@
 class Validator {
   static validateNumber(value) {
-    const regExp = /[0-9]/g;
+    const regExp = /[0-9]/;
     const matchArr = value.match(regExp);
     if (!matchArr || matchArr.length !== value.length) {
-      throw new Error('[ERROR] 다리 길이는 숫자를 입력해야 합니다.');
+      return false;
     }
+    return true;
   }
 
   static validateNumberInRange(value) {
     const num = parseInt(value, 10);
     if (num > 20 || num < 3) {
-      throw new Error('[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.');
+      return false;
     }
+    return true;
   }
 
   static validateUpDown(value) {
     if (value !== 'U' && value !== 'D') {
-      throw new Error('[ERROR] U 또는 D 중 한 문자만 입력해주세요.');
+      return false;
     }
+    return true;
   }
 
   static validateGameCommand(value) {
     if (value !== 'R' && value !== 'Q') {
-      throw new Error('[ERROR] R 또는 Q 중 한 문자만 입력해주세요.');
+      return false;
     }
+    return true;
   }
 }
 
