e2afb82ff76eebde44394757f5aa11c0ceca959a
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-11-21 17:52:51 +0900

    feat(InputView): 입력 메서드

--- src/InputView.js
+++ src/InputView.js
@@ -34,17 +34,23 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize(message, callbackFn) {
+    this.readLine(message, callbackFn);
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving(message, callbackFn) {
+    this.readLine(message, callbackFn);
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand(message, callbackFn) {
+    this.readLine(message, callbackFn);
+  },
 };
 
 module.exports = InputView;
