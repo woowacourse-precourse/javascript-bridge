e47a84cd6f57737b1886a0249d8818a24d4d504a
Subin Jo <sbjo1202@naver.com>
2022-11-21 14:18:29 +0900

    refactor: try-catch문 위치 변경
    InputView의 메서드들은 단순히 입력을 받기 위해 사용하려고 수정함
    InputView의 try-catch문을 GameController의 checkRetryOrEnd 함수로 이동시킴

--- src/InputView.js
+++ src/InputView.js
@@ -22,18 +22,8 @@ const InputView = {
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand(callback) {
-    MissionUtils.Console.readLine('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n', (inputRetryOrEnd) => {
-      try {
-        Validation.checkInputRetryOrEnd(inputRetryOrEnd);
-      } catch (Error) {
-        MissionUtils.Console.print(`${Error.message} \n`);
-        return this.readGameCommand(callback);
-      }
-
-      console.log(inputRetryOrEnd);
-      callback(inputRetryOrEnd);
-    });
+   readGameCommand(inputRetryOrEnd) {
+    MissionUtils.Console.readLine('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n', (inputRetryOrEnd));
   },
 };
 
