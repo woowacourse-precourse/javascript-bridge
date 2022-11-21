370629a2a3bffdce9e264c2506ac9a9862901de8
ashleysyheo <ashleysyheo@gmail.com>
2022-11-22 00:54:52 +0900

    feat: check if game has failed in progress

--- src/Validation.js
+++ src/Validation.js
@@ -3,6 +3,14 @@ const Utils = require('./utils/Utils');
 class Validation {
   checkRestartRequirement() {}
 
+  hasFailedInProgress(bridgeGameLog) {
+    const MOVE_POSITION = bridgeGameLog[0].length - 1; 
+  
+    if (!this.checkMoveSuccess(bridgeGameLog, MOVE_POSITION)) return true;
+    
+    return false; 
+  }
+
   checkIsLastMove(moveCount, bridge) {
     if (moveCount === bridge.length) return true;
 
