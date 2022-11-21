07ca082f62c135eb90673bc77461539adc231f0f
seongminn <berry9901@gmail.com>
2022-11-21 16:09:18 +0900

    fix(InputView): 에러가 발생한 경우 사용자 값을 입력 받는 메서드 재귀호출하는 기능 구현

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -9,21 +9,40 @@ const InputView = {
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize(callback) {
-    Console.readLine(INPUT_MESSAGE.BRIDGE_LENGTH, callback);
+    this.readInput(
+      INPUT_MESSAGE.BRIDGE_LENGTH,
+      callback,
+      this.readBridgeSize.bind(this)
+    );
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving(callback) {
-    Console.readLine(INPUT_MESSAGE.MOVING, callback);
+    this.readInput(INPUT_MESSAGE.MOVING, callback, this.readMoving.bind(this));
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand(callback) {
-    Console.readLine(INPUT_MESSAGE.RETRY, callback);
+    this.readInput(
+      INPUT_MESSAGE.RETRY,
+      callback,
+      this.readGameCommand.bind(this)
+    );
+  },
+
+  readInput(message, callback, reDo) {
+    Console.readLine(message, (input) => {
+      try {
+        callback(input);
+      } catch (err) {
+        Console.print(err.message);
+        reDo(callback);
+      }
+    });
   },
 };
 
