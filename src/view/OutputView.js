210297b19fb3fa98d0cb6119bd71f89d04405c40
wzrabbit <curious.wzrabbit@gmail.com>
2022-11-21 12:05:11 +0900

    feat(outputview): 최종 게임 결과 출력하는 기능 구현

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -10,6 +10,18 @@ const OutputView = {
   printMap(gameResult) {
     Console.print(`${gameResult.bridge}\n`);
   },
+
+  printResult(gameResult) {
+    Console.print(
+      Trimmer.templateTrim(`
+        최종 게임 결과
+        ${gameResult.bridge}
+    
+        게임 성공 여부: ${gameResult.roundResult === MOVE_RESULT.CLEAR ? '성공' : '실패'}
+        총 시도한 횟수: ${gameResult.attempt}`)
+    );
+    Console.close();
+  },
 };
 
 module.exports = OutputView;
