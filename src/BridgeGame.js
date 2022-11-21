8f3c0068971cfea74c0d7d8c9435262044195aa0
sunny <huiseon37@gmail.com>
2022-11-22 03:10:04 +0900

    feat: up, down 다리 상태 반환 함수 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -10,29 +10,25 @@ class BridgeGame {
   #bridgeMap;
   #step;
 
-
   constructor(bridgeSize){
     this.#bridge = new Bridge(makeBridge(bridgeSize, generate));
     this.#bridgeMap = new BridgeMap();
     this.#step = -1;
   }
-  /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
+
   move(moving) {
     this.#step += 1;
     const isMatch = this.#bridge.matchMoveBridge(moving, this.#step);
-    this.#bridgeMap(isMatch);
+    this.#bridgeMap.buildMap(isMatch);
+  }
+  
+  getMap(isUp, moving) {
+    return isUp ? this.#bridgeMap.upMap(moving) : this.#bridgeMap.downMap(moving);
   }
 
-  /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
   retry() {}
+  
+  
 }
 
 module.exports = BridgeGame;
