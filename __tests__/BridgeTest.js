057e4d80526e27cb4b62ac69530afeeec6ce0c1e
YMS <yms1789@naver.com>
2022-11-21 16:15:34 +0900

    Test: 다리 건너기 기능 테스트

--- __tests__/BridgeTest.js
+++ __tests__/BridgeTest.js
@@ -1,6 +1,8 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const BridgeGame = require("../src/BridgeGame");
 const BridgeMaker = require("../src/BridgeMaker");
 const BridgeRandomNumberGenerator = require("../src/BridgeRandomNumberGenerator");
+const OutputView = require("../src/OutputView");
 describe("BridgeMaker 클래스 테스트", () => {
   test("입력받은 다리 길이에 따른 다리 생성 길이", () => {
     expect(
@@ -8,3 +10,19 @@ describe("BridgeMaker 클래스 테스트", () => {
     ).toHaveLength(5);
   });
 });
+
+describe("BridgeGame 클래스 테스트", () => {
+  let bridgeGame;
+  beforeEach(() => {
+    bridgeGame = new BridgeGame();
+  });
+  test("건널 수 있는 칸을 건넜을 때", () => {
+    const expected = [1, ["O"], ["U"]];
+    expect(bridgeGame.move("U", 0, ["U", "D", "U"])).toEqual(expected);
+  });
+  test("건널 수 없는 칸을 건넜을 때", () => {
+    const expected = [1, ["X"], ["D"]];
+    expect(bridgeGame.move("D", 0, ["U", "D", "U"])).toEqual(expected);
+  });
+});
+
