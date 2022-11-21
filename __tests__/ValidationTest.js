2f11ad2d84f3179e24ed1d3af0ee78c180910504
JaeyeonHong <39540473+HongJaeyeon@users.noreply.github.com>
2022-11-22 01:18:30 +0900

    refactor: describe 추가하여 테스트 분리

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -2,43 +2,45 @@ const Validation = require('../src/Validation.js');
 const { ErrorMessage } = require("../src/constant/Constants.js");
 
 describe("Validation 테스트", () => {
+  describe("다리 길이 입력 테스트", () => {
+    test("문자값 입력 테스트", () => {
+      expect(() => {
+        Validation.isNumber("a");
+      }).toThrow(ErrorMessage.IS_NOT_NUMBER);
+    });
 
-  test("다리 길이에 문자 값 입력 테스트", () => {
-    expect(() => {
-      Validation.isNumber("a");
-    }).toThrow(ErrorMessage.IS_NOT_NUMBER);
-  });
+    test("범위에 벗어난 값 입력 테스트", () => {
+      expect(() => {
+        Validation.isValidRangeSize(1);
+      }).toThrow(ErrorMessage.BRIDGE_SIZE);
+    });
+  }),
 
-  test("다리 길이에 범위 벗어난 값 입력 테스트", () => {
-    expect(() => {
-      Validation.isValidRangeSize(1);
-    }).toThrow(ErrorMessage.BRIDGE_SIZE);
-  });
+  describe("움직일 방향 입력 테스트", () => {
+    test("숫자값 입력 테스트", () => {
+      expect(() => {
+        Validation.isValidDirection(1);
+      }).toThrow(ErrorMessage.MOVING_DIRECTION);
+    });
 
-  
-  test("움직일 방향에 숫자 값 입력 테스트", () => {
-    expect(() => {
-      Validation.isValidDirection(1);
-    }).toThrow(ErrorMessage.MOVING_DIRECTION);
-  });
+    test("U와 D가 아닌 값 입력 테스트", () => {
+      expect(() => {
+        Validation.isValidDirection("K");
+      }).toThrow(ErrorMessage.MOVING_DIRECTION);
+    });
+  }),
 
-  test("움직일 방향에 U와 D가 아닌 값 입력 테스트", () => {
-    expect(() => {
-      Validation.isValidDirection("K");
-    }).toThrow(ErrorMessage.MOVING_DIRECTION);
-  });
-
-
-  test("게임 커맨드에 숫자 값 입력 테스트", () => {
-    expect(() => {
-      Validation.isValidRetryOrQuitInput(1);
-    }).toThrow(ErrorMessage.RETRY_OR_QUIT);
-  });
-  
-  test("게임 커맨드에 R과 Q가 아닌 값 입력 테스트", () => {
-    expect(() => {
-      Validation.isValidRetryOrQuitInput("K");
-    }).toThrow(ErrorMessage.RETRY_OR_QUIT);
-  });
-  
-});
+  describe("게임 커맨드 입력 테스트", () => {
+    test("숫자값 입력 테스트", () => {
+      expect(() => {
+        Validation.isValidRetryOrQuitInput(1);
+      }).toThrow(ErrorMessage.RETRY_OR_QUIT);
+    });
+    
+    test("R과 Q가 아닌 값 입력 테스트", () => {
+      expect(() => {
+        Validation.isValidRetryOrQuitInput("K");
+      }).toThrow(ErrorMessage.RETRY_OR_QUIT);
+    });
+  })
+})
