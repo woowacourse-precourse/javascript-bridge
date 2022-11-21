466d3453b6d5583c7ee981a29bb02ce562b51679
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-11-21 16:03:02 +0900

    refactor: Input 메세지 상수화 적용

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,5 +1,6 @@
 const { Console } = require('@woowacourse/mission-utils');
 const { isCollectBridgeLength, isValidateMoveInput, isValidateRetryInput } = require('../utils/bridgeGameValidator.js');
+const { INFORMATION_MESSAGE } = require('../constants/index.js');
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -9,7 +10,7 @@ const InputView = {
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize(callback) {
-    Console.readLine('다리의 길이를 입력해주세요.\n', (userInput) => {
+    Console.readLine(INFORMATION_MESSAGE.READ_BRIDGE_SIZE, (userInput) => {
       try {
         isCollectBridgeLength(userInput);
         callback(userInput);
@@ -28,7 +29,7 @@ const InputView = {
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving(callback) {
-    Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)\n', (userInput) => {
+    Console.readLine(INFORMATION_MESSAGE.READ_MOVE, (userInput) => {
       try {
         isValidateMoveInput(userInput);
         callback(userInput);
@@ -47,7 +48,7 @@ const InputView = {
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand(callback) {
-    Console.readLine('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n', (userInput) => {
+    Console.readLine(INFORMATION_MESSAGE.READ_GAME_COMMAND, (userInput) => {
       try {
         isValidateRetryInput(userInput);
         callback(userInput);
