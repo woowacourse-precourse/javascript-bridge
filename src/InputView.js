5691bf1d6ae54e7ab50e4557fa6256a75335eea7
moonelysian <dandyblack@naver.com>
2022-11-22 01:38:10 +0900

    style(InputView): 잘못된 들여쓰기 수정

--- src/InputView.js
+++ src/InputView.js
@@ -1,7 +1,7 @@
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
- const { Console } = require('@woowacourse/mission-utils');
+const { Console } = require('@woowacourse/mission-utils');
 const { INPUT_MESSAGE } = require('./utils/constructor');
 const { handleInputError } = require('./utils/handler');
 const { validBridgeSize, validMoving, validCommand } = require('./utils/Validation');
