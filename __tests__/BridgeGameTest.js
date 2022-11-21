0ec056b62f340157dc2cbbfc7dc7658fdba14c11
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-22 03:06:32 +0900

    test(BridgeGameTest): move메서드 테스트 케이스 추가 및 테스트 완료

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -2,6 +2,28 @@ const BridgeGame = require('../src/BridgeGame.js');
 const Bridge = require('../src/Bridge.js');
 
 describe('BridgeGame 클래스 테스트', () => {
+  describe('move 메서드 테스트', () => {
+    test('생성된 다리와 방향 입력이 일치하면, {isCrossable: true, direction: 방향 입력값} 형식의 객체가 로그에 저장되어야 한다.', () => {
+      const bridge = new Bridge(['U', 'D', 'U']);
+      const bridgeGame = new BridgeGame(bridge);
+
+      bridgeGame.move('U');
+      const result = bridgeGame.getMovementLogs()[0];
+
+      expect(result).toEqual({ isCrossable: true, direction: 'U' });
+    });
+
+    test('생성된 다리와 방향 입력이 일치하지 않으면, {isCrossable: false, direction: 방향 입력값} 형식의 객체가 로그에 저장되어야 한다.', () => {
+      const bridge = new Bridge(['U', 'D', 'U']);
+      const bridgeGame = new BridgeGame(bridge);
+
+      bridgeGame.move('D');
+      const result = bridgeGame.getMovementLogs()[0];
+
+      expect(result).toEqual({ isCrossable: false, direction: 'D' });
+    });
+  });
+
   describe('isLatestMoveSucceeded 메서드 테스트', () => {
     test('생성된 다리와 방향 입력이 일치하면, isLatestMoveSucceeded 메서드는 true를 return한다.', () => {
       const bridge = new Bridge(['U', 'D', 'U']);
