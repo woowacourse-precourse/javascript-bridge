6e7e21ffd10422ad767586ff19566016d6639038
terry <kdjtiger@naver.com>
2022-11-21 19:30:50 +0900

    refactor: 사이즈 길이 입력 리팩토링

--- src/InputView.js
+++ src/InputView.js
@@ -17,16 +17,7 @@ const InputView = {
    */
   readBridgeSize() {
     return Console.readLine(MESSAGE.ASK_BRIDGE_SIZE, (bridgeSize) => {
-      try {
-        if (isNaN(bridgeSize)) { throw new Error(); }
-        if (bridgeSize < 3 || bridgeSize > 20) { throw new Error(); }
-        let bridge = makeBridge(bridgeSize, generate);
-        console.log(bridge); // 지워야 될 코드
-        this.readMoving(bridge, CONSTANT.ZERO_INDEX, CONSTANT.FIRST_GAME);       
-      } catch (error) {
-        Console.print(ERROR.BRIDGE_RANGE);
-        this.readBridgeSize();
-      }
+      this.bridgeSizeCheck(bridgeSize);
     });
   },
 
@@ -75,6 +66,22 @@ const InputView = {
         this.readGameCommand(bridge, index, totalgames);
       }
     })
+  },
+
+  bridgeSizeCheck(bridgeSize) {
+    try {
+      this.bridgeSizeError(bridgeSize);
+      let bridge = makeBridge(bridgeSize, generate);
+      this.readMoving(bridge, CONSTANT.ZERO_INDEX, CONSTANT.FIRST_GAME);
+    } catch (error) {
+      Console.print(ERROR.BRIDGE_RANGE);
+      this.readBridgeSize();
+    }
+  },
+
+  bridgeSizeError(bridgeSize) {
+    if (isNaN(bridgeSize)) { throw new Error(); }
+    if (bridgeSize < 3 || bridgeSize > 20) { throw new Error(); }
   },
 };
 
