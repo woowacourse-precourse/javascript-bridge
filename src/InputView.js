6412cfea9839d060151eebc20db8136d2808fa5a
DongWooKim97 <nanaukim@naver.com>
2022-11-21 23:54:59 +0900

    feat: 입력한 값에 대한 예외처리 기능 작성 및 BridgeGame 인스턴스 생성

--- src/InputView.js
+++ src/InputView.js
@@ -1,10 +1,34 @@
 const { Console } = require("@woowacourse/mission-utils");
 const { GAME_MESSAGES } = require("./Constants");
 const InputView = {
+  size: "",
+  gameClient: "",
+
+  insertSize(size) {
+    this.size = Number(size);
+    this.gameClient = new BridgeGame(this.size);
+    this.readMoving();
+  },
+
   readBridgeSize() {
     Console.readLine(GAME_MESSAGES.ASK_TO_BRIDGE_LENGTH, (size) => {
-      Console.print(size);
+      try {
+        this.sizeValidate(size);
+        this.insertSize(size);
+      } catch (error) {
+        this.printInputSizeError(error);
+      }
     });
+  },
+
+  sizeValidate(size) {
+    Validate.checkSizeInputType(size);
+    Validate.checkSizeInputRange(size);
+  },
+
+  printInputSizeError(error) {
+    OutputView.printError(error);
+    this.readBridgeSize();
   },
 
   readMoving() {},
