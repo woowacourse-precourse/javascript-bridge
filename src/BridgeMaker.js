453a5f91490f9d4f9e9c81f22b3366318da5c3e6
seunghoria <102285042+seunghoria@users.noreply.github.com>
2022-11-21 16:09:49 +0900

    refactor: 다리 생성 기능 변수명 변경

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -8,13 +8,13 @@ const BridgeMaker = {
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
   makeBridge(size, generateRandomNumber) {
-    return new Array(size).fill(null).map(() => {
+    return new Array(size).fill("").map(() => {
       const number = generateRandomNumber();
 
-      if (number === 0) direction = "D";
-      if (number === 1) direction = "U";
+      if (number === 0) moveKey = "D";
+      if (number === 1) moveKey = "U";
 
-      return direction;
+      return moveKey;
     });
   },
 };
