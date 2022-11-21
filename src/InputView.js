ce1dea2a0b1e4c9bb2362cc1111fe1ac698acdd2
sohyun <park.sohyun0430@gmail.com>
2022-11-21 15:43:20 +0900

    feat: inputView 함수 구현
    readBridgeSize : 다리 길이 입력받는 함수
    readMoving : 사용자가 이동할 칸 입력받는 함수
    readGameCommand : 사용자가 게임을 다시 시작할지 종료할지 여부를 입력받는 함수

--- src/InputView.js
+++ src/InputView.js
@@ -1,21 +1,34 @@
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
+const { Console } = require('@woowacourse/mission-utils');
+const {
+  BRIDGE_SIZE_REQUEST,
+  MOVE_REQUEST,
+  RETRY_REQUEST,
+} = require('./constants');
+
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize(callback) {
+    Console.readLine(BRIDGE_SIZE_REQUEST, callback);
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving(callback) {
+    Console.readLine(MOVE_REQUEST, callback);
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand(callback) {
+    Console.readLine(RETRY_REQUEST, callback);
+  },
 };
 
 module.exports = InputView;
