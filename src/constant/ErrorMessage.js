d2a2ad93b841d9076418ac2971af48a61d287388
cofla <cofla159@naver.com>
2022-11-21 21:49:42 +0900

    feat: 다리 길이 에러메세지 세분화

--- src/constant/ErrorMessage.js
+++ src/constant/ErrorMessage.js
@@ -1,3 +1,6 @@
-const BRIDGE_ERROR = "[ERROR] 3 이상 20 이하의 숫자를 하나 입력해주세요.";
+const BRIDGE_ERROR = {
+  notANumber: "[ERROR] 숫자를 하나만 입력해주세요.",
+  outOfRange: "[ERROR] 3 이상 20 이하의 값을 입력해주세요.",
+};
 
 module.exports = { BRIDGE_ERROR };
