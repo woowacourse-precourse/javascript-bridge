19bac85ae1b04510f727b1962ef7fbeb99c39453
yong <tkddbs1644@gmail.com>
2022-11-21 12:05:43 +0900

    feat(BridgeGame): 게임 재시작 기능 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -46,7 +46,12 @@ class BridgeGame {
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  retry(command) {}
+  retry(command) {
+    if (command === "R") {
+      this.#position = 0;
+      this.#totalTrial += 1;
+    }
+  }
 }
 
 module.exports = BridgeGame;
