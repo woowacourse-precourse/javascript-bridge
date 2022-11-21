22302dd1c9018535585c8ee601517488fa0e8edb
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-11-21 23:54:17 +0900

    test: retry 입력 유효성 테스트

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,4 +1,8 @@
-const { isCollectBridgeLength, isValidateMoveInput } = require('../src/utils/bridgeGameValidator.js');
+const {
+  isCollectBridgeLength,
+  isValidateMoveInput,
+  isValidateRetryInput,
+} = require('../src/utils/bridgeGameValidator.js');
 
 describe('입력 유효성 검사 테스트', () => {
   test.each([['1'], ['-11'], ['어머'], [''], ['/n'], ['100000000000']])(
@@ -12,11 +16,23 @@ describe('입력 유효성 검사 테스트', () => {
 
   test.each([['1'], ['-11'], ['어머'], [''], ['/n'], ['UU'], ['DD']])('U 과 D가 아닌 값을 입력했을 때', (input) => {
     expect(() => {
+      isValidateRetryInput(input);
+    }).toThrow('[ERROR]');
+  });
+
+  test.each([['R'], ['Q']])('U 과 D가 아닌 값을 입력했을 때', (input) => {
+    expect(() => {
+      isValidateRetryInput(input);
+    }).not.toThrow('[ERROR]');
+  });
+
+  test.each([['1'], ['-11'], ['어머'], [''], ['/n'], ['RR'], ['QQ']])('R 과 Q가 아닌 값을 입력했을 때', (input) => {
+    expect(() => {
       isValidateMoveInput(input);
     }).toThrow('[ERROR]');
   });
 
-  test.each([['U'], ['D']])('U 과 D가 아닌 값을 입력했을 때', (input) => {
+  test.each([['U'], ['D']])('R 과 Q인 값을 입력했을 때', (input) => {
     expect(() => {
       isValidateMoveInput(input);
     }).not.toThrow('[ERROR]');
