715329ae81123ba62eec3a4537aa141553be3b10
woo-jk <201702042@o.cnu.ac.kr>
2022-11-22 02:25:12 +0900

    test: BridgeTest 테스트 코드 리팩토링, getMovedPlayer() 함수 생성

--- __tests__/BridgeTest.js
+++ __tests__/BridgeTest.js
@@ -1,37 +1,34 @@
 const Bridge = require("../src/Bridge");
 const Player = require("../src/Player");
 
+const getMovedPlayer = (moveCount) => {
+  const player = new Player();
+  for (let count = 0; count < moveCount; count++) {
+    player.move("U");
+  }
+  return player;
+};
+
 describe("다리 클래스 테스트", () => {
   test("플레이어의 다음 칸 방향 얻기", () => {
-    const path = ["U", "U", "D", "D", "U"];
+    const bridge = new Bridge(["U", "U", "D", "D", "U"]);
 
-    const bridge = new Bridge(path);
-    const player = new Player();
+    const playerNotMoving = new Player();
+    const playerMovedOnce = getMovedPlayer(1);
+    const playerMovedTwice = getMovedPlayer(2);
 
-    const directionFirst = bridge.getNextDirection(player);
-    player.move("U");
-    const directionSecond = bridge.getNextDirection(player);
-    player.move("U");
-    const directionThird = bridge.getNextDirection(player);
-
-    expect(directionFirst).toEqual("U");
-    expect(directionSecond).toEqual("U");
-    expect(directionThird).toEqual("D");
+    expect(bridge.getNextDirection(playerNotMoving)).toEqual("U");
+    expect(bridge.getNextDirection(playerMovedOnce)).toEqual("U");
+    expect(bridge.getNextDirection(playerMovedTwice)).toEqual("D");
   });
 
   test("플레이어가 다리의 끝에 섰는지 확인", () => {
-    const path = ["U", "U", "U"];
+    const bridge = new Bridge(["U", "U", "U"]);
 
-    const bridge = new Bridge(path);
-    const player = new Player();
+    const playerMovedOnce = getMovedPlayer(1);
+    const playerMovedThreeTimes = getMovedPlayer(3);
 
-    player.move("U");
-    player.move("U");
-    const twiceMove = bridge.isLastPosition(player);
-    player.move("U");
-    const threeTimesMove = bridge.isLastPosition(player);
-
-    expect(twiceMove).toBeFalsy();
-    expect(threeTimesMove).toBeTruthy();
+    expect(bridge.isLastPosition(playerMovedOnce)).toBeFalsy();
+    expect(bridge.isLastPosition(playerMovedThreeTimes)).toBeTruthy();
   });
 });
