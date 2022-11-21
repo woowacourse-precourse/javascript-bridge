772d9428a61c11648446e375511dfc6e9e485548
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-11-22 01:51:29 +0900

    feat(constant): add new notice message
    Add direction input notice message

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,7 @@ module.exports = {
   NOTICE: Object.freeze({
     START_GAME: "다리 건너기 게임을 시작합니다.\n",
     INPUT_SIZE: "다리의 길이를 입력해주세요.\n",
+    INPUT_DIRECTION: "이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
   }),
 
   ERROR: Object.freeze({
