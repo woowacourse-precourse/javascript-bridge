bde2f8af3d816fb80d240c9a36af580d503733a2
sying012 <syi369@naver.com>
2022-11-21 22:46:41 +0900

    feat : 게임 시작 메세지 출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+const BridgeGame = require("./BridgeGame");
+const InputView = require("./ui/InputView");
+const OutputView = require("./ui/OutputView");
+
 class App {
-  play() {}
+  #bridgeGame = new BridgeGame();
+
+  play() {
+    OutputView.printStart();
+    // this.#bridgeGame.make();
+  }
 }
 
 module.exports = App;
--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,6 +1,8 @@
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
+const BridgeMaker = require("./BridgeMaker");
+
 class BridgeGame {
   #retryCount = 1;
 
