5f6226f4dc6b477ec6bcc162556b365e39702681
KES <aprilsmaple@gmail.com>
2022-11-22 03:49:18 +0900

    feat: inputview에서 동기적으로 입력 받기

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,11 +1,14 @@
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+const { Console } = require('@woowacourse/mission-utils');
+
+const MESSAGES = require('../constants/Messages');
+
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  async readBridgeSize() {
+    return this.readLine(MESSAGES.bridgeLength);
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
@@ -16,6 +19,12 @@ const InputView = {
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand() {},
+
+  async readLine(query) {
+    return new Promise((resolve) => {
+      Console.readLine(query, (input) => resolve(input));
+    });
+  },
 };
 
 module.exports = InputView;
