fee5fe433bd545cfc937f6462d9e1546511c7edf
coolIlive <galilee155@naver.com>
2022-11-21 23:02:03 +0900

    Refactor: 테스트에서 자료타입 변경, 정리
    - 10줄 넘어가는 코드 정리, 다리 생성 시 자료타입 변경

--- __tests__/MakeBridgeTest.js
+++ __tests__/MakeBridgeTest.js
@@ -1,10 +1,11 @@
 const BridgeMaker = require('../src/BridgeMaker');
 const BridgeRandomNumberGenerator = require('../src/BridgeRandomNumberGenerator');
+const sizeValue = [3, 10, 15, 20];
+const randomNumbers = [[1, 0, 0], [0, 0, 0], [1, 1, 1]];
+const answer = [['U', 'D', 'D'], ['D', 'D', 'D'], ['U', 'U', 'U']];
 
 describe('다리 생성 테스트', () => {
   test('입력한 길이에 맞게 다리 생성', () => {
-    const sizeValue = [3, 10, 15, 20];
-
     sizeValue.forEach((size) => {
       const bridge = BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate);
       expect(bridge).toHaveLength(size);
@@ -12,9 +13,6 @@ describe('다리 생성 테스트', () => {
   });
 
   test('올바른 문자열로 바뀌는지 테스트', () => {
-    const randomNumbers = [['1', '0', '0'], ['0', '0', '0'], ['1', '1', '1']];
-    const answer = [['U', 'D', 'D'], ['D', 'D', 'D'], ['U', 'U', 'U']];
-
     for (let i = 0; i < 3; i++) {
       let mockGenerator = randomNumbers[i].reduce((acc, number) => {
         return acc.mockReturnValueOnce(number);
