0b2b9f4f28fceba76bfbe6b0022ac11a36e00357
freemoon99 <soloist132@gmail.com>
2022-11-21 19:48:48 +0900

    feat: 최종 결과를 출력하는 기능

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,4 +1,5 @@
 const MissionUtils = require('@woowacourse/mission-utils');
+const InputView = require('./InputView');
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -15,6 +16,7 @@ const OutputView = {
   printMap(bridegame, { firstfloor, secondfloor }) {
     MissionUtils.Console.print(firstfloor.join(' '));
     MissionUtils.Console.print(secondfloor.join(' '));
+    bridegame.NextMove();
   },
 
   /**
@@ -22,7 +24,15 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult(){},
+  printResult({ firstfloor, secondfloor }, totalTry, success) {
+    MissionUtils.Console.print('최종 게임 결과');
+    MissionUtils.Console.print(firstfloor.join(' '));
+    MissionUtils.Console.print(secondfloor.join(' '));
+    MissionUtils.Console.print(`게임 성공 여부: ${success}`);
+    MissionUtils.Console.print(`총 시도한 횟수: ${totalTry}`);
+    MissionUtils.Console.close();
+  },
+
 };
 
 module.exports = OutputView;
