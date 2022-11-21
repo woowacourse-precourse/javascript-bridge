4a7a7139267e63b079597a07a161fcdfa928021f
nuheajiohc <jaehun341012@gmail.com>
2022-11-21 12:43:10 +0900

    refactor: 출력 하는 객체 리팩토링

--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -10,8 +10,11 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap(upper, lower) {
-    printMessage(`${upper}\n${lower}`);
+
+  printMap(bridgeMap) {
+    const upperBridge = `[ ${bridgeMap['U'].join(' | ')} ]`;
+    const lowerBridge = `[ ${bridgeMap['D'].join(' | ')} ]`;
+    printMessage(`${upperBridge}\n${lowerBridge}`);
   },
 
   /**
@@ -19,11 +22,11 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult(upper, lower, isSuccess, cnt) {
+  printResult(bridgeMap, str, count) {
     printMessage('최종 게임 결과');
-    this.printMap(upper, lower);
-    printMessage(`\n게임 성공 여부: ${isSuccess}`);
-    printMessage(`총 시도한 횟수: ${cnt}`);
+    this.printMap(bridgeMap);
+    printMessage(`게임 성공 여부: ${str} `);
+    printMessage(`총 시도한 횟수: ${count} `);
   },
 
   printStartMessage() {
