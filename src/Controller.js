6654831be501aed0c5bd5655060843fe577a6b3f
Lee-Young-Jae <yjlee1937@naver.com>
2022-11-21 21:29:15 +0900

    refactor: 이동할 칸을 입력받는 로직을 기능에 따라 Controller로 분리

--- src/Controller.js
+++ src/Controller.js
@@ -1,37 +1,127 @@
-const { printStart, printMap, printResult } = require("./OutputView");
+const { Console } = require("@woowacourse/mission-utils");
 
+const { printStart, printMap, printResult } = require("./OutputView");
 const InputView = require("./InputView");
 const { isInvalidBridgeLength, isInvalidMoving, isInvalidGameCommand } = require("./exception");
 const { generate } = require("./BridgeRandomNumberGenerator");
 const { toNumber } = require("./helpers/common");
 const BridgeGame = require("./BridgeGame");
 const { makeBridge } = require("./BridgeMaker");
+const { RESTART_COMMAND, QUIT_COMMAND } = require("./constant");
 
 class Controller {
+  #bridgeGame;
+
+  setBridgeGame(bridgeGame) {
+    this.#bridgeGame = bridgeGame;
+  }
+
   start() {
     printStart();
     InputView.readBridgeSize((input) => {
-      this.getBridgeSize(input);
+      this.checkBridgeSize(input);
     });
   }
 
-  getBridgeSize = (input) => {
+  checkBridgeSize = (input) => {
     isInvalidBridgeLength(input) && InputView.readBridgeSize();
-    const bridgeSize = toNumber(input);
-    this.makeAnswerBridge(bridgeSize);
+    this.makeAnswerBridge(input);
   };
 
-  makeAnswerBridge(size) {
-    const bridge = makeBridge(size, generate);
-    const bridgeGame = new BridgeGame(bridge);
-    InputView.readMoving(bridgeGame);
+  makeAnswerBridge(input) {
+    const bridgeSize = toNumber(input);
+    const bridge = makeBridge(bridgeSize, generate);
+    this.setBridgeGame(new BridgeGame(bridge));
+    InputView.readMoving((input) => {
+      this.checkMoving(input);
+    });
+  }
+
+  checkMoving = (input) => {
+    if (isInvalidMoving(input)) {
+      InputView.readMoving((input) => {
+        this.checkMoving(input);
+      });
+      return;
+    }
+    this.setMoving(input);
+  };
+
+  setMoving(input) {
+    this.#bridgeGame.move(input);
+    printMap(this.#bridgeGame.getUserState());
+    if (this.#bridgeGame.isVictory()) {
+      printResult(this.#bridgeGame);
+      Console.close();
+      return;
+    }
+    if (this.#bridgeGame.isFinish) {
+      InputView.readGameCommand((input) => {
+        this.checkGameCommand(input);
+      });
+      return;
+    }
+    InputView.readMoving((input) => {
+      this.checkMoving(input);
+    });
+  }
+
+  checkGameCommand = (input) => {
+    if (isInvalidGameCommand(input)) {
+      InputView.readGameCommand((input) => {
+        this.checkGameCommand(input);
+      });
+      return;
+    }
+    this.turnOnOffGame(input);
+  };
+
+  turnOnOffGame(input) {
+    if (input === RESTART_COMMAND) {
+      this.#bridgeGame.retry();
+      InputView.readMoving((input) => {
+        this.checkMoving(input);
+      });
+      return;
+    }
+    if (input === QUIT_COMMAND) {
+      printResult(this.#bridgeGame);
+    }
+    Console.close();
   }
 
   // (input) => {
-  //   isInvalidBridgeLength(input) && InputView.readBridgeSize();
-  //   const bridgeSize = toNumber(input);
-  //   const bridge = makeBridge(bridgeSize, generate);
-  //   const bridgeGame = new BridgeGame(bridge);
+  //   if (isInvalidGameCommand(input)) {
+  //     InputView.readGameCommand(bridgeGame);
+  //     return;
+  //   }
+  //   if (input === RESTART_COMMAND) {
+  //     bridgeGame.retry();
+  //     InputView.readMoving(bridgeGame);
+  //     return;
+  //   }
+  //   if (input === QUIT_COMMAND) {
+  //     printResult(bridgeGame);
+  //   }
+  //   Console.close();
+  // }
+
+  // (input) => {
+  //   if (isInvalidMoving(input)) {
+  //     InputView.readMoving(bridgeGame);
+  //     return;
+  //   }
+  //   bridgeGame.move(input);
+  //   printMap(bridgeGame.getUserState());
+  //   if (bridgeGame.isVictory()) {
+  //     printResult(bridgeGame);
+  //     Console.close();
+  //     return;
+  //   }
+  //   if (bridgeGame.isFinish) {
+  //     InputView.readGameCommand(bridgeGame);
+  //     return;
+  //   }
   //   InputView.readMoving(bridgeGame);
   // }
 }
--- src/InputView.js
+++ src/InputView.js
@@ -1,13 +1,5 @@
 const { Console } = require("@woowacourse/mission-utils");
-
-const BridgeGame = require("./BridgeGame");
-const { isInvalidBridgeLength, isInvalidMoving, isInvalidGameCommand } = require("./exception");
-const { makeBridge } = require("./BridgeMaker");
-const { toNumber } = require("./helpers/common");
-const { generate } = require("./BridgeRandomNumberGenerator");
-const { printMap, printResult } = require("./OutputView");
 const { INPUT_LENGTH, INPUT_MOVE, INPUT_RETRY } = require("./constant/message");
-const { RESTART_COMMAND, QUIT_COMMAND } = require("./constant");
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -23,46 +15,15 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving(bridgeGame) {
-    Console.readLine(INPUT_MOVE, (input) => {
-      if (isInvalidMoving(input)) {
-        InputView.readMoving(bridgeGame);
-        return;
-      }
-      bridgeGame.move(input);
-      printMap(bridgeGame.getUserState());
-      if (bridgeGame.isVictory()) {
-        printResult(bridgeGame);
-        Console.close();
-        return;
-      }
-      if (bridgeGame.isFinish) {
-        InputView.readGameCommand(bridgeGame);
-        return;
-      }
-      InputView.readMoving(bridgeGame);
-    });
+  readMoving(callback) {
+    Console.readLine(INPUT_MOVE, callback);
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand(bridgeGame) {
-    Console.readLine(INPUT_RETRY, (input) => {
-      if (isInvalidGameCommand(input)) {
-        InputView.readGameCommand(bridgeGame);
-        return;
-      }
-      if (input === RESTART_COMMAND) {
-        bridgeGame.retry();
-        InputView.readMoving(bridgeGame);
-        return;
-      }
-      if (input === QUIT_COMMAND) {
-        printResult(bridgeGame);
-      }
-      Console.close();
-    });
+  readGameCommand(callback) {
+    Console.readLine(INPUT_RETRY, callback);
   },
 };
 
