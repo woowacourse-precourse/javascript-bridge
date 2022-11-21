fc57eb34b5763d09c1bb14bc08c4bc87dff4dffd
oseunghyun <ohsh1477@gmail.com>
2022-11-21 23:32:05 +0900

    refactor(BridgeGame): move 메소드 분리

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -25,29 +25,36 @@ class BridgeGame {
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   static move(userPosition) {
-    const IS_BRIDGE_LEFT = bridge.isBridgeLeft();
-    const IS_USER_CAN_GO = bridge.askUserCanGo(userPosition);
+    if (BridgeGame.isBridgeLeft()) {
+      return BridgeGame.keepGoing(userPosition);
+    }
+    return BridgeGame.stopGoing(userPosition);
+  }
 
-    if (IS_BRIDGE_LEFT) {
-      if (IS_USER_CAN_GO) {
-        bridge.drawNowMap(MOVE.GO);
-        return gameManager.askWhereToGo(BridgeGame.move);
-      }
-      if (!IS_USER_CAN_GO) {
-        bridge.drawNowMap(MOVE.FALL);
-        return gameManager.askRetry(BridgeGame.retry);
-      }
+  static isBridgeLeft() {
+    return bridge.isBridgeLeft();
+  }
+
+  static keepGoing(userPosition) {
+    if (BridgeGame.isUserCanGo(userPosition)) {
+      bridge.drawNowMap(MOVE.GO);
+      return gameManager.askWhereToGo(BridgeGame.move);
     }
-    if (!IS_BRIDGE_LEFT) {
-      if (IS_USER_CAN_GO) {
-        bridge.drawNowMap(MOVE.GO);
-        bridge.drawResultMap(MOVE.GO, RESULT.SUCCESS);
-      }
-      if (!IS_USER_CAN_GO) {
-        bridge.drawNowMap(MOVE.FALL);
-        gameManager.askRetry(BridgeGame.retry);
-      }
+    bridge.drawNowMap(MOVE.FALL);
+    return gameManager.askRetry(BridgeGame.retry);
+  }
+
+  static stopGoing(userPosition) {
+    if (BridgeGame.isUserCanGo(userPosition)) {
+      bridge.drawNowMap(MOVE.GO);
+      return bridge.drawResultMap(MOVE.GO, RESULT.SUCCESS);
     }
+    bridge.drawNowMap(MOVE.FALL);
+    return gameManager.askRetry(BridgeGame.retry);
+  }
+
+  static isUserCanGo(userPosition) {
+    return bridge.askUserCanGo(userPosition);
   }
 
   /**
