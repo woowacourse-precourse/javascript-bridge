e0b5f5e02fff855a09519d852939fd5d13819f94
ashleysyheo <ashleysyheo@gmail.com>
2022-11-21 20:20:30 +0900

    feat: read user move input

--- src/InputView.js
+++ src/InputView.js
@@ -32,7 +32,15 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    MissionUtils.Console.readLine(CONSOLE_MESSAGE.moveInput, (input) => {
+      try {
+        this.exception.validateMove(input);
+      } catch (error) {
+        return this.readMoving();
+      }
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
