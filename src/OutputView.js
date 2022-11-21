0fa2ce61962f88320294ac9b2211f6e634ed0927
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-11-22 08:19:08 +0900

    feat(method): add new print methods
    Add printEndingMessage
    Add printClear
    Add printRunCount
    Add printResult

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,5 +1,5 @@
 const { Console } = require("@woowacourse/mission-utils");
-const { NOTICE } = require("./constants");
+const { NOTICE, SUCCESS, FAILURE } = require("./constants");
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -10,6 +10,10 @@ const OutputView = {
    */
   printStartingMessage() {
     Console.print(NOTICE.START_GAME);
+  },
+
+  printEndingMessage() {
+    Console.print(NOTICE.END_GAME);
   },
 
   /**
@@ -31,12 +35,27 @@ const OutputView = {
     });
   },
 
+  printClear(bridgeGame) {
+    const isClear = bridgeGame.isClear();
+    Console.print(`\n게임 성공 여부: ${isClear ? SUCCESS : FAILURE}`);
+  },
+
+  printRunCount(bridgeGame) {
+    const runCount = bridgeGame.getRunCount();
+    Console.print(`총 시도한 횟수: ${runCount}`);
+  },
+
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() {},
+  printResult(bridgeGame) {
+    OutputView.printEndingMessage();
+    OutputView.printMap(bridgeGame);
+    OutputView.printClear(bridgeGame);
+    OutputView.printRunCount(bridgeGame);
+  },
 };
 
 module.exports = OutputView;
