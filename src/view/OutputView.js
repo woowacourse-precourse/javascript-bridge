fd13a1e575bfb190b7064e5681c593bddf08f0c2
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-11-21 16:03:21 +0900

    refactor: print 메세지 상수화 적용

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,11 +1,12 @@
 const { Console } = require('@woowacourse/mission-utils');
+const { PRINT_MESSAGE } = require('../constants/index.js');
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 const OutputView = {
   welcomeMessage() {
-    Console.print('다리 건너기 게임을 시작합니다.\n');
+    Console.print(PRINT_MESSAGE.WELLCOME);
   },
 
   /**
@@ -14,8 +15,8 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printMap(stepResult) {
-    Console.print(`[${stepResult.upperBridge}]`);
-    Console.print(`[${stepResult.lowerBridge}]`);
+    Console.print(PRINT_MESSAGE.MAP(stepResult.upperBridge));
+    Console.print(PRINT_MESSAGE.MAP(stepResult.lowerBridge));
   },
 
   /**
@@ -24,12 +25,12 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printResult(isSuccess, retryCount, bridge) {
-    const gameResult = isSuccess ? '성공' : '실패';
-    Console.print('최종 게임 결과');
+    const gameResult = isSuccess ? PRINT_MESSAGE.SUCCESS : PRINT_MESSAGE.FAIL;
+    Console.print(PRINT_MESSAGE.RESULT_INFORMATION);
     this.printMap(bridge);
     Console.print('');
-    Console.print(`게임 성공 여부: ${gameResult} `);
-    Console.print(`총 시도한 횟수: ${retryCount} `);
+    Console.print(PRINT_MESSAGE.GAME_RESULT(gameResult));
+    Console.print(PRINT_MESSAGE.TRY_COUNT(retryCount));
     Console.close();
   },
 };
