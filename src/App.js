f6226454286e99b1c0333ab611d2218833fb3f46
Deemou <4106243@naver.com>
2022-11-21 16:36:51 +0900

    feat(App): 게임 시작 문구 출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const { Console } = require('@woowacourse/mission-utils');
+const { MESSAGE, COMMAND } = require('./constant/Bridge');
+const BridgeGame = require('./BridgeGame');
+const InputView = require('./InputView');
+const OutputView = require('./OutputView');
+const Validator = require('./Validator');
+
 class App {
-  play() {}
+  play() {
+    Console.print(MESSAGE.START);
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
