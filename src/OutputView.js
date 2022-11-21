9c3b98a1b6beaf8296c96c530dbff187bee5b861
freemoon99 <soloist132@gmail.com>
2022-11-21 19:45:04 +0900

    feat: 이동한 맵을 출력하는 기능
    이동한 맵을 출력하는 메소드

--- src/OutputView.js
+++ src/OutputView.js
@@ -12,7 +12,10 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {},
+  printMap(bridegame, { firstfloor, secondfloor }) {
+    MissionUtils.Console.print(firstfloor.join(' '));
+    MissionUtils.Console.print(secondfloor.join(' '));
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
