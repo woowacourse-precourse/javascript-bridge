770bb73b9ef5622008e968e841a1e7222af058a8
dhkham <khamdong@gmail.com>
2022-11-21 13:36:29 +0900

    feat(OutputView): add printResult method that prints the final game results

--- src/OutputView.js
+++ src/OutputView.js
@@ -20,7 +20,6 @@ const OutputView = {
     if (gameRec.correctOrNot === "X") {
       InputView.readGameCommand(gameRec); // 실패 => 게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)
     } else if (gameRec.correctOrNot === "O" && gameRec.bridgeAnswer.length === gameRec.moveNum) {
-      gameRec.success = true;
       OutputView.printResult(gameRec); // 끝까지 성공 => printResult
     } else {
       InputView.readMoving(gameRec); // 아직 진행 중 => readMoving 반복
@@ -31,7 +30,16 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() {},
+  printResult(gameRec) {
+    MissionUtils.Console.print(`${MESSAGES.FINAL_RESULT}${gameRec.bridgeOutput.firstBridge}\n${gameRec.bridgeOutput.secondBridge}\n`); // "최종 게임 결과\n"
+    if (gameRec.correctOrNot === "O" && gameRec.bridgeAnswer.length === gameRec.moveNum) {
+      MissionUtils.Console.print(MESSAGES.SUC_OR_FAIL + MESSAGES.SUCCESS); // 게임 성공 여부: 성공
+    } else {
+      MissionUtils.Console.print(MESSAGES.SUC_OR_FAIL + MESSAGES.FAIL); // 게임 성공 여부: 실패
+    }
+    MissionUtils.Console.print(MESSAGES.ATTEMPT_NUM + gameRec.attemptNum); // 총 시도한 횟수: 1
+    // MissionUtils.Console.close();
+  },
 };
 
 module.exports = OutputView;
