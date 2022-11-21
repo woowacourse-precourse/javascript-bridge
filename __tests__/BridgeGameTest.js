27d5f5d4f6219f58f32cacf4c304a4875bf5a1aa
Deemou <4106243@naver.com>
2022-11-21 21:27:08 +0900

    refactor(BridgeGameTest): 상수 사용

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -11,30 +11,32 @@ const mockRandoms = (numbers) => {
 
 describe('BridgeGame 클래스 테스트', () => {
   test('다리를 건너고 현재까지 다리를 건넌 상태를 저장한다.', () => {
-    mockRandoms([1, 0, 0]);
+    mockRandoms([BRIDGE.UPPER, BRIDGE.LOWER, BRIDGE.LOWER]);
 
     const inputs = [BRIDGE.UP, BRIDGE.DOWN, BRIDGE.UP];
     const upper = [MAP.PASS, MAP.BLANK, MAP.NONPASS];
     const lower = [MAP.BLANK, MAP.PASS, MAP.BLANK];
     const expectedOutput = [lower, upper];
+    const size = 3;
+    const game = new BridgeGame(size);
 
-    const game = new BridgeGame(3);
     inputs.forEach((input) => {
       game.move(input);
     });
-    const map = game.getMap();
 
+    const map = game.getMap();
     expect(map).toEqual(expectedOutput);
   });
 
   test('다리를 무사히 건넜는지 확인한다.', () => {
-    mockRandoms([1, 0, 0]);
+    mockRandoms([BRIDGE.UPPER, BRIDGE.LOWER, BRIDGE.LOWER]);
 
     const inputs = [BRIDGE.UP, BRIDGE.DOWN];
     const expectedOutput = [true, false];
+    const size = 3;
 
     inputs.forEach((input, index) => {
-      const game = new BridgeGame(3);
+      const game = new BridgeGame(size);
       game.move(input);
       const output = game.isPass();
       expect(output).toEqual(expectedOutput[index]);
@@ -42,62 +44,70 @@ describe('BridgeGame 클래스 테스트', () => {
   });
 
   test('다리를 전부 건넜는지 확인한다. case1', () => {
-    mockRandoms([1, 0, 0]);
+    mockRandoms([BRIDGE.UPPER, BRIDGE.LOWER, BRIDGE.LOWER]);
 
     const inputs = [BRIDGE.UP, BRIDGE.DOWN, BRIDGE.DOWN];
     const expectedOutput = true;
-    const game = new BridgeGame(3);
+    const size = 3;
+    const game = new BridgeGame(size);
 
     inputs.forEach((input) => {
       game.move(input);
     });
+
     const output = game.isClear();
     expect(output).toEqual(expectedOutput);
   });
 
   test('다리를 전부 건넜는지 확인한다. case2', () => {
-    mockRandoms([1, 0, 0]);
+    mockRandoms([BRIDGE.UPPER, BRIDGE.LOWER, BRIDGE.LOWER]);
 
     const inputs = [BRIDGE.UP, BRIDGE.DOWN, BRIDGE.UP];
     const expectedOutput = false;
-    const game = new BridgeGame(3);
+    const size = 3;
+    const game = new BridgeGame(size);
 
     inputs.forEach((input) => {
       game.move(input);
     });
+
     const output = game.isClear();
     expect(output).toEqual(expectedOutput);
   });
 
   test('다리를 전부 건넜는지 확인한다. case3', () => {
-    mockRandoms([1, 0, 0]);
+    mockRandoms([BRIDGE.UPPER, BRIDGE.LOWER, BRIDGE.LOWER]);
 
     const inputs = [BRIDGE.UP, BRIDGE.DOWN];
     const expectedOutput = false;
-    const game = new BridgeGame(3);
+    const size = 3;
+    const game = new BridgeGame(size);
 
     inputs.forEach((input) => {
       game.move(input);
     });
+
     const output = game.isClear();
     expect(output).toEqual(expectedOutput);
   });
 
   test('재시도를 위한 설정을 한다.', () => {
-    mockRandoms([1, 0, 0]);
+    mockRandoms([BRIDGE.UPPER, BRIDGE.LOWER, BRIDGE.LOWER]);
 
     const inputs = [BRIDGE.UP, BRIDGE.DOWN, BRIDGE.UP];
     const upper = [MAP.PASS];
     const lower = [MAP.BLANK];
     const expectedTryCount = 2;
     const expectedMap = [lower, upper];
-    const game = new BridgeGame(3);
+    const size = 3;
+    const game = new BridgeGame(size);
 
     inputs.forEach((input) => {
       game.move(input);
     });
     game.retry();
     game.move(BRIDGE.UP);
+
     const tryCount = game.getTryCount();
     const map = game.getMap();
     expect(tryCount).toBe(expectedTryCount);
