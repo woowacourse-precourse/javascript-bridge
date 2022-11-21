6cac9e13e0a126d13063f3a53324eba3374b285b
하지훈 <jnh03336@gmail.com>
2022-11-21 16:29:53 +0900

    style(view): 인자 타입 힌트 변경

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -23,7 +23,7 @@ const OutputView = {
 
   /**
    * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-   * @param {('U' | 'D')[]} moveStatus 여태까지의 움직임 점보를 받는다.
+   * @param {string[]} moveStatus 여태까지의 움직임 점보를 받는다.
    * @param {boolean[]} movesPossible 각각의 움직임이 가능한지 받는다.
    * @return {string} 출력
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
