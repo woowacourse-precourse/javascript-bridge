b28993ec9fb1a7d4f0021a6d0448b880f72476d1
hyeongdong <khd941018@gmail.com>
2022-11-21 21:49:50 +0900

    feat(OutputView) : 게임 결과를 출력하는 메서드 구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -10,6 +10,13 @@ const OutputView = {
     Console.print(`[ ${upBridge.join(" | ")} ]`);
     Console.print(`[ ${downBridge.join(" | ")} ]`);
   },
+
+  printResult(attemptCount, isBridgeCrossed, bridgeMap) {
+    Console.print("최종 게임 결과");
+    this.printMap(bridgeMap);
+    Console.print(`게임 성공 여부: ${isBridgeCrossed ? "성공" : "실패"}`);
+    Console.print(`총 시도한 횟수: ${attemptCount}`);
+  },
 };
 
 module.exports = OutputView;
