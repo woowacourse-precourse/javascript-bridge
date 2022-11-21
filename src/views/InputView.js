791032c487f257a0a539a785a5cc58285171dddc
nangkyeonglim <nangkyeonglim@gmail.com>
2022-11-21 20:20:41 +0900

    refactor(InputView): 상수화 적용

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,16 +1,17 @@
 const { Console } = require("@woowacourse/mission-utils");
+const { MESSAGE } = require("../constants/constants");
 
 const InputView = {
   readBridgeSize(callback) {
-    Console.readLine('다리의 길이를 입력해주세요.\n', callback);
+    Console.readLine(MESSAGE.readBridgeSize, callback);
   },
 
   readMoving(callback) {
-    Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)\n', callback);
+    Console.readLine(MESSAGE.readMoving, callback);
   },
 
   readGameCommand(callback) {
-    Console.readLine('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n', callback);
+    Console.readLine(MESSAGE.readGameCommand, callback);
   },
 };
 
