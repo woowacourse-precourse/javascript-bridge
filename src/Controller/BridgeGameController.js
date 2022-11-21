df47a3bba1b9658ff8afb1d762c90fc20e2cf81f
JangHoonSeok <orirori512@naver.com>
2022-11-21 21:48:33 +0900

    ✨ feat: 다리 랜덤 생성 및 이동할 방향 입력 받는 기능 컨트롤러에서 구현

--- src/Controller/BridgeGameController.js
+++ src/Controller/BridgeGameController.js
@@ -1,11 +1,35 @@
 const OutputView = require("../View/OutputView");
 const InputView = require("../View/InputView");
-
+const BridgeMaker = require("../BridgeMaker");
+const BridgeRandomNumberGenerator = require("../BridgeRandomNumberGenerator");
+const BridgeGame = require("../Model/BridgeGame");
 class BridgeGameController {
+  #bridge_size;
+
   start() {
     OutputView.printStartMessage();
-    InputView.readBridgeSize();
+    this.#bridge_size = InputView.readBridgeSize();
+    this.generateBridge(this.#bridge_size);
   }
+
+  generateBridge(size) {
+    const randomBridge = BridgeMaker.makeBridge(
+      size,
+      BridgeRandomNumberGenerator.generate()
+    );
+    BridgeGame.setBridge(randomBridge);
+    this.move(this.inputMoveDirection());
+  }
+
+  inputMoveDirection() {
+    return InputView.readMoving();
+  }
+
+  /**
+   * 
+   * @param {char} moving 'U', 'D' 둘 중 하나로 들어옴. 
+   */
+  move(moving) {}
 }
 
 module.exports = BridgeGameController;
