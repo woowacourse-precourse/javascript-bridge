49ea3f5ac6b54593ce1c35aab937cb6fc0dc3e82
Monte <monte6198@gmail.com>
2022-11-22 08:41:08 +0900

    feat: 플레이어 다리 길이 입력 단위 테스트 수정

--- __tests__/UnitTest/InputViewTest.js
+++ __tests__/UnitTest/InputViewTest.js
@@ -2,7 +2,7 @@ const { Console } = require('@woowacourse/mission-utils');
 const InputView = require('../../src/view/InputView');
 
 describe.only('다리 건너기 테스트', () => {
-  test('다리 생성 테스트', () => {
+  test('다리 길이 입력 테스트', () => {
     const bridgeSize = 5;
     const nextStep = jest.fn();
     // InputView.validateBridgeSize = jets.fn()
