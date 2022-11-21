0d7bf8d1c178022547a16e6fac5424436e65a467
dhkham <khamdong@gmail.com>
2022-11-21 19:24:12 +0900

    style(BridgeSizeCheck): remove unnecessary field

--- src/Check/BridgeSizeCheck.js
+++ src/Check/BridgeSizeCheck.js
@@ -1,14 +1,12 @@
 const { MissionUtils } = require("@woowacourse/mission-utils");
 
 class BridgeSizeCheck {
-  #userInput;
-
   constructor(userInput) {
-    this.#userInput = userInput;
+    this.userInput = userInput;
     this.checkInputType(userInput);
     this.checkValidLength(userInput);
   }
-  
+
   checkInputType(userInput) {
     if (!Number.isInteger(Number(userInput))) {
       throw new Error("[ERROR] 다리의 길이는 정수여야 합니다.");
