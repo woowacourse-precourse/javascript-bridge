ec0d8c4ab5e3133d8f860784260ba9128f35b5fa
osdoonhyun <skw1996@naver.com>
2022-11-21 18:29:26 +0900

    feat(App,BridgeGame): 사용자가 이동할 칸 입력 받는 기능 & 움직일 수 있는지 확인하고 움직이는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
 const OutputView = require("./OutputView");
 const InputView = require("./InputView");
+const Bridge = require("./Bridge");
+const BridgeGame = require("./BridgeGame");
 
 class App {
+  #bridge;
+  #bridgeGame;
+
+  constructor() {
+    this.#bridge = new Bridge();
+    this.#bridgeGame = new BridgeGame();
+  }
+
   play() {
     OutputView.printGameStartPhrase();
-    InputView.readBridgeSize();
+    this.inputBridgeSize();
+  }
+
+  inputBridgeSize() {
+    InputView.readBridgeSize((playerInput) => {
+      const bridgeSize = InputView.getBridgeSize(playerInput);
+      this.makeBridge(bridgeSize);
+    });
+  }
+
+  makeBridge(bridgeSize) {
+    console.log("bridgeSize:", bridgeSize);
+    this.#bridgeGame.makeBridge(bridgeSize);
+    this.inputMoving();
+  }
+
+  inputMoving() {
+    InputView.readMoving((playerInput) => {
+      const moving = InputView.getMoving(playerInput);
+      this.moveOnBridge(moving);
+    });
+  }
+
+  moveOnBridge(moving) {
+    this.#bridgeGame.move(moving);
+    if (this.#bridgeGame.isMovable()) {
+      this.inputMoving();
+    } else {
+      this.askRetryOrQuit();
+    }
+  }
+
+  askRetryOrQuit() {
+    console.log("retry or quit?");
   }
 }
 
--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,14 +1,44 @@
+const Bridge = require("./Bridge");
+const BridgeMaker = require("./BridgeMaker");
+const { generate } = require("./BridgeRandomNumberGenerator");
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  #bridge;
+  #bridgeIndex;
+  #bridgeArray;
+
+  constructor() {
+    this.#bridge = new Bridge();
+    this.#bridgeIndex = 0;
+  }
+
+  makeBridge(bridgeSize) {
+    this.#bridgeArray = BridgeMaker.makeBridge(bridgeSize, generate);
+    console.log("bridge::", this.#bridgeArray);
+  }
+
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  move() {}
+   InputView, OutputView 사용 X 
+  */
+  isMovable() {
+    let canMove = true;
+    if (this.#bridge.hasX()) canMove = false;
+    if (!this.#bridge.hasValidLength(this.#bridgeArray, this.#bridgeIndex))
+      canMove = false;
+    return canMove;
+  }
 
+  move(moving) {
+    this.#bridge.setBridge(this.#bridgeArray, this.#bridgeIndex++, moving);
+  }
+  // showBridge() {
+  //   return this.#bridge.getBridgeArray();
+  // }
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
    * <p>
