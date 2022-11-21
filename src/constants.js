6df64ddd1eb14e41a7cfc4bdec64c4c2313742d3
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-11-21 23:07:42 +0900

    feat(constant): add new error message
    Add error message of wrong bridge size

--- src/constants.js
+++ src/constants.js
@@ -4,6 +4,10 @@ module.exports = {
     INPUT_SIZE: "다리의 길이를 입력해주세요.\n",
   }),
 
+  ERROR: Object.freeze({
+    WRONG_BRIDGE_SIZE: "\n[ERROR] 다리 길이는 3 이상 20 이하의 정수여야 합니다.\n",
+  }),
+
   U: "U",
   D: "D",
 };
