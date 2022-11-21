b5dd19658311bbe959da884ce89ada6202c669e1
NaveOWO <pianowell96@gmail.com>
2022-11-21 12:35:30 +0900

    [ refactor ] input 기능 app.js로 분리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,65 @@
+const BridgeGame = require("./BridgeGame");
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+const InputView = require("./InputView");
+const OutputView = require("./OutputView");
+const BridgeMaker = require("./BridgeMaker");
+
 class App {
-  play() {}
+  startGame() {
+    InputView.readBridgeSize((size) => {
+      const bridge = new BridgeGame(
+        BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate)
+      );
+      this.progressGame(bridge, size);
+    });
+  }
+
+  progressGame(bridge, size) {
+    console.log(bridge.getBridge());
+    InputView.readMoving((answer) => {
+      if (bridge.checkInputIsCorrect(answer, bridge.step)) {
+        OutputView.printMap(bridge.step, bridge.getBridge());
+        if (bridge.checkIsLastStep(bridge.step, size - 1)) {
+          OutputView.printResult(
+            "성공",
+            bridge.round,
+            bridge.step,
+            bridge.getBridge()
+          );
+          return;
+        }
+        bridge.addStep();
+        this.progressGame(bridge, size);
+        return;
+      }
+      OutputView.printWrongMap(bridge.step, bridge.getBridge());
+      this.askRetry(bridge, size);
+    });
+  }
+
+  askRetry(bridge, size) {
+    InputView.readGameCommand((answer) => {
+      if (answer === "Q") {
+        OutputView.printResult(
+          "실패",
+          bridge.round,
+          bridge.step,
+          bridge.getBridge()
+        );
+        return;
+      }
+      if (answer === "R") {
+        bridge.addRound();
+        bridge.resetStep();
+        this.progressGame(bridge, size);
+      }
+    });
+  }
+  play() {
+    this.startGame();
+  }
 }
+const app = new App();
+app.play();
 
 module.exports = App;
--- src/InputView.js
+++ src/InputView.js
@@ -1,7 +1,4 @@
 const { Console } = require("@woowacourse/mission-utils");
-const BridgeGame = require("./BridgeGame");
-const BridgeMaker = require("./BridgeMaker");
-const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
 const OutputView = require("./OutputView");
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -11,13 +8,17 @@ const InputView = {
    * 다리의 길이를 입력받는다.
    */
 
-  readBridgeSize() {
-    Console.readLine("다리의 길이를 입력해주세요.", (size) => {
-      const bridge = new BridgeGame(
-        BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate)
-      );
-      InputView.readMoving(bridge, Number(size), 0, 1);
-    });
+  // readBridgeSize() {
+  //   Console.readLine("다리의 길이를 입력해주세요.", (size) => {
+  //     const bridge = new BridgeGame(
+  //       BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate)
+  //     );
+  //     InputView.readMoving(bridge, Number(size), 0, 1);
+  //   });
+  // },
+
+  readBridgeSize(callBack) {
+    Console.readLine("다리의 길이를 입력해주세요.", callBack);
   },
 
   /**
@@ -25,37 +26,52 @@ const InputView = {
    */
   // step: 0,
 
-  readMoving(bridge, size, step, round) {
-    Console.readLine("이동할 칸을 선택해주세요. (위: U, 아래: D)", (answer) => {
-      if (bridge.checkInputIsCorrect(answer, step)) {
-        if (!bridge.move(step, size)) {
-          this.readMoving(bridge, size, step + 1, round);
-        }
-        OutputView.printResult("성공", round, step, bridge);
-        return;
-      }
-      OutputView.printWrongMap(step, bridge);
-      this.readGameCommand(bridge, size, step, round);
-    });
+  // readMoving(bridge, size, step, round) {
+  // Console.readLine("이동할 칸을 선택해주세요. (위: U, 아래: D)", (answer) => {
+  //   if (bridge.checkInputIsCorrect(answer, step)) {
+  //     OutputView.printMap(step, bridge.getBridge());
+  //     if (bridge.checkIsLastStep(step, size - 1)) {
+  //       OutputView.printResult("성공", round, step, bridge.getBridge());
+  //       return;
+  //     }
+  //     this.readMoving(bridge, size, step + 1, round);
+  //     return;
+  //   }
+  //   OutputView.printWrongMap(step, bridge.getBridge());
+  //   this.readGameCommand(bridge, size, step, round);
+  // });
+  // bridge.move();
+
+  // },
+
+  readMoving(callBack) {
+    Console.readLine("이동할 칸을 선택해주세요. (위: U, 아래: D)", callBack);
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand(bridge, size, step, round) {
-    Console.readLine(
-      "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n",
-      (answer) => {
-        if (answer === "Q") {
-          OutputView.printResult("실패", round, step, bridge);
-          return;
-        }
-        if (answer === "R") {
-          this.readMoving(bridge, size, step, round + 1);
-        }
-      }
-    );
-  },
+  //   readGameCommand(bridge, size, step, round) {
+  //     Console.readLine(
+  //       "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n",
+  //       (answer) => {
+  //         if (answer === "Q") {
+  //           OutputView.printResult("실패", round, step, bridge.getBridge());
+  //           return;
+  //         }
+  //         if (answer === "R") {
+  //           this.readMoving(bridge, size, 0, round + 1);
+  //         }
+  //       }
+  //     );
+  //   },
+
+  // readGameCommand(callBack) {
+  //   Console.readLine(
+  //     "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n",
+  //     callBack
+  //   );
+  // },
 };
 
 module.exports = InputView;
