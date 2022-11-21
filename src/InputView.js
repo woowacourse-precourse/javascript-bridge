371191fa4eb60340638e171358dcb3052def61f1
yuna Kim <yuna000804@gmail.com>
2022-11-22 02:56:23 +0900

    feat : 게임 재시도, 종료 여부 문구 출력 후 재시작(R), 종료(Q) 문자 입력

--- src/InputView.js
+++ src/InputView.js
@@ -4,6 +4,7 @@ const { generate } = require("./BridgeRandomNumberGenerator");
 const { makeBridge } = require("./BridgeMaker");
 const BridgeGame = require("./BridgeGame");
 const OutputView = require("./OutputView");
+const { printResult } = require("./OutputView");
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -12,14 +13,14 @@ const InputView = {
    * 다리의 길이를 입력받는다.
    */
   bridgeGame: new BridgeGame(),
+  retryNum: 1,
+  isSuccess: true,
 
   readBridgeSize() {
     Console.readLine(USER_INPUT.ENTER_LENGTH, (size) => {
       const bridgeList = makeBridge(size, generate);
-      console.log(bridgeList);
+      // console.log(bridgeList);
       InputView.readMoving(bridgeList);
-      
-      // this.readMoving(this.bridgeGame);
     });
   },
   
@@ -34,16 +35,23 @@ const InputView = {
   },
   repeatMoving(bridgeList, result) {
     const [upList, downList, tryNum] = result;
-    // console.log(upList[upList.length-1], downList[downList.length-1]);
     const upAndDown = ['[ ' + upList.join(' | ') + ' ]', '[ ' + downList.join(' | ') + ' ]'];
     OutputView.printMap(upAndDown);
-    
+    if([upList[upList.length-1], downList[downList.length-1]].includes('X')) {
+      this.readGameCommand(upAndDown, bridgeList);
+    }
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand(upAndDown, bridgeList) {
+    Console.readLine(RETRY, (retryOrQuit) => {
+      if(retryOrQuit === 'R') {
+      } else if(retryOrQuit === 'Q') {
+      }
+    })
+  },
 };
 
 module.exports = InputView;
--- src/Messages.js
+++ src/Messages.js
@@ -4,5 +4,9 @@ const USER_INPUT = {
   ENTER_MOVEMENT: '\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
 }
 const RETRY = '\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n';
-
-module.exports = {START_GAME, USER_INPUT, RETRY};
+const END_GAME = {
+  RESULT: '\n최종 게임 결과',
+  IS_SUCCESS: '\n게임 성공 여부: ',
+  TOTAL_RETRY: '총 시도한 횟수: '
+}
+module.exports = {START_GAME, USER_INPUT, RETRY, END_GAME};
