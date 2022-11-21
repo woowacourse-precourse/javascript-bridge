5e4ec595b0d26d0b650975466851e1d7b3f6fd1c
chex <chex417@naver.com>
2022-11-21 23:26:21 +0900

    refactor: 불필요한 주석 제거

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -3,9 +3,6 @@ const BridgeResult = require('./domain/BridgeResult');
 const BridgeMaker = require('./BridgeMaker');
 const { generate } = require('./BridgeRandomNumberGenerator');
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
 class BridgeGame {
   #bridge;
   #bridgeResult;
@@ -17,11 +14,6 @@ class BridgeGame {
     this.#bridgeResult = new BridgeResult(bridgePattern.length);
   }
 
-  /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
   move(moving) {
     this.#player.movings.push(moving);
     const { bridgeMap, checking } = this.#bridge.match(this.#player.movings);
@@ -30,11 +22,6 @@ class BridgeGame {
     return { bridgeMap, checking, isSuccess };
   }
 
-  /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
   retry() {
     this.#player.movings = [];
     this.#bridgeResult.addTryCount();
