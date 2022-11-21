b3bfa8cd17e75a142e5107ceabdb7606c91ac430
dongyeopca <ckehdduq95@gmail.com>
2022-11-21 12:55:27 +0900

    feat(App): 게임 생성 및 시작 코드 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+const { Console } = require('@woowacourse/mission-utils');
+const BridgeGame = require('./BridgeGame');
+const InputView = require('./InputView');
+const OutputView = require('./OutputView');
+
 class App {
-  play() {}
+  play() {
+    const bridgegame = new BridgeGame();
+    this.startGame(bridgegame);
+  }
+
+  startGame(game) {
+    OutputView.printStartMessage();
+    InputView.readBridgeSize(game);
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
