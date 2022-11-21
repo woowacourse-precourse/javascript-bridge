ba6e24bb058b2f24732e8327a25b439c270f1d5b
nangkyeonglim <nangkyeonglim@gmail.com>
2022-11-21 20:18:26 +0900

    refactor(BridgeMaker): 상수화 적용

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,8 +1,10 @@
+const { HOTKEY } = require("./constants/constants");
+
 const BridgeMaker = {
   makeBridge(size, generateRandomNumber) {
     const bridge = [];
     for (let count = 0; count < size; count++){
-      generateRandomNumber() ? bridge.push('U') : bridge.push('D');
+      generateRandomNumber() ? bridge.push(HOTKEY.up) : bridge.push(HOTKEY.down);
     }
     return bridge;
   },
