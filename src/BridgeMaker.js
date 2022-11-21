a184d1e73d544aa706b6e0f0d27465d02046fa8e
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-21 12:20:14 +0900

    refactor(BridgeMaker): 자료형 Number로 고정 및 주석 삭제

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -3,10 +3,9 @@ const { DOWNSIDE_SYMBOL_NUM, DOWNSIDE_SYMBOL, UPSIDE_SYMBOL } = require('./const
 const BridgeMaker = {
   makeBridge(size, generateRandomNumber) {
     return Array.from({ length: size }, () => {
-      // ApplicationTest 테스트 코드 문자열로 주어짐... -->  ["1", "0", "0"];
-      const randomNum = String(generateRandomNumber());
+      const randomNum = Number(generateRandomNumber());
 
-      if (randomNum === String(DOWNSIDE_SYMBOL_NUM)) return DOWNSIDE_SYMBOL;
+      if (randomNum === DOWNSIDE_SYMBOL_NUM) return DOWNSIDE_SYMBOL;
 
       return UPSIDE_SYMBOL;
     });
