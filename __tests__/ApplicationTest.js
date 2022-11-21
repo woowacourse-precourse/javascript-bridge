54681d728dc801fa5478cf1614c7ff99dff2a5e3
WaiNaat <qoli0703@gmail.com>
2022-11-21 23:10:37 +0900

    test: parameterize feature test

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -63,23 +63,30 @@ describe('다리 건너기 테스트', () => {
     expect(bridge).toEqual(['U', 'D', 'D']);
   });
 
-  test('기능 테스트', () => {
+  test.each([
+    [
+      [1, 0, 1],
+      ['3', 'U', 'D', 'U'],
+      [
+        '최종 게임 결과',
+        '[ O |   | O ]',
+        '[   | O |   ]',
+        '게임 성공 여부: 성공',
+        '총 시도한 횟수: 1',
+      ],
+      ['[ O |   | O ]', '[   | O |   ]'],
+    ],
+  ])('기능 테스트', (bridge, inputs, solutionLog, solutionBridge) => {
     const logSpy = getLogSpy();
-    mockRandoms([1, 0, 1]);
-    mockQuestions(['3', 'U', 'D', 'U']);
+    mockRandoms(bridge);
+    mockQuestions(inputs);
 
     const app = new App();
     app.play();
 
     const log = getOutput(logSpy);
-    expectLogContains(log, [
-      '최종 게임 결과',
-      '[ O |   | O ]',
-      '[   | O |   ]',
-      '게임 성공 여부: 성공',
-      '총 시도한 횟수: 1',
-    ]);
-    expectBridgeOrder(log, '[ O |   | O ]', '[   | O |   ]');
+    expectLogContains(log, solutionLog);
+    expectBridgeOrder(log, ...solutionBridge);
   });
 
   test('예외 테스트', () => {
