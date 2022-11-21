e90ea82c92fad72b68b30ab19a92daccc28156c3
moonelysian <dandyblack@naver.com>
2022-11-21 15:52:11 +0900

    feat: move 함수 작성
    - bridge: 현재 다리 모양
    - currentIndex: 사용자의 위치
    - count: 시도 횟수
    - map: 사용자가 움직인 지도
    
    - move(): 사용자가 움직인 위치와 다리가 모양이 맞는지 확인하고 마지막 위치인지 확인한다.

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,13 +1,33 @@
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
+const { MOVEMENT, COMMAND, BRIDGE_SIZE, ERROR_MESSAGE } = require('./constructor.js');
 class BridgeGame {
+  #bridge
+  #currentIndex
+  #count
+  #map
+
+  constructor(bridge) {
+    this.#bridge = bridge;
+    this.#currentIndex = 0;
+    this.#count = 1;
+    this.#map = { 1: [], 0: [] }
+  }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(movement) {
+    const isCorrect = this.#bridge[this.#currentIndex] === movement;
+    const isEnd = this.#currentIndex === this.#bridge.length - 1;
+    const mark = isCorrect ? 'O' : 'X';
+    const markPosition = movement === MOVEMENT.UP ? BRIDGE_POSITION.UP : BRIDGE_POSITION.DOWN;
+    this.#map[markPosition].push(mark);
+    if (isCorrect) this.#currentIndex += 1;
+    return { isCorrect, isEnd, count: this.#count, map: this.#map,  }
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
