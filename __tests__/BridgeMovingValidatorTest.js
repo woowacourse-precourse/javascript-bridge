e5614b0b90b656747fa7863d056c88dfd3fea341
Suyoung <jsy8492@gmail.com>
2022-11-21 14:16:48 +0900

    test(BridgeMovingValidatorTest): 사용자가 입력한 이동할 칸의 유효성을 검사하는 객체 테스트 작성

---
+++ __tests__/BridgeMovingValidatorTest.js
@@ -0,0 +1,10 @@
+const BridgeMovingValidator = require('../src/validators/BridgeMovingValidator');
+
+describe('사용자가 입력한 이동할 칸의 유효성을 검사하는 객체 테스트', () => {
+  test.each([['DD'], ['UU'], ['A'], ['ABC']])(
+    '유효하지 않은 이동인 경우 예외 처리',
+    input => {
+      expect(input => BridgeMovingValidator.validate(input)).toThrow('[ERROR]');
+    },
+  );
+});
