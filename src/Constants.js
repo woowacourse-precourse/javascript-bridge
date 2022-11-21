9dda436a1cd8cb91d4918999555b72dabede3c7f
jinseo <ghkdwlstj99@gmail.com>
2022-11-21 14:00:50 +0900

    refactor: 게임 결과 문자열 상수화

--- src/Constants.js
+++ src/Constants.js
@@ -4,4 +4,12 @@ const INPUT_MESSAGE = {
     GAME_RUNNING_MASSAGE : "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"
 }
 
-module.exports = {INPUT_MESSAGE};
+const PRINT_MESSAGE = {
+    GAME_START : "다리 건너기 게임을 시작합니다.\n",
+    FINAL_GAME_RESULT : "최종 게임 결과\n",
+    GAME_RESULT_SUCESS : "게임 성공 여부: 성공\n",
+    GAME_RESULT_FAIL : "게임 성공 여부: 실패\n",
+    GAME_TOTAL_TRY : "총 시도한 횟수: "
+}
+
+module.exports = {INPUT_MESSAGE, PRINT_MESSAGE};
--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -1,11 +1,12 @@
 const { Console } = require("@woowacourse/mission-utils");
+const { PRINT_MESSAGE } = require("../Constants");
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
  const OutputView = {
 
   printStart(){
-    Console.print("다리 건너기 게임을 시작합니다.");
+    Console.print(PRINT_MESSAGE.GAME_START);
   },
   /**
    * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -24,18 +25,18 @@ const { Console } = require("@woowacourse/mission-utils");
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printResult(userBridgeUp, userBridgeDown, count){
-    Console.print("최종 게임 결과\n");
+    Console.print(PRINT_MESSAGE.FINAL_GAME_RESULT);
     this.printMap(userBridgeUp, userBridgeDown);
-    Console.print("게임 성공 여부: 성공\n");
-    Console.print("총 시도한 횟수: " + `${count}`);
+    Console.print(PRINT_MESSAGE.GAME_RESULT_SUCESS);
+    Console.print(PRINT_MESSAGE.GAME_TOTAL_TRY + `${count}`);
     Console.close();
   },
 
   printFailResult(userBridgeUp, userBridgeDown, count){
-    Console.print("최종 게임 결과\n");
+    Console.print(PRINT_MESSAGE.FINAL_GAME_RESULT);
     this.printMap(userBridgeUp, userBridgeDown);
-    Console.print("게임 성공 여부: 실패\n");
-    Console.print("총 시도한 횟수: " + `${count}`);
+    Console.print(PRINT_MESSAGE.GAME_RESULT_FAIL);
+    Console.print(PRINT_MESSAGE.GAME_TOTAL_TRY + `${count}`);
     Console.close();
   }
 };
