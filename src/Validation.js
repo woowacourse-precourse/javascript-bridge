5e851bb510ef5086c12dd0365c0d0805f29f0d30
seunghoria <102285042+seunghoria@users.noreply.github.com>
2022-11-21 11:51:51 +0900

    fix: 다리 이동 입력값 유효성 검증 기능 수정

--- src/Validation.js
+++ src/Validation.js
@@ -1,11 +1,16 @@
 class Validation {
   static checkBridgeSize(size) {
-    if (size < 3 || size > 20) throw Error;
-    if (isNaN(size)) throw Error;
+    if (size < 3 || size > 20) throw new Error();
+    if (isNaN(size)) throw new Error();
   }
 
   static checkDirectionInput(direction) {
-    if (direction === "U" || direction === "D") throw new Error();
+    if (!this.#isValidDirection(direction)) throw new Error();
+  }
+
+  static #isValidDirection(direction) {
+    return direction === "D" || direction === "U";
   }
 }
+
 module.exports = Validation;
