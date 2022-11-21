fbbc6e2c38a0d89f976e8a2e2914d926f3658c2c
Wang HoEun <hoeunwang@gmail.com>
2022-11-21 09:43:25 +0900

    feat: 다리 길이 입력 후 유효성 검사

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 const { Console } = require("@woowacourse/mission-utils");
+const InputView = require("./InputView");
+const Validation = require("./Validation");
 class App {
   play() {
     Console.print("다리 건너기 게임을 시작합니다.\n");
+    this.requestBridgeSize();
+  }
+  requestBridgeSize() {
+    InputView.readBridgeSize((size) => {
+      Validation.validateBridgeSize(size);
+    });
   }
 }
 const app = new App();
