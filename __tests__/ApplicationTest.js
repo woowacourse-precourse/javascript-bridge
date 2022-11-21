f313c8e803da9d408436749b238cb9669b3aede0
jsh3418 <albert5428@gmail.com>
2022-11-21 21:05:18 +0900

    test(validate): 유효성 검사 테스트 코드 작성

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,6 +1,9 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const App = require("../src/App");
 const BridgeMaker = require("../src/BridgeMaker");
+const validateBridgeSize = require("../src/validation/validateBridgeSize");
+const validateMoveUpOrDownAnswer = require("../src/validation/validateMoveUpOrDownAnswer");
+const validateRetryCommand = require("../src/validation/validateRetryCommand");
 
 const mockQuestions = (answers) => {
   MissionUtils.Console.readLine = jest.fn();
@@ -71,17 +74,31 @@ describe("다리 건너기 테스트", () => {
     app.play();
 
     const log = getOutput(logSpy);
-    expectLogContains(log, [
-      "최종 게임 결과",
-      "[ O |   | O ]",
-      "[   | O |   ]",
-      "게임 성공 여부: 성공",
-      "총 시도한 횟수: 1",
-    ]);
+    expectLogContains(log, ["최종 게임 결과", "[ O |   | O ]", "[   | O |   ]", "게임 성공 여부: 성공", "총 시도한 횟수: 1"]);
     expectBridgeOrder(log, "[ O |   | O ]", "[   | O |   ]");
   });
 
   test("예외 테스트", () => {
     runException(["a"]);
+  });
+});
+
+describe("예외 테스트", () => {
+  test.each([["2"], ["21"], ["Q"]])("다리 사이즈를 입력할 때 유효하지 않은 값을 넣었을 때 에러 처리해야 한다.", (input) => {
+    expect(() => {
+      validateBridgeSize(input);
+    }).toThrow("[ERROR]");
+  });
+
+  test.each([["u"], ["d"], ["UD"]])("게임 진행할 때 'U', 'D'외에 유효하지 않은 값을 넣었을 때 에러 처리해야 한다.", (input) => {
+    expect(() => {
+      validateMoveUpOrDownAnswer(input);
+    }).toThrow("[ERROR]");
+  });
+
+  test.each([["r"], ["q"], ["RQ"]])("게임 재시도할지 물어볼 때 'R', 'Q'외에 유효하지 않은 값을 넣었을 때 에러 처리해야 한다.", (input) => {
+    expect(() => {
+      validateRetryCommand(input);
+    }).toThrow("[ERROR]");
   });
 });
