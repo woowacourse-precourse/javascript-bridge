5fca6b486b05ca1f75b376d917a952144d6b0a90
Lee-Young-Jae <yjlee1937@naver.com>
2022-11-21 13:54:19 +0900

    fix: 다리 생성시 문자열 숫자 입력도 처리하도록 수정

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,3 +1,4 @@
+const { toNumber } = require("./helpers/common");
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
@@ -8,7 +9,10 @@ const BridgeMaker = {
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
   makeBridge(size, generateRandomNumber) {
-    return Array.from({ length: size }, () => (generateRandomNumber() === "1" ? "U" : "D"));
+    return Array.from({ length: size }, () => {
+      const randomNumber = toNumber(generateRandomNumber());
+      return randomNumber > 0.5 ? "U" : "D";
+    });
   },
 };
 
