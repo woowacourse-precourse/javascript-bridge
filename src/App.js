41a27cf8dd96a45a193b7939a111303c76b55626
Wang HoEun <hoeunwang@gmail.com>
2022-11-21 10:09:15 +0900

    feat: 다리 인스턴스 변수 생성 후 전달

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 const { Console } = require("@woowacourse/mission-utils");
+const BridgeGame = require("./BridgeGame");
 const BridgeMaker = require("./BridgeMaker");
 const InputView = require("./InputView");
 const Validation = require("./Validation");
 class App {
+  constructor() {
+    this.bridgeGame = null;
+  }
   play() {
     Console.print("다리 건너기 게임을 시작합니다.\n");
     this.requestBridgeSize();
@@ -14,7 +18,8 @@ class App {
         Console.print(errorMsg);
         return this.requestBridgeSize();
       }
-      BridgeMaker.makeBridge(Number(size), generate);
+      const bridge = BridgeMaker.makeBridge(Number(size), generate);
+      this.bridgeGame = new BridgeGame(bridge);
     });
   }
 }
