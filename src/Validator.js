f3cd6fe302e3d0692b0e0b7f740a93b327a4b27d
Suhyeon Lee <suhyeonlee.dev@gmail.com>
2022-11-22 03:49:13 +0900

    feat: 기능 4 이동할 칸 입력

--- src/Validator.js
+++ src/Validator.js
@@ -1,13 +1,19 @@
 const Validator = {
   checkNumber(userInput) {
     if (!userInput.match(/^[0-9]+$/)) {
-      throw new Error('숫자를 입력해주세요.');
+      throw new Error('[ERROR] 숫자를 입력해주세요.');
     }
   },
 
   checkRange(userInput) {
     if (userInput < 3 || userInput > 20) {
-      throw new Error('3이상 20이하의 숫자를 입력해주세요.');
+      throw new Error('[ERROR] 3이상 20이하의 숫자를 입력해주세요.');
+    }
+  },
+
+  checkDirection(userInput) {
+    if (userInput !== 'U' && userInput !== 'D') {
+      throw new Error('[ERROR] U 또는 D를 입력해주세요.');
     }
   },
 };
