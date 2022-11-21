e9f872cdf99946aebc78a88f047c037d2f4b2425
jhyns <tockiib@gmail.com>
2022-11-21 22:33:34 +0900

    feat: Add hasError method for Validate class
    validate 클래스에서 에러를 catch하는 메소드 추가

--- src/Validate.js
+++ src/Validate.js
@@ -1,32 +1,39 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const { ERROR } = require('./Constants');
+
 const Validate = {
   bridgeSize(size) {
     if (isNaN(size)) {
-      throw new Error(MESSAGES.ERROR.BRIDGE_LENGTH_NAN);
+      throw new Error(ERROR.BRIDGE_LENGTH_NAN);
     }
     if (!Number.isInteger(size)) {
-      throw new Error(MESSAGES.ERROR.BRIDGE_LENGTH_NOT_INTEGER);
+      throw new Error(ERROR.BRIDGE_LENGTH_NOT_INTEGER);
     }
     if (size < 3 || size > 20) {
-      throw new Error(MESSAGES.ERROR.BRIDGE_LENGTH_RANGE);
+      throw new Error(ERROR.BRIDGE_LENGTH_RANGE);
     }
   },
 
   moving(moving) {
     if (moving !== 'U' && moving !== 'D') {
-      throw new Error(MESSAGES.ERROR.MOVING_CMD_NOT_UD);
+      throw new Error(ERROR.MOVING_CMD_NOT_UD);
     }
   },
 
   gameCommand(command) {
     if (command !== 'R' && command !== 'Q') {
-      throw new Error(MESSAGES.ERROR.RETRY_CMD_NOT_RQ);
+      throw new Error(ERROR.RETRY_CMD_NOT_RQ);
     }
   },
 
-  bridgeGenerateNumber(randomNumber) {
-    if (randomNumber !== 0 && randomNumber !== 1) {
-      throw new Error(ERROR.BRIDGE_GENERATOR_RANGE);
+  hasError(validator, argument) {
+    try {
+      validator(argument);
+    } catch (error) {
+      MissionUtils.Console.print(error.message);
+      return true;
     }
+    return false;
   },
 };
 
