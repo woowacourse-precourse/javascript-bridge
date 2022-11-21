715a110038f4e0818d38d2ee028121ca4a5e4d51
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-11-21 13:10:52 +0900

    refactoring($errors): 에러 constants 리팩토링

--- src/constants/errors/MakeBridgeError.js
+++
@@ -1,9 +0,0 @@
-const IS_INPUT_NOT_NUMBER = '[ERROR] 다리 길이는 숫자여야 합니다.';
-const IS_INPUT_EMPTY = '[ERROR] 다리 길이를 입력해주세요.';
-const IS_INPUT_NOT_IN_RANGE = '[ERROR] 다리의 길이는 3이상 20이하입니다.';
-
-module.exports = {
-  IS_INPUT_NOT_NUMBER,
-  IS_INPUT_EMPTY,
-  IS_INPUT_NOT_IN_RANGE
-};
---
+++ src/constants/errors/index.js
@@ -0,0 +1,4 @@
+const IS_SIZE_INPUT_ERROR = '[ERROR] 다리의 길이는 3 이상 20 이하의 숫자여야 합니다.';
+const IS_MOVING_INPUT_ERROR = '[ERROR] U 또는 D를 입력해주세요.';
+
+module.exports = { IS_SIZE_INPUT_ERROR, IS_MOVING_INPUT_ERROR };
