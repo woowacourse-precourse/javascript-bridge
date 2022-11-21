3c49b50c065a4d7a8a6ccb8c4756ee2df06d0441
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2022-11-21 17:37:13 +0900

    feat: add function to print bridge line by line

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,3 +1,4 @@
+const { Console } = require("@woowacourse/mission-utils");
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -8,6 +9,15 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printMap() {},
+  makeBridgeLine(bridge, direction) {
+    const bridgeLine = bridge.map((block) => {
+      if (block === direction) {
+        return "O";
+      }
+      return " ";
+    });
+    return bridgeLine;
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
