3643c40053b1f26555c804dbaa99fb623287f079
Raehyeon Jeong <raehy19@gmail.com>
2022-11-21 19:14:12 +0900

    feat(src/inputView.js) : 사용자에게 다리 위 아래 선택 입력받고 예외처리 메소드

--- src/InputView.js
+++ src/InputView.js
@@ -3,6 +3,8 @@ const MissionUtils = require('@woowacourse/mission-utils');
 const MSG = {
   BRIDGE_SIZE: '다리의 길이를 입력해주세요.\n',
   BRIDGE_SIZE_ERR: '[ERROR] 다리 길이는 3부터 20 사이의 정수 여야 합니다.\n',
+  MOVING: '이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
+  MOVING_ERR: '[ERROR] 이동할 칸은 U 또는 D 여야 합니다.\n',
 };
 
 const InputView = {
@@ -29,10 +31,22 @@ const InputView = {
     }
   },
 
-  /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
-  readMoving() {},
+  readMoving(setMove) {
+    MissionUtils.Console.readLine(MSG.MOVING, (input) => {
+      try {
+        this.validateMoving(input);
+        setMove(input);
+      } catch (e) {
+        MissionUtils.Console.print(e);
+        this.readMoving(setMove);
+      }
+    });
+  },
+  validateMoving(input) {
+    if (input !== 'U' && input !== 'D') {
+      throw new Error(MSG.MOVING_ERR);
+    }
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
