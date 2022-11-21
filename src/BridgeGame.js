179fe22705482e876367cf06eed984b1a74fbd08
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2022-11-21 21:46:32 +0900

    feat: add function to print final result

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -26,6 +26,10 @@ class BridgeGame {
     InputView.readMoving(this);
   }
 
+  moveFinalRound(){
+    OutputView.printResult(this.#round,this.#bridgeBoard.getClearedBridge());
+  }
+
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
    * <p>
