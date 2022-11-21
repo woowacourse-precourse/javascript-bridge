ecda711bd37bda5419c06a18e5455a31b07c711c
1 <wrasf175@naver.com>
2022-11-21 13:59:23 +0900

    feat(BridgeGame): move 메서드 기능 구현

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -3,8 +3,8 @@ const BridgeGame = require('../src/BridgeGame');
 describe('다리 건너기 게임 단위 테스트', () => {
     it('다리 앞으로 건너가기', () => {
         const bridgeGame = new BridgeGame();
-        const test1 = bridgeGame.move('U');
-        const test2 = bridgeGame.move('D');
+        const test1 = bridgeGame.move('U', []);
+        const test2 = bridgeGame.move('D', []);
         expect(test1).toEqual([1]);
         expect(test2).toEqual([0]);
     });
--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -2,12 +2,24 @@
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  #userInputArray
+  constructor(){
+    this.#userInputArray = [];
+  }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(moveInput, inputArray) {
+    if(moveInput === 'U'){
+      inputArray.push(1);
+    }
+    if(moveInput === 'D'){
+      inputArray.push(0);
+    }
+    return inputArray;
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
