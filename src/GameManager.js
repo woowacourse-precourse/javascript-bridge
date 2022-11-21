542a5434266dcd9d82cf838e812a9f69eeeda8ed
kwh97 <zzzang0512@naver.com>
2022-11-21 17:40:27 +0900

    feat: 기존의 다리길이 입력값을 받는 기능을 하는 함수를 GameManager 클래스에서 새롭게 구현

---
+++ src/GameManager.js
@@ -0,0 +1,24 @@
+const InputView = require("./InputView");
+const Validation = require("./Validation");
+const OutputView = require("./OutputView");
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+const BridgeMaker = require("./BridgeMaker");
+
+
+const { INPUT_QUESTION } = require("./utils/Constants");
+
+
+
+class GameManager {
+
+  inputBridgeSize(callback) {
+    InputView.readBridgeSize(INPUT_QUESTION.bridgeLen, (bridgeSize) => {
+      Validation.checkBridgeLength(bridgeSize);
+      const bridge = BridgeMaker.makeBridge(bridgeSize, BridgeRandomNumberGenerator.generate);
+      callback(bridge);
+    });
+  }
+
+}
+
+module.exports = GameManager;
