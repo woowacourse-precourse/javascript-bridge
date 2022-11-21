aa5eb936245265fb94f80e44f5d8baa5f16458af
KimJerry <k90966277@gmail.com>
2022-11-21 23:03:10 +0900

    feat: 사용자 입력 비동기 처리 및 출력 확인

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const InputView = require('./InputView');
+const OutputView = require('./OutputView');
+const BridgeMaker = require('./BridgeMaker');
+const BridgeGame = require('./BridgeGame');
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+    async play() {
+        MissionUtils.Console.print('다리 건너기 게임을 시작합니다.\n');
+        const result = await InputView.readBridgeSize();
+        MissionUtils.Console.print(result);
+        MissionUtils.Console.close();
+    }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
