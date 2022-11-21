5ac31cc8b8a7ff9549490190a5943c65df5fd868
chsua <csj1919@naver.com>
2022-11-21 15:31:18 +0900

    feat: 게임  결과 출력 함수(OutputView.js)

--- src/OutputView.js
+++ src/OutputView.js
@@ -20,11 +20,19 @@ const MissionUtils = require("@woowacourse/mission-utils");
   },
 
   /**
-   * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-   * <p>
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+   * 게임의 최종 결과 출력 함수
+   * @param {arr} arr 다리를 프린팅하기 위한 배열 (ex.[["   ","   "," O "],[" O "," O ","   "]])
+   * @param {number} tryCount 지금까지의 게임 시도횟수 
+   * @param {string} result 게임 결과 (ex. "성공","실패")
    */
-  printResult() {},
+   printResult(arr, tryCount, result) {
+    MissionUtils.Console.close();
+    let [one, two] = arr ;
+    let successFail = (result == "Success") ? "성공" : "실패" ;
+    MissionUtils.Console.print("최종 게임 결과");
+    MissionUtils.Console.print(`[ ${one.join(" | ")} ]\n[ ${two.join(" | ")} ]`);
+    MissionUtils.Console.print(`게임 성공 여부: ${successFail}\n총 시도한 횟수: ${tryCount}`);
+  },
 };
 
 module.exports = OutputView;
