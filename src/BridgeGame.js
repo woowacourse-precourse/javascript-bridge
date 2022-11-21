3f5cb5732d6681a6fb91f25c0090b6f976acda1e
KINHYEONJI <hyeonji0718@gmail.com>
2022-11-22 02:26:41 +0900

    feat(BridgeGame) : 다리를 건너고 난 후 다리 상태를 출력해주는 함수 nowBridgeState 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -5,6 +5,8 @@ class BridgeGame {
   #bridge;
   #moveCount = 0;
   #playerInput;
+  #nowUpBridge = [];
+  #nowDownBridge = [];
 
   constructor(bridge) {
     this.#bridge = bridge;
@@ -13,6 +15,29 @@ class BridgeGame {
   haveBridge() {
     if (this.#playerInput === this.#bridge[this.#moveCount - 1]) {
       return true;
+    }
+  }
+
+  nowBridgeState() {
+    if (this.#bridge[this.#moveCount] === "U") {
+      if (this.#playerInput === "U") {
+        this.#nowUpBridge.push("O");
+        this.#nowDownBridge.push(" ");
+      }
+      if (this.#playerInput === "D") {
+        this.#nowUpBridge.push(" ");
+        this.#nowDownBridge.push("X");
+      }
+    }
+    if (this.#bridge[this.#moveCount] === "D") {
+      if (this.#playerInput === "D") {
+        this.#nowUpBridge.push(" ");
+        this.#nowDownBridge.push("O");
+      }
+      if (this.#playerInput === "U") {
+        this.#nowUpBridge.push("X");
+        this.#nowDownBridge.push(" ");
+      }
     }
   }
 }
