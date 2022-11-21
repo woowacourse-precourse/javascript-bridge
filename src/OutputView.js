ddadf05d6d7b0f4b4d02a12be9b3f41c213e6cd5
wang-yurin <king_1994@naver.com>
2022-11-21 20:09:00 +0900

    feat(OutputView): 다리 모양 출력 기능 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,11 +1,15 @@
 const { Console } = require("@woowacourse/mission-utils");
+const Player = require("./Player");
 
 const OutputView = {
   printStart() {
     Console.print("다리 건너기 게임을 시작합니다.\n");
   },
 
-  printMap() {},
+  printMap() {
+    Console.print(`[${Player.state[0].join("|")}]`);
+    Console.print(`[${Player.state[1].join("|")}]`);
+  },
 
   printResult() {},
 };
