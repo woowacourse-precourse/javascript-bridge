ee037bb4c456f674b36a003af713fc83d68e353d
Buyoung Chung <sunarol@naver.com>
2022-11-21 19:59:06 +0900

    feat: function printMap print bridges

---
+++ src/View/OutputView.js
@@ -0,0 +1,26 @@
+const Message = require("../utils/Message");
+const { Console } = require("@woowacourse/mission-utils");
+/**
+ * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
+ */
+const OutputView = {
+  /**
+   *게임의 첫 시작을 알리는 문구 출력
+   */
+  printGreeting() {
+    Console.print(Message.GREETING);
+  },
+
+  /**
+   * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
+   * @param {string[]} bridges upBridge와 downBridge가 순서대로 담긴 배열
+   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+   */
+  printMap(bridges) {
+    bridges.forEach((bridge) => {
+      Console.print(bridge);
+    });
+  },
+};
+
+module.exports = OutputView;
