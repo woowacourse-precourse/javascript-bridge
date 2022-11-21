684327b94883d6054860a24d072d013d5774c8bd
vinivin153 <vinivin153@naver.com>
2022-11-22 06:28:42 +0900

    refactor: 주석 제거

--- src/InputView.js
+++ src/InputView.js
@@ -1,31 +1,18 @@
 const { Console } = require('@woowacourse/mission-utils');
 const { MESSAGE_INPUT } = require('./utils/constant');
-const { makeBridge } = require('./BridgeMaker');
-const { generate } = require('./BridgeRandomNumberGenerator');
-const { bridgeSizeValidator, directionValidator } = require('./Validator');
-const OutputView = require('./OutputView');
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+
 const InputView = {
-  /**
-   * 다리의 길이를 입력받는다.
-   */
   readBridgeSize(callback) {
     Console.readLine(MESSAGE_INPUT.BRIDGE_SIZE, callback);
   },
 
-  /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
   readMoving(callback) {
     Console.readLine(MESSAGE_INPUT.MOVING, callback);
   },
 
-  /**
-   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-   */
-  readGameCommand() {},
+  readGameCommand(callback) {
+    Console.readLine(MESSAGE_INPUT.GAME_COMMAND, callback);
+  },
 };
 
 module.exports = InputView;
