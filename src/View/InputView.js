849da9f49612ae98817a40c28937ddd6a60c91ba
leeharyung <lhr4884@naver.com>
2022-11-22 04:08:04 +0900

    chore: 주석 삭제

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -7,31 +7,19 @@ const INPUT = {
 		'\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
 };
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 const InputView = {
-	/**
-	 * 다리의 길이를 입력받는다.
-	 */
 	readBridgeSize(callback) {
 		Console.readLine(INPUT.BRIDGE_SIZE, input => {
 			callback.bind(this)(input);
 		});
 	},
 
-	/**
-	 * 사용자가 이동할 칸을 입력받는다.
-	 */
 	readMoving(callback) {
 		Console.readLine(INPUT.MOVING, input => {
 			callback.bind(this)(input);
 		});
 	},
 
-	/**
-	 * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-	 */
 	readGameCommand(callback) {
 		Console.readLine(INPUT.GAME_COMMAND, input => {
 			callback.bind(this)(input);
--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,8 +1,5 @@
 const Bridge = require('./Bridge');
 const { checkMoveInput, checkGameCommand } = require('./util/validationInput');
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
 
 class BridgeGame {
 	#bridge;
@@ -25,11 +22,7 @@ class BridgeGame {
 	getResult() {
 		return this.#result;
 	}
-	/**
-	 * 사용자가 칸을 이동할 때 사용하는 메서드
-	 * <p>
-	 * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-	 */
+
 	move(input) {
 		checkMoveInput(input);
 		this.#result.isMovable = this.#bridge.movable(
@@ -39,11 +32,6 @@ class BridgeGame {
 		this.#currentIndex++;
 	}
 
-	/**
-	 * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-	 * <p>
-	 * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-	 */
 	retry(input) {
 		checkGameCommand(input);
 		this.#currentIndex = -1;
