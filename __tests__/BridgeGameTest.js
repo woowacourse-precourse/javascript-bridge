5986f8834d289a875d96e43aa58d9fad7c53257d
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-11-22 02:12:15 +0900

    ✅ [test]: BridgeGameTest

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -1,7 +1,8 @@
 const BridgeGame = require("../src/Model/BridgeGame");
+const { RESULT } = require("../src/Utils/Constants");
 
 describe("BridgeGame 클래스 테스트", () => {
-  test("사용자가 칸을 이동하는 경우", () => {
+  test("플레이어가 다리를 건널 수 있는 경우", () => {
     const bridgeGame = new BridgeGame(["U", "U", "D"]);
     let [canCross, upperBridge, lowerBridge] = bridgeGame.move("U");
 
@@ -16,27 +17,39 @@ describe("BridgeGame 클래스 테스트", () => {
     expect(lowerBridge).toEqual([" ", "X"]);
   });
 
-  test("사용자가 게임을 재시작하는 경우", () => {
+  test("플레이어가 다리를 건널 수 없는 경우", () => {
+    const bridgeGame = new BridgeGame(["U", "U", "D"]);
+    let [canCross, upperBridge, lowerBridge] = bridgeGame.move("D");
+
+    expect(canCross).toBe(false);
+    expect(upperBridge).toEqual([" "]);
+    expect(lowerBridge).toEqual(["X"]);
+  });
+
+  test("플레이어가 게임을 재시작하는 경우", () => {
     const bridgeGame = new BridgeGame(["U", "U", "D"]);
     bridgeGame.retry();
 
-    expect(bridgeGame.attempsCount).toEqual(2);
+    const [playerUpperBridgeState, playerLowerBridgeState, result] =
+      bridgeGame.getResult();
+
+    expect(result.get(RESULT.TOTAL_ATTEMPTS_COUNT)).toEqual(2);
   });
 
-  test("사용자의 위치가 다리의 마지막 위치인 경우", () => {
+  test("플레이어의 위치가 다리의 마지막 위치인 경우", () => {
     const bridgeGame = new BridgeGame(["U", "U", "D", "D"]);
-    const playerBridgeLength = 4;
+    const playerBridgeStateLength = 4;
 
-    const isLast = bridgeGame.isLastPosition(playerBridgeLength);
+    const isLast = bridgeGame.isLastPosition(playerBridgeStateLength);
 
     expect(isLast).toBe(true);
   });
 
-  test("사용자의 위치가 다리의 마지막 위치가 아닌 경우", () => {
+  test("플레이어의 위치가 다리의 마지막 위치가 아닌 경우", () => {
     const bridgeGame = new BridgeGame(["U", "U", "D", "D"]);
-    const playerBridgeLength = 2;
+    const playerBridgeStateLength = 2;
 
-    const isLast = bridgeGame.isLastPosition(playerBridgeLength);
+    const isLast = bridgeGame.isLastPosition(playerBridgeStateLength);
 
     expect(isLast).toBe(false);
   });
