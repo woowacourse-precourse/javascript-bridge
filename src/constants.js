099f4657d961ba4b0e578759b6e76bec43a0fade
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-11-22 07:50:27 +0900

    feat(constant): add new constants
    Add game ending message
    Add success and failure

--- src/constants.js
+++ src/constants.js
@@ -4,6 +4,7 @@ module.exports = {
     INPUT_SIZE: "다리의 길이를 입력해주세요.\n",
     INPUT_DIRECTION: "이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
     INPUT_RETRY: "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n",
+    END_GAME: "최종 게임 결과",
   }),
 
   ERROR: Object.freeze({
@@ -20,4 +21,7 @@ module.exports = {
   Q: "Q",
 
   BLANK_SPACE: " ",
+
+  SUCCESS: "성공",
+  FAILURE: "실패",
 };
