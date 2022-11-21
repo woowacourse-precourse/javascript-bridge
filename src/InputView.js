b358e551a5a2df483c591a8a30e223f5e77e39a2
aram <snsdl0905@naver.com>
2022-11-22 00:19:08 +0900

    feat: 다리의 길이를 입력 받아 랜덤한 다리를 만든다.

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,8 @@
+const { Console } = require("@woowacourse/mission-utils");
+const BridgeMaker = require("./BridgeMaker");
+const generateRandomNumber = require("./BridgeRandomNumberGenerator");
+const BridgeGame = require("./BridgeGame");
+let game = new BridgeGame();
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,7 +10,11 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine(`다리의 길이를 입력해주세요.\n`,(getSize)=>{
+      game.answer = BridgeMaker.makeBridge(getSize, generateRandomNumber);
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
