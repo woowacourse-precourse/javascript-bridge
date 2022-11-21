8a3de2d6ae734329b270be917829a1c4814e151f
dongyeopca <ckehdduq95@gmail.com>
2022-11-21 12:49:36 +0900

    feat(BridgeGame): 게임 로직 - 이동시 다리의 값과 같으면 이동, 다르면 재개 여부 물음, 모두 같을 경우 게임 종료

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,13 +1,57 @@
+const BridgeMaker = require('./BridgeMaker');
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  #bridge;
+  #tryCount;
+  #history;
+  constructor() {
+    this.#bridge = null;
+    this.#tryCount = 1;
+    this.#history = [];
+  }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+
+  move(player) {
+    if (this.checkMove(player)) return this.checkEnd();
+    return false;
+  }
+
+  checkEnd() {
+    if (this.#bridge.length == this.#history.length) return 'END';
+    return true;
+  }
+
+  checkMove(player) {
+    if (player == 'U' && this.#bridge[this.#history.length] == 'U') {
+      this.#history.push([player, true]);
+      return true;
+    }
+    if (player == 'D' && this.#bridge[this.#history.length] == 'D') {
+      this.#history.push([player, true]);
+      return true;
+    }
+    this.#history.push([player, false]);
+    return false;
+  }
+
+  setBridge(size) {
+    this.#bridge = BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate);
+  }
+
+  getTry() {
+    return this.#tryCount;
+  }
+
+  getHistory() {
+    return this.#history;
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
