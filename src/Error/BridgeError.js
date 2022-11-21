dc199521193114ec04c8e2477e88bcfa5d44d457
wonkyum-kim <dodo12120@naver.com>
2022-11-21 22:57:42 +0900

    refactor(Error): move some validate methods
    - Some validate methods from BridgeGame are moved to BridgeError.

--- src/Error/BridgeError.js
+++ src/Error/BridgeError.js
@@ -1,4 +1,4 @@
-const GameProgress = require('../IO/GameProgress');
+const GameProgress = require('../GameProgress');
 
 const BridgeError = {
   throwErrorHandler(errorMessage, possibeError) {
@@ -6,6 +6,34 @@ const BridgeError = {
       GameProgress.printErrorMessage(errorMessage);
       throw new Error();
     }
+  },
+
+  /**
+ * @param {string} input 1 or 2 자리 숫자로 이루어진 문자열
+ * @returns {boolean} 1 or 2 자리 숫자이면 true 아니면 false를 반환
+ */
+  isOneorTwoDigitNumber(input) {
+    const VALID_NUM_REGEX = /^\d{1,2}$/;
+    return VALID_NUM_REGEX.test(input);
+  },
+
+  isValidBridgeSize(size) {
+    if (!this.isOneorTwoDigitNumber(size)) {
+      return false;
+    }
+    const BRIDGE_LOWER_BOUND = 3;
+    const BRIDGE_UPPER_BOUND = 20;
+    return +(size) >= BRIDGE_LOWER_BOUND && +(size) <= BRIDGE_UPPER_BOUND;
+  },
+
+  isValidMoving(input) {
+    const VALID_MOVING_REGEX = /^[U|D]{1}$/;
+    return VALID_MOVING_REGEX.test(input);
+  },
+
+  isValidRetryInput(input) {
+    const VALID_RETRY_INPUT = /^[R|Q]{1}$/;
+    return VALID_RETRY_INPUT.test(input);
   },
 };
 
