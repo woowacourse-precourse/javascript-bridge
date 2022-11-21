21c9779ac516d4432458fc3804edcc1d2b111d09
seolyeongbae <peixueying@gmail.com>
2022-11-22 01:23:39 +0900

    feat: implement prnt function, announce start

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+const { MissionUtils } = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  print(message) {
+    MissionUtils.Console.print(message);
+    MissionUtils.Console.close();
+  }
+
+  play() {
+    this.print(`다리 건너기 게임을 시작합니다.`);
+  }
 }
 
 module.exports = App;
