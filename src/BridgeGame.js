8708a34c1eba60e8d3feecaff69a47773473872d
imhynni <omelas329@gmail.com>
2022-11-22 04:08:17 +0900

    feat: 재시도를 선택하면 게임 다시 시작

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -2,33 +2,39 @@ const { GAME_STATE } = require("./Constants");
 
 class BridgeGame {
   #current;
-  #state;
+  #gameState;
+  #bridgeState;
+  #attempts;
 
   constructor() {
     this.#current = 0;
-    this.#state = [];
+    this.#bridgeState = [];
+    this.#attempts = 1;
   }
 
   move(bridge, direction) {
     if (bridge[this.#current] === direction) {
-      this.#state.push([direction, true]);
+      this.#gameState = GAME_STATE.PASS;
       this.#current++;
-      if (this.#current === bridge.length) return GAME_STATE.SUCCESS;
-      return GAME_STATE.PASS;
-    }
-    this.#state.push([direction, false]);
-    return GAME_STATE.FAIL;
+      if (this.#current === bridge.length) this.#gameState = GAME_STATE.SUCCESS;
+    } else this.#gameState = GAME_STATE.FAIL;
+    this.#bridgeState.push([direction, this.#gameState]);
+
+    return this.#gameState;
   }
 
-  /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  retry() {}
+  retry() {
+    this.#current = 0;
+    this.#bridgeState = [];
+    this.#attempts++;
+  }
 
   getCurrentState() {
-    return this.#state;
+    return {
+      bridgeState: this.#bridgeState,
+      gameState: this.#gameState,
+      attempts: this.#attempts,
+    };
   }
 }
 
--- src/BridgeGameManager.js
+++ src/BridgeGameManager.js
@@ -8,7 +8,6 @@ const { GAME_STATE } = require("./Constants");
 const bridgeGame = new BridgeGame();
 
 class BridgeGameManager {
-  #bridgeSize;
   #bridge;
 
   game() {
@@ -18,7 +17,6 @@ class BridgeGameManager {
 
   manageBridge(size) {
     Validation.checkBridgeSize(size);
-    this.#bridgeSize = size;
     this.#bridge = makeBridge(size, generate);
     console.log(this.#bridge);
     return InputView.readMoving(this.manageMoving.bind(this));
@@ -26,19 +24,26 @@ class BridgeGameManager {
 
   manageMoving(direction) {
     Validation.checkDirection(direction);
+
     const gameState = bridgeGame.move(this.#bridge, direction);
-    if (gameState === GAME_STATE.SUCCESS) return OutputView.printResult();
-    OutputView.printMap(bridgeGame.getCurrentState());
+    OutputView.printMap(bridgeGame.getCurrentState().bridgeState);
     if (gameState === GAME_STATE.PASS)
       return InputView.readMoving(this.manageMoving.bind(this));
     if (gameState === GAME_STATE.FAIL)
       return InputView.readGameCommand(this.manageRetry.bind(this));
+    if (gameState === GAME_STATE.SUCCESS)
+      return OutputView.printResult(bridgeGame.getCurrentState());
   }
 
   manageRetry(command) {
     Validation.checkGameCommand(command);
-    if (command === "R") bridgeGame.retry();
+    if (command === "R") {
+      bridgeGame.retry();
+      InputView.readMoving(this.manageMoving.bind(this));
+    }
   }
+
+  makeBridgeState() {}
 }
 
 module.exports = BridgeGameManager;
