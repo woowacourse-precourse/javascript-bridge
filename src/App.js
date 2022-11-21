41bb61af079ac9a4e915aa3f9d8f7b094ab22f47
김혜원 <dev.blair06@gmail.com>
2022-11-21 16:19:06 +0900

    feat: 랜덤난수를 이용해 다리 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+const BridgeMaker = require("./BridgeMaker");
+const BridgeRandom = require("./BridgeRandomNumberGenerator");
 class App {
   play() {}
 }
 
+const app = new App();
+app.play();
 module.exports = App;
--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -7,7 +7,17 @@ const BridgeMaker = {
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
-  makeBridge(size, generateRandomNumber) {},
+  makeBridge(size, generateRandomNumber) {
+    const tempBridge = new Array(size).fill(0);
+
+    const bridge = tempBridge.map((elem) => {
+      const tempNumber = generateRandomNumber.generate();
+      console.log(`tempNum: ${tempNumber}`);
+      if (tempNumber) return "U";
+      if (!tempNumber) return "D";
+    });
+    return bridge;
+  },
 };
 
 module.exports = BridgeMaker;
