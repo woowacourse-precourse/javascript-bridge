92572f03165d3245c50f61ecff4faca9f61bbe6a
leeharyung <lhr4884@naver.com>
2022-11-22 01:16:10 +0900

    chore: 필요 없는 파일 삭제

--- src/util/generateRandomNumber.js
+++
@@ -1,7 +0,0 @@
-const { Random } = require('@woowacourse/mission-utils');
-
-const generateRandomNumber = () => {
-	return Random.pickNumberInRange(0, 1);
-};
-
-module.exports = generateRandomNumber;
