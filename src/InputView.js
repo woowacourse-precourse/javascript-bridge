c62ba6b648f91d09e94835f508539f2148952aba
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-21 18:40:37 +0900

    feat(InputView): readBridgeSize 메서드 구현

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,5 @@
+const {Console} = require('@woowacourse/mission-utils');
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,7 +7,41 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize(callback) {
+    Console.readLine('다리의 길이를 입력해주세요.', (input) => {
+      try {
+        const bridgeSize = this.parseBridgeSize(input);
+        callback(bridgeSize);
+      } catch (error) {
+        Console.print(error.message);
+
+        this.readBridgeSize(callback);
+      }
+    });
+  },
+
+  parseBridgeSize(input) {
+    this.validateSpace(input);
+
+    const bridgeSize = Number(input);
+    this.validateBridgeSizeRange(bridgeSize);
+
+    return bridgeSize;
+  },
+
+  validateSpace(input) {
+    const MESSAGE = '[ERROR] 공백을 포함시킬 수 없습니다.';
+
+    if (input.includes(' ')) throw new Error(MESSAGE);
+  },
+
+  validateBridgeSizeRange(bridgeSize) {
+    const MINIMUM_VALUE = 3;
+    const MAXIMUM_VALUE = 20;
+    const MESSAGE = `[ERROR] ${MINIMUM_VALUE} 이상 ${MAXIMUM_VALUE} 이하의 숫자만 입력할 수 있습니다.`;
+
+    if (!(MINIMUM_VALUE <= bridgeSize && bridgeSize <= MAXIMUM_VALUE)) throw new Error(MESSAGE);
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
