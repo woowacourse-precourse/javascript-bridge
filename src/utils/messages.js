0bf4f538d6021b3a70ea35f677f8275c6fb532c4
nalsae <nalsae0207@naver.com>
2022-11-22 00:44:48 +0900

    feat: Add messages about information
    결과 출력 관련 게임 안내 메시지 추가

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -2,6 +2,9 @@ const INFO_MESSAGES = Object.freeze({
   BRIDGE_SIZE: '다리의 길이를 입력해주세요.\n',
   MOVING: '\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
   RETRY: '\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
+  RESULT_TITLE: '\n최종 게임 결과\n',
+  SUCCESS_TITLE: '\n게임 성공 여부: ',
+  TRY_TITLE: '\n총 시도한 횟수: ',
 });
 
 const ERROR_MESSAGES = Object.freeze({
