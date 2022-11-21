9936bfaaee3069e624655bfaa779ed2ba3ab2cdf
wang-yurin <king_1994@naver.com>
2022-11-21 17:11:31 +0900

    feat(BridgeMaker): 다리 건너는 칸 생성 메서드 추가

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -7,7 +7,15 @@ const BridgeMaker = {
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
-  makeBridge(size, generateRandomNumber) {},
+  makeBridge(size, generateRandomNumber) {
+    const bridgeShape = [];
+    for (let count = 0; count < size; count++) {
+      const number = generateRandomNumber();
+      if (number === 1) bridgeShape.push("U");
+      if (number === 0) bridgeShape.push("D");
+    }
+    return bridgeShape;
+  },
 };
 
 module.exports = BridgeMaker;
