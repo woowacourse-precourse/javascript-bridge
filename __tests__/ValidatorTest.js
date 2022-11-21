eb53bf5d62ca93ccd2fdfe43869995fdf6a40b79
bassyu <yrkdgus@gmail.com>
2022-11-22 00:39:31 +0900

    refactor(ValidatorTest): 예외 테스트 함수 추출

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -1,42 +1,38 @@
 const Validator = require('../src/Validator');
 
+const getExceptionTest = (inputList, callback) => {
+  return () => {
+    inputList.forEach((input) => {
+      expect(() => {
+        callback(input);
+      }).toThrow();
+    });
+  };
+};
+
 describe('Validator 테스트', () => {
-  test('정수가 아니면 예외가 발생한다.', () => {
-    const input = '3.0';
-    expect(() => {
-      Validator.checkBridgeSize(input);
-    }).toThrow();
-  });
+  test(
+    '문자열 중 숫자가 아니면 예외가 발생한다.',
+    getExceptionTest(['3.0', '+3'], Validator.checkBridgeSize)
+  );
 
-  test('다리 길이가 3 미만이면 예외가 발생한다.', () => {
-    const input = '2';
-    expect(() => {
-      Validator.checkBridgeSize(input);
-    }).toThrow();
-  });
+  test(
+    '다리 길이가 3 미만이면 예외가 발생한다.',
+    getExceptionTest(['2', '-3'], Validator.checkBridgeSize)
+  );
 
-  test('다리 길이가 20 초과면 예외가 발생한다.', () => {
-    const input = '21';
-    expect(() => {
-      Validator.checkBridgeSize(input);
-    }).toThrow();
-  });
+  test(
+    'U 와 D 중 한 글자가 아니면 예외가 발생한다.',
+    getExceptionTest(['UU', 'DD', 'U ', 'S'], Validator.checkBridgeSize)
+  );
 
-  test('U 와 D 중 한 글자가 아니면 예외가 발생한다.', () => {
-    const inputList = ['UU', 'DD', 'U ', 'S'];
-    inputList.forEach((input) => {
-      expect(() => {
-        Validator.checkBridgeSize(input);
-      }).toThrow();
-    });
-  });
+  test(
+    'R 과 Q 중 한 글자가 아니면 예외가 발생한다.',
+    getExceptionTest(['RR', 'QQ', 'R ', 'S'], Validator.checkBridgeSize)
+  );
 
-  test('R 과 Q 중 한 글자가 아니면 예외가 발생한다.', () => {
-    const inputList = ['RR', 'QQ', 'R ', 'S'];
-    inputList.forEach((input) => {
-      expect(() => {
-        Validator.checkBridgeSize(input);
-      }).toThrow();
-    });
-  });
+  test(
+    '다리 길이가 20 초과면 예외가 발생한다.',
+    getExceptionTest(['21'], Validator.checkBridgeSize)
+  );
 });
