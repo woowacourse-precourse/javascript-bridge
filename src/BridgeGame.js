920e0f60d201406969d21ceef8a2e796f14ae97d
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2022-11-21 17:47:08 +0900

    feat: add field to count game round

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -4,15 +4,21 @@ const BridgeBoard = require("./BridgeBoard");
  */
 class BridgeGame {
   #bridgeBoard;
+  #round;
   constructor() {
     this.#bridgeBoard = new BridgeBoard();
+    this.#round = 0;
   }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(direction) {
+    if (this.#bridgeBoard.moveTo(direction, this.#round)) {
+      this.#round++;
+    }
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
