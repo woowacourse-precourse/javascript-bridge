a2be9dfadacfeaf53b60873317d807bad1c46089
nangkyeonglim <nangkyeonglim@gmail.com>
2022-11-21 20:12:06 +0900

    refactor(constants): 상수파일 상수화

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -24,19 +24,21 @@ const RESULT_MAP = {
   separator: ' | ',
 };
 
-const ERROR = {
-  notNumber: '[ERROR] 다리 길이는 숫자만 입력 가능합니다.\n',
-  notRangeOfBridgeLength: '[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.\n',
-  notValidDirectionInput: '[ERROR] 이동할 칸은 위: U, 아래: D 만 입력 가능합니다.\n',
-  notValidRetryInput: '[ERROR] 재시도: R, 종료: Q 만 입력 가능합니다.\n',
-};
-
 const MESSAGE = {
   readBridgeSize: '다리의 길이를 입력해주세요.\n',
-  readMoving: '이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
-  readGameCommand: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
+  readMoving: `이동할 칸을 선택해주세요. (위: ${HOTKEY.up}, 아래: ${HOTKEY.down})\n`,
+  readGameCommand: `게임을 다시 시도할지 여부를 입력해주세요. (재시도: ${HOTKEY.retry}, 종료: ${HOTKEY.quit})\n`,
   printInitial: '다리 건너기 게임을 시작합니다.\n',
   printResultTitle: '최종 게임 결과',
   printResult: '게임 성공 여부: ',
   printRetry: '총 시도한 횟수: ',
 };
+
+const ERROR = {
+  notNumber: '[ERROR] 다리 길이는 숫자만 입력 가능합니다.\n',
+  notRangeOfBridgeLength: `[ERROR] 다리 길이는 ${BRIDGE.min}부터 ${BRIDGE.max} 사이의 숫자여야 합니다.\n`,
+  notValidDirectionInput: `[ERROR] 이동할 칸은 위: ${HOTKEY.up}, 아래: ${HOTKEY.down} 만 입력 가능합니다.\n`,
+  notValidRetryInput: `[ERROR] 재시도: ${HOTKEY.retry}, 종료: ${HOTKEY.quit} 만 입력 가능합니다.\n`,
+};
+
+module.exports = { BRIDGE, RESULT, HOTKEY, RESULT_MAP, MESSAGE, ERROR };
