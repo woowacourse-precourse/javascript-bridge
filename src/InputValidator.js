cf6af7b3ef2e2037f0b5b654510dc8a605a0bc8b
hyeongdong <khd941018@gmail.com>
2022-11-22 05:01:16 +0900

    feat(InputValidator) : 재시작여부 입력값 유효성 확인하는 기능 구현

--- src/InputValidator.js
+++ src/InputValidator.js
@@ -8,7 +8,12 @@ const InputValidator = {
     throw new Error("[ERROR] 이동할 칸은 U나 D만 입력하셔야 합니다.");
   },
 
-  checkGameCommand(command) {},
+  checkGameCommand(command) {
+    if (command === "R" || command === "Q") {
+      return;
+    }
+    throw new Error("[ERROR] 게임을 재시작하려면 R, 종료하시려면 Q를 입력하셔야 합니다.");
+  },
 };
 
 module.exports = InputValidator;
