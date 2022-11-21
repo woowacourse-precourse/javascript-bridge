1681ee4cd95eb94e93f5b4ace3446afe3f874a92
ReturnReason <reasonz@daum.net>
2022-11-21 23:01:13 +0900

    test(User): 현재 유저의 위치를 한 칸 이동시키는 메서드 테스트

--- __tests__/UserTest.js
+++ __tests__/UserTest.js
@@ -1,14 +1,14 @@
 const User = require('../src/model/User');
 
 describe('🎮 User 클래스 테스트', () => {
-  test('⭐ 현재 유저의 위치와 맵 사이즈가 같은지 비교 : 유저 위치 : 0, 게임 맵 사이즈 : 5일 때 false를 반환합니다.', () => {
+  test('⭐ 현재 유저의 위치와 맵 사이즈가 같은지 비교 ▶ 유저 위치 : 0, 게임 맵 사이즈 : 5일 때 false를 반환합니다.', () => {
     const user = new User();
     const gameMapLength = 5;
 
     expect(user.isArrival(gameMapLength)).toBeFalsy();
   });
 
-  test('⭐ 현재 유저의 위치와 맵 사이즈가 같은지 비교 : 유저 위치 : 5, 게임 맵 사이즈 : 5일 때 true를 반환합니다.', () => {
+  test('⭐ 현재 유저의 위치와 맵 사이즈가 같은지 비교 ▶ 유저 위치 : 5, 게임 맵 사이즈 : 5일 때 true를 반환합니다.', () => {
     const user = new User();
     const increaseCount = 5;
     for (let i = 0; i < increaseCount; i++) {
@@ -17,5 +17,12 @@ describe('🎮 User 클래스 테스트', () => {
     const gameMapLength = 5;
 
     expect(user.isArrival(gameMapLength)).toBeTruthy();
+  });
+
+  test('⭐ 현재 유저의 위치를 한 칸 이동시킬 수 있습니다. ▶ 초기 유저 위치 : 0, 이동 후 : 1', () => {
+    const user = new User();
+    user.increaseLocation();
+
+    expect(user.getLocation()).toBe(1);
   });
 });
