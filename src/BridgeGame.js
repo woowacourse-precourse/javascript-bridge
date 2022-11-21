b9f2eb0480d1843b1ab82ac533bf64aea2441d89
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-11-22 00:35:19 +0900

    feat(class): add BridgeGame constructor and field
    Add to BridgeGame
    - Fileds: bridge, results array, run count
    - constructor

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -2,6 +2,16 @@
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  #bridge;
+  #results;
+  #runCount;
+
+  constructor(bridge) {
+    this.#bridge = bridge;
+    this.#results = [];
+    this.#runCount = 1;
+  }
+
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
