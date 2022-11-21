0d648d54dcb516526c4865e807d5e225d36cd270
hjy0951 <hjy0951@gmail.com>
2022-11-22 03:16:49 +0900

    feat: 게임 실행 횟수 수정

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -15,7 +15,7 @@ class BridgeGame {
       BridgeRandomNumberGenerator.generate
     );
     this.#board = [[], []];
-    this.#count = 0;
+    this.#count = 1;
   }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
