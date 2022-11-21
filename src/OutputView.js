cabbac8ec7f69ffa9f9f2c6b99049ab8dfe15c53
cofla <cofla159@naver.com>
2022-11-21 21:50:28 +0900

    feat: 에러메세지 출력 함수

--- src/OutputView.js
+++ src/OutputView.js
@@ -19,11 +19,16 @@ const OutputView = {
 
   /**
    * 게임 시작을 알리는 문구를 출력한다.
-   * <p>
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printStart() {
-    Console.print("다리 건너기 게임을 시작합니다.\n");
+    Console.print("다리 건너기 게임을 시작합니다.");
+  },
+
+  /**
+   * 에러 메세지를 출력한다.
+   */
+  printError(errorMessage) {
+    Console.print(errorMessage);
   },
 };
 
