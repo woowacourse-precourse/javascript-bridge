f3c655ade994931a54c8b530a24e9926cb210dad
vinivin153 <vinivin153@naver.com>
2022-11-22 03:25:31 +0900

    feat: 게임 결과 출력 기능 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -2,17 +2,26 @@ const { Console } = require('@woowacourse/mission-utils');
 const { MESSAGE_START } = require('./utils/constant');
 
 const OutputView = {
-  printMap(path) {
-    Console.print('[' + path.upside.join('|') + ']');
-    Console.print('[' + path.downside.join('|') + ']\n');
-  },
-
   printStartGame() {
     Console.print(MESSAGE_START);
   },
 
   printError(errorMessage) {
     Console.print(errorMessage);
+  },
+
+  printMap(path) {
+    Console.print('[' + path.upside.join('|') + ']');
+    Console.print('[' + path.downside.join('|') + ']\n');
+  },
+
+  printResult(path, gameResult, attempts) {
+    Console.print('최종 게임 결과');
+    OutputView.printMap(path);
+
+    Console.print('게임 성공 여부: ' + gameResult);
+    Console.print('총 시도한 횟수: ' + attempts);
+    Console.close();
   },
 };
 
