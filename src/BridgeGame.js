9d05ce4f656e45366a91dfa6f7686c872ff809c8
sojinjang <jsojin6119@gmail.com>
2022-11-22 03:34:58 +0900

    :sparkles: feat(model): 재시도 실행시 건너온 다리 모양 초기화 후 총 시도 횟수 올리기

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,11 +1,6 @@
 const { generate } = require("./BridgeRandomNumberGenerator");
 const { makeBridge } = require("./BridgeMaker");
 const Values = require("./constants/Values.js");
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
-
-// InputView, Outputview 사용하면 안됨
 
 class BridgeGame {
   #bridgeArr;
@@ -13,6 +8,7 @@ class BridgeGame {
   constructor(length) {
     this.#bridgeArr = makeBridge(length, generate);
     this.stepObj = { upperPart: [], lowerPart: [] };
+    this.numOfTrials = 0;
   }
 
   move(inputDirection) {
@@ -20,22 +16,27 @@ class BridgeGame {
     this.updateProgressOfGame(inputDirection);
     if (this.isSuccess) this.judgeGameCleard();
   }
-  /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
+
   retry() {
-    // 처음에 만든 정답 다리 리스트, 총 시도 횟수 기억한채로 iii.으로 다시 돌아감
+    this.numOfTrials += 1;
+    this.stepObj = { upperPart: [], lowerPart: [] };
   }
+
   judgeSuccess(index, inputDirection) {
     this.isSuccess = this.#bridgeArr[index] === inputDirection;
   }
+
+  getCurrentMark() {
+    if (this.isSuccess) return Values.SUCCESS_MARK;
+    return Values.FAIL_MARK;
+  }
+
   updateProgressOfGame(inputDirection) {
     const MARK = this.getCurrentMark();
     if (inputDirection === Values.UPWARD_MOVEMENT) return this.moveToUpper(MARK);
     return this.moveToLower(MARK);
   }
+
   moveToUpper(MARK) {
     this.stepObj.upperPart.push(MARK);
     this.stepObj.lowerPart.push(Values.BLANK);
@@ -44,11 +45,6 @@ class BridgeGame {
   moveToLower(MARK) {
     this.stepObj.lowerPart.push(MARK);
     this.stepObj.upperPart.push(Values.BLANK);
-  }
-
-  getCurrentMark() {
-    if (this.isSuccess) return Values.SUCCESS_MARK;
-    return Values.FAIL_MARK;
   }
 
   getNumOfSteps() {
