2e8c8ba89245ee51b4d06aaa432ebd04b8b90216
yaena1223 <yaena1223@dgu.ac.kr>
2022-11-21 11:03:00 +0900

    refactor: add \n in result messages

--- src/Constant.js
+++ src/Constant.js
@@ -1,6 +1,6 @@
 const INPUT_MESSAGE = {
   BRIDGE_SIZE: '다리의 길이를 입력해주세요.\n',
-  READ_MOVE: '이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
+  READ_MOVE: '\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
   READ_GAME_COMMAND:
     '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
 };
@@ -8,7 +8,7 @@ const INPUT_MESSAGE = {
 const START_MESSAGE = '다리 건너기 게임을 시작합니다.\n';
 
 const OUTPUT_MESSAGE = {
-  RESULT: '최종 게임 결과\n',
+  RESULT: '\n최종 게임 결과\n',
   SUCCESS: '게임 성공 여부: 성공',
   FAIL: '게임 성공 여부: 실패',
   COUNT_TRY: '총 시도한 횟수: ',
