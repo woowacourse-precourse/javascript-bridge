5e469e02b19cf2a99f066682f92a7b1289ef1718
JangHoonSeok <orirori512@naver.com>
2022-11-21 12:44:19 +0900

    ✨ feat(Validator): Move입력에 대한 에러 체크 함수 구현

--- src/Validator.js
+++ src/Validator.js
@@ -1,10 +1,16 @@
 const { ERROR_MESSAGE } = require("./constants/Messages.js");
 const Validator = {
-  BRIDGE_LENGTH_REGEX: /^[3-9]{1}$|^1{1}[0-9]{1}$|20/,
+  BRIDGE_SIZE_REGEX: /^[3-9]{1}$|^1{1}[0-9]{1}$|20/,
+  MOVE_REGEX: /^[UD]$/,
   // 3이상 20이하의 숫자가 아니면 Error
   checkBridgeLengthInput(input) {
-    if (!this.BRIDGE_LENGTH_REGEX.test(input))
-      throw new Error(ERROR_MESSAGE.BRIDGE_LENGTH_ERROR);
+    if (!this.BRIDGE_SIZE_REGEX.test(input))
+      throw new Error(ERROR_MESSAGE.BRIDGE_SIZE_ERROR);
+  },
+  // 'U', 'D'가 아니면 Error
+  checkValidMove(input) {
+    if (!this.MOVE_REGEX.test(input))
+      throw new Error(ERROR_MESSAGE.MOVE_INPUT_ERROR);
   },
 };
 
