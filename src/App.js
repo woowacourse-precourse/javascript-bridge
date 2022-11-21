84f32815da3c875a2a73fdda2e5f2ccb633df8b8
sohyun <park.sohyun0430@gmail.com>
2022-11-21 16:10:10 +0900

    feat:requestMoving, moveBlock 함수 구현
    requestMoving : 사용자가 이동할 방향 입력 받기
    moveBlock : 입력받은 방향으로 bridgeGame.move 실행
    입력이 유효하지 않은 경우, throw error를 통해 에러 출력 후 다시 입력받기

--- src/App.js
+++ src/App.js
@@ -1,27 +1,49 @@
 const { Console } = require('@woowacourse/mission-utils');
 const OutputView = require('./OutputView');
 const InputView = require('./InputView');
-const {
-  START_GAME_MSG,
-  BRIDGE_SIZE_ERROR,
-  MIN_BRIDGE_SIZE,
-  MAX_BRIDGE_SIZE,
-} = require('./constants');
+const { START_GAME_MSG } = require('./constants');
+const BridgeGame = require('./BridgeGame');
 
 class App {
-  play() {
-    OutputView.printGuide(START_GAME_MSG);
+  constructor() {
+    this.bridgeGame = new BridgeGame();
   }
 
-  isValidBridgeSize(bridgeSize) {
-    if (
-      isNaN(bridgeSize) ||
-      bridgeSize < MIN_BRIDGE_SIZE ||
-      bridgeSize > MAX_BRIDGE_SIZE ||
-      bridgeSize === ''
-    ) {
-      throw new Error(BRIDGE_SIZE_ERROR);
+  play() {
+    OutputView.printGuide(START_GAME_MSG); // 게임시작 문구 출력
+    // 다리 길이 받아서 다리 만들기
+    this.requestBridgeSize();
+  }
+
+  requestBridgeSize() {
+    InputView.readBridgeSize(this.makeBridge.bind(this));
+  }
+
+  makeBridge(size) {
+    try {
+      this.bridgeGame.buildBridge(size);
+      this.requestMoving();
+    } catch (err) {
+      OutputView.printGuide(err.message);
+      this.tryAgain(this.requestBridgeSize);
     }
+  }
+
+  requestMoving() {
+    InputView.readMoving(this.moveBlock.bind(this));
+  }
+
+  moveBlock(moveInput) {
+    try {
+      this.bridgeGame.move(moveInput);
+    } catch (err) {
+      OutputView.printGuide(err.message);
+      this.tryAgain(this.requestMoving);
+    }
+  }
+
+  tryAgain(tryAgainFunc) {
+    tryAgainFunc.call(this);
   }
 }
 
