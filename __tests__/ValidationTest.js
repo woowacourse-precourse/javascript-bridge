e266206a38aa5592d0ef6a05fef08715eb6d45aa
dragonfruitlemonade <tpghks9245@gmail.com>
2022-11-21 20:49:17 +0900

    feat(ValidationTest): bridgeLength 입력값이 3~20 사이인지 검사, 경계값 검사

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -13,23 +13,23 @@ describe("Validation 클래스 테스트", () => {
     expect(() => Validation.bridgeLength(input)).toThrow('[ERROR] 숫자를 입력해주세요.');
   });
 
-  test("다리 길이 입력 값이 숫자가 아닐경우", () => {
+  test("다리 길이 입력 값이 비었을 경우", () => {
     const input = "number";
 
     expect(() => Validation.bridgeLength(input)).toThrow('[ERROR] 숫자를 입력해주세요.');
   });
 
-//   test("다리 길이 입력 값이 비었을 경우", () => {
-//     const input = "";
+  test("다리 길이 입력 값이 3~20 사이가 아닐 경우", () => {
+    const input = "2";
 
-//     expect(() => Validation.bridgeLength(input)).toThrow('[ERROR] 값을 입력해주세요.');
-//   });
+    expect(() => Validation.bridgeLength(input)).toThrow('[ERROR] 3과 20사이의 값을 입력해주세요.');
+  });
 
-//   test("다리 길이 입력 값이 비었을 경우", () => {
-//     const input = "";
+  test("다리 길이 입력 값이 3~20 사이가 아닐 경우", () => {
+    const input = "21";
 
-//     expect(() => Validation.bridgeLength(input)).toThrow('[ERROR] 값을 입력해주세요.');
-//   });
+    expect(() => Validation.bridgeLength(input)).toThrow('[ERROR] 3과 20사이의 값을 입력해주세요.');
+  });
 
 //   test("다리 길이 입력 값이 비었을 경우", () => {
 //     const input = "";
