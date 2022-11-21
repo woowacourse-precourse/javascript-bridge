486825f3e7bbcaa1c5ec6c63cf9f99b1d4e0179d
KINHYEONJI <hyeonji0718@gmail.com>
2022-11-21 21:10:26 +0900

    feat(App) : 게임 시작을 출력하는 함수 호출

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const OutputView = require("./OutputView");
+
 class App {
-  play() {}
+  constructor() {
+    this.outputView = OutputView;
+  }
+
+  play() {
+    this.outputView.print("다리 건너기 게임을 시작합니다.");
+  }
 }
 
 module.exports = App;
