b01bb71ec395b0bf25c64533ad6e67aad59d942c
dhkham <khamdong@gmail.com>
2022-11-21 20:18:34 +0900

    refactor(MoveCheckTest): use test.each to refactor the code in a simpler form

--- __tests__/MoveCheckTest.js
+++ __tests__/MoveCheckTest.js
@@ -1,23 +1,11 @@
 const MoveCheck = require("../src/Check/MoveCheck");
 
 describe("다리 이동 입력 테스트", () => {
-  test("다리 이동 U 입력 시 통과", () => {
-    expect(() => new MoveCheck("U")).not.toThrow("[ERROR]");
+  test.each(["U", "D"])("다리 이동 정상 입력 (U 혹은 D)", input => {
+    expect(() => new MoveCheck(input)).not.toThrow("[ERROR]");
   });
 
-  test("다리 이동 D 입력 시 통과", () => {
-    expect(() => new MoveCheck("D")).not.toThrow("[ERROR]");
-  });
-
-  test("다리 이동 숫자 입력 시 에러", () => {
-    expect(() => new MoveCheck("2.7")).toThrow("[ERROR]");
-  });
-
-  test("다리 이동 다른 문자 입력 시 에러", () => {
-    expect(() => new MoveCheck("a")).toThrow("[ERROR]");
-  });
-
-  test("다리 이동 다른 문자 입력 시 에러", () => {
-    expect(() => new MoveCheck("가")).toThrow("[ERROR]");
+  test.each(["2.7", "a", "가"])("다리 이동 정상 입력 (U 혹은 D)", input => {
+    expect(() => new MoveCheck(input)).toThrow("[ERROR]");
   });
 });
