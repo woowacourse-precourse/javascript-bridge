0c3f91462676974fed7a4ba73c6a6bb2e8f5f456
Jin1won <wlsdnjs16521@gmail.com>
2022-11-21 14:54:19 +0900

    feat: 최종 게임 결과를 출력한다.

--- src/InputView.js
+++ src/InputView.js
@@ -33,7 +33,7 @@ const InputView = {
 
       OutputView.printMap(up, down);
 
-      if (!proceedMove) this.readGameCommand(bridgeGame);
+      if (!proceedMove) this.readGameCommand(bridgeGame, up, down);
 
       this.readMoving(bridgeGame);
     });
@@ -42,14 +42,17 @@ const InputView = {
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand(bridgeGame) {
+  readGameCommand(bridgeGame, up, down) {
     inputUserValue(GAME_MESSAGE.RESTART, (restart) => {
       isRestartValid(restart);
       if (restart === RESTART_VALUE.RESTART) {
         bridgeGame.retry();
         this.readMoving(bridgeGame);
       }
-      if (restart === RESTART_VALUE.QUIT) bridgeGame.quit();
+      if (restart === RESTART_VALUE.QUIT) {
+        OutputView.printResult(up, down);
+        bridgeGame.quit();
+      }
     });
   },
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -20,6 +20,7 @@ const GAME_MESSAGE = Object.freeze({
   INPUT_BRIDGE_LENGTH: "다리의 길이를 입력해주세요.",
   INPUT_MOVE: "이동할 칸을 선택해주세요. (위: U, 아래: D)",
   RESTART: "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
+  RESULT: "최종 게임 결과",
 });
 
 const ERROR_MESSAGE = Object.freeze({
