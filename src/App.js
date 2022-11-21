2067acbaf485dcb3c0e51438506f6e1ee0e4f3b2
Raehyeon Jeong <raehy19@gmail.com>
2022-11-21 22:38:20 +0900

    feat(src/App.js) : BridgeGame 클래스 생성 및 초기화, 시작메시지

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const BridgeGame = require('./BridgeGame');
+const InputView = require('./InputView');
+const OutputView = require('./OutputView');
+
 class App {
-  play() {}
+  #bridgeGame;
+
+  play() {
+    MissionUtils.Console.print('다리 건너기 게임을 시작합니다.\n');
+    this.#getBridgeSize();
+  }
+
+  #getBridgeSize() {
+    InputView.readBridgeSize((bridgeSize) => {
+      this.#bridgeGame = new BridgeGame(bridgeSize);
+    });
+  }
 }
 
 module.exports = App;
