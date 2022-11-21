f95bdab9be03d3a4eea92dd9f4fdab05a725d075
2je0 <bulljy@naver.com>
2022-11-21 22:39:07 +0900

    💎 style(constants): 입출력 개행문자 수정

--- src/Constants.js
+++ src/Constants.js
@@ -7,12 +7,12 @@ const MOVEMENT_RESULT = Object.freeze({
 const ERROR_PREFIX = '[ERROR]';
 const MESSAGE = Object.freeze({
   START_GAME: '다리 건너기 게임을 시작합니다.',
-  ASK_BRIDGE_SIZE: '다리의 길이를 입력해주세요.\n',
-  ASK_MOVING: '이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
+  ASK_BRIDGE_SIZE: '\n다리의 길이를 입력해주세요.\n',
+  ASK_MOVING: '\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
   FINAL_RESULT: '최종 게임 결과',
-  SUCCESS_OR_FAIL: (win) => `게임 성공 여부: ${win === MOVEMENT_RESULT.GAME_SUCCESS ? '성공' : '실패'}`,
+  SUCCESS_OR_FAIL: (win) => `\n게임 성공 여부: ${win === MOVEMENT_RESULT.GAME_SUCCESS ? '성공' : '실패'}`,
   NUMBER_OF_ATTEMPTS: (attempts) => `총 시도한 횟수: ${attempts}`,
-  ASK_GAME_COMMAND: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
+  ASK_GAME_COMMAND: '\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
   ERROR_NOT_A_NUMBER: `${ERROR_PREFIX} 숫자를 입력해주세요.`,
   ERROR_OUT_OF_RANGE: `${ERROR_PREFIX} 3이상 20이하 숫자를 입력해주세요.`,
   ERROR_NOT_U_OR_D: `${ERROR_PREFIX} U or D를 입력해주세요.`,
