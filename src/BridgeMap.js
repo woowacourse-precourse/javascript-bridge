40d864c10faca9992830007d8e836aa2443e3548
nalsae <nalsae0207@naver.com>
2022-11-22 02:08:06 +0900

    refactor: Simplify methods by seperating logics
    로직을 분리하여 메서드 단순화
    - buildBridge 분리
    - completeBridge 분리
    - resetBridge 분리
    - createMapInfo 분리

--- src/BridgeMap.js
+++ src/BridgeMap.js
@@ -1,41 +1,57 @@
 const OutputView = require('./OutputView');
 
 class BridgeMap {
-  static createMap(mapInfo, readFuncs) {
-    const { gameState, originBridgeSize, tryCount } = mapInfo;
+  static upBridge = '[ ';
+  static downBridge = '[ ';
 
-    let upBridge = '[ ';
-    let downBridge = '[ ';
+  static createMap(gameInfo, readFuncs) {
+    gameInfo.gameState.forEach(([moving, gameResult], index) => {
+      this.buildBridge(moving, gameResult);
 
-    gameState.forEach(([moving, gameResult], index) => {
-      if (moving === 'U') {
-        upBridge += `${gameResult} `;
-        downBridge += '  ';
-      }
-
-      if (moving === 'D') {
-        downBridge += `${gameResult} `;
-        upBridge += '  ';
-      }
-
-      if (index === gameState.length - 1) {
-        upBridge += ']';
-        downBridge += ']';
-      } else {
-        upBridge += '| ';
-        downBridge += '| ';
-      }
+      this.completeBridge(index === gameInfo.gameState.length - 1);
     });
 
-    const gameInfo = {
-      map: `${upBridge}\n${downBridge}`,
+    const mapInfo = this.createMapInfo(gameInfo);
+    this.resetBridge();
+
+    OutputView.printMap(mapInfo, readFuncs);
+  }
+
+  static buildBridge(moving, gameResult) {
+    if (moving === 'U') {
+      this.upBridge += `${gameResult} `;
+      this.downBridge += '  ';
+    }
+
+    if (moving === 'D') {
+      this.downBridge += `${gameResult} `;
+      this.upBridge += '  ';
+    }
+  }
+
+  static completeBridge(isEndOfBridge) {
+    if (isEndOfBridge) {
+      this.upBridge += ']';
+      this.downBridge += ']';
+    } else {
+      this.upBridge += '| ';
+      this.downBridge += '| ';
+    }
+  }
+
+  static resetBridge() {
+    this.upBridge = '[ ';
+    this.downBridge = '[ ';
+  }
+
+  static createMapInfo({ gameState, originBridgeSize, tryCount }) {
+    return {
+      map: `${this.upBridge}\n${this.downBridge}`,
       lastResult: gameState.at(-1)[1],
       originBridgeSize,
       currentBridgeSize: gameState.length,
       tryCount,
     };
-
-    OutputView.printMap(gameInfo, readFuncs);
   }
 }
 
