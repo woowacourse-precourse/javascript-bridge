d1ca1905eb02e227833beb6522d6dbe96e1e866a
chchaeun <kce3615@naver.com>
2022-11-21 20:52:51 +0900

    feat: 다리 건너기 진행상황 출력

--- src/OutputView.js
+++ src/OutputView.js
@@ -2,8 +2,12 @@ const MissionUtils = require('@woowacourse/mission-utils');
 const { Console } = MissionUtils;
 
 const MESSAGE = require('./consts/Output');
+const GAME = require('./consts/Game');
 
 const OutputView = {
+  SUCCESS: 'O',
+  FAIL: 'X',
+  EMPTY: ' ',
   printStart() {
     Console.print(MESSAGE.START);
   },
@@ -12,8 +16,43 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {},
+  printMap(bridge, turn, isCurrentTurnSuccess) {
+    let up = this.makeResultArr(bridge, turn, GAME.UP);
+    let down = this.makeResultArr(bridge, turn, GAME.DOWN);
 
+    if (!isCurrentTurnSuccess) {
+      up = this.changeLastToFail(up);
+      down = this.changeLastToFail(down);
+    }
+
+    Console.print(this.makePrintFormat(up));
+    Console.print(this.makePrintFormat(down));
+  },
+  makeResultArr(bridge, turn, correct) {
+    let resultArr = [];
+
+    for (let i = 0; i < turn + 1; i++) {
+      const result = bridge[i] === correct ? this.SUCCESS : this.EMPTY;
+      resultArr.push(result);
+    }
+
+    return resultArr;
+  },
+  changeLastToFail(arr) {
+    let copyArr = [...arr];
+    if (arr.at(-1) === this.SUCCESS) {
+      copyArr.splice(copyArr.length - 1, 1, this.FAIL);
+    }
+
+    return copyArr;
+  },
+  makePrintFormat(result) {
+    const BAR = ' | ';
+    const SPACE = ' ';
+    const str = ['[', result.join(BAR), ']'].join(SPACE);
+
+    return str;
+  },
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
    * <p>
