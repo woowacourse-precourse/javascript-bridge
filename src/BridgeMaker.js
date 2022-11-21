767db418bc34341184a35ba5e8545b3fb6a677ec
KINHYEONJI <hyeonji0718@gmail.com>
2022-11-21 22:35:26 +0900

    feat(BridgeMaker) : 0, 1을 U, D으로 바꿔주는 함수 changeBridgeElements 구현
    - 0->"U"
    - 1->"D"

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -4,6 +4,16 @@ const BridgeMaker = {
     for (let i = 0; i < size; i++) {
       bridgeByNumber.push(generateRandomNumber());
     }
+    return this.changeBridgeElements(bridgeByNumber);
+  },
+
+  changeBridgeElements(bridgeByNumber) {
+    let bridgeByDirection = [];
+    for (let i = 0; i < bridgeByNumber.length; i++) {
+      if (bridgeByNumber[i] === 1) bridgeByDirection.push("D");
+      if (bridgeByNumber[i] === 0) bridgeByDirection.push("U");
+    }
+    return bridgeByDirection;
   },
 };
 
