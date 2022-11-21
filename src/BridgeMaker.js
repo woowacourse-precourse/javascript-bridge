422f2d260c8ba786e0fde2cb52ef1fdeebadec88
WaiNaat <qoli0703@gmail.com>
2022-11-21 19:14:33 +0900

    feat(BridgeMaker): remove type change feature
    참고: 2f8da009973058b2bd71a319428e66fd4c38b1f2 , 3775ccff16a44d271a4c3816f22d56401311464f
    generateRandomNumber()의 반환값이 Number임을 믿을 수 있도록
    바뀌었기 때문에 기존의 Number 형 변환 과정을 제거함.

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -10,7 +10,7 @@ const BridgeMaker = {
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
   makeBridge(size, generateRandomNumber) {
-    let bridge = Array.from(new Array(size), () => Number(generateRandomNumber()));
+    let bridge = Array.from(new Array(size), () => generateRandomNumber());
     bridge = bridge.map(
       (value) => (value === BridgeConfig.UP_VALUE ? BridgeConfig.UP : BridgeConfig.DOWN),
     );
