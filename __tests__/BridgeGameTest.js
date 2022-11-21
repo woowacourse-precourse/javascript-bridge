5ab5564dac18541e4aeb841ebdc0aeb2e951ae8b
1 <wrasf175@naver.com>
2022-11-21 14:17:09 +0900

    feat(BridgeGame):retry 기능 구현

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -3,10 +3,11 @@ const BridgeGame = require('../src/BridgeGame');
 describe('다리 건너기 게임 단위 테스트', () => {
     it('다리 앞으로 건너가기', () => {
         const bridgeGame = new BridgeGame();
-        const test1 = bridgeGame.move('U', []);
-        const test2 = bridgeGame.move('D', []);
-        expect(test1).toEqual([1]);
-        expect(test2).toEqual([0]);
+        bridgeGame.move('U');
+        const test = bridgeGame.userInputArray;
+        expect(test).toEqual([1]);
+        bridgeGame.move('D');
+        expect(test).toEqual([1,0]);
     });
     it('재시작시 사용자의 입력값 초기화',()=>{
         const bridgeGame = new BridgeGame();
--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -2,23 +2,23 @@
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
-  #userInputArray
+  userInputArray
   constructor(){
-    this.#userInputArray = [];
+    this.userInputArray = [];
   }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move(moveInput, inputArray) {
+  move(moveInput) {
     if(moveInput === 'U'){
-      inputArray.push(1);
+      this.userInputArray.push(1);
     }
     if(moveInput === 'D'){
-      inputArray.push(0);
+      this.userInputArray.push(0);
     }
-    return inputArray;
+    return this.userInputArray;
   }
 
   /**
@@ -26,7 +26,10 @@ class BridgeGame {
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  retry() {}
+  retry() {
+    this.userInputArray = [];
+    return this.userInputArray;
+  }
 }
 
 module.exports = BridgeGame;
