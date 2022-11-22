f6301cc5c4d747c0f532b41d81aa2bdc86d37b8c
marugy <gyulife7301@naver.com>
2022-11-22 10:11:28 +0900

    feat(OutputView): printResult, printSuccess 게임의 최종 결과를 형식에 맞춰 출력하는 기능

--- src/OutputView.js
+++ src/OutputView.js
@@ -40,8 +40,17 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() {
-    MissionUtils.Console.print();
+  printResult(bridge, location, gameCnt) {
+    MissionUtils.Console.print("최종 게임 결과\n");
+    this.print(bridge, location);
+    MissionUtils.Console.print(
+      `게임 성공 여부: ${this.printSuccess(bridge, location)}\n`
+    );
+    MissionUtils.Console.print(`총 시도한 횟수 ${gameCnt}\n`);
+  },
+  printSuccess(bridge, location) {
+    if (bridge[location] == 2 || bridge[location] == 3) return "성공";
+    return "실패";
   },
 };
 
