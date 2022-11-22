989d8e8e8b99ad24fb1d3cf614437b5167e7a5bb
dragonfruitlemonade <tpghks9245@gmail.com>
2022-11-22 17:14:04 +0900

    feat(settings): 예외상항 검사 클래스 상수 만들기

--- src/constants/settings.js
+++ src/constants/settings.js
@@ -30,10 +30,27 @@ const PLAYERS_MAP = Object.freeze({
 
 const START = 0;
 
+const VALIDATION = Object.freeze({
+    lower_u: 'u',
+    lower_d: 'd',
+    lower_r: 'r',
+    lower_q: 'q',
+    upper_r: 'R',
+    upper_q: 'Q',
+    upper_u: 'U',
+    upper_d: 'D',
+    one_value: 1,
+    empty_value: 0,
+    min_value: 2,
+    max_value: 21, 
+    number_role: /^[0-9]+$/,
+})
+
 module.exports = {
     BRIDGE_GAME,
     BRIDGE_GAME_PROCEED,
     BRIDGE_MAKER,
     PLAYERS_MAP,
     START,
+    VALIDATION,
 };
