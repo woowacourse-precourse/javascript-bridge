cd392e2c6e5e27191ccf5fbef29f5b4ecf16ad25
khj0426 <706shin1728@naver.com>
2022-11-22 10:26:10 +0900

    feat:재시도 또는 종료로 분기

---
+++ src/controller/gameRetry/gameRetry.js
@@ -0,0 +1,20 @@
+const { closeConsole } = require('../../view/InputErrorView');
+const { printLoseResult, printMap } = require('../../view/OutputView');
+
+const End = (curgameobj, curindex) => {
+  printMap(curgameobj, curindex);
+  closeConsole();
+  return false;
+};
+
+const Retry = () => true;
+
+const ifRetryorEnd = (gameobj, gameindex) => {
+  const RetryorEnd = gameobj.retry();
+  if (RetryorEnd === 'Q') {
+    return End(gameobj, gameindex);
+  }
+  return Retry();
+};
+
+module.exports = ifRetryorEnd;
