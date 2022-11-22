af8e3602edb73d7446c98b6e4dc8c933cd8cbb7a
JiYoon Lee <wldbseja@daum.net>
2022-11-22 22:17:12 +0900

    feat (BridgeGame) :  move 테스트 구현

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -20,4 +20,29 @@ describe('게임 기능 테스트', () => {
     expect(() => bridgeGame.validateUnd('U')).not.toThrowError();
     expect(() => bridgeGame.validateUnd('D')).not.toThrowError();
   });
+
+  test('move', () => {
+    BridgeRandomNumberGenerator.generate = jest.fn();
+
+    BridgeRandomNumberGenerator.generate.mockReturnValue(0);
+    const bridgeGame = new BridgeGame(3);
+    let [condition, bridge, userState, countTry] = bridgeGame.move('U');
+
+    expect(condition).toEqual(2);
+    expect(bridge).toEqual(['D', 'D', 'D']);
+    expect(userState).toEqual(['U']);
+    expect(countTry).toEqual(1);
+
+    [condition, bridge, userState, countTry] = bridgeGame.move('D');
+    expect(condition).toEqual(1);
+    expect(bridge).toEqual(['D', 'D', 'D']);
+    expect(userState).toEqual(['U', 'D']);
+    expect(countTry).toEqual(1);
+
+    [condition, bridge, userState, countTry] = bridgeGame.move('D');
+    expect(condition).toEqual(0);
+    expect(bridge).toEqual(['D', 'D', 'D']);
+    expect(userState).toEqual(['U', 'D', 'D']);
+    expect(countTry).toEqual(1);
+  });
 });
