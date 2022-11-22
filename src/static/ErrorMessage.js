b37577ab1890784ed179f91f59561dd1b46d218f
GuHyeonWoo <ghw9174@gmail.com>
2022-11-22 22:45:15 +0900

    refactor: 출력 메세지 정적 데이터 참조로 수정
    - ErrorMessage: 예외 발생 시 출력하는 메세지
    - InputMessage: 입력 받을 시 출력하는 메세지
    - OutputMessage: 결과 값 출력시 사용되는 메세지

---
+++ src/static/ErrorMessage.js
@@ -0,0 +1,8 @@
+const ErrorMessage = {
+    numberError: '[ERROR] 다리 길이는 숫자를 입력해야 합니다.',
+    rangeError: '[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.',
+    movingAnswerError: '[ERROR] "U" 혹은 "D"를 입력하여 주세요.',
+    retryAnswerError: '[ERROR] "R" 혹은 "Q"를 입력하여 주세요.',
+}
+
+module.exports = ErrorMessage;
---
+++ src/static/InputMessage.js
@@ -0,0 +1,7 @@
+const InputMessage = {
+    size: '다리의 길이를 입력해주세요.',
+    moving: '이동할 칸을 선택해주세요. (위: U, 아래: D)',
+    retry: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)',
+}
+
+module.exports = InputMessage;
