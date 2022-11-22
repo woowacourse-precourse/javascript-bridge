3937ceb81256b9c317935d7fda02f49d3a998282
seoyeon <wjstjdus96@naver.com>
2022-11-22 14:56:02 +0900

    feat: 유효성 검증 기능

---
+++ src/Validator.js
@@ -0,0 +1,38 @@
+const ERROR_IS_NUMBER_VALID = "[ERROR] 숫자만 입력해야 합니다.";
+const ERROR_IS_RANGE_VALID =
+  "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+const ERROR_IS_MOVING_COMMAND = "[ERROR] U 이나 D만 입력해야 합니다.";
+const ERROR_IS_GAME_COMMAND = "[ERROR] R 이나 Q만 입력해야 합니다.";
+
+const isNumberValid = (num) => {
+  if (Number.isNaN(num)) {
+    console.print(ERROR_IS_NUMBER_VALID);
+    throw new Error(ERROR_IS_NUMBER_VALID);
+  }
+};
+const isRangeValid = (num) => {
+  if (num < 3 || num > 20) {
+    OutputView.print(ERROR_IS_RANGE_VALID);
+    throw new Error(ERROR_IS_RANGE_VALID);
+  }
+};
+const isBridgeSizeValid = (number) => {
+  const num = Number(number);
+  isNumberValid(num);
+  isRangeValid(num);
+};
+const isMovingCommandValid = (input) => {
+  if (input !== "U" && input !== "D") {
+    throw new Error(ERROR_IS_MOVING_COMMAND);
+  }
+};
+const isGameCommandValid = (input) => {
+  if (input !== "R" && input !== "Q") {
+    throw new Error(ERROR_IS_GAME_COMMAND);
+  }
+};
+module.exports = {
+  isBridgeSizeValid,
+  isMovingCommandValid,
+  isGameCommandValid,
+};
