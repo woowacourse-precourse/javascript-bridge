fbe520d859acfeb0a38773188d0aa5e2de762592
Jungwon Kim <dhkfkr134@naver.com>
2022-11-22 17:28:10 +0900

    refactor(OutputView, InputView): OutputView 일부 메서드  InputView로 통합

--- src/InputView.js
+++ src/InputView.js
@@ -1,21 +1,16 @@
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+const { Console } = require('@woowacourse/mission-utils');
 const InputView = {
-  /**
-   * 다리의 길이를 입력받는다.
-   */
-  readBridgeSize() {},
 
-  /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
-  readMoving() {},
+  readBridgeSize(callback) {
+    Console.readLine("다리의 길이를 입력해주세요.", callback);
+  },
 
-  /**
-   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-   */
-  readGameCommand() {},
+  readMoving(callback) {
+    Console.readLine("이동할 칸을 선택해주세요. (위: U, 아래: D)", callback);
+  },
+
+  readGameCommand(callback) {
+    console.readLine("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)", callback);
+  },
 };
-
 module.exports = InputView;
