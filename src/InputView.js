e63ff9f1bf20f04192264c9d77583b5dbb123c25
lsy20140 <lsy20140@naver.com>
2022-11-22 17:51:14 +0900

    feat: readGameCommand 메소드에 재시작 여부 물어보는 기능 구현

--- src/InputView.js
+++ src/InputView.js
@@ -39,7 +39,17 @@ const InputView = {
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand(callback) {
+    Console.readLine('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)', (key) => {
+      try {
+        //checkRetryKey(key);
+        callback(key === 'R');
+      } catch (e) {
+        //Console.print(e);
+        this.readGameCommand(callback);
+      }
+    });
+  },
 };
 
 module.exports = InputView;
