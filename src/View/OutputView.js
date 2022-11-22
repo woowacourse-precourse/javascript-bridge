10c142f7082405cc3d1b55856eea36a8e7252a7a
doheeMac <thsehgml37@korea.ac.kr>
2022-11-22 21:57:29 +0900

    fix: print format update

--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -67,7 +67,7 @@ const OutputView = {
   },
 
   printBeginAnnouncement() {
-    MissionUtils.Console.print("다리 건너기 게임을 시작합니다.\n");
+    MissionUtils.Console.print("다리 건너기 게임을 시작합니다.\n\n");
   },
 
   printError(errorMessage) {
