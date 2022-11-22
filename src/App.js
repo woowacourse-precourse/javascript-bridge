dab4a414c03aecb106513dbe7b521dca6d8c02e9
rovxxmjxx <miju3545@gmail.com>
2022-11-22 22:15:29 +0900

    feat(App): 시작 메시지 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const OutputView = require('./OutputView');
+const { MESSAGE } = require('./constants');
+
 class App {
-  play() {}
+  play = async () => {
+    OutputView.printMessage(MESSAGE.ENTRY);
+  };
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
