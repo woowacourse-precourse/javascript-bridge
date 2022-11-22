ab5344b326df41ff4ca5997c22e1e98d3c752df6
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-11-22 14:33:34 +0900

    refactor: BridgeGame 클래스 Jsdoc 적용

--- src/model/BridgeGame.js
+++ src/model/BridgeGame.js
@@ -4,6 +4,8 @@ const { RULES } = require('../constants/index.js');
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  /** @type {number} #retryCount - 재시도 횟수 */
+  /** @type {number} #turn - 사용자가 지나고 있는 다리의 index  */
   #retryCount;
 
   #turn;
@@ -15,8 +17,10 @@ class BridgeGame {
 
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+   * @param {string} thisTurnAnswer 이번 다리의 정답 (U 또는 D)
+   * @param {string} move 사용자가 선택한 위치 (U 또는 D)
+   * @param {StepResult} stepResult
+   * @return isThisTurnCorrect의 리턴값에 따라 stepResult의 메소드가 실행된다.
    */
   move(thisTurnAnswer, move, stepResult) {
     if (this.#turn > 0) stepResult.insert(RULES.SEPERATION, RULES.SEPERATION);
@@ -27,14 +31,18 @@ class BridgeGame {
     return stepResult.wrongStepRecord(move);
   }
 
+  /**
+   * 이번턴의 결과가 정답인지 아닌지 반환하는 메소드
+   * @param {string} thisTurnAnswer 이번 다리의 정답 (U 또는 D)
+   * @param {string} move 사용자가 선택한 위치 (U 또는 D)
+   * @return {boolean}
+   */
   isThisTurnCorrect(thisTurnAnswer, move) {
     return thisTurnAnswer === move;
   }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   retry() {
     this.#turn = 0;
