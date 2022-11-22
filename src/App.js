4718aa98993da0ea512c1b2c41140e24bb1ae5d9
RooT_Kim <bdd3452@gmail.com>
2022-11-22 18:34:19 +0900

    feat: App 클래스 내 메소드 구현 createNewBridge - 입력 메소드를 따로 분리하여 리팩토링한 메소드 selectMove - 입력 메소드를 따로 분리하여 리팩토링한 메소드 askRetryOrQuit - 게임을 재도전할지 종료할지 입력값을 받을 메소드 selectRetryOrQuit - 입력값을 받아 게임을 재도전할지 종료할지 결정하는 메소드 retryGame - 게임을 재도전할 메소드 quitGame - 게임 종료 메소드 fail - 게임 실패 처리 메소드 win - 게임 성공 처리 메소드

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 const InputView = require('./input_output/InputView');
 const OutputView = require('./input_output/OutputView');
 const BridgeGame = require('./BridgeGame');
-const { Console } = require('@woowacourse/mission-utils');
+const { SELECT } = require('./utils/Constants');
 class App {
 	constructor() {
 		this.bridgeGame = new BridgeGame();
@@ -13,12 +13,20 @@ class App {
 
 	gameStart() {
 		OutputView.printStart();
+		this.createNewBridge();
+	}
+
+	createNewBridge() {
 		InputView.readBridgeSize(this.runBridgeGame.bind(this));
 	}
 
 	runBridgeGame(size) {
 		this.bridgeGame.createBridge(size);
 		OutputView.printEnter();
+		this.selectMove();
+	}
+
+	selectMove() {
 		InputView.readMoving(this.checkMoving.bind(this));
 	}
 
@@ -29,10 +37,47 @@ class App {
 
 	checkMoveResult() {
 		OutputView.printMap(this.bridgeGame.getMovingLogs());
+
+		if (this.bridgeGame.isNotMatched()) {
+			return this.askRetryOrQuit();
+		}
+
+		if (this.bridgeGame.checkFinish()) {
+			return this.win();
+		}
+
+		return this.selectMove();
+	}
+
+	askRetryOrQuit() {
+		InputView.readGameCommand(this.selectRetryOrQuit.bind(this));
+	}
+
+	selectRetryOrQuit(select) {
+		select === SELECT.retry ? this.retryGame() : this.fail();
+	}
+
+	retryGame() {
+		this.bridgeGame.retry();
+		this.selectMove();
+	}
+
+	quitGame() {
+		OutputView.printFinishMessage();
+		OutputView.printMap(this.bridgeGame.getMovingLogs());
+		OutputView.printResult(this.bridgeGame.getGameResult());
+		OutputView.printTryCount(this.bridgeGame.getTryCount());
+	}
+
+	fail() {
+		this.bridgeGame.fail();
+		this.quitGame();
+	}
+
+	win() {
+		this.bridgeGame.win();
+		this.quitGame();
 	}
 }
-
-const app = new App();
-app.play();
 
 module.exports = App;
