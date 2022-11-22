cd15a556a4f0a0a23f115257e6ef06f1a393fea2
KINHYEONJI <hyeonji0718@gmail.com>
2022-11-22 15:29:35 +0900

    fix(Validator) : Validator를 const로 수정
    - class -> const
    - static 제거

--- src/Validator.js
+++ src/Validator.js
@@ -1,35 +1,35 @@
-class Validator {
-  static isNotANumber(input) {
+const Validator = {
+  isNotANumber(input) {
     if (isNaN(input)) {
       throw new Error("다리 개수는 숫자로 입력하셔야 합니다.");
       return true;
     }
     return false;
-  }
+  },
 
-  static isRangeIn(start, end, input) {
+  isRangeIn(start, end, input) {
     if (start > input || input < end) {
       throw new Error("3부터 20사이의 숫자만 입력하셔야 합니다.");
       return false;
     }
     return true;
-  }
+  },
 
-  static isMoveInputValidate(input) {
+  isMoveInputValidate(input) {
     if (input !== "U" || input !== "D") {
       throw new Error("이동할 칸은 U 또는 D로 입력하셔야 합니다.");
       return false;
     }
     return true;
-  }
+  },
 
-  static isRestartInputValidate(input) {
+  isRestartInputValidate(input) {
     if (input !== "R" || input !== "Q") {
       throw new Error("재시작 여부는 R 또는 Q로 입력하셔야 합니다.");
       return false;
     }
     return true;
-  }
-}
+  },
+};
 
 module.exports = Validator;
