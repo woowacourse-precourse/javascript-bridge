1d433ad046f8307903e51ab8305d2bfa4aa2fb68
lsy20140 <lsy20140@naver.com>
2022-11-22 20:40:57 +0900

    refactor: setMap 메소드에서 else문 삭제 -> else 문으로 변경

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,3 +1,5 @@
+const {Console} = require("@woowacourse/mission-utils");
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -8,28 +10,36 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
 
-  setMap(game) {
+   setMap(game) {
     const map = Array.from({ length: 2 }, () => Array(game.state.length).fill(' '));
-
     game.state.forEach((isEqual, idx) => {
       const bridgePos = game.bridge[idx] === 'U' ? 0 : 1;
       if (isEqual === true){
         map[bridgePos][idx] = 'O';
+        return;
       }
-      else {
-        map[1 - bridgePos][idx] = 'X';
-      }
+      map[1 - bridgePos][idx] = 'X';
     });
-
     return map;
   },
 
+  /**
+   * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
+   * <p>
+   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+   * @param {BridgeGame} game 다리 건너기 게임
+   */
+
   printMap(game) {
-    const map = this.setMap(game);
-    map.forEach((row) => {
-      Console.print(`[${row.join('|')}]`);
+    let mapResult = this.setMap(game);
+
+    mapResult.map((row) => {
+      Console.print(`[ ${row.join(' | ')} ]`);
     });
+
   },
+  
+
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
@@ -37,17 +47,13 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printResult(game) {
-    const isSuccess = game.state.every((isSuccess) => isSuccess === true);
-    const result = '';
-    const tryCount = game.tryCount + 1;
-    if(isSuccess){
-      result = '성공';
-    }else{
-      result = '실패';
-    }
+    const isSuccess = game.state.every((isSuccess) => isSuccess);
+    const result = isSuccess ? '게임 성공 여부: 성공' : '게임 성공 여부: 실패';
+    let tryCount = game.tryCount += 1;
+
     Console.print('최종 게임 결과');
-    this.setMap(game);
-    Console.print(`게임 성공 여부: ${result}`);
+    this.printMap(game);
+    Console.print(result);
     Console.print(`총 시도한 횟수: ${tryCount}`);
     Console.close();
 
