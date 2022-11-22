54e1cd80fd86b65999310a26781aee20081665d3
kwh97 <zzzang0512@naver.com>
2022-11-22 21:56:43 +0900

    test: 사용자 입력값이 예외사항이 아닌 정상인 경우의 테스트코드 구현

--- __tests__/InputValidationTest.js
+++ __tests__/InputValidationTest.js
@@ -14,3 +14,17 @@ describe("사용자 입력값 예외 테스트", () => {
   });
 });
 
+describe("사용자 입력값 정상 테스트", () => {
+  test.each([[3], [20]])("다리 길이 정상 입력값 테스트", (input) => {
+    expect(() => Validation.checkBridgeLength(input)).not.toThrow(); 
+  });
+
+  test.each([["U"], ["D"]])("이동할 칸 입력값에 대한 예외 테스트", (input) => {
+    expect(() => Validation.checkSpace(input)).not.toThrow(); 
+  });
+
+  test.each([["R"], ["Q"]])("재시도 여부 입력값에 대한 예외 테스트", (input) => {
+    expect(() => Validation.checkRetryCommand(input)).not.toThrow(); 
+  });
+});
+
