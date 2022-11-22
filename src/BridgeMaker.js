b60ba31fdc44c11f04a2d37b5d566397d1c14ee1
wusi-hub <jws9595@naver.com>
2022-11-22 21:29:42 +0900

    feat: 다리 생성을 위한 메소드 추가

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -8,7 +8,12 @@
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
   makeBridge(size, generateRandomNumber) {
-    return generateRandomNumber(size);
+    const bridge = [];
+    for (let i = 0; i < size; i++) {
+      const space = String(generateRandomNumber(size)) === "1" ? "U" : "D";
+      bridge.push(space);
+    }
+    return bridge;
   },
 };
 
