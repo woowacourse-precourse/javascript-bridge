45295bdbfa05379e4e2c4b7eb832da97b3dcd94c
rayark1 <max980203@snu.ac.kr>
2022-11-22 18:04:16 +0900

    form(src/InputView.js): JSDoc 추가

--- src/InputView.js
+++ src/InputView.js
@@ -9,12 +9,14 @@ const InputView = {
   MESSAGE_INPUT_COMMAND: "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
   /**
    * 다리의 길이를 입력받는다.
+   * @param {function(): void} callback - 다리의 길이를 입력받은 후 실행할 콜백 함수
    */
   readBridgeSize(callback) {
     Console.readLine(`${InputView.MESSAGE_INPUT_BRIDGE_LENGTH}\n`, callback);
   },
   /**
    * 사용자가 이동할 칸을 입력받는다.
+   * @param {function(): void} callback - 사용자가 이동할 칸을 입력받은 후 실행할 콜백 함수
    */
   readMoving(callback) {
     Console.readLine(`\n${InputView.MESSAGE_INPUT_MOVING_DIRECTION}\n`, callback);
@@ -22,6 +24,7 @@ const InputView = {
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+   * @param {function(): void} callback - 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받은 후 실행할 콜백 함수
    */
   readGameCommand(callback) {
     Console.readLine(`${InputView.MESSAGE_INPUT_COMMAND}\n`, callback);
