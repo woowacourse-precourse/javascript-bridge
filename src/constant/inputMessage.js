f3794b7ff52b3d840b62253472ccbb73ca3aa3b5
Hchan2 <seongchan_@naver.com>
2022-11-22 18:48:10 +0900

    줄바꿈 추가

--- src/constant/inputMessage.js
+++ src/constant/inputMessage.js
@@ -1,6 +1,6 @@
-const BRIDGE_LENGTH = '다리의 길이를 입력해주세요.';
-const MOVE_SPACE = '이동할 칸을 선택해주세요. (위: U, 아래: D)';
-const RE_OR_END = '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)';
+const BRIDGE_LENGTH = '다리의 길이를 입력해주세요.\n';
+const MOVE_SPACE = '이동할 칸을 선택해주세요. (위: U, 아래: D)\n';
+const RE_OR_END = '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n';
 
 module.exports = {
     BRIDGE_LENGTH,
--- src/constant/outputMessage.js
+++ src/constant/outputMessage.js
@@ -1,9 +1,9 @@
-const GAME_RESULT = (result) => { return `게임 성공 여부: ${result}` };
-const TOTAL_ATTEMPT = (attemptNumber) => { return `총 시도한 횟수: ${attemptNumber}` };
-const BRIDGE_START = '다리 건너기 게임을 시작합니다.';
-const RESULT_MESSAGE = '최종 게임 결과';
-const SUCCESS_MESSAGE = '성공';
-const FAIL_MESSAGE = '실패';
+const GAME_RESULT = (result) => { return `게임 성공 여부: ${result}\n` };
+const TOTAL_ATTEMPT = (attemptNumber) => { return `총 시도한 횟수: ${attemptNumber}\n` };
+const BRIDGE_START = '다리 건너기 게임을 시작합니다.\n';
+const RESULT_MESSAGE = '최종 게임 결과\n';
+const SUCCESS_MESSAGE = '성공\n';
+const FAIL_MESSAGE = '실패\n';
 
 module.exports = {
     GAME_RESULT,
