ae25a0946613d8b0bcddc9411636db1219e1ed6a
pitang <wpeffect@gmail.com>
2022-11-22 22:33:30 +0900

    feat: 출력

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,7 +1,28 @@
-const OutputView = {
-  printMap() {},
+const { Console } = require("@woowacourse/mission-utils");
+const { RESULT_MESSAGES, GAME_MESSAGES } = require("./Constants");
 
-  printResult() {},
+const OutputView = {
+  printInitGame() {
+    Console.print(GAME_MESSAGES.START_GAME);
+  },
+
+  printError(error) {
+    Console.print(error);
+  },
+
+  printMap(value) {
+    Console.print(RESULT_MESSAGES.PRINT_MAP(value));
+  },
+
+  printEndGame() {
+    Console.print(RESULT_MESSAGES.PRINT_END_GAME);
+  },
+
+  printResult(count, value) {
+    Console.print(RESULT_MESSAGES.PRINT_COUNT(count));
+    Console.print(RESULT_MESSAGES.PRINT_SUCCESS_OR_FAILURE(value));
+    Console.close();
+  },
 };
 
 module.exports = OutputView;
