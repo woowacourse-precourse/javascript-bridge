d7c824fbbe13e0c189f74713a7e8b80b1bd1ece3
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-11-22 10:40:37 +0900

    feat(App): App에서 기능 통합함

--- src/App.js
+++ src/App.js
@@ -1,5 +1,85 @@
+const InputView = require("./InputView");
+const OutputView = require("./OutputView");
+const BridgeGame = require("./BridgeGame");
+const { eventEmitter } = require("./EventEmitter");
+const { Console } = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  constructor() {
+    this.game = new BridgeGame();
+    this.eventEmitter = eventEmitter;
+    this.eventInitialize();
+  }
+
+  eventInitialize() {
+    this.eventEmitter.on("readBridgeSize", (bridgeLength) => {
+      this.setBridge(bridgeLength);
+      this.getMovingDirection();
+    });
+    this.eventEmitter.on("readMoving", (direction) =>
+      this.checkRightDirection(direction)
+    );
+    this.eventEmitter.on("retryOrExit", (answer) => this.retryOrExit(answer));
+  }
+
+  play() {
+    OutputView.printStart();
+    this.createBridge();
+  }
+
+  createBridge() {
+    InputView.readBridgeSize();
+  }
+
+  setBridge(bridgeLength) {
+    this.game.setBridge(bridgeLength);
+  }
+
+  getMovingDirection() {
+    InputView.readMoving();
+  }
+
+  checkRightDirection(direction) {
+    const result = this.game.move(direction.toUpperCase());
+    this.printResult(result);
+  }
+
+  printResult({ history, winThisTurn, gameEnd }) {
+    OutputView.printMap(history);
+
+    if (gameEnd) return this.finishGame(history);
+
+    if (winThisTurn) return InputView.readMoving();
+
+    this.game.removeLastHistory();
+    InputView.readGameCommand();
+  }
+
+  finishGame(history) {
+    OutputView.printMap(history, true);
+    OutputView.printResult({
+      result: this.game.result,
+      totalTry: this.game.totalTry,
+    });
+    this.exit();
+  }
+
+  retryOrExit(answer) {
+    if (answer === "R") {
+      return this.retry();
+    }
+    OutputView.printMap(this.game.history, true);
+    this.exit();
+  }
+
+  retry() {
+    this.game.retry();
+    this.getMovingDirection();
+  }
+
+  exit() {
+    Console.close();
+  }
 }
 
 module.exports = App;
