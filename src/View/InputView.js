58e485019e7aa1acafc2f57c31c4defc0979949c
knh6269 <knh6269@gmail.com>
2022-11-22 23:54:51 +0900

    feat(InputView): 콜백 함수

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -2,6 +2,18 @@ const Io = require('../Io');
 const OutputView = require('../View/OutputView');
 
 const InputView = {
+  question(str, callback, correctFunction) {
+    Io.input(str, input => {
+      try{
+        correctFunction(input);
+        callback(input);
+      } catch (error) {
+        OutputView.error(error);
+        InputView.question(str, callback, correctFunction);
+      }
+    });
+  },
+
 
 };
 
