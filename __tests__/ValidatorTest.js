9c26143d1e93eb7c59359873da4fcada7daf068c
useon <yoosk4986@naver.com>
2022-11-22 22:13:13 +0900

    test: 재시작 여부 입력 값의 유효성 검사를 테스트 한다

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -50,3 +50,26 @@ describe('이동할 칸 입력 값 유효성 테스트', () => {
     }).toThrow('[ERROR]');
   });
 });
+
+describe('재시작 여부 입력 값 유효성 테스트', () => {
+  test('숫자를 입력한 경우 예외 처리한다.', () => {
+    expect(() => {
+      Validator.retryOrQuit(1);
+    }).toThrow('[ERROR]');
+  });
+  test('특수 문자를 입력한 경우 예외 처리한다.', () => {
+    expect(() => {
+      Validator.retryOrQuit('@');
+    }).toThrow('[ERROR]');
+  });
+  test('R(재시작), Q(종료) 이외의 한글 문자를 입력한 경우 예외 처리한다.', () => {
+    expect(() => {
+      Validator.retryOrQuit('재시작');
+    }).toThrow('[ERROR]');
+  });
+  test('R(재시작), Q(종료) 이외의 영어 문자를 입력한 경우 예외 처리한다.', () => {
+    expect(() => {
+      Validator.retryOrQuit('V');
+    }).toThrow('[ERROR]');
+  });
+});
