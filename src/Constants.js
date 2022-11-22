4de13693357198892b5a51616a7b96823ba8fec8
Tory <88531407+1two13@users.noreply.github.com>
2022-11-22 22:22:27 +0900

    feat(Constants): 상수 관리 파일 추가

---
+++ src/Constants.js
@@ -0,0 +1,10 @@
+class Constants {
+  static CORRECT = 'O';
+  static UN_CORRECT = 'X';
+  static UP = 'U';
+  static DOWN = 'D';
+  static RETRY = 'R';
+  static QUIT = 'Q';
+}
+
+module.exports = Object.freeze(Constants);
