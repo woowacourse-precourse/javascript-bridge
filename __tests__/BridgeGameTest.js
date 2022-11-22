9a4a4e793c81c543404c056a6eb178ee24ac996d
tkdrb12 <tkdrb15@gmail.com>
2022-11-22 22:22:12 +0900

    refact : 테스트 코드에서 쓸모없는 내용 삭제

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -1,13 +1,6 @@
-const MissionUtils = require("@woowacourse/mission-utils");
 const BridgeGame = require("../src/BridgeGame");
 
 describe("BridgeMaker 클래스 테스트", () => {
-//   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
-//     expect(() => {
-//       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-//     }).toThrow("[ERROR]");
-//   });
-
   test("플레이어 이동 함수가 잘 작동하는지 테스트", () => {
     let bridgeGame = new BridgeGame(3);
 
