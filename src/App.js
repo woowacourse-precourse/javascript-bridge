40d8a30f81635c6e3d400fa3938ed9c8a48b0fa6
piporica <49173614+piporica@users.noreply.github.com>
2022-11-22 20:45:47 +0900

    refactor: 게임시작 문구 분리
    시작 문구 분리

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 const InputView = require("./InputView");
 const { Console, Random } = require("@woowacourse/mission-utils");
+const OutputView = require("./OutputView");
 
 class App {
   play() {
+    this.gameStart();
+  }
+
+  gameStart() {
+    OutputView.gameStart();
     InputView.readBridgeSize((input) => {
       console.log(input);
       this.stopProgram();
--- src/InputView.js
+++ src/InputView.js
@@ -8,7 +8,7 @@ const InputView = {
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize(callback) {
-    Console.readLine(MESSAGES.GAME_START, (input) => {
+    Console.readLine(MESSAGES.GET_LENGTH, (input) => {
       //길이 입력 예외처리 들어갈 부분
       callback(input);
     });
@@ -17,7 +17,9 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving(callback) {
+    Console.readLine();
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
