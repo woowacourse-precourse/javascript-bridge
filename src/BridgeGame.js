b538f9ce679bd60893b79ff806369a2903dbe865
jin-Pro <dnjun2@ajou.ac.kr>
2022-11-22 21:13:31 +0900

    feat: BridgeGame getCount, retry method

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -5,12 +5,9 @@ const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  #count = 0;
   #bridge;
-  #count;
 
-  constructor() {
-    this.initCount();
-  }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
@@ -30,7 +27,9 @@ class BridgeGame {
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  retry() {}
+  retry() {
+    this.increaseCount();
+  }
 
   setBridge(num) {
     this.#bridge = BridgeMaker.makeBridge(
@@ -47,8 +46,8 @@ class BridgeGame {
     this.#count += 1;
   }
 
-  initCount() {
-    this.#count = 0;
+  getCount() {
+    return this.#count;
   }
 }
 
