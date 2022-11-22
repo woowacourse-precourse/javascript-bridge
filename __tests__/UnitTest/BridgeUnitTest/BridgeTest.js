6456a4375986e378e0d5b60296b93100a4b5598a
Monte <monte6198@gmail.com>
2022-11-22 19:53:30 +0900

    feat: 다리 생성 단위 테스트 수정

--- __tests__/UnitTest/BridgeUnitTest/BridgeTest.js
+++ __tests__/UnitTest/BridgeUnitTest/BridgeTest.js
@@ -2,17 +2,17 @@ const Bridge = require('../../../src/model/Bridge');
 const BridgeMaker = require('../../../src/BridgeMaker');
 const { Random } = require('@woowacourse/mission-utils');
 
-const mockRandoms = (numbers) => {
-  Random.pickNumberInRange = jest.fn();
-  numbers.reduce((acc, number) => {
-    return acc.mockReturnValueOnce(number);
-  }, Random.pickNumberInRange);
-};
-const getLogSpy = () => {
-  const logSpy = jest.spyOn(BridgeMaker, 'makeBridge');
-  logSpy.mockClear();
-  return logSpy;
-};
+// const mockRandoms = (numbers) => {
+//   Random.pickNumberInRange = jest.fn();
+//   numbers.reduce((acc, number) => {
+//     return acc.mockReturnValueOnce(number);
+//   }, Random.pickNumberInRange);
+// };
+// const getLogSpy = () => {
+//   const logSpy = jest.spyOn(BridgeMaker, 'makeBridge');
+//   logSpy.mockClear();
+//   return logSpy;
+// };
 
 describe('다리 생성 테스트', () => {
   test('다리 생성 테스트', () => {
