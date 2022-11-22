cc0115239865786ebe9cbb55e593ebc54b4246e4
jaehak26 <jack10829@naver.com>
2022-11-22 21:48:43 +0900

    test 구현,  통과

--- src/Exception.js
+++ src/Exception.js
@@ -5,12 +5,12 @@ class Exception {
         const NUM_REG = /[^0-9]/g;
         if (('' + bridgeLength).match(NUM_REG) != null) {
             MissionUtils.Console.print('[ERROR] 다리 길이는 정수 값이어야 합니다.');
-            throw new Error("exceptBridgeLength - not integer")
+            throw new Error('[ERROR] 다리 길이는 정수 값이어야 합니다.')
         }
         if (bridgeLength < 3 || 20 < bridgeLength) {
             MissionUtils.Console.print('[ERROR] 다리 길이는 3부터 20 사이의 값이여야 합니다.');
 
-            throw new Error("exceptBridgeLength - bridgeLength < 3 || 20 < bridgeLength")
+            throw new Error('[ERROR] 다리 길이는 3부터 20 사이의 값이여야 합니다.')
         }
     }
 
@@ -18,14 +18,14 @@ class Exception {
         if (movingUpDown !== 'U' && movingUpDown !== 'D') {
 
             MissionUtils.Console.print('[ERROR] 입력값에는 대문자 U와 대문자 D만 들어갈 수 있습니다.');
-            throw new Error("exceptMovingUpDown")
+            throw new Error('[ERROR] 입력값에는 대문자 U와 대문자 D만 들어갈 수 있습니다.')
         }
     }
 
     exceptRetryQuit(retryQuit) {
         if (retryQuit !== 'R' && retryQuit !== 'Q') {
             MissionUtils.Console.print('[ERROR] 입력값에는 대문자 R와 대문자 Q만 들어갈 수 있습니다.');
-            throw new Error("exceptRetryQuit")
+            throw new Error('[ERROR] 입력값에는 대문자 R와 대문자 Q만 들어갈 수 있습니다.')
         }
     }
 };
