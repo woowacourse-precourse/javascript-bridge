8989ae1811b6534a850e1c5360d4a88668ab59ee
Sanggil Han <tkdrlf95@naver.com>
2022-11-22 20:23:27 +0900

    feat(Constants): Add ERROR messages
    에러 메시지를 상수화하여 추가하였음

--- src/Constants.js
+++ src/Constants.js
@@ -2,6 +2,7 @@ const MESSAGE = Object.freeze({
   READ_BRIDGE_SIZE: '다리의 길이를 입력해주세요.\n',
   READ_MOVING: '이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
   READ_GAME_COMMAND: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
+  GAME_START: '다리 건너기 게임을 시작합니다.\n'
 });
 
 const PAD = Object.freeze({
@@ -17,8 +18,15 @@ const COMMAND = Object.freeze({
   QUIT: 'Q',
 });
 
+const ERROR = Object.freeze({
+  BRIDGE_SIZE_MUST_BE_NUMBER: '[ERROR] 다리 길이는 자연수여야 합니다.\n',
+  BRIDGE_SIZE_MUST_BE_3_TO_20: '[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.\n'
+
+});
+
 module.exports = Object.freeze({
   MESSAGE,
   PAD,
   COMMAND,
+  ERROR,
 });
