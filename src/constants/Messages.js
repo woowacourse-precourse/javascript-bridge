eaf34708a32cf343b08bf5e8864d84f4b5372a70
areumsheep <areumsheep@gmail.com>
2022-11-22 21:43:14 +0900

    refactor(Messages): 메세지 상수에 역슬래시 추가

--- src/constants/Messages.js
+++ src/constants/Messages.js
@@ -1,10 +1,10 @@
 const MESSAGES = Object.freeze({
   START: '다리 건너기 게임을 시작합니다.\n',
-  INPUT_BRIDGE_SIZE: '다리의 길이를 입력해주세요.\n',
-  INPUT_MOVE_BLOCK: '이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
-  INPUT_RETRY: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
+  INPUT_BRIDGE_SIZE: '\n다리의 길이를 입력해주세요.\n',
+  INPUT_MOVE_BLOCK: '\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
+  INPUT_RETRY: '\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
   RESULT: {
-    TITLE: '최종 게임 결과',
+    TITLE: '\n최종 게임 결과',
     SCORE: (result) => `게임 성공 여부: ${result ? '성공' : '실패'}`,
     TRY_COUNT: (count) => `총 시도한 횟수: ${count}`,
   },
