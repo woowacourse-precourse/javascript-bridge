d38fc2f1c956f677ab3824e3419bfd82c5dc7e36
OrangeChick <retww@naver.com>
2022-11-22 21:34:36 +0900

    feat: Implement exception handling function related to leg length
    - 다리 길이 관련 예외 처리 기능을 구현하였습니다.

--- src/Validate.js
+++ src/Validate.js
@@ -2,6 +2,12 @@ const RETRY_LETTER = "R";
 const END_LETTER = "Q";
 
 const Validate = {
+  bridgeSize(bridgeLength) {
+    if (bridgeLength < 3 || bridgeLength > 20) {
+      throw new Error("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+    }
+  },
+
   retry(inputLetter) {
     if (inputLetter !== RETRY_LETTER && inputLetter !== END_LETTER)
       throw new Error("[ERROR] R 또는 Q를 입력해주세요.");
