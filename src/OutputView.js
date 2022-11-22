ebdc43926e52b97ab447bbd753ebac93f3ba9ef6
yeonkyoung <keep.ykk@gmail.com>
2022-11-22 19:48:37 +0900

    feat: printMap 함수

--- src/OutputView.js
+++ src/OutputView.js
@@ -10,7 +10,10 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {},
+  printMap(upBridge, downBridge) {
+    MissionUtils.Console.print(this.createPrintForm(upBridge));
+    MissionUtils.Console.print(this.createPrintForm(downBridge));
+  },
 
   createPrintForm(bridge) {
     let answer = "[";
