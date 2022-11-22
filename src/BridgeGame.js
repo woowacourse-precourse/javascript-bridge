815446c8868c821ba96beb434fc88674ac58970f
jiyun1006 <jiyun4251@gmail.com>
2022-11-22 18:03:12 +0900

    feat : 객체의 깊은 복사 수정 (JSON.parse를 활용한 방법이 기능이 작동을 하지 않았음.)
    재귀함수를 이용하여 깊은 복사 구현.

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -3,6 +3,8 @@ const InputView = require("./InputView");
 const Script = require("./Script");
 const OutputView = require("./OutputView");
 
+
+
 class BridgeGame {
   bridge;
   movingDirection;
@@ -17,12 +19,24 @@ class BridgeGame {
     InputView.readBridgeSize()
       .then((bridgeData) => {
         this.bridge = bridgeData
-        let tmpBridge = JSON.parse(JSON.stringify(this.bridge));
+        let tmpBridge = this.deepCopy(this.bridge)
         this.bridgeArr = [];
         this.move(tmpBridge);
       });
     }
     
+  deepCopy(object) {
+  if (object === null || typeof object !== "object") {
+    return object;
+  }
+    
+  const copy = Array.isArray(object) ? [] : {};
+ 
+  for (let key of Object.keys(object)) {
+    copy[key] = this.deepCopy(object[key]);
+  }
+  return copy;
+  }
   
   move(bridge) {
     InputView.readMoving()
@@ -33,11 +47,13 @@ class BridgeGame {
   }
 
   checkBridge(bridge, movingDirection) {
-    console.log(bridge, "현재 다리 상태");
     const bridgeDirection = bridge.shift();
     this.bridgeArr = OutputView.printMap(bridgeDirection, movingDirection, this.bridgeArr);
+    if (bridge.length === 0) return OutputView.printResult();
     if (bridgeDirection === movingDirection) return this.move(bridge);
+
     this.retry(this.bridge);
+
   }
 
   
@@ -47,7 +63,14 @@ class BridgeGame {
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   retry(bridge) {
-    console.log(bridge, "ASD");
+    InputView.readGameCommand()
+      .then((retryAnswer) => {
+        if (retryAnswer === "R") {
+          this.bridgeArr = [];
+          return this.move(bridge);
+        }
+        return OutputView.printResult();
+      });
   }
 }
 
