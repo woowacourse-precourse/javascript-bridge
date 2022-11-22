0fd560f9c776e6791321c4de683d70baf5082a93
jihong <rlawyd9@kookmin.ac.kr>
2022-11-22 22:07:22 +0900

    refactor: 올바른 입력인지 확인하는 함수 분할

--- src/App.js
+++ src/App.js
@@ -2,29 +2,29 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const InputView = require("./InputView");
 
 class App {
-  checkBridgeLen(bridge_len){
-    if(isNaN(bridge_len[0])){
+  checkBridgeSize(size){
+    if(isNaN(size)){
       throw new Error("[ERROR] 올바르지 않은 입력입니다.")
     }
     return true;
   }
 
-  startGame(){
-    MissionUtils.Console.print('다리 건너기 게임을 시작합니다.');
-    
-    const bridge_len = [];
+  inputBridgeSize(bridge_len){
     const inputView = InputView;
     inputView.readBridgeSize(bridge_len);
-    console.log(bridge_len);
     try{
-      if(this.checkBridgeLen(bridge_len)){
-
+      if(this.checkBridgeSize(bridge_len[0])){
+        console.log(bridge_len[0]);
       }
-      
     }catch(e){
       console.log(e);
     }
+  }
 
+  startGame(){
+    MissionUtils.Console.print('다리 건너기 게임을 시작합니다.');
+    const bridge_len = [];
+    this.inputBridgeSize(bridge_len);
   }
 
   play() {
