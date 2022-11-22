bdfaf526105cce5f9f811c8fc9e9e82b95ada10d
Dongkeun <hongdongk@naver.com>
2022-11-22 19:21:31 +0900

    기능2: 다리 길이입력받기

--- src/InputView.js
+++ src/InputView.js
@@ -5,7 +5,10 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine("다리의 길이를 입력해주세요", (bridgesize) => {
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
