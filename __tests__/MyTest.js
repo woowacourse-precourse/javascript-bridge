801001d885157422145f693a12ff6d4237891f63
GC-Park <kuyt3122@naver.com>
2022-11-22 09:33:10 +0900

    test:테스트 코드 추가

--- __tests__/MyTest.js
+++ __tests__/MyTest.js
@@ -53,6 +53,54 @@ const expectBridgeOrder = (received, upside, downside) => {
 
 describe("예외 처리 테스트", () => {
 
+  test("다리 5개 생성 테스트", () => {
+    const randomNumbers = [0, 0, 1, 0, 1];
+    const mockGenerator = randomNumbers.reduce((acc, number) => {
+      return acc.mockReturnValueOnce(number);
+    }, jest.fn());
+
+    const bridge = BridgeMaker.makeBridge(5, mockGenerator);
+    expect(bridge).toEqual(["D", "D", "U", "D", "U"]);
+  });
+
+  test("중간에 실패한 기능 테스트", () => {
+    const logSpy = getLogSpy();
+    mockRandoms([1, 0, 1]);
+    mockQuestions(["3", "U", "U", "R", "U", "D", "U"]);
+
+    const app = new App();
+    app.play();
+
+    const log = getOutput(logSpy);
+    expectLogContains(log, [
+      "최종 게임 결과",
+      "[ O |   | O ]",
+      "[   | O |   ]",
+      "게임 성공 여부: 성공",
+      "총 시도한 횟수: 2",
+    ]);
+    expectBridgeOrder(log, "[ O |   | O ]", "[   | O |   ]");
+  });
+
+  test("마지막에 틀리고 다시 실행하는 테스트", () => {
+    const logSpy = getLogSpy();
+    mockRandoms([1, 0, 1]);
+    mockQuestions(["3", "U", "D", "D", "R", "U", "D", "U"]);
+
+    const app = new App();
+    app.play();
+
+    const log = getOutput(logSpy);
+    expectLogContains(log, [
+      "최종 게임 결과",
+      "[ O |   | O ]",
+      "[   | O |   ]",
+      "게임 성공 여부: 성공",
+      "총 시도한 횟수: 2",
+    ]);
+    expectBridgeOrder(log, "[ O |   | O ]", "[   | O |   ]");
+  });
+
   test("생성할 다리 길이 3이상 20이하의 숫자가 아니면 예외 처리", () => {
     runException(["40"]);
   });
