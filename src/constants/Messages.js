2a092881b0182d19a798f84b03482b68f494977a
JangHoonSeok <orirori512@naver.com>
2022-11-22 12:09:12 +0900

    ✨ feat(constants): 각 메세지 개행문자 추가

--- src/constants/Messages.js
+++ src/constants/Messages.js
@@ -1,5 +1,5 @@
 const PRINT_MESSAGE = Object.freeze({
-  START: "다리 건너기 게임을 시작합니다.",
+  START: "다리 건너기 게임을 시작합니다.\n",
   MAP: (upBridge, downBridge) =>
     `[ ${upBridge.join(" | ")} ]\n[ ${downBridge.join(" | ")} ]\n`,
   RESULT: "최종 게임 결과",
@@ -7,9 +7,9 @@ const PRINT_MESSAGE = Object.freeze({
   ATTEMPT: (attempt) => `총 시도한 횟수: ${attempt}`,
 });
 const REQUEST_MESSAGE = Object.freeze({
-  BRIDGE_SIZE: "다리의 길이를 입력해주세요.",
-  SELECT_MOVE: "이동할 칸을 선택해주세요. (위: U, 아래: D)",
-  RESTART: "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
+  BRIDGE_SIZE: "다리의 길이를 입력해주세요.\n",
+  SELECT_MOVE: "이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
+  RESTART: "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n",
 });
 const ERROR_MESSAGE = Object.freeze({
   BRIDGE_SIZE_ERROR: "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.",
