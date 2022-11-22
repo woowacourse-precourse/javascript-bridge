d4b2804a51e5cbe8348e65188ba142d09c87f5a2
박겸영 <yong_drg@naver.com>
2022-11-22 17:46:53 +0900

    feat(ErrorHandling): 이동할 칸 입력값이 U(위 칸)와 D(아래 칸) 중 하나의 문자가 아니면 예외 처리하는 기능 추가

--- src/ErrorHandling.js
+++ src/ErrorHandling.js
@@ -1,5 +1,5 @@
-const { MIN_BRIDGE_SIZE, MAX_BRIDGE_SIZE } = require('./Command');
-const { BRIDGE_SIZE_NATURAL_NUMBER, BRIDGE_SIZE_RANGE } = require('./ErrorMessage');
+const { MIN_BRIDGE_SIZE, MAX_BRIDGE_SIZE, UP, DOWN } = require('./Command');
+const { BRIDGE_SIZE_NATURAL_NUMBER, BRIDGE_SIZE_RANGE, MOVING_COMMAND } = require('./ErrorMessage');
 
 const ErrorHandling = {
   throwError(isInvalid, message) {},
@@ -11,6 +11,11 @@ const ErrorHandling = {
     size = Number(size);
     const isOutOfRange = size < MIN_BRIDGE_SIZE || size > MAX_BRIDGE_SIZE;
     ErrorHandling.throwError(isOutOfRange, BRIDGE_SIZE_RANGE);
+  },
+
+  validateMovingCommand(movingCommand) {
+    const isInvalid = movingCommand !== UP && movingCommand !== DOWN;
+    ErrorHandling.throwError(isInvalid, MOVING_COMMAND);
   },
 };
 
