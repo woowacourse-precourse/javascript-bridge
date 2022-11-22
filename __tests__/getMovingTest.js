b4160a9397bdc51054f37b4377fa4c1e9ce358a7
Subin Jo <sbjo1202@naver.com>
2022-11-22 16:14:47 +0900

    refactor: 사용되지 않는 변수 삭제

--- __tests__/getMovingTest.js
+++ __tests__/getMovingTest.js
@@ -1,40 +1,4 @@
-const MissionUtils = require("@woowacourse/mission-utils");
-const GameController = require("../src/Controller/GameController");
 const Validation = require("../src/Utils/Validation");
-
-const mockQuestions = (answers) => {
-  MissionUtils.Console.readLine = jest.fn();
-  answers.reduce((acc, input) => {
-    return acc.mockImplementationOnce((_, callback) => {
-      callback(input);
-    });
-  }, MissionUtils.Console.readLine);
-};
-
-const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, "print");
-  logSpy.mockClear();
-  return logSpy;
-};
-
-const getOutput = (logSpy) => {
-  return [...logSpy.mock.calls].join("");
-};
-
-const runException = (inputs) => {
-  mockQuestions(inputs);
-  const logSpy = getLogSpy();
-  const gameController = new GameController();
-  gameController.getMoving();
-
-  expectLogContains(getOutput(logSpy), ["[ERROR]"]);
-};
-
-const expectLogContains = (received, logs) => {
-  logs.forEach((log) => {
-    expect(received).toEqual(expect.stringContaining(log));
-  });
-};
 
 describe('이동할 칸 입력 예외 테스트(U 또는 D가 아닌 다른 문자 입력)', () => {
 
