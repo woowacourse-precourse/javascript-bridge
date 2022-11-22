1f6b962f5988eac73a56e74267c3b1557568b700
Sang-Mini <sangmin3062@gmail.com>
2022-11-22 19:33:19 +0900

    feat : 다리의 길이를 입력받는 함수 구현.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+const { Console } = require("@woowacourse/mission-utils");
+const {
+  OUTPUT_MESSAGES,
+} = require("./constants");
+const InputView = require("./InputView");
+
 class App {
-  play() {}
+  play() {
+    Console.print(OUTPUT_MESSAGES.START_GAME);
+    this.startGame();
+  }
+
+  startGame() {
+    InputView.readBridgeSize();
+  }
 }
 
 module.exports = App;
--- src/InputView.js
+++ src/InputView.js
@@ -1,21 +1,41 @@
+const { Console } = require("@woowacourse/mission-utils");
+const InputCheck = require("./utils/validates");
+const BridgeMaker = require("./BridgeMaker");
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
+const OutputView = require('./OutputView');
+const BridgeGame = require("./BridgeGame");
+const { INPUT_MESSAGES } = require("./utils/constants");
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
+
+function generateRandomNumber() {
+  return BridgeRandomNumberGenerator.generate();
+}
+
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine(INPUT_MESSAGES.BRIDGE_LENGTH, (length) => {
+      const inputCheck = new InputCheck();
+      inputCheck.lengthCheck(length);
+      const bridge = BridgeMaker.makeBridge(length, generateRandomNumber);
+      this.readMoving(bridge, [], 1);
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() { },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() { },
 };
 
 module.exports = InputView;
