26eeede6119daece9bb4789e5d15fc5797ce3cf1
soomin <qkrtnals970804@gmail.com>
2022-11-22 21:18:01 +0900

    feat: makeBridge 기능 함수 구현

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -7,7 +7,19 @@ const BridgeMaker = {
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
-  makeBridge(size, generateRandomNumber) {},
+  makeBridge(size, generateRandomNumber) {
+    const upBridge = new Array(size).fill('X');
+    const downBridge = new Array(size).fill('X');
+
+    for (let i = 0; i < size; i++) {
+      const randomNumber = generateRandomNumber();
+      const IS_UPPER_CORRECT = randomNumber === 1;
+
+      IS_UPPER_CORRECT ? (upBridge[i] = 'O') : (downBridge[i] = 'O');
+    }
+
+    return [upBridge.join(''), downBridge.join('')];
+  },
 };
 
 module.exports = BridgeMaker;
