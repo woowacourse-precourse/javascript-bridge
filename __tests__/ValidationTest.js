4c1d34769a3a17ef7b680406ffc8f53639f4e24b
Jaehyeon <jaehyeonp74@gmail.com>
2022-11-22 12:59:10 +0900

    refactor: toThrowError를 사용해 특정 메세지 출력 확인

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,10 +1,11 @@
 const Validator = require('../src/Validator');
+const { ERROR_MESSAGE } = require('../src/constants');
 
 describe('유효성 검사 테스트 1', () => {
   test('다리 길이 입력 값이 숫자가 아니면 예외가 발생', () => {
     expect(() => {
       Validator.validateNumber('안녕');
-    }).toThrow('[ERROR]');
+    }).toThrowError(ERROR_MESSAGE.SIZE_NUMBER_ERROR);
   });
 });
 
@@ -12,7 +13,7 @@ describe('유효성 검사 테스트 2', () => {
   test('다리 길이 입력 값이 숫자가 범위를 벗어나면 예외가 발생', () => {
     expect(() => {
       Validator.validateNumberInRange('45');
-    }).toThrow('[ERROR]');
+    }).toThrowError(ERROR_MESSAGE.SIZE_RANGE_ERROR);
   });
 });
 
@@ -20,22 +21,14 @@ describe('유효성 검사 테스트 3', () => {
   test('다리 이동 칸 입력 값이 범위를 벗어나면 예외가 발생', () => {
     expect(() => {
       Validator.validateUpDown('R');
-    }).toThrow('[ERROR]');
+    }).toThrowError(ERROR_MESSAGE.MOVING_ERROR);
   });
 });
 
 describe('유효성 검사 테스트 4', () => {
-  test('다리 이동 칸 입력 값이 범위를 벗어나면 예외가 발생', () => {
-    expect(() => {
-      Validator.validateUpDown('R');
-    }).toThrow('[ERROR]');
-  });
-});
-
-describe('유효성 검사 테스트 5', () => {
   test('게임 재시작/종료 입력 값이 범위를 벗어나면 예외가 발생', () => {
     expect(() => {
       Validator.validateUpDown('X');
-    }).toThrow('[ERROR]');
+    }).toThrowError(ERROR_MESSAGE.COMMAND_ERROR);
   });
 });
