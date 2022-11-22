8e842a3d1f1c676241801b4808304d19cf74549d
chee9835 <huisu121@gmail.com>
2022-11-22 19:55:58 +0900

    refactor: result -> isSuccess 구체적인 변수명으로 수정

--- src/constants/views.js
+++ src/constants/views.js
@@ -7,7 +7,7 @@ const INPUT = Object.freeze({
 const OUTPUT = Object.freeze({
   START: '다리 건너기 게임을 시작합니다.',
   RESULT_TITLE: '최종 게임 결과',
-  ISSUCCESS: (result) => `게임 성공 여부: ${result}`,
+  ISSUCCESS: (isSuccess) => `게임 성공 여부: ${isSuccess}`,
   TRIAL: (trial) => `총 시도한 횟수: ${trial}`,
 });
 
