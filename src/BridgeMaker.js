4ebf72edaca530d3957b93be36252df031f72c27
jaewoogwak <jaewoo010207@gmail.com>
2022-11-22 12:08:18 +0900

    refactor(BridgeMaker): BridgeMaker 리팩터링
    이전에 BridgeGame에 대한 의존성을 제거

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,4 +1,4 @@
-const { createTokens } = require('./BridgeGame');
+const { DIRECTION } = require('./Constants/constant');
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -10,12 +10,9 @@ const BridgeMaker = {
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
   makeBridge(size, generateRandomNumber) {
-    const tokens = createTokens(size, generateRandomNumber);
-    const bridge = tokens.reduce((acc, cur) => {
-      acc.push(parseInt(cur, 10) === 1 ? 'U' : 'D');
-      return acc;
-    }, []);
-    return bridge;
+    return Array.from({ length: size }, generateRandomNumber).map((number) =>
+      number === 1 ? DIRECTION.up : DIRECTION.down
+    );
   },
 };
 
