e8e430a5e8e557060515b897395899f570c79927
Kim jinwoo <49385170+Sunny-jinn@users.noreply.github.com>
2022-11-22 14:15:21 +0900

    feat(BridgeGame): add constructor to make bridge

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,7 +1,20 @@
+const BrideMaker = require("./BridgeMaker");
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  #bridge;
+  #totalCount = 0;
+
+  constructor(size) {
+    this.#bridge = BrideMaker.makeBridge(
+      size,
+      BridgeRandomNumberGenerator.generate
+    );
+  }
+
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
