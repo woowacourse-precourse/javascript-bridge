068b9f6e7104f640390020fe88cb3f5a78ccec70
TaeeunKim <xodms0309@naver.com>
2022-11-22 16:21:28 +0900

    refactor: 에러 메시지 상수화

---
+++ src/utils/constants.js
@@ -0,0 +1,10 @@
+const ERROR_PREFIX = '[ERROR]';
+
+const ERROR = {
+  NOT_A_NUMBER: `${ERROR_PREFIX} 입력값은 숫자여야 합니다.`,
+  OUT_OF_RANGE: `${ERROR_PREFIX} 다리 길이는 3~20 사이의 숫자여야 합니다.`,
+  WRONG_MOVE_INPUT: `${ERROR_PREFIX} 이동할 칸은 U 또는 D로 입력해야 합니다.`,
+  WRONG_COMMAND: `${ERROR_PREFIX} 재시작/종료는 R 도는 Q로 입력해야 합니다.`,
+};
+
+module.exports = { ERROR };
--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,27 +1,28 @@
 const { Console } = require('@woowacourse/mission-utils');
+const { ERROR } = require('./constants');
 
 const validateBridge = (size) => {
   if (isNaN(size)) {
-    Console.print('[ERROR] 다리 길이는 숫자만 입력 가능합니다.');
+    Console.print(ERROR.NOT_A_NUMBER);
     throw new Error();
   }
 
   if (size < 3 || size > 20) {
-    Console.print('[ERROR] 다리 길이는 3~20 사이의 숫자여야 합니다.');
+    Console.print(ERROR.OUT_OF_RANGE);
     throw new Error();
   }
 };
 
 const validateMove = (move) => {
   if (/[^UD]/.test(move)) {
-    Console.print('[ERROR] 이동할 칸은 U 또는 D로 입력해야 합니다.');
+    Console.print(ERROR.WRONG_MOVE_INPUT);
     throw new Error();
   }
 };
 
 const validateRetry = (answer) => {
   if (/[^RQ]/.test(answer)) {
-    Console.print('[ERROR] 재시작/종료는 R 도는 Q로 입력해야 합니다.');
+    Console.print(ERROR.WRONG_COMMAND);
     throw new Error();
   }
 };
