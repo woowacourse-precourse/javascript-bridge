de33f16e4aac2385f6aedc52600d60a8926b94cd
areumsheep <areumsheep@gmail.com>
2022-11-22 15:37:53 +0900

    refactor(InputView,OutputView): 경로 변경
    - 화면 출력 상수 추가

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,5 +1,5 @@
-const { MESSAGES } = require('./constants/Messages');
-const readLineAsync = require('./utils/readLineAsync');
+const { MESSAGES } = require('../constants/Messages');
+const readLineAsync = require('../utils/readLineAsync');
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
