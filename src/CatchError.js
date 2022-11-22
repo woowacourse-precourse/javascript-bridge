2022b77777f79f38766a8d2fc774180a8e373cd0
Steadystudy <sgmin206@naver.com>
2022-11-22 21:15:40 +0900

    delete: 불필요한 console 제거

--- src/CatchError.js
+++ src/CatchError.js
@@ -3,7 +3,6 @@ const MissionUtils = require('@woowacourse/mission-utils');
 function catchError(validateFn, arg, errorFn) {
   try {
     validateFn(arg);
-    console.log(arg);
   } catch (error) {
     MissionUtils.Console.print(error);
     errorFn();
