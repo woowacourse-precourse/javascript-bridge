8275d88f4ef46b787b8c301f19e439ab59888152
kyeongwxx <kyeongwxx@gmail.com>
2022-11-22 21:32:31 +0900

    refactor: 유효성 검사 함수 throw 문 수정

--- src/validation/Validation.js
+++ src/validation/Validation.js
@@ -1,23 +1,20 @@
 const Validation = {
   validateBridgeSize(size) {
-    if (isNaN(size)) return { error: `\n[ERROR] 숫자만 입력가능합니다.\n` };
+    if (isNaN(size)) throw new Error(`\n[ERROR] 숫자만 입력가능합니다.\n`);
     if (size < 3 || size > 20)
-      return { error: `\n[ERROR] 입력 가능 범위는 3이상 20이하입니다.` };
+      throw new Error(`\n[ERROR] 입력 가능 범위는 3이상 20이하입니다.`);
     if (!Number.isInteger(Number(size)))
-      return { error: `\n[ERROR] 정수만 입력가능합니다.\n` };
-    return { error: "[ERROR] undefined" };
+      throw new Error(`\n[ERROR] 정수만 입력가능합니다.\n`);
   },
 
   validateUpsideDown(upsideDown) {
-    if (upsideDown === "U" || upsideDown === "D")
-      return { error: "[ERROR] undefined" };
-    return { error: `\n[ERROR] "U" 또는 "D"만 입력가능합니다.` };
+    if (upsideDown === "U" || upsideDown === "D") return;
+    throw new Error(`\n[ERROR] "U" 또는 "D"만 입력가능합니다.`);
   },
 
   validateRetryQuit(retryQuit) {
-    if (retryQuit === "R" || retryQuit === "Q")
-      return { error: "[ERROR] undefined" };
-    return { error: `\n[ERROR] "R" 또는 "Q"만 입력가능합니다.` };
+    if (retryQuit === "R" || retryQuit === "Q") return;
+    throw new Error(`\n[ERROR] "R" 또는 "Q"만 입력가능합니다.`);
   },
 };
 
