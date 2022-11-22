b5838b2b0931b380e7b54464f8f89a8f54d5b892
jinyong <zhaqh146@naver.com>
2022-11-22 17:46:29 +0900

    feat: 1-1 생성할 다리 길이 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+const InputView = require('./InputView');
+
 class App {
-  play() {}
+  play() {
+    InputView.readBridgeSize();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
