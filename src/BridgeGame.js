9733a61d4775dc077284600ab5e5e2cfc805f304
jiyun1006 <jiyun4251@gmail.com>
2022-11-22 17:32:49 +0900

    feat : 처음 랜덤으로 만들어진 다리객체의 깊은 복사(JSON.parse)
    처음 만들어진 다리를 다시 시작할 때 다시 사용해야 하기 때문에, 깊은 복사로 원본 객체로부터의 참조를 끊은 새로운 객체를 사용.

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,42 +1,54 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const InputView = require("./InputView");
 const Script = require("./Script");
-
+const OutputView = require("./OutputView");
 
 class BridgeGame {
   bridge;
   movingDirection;
+  bridgeArr;
+
   play() {
     MissionUtils.Console.print(`${Script.STARTSCRIPT}\n`);
     this.input();
-
   }
   
   input() {
     InputView.readBridgeSize()
       .then((bridgeData) => {
         this.bridge = bridgeData
-        this.move();
+        let tmpBridge = JSON.parse(JSON.stringify(this.bridge));
+        this.bridgeArr = [];
+        this.move(tmpBridge);
+      });
+    }
+    
+  
+  move(bridge) {
+    InputView.readMoving()
+      .then((movingDirectionData) => {
+        this.movingDirection = movingDirectionData
+        this.checkBridge(bridge, this.movingDirection);
       });
   }
 
-
-  /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  move() {
-    InputView.readMoving()
-      .then((movingDirectionData) => this.movingDirection = movingDirectionData);
+  checkBridge(bridge, movingDirection) {
+    console.log(bridge, "현재 다리 상태");
+    const bridgeDirection = bridge.shift();
+    this.bridgeArr = OutputView.printMap(bridgeDirection, movingDirection, this.bridgeArr);
+    if (bridgeDirection === movingDirection) return this.move(bridge);
+    this.retry(this.bridge);
   }
 
+  
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  retry() {}
+  retry(bridge) {
+    console.log(bridge, "ASD");
+  }
 }
 
 module.exports = BridgeGame;
