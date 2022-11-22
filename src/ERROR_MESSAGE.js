8bc5d35a538a73e01b1db490233b2dd2445043da
banma1234 <banma1234@gmail.com>
2022-11-22 23:53:13 +0900

    refactor: 에러메시지 모듈화 및 예외처리 함수 최적화

---
+++ src/ERROR_MESSAGE.js
@@ -0,0 +1,8 @@
+const ERROR_MESSAGE = {
+    INPUT_ERROR : "[ERROR] 다리 길이는 숫자만 입력해야 합니다.",
+    LENGTH_ERROR : "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.",
+    MOVE_ERROR : "[ERROR] 입력은 U 또는 D만 가능합니다.",
+    RETRY_ERROR : "[ERROR] 입력은 R 또는 Q만 가능합니다."
+}
+
+module.exports = ERROR_MESSAGE;
--- src/ErrorHandler.js
+++ src/ErrorHandler.js
@@ -1,3 +1,4 @@
+const ERROR = require('./ERROR_MESSAGE');
 
 const ErrorHandler = {
     isValid(input) {
@@ -7,22 +8,22 @@ const ErrorHandler = {
     isBridgeSizeCorrect(input) {
         const regex = /^[0-9]+$/;
         if (input < 3 | input > 20) {
-            throw new Error("[Error] 다리의 크기는 3 ~ 20 사이의 숫자만 가능합니다.");
+            throw new Error(ERROR.LENGTH_ERROR);
         }
         if (!regex.test(input)) {
-            throw new Error("[Error] 숫자만 입력 가능합니다.");
+            throw new Error(ERROR.INPUT_ERROR);
         }
     },
 
     isRetryCorrect(input) {
         if (input != "R" | input != "Q") {
-            throw new Error("[Error] 재시작시 R 혹은 Q를 입력해주세요.");
+            throw new Error(ERROR.RETRY_ERROR);
         }
     },
 
     isStringCorrect(input) {
         if (input != "U" | input != "D") {
-            throw new Error("[Error] 이동시 U 혹은 D를 입력해주세요.");
+            throw new Error(ERROR.MOVE_ERROR);
         }
     }
 }
