a951fbeef4a8034fed0c1ab05ee02e68e6ac80c1
OrangeChick <retww@naver.com>
2022-11-22 23:56:18 +0900

    feat: Implement bridge crossing result output function
    - 다리 건너기 기능을 구현하였습니다.

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,14 +1,24 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
   constructor() {
-    this.bridge = null;
-    this.brideMap = {
+    this.bridgeSize = null;
+    this.bridgeMap = {
       U: [],
       D: [],
     };
     this.tryCount = 1;
+  }
+
+  setBridge(size) {
+    this.bridgeSize = size;
+  }
+
+  getBridgeMap() {
+    return this.bridgeMap;
   }
 
   /**
@@ -16,7 +26,23 @@ class BridgeGame {
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(moveLetter) {
+    this.drawMap(moveLetter);
+    Console.print(
+      `[ ${this.bridgeMap.U.join(" | ")} ]\n[ ${this.bridgeMap.D.join(
+        " | "
+      )} ]\n`
+    );
+  }
+
+  drawMap(moveLetter) {
+    const currentIndex = this.bridgeMap.U.length;
+    const checkRightSpace =
+      this.bridgeSize[currentIndex] === moveLetter ? "O" : "X";
+    this.bridgeMap[moveLetter].push(checkRightSpace);
+    const otherCase = moveLetter === "U" ? "D" : "U";
+    this.bridgeMap[otherCase].push(" ");
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
