923f7908e5857ad0fcaa9ce958632471424e28a7
cocoa <jwv0323@kau.kr>
2022-11-22 20:20:35 +0900

    refactor: InputView의 에러처리 수정

--- src/InputView.js
+++ src/InputView.js
@@ -1,5 +1,10 @@
 const { Console } = require('@woowacourse/mission-utils');
 const { NUMBER, COMMAND, MESSAGE, ERROR, } = require('./BridgeConstant');
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
+const OutputView = require('./OutputView');
+const BridgeMaker = require('./BridgeMaker');
+const BridgeGame = require('./BridgeGame');
+const bridgeGame = new BridgeGame();
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -9,56 +14,107 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  async readBridgeSize() {
-    const bridgeSize = await new Promise(answer => {
-      Console.readLine(MESSAGE.BRIDGE_SIZE, answer);
-    });
-    this.validateBridgeSize(bridgeSize);
-    return Number(bridgeSize);
+  readBridgeSize() {
+    Console.readLine(MESSAGE.BRIDGE_SIZE, this.validateBridgeSize.bind(this));
   },
 
   validateBridgeSize(answer) {
+    try {
+      this.exceptionBridgeSize(answer);
+      this.setBridgeSize(answer);
+    } catch(error) {
+      OutputView.printError(error);
+      this.readBridgeSize();
+    }
+  },
+
+  exceptionBridgeSize(answer) {
     if (/[^0-9]/g.test(answer)) {
-      throw new Error(ERROR.BRIDGE_SIZE_NUMBER);
+      throw ERROR.BRIDGE_SIZE_NUMBER;
     }
     if (Number(answer) < NUMBER.BIRDGE_SIZE_MINIMUM || Number(answer) > NUMBER.BIRDGE_SIZE_MAXIMUM) {
-      throw new Error(ERROR.BRIDGE_SIZE_RANGE);
+      throw ERROR.BRIDGE_SIZE_RANGE;
     }
+  },
+
+  setBridgeSize(answer) {
+    bridgeGame.setBridge(BridgeMaker.makeBridge(answer, BridgeRandomNumberGenerator.generate));
+    this.readMoving();
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  async readMoving() {
-    const moving = await new Promise(answer => {
-      Console.readLine(MESSAGE.MOVING, answer);
-    });
-    this.validateMoving(moving);
-    return moving;
+  readMoving() {
+    Console.readLine(MESSAGE.MOVING, this.validateMoving.bind(this));
   },
 
   validateMoving(answer) {
+    try {
+      this.exceptionMoving(answer);
+      this.setMoving(answer);
+    } catch(error) {
+      OutputView.printError(error);
+      this.readMoving();
+    }
+  },
+
+  exceptionMoving(answer) {
     if (answer !== COMMAND.UP && answer !== COMMAND.DOWN) {
-      throw new Error(ERROR.MOVING);
+      throw ERROR.MOVING;
+    }
+  },
+
+  setMoving(answer) {
+    bridgeGame.move(answer);
+    this.printMoveResult();
+  },
+
+  getMoving() {
+    OutputView.printMap(bridgeGame.getMovingResult());
+    if (bridgeGame.isFail()) {
+      this.readGameCommand();
+    }
+    if (bridgeGame.isSuccess()) {
+      OutputView.printResult(bridgeGame.getGameResult());
+    }
+    else {
+      this.readMoving();
     }
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  async readGameCommand() {
-    const gameCommand = await new Promise(answer => {
-      Console.readLine(MESSAGE.GAME_COMMAND, answer);
-    });
-    this.validateGameCommand(gameCommand);
-    return gameCommand;
+  readGameCommand() {
+    Console.readLine(MESSAGE.GAME_COMMAND, this.validateGameCommand.bind(this));
   },
 
   validateGameCommand(answer) {
-    if (answer !== COMMAND.RESTART && answer !== COMMAND.QUIT) {
-      throw new Error(ERROR.GAME_COMMAND);
+    try {
+      this.exceptionGameCommand(answer);
+      this.setGameCommand(answer);
+    } catch(error) {
+      OutputView.printError(error);
+      this.readGameCommand();
     }
   },
+
+  exceptionGameCommand(answer) {
+    if (answer !== COMMAND.RESTART && answer !== COMMAND.QUIT) {
+      throw ERROR.GAME_COMMAND;
+    }
+  },
+
+  setGameCommand(answer) {
+    if (answer === COMMAND.RESTART) {
+      bridgeGame.retry();
+      this.readMoving();
+    }
+    if (answer === COMMAND.QUIT) {
+      OutputView.printResult(bridgeGame.getGameResult());
+    }
+  }
 
 };
 
