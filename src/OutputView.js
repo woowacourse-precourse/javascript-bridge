bb8734cb08c0182f231077864ebe7d0b62c83ed6
JSH99 <tjgus9966@naver.com>
2022-11-22 23:35:04 +0900

    refactor

--- src/OutputView.js
+++ src/OutputView.js
@@ -34,18 +34,6 @@ const OutputView = {
     MissionUtils.Console.print('다리 건너기 게임을 시작합니다.\n');
   },
 
-  printInputSize() {
-    MissionUtils.Console.print('다리의 길이를 입력해주세요.');
-  },
-
-  printInputMove() {
-    MissionUtils.Console.print('이동할 칸을 선택해주세요. (위 : U, 아래 : D)');
-  },
-
-  printInputRetry() {
-    MissionUtils.Console.print('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)');
-  },
-
   printGameSuccess(gameSuccess) {
     const result = gameSuccess ? '성공' : '실패';
     MissionUtils.Console.print(`\n게임 성공 여부: ${result}`);
