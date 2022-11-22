e80fb9e8d7bf5d4ffafdfcb406eb0b4da85f006c
solo5star <solo_5@naver.com>
2022-11-22 11:38:22 +0900

    test: errorExpression으로 생성한 예외가 throw되지 않던 오류 수정

--- src/validators/Validator.js
+++ src/validators/Validator.js
@@ -24,7 +24,7 @@ class Validator {
   should(expression, errorExpression) {
     const assertion = typeof expression === 'function' ? expression(this.#value) : expression;
     if (!assertion) {
-      if (typeof errorExpression === 'function') errorExpression(this.#value);
+      if (typeof errorExpression === 'function') throw errorExpression(this.#value);
       if (typeof errorExpression === 'string') throw new ValidationError(errorExpression);
       throw new ValidationError('Generic Validation Error');
     }
