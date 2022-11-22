812d65ce979f156351bacd858876438e1c4f4e6f
KINHYEONJI <hyeonji0718@gmail.com>
2022-11-22 19:18:41 +0900

    feat(InputView) : 잘못 된 값이 입력 되면 다시 입력할 수 있도록 try catch문 구현

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,20 +1,48 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const GameConfig = require("../util/GameConfig");
+const Validator = require("../Validator");
 
 const InputView = {
   readBridgeSize(callback, message) {
     MissionUtils.Console.readLine(message, (answer) => {
+      try {
+        Validator.isNotANumber(answer);
+        Validator.isRangeIn(
+          GameConfig.MIN_IN_RANGE,
+          GameConfig.MAX_IN_RANGE,
+          answer
+        );
+      } catch (e) {
+        MissionUtils.Console.print(e.message);
+        this.readBridgeSize(callback, message);
+        return;
+      }
       callback(answer);
     });
   },
 
   readMoving(callback, message) {
     MissionUtils.Console.readLine(message, (answer) => {
+      try {
+        Validator.isMoveInputValidate(answer);
+      } catch (e) {
+        MissionUtils.Console.print(e.message);
+        this.readMoving(callback, message);
+        return;
+      }
       callback(answer);
     });
   },
 
   readRestart(callback, message) {
     MissionUtils.Console.readLine(message, (answer) => {
+      try {
+        Validator.isRestartInputValidate(answer);
+      } catch (e) {
+        MissionUtils.Console.print(e.message);
+        this.readRestart(callback, message);
+        return;
+      }
       callback(answer);
     });
   },
