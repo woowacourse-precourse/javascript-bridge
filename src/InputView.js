fcced7e856e28ab541ca25e9498a65522e4696b7
Sanggil Han <tkdrlf95@naver.com>
2022-11-22 20:21:15 +0900

    fix(InputView): Fix input function where doesn't get values correctly
    InputView에서 값을 잘못 가져오고 있는 부분 해결

--- src/InputView.js
+++ src/InputView.js
@@ -8,27 +8,36 @@ const InputView = {
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize() {
-    Console.readLine(MESSAGE.READ_BRIDGE_SIZE, (bridgeSize) => {
-      return bridgeSize;
+    let bridgeSize = '';
+    Console.readLine(MESSAGE.READ_BRIDGE_SIZE, (readInput) => {
+      bridgeSize = readInput;
     })
+    Console.close();
+    return bridgeSize;
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving() {
-    Console.readLine(MESSAGE.READ_MOVING, (moving) => {
-      return moving;
+    let moving = '';
+    Console.readLine(MESSAGE.READ_MOVING, (readInput) => {
+      moving = readInput;
     })
+    Console.close();
+    return moving;
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand() {
-    Console.readLine(MESSAGE.READ_GAME_COMMAND, (gameCommand) => {
-      return gameCommand;
+    let gameCommand = '';
+    Console.readLine(MESSAGE.READ_GAME_COMMAND, (readInput) => {
+      gameCommand = readInput;
     })
+    Console.close();
+    return gameCommand;
   },
 };
 
