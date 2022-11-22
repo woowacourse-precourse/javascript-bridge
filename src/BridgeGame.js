2dcb3be0c85c23c8836f538991d8812d2aa0a41b
banma1234 <banma1234@gmail.com>
2022-11-22 23:06:32 +0900

    refactor: 재도전 함수 모듈화 및 최적화

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -7,6 +7,7 @@ const OutputHandler = require('./OutputView');
 
 class BridgeGame {
   #userInput = "";
+  #COUNT = 1;
 
   constructor() {}
   /**
@@ -31,7 +32,17 @@ class BridgeGame {
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-
+  retry() {
+    let input = InputHandler.readGameCommand();
+    if (input == "R") {
+      OutputHandler.initArray();
+      this.#COUNT += 1;
+    }
+    else if (input == "Q") {
+      OutputHandler.printResult("실패", this.#COUNT);
+      MissionUtils.Console.close();
+    }
+  }
 }
 
 module.exports = BridgeGame;
