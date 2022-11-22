d0f76d463e27ed88d6f0ba299905ee44b710ec97
Yesong <to06109@naver.com>
2022-11-22 17:29:41 +0900

    style(All): 주석 제거

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,6 +1,3 @@
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
 class BridgeGame {
   #bridge;
   #curBridge;
@@ -9,11 +6,7 @@ class BridgeGame {
     this.#bridge = bridge;
     this.#curBridge = Array.from({ length: 2 }, () => []);
   }
-  /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
+
   move(moveInput, brigeIndex) {
     if (this.canMove(moveInput, brigeIndex)) {
       moveInput === 'U' ? this.moveUpSuccess() : this.moveDownSuccess();
@@ -23,11 +16,6 @@ class BridgeGame {
     return this.#curBridge;
   }
 
-  /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
   retry() {
     this.#curBridge[0] = [];
     this.#curBridge[1] = [];
