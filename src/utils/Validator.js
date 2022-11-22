a020350b70476f911f899db14c035fbca8616807
s0ojin <onepst@hanyang.ac.kr>
2022-11-22 20:26:45 +0900

    fix(validator.js) : 정규식 오류 수정

--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -3,7 +3,7 @@ const { DEFAULT, ERROR_MESSAGE } = require('./constants.js');
 const Validator = {
   isValidRange(input) {
     const regex = /^([3-9]|1[0-9]|20)$/;
-    if (!regex.has(input)) {
+    if (!regex.test(input)) {
       throw new Error(ERROR_MESSAGE.RANGE);
     }
   },
