dc2a1b59bb4c115069926d91d55f4a76b4bc05e0
ReturnReason <reasonz@daum.net>
2022-11-22 15:54:22 +0900

    test(ValidatorTest): 클래스 파일 경로 변경으로 인한 테스트 코드 수정

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -1,4 +1,4 @@
-const Validator = require('../src/view/Validator');
+const Validator = require('../src/validate/Validator');
 
 describe('🌈 Validator 클래스 테스트 ', () => {
   test('⚡ 사용자가 공백을 입력하면 emptyInput 예외 처리됩니다.', () => {
