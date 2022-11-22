a19d8a9d67840cde06d5e90ceaab95193e2bd10f
deli-ght <minjju0660@gmail.com>
2022-11-22 22:52:19 +0900

    feat(validateRetryInput): 재시도 유저 답장 유효성 검사 함수 추가

--- src/Validation.js
+++ src/Validation.js
@@ -1,6 +1,7 @@
 const ErrorMessage = {
   BRIDGE_LENGTH_INPUT: "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.",
-  BRIDGE_MOVE_INPUT: "[ERROR] 입력값은 D 또는 U 둘 중의 하나만 입력해 주세요.",
+  BRIDGE_MOVE_INPUT: "[ERROR] 입력값은 D 또는 U 둘 중 하나만 입력해 주세요.",
+  RETRY_INPUT: "[ERROR] 입력값은 R 또는 Q 둘 중 하나만 입력해 주세요.",
 };
 
 const Validation = {
@@ -12,12 +13,22 @@ const Validation = {
     if (!/^\d+$/.test(input)) throw Error(ErrorMessage.BRIDGE_LENGTH_INPUT);
     if (input < 3 || input > 20) throw Error(ErrorMessage.BRIDGE_LENGTH_INPUT);
   },
+
   /**
    *
    * @param {*} input
    */
   validateBridgeMove(input) {
     if (input !== "U" && input !== "D")
+      throw Error(ErrorMessage.BRIDGE_MOVE_INPUT);
+  },
+
+  /**
+   *
+   * @param {*} input
+   */
+  validateRetryInput(input) {
+    if (input !== "R" && input !== "Q")
       throw Error(ErrorMessage.BRIDGE_MOVE_INPUT);
   },
 };
