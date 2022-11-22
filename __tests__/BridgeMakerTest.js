8df8fc4b462781bb9792309542b84ca6b932b658
jjjjjeongwon <jjjjjwon21@gmail.com>
2022-11-22 23:50:25 +0900

    test(BridgeMakerTest): 여러 값 반복테스트 추가

--- __tests__/BridgeMakerTest.js
+++ __tests__/BridgeMakerTest.js
@@ -23,19 +23,21 @@ describe('다리 생성 테스트', () => {
 });
 
 describe('다리길이 입력 예외 테이스', () => {
-  test('다리길이 입력값이 숫자가 아니면 예외가 발생한다.', () => {
-    const input = 'ddd';
 
+  test.each([['ddd'], ['22c'], ['2*']])('다리길이 입력값이 숫자가 아니면 예외가 발생한다.',
+  (input) => {
     expect(() => {
       InputValidator.isRightBridgeLength(input);
     }).toThrow(INPUT_ERROR.IS_NOT_NUMBER);
-  });
+  }
+  )
 
-  test('다리길이 입력값이 3이상 20이하가 아니면 예외가 발생한다.', () => {
-    const input = '2';
-
+  test.each([['2'], ['0'], ['32']])('다리길이 입력값이 3이상 20이하가 아니면 예외가 발생한다.',
+  (input) => {
     expect(() => {
       InputValidator.isRightBridgeLength(input);
     }).toThrow(INPUT_ERROR.IS_NOT_IN_RANGE);
-  });
+  }
+  )
+
 });
