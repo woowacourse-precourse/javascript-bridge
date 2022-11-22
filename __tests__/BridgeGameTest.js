6d33a0bd663e7abd035f4da51c78091f3069451d
hahagarden <88613455+hahagarden@users.noreply.github.com>
2022-11-22 15:12:19 +0900

    refactor: 메서드를 통해 BridgeGame 초기화하도록 수정

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -2,7 +2,8 @@ const BridgeGame = require("../src/BridgeGame");
 
 describe("BridgeGame 클래스 테스트", () => {
   test("사용자가 입력한 값과 정답을 비교하여 결과를 도출한다.", () => {
-    const bridgeGame = new BridgeGame(["U", "D", "U"]);
+    const bridgeGame = new BridgeGame();
+    bridgeGame.setBridge(["U", "D", "U"]);
 
     expect(bridgeGame.move("U")).toBe("O");
     expect(bridgeGame.move("D")).toBe("O");
@@ -10,7 +11,8 @@ describe("BridgeGame 클래스 테스트", () => {
   });
 
   test("다리 건너기 성공했는지 판단하는 기능", () => {
-    const bridgeGame = new BridgeGame(["U", "D"]);
+    const bridgeGame = new BridgeGame();
+    bridgeGame.setBridge(["U", "D"]);
 
     bridgeGame.move("U");
     expect(bridgeGame.isAllPassed()).toBe(false);
