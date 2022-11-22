d4713e4977f6752fdbf13658fbfc987d26b4eb1d
hwnim5324 <hwnim5324@naver.com>
2022-11-22 13:26:18 +0900

    feat: 전체 진행 기능 구현.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+const InputView = require("./InputView");
+const OutputView = require("./OutputView");
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+const BridgeGame = require("./BridgeGame");
+
 class App {
-  play() {}
+  play() {
+    const bridgeGame = new BridgeGame(InputView.readBridgeSize());
+
+    do{
+      do{
+        bridgeGame.move(InputView.readMoving());
+        OutputView.printMap(bridgeGame.getBridge(), bridgeGame.getPlace());
+      }while(!bridgeGame.isEnd().round);
+      if(bridgeGame.isEnd().game) break;
+    }while(bridgeGame.retry(InputView.readGameCommand()));
+
+    OutputView.printResult(bridgeGame.getBridge(), bridgeGame.getPlace(), bridgeGame.getTry());
+  }
 }
 
 module.exports = App;
