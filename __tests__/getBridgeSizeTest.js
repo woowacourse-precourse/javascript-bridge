8e32406973c04a7ee8a24e908f9ddc55156521d3
Subin Jo <sbjo1202@naver.com>
2022-11-22 16:16:14 +0900

    refactor: 반복적인 테스트 코드 each 사용하여 리팩토링

--- __tests__/getBridgeSizeTest.js
+++ __tests__/getBridgeSizeTest.js
@@ -1,50 +1,13 @@
-const MissionUtils = require("@woowacourse/mission-utils");
-const GameController = require("../src/Controller/GameController");
-
-const mockQuestions = (answers) => {
-  MissionUtils.Console.readLine = jest.fn();
-  answers.reduce((acc, input) => {
-    return acc.mockImplementationOnce((_, callback) => {
-      callback(input);
-    });
-  }, MissionUtils.Console.readLine);
-};
-
-const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, "print");
-  logSpy.mockClear();
-  return logSpy;
-};
-
-const getOutput = (logSpy) => {
-  return [...logSpy.mock.calls].join("");
-};
-
-const runException = (inputs) => {
-  mockQuestions(inputs);
-  const logSpy = getLogSpy();
-  const gameController = new GameController();
-  gameController.getBridgeSize();
-
-  expectLogContains(getOutput(logSpy), ["[ERROR]"]);
-};
-
-const expectLogContains = (received, logs) => {
-  logs.forEach((log) => {
-    expect(received).toEqual(expect.stringContaining(log));
-  });
-};
+const Validation = require("../src/Utils/Validation");
 
 describe('다리 길이 입력 예외 테스트', () => {
-  test("숫자가 아닌 문자를 입력한 경우", () => {
-    runException(["?"]);
-  });
 
-  test("정수가 아닌 실수를 입력한 경우", () => {
-    runException(["3.5"]);
-  });
-
-  test("범위를 벗어난 숫자를 입력한 경우", () => {
-    runException(["21"]);
-  });
+  test.each([['?'], ['3.5'], ['21']])(
+    '숫자가 아닌 문자를 입력한 경우, 정수가 아닌 실수를 입력한 경우, 범위를 벗어난 숫자를 입력한 경우',
+    (inputBridgeSize) => {
+      expect(() => {
+        Validation.checkBridgeSize(inputBridgeSize);
+      }).toThrow('[ERROR]');
+    }
+  );
 });
