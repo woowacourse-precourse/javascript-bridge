58559d34c1728d21e230bbc0e54f64366dfeeccf
Sanggil Han <tkdrlf95@naver.com>
2022-11-22 23:16:51 +0900

    feat(Validation): Implement checking if moving value is valid
    moving 값이 유효한지 유효성 체크하는 코드를 구현하였음

--- src/Validation.js
+++ src/Validation.js
@@ -7,9 +7,19 @@ const Validation = {
       throw new Error(ERROR.BRIDGE_SIZE_MUST_BE_NUMBER);
     }
     if (bridgeSize < 3 || bridgeSize > 20) {
-      throw new Error(ERROR.BRIDGE_SIZE_MUST_BE_3_TO_20)
+      throw new Error(ERROR.BRIDGE_SIZE_MUST_BE_3_TO_20);
     }
-  }
+  },
+
+  isValidMoving(moving) {
+    if (!moving) return;
+    if (moving.length >= 2 || moving.length < 1) {
+      throw new Error(ERROR.MOVING_MUST_BE_ONE_LETTER);
+    }
+    if (moving.match(/[^DU]/g)) {
+      throw new Error(ERROR.MOVING_MUST_BE_U_OR_D);
+    }
+  },
 }
 
 module.exports = Validation;
