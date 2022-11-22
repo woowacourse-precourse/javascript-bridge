adefcdee7893e09ccf4709bbf1bef9bb1f32f45c
김준환 <hvvany@gimjunhwan-ui-MacBookAir.local>
2022-11-22 23:49:23 +0900

    fix: 8. 재시도 기능 보완(재시작, 종료 구현)

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -3,6 +3,9 @@ const bridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
 const outputView = require("./OutputView");
 const inputView = require("./InputView");
 
+
+
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
@@ -22,45 +25,50 @@ class BridgeGame {
     this.#upBridgeReultArr = [];
     this.#downBridgeReultArr = [];
   }
+
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
+ 
   /** 5. 사용자 입력값 대비 건널 수 있는지 여부 비교해서 배열에 결과 값 넣어주기 */
-  move(movingInput) {
+  async move() {
+    const movingInput = await inputView.inputMoving(this.#bridgeArr);
+    console.log('###movingInput:', movingInput);
+
     let result = "X";
     if(this.#bridgeArr[this.#currentBridgeIndex] === movingInput) {
       result = "O";
       this.#currentBridgeIndex++;
     }
 
-  switch(movingInput) {
-    case 'U':
-      this.#upBridgeReultArr.push(result);
-      this.#downBridgeReultArr.push(' ');
-      break;
-    case 'D':
-      this.#downBridgeReultArr.push(result);
-      this.#upBridgeReultArr.push(' ');
-      break;
+    switch(movingInput) {
+      case 'U':
+        this.#upBridgeReultArr.push(result);
+        this.#downBridgeReultArr.push(' ');
+        break;
+      case 'D':
+        this.#downBridgeReultArr.push(result);
+        this.#upBridgeReultArr.push(' ');
+        break;
+    }
+    console.log(this.#upBridgeReultArr);
+    console.log(this.#downBridgeReultArr);
+
+
+    if(result === 'X') {
+      this.retry();
+    }
+
   }
-  console.log(this.#upBridgeReultArr);
-  console.log(this.#downBridgeReultArr);
-
-  if(result === 'X') {
-    this.retry();
-  }
-
-
-}
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-   async retry() {
+  async retry() {
     const retryInput = await inputView.inputRetry();
     console.log('###retry:', retryInput);
 
@@ -68,8 +76,11 @@ class BridgeGame {
       this.#upBridgeReultArr.pop();
       this.#downBridgeReultArr.pop();
       this.#tryCount++
-
+      this.move();
+    } else if(retryInput === 'Q') {
+      outputView.printResult();
     }
+
   }
 }
 
--- src/OutputView.js
+++ src/OutputView.js
@@ -1,29 +1,52 @@
 const { Console, Random } = require("@woowacourse/mission-utils");
-const { GUIDE_MESSAGE } = require("./Constant");
-
+const { GUIDE_MESSAGE, ERROR_MESSAGE, RESULT_MESSAGE } = require("./Constant");
+const app = require("./App");
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 const OutputView = {
-    /** 1. 게임시작 안내 문구 출력 */
-    printStart() {
-      Console.print(GUIDE_MESSAGE.START);
-    },
-  
+
+  /** 1. 게임시작 안내 문구 출력 */
+  printStart() {
+    Console.print(GUIDE_MESSAGE.START);
+  },
+
+  printRetry() {
+    Console.print(GUIDE_MESSAGE.INPUT_TRY);
+  },
+
+
   /**
    * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {},
+  printMap() {
+    let upBridgeReultArr = []; // o,x,공백으로 들어와야함
+    let downBridgeReultArr = [];
+    if(upBridgeReultArr > 1) {
+      return upBridgeReultArr.join(" | ")
+    }
+
+    Console.print("[ " + upBridgeReultArr + " ]")
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() {}
+  printResult() {
+    Console.print(RESULT_MESSAGE.TITTLE)
+
+    this.printMap();
+
+    Console.print(RESULT_MESSAGE.IS_SUCCESS,)//성공여부
+    Console.print(RESULT_MESSAGE.TRY_COUNT,)//성공 횟수
+
+    Console.close();
+  }
 };
 
 module.exports = OutputView;
