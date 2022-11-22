8b0be81b69364d15c4a84d81bf8e54f799c79b8c
kimeuns <dmstn2006@gmail.com>
2022-11-22 23:27:49 +0900

    refactor: InputView 분리

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -14,21 +14,24 @@ const InputView = {
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize(callback) {
-    Console.readLine(INPUT.ASK_BRIDGE_SIZE, callback);
+    Console.print(INPUT.ASK_BRIDGE_SIZE);
+    Console.readLine('', callback);
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving(callback) {
-    Console.readLine(INPUT.ASK_MOVEMENT, callback);
+    Console.print(INPUT.ASK_MOVEMENT);
+    Console.readLine('', callback);
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand(callback) {
-    Console.readLine(INPUT.ASK_RETRY, callback);
+    Console.print(INPUT.ASK_RETRY);
+    Console.readLine('', callback);
   },
 };
 
