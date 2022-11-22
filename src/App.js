0657cbea5bb8919cbbd1066dbfd05e717733b774
spsp8426 <spsp8426@naver.com>
2022-11-22 09:52:03 +0900

    feat : 다리길이 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+const { Console } = require('@woowacourse/mission-utils');
+const BridgeGame = require('./BridgeGame');
+const BridgeMaker = require('./BridgeMaker');
+const { generate } = require('./BridgeRandomNumberGenerator');
+const InputView = require('./InputView');
+const OutputView = require('./OutputView');
+
 class App {
-  play() {}
+  constructor() {
+    this.bridgeGame = null;
+  }
+
+  play() {
+    Console.print('다리 건너기 게임을 시작합니다.\n');
+    this.requestBridgeSize();
+  }
+  requestBridgeSize() {
+    InputView.readBridgeSize((size) => {
+      const bridge = BridgeMaker.makeBridge(Number(size), generate);
+      this.bridgeGame = new BridgeGame(bridge);
+    })
+  }
 }
 
+const app = new App();
+app.play();
 module.exports = App;
