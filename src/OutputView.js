e8465fa4c89ebb8ecc086f24576f4825b1ea010b
Wooseong Kim <kimwooseong8313@gmail.com>
2022-11-22 18:41:03 +0900

    feat: 다리 건넌 상태 문자열로 만드는 기능

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,4 +1,6 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const command = require("./utils/command");
+const step = require("./utils/step");
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -14,14 +16,44 @@ const OutputView = {
     return this;
   },
 
-  printMap() { },
+  printMap(answerSteps, inputSteps) {
+    const upBridgeString = `${step.START}${this.makeBridgeString(
+      answerSteps,
+      inputSteps,
+      command.UP
+    )}${step.END}`;
+    const downBridgeString = `${step.START}${this.makeBridgeString(
+      answerSteps,
+      inputSteps,
+      command.DOWN
+    )}${step.END}`;
+
+    this.printMessage(upBridgeString);
+    this.printMessage(downBridgeString);
+  },
+
+  makeBridgeString(answerSteps, inputSteps, direction) {
+    const steps = [];
+    for (let i = 0; i < inputSteps.length; i++) {
+      if (direction !== inputSteps[i]) {
+        steps.push(step.EMPTY);
+        continue;
+      }
+      if (inputSteps[i] === answerSteps[i]) {
+        steps.push(step.CORRECT);
+        continue;
+      }
+      steps.push(step.WRONG);
+    }
+    return steps.join(step.LINE);
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() { },
+  printResult() {},
 };
 
 module.exports = OutputView;
