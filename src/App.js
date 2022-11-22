22d6254b30a67977df79851417be6f1db28d42e1
banma1234 <banma1234@gmail.com>
2022-11-22 23:09:01 +0900

    refactor: feature파일 내용 App으로 이전 및 최적화

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 const InputHandler = require('./InputView');
+const OutputHandler = require('./OutputView');
 const BridgeMaker = require('./BridgeMaker');
 const MakeRandomValue = require('./BridgeRandomNumberGenerator');
+const BridgeGame = require('./BridgeGame');
 
 class App{
   play() {
     let bridge = this.buildBridge();
+    const bridgeHandler = new BridgeGame();
+    bridge.forEach((item, index) => {
+      if(!bridgeHandler.move(item))  {
+        bridgeHandler.retry();
+        index = 0;
+      }
+    })
+    OutputHandler.printResult("성공", bridgeHandler.getCount());
+    MissionUtils.Console.close();
   }
 
   buildBridge() {
