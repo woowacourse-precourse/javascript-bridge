9e691a60db44197cee6516c4d6c24ba6ade66cba
Sangjun-man <molung@icloud.com>
2022-11-22 19:24:50 +0900

    fix(InputView) : inputView 객체 코드 수정
    InputView객체에서 유효성 체크 하는 로직을 빼고, Console.readLine만 실행시켜주는 방식으로 로직을 수정했습니다.
    - 기존의 로직인 값을 입력받는 콜백함수를 InputView 객체의 inputHelper로 전달하는 과정에서 this 바인딩이 풀리는 현상이 발생했습니다
      이 현상을 해결하기 위해선, 콜백으로 넘겨줄 때마다 this 바인딩을 해주어야 하는데 이것이 좋은 방식이 아닌것이라고 판단했습니다.
      따라서 InputView에서 별다른 로직을 수행하지 않고, 유효성 체크 기능이 있는 메서드를 한번만 전달받아 실행시켜주는 것으로 입력 로직을 수정했습니다/

--- src/InputView.js
+++ src/InputView.js
@@ -1,46 +1,32 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const InputMessage = require("./utils/const/inputViewMessage");
-const VaildationCheck = require("./VaildationCheck");
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
-  inputContainer: null,
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {
-    const bridgeSize = Number(this.inputHelper(InputMessage.readBridgeSize));
-    VaildationCheck.isNumber(bridgeSize);
-    VaildationCheck.isNumberIntheRange([3, 20], bridgeSize);
-    return bridgeSize;
+  readBridgeSize(callback) {
+    MissionUtils.Console.readLine(InputMessage.readBridgeSize, callback);
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {
-    const move = this.inputHelper(InputMessage.readMoving);
-    VaildationCheck.isStringIntheList(["U", "D"], move);
-    return move;
+  readMoving(callback) {
+    MissionUtils.Console.readLine(InputMessage.readMoving, callback);
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {
-    const command = this.inputHelper(InputMessage.readGamdCommand);
-    VaildationCheck.isStringIntheList(["R", "Q"], command);
-    return command;
+  readGameCommand(callback) {
+    MissionUtils.Console.readLine(InputMessage.readGamdCommand, callback);
   },
 
-  inputHelper(message) {
-    MissionUtils.Console.readLine(message, (input) => {
-      this.inputContainer = input;
-    });
-    const buffer = this.inputContainer;
-    this.inputContainer = null;
-    return buffer;
+  close() {
+    MissionUtils.Console.close();
   },
 };
 
