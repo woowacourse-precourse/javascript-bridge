9c6027f002e86bf8fa79e2667c3c35d29c5952e1
RooT_Kim <bdd3452@gmail.com>
2022-11-22 13:58:03 +0900

    feat: Bridge 클래스 내 메소드 구현 createBridge - 랜덤으로 다리를 생성 후 반환받아서 필드에 저장 move - 사용자가 칸을 입력한 후 이동을 처리하기 위한 메소드 getMovingLogs - 이동 기록을 남기기 위한 메소드 #checkState - 게임의 현재 상태를 파악하기 위한 메소드

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,20 +1,47 @@
+const { BRIDGE_VIEW } = require('./utils/Constants');
+const BridgeMaker = require('./BridgeMaker');
+const BridgeRandomNumberGenerator = require('./utils/BridgeRandomNumberGenerator');
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
-  /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  move() {}
+	#infomation = {
+		bridge: [],
+		round: 0,
+		state: true,
+		movingLogs: [],
+		view: '',
+	};
 
-  /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  retry() {}
+	createBridge(size) {
+		this.#infomation.bridge = BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate);
+	}
+	/**
+	 * 사용자가 칸을 이동할 때 사용하는 메서드
+	 * <p>
+	 * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+	 */
+	move(moving) {
+		this.#infomation.state = this.#checkState(moving);
+		this.#infomation.movingLogs.push({ moving, state: this.#infomation.state });
+		this.#infomation.round += 1;
+	}
+
+	getMovingLogs() {
+		return this.#infomation.movingLogs;
+	}
+
+	/**
+	 * 사용자가 게임을 다시 시도할 때 사용하는 메서드
+	 * <p>
+	 * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+	 */
+	retry() {}
+
+	#checkState(moving) {
+		const { bridge, round } = this.#infomation;
+		return moving === bridge[round];
+	}
 }
 
 module.exports = BridgeGame;
