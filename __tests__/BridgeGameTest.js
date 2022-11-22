7795bf0406072c9f8d01e7644bfc459d64cc8fac
woo-jk <201702042@o.cnu.ac.kr>
2022-11-22 13:02:37 +0900

    test: BridgeGameTest runRetry() 함수, runMove() 함수 생성

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -1,35 +1,63 @@
 const BridgeGame = require("../src/BridgeGame");
 
+const runRetry = (bridgeGame, retryCount) => {
+  for (let count = 0; count < retryCount; count++) {
+    bridgeGame.retry();
+  }
+};
+
+const runMove = (bridgeGame, route) => {
+  route.forEach((direction) => {
+    bridgeGame.move(direction);
+  });
+};
+
 describe("브릿지 게임 클래스 테스트", () => {
+  test("잘못 된 길로 이동 했을 때, 추락", () => {
+    const bridgeGame = new BridgeGame(["U", "D", "U"]);
+    const playerRoute = ["U", "U"];
+
+    runMove(bridgeGame, playerRoute);
+
+    expect(bridgeGame.isFallen()).toBeTruthy();
+  });
+
+  test("올바른 된 길로 이동 했을 때, 추락하지 않음", () => {
+    const bridgeGame = new BridgeGame(["U", "D", "U"]);
+    const playerRoute = ["U", "D"];
+
+    runMove(bridgeGame, playerRoute);
+
+    expect(bridgeGame.isFallen()).toBeFalsy();
+  });
+
   test("재시도 두 번, 둘째 칸에서 실패", () => {
-    const bridge = ["U", "D", "U"];
-    const bridgeGame = new BridgeGame(bridge);
+    const bridgeGame = new BridgeGame(["U", "D", "U"]);
+    const retryCount = 2;
+    const playerRoute = ["U", "U"];
 
-    bridgeGame.retry();
-    bridgeGame.retry();
-    bridgeGame.move("U");
-    bridgeGame.out("U");
+    runRetry(bridgeGame, retryCount);
+    runMove(bridgeGame, playerRoute);
 
-    const [map, success, tryCount] = bridgeGame.getResult();
+    const [map, isClear, tryCount] = bridgeGame.getResult();
 
     expect(map).toEqual(["[ O | X ]", "[   |   ]"]);
-    expect(success).toBeFalsy();
+    expect(isClear).toBeFalsy();
     expect(tryCount).toEqual(3);
   });
 
   test("재시도 한 번, 성공", () => {
-    const bridge = ["U", "D", "U"];
-    const bridgeGame = new BridgeGame(bridge);
+    const bridgeGame = new BridgeGame(["U", "D", "U"]);
+    const retryCount = 1;
+    const playerRoute = ["U", "D", "U"];
 
-    bridgeGame.retry();
-    bridgeGame.move("U");
-    bridgeGame.move("D");
-    bridgeGame.move("U");
+    runRetry(bridgeGame, retryCount);
+    runMove(bridgeGame, playerRoute);
 
-    const [map, success, tryCount] = bridgeGame.getResult();
+    const [map, isClear, tryCount] = bridgeGame.getResult();
 
     expect(map).toEqual(["[ O |   | O ]", "[   | O |   ]"]);
-    expect(success).toBeTruthy();
+    expect(isClear).toBeTruthy();
     expect(tryCount).toEqual(2);
   });
 });
