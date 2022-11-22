83521b8a20594e362b4ac022fd63b282fc079629
김준환 <hvvany@gimjunhwan-ui-MacBookAir.local>
2022-11-22 23:18:13 +0900

    feat: 2. 다리길이 입력 및 안내

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,6 @@
+const { Console, Random } = require("@woowacourse/mission-utils");
+const { GUIDE_MESSAGE } = require("./Constant");
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,7 +8,10 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  /** 2. 다리 길이 입력 문구 출력 및 입력 */
+  readBridgeSize(callback) {
+    Console.readLine(GUIDE_MESSAGE.INPUT_LENGTH, callback);
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
