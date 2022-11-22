9b0bdb31bd95d6e1c31b4a6892317eaff1ea5b74
ho-ji <wkdd0205@gmail.com>
2022-11-22 19:42:09 +0900

    feat: Add checkBridgeSize() in InputView
    입력한 다리 길이가 조건에 맞지 않을 때, 에러를 발생하고 에러 메시지를 출력하는 함수를 구현함

--- src/InputView.js
+++ src/InputView.js
@@ -11,6 +11,7 @@ const InputView = {
     MissionUtils.Console.readLine("다리의 길이를 입력해주세요.", (answer) => {
       bridgeSize = Number(answer);
     });
+    this.checkBridgeSize(bridgeSize);
     return bridgeSize;
   },
   /**
@@ -22,6 +23,14 @@ const InputView = {
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand() {},
+  checkBridgeSize(bridgeSize) {
+    try {
+      if (!(bridgeSize >= 3 && bridgeSize <= 20))
+        throw new Error("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+    } catch (e) {
+      MissionUtils.Console.print(e.message);
+    }
+  },
 };
 
 module.exports = InputView;
