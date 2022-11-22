319a622d3b70c895841a336cbf6ba24c32163d9b
pinkishincoloragain <smb1103@gmail.com>
2022-11-22 21:22:00 +0900

    fix: validators throw new Errors

--- src/validate.js
+++ src/validate.js
@@ -7,20 +7,20 @@ const validateBridgeSize = (bridgeSize) => {
   const isNumber = !isNaN(bridgeSize);
   const isInRange = +bridgeSize >= BRIDGE_RANGE.MIN && +bridgeSize <= BRIDGE_RANGE.MAX;
 
-  if (isNumber === false) throw Error(ERRORS.INVALID_BRIDGE_TYPE);
-  if (isInRange === false) throw Error(ERRORS.INVALID_BRIDGE_RANGE);
+  if (isNumber === false) throw new Error(ERRORS.INVALID_BRIDGE_TYPE);
+  if (isInRange === false) throw new Error(ERRORS.INVALID_BRIDGE_RANGE);
 };
 
 const validateMove = (move) => {
   const isMove = move === MOVE.UP || move === MOVE.DOWN;
 
-  if (isMove === false) throw Error(ERRORS.INVALID_MOVE_TYPE);
+  if (isMove === false) throw new Error(ERRORS.INVALID_MOVE_TYPE);
 };
 
 const validateRestart = (restart) => {
   const isRestart = restart === RETRY.YES || restart === RETRY.NO;
 
-  if (isRestart === false) throw Error(ERRORS.INVALID_RESTART_TYPE);
+  if (isRestart === false) throw new Error(ERRORS.INVALID_RESTART_TYPE);
 };
 
 module.exports = { validateBridgeSize, validateMove, validateRestart };
