ff46cb767856a6c2e9fd75922e306e3d73cfa7ab
jihong <rlawyd9@kookmin.ac.kr>
2022-11-22 21:28:33 +0900

    feat: 다리 길이 입력받기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const InputView = require("./InputView");
+
 class App {
-  play() {}
+  startGame(){
+    MissionUtils.Console.print('다리 건너기 게임을 시작합니다.');
+    
+    const bridge_len = [];
+    const inputView = InputView;
+    inputView.readBridgeSize(bridge_len);
+    console.log(bridge_len);
+  }
+
+  play() {
+    this.startGame();
+  }
 }
 
-const app = App();
+const app = new App();
 app.play();
 module.exports = App;
