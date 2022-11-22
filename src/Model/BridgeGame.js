981c2b77d7bb9e6b4e2b18ac0792db1bf7f65181
knh6269 <knh6269@gmail.com>
2022-11-22 23:53:07 +0900

    feat(BridgeGame): 이동 함수

--- src/Model/BridgeGame.js
+++ src/Model/BridgeGame.js
@@ -2,12 +2,15 @@ const BridgeMaker = require('../BridgeMaker');
 const BridgeRandomNumberGenerator = require ('../BridgeRandomNumberGenerator');
 
 class BridgeGame {
-  
   constructor() {
     this.moving = [];
     this.bridge = [];
     this.count = 1;
   }
+  move(input) {
+    this.moving.push(input);
+  }
+
 }
 
 module.exports = BridgeGame;
