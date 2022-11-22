8866f30bdae961a9de6e62a16f4b0caccd275645
Dongkeun <hongdongk@naver.com>
2022-11-22 19:35:20 +0900

    기능4(1): 이동할 칸 입력받기

--- src/InputView.js
+++ src/InputView.js
@@ -11,6 +11,7 @@ const InputView = {
   readBridgeSize() {
     Console.readLine("다리의 길이를 입력해주세요", (bridgesize) => {
       this.checkBridgeSize(bridgesize);
+      this.readMoving(this.makeBridge(bridgesize));
     });
   },
 
@@ -23,12 +24,13 @@ const InputView = {
     return BridgeMaker.makeBridge(bridgeSize, BridgeRandomNumberGenerator.generate);
   },
 
-
-
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving(bridge) {
+    Console.readLine("이동할 칸을 선택해주세요. (위: U, 아래: D)", (moving) => {
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
