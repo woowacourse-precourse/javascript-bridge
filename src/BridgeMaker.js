d03dc1b48e4b075a81b93aebd4c03fefc21c23d7
yunjjeongjo <two6885@gmail.com>
2022-11-22 20:01:14 +0900

    feat: BridgeMaker 클래스 구현

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,3 +1,5 @@
+const { BRIDGE } = require("./constants/constants");
+
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
@@ -7,7 +9,20 @@ const BridgeMaker = {
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
-  makeBridge(size, generateRandomNumber) {},
+  makeBridge(size, generateRandomNumber) {
+    const bridge = [];
+    for (let i = 0; i < size; i++) {
+      const bridgeIndex = generateRandomNumber();
+      if (bridgeIndex === BRIDGE.UP_INDEX) {
+        bridge.push(BRIDGE.UP);
+      }
+      if (bridgeIndex === BRIDGE.DOWN_INDEX) {
+        bridge.push(BRIDGE.DOWN);
+      }
+
+      return bridge;
+    }
+  },
 };
 
 module.exports = BridgeMaker;
