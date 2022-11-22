a7066167f43700a314660534581db7519906d1ea
ho-ji <wkdd0205@gmail.com>
2022-11-22 19:46:03 +0900

    feat: Add input for receiving bridge size
    다리 길이를 입력 받음

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 const OutputView = require("../src/OutputView");
+const InputView = require("../src/InputView");
 class App {
   play() {
     OutputView.printStart();
+    const bridgeSize = InputView.readBridgeSize();
   }
 }
 
