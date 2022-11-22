7b17f1706c1b948196d6b09393d1196dc7cc0c41
woo-jk <201702042@o.cnu.ac.kr>
2022-11-22 13:59:16 +0900

    feat: constants 파일 생성

---
+++ src/utils/constants.js
@@ -0,0 +1,16 @@
+module.exports = {
+  TRY_COUNT_INITIAL_NUMBER,
+
+  BRIDGE_LENGTH: {
+    MIN: 3,
+    MAX: 20,
+  },
+  DIRECTION: {
+    TO_UPPER: "U",
+    TO_LOWER: "D",
+  },
+  COMMAND: {
+    RETRY: "R",
+    QUIT: "Q",
+  },
+};
