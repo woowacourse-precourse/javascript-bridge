fcb07ded66a30157012af182750ac532a4208c7c
BB-choi <choibobae0809@gmail.com>
2022-11-22 17:39:23 +0900

    test(UnitTest): MapMaker 클래스로 변경 적용

--- __tests__/UnitTest.js
+++ __tests__/UnitTest.js
@@ -1,3 +1,4 @@
+const BridgeGame = require('../src/BridgeGame');
 const MapMaker = require('../src/MapMaker');
 
 describe('MapMaker 테스트', () => {
@@ -6,20 +7,14 @@ describe('MapMaker 테스트', () => {
 
   test('이동불가능한 입력이 있는 경우 테스트', () => {
     const result = [true, false];
-    const map = MapMaker.create(
-      result.length,
-      getUserInputResult(result),
-    );
+    const map = new MapMaker(result.length, getUserInputResult(result)).create();
 
     expect(map).toBe(['[ O |   ]', '[   | X ]'].join('\n'));
   });
 
   test('모두 이동한 경우 테스트', () => {
     const result = Array.from({ length: commands.length }).fill(true);
-    const map = MapMaker.create(
-      result.length,
-      getUserInputResult(result),
-    );
+    const map = new MapMaker(result.length, getUserInputResult(result)).create();
 
     expect(map).toBe(['[ O |   |   | O |   ]', '[   | O | O |   | O ]'].join('\n'));
   });
