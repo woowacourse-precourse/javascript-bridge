02ef3425474bea1845132041d1b0cb6bafa48183
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-22 18:55:39 +0900

    feat: 사용자 이동 입력 함수 구현

--- src/InputView.js
+++ src/InputView.js
@@ -15,11 +15,27 @@ const InputView = {
       }
     });
   },
-  readMoving(game) {},
+  readMoving(game) {
+    Console.readLine(QUERY.MOVE, (char) => {
+      try {
+        validator.validateMove(char);
+        game.move(char);
+        this.gameProgress(game);
+      } catch (err) {
+        this.readError(err, () => this.readMoving(game));
+      }
+    });
+  },
   readGameCommand(game) {},
   readError(err, callback) {
     OutputView.printError(err);
     callback();
+  },
+  gameProgress(game) {
+    OutputView.printMap(game);
+    if (game.clear()) OutputView.printResult(game);
+    if (!game.over()) this.readGameCommand(game);
+    this.readMoving(game);
   },
 };
 
