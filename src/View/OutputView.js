da7ef92beeba5ce9493269873ae403fc095f5683
ming0830 <minsun0830@naver.com>
2022-11-22 17:20:49 +0900

    refactor : 테스트위한 최종 결과 분리

--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -26,8 +26,10 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printResult(result,successOrFailure,count) {
-    MissionUtils.Console.print(
-      `\n최종 게임 결과\n${result}\n\n게임 성공 여부 : ${successOrFailure}\n총 시도한 횟수: ${count}`);
+    MissionUtils.Console.print(`최종 게임 결과\n${result}\n`);
+    MissionUtils.Console.print(`\n게임 성공 여부: ${successOrFailure}`);
+    MissionUtils.Console.print(`\n총 시도한 횟수: ${count}`);
+
   },
 };
 
