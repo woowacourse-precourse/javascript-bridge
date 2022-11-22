92919528b7262bb27d2e5f4ed6ffe57e66ae9ae6
cybersyn2 <quandonestpetit@outlook.com>
2022-11-22 22:47:54 +0900

    feat(InputView) first draft of all required features

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,5 @@
+const { MissionUtils } = require("@woowacourse/mission-utils");
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,17 +7,53 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize(bridgeLength) {
+    MissionUtils.Console.readLine("다리의 길이를 입력해주세요.", bridgeLength);
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    let moving;
+    MissionUtils.Console.readLine(
+      "이동할 칸을 선택해주세요. (위: U, 아래: D)",
+      (userInput) => {
+        this.validateMoving(userInput);
+        moving = userInput;
+      }
+    );
+    return moving;
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+    let gameCommand;
+    MissionUtils.Console.readLine(
+      "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
+      (userInput) => {
+        this.validateGameCommand(userInput);
+        gameCommand = userInput;
+      }
+    );
+    return gameCommand;
+  },
+
+  validateMoving(userInput) {
+    if (userInput !== "U" && userInput !== "D") {
+      MissionUtils.Console.close();
+      throw new Error("[ERROR] U와 D 중 하나의 문자를 입력하세요.");
+    }
+  },
+
+  validateGameCommand(userInput) {
+    if (userInput !== "R" && userInput !== "Q") {
+      MissionUtils.Console.close();
+      throw new Error("[ERROR] R와 Q 중 하나의 문자를 입력하세요.");
+    }
+  },
 };
 
 module.exports = InputView;
