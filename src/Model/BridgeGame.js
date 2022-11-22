852f8332a77a5559a9a2dfcd7f292cb7aea3fae6
JangHoonSeok <orirori512@naver.com>
2022-11-22 19:27:54 +0900

    💡 comment: BridgeGame 클래스 함수 설명 주석 추가

--- src/Model/BridgeGame.js
+++ src/Model/BridgeGame.js
@@ -7,34 +7,63 @@ class BridgeGame {
   #bridge_map = new Bridge();
   #step = 0;
 
+  /**
+   * 랜덤으로 생성된 bridge를 set하는 메서드.
+   * @param {string[]} bridge
+   */
   setBridge(bridge) {
-    // 랜덤으로 생성된 bridge 설정하는 함수
     this.#bridge = bridge;
   }
 
+  /**
+   * 랜덤으로 생성된 bridge정보를 return하는 메서드.
+   * @returns 설정된 bridge를 return
+   */
   getBridge() {
-    // 랜덤으로 생성된 bridge정보 받아오는 함수.
     return this.#bridge;
   }
 
-  increaseStep() {
-    this.#step += 1;
-  }
-
+  /**
+   * 현재 마킹된 map을 return하는 메서드.
+   * @returns 마킹된 map 정보
+   */
   getCurrentMap() {
     return this.#bridge_map.getTotalBridge();
   }
 
+  /**
+   * step을 증가시키는 메서드.
+   * 여기서 step은 현재 탐색해야 할 bridge의 index를 뜻한다.
+   */
+  increaseStep() {
+    this.#step += 1;
+  }
+
+  /**
+   * 끝까지 bridge 정보를 탐색했는지 확인하는 메서드.
+   * @returns 끝까지 bridge를 탐색했다면 true, 아직 탐색해야 할 bridge 정보가 남아있다면 false를 리턴.
+   */
   isEnd() {
     const step = this.#step;
     if (step === this.#bridge.length - 1) return true;
     if (step !== this.#bridge.length - 1) return false;
   }
+
+  /**
+   * 사용자가 입력한 이동 커멘드와 현재 step에 해당하는 브릿지 정보를 비교하는 메서드.
+   * @param {string} moveCmd 사용자가 입력한 이동 커멘드 -> 'U', 'D' 중 하나.
+   * @param {string} currentBridge 현재 step에 해당하는 브릿지 정보 -> 'U', 'D' 중 하나.
+   * @returns 사용자 커멘드와 현재 step에 해당하는 브릿지 정보가 같다면 true, 아니면 false를 리턴.
+   */
+  checkSuccess(moveCmd, currentBridge) {
+    if (moveCmd === currentBridge) return true;
+    if (moveCmd !== currentBridge) return false;
+  }
+
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   * return : 성공하면 true, 성공안하면 false
+   * @param {string} moveCmd 사용자가 입력한 이동 커멘드 -> 'U', 'D' 중 하나.
+   * @returns 성공하면 true, 성공안하면 false
    */
   move(moveCmd) {
     const step = this.#step;
@@ -42,25 +71,24 @@ class BridgeGame {
     return this.checkSuccess(moveCmd, this.#bridge[step]);
   }
 
-  checkSuccess(moveCmd, currentBridge) {
-    if (moveCmd === currentBridge) return true;
-    if (moveCmd !== currentBridge) return false;
-  }
-
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   retry() {
     this.resetMoveBridgeMap();
     this.resetStep();
   }
 
+  /**
+   * 사용자가 게임을 다시 시도할 때 마킹된 map을 초기화 시키는 메서드
+   */
   resetMoveBridgeMap() {
     this.#bridge_map.resetMark();
   }
 
+  /**
+   * 사용자가 게임을 다시 시도할 때 step 정보를 초기화 시키는 메서드
+   */
   resetStep() {
     this.#step = 0;
   }
