a0436ae11c04e124e7418fca108794db21d7c1b4
22yuu <48381447+22yuu@users.noreply.github.com>
2022-11-22 19:56:18 +0900

    refactor(BridgeGame): 게임 결과 텍스트 코드 리팩토링

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -22,34 +22,30 @@ class BridgeGame {
   showResult() {
     return `[${this.topText}]\n[${this.bottomText}]\n`;
   }
-
+  
   buildResult(correct) {
     const len = this.#userInputArray.length;
     const value = this.#userInputArray[len - 1];
     const ox = correct ? ' O ' : ' X ';
 
     if (value == 'U') {
-      this.topText =
-        len == 1
-          ? this.buildTextResult(this.topText, `${ox}`)
-          : this.buildTextResult(this.topText, `|${ox}`);
-      this.bottomText =
-        len == 1
-          ? this.buildTextResult(this.bottomText, '   ')
-          : this.buildTextResult(this.bottomText, '|   ');
+      this.topText = this.buildCorrectText(len, this.topText, ox);
+      this.bottomText = this.buildEmptyText(len, this.bottomText);
     } else {
-      this.topText =
-        len == 1
-          ? this.buildTextResult(this.topText, '   ')
-          : this.buildTextResult(this.topText, '|   ');
-      this.bottomText =
-        len == 1
-          ? this.buildTextResult(this.bottomText, `${ox}`)
-          : this.buildTextResult(this.bottomText, `|${ox}`);
+      this.topText = this.buildEmptyText(len, this.topText);
+      this.bottomText = this.buildCorrectText(len, this.bottomText, ox);
     }
   }
 
-  buildTextResult(totalText, text) {
+  buildCorrectText(len, text, ox) {
+    return len == 1 ? this.addText(text, `${ox}`) : this.addText(text, `|${ox}`);
+  }
+
+  buildEmptyText(len, text) {
+    return len == 1 ? this.addText(text, '   ') : this.addText(text, '|   ');
+  }
+
+  addText(totalText, text) {
     return (totalText += text);
   }
 
@@ -58,21 +54,11 @@ class BridgeGame {
     return this.#bridge[currentIndex] == input;
   }
 
-  /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
   move(input, correct) {
     this.#userInputArray.push(input);
     this.buildResult(correct);
   }
 
-  /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
   retry(input) {
     if (input == 'R') {
       this.#tryCount += 1;
