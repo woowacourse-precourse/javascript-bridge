310302acac8977ed139194bbeae25c49ed0339c2
김태욱 <woo7i@gimtaeug-ui-MacBookPro.local>
2022-11-22 20:44:15 +0900

    다리 길이 입력 기능 구현

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,6 @@
+const { Console } = require("@woowacourse/mission-utils");
+const gameConst = require("../constant/constant.js");
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,8 +8,17 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
-
+   readBridgeSize(makeBridge) {
+    Console.readLine(gameConst.process.INPUT_LENGTH_MESSAGE + "\n", (length) => {
+        try {
+          makeBridge(Number(length));
+        } catch (err) {
+          Console.print(err.message);
+          this.readBridgeSize(makeBridge);
+        }
+      }
+    );
+  },
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
