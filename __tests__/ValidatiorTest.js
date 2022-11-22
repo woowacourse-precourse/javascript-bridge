c33e556702f3099b2cd336963d30a6f3ee3af5d6
연 <mogoo22@naver.com>
2022-11-22 18:18:54 +0900

    test: 이동칸 입력값에 대한 예외 처리 테스트 추가

--- __tests__/ValidatiorTest.js
+++ __tests__/ValidatiorTest.js
@@ -10,4 +10,12 @@ describe('입력값 예외 처리 테스트', () => {
     runException(input, error);
   });
 
+  test.each([
+    [['3', 0], ERROR_MESSAGE.INPUT_STR],
+    [['3', 'U '], ERROR_MESSAGE.INPUT_BLANK],
+    [['3', 'A'], ERROR_MESSAGE.MOVING],
+  ])('이동할 칸이 위(U) 또는 아래(D)가 아닌 경우 예외 처리한다.', (input, error) => {
+    runException(input, error);
+  });
+
 });
