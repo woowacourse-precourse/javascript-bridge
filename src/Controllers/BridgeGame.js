9d50d06ce4d62f9ca63da3c5123a38ebffa5cde7
ilgon0110 <moses011010@gmail.com>
2022-11-22 18:07:19 +0900

    refactor: BridgeGame 클래스 메서드 분리
    trycatch 구문 메서드 따로 생성하여 메서드 분리
    인터페이스 상속 받기

--- src/Controllers/BridgeGame.js
+++ src/Controllers/BridgeGame.js
@@ -1,14 +1,17 @@
 const Validation = require('../Utilities/Validation');
 const { MESSAGE, INPUT } = require('../Constants');
+const IBridgeGame = require('./IBridgeGame');
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
-class BridgeGame {
+class BridgeGame extends IBridgeGame {
   #turn = 0;
   #userLife = true;
   #attemptNumber = 1;
 
   constructor(model, view) {
+    super();
     this.model = model;
     this.view = view;
   }
@@ -19,14 +22,18 @@ class BridgeGame {
 
   getBridgeSize() {
     this.view.readBridgeSize(MESSAGE.BRIDGE_SIZE, (bridgeSize) => {
-      try {
-        Validation.isBridgeSizeValid(bridgeSize);
-        this.model.genBridge(bridgeSize);
-        this.move();
-      } catch (error) {
-        this.catchError(this.getBridgeSize, error);
-      }
+      this.genBridge(bridgeSize);
     });
+  }
+
+  genBridge(bridgeSize) {
+    try {
+      Validation.isBridgeSizeValid(bridgeSize);
+      this.model.genBridge(bridgeSize);
+      this.move();
+    } catch (error) {
+      this.catchError(this.getBridgeSize, error);
+    }
   }
 
   catchError(fn, error) {
@@ -34,25 +41,18 @@ class BridgeGame {
     fn.bind(this)();
   }
 
-  /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
   move() {
     this.view.readMoving(MESSAGE.UPORDOWN, (userMove) => {
-      try {
-        if (this.tryMove(userMove)) return;
-        this.#userLife ? this.move() : this.retry();
-      } catch (error) {
-        this.catchError(this.move, error);
-      }
+      this.tryCatchMove(userMove);
     });
   }
 
-  isUserSucess() {
-    if (this.#userLife && this.#turn === this.model.bridge.length) {
-      return this.end();
+  tryCatchMove(userMove) {
+    try {
+      if (this.tryMove(userMove)) return;
+      this.#userLife ? this.move() : this.retry();
+    } catch (error) {
+      this.catchError(this.move, error);
     }
   }
 
@@ -62,38 +62,45 @@ class BridgeGame {
     this.#turn += 1;
     this.view.printMap(this.model.upsideBridge, this.model.downSideBridge);
     if (this.isUserSucess()) return true;
+    return null;
   }
 
-  /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
+  isUserSucess() {
+    if (this.#userLife && this.#turn === this.model.bridge.length) {
+      return this.end();
+    }
+    return null;
+  }
+
   retry() {
-    this.#turn = 0;
-    this.askUserRetry();
+    this.view.readGameCommand(MESSAGE.RETRYORQUIT, (userRetry) => {
+      this.tryCatchRetry(userRetry);
+    });
   }
 
-  askUserRetry() {
-    this.view.readGameCommand(MESSAGE.RETRYORQUIT, (userRetry) => {
-      try {
-        Validation.isUserRetryValid(userRetry);
-        this.retryOrQuit(userRetry);
-      } catch (error) {
-        this.catchError(this.askUserRetry, error);
-      }
-    });
+  tryCatchRetry(userRetry) {
+    try {
+      Validation.isUserRetryValid(userRetry);
+      this.retryOrQuit(userRetry);
+    } catch (error) {
+      this.catchError(this.retry, error);
+    }
   }
 
   retryOrQuit(userRetry) {
     if (userRetry === INPUT.RESTART) {
       this.model.reset();
-      this.#attemptNumber += 1;
-      this.#userLife = true;
+      this.reset();
       this.move();
     } else {
       this.end();
     }
+  }
+
+  reset() {
+    this.#turn = 0;
+    this.#attemptNumber += 1;
+    this.#userLife = true;
   }
 
   end() {
