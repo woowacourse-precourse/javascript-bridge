7939dae8950ad714efb2bf08aaff12dcbd552a35
Minhye Kang <alsgp9898@naver.com>
2022-11-22 21:50:17 +0900

    refactor(BridgeMaker): 삼항연산자 사용 및 메서드 체이닝 활용하여 가독성 증가

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -5,17 +5,12 @@ const BridgeMaker = {
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
   makeBridge(size, generateRandomNumber) {
-    let bridgeArray = Array(Number(size)).fill('');
-    bridgeArray = bridgeArray.map((item) => item + generateRandomNumber());
-    
+    let bridgeArray = Array(Number(size))
+      .fill('')
+      .map((item) => item + generateRandomNumber());
     return bridgeArray.map((zeroOrOne) =>
-      zeroOrOne.replace(zeroOrOne, replaceString(zeroOrOne))
+      zeroOrOne.replace(zeroOrOne, zeroOrOne === '1' ? 'U' : 'D')
     );
-  },
+  }
 };
-
-const replaceString = (zeroOrOne) => {
-    if (zeroOrOne === '1') return 'U';
-    if (zeroOrOne === '0') return 'D';
-}
 module.exports = BridgeMaker;
