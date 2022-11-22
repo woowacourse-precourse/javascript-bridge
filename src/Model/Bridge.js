2caf36fefc613b50d01fa991f8234f4a8da1f9e6
JangHoonSeok <orirori512@naver.com>
2022-11-22 18:32:20 +0900

    ♻️ refactor: Bridge 클래스 변수 private으로 변경 및 변수명 변경

--- src/Model/Bridge.js
+++ src/Model/Bridge.js
@@ -1,25 +1,25 @@
 const { BRIDGE_DIRECTION } = require("../constants/GameCondition.js");
 class Bridge {
-  upBridge = [];
-  downBridge = [];
+  #upBridge = [];
+  #downBridge = [];
 
-  currentBridgeMap(cmd, currentBridge) {
-    if (cmd === BRIDGE_DIRECTION.UP) {
-      this.upBridge.push(currentBridge === cmd ? "O" : "X");
-      this.downBridge.push(" ");
+  currentBridgeMap(moveCmd, currentBridge) {
+    if (moveCmd === BRIDGE_DIRECTION.UP) {
+      this.#upBridge.push(moveCmd === currentBridge ? "O" : "X");
+      this.#downBridge.push(" ");
     }
-    if (cmd === BRIDGE_DIRECTION.DOWN) {
-      this.downBridge.push(currentBridge === cmd ? "O" : "X");
-      this.upBridge.push(" ");
+    if (moveCmd === BRIDGE_DIRECTION.DOWN) {
+      this.#downBridge.push(moveCmd === currentBridge ? "O" : "X");
+      this.#upBridge.push(" ");
     }
   }
 
   getTotalBridge() {
-    return [this.upBridge, this.downBridge];
+    return [this.#upBridge, this.#downBridge];
   }
   resetMark() {
-    this.upBridge = [];
-    this.downBridge = [];
+    this.#upBridge = [];
+    this.#downBridge = [];
   }
 }
 
