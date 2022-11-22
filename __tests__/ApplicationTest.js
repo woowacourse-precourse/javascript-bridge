2a879583a53352c1a30bc30f0cb97fe2fa699aef
cjm0611 <81799492+cjm0611@users.noreply.github.com>
2022-11-22 21:33:42 +0900

    refactor(ApplicationTest): 파일 경로 수정, 코드 컨벤션 관련 수정

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,8 +1,8 @@
-const MissionUtils = require("@woowacourse/mission-utils");
-const App = require("../src/App");
-const BridgeMaker = require("../src/BridgeMaker");
-const { ERROR_MESSAGES_BRIDGE } = require('../src/Messages');
-const InputView = require('../src/InputView');
+const MissionUtils = require('@woowacourse/mission-utils');
+const App = require('../src/App');
+const BridgeMaker = require('../src/BridgeMaker');
+const { ERROR_MESSAGES_BRIDGE } = require('../src/constant/Messages');
+const InputView = require('../src/view/InputView');
 
 const mockQuestions = (answers) => {
   MissionUtils.Console.readLine = jest.fn();
@@ -27,7 +27,7 @@ const getLogSpy = () => {
 };
 
 const getOutput = (logSpy) => {
-  return [...logSpy.mock.calls].join("");
+  return [...logSpy.mock.calls].join('');
 };
 
 const runException = (inputs) => {
@@ -53,37 +53,37 @@ const expectBridgeOrder = (received, upside, downside) => {
   expect(upsideIndex).toBeLessThan(downsideIndex);
 };
 
-describe("다리 건너기 테스트", () => {
-  test("다리 생성 테스트", () => {
+describe('다리 건너기 테스트', () => {
+  test('다리 생성 테스트', () => {
     const randomNumbers = [1, 0, 0];
     const mockGenerator = randomNumbers.reduce((acc, number) => {
       return acc.mockReturnValueOnce(number);
     }, jest.fn());
 
     const bridge = BridgeMaker.makeBridge(3, mockGenerator);
-    expect(bridge).toEqual(["U", "D", "D"]);
+    expect(bridge).toEqual(['U', 'D', 'D']);
   });
 
-  test("기능 테스트", () => {
+  test('기능 테스트', () => {
     const logSpy = getLogSpy();
     mockRandoms([1, 0, 1]);
-    mockQuestions(["3", "U", "D", "U"]);
+    mockQuestions(['3', 'U', 'D', 'U']);
 
     const app = new App();
     app.play();
 
     const log = getOutput(logSpy);
     expectLogContains(log, [
-      "최종 게임 결과",
-      "[ O |   | O ]",
-      "[   | O |   ]",
-      "게임 성공 여부: 성공",
-      "총 시도한 횟수: 1",
+      '최종 게임 결과',
+      '[ O |   | O ]',
+      '[   | O |   ]',
+      '게임 성공 여부: 성공',
+      '총 시도한 횟수: 1',
     ]);
-    expectBridgeOrder(log, "[ O |   | O ]", "[   | O |   ]");
+    expectBridgeOrder(log, '[ O |   | O ]', '[   | O |   ]');
   });
 
-  test("예외 테스트", () => {
+  test('예외 테스트', () => {
     runException(["a"]);
   });
 });
