b8b19d3cfcc114609de3789cc7d4eed4a99f6710
RosaDamascena <hsj990604@gmail.com>
2022-11-22 23:49:07 +0900

    feat : InputView 메소드 정리

--- src/InputView.js
+++ src/InputView.js
@@ -10,7 +10,7 @@ const InputView = {
   readBridgeSize() {
     let size = 0;
 
-    MissionUtils.Console.readLine("다리의 길이를 입력해주세요.", (number) => { 
+    MissionUtils.Console.readLine("다리의 길이를 입력해주세요. \n", (number) => { 
       validateRangeOfNumber();
       return size = number;
     });
@@ -19,12 +19,27 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    const MOVING_CHOICE;
+    MissionUtils.Console.readLine(
+      "\n이동할 칸을 선택해주세요. (위: U, 아래: D) \n",
+      (movekey) => {
+        return MOVING_CHOICE = movekey;
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+    const RETRY_OR_FINISH;
+
+    MissionUtils.Console.readLine(
+      "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q) \n",
+      (endKey) => {
+        return RETRY_OR_FINISH = endKey;
+     });
+  },
 };
 
 module.exports = InputView;
