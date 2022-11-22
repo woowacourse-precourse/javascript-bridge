900911810d60db68a9b8328f558386ea49f6e033
SeungWoo Kim <osabero@naver.com>
2022-11-22 19:09:33 +0900

    예외처리를 구현하였습니다.

--- src/InputView.js
+++ src/InputView.js
@@ -14,8 +14,7 @@ const InputView = {
    */
   readBridgeSize() {
     Console.readLine('다리 길이를 입력해 주세요', (size)=>{
-      // if (isNaN(size)){Console.print('[ERROR]')}
-      Validate.validateSize(size)
+      if(Validate.validateSize(size)){return this.readBridgeSize()}
       const generateRandomNumber = BridgeRandomNumberGenerator.generate
       const BRIDGE = BridgeMaker.makeBridge(size, generateRandomNumber)
       const TRIALS = 1
@@ -30,7 +29,7 @@ const InputView = {
     if(BRIDGE.length == CURRENTLOCATION){return OutputView.printResult(CURRENTBRIDGE, CURRENTLOCATION, TRIALS)}
     const SUCCESS = ' O '
     Console.readLine('이동할 칸을 입력해주세요. (위: U, 아래: D)', (moveinput)=>{
-      Validate.validateMoveInput(moveinput)
+      if(Validate.validateMoveInput(moveinput)){return this.readMoving(BRIDGE, CURRENTBRIDGE, CURRENTLOCATION, TRIALS)}
       const CHECK = BridgeGame.move(BRIDGE, CURRENTLOCATION, moveinput)
       const NEXTBRIDGE = OutputView.printMap(CURRENTBRIDGE, CHECK, moveinput)
       OutputView.printCurrentBridge(NEXTBRIDGE)
@@ -45,7 +44,7 @@ const InputView = {
    */
   readGameCommand(BRIDGE, NEXTBRIDGE, CURRENTLOCATION,TRIALS) {
     Console.readLine('게임을 다시 시도할지 여부를 입력해주세요.', (retryInput)=>{
-      if(retryInput !== 'R' && retryInput !== 'Q') {throw '[ERROR]'}
+      if(Validate.validateRetryinput(retryInput)){return this.readGameCommand(BRIDGE, NEXTBRIDGE, CURRENTLOCATION,TRIALS)}
       const RETRYCHECK = BridgeGame.retry(retryInput)
       if (RETRYCHECK == 0) {return this.readMoving(BRIDGE, [[],[]], 0, TRIALS+1)}
       OutputView.printResult(NEXTBRIDGE, CURRENTLOCATION, TRIALS)
--- src/Validate.js
+++ src/Validate.js
@@ -1,21 +1,31 @@
 const { Console } = require("@woowacourse/mission-utils");
 
-class Validate  {
-    static validateSize(size) {
-        if(isNaN(size)) {
-            Console.close();
-            throw new Error('[ERROR] 다리 길이는 3부터 20사이의 숫자여야 합니다.');
+
+const Validate = {
+    validateSize(size) {
+        try {
+            if (isNaN(size)) throw new Error("[ERROR] 숫자를 입력해 주세요");
+            if (size<3 || size>20) throw new Error("[ERROR] 3부터 20사이의 숫자를 입력해 주세요");
+          } catch(e) {
+            Console.print(e);
+            return true;
+          }
+    },
+    validateMoveInput(moveinput) {
+        try {
+            if(!(moveinput == 'U' || moveinput == 'D')) throw new Error('[ERROR] U나 D를 입력해주세요')
+        } catch(e) {
+            Console.print(e)
+            return true
+        }   
+    },
+    validateRetryinput(retryInput){
+        try {
+            if(!(retryInput == 'R' || retryInput == 'Q')) throw new Error('[ERROR] R이나 Q를 입력해주세요.')
+        } catch(e) {
+            Console.print(e)
+            return true
         }
-        if(size>20 || size<3) {
-            throw new Error('[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.')
-        }
-        return true
-    }
-    static validateMoveInput(moveinput) {
-        if(moveinput == 'U' || moveinput == 'D') {
-            return
-        }
-        throw '[ERROR] U나 D를 입력해주세요'
     }
 }
 
