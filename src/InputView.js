fd04db1edff1a3fc70fe9a40fe805bd9634302c9
devMingu <rooster100@naver.com>
2022-11-22 13:29:19 +0900

    refactor: count 변수 변경해서 인자값 줄이기

--- src/InputView.js
+++ src/InputView.js
@@ -10,6 +10,7 @@ const BridgeGame = require("./BridgeGame");
  */
 const InputView = {
   bridgeGame: new BridgeGame(),
+  count: 1,
   /**
    * 다리의 길이를 입력받는다.
    */
@@ -20,10 +21,9 @@ const InputView = {
       }
       else{
         let idx = 0;
-        let count = 1;
         const bridge = BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate);
         const currentBridge = [[], []];
-        this.readMoving(bridge, currentBridge, idx, count);
+        this.readMoving(bridge, currentBridge, idx);
       }
     })
   },
@@ -31,15 +31,15 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving(bridge, currentBridge, idx, count) {
+  readMoving(bridge, currentBridge, idx) {
     Console.readLine(`${Message.INPUT_MESSAGE.MOVING_TEXT}\n`, (move) => {
       if (ErrorHandler.readMovingErrorHandler(move)) {
         Console.close();
       }
       else {
-        const checkField = this.bridgeGame.move(bridge, currentBridge, idx, move, count);
+        const checkField = this.bridgeGame.move(bridge, currentBridge, idx, move, this.count);
         if (!checkField) {
-         this.readGameCommand(bridge, currentBridge, idx, count, move, count);
+         this.readGameCommand(bridge, currentBridge, idx);
         }
         else {
           idx = checkField[0];
@@ -47,7 +47,7 @@ const InputView = {
             Console.close();
           }
           else{
-           this.readMoving(bridge, currentBridge, idx, count);
+           this.readMoving(bridge, currentBridge, idx);
           }
         }
       }
@@ -58,16 +58,16 @@ const InputView = {
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand(bridge, currentBridge, idx, count, move) {
+  readGameCommand(bridge, currentBridge, idx) {
     Console.readLine(`${Message.INPUT_MESSAGE.READ_GAME_COMMAND_TEXT}\n`, (command) => {
       if (ErrorHandler.readGameCommandErrorHandler(command)) {
         Console.close();
       }
       else {
-        const checkReturn = this.bridgeGame.retry(bridge, command, currentBridge, idx, count);
+        const checkReturn = this.bridgeGame.retry(bridge, command, currentBridge, idx, this.count);
         if (checkReturn) {
-          [currentBridge, idx, count] = checkReturn;
-          this.readMoving(bridge, currentBridge, idx, count);
+          [currentBridge, idx, this.count] = checkReturn;
+          this.readMoving(bridge, currentBridge, idx);
         }
         else {
           Console.close();
