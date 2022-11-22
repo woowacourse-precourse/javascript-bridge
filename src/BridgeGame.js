b53b59fbf1fdbca6a25c4a4bc6d93a3c1ffa1a1d
suaJeong <studyingspacewhale@gmail.com>
2022-11-22 23:53:10 +0900

    bridge

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -7,6 +7,7 @@ class BridgeGame {
   #tryCount = 1;
   #up=[];
   #dow=[];
+  #result = 1 
 
 
 
@@ -26,7 +27,8 @@ class BridgeGame {
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   retry() {
-    this.#nowIndex
+    this.#nowIndex = 0
+    this.#result = 1
   }
 }
 
