5dad59b19d220d75566c45a728f3c120e3ffedb4
mjmj0217 <mjmj0217@naver.com>
2022-11-22 20:20:30 +0900

    fix(InputView): 에러 내용 전체를 출력하는 오류 수정

--- src/InputView.js
+++ src/InputView.js
@@ -4,9 +4,10 @@ const gameConst = require("./const");
 const InputView = {
   readBridgeSize(makeBridge) {
     Console.readLine(gameConst.process.INPUT_LENGTH_MESSAGE + "\n", (length) => {
-        try { makeBridge(Number(length)); } 
-        catch (err) {
-          Console.print(err);
+        try {
+          makeBridge(Number(length));
+        } catch (err) {
+          Console.print(err.message);
           this.readBridgeSize(makeBridge);
         }
       }
@@ -15,9 +16,10 @@ const InputView = {
 
   readMoving(move) {
     Console.readLine(gameConst.process.INPUT_CHOOSE_MESSAGE + "\n", (cmd) => {
-      try { move(cmd); } 
-      catch (err) {
-        Console.print(err);
+      try {
+        move(cmd);
+      } catch (err) {
+        Console.print(err.message);
         this.readMoving(move);
       }
     });
@@ -25,9 +27,10 @@ const InputView = {
 
   readGameCommand(makeDecision) {
     Console.readLine(gameConst.process.INPUT_RESART_MESSAGE + "\n", (cmd) => {
-      try {makeDecision(cmd); } 
-      catch (err) {
-        Console.print(err);
+      try {
+        makeDecision(cmd);
+      } catch (err) {
+        Console.print(err.message);
         this.readGameCommand(makeDecision);
       }
     });
