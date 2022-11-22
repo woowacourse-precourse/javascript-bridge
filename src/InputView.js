6cabdaced0a8de75bcb5bdd672d1adb1061a7e84
pitang <wpeffect@gmail.com>
2022-11-22 22:31:02 +0900

    feat: 다리의 길이 입력 함수

--- src/InputView.js
+++ src/InputView.js
@@ -1,7 +1,25 @@
+const { GAME_MESSAGES } = require("./Constants");
 const InputView = {
-  readBridgeSize() {},
-  readMoving() {},
-  readGameCommand() {},
+  size: "",
+  gameClient: "",
+
+  insertSize(size) {
+    this.size = Number(size);
+    this.gameClient = new BridgeGame(this.size);
+    this.readMoving();
+  },
+
+  readBridgeSize() {
+    Console.readLine(GAME_MESSAGES.ASK_TO_BRIDGE_LENGTH, (size) => {
+      try {
+        this.sizeValidate(size);
+        this.insertSize(size);
+      } catch (error) {
+        this.printInputSizeError(error);
+      }
+    });
+  },
+
 };
 
 module.exports = InputView;
