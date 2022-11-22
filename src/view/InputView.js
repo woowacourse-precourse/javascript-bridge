18cd492630a0a537a86f8002470aadef9f31d4ed
yesolyo <ahnyelim77@gmail.com>
2022-11-22 20:48:03 +0900

    refactor: 에러 메시지 발생 후 재진행

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,8 +1,6 @@
 const { Console } = require("@woowacourse/mission-utils");
+const { ERROR_MESSAGE } = require("../model/component");
 const { INPUT_MESSAGE } = require("../model/component");
-const LengthError = require("../error/LengthError");
-const CellError = require("../error/CellError");
-const RestartError = require("../error/RestartError");
 const BridgeMaker = require("../BridgeMaker");
 const BridgeGame = require("../controller/BridgeGame");
 const { generate } = require("../BridgeRandomNumberGenerator");
@@ -19,42 +17,45 @@ const InputView = {
   readBridgeSize() {
     console.log(INPUT_MESSAGE.START_MESSAGE);
     Console.readLine(INPUT_MESSAGE.BRIDGE_MESSAGE, (input) => {
-      new LengthError(input);
       this.lengthInput = input;
+      this.errorLength();
       this.computerNum = BridgeMaker.makeBridge(this.lengthInput, generate);
       this.readMoving();
     });
   },
+
+  errorLength() {
+    try {
+      if (!(this.lengthInput >= 3 && this.lengthInput <= 20)) {
+        throw ERROR_MESSAGE.LENGTH_ERROR;
+      }
+    } catch (e) {
+      Console.print(e);
+      this.readBridgeSize();
+    }
+  },
+
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving() {
     Console.readLine(INPUT_MESSAGE.CELL_MESSAGE, (input) => {
-      new CellError(input);
       this.cellInput = input;
+      this.errorCell();
       this.choiceRetry();
       printMap(this.result);
       this.tryLength();
     });
   },
 
-  choiceRetry() {
-    const birdgeGame = new BridgeGame();
-    if (this.restart !== "R") {
-      this.result = birdgeGame.move(
-        this.cellInput,
-        this.computerNum,
-        this.count
-      );
-    }
-    if (this.restart === "R") {
-      this.count = 0;
-      this.result = birdgeGame.retry(
-        this.cellInput,
-        this.computerNum,
-        this.count
-      );
-      this.restart = null;
+  errorCell() {
+    try {
+      if (!(this.cellInput === "U" || this.cellInput === "D")) {
+        throw ERROR_MESSAGE.CELL_ERROR_MESSAGE;
+      }
+    } catch (e) {
+      Console.print(e);
+      this.readMoving();
     }
   },
 
@@ -98,11 +99,22 @@ const InputView = {
    */
   readGameCommand() {
     Console.readLine(INPUT_MESSAGE.RESTART_MESSAGE, (input) => {
-      new RestartError(input);
       this.restart = input;
+      this.errorRestart();
       this.choiceRestart();
       this.choiceEnd();
     });
+  },
+
+  errorRestart() {
+    try {
+      if (!(this.restart === "R" || this.restart === "Q")) {
+        throw ERROR_MESSAGE.RESTART_ERROR_MESSAGE;
+      }
+    } catch (e) {
+      Console.print(e);
+      this.readGameCommand();
+    }
   },
 
   choiceRestart() {
@@ -117,6 +129,26 @@ const InputView = {
       this.result[2] = "실패";
       this.result[3] = this.startCount;
       printResult(this.result);
+    }
+  },
+
+  choiceRetry() {
+    const birdgeGame = new BridgeGame();
+    if (this.restart !== "R") {
+      this.result = birdgeGame.move(
+        this.cellInput,
+        this.computerNum,
+        this.count
+      );
+    }
+    if (this.restart === "R") {
+      this.count = 0;
+      this.result = birdgeGame.retry(
+        this.cellInput,
+        this.computerNum,
+        this.count
+      );
+      this.restart = null;
     }
   },
 };
