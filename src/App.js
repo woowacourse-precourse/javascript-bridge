4e62bc5c797c47468f0355953d00235c0242e2b0
Sanggil Han <tkdrlf95@naver.com>
2022-11-22 20:26:45 +0900

    feat(App): Implement setting bridge's size
    다리의 길이를 정하는 부분을 구현하였음

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+const InputView = require('./InputView');
+const OutputView = require('./OutputView');
+const BridgeGame = require('./BridgeGame');
+const Validation = require('./Validation');
+
 class App {
-  play() {}
+  #bridgeGame;
+
+  play() {
+    OutputView.startGame();
+    this.#readBridgeSize();
+  }
+
+  #readBridgeSize() {
+    const bridgeSize = InputView.readBridgeSize();
+    try {
+      Validation.isValidBridgeSize(bridgeSize);
+      this.#bridgeGame = new BridgeGame(parseInt(bridgeSize));
+    } catch (error) {
+      OutputView.printError(error.message);
+      this.#readBridgeSize();
+    }
+  }
 }
 
 module.exports = App;
