d7d556f86bebeea094ca4474637f1123dc1041a0
cmun2 <cmun2@illinois.edu>
2022-11-22 23:37:39 +0900

    feat: 게임 결과 출력 함수 추가

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -21,7 +21,13 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() {},
+  printResult(map, isSuccess, count) {
+    Console.print(OUTPUT_MESSAGE.END);
+    Console.print(OUTPUT_MESSAGE.MAP(map[0], map[1]));
+    Console.print(OUTPUT_MESSAGE.IS_SUCCESS(isSuccess));
+    Console.print(OUTPUT_MESSAGE.COUNT(count));
+    Console.close();
+  },
   printError(error) {
     Console.print(error);
   },
