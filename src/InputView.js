c9f794dd9d8f3d31375d1b91cc5d8bb56299f975
HelloHailie <alsltjsals@gmail.com>
2022-11-22 21:08:03 +0900

    feat:이동할 칸 입력받기

--- src/InputView.js
+++ src/InputView.js
@@ -33,7 +33,19 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    Console.readLine(
+      "\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
+      (move) => {
+        try {
+          InputView.validateMoving(move);
+        } catch (error) {
+          Console.print(error.message);
+          InputView.readMoving(bridgeGame);
+        }
+      }
+    );
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
