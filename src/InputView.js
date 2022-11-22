6a19ea27a95848e70c91f222a89076f09ac7b13a
ho-ji <wkdd0205@gmail.com>
2022-11-22 20:34:13 +0900

    feat: Add readMoving() in InputView
    사용자가 이동할 칸을 입력하는 함수를 구현함

--- src/InputView.js
+++ src/InputView.js
@@ -17,7 +17,14 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    let moving;
+    MissionUtils.Console.readLine("이동할 칸을 선택해주세요. (위: U, 아래: D)", (answer) => {
+      moving = answer;
+    });
+    this.checkMoving(moving);
+    return moving;
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
