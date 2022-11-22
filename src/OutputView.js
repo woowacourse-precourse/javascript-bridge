217d5ec4849301c8df1006521bcab6eabfed5da4
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-22 21:14:36 +0900

    fest(OutputView): printMap 메서드 수정

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,3 +1,5 @@
+const {Console} = require('@woowacourse/mission-utils');
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -7,14 +9,38 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {},
+  printMap(bridgeGame) {
+    const [upside, downside] = bridgeGame.getMap();
+
+    Console.print(this.convertMapForm(upside));
+    Console.print(this.convertMapForm(downside));
+  },
+
+  convertMapForm(mapRow) {
+    const mapForm = mapRow.reduce((form, check, index) => {
+      const preFix = index > 0 ? '|' : '';
+      form += `${preFix} ${check} `;
+
+      return form;
+    }, '');
+
+    return `[${mapForm}]`;
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() {},
+  printResult(bridgeGame) {
+    const result = bridgeGame.isSuccess() ? '성공' : '실패';
+    const count = bridgeGame.getChallengeCount();
+
+    Console.print('최종 게임 결과');
+    this.printMap(bridgeGame);
+    Console.print(`게임 성공 여부: ${result}`);
+    Console.print(`총 시도한 횟수: ${count}`);
+  },
 };
 
 module.exports = OutputView;
