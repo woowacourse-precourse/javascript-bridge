41a63e4e7e13bcab450858c730fdff5bc0473005
youzysu <111998760+youzysu@users.noreply.github.com>
2022-11-22 11:34:10 +0900

    style: 함수 순서 변경, 주석 삭제

--- src/Model/BridgeGame.js
+++ src/Model/BridgeGame.js
@@ -20,10 +20,6 @@ class BridgeGame {
     this.#bridge = BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate);
   }
 
-  getBridge() {
-    return this.#bridge;
-  }
-
   move(direction) {
     const nextBlock = this.#upperTrack.length;
     if (direction === BLOCK.LOWER) return this.moveDown(direction, nextBlock);
@@ -42,14 +38,6 @@ class BridgeGame {
     if (direction !== this.#bridge[nextBlock]) this.#lowerTrack.push(PATH.WRONG);
   }
 
-  getUpperTrack() {
-    return this.#upperTrack;
-  }
-
-  getLowerTrack() {
-    return this.#lowerTrack;
-  }
-
   isWrongPath() {
     const currentLocation = this.#upperTrack.length - 1;
     const lastPath = [this.#upperTrack[currentLocation], this.#lowerTrack[currentLocation]];
@@ -63,15 +51,22 @@ class BridgeGame {
     return this.#success;
   }
 
-  /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
   retry() {
     this.#trial += 1;
     this.#upperTrack = [];
     this.#lowerTrack = [];
+  }
+
+  getBridge() {
+    return this.#bridge;
+  }
+
+  getUpperTrack() {
+    return this.#upperTrack;
+  }
+
+  getLowerTrack() {
+    return this.#lowerTrack;
   }
 
   getTrialCount() {
