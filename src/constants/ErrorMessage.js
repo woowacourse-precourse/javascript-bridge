8c45cffea80e3411a91906322adba535bc827c39
sunny <huiseon37@gmail.com>
2022-11-22 20:23:30 +0900

    chore: 에러 메시지 수정

--- src/constants/ErrorMessage.js
+++ src/constants/ErrorMessage.js
@@ -1,5 +1,5 @@
 const ERROR_MESSAGE = {
-  BRIDGE_LENGTH: '[ERROR] 다리 길이는 3 ~ 20 사이의 숫자여야 합니다.',
+  BRIDGE_LENGTH: '[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.',
   INPUT_MOVING: '[ERROR] 이동 칸은 U 혹은 D 입니다.',
   INPUT_RETRY: '[ERROR] 재시도 입력은 R 혹은 Q 입니다.'
 }
