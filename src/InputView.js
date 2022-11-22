115cc0c921ce817ed70a379a5a0192d37f6c5085
soomin <qkrtnals970804@gmail.com>
2022-11-22 21:20:27 +0900

    feat: readMoving 기능 함수 구현하기

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,4 @@
+const { readLine } = require('./Utils');
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -10,7 +11,19 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving(game) {
+    readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)', type => {
+      const IS_UP = type === 'U';
+      const IS_DOWN = type === 'D';
+
+      if (IS_UP || IS_DOWN) {
+        game.move(type);
+        readMoving(move);
+      } else {
+        throw new Error('U, D 만 입력 가능 합니다.');
+      }
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
