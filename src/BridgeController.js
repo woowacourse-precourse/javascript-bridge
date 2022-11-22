3ddce14d1f1397e4e5d2a8c9b9e1c0d57bc92040
Moyujin <scab12341@gmail.com>
2022-11-22 20:39:49 +0900

    feat: 다리 생성

--- src/BridgeController.js
+++ src/BridgeController.js
@@ -1,18 +1,32 @@
+const BridgeGame = require('./BridgeGame');
+const BridgeMaker = require('./BridgeMaker');
 const InputView = require('./InputView');
 const OutputView = require('./OutputView');
 const Validate = require('./Validate');
 
 class bridgeController {
-  constructor() {}
+  #bridge;
+  constructor() {
+    this.#bridge = new BridgeGame();
+  }
 
   run() {
     OutputView.printInit();
-    this.inputSize();
     InputView.readBridgeSize(this.inputSize.bind(this));
   }
 
   inputSize(input) {
     Validate.isNumber(input);
+    this.#bridge.setBridge();
+    this.getMoveInput();
+  }
+
+  getMoveInput() {
+    InputView.readMoving(this.inputMoveSpace.bind(this));
+  }
+
+  inputMoveSpace(input) {
+    Validate.isMoveInput(input);
   }
 }
 
