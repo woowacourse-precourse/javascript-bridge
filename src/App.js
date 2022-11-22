b24be54b712fa9d0c51e9b5b8b8d6cad445ca5b5
HelloHailie <alsltjsals@gmail.com>
2022-11-22 20:55:05 +0900

    feat:게임 시작 알리기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const BridgeGame = require("./BridgeGame");
+const { readBridgeSize } = require("./InputView");
+const { Console } = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    Console.print("다리 건너기 게임을 시작합니다.\n");
+    const bridgeGame = new BridgeGame();
+    readBridgeSize(bridgeGame);
+  }
 }
 
 module.exports = App;
