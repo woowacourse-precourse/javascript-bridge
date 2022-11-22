1fa9012e6d65db2a77750782d8809583e6957f3e
yesolyo <ahnyelim77@gmail.com>
2022-11-22 15:06:39 +0900

    refactor: 코드 순서 변경

--- src/controller/BridgeGame.js
+++ src/controller/BridgeGame.js
@@ -8,12 +8,18 @@ class BridgeGame {
     this.computer = computer;
     this.count = count;
   }
-  /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
+
   move(upDown, computer, count) {
+    this.upDown = upDown;
+    this.computer = computer;
+    this.count = count;
+    this.saveMove(upDown, computer, count);
+    return [resultUp, resultDown];
+  }
+
+  retry(upDown, computer, count) {
+    resultUp = [];
+    resultDown = [];
     this.upDown = upDown;
     this.computer = computer;
     this.count = count;
@@ -54,21 +60,6 @@ class BridgeGame {
       resultUp.push(" ");
       resultDown.push(CELL.ONE);
     }
-  }
-
-  /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  retry(upDown, computer, count) {
-    resultUp = [];
-    resultDown = [];
-    this.upDown = upDown;
-    this.computer = computer;
-    this.count = count;
-    this.saveMove();
-    return [resultUp, resultDown];
   }
 }
 
