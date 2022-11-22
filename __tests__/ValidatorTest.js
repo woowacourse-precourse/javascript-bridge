8f529a28e5abe24b3d8cba61970101052fd18513
doheeMac <thsehgml37@korea.ac.kr>
2022-11-22 22:04:16 +0900

    refractor: validator test

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -9,12 +9,13 @@ const temporaryCallback = () => {
 
 describe("Validator 테스트", () => {
   test("자연수 validatation 테스트", () => {
-    const RESULT = Constant.NATURAL_VALIDATION;
-    for (let index = 0; index < TEST_ELEMENT.length; index++) {
-      expect(Validator.isNaturalNumber(TEST_ELEMENT[index])).toEqual(
-        RESULT[index]
-      );
-    }
+    //const RESULT = Constant.NATURAL_VALIDATION;
+
+    expect(Validator.isNaturalNumber(0)).toEqual(false);
+    // for (let index = 0; index < TEST_ELEMENT.length; index++) {
+    //   expect(Validator.isNaturalNumber(TEST_ELEMENT[index])).toEqual(
+    //     RESULT[index]
+    //   );
   });
 
   // test("다리 길이 validatation 테스트", () => {
@@ -28,7 +29,7 @@ describe("Validator 테스트", () => {
 
   // test("다리 생성 0/1 validatation 테스트", () => {
   //   const RESULT = Constant.BOOL_NUMBER_VALIDATION;
-  //   for (let index = 0; index < TEST_ELEMENT.length; index++) {
+  //   for (let index = 0; index < TEST_ELEMENT.length; index++) {ß
   //     expect(
   //       Validator.isBoolNumber(temporaryCallback, TEST_ELEMENT[index])
   //     ).toEqual(RESULT[index]);
