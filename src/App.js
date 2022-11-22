8dffd26c1fee23e1e1ab5391085a3f69dafec8bf
jinyong <zhaqh146@naver.com>
2022-11-22 19:50:12 +0900

    docs: 완료한 기능 목록 체크

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 const InputView = require('./InputView');
 const OutputView = require('./OutputView');
 const Validation = require('./Validation');
+const { makeBrdige } = require('./BridgeMaker');
+const { generate } = require('./BridgeRandomNumberGenerator');
 
 class App {
+
+  constructor() {
+    this.count = 1;
+    this.bridge = [];
+  }
+
   play() {
     InputView.readBridgeSize(this.readBridgeSizeCallBack.bind(this));
   };
@@ -10,6 +18,7 @@ class App {
   readBridgeSizeCallBack(userInput) {
     try {
       Validation.bridgeSize(userInput);
+      this.bridge = makeBrdige(userInput, generate);
       InputView.readMoving(this.readMovingCallBack.bind(this));
     } catch (error) {
       OutputView.printError(error);
