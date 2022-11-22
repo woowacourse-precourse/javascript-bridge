1e8c20f5907d8729a533ba0117c359b57a165775
violetwhenisawu <anashape710@gmail.com>
2022-11-22 11:03:07 +0900

    style: 불필요한 주석 제거

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,26 +1,15 @@
 const { Console } = require('@woowacourse/mission-utils');
 const { INPUT_MESSAGE } = require('../utils/gameMessage');
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+
 const InputView = {
-  /**
-   * 다리의 길이를 입력받는다.
-   */
   readBridgeSize(callback) {
     Console.readLine(INPUT_MESSAGE.GET_BRIDGE_LENGTH, callback);
   },
 
-  /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
   readMoving(callback) {
     Console.readLine(INPUT_MESSAGE.SELECT_MOVE, callback);
   },
 
-  /**
-   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-   */
   readGameCommand(callback) {
     Console.readLine(INPUT_MESSAGE.SELECT_RETRY, callback);
   },
