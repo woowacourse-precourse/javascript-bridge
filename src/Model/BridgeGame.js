baa6fdd7c814b8b0b5e6d3983e5b8a281c672664
ming0830 <minsun0830@naver.com>
2022-11-22 15:12:12 +0900

    feat : 시도한 횟수 초기값 1로 세팅

--- src/Model/BridgeGame.js
+++ src/Model/BridgeGame.js
@@ -5,6 +5,7 @@
 class BridgeGame {
   constructor(){
     this.gameCount = 0;
+    this.retryCount = 1;
     this.upList = [];
     this.downList = [];
     this.RIGHT = "O";
@@ -26,9 +27,10 @@ class BridgeGame {
     }
     if(moveInput !== answer[this.gameCount]){
       this.ingMove(this.WRONH,moveInput)
-      return false
+      return false;
     }
   }
+
   ingMove(goOrStop,moveInput){
     if(moveInput === "U"){
       this.upList.push(` ${goOrStop} `);
@@ -38,9 +40,7 @@ class BridgeGame {
       this.downList.push(` ${goOrStop} `);
       this.upList.push('   ')
     }
-    
   }
-  
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
@@ -48,6 +48,7 @@ class BridgeGame {
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   retry() {
+    this.retryCount += 1;
     this.gameCount = 0;
     this.upList = [];
     this.downList = [];
