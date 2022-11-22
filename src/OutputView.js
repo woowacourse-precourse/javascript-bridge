11415c4354a485b65df2aeeb3eed998355aa6f71
dongyeopca <ckehdduq95@gmail.com>
2022-11-22 17:13:34 +0900

    style(OutputView): 사용하지 않는 모듈 임포트문 제거

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,5 +1,4 @@
 const { Console } = require('@woowacourse/mission-utils');
-const InputView = require('./InputView');
 const MESSAGE = require('../constants/Message');
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
