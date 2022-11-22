700a5b0d526cd893660d28ed42128696eb1a47f6
이여진 <jinspace3710@gmail.com>
2022-11-22 18:34:54 +0900

    feat: 다리 생성 구현

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 const InputView = require('./InputView');
 const OutputView = require('./OutputView');
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
+const BridgeMaker = require('./BridgeMaker');
 
 class App {
   constructor() {
     this.bridgeSize;
+    this.bridge = [];
   }
 
   play() {
     OutputView.printGameStart();
     this.bridgeSize = InputView.getBridgeSize();
+    this.bridge = BridgeMaker.makeBridge(
+      this.bridgeSize,
+      BridgeRandomNumberGenerator.generate
+    );
   }
 }
 
