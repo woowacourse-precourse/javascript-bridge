a1d0dd4ac92cb2927f76c3d9e33d344deca504be
Minju Park <project.mizzu@gmail.com>
2022-11-22 19:55:42 +0900

    Feat : 무작위 숫자 값을 문자로 변환
    - 입력받은 다리 길이만큼 각 라운드 당 0과 1중 무작위 값을 뽑는다.
    - 무작위 값이 0인 경우 "D", 1인 경우 "U"가 정답이 된다.

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,3 +1,5 @@
+const { Console } = require('@woowacourse/mission-utils');
+const { BRIDGE_INFO } = require('./constants/BridgeGameSetting');
 const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -8,7 +10,23 @@ const BridgeMaker = {
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
-  makeBridge(size, generateRandomNumber) {},
+  makeBridge(size) {
+    const result = [];
+    for (let i = 0; i < size; i += 1) {
+      result.push(BridgeRandomNumberGenerator.generate());
+    }
+    result.forEach((number, idx) => {
+      if (number === BRIDGE_INFO.RANDOM_UP) {
+        result[idx] = BRIDGE_INFO.SELECT_UP;
+      }
+      if (number === BRIDGE_INFO.RANDOM_DOWN) {
+        result[idx] = BRIDGE_INFO.SELECT_DOWN;
+      }
+      return result;
+    });
+  },
 };
+
+Console.print(BridgeMaker.makeBridge(5));
 
 module.exports = BridgeMaker;
