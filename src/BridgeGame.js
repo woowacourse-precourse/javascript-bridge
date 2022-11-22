9f4d49626bc47d40849e8e72f85f1cddf06bbb2a
ksg2388 <ksg2388@naver.com>
2022-11-22 17:14:43 +0900

    feat(OutputView): 이동 결과 출력

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,7 +1,9 @@
+const { MissionUtils } = require('@woowacourse/mission-utils');
 const BridgeMaker = require('./BridgeMaker');
 const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
 const ValidateCheck = require('./utils/ValidatateCheck');
 const InputView = require('./Views/InputView');
+const { printMap } = require('./Views/OutputView');
 const OutputView = require('./Views/OutputView');
 
 /**
@@ -10,9 +12,11 @@ const OutputView = require('./Views/OutputView');
 class BridgeGame {
   #length = 0;
   #bridge = [];
+  #moveList = [];
   constructor() {
     this.#length = this.#length;
     this.#bridge = this.#bridge;
+    this.#moveList = this.#moveList;
   }
 
   /**
@@ -27,8 +31,12 @@ class BridgeGame {
         this.#length,
         BridgeRandomNumberGenerator.generate,
       );
+
+      OutputView.printTest(this.#bridge); // 테스트용 코드
+
       InputView.readMoving(input => {
         ValidateCheck.moveMessageCheck(input);
+        this.move(input);
       });
     });
   }
@@ -38,7 +46,20 @@ class BridgeGame {
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(message) {
+    this.#moveList.push(message);
+    if (message === this.#bridge[this.#moveList.length - 1]) {
+      printMap(this.#bridge, this.#moveList);
+      InputView.readMoving(input => {
+        ValidateCheck.moveMessageCheck(input);
+        this.move(input);
+      });
+    }
+
+    if (message !== this.#bridge[this.#moveList.length - 1]) {
+      printMap(this.#bridge, this.#moveList);
+    }
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
