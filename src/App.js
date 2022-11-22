b14a324ba59a61d05a583b02681f560f185774c0
rovxxmjxx <miju3545@gmail.com>
2022-11-22 22:16:35 +0900

    feat(App): 사용자 값 InputView 를 가져오는 논리는 세 경우 모두 동일하여 한번에 동일 논리 구조로 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 const OutputView = require('./OutputView');
+const InputView = require('./InputView');
 const { MESSAGE } = require('./constants');
 
 class App {
   play = async () => {
     OutputView.printMessage(MESSAGE.ENTRY);
+  };
+
+  getBridgeSize = async () => {
+    try {
+      return await InputView.readBridgeSize();
+    } catch (error) {
+      OutputView.printMessage(error.message);
+      return this.getBridgeSize();
+    }
+  };
+
+  getDirection = async () => {
+    try {
+      return await InputView.readMoving();
+    } catch (error) {
+      OutputView.printMessage(error.message);
+      return this.getDirection();
+    }
+  };
+
+  getGameCommand = async () => {
+    try {
+      return await InputView.readGameCommand();
+    } catch (error) {
+      OutputView.printMessage(error.message);
+      this.getGameCommand();
+    }
   };
 }
 
