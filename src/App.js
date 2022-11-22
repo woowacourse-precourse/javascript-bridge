003581567c43205c55abeb68098bfad39ddb999c
s0ojin <onepst@hanyang.ac.kr>
2022-11-22 21:36:44 +0900

    feat(App.js) : makeBridge로 생성한 다리를 BridgeGame으로 보내며 게임 생성하는 makeBridgeGame 메서드 작성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 const OutputView = require('./view/OutputView');
-
+const InputView = require('./view/InputView');
+const BridgeGame = require('./BridgeGame');
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
+const BridgeMaker = require('./BridgeMaker');
 class App {
+  bidgeGame;
+
+  constructor() {
+    this.game = new BridgeGame();
+  }
+
   play() {
     OutputView.printGreeting();
+    this.makeBridgeGame();
+  }
+
+  makeBridgeGame() {
+    const bridge = BridgeMaker.makeBridge(
+      InputView.readBridgeSize(),
+      BridgeRandomNumberGenerator.generate(),
+    );
+    return new BridgeGame(bridge);
   }
 }
 
