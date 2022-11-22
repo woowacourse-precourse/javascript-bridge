22e2d8a2c5db19a4eaafdf42a01489e9c60518d9
Buyoung Chung <sunarol@naver.com>
2022-11-22 20:42:33 +0900

    feat: Object Message has error messages and ExceptionHandler use them

--- src/utils/ExceptionHandler.js
+++ src/utils/ExceptionHandler.js
@@ -1,28 +1,29 @@
 const Constant = require("./Constant");
+const Message = require("./Message");
 
 const ExceptionHandler = {
   isNotaNumber(input) {
     if (isNaN(input)) {
-      throw "\n[ERROR] 숫자를 입력해주세요.\n";
+      throw Message.ISNAN_ERROR;
     }
   },
 
   validateSizeInput(size) {
     const intSize = parseInt(size);
     if (intSize < Constant.MIN_SIZE || intSize > Constant.MAX_SIZE) {
-      throw "\n[ERROR] 3부터 20 사이의 숫자를 입력해주세요.\n";
+      throw Message.SIZE_RANGE_ERROR;
     }
   },
 
   validateMoveInput(input) {
     if (input !== Constant.UP && input !== Constant.DOWN) {
-      throw "\n[ERROR] 대문자 U, D를 정확히 입력해주세요.\n";
+      throw Message.MOVE_INPUT_ERROR;
     }
   },
 
   validateQuitOrRetryInput(input) {
     if (input !== Constant.QUIT && input !== Constant.RETRY) {
-      throw "\n[ERROR] 대문자 Q, R을 정확히 입력해주세요.\n";
+      throw Message.QUIT_OR_RETRY_INPUT_ERROR;
     }
   },
 };
--- src/utils/Message.js
+++ src/utils/Message.js
@@ -5,6 +5,10 @@ const Message = {
   ASK_REPLAY_CLOSE: "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n",
   RESULT: "\n최종 게임 결과",
 
+  ISNAN_ERROR: "\n[ERROR] 숫자를 입력해주세요.\n",
+  SIZE_RANGE_ERROR: "\n[ERROR] 3부터 20 사이의 숫자를 입력해주세요.\n",
+  MOVE_INPUT_ERROR: "\n[ERROR] 대문자 U, D를 정확히 입력해주세요.\n",
+  QUIT_OR_RETRY_INPUT_ERROR: "\n[ERROR] 대문자 Q, R을 정확히 입력해주세요.\n",
   returnResultMessage(bool) {
     if (bool) {
       return `\n게임 성공 여부: 실패`;
