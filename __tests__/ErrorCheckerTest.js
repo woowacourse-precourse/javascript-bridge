ea0051d48bf0d6bcf9b13ac05d503860276d241b
yeongseokim <1903020@ewhain.net>
2022-11-22 15:23:43 +0900

    test(ErrorCheckerTest): checkValidChar 에러 체크 테스트 추가

--- __tests__/ErrorCheckerTest.js
+++ __tests__/ErrorCheckerTest.js
@@ -1,4 +1,4 @@
-const { ERROR_MESSAGE } = require("../src/constants");
+const { ERROR_MESSAGE, VALID_CHAR } = require("../src/constants");
 const ErrorChecker = require("../src/ErrorChecker");
 
 describe("ErrorChecker 테스트", () => {
@@ -20,5 +20,35 @@ describe("ErrorChecker 테스트", () => {
         bridgeSizeInputs.map((input) => {
             expect(() => { ErrorChecker.checkBridgeSizeValidation(input) }).toBeTruthy();
         })
-    })
+    });
+
+    test("checkValidChar(U,D): 비정상 input에 대한 throw 테스트", () => {
+        const bridgeSizeInputs = ["r", "q", "UD", "3"];
+        const message = ERROR_MESSAGE.NOT_A_VALID_CHAR(VALID_CHAR.UP, VALID_CHAR.DOWN);
+        bridgeSizeInputs.map((input) => {
+            expect(() => { ErrorChecker.checkValidChar(VALID_CHAR.UP, VALID_CHAR.DOWN, input) }).toThrow(message);
+        })
+    });
+
+    test("checkValidChar(U,D): 정상 input U, D에 대한 true 반환 테스트", () => {
+        const bridgeSizeInputs = ["D", "U"];
+        bridgeSizeInputs.map((input) => {
+            expect(() => { ErrorChecker.checkValidChar(VALID_CHAR.UP, VALID_CHAR.DOWN, input) }).toBeTruthy();
+        })
+    });
+
+    test("checkValidChar(R,Q): 비정상 input에 대한 throw 테스트", () => {
+        const bridgeSizeInputs = ["u", "d", "RQ", "3"];
+        const message = ERROR_MESSAGE.NOT_A_VALID_CHAR(VALID_CHAR.REPLAY, VALID_CHAR.QUIT);
+        bridgeSizeInputs.map((input) => {
+            expect(() => { ErrorChecker.checkValidChar(VALID_CHAR.REPLAY, VALID_CHAR.QUIT, input) }).toThrow(message);
+        })
+    });
+
+    test("checkValidChar(R,Q): 정상 input R, Q에 대한 true 반환 테스트", () => {
+        const bridgeSizeInputs = ["R", "Q"];
+        bridgeSizeInputs.map((input) => {
+            expect(() => { ErrorChecker.checkValidChar(VALID_CHAR.REPLAY, VALID_CHAR.QUIT, input) }).toBeTruthy();
+        })
+    });
 })
