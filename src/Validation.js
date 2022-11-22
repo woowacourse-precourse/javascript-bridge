143ab3ab16a77023255b06e48804edae97eb1bc2
yuna Kim <yuna000804@gmail.com>
2022-11-22 15:00:52 +0900

    refactor : 상수 만들고 이름 부여

--- src/Validation.js
+++ src/Validation.js
@@ -1,9 +1,19 @@
-const { ERROR } = require("./Messages");
+const { ERROR, RETRY } = require("./Messages");
 
+const CONSTANTS = {
+  BRIDGE_MIN_LENGTH: 3,
+  BRIDGE_MAX_LENGTH: 20,
+  UP: 'U',
+  DOWN: 'D',
+  RETRY: 'R',
+  QUIT: 'Q',
+  INPUT_LENGTH: 1,
+}
+  
 const Validation = {
 
   validateBridgeSize(size) {
-    if(size < 3 || size > 20) {
+    if(size < CONSTANTS.BRIDGE_MIN_LENGTH || size > CONSTANTS.BRIDGE_MAX_LENGTH) {
       throw new Error(ERROR.BRIDGE_SIZE_LENGTH_ERROR);
     };
     if(isNaN(size)) {
@@ -12,19 +22,19 @@ const Validation = {
   },
 
   validateUserMove(upOrDown) {
-    if(!['U', 'D'].includes(upOrDown)) {
+    if(![CONSTANTS.UP, CONSTANTS.DOWN].includes(upOrDown)) {
       throw new Error(ERROR.BRIDGE_MOVE_INPUT_ERROR);
     }
-    if(upOrDown.length !== 1) {
+    if(upOrDown.length !== CONSTANTS.INPUT_LENGTH) {
       throw new Error(ERROR.BRIDGE_MOVE_INPUT_LENGTH_ERROR);
     }
   },
   
   validateGameCommand(retryOrQuit) {
-    if(retryOrQuit.length !== 1) {
+    if(retryOrQuit.length !== CONSTANTS.INPUT_LENGTH) {
       throw new Error(ERROR.GAME_COMMAND_INPUT_LENGTH_ERROR);
     }
-    if(!['R', 'Q'].includes(retryOrQuit)) {
+    if(![CONSTANTS.RETRY, CONSTANTS.QUIT].includes(retryOrQuit)) {
       throw new Error(ERROR.GAME_COMMAND_INPUT_ERROR);
     }
   },
