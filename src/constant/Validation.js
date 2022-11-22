49c7bc81c5f11a4700924955f7338ff8c24e9c5c
semnil5202 <semnil5202@gmail.com>
2022-11-22 21:47:50 +0900

    refactor: 하드 코딩 수정

--- src/constant/Validation.js
+++ src/constant/Validation.js
@@ -1,18 +1,20 @@
-const { UTIL } = require('./constant');
+const { UTIL, ERROR } = require('./constant');
 
 const Validation = {
   validateSize(input) {
-    if (Number.isNaN(input)) throw new Error('[ERROR]');
-    if (!Number.isInteger(input)) throw new Error('[ERROR]');
-    if (input < 3 || input > 20) throw new Error('[ERROR]');
+    if (Number.isNaN(input)) throw new Error(ERROR.SIZE_NUMBER);
+    if (!Number.isInteger(input)) throw new Error(ERROR.SIZE_INTEGER);
+    if (input < UTIL.MIN || input > UTIL.MAX) throw new Error(ERROR.SIZE_RANGE);
   },
 
   validateMove(input) {
-    if (input !== UTIL.UP && input !== UTIL.DOWN) throw new Error('[ERROR]');
+    if (input !== UTIL.UP && input !== UTIL.DOWN) throw new Error(ERROR.MOVE);
   },
 
   validateReGame(input) {
-    if (input !== UTIL.RETRY && input !== UTIL.QUIT) throw new Error('[ERROR]');
+    if (input !== UTIL.RETRY && input !== UTIL.QUIT) {
+      throw new Error(ERROR.REGAME);
+    }
   },
 };
 
