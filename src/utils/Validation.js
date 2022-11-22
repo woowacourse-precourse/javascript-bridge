b0054acbc84c2bb317c2b736ca40dc5bcacc5de4
Kim jinwoo <49385170+Sunny-jinn@users.noreply.github.com>
2022-11-22 21:52:22 +0900

    refactor(Validation): fix error messages

--- src/utils/Validation.js
+++ src/utils/Validation.js
@@ -5,10 +5,11 @@ const Validation = {
   },
 
   isNotNumber(input) {
-    if (isNaN(input)) throw new Error("[ERROR]");
+    if (isNaN(input)) throw new Error("[ERROR] 길이는 숫자만 입력 가능합니다.");
   },
   isInRange(input) {
-    if (input < 3 || input > 20) throw new Error("[ERROR]");
+    if (input < 3 || input > 20)
+      throw new Error("[ERROR] 길이는 3이상 20이하여야 합니다.");
   },
 };
 
