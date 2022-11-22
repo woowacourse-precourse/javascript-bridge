adf12f0f8718cf8f9db741bb2f55cdbaff5cd4d5
spsp8426 <spsp8426@naver.com>
2022-11-22 10:01:07 +0900

    feat : 예외처리 부분 파일 추가

---
+++ src/Validation.js
@@ -0,0 +1,27 @@
+const Validation = {
+  checkBridgeSize(size) {
+    if (isNaN(size))
+      return { errorMsg: new Error('\n[ERROR] 숫자만 입력할 수 있습니다.\n') };
+
+    if (size < 3 || size > 20)
+      return {
+        errorMsg:new Error('\n[ERROR] 3 이상 20 이하의 숫자만 입력할 수 있습니다.\n')
+      };
+
+    return { errorMsg: undefined };
+  },
+
+  checkDirection(direction) {
+    if (direction === 'U' || direction === 'D') return { errorMsg: undefined };
+
+    return { errorMsg: new Error('\n[ERROR] U 또는 D만 입력할 수 있습니다.') };
+  },
+
+  checkCommandOption(commandOption) {
+    if (commandOption === 'R' || commandOption === 'Q') return { errorMsg: undefined };
+
+    return { errorMsg: new Error('\n[ERROR] R 또는 Q만 입력할 수 있습니다.') };
+  },
+};
+
+module.exports = Validation;
