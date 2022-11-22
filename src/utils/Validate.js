8a7917daf46ce98261885297c19a6031e73acc3a
minseok1109 <park485201@naver.com>
2022-11-22 16:16:33 +0900

    test: 사용자 입력에 대한 테스트 코드 작성

--- src/utils/Validate.js
+++ src/utils/Validate.js
@@ -2,6 +2,10 @@ const Constant = require("./Constant");
 class Validate {
   //다리 크기 입력검증
   static validateBridgeSize(bridgeSize) {
+    if (String(bridgeSize).length > 2) {
+      throw new Error(Constant.ERROR.ONLY_ONE_NUMBER);
+    }
+
     if (isNaN(bridgeSize)) {
       throw new Error(Constant.ERROR.ONLY_NUMBER);
     }
@@ -11,23 +15,21 @@ class Validate {
     ) {
       throw new Error(Constant.ERROR.BRIDGE_SIZE);
     }
-    if (String(bridgeSize).length > 2) {
-      throw new Error(Constant.ERROR.ONLY_ONE_NUMBER);
-    }
   }
 
   //사용자 움직일 칸에 대한 입력 유효성 검사
   static validateUserInputMove(userInput) {
     let regexOnlyUorD = /[UD]/g;
-    if (!regexOnlyUorD.test(userInput)) {
-      throw new Error(Constant.ERROR.ONLY_U_OR_D);
-    }
     if (
       userInput === Constant.UP.toLowerCase() ||
       userInput === Constant.DOWN.toLowerCase()
     ) {
       throw new Error(Constant.ERROR.ONLY_UPPERCASE);
     }
+    if (!regexOnlyUorD.test(userInput)) {
+      throw new Error(Constant.ERROR.ONLY_U_OR_D);
+    }
+
     if (userInput.length >= 2) {
       throw new Error(Constant.ERROR.ONLY_INPUT_ONE);
     }
@@ -36,15 +38,16 @@ class Validate {
   //게임 다시 시작할 지 입력할 때 검증
   static validateUserInputRetry(inputRetry) {
     let regexOnlyRorD = /[RQ]/g;
-    if (!regexOnlyRorD.test(inputRetry)) {
-      throw new Error(Constant.ERROR.ONLY_R_OR_Q);
-    }
     if (
       inputRetry === Constant.RETRY.toLowerCase() ||
       inputRetry === Constant.QUIT.toLowerCase()
     ) {
       throw new Error(Constant.ERROR.ONLY_UPPERCASE);
     }
+    if (!regexOnlyRorD.test(inputRetry)) {
+      throw new Error(Constant.ERROR.ONLY_R_OR_Q);
+    }
+
     if (inputRetry.length >= 2) {
       throw new Error(Constant.ERROR.ONLY_INPUT_ONE);
     }
