34468ef5fbf1700b23f5eb0ecddc750ae6a7b72c
Steadystudy <sgmin206@naver.com>
2022-11-22 15:12:28 +0900

    delete: 주석 제거

--- src/InputView.js
+++ src/InputView.js
@@ -7,19 +7,9 @@ const Validate = require('./Validate');
 const Constants = require('./Constants');
 const catchError = require('./CatchError');
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- * - 제공된 `InputView` 객체를 활용해 구현해야 한다.
-- `InputView`의 파일 경로는 변경할 수 있다.
-- `InputView`의 메서드의 인자는 변경할 수 있다.
-- 사용자 값 입력을 위해 필요한 메서드를 추가할 수 있다.
- */
 let game;
 
 const InputView = {
-  /**
-   * 다리의 길이를 입력받는다.
-   */
   readBridgeSize() {
     MissionUtils.Console.readLine(Constants.Input.BRIDGE_SIZE, (size) => {
       catchError(Validate.bridge, size);
@@ -29,10 +19,6 @@ const InputView = {
       InputView.readMoving();
     });
   },
-
-  /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
   readMoving() {
     MissionUtils.Console.readLine(Constants.Input.MOVE, (movement) => {
       catchError(Validate.upOrDown, movement);
@@ -43,9 +29,6 @@ const InputView = {
       InputView.readMoveResult(moveResult);
     });
   },
-  /**
-   * 사용자가 이동한 결과 값을 받는다.
-   */
   readMoveResult(moveResult) {
     const { map, tryNumber } = game.getResult();
     switch (moveResult) {
@@ -57,10 +40,6 @@ const InputView = {
         return OutputView.printResult(map, tryNumber, '성공');
     }
   },
-
-  /**
-   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-   */
   readGameCommand() {
     MissionUtils.Console.readLine(Constants.Input.GAME_COMMAND, (command) => {
       try {
@@ -71,7 +50,6 @@ const InputView = {
       readCommand(command);
     });
   },
-
   readCommand(command) {
     const { map, tryNumber } = game.getResult();
     switch (command) {
--- src/OutputView.js
+++ src/OutputView.js
@@ -1,30 +1,12 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const Constants = require('./Constants');
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- * - 제공된 `OutputView` 객체를 활용해 구현해야 한다.
-- `OutputView`의 파일 경로는 변경할 수 있다.
-- `OutputView`의 메서드의 이름은 변경할 수 없고, 인자는 필요에 따라 추가하거나 변경할 수 있다.
-- 값 출력을 위해 필요한 메서드를 추가할 수 있다.
- */
 const OutputView = {
-  /**
-   * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-   * <p>
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
   printMap(map) {
     const [up, down] = OutputView.convertMap(map);
     MissionUtils.Console.print(up);
     MissionUtils.Console.print(down);
   },
-
-  /**
-   * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-   * <p>
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
   printResult(map, tryNumber, result) {
     const [up, down] = OutputView.convertMap(map);
     MissionUtils.Console.print(Constants.Output.FINAL_RESULT);
@@ -34,7 +16,6 @@ const OutputView = {
     MissionUtils.Console.print(`${Constants.Output.NUMBER_OF_TRY} ${tryNumber}`);
     MissionUtils.Console.close();
   },
-
   convertMap(map) {
     const { U, D } = map;
     const up = `[ ${U.join(' | ')} ]`;
