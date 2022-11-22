2e459049503445dcad195e443ab86973ee268be5
kimeuns <dmstn2006@gmail.com>
2022-11-22 23:33:49 +0900

    refactor: 겹치는 부분 제거

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -5,34 +5,21 @@ const { Console } = require('@woowacourse/mission-utils');
 const { GAME, MOVEMENT } = require('../utils/Constants');
 
 const OutputView = {
-  /**
-   * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-   * <p>
-   * @param {Array} bridgeMoving 다리 건넌 진행상황 : 위 칸이면 U, 아래 칸이면 D로 표현한다.
-   * @return {string[]}
-   *
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-
   printGameStart() {
     Console.print(GAME.START);
   },
 
-  makeUpside(lastPoint, upMove) {
-    return upMove.reduce((arr, value, idx) => {
-      if (idx === upMove.length - 1) {
-        value ? arr.push(lastPoint) : arr.push(' ');
-        return arr;
-      }
-      value ? arr.push(MOVEMENT.SUCCESS) : arr.push(' ');
-      return arr;
-    }, []);
+  printMap(isSuccess, [upSide, downSide]) {
+    Console.print(
+    );
+    Console.print(
+    );
   },
 
-  makeDownside(lastPoint, downMove) {
-    return downMove.reduce((arr, value, idx) => {
-      if (idx === downMove.length - 1) {
-        value ? arr.push(lastPoint) : arr.push(' ');
+  makeSide(lastPoint, moveResult) {
+    const lastPoint = isSuccess ? MOVEMENT.SUCCESS : MOVEMENT.FAIL;
+    return moveResult.reduce((arr, value, idx) => {
+      if (idx === moveResult.length - 1) {
         return arr;
       }
       value ? arr.push(MOVEMENT.SUCCESS) : arr.push(' ');
@@ -44,6 +31,8 @@ const OutputView = {
     const lastPoint = isSuccess ? MOVEMENT.SUCCESS : MOVEMENT.FAIL;
     Console.print(`[ ${this.makeUpside(lastPoint, upMove).join(' | ')} ]`);
     Console.print(`[ ${this.makeDownside(lastPoint, downMove).join(' | ')} ]`);
+  printExceptionMessage(e) {
+    Console.print(e.message);
   },
 
   /**
@@ -55,17 +44,14 @@ const OutputView = {
     Console.print(GAME.END_RESULT);
   },
 
-  printExceptionMessage(e) {
-    Console.print(e.message);
+  printSucessOrFail(gameSuccess) {
+    Console.print(`${GAME.RESULT}${gameSuccess? GAME.SUCCESS : GAME.FAIL}`);
   },
-
-  printSucessOrFail(isSuccess) {
-    Console.print(`${GAME.RESULT}${isSuccess ? GAME.SUCCESS : GAME.FAIL}`);
-  },
-
+  
   printTryCount(gameCount) {
     Console.print(`${GAME.TRY_COUNT}${gameCount}`);
   },
+    
 };
 
 module.exports = OutputView;
