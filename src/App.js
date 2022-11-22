617caea91aaa62e69979cfa0ad127b10840a3a02
금가경 <gageum0@gmail.com>
2022-11-22 19:44:07 +0900

    feat(bridge) : 각 기능 실행 가능하도록 연결

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const InputView = require('./InputView');
+const OutputView = require('./OutputView');
+
 class App {
-  play() {}
+  constructor() {
+    OutputView.printStartMessage();
+  }
+
+  play() {
+    InputView.readBridgeSize();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
--- src/InputView.js
+++ src/InputView.js
@@ -2,13 +2,19 @@ const MissionUtils = require('@woowacourse/mission-utils');
 const OutputView = require('./OutputView');
 const ServiceMessages = require('./ServiceMessages');
 const Validator = require('./Validator');
+const BridgeMaker = require('./BridgeMaker');
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
+const BridgeGame = require('./BridgeGame');
 
 const validator = new Validator();
+const bridgeGame = new BridgeGame();
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
+  tryCount: 1,
+  totalCount: 1,
   /**
    * 다리의 길이를 입력받는다.
    */
@@ -22,6 +28,13 @@ const InputView = {
           OutputView.printErrorMessages(error);
           this.readBridgeSize();
         }
+        this.bridgeSize = bridgeSize;
+
+        this.bridge = BridgeMaker.makeBridge(
+          this.bridgeSize,
+          BridgeRandomNumberGenerator.generate
+        );
+        this.readMoving();
       }
     );
   },
@@ -37,6 +50,39 @@ const InputView = {
         OutputView.printErrorMessages(error);
         this.readBridgeSize();
       }
+
+      this.moving = upOrDown;
+
+      const result = bridgeGame.move(this.moving, this.bridge);
+      if (result === 'X') {
+        OutputView.printMap(
+          result,
+          this.moving,
+          this.tryCount,
+          this.totalCount
+        );
+        this.tryCount = 1;
+        OutputView.printResult('실패', this.totalCount);
+        InputView.readGameCommand();
+      }
+      if (result === 'O') {
+        OutputView.printMap(
+          result,
+          this.moving,
+          this.tryCount,
+          this.totalCount
+        );
+        this.tryCount += 1;
+        this.bridge.shift();
+      }
+
+      if (this.bridge.length === 0) {
+        this.tryCount = 1;
+        OutputView.printResult('성공', this.totalCount);
+        this.readGameCommand();
+      } else {
+        this.readMoving();
+      }
     });
   },
 
@@ -50,6 +96,12 @@ const InputView = {
       } catch (error) {
         OutputView.printErrorMessages(error);
         this.readBridgeSize();
+      }
+      if (bridgeGame.retry(restartOrQuit)) {
+        this.totalCount += 1;
+        this.readMoving();
+      } else {
+        MissionUtils.Console.close();
       }
     });
   },
