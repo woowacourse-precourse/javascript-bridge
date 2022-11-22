aad753372014f0d62594646207479c00dc24e776
Sallyvely <tpdus4159@naver.com>
2022-11-22 13:13:44 +0900

    feat : 게임 시작 기능 구현

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ const MESSAGE = require("./message");
 let bridgeSize,bridgeBase;
 class App {
   play() {
+    this.gameStart();
+  }
 
+  gameStart() {
+    this.makeBridgeBase();
   }
 
   failPlace(inputMove) {
