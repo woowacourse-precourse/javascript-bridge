ee1a231b73ad5e0e0957c5ed542a07d1f920c243
banma1234 <banma1234@gmail.com>
2022-11-22 22:56:57 +0900

    feature: 다리의 크기 및 사용자가 건널 칸 입력받는 함수

--- src/Feature.js
+++ src/Feature.js
@@ -1,7 +1,27 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const BridgeMaker = require("../src/BridgeMaker");
+const MakeRandomValue = require('../src/BridgeRandomNumberGenerator');
 
 class Feature {
     play() {
-        
+        this.inputBridge();
+    }
+
+    inputBridge() {
+        let bridge = [];
+        MissionUtils.Console.readline("다리크기 입력.", (INPUT) => {
+            bridge = BridgeMaker.makeBridge(INPUT, MakeRandomValue.generate());
+            this.crossBridge(bridge);
+        });
+    }
+
+    inputUserMove() {
+        let result = "";
+        MissionUtils.Console.readline("이동할 칸 선택.", (INPUT) => {
+            this.inputUserMoveError(INPUT);
+            result = INPUT;
+        })
+
+        return result;
     }
 }
