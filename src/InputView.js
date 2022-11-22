3862a3cdf4f09bdf10e9585ee5279f7216ab823b
soomin <qkrtnals970804@gmail.com>
2022-11-22 21:29:55 +0900

    fix: 한 줄 띄우기 및 readMoving 함수 수정

--- src/InputView.js
+++ src/InputView.js
@@ -1,4 +1,4 @@
-const { readLine } = require('./Utils');
+const { readLine, print } = require('./Utils');
 const BridgeGame = require('./BridgeGame');
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -8,9 +8,9 @@ const InputView = {
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize() {
-    print('다리 건너기 게임을 시작합니다.');
-    readLine('다리의 길이를 입력해주세요.', size => {
-      readMoving(new BridgeGame(size));
+    print('다리 건너기 게임을 시작합니다.\n');
+    readLine('다리의 길이를 입력해주세요.\n', size => {
+      this.readMoving(new BridgeGame(Number(size)));
     });
   },
 
@@ -18,13 +18,13 @@ const InputView = {
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving(game) {
-    readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)', type => {
+    readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)\n', type => {
       const IS_UP = type === 'U';
       const IS_DOWN = type === 'D';
 
       if (IS_UP || IS_DOWN) {
         game.move(type);
-        readMoving(move);
+        this.readMoving(game);
       } else {
         throw new Error('U, D 만 입력 가능 합니다.');
       }
