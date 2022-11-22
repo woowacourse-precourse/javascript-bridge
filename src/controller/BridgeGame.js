35b682e97fa5589fd3e5f2c8e0045a7c8c4d46db
yesolyo <ahnyelim77@gmail.com>
2022-11-22 14:39:24 +0900

    feat: 위 칸 선택시 U이면 O표시, D이면 X표시

--- src/controller/BridgeGame.js
+++ src/controller/BridgeGame.js
@@ -2,12 +2,44 @@
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  constructor(upDown, computer, count) {
+    this.upDown = upDown;
+    this.computer = computer;
+    this.count = count;
+  }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(upDown, computer, count) {
+    this.upDown = upDown;
+    this.computer = computer;
+    this.count = count;
+    this.saveMove();
+    return [resultUp, resultDown];
+  }
+
+  saveMove() {
+    this.changeUpDown();
+  }
+
+  changeUpDown() {
+    if (this.upDown === "U") {
+      this.saveUp();
+    }
+  }
+
+  saveUp() {
+    if (this.computer[this.count] !== "U") {
+      resultUp.push(CELL.ZERO);
+      resultDown.push(" ");
+    }
+    if (this.computer[this.count] === "U") {
+      resultUp.push(CELL.ONE);
+      resultDown.push(" ");
+    }
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
