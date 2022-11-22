5554df2b1869f0f9f258b38d85bdba0b1499bd4e
JangHoonSeok <orirori512@naver.com>
2022-11-22 13:49:39 +0900

    ✨ feat(constants): state 상수 추가

--- src/constants/GameCondition.js
+++ src/constants/GameCondition.js
@@ -2,5 +2,9 @@ const BRIDGE_DIRECTION = {
   UP: "U",
   DOWN: "D",
 };
+const STATE = {
+  SUCCESS: "성공",
+  FAIL: "실패",
+};
 
-module.exports = { BRIDGE_DIRECTION };
+module.exports = { BRIDGE_DIRECTION, STATE };
