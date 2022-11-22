82c819772fc57eea416bf476c734f2a499cfe62a
SeongOh Park <qkrtjddh1212@naver.com>
2022-11-22 12:25:31 +0900

    fix(test): fix test code for exception
    - cause try catch

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,6 +1,7 @@
 const BridgeGame = require('../src/BridgeGame');
 const BridgeMaker = require('../src/BridgeMaker');
 const BridgeRandomNumberGenerator = require('../src/BridgeRandomNumberGenerator');
+const { checkValidDirection } = require('../src/utils/validator');
 
 describe('예외 테스트', () => {
   test.each([[2], [-12], [42], ['ab']])('다리 사이즈 예외처리', (input) => {
@@ -11,8 +12,7 @@ describe('예외 테스트', () => {
 
   test.each([[2], ['A'], [-2], ['ab']])('다리 건너기 예외처리', (input) => {
     expect(() => {
-      const game = new BridgeGame();
-      game.move(input);
+      checkValidDirection(input);
     }).toThrow('[ERROR] 이동할 칸은 U또는 D여야 합니다.');
   });
 });
