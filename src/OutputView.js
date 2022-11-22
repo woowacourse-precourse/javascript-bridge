074b0ad23a968a342ee46c080f3cbc6f231cc6a8
pp449 <mma7710@naver.com>
2022-11-22 15:40:41 +0900

    refactor: OutputView에서 출력문을 constant파일의 상수값을 가져와 출력하는 것으로 변경

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,4 +1,5 @@
 const { Console } = require('@woowacourse/mission-utils');
+const MESSAGE = require('./utils/constants');
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -52,10 +53,10 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printResult(isSuccess, bridgeHistory, attempts, direction) {
-    Console.print('\n최종 게임 결과');
+    Console.print(MESSAGE.GAMERESULT);
     OutputView.printMap(bridgeHistory, direction);
-    Console.print(`\n게임 성공 여부: ${isSuccess}`);
-    Console.print(`총 시도한 횟수: ${attempts}`);
+    Console.print(MESSAGE.GAMESUCCESS + isSuccess);
+    Console.print(MESSAGE.GAMEATTEMPTS + attempts);
     Console.close();
   },
 };
