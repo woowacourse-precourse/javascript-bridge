9209f4f73e8e38463571ba2772120e00f82f1526
soomin <qkrtnals970804@gmail.com>
2022-11-22 22:05:38 +0900

    fix: makeBridge 함수 기능 수정

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -8,17 +8,16 @@ const BridgeMaker = {
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
   makeBridge(size, generateRandomNumber) {
-    const upBridge = new Array(size).fill('X');
-    const downBridge = new Array(size).fill('X');
+    const bridge = [];
 
     for (let i = 0; i < size; i++) {
       const randomNumber = generateRandomNumber();
-      const IS_UPPER_CORRECT = randomNumber === 1;
-
-      IS_UPPER_CORRECT ? (upBridge[i] = 'O') : (downBridge[i] = 'O');
+      const IS_UPPER_CORRECT = randomNumber === '1';
+      const position = IS_UPPER_CORRECT ? 'U' : 'D';
+      bridge.push(position);
     }
 
-    return [upBridge.join(''), downBridge.join('')];
+    return bridge;
   },
 };
 
