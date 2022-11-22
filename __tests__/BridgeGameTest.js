264a3a5b06ba9da95c8320901e46369d5b260492
Jaehyeon <jaehyeonp74@gmail.com>
2022-11-22 10:19:10 +0900

    test: BridgeGame move 메소드 테스트 구현

---
+++ __tests__/BridgeGameTest.js
@@ -0,0 +1,38 @@
+const BridgeGame = require('../src/BridgeGame');
+const Model = require('../src/Model');
+
+describe('BridgeGame 클래스 테스트 1', () => {
+  test('move 메소드 - U 입력하고 맞췄을 경우', () => {
+    const testModel = new Model();
+    BridgeGame.move('U', ' O ', testModel);
+    const testResult = testModel.getCurrentMap();
+    expect(testResult).toBe(`[ O ]\n[   ]`);
+  });
+});
+
+describe('BridgeGame 클래스 테스트 2', () => {
+  test('move 메소드 - U 입력하고 틀렸을 경우', () => {
+    const testModel = new Model();
+    BridgeGame.move('U', ' X ', testModel);
+    const testResult = testModel.getCurrentMap();
+    expect(testResult).toBe(`[ X ]\n[   ]`);
+  });
+});
+
+describe('BridgeGame 클래스 테스트 3', () => {
+  test('move 메소드 - D 입력하고 맞췄을 경우', () => {
+    const testModel = new Model();
+    BridgeGame.move('D', ' O ', testModel);
+    const testResult = testModel.getCurrentMap();
+    expect(testResult).toBe(`[   ]\n[ O ]`);
+  });
+});
+
+describe('BridgeGame 클래스 테스트 4', () => {
+  test('move 메소드 - D 입력하고 틀렸을 경우', () => {
+    const testModel = new Model();
+    BridgeGame.move('D', ' X ', testModel);
+    const testResult = testModel.getCurrentMap();
+    expect(testResult).toBe(`[   ]\n[ X ]`);
+  });
+});
