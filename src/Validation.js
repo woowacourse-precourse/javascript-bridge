93a9d90c344d6bc68e4561c2cd6bfeb14b21d83f
deli-ght <minjju0660@gmail.com>
2022-11-22 21:14:47 +0900

    feat(validateBridgeMove): 유저의 이동 입력값 유효성 검사

--- src/Validation.js
+++ src/Validation.js
@@ -1,5 +1,6 @@
 const ErrorMessage = {
   BRIDGE_LENGTH_INPUT: "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.",
+  BRIDGE_MOVE_INPUT: "[ERROR] 입력값은 D 또는 U 둘 중의 하나만 입력해 주세요.",
 };
 
 const Validation = {
@@ -10,6 +11,14 @@ const Validation = {
   validateBridgeNumber(input) {
     if (!/^\d+$/.test(input)) throw Error(ErrorMessage.BRIDGE_LENGTH_INPUT);
     if (input < 3 || input > 20) throw Error(ErrorMessage.BRIDGE_LENGTH_INPUT);
+  },
+  /**
+   *
+   * @param {*} input
+   */
+  validateBridgeMove(input) {
+    if (input !== "U" && input !== "D")
+      throw Error(ErrorMessage.BRIDGE_MOVE_INPUT);
   },
 };
 
