c269d14e4f891ad95096034fabf0464f4b9eb208
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-11-22 21:14:03 +0900

    test: test.each 함수 이용해 입력값에 대한 예외테스트 간결하게 수정

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -86,7 +86,7 @@ describe('다리 건너기 테스트', () => {
     runException(['a']);
   });
 
-  test('기능 테스트 : 이동할 수 없는 칸 선택한 이후 재시도를 선택하면 게임 재기한다', () => {
+  test('기능 테스트 : 이동할 수 없는 칸 선택한 이후 재시도를 선택하면 게임 재시작한다', () => {
     const logSpy = getLogSpy();
     mockRandoms([1, 0, 0, 1, 1, 1]);
     mockQuestions([
@@ -145,11 +145,7 @@ describe('다리 건너기 테스트', () => {
     expectBridgeOrder(log, '[   | O |   ]', '[ O |   | X ]');
   });
 
-  test('예외 테스트 : 이동할 칸으로 잘못된 값을 입력받은 경우 에러 메시지 출력 후 그 부분부터 다시 입력 받는다', () => {
-    runException([15, 'a', 'u', 123]);
-  });
-
-  test('예외 테스트 : 재시작 또는 종료 여부에 대해 잘못된 값을 입력받은 경우 에러 메시지 출력 후 그 부분부터 다시 입력 받는다', () => {
+  test('예외 테스트 : 재시작 또는 종료 여부에 대해 잘못된 값을 입력받은 경우 에러 메시지 출력 후 다시 입력 받는다', () => {
     const logSpy = getLogSpy();
     mockRandoms([1, 1, 1]);
     mockQuestions(['3', 'D', 'r', 123, 'quit', 'R', 'U', 'U', 'U']);
@@ -166,38 +162,6 @@ describe('다리 건너기 테스트', () => {
       '게임 성공 여부: 성공',
       '총 시도한 횟수: 2',
     ]);
-  });
-
-  test('validateSize 메서드 기능 테스트 : 다리의 길이에 대해 잘못된 입력값 받은 경우 에러 메시지 출력하고 inputBridgeSize 메서드가 호출된다', () => {
-    const logSpy = getLogSpy();
-    const controller = new Controller();
-    controller.inputBridgeSize = jest.fn();
-
-    const inputBridgeSize = controller.inputBridgeSize;
-    controller.validateSize(21);
-
-    const log = getOutput(logSpy);
-    expectLogContains(log, [
-      '[ERROR] 다리의 길이는 3 이상 20 이하만 가능합니다.',
-    ]);
-
-    expect(inputBridgeSize).toBeCalledTimes(1);
-  });
-
-  test('validateDirection 메서드 기능 테스트 : 이동할 칸에 대해 잘못된 입력값 받은 경우 에러 메시지 출력하고 inputMoving 메서드가 호출된다', () => {
-    const logSpy = getLogSpy();
-    const controller = new Controller();
-    controller.inputMoving = jest.fn();
-
-    const inputMoving = controller.inputMoving;
-    controller.validateDirection('');
-
-    const log = getOutput(logSpy);
-    expectLogContains(log, [
-      '[ERROR] 공백을 입력할 수 없습니다. 값을 입력해주세요.',
-    ]);
-
-    expect(inputMoving).toBeCalledTimes(1);
   });
 
   test('checkSuccess 메서드 기능 테스트 : 이동할 수 있는 칸을 선택하였고 아직 다리를 다 건너지 못했다면 inputMoving 메서드가 호출된다', () => {
@@ -230,19 +194,85 @@ describe('다리 건너기 테스트', () => {
     expect(inputGameCommand).toBeCalledTimes(1);
   });
 
-  test('validateCommand 메서드 기능 테스트 : 게임 재시도 여부에 대해 잘못된 입력값 받은 경우 에러 메시지 출력하고 inputGameCommand 메서드가 호출된다', () => {
-    const logSpy = getLogSpy();
-    const controller = new Controller();
-    controller.inputGameCommand = jest.fn();
+  test.each([
+    [21, '[ERROR] 다리의 길이는 3 이상 20 이하만 가능합니다.'],
+    [2, '[ERROR] 다리의 길이는 3 이상 20 이하만 가능합니다.'],
+    ['play', '[ERROR] 문자와 특수기호를 제외한 숫자를 입력해주세요.'],
+    ['', '[ERROR] 공백을 입력할 수 없습니다. 값을 입력해주세요.'],
+    [0, '[ERROR] 다리의 길이는 3 이상 20 이하만 가능합니다.'],
+    [-999, '[ERROR] 다리의 길이는 3 이상 20 이하만 가능합니다.'],
+    [10000, '[ERROR] 다리의 길이는 3 이상 20 이하만 가능합니다.'],
+  ])(
+    'validateSize 메서드 기능 테스트 : 다리의 길이에 대해 잘못된 입력값 받은 경우 에러 메시지 출력하고 inputBridgeSize 메서드가 호출된다',
+    (input, expected) => {
+      const logSpy = getLogSpy();
+      const controller = new Controller();
+      controller.inputBridgeSize = jest.fn();
+      const inputBridgeSize = controller.inputBridgeSize;
 
-    const inputGameCommand = controller.inputGameCommand;
-    controller.validateCommand('quit');
+      controller.validateSize(input);
 
-    const log = getOutput(logSpy);
-    expectLogContains(log, [
-      '[ERROR] R (재시도) 와 Q (종료) 중에서만 입력해주세요.',
-    ]);
+      const log = getOutput(logSpy);
+      expectLogContains(log, [expected]);
 
-    expect(inputGameCommand).toBeCalledTimes(1);
-  });
+      expect(inputBridgeSize).toBeCalledTimes(1);
+    }
+  );
+
+  test.each([
+    ['', '[ERROR] 공백을 입력할 수 없습니다. 값을 입력해주세요.'],
+    ['3', '[ERROR] 숫자를 제외한 문자를 입력해주세요.'],
+    ['20', '[ERROR] 숫자를 제외한 문자를 입력해주세요.'],
+    ['u', '[ERROR] 소문자가 아닌 대문자를 입력해주세요.'],
+    ['d', '[ERROR] 소문자가 아닌 대문자를 입력해주세요.'],
+    [
+      'R',
+      '[ERROR] U (위칸) 와 D (아래칸) 중에서만 이동할 칸을 정해 입력해주세요.',
+    ],
+    [
+      'Q',
+      '[ERROR] U (위칸) 와 D (아래칸) 중에서만 이동할 칸을 정해 입력해주세요.',
+    ],
+  ])(
+    'validateDirection 메서드 기능 테스트 : 이동할 칸에 대해 잘못된 입력값 받은 경우 에러 메시지 출력하고 inputMoving 메서드가 호출된다',
+    (input, expected) => {
+      const logSpy = getLogSpy();
+      const controller = new Controller();
+      controller.inputMoving = jest.fn();
+
+      const inputMoving = controller.inputMoving;
+      controller.validateDirection(input);
+
+      const log = getOutput(logSpy);
+      expectLogContains(log, [expected]);
+
+      expect(inputMoving).toBeCalledTimes(1);
+    }
+  );
+
+  test.each([
+    ['quit', '[ERROR] R (재시도) 와 Q (종료) 중에서만 입력해주세요.'],
+    ['retry', '[ERROR] R (재시도) 와 Q (종료) 중에서만 입력해주세요.'],
+    ['U', '[ERROR] R (재시도) 와 Q (종료) 중에서만 입력해주세요.'],
+    ['D', '[ERROR] R (재시도) 와 Q (종료) 중에서만 입력해주세요.'],
+    ['r', '[ERROR] 소문자가 아닌 대문자를 입력해주세요.'],
+    ['q', '[ERROR] 소문자가 아닌 대문자를 입력해주세요.'],
+    ['', '[ERROR] 공백을 입력할 수 없습니다. 값을 입력해주세요.'],
+    ['123', '[ERROR] 숫자를 제외한 문자를 입력해주세요.'],
+  ])(
+    'validateCommand 메서드 기능 테스트 : 게임 재시도 여부에 대해 잘못된 입력값 받은 경우 에러 메시지 출력하고 inputGameCommand 메서드가 호출된다',
+    (input, expected) => {
+      const logSpy = getLogSpy();
+      const controller = new Controller();
+      controller.inputGameCommand = jest.fn();
+
+      const inputGameCommand = controller.inputGameCommand;
+      controller.validateCommand(input);
+
+      const log = getOutput(logSpy);
+      expectLogContains(log, [expected]);
+
+      expect(inputGameCommand).toBeCalledTimes(1);
+    }
+  );
 });
