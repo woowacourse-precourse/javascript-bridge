60e2cde1f0fb20c3a89332ed4b8ec61d275cc58e
emma <jhj191058@gmail.com>
2022-11-22 14:59:44 +0900

    feat : 다리 길이 입력 받기

--- src/InputView.js
+++ src/InputView.js
@@ -1,21 +1,21 @@
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
+const MissionUtils = require("@woowacourse/mission-utils");
+const BridgeMaker = require("./BridgeMaker");
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    MissionUtils.Console.readLine('다리의 길이를 입력해주세요.', (size) => {
+      if(BridgeMaker.size < 3 || BridgeMaker.size >20 || !Number.isInteger(size)){
+        throw Error('[ERROR] 다리 길이는 3 ~ 20 사이의 숫자여야 합니다. ');
+      }
+    });
+  },
 
-  /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
-  readMoving() {},
-
-  /**
-   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-   */
-  readGameCommand() {},
 };
 
 module.exports = InputView;
+
