92931394b3e783c35d298e3c026252c890d1882b
jiho1996 <jhp9270@naver.com>
2022-11-22 11:47:01 +0900

    refactor : map분리에 따른 bridgeGame클래스 함수 소거

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,10 +1,7 @@
-const { STATE } = require("./constants/message");
-const { REPRESENTATION } = require("./constants/values");
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
-  #currentMap = { upperPart: [], lowerPart: [] };
   #myPosition = 0;
   #numberOfAttempt = 1;
   get myCurrentPosition() {
@@ -15,61 +12,33 @@ class BridgeGame {
     return this.#numberOfAttempt;
   }
 
-  get currentMap() {
-    return this.#currentMap;
-  }
-
-  isValidPath(toBeMoveDirection, validPath) {
-    return toBeMoveDirection === validPath[this.#myPosition];
+  #isCorrectPath(toBeMoveDirection, answerPath) {
+    return toBeMoveDirection === answerPath[this.#myPosition];
   }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move(direction, validPath) {
-    if (this.isValidPath(direction, validPath)) {
-      this.updateMyPositionForward(direction, STATE.VALID.symbol);
+  move(direction, answerPath) {
+    if (this.#isCorrectPath(direction, answerPath)) {
+      this.#myPosition += 1;
       return true;
     }
-    this.updateMyPositionForward(direction, STATE.NOT_VALID.symbol);
+    this.#myPosition += 1;
     return false;
   }
-
-  updateMyPositionForward(direction, symbol) {
-    this.fillPathAccordingInput(direction, symbol);
-    this.#myPosition += 1;
-    return this;
-  }
-
-  fillPathAccordingInput(input, symbol) {
-    if (input === REPRESENTATION.UPPER.abbreviatedForm) {
-      this.#currentMap.upperPart.push(symbol);
-      this.fillBlankUnselectedPath(REPRESENTATION.LOWER.numericalForm);
-    }
-    if (input === REPRESENTATION.LOWER.abbreviatedForm) {
-      this.#currentMap.lowerPart.push(symbol);
-      this.fillBlankUnselectedPath(REPRESENTATION.UPPER.numericalForm);
-    }
-  }
-  fillBlankUnselectedPath(direction) {
-    direction === REPRESENTATION.UPPER.numericalForm
-      ? this.#currentMap.upperPart.push("   ")
-      : this.#currentMap.lowerPart.push("   ");
-  }
-
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   retry() {
-    this.initState();
+    this.#initPosition();
     this.#numberOfAttempt += 1;
   }
 
-  initState() {
-    this.#currentMap = { upperPart: [], lowerPart: [] };
+  #initPosition() {
     this.#myPosition = 0;
   }
 }
