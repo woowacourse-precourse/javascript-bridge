2f377c26b17121a7a711e6d4f4ad3f9a8ca4b5b8
knh6269 <knh6269@gmail.com>
2022-11-22 23:52:52 +0900

    feat(BridgeGame): 생성자 함수

--- src/Model/BridgeGame.js
+++ src/Model/BridgeGame.js
@@ -3,6 +3,11 @@ const BridgeRandomNumberGenerator = require ('../BridgeRandomNumberGenerator');
 
 class BridgeGame {
   
+  constructor() {
+    this.moving = [];
+    this.bridge = [];
+    this.count = 1;
+  }
 }
 
 module.exports = BridgeGame;
