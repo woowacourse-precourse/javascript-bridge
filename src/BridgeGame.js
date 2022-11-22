db6f8678c25e9a48d0a7cf6cac5eed0b2a78cea7
Doyoung.Kim <dyflower@nate.com>
2022-11-22 18:55:33 +0900

    refactor : 객체 상태 접근 제한

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,43 +1,48 @@
 class BridgeGame {
+  #round;
+  #totalGame;
+  #up;
+  #down;
+
   constructor() {
-    this.round = 0;
-    this.totalGame = 0;
-    this.up = [];
-    this.down = [];
+    this.#round = 0;
+    this.#totalGame = 0;
+    this.#up = [];
+    this.#down = [];
   }
 
   move() {
-    this.round++;
+    this.#round++;
 
-    return this.round;
+    return this.#round;
   }
 
   retry() {
-    this.round = 0;
-    this.up = [];
-    this.down = [];
+    this.#round = 0;
+    this.#up = [];
+    this.#down = [];
 
-    return this.round;
+    return this.#round;
   }
-  
-  countGame() {
-    this.totalGame++;
 
-    return this.totalGame;
+  countGame() {
+    this.#totalGame++;
+
+    return this.#totalGame;
   }
 
   makeBridge(Bridge, upDown) {
     let UD = { U: ' ', D: ' ' };
     Bridge === upDown ? (UD[upDown] = 'O') : (UD[upDown] = 'X');
 
-    this.up.push(UD['U']);
-    this.down.push(UD['D']);
+    this.#up.push(UD['U']);
+    this.#down.push(UD['D']);
 
-    return [this.up, this.down, Bridge === upDown];
+    return [this.#up, this.#down, Bridge === upDown];
   }
 
   getBridge() {
-    return [this.up, this.down];
+    return [this.#up, this.#down];
   }
 }
 
