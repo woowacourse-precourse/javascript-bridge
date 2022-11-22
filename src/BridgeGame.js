16c44ace3e065a929759ef68ee2623dde77cc354
Jyeo <dhkfkr134@naver.com>
2022-11-22 13:10:08 +0900

    feat(OutputView): OutputView 객체 생성

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,20 +1,33 @@
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
+const { printMap } = require('./OutputView');
 class BridgeGame {
-  /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  move() {}
+  bridge;
+  crossingOrder;
+  attemptCount;
 
-  /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  retry() {}
+  constructor(bridge) {
+    this.bridge = bridge;
+    this.crossingOrder = [];
+    this.attemptCount = 1;
+  }
+  move(direction) {
+    this.crossingOrder.push([direction, direction === "U" ? 0 : 1]);
+  }
+
+  getBridgeCrossingResult() {
+    let bridgeCrossingResult = [[], []];
+    this.crossingOrder.forEach(([direction, directionNumber], idx) => {
+      const isCross = direction === this.bridge[idx];
+
+      bridgeCrossingResult[directionNumber].push(isCross ? "O" : "X");
+      bridgeCrossingResult[Math.abs(directionNumber - 1)].push(" ");
+    });
+    return bridgeCrossingResult;
+  }
 }
 
-module.exports = BridgeGame;
+// let bg = new BridgeGame(['U', 'D', 'D']);
+// bg.move('U');
+// bg.move('D');
+// console.log(bg.getBridgeCrossingResult());
+
+// printMap(bg.getBridgeCrossingResult());
