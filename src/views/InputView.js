e6e449a162dc4f67f98bdb02a94a2dd2bcc8491e
chee9835 <huisu121@gmail.com>
2022-11-22 20:16:45 +0900

    refactor: 파일 이동으로 인한 경로 수정

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,6 +1,6 @@
-const Io = require('./utils/io');
-const { INPUT } = require('./constants/views');
-const { Validator } = require('./utils/validator');
+const Io = require('../utils/io');
+const { INPUT } = require('../constants/views');
+const { Validator } = require('../utils/validator');
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
