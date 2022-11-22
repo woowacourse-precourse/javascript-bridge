54207cefcb4ab405334192005f290444f566968b
yeongseokim <1903020@ewhain.net>
2022-11-22 15:14:05 +0900

    test(ErrorCheckerTest): 정상 input에 대한 true 반환 테스트

--- __tests__/ErrorCheckerTest.js
+++ __tests__/ErrorCheckerTest.js
@@ -2,7 +2,7 @@ const { ERROR_MESSAGE } = require("../src/constants");
 const ErrorChecker = require("../src/ErrorChecker");
 
 describe("ErrorChecker 테스트", () => {
-    test("checkBridgeSizeValidation 테스트", () => {
+    test("checkBridgeSizeValidation: 비정상 input에 대한 throw 테스트", () => {
         const bridgeSizeInputs = ["a", "4.5", "2", "21"];
         const message = [
             ERROR_MESSAGE.NOT_A_NUMBER,
@@ -12,6 +12,13 @@ describe("ErrorChecker 테스트", () => {
         ];
         bridgeSizeInputs.map((input, index) => {
             expect(() => { ErrorChecker.checkBridgeSizeValidation(input) }).toThrow(message[index]);
+        })
+    });
+
+    test("checkBridgeSizeValidation: 정상 input에 대한 true 반환 테스트", () => {
+        const bridgeSizeInputs = ["3", "4", "5", "20"];
+        bridgeSizeInputs.map((input) => {
+            expect(() => { ErrorChecker.checkBridgeSizeValidation(input) }).toBeTruthy();
         })
     })
 })
