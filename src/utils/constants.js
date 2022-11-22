8dcbb00ace162ae5c024de2534a1339a5a3a7a5a
nalsae <nalsae0207@naver.com>
2022-11-22 10:31:34 +0900

    feat: Add constants about size
    다리 길이 관련 상수 추가

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,26 +2,31 @@ const INITIAL_COUNT = 0;
 
 const INITIAL_STATE = [];
 
+const SIZE = Object.freeze({
+  MIN: 3,
+  MAX: 20,
+});
+
 const DIRECTION = Object.freeze({
-  UP: 'U',
-  DOWN: 'D',
+  UP: "U",
+  DOWN: "D",
 });
 
 const RESULT = Object.freeze({
-  RIGHT: 'O',
-  WRONG: 'X',
+  RIGHT: "O",
+  WRONG: "X",
 });
 
 const BRIDGE = Object.freeze({
-  INITIAL: '[ ',
-  NO_RESULT: '  ',
-  END: ']',
-  NOT_END: '| ',
+  INITIAL: "[ ",
+  NO_RESULT: "  ",
+  END: "]",
+  NOT_END: "| ",
 });
 
 const INPUT = Object.freeze({
-  RETRY: 'R',
-  END: 'Q',
+  RETRY: "R",
+  END: "Q",
 });
 
 const IS_SUCCESS = Object.freeze({
@@ -29,13 +34,14 @@ const IS_SUCCESS = Object.freeze({
   FALSE: false,
 });
 
-const SUCCESS_STRING = '성공';
+const SUCCESS_STRING = "성공";
 
-const FAILURE_STRING = '실패';
+const FAILURE_STRING = "실패";
 
 module.exports = {
   INITIAL_COUNT,
   INITIAL_STATE,
+  SIZE,
   DIRECTION,
   RESULT,
   BRIDGE,
