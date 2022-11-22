536c1b8154dc02640d3f04a84865c64fa2a7f678
OrangeVinyl <hap_hazard@naver.com>
2022-11-22 21:44:06 +0900

    fix: 예제테스트 오류 수정

--- src/util/Constant.js
+++ src/util/Constant.js
@@ -2,8 +2,8 @@ const CONSTANT = {
   MESSAGE: {
     START_GAME: '다리 건너기 게임을 시작합니다.\n',
     INPUT_BRIDGE_SIZE: '다리의 길이를 입력해주세요.\n',
-    UPDOWN: '\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
-    END_GAME: '\n최종 게임 결과',
+    UPDOWN: '이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
+    END_GAME: '최종 게임 결과',
     COUNT: '총 시도한 횟수: ',
     CHECK_ANSWER: '게임 성공 여부: ',
     RETRY_QUIT: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
