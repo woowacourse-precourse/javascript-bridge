be854c7918e39c1477aa38588ae1b1ee3601a5d6
ilgon0110 <moses011010@gmail.com>
2022-11-22 18:11:02 +0900

    refactor: 유틸리티 생성
    editBridge: 생성된 bridge를 출력 형식에 맞게 변경해주는 함수
    deepFreeze: Object.freeze를 내부 객체까지 적용시키는 함수

---
+++ src/Utilities/index.js
@@ -0,0 +1,12 @@
+const editBridge = (bridge) => `[${bridge.slice(0, bridge.length - 1)}]`;
+
+const deepFreeze = (targetObj) => {
+  Object.keys(targetObj).forEach((key) => {
+    if (typeof key === 'object') {
+      deepFreeze(targetObj[key]);
+    }
+  });
+  return Object.freeze(targetObj);
+};
+
+module.exports = { editBridge, deepFreeze };
