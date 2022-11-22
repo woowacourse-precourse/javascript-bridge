1f6fd6afd718176cbb61c57a771973956c38697f
leeseooo <leeseoooo@naver.com>
2022-11-22 12:17:30 +0900

    feat: 다리 길이 입력받기

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ const { Console } = require("@woowacourse/mission-utils");
 class App {
   play() {
     this.start();
+    this.readBridgeSize();
   }
   start() {
     Console.print("다리 건너기 게임을 시작합니다.\n");
+  }
+  readBridgeSize() {
+    InputView.readBridgeSize((size) => {
+      this.gameManager.size = +size;
+    });
   }
 }
 
