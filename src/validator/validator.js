b8aa04a2e88fac4b14c64172db1b12b0fdc78093
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-22 18:27:32 +0900

    fix: 입력값 오류시 메세지만 throw

--- src/validator/validator.js
+++ src/validator/validator.js
@@ -1,29 +1,41 @@
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
-// InputView 에서만 MissionUtils의 Console.readLine() 을 이용해 사용자의 입력을 받을 수 있다.
 const ERROR_MESSAGES = require('../constants/ErrorMessages');
-const { BRIDGE } = require('../constants/constants');
+const { BRIDGE, COMMAND } = require('../constants/constants');
 
 const validator = {
   validateBridgeSize(size) {
     if (!this.isNumber(size)) {
-      throw new Error(ERROR_MESSAGES.NAN);
+      throw ERROR_MESSAGES.NAN;
     }
     if (!this.isValidSize(size)) {
-      throw new Error(ERROR_MESSAGES.SIZE);
+      throw ERROR_MESSAGES.SIZE;
     }
   },
   validateMove(char) {
-    if (char !== BRIDGE.UP_CHAR && char !== BRIDGE.DOWN_CHAR) {
-      throw new Error(ERROR_MESSAGES.MOVING);
+    switch (char) {
+      case BRIDGE.UP_CHAR:
+      case BRIDGE.DOWN_CHAR:
+        return true;
+      default:
+        throw ERROR_MESSAGES.MOVING;
     }
+  },
+  validateCommand(command) {
+    switch (command) {
+      case COMMAND.RETRY:
+      case COMMAND.QUIT:
+        return true;
+      default:
+        throw ERROR_MESSAGES.COMMAND;
+    }
+  },
+  isSameArr(arr1, arr2) {
+    return JSON.stringify(arr1) === JSON.stringify(arr2);
   },
   isNumber(size) {
     return Number.isInteger(Number(size));
   },
   isValidSize(size) {
-    return BRIDGE.MIN <= Number(size) || Number(size) <= BRIDGE.MAX;
+    return BRIDGE.MIN <= Number(size) && Number(size) <= BRIDGE.MAX;
   },
 };
 
