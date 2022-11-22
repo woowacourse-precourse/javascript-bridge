8fc8cbd460698a0441e28ea8f20bcf35b7b36574
금가경 <gageum0@gmail.com>
2022-11-22 19:11:59 +0900

    fix(bridge) : 오타 수정

--- src/InputView.js
+++ src/InputView.js
@@ -30,16 +30,14 @@ const InputView = {
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving() {
-    readMoving() {
-      MissionUtils.Console.readLine(
-        ServiceMessages.SELECT_SPACE,
-        (upOrDown) => {
-          try {
-            validator.checkUserMoving(upOrDown);
-          } catch (error) {
-            OutputView.printErrorMessages(error);
-            this.readBridgeSize();
-          }
+    MissionUtils.Console.readLine(ServiceMessages.SELECT_SPACE, (upOrDown) => {
+      try {
+        validator.checkUserMoving(upOrDown);
+      } catch (error) {
+        OutputView.printErrorMessages(error);
+        this.readBridgeSize();
+      }
+    });
   },
 
   /**
