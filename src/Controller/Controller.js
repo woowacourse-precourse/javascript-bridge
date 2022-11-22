1cb96bcaf6f625eabd9e01970273efb06ac46b7b
knh6269 <knh6269@gmail.com>
2022-11-22 23:49:35 +0900

    feat(Controller): 생성자 함수

--- src/Controller/Controller.js
+++ src/Controller/Controller.js
@@ -2,7 +2,11 @@ const InputView = require("../View/InputView");
 const OutputView = require("../View/OutputView");
 
 const Controller = {
-   
+    play(bridgeGame) {
+        OutputView.printStart();
+        Controller.makeBridge(bridgeGame);
+    },
+
 }
 
 module.exports = Controller;
