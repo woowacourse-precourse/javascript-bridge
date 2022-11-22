66aac2bc97797056329d3ad800ffd0ae283ed08b
dladncks1217 <dlaxodud1217@gmail.com>
2022-11-22 23:09:10 +0900

    style(InputView): 주석 스타일 수정

--- src/UI/InputView.js
+++ src/UI/InputView.js
@@ -2,22 +2,18 @@ const { Console } = require('@woowacourse/mission-utils');
 const { GAME_MESSAGE } = require('../constants/constants');
 
 // 사용자로부터 입력을 받는 역할을 한다.
-
 const InputView = {
   //  다리의 길이를 입력받는다.
-
   readBridgeSize(callback) {
     Console.readLine(GAME_MESSAGE.SET_BRIDGE_LENGTH, callback);
   },
 
   // 사용자가 이동할 칸을 입력받는다.
-
   readMoving(callback) {
     Console.readLine(GAME_MESSAGE.MOVE_CHOICE, callback);
   },
 
   // 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-
   readGameCommand(callback) {
     Console.readLine(GAME_MESSAGE.GAME_RETRY, callback);
   },
