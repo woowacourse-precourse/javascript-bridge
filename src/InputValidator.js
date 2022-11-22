aabbe770743b04c948cae7dfcfed4bb602f8e65f
Wooseong Kim <kimwooseong8313@gmail.com>
2022-11-22 18:17:09 +0900

    feat: 이동할 칸 유효성 검사

--- src/InputValidator.js
+++ src/InputValidator.js
@@ -1,3 +1,5 @@
+const command = require("./utils/command");
+
 const InputValidator = {
   isValidLength(input) {
     if (!Number.isInteger(+input)) {
@@ -7,6 +9,10 @@ const InputValidator = {
       return false;
     }
     return 3 <= +input && +input <= 20;
+  },
+
+  isValidStep(input) {
+    return input === command.UP || input === command.DOWN;
   },
 };
 
