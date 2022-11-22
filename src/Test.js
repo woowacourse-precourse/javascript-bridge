de90da765afc4f019610cf5c234087b8686ff9d2
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2022-11-22 21:27:52 +0900

    rename : 파일위치 변경

--- src/Test.js
+++ src/Test.js
@@ -1,6 +1,6 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-const App = require("../src/App");
-const BridgeMaker = require("../src/BridgeMaker");
+const App = require("./App");
+const BridgeMaker = require("./BridgeMaker");
 
 const mockQuestions = (answers) => {
   MissionUtils.Console.readLine = jest.fn();
