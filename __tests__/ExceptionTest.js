6fe6d2b2346664a9cdb3d4a70178c25e7a152ed1
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-11-22 17:58:20 +0900

    test(ExceptionTest): 예외 처리를 Exception 객체에서 검사하는 것으로 변경
    Changed
      before: bridgeController의 객체를 생성하여 예외처리 함수를 각각 테스트
      after: Exception 객체 내의 메서드를 테스트
      -> 변경 이유 : 기존의 bridgeController의 예외처리 함수 내에서 error을 throw하는지 테스트했으나, try-catch문으로 변경하면서 catch문에서 error을 handling 해줌.
      따라서 테스트하기 쉬운 Exception 객체의 메서드로 변경.

--- __tests__/ExceptionTest.js
+++ __tests__/ExceptionTest.js
@@ -1,14 +1,14 @@
 const BridgeController = require('../src/BridgeController');
 const InputView = require('../src/InputView');
 const OutputView = require('../src/OutputView');
+const Exception = require('../src/utils/Exceptions');
 
 const views = {
     inputView: InputView,
     outputView: OutputView
-  }
+}
 
 describe('1. 다리 길이 입력에 대한 예외처리', () => {
-    const controller = new BridgeController(views);
     test.each([
         ',',
         '1ㄷ',
@@ -17,13 +17,12 @@ describe('1. 다리 길이 입력에 대한 예외처리', () => {
         'abc'
     ])('다리 길이의 입력값 %s일 때 에러를 반환한다.', (input) => {
         expect(() => { 
-             controller.bridgeSizeException(input)
-        } ).toThrow();
+            Exception.bridgeSizeException(input)
+       } ).toThrow();
     })
 })
 
 describe('2. 사용자가 이동할 칸 입력에 대한 예외처리', () => {
-    const controller = new BridgeController(views);
     test.each([
         ',',
         '1ㄷ',
@@ -32,7 +31,21 @@ describe('2. 사용자가 이동할 칸 입력에 대한 예외처리', () => {
         'abc'
     ])('사용자가 이동할 칸 입력값 %s일 때 에러를 반환한다.', (input) => {
         expect(() => { 
-             controller.movingException(input)
+            Exception.movingException(input)
+        } ).toThrow();
+    })
+})
+
+describe('3. 사용자의 재시작 여부 입력에 대한 예외처리', () => {
+    test.each([
+        ',',
+        '1ㄷ',
+        '40',
+        ' ',
+        'abc'
+    ])('사용자의 재시작 여부 입력이 %s일 때 에러를 반환한다.', (input) => {
+        expect(() => { 
+            Exception.commandException(input)
         } ).toThrow();
     })
 })
