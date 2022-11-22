1d8964e594cbb5c26e244f398ffb6f82eefe8327
Pengoose <73521518+pengooseDev@users.noreply.github.com>
2022-11-22 17:55:16 +0900

    feat: 반복적인 입력오류를 동반하며 한 번에 클리어하는 테스트케이스 추가

--- __tests__/BridgeTest.js
+++ __tests__/BridgeTest.js
@@ -1,24 +1,27 @@
 const { convertBlueprintToBridge, createBlueprint } = require('../src/utils/bridgeHandler');
 const {generateRandomNumber} = require('../src/utils/bridgeHandler')
 
+const createBridge = (size) => {
+  const blueprintArray = createBlueprint(size);
+  const blueprintBridge = blueprintArray.map(() => generateRandomNumber());
+  const bridge = convertBlueprintToBridge(blueprintBridge);
+  return bridge;
+}
+
 describe("다리 생성 테스트", () => {
   test("다리의 길이를 확인한다", () => {
     const size = 5;
-    const blueprintArray = createBlueprint(size);
-    const blueprintRow = blueprintArray.map(() => generateRandomNumber());
-    const row = convertBlueprintToBridge(blueprintRow);
+    const bridge = createBridge(size)
 
-    expect(row.length).toEqual(5);
+    expect(bridge.length).toEqual(5);
   });
 
-  test("0과 1로 이루어진 배열인지 확인한다", () => {
+  test("U과 D로 이루어진 배열인지 확인한다", () => {
     const size = 100;
-    const blueprintArray = createBlueprint(size);
-    const blueprintRow = blueprintArray.map(() => generateRandomNumber());
-    const row = convertBlueprintToBridge(blueprintRow);
-    row.push("U");
-    row.push("D");
+    const bridge = createBridge(size);
+    bridge.push("U");
+    bridge.push("D");
 
-    expect(new Set(row).size).toEqual(2);
+    expect(new Set(bridge).size).toEqual(2);
   });
 });
