68afa24aa61863561fcebf86bb139fc71a43810f
jin-Pro <dnjun2@ajou.ac.kr>
2022-11-22 17:09:28 +0900

    feat: OutputView Util Func makeMap Func

--- src/Util/OutputView.util.js
+++ src/Util/OutputView.util.js
@@ -15,7 +15,26 @@ const makeMapObj = (answer, input) => {
   }
   return map;
 };
-const makeMap = (input, obj) => {};
+const makeMap = (input, obj) => {
+  const length = Math.min(input.length, Object.keys(obj).length);
+  const firstFloor = [];
+  const secondFloor = [];
+  for (let i = 0; i < length; i++) {
+    if (validationMakeMapObjProps(input[i])) {
+      throw new Error("[ERROR] input은 0과 1로 이루어져야합니다.");
+    }
+    if (typeof obj[i] !== "boolean") {
+      throw new Error("[ERROR] obj의 value는 boolean 타입이어야합니다.");
+    }
+    const target = input[i] === 0 ? firstFloor : secondFloor;
+    const other = input[i] === 0 ? secondFloor : firstFloor;
+    target.push(obj[i] ? " O " : " X ");
+    other.push("   ");
+  }
+
+  return `[${firstFloor.join("|")}]
+[${secondFloor.join("|")}]`;
+};
 
 exports.validationMakeMapObjProps = validationMakeMapObjProps;
 exports.makeMapObj = makeMapObj;
