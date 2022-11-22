b26946bd6216e35434da8a794a32427cb1653a5e
Leejin-Yang <ejin1996@gmail.com>
2022-11-22 17:16:05 +0900

    refactor: App 클래스 수정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 const BridgeGameController = require('./controllers/BridgeGameController');
 
+/**
+ * 어플리케이션 클래스
+ * @class
+ */
 class App {
+  /**
+   * 컨트롤러 타입
+   * @type {BridgeGameController}
+   */
   #controller;
 
-  constructor() {
-    this.#controller = new BridgeGameController();
-  }
-
   play() {
+    this.#controller = new BridgeGameController();
     this.#controller.play();
   }
 }
