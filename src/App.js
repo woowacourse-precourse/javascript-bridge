40b2d0a70dd63e13e6787373755b7d1f790c3986
HeoJiye <heoze1031@gmail.com>
2022-11-22 12:49:48 +0900

    refactor: App에서 BridgeGame 필드 값을 OutputView에 넘기도록 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,65 @@
-class App {
-  play() {}
-}
+const BridgeGame = require("./BridgeGame");
+const InputView = require("./InputView");
+const OutputView = require("./OutputView");
 
+class App {
+  #bridgeGame;
+
+  play() {
+      console.log('다리 건너기 게임을 시작합니다.');
+
+      this.startBridgeGame();
+  }
+
+  startBridgeGame() {
+    InputView.readBridgeSize((size) => {
+      this.#bridgeGame = new BridgeGame(size);
+
+      this.playBridgeGame();
+    });
+  }
+
+  playBridgeGame() {
+    InputView.readMoving((moving) => {
+      this.#bridgeGame.move(moving);
+      
+      this.playBridgeGameCallback();
+    })
+  }
+
+  playBridgeGameCallback() {
+    switch (this.#bridgeGame.checkResult()) {
+      case -1:
+        OutputView.printMap(this.#bridgeGame.getBridge(), this.#bridgeGame.getPath());
+        this.playBridgeGame();
+        break;
+      case 0:
+        InputView.readGameCommand((command) => readGameCommandCallback(command));
+        break;
+      case 1:
+        OutputView.printResult(
+          this.#bridgeGame.getBridge(), 
+          this.#bridgeGame.getPath(),
+          1,
+          this.#bridgeGame.getTryNum());
+        break;
+    }
+  }
+  
+  readGameCommandCallback(command) {
+    if (command === InputView.INPUT_QUIT) {
+      OutputView.printResult(
+        this.#bridgeGame.getBridge(), 
+        this.#bridgeGame.getPath(),
+        0,
+        this.#bridgeGame.getTryNum());
+      return;
+    }
+    if (command === InputView.INPUT_RESTART) {
+      this.#bridgeGame.retry();
+      this.playBridgeGame();
+    }
+  }
+}
+      
 module.exports = App;
--- src/OutputView.js
+++ src/OutputView.js
@@ -12,8 +12,8 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap(bridgeGame) {
-    const map = getMap(bridgeGame.getBridge(), bridgeGame.getPath());
+  printMap(bridge, path) {
+    const map = getMap(bridge, path);
 
     MissionUtils.Console.print(getMapLine(map, InputView.INPUT_UP_MOVING));
     MissionUtils.Console.print(getMapLine(map, InputView.INPUT_DOWN_MOVING));
@@ -24,12 +24,12 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult(bridgeGame, result) {
+  printResult(bridge, path, result, tryNum) {
     MissionUtils.Console.print('최종 게임 결과');
-    printMap(bridgeGame.getBridge(), bridgeGame.getPath());
+    OutputView.printMap(bridge, path);
 
     MissionUtils.Console.print('게임 성공 여부: ' + OutputView.RESULT_TEXT[result]);
-    MissionUtils.Console.print('총 시도한 횟수: ' + bridgeGame.getTryNum());
+    MissionUtils.Console.print('총 시도한 횟수: ' + tryNum);
   },
 };
 
