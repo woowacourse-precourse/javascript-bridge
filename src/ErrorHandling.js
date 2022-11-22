a10f54b9348a691045c5ec943f843177a56448c5
박겸영 <yong_drg@naver.com>
2022-11-22 17:45:15 +0900

    feat(ErrorMessage): 다리 길이 입력값이 3 이상 20 이하의 수가 아니면 예외 처리하는 기능 추가

--- src/ErrorHandling.js
+++ src/ErrorHandling.js
@@ -1,4 +1,5 @@
-const { BRIDGE_SIZE_NATURAL_NUMBER } = require('./ErrorMessage');
+const { MIN_BRIDGE_SIZE, MAX_BRIDGE_SIZE } = require('./Command');
+const { BRIDGE_SIZE_NATURAL_NUMBER, BRIDGE_SIZE_RANGE } = require('./ErrorMessage');
 
 const ErrorHandling = {
   throwError(isInvalid, message) {},
@@ -6,6 +7,10 @@ const ErrorHandling = {
   validateBridgeSize(size) {
     const isNotNaturalNumber = !(/^\d+$/.test(size) && Number(size));
     ErrorHandling.throwError(isNotNaturalNumber, BRIDGE_SIZE_NATURAL_NUMBER);
+
+    size = Number(size);
+    const isOutOfRange = size < MIN_BRIDGE_SIZE || size > MAX_BRIDGE_SIZE;
+    ErrorHandling.throwError(isOutOfRange, BRIDGE_SIZE_RANGE);
   },
 };
 
