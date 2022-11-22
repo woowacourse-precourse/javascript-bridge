67ab63cb10da1a7426f89d84cfbdf82a52db4664
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-22 18:20:18 +0900

    feat: 앱 시작시 게임 인스턴스 생성 및 다리길이 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+const BridgeGame = require('./BridgeGame');
+const InputView = require('./InputView');
+const OutputView = require('./OutputView');
+
 class App {
-  play() {}
+  play() {
+    const game = new BridgeGame();
+
+    OutputView.printStartMessage();
+    InputView.readBridgeSize(game);
+  }
 }
+
+new App().play();
 
 module.exports = App;
