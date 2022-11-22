4374fc983fad510dfe5eec844dcf98b4f5eda18e
고동현 <dhjk35@naver.com>
2022-11-22 19:39:09 +0900

    chore(constants): 잚못된 내보내기 설정 수정

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
-const { DIRECTION } = require('./direction');
-const { COMMAND_OPTION } = require('./command');
-const { ERROR_MESSAGE } = require('./errorMessage');
+const DIRECTION = require('./direction');
+const COMMAND_OPTION = require('./command');
+const ERROR_MESSAGE = require('./errorMessage');
 
 module.exports = {
   DIRECTION,
