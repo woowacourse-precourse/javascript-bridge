a4426d072d09892f424ab6edd9d4b27c49abb0b7
김아현 <dhflxhdxhd@gmail.com>
2022-11-22 19:56:56 +0900

    Feat : 다리 길이 범위 안에 있는지 확인, 숫자인지 확인

--- src/Exception.js
+++ src/Exception.js
@@ -1,9 +1,24 @@
-
+const { Console } = require("@woowacourse/mission-utils");
 class Exception{
 
     checkBridgeSize(size){
-        if (size < 3 || size > 20) throw new Error("[ERROR] 다리 길이는 3이상 20이하여야합니다.");
+        try{
+            if(this.isNotNumber(size)) throw new Error("[ERROR] 숫자를 입력해주세요");
+            if(this.isNotInRange(size)) throw new Error("[ERROR] 다리 길이는 3이상 20이하여야 합니다.");
+        }catch(error){
+            Console.print(error.message);
+            return true;
+        }
     }
+
+    isNotInRange(size){
+        return size < 3 || size > 20;
+    }
+
+    isNotNumber(number){
+        return (isNaN(number));
+    }
+
 
     checkSpace(space){
         if (space !== "U" || space !== "D") throw new Error("[ERROR] U 또는 D를 입력해주세요.");
