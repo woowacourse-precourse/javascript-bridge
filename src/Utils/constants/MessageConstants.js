1e303cbab77e7b43439c7ccb32d9e16e850a28c1
Subin Jo <sbjo1202@naver.com>
2022-11-22 14:19:28 +0900

    refactor: Object.freeze 추가, 객체를 동결하여 변경 방지

--- src/Utils/constants/MessageConstants.js
+++ src/Utils/constants/MessageConstants.js
@@ -12,21 +12,21 @@ const OUTPUT = Object.freeze({
     RESULT_TEXT: '최종 게임 결과',
 });
 
-const MAP = {
+const MAP = Object.freeze({
     PRINT_MAP: (map) => `[ ${map.join(' | ')} ]`
-}
+});
 
-const RESULT = {
+const RESULT = Object.freeze({
     SUCCESS_OR_FAIL: (checkForSuccess) => `게임 성공 여부: ${checkForSuccess}`,
     TRY_COUNT: (tryCount) => `총 시도한 횟수: ${tryCount}`
-}
+});
 
-const ERROR = {
+const ERROR = Object.freeze({
     NOT_NUMBER: '[ERROR] 다리 길이는 숫자여야 합니다.',
     NOT_INTEGER: '[ERROR] 다리 길이는 실수가 아닌 정수여야 합니다.',
     RANGE_IS_WRONG: '[ERROR] 다리 길이는 3 이상 20 이하의 숫자여야 합니다.',
     PLEASE_INPUT_U_OR_D: '[ERROR] 이동할 칸을 선택하기 위해선 \'U\' 또는 \'D\'를 입력해야 합니다.',
-    PLEASE_INPUT_R_OR_Q: '[ERROR] 재시도하려면 \'R\'를, 종료하려면 \'Q\'를 입력해야 합니다.'
-}
+    PLEASE_INPUT_R_OR_Q: '[ERROR] 재시도하려면 \'R\'를, 종료하려면 \'Q\'를 입력해야 합니다.',
+});
 
 module.exports = { INPUT, OUTPUT, MAP, RESULT, ERROR };
