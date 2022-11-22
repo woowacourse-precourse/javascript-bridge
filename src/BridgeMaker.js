83083264aa95257719270683b8584ea6b18b6137
knh6269 <knh6269@gmail.com>
2022-11-22 23:49:17 +0900

    feat(BridgeMaker): 다리를 만드는 함수

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,5 +1,16 @@
 const BridgeMaker = {
-  
-};
+  makeBridge(size, generateRandomNumber) {
+    const sample = [];
+    for (let i = 0; i < size; i++) {
+      const randNumber = generateRandomNumber();
+      if (Number(randNumber)) {
+        sample.push('U');
+        continue
+      }
+      sample.push('D');
+    }
+    return sample;
+  },
+}
 
 module.exports = BridgeMaker;
