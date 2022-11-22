0de45f443a4aac1f6f893db422603fe7a815809f
tkdrb12 <tkdrb15@gmail.com>
2022-11-22 22:13:26 +0900

    test : 예외처리를 테스트 하는 기능 추가

--- __tests__/BridgeMakerTest.js
+++ __tests__/BridgeMakerTest.js
@@ -3,12 +3,12 @@ const BridgeMaker = require("../src/BridgeMaker");
 
 describe("BridgeMaker 클래스 테스트", () => {
   test("다리 생성 테스트", () => {
-      const randomNumbers = ["0", "1", "0", "1"];
-      const mockGenerator = randomNumbers.reduce((acc, number) => {
-        return acc.mockReturnValueOnce(number);
-      }, jest.fn());
-  
-      const bridge = BridgeMaker.makeBridge(4, mockGenerator);
-      expect(bridge).toEqual(["D", "U", "D", "U"]);
-    });
+    const randomNumbers = ["0", "1", "0", "1"];
+    const mockGenerator = randomNumbers.reduce((acc, number) => {
+      return acc.mockReturnValueOnce(number);
+    }, jest.fn());
+
+    const bridge = BridgeMaker.makeBridge(4, mockGenerator);
+    expect(bridge).toEqual(["D", "U", "D", "U"]);
+  });
 });
