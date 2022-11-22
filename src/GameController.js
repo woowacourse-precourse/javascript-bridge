4521812a45b0537ad84cc841a87737529ff9b275
cjm0611 <81799492+cjm0611@users.noreply.github.com>
2022-11-22 18:11:47 +0900

    refactor(GameController): view, game을 private class field로 관리

--- src/GameController.js
+++ src/GameController.js
@@ -6,36 +6,34 @@ const BridgeGame = require('./BridgeGame');
 const { BRIDGE_CONSTANTS } = require('./GameConstants');
 
 class GameController {
-  constructor() {
-    this.inputView = InputView;
-    this.outputView = OutputView;
-    this.game = new BridgeGame();
-  }
+  #inputView = InputView;
+  #outputView = OutputView;
+  #game = new BridgeGame();
 
   start() {
-    this.outputView.printStartMessage();
+    this.#outputView.printStartMessage();
     this.makeBridge();
   }
 
   makeBridge() {
     const onDeliverySizeInputted = (brigeSize) => {
       const bridge = BridgeMaker.makeBridge(brigeSize, BridgeRandomNumberGenerator.generate);
-      this.game.setBridge(bridge);
+      this.#game.setBridge(bridge);
       const START_INDEX = 0;
       this.crossBridge(START_INDEX);
     };
 
-    this.inputView.readBridgeSize(onDeliverySizeInputted);
+    this.#inputView.readBridgeSize(onDeliverySizeInputted);
   }
 
   crossBridge(index) {
     const onDeliveryMoving = (moving) => {
-      const IS_CORRECT_MOVE = this.game.move(moving, index);
-      this.outputView.printMap(this.game.getMaps());
+      const IS_CORRECT_MOVE = this.#game.move(moving, index);
+      this.#outputView.printMap(this.#game.getMaps());
       this.resultOfCrossing(IS_CORRECT_MOVE, index);
     };
 
-    this.inputView.readMoving(onDeliveryMoving);
+    this.#inputView.readMoving(onDeliveryMoving);
   }
 
   resultOfCrossing(IS_CORRECT_MOVE, index) {
@@ -48,7 +46,7 @@ class GameController {
   }
 
   nextAction(index) {
-    if (index === this.game.getBridgeLength() - 1) {
+    if (index === this.#game.getBridgeLength() - 1) {
       this.finalGameResult(true);
       return;
     }
@@ -61,7 +59,7 @@ class GameController {
       this.retryOrQuit(command);
     };
 
-    this.inputView.readGameCommand(onDeliveryCommand);
+    this.#inputView.readGameCommand(onDeliveryCommand);
   }
 
   retryOrQuit(command) {
@@ -74,15 +72,15 @@ class GameController {
   }
 
   retryGame() {
-    this.game.retry();
+    this.#game.retry();
     const START_INDEX = 0;
     this.crossBridge(START_INDEX);
   }
 
   finalGameResult(IS_SUCCESS) {
-    const maps = this.game.getMaps();
-    const totalAttempts = this.game.getNumberOfAttempts();
-    this.outputView.printResult(maps, IS_SUCCESS, totalAttempts);
+    const maps = this.#game.getMaps();
+    const totalAttempts = this.#game.getNumberOfAttempts();
+    this.#outputView.printResult(maps, IS_SUCCESS, totalAttempts);
   }
 }
 
