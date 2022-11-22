6d57f294d3ad7e5fdcf66b793c1c74b00ad01997
c0c0pang <coc1789@naver.com>
2022-11-22 14:21:22 +0900

    feat: 재시작 여부에 대해 R이라면 모두 초기화

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -64,7 +64,17 @@ class BridgeGame {
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  retry() {};
+  retry(COMMAND) {
+    if (COMMAND === "R") {
+      this.up = [];
+      this.down = [];
+      this.cnt = -1;
+      this.tryCnt+=1;
+      return;
+    };
+    this.cnt=2;
+    this.success = "실패";
+  };
 }
 
 module.exports = BridgeGame;
