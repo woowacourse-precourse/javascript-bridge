1727b00d7b3cfd4f33f51396542cdc82be63a9bd
seoye0ng <101791501+seoye0ng@users.noreply.github.com>
2022-11-22 21:17:49 +0900

    fix : 다리 생성 오류 수정

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -11,6 +11,8 @@ class BridgeGame {
   bridge = BridgeMaker.makeBridge();
   userStep = [];
   matchMap = [];
+  upBridge = [];
+  downBridge = [];
 
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -18,8 +20,8 @@ class BridgeGame {
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   move() {
-    let upBridge = [];
-    let downBridge = [];
+    //let upBridge = [];
+    //let downBridge = [];
     for (let i = 0; i < this.userStep.length; i++) {
       if (
         this.userStep[i] === 'U' &&
@@ -35,8 +37,9 @@ class BridgeGame {
         downBridge.push(this.matchMap[i]);
         upBridge.push(' ');
       }
+      OutputView.printMap();
     }
-    return [upBridge, downBridge];
+    //return [upBridge, downBridge];
   }
 
   // 일단 O인지 X인지 거르기
