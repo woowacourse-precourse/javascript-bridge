47fbe2e3569a6daa67ea45900c21437df896c501
Suyoung <jsy8492@gmail.com>
2022-11-22 13:52:05 +0900

    refactor(InputView): 불필요한 파라미터와 함수 호출문 삭제

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -8,9 +8,7 @@ const InputView = {
    * @param {function(string): void} callback 입력값을 전달할 콜백 함수
    */
   readBridgeSize(callback) {
-    Console.readLine('\n다리의 길이를 입력해주세요.\n', bridgeSizeInput => {
-      callback(bridgeSizeInput);
-    });
+    Console.readLine('\n다리의 길이를 입력해주세요.\n', callback);
   },
 
   /**
@@ -20,9 +18,7 @@ const InputView = {
   readMoving(callback) {
     Console.readLine(
       '\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
-      moving => {
-        callback(moving);
-      },
+      callback,
     );
   },
 
@@ -33,9 +29,7 @@ const InputView = {
   readGameCommand(callback) {
     Console.readLine(
       '\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
-      gameCommand => {
-        callback(gameCommand);
-      },
+      callback,
     );
   },
 };
