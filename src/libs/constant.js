1563df005968e53a816247ac2372c411289cb984
jiho-bae <jiho@dgu.ac.kr>
2022-11-22 12:43:03 +0900

    Refactor: 상수객체 동결

--- src/libs/constant.js
+++ src/libs/constant.js
@@ -1,4 +1,4 @@
-const MSG = {
+const MSG = Object.freeze({
   startGame: '다리 건너기 게임을 시작합니다.\n',
   endGame: '최종 게임 결과',
   inputBridgeSize: '다리의 길이를 입력해주세요.\n',
@@ -9,37 +9,37 @@ const MSG = {
 
     return `게임 성공 여부: ${resultTxt}\n총 시도한 횟수: ${cnt}`;
   },
-};
+});
 
-const ERROR_MSG = {
+const ERROR_MSG = Object.freeze({
   invalidBridgeSize: '[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.',
   invalidMoveDirection: '[ERROR] 이동할 칸은 U(위) 혹은 D(아래)여야 합니다.',
   invalidGameCmd: '[ERROR] 입력은 R(재시도) 혹은 Q(종료)여야 합니다.',
-};
+});
 
-const DIRECTION = {
+const DIRECTION = Object.freeze({
   up: 'U',
   down: 'D',
   correct: 'O',
   wrong: 'X',
-};
+});
 
-const ROW_IDX = {
+const ROW_IDX = Object.freeze({
   U: 0,
   D: 1,
-};
+});
 
 const EMPTY = ' ';
 
-const NEXT_STEP = {
+const NEXT_STEP = Object.freeze({
   correctMove: 'correctMove',
   wrongMove: 'wrongMove',
   endGame: 'endGame',
-};
+});
 
-const GAME_CMD = {
+const GAME_CMD = Object.freeze({
   restart: 'R',
   quit: 'Q',
-};
+});
 
 module.exports = { MSG, ERROR_MSG, DIRECTION, ROW_IDX, EMPTY, NEXT_STEP, GAME_CMD };
