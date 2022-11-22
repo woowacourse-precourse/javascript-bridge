71d22ce9771424a3a5e5673359f2a426d7acbed0
woo-jk <201702042@o.cnu.ac.kr>
2022-11-22 16:06:40 +0900

    refactor: View 객체에서 MissionUtils 사용하는 부분 함수로 감싸도록 수정

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -4,12 +4,16 @@ const { MESSAGE_OUTPUT, CLAER, FAILURE } = require("../utils/messages");
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 const OutputView = {
+  print(message) {
+    MissionUtils.Console.print(message);
+  },
+
   printOpening() {
-    MissionUtils.Console.print(`${MESSAGE_OUTPUT.GAME_OPENING}\n`);
+    this.print(`${MESSAGE_OUTPUT.GAME_OPENING}\n`);
   },
 
   printErrorMessage(message) {
-    MissionUtils.Console.print(message);
+    this.print(message);
   },
   /**
    * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -17,8 +21,8 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printMap({ upperBridgeMap, lowerBridgeMap }) {
-    MissionUtils.Console.print(upperBridgeMap);
-    MissionUtils.Console.print(lowerBridgeMap);
+    this.print(upperBridgeMap);
+    this.print(lowerBridgeMap);
   },
 
   /**
@@ -27,10 +31,10 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printResult(map, isClaer, tryCount) {
-    MissionUtils.Console.print(`${MESSAGE_OUTPUT.GAME_RESULT}`);
+    this.print(`${MESSAGE_OUTPUT.GAME_RESULT}`);
     this.printMap(map);
-    MissionUtils.Console.print(`${MESSAGE_OUTPUT.IS_GAME_CLEAR} ${isClaer ? CLAER : FAILURE}`);
-    MissionUtils.Console.print(`${MESSAGE_OUTPUT.TRY_COUNT} ${tryCount}`);
+    this.print(`${MESSAGE_OUTPUT.IS_GAME_CLEAR} ${isClaer ? CLAER : FAILURE}`);
+    this.print(`${MESSAGE_OUTPUT.TRY_COUNT} ${tryCount}`);
   },
 };
 
