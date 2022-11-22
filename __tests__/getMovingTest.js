908fc6fb4640b4dee9e48fb3f91a6044d2ec66e7
Subin Jo <sbjo1202@naver.com>
2022-11-22 16:13:32 +0900

    refactor: 반복적인 테스트 코드 each 사용하여 리팩토링

--- __tests__/getMovingTest.js
+++ __tests__/getMovingTest.js
@@ -1,5 +1,6 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const GameController = require("../src/Controller/GameController");
+const Validation = require("../src/Utils/Validation");
 
 const mockQuestions = (answers) => {
   MissionUtils.Console.readLine = jest.fn();
@@ -36,15 +37,13 @@ const expectLogContains = (received, logs) => {
 };
 
 describe('이동할 칸 입력 예외 테스트(U 또는 D가 아닌 다른 문자 입력)', () => {
-  test('소문자 u를 입력한 경우', () => {
-    runException(['u']);
-  });
 
-  test('특수문자를 입력한 경우', () => {
-    runException(['?']);
-  });
-
-  test('숫자를 입력한 경우', () => {
-    runException(['5']);
-  });
+  test.each([['u'], ['?'], ['5']])(
+    '소문자 u를 입력한 경우, 특수문자를 입력한 경우, 숫자를 입력한 경우',
+    (inputMoving) => {
+      expect(() => {
+        Validation.checkMoving(inputMoving);
+      }).toThrow('[ERROR]');
+    }
+  );
 });
