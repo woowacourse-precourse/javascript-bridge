eb9fed58ddf41465fc4d0b50853ba51e83528a19
ho-ji <wkdd0205@gmail.com>
2022-11-22 20:03:24 +0900

    feat: Add create bridge game
    다리 길이에 해당하는 다리 모양을 생성하여 새로운 다리 건너기 게임을 생성함

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 const OutputView = require("../src/OutputView");
 const InputView = require("../src/InputView");
+const BridgeRandomNumberGenerator = require("../src/BridgeRandomNumberGenerator");
+const BridgeMaker = require("../src/BridgeMaker");
+const BridgeGame = require("../src/BridgeGame");
+
 class App {
+  #bridgeGame;
   play() {
     OutputView.printStart();
     const bridgeSize = InputView.readBridgeSize();
+    this.#bridgeGame = new BridgeGame(
+      BridgeMaker.makeBridge(bridgeSize, BridgeRandomNumberGenerator.generate)
+    );
   }
 }
 
