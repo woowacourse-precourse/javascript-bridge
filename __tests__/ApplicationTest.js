1c9cd61ef028d0f2785f73e484206fedac465b0e
letsjo <hyunoh.jo@gmail.com>
2022-11-22 14:47:10 +0900

    test(*): 테스트파일 randomNumber 숫자형으로 변경

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -55,7 +55,6 @@ describe('다리 건너기 테스트', () => {
     const randomNumbers = [1, 0, 0];
     const mockGenerator = randomNumbers
       .reduce((acc, number) => acc.mockReturnValueOnce(number), jest.fn());
-
     const bridge = BridgeMaker.makeBridge(3, mockGenerator);
     expect(bridge).toEqual(['U', 'D', 'D']);
   });
