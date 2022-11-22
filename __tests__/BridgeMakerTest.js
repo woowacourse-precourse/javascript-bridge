0ba48e3f90bf5fb3089d538adbe9a1b276e11215
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-22 14:58:59 +0900

    test(BridgeMaker): 각 테스트 케이스의 .each 배열 변수 분리로 가독성 개선

--- __tests__/BridgeMakerTest.js
+++ __tests__/BridgeMakerTest.js
@@ -2,7 +2,13 @@ const { makeBridge } = require('../src/BridgeMaker.js');
 const { generate } = require('../src/utils/BridgeRandomNumberGenerator.js');
 
 describe('BridgeMaker 객체 테스트', () => {
-  test.each([[{ size: 3, generate }], [{ size: 4, generate }], [{ size: 5, generate }]])(
+  const input = [
+    { size: 3, generate },
+    { size: 4, generate },
+    { size: 5, generate },
+  ];
+
+  test.each(input)(
     'makeBridge메서드의 return값은 Array의 instance여야 한다.',
     ({ size, generate }) => {
       const result = makeBridge(size, generate);
@@ -11,7 +17,7 @@ describe('BridgeMaker 객체 테스트', () => {
     }
   );
 
-  test.each([[{ size: 3, generate }], [{ size: 4, generate }], [{ size: 5, generate }]])(
+  test.each(input)(
     'makeBridge메서드는 주어진 size값을 length로 가지는 배열을 return 해야 한다.',
     ({ size, generate }) => {
       const result = makeBridge(size, generate);
