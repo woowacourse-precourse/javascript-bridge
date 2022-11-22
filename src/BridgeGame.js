b268d1e9ca6b1473ca4276df159e10943b2e6577
HelloHailie <alsltjsals@gmail.com>
2022-11-22 21:27:24 +0900

    feat:현재까지 건넌 다리 출력

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -18,7 +18,18 @@ class BridgeGame {
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(input) {
+    this.paintMap(input);
+    printMap(this.bridgeMap);
+  }
+
+  paintMap(input) {
+    const curIdx = this.bridgeMap.U.length;
+    const check = this.bridge[curIdx] === input ? "O" : "X";
+    this.bridgeMap[input].push(check);
+    const other = input === "U" ? "D" : "U";
+    this.bridgeMap[other].push(" ");
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
