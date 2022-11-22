68d64242d28387402910ae223b4d773021d278bd
imhynni <omelas329@gmail.com>
2022-11-22 19:00:39 +0900

    tets: 재시도 테스트

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -117,4 +117,23 @@ describe("다리 건너기 테스트", () => {
       runException([size, move, command]);
     }
   );
+
+  test("재시도 테스트", () => {
+    const logSpy = getLogSpy();
+    mockRandoms([1, 0, 1]);
+    mockQuestions([3, "U", "U", "R", "U", "D", "U"]);
+
+    const app = new App();
+    app.play();
+
+    const log = getOutput(logSpy);
+    expectLogContains(log, [
+      "최종 게임 결과",
+      "[ O |   | O ]",
+      "[   | O |   ]",
+      "게임 성공 여부: 성공",
+      "총 시도한 횟수: 2",
+    ]);
+    expectBridgeOrder(log, "[ O |   | O ]", "[   | O |   ]");
+  });
 });
