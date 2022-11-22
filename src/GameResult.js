6bcfd577e898b4ceba63ad95676567ad262d3f51
rovxxmjxx <miju3545@gmail.com>
2022-11-22 23:24:51 +0900

    feat(GameResult): History 메시지 만들기

--- src/GameResult.js
+++ src/GameResult.js
@@ -44,6 +44,30 @@ class GameResult {
   showTryCountSummary() {
     return `${MESSAGE.TRY_COUNT}${this.#tryCount}`;
   }
+
+  makeHistory() {
+    const history = this.getResultAsArray().filter(([, value]) => value.player);
+    const sides = Array.from({ length: 2 }, () => []);
+
+    for (let i = 0; i < history.length; i++) {
+      const [up, down] = this.makeHistoryLine(history[i][1]);
+
+      sides[0].push(up);
+      sides[1].push(down);
+    }
+
+    return sides;
+  }
+
+  makeHistoryLine(value) {
+    const { machine, player } = value;
+    const selected = machine === player ? OUTPUT_FORMAT.MATCH : OUTPUT_FORMAT.UNMATCH;
+    const notSelected = OUTPUT_FORMAT.NOT_SELECTED;
+
+    const [up, down] = player === INPUT_FORMAT.UPSIDE ? [selected, notSelected] : [notSelected, selected];
+
+    return [up, down];
+  }
 }
 
 module.exports = GameResult;
