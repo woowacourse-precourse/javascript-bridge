40c38c1d8de0e4f95a432875c4122c3f26ae02ea
Minhye Kang <alsgp9898@naver.com>
2022-11-22 21:33:19 +0900

    style: 주석 제거

--- src/InputView.js
+++ src/InputView.js
@@ -1,27 +1,16 @@
 const {INPUT_QUERY} = require("./Constants")
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 const { Console } = require('@woowacourse/mission-utils');
 
 const InputView = {
-  /**
-   * 다리의 길이를 입력받는다.
-   */
+
   readBridgeSize(callback) {
     Console.readLine(INPUT_QUERY.BRIDGE_SIZE, callback);
   },
 
-  /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
   readMoving(callback) {
     Console.readLine(INPUT_QUERY.MOVING_SPACE, callback);
   },
 
-  /**
-   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-   */
   readGameCommand(callback) {
     Console.readLine(INPUT_QUERY.GAME_COMMAND,callback)
   },
--- src/OutputView.js
+++ src/OutputView.js
@@ -1,26 +1,13 @@
 const { Console } = require('@woowacourse/mission-utils');
 const { OUTPUT, GAME_OUTCOME, LETTER_SIGN } = require('./Constants');
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
+
 const OutputView = {
-  /**
-   * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-   * <p>
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  CONNECTING_CHARACTER: ' | ',
 
   printMap(upperMapArray, lowerMapArray) {
-    Console.print(`[ ${upperMapArray.join(this.CONNECTING_CHARACTER)} ]`);
-    Console.print(`[ ${lowerMapArray.join(this.CONNECTING_CHARACTER)} ]`);
+    Console.print(`[ ${upperMapArray.join(LETTER_SIGN.CONNECTING_LETTER)} ]`);
+    Console.print(`[ ${lowerMapArray.join(LETTER_SIGN.CONNECTING_LETTER)} ]`);
   },
 
-  /**
-   * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-   * <p>
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
   printResult(upperMapArray, lowerMapArray, attemptNumber) {
     const totalMapArray = [...upperMapArray, ...lowerMapArray];
     const outcome = totalMapArray.includes(LETTER_SIGN.WRONG) ? GAME_OUTCOME.FAIL : GAME_OUTCOME.SUCCESS;
