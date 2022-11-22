20d011416eb28c72baf9c2db2c17817637a42cd4
박겸영 <yong_drg@naver.com>
2022-11-22 17:55:35 +0900

    feat(InputView): 예외 발생 시 잘못된 값을 입력한 부분부터 입력 다시 받는 기능 추가

--- src/InputView.js
+++ src/InputView.js
@@ -1,33 +1,45 @@
 const { Console } = require('@woowacourse/mission-utils');
 const { BRIDGE_SIZE, MOVING_COMMAND, GAME_COMMAND } = require('./Message');
+const {
+  validateBridgeSize,
+  validateMovingCommand,
+  validateGameCommand,
+} = require('./ErrorHandling');
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
-  readInput(message, callback) {
-    Console.readLine(message, (input) => callback(input));
+  readInput(message, callback, validate) {
+    Console.readLine(message, (input) => {
+      try {
+        validate(input);
+        callback(input);
+      } catch (error) {
+        InputView.readInput(message, callback, validate);
+      }
+    });
   },
 
   /**
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize(setBridge) {
-    InputView.readInput(BRIDGE_SIZE, setBridge);
+    InputView.readInput(BRIDGE_SIZE, setBridge, validateBridgeSize);
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving(move) {
-    InputView.readInput(MOVING_COMMAND, move);
+    InputView.readInput(MOVING_COMMAND, move, validateMovingCommand);
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand(decideToRetryOrQuit) {
-    InputView.readInput(GAME_COMMAND, decideToRetryOrQuit);
+    InputView.readInput(GAME_COMMAND, decideToRetryOrQuit, validateGameCommand);
   },
 };
 
