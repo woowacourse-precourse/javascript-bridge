d2c8dae32c7a2409f53f3a5253d54be966683839
Sungjae.Kwak <sungjae8970@gmail.com>
2022-11-22 12:41:18 +0900

    feat(App, OutputView): 게임시작 기능 추가
    - 게임 시작 문구 출력 및 게임 시작 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+const OutputView = require('./OutputView');
+const InputView = require('./InputView');
+
 class App {
-  play() {}
+  play() {
+    OutputView.printStart();
+    InputView.readBridgeSize();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
