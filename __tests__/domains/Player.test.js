cc176173b0bb1894c3a8b6670359bb44c3f3e806
solo5star <solo_5@naver.com>
2022-11-22 11:24:34 +0900

    test: Player, Moving 클래스에 대한 테스트 추가

---
+++ __tests__/domains/Player.test.js
@@ -0,0 +1,48 @@
+const { Tile } = require('../../src/constants');
+const Bridge = require('../../src/domains/Bridge');
+const Player = require('../../src/domains/Player');
+
+describe('Player 클래스 테스트', () => {
+  test('기본 동작 테스트', () => {
+    const bridge = new Bridge([Tile.UP, Tile.DOWN, Tile.UP]);
+    const player = new Player(bridge);
+
+    expect(player.isArrived()).toBe(false);
+    expect(player.getNextPosition()).toBe(0);
+
+    expect(player.move(Tile.UP)).toBe(true);
+    expect(player.isArrived()).toBe(false);
+
+    expect(player.move(Tile.DOWN)).toBe(true);
+    expect(player.move(Tile.UP)).toBe(true);
+    expect(player.isArrived()).toBe(true);
+  });
+
+  test('다음 위치를 잘 반환하는지', () => {
+    const bridge = new Bridge([Tile.UP, Tile.DOWN, Tile.UP]);
+    const player = new Player(bridge);
+
+    expect(player.getNextPosition()).toBe(0);
+    player.move(Tile.UP);
+    expect(player.getNextPosition()).toBe(1);
+  });
+
+  test('움직임이 잘 기록되는지', () => {
+    const bridge = new Bridge([Tile.UP, Tile.DOWN, Tile.UP, Tile.UP]);
+    const player = new Player(bridge);
+
+    player.move(Tile.UP);
+    player.move(Tile.DOWN);
+    player.move(Tile.DOWN);
+
+    const movingHistory = player
+      .getMovingHistory()
+      .map((moving) => [moving.getTile(), moving.isSurvived()]);
+
+    expect(movingHistory).toEqual([
+      [Tile.UP, true],
+      [Tile.DOWN, true],
+      [Tile.DOWN, false],
+    ]);
+  });
+});
