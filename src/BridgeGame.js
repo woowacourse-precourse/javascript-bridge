c02e843aae93bca17297c5cba35bd1885b784251
kyw0716 <kyw0716@naver.com>
2022-11-22 17:37:53 +0900

    refactor: 모든 필드 private 필드로 변경

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,43 +1,47 @@
 class BridgeGame {
   #answer;
-  currentPosition;
-  totalTrial;
+  #currentPosition;
+  #totalTrial;
 
   constructor(bridge) {
     this.#answer = bridge;
-    this.currentPosition = 0;
-    this.totalTrial = 1;
+    this.#currentPosition = 0;
+    this.#totalTrial = 1;
   }
 
   move() {
-    if (this.currentPosition < this.#answer.length) this.currentPosition += 1;
+    if (this.#currentPosition < this.#answer.length) this.#currentPosition += 1;
   }
 
   retry() {
-    this.totalTrial += 1;
-    this.currentPosition = 0;
+    this.#totalTrial += 1;
+    this.#currentPosition = 0;
+  }
+
+  get totalTrial() {
+    return this.#totalTrial;
+  }
+
+  get currentPosition() {
+    return this.#currentPosition;
   }
 
   getCorrectDirection() {
-    return this.#answer[this.currentPosition];
+    return this.#answer[this.#currentPosition];
   }
 
   getIsLastPosition() {
-    return this.currentPosition === this.#answer.length;
-  }
-
-  getTotalTrial() {
-    return this.totalTrial;
+    return this.#currentPosition === this.#answer.length;
   }
 
   getCrossState(state) {
     if (state === "failed")
       return [
-        ...this.#answer.filter((v, i) => i < this.currentPosition),
-        `X${this.#answer[this.currentPosition]}`,
+        ...this.#answer.filter((v, i) => i < this.#currentPosition),
+        `X${this.#answer[this.#currentPosition]}`,
       ];
 
-    return this.#answer.filter((v, i) => i < this.currentPosition);
+    return this.#answer.filter((v, i) => i < this.#currentPosition);
   }
 }
 
