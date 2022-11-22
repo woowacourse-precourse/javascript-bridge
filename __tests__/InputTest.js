95b91a4c6c7fcc727e6cdf0fd5c9fd5d418119f3
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-22 20:29:43 +0900

    test: 입력값 테스트 추가 ( 재시작 명령어 )

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -15,19 +15,19 @@ describe('다리 길이 입력값 테스트', () => {
     }).toThrow(ERROR_MESSAGES.NAN);
   });
 
-  test('다리 길이 범위 테스트 1', () => {
+  test('다리 길이 범위 ( 2 )', () => {
     expect(() => {
       validator.validateBridgeSize('2');
     }).toThrow(ERROR_MESSAGES.SIZE);
   });
 
-  test('다리 길이 범위 테스트 2', () => {
+  test('다리 길이 범위 ( 21 )', () => {
     expect(() => {
       validator.validateBridgeSize('21');
     }).toThrow(ERROR_MESSAGES.SIZE);
   });
 
-  test('다리 길이 범위 테스트 3', () => {
+  test('다리 길이 범위 ( 3 )', () => {
     expect(() => {
       validator.validateBridgeSize('3');
     }).toBeTruthy();
@@ -35,21 +35,45 @@ describe('다리 길이 입력값 테스트', () => {
 });
 
 describe('사용자 이동 입력값 테스트', () => {
-  test('사용자 이동 입력값 테스트 ( 문자 )', () => {
-    expect(() => {
-      validator.validateMove('a');
-    }).toThrow(ERROR_MESSAGES.MOVING);
+  test('사용자 이동 입력값  ( a )', () => {
+    expect(() => validator.validateMove('a')).toThrow(ERROR_MESSAGES.MOVING);
   });
 
-  test('사용자 이동 입력값 테스트 ( 문자 )', () => {
-    expect(() => {
-      validator.validateMove('UD');
-    }).toThrow(ERROR_MESSAGES.MOVING);
+  test('사용자 이동 입력값  ( 2 )', () => {
+    expect(() => validator.validateMove('2')).toThrow(ERROR_MESSAGES.MOVING);
   });
 
-  test('사용자 이동 입력값 테스트 ( 문자 )', () => {
-    expect(() => {
-      validator.validateMove('U');
-    }).toBeTruthy();
+  test('사용자 이동 입력값  ( UD )', () => {
+    expect(() => validator.validateMove('UD')).toThrow(ERROR_MESSAGES.MOVING);
+  });
+
+  test('사용자 이동 입력값  ( U )', () => {
+    expect(() => validator.validateMove('U')).toBeTruthy();
+  });
+
+  test('사용자 이동 입력값  ( P )', () => {
+    expect(() => validator.validateMove('P')).toBeTruthy();
+  });
+});
+
+describe('재시도 입력값 테스트', () => {
+  test('재시도 입력값 ( 2 )', () => {
+    expect(() => validator.validateCommand('2')).toThrow(
+      ERROR_MESSAGES.COMMAND
+    );
+  });
+
+  test('재시도 입력값 ( RQ )', () => {
+    expect(() => validator.validateCommand('RQ')).toThrow(
+      ERROR_MESSAGES.COMMAND
+    );
+  });
+
+  test('재시도 입력값 ( R )', () => {
+    expect(() => validator.validateCommand('R')).toBeTruthy();
+  });
+
+  test('재시도 입력값 ( Q )', () => {
+    expect(() => validator.validateCommand('Q')).toBeTruthy();
   });
 });
