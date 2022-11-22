36ca6c998f0eb57969bbcd75df4662d375043421
sangpok <48979587+sangpok@users.noreply.github.com>
2022-11-22 16:24:56 +0900

    chore: 주석 추가
    BridgeGame, BridgeGameController

--- src/domains/BridgeGameController.js
+++ src/domains/BridgeGameController.js
@@ -12,17 +12,28 @@ const BridgeGame = require('./BridgeGame');
 const Utils = require('../utils/Utils');
 
 /**
+ * 한 게임에 해당하는 인스턴스
+ *
  * @type {BridgeGame}
  */
 let bridgeGame = null;
 
+/**
+ * 게임 진행을 책임하는 개체
+ */
 const BridgeGameController = {
+  /**
+   * 게임을 시작한다.
+   */
   start() {
     OutputView.printStartMessage();
 
     this.initializeBridgeGame();
   },
 
+  /**
+   * 게임 상태를 초기화한다.
+   */
   initializeBridgeGame() {
     InputView.read(READ_TYPE.BRIDGE_SIZE, (bridgeSize) => {
       BridgeState.setBridgeSize(Utils.convertToNumber(bridgeSize));
@@ -36,6 +47,9 @@ const BridgeGameController = {
     });
   },
 
+  /**
+   * 게임을 진행하는 Loop
+   */
   loopGame() {
     InputView.read(READ_TYPE.MOVING, (moving) => {
       bridgeGame.move(moving);
@@ -49,6 +63,11 @@ const BridgeGameController = {
     });
   },
 
+  /**
+   * 재시도를 해야하는 상황인지, 게임이 끝난 상황인지 체크한다.
+   *
+   * @returns {boolean}
+   */
   checkRetryOrAllDone() {
     const isRetry = this.checkRetry();
     if (isRetry) return true;
@@ -59,6 +78,11 @@ const BridgeGameController = {
     return false;
   },
 
+  /**
+   * 재시도를 해야하는지 확인한다.
+   *
+   * @returns {boolean}
+   */
   checkRetry() {
     const isRetry = bridgeGame.retry();
 
@@ -70,6 +94,11 @@ const BridgeGameController = {
     return false;
   },
 
+  /**
+   * 다리를 다 건넜다면 게임을 끝낸다.
+   *
+   * @returns {boolean}
+   */
   checkAllDone() {
     const isAllDone = bridgeGame.allDone();
 
@@ -81,6 +110,9 @@ const BridgeGameController = {
     return false;
   },
 
+  /**
+   * 게임 커맨드를 입력받은 뒤, 그에 따라 진행한다.
+   */
   DoQuitOrNew() {
     InputView.read(READ_TYPE.GAME_COMMAND, (gameCommand) => {
       const isRetry = this.checkCommandRetry(gameCommand);
@@ -91,6 +123,11 @@ const BridgeGameController = {
     });
   },
 
+  /**
+   * 입력한 커맨드 값이 R이라면 게임을 다시 시작한다.
+   *
+   * @param {GAME_COMMAND} gameCommand
+   */
   checkCommandRetry(gameCommand) {
     if (gameCommand === GAME_COMMAND.RETRY) {
       bridgeGame = new BridgeGame();
@@ -101,6 +138,12 @@ const BridgeGameController = {
     return false;
   },
 
+  /**
+   * 입력한 커맨드 값이 Q라면 게임을 끝낸다.
+   *
+   * @param {GAME_COMMAND} gameCommand
+   * @returns {boolean}
+   */
   checkCommandQuit(gameCommand) {
     if (gameCommand === GAME_COMMAND.QUIT) {
       this.gameEnd();
@@ -110,6 +153,11 @@ const BridgeGameController = {
     return false;
   },
 
+  /**
+   * 게임 종료시 결과를 출력하고 종료한다.
+   *
+   * @returns {void}
+   */
   gameEnd() {
     OutputView.printResult();
     OutputView.close();
