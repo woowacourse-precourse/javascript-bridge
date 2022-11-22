9a9a93891e16cf6a7010b76c6fa31336e7594843
SoobinJ <soobin08011812@gmail.com>
2022-11-22 20:59:05 +0900

    feat : BridgeInfo 생성, 다리 정보를 저장

---
+++ src/BridgeInfo.js
@@ -0,0 +1,5 @@
+const BridgeInfo = {
+  bridge: [],
+};
+
+module.exports = { BridgeInfo };
--- src/InputView.js
+++ src/InputView.js
@@ -2,6 +2,8 @@
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const MissionUtils = require("@woowacourse/mission-utils");
+const BridgeGame = require("./BridgeGame");
+const { BridgeInfo } = require("./BridgeInfo");
 const BridgeMaker = require("./BridgeMaker");
 const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
 
@@ -9,7 +11,6 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-
   readBridgeSize() {
     MissionUtils.Console.readLine(
       "다리의 길이를 입력해주세요.\n",
@@ -20,7 +21,7 @@ const InputView = {
           } else if (bridgeSize < 3 || bridgeSize > 20) {
             throw "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
           }
-          BridgeMaker.makeBridge(
+          BridgeInfo.bridge = BridgeMaker.makeBridge(
             bridgeSize,
             BridgeRandomNumberGenerator.generate
           );
@@ -44,6 +45,8 @@ const InputView = {
           if (moveLocation !== "D" && moveLocation !== "U") {
             throw "[ERROR] U 혹은 D만 입력 가능합니다.";
           }
+          const bridgeGame = new BridgeGame();
+          bridgeGame.move();
         } catch (e) {
           MissionUtils.Console.print(e);
           this.readMoving();
