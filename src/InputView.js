5e5aff41dbd5375f1118a3c99aa13dc701900bfc
greeneeya <suzythetravelerr@gmail.com>
2022-11-22 10:27:21 +0900

    플레이어가 이동할 칸을 선택받는다

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,4 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,17 +6,32 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    MissionUtils.Console.readLine(
+      "\n다리의 길이를 입력해주세요.\n",
+      (answer) => {}
+    );
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    MissionUtils.Console.readLine(
+      "\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
+      (answer) => {}
+    );
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+    MissionUtils.Console.readLine(
+      "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n",
+      (answer) => {}
+    );
+  },
 };
 
 module.exports = InputView;
