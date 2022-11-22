03f3128f2a834ff1603f97847207be9848d3300c
wonseok <rhdqngkrh77@khu.ac.kr>
2022-11-22 22:14:04 +0900

    feat: 다리 건너기 기능 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -7,7 +7,14 @@ class BridgeGame {
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(moving) {
+    if (moving === this.bridge[this.inputBridge.length]) {
+      this.inputBridge.push("true");
+    } else if (moving === this.bridge[this.inputBridge.length]) {
+      this.inputBridge.push("false");
+    }
+    return this.inputBridge;
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
