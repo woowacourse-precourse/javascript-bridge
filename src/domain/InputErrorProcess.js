10d93ae728172ef9ac7d1b917ee0a8c137deffac
Chanwoong1 <jcw7468@gmail.com>
2022-11-22 13:38:05 +0900

    chore: try - catch 처리 변경

--- src/domain/InputErrorProcess.js
+++ src/domain/InputErrorProcess.js
@@ -5,11 +5,11 @@ class InputErrorProcess {
   inputErrorProcess(validClass, inputValue, objectCode) {
     try {
       GameInfo[objectCode] = new validClass(inputValue)[objectCode];
+      return true;
     } catch (error) {
       OutputView.printMessage(error);
       return false;
     }
-    return true;
   }
 }
 
