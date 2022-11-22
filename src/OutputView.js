94f1cf5396c649e93a31e5f72ee5382c234423b5
ho-ji <wkdd0205@gmail.com>
2022-11-22 20:33:25 +0900

    feat: Add printResult() in OuputView
    게임의 최종 결과를 출력하는 함수를 구현함

--- src/OutputView.js
+++ src/OutputView.js
@@ -21,7 +21,14 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() {},
+  printResult(mapBridge, result, numberOfTry) {
+    MissionUtils.Console.print("최종 게임 결과");
+    MissionUtils.Console.print(mapBridge[0]);
+    MissionUtils.Console.print(mapBridge[1]);
+    MissionUtils.Console.print("\n");
+    MissionUtils.Console.print(`게임 성공 여부: ${result}`);
+    MissionUtils.Console.print(`총 시도한 횟수: ${numberOfTry}`);
+  },
 };
 
 module.exports = OutputView;
