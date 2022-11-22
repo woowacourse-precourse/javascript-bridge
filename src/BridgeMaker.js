f6ac834d75bc162f9e534c2b28209172954c8c1e
Sallyvely <tpdus4159@naver.com>
2022-11-22 13:06:43 +0900

    feat : 정답인 다리를 생성하는 기능 구현

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,3 +1,4 @@
+const MESSAGE = require("./message");
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
@@ -7,7 +8,23 @@ const BridgeMaker = {
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
-  makeBridge(size, generateRandomNumber) {},
+  
+  makeBridge(size, generateRandomNumber) {
+    let bridge=[];
+    let successNumber = generateRandomNumber();
+    for(let index=0; index < size; index++){
+      if(successNumber == 0 ) {
+        bridge.push("D");
+      }
+      if (successNumber == 1) {
+        bridge.push("U");
+      }
+      if(bridge.length != size){
+        successNumber = generateRandomNumber();
+      }
+    }
+    return bridge;
+  }
 };
 
 module.exports = BridgeMaker;
