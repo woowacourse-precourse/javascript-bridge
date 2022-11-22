223e044cdbdacc4e2a9734433c82a5d3bf27c922
Suyoung <jsy8492@gmail.com>
2022-11-22 09:49:41 +0900

    refactor(ValidatorTests): 불필요한 파라미터 제거

--- __tests__/BridgeSizeValidatorTest.js
+++ __tests__/BridgeSizeValidatorTest.js
@@ -3,14 +3,14 @@ const BridgeSizeValidator = require('../src/validators/BridgeSizeValidator');
 describe('사용자가 입력한 다리 길이의 유효성을 검사하는 객체 테스트', () => {
   test.each([['a'], ['abc'], ['a3'], ['2a']])(
     '다리 길이를 위한 입력값에 숫자가 아닌 문자가 포함된 경우 예외처리',
-    input => {
+    () => {
       expect(input => BridgeSizeValidator.validate(input)).toThrow('[ERROR]');
     },
   );
 
   test.each([['21'], ['-1'], ['1'], ['2'], ['999']])(
     '유효한 다리 길이 범위 이외의 값인 경우 예외처리',
-    input => {
+    () => {
       expect(input => BridgeSizeValidator.validate(input)).toThrow('[ERROR]');
     },
   );
