0a68421987c3628c0cb570f99b521184fcac4485
eunha <dmsgk2323@naver.com>
2022-11-22 16:25:32 +0900

    feat: 다리 길이 입력 기능 예외 추가

--- src/InputView.js
+++ src/InputView.js
@@ -1,8 +1,21 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const BridgeGame = require("./BridgeGame");
+const BridgeMaker = require("./BridgeMaker");
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+const OutputView = require("./OutputView");
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
+  MOVE_UP: "U",
+  MOVE_DOWN: "D",
+  RETRY_COMMAND: "R",
+  QUIT_COMMAND: "Q",
+
+  gameContinue: 0,
+  bridge: [],
+  tryCount: 1,
   /**
    * 다리의 길이를 입력받는다.
    */
@@ -10,9 +23,29 @@ const InputView = {
     MissionUtils.Console.readLine(
       "다리 건너기 게임을 시작합니다.다리의 길이를 입력해주세요.",
       (answer) => {
-        console.log(`다리의 길이: ${answer}`);
+        this.bridgeSizeExceptionCatch(answer);
+        this.bridge = BridgeMaker.makeBridge(
+          answer,
+          BridgeRandomNumberGenerator.generate
+        );
+        this.readMoving(0);
       }
     );
+  },
+  // 다리 길이 예외 처리
+  bridgeSizeExceptionCatch(answer) {
+    try {
+      this.bridgeSizeException(answer);
+    } catch (e) {
+      MissionUtils.Console.print(e);
+      this.readBridgeSize();
+    }
+  },
+
+  bridgeSizeException(answer) {
+    if (isNaN(answer)) throw "[ERROR] 문자를 입력하실 수 없습니다.";
+    if (answer < 3 || answer > 20)
+      throw "[ERROR] 3이상 20이하의 수만 입력할 수 있습니다.";
   },
 
   /**
