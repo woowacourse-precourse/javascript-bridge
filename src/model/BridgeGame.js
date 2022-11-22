5ec12597a0468bdc9e56bd91ba38933c354895bc
Ryan-Dia <cjfdnjs158@naver.com>
2022-11-22 11:13:25 +0900

    refactor(BridgeGame):Model폴더 안으로 경로 변경

--- src/model/BridgeGame.js
+++ src/model/BridgeGame.js
@@ -1,6 +1,6 @@
 const { Console } = require('@woowacourse/mission-utils');
-const BridgeMaker = require('./BridgeMaker');
-const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
+const BridgeMaker = require('../BridgeMaker');
+const BridgeRandomNumberGenerator = require('../BridgeRandomNumberGenerator');
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
