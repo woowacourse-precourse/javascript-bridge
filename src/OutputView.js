c44f2fca9bb9aa0a7927e4609a9cdf32a89b47d2
sookyeongyeom <sookyeong.dev@gmail.com>
2022-11-22 22:16:24 +0900

    Feat: 이동 결과 출력(2) - 맵 출력

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,5 +1,7 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const Console = MissionUtils.Console;
+const MapMaker = require('./MapMaker');
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -18,6 +20,8 @@ const OutputView = {
 	 * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
 	 */
 	printMap(recentMove, isCorrect) {
+		const map = MapMaker.makeMap(recentMove, isCorrect);
+		this.print(map);
 	},
 
   /**
