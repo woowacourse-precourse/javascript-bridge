39f03f21fc5fd2155eda0a90505f17e89ceec27c
1 <wrasf175@naver.com>
2022-11-22 16:50:24 +0900

    feat(GameManager): 게임진행을 위한 프라이빗 필드 생성

--- src/GameManager.js
+++ src/GameManager.js
@@ -2,15 +2,15 @@ const Print = require('./Print');
 const OutputView  = require('./OutputView');
 const InputView = require('./InputView');
 const BridgeGame = require('./BridgeGame');
-const BridgeMaker = require('./BridgeMaker');
 const { Console } = require('@woowacourse/mission-utils');
-const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
+const { BridgeLengthInput, DirectionChoiceInput, RetryInput} = require('./Validator/Validator');
 
 class GameManager {
+    #bridgeGame
 
 
     constructor() {
-
+        this.#bridgeGame = new BridgeGame();
     }
 
     start() {
