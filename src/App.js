3f0cee0ca1a40305b161b0c35c0260aa35f36523
piporica <49173614+piporica@users.noreply.github.com>
2022-11-22 21:03:59 +0900

    feat : 브릿지 제작
    게임 시작을 위한 브릿지 제작

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 const InputView = require("./InputView");
 const { Console, Random } = require("@woowacourse/mission-utils");
 const OutputView = require("./OutputView");
+const BridgeMaker = require("./BridgeMaker");
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+const BridgeGame = require("./BridgeGame");
 
 class App {
   play() {
@@ -9,10 +12,18 @@ class App {
 
   gameStart() {
     OutputView.gameStart();
-    InputView.readBridgeSize((input) => {
-      console.log(input);
+    InputView.readBridgeSize((size) => {
+      let bridgeGame = this.generateBridge(size);
       this.stopProgram();
     });
+  }
+
+  generateBridge(size) {
+    let bridge = BridgeMaker.makeBridge(
+      size,
+      BridgeRandomNumberGenerator.generate
+    );
+    return new BridgeGame(bridge);
   }
 
   stopProgram() {
