67ff2a58df402610ad4bde16016c2a194bc79e68
doheeMac <thsehgml37@korea.ac.kr>
2022-11-22 20:53:52 +0900

    feat: game test 추가

--- src/Component/Game.js
+++ src/Component/Game.js
@@ -28,45 +28,6 @@ class Game {
   get getBridgeGame() {
     return this.#bridgeGame;
   }
-  // get getBridgeStatus() {
-  //   return this.#bridgeStatus;
-  // }
-
-  // playAlgorithms(bridgeLength) {
-  //   for (let moveCount = 0; moveCount < bridgeLength; moveCount++) {
-  //     const DIRECTION = InputView.readMoving();
-
-  //     const MOVE_RESULT = this.moveTurn(DIRECTION, moveCount);
-  //     if (MOVE_RESULT === "R") {
-  //       moveCount = 0;
-  //     }
-  //     if (MOVE_RESULT === "Q") {
-  //       return false;
-  //     }
-  //   }
-  //   return true;
-  // }
-
-  // moveTurn(direction, moveCount) {
-  //   const MOVE_RESULT = this.#bridgeGame.move(direction, moveCount);
-  //   if (!MOVE_RESULT) {
-  //     const IS_QUIT = this.askQuit();
-  //     return IS_QUIT;
-  //   }
-  //   return "M";
-  // }
-
-  // askQuit() {
-  //   const IS_QUIT = InputView.readGameCommand();
-  //   if (IS_QUIT === "Q") {
-  //     this.getPrintResult();
-  //   }
-  //   if (IS_QUIT === "R") {
-  //     this.increasePlayCount();
-  //     this.#bridgeGame.retry();
-  //   }
-  //   return IS_QUIT;
-  // }
 
   getGameResult() {
     const JUMP_HISTORY = this.#bridgeGame.getJumpHistory;
