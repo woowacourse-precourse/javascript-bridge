f5cb4d420aaa4b48a952d207f20136aec65b8932
AhnGeunTaek <dksrmsxor@gmail.com>
2022-11-22 14:44:35 +0900

    refactor: 다리 이동 입력값 예외 처리 테스트

--- __tests__/InputViewTest.js
+++ __tests__/InputViewTest.js
@@ -34,6 +34,14 @@ const runException = (inputs) => {
   expectLogContains(getOutput(logSpy), ["[ERROR]"]);
 };
 
+const runExceptionMoving = (inputs) => {
+  mockQuestions(inputs);
+  const logSpy = getLogSpy();
+  InputView.readMoving();
+
+  expectLogContains(getOutput(logSpy), ["[ERROR]"]);
+};
+
 describe("InputView 테스트", () => {
   test("다리 길이 입력값 유효 테스트", () => {
     runException(["a"]);
@@ -48,11 +56,7 @@ describe("InputView 테스트", () => {
   });
 
   test("이동할 칸이 U 또는 D가 아닌 경우 예외 처리한다.", () => {
-    mockQuestions(["G"]);
-
-    expect(() => {
-      InputView.readMoving();
-    }).toThrow("[ERROR] 이동할 다리 칸은 U 또는 D만 입력 가능합니다.");
+    runExceptionMoving(["A"]);
   });
 
   test("게임 진행 옵션이 R이나 Q가 아닌 경우 예외 처리한다.", () => {
