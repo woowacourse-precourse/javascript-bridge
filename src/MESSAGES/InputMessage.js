3b42bf0888748cdaab9699881051421a4381e2b7
khj0426 <706shin1728@naver.com>
2022-11-22 10:30:57 +0900

    refactor:최종 상수 메시지 수정

--- src/MESSAGES/InputMessage.js
+++ src/MESSAGES/InputMessage.js
@@ -1,8 +1,12 @@
+const { DOWNUP_MESSAGE, UPDOWN_MESSAGE } = require('./BridgeMessage');
+
 const INPUT_MESSAGE = {
   BRIDGE_INPUT: '다리의 길이를 입력해주세요.',
   BRIDGE_INPUT_ERROR: '[ERROR] 다리 길이는 3부터 20사이의 숫자여야 합니다.',
   REGAME_INPUT_ERROR: '[ERROR] 재시도는 R,종료는 Q입니다.다시 입력해주세요',
   MOVE_INPUT_ERROR: '[ERROR] 위는 U,아래는 D입니다. 다시 입력해주세요',
+  UPDOWN_ERROR_MESSAGE: new Error(UPDOWN_MESSAGE),
+  DOWNUP_ERROR_MESSAGE: new Error(DOWNUP_MESSAGE),
 };
 
 Object.freeze(INPUT_MESSAGE);
--- src/MESSAGES/NumberMessage.js
+++ src/MESSAGES/NumberMessage.js
@@ -1,12 +1,6 @@
 const NUMBER_MESSAGE = {
   BRIDGE_START: 3,
   BRIDGE_END: 20,
-  flagBridge(randomnum) {
-    if (randomnum === '1') {
-      return 'U';
-    }
-    return 'D';
-  },
 };
 
 Object.freeze(NUMBER_MESSAGE);
