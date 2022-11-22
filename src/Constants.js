424131428e1b999cd3ff0c34e26529561c1e9d81
Sanggil Han <tkdrlf95@naver.com>
2022-11-22 13:08:35 +0900

    feat(Constants): Create Constants.js and make and use MESSAGE const
    Constants.js 파일을 만들어 MESSAGE 상수를 만들었고 InputView에서 사용함

---
+++ src/Constants.js
@@ -0,0 +1,9 @@
+const MESSAGE = Object.freeze({
+  READ_BRIDGE_SIZE: '다리의 길이를 입력해주세요.\n',
+  READ_MOVING: '이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
+  READ_GAME_COMMAND: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
+});
+
+module.exports = Object.freeze({
+  MESSAGE,
+})
--- src/InputView.js
+++ src/InputView.js
@@ -1,4 +1,5 @@
-const { Console } = require("@woowacourse/mission-utils");
+const { Console } = require('@woowacourse/mission-utils');
+const { MESSAGE } = require('./Constants');
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -7,7 +8,7 @@ const InputView = {
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize() {
-    Console.readLine("다리의 길이를 입력해주세요.\n", (bridgeSize) => {
+    Console.readLine(MESSAGE.READ_BRIDGE_SIZE, (bridgeSize) => {
       return bridgeSize;
     })
   },
@@ -16,7 +17,7 @@ const InputView = {
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving() {
-    Console.readLine("이동할 칸을 선택해주세요. (위: U, 아래: D)\n", (moving) => {
+    Console.readLine(MESSAGE.READ_MOVING, (moving) => {
       return moving;
     })
   },
@@ -25,7 +26,7 @@ const InputView = {
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand() {
-    Console.readLine("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n", (gameCommand) => {
+    Console.readLine(MESSAGE.READ_GAME_COMMAND, (gameCommand) => {
       return gameCommand;
     })
   },
