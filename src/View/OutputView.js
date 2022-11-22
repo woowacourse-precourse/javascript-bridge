73aa9673b92f29926dbe66a3e5e90fa28c85d33f
youzysu <111998760+youzysu@users.noreply.github.com>
2022-11-22 19:24:54 +0900

    style: 불필요한 주석 삭제

--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -1,18 +1,11 @@
 const { Console } = require('@woowacourse/mission-utils');
 const { MESSAGE } = require('../Constants');
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
 const OutputView = {
   DIVIDER: ' | ',
   SUCCESS: '성공',
   FAILURE: '실패',
-  /**
-   * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-   * <p>
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
+
   printMap(bridgeGame) {
     const upperMap = bridgeGame.getUpperTrack().join(OutputView.DIVIDER);
     const LowerMap = bridgeGame.getLowerTrack().join(OutputView.DIVIDER);
@@ -20,11 +13,6 @@ const OutputView = {
     Console.print(`[ ${LowerMap} ]`);
   },
 
-  /**
-   * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-   * <p>
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
   printResult(bridgeGame) {
     const trial = bridgeGame.getTrialCount();
     let outcome;
