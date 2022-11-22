6c5e2cc5428875a78e95953a68e80ef65371bc4d
knh6269 <knh6269@gmail.com>
2022-11-22 23:48:52 +0900

    feat(Io): 입출력 편리사용 도움 클래스

--- src/Io.js
+++ src/Io.js
@@ -2,6 +2,17 @@ const {Console} = require('@woowacourse/mission-utils');
 
 class Io {
 
+    static input (message, callback) {
+        Console.readLine(message, callback);
+    }
+
+    static output (message) {
+        Console.print(message);
+    }
+    
+    static close () {
+        Console.close();
+    }
 }
 
 module.exports = Io;
