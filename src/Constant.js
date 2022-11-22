e1826937594b08b09056cfc7a88aa8f26f6fe80d
김준환 <hvvany@gimjunhwan-ui-MacBookAir.local>
2022-11-22 23:42:59 +0900

    fix: 메시지 상수 수정(retry 에러메시지 추가)

--- src/Constant.js
+++ src/Constant.js
@@ -7,15 +7,15 @@ const GUIDE_MESSAGE = {
 
 const ERROR_MESSAGE = {
   LENGTH: "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.",
-  MOVE: "[ERROR] 이동할 칸은 (위: U, 아래: D) 영어 대문자로만 입력해야 합니다."
-  RE_TRY: "[ERROR] 게임 재시작 및 종료는 (재시도: R, 종료: Q) 영어 대문자로만 입력해야 합니다. "
+  MOVE: "[ERROR] 이동할 칸은 (위: U, 아래: D) 영어 대문자로만 입력해야 합니다.",
+  RE_TRY: "[ERROR] 게임 재시작 및 종료는 (재시도: R, 종료: Q) 영어 대문자로만 입력해야 합니다. ",
 }
 
 const RESULT_MESSAGE = {
   TITTLE: "최종 게임 결과 \n",
   RESULT_TABLE: " ",
   IS_SUCCESS: "게임 성공 여부: ",
-  TRY_COUNT: "총 시도한 횟수: "
+  TRY_COUNT: "총 시도한 횟수: ",
 }
 
 
