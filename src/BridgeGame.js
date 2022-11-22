fef9463046608886615ffb666a6fabe98d469ae7
KES <aprilsmaple@gmail.com>
2022-11-22 21:21:16 +0900

    refactor: final

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -7,21 +7,12 @@ const BridgeRandomNumberGenerator = require('./lib/BridgeRandomNumberGenerator')
 class BridgeGame {
   #size;
   #bridge;
-  /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
+
   move(command, round) {
     MoveCommandValidation(command);
     return this.#isMovable(command, round);
   }
 
-  /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
   retry(reply) {
     GameCommandValidation(reply);
     return reply === 'R' ? true : false;
@@ -34,7 +25,6 @@ class BridgeGame {
 
   makeBridge() {
     this.#bridge = BridgeMaker.makeBridge(this.#size, BridgeRandomNumberGenerator.generate);
-    console.log(this.#bridge);
   }
 
   #isMovable(command, round) {
--- src/constants/Result.js
+++ src/constants/Result.js
@@ -1,7 +1,7 @@
 const RESULT = Object.freeze({
   title: '최종 게임 결과',
   successOrFailure: (result) => `게임 성공 여부: ${result}`,
-  attempts: (attempts) => `게임 성공 여부: ${attempts}`,
+  attempts: (attempts) => `총 시도한 횟수: ${attempts}`,
 });
 
 module.exports = RESULT;
