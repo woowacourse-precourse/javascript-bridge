b3b901cee12d423b09e6e089d0a3e8ce59975f34
s0ojin <onepst@hanyang.ac.kr>
2022-11-22 23:15:44 +0900

    chore(constant.js) : 상수 변경

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,22 +1,21 @@
 const DEFAULT = {
-  START_NUM: 3,
-  END_NUM: 20,
   UP: 'U',
   DOWN: 'D',
   CORRECT: 'O',
   INCORRECT: 'X',
   RESTART: 'R',
   QUIT: 'Q',
+  EMPTY: ' ',
 };
 
 const GAME_MESSAGE = {
-  START: '다리 건너기 게임을 시작합니다.',
-  BRIDGE_SIZE: '다리의 길이를 입력해주세요.',
-  CHOOSE: '이동할 칸을 선택해주세요. (위: U, 아래: D)',
-  RESTART: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)',
-  RESULT: '최종 게임 결과',
-  SUCCESS: '게임 성공 여부: 성공',
-  FAIL: '게임 성공 여부: 실패',
+  START: '다리 건너기 게임을 시작합니다.\n',
+  BRIDGE_SIZE: '다리의 길이를 입력해주세요.\n',
+  CHOOSE: '\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
+  RESTART: '\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
+  RESULT: '최종 게임 결과\n',
+  SUCCESS: '\n게임 성공 여부: 성공',
+  FAIL: '\n게임 성공 여부: 실패',
   ATTEMPT: '총 시도한 횟수: ',
 };
 
