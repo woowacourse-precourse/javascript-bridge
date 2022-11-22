6f0d1f87fcc592919632c076137d5daa5e3927d5
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2022-11-22 18:49:11 +0900

    feat : 게임 재시작 예외처리

--- src/Validation.js
+++ src/Validation.js
@@ -13,6 +13,12 @@ class CheckUserInput {
       MissionUtils.Console.print('[ERROR] 이동할 칸은 U 또는 D여야 합니다.(위: U, 아래: D)')
     }
   }
+
+  checkRetry(userInput){
+    if(userInput !== 'R' && userInput !== 'Q'){
+      MissionUtils.Console.print("[ERROR] 종료하려면 R 또는 Q를 입력하세요.");
+    }
+  }
 }
 
 module.exports = CheckUserInput;
