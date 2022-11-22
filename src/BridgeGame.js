24a4426dd696c392f0fc85b9c69aa6e5ccc45a24
yunjjeongjo <two6885@gmail.com>
2022-11-22 20:34:50 +0900

    feat: move 메서드 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,4 +1,5 @@
 const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+const { BRIDGE } = require("./constants/constants");
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
@@ -29,12 +30,33 @@ class BridgeGame {
   try() {
     this.#trial += 1;
   }
+
+  fail() {
+    this.#isFail = true;
+  }
+
+  success() {
+    this.#isSuccess = true;
+  }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(userInput) {
+    const answer = this.#bridge[this.#currentLocation];
+    if (userInput === answer) {
+      this.changeBridgeState(userInput, BRIDGE.SUCCESS);
+    }
+    if (userInput !== answer) {
+      this.changeBridgeState(userInput, BRIDGE.FAIL);
+      this.fail();
+    }
+    this.#currentLocation += 1;
+    if (this.#currentLocation === this.#bridgeLength - 1 && !this.#isFail) {
+      this.success();
+    }
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
