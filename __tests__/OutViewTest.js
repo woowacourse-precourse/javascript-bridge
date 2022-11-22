97d0b64b9f3b1ecd66a434aa1a8010d74e8089f3
연 <mogoo22@naver.com>
2022-11-22 18:51:03 +0900

    test: 최종 게임 결과 출력 테스트 추가

--- __tests__/OutViewTest.js
+++ __tests__/OutViewTest.js
@@ -43,4 +43,37 @@ describe('게임 출력 테스트', () => {
     expectLogContains(log, [MESSAGE.GAME_START, ...maps]);
   });
 
+  test.each([
+    [
+      ['3', 'U', 'D', 'D', 'R', 'U', 'D', 'U'],
+      [
+        //
+        '[ O |   | O ]',
+        '[   | O |   ]',
+        '게임 성공 여부: 성공',
+        '총 시도한 횟수: 2',
+      ],
+    ],
+    [
+      ['3', 'U', 'U', 'R', 'D', 'R', 'U', 'U', 'Q'],
+      [
+        //
+        '[ O | X ]',
+        '[   |   ]',
+        '게임 성공 여부: 실패',
+        '총 시도한 횟수: 3',
+      ],
+    ],
+  ])('게임 종료 후 최종 게임 결과를 출력한다.', (questions, map) => {
+    const logSpy = getLogSpy();
+    const randoms = [1, 0, 1];
+
+    runBridgeGame(randoms, questions);
+
+    const log = getOutput(logSpy);
+    expectLogContains(log, [MESSAGE.GAME_RESULT, ...map]);
+
+    const [upSide, downSide] = map;
+    expectBridgeOrder(log, upSide, downSide);
+  });
 });
