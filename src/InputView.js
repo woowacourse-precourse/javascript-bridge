0d3149a1f6e917c3a455b03a194393d81b289f77
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-22 18:54:05 +0900

    feat: 다리길이 입력 구현

--- src/InputView.js
+++ src/InputView.js
@@ -1,29 +1,26 @@
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
-// InputView 에서만 MissionUtils의 Console.readLine() 을 이용해 사용자의 입력을 받을 수 있다.
 const { Console } = require('@woowacourse/mission-utils');
 const { QUERY } = require('./constants/constants');
+const OutputView = require('./OutputView');
+const validator = require('./validator/validator');
 
 const InputView = {
-  /**
-   * 다리의 길이를 입력받는다.
-   */
-  readBridgeSize(setBridge) {
-    Console.readLine(QUERY.SIZE, setBridge);
+  readBridgeSize(game) {
+    Console.readLine(QUERY.SIZE, (size) => {
+      try {
+        validator.validateBridgeSize(size);
+        game.generateBridge(size);
+        this.readMoving(game);
+      } catch (err) {
+        this.readError(err, () => this.readBridgeSize(game));
+      }
+    });
   },
-
-  /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
-  readMoving(callback) {
-    Console.readLine(QUERY.MOVE, callback);
+  readMoving(game) {},
+  readGameCommand(game) {},
+  readError(err, callback) {
+    OutputView.printError(err);
+    callback();
   },
-
-  /**
-   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-   */
-  readGameCommand() {},
 };
 
 module.exports = InputView;
