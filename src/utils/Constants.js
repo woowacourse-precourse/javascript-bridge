c324867b0b970a002d363ce19f5af0d522354a39
y00eunji <aine0227@naver.com>
2022-11-22 20:48:03 +0900

    fix: Fix constants

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -24,9 +24,9 @@ const GAME_CONDITION = Object.freeze({
 const GAME_MESSAGE = Object.freeze({
   START_GAME: "다리 건너기 게임을 시작합니다.",
   INPUT_BRIDGE_LENGTH: "다리의 길이를 입력해주세요.\n",
-  INPUT_BRIDGE_SPACE_TO_MOVE: "이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
+  INPUT_BRIDGE_SPACE_TO_MOVE: "\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
   RESTART_OR_QUIT:
-    "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n",
+    "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n",
 });
 
 const GAME_RESULT = Object.freeze({
--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -7,7 +7,7 @@ const { GAME_MESSAGE, GAME_RESULT } = require("../utils/Constants");
 
 const OutputView = {
   startGame() {
-    Console.print(GAME_MESSAGE.START_GAME);
+    Console.print(`${GAME_MESSAGE.START_GAME}\n`);
   },
   /**
    * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -15,7 +15,7 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printMap(bridgeMap) {
-    Console.print(bridgeMap);
+    Console.print(`${bridgeMap}\n`);
   },
 
   /**
