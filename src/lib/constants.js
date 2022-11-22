8a14e026c091e9c15aac72c08882d0e992953ff5
n0eyes <doll4586633@naver.com>
2022-11-22 23:08:45 +0900

    feat: 게임 내 메세지 수정

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,8 +1,8 @@
 const MESSAGE = Object.freeze({
-  INTRO: "다리 건너기 게임을 시작합니다.",
-  SIZE: "다리의 길이를 입력해주세요.",
-  DIRECTION: "이동할 칸을 선택해주세요. (위: U, 아래: D)",
-  RETRY: "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
+  INTRO: "다리 건너기 게임을 시작합니다.\n",
+  SIZE: "다리의 길이를 입력해주세요.\n",
+  DIRECTION: "이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
+  RETRY: "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n",
   FINAL_RESULT: "최종 게임 결과",
   GAME_RESULT: "게임 성공 여부:",
   TRY_COUNT: "총 시도한 횟수:",
