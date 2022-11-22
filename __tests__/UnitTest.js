b1570e74ef7d57acc008e9b6afd4b71f9da36d88
강인영 <kiy@gang-in-yeong-ui-MacBookAir.local>
2022-11-22 20:37:50 +0900

    fix: Edit test list

--- __tests__/UnitTest.js
+++ __tests__/UnitTest.js
@@ -1,8 +1,9 @@
-const MissionUtils = require("@woowacourse/mission-utils");
-const App = require("../src/App");
+
 const BridgeMaker = require("../src/BridgeMaker");
 const BridgeRandomNumberGenerator = require("../src/BridgeRandomNumberGenerator");
 const BridgeGame = require("../src/BridgeGame");
+const InputView = require("../src/InputView");
+const InputValidation = require("../src/InputValidation");
 
 describe("다리 건너기 기능 단위별 테스트", () => {
 
@@ -10,48 +11,39 @@ describe("다리 건너기 기능 단위별 테스트", () => {
         expect(BridgeMaker.makeBridge(4, BridgeRandomNumberGenerator.generate)).toHaveLength(4);    
     });
 
-    test("다리의 위아래 칸 선택한 후 정답인지 확인하기", () => {
+    test("다리의 위아래 칸 선택한 후 정답인지 테스트", () => {
         const answerList = ['U', 'D', 'U', 'U', 'D', 'D', 'U']; 
         const bridgeGame = new BridgeGame(answerList, answerList.length);
         const isAnswer = bridgeGame.move('D'); //정답(answerList)의 첫번째 값(U)와 플레이어의 추측값(D)이 일치하지 않으므로 false 저장 
 
         expect(isAnswer).toEqual(false); 
-    })
+    });
 
-/*
-    test("로또 구입 금액이 숫자가 아니면 예외가 발생한다.", () => {
-        expect(() => {
-            payValidate(['1000k']);
-        }).toThrow("[ERROR]");
-      });
+    test("게임 종료 조건 - 정답을 다 맞혔는지 여부 테스트", () => {
+        const answerList = ['U']; 
+        const bridgeGame = new BridgeGame(answerList, answerList.length);
+        
+        expect(InputView.guessAllAnswers(bridgeGame, answerList.length)).toEqual(false); 
+    });
+
+    test("예외 테스트 - 다리 길이 값이 숫자인지 테스트", () => {
+        const isValidNumber = InputValidation.validateNumber('3k');
+        expect(isValidNumber).toEqual(true); 
+    });
     
-    test("로또 구입 금액이 1000원 단위가 아니면 예외가 발생한다.", () => {
-        expect(() => {
-            payValidate(['1004']);
-        }).toThrow("[ERROR]");
+    test("예외 테스트 - 다리 길이 값의 범위가 올바른지 테스트", () => {
+        const isValidRange = InputValidation.validateRange(2);
+        expect(isValidRange).toEqual(true); 
     });
 
-    test("구입 금액에 따른 로또 수량이 틀리면 예외가 발생한다.", () => {
-        const app = new App();
-        expect(app.calculateTicketNums('3000')).toEqual(3);    
+    test("예외 테스트 - 플레이어가 선택한 칸이 올바른 값인지 테스트", () => {
+        const isValidMove = InputValidation.validateMove('UD');
+        expect(isValidMove).toEqual(true); 
     });
 
-    test("로또 한 줄에서 정답으로 일치하는 횟수가 틀리면 예외가 발생한다.", () => {
-        const app = new App();
-        const winningNums = [1, 2, 3, 4, 5, 6];
-        const oneLine = [1, 3, 4, 5, 9, 11]; //4개 일치
-        expect(app.countAnswerNums(oneLine, winningNums)).toEqual(4);    
-    });
+    test("예외 테스트 - 플레이어가 선택한 재시작/종료 여부가 올바른 값인지 테스트", () => {
+        const isValidCommand = InputValidation.validateCommand('K');
+        expect(isValidCommand).toEqual(true); 
+    });    
 
-    test("로또 등수에 따른 횟수가 틀리면 예외가 발생한다.", () => {
-        const app = new App();
-        const rankList = [5, 0, 4, 0, 0, 0];
-        expect(app.countRank(rankList)).toEqual([1, 1, 0, 0, 0]);    
-    }); 
- 
-    test("로또 수익률이 틀리면 예외가 발생한다.", () => {
-        const app = new App();
-        expect(app.calculateProfitRate(10000, 8000)).toEqual("125.0");    
-    }); 
-*/
 });
