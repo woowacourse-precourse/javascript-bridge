b8820f6d3e2cda38a4dc5f285ebbb1da71a7ba48
banma1234 <banma1234@gmail.com>
2022-11-22 23:08:28 +0900

    refactor: App에서 다리생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-class App {
-  play() {}
+const InputHandler = require('./InputView');
+const BridgeMaker = require('./BridgeMaker');
+const MakeRandomValue = require('./BridgeRandomNumberGenerator');
+
+class App{
+  play() {
+    let bridge = this.buildBridge();
+  }
+
+  buildBridge() {
+    let inputBridgeSize = InputHandler.readBridgeSize();
+    let bridge = BridgeMaker.makeBridge(inputBridgeSize, MakeRandomValue.generate);
+
+    return bridge;
+  }
 }
+
+// const app = new App();
+// app.play();
 
 module.exports = App;
