5bcb5c6683965b646f6ae3e223ffa0391231bc95
김상두 <puki4416@gmail.com>
2022-11-22 13:04:19 +0900

    feat : App 클래스에서 play 메소드로 프로그램을 시작할수 있도록 코드작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const BridgeGame = require("./BridgeGame");
+const InputView = require("./InputView");
+
 class App {
-  play() {}
+  #gameManager;
+  constructor() {
+    this.#gameManager = new BridgeGame();
+  }
+  play() {
+    MissionUtils.Console.print("다리 건너기 게임을 시작합니다.");
+    InputView.readBridgeSize(this.#gameManager);
+  }
 }
 
 module.exports = App;
