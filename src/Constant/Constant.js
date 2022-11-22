19d721ea4d50a345d30a78fb90f12413260f5276
dudgp128 <20181048@sungshin.ac.kr>
2022-11-22 18:04:38 +0900

    FEAT(Constant) : 문장 출력 상수화

---
+++ src/Constant/Constant.js
@@ -0,0 +1,14 @@
+const Constant = {
+  question: {
+    bridgeSize: "다리의 길이를 입력해주세요.\n",
+    moving: "이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
+    retry: "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n",
+  },
+
+  error: {
+    notRange: "[ERROR] 3과 20사이의 자연수를 입력해주세요.\n",
+    notUD: "[ERROR] 유효한 값(U or D)을 입력해주세요.\n",
+    notRQ: "[ERROR] 유효한 값(R or Q)을 입력해주세요.\n",
+  },
+};
+module.exports = Constant;
