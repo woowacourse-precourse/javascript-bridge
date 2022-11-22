045b58943d07119101963014504e914705e15ce5
oio337a <oio337a@gmail.com>
2022-11-22 13:52:25 +0900

    test : retry test

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -81,6 +81,25 @@ describe('다리 건너기 테스트', () => {
     expectBridgeOrder(log, '[ O |   | O ]', '[   | O |   ]');
   });
 
+  test('기능테스트 (재시도)', () => {
+    const logSpy = getLogSpy();
+    mockRandoms([1, 0, 1, 0, 1]);
+    mockQuestions(['5', 'D', 'R', 'U', 'D', 'D', 'R', 'U', 'D', 'U', 'D', 'U']);
+
+    const app = new App();
+    app.play();
+
+    const log = getOutput(logSpy);
+    expectLogContains(log, [
+      '최종 게임 결과',
+      '[ O |   | O |   | O ]',
+      '[   | O |   | O |   ]',
+      '게임 성공 여부: 성공',
+      '총 시도한 횟수: 3',
+    ]);
+    expectBridgeOrder(log, '[ O |   | O |   | O ]', '[   | O |   | O |   ]');
+  });
+
   test('예외 테스트', () => {
     runException(['a']);
   });
