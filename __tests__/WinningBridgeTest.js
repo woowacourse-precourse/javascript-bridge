54abd69492db4084fd3e879e90df02951ec36844
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-11-22 20:03:46 +0900

    test: test.each 함수 이용해 WinningBridge 클래스에 대한 테스트 코드 간결하게 수정

--- __tests__/WinningBridgeTest.js
+++ __tests__/WinningBridgeTest.js
@@ -1,24 +1,21 @@
 const WinningBridge = require('../src/model/WinningBridge');
 
 const validateSize = (input) => {
-  const winningBridge = new WinningBridge();
   return WinningBridge.validate(input);
 };
 
 describe('WinningBridge 클래스 테스트', () => {
-  test('예외 테스트 : 입력값이 숫자가 아닌 경우 예외 처리한다', () => {
-    expect(() => validateSize(NaN)).toThrow(
-      '[ERROR] 문자와 특수기호를 제외한 숫자를 입력해주세요.'
-    );
-  });
-  test('예외 테스트 : 입력값이 3 이상 20 이하의 값이 아닌 경우 예외 처리한다', () => {
-    expect(() => validateSize(2)).toThrow(
-      '[ERROR] 다리의 길이는 3 이상 20 이하만 가능합니다.'
-    );
-  });
-  test('예외 테스트 : 입력값이 빈칸인 경우 예외 처리한다', () => {
-    expect(() => validateSize('')).toThrow(
-      '[ERROR] 공백을 입력할 수 없습니다. 값을 입력해주세요.'
-    );
-  });
+  test.each([
+    ['', '[ERROR] 공백을 입력할 수 없습니다. 값을 입력해주세요.'],
+    [NaN, '[ERROR] 문자와 특수기호를 제외한 숫자를 입력해주세요.'],
+    [21, '[ERROR] 다리의 길이는 3 이상 20 이하만 가능합니다.'],
+    [2, '[ERROR] 다리의 길이는 3 이상 20 이하만 가능합니다.'],
+    [-11, '[ERROR] 다리의 길이는 3 이상 20 이하만 가능합니다.'],
+    [99, '[ERROR] 다리의 길이는 3 이상 20 이하만 가능합니다.'],
+  ])(
+    '예외 테스트 : 입력값이 숫자가 아닌 경우 예외 처리한다',
+    (input, expected) => {
+      expect(() => validateSize(input)).toThrow(expected);
+    }
+  );
 });
