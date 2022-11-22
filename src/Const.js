2bf5ce90c6753cea07b83aae6cc8d9a5ba7e234f
nailen1 <nietsnie1@gmail.com>
2022-11-22 10:03:27 +0900

    fix: 상수의 메세지 변경

--- src/Const.js
+++ src/Const.js
@@ -1,7 +1,7 @@
 const CONST = {
     MESSAGE: {
         START: '다리 건너기 게임을 시작합니다.',
-        READ_SIZE: '다리의 길이를 입력해주세요',
+        READ_SIZE: '다리의 길이를 입력해주세요.',
         READ_MOVING: '이동할 칸을 선택해주세요. (위: U, 아래: D)',
         READ_COMMAND: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)',
         RESULT_TITLE: '최종 게임 결과',
