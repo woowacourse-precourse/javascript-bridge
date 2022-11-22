39b0c992f99a0d74498a0a8c3b67f084c7a2ca71
jungsmins1 <jungsmins00@gmail.com>
2022-11-22 12:46:46 +0900

    refactor: 현재까지 이동한 다리의 상태를 만드는 메서드 로직 변경

--- src/BridgeSet.js
+++ src/BridgeSet.js
@@ -1,83 +1,71 @@
-const InputView = require("./InputView");
-const { printResult, printMap } = require("./OutputView");
 const OutputView = require("./OutputView");
 const { SPACE_TO_MOVE, OUTPUT_MESSAGE, GAME_RESULT } = require("./Utils");
 
 const BridgeSet = {
-  overBridge: [],
-  underBridge: [],
 
-  repeat() {
-    InputView.readMoving();
-  },
-
-  gameStop() {
-    InputView.readGameCommand();
-  },
-
-  BridgePass(moving) {
+  bridgePass(moving, overBridge, underBridge) {
     if(moving === SPACE_TO_MOVE.MOVE_UP) {
-      BridgeSet.overBridgePass();
+      return this.overBridgePass(overBridge, underBridge);
     }
     if(moving === SPACE_TO_MOVE.MOVE_DOWN) {
-      BridgeSet.underBridgePass();
+      return this.underBridgePass(overBridge, underBridge);
     }
-    return OutputView.printMap(this.overBridge, this.underBridge);
   },
 
-  BridgeFail(moving) {
+  bridgeFail(moving, overBridge, underBridge) {
     if(moving === SPACE_TO_MOVE.MOVE_UP) {
-      BridgeSet.overBridgefail();
+      return this.overBridgefail(overBridge, underBridge);
     }
     if(moving === SPACE_TO_MOVE.MOVE_DOWN) {
-      BridgeSet.underBridgefail();
+      return this.underBridgefail(overBridge, underBridge);
     }
-    return OutputView.printMap(this.overBridge, this.underBridge);
   },
 
-  overBridgePass() {
-    if(this.overBridge.length === 0 && this.underBridge.length === 0) {
-      this.overBridge.push(OUTPUT_MESSAGE.MOVE_SUCCESS);
-      this.underBridge.push(OUTPUT_MESSAGE.BRIDGE_EMPTY);
+  overBridgePass(overBridge, underBridge) {
+    if(overBridge.length > 0 && underBridge.length > 0) {
+      overBridge.push(OUTPUT_MESSAGE.BRIDGE_BETWEEN + OUTPUT_MESSAGE.MOVE_SUCCESS);
+      underBridge.push(OUTPUT_MESSAGE.BRIDGE_BETWEEN + OUTPUT_MESSAGE.BRIDGE_EMPTY);
     }
-    if(this.overBridge.length > 0 && this.underBridge.length > 0) {
-      this.overBridge.push(OUTPUT_MESSAGE.BRIDGE_BETWEEN + OUTPUT_MESSAGE.MOVE_SUCCESS);
-      this.underBridge.push(OUTPUT_MESSAGE.BRIDGE_BETWEEN + OUTPUT_MESSAGE.BRIDGE_EMPTY);
+    if(overBridge.length === 0 && underBridge.length === 0) {
+      overBridge.push(OUTPUT_MESSAGE.MOVE_SUCCESS);
+      underBridge.push(OUTPUT_MESSAGE.BRIDGE_EMPTY);
     }
   },
+
+  underBridgePass(overBridge, underBridge) {
+    if(overBridge.length > 0 && underBridge.length > 0) {
+      overBridge.push(OUTPUT_MESSAGE.BRIDGE_BETWEEN + OUTPUT_MESSAGE.BRIDGE_EMPTY);
+      underBridge.push(OUTPUT_MESSAGE.BRIDGE_BETWEEN + OUTPUT_MESSAGE.MOVE_SUCCESS);
+    }
+    if(overBridge.length === 0 && underBridge.length === 0) {
+      overBridge.push(OUTPUT_MESSAGE.BRIDGE_EMPTY);
+      underBridge.push(OUTPUT_MESSAGE.MOVE_SUCCESS);
+    }
+  },
+
+  overBridgefail(overBridge, underBridge) {
+    if(overBridge.length > 0 && underBridge.length > 0) {
+      overBridge.push(OUTPUT_MESSAGE.BRIDGE_BETWEEN + OUTPUT_MESSAGE.MOVE_FAILURE);
+      underBridge.push(OUTPUT_MESSAGE.BRIDGE_BETWEEN + OUTPUT_MESSAGE.BRIDGE_EMPTY);
+    }
+    if(overBridge.length === 0 && underBridge.length === 0) {
+      overBridge.push(OUTPUT_MESSAGE.MOVE_FAILURE);
+      underBridge.push(OUTPUT_MESSAGE.BRIDGE_EMPTY);
+    }
+  },
+
+  underBridgefail(overBridge, underBridge) {
+    if(overBridge.length > 0 && underBridge.length > 0) {
+      overBridge.push(OUTPUT_MESSAGE.BRIDGE_BETWEEN + OUTPUT_MESSAGE.BRIDGE_EMPTY);
+      underBridge.push(OUTPUT_MESSAGE.BRIDGE_BETWEEN + OUTPUT_MESSAGE.MOVE_FAILURE);
+    }
+    if(overBridge.length === 0 &&underBridge.length === 0) {
+      overBridge.push(OUTPUT_MESSAGE.BRIDGE_EMPTY);
+      underBridge.push(OUTPUT_MESSAGE.MOVE_FAILURE);
+    }
+  },
+
   
-  underBridgePass() {
-    if(this.overBridge.length === 0 && this.underBridge.length === 0) {
-      this.overBridge.push(OUTPUT_MESSAGE.BRIDGE_EMPTY);
-      this.underBridge.push(OUTPUT_MESSAGE.MOVE_SUCCESS);
-    }
-    if(this.overBridge.length > 0 && this.underBridge.length > 0) {
-      this.overBridge.push(OUTPUT_MESSAGE.BRIDGE_BETWEEN + OUTPUT_MESSAGE.BRIDGE_EMPTY);
-      this.underBridge.push(OUTPUT_MESSAGE.BRIDGE_BETWEEN + OUTPUT_MESSAGE.MOVE_SUCCESS);
-    }
-  },
-
-  overBridgefail() {
-    if(this.overBridge.length === 0 && this.underBridge.length === 0) {
-      this.overBridge.push(OUTPUT_MESSAGE.MOVE_FAILURE);
-      this.underBridge.push(OUTPUT_MESSAGE.BRIDGE_EMPTY);
-    }
-    if(this.overBridge.length > 0 && this.underBridge.length > 0) {
-      this.overBridge.push(OUTPUT_MESSAGE.BRIDGE_BETWEEN + OUTPUT_MESSAGE.MOVE_FAILURE);
-      this.underBridge.push(OUTPUT_MESSAGE.BRIDGE_BETWEEN + OUTPUT_MESSAGE.BRIDGE_EMPTY);
-    }
-  },
-
-  underBridgefail() {
-    if(this.overBridge.length === 0 && this.underBridge.length === 0) {
-      this.overBridge.push(OUTPUT_MESSAGE.BRIDGE_EMPTY);
-      this.underBridge.push(OUTPUT_MESSAGE.MOVE_FAILURE);
-    }
-    if(this.overBridge.length > 0 && this.underBridge.length > 0) {
-      this.overBridge.push(OUTPUT_MESSAGE.BRIDGE_BETWEEN + OUTPUT_MESSAGE.BRIDGE_EMPTY);
-      this.underBridge.push(OUTPUT_MESSAGE.BRIDGE_BETWEEN + OUTPUT_MESSAGE.MOVE_FAILURE);
-    }
-  },
 
   beforeBridge() {
     this.overBridge.pop();
@@ -86,13 +74,13 @@ const BridgeSet = {
   },
 
   successEnd(tryCount) {
-    printResult(GAME_RESULT.SUCCESS, tryCount);
+    OutputView.printResult(GAME_RESULT.SUCCESS, tryCount);
     this.overBridge = [];
     this.underBridge = [];
   },
 
   failureEnd(tryCount) {
-    printResult(GAME_RESULT.FAILURE, tryCount);
+    OutputView.printResult(GAME_RESULT.FAILURE, tryCount);
     this.overBridge = [];
     this.underBridge = [];
   }
