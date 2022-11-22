0b5039a1b16803a4ce9cf92ee19e846486f86920
고동현 <dhjk35@naver.com>
2022-11-22 18:10:50 +0900

    refactor(OuputView): 불필요한 주석 제거 및 사용되는 문자열 상수로 정의

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,50 +1,56 @@
 const { Console } = require('@woowacourse/mission-utils');
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
+const OUTPUT_MESSAGE = Object.freeze({
+  start: '다리 건너기 게임을 시작합니다.\n',
+  gameResult: '최종 게임 결과',
+  successOrFailure: '게임 성공 여부',
+  attemptsCount: '총 시도한 횟수',
+});
+
+const MOVE_RESULT = Object.freeze({
+  success: 'O',
+  failure: 'X',
+  notSelected: ' ',
+  seperator: ' | ',
+});
+
+const GAME_RESULT = Object.freeze({
+  success: '성공',
+  failure: '실패',
+});
+
 const OutputView = {
-  /**
-   * 게임 시작 문구를 출력한다.
-   */
-  printStart() {
-    Console.print('다리 건너기 게임을 시작합니다.\n');
+  print(message) {
+    Console.print(message);
   },
-  /**
-   * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-   * <p>
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
+
+  printStart() {
+    Console.print(OUTPUT_MESSAGE.start);
+  },
+
   printMap(map) {
     const parts = map.map(([direction, movingState]) => {
-      const mark = movingState ? 'O' : 'X';
-      return direction === 'U' ? [mark, ' '] : [' ', mark];
+      const mark = movingState ? MOVE_RESULT.success : MOVE_RESULT.failure;
+      const currentPart = [mark, MOVE_RESULT.notSelected];
+
+      return direction === 'U' ? currentPart : currentPart.reverse();
     });
+
     const upperPart = parts.map(([upper]) => upper);
     const lowerPart = parts.map(([, lower]) => lower);
 
-    Console.print(`[ ${upperPart.join(' | ')} ]`);
-    Console.print(`[ ${lowerPart.join(' | ')} ]\n`);
+    Console.print(`[ ${upperPart.join(MOVE_RESULT.seperator)} ]`);
+    Console.print(`[ ${lowerPart.join(MOVE_RESULT.seperator)} ]\n`);
   },
 
-  /**
-   * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-   * <p>
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
   printResult({ map, attempts, isSuccess }) {
-    const gameResult = isSuccess ? '성공' : '실패';
+    const gameResult = isSuccess ? GAME_RESULT.success : GAME_RESULT.failure;
 
-    Console.print('최종 게임 결과');
+    Console.print(OUTPUT_MESSAGE.gameResult);
     this.printMap(map);
 
-    Console.print(`게임 성공 여부: ${gameResult}`);
-    Console.print(`총 시도한 횟수: ${attempts}`);
-    Console.close();
-  },
-
-  print(message) {
-    Console.print(message);
+    Console.print(`${OUTPUT_MESSAGE.successOrFailure}: ${gameResult}`);
+    Console.print(`${OUTPUT_MESSAGE.attemptsCount}: ${attempts}`);
   },
 };
 
