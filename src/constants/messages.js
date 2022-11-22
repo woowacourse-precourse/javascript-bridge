2afde723e12393665811c8c225672a2cca2252c1
oseunghyun <ohsh1477@gmail.com>
2022-11-22 15:40:02 +0900

    refactor(validations): 에러 메세지 상수화 및 메세지 내용 변경

---
+++ src/constants/messages.js
@@ -0,0 +1,11 @@
+const ERROR_MESSAGE = {
+  ONLY_NUM: '[ERROR] 숫자만 입력할 수 있습니다.',
+  NOT_INT_RANGE: '[ERROR] 3부터 20 사이의 숫자여야 합니다.',
+  NOT_DECIMAL_POINT: '[ERROR] 소수점은 입력할 수 없습니다.',
+  NOT_SMALL_LETTER: '[ERROR] 대문자로 입력해야 합니다.',
+  ONLY_MOVE_KEY: '[ERROR] U 또는 D를 입력하여 이동할 수 있습니다.',
+  ONLY_RETRY_KEY:
+    '[ERROR] R 또는 Q를 입력하여 재시도/종료 여부를 결정할 수 있습니다.',
+};
+
+module.exports = { ERROR_MESSAGE };
--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,21 +1,23 @@
+const { ERROR_MESSAGE } = require('../constants/messages');
+
 const validateBridgeSize = (size) => {
-  const NOT_NUM = '[ERROR] 숫자로 입력해주세요.';
-  const MESSAGE = '[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.';
-  if (!Number(size)) throw NOT_NUM;
-  if (3 > size || size > 20) throw MESSAGE;
+  if (!Number(size)) throw ERROR_MESSAGE.ONLY_NUM;
+  if (3 > size || size > 20) throw ERROR_MESSAGE.NOT_INT_RANGE;
+  if (size % 1 !== 0) throw ERROR_MESSAGE.NOT_DECIMAL_POINT;
   return true;
 };
 
 const validateUserPosition = (position) => {
-  const TYPO_ERROR = '[ERROR] U 또는 D를 입력하여 이동할 수 있습니다.';
-  if (!(position === 'U' || position == 'D')) throw TYPO_ERROR;
+  if (position === 'u' || position === 'd')
+    throw ERROR_MESSAGE.NOT_SMALL_LETTER;
+  if (!(position === 'U' || position === 'D'))
+    throw ERROR_MESSAGE.ONLY_MOVE_KEY;
   return true;
 };
 
 const validateRetryAnswer = (answer) => {
-  const RETRY_MESSAGE =
-    '[ERROR] 게임을 끝내려면 Q를 재시도하려면 R을 입력해주세요.';
-  if (!(answer === 'Q' || answer == 'R')) throw RETRY_MESSAGE;
+  if (answer === 'q' || answer === 'r') throw ERROR_MESSAGE.NOT_SMALL_LETTER;
+  if (!(answer === 'Q' || answer === 'R')) throw ERROR_MESSAGE.ONLY_RETRY_KEY;
   return true;
 };
 
