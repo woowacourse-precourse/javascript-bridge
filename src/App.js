79c7e361e068c2e8bb514c7ef38b425046ead456
Sangjun-man <molung@icloud.com>
2022-11-22 20:03:21 +0900

    feat(App) : 애플리케이션 관리 기능 구현
    애플리케이션 진행 상황 관리 기능, 게임 단계에 맞는 진행 기능이 있는 App 클래스를 구현했습니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,75 @@
+const InputView = require("./InputView");
+const OutputView = require("./OutputView");
+const BridgeGame = require("./BridgeGame");
+const VaildationCheck = require("./VaildationCheck");
+
 class App {
-  play() {}
+  bridgeGame;
+
+  play() {
+    OutputView.printStart();
+    InputView.readBridgeSize(this.configBridge.bind(this));
+  }
+
+  playing() {
+    if (this.gameSuccessCheck()) return this.end();
+
+    if (!this.gamePlayingCheck())
+      return InputView.readGameCommand(this.commmand.bind(this));
+
+    return InputView.readMoving(this.move.bind(this));
+  }
+
+  configBridge(bridgeSize) {
+    try {
+      bridgeSize = Number(bridgeSize);
+      VaildationCheck.isNumber(bridgeSize);
+      VaildationCheck.isNumberIntheRange([3, 20], bridgeSize);
+      this.bridgeGame = new BridgeGame(bridgeSize);
+    } catch (e) {
+      OutputView.print(e);
+      return InputView.readBridgeSize(this.configBridge.bind(this));
+    }
+    this.playing();
+  }
+
+  move(moveInput) {
+    try {
+      VaildationCheck.isStringIntheList(["U", "D"], moveInput);
+      const { movementStatus } = this.bridgeGame.move(moveInput);
+      OutputView.printMap(movementStatus);
+    } catch (e) {
+      OutputView.print(e);
+      return InputView.readMoving(this.move.bind(this));
+    }
+    return this.playing();
+  }
+
+  commmand(retryInput) {
+    try {
+      const retryResult = this.bridgeGame.retry(retryInput);
+      if (retryResult) return this.playing();
+      return this.end();
+    } catch (e) {
+      OutputView.print(e);
+      return InputView.readGameCommand(this.commmand.bind(this));
+    }
+  }
+
+  gamePlayingCheck() {
+    return this.bridgeGame.gameStatus.playing;
+  }
+  gameSuccessCheck() {
+    return this.bridgeGame.gameStatus.success;
+  }
+  end() {
+    OutputView.printEnd();
+    OutputView.printResult(this.bridgeGame.gameStatus);
+    InputView.close();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
