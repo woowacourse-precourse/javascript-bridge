c0d0891b2daecbfa7ee82399661bc81bc10980fd
Ryan-Dia <cjfdnjs158@naver.com>
2022-11-22 16:10:02 +0900

    docs:기본 세팅파일 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const BridgeGame = require('./model/BridgeGame');
+const InputView = require('./view/InputView');
+const OutputView = require('./view/OutputView');
+const BridgeController = require('./controller/BridgeController');
+
 class App {
-  play() {}
+  constructor() {
+    this.inputView = InputView;
+    this.outputView = OutputView;
+    this.bridgeGame = new BridgeGame();
+    this.bridgeCntroller = new BridgeController(this.inputView, this.outputView, this.bridgeGame);
+  }
+
+  play() {
+    this.bridgeCntroller.runProcess();
+  }
 }
 
 module.exports = App;
