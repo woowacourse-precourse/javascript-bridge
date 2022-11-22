732ac40a691ba4a76c08e12fdc296665e704c693
SeungHun Park <tmdgns1126@naver.com>
2022-11-22 17:37:44 +0900

    feat: 시도횟수 App에 초기화

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ const { Console } = require("@woowacourse/mission-utils");
 const InputView = require("./InputView");
 
 class App {
+  #tryCount;
+  constructor() {
+    this.#tryCount = 1;
+  }
   play() {
     Console.print("다리 건너기 게임을 시작합니다.\n");
     InputView.readBridgeSize();
