ff6ab25220e9764e4ad6e806c0ebf4621c88894c
sunub <bsc5672@gmail.com>
2022-11-22 15:28:11 +0900

    refactor : 기존의 불명확한 변수 이름과 중복되는 함수 리펙토링
    기존의 테스트 코드는 중복되는 부분이 많았다. 그래서 이 부분들을 함수로
    정리하여 간단하게 한줄로 정리 할 수 있게끔 리펙토링 하였고 각각의 변수의
    이름이 명확하게 지어지지 않았던 문제가 있어서 리펙토링 하였다.

--- __tests__/my.test.js
+++ __tests__/my.test.js
@@ -6,22 +6,8 @@ const BridgeRandomNumberGenerator = require("../src/BridgeRandomNumberGenerator"
 const BridgeGame = require("../src/BridgeGame");
 const OutputView = require("../src/OutputView");
 
-const mockQuestions = (answers) => {
-  MissionUtils.Console.readLine = jest.fn();
-  answers.reduce((acc, input) => {
-    return acc.mockImplementationOnce((_, callback) => {
-      callback(input);
-    });
-  }, MissionUtils.Console.readLine);
-};
 const error = new Error();
 const bridgeGame = new BridgeGame();
-
-const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, "print");
-  logSpy.mockClear();
-  return logSpy;
-};
 
 describe("다리 길이의 입력 값이", () => {
   test("3~20 사이의 숫자가 아닐 경우 예외가 발생하는지", () => {
@@ -29,16 +15,17 @@ describe("다리 길이의 입력 값이", () => {
 
     answers.forEach((answer) => {
       expect(() => {
-        const result = error.bridgeSizeError(answer);
+        error.bridgeSizeError(answer);
       }).toThrow(expect.stringContaining("[ERROR]"));
     });
   });
 
   test("주어진 만큼 다리의 개수가 생성이 되었는지", () => {
     const sizes = [3, 1000, 600];
+
     sizes.forEach((size) => {
-      const bridge = BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate);
-      expect(bridge).toHaveLength(size);
+      const bridgeLength = BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate);
+      expect(bridgeLength).toHaveLength(size);
     });
   });
 });
@@ -66,20 +53,16 @@ describe("이동 값 U가", () => {
 
   test("정답일 경우 정답 표시인 'O'를 반환 하는지", () => {
     const correctAnswer = "U";
-    const expectedValue = "O";
-
     userData["currentAnswer"] = correctAnswer;
-    const result = bridgeGame.move(userData);
-    expect(result["upperBridge"]).toContainEqual(expect.stringContaining(expectedValue));
+
+    correctCase(userData, "upperBridge");
   });
 
   test("오답일 경우 오답 표시인 'X'를 반환 하는지", () => {
     const incorrectAnswer = "D";
-    const expectedValue = "X";
-
     userData["currentAnswer"] = incorrectAnswer;
-    const result = bridgeGame.move(userData);
-    expect(result["lowerBridge"]).toContainEqual(expect.stringContaining(expectedValue));
+
+    incorrectCase(userData, "lowerBridge");
   });
 });
 
@@ -94,24 +77,24 @@ describe("이동 값 D가", () => {
 
   test("정답일 경우 정답 표시인 'O'를 반환 하는지", () => {
     const correctAnswer = "D";
-    const expectedValue = "O";
-
     userData["currentAnswer"] = correctAnswer;
-    const result = bridgeGame.move(userData);
-    expect(result["lowerBridge"]).toContainEqual(expect.stringContaining(expectedValue));
+
+    correctCase(userData, "lowerBridge");
   });
 
   test("오답일 경우 오답 표시인 'X'를 반환 하는지", () => {
     const incorrectAnswer = "U";
-    const expectedValue = "X";
-
     userData["currentAnswer"] = incorrectAnswer;
-    const result = bridgeGame.move(userData);
-    expect(result["upperBridge"]).toContainEqual(expect.stringContaining(expectedValue));
+
+    incorrectCase(userData, "upperBridge");
   });
 });
 
 describe("게임을 다시 시작할 때 값이", () => {
+  const userData = {
+    currentAnswer: "",
+  };
+
   test("R또는 Q 이외의 값이 주어졌을때 예외가 발생하는지", () => {
     const answers = [1, "d", "ㅎㅎ"];
 
@@ -122,16 +105,12 @@ describe("게임을 다시 시작할 때 값이", () => {
     });
   });
 
-  const userData = {
-    currentAnswer: "",
-  };
-
   test("R일 경우 이동 값을 다시 선택할 수 있게 이어지는지,", () => {
     userData["currentAnswer"] = "R";
     const readMoving = jest.spyOn(InputView, "readMoving");
     const gameCommand = bridgeGame.retry(userData);
 
-    InputView.checkStatus(gameCommand);
+    InputView.retryOrNot(gameCommand);
     expect(readMoving).toHaveBeenCalled();
   });
 
@@ -140,7 +119,7 @@ describe("게임을 다시 시작할 때 값이", () => {
     const printResult = jest.spyOn(OutputView, "printResult");
     const gameCommand = bridgeGame.retry(userData);
 
-    InputView.checkStatus(gameCommand);
+    InputView.retryOrNot(gameCommand);
     expect(printResult).toHaveBeenCalled();
   });
 
@@ -148,7 +127,19 @@ describe("게임을 다시 시작할 때 값이", () => {
     const consoleEnd = jest.spyOn(MissionUtils.Console, "close");
     const gameCommand = bridgeGame.retry(userData);
 
-    InputView.checkStatus(gameCommand);
+    InputView.retryOrNot(gameCommand);
     expect(consoleEnd).toHaveBeenCalled();
   });
 });
+
+function correctCase(userData, bridge) {
+  const expectedValue = "O";
+  const result = bridgeGame.move(userData)[bridge];
+  expect(result).toContainEqual(expect.stringContaining(expectedValue));
+}
+
+function incorrectCase(userData, bridge) {
+  const expectedValue = "X";
+  const result = bridgeGame.move(userData)[bridge];
+  expect(result).toContainEqual(expect.stringContaining(expectedValue));
+}
