d7182e54689a2ad28e942aaf810813d70d143e1e
NaveOWO <pianowell96@gmail.com>
2022-11-22 18:23:30 +0900

    [ test ] BridgeMaker 함수 테스트 비교값 개선

--- __tests__/BridgeMakerTest.js
+++ __tests__/BridgeMakerTest.js
@@ -12,10 +12,15 @@ describe("BridgeMaker 함수 기능테스트", () => {
     ).toBe(size);
   });
 
-  test("입력받은 다리 크기에 맞게 랜덤 다리를 생성하는 함수의 길이 테스트", () => {
+  test("랜덤 locations에 따른 다리 배열을 생성하는 함수 테스트", () => {
     const size = 3;
-    expect(
-      BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate).length
-    ).toBe(size);
+    const generateRandomNumber = () => {
+      return 0;
+    };
+    expect(BridgeMaker.makeBridge(size, generateRandomNumber)).toEqual([
+      "D",
+      "D",
+      "D",
+    ]);
   });
 });
