5bd339bbc3cc83ddf2d1aa087190bc4324928160
Sallyvely <tpdus4159@naver.com>
2022-11-22 13:09:07 +0900

    feat : 다리 입력과 정답다리를 가져오는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+const { makeBridge } = require("./BridgeMaker");
+const { generate } = require("./BridgeRandomNumberGenerator");
+const { readBridgeSize, readMoving, readGameCommand } = require("./InputView");
+const { Console } = require('@woowacourse/mission-utils');
+const { printFail, printBridgeMap, resetBridge, printResult, resetCount } = require("./OutputView");
+const BridgeGame = require("./BridgeGame");
+const MESSAGE = require("./message");
+let bridgeSize,bridgeBase;
 class App {
-  play() {}
+  play() {
+
+  }
+  
+  makeBridgeBase() {
+    bridgeSize=readBridgeSize()
+    bridgeBase=makeBridge(bridgeSize,generate);
+    this.bridgeTransfer();
+  }
+
+
 }
 
 module.exports = App;
