d088f14640f24ba013691d03971d88355bce64f4
박겸영 <yong_drg@naver.com>
2022-11-22 17:06:40 +0900

    feat(BridgeGame): 재시작 시 다리 건너기 결과 초기화하는 기능 추가
    - 다리 건넌 기록 초기화
    - 현재 건너는 다리 칸 index 초기화

--- src/Bridge.js
+++ src/Bridge.js
@@ -26,6 +26,10 @@ class Bridge {
     const isEnd = this.#currentIndex === this.#targetBridge.length - 1;
     return isMovable && isEnd;
   }
+
+  reset() {
+    this.#currentIndex = 0;
+  }
 }
 
 module.exports = Bridge;
