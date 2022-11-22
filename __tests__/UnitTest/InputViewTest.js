e2fbdc2ac2599e5210890ae760dc5e63615409e1
Monte <monte6198@gmail.com>
2022-11-22 18:38:37 +0900

    feat: 플레이어 입력에 대한 단위 테스트 추가

--- __tests__/UnitTest/InputViewTest.js
+++ __tests__/UnitTest/InputViewTest.js
@@ -1,7 +1,7 @@
 const { Console } = require('@woowacourse/mission-utils');
 const InputView = require('../../src/view/InputView');
 
-describe.only('다리 건너기 테스트', () => {
+describe('사용자 입력 테스트', () => {
   test('다리 길이 입력 테스트', () => {
     const bridgeSize = 5;
     const nextStep = jest.fn();
@@ -12,5 +12,27 @@ describe.only('다리 건너기 테스트', () => {
     InputView.readBridgeSize(nextStep);
 
     expect(nextStep).toHaveBeenCalledWith(bridgeSize);
+  });
+  test('다리 이동 입력 테스트', () => {
+    const movement = 'U';
+    const nextStep = jest.fn();
+    // InputView.validateBridgeSize = jets.fn()
+    Console.readLine = jest.fn((message, callback) => {
+      nextStep(movement);
+    });
+    InputView.readMoving(nextStep);
+
+    expect(nextStep).toHaveBeenCalledWith(movement);
+  });
+  test('게임 재시작 및 종료 입력 테스트', () => {
+    const restartOrQuit = 'Q';
+    const nextStep = jest.fn();
+    // InputView.validateBridgeSize = jets.fn()
+    Console.readLine = jest.fn((message, callback) => {
+      nextStep(restartOrQuit);
+    });
+    InputView.readMoving(nextStep);
+
+    expect(nextStep).toHaveBeenCalledWith(restartOrQuit);
   });
 });
