7dd5ffd43faf41ee746c57f53169cc75b78b5759
ilgon0110 <moses011010@gmail.com>
2022-11-22 23:18:16 +0900

    fix: deepFreeze 유틸 복구(예기치 못한 오류 복구)

--- src/Constants.js
+++ src/Constants.js
@@ -1,23 +1,25 @@
-const MESSAGE = {
+const { deepFreeze } = require('./Utilities');
+
+const MESSAGE = deepFreeze({
   BRIDGE_SIZE: '다리의 길이를 입력해주세요. ',
   UPORDOWN: '이동할 칸을 선택해주세요. (위: U, 아래: D)',
   RETRYORQUIT: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)',
-};
+});
 
-const INPUT = {
+const INPUT = deepFreeze({
   UP: 'U',
   DOWN: 'D',
   RESTART: 'R',
   QUIT: 'Q',
-};
+});
 
-const BRIDGE = {
+const BRIDGE = deepFreeze({
   SUCESS: ' O |',
   FAIL: ' X |',
   NOTHING: '   |',
-};
+});
 
-const ERROR = {
+const ERROR = deepFreeze({
   SIZE: '[ERROR] 3~20 사이 숫자만 입력 가능합니다.',
   MOVE: '[ERROR] 대문자 U나 D만 입력 가능합니다.',
   RETRY: '[ERROR] 대문자 R이나 Q만 입력 가능합니다.',
@@ -25,21 +27,21 @@ const ERROR = {
     INSTANCE: '인터페이스 클래스로 인스턴스를 생성하였습니다.',
     METHOD: '인터페이스 클래스입니다. 메서드 구현이 필요합니다.',
   },
-};
+});
 
-const SIZE_RANGE = {
+const SIZE_RANGE = deepFreeze({
   MIN: 3,
   MAX: 20,
-};
+});
 
-const RESULT = {
+const RESULT = deepFreeze({
   TITLE: '최종 게임 결과 \n',
   CHART: {
     SUCESS_OR_FAIL: (userLife) =>
       `\n 게임 성공 여부: ${userLife ? '성공' : '실패'}`,
     ATTEMPT: (attemptNumber) => `총 시도한 횟수: ${attemptNumber}`,
   },
-};
+});
 
 module.exports = {
   MESSAGE,
--- src/Utilities/index.js
+++ src/Utilities/index.js
@@ -1,3 +1,11 @@
 const editBridge = (bridge) => `[${bridge.slice(0, bridge.length - 1)}]`;
 
-module.exports = { editBridge };
+const deepFreeze = (targetObj) => {
+  Object.keys(targetObj).forEach((key) => {
+    if (typeof key === 'object') {
+      deepFreeze(targetObj[key]);
+    }
+  });
+  return Object.freeze(targetObj);
+};
+module.exports = { editBridge, deepFreeze };
