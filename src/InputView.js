2aceab067392149e52765de3b85c7eb8f88d483c
pitang <wpeffect@gmail.com>
2022-11-22 22:33:49 +0900

    feat: 입력

--- src/InputView.js
+++ src/InputView.js
@@ -1,4 +1,9 @@
+const { Console } = require("@woowacourse/mission-utils");
 const { GAME_MESSAGES } = require("./Constants");
+const BridgeGame = require("./BridgeGame");
+const OutputView = require("./OutputView");
+const Validate = require("./Validate");
+
 const InputView = {
   size: "",
   gameClient: "",
@@ -20,6 +25,79 @@ const InputView = {
     });
   },
 
+  sizeValidate(size) {
+    Validate.checkSizeInputType(size);
+    Validate.checkSizeInputRange(size);
+  },
+
+  printInputSizeError(error) {
+    OutputView.printError(error);
+    this.readBridgeSize();
+  },
+
+  gameRestart() {
+    this.gameClient.retry();
+    this.readMoving(this.gameClient, this.size);
+  },
+
+  readMoving() {
+    Console.readLine(GAME_MESSAGES.ASK_TO_MOVE_MAPS, (direction) => {
+      try {
+        Validate.checkDirectionInputType(direction);
+        this.insertMoving(direction);
+      } catch (error) {
+        this.printInputDirectionError(error);
+      }
+    });
+  },
+
+  printInputDirectionError(error) {
+    OutputView.printError(error);
+    this.readMoving();
+  },
+
+  insertMoving(direction) {
+    this.gameClient.move(direction);
+    this.checkIncludeWrongDirection();
+    this.checkMatchBridgeSize();
+  },
+
+  checkIncludeWrongDirection() {
+    if (!this.gameClient.checkWrongInput()) {
+      this.readGameCommand();
+    }
+  },
+
+  checkMatchBridgeSize() {
+    if (!this.gameClient.getMatchSize()) {
+      this.readMoving();
+    }
+  },
+
+  readGameCommand() {
+    Console.readLine(GAME_MESSAGES.ASK_OPINION_FOR_RESTART, (command) => {
+      try {
+        Validate.checkRetryCommandType(command);
+        this.insertCommand(command);
+      } catch (error) {
+        this.printInputCommandError(error);
+      }
+    });
+  },
+
+  printInputCommandError(error) {
+    OutputView.printError(error);
+    this.readGameCommand();
+  },
+
+  insertCommand(command) {
+    if (command === "R") {
+      this.gameRestart();
+      return;
+    }
+    this.gameClient.fail();
+    return;
+  },
 };
 
 module.exports = InputView;
