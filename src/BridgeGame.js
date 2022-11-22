8356a6007a6665c47d3e03972dbd5e94a0829b74
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-22 18:41:09 +0900

    feat: 실패인지 확인 하는 함수

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,6 +1,7 @@
 // BridgeGame 클래스에서 InputView, OutputView 를 사용하지 않는다.
 const BridgeMaker = require('./BridgeMaker');
 const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
+const validator = require('./validator/validator');
 
 class BridgeGame {
   #bridge;
@@ -19,6 +20,13 @@ class BridgeGame {
   }
 
   retry(command) {}
+
+  over() {
+    return validator.isSameArr(
+      this.#bridge.slice(0, this.#history.length),
+      this.#history
+    );
+  }
 
   generateBridge(size) {
     this.#bridge = BridgeMaker.makeBridge(
