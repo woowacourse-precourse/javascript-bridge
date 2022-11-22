4a1a2e2b55b8905d93981c0f29c6faec9ab1d2bb
KINHYEONJI <hyeonji0718@gmail.com>
2022-11-22 19:32:43 +0900

    refactor(App) : 다리를 모두 건너 게임을 성공했을 때 결과를 출력하는 부분을 successGame 추출

--- src/App.js
+++ src/App.js
@@ -37,17 +37,21 @@ class App {
 
   upDown() {
     if (this.#bridgeGame.isGameEnd()) {
-      this.outputView.printResult(
-        this.#bridgeGame.nowBridge,
-        "성공",
-        this.#bridgeGame.tryCount
-      );
-      this.inputView.close();
+      this.successGame();
     }
     this.inputView.readMoving(
       this.upDownCallback.bind(this),
       "이동할 칸을 선택해주세요. (위: U, 아래: D)"
     );
+  }
+
+  successGame() {
+    this.outputView.printResult(
+      this.#bridgeGame.nowBridge,
+      "성공",
+      this.#bridgeGame.tryCount
+    );
+    this.inputView.close();
   }
 
   upDownCallback(upDownInput) {
