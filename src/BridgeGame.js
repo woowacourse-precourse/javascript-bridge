7cfdd9f669255e68068b6ed02626a53e7ec5abe1
Hungeun2 <bepoohun@gmail.com>
2022-11-22 20:08:58 +0900

    Fix: 불필요한 import 삭제

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,6 +1,6 @@
 const { checkSide } = require('./util/Validation');
 const BridgeJudge = require('./util/BridgeJudge');
-const { GAME } = require('./util/Message');
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
