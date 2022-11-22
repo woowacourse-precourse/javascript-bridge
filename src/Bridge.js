03d5b5a8c580d663d604a5a4fa0d33c77f04d251
park junwoo <always1997@kakao.com>
2022-11-22 12:32:48 +0900

    feat: 게임 시도횟수, 단계를 조정하는 메서드 구현

--- src/Bridge.js
+++ src/Bridge.js
@@ -44,3 +44,4 @@ class Bridge {
     //gameOver()
   }
 }
+
--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,20 +1,28 @@
 const BridgeMaker = require("./BridgeMaker");
 const Bridge = requier("./Bridge");
+const InputView = requier("./InputView");
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
   #bridge = new Bridge();
-  #attemp = 1;
-  #stage = 0;
+  #attemp;
+  #stage;
+
+  constructor(){
+    this.#attemp = 1;
+    this.#stage = 0;
+  }
+
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
 
-  move(command, stage) {
-    this.#bridge.move(command, stage);
+  move() {
+    
+    this.#bridge.move(InputView.readMoving(), this.#stage);
   }
 
   /**
@@ -22,7 +30,18 @@ class BridgeGame {
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  retry() {}
+  retry() {
+
+  }
+
+  addAttemp(){
+    this.#attemp++;
+  }
+
+  stageReset(){
+    this.#stage = 0;
+  }
+
 }
 
 module.exports = BridgeGame;
