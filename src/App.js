ccb78a25b768f7252a47adf2d2defe7f639f83ed
rayark1 <max980203@snu.ac.kr>
2022-11-22 22:01:28 +0900

    chore(src/App.js): 상수 추가

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+const OutputView = require("./OutputView");
+const InputView = require("./InputView");
+const BridgeGame = require("./BridgeGame");
+const Validator = require("./Validator");
+
 class App {
   play() {}
 }
