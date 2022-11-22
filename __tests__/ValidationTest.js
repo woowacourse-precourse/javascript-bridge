df20a121b30c7586ebb49756119fdb7e9c1d2a2b
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-11-22 14:00:23 +0900

    refactor: ValidationTest의 test case 다양화

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -2,25 +2,24 @@
 const { isBridgeSizeValid, isUserMovingInputValid, isGameCommandValid } = require('../src/utils/validation');
 
 describe('유효 데이터 테스트', () => {
-  test('다리 생성 값이 숫자인가?', () => {
+  test.each(['d', ''])('다리 생성 값이 숫자가 아닌 경우', (input) => {
     expect(() => {
-      const input = 'dd';
       isBridgeSizeValid(input);
     }).toThrow('[ERROR]');
   });
-  test('다리 길이 범위는 3이상 ~ 20 이하의 숫자여야 한다.', () => {
+  test.each([2, 21])('다리 길이 범위가 3이상 ~ 20 이하의 숫자가 아닌 경우', (input) => {
     expect(() => {
-      isBridgeSizeValid(2);
+      isBridgeSizeValid(input);
     }).toThrow('[ERROR]');
   });
-  test('명령어 검증 : 이동 명령어가 U또는 D가 아닌 경우', () => {
+  test.each(['A', 'R', '1', 'r', ''])('명령어 검증 : 이동 명령어가 U또는 D가 아닌 경우', (input) => {
     expect(() => {
-      isUserMovingInputValid('Z');
+      isUserMovingInputValid(input);
     }).toThrow('[ERROR]');
   });
-  test('명령어 검증 : 게임 커맨드가 R또는 Q가 아닌 경우', () => {
+  test.each(['A', 'U', '1', 'r', ''])('명령어 검증 : 게임 커맨드가 R또는 Q가 아닌 경우', (input) => {
     expect(() => {
-      isGameCommandValid('Z');
+      isGameCommandValid(input);
     }).toThrow('[ERROR]');
   });
 });
