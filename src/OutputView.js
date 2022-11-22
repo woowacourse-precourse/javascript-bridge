a77bcd5d485ffbe7358cf21d2b4af09b41a23484
1 <wrasf175@naver.com>
2022-11-22 22:58:55 +0900

    refactor(OutputView): 게임 결과 메시지 상수로 분리

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,4 +1,5 @@
 const { Console } =require('@woowacourse/mission-utils');
+const NOTICE = require('../src/Constants/Notice');
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -21,15 +22,15 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printResult(userInput, bridgeInfo, count) {
-    Console.print('최종 게임 결과\n');
+    Console.print(NOTICE.GAME_RESULT);
     this.printMap(userInput, bridgeInfo);
     if(userInput.length === bridgeInfo.length){
-      Console.print(`게임 성공 여부: 성공\n`);
+      Console.print(NOTICE.FINAL_RESULT_WIN);
     }
     if(userInput.length !== bridgeInfo.length){
-      Console.print(`게임 성공 여부: 실패\n`);
+      Console.print(NOTICE.FINAL_RESULT_DEFEAT);
     }
-    Console.print('총 시도한 횟수: ' + count);
+    Console.print(NOTICE.TOTAL_TRY_COUNT + count);
     Console.close();
   },
   generateUpside(userInput, bridgeInfo) {
