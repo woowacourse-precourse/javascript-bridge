f9283ddd9f8ab2997239dbaad1bf07ecf89ac670
rayark1 <max980203@snu.ac.kr>
2022-11-22 21:10:42 +0900

    chore(src/BridgeGame.js): 모듈 가져오기

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,5 +1,7 @@
 const { makeBridge } = require("./BridgeMaker");
-const Constant = require("./Constant");
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+const OutputView = require("./OutputView");
+const { Console } = require("@woowacourse/mission-utils");
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
