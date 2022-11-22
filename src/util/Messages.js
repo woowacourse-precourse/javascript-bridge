bac07f4592eba82963d5cf2c962feaec08b54431
cmun2 <cmun2@illinois.edu>
2022-11-22 23:32:12 +0900

    feat: restart 재시작 메세지 추가

--- src/util/Messages.js
+++ src/util/Messages.js
@@ -1,7 +1,10 @@
 const OUTPUT_MESSAGE = Object.freeze({
   START: "다리 건너기 게임을 시작합니다.",
   MAP: (upBridge, downBridge) =>
-    `[${upBridge.join(" | ")}]\n[${downBridge.join(" | ")}]\n`,
+    `[ ${upBridge.join(" | ")} ]\n[ ${downBridge.join(" | ")} ]\n`,
+  END: "최종 게임 결과",
+  IS_SUCCESS: (isSuccess) => `게임 성공 여부: ${isSuccess}`,
+  COUNT: (count) => `총 시도한 횟수: ${count}`,
 });
 
 const OPTION_MESSAGE = Object.freeze({
@@ -11,8 +14,9 @@ const OPTION_MESSAGE = Object.freeze({
 });
 
 const ERROR_MESSAGE = Object.freeze({
-  LENGTH_ERROR: "[ERROR]: 다리 길이는 3 ~ 20 사이의 숫자를 입력해야 합니다.",
-  MOVE_ERROR: "[ERROR]: 이동할 칸 입력은 U와 D중 하나만 입력해야 합니다.",
+  LENGTH_ERROR: "[ERROR] 다리 길이는 3 ~ 20 사이의 숫자를 입력해야 합니다.",
+  MOVE_ERROR: "[ERROR] 이동할 칸 입력은 U와 D중 하나만 입력해야 합니다.",
+  RESTART_ERROR: "[ERROR] 재시작은 'R'을, 종료는 'Q'를 입력해야 합니다. ",
 });
 
 module.exports(OUTPUT_MESSAGE, OPTION_MESSAGE, ERROR_MESSAGE);
