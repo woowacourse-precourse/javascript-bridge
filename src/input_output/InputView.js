b8955058a97a6e274c2ba12ceb7543aa407fdb5c
RooT_Kim <bdd3452@gmail.com>
2022-11-22 11:05:51 +0900

    feat: 메소드 구현 다리 길이를 입력받는 readBridgeSize 메소드 구현 사용자가 이동할 칸을 입력 받는 readMoving 메소드 구현

--- src/input_output/InputView.js
+++ src/input_output/InputView.js
@@ -1,17 +1,39 @@
 const { Console } = require('@woowacourse/mission-utils');
 const { MESSAGES } = require('../utils/Constants');
+const { checkSizeValidation, checkMovingValidation } = require('../utils/Validation');
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
+	/**
+	 * 다리의 길이를 입력받는다.
+	 */
 	readBridgeSize(callback) {
-		Console.readLine(MESSAGES.bridgeSize, callback);
+		Console.readLine(MESSAGES.bridgeSize, (size) => {
+			try {
+				checkSizeValidation(size);
+				callback(size);
+			} catch (error) {
+				Console.print(error);
+				this.readBridgeSize(callback);
+			}
+		});
 	},
 
 	/**
 	 * 사용자가 이동할 칸을 입력받는다.
 	 */
-	readMoving(callback) {},
+	readMoving(callback) {
+		Console.readLine(MESSAGES.chooseMoving, (moving) => {
+			try {
+				checkMovingValidation(moving);
+				callback(moving);
+			} catch (error) {
+				Console.print(error);
+				this.readMoving(callback);
+			}
+		});
+	},
 
 	/**
 	 * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
