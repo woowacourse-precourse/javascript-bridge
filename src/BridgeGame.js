a8c47cdef8ec4c21556331278aebf86c89c828ab
Jungkyu Yang <67031524+jungkyuYang@users.noreply.github.com>
2022-11-22 19:56:12 +0900

    feat: add 칸이동 기능

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -2,12 +2,55 @@
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  #bridge;
+  #moveRoute;
+  #index;
+
+  constructor(bridge) {
+    this.#bridge = bridge;
+    this.#moveRoute = [[], []];
+    this.#index = 0;
+  }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(selectedBlock) {
+    if (selectedBlock === this.#bridge[this.#index]) {
+      this.markCorrect(selectedBlock);
+    } else if (selectedBlock !== this.#bridge[this.#index]) {
+      this.markWrong(selectedBlock);
+    }
+
+    return this.#moveRoute;
+  }
+
+  markCorrect(selectedBlock) {
+    if (selectedBlock === "U") {
+      this.#moveRoute[0].push("O");
+      this.#moveRoute[1].push(" ");
+      this.#index += 1;
+    }
+
+    if (selectedBlock === "D") {
+      this.#moveRoute[0].push(" ");
+      this.#moveRoute[1].push("O");
+      this.#index += 1;
+    }
+  }
+
+  markWrong(selectedBlock) {
+    if (selectedBlock === "U") {
+      this.#moveRoute[0].push("X");
+      this.#moveRoute[1].push(" ");
+    }
+
+    if (selectedBlock === "D") {
+      this.#moveRoute[0].push(" ");
+      this.#moveRoute[1].push("X");
+    }
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,6 @@
+const BridgeGame = require("./BridgeGame");
+const BridgeMaker = require("./BridgeMaker");
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
 const Validator = require("./Validation/Validator");
 
 const { Console } = require("@woowacourse/mission-utils");
@@ -10,23 +13,33 @@ const InputView = {
    */
   readBridgeSize() {
     Console.readLine("다리의 길이를 입력해주세요.\n", (bridgeSize) => {
-      if (Validator.checkBetweenThreeToTwenty(Number(bridgeSize)))
+      if (Validator.checkBetweenThreeToTwenty(bridgeSize))
         return this.readBridgeSize();
 
-      return;
+      const bridge = BridgeMaker.makeBridge(
+        bridgeSize,
+        BridgeRandomNumberGenerator.generate
+      );
+
+      const bridgeGame = new BridgeGame(bridge);
+
+      return this.readMoving(bridgeGame);
     });
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {
+  readMoving(bridgeGame) {
     Console.readLine(
       "이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
       (selectedBlock) => {
-        if (Validator.checkUpOrDown(selectedBlock)) return this.readMoving();
+        if (Validator.checkUpOrDown(selectedBlock))
+          return this.readMoving(bridgeGame);
 
-        return;
+        const moveRoute = bridgeGame.move(selectedBlock);
+
+        return this.readMoving(bridgeGame);
       }
     );
   },
@@ -37,8 +50,8 @@ const InputView = {
   readGameCommand() {
     Console.readLine(
       "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n",
-      (request) => {
-        if (Validator.checkRetryOrQuit(request)) return this.readGameCommand();
+      (answer) => {
+        if (Validator.checkRetryOrQuit(answer)) return this.readGameCommand();
 
         return;
       }
