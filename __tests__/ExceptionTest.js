2a7b1695c0024475753de1940173036430cf3e04
Hoon <si932174@gmail.com>
2022-11-22 10:41:23 +0900

    test : 재시도 예외 테스트 구현

--- __tests__/ExceptionTest.js
+++ __tests__/ExceptionTest.js
@@ -1,21 +1,28 @@
 const InputErrorCheck = require('../src/InputErrorCheck');
 
-describe('예외처리 테스트', () => {
-  test('다리생성 예외 테스트', () => {
-    const exceptionInputs = ['1', '2', '21', 's', 'ㅇ'];
-    exceptionInputs.map((input) => {
-      expect(() => {
-        InputErrorCheck.bridgeSize(input);
-      }).toThrow('[ERROR]');
-    });
+test('다리생성 예외 테스트', () => {
+  const exceptionInputs = ['1', '2', '21', 's', 'ㅇ'];
+  exceptionInputs.map((input) => {
+    expect(() => {
+      InputErrorCheck.bridgeSize(input);
+    }).toThrow('[ERROR]');
   });
+});
 
-  test('이동 예외 테스트', () => {
-    const exceptionInputs = ['1', 'U ', 'u', 'd', 'ㅓ'];
-    exceptionInputs.map((input) => {
-      expect(() => {
-        InputErrorCheck.way(input);
-      }).toThrow('[ERROR]');
-    });
+test('이동 예외 테스트', () => {
+  const exceptionInputs = ['1', 'U ', 'u', 'd', 'ㅓ', 'D '];
+  exceptionInputs.map((input) => {
+    expect(() => {
+      InputErrorCheck.way(input);
+    }).toThrow('[ERROR]');
+  });
+});
+
+test('재시도 예외 테스트', () => {
+  const exceptionInputs = ['1', 'R ', 'Q ', 'u', 'd', 'ㅓ'];
+  exceptionInputs.map((input) => {
+    expect(() => {
+      InputErrorCheck.gameCommand(input);
+    }).toThrow('[ERROR]');
   });
 });
