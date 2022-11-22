18abbac0415ea6f8bf869bec72f2f1ac45155937
HelloHailie <alsltjsals@gmail.com>
2022-11-22 20:58:20 +0900

    feat:다리 길이 숫자로 입력받기

--- src/InputView.js
+++ src/InputView.js
@@ -1,4 +1,4 @@
-const MissionUtils = require("@woowacourse/mission-utils");
+const { Console } = require("@woowacourse/mission-utils");
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -7,14 +7,23 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
+
+  validateBridgeSize(value) {
+    const bridgeSizeReg = /^[3-9]{1}$|^[1]{1}[0-9]{1}$|20$/;
+    if (!bridgeSizeReg.test(value)) {
+      throw new Error(
+        "[ERROR] 다리의 길이는 3 이상 20 이하의 숫자여야 합니다."
+      );
+    }
+  },
   readBridgeSize() {
-    MissionUtils.Console.print("다리 건너기 게임을 시작합니다.");
-    MissionUtils.Console.readLine(
-      "다리의 길이를 입력해주세요.",
-      (bridgeSize) => {
-        console.log(bridgeSize);
+    Console.readLine("다리의 길이를 입력해주세요.", (answer) => {
+      try {
+        this.validateBridgeSize(Number(answer));
+      } catch {
+        this.readBridgeSize();
       }
-    );
+    });
   },
 
   /**
