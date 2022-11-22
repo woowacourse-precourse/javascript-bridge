898d203d8163ae0f1ab36333d8efab918837761c
hyeongdong <khd941018@gmail.com>
2022-11-22 21:29:06 +0900

    feat(App) : 게임시작 문구를 출력하는 코드 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 const BridgeGameController = require("./controller/BridgeGameController");
+const { Console } = require("@woowacourse/mission-utils");
 
 class App {
+  START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+
   play() {
+    Console.print(this.START_MESSAGE);
     const bridgeGameController = new BridgeGameController();
     bridgeGameController.getBridgeSize();
   }
 }
-
-const app = new App();
-app.play();
 
 module.exports = App;
