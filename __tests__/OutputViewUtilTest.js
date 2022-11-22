b2f938c5146fe1a2d00f95d489ba3353ce0ce53e
jin-Pro <dnjun2@ajou.ac.kr>
2022-11-22 16:41:42 +0900

    feat: OutputView Util Func build && test code

---
+++ __tests__/OutputViewUtilTest.js
@@ -0,0 +1,66 @@
+const { makeMapObj, makeMap } = require("../src/Util/OutputView.util");
+
+describe("OutputView Util Func Test", () => {
+  test("makeMapObj execute", () => {
+    //given
+    const answer = [0, 1, 0, 1];
+    const input = [0, 0];
+    //when
+    const result = makeMapObj(answer, input);
+    const expectResult = { 0: true, 1: false };
+    //then
+    expect(result).toEqual(expectResult);
+  });
+  test("makeMapObj는 최대 answer의 배열 크기만큼 진행합니다.", () => {
+    //given
+    const answer = [0, 1, 0, 1];
+    const input = [0, 1, 0, 1, 1];
+    //when
+    const result = makeMapObj(answer, input);
+    const expectResult = { 0: true, 1: true, 2: true, 3: true };
+    //then
+    expect(result).toEqual(expectResult);
+  });
+  test("makeMapObj의 인자는 0과 1로 이뤄진 배열이어야합니다.", () => {
+    //given
+    const answer = [0, 1, 0, 1];
+    const input = [0, 2];
+    //when
+    //then
+    expect(() => {
+      makeMapObj(answer, input);
+    }).toThrow("[ERROR]");
+  });
+
+  test("makeMap execute", () => {
+    //given
+    const input = [0, 1];
+    const obj = { 0: true, 1: false };
+    //when
+    const result = makeMap(input, obj);
+    const expectResult = `[ O |   ]
+[   | X ]`;
+    //then
+    expect(result).toBe(expectResult);
+  });
+  test("makeMap의 input은 0과 1로 이뤄진 배열이어야합니다", () => {
+    //given
+    const input = [0, 2];
+    const obj = { 0: true, 1: false };
+    //when
+    //then
+    expect(() => {
+      makeMap(input, obj);
+    }).toThrow("[ERROR]");
+  });
+  test("makeMap의 obj는 boolean이어야합니다.", () => {
+    //given
+    const input = [0, 1];
+    const obj = { 0: true, 1: 3 };
+    //when
+    //then
+    expect(() => {
+      makeMap(input, obj);
+    }).toThrow("[ERROR]");
+  });
+});
---
+++ src/Util/OutputView.util.js
@@ -0,0 +1,5 @@
+const makeMapObj = (answer, input) => {};
+const makeMap = (input, obj) => {};
+
+exports.makeMapObj = makeMapObj;
+exports.makeMap = makeMap;
