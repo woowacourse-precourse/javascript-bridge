a574fd86040c7aa8619472c1e8a277f373d723ca
HeoJiye <heoze1031@gmail.com>
2022-11-22 13:09:53 +0900

    test: 수정된 OutputView에 맞게 테스트 코드 수정

--- __tests__/InputValidationTest.js
+++ __tests__/InputValidationTest.js
@@ -10,28 +10,60 @@ const mockQuestions = (answers) => {
     }, MissionUtils.Console.readLine);
 };
 
+const getLogSpy = () => {
+    const logSpy = jest.spyOn(MissionUtils.Console, "print");
+    logSpy.mockClear();
+    return logSpy;
+};
+
+const getOutput = (logSpy) => {
+    return [...logSpy.mock.calls].join("");
+};
+
+const expectLogContains = (received, logs) => {
+    logs.forEach((log) => {
+      expect(received).toEqual(expect.stringContaining(log));
+    });
+};
+
 describe("다리의 길이를 입력받는다", () => {
     test("[예외 처리] 입력한 값이 숫자가 아닌 경우", () => {
         mockQuestions(["dd"]);
-        expect(() => InputView.readBridgeSize()).toThrow("[ERROR]");
+
+        const logSpy = getLogSpy();
+        InputView.readBridgeSize();
+
+        expectLogContains(getOutput(logSpy), ["[ERROR]"]);
     });
 
     test.each(["999", "0", "-123"])("[예외 처리] 입력한 숫자가 범위에서 벗어난 경우", (input) => {
         mockQuestions([input]);
-        expect(() => InputView.readBridgeSize()).toThrow("[ERROR]");
+
+        const logSpy = getLogSpy();
+        InputView.readBridgeSize();
+
+        expectLogContains(getOutput(logSpy), ["[ERROR]"]);
     });
 });
 
 describe("사용자가 이동할 칸을 입력받는다", () => {
     test.each(["3", "S", "d"])("[예외 처리] 입력한 값이 정해진 후보 값 중 하나가 아닐 경우", (input) => {
         mockQuestions([input]);
-        expect(() => InputView.readMoving()).toThrow("[ERROR]");
+
+        const logSpy = getLogSpy();
+        InputView.readMoving();
+
+        expectLogContains(getOutput(logSpy), ["[ERROR]"]);
     });
 });
 
 describe("사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다", () => {
     test.each(["3", "S", "r"])("[예외 처리] 입력한 값이 정해진 후보 값 중 하나가 아닐 경우", (input) => {
         mockQuestions([input]);
-        expect(() => InputView.readGameCommand()).toThrow("[ERROR]");
+
+        const logSpy = getLogSpy();
+        InputView.readGameCommand();
+
+        expectLogContains(getOutput(logSpy), ["[ERROR]"]);
     });
 });
