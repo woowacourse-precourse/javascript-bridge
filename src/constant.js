de33a7766718c1a0f39373f02f0966cc0dca15e2
BaeDongWoo <ehddn5410@naver.com>
2022-11-22 21:29:41 +0900

    chore(constant) : 상수 추가

--- src/constant.js
+++ src/constant.js
@@ -13,13 +13,38 @@ const KEY=Object.freeze({
 });
 
 const ERROR=Object.freeze({
-  BRIDGE_ERROR : '다리 길이가 올바르지 않습니다.',
-  MOVING_ERROR : '이동할 칸이 올바르지 않습니다.',
-  RESTART_ERROR : '재시작 입력이 올바르지 않습니다.',
+  BRIDGE_ERROR : '[ERROR]다리 길이가 올바르지 않습니다.',
+  MOVING_ERROR : '[ERROR]이동할 칸이 올바르지 않습니다.',
+  RESTART_ERROR : '[ERROR]재시작 입력이 올바르지 않습니다.',
+})
+
+const RESULT_MESSAGE=Object.freeze({
+  SUCCESS : '게임 성공 여부: 성공',
+  FAIL : '게임 성공 여부: 실패',
+  MATCH : '총 시도한 횟수:',
+  RESULT : '최종 게임 결과',
+})
+
+const UTILS=Object.freeze({
+  BLANK : ' ',
+  WALL : ' | ',
+  PASS : 'O',
+  STOP : 'X',
+  BRIDGE_MIN : 3,
+  BRIDGE_MAX : 20,
+})
+const OUTPUT_MESSAGES =Object.freeze ({
+  PRINT_RESULT: 'printResult',
+  PRINT_MAP: 'printMap',
+  READ_MOVING: 'readMoving',
+  READ_GAME_COMMAND: 'readGameCommand',
 })
 
 module.exports=Object.freeze({
   MESSAGE,
   KEY,
   ERROR,
+  RESULT_MESSAGE,
+  UTILS,
+  OUTPUT_MESSAGES,
 });
