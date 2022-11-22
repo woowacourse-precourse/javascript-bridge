186583152250cf542b0cd2ebb34d3582196ee372
RooT_Kim <bdd3452@gmail.com>
2022-11-22 14:01:47 +0900

    feat: App 클래스 내 메소드 구현 gameStart - 게임시작 메시지 출력 후 다리 길이를 입력 받는 메소드 runBridgeGame - 다리 길이를 입력받은 후 랜덤으로 다리를 생성하여 플레이어에게 이동할 칸을 입력 받는 메소드 checkMoving - 플레이어의 이동을 파악할 메소드 checkMoveResult - 다리 이미지를 출력하고 게임을 진행할지 말지 결정할 메소드

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+const InputView = require('./input_output/InputView');
+const OutputView = require('./input_output/OutputView');
+const BridgeGame = require('./BridgeGame');
+const { Console } = require('@woowacourse/mission-utils');
 class App {
-  play() {}
+	constructor() {
+		this.bridgeGame = new BridgeGame();
+	}
+
+	play() {
+		this.gameStart();
+	}
+
+	gameStart() {
+		OutputView.printStart();
+		InputView.readBridgeSize(this.runBridgeGame.bind(this));
+	}
+
+	runBridgeGame(size) {
+		this.bridgeGame.createBridge(size);
+		OutputView.printEnter();
+		InputView.readMoving(this.checkMoving.bind(this));
+	}
+
+	checkMoving(moving) {
+		this.bridgeGame.move(moving);
+		this.checkMoveResult();
+	}
+
+	checkMoveResult() {
+		OutputView.printMap(this.bridgeGame.getMovingLogs());
+	}
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
