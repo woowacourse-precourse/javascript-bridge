d4a3e60706de6ec6fff4ccd8c2c5c0fb5690d7bb
Wooseong Kim <kimwooseong8313@gmail.com>
2022-11-22 18:33:06 +0900

    feat: 다리 설정 기능

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+const BridgeMaker = require("./BridgeMaker");
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
 const InputValidator = require("./InputValidator");
 const InputView = require("./InputView");
 const OutputView = require("./OutputView");
 
 class App {
   constructor() {
-    this.game;
+    this.bridges;
   }
 
   play() {
@@ -16,11 +18,20 @@ class App {
       if (!InputValidator.isValidLength(input)) {
         throw new Error("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
       }
-      this.game = +input;
+      this.initBridges(+input);
+      InputView.readMoving(this.handleInputStep.bind(this));
     } catch (error) {
       OutputView.printMessage(error.message);
       InputView.readBridgeSize(this.handleInputLength.bind(this));
     }
+  }
+
+  initBridges(size) {
+    this.bridgeSize = size;
+    this.bridges = BridgeMaker.makeBridge(
+      this.bridgeSize,
+      BridgeRandomNumberGenerator.generate
+    );
   }
 }
 
