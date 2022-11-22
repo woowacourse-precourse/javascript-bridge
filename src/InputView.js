fbfdfc60a818274d7d941cb81d85bd6df1a254ed
kaori-killer <sksbthwjd@daum.net>
2022-11-22 09:12:15 +0900

    feat: 플레이어에게 R과 Q의 선택지를 준다.

--- src/InputView.js
+++ src/InputView.js
@@ -4,7 +4,6 @@ const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
 const BridgeMaker = require('./BridgeMaker');
 const BridgeCompare = require('./BridgeCompare');
 const OutputView = require('./OutputView');
-const { output } = require("./Io");
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -13,10 +12,10 @@ const InputView = {
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize(callback) {
-      Io.input('다리의 길이를 입력해주세요.', (input) => {
-        Validation.validatePositiveInteger(input);
-        Validation.validateNumberRange(input);
-        const SIZE = Number(input);
+      Io.input('다리의 길이를 입력해주세요.', (userInput) => {
+        Validation.validatePositiveInteger(userInput);
+        Validation.validateNumberRange(userInput);
+        const SIZE = Number(userInput);
         const BridgeStatus = BridgeMaker.makeBridge(SIZE, BridgeRandomNumberGenerator.generate);
         callback(SIZE, BridgeStatus);
       });
@@ -26,19 +25,19 @@ const InputView = {
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving(SIZE, BridgeStatus, BridgeIndex, BridgeResultArray) {
-    Io.input('이동할 칸을 선택해주세요. (위: U, 아래: D)', (userChoice) => {
-      Validation.validateUserChoice(userChoice);
-      console.log(BridgeStatus, userChoice);
+    Io.input('이동할 칸을 선택해주세요. (위: U, 아래: D)', (userInput) => {
+      Validation.validateUserChoice(userInput);
+      console.log(BridgeStatus, userInput);
       const NowBridgeValue = BridgeStatus[BridgeIndex];
-      const CompareResult = BridgeCompare.isSameBridge(userChoice, NowBridgeValue);
-      BridgeResultArray = BridgeCompare.makeBridgeResultArray(userChoice, CompareResult, BridgeResultArray);
+      const CompareResult = BridgeCompare.isSameBridge(userInput, NowBridgeValue);
+      BridgeResultArray = BridgeCompare.makeBridgeResultArray(userInput, CompareResult, BridgeResultArray);
       OutputView.printMap(BridgeResultArray);
       const NextBridgeSize = BridgeIndex + 1;
       if(BridgeCompare.isCompleteBridge(SIZE, NextBridgeSize)) {
         OutputView.printResult(BridgeResultArray, CompareResult, 10);      
       }
       if(CompareResult){
-        this.readGameCommand();
+        this.readGameCommand(BridgeResultArray, CompareResult);
       } 
       this.readMoving(SIZE, BridgeStatus, NextBridgeSize, BridgeResultArray);
     });
@@ -47,8 +46,15 @@ const InputView = {
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {
-    // OutputView.printResult(BridgeResultArray, CompareResult, count);
+  readGameCommand(BridgeResultArray, CompareResult) {
+    Io.input('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)', (userInput) => {
+      if(userInput === 'R'){
+
+      }
+      if(userInput == 'Q'){
+        
+      }
+    })
   },
 };
 
