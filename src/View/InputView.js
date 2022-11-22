69d7258e79134a19ba32be51030eb2ea0327cf2c
JangHoonSeok <orirori512@naver.com>
2022-11-22 11:24:06 +0900

    🐛 fix: 입력 함수 오류 해결

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -9,45 +9,51 @@ const InputView = {
   /**
    * @return {number} 다리 길이를 number형식으로 리턴.
    */
-  readBridgeSize() {
+  readBridgeSize(generateBridge) {
+    let input;
     Console.readLine(REQUEST_MESSAGE.BRIDGE_SIZE, (length) => {
       try {
         Validator.checkBridgeLengthInput(Number(length));
-        return Number(length);
+        generateBridge(Number(length));
       } catch (error) {
         OutputView.printErrorMessage(error);
         this.readBridgeSize();
       }
     });
+    return input;
   },
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving() {
+    let input;
     Console.readLine(REQUEST_MESSAGE.SELECT_MOVE, (cmd) => {
       try {
         Validator.checkValidMove(cmd);
-        return cmd;
+        input = cmd;
       } catch (error) {
         OutputView.printErrorMessage(error);
         this.readMoving();
       }
     });
+    return input;
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand() {
+    let input;
     Console.readLine(REQUEST_MESSAGE.RESTART, (cmd) => {
       try {
         Validator.checkValidRestart(cmd);
-        return cmd;
+        input = cmd;
       } catch (error) {
         OutputView.printErrorMessage(error);
         this.readGameCommand();
       }
     });
+    return input;
   },
 };
 
