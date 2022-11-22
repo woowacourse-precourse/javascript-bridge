61e5d971867f2bf40cacafed1e96a99bfb5e0ed9
jjongs2 <jjongs@hanyang.ac.kr>
2022-11-22 20:38:20 +0900

    docs: JSDoc 제거

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -7,27 +7,15 @@ const MESSAGE = Object.freeze({
     '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
 });
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 const InputView = {
-  /**
-   * 다리의 길이를 입력받는다.
-   */
   readBridgeSize(callback) {
     Console.readLine(MESSAGE.INPUT_BRIDGE_SIZE, callback);
   },
 
-  /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
   readMoving(callback) {
     Console.readLine(MESSAGE.INPUT_MOVING, callback);
   },
 
-  /**
-   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-   */
   readGameCommand(callback) {
     Console.readLine(MESSAGE.INPUT_GAME_COMMAND, callback);
   },
