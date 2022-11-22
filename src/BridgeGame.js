52dab5e4df21ad98923574e5ec32f25c6f3c1c40
RooT_Kim <bdd3452@gmail.com>
2022-11-22 15:13:27 +0900

    feat: 메소드 추가 isNotMatched - 다리 이동 후 이동할 수 있는 칸인지 확인 resetInformation - 게임 재시도시 필드 초기화

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -10,7 +10,6 @@ class BridgeGame {
 		round: 0,
 		state: true,
 		movingLogs: [],
-		view: '',
 	};
 
 	createBridge(size) {
@@ -31,12 +30,24 @@ class BridgeGame {
 		return this.#infomation.movingLogs;
 	}
 
+	isNotMatched() {
+		return this.#infomation.state === false;
+	}
+
 	/**
 	 * 사용자가 게임을 다시 시도할 때 사용하는 메서드
 	 * <p>
 	 * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
 	 */
-	retry() {}
+	retry() {
+		this.#resetInformation();
+	}
+
+	#resetInformation() {
+		this.#infomation.round = 0;
+		this.#infomation.state = true;
+		this.#infomation.movingLogs = [];
+	}
 
 	#checkState(moving) {
 		const { bridge, round } = this.#infomation;
