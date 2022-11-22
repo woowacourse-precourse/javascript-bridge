8eb7a04f1bbd38cd4c25596eb0578a52adefd6d8
Yesong <to06109@naver.com>
2022-11-22 22:06:54 +0900

    refactor(BridgeGameTest): 중복 코드 함수로 생성

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -1,6 +1,14 @@
 const BridgeGame = require('../src/BridgeGame');
 
 const bridge = ['U', 'D', 'U'];
+const bridgeGameInit = new BridgeGame(bridge);
+
+const makeBridgeTest = (input, logs, bridgeGame) => {
+  input.reduce((acc, input, bridgeIndex) => {
+    acc = bridgeGame.move(input, bridgeIndex);
+    expect(acc).toEqual(logs[bridgeIndex]);
+  }, 0);
+};
 
 describe('다리 건너기 게임 테스트', () => {
   test('(move) 현재까지 건넌 다리 만들기 - 정답일 경우', () => {
@@ -18,10 +26,7 @@ describe('다리 건너기 게임 테스트', () => {
     ];
 
     const bridgeGame = new BridgeGame(bridge);
-    input.reduce((acc, input, bridgeIndex) => {
-      acc = bridgeGame.move(input, bridgeIndex);
-      expect(acc).toEqual(logs[bridgeIndex]);
-    }, 0);
+    makeBridgeTest(input, logs, bridgeGame);
   });
 
   test('(move) 현재까지 건넌 다리 만들기 - 오답일 경우', () => {
@@ -35,10 +40,7 @@ describe('다리 건너기 게임 테스트', () => {
     ];
 
     const bridgeGame = new BridgeGame(bridge);
-    input.reduce((acc, input, bridgeIndex) => {
-      acc = bridgeGame.move(input, bridgeIndex);
-      expect(acc).toEqual(logs[bridgeIndex]);
-    }, 0);
+    makeBridgeTest(input, logs, bridgeGame);
   });
 
   test('(retry) 현재까지 건넌 다리 초기화 기능 테스트', () => {
@@ -56,15 +58,13 @@ describe('다리 건너기 게임 테스트', () => {
     const moveInput = 'U';
     const bridgeIndex = 0;
 
-    const bridgeGame = new BridgeGame(bridge);
-    expect(bridgeGame.canMove(moveInput, bridgeIndex)).toBeTruthy();
+    expect(bridgeGameInit.canMove(moveInput, bridgeIndex)).toBeTruthy();
   });
 
   test('(canMove) 건널 수 있는지 판단하는 기능 테스트 - 건널 수 없을 때', () => {
     const moveInput = 'D';
     const bridgeIndex = 0;
 
-    const bridgeGame = new BridgeGame(bridge);
-    expect(bridgeGame.canMove(moveInput, bridgeIndex)).toBeFalsy();
+    expect(bridgeGameInit.canMove(moveInput, bridgeIndex)).toBeFalsy();
   });
 });
