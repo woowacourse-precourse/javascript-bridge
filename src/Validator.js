4187e6c8971c9f53d647559aaff02512b0f8d368
sookyeongyeom <sookyeong.dev@gmail.com>
2022-11-22 23:37:47 +0900

    Refactor: 메소드 간 개행 포맷 맞추기

--- src/Validator.js
+++ src/Validator.js
@@ -6,13 +6,16 @@ class Validator {
 	static isNumber(value) {
 		if (isNaN(value)) throw new Error(ErrorMessage.NOT_NUMBER);
 	}
+
 	static isInValidRange(number) {
 		if (!(3 <= number && number <= 25)) throw new Error(ErrorMessage.NOT_IN_VALID_RANGE);
 	}
+
 	static isValidMove(move) {
 		if (!(move === MoveCommand.UP || move === MoveCommand.DOWN))
 			throw new Error(ErrorMessage.NOT_VALID_MOVE);
 	}
+
 	static isValidCommand(command) {
 		if (!(command === RetryCommand.RETRY || command === RetryCommand.QUIT))
 			throw new Error(ErrorMessage.NOT_VALID_COMMAND);
