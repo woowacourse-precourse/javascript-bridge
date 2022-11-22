ce76310aaa82812bd32ae6998ece5fc86cdccd6a
Steadystudy <sgmin206@naver.com>
2022-11-22 19:13:19 +0900

    refactor: InputView 함수 분리하기

--- src/InputView.js
+++ src/InputView.js
@@ -12,25 +12,32 @@ let game;
 const InputView = {
   readBridgeSize() {
     MissionUtils.Console.readLine(Constants.Input.BRIDGE_SIZE, (size) => {
-      catchError(Validate.bridge, size, InputView.readBridgeSize);
-
-      const bridge = BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate);
-      game = new BridgeGame(bridge);
-      InputView.readMoving();
+      catchError(this.getBridgeSize, size, InputView.readBridgeSize);
     });
   },
+  getBridgeSize(size) {
+    Validate.bridge(size);
+
+    const bridge = BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate);
+    game = new BridgeGame(bridge);
+    InputView.readMoving();
+  },
+
   readMoving() {
     MissionUtils.Console.readLine(Constants.Input.MOVE, (movement) => {
-      catchError(Validate.upOrDown, movement, InputView.readMoving);
-
-      const moveResult = game.move(movement);
-      const { map } = game.getResult();
-      OutputView.printMap(map);
-      InputView.readMoveResult(moveResult);
+      catchError(InputView.getMoving, movement, InputView.readMoving);
     });
   },
-  readMoveResult(moveResult) {
+  getMoving(movement) {
+    Validate.upOrDown(movement);
+
+    const moveResult = game.move(movement);
     const { map, tryNumber } = game.getResult();
+
+    OutputView.printMap(map);
+    InputView.switchMoveResult(moveResult, map, tryNumber);
+  },
+  switchMoveResult(moveResult, map, tryNumber) {
     switch (moveResult) {
       case Constants.Result.SUCCESS:
         return InputView.readMoving();
@@ -40,15 +47,19 @@ const InputView = {
         return OutputView.printResult(map, tryNumber, '성공');
     }
   },
+
   readGameCommand() {
     MissionUtils.Console.readLine(Constants.Input.GAME_COMMAND, (command) => {
-      catchError(Validate.gameCommand, command, InputView.readGameCommand);
-
-      InputView.readCommand(command);
+      catchError(InputView.getCommand, command, InputView.readGameCommand);
     });
   },
-  readCommand(command) {
+  getCommand(command) {
+    Validate.gameCommand(command);
+
     const { map, tryNumber } = game.getResult();
+    InputView.switchCommand(command, map, tryNumber);
+  },
+  switchCommand(command, map, tryNumber) {
     switch (command) {
       case 'R':
         game.retry();
