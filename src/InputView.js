7d9cdf488f69bf0c1fcdcefdeaae0032ea499447
Sang-Mini <sangmin3062@gmail.com>
2022-11-22 19:50:25 +0900

    refactor : readGameCommand() 입력 문구 수정

--- src/InputView.js
+++ src/InputView.js
@@ -58,7 +58,7 @@ const InputView = {
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand(bridge, attempt) {
-    Console.readLine('게임을 다시 시도할지 여부를 입력해 주세요. (재시도: R, 종료: Q)', (retry) => {
+    Console.readLine(INPUT_MESSAGES.RETRY, (retry) => {
       const inputCheck = new InputCheck();
       inputCheck.retryCheck(retry);
       const bridgeGame = new BridgeGame();
