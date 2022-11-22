e6de7a7de79fe00d775e89bdea23729ec3949fdd
soohyun_kim5 <93cherry@naver.com>
2022-11-22 10:48:19 +0900

    feat: 결과 출력

--- src/OutputView.js
+++ src/OutputView.js
@@ -20,7 +20,14 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() {},
+  printResult(result, bridgeGame) {
+    const [movingList, numberOfAttempts] = bridgeGame.getScore();
+    Console.print(`\n최종 게임 결과`);
+    this.printMap(movingList);
+    Console.print(`\n게임 성공 여부: ${result}`)
+    Console.print(`\n총 시도한 횟수: ${numberOfAttempts}`)
+    Console.close();
+  },
 };
 
 module.exports = OutputView;
