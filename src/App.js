ba427fc6ec278a44b315439eeeab2b0bdd9ebacf
seungki <sysh9498@gmail.com>
2022-11-22 10:49:44 +0900

    refactor: play 함수 실행 구조 변경

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-const Const = require('./Const');
+const ConstValue = require('./ConstValue');
 const MissionUtils = require('@woowacourse/mission-utils');
 const { Console } = MissionUtils;
+const OutputView = require('./OutputView');
+const InputView = require('./InputView');
+const BridgeMaker = require('./BridgeMaker');
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
+const BridgeGame = require('./BridgeGame');
 class App {
-  play() {}
+  constructor() {
+    this.bridgeArray = [' ', ' '];
+    this.gameStatus = true;
+    this.moveCount = 0;
+  }
+  play() {
+    OutputView.printStartMessage();
+    // const bridgeSize = InputView.readBridgeSize();
+    // const bridge = BridgeMaker.makeBridge(bridgeSize, BridgeRandomNumberGenerator.generate);
+    // console.log('bridgeSize:' + bridgeSize);
+    // console.log('bridge' + bridge);
+    // const bridgeGame = new BridgeGame(bridge);
+    // while (this.gameStatus) {
+    //   let restartCheck = this.gameRotate(bridge, bridgeGame);
+    //   if (restartCheck === 'P' || restartCheck === 'Q') {
+    //     this.checkRestart(restartCheck, bridgeGame);
+    //   }
+    // }
+  }
 
-  printStartMessage() {
-    Console.print(Const.START_MESSAGE);
+  makeBridge() {
+    const bridgeSize = InputView.readBridgeSize();
+    const bridge = BridgeMaker.makeBridge(bridgeSize, BridgeRandomNumberGenerator.generate);
+
+    return bridge;
   }
 }
 
