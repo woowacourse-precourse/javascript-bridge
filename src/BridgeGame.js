e0ef0a949dce4d17d2e0d671ecdb7367dcc1b545
SeungWoo Kim <osabero@naver.com>
2022-11-22 16:44:08 +0900

    입력값을 받아 다리건너기를 구현하였습니다.

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -7,14 +7,21 @@ class BridgeGame {
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  static move(BRIDGE, CURRENTLOCATION, moveinput) {
+    let check = ' X '
+    if(BRIDGE[CURRENTLOCATION] == moveinput) {
+      check = ' O '
+      return check
+    }
+    return check
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  retry() {}
+  static retry() {}
 }
 
 module.exports = BridgeGame;
--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,11 @@
+const { Console } = require("@woowacourse/mission-utils");
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator')
+const BridgeMaker = require('./BridgeMaker')
+const BridgeGame = require('./BridgeGame')
+const Validate = require('./Validate');
+const OutputView = require("./OutputView");
+const { move } = require("./BridgeGame");
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,17 +13,38 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
-
+  readBridgeSize() {
+    Console.readLine('다리 길이를 입력해 주세요', (size)=>{
+      Validate.validateSize(size)
+      const generateRandomNumber = BridgeRandomNumberGenerator.generate
+      const BRIDGE = BridgeMaker.makeBridge(size, generateRandomNumber)
+      const TRIALS = 1
+      const STARTBRIDGE = [[],[]]
+      this.readMoving(BRIDGE, STARTBRIDGE, 0, TRIALS)
+    })
+  },
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
-
+  readMoving(BRIDGE, CURRENTBRIDGE, CURRENTLOCATION, TRIALS) {
+    if(BRIDGE.length == CURRENTLOCATION){return OutputView.printResult(CURRENTBRIDGE, TRIALS)}
+    const SUCCESS = ' O '
+    Console.readLine('이동할 칸을 입력해주세요. (위: U, 아래: D)', (moveinput)=>{
+      const CHECK = BridgeGame.move(BRIDGE, CURRENTLOCATION, moveinput)
+      const NEXTBRIDGE = OutputView.printMap(CURRENTBRIDGE, CHECK, moveinput)
+      OutputView.printCurrentBridge(NEXTBRIDGE)
+      if (CHECK == SUCCESS) {
+        this.readMoving(BRIDGE, NEXTBRIDGE, CURRENTLOCATION+1, TRIALS)
+      } 
+      else {this.readGameCommand(BRIDGE, NEXTBRIDGE, CURRENTLOCATION, TRIALS)}
+    })
+  },
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand(BRIDGE, NEXTBRIDGE, CURRENTLOCATION,TRIALS) {
+
+  },
 };
 
 module.exports = InputView;
