8a92d732143425341f7782f965d7566401ea64b5
curomame <curomame@naver.com>
2022-11-22 14:13:50 +0900

    🔍test - <UnitTest> [다리 길이 입력 테스트]
    다리 길이가 올바른지 확인하는 함수가 올바른지 확인하는 테스트 작성

--- __tests__/UnitTest.js
+++ __tests__/UnitTest.js
@@ -1,4 +1,41 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const App = require("../src/App");
 const { returnCheckedMap } = require("../src/OutputView");
+
+const mockQuestions = (answers) => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce((acc, input) => {
+    return acc.mockImplementationOnce((_, callback) => {
+      callback(input);
+    });
+  }, MissionUtils.Console.readLine);
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
+const getOutput = (logSpy) => {
+  return [...logSpy.mock.calls].join("");
+};
+
+const runException = (inputs) => {
+  mockQuestions(inputs);
+  const logSpy = getLogSpy();
+  const app = new App();
+
+  app.play();
+
+  expectLogContains(getOutput(logSpy), ["[ERROR]"]);
+};
+
+const expectLogContains = (received, logs) => {
+  logs.forEach((log) => {
+    expect(received).toEqual(expect.stringContaining(log));
+  });
+};
 
 describe("다리 상태 반환 테스트", () => {
   test("case 1", () => {
@@ -31,3 +68,15 @@ describe("다리 상태 반환 테스트", () => {
     ]);
   });
 });
+
+describe("다리 길이 입력 테스트",() => {
+
+  test("case 1) number range", () => {
+    runException(["21"]);
+  });
+
+  test("case 2) none input", () => {
+    runException([""]);
+  });
+
+})
