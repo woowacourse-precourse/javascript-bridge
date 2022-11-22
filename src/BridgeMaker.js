8925347cbc6161f0f08fad4e69ab5ae9dad33abb
BaeDongWoo <ehddn5410@naver.com>
2022-11-22 21:36:15 +0900

    chore(BridgeMaker) : bridge 배열에 입력받도록 변경

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -6,8 +6,13 @@ const BridgeMaker = {
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
-  makeBridge(size, generateRandomNumber) {
-    return Array(size).fill('').map(()=>(Number(generateRandomNumber())=== 1 ? KEY.MOVE_UP : KEY.MOVE_DOWN));
+   makeBridge(size, generateRandomNumber) {
+    const bridge = [];
+    for (let i = 0; i < size; i += 1) {
+      bridge[i] = generateRandomNumber() === 1 ? KEY.MOVE_UP : KEY.MOVE_DOWN;
+    }
+    return bridge;
   },
 };
+
 module.exports = BridgeMaker;
