8ba52a87eee37960ea79bdd642b5dbee1cffeb04
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-11-22 10:43:30 +0900

    refactoring($Validator): 메소드 리팩토링

--- src/Validator.js
+++ src/Validator.js
@@ -1,27 +1,36 @@
+const { Console } = require('@woowacourse/mission-utils');
 const {
   IS_SIZE_INPUT_ERROR,
   IS_MOVING_INPUT_ERROR,
   IS_RETRY_INPUT_ERROR,
 } = require('./constants/errors');
-const { FINAL_COMMAND_GROUP, POSITION } = require('./enums');
+const { FINAL_COMMAND_GROUP, POSITION, BRIDGE_SIZE_RANGE } = require('./enums');
 
 const Validator = {
   validateBridgeSizeInput: (bridgeSizeInput) => {
-    if (!(Number(bridgeSizeInput) >= 3 && Number(bridgeSizeInput) <= 20)) {
+    if (
+      !(
+        Number(bridgeSizeInput) >= BRIDGE_SIZE_RANGE.MIN &&
+        Number(bridgeSizeInput) <= BRIDGE_SIZE_RANGE.MAX
+      )
+    ) {
       throw new Error(IS_SIZE_INPUT_ERROR);
     }
+    return true;
   },
 
   validateMoveInput: (moveInput) => {
     if (!(moveInput === POSITION.UP || moveInput === POSITION.DOWN)) {
-      throw new Error(IS_MOVING_INPUT_ERROR);
+      throw IS_MOVING_INPUT_ERROR;
     }
+    return true;
   },
 
   validateRetryInput: (retryInput) => {
     if (!(retryInput === FINAL_COMMAND_GROUP.RETRY || retryInput === FINAL_COMMAND_GROUP.QUIT)) {
-      throw new Error(IS_RETRY_INPUT_ERROR);
+      throw IS_RETRY_INPUT_ERROR;
     }
+    return true;
   },
 };
 
