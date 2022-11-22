4b006166fdd976c38470a869e0414377ebb191a2
jihong <rlawyd9@kookmin.ac.kr>
2022-11-22 21:44:42 +0900

    feat: 올바른 입력인지 확인

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const InputView = require("./InputView");
 
 class App {
+  checkBridgeLen(bridge_len){
+    if(isNaN(bridge_len[0])){
+      throw new Error("[ERROR] 올바르지 않은 입력입니다.")
+    }
+    return true;
+  }
+
   startGame(){
     MissionUtils.Console.print('다리 건너기 게임을 시작합니다.');
     
@@ -9,6 +16,15 @@ class App {
     const inputView = InputView;
     inputView.readBridgeSize(bridge_len);
     console.log(bridge_len);
+    try{
+      if(this.checkBridgeLen(bridge_len)){
+
+      }
+      
+    }catch(e){
+      console.log(e);
+    }
+
   }
 
   play() {
