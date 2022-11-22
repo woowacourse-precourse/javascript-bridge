40c7afe22b2b220b289ee1b17b2bed1bf7f47c72
SeungHun Park <tmdgns1126@naver.com>
2022-11-22 17:29:04 +0900

    feat: 게임 결과 및 시도 횟수 출력 문구 작성

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,3 +1,5 @@
+const { Console } = require("@woowacourse/mission-utils");
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -14,7 +16,23 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() {},
+  printResult(result, tryCount) {
+    if (result === "성공") this.printSuccess();
+    if (result === "실패") this.printFailure();
+    this.printTryCount(tryCount);
+  },
+
+  printSuccess() {
+    Console.print("\n게임 성공 여부: 성공");
+  },
+
+  printFailure() {
+    Console.print("\n게임 성공 여부: 실패");
+  },
+
+  printTryCount(tryCount) {
+    Console.print(`총 시도한 횟수: ${tryCount}`);
+  },
 };
 
 module.exports = OutputView;
