b5aaeca3165c3e6f4f24198123b8e2d89b3fd81e
c0c0pang <coc1789@naver.com>
2022-11-22 14:35:23 +0900

    feat: 게임 실행에 위한 초기 셋팅 기능구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+const InputView = require("./InputView");
+const BridgeMaker = require("./BridgeMaker");
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+const BridgeGame = require("./BridgeGame");
+const OutputView = require("./OutputView");
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
-  play() {}
+  play() {
+    MissionUtils.Console.print("다리 건너기 게임을 시작합니다.\n");
+    InputView.readBridgeSize();
+    this.exceptionHandlingSize();
+    const game = new BridgeGame(InputView.BRIDGE,BridgeMaker.makeBridge(InputView.BRIDGE,BridgeRandomNumberGenerator.generate));
+    this.movePlay(game);
+    OutputView.printResult(game.up,game.down,game);
+  };
 }
 
 module.exports = App;
