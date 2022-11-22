3b8612f857e0b9d07d30c592a8b3250180c68757
DongWooKim97 <nanaukim@naver.com>
2022-11-22 22:29:18 +0900

    style: 상수 변수를 활용하도록 매개변수 변경

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,10 +1,11 @@
+const { SIGN } = require("./Constants");
 const BridgeMaker = {
   makeBridge(size, generateRandomNumber) {
     return Array(size)
       .fill()
       .map(() => {
         const number = generateRandomNumber();
-        return number === 1 ? "U" : "D";
+        return number === 1 ? SIGN.UP_DIRECTION_SIGN : SIGN.DOWN_DIRECTION_SIGN;
       });
   },
 };
