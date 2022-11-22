6dc39c83219519b9ffa725a8445be65c12a5337f
하송희 <99heesong@naver.com>
2022-11-22 13:28:20 +0900

    파일수정: 라인당 width증가로 수정되었던 파일 원상복구

--- src/BridgeRandomNumberGenerator.js
+++ src/BridgeRandomNumberGenerator.js
@@ -4,7 +4,10 @@ const BridgeRandomNumberGenerator = {
   RANDOM_LOWER_INCLUSIVE: 0,
   RANDOM_UPPER_INCLUSIVE: 1,
   generate() {
-    return MissionUtils.Random.pickNumberInRange(BridgeRandomNumberGenerator.RANDOM_LOWER_INCLUSIVE, BridgeRandomNumberGenerator.RANDOM_UPPER_INCLUSIVE);
+    return MissionUtils.Random.pickNumberInRange(
+      BridgeRandomNumberGenerator.RANDOM_LOWER_INCLUSIVE,
+      BridgeRandomNumberGenerator.RANDOM_UPPER_INCLUSIVE
+    );
   },
 };
 
