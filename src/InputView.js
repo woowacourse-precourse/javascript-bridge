6de96f86218b9e1fac3d94b1e657002750ff1d29
rayark1 <max980203@snu.ac.kr>
2022-11-22 17:58:38 +0900

    feat(src/InputView.js): Console.readline을 이용해 값을 입력받는 InputView Object 구현

--- src/InputView.js
+++ src/InputView.js
@@ -1,26 +1,31 @@
-const MissionUtils = require("@woowacourse/mission-utils");
-const { makeBridge } = require("./BridgeMaker");
-const { generate } = require("./BridgeRandomNumberGenerator");
+const { Console } = require("@woowacourse/mission-utils");
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
+  MESSAGE_INPUT_BRIDGE_LENGTH: "다리의 길이를 입력해주세요.",
+  MESSAGE_INPUT_MOVING_DIRECTION: "이동할 칸을 선택해주세요. (위: U, 아래: D)",
+  MESSAGE_INPUT_COMMAND: "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
   /**
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize(callback) {
-    MissionUtils.Console.readLine("다리의 길이를 입력해주세요.", callback);
+    Console.readLine(`${InputView.MESSAGE_INPUT_BRIDGE_LENGTH}\n`, callback);
   },
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving(callback) {
+    Console.readLine(`\n${InputView.MESSAGE_INPUT_MOVING_DIRECTION}\n`, callback);
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand(callback) {
+    Console.readLine(`${InputView.MESSAGE_INPUT_COMMAND}\n`, callback);
+  },
 };
 
 module.exports = InputView;
