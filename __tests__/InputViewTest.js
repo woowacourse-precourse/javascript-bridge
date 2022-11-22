7edcd65815f4267e2f11caccccab9ec71dfaec3b
AhnGeunTaek <dksrmsxor@gmail.com>
2022-11-22 13:49:56 +0900

    refactor: 다리 길이 입력값 예외 처리 테스트

--- __tests__/InputViewTest.js
+++ __tests__/InputViewTest.js
@@ -10,28 +10,41 @@ const mockQuestions = (answers) => {
   }, MissionUtils.Console.readLine);
 };
 
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
+const getOutput = (logSpy) => {
+  return [...logSpy.mock.calls].join("");
+};
+
+const expectLogContains = (received, logs) => {
+  logs.forEach((log) => {
+    expect(received).toEqual(expect.stringContaining(log));
+  });
+};
+
+const runException = (inputs) => {
+  mockQuestions(inputs);
+  const logSpy = getLogSpy();
+  InputView.readBridgeSize();
+
+  expectLogContains(getOutput(logSpy), ["[ERROR]"]);
+};
+
 describe("InputView 테스트", () => {
   test("다리 길이 입력값 유효 테스트", () => {
-    mockQuestions(["g"]);
-    expect(() => {
-      InputView.readBridgeSize();
-    }).toThrow("[ERROR] 다리의 길이는 숫자만 입력해야 합니다.");
+    runException(["a"]);
   });
 
   test("다리 길이가 3보다 작은 숫자인 경우 예외 처리한다.", () => {
-    mockQuestions(["2"]);
-
-    expect(() => {
-      InputView.readBridgeSize();
-    }).toThrow("[ERROR] 다리의 길이는 3 이상 20 이하여야 합니다.");
+    runException(["2"]);
   });
 
   test("다리 길이가 20보다 큰 숫자인 경우 예외 처리한다.", () => {
-    mockQuestions(["21"]);
-
-    expect(() => {
-      InputView.readBridgeSize();
-    }).toThrow("[ERROR] 다리의 길이는 3 이상 20 이하여야 합니다.");
+    runException(["21"]);
   });
 
   test("이동할 칸이 U 또는 D가 아닌 경우 예외 처리한다.", () => {
@@ -40,5 +53,13 @@ describe("InputView 테스트", () => {
     expect(() => {
       InputView.readMoving();
     }).toThrow("[ERROR] 이동할 다리 칸은 U 또는 D만 입력 가능합니다.");
+  });
+
+  test("게임 진행 옵션이 R이나 Q가 아닌 경우 예외 처리한다.", () => {
+    mockQuestions(["A"]);
+
+    expect(() => {
+      InputView.readGameCommand();
+    }).toThrow("[ERROR] 게임 진행 옵션은 R과 Q만 입력 가능합니다.");
   });
 });
