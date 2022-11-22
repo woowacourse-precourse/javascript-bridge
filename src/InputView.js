023d0ed4fc611f2fc14f3fc8974e0a26d5c6e4ac
xownsdl0 <xownsdl0@naver.com>
2022-11-22 10:18:14 +0900

    feat: 잘못된 입력 이후 재입력 하도록 try/catch문 추가

--- src/InputView.js
+++ src/InputView.js
@@ -6,19 +6,34 @@ const InputView = {
 
   readBridgeSize(callback) {
     MissionUtils.Console.readLine(MESSAGE.BRIDGE_SIZE, size => {
-      callback(isNumber(size))
+      try {
+        callback(isNumber(size))
+      } catch (error) {
+        MissionUtils.Console.print(error)
+        this.readBridgeSize(callback)
+      }
     })
   },
 
   readMoving(callback) {
     MissionUtils.Console.readLine(MESSAGE.MOVE, move => {
-      callback(isUpDown(move))
+      try {
+        callback(isUpDown(move))
+      } catch (error) {
+        MissionUtils.Console.print(error)
+        this.readMoving(callback)
+      }
     })
   },
 
   readGameCommand(callback) {
     MissionUtils.Console.readLine(MESSAGE.RETRY, retry => {
-      callback(isRetryQuit(retry))
+      try {
+        callback(isRetryQuit(retry))
+      } catch (error) {
+        MissionUtils.Console.print(error)
+        this.readGameCommand(callback)
+      }
     })
   },
 };
