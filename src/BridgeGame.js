d1dc0297fc5b88debeb7df0df0762cbecb2b5982
KUD <kimud6003@gmail.com>
2022-11-22 21:19:57 +0900

    ✨ Feature : BridgeGame.js  - 다리 건너기 게임의 콜백함수를 모아 놓은 공간

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,20 +1,63 @@
+const BridgeMap = require('./BridgeMap');
+const User = require('./User');
+const Validator = require('./Utils/Validator');
+const BridgeControl = require('./BridgeControl');
+const { QUIT, RETRY } = require('./Utils/Constant').COMMAND;
+const { SUCCESS, FAIL, CONTINUE } = require('./Utils/Constant').BRIDGE;
 /**
- * 다리 건너기 게임을 관리하는 클래스
+ * @class BridgeGame
+ * @desc 다리 게임을 진행하는 클래스
  */
 class BridgeGame {
-  /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  move() {}
+  #ctrl;
+  #bridgeMap;
+  #user;
 
-  /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  retry() {}
+  constructor() {
+    this.#user = new User();
+    this.#ctrl = new BridgeControl();
+  }
+
+  play() {
+    this.#ctrl.intro();
+    this.#ctrl.make(this.makeCallBack);
+  }
+
+  makeCallBack = (input) => {
+    Validator.bridgeSize(input);
+    this.#bridgeMap = new BridgeMap(input);
+    this.#ctrl.move(this.moveCallBack);
+  };
+
+  moveCallBack = (input) => {
+    Validator.moving(input);
+    const status = this.#bridgeMap.move(input, this.#user.position);
+    this.#ctrl.showBridge(this.#bridgeMap, this.#user.position);
+    this.selector(status);
+  };
+
+  retryCallBack = (input) => {
+    Validator.gmaeCommand(input);
+    if (input === QUIT) this.#ctrl.end(FAIL, this.#user.round);
+    if (input === RETRY) this.resetPlay();
+  };
+
+  selector(status) {
+    if (status === CONTINUE) this.continuePlay();
+    if (status === FAIL) this.#ctrl.retry(this.retryCallBack);
+    if (status === SUCCESS) this.#ctrl.End(SUCCESS, this.#user.round);
+  }
+
+  continuePlay() {
+    this.#user.setPositionPlus();
+    this.#ctrl.move(this.moveCallBack);
+  }
+
+  resetPlay() {
+    this.#user.setRoundPlus();
+    this.#bridgeMap.resetUserAnswer();
+    this.#user.resetPosition();
+    this.#ctrl.move(this.moveCallBack);
+  }
 }
-
 module.exports = BridgeGame;
