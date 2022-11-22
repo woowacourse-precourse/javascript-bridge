d51577eb162d28497cd99b9d2afac3fcd02102e8
ming0830 <minsun0830@naver.com>
2022-11-22 09:51:22 +0900

    feat : 사용자의 값이 일치/불일치에 따라 O와 X를 up층과 down층으로 저장하는 기능 구현

--- src/Model/BridgeGame.js
+++ src/Model/BridgeGame.js
@@ -1,10 +1,16 @@
-const {makeBridge} = require('./BridgeMaker');
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
   constructor(){
     this.gameCount = 0;
+    this.upList = [];
+    this.downList = [];
+    this.GO = "O";
+    this.STOP = "X";
+
+
   }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -13,12 +19,32 @@ class BridgeGame {
    */
 
   move(moveInput,answer) {
-    if(moveInput === answer[this.gameCount]){
-      this.gameCount++
-      return true
+    if(this.gameCount === answer.length-1){
+      this.ingMove(this.GO,moveInput);
+      return false;
     }
-    if(moveInput !== answer[this.gameCount]) return false
+    if(moveInput === answer[this.gameCount]){
+      this.ingMove(this.GO,moveInput);
+      this.gameCount++;
+      return true;
+    } 
+    if(moveInput !== answer[this.gameCount]){
+      this.ingMove(this.STOP,moveInput)
+      return false
+    } 
   }
+  
+  ingMove(goOrStop,moveInput){
+    if(moveInput === "U"){
+      this.upList.push(` ${goOrStop} `);
+      this.downList.push('   ')
+    }
+    if(moveInput === "D"){
+      this.downList.push(` ${goOrStop} `);
+      this.upList.push('   ')
+    }
+  }
+  
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
