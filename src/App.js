343f009b28a58622c08ba12dee4d19fa3a14a0f7
jihong <rlawyd9@kookmin.ac.kr>
2022-11-22 22:34:08 +0900

    feat: 이동할 칸 입력받기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const InputView = require("./InputView");
+const BridgeMaker = require("./BridgeMaker");
+const BridgeGame = require("./BridgeGame");
+
 
 class App {
+  constructor(){
+    this.inputView = InputView;
+    this.bridgeMaker = BridgeMaker;
+    this.bridgeGame = new BridgeGame();
+    this.bridge_len;
+  }
+
   checkBridgeSize(size){
     if(isNaN(size)){
       throw new Error("[ERROR] 올바르지 않은 입력입니다.")
@@ -9,22 +19,28 @@ class App {
     return true;
   }
 
-  inputBridgeSize(bridge_len){
-    const inputView = InputView;
-    inputView.readBridgeSize(bridge_len);
+  inputBridgeSize(bridge_len_arr){
+    this.inputView.readBridgeSize(bridge_len_arr);
     try{
-      if(this.checkBridgeSize(bridge_len[0])){
-        console.log(bridge_len[0]);
+      if(this.checkBridgeSize(bridge_len_arr[0])){
+        this.bridge_len = bridge_len_arr[0];
+        this.inputMoving();
       }
     }catch(e){
-      console.log(e);
+      // console.log(e);
     }
+  }
+  
+  inputMoving(){
+    const moving_arr = [];
+    this.inputView.readMoving(moving_arr);
+    console.log(moving_arr);
   }
 
   startGame(){
     MissionUtils.Console.print('다리 건너기 게임을 시작합니다.');
-    const bridge_len = [];
-    this.inputBridgeSize(bridge_len);
+    const bridge_len_arr = [];
+    this.inputBridgeSize(bridge_len_arr);
   }
 
   play() {
--- src/InputView.js
+++ src/InputView.js
@@ -17,7 +17,12 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving(moving_arr) {
+    MissionUtils.Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)', (input) => {
+      MissionUtils.Console.close();
+      moving_arr.push(input);
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
