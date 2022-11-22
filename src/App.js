dc471430e0e40a92b5e5fab6919a2bc2ebf7ae9d
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2022-11-22 23:52:17 +0900

    feat: add functions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+const BridgeGame = require('./BridgeGame');
+const InputView = require('./InputView');
+
 class App {
-  play() {}
+  play() {
+    const game = new BridgeGame();
+    InputView.readBridgeSize(game);
+  }
 }
 
+new App().play();
 module.exports = App;
--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,5 @@
+const { Console } = require("@woowacourse/mission-utils");
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,17 +7,19 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize(input) {
+    Console.print('다리 건너기 게임을 시작합니다.\n');
+    Console.readLine('다리의 길이를 입력해주세요.\n', size => {
+      if (isNaN(Number(size))) {
+        Console.print('[ERROR] 3-20 사이의 숫자를 입력해주세요');
+        Console.close();
+      }
+      input.createUserBridge(Number(size));
+    });
+  },
 
-  /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
-  readMoving() {},
 
-  /**
-   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-   */
-  readGameCommand() {},
+ 
 };
 
 module.exports = InputView;
