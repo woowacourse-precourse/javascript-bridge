b9439758af274d13261aacec45efd94c9a3e77b1
저니녁 <wjs_5025@naver.com>
2022-11-22 16:40:49 +0900

    feat : 에러 메시지 출력을 위한 try-catch문 적용

--- src/utils/validators/validators.js
+++ src/utils/validators/validators.js
@@ -8,15 +8,18 @@ const isInteger = Number.isInteger;
  * @return {boolean} 입력값이 유효하면 true 반환
  */
 const validateBridgeSizeInput = (input) => {
-  const convertedInput = Number(input);
-  if (input.length > 2)
-    throw new Error(MESSAGES.EXCEPTIONS.BRIDGE.COUNT_EXCEPTION);
-  if (isNaN(convertedInput) || !isInteger(convertedInput))
-    // throw new Error(MESSAGES.EXCEPTIONS.BRIDGE.TYPE_EXCEPTION);
-    Console.print(MESSAGES.EXCEPTIONS.BRIDGE.TYPE_EXCEPTION);
-  if (convertedInput < 3 || convertedInput > 20)
-    throw new Error(MESSAGES.EXCEPTIONS.BRIDGE.RANGE_EXCEPTION);
-  return true;
+  try {
+    const convertedInput = Number(input);
+    if (input.length > 2)
+      throw new Error(MESSAGES.EXCEPTIONS.BRIDGE.COUNT_EXCEPTION);
+    if (isNaN(convertedInput) || !isInteger(convertedInput))
+      throw new Error(MESSAGES.EXCEPTIONS.BRIDGE.TYPE_EXCEPTION);
+    if (convertedInput < 3 || convertedInput > 20)
+      throw new Error(MESSAGES.EXCEPTIONS.BRIDGE.RANGE_EXCEPTION);
+    return true;
+  } catch (error) {
+    Console.print(error.message);
+  }
 };
 
 /**
@@ -24,10 +27,14 @@ const validateBridgeSizeInput = (input) => {
  * @param {string} input 이동 시 입력값
  */
 const validateMoveInput = (input) => {
-  if (input.length !== 1)
-    throw new Error(MESSAGES.EXCEPTIONS.MOVE.LENGTH_EXCEPTION);
-  if (!ALLOWED_CHAR.MOVE.includes(input))
-    throw new Error(MESSAGES.EXCEPTIONS.MOVE.VALUE_EXCEPTION);
+  try {
+    if (input.length !== 1)
+      throw new Error(MESSAGES.EXCEPTIONS.MOVE.LENGTH_EXCEPTION);
+    if (!ALLOWED_CHAR.MOVE.includes(input))
+      throw new Error(MESSAGES.EXCEPTIONS.MOVE.VALUE_EXCEPTION);
+  } catch (error) {
+    Console.print(error.message);
+  }
 };
 
 /**
@@ -35,10 +42,14 @@ const validateMoveInput = (input) => {
  * @param {string} input 재시작/종료 여부의 입력값
  */
 const validateGameCommandInput = (input) => {
-  if (input.length !== 1)
-    throw new Error(MESSAGES.EXCEPTIONS.RETRY.LENGTH_EXCEPTION);
-  if (!ALLOWED_CHAR.RETRY.includes(input))
-    throw new Error(MESSAGES.EXCEPTIONS.RETRY.VALUE_EXCEPTION);
+  try {
+    if (input.length !== 1)
+      throw new Error(MESSAGES.EXCEPTIONS.RETRY.LENGTH_EXCEPTION);
+    if (!ALLOWED_CHAR.RETRY.includes(input))
+      throw new Error(MESSAGES.EXCEPTIONS.RETRY.VALUE_EXCEPTION);
+  } catch (error) {
+    Console.print(error.message);
+  }
 };
 
 module.exports = {
