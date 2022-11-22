a87c53567ce5e69b2bb7f016e9787adc38d77dfd
JangHoonSeok <orirori512@naver.com>
2022-11-22 13:56:57 +0900

    ✨ feat(constants): 게임 재시작, 종료 상수 추가

--- src/constants/GameCondition.js
+++ src/constants/GameCondition.js
@@ -6,5 +6,9 @@ const STATE = {
   SUCCESS: "성공",
   FAIL: "실패",
 };
+const GAME_END = {
+  RESTART: "R",
+  QUIT: "Q",
+};
 
-module.exports = { BRIDGE_DIRECTION, STATE };
+module.exports = { BRIDGE_DIRECTION, STATE, GAME_END };
