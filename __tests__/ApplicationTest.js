5d88228c8d0f133638063cb0d28ad80dc67e6aa5
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-11-22 14:44:11 +0900

    chore: 메인 브랜치 업데이트 사항 반영

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -52,11 +52,9 @@ const expectBridgeOrder = (received, upside, downside) => {
 
 describe('다리 건너기 테스트', () => {
   test('다리 생성 테스트', () => {
-    const randomNumbers = ['1', '0', '0'];
-    const mockGenerator = randomNumbers.reduce(
-      (acc, number) => acc.mockReturnValueOnce(number),
-      jest.fn(),
-    );
+    const randomNumbers = [1, 0, 0];
+    // eslint-disable-next-line max-len
+    const mockGenerator = randomNumbers.reduce((acc, number) => acc.mockReturnValueOnce(number), jest.fn());
 
     const bridge = BridgeMaker.makeBridge(3, mockGenerator);
     expect(bridge).toEqual(['U', 'D', 'D']);
@@ -64,7 +62,7 @@ describe('다리 건너기 테스트', () => {
 
   test('기능 테스트', () => {
     const logSpy = getLogSpy();
-    mockRandoms(['1', '0', '1']);
+    mockRandoms([1, 0, 1]);
     mockQuestions(['3', 'U', 'D', 'U']);
 
     const app = new App();
