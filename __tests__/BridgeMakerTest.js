a9b096a7391296fa1d42ca441333aa2e7e460f42
한수지 <fatholic5@gmail.com>
2022-11-22 12:58:48 +0900

    :recycle: refactor: (BridgeMakerTest) 문자열 인자값을 숫자타입으로 변경

--- __tests__/BridgeMakerTest.js
+++ __tests__/BridgeMakerTest.js
@@ -3,7 +3,7 @@ const BridgeMaker = require("../src/BridgeMaker");
 
 describe("다리 생성 기능 테스트", () => {
   test("다리 생성 테스트", () => {
-    const randomNumbers = ["0", "1", "1", "0", "1"];
+    const randomNumbers = [0, 1, 1, 0, 1];
     const mockGenerator = randomNumbers.reduce((acc, number) => {
       return acc.mockReturnValueOnce(number);
     }, jest.fn());
