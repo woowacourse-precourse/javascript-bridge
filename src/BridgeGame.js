f22ce14f3c84e57ebc863359e75c7b22efbb2ebf
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2022-11-22 19:28:44 +0900

    feat: add function to restart game

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -55,15 +55,20 @@ class BridgeGame {
   setGameCommand(command){
     if(command === 'Q'){
       OutputView.close();
+    }else{
+      this.retry();
     }
-    this.retry();
+
   }
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  retry() {}
+  retry() {
+    this.#bridgeBoard.resetBoard();
+    InputView.readMoving(this);
+  }
   play(size) {
     try{
       this.validateSize(size)
