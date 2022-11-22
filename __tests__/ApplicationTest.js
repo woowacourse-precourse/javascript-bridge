5f7b662f5961f039cdf8c619bc768946f2b97fdc
lsy20140 <lsy20140@naver.com>
2022-11-22 21:40:51 +0900

    test: 다리 생성 테스트 코드 작성

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -62,6 +62,26 @@ describe("다리 건너기 테스트", () => {
     expect(bridge).toEqual(["U", "D", "D"]);
   });
 
+  test('다리 생성 테스트', () => {
+    const randomNumbers = ["1", "1", "1", "0"];
+    const mockGenerator = randomNumbers.reduce((acc, number) => {
+      return acc.mockReturnValueOnce(number);
+    }, jest.fn());
+
+    const bridge = BridgeMaker.makeBridge(4, mockGenerator);
+    expect(bridge).toEqual(["U", "U", "U", "D"]);
+  });
+
+  test('다리 생성 테스트', () => {
+    const randomNumbers = ["1", "1", "1", "0", "1", "0", "1"];
+    const mockGenerator = randomNumbers.reduce((acc, number) => {
+      return acc.mockReturnValueOnce(number);
+    }, jest.fn());
+
+    const bridge = BridgeMaker.makeBridge(7, mockGenerator);
+    expect(bridge).toEqual(["U", "U", "U", "D", "U", "D", "U"]);
+  });
+
   test("기능 테스트", () => {
     const logSpy = getLogSpy();
     mockRandoms(["1", "0", "1"]);
