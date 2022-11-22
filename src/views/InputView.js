bde0440a3be6cfacd4c737382bbe92d9c7473c2b
KES <aprilsmaple@gmail.com>
2022-11-22 13:44:12 +0900

    feat: 재시도 여부 입력 받기

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -20,7 +20,9 @@ const InputView = {
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+    return this.readLine(MESSAGES.retryOrQuit);
+  },
 
   async readLine(query) {
     return new Promise((resolve) => {
--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -19,6 +19,7 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printMap(moves) {
+    // utils 폴더 만들어서 빼기?
     const gameMap = moves.reduce(
       (acc, [result, command]) => {
         let row = COMMAND_MATCH_INDEX[command];
