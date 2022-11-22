090f3b9f3cd87be9839ecc81134d4911848ef338
nlom0218 <nlom0218@naver.com>
2022-11-22 17:55:36 +0900

    refactor: 유효성 검사 항목 분리
    유효성 검사 함수에서 각 항복을 검사하는 부분을 함수로 만들어 분리한다.
    즉, if()문 가로에 들어가는 부분을 분리한다.

--- src/libs/Validation.js
+++ src/libs/Validation.js
@@ -9,30 +9,43 @@ const Validation = {
   checkBridgeSize(size) {
     if (isNaN(size)) return { errorMsg: ERROR_MESSAGE.number };
 
-    if (size < BRIDGE_SIZE.min || size > BRIDGE_SIZE.max)
-      return { errorMsg: ERROR_MESSAGE.range };
+    if (!this.isValidSizeRange(size)) return { errorMsg: ERROR_MESSAGE.range };
 
-    if (!Number.isInteger(Number(size)))
-      return { errorMsg: ERROR_MESSAGE.integer };
+    if (!this.isInteger(size)) return { errorMsg: ERROR_MESSAGE.integer };
 
     return { errorMsg: undefined };
   },
 
+  isValidSizeRange(size) {
+    return size >= BRIDGE_SIZE.min && size <= BRIDGE_SIZE.max;
+  },
+
+  isInteger(size) {
+    return Number.isInteger(Number(size));
+  },
+
   checkDirection(direction) {
-    if (direction === DIRECTION.up || direction === DIRECTION.down)
-      return { errorMsg: undefined };
+    if (this.isValidDirection(direction)) return { errorMsg: undefined };
 
     return { errorMsg: ERROR_MESSAGE.direction };
   },
 
+  isValidDirection(direction) {
+    return direction === DIRECTION.up || direction === DIRECTION.down;
+  },
+
   checkCommandOption(commandOption) {
-    if (
-      commandOption === COMMAND_OPTION.restart ||
-      commandOption === COMMAND_OPTION.quit
-    )
+    if (this.isValidCommandOption(commandOption))
       return { errorMsg: undefined };
 
     return { errorMsg: ERROR_MESSAGE.commandOption };
+  },
+
+  isValidCommandOption(commandOption) {
+    return (
+      commandOption === COMMAND_OPTION.restart ||
+      commandOption === COMMAND_OPTION.quit
+    );
   },
 };
 
