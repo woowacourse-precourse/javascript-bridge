a0f0024403e2babab2b2cbe5cba390507de04a9f
Sallyvely <tpdus4159@naver.com>
2022-11-22 12:51:11 +0900

    feat : 다리 길이 3~20이여야하는 예외사항 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -25,6 +25,11 @@ class BridgeGame {
     }
   }
   
+  checkBridgeSize(input) {
+    if(!(input >= 3 && input <= 20)){
+      Console.print("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+    }
+  }
 
 }
 
