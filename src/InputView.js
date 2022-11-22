6b5ca0eadb5b4235b13ec590f9f21c734fc3b773
Doyoung.Kim <dyflower@nate.com>
2022-11-22 19:13:54 +0900

    refactor : try-catch문으로 예외처리

--- src/InputView.js
+++ src/InputView.js
@@ -9,15 +9,15 @@ const InputView = {
 
   readBridgeSize() {
     Console.readLine('다리의 길이를 입력해주세요.\n', (input) => {
-      this.validateBridgeSize(input);
-      input = Number(input);
-
-      this.readMoving(this.bridgeMaker(input));
+      try {
+        this.validateBridgeSize(input);
+        Console.print('');
+        this.readMoving(this.bridgeMaker(input));
+      } catch (error) {
+        Console.print('[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.');
+        this.readBridgeSize();
+      }
     });
-  },
-
-  bridgeMaker(bridgeSize) {
-    return BridgeMaker.makeBridge(bridgeSize, BridgeRandomNumberGenerator.generate);
   },
 
   validateBridgeSize(bridgeSize) {
@@ -25,19 +25,26 @@ const InputView = {
       throw new Error('[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.');
   },
 
-  readMoving(bridge) {
-    Console.readLine('\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n', (input) => {
-      this.validateMoving(input);
-      const round = this.bridgeGame.move();
+  bridgeMaker(bridgeSize) {
+    return BridgeMaker.makeBridge(bridgeSize, BridgeRandomNumberGenerator.generate);
+  },
 
-      this.proceedRound(bridge, round, input);
+  readMoving(bridge) {
+    Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)\n', (input) => {
+      try {
+        this.validateMoving(input);
+        const round = this.bridgeGame.move();
+        this.proceedRound(bridge, round, input);
+      } catch (error) {
+        Console.print('[ERROR] 이동할 칸("U" 또는 "D")을 입력해주세요.');
+        this.readMoving(bridge);
+      }
     });
   },
 
   proceedRound(bridge, round, upDown) {
     const [up, down, gameResult] = this.bridgeGame.makeBridge(bridge[round - 1], upDown);
     OutputView.printMap(up, down);
-
     if (!gameResult) this.readGameCommand(bridge);
     else if (round === bridge.length && gameResult) {
       const totalGame = this.bridgeGame.countGame();
@@ -54,8 +61,13 @@ const InputView = {
     Console.readLine(
       '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
       (input) => {
-        this.validateGameCommand(input);
-        this.restartGame(bridge, input);
+        try {
+          this.validateGameCommand(input);
+          this.restartGame(bridge, input);
+        } catch (error) {
+          Console.print('[ERROR] 재시작 또는 종료("R" 또는 "Q")를 입력해주세요.');
+          this.readGameCommand(bridge);
+        }
       }
     );
   },
