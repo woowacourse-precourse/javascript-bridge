9bb5677a2b6eca90508f9fc4dcab18fd1ef45c61
RooT_Kim <bdd3452@gmail.com>
2022-11-22 14:56:51 +0900

    feat: 게임 재시도, 종료 여부 확인 메소드 구현

--- src/input_output/InputView.js
+++ src/input_output/InputView.js
@@ -1,6 +1,6 @@
 const { Console } = require('@woowacourse/mission-utils');
 const { MESSAGES } = require('../utils/Constants');
-const { checkSizeValidation, checkMovingValidation } = require('../utils/Validation');
+const { checkSizeValidation, checkMovingValidation, checkCommandValidation } = require('../utils/Validation');
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -38,7 +38,17 @@ const InputView = {
 	/**
 	 * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
 	 */
-	readGameCommand() {},
+	readGameCommand(callback) {
+		Console.readLine(MESSAGES.retryOrQuit, (select) => {
+			try {
+				checkCommandValidation(select);
+				callback(select);
+			} catch (error) {
+				Console.print(error);
+				this.readGameCommand(callback);
+			}
+		});
+	},
 };
 
 module.exports = InputView;
