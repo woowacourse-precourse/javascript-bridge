a9c3fcc456f405a29daca40a230031037d008780
jinyong <zhaqh146@naver.com>
2022-11-22 18:20:13 +0900

    feat: 다리 길이 입력 테스트 기능 구현

---
+++ __tests__/BridgeTest.js
@@ -0,0 +1,15 @@
+const Validation = require('../src/Validation');
+
+describe('다리 길이 입력 검사 테스트', () => {
+  test('다리 길이에 숫자 이외의 값을 입력하는 경우 예외가 발생한다.', () => {
+    expect(() => {
+      Validation.bridgeSize('3km');
+    }).toThrow('[ERROR] 다리 길이는 정수를 입력해야 합니다.');
+  });
+
+  test('다리 길이가 3 ~ 20 사이가 아니라면 예외가 발생한다.', () => {
+    expect(() => {
+      Validation.bridgeSize('30');
+    }).toThrow('[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.');
+  });
+});
