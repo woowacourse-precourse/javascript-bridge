5c8b57e21e541ca554aebf705d52be79cb077931
khj0426 <706shin1728@naver.com>
2022-11-22 10:20:44 +0900

    rename:InputErrorview파일 위치 수정

---
+++ src/view/InputErrorView.js
@@ -0,0 +1,40 @@
+const { Console } = require('@woowacourse/mission-utils');
+const { BRIDGE_START, BRIDGE_END } = require('../MESSAGES/NumberMessage');
+const {
+  BRIDGE_INPUT_ERROR,
+  REGAME_INPUT_ERROR,
+  MOVE_INPUT_ERROR,
+} = require('../MESSAGES/InputMessage');
+
+const InputErrorView = {
+  bridgeSizeError(bridgeSize) {
+    if (Number.isNaN(Number(bridgeSize))) {
+      throw BRIDGE_INPUT_ERROR;
+    }
+    if (Number(bridgeSize) < BRIDGE_START || BRIDGE_END < Number(bridgeSize)) {
+      throw BRIDGE_INPUT_ERROR;
+    }
+    return true;
+  },
+
+  reGameError(reGame) {
+    if (reGame === 'R' || reGame === 'Q') {
+      return true;
+    }
+    throw REGAME_INPUT_ERROR;
+  },
+
+  movingError(move) {
+    if (move === 'U' || move === 'D') {
+      return true;
+    }
+    throw MOVE_INPUT_ERROR;
+  },
+
+  closeConsole() {
+    Console.close();
+  },
+};
+
+Object.freeze(InputErrorView);
+module.exports = InputErrorView;
