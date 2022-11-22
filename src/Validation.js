41e803da5d1888fdf8b1291543c70a839717e11f
spsp8426 <spsp8426@naver.com>
2022-11-22 10:27:22 +0900

    feat : 예외 처리 부분 추가

--- src/Validation.js
+++ src/Validation.js
@@ -2,7 +2,10 @@ const Validation = {
   checkBridgeSize(size) {
     if (isNaN(size))
       return { errorMsg: new Error('\n[ERROR] 숫자만 입력할 수 있습니다.\n') };
-
+    
+    if(!(size % 1 === 0)) return {
+      errorMsg:new Error('\n[ERROR] 숫자는 정수만 입력가능합니다.')
+    };
     if (size < 3 || size > 20)
       return {
         errorMsg:new Error('\n[ERROR] 3 이상 20 이하의 숫자만 입력할 수 있습니다.\n')
