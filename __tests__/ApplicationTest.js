b4a1d70fad999db28a312380a8187efc817fcf1c
jw <always1997@kakao.com>
2022-11-22 23:42:37 +0900

    fix: 다리생성 메서드 수정

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -53,13 +53,14 @@ const expectBridgeOrder = (received, upside, downside) => {
 
 describe("다리 건너기 테스트", () => {
   test("다리 생성 테스트", () => {
-    const randomNumbers = ["1", "0", "0"];
+    const randomNumbers = ["0", "0", "0"];
     const mockGenerator = randomNumbers.reduce((acc, number) => {
       return acc.mockReturnValueOnce(number);
     }, jest.fn());
 
     const bridge = BridgeMaker.makeBridge(3, mockGenerator);
-    expect(bridge).toEqual(["U", "D", "D"]);
+    console.log("다리가 어떻게생겻는데[?", bridge);
+    expect(bridge).toEqual(["D", "D", "D"]);
   });
 
   test("기능 테스트", () => {
--- src/Bridge.js
+++ src/Bridge.js
@@ -45,7 +45,6 @@ class Bridge {
         break;
       }
     }
-    //gameOver()
   }
 
   setRetry() {
