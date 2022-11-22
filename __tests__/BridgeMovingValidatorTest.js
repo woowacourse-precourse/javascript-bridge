474da59625db0ed64649e3f27c84fea0ede48847
Suyoung <jsy8492@gmail.com>
2022-11-22 17:53:46 +0900

    test(BridgeMovingValidatorTest): 영어 소문자 입력에 대한 예외 처리 테스트 케이스 추가

--- __tests__/BridgeMovingValidatorTest.js
+++ __tests__/BridgeMovingValidatorTest.js
@@ -1,7 +1,7 @@
 const BridgeMovingValidator = require('../src/validators/BridgeMovingValidator');
 
 describe('사용자가 입력한 이동할 칸의 유효성을 검사하는 객체 테스트', () => {
-  test.each([['DD'], ['UU'], ['A'], ['ABC'], ['']])(
+  test.each([['DD'], ['UU'], ['A'], ['ABC'], [''], ['d'], ['u']])(
     '유효하지 않은 이동인 경우 예외 처리',
     () => {
       expect(input => BridgeMovingValidator.validate(input)).toThrow('[ERROR]');
