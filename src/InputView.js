6e3aa1f71fb166afb109e3eef0202f771933af15
jihun1233 <leejihun1233@gmail.com>
2022-11-22 22:49:13 +0900

    feat: 입력값 체크 함수 추가

--- src/InputView.js
+++ src/InputView.js
@@ -9,22 +9,43 @@ const InputView = {
   readBridgeSize() {
     return new Promise((resolve)=>{
       MissionUtils.Console.readLine('',(input)=>{
-        resolve(parseInt(input, 10))
+        this.bridgeSizeInputCheck(input)
+
+        const parsedInput = parseInt(input, 10)
+
+        resolve(parsedInput)
       })
     })
   },
-
+  bridgeSizeInputCheck(input){
+    try{
+      const parsedInput = parseInt(input, 10)
+            
+      if(isNaN(parsedInput))throw new Error('[ERROR]: 숫자를 입력해주세요')
+      if(parsedInput < 3 || 20 < parsedInput)throw new Error('[ERROR]: 3 이상 20 이하여야합니다')
+    } catch (error) {
+      MissionUtils.Console.print(error)
+    }
+  },
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving() {
     return new Promise((resolve)=>{
       MissionUtils.Console.readLine('',(input)=>{
+        this.movingInputCheck(input)
+
         resolve(input)
       })
     })
   },
-
+  movingInputCheck(input){
+    try{
+      if(!(input === 'U' || input === 'D'))throw new Error('[ERROR]: U 또는 D로 방향을 입력해주세요')
+    }catch(error){
+      MissionUtils.Console.print(error)
+    }
+  },
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
