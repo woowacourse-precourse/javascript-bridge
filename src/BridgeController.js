45d6f68c7fab20558b2dd794d1e41e6c2317c763
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-11-22 20:04:12 +0900

    style: 들여쓰기 공백2로 설정

--- src/BridgeController.js
+++ src/BridgeController.js
@@ -4,92 +4,92 @@ const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
 const BridgeGame = require('./BridgeGame');
 
 class BridgeController {
-    constructor(views) {
-        this.bridgeGame = new BridgeGame();
-        this.inputView = views.inputView,
-        this.outputView = views.outputView
-        this.start()
-    }
+  constructor(views) {
+    this.bridgeGame = new BridgeGame();
+    this.inputView = views.inputView,
+    this.outputView = views.outputView
+    this.start()
+  }
 
-    start() {
-        this.outputView.printStart();
-        this.requestBridegeSize();
-    }
-    
-    requestBridegeSize() {
-        this.inputView.readBridgeSize(this.createBridge.bind(this));
-    }
+  start() {
+    this.outputView.printStart();
+    this.requestBridegeSize();
+  }
+  
+  requestBridegeSize() {
+    this.inputView.readBridgeSize(this.createBridge.bind(this));
+  }
 
-    createBridge(bridgeSize) {
-        this.bridgeSizeException(bridgeSize);
-        const bridge = BridgeMaker.makeBridge(bridgeSize, BridgeRandomNumberGenerator.generate);
-        this.bridgeGame.setBridge(bridge);
-        this.requestMoving();
-    }
+  createBridge(bridgeSize) {
+    this.bridgeSizeException(bridgeSize);
+    const bridge = BridgeMaker.makeBridge(bridgeSize, BridgeRandomNumberGenerator.generate);
+    this.bridgeGame.setBridge(bridge);
+    this.requestMoving();
+  }
 
-    bridgeSizeException(bridgeSize) {
-        try{
-            Exception.isInvalidBridgeSize(bridgeSize);
-        } catch(err) {
-            this.outputView.printError(err);
-            this.requestBridegeSize();
-        }
+  bridgeSizeException(bridgeSize) {
+    try{
+      Exception.isInvalidBridgeSize(bridgeSize);
+    } catch(err) {
+      this.outputView.printError(err);
+      this.requestBridegeSize();
     }
+  }
 
-    requestMoving() {
-        this.inputView.readMoving(this.createMoving.bind(this));
-    }
+  requestMoving() {
+    this.inputView.readMoving(this.createMoving.bind(this));
+  }
 
-    createMoving(moving) {
-        this.movingException(moving);
-        this.handleMovingResult(this.bridgeGame.move(moving)); 
-    }
+  createMoving(moving) {
+    this.movingException(moving);
+    this.handleMovingResult(this.bridgeGame.move(moving)); 
+  }
 
-    movingException(moving) {
-        try{
-            Exception.isInvalidMoving(moving);
-        } catch(err) {
-            this.outputView.printError(err);
-            this.requestMoving();
-        }
+  movingException(moving) {
+    try{
+      Exception.isInvalidMoving(moving);
+    } catch(err) {
+      this.outputView.printError(err);
+      this.requestMoving();
     }
+  }
 
-    handleMovingResult(result) {
-        this.outputView.printMap(this.bridgeGame.getMoveInfo());
-        switch (result) {
-            case 'next': this.requestMoving(); break;
-            case 'fail': this.requestRetry(); break;
-            case 'success': 
-                this.outputView.printResult(this.bridgeGame.getMoveInfo(), this.bridgeGame.getTryCnt(), true);
-                break;
-        }
+  handleMovingResult(result) {
+    this.outputView.printMap(this.bridgeGame.getMoveInfo());
+    switch (result) {
+      case 'next': this.requestMoving(); break;
+      case 'fail': this.requestRetry(); break;
+      case 'success': 
+        this.outputView.printResult(this.bridgeGame.getMoveInfo(), this.bridgeGame.getTryCnt(), true);
+        break;
     }
+  }
 
-    requestRetry() {
-        this.inputView.readGameCommand(this.createRetry.bind(this));
-    }
+  requestRetry() {
+    this.inputView.readGameCommand(this.createRetry.bind(this));
+  }
 
-    createRetry(command) {
-        this.commandException(command);
-        this.handleRetryResult(this.bridgeGame.retry(command));   
-    }
+  createRetry(command) {
+    this.commandException(command);
+    this.handleRetryResult(this.bridgeGame.retry(command));   
+  }
 
-    handleRetryResult(result) {
-        if(result === 'retry') {
-            this.requestMoving();
-        } else if(result === 'quit') {
-            this.outputView.printResult(this.bridgeGame.getMoveInfo(), this.bridgeGame.getTryCnt(), false);
-        }
+  handleRetryResult(result) {
+    if(result === 'retry') {
+      this.requestMoving();
+    } else if(result === 'quit') {
+      this.outputView.printResult(this.bridgeGame.getMoveInfo(), this.bridgeGame.getTryCnt(), false);
     }
-
-    commandException(command) {
-        try {
-            Exception.isInvalidCommand(command);
-        } catch(err) {
-            this.outputView.printError(err);
-            this.requestRetry();
-        }
+  }
+  
+  commandException(command) {
+    try {
+      Exception.isInvalidCommand(command);
+    } catch(err) {
+      this.outputView.printError(err);
+      this.requestRetry();
     }
+  }
 }
 
 module.exports = BridgeController;
--- src/utils/Exceptions.js
+++ src/utils/Exceptions.js
@@ -1,33 +1,33 @@
 const Exception = {
-    isInvalidBridgeSize(input) {
-        if(this.isNotNumber(input) || this.bridgeSizeOutofIndex(input)) {
-            throw'[ERROR] 다리 길이는 3이상 20이하의 숫자로 입력해야 합니다.'; 
-        }
-    },
-
-    isNotNumber(input) {
-        if(isNaN(input) || input.match(/\s/g)) {
-            return true;
-        }
-    },
-
-    bridgeSizeOutofIndex(input) {
-        if(Number(input) < 3 || Number(input) > 20) {
-            return true;
-        }
-    },
-
-    isInvalidMoving(input) {
-        if(!input.match(/^[UD]$/)) {
-            throw '[ERROR] 이동할 칸에 대한 입력은 U 또는 D만 입력 가능합니다.';
-        }
-    },
-
-    isInvalidCommand(input) {
-        if(!input.match(/^[RQ]$/)) {
-            throw '[ERROR] 재시작/종료 여부 입력은 R 또는 Q만 입력 가능합니다.';
-        }
+  isInvalidBridgeSize(input) {
+    if(this.isNotNumber(input) || this.bridgeSizeOutofIndex(input)) {
+      throw'[ERROR] 다리 길이는 3이상 20이하의 숫자로 입력해야 합니다.'; 
     }
+  },
+
+  isNotNumber(input) {
+    if(isNaN(input) || input.match(/\s/g)) {
+      return true;
+    }
+  },
+
+  bridgeSizeOutofIndex(input) {
+    if(Number(input) < 3 || Number(input) > 20) {
+      return true;
+    }
+  },
+
+  isInvalidMoving(input) {
+    if(!input.match(/^[UD]$/)) {
+      throw '[ERROR] 이동할 칸에 대한 입력은 U 또는 D만 입력 가능합니다.';
+    }
+  },
+
+  isInvalidCommand(input) {
+    if(!input.match(/^[RQ]$/)) {
+      throw '[ERROR] 재시작/종료 여부 입력은 R 또는 Q만 입력 가능합니다.';
+    }
+  }
 }
 
 module.exports = Exception;
