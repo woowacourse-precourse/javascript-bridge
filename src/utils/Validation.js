78f80912095e90cb211ab6acbd4d05198830949d
RooT_Kim <bdd3452@gmail.com>
2022-11-22 14:58:42 +0900

    feat: 게임 재시도, 종료 여부 확인 입력값 유효성 검사 메소드 구현 - checkCommandValidation

--- src/utils/Validation.js
+++ src/utils/Validation.js
@@ -1,4 +1,4 @@
-const { ERROR_MESSAGES, MOVING, BRIDGE_SIZE, REGEXP } = require('./Constants');
+const { ERROR_MESSAGES, MOVING, SELECT, BRIDGE_SIZE, REGEXP } = require('./Constants');
 
 const checkSizeValidation = (size) => {
 	if (!size.match(REGEXP.number)) {
@@ -15,7 +15,14 @@ const checkMovingValidation = (moving) => {
 	}
 };
 
+const checkCommandValidation = (select) => {
+	if (![SELECT.retry, SELECT.quit].includes(select)) {
+		throw new Error(ERROR_MESSAGES.retryOrQuit);
+	}
+};
+
 module.exports = {
 	checkSizeValidation,
 	checkMovingValidation,
+	checkCommandValidation,
 };
