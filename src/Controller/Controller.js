c6e04dfcc26ff270de32337525b3e3125264d6c9
ming0830 <minsun0830@naver.com>
2022-11-22 09:52:26 +0900

    feat : BridgeGame에서 데이터를 받아 OutputView를 통해 알려주는 기능

--- src/Controller/Controller.js
+++ src/Controller/Controller.js
@@ -2,25 +2,41 @@ const OutputView = require('../View/OutputView');
 const InputView = require('../View/InputView');
 const BridgeMaker = require('../Model/BridgeMaker');
 const {generate} = require('../util/BridgeRandomNumberGenerator');
+const BridgeGame = require('../Model/BridgeGame')
 const  MissionUtils  = require('@woowacourse/mission-utils');
 
 class Controller {
   #answerBirdgeList;
   constructor(){
     this.#answerBirdgeList;
+    this.bridgeGame = new BridgeGame();
   }
   start(){
     OutputView.printStart();
-    this.askBridegeSize();
+    this.getBridegeSize();
   }
 
-  askBridegeSize(){
-    InputView.readBridgeSize(this.getBirdgeAnswer.bind(this));
-    
+  getBridegeSize(){ 
+    InputView.readBridgeSize(this.getBirdgeAnswer.bind(this));  
+  }
+
+  getMove(){ 
+    InputView.readMoving(this.compare.bind(this));
   }
 
   getBirdgeAnswer(sizeNumber){
     this.#answerBirdgeList = BridgeMaker.makeBridge(sizeNumber,generate);
+    // MissionUtils.Console.print(this.#answerBirdgeList)
+    this.getMove()
+  }
+
+  compare(moveInput){
+    if(this.bridgeGame.move(moveInput,this.#answerBirdgeList)){
+      OutputView.printMap(this.bridgeGame.upList,this.bridgeGame.downList)
+      this.getMove()
+    } else {
+      OutputView.printMap(this.bridgeGame.upList,this.bridgeGame.downList)
+    }
   }
 
 
