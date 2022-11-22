474bcd6a1489f870d96a31aba04dd490d8445f4b
doheeMac <thsehgml37@korea.ac.kr>
2022-11-22 21:53:09 +0900

    fix: print format update

--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -61,7 +61,8 @@ const OutputView = {
     MissionUtils.Console.print("최종 게임 결과\n");
     this.printMap(bridgeStatus, jumpHistory);
     const RESULT_INDEX = this.calculateResult(bridgeStatus, jumpHistory);
-    MissionUtils.Console.print(`게임 성공 여부: ${RESULT[RESULT_INDEX]}\n`);
+
+    MissionUtils.Console.print(`\n게임 성공 여부: ${RESULT[RESULT_INDEX]}\n`);
     MissionUtils.Console.print(`총 시도한 횟수: ${playCount}\n`);
   },
 
