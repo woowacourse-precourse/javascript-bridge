32e8ff0f15fd97792f4c8a7129d50a7b649dfe68
hwnim5324 <hwnim5324@naver.com>
2022-11-22 13:14:51 +0900

    feat: 사용자 입력 관련 기능 구현.

--- src/InputView.js
+++ src/InputView.js
@@ -1,21 +1,33 @@
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+const MissionUtils = require("@woowacourse/mission-utils");
+const Validator = require("./Validator");
+
 const InputView = {
-  /**
-   * 다리의 길이를 입력받는다.
-   */
-  readBridgeSize() {},
+  readBridgeSize() {
+    let bridgeSize = 0;
+    MissionUtils.Console.readLine('다리의 길이를 입력해주세요.\n', (answer) => {
+      Validator.isBridgeSize(answer);
+      bridgeSize = answer;
+    });
+    return bridgeSize;
+  },
 
-  /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
-  readMoving() {},
+  readMoving() {
+    let move = '';
+    MissionUtils.Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)\n', (answer) => {
+      Validator.isMove(answer);
+      move = answer;
+    });
+    return move;
+  },
 
-  /**
-   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-   */
-  readGameCommand() {},
+  readGameCommand() {
+    let gameCommand = '';
+    MissionUtils.Console.readLine('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n', (answer) => {
+      Validator.isGameCommand(answer);
+      gameCommand = answer;
+    });
+    return gameCommand;
+  }
 };
 
 module.exports = InputView;
