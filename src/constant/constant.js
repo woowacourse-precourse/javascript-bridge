7e7fa83ed15302146985232b89bb0f5f4344a9b4
kitez <duswl339@ajou.ac.kr>
2022-11-22 09:39:04 +0900

    feat: Key Constant 추가

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -1,14 +1,21 @@
 const CONSTANT = Object.freeze({
-    BRIDGE_FIRST : 3,
-    BRIDGE_END : 20
+  BRIDGE_FIRST: 3,
+  BRIDGE_END: 20,
 });
 
 const BRIDGE = Object.freeze({
-   "0" : "D",
-   "1" : "U"
+  0: 'D',
+  1: 'U',
 });
 
-const MOVE = Object.freeze(["U", "D"]);
-const RETRY = Object.freeze(["R","Q"]);
+const MOVE = Object.freeze(['U', 'D']);
+const RETRY = Object.freeze(['R', 'Q']);
 
-module.exports = {CONSTANT, BRIDGE, MOVE, RETRY};
+const KEY = {
+  BRIDGE: 'bridge',
+  CURRENT: 'current',
+};
+
+module.exports = {
+  CONSTANT, BRIDGE, MOVE, RETRY, KEY,
+};
