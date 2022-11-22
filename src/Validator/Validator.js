82342a020669e21ee34c51ea0bdf5749059cb1a9
1 <wrasf175@naver.com>
2022-11-22 13:46:25 +0900

    feat(Validator): 예외 처리 기능 구현

--- src/Validator/Validator.js
+++ src/Validator/Validator.js
@@ -1,77 +1,81 @@
 const Print = require('../Print');
 
-
 class BridgeLengthInput {
-    #userInput
+  #userInput;
 
-    constructor (userInput) {
-        this.#userInput = Number(userInput);
-    }
+  constructor(userInput) {
+    this.#userInput = Number(userInput);
+  }
 
-    check() {
-        try{
-            if (this.isValidNumber() || this.isValidRange() || this.isNotInteger())
-                throw new Error(Print.ErrorMessage('[ERROR] 3~20 사이의 숫자만 입력 가능합니다.'));
-                return true;
-        }catch {
-            return false;
-        }
+  check() {
+    try {
+      if (this.isValidNumber() || this.isValidRange() || this.isNotInteger())
+        throw new Error(
+          Print.ErrorMessage('[ERROR] 3~20 사이의 숫자만 입력 가능합니다.')
+        );
+      return true;
+    } catch {
+      return false;
     }
+  }
 
-    isValidRange() {
-        return this.#userInput < 3 || this.#userInput > 20;
-    }
+  isValidRange() {
+    return this.#userInput < 3 || this.#userInput > 20;
+  }
 
-    isValidNumber() {
-        return isNaN(this.#userInput);
-    }
-    isNotInteger() {
-        return !Number.isInteger(this.#userInput);
-    }
+  isValidNumber() {
+    return isNaN(this.#userInput);
+  }
+  isNotInteger() {
+    return !Number.isInteger(this.#userInput);
+  }
 }
 
 class DirectionChoiceInput {
-    #userInput
+  #userInput;
 
-    constructor (userInput) {
-        this.#userInput = userInput;
-    }
+  constructor(userInput) {
+    this.#userInput = userInput;
+  }
 
-    check() {
-        try{
-            if (!this.isValidInput())
-                throw new Error(Print.ErrorMessage('[ERROR] U 또는 D만 입력 가능합니다.'));
-                return true;
-        }catch {
-            return false;
-        }
+  check() {
+    try {
+      if (!this.isValidInput())
+        throw new Error(
+          Print.ErrorMessage('[ERROR] U 또는 D만 입력 가능합니다.')
+        );
+      return true;
+    } catch {
+      return false;
     }
-    isValidInput() {
-        return this.#userInput === 'U' || this.#userInput === 'D';
-    }
+  }
+  isValidInput() {
+    return this.#userInput === 'U' || this.#userInput === 'D';
+  }
 }
 
 class RetryInput {
-    #userInput
+  #userInput;
 
-    constructor (userInput) {
-        this.#userInput = userInput;
-    }
+  constructor(userInput) {
+    this.#userInput = userInput;
+  }
 
-    check() {
-        try{
-            if (!this.isValidInput())
-                throw new Error(Print.ErrorMessage('[ERROR] Q 또는 R만 입력 가능합니다.'));
-                return true;
-        }catch {
-            return false;
-        }
+  check() {
+    try {
+      if (!this.isValidInput())
+        throw new Error(
+          Print.ErrorMessage('[ERROR] Q 또는 R만 입력 가능합니다.')
+        );
+      return true;
+    } catch {
+      return false;
     }
+  }
 
-    isValidInput() {
-        return this.#userInput === 'Q' || this.#userInput === 'R';
-    }
+  isValidInput() {
+    return this.#userInput === 'Q' || this.#userInput === 'R';
+  }
 }
 
-
-module.exports = {BridgeLengthInput, DirectionChoiceInput, RetryInput};
+module.exports = { BridgeLengthInput, DirectionChoiceInput, RetryInput };
