71d354fbd522ae8cf781e95e0a9eda17b22504f7
JiYoon Lee <wldbseja@daum.net>
2022-11-22 18:42:24 +0900

    feat (BridgeGame) : validateSizeRange 다리 길이 에러 발생 기능

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -19,6 +19,12 @@ class BridgeGame {
     this.#userState = [];
     this.#countTry = 0;
   }
+
+  validateSizeRange(size) {
+    if (!(size >= 3 && size <= 20)) {
+      throw Error('[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.');
+    }
+  }
 }
 
 module.exports = BridgeGame;
