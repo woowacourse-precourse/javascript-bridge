441fa52bddb9064d537caf55b3771f8f53c4c0cc
knh6269 <knh6269@gmail.com>
2022-11-22 23:55:43 +0900

    feat(InputView): 재시작 입력 확인 함수

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -52,6 +52,9 @@ const InputView = {
     throw new Error(`[ERROR] 유효하지 않은 입력입니다.`);
   },
 
+  readGameCommand(callback) {
+    InputView.question('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n', callback, InputView.correctCommand);
+  },
 
 };
 
