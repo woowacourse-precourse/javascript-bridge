ca9ae745360f712c22c131ef753f6b27e7ff0585
wonkyung <mwk0725@gmail.com>
2022-11-22 14:58:07 +0900

    refactor: 유효성 검사 부분 상수화 수정

--- src/Validate.js
+++ src/Validate.js
@@ -3,18 +3,16 @@ const { ERROR_MSG, SELECT, BRIDGE } = require('./constant');
 const Validate = {
   size(size) {
     const number = /^(0|[-]?[1-9]\d*)$/.test(size) || typeof size === 'number';
-    const length = size <= 20 && size >= 3;
+    const length = size >= BRIDGE.SIZE_MINIMUM && size <= BRIDGE.SIZE_MAXIMUM;
 
     if (!length || !number) {
-      throw new Error('[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.');
+      throw new Error(ERROR_MSG.ENTER_NUMBER_THREE_TO_TWENTY);
     }
   },
 
   move(move) {
-    if (move !== 'U' && move !== 'D') {
-      throw new Error(
-        '[ERROR] 이동할 칸은 U(위 칸)와 D(아래 칸) 중 하나의 문자를 입력해야 합니다.\n',
-      );
+    if (move !== BRIDGE.UP && move !== BRIDGE.DOWN) {
+      throw new Error(ERROR_MSG.ENTER_U_OR_D);
     }
   },
 
