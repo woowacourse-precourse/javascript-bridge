45baae0622d263e119489d917666b5a230eb2327
pitang <wpeffect@gmail.com>
2022-11-22 22:38:03 +0900

    feat: BridgeGame

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,6 +1,67 @@
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+const BridgeMaker = require("./BridgeMaker");
+const BridgeInformation = require("./BridgeInformation");
+const { SIGN } = require("./Constants");
+
 class BridgeGame {
-  move() {}
-  retry() {}
+  #movingRoute = [];
+  #info;
+  #tryCount = 1;
+  #inputIndex = -1;
+
+  constructor(size) {
+    const correctRoute = BridgeMaker.makeBridge(
+      size,
+      BridgeRandomNumberGenerator.generate
+    );
+    this.#info = new BridgeInformation(size, correctRoute);
+  }
+
+  changeState(direction) {
+    this.#movingRoute.push(direction);
+    this.#inputIndex += 1;
+  }
+
+  move(direction) {
+    this.changeState(direction);
+    this.#info.makeRouteMap(direction, this.#inputIndex);
+    this.checkStatus();
+  }
+
+  checkStatus() {
+    this.getRouteMap();
+    this.checkWrongInput();
+    this.checkSuccess();
+  }
+
+  retry() {
+    this.#tryCount += 1;
+    this.#inputIndex = -1;
+    this.#movingRoute = [];
+    this.#info.resetRoute();
+  }
+
+  fail() {
+    this.#info.finishGame(this.#tryCount, SIGN.FAILURE);
+  }
+
+  getRouteMap() {
+    this.#info.printRouteMap();
+  }
+
+  getMatchSize() {
+    return this.#info.checkMatchLength(this.#movingRoute.length);
+  }
+
+  checkWrongInput() {
+    return this.#info.getWrongRoute();
+  }
+
+  checkSuccess() {
+    if (this.#info.successStatus(this.#movingRoute.length)) {
+      this.#info.finishGame(this.#tryCount, SIGN.SUCCESS);
+    }
+  }
 }
 
 module.exports = BridgeGame;
