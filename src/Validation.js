ab0630c3c75e25674e7bcd012b2cea221b21ede6
lsy20140 <lsy20140@naver.com>
2022-11-22 20:35:28 +0900

    refactor: Validation 판단 형식 수정

--- src/Validation.js
+++ src/Validation.js
@@ -1,14 +1,21 @@
 const checkBridgeSize = (size) => {
-  if(isNaN(size)) throw new Error("[ERROR] 숫자를 입력해주세요.");
-  if(size < 3 || size > 20) throw new Error("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+  const isNumber = !isNaN(size);
+  const isValidRange = size >= 3 && size <= 20;
+
+  if(isNumber === false) throw new Error("[ERROR] 숫자를 입력해주세요.");
+  if(isValidRange === false) throw new Error("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
 };
 
 const checkSideInput = (side) => {
-  if(side !== "U"  || side !== "D") throw new Error("[ERROR] U 또는 D를 입력해주세요.");
+  const isMove = side === 'U' || side === 'D';
+
+  if(isMove === false) throw new Error("[ERROR] U 또는 D를 입력해주세요.");
 };
 
 const checkRetryKey = (key) => {
-  if(key !== "R"  || key !== "Q") throw new Error("[ERROR] R 또는 Q를 입력해주세요.");
+  const toRetry = key === 'R' || key === 'Q';
+  
+  if(toRetry === false) throw new Error("[ERROR] R 또는 Q를 입력해주세요.");
 }
 
-module.exports = {checkBridgeSize, checkSideInput};
+module.exports = {checkBridgeSize, checkSideInput, checkRetryKey};
