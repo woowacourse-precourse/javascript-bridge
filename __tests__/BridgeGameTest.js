a3fc44f508ff3647c4bf81ce85fe431bb2233c7f
이석준 <sukjune96@gmail.com>
2022-11-22 18:11:40 +0900

    test: BridgeGame클래스 출력 함수 테스트

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -1,4 +1,5 @@
 const BridgeGame = require("../src/BridgeGame");
+
 describe("BridgeGame 클래스 테스트", () => {
   describe("compare함수 테스트", () => {
     const bridgeGame = new BridgeGame();
@@ -10,6 +11,20 @@ describe("BridgeGame 클래스 테스트", () => {
     test("정답인 다리와 입력 값이 일치하는 경우 False 리턴", () => {
       const input = "D";
       expect(bridgeGame.compareBridge(input)).toEqual(false);
+    });
+  });
+
+  describe("getBridgeString함수 테스트", () => {
+    const bridgeGame = new BridgeGame();
+    const testBridge = ["U", "D", "U", "U"];
+    bridgeGame.setBridge(testBridge);
+    testBridge.forEach((e) => {
+      bridgeGame.addCorrect();
+    });
+    const [upString, downString] = bridgeGame.getBridgeString();
+    test("정답 입력한 경우 출력 값 테스트", () => {
+      expect(upString).toEqual("[ O |   | O | O ]");
+      expect(downString).toEqual("[   | O |   |   ]");
     });
   });
 });
