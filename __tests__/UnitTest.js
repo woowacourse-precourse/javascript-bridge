747438b4b68bb8f67938861a99b2f65f96ce913c
Wooseong Kim <kimwooseong8313@gmail.com>
2022-11-22 18:14:10 +0900

    feat: 다리 생성 기능 테스트

--- __tests__/UnitTest.js
+++ __tests__/UnitTest.js
@@ -1,4 +1,14 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const BridgeMaker = require("../src/BridgeMaker");
+const BridgeRandomNumberGenerator = require("../src/BridgeRandomNumberGenerator");
 const InputValidator = require("../src/InputValidator");
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+};
 
 describe("Validator 테스트", () => {
   test("입력한 길이 유효성 검사", () => {
@@ -6,5 +16,14 @@ describe("Validator 테스트", () => {
     expect(InputValidator.isValidLength("3.14")).toEqual(false);
     expect(InputValidator.isValidLength("-22")).toEqual(false);
     expect(InputValidator.isValidLength("22")).toEqual(false);
+  });
+});
+
+describe("BridgeMaker 테스트", () => {
+  test("랜덤한 다리 생성 기능", () => {
+    mockRandoms([0, 1, 1, 0, 0, 1]);
+    expect(
+      BridgeMaker.makeBridge(6, BridgeRandomNumberGenerator.generate)
+    ).toEqual(["D", "U", "U", "D", "D", "U"]);
   });
 });
