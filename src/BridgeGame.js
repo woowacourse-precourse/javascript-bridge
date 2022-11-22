a0754519a42a492cfefeddcd4b4655932df29e1a
wonseok <rhdqngkrh77@khu.ac.kr>
2022-11-22 22:15:19 +0900

    feat: 재시작 기능 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -2,6 +2,11 @@
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  constructor(bridge, inputBridge) {
+    this.bridge = bridge;
+    this.inputBridge = inputBridge;
+    this.gameCount = 1;
+  }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
@@ -21,7 +26,16 @@ class BridgeGame {
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  retry() {}
+  retry(gameCommand) {
+    if (gameCommand === "R") {
+      this.gameCount++;
+      return true;
+    }
+    return false;
+  }
+  getGameCount() {
+    return this.gameCount;
+  }
 }
 
 module.exports = BridgeGame;
