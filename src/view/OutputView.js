f983b419af2a4bfc2cefd6b8c972ca631ea74754
minjaeKim <smallkdb@gmail.com>
2022-11-22 10:46:09 +0900

    style(view): 주석 삭제

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,8 +1,6 @@
 const { Console } = require('@woowacourse/mission-utils');
-const { OUTPUT_MESSAGE, ERROR } = require('../constants/Messages');
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
+const { OUTPUT_MESSAGE } = require('../constants/Messages');
+
 const OutputView = {
   printStart() {
     Console.print(OUTPUT_MESSAGE.start);
@@ -14,21 +12,11 @@ const OutputView = {
     OutputView.printNewLine();
     Console.print(message);
   },
-  /**
-   * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-   * <p>
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
   printMap({ upBridge, downBridge }) {
     Console.print(upBridge);
     Console.print(downBridge);
     OutputView.printNewLine();
   },
-  /**
-   * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-   * <p>
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
   printResult({ bridge, result, count }) {
     Console.print(OUTPUT_MESSAGE.result);
     OutputView.printMap(bridge);
