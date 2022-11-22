a0939c2c5c6b23c99f83b25396356e805921accb
yunjjeongjo <two6885@gmail.com>
2022-11-22 18:03:05 +0900

    chore: 게임 메시지 상수화

---
+++ src/constants/constants.js
@@ -0,0 +1,14 @@
+const MESSAGE = {
+  START: "다리 건너기 게임을 시작합니다.",
+  INPUT_LENGTH: "다리의 길이를 입력해주세요.",
+  INPUT_MOVE: "이동할 칸을 선택해주세요.",
+  RESTART_OR_TERMINATE:
+    "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
+  RESULT_SUCCESS: "게임 성공 여부: 성공",
+  RESULT_FAIL: "게임 성공 여부: 실패",
+  RESULT_TRIAL: "총 시도한 횟수: ",
+};
+
+module.exports = {
+  MESSAGE,
+};
