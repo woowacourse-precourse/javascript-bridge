9147c085f729b730e53346739bd2e31b8293e72a
Circlewee <pnj1118@naver.com>
2022-11-22 18:50:39 +0900

    feat(OutputView): 결과 출력 메소드 정의 (#12)
    1. 최종 결과를 출력하는 printResult() 정의

--- src/OutputView.js
+++ src/OutputView.js
@@ -21,8 +21,21 @@ const OutputView = {
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+   *
+   * @param {import('./BridgeGame').Flag} result
    */
-  printResult() {},
+  printResult(result) {
+    const messages = [
+      `\n게임 성공 여부: ${result.flag === 'GAME_END' ? '성공' : '실패'}`,
+      `총 시도한 횟수: ${result.status.accMoveCount}`,
+    ];
+
+    this.printMessage('\n최종 게임 결과');
+    this.printMap(result);
+    messages.forEach((message) => this.printMessage(message));
+
+    Console.close();
+  },
 
   /**
    * message를 출력하는 메소드
