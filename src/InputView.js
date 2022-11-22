3a70e6dd91f2f32655cfde12e618c2742ac806ea
cheonyeji <ssaltuck@gmail.com>
2022-11-22 23:56:56 +0900

    [feat] 다리 건너기 게임 결과 출력

--- src/InputView.js
+++ src/InputView.js
@@ -1,16 +1,21 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const bridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
 const bridgeMaker = require("./BridgeMaker");
+const BridgeGame = require("./BridgeGame");
+const OutputView = require("./OutputView");
+const bridgeGame = new BridgeGame();
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
+  // 다리 정보
+  bridge: [],
   /**
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize() {
     MissionUtils.Console.readLine("다리의 길이를 입력해주세요.", (input) => {
-      bridgeMaker.makeBridge(
+      this.bridge = bridgeMaker.makeBridge(
         Number(input),
         bridgeRandomNumberGenerator.generate
       );
@@ -23,6 +28,8 @@ const InputView = {
    */
   readMoving() {
     MissionUtils.Console.readLine("이동할 칸을 선택해주세요.", (input) => {
+      const result = bridgeGame.move(input, this.bridge);
+      OutputView.printMap(input, result);
       this.readMoving();
     });
   },
--- src/OutputView.js
+++ src/OutputView.js
@@ -1,13 +1,34 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 const OutputView = {
+  // 다리 정보 저장 (위 / 아래)
+  bridge: [[], []],
+
+  /**
+   * 게임 시작 문구를 출력한다.
+   */
+  printStart() {
+    MissionUtils.Console.print("다리건너기 게임을 시작합니다.");
+  },
+
   /**
    * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
    * <p>
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+   * result : O / X 값을 전달받는다
    */
-  printMap() {},
+  printMap(userInput, result) {
+    if (userInput === "D") {
+      this.bridge[0].push(" ");
+      this.bridge[1].push(result);
+    }
+    if (userInput === "U") {
+      this.bridge[0].push(result);
+      this.bridge[1].push(" ");
+    }
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
