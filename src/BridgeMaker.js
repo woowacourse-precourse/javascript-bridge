ec4f5c0ae3f5d219ae53aa119279dd17405cbf14
GuHyeonWoo <ghw9174@gmail.com>
2022-11-22 15:14:50 +0900

    fix (ApplicationTest.js): 테스트 코드 중 조건 변경
    - ApplicationTest.js: 원본 레포지토리 변경에 의해 테스트 코드 중 조건을 수정
    - BridgeMaker.js: 테스트 코드의 변경에 따라 로직 변경

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -2,7 +2,7 @@ const BridgeMaker = {
   makeBridge(size, generateRandomNumber) {
     const bridge = [];
     for(let i = 0; i < size; i++) {
-      bridge.push(String(generateRandomNumber()) === "1" ? "U" : "D");
+      bridge.push(generateRandomNumber() === 1 ? "U" : "D");
     }
     return bridge;
   },
