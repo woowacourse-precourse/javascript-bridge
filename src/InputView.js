6af259ac062d49e8b374f28962d7575bed5f7c47
wlthdud <thdudzi@naver.com>
2022-11-22 17:25:49 +0900

    fix: readLine 오타 수정

--- src/InputView.js
+++ src/InputView.js
@@ -1,5 +1,7 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+const generate = require("./BridgeRandomNumberGenerator");
+const makeBridge = require('./BridgeMaker');
+
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -9,9 +11,9 @@ const InputView = {
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize() {
-    MissionUtils.Console.readline('다리의 길이를 입력해주세요.', (size) => {
+    MissionUtils.Console.readLine('다리의 길이를 입력해주세요.', (size) => {
       this.exceptionOfReadBridgeSize(size);
-      BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate);
+      makeBridge(size, generate);
     });
   },
 
@@ -19,7 +21,7 @@ const InputView = {
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving() {
-    MissionUtils.Console.readline('이동할 칸을 선택해주세요. (위: U, 아래: D)', (space) => {
+    MissionUtils.Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)', (space) => {
       this.exceptionOfReadMoving(space);
     });
   },
@@ -28,7 +30,7 @@ const InputView = {
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand() {
-    MissionUtils.Console.readline('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)', (game) => {
+    MissionUtils.Console.readLine('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)', (game) => {
       this.exceptionOfReadGameCommand(game);
     });
   },
