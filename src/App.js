a2b945660a4878b96e17bd8c897a780760ab5dfd
kyeongwxx <kyeongwxx@gmail.com>
2022-11-22 19:27:57 +0900

    feat: 기능 4 - 다리 생성 기능 구현
    위 칸과 아래 칸 중 건널 수 있는 칸은 0과 1중 무작위 값을 이용해서 정한다.
    위 칸을 건널 수 있는 경우 U, 아래 칸을 건널 수 있는 경우 D값으로 나타낸다.
    무작위 값이 0인 경우 아래 칸, 1인 경우 위 칸이 건널 수 있는 칸이 된다.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 const { Console } = require("@woowacourse/mission-utils");
 const InputView = require("./InputView");
 const Validation = require("./validation/Validation");
+const BridgeMaker = require("./BridgeMaker");
+const { generate } = require("./BridgeRandomNumberGenerator");
 
 class App {
   play() {
     Console.print("다리 건너기 게임을 시작합니다.\n");
-    this.InputBridgeSize()
+    this.InputBridgeSize();
   }
 
   InputBridgeSize() {
@@ -15,6 +17,7 @@ class App {
         Console.print(error);
         return this.InputBridgeSize();
       }
+      BridgeMaker.makeBridge(Number(size), generate);
     });
   }
 }
