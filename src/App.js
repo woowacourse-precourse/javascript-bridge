b775886b82369245c9f08c726a1bbb18f13f6f9d
jjo-mi <whdal2636@naver.com>
2022-11-22 11:25:58 +0900

    fix: 4-5 이동할 칸 입력 및 사용자 입력값과 답 비교한 결과 출력 (async, awiat -> callback방식으로 변경)

--- src/App.js
+++ src/App.js
@@ -10,13 +10,10 @@ class App {
   #bridgeGame;
   #bridgeArr;
   #currentBridgeIndex;
-  #gameResult;
   #size;
-  #moveCount;
 
   constructor() {
     this.#currentBridgeIndex = 0;
-    this.#moveCount = 0;
   }
 
   play() {
@@ -29,17 +26,58 @@ class App {
     inputView.readBridgeSize((size) => {
       try {
         this.vaildSizeInput(size);
-        this.#size = size;
+        this.#size = Number(size);
 
         console.log('### size ', this.#size);
         this.#bridgeArr = bridgeMaker.makeBridge(this.#size, bridgeRandomNumberGenerator.generate);
         console.log('#### ', this.#bridgeArr);
 
+        this.#bridgeGame = new BridgeGame();
+        this.repeatMovingInput();
       } catch(e) {
         Console.print(e.message);
         this.startGame();
       }
     });
+  }
+
+  /** 4-5 이동할 칸 입력 및 사용자 입력값과 답 비교한 결과 출력 */
+  repeatMovingInput() {
+    // console.log('### repeatMovingInput ');
+    // console.log('### cu ', this.#currentBridgeIndex);
+    // console.log('### s ', this.#size);
+    if (this.#currentBridgeIndex === this.#size) {
+      // console.log('###### if');
+      this.#bridgeGame.gameResult = '성공';
+      outputView.printResult(this.#bridgeGame.makeBridgeResult());
+      return;
+    }
+
+    inputView.readMoving((move) => {
+      try { 
+        this.validMovingInput(move);
+        
+        const movingResult = this.#bridgeGame.move(this.#bridgeArr[this.#currentBridgeIndex], move);
+        console.log('#### m re', movingResult);
+        // console.log('#### m up', this.#bridgeGame.upBridgeReultArr);
+        // console.log('#### m down', this.#bridgeGame.downBridgeReultArr);
+        
+        outputView.printMap(this.#bridgeGame.upBridgeReultArr, this.#bridgeGame.downBridgeReultArr);
+        if (movingResult === 'O') {
+          this.#currentBridgeIndex++;
+          if (this.#currentBridgeIndex <= this.#size) {
+            this.repeatMovingInput();
+          }
+        } else if (movingResult === 'X') {
+          // retry 호출
+        }
+      } catch(e) {
+        Console.print(e.message);
+        this.repeatMovingInput();
+      }
+    })
+
+    
   }
 
   /** 8. 다리 선택 실패시 재시작 기능 */
