cddf34b0901d67c711291076fae3a5cd6bdc3168
soomin <qkrtnals970804@gmail.com>
2022-11-22 21:23:29 +0900

    feat: 다리 생성자 추가하기

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,7 +1,14 @@
+const BridgeMaker = require('./BridgeMaker');
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  constructor(size) {
+    this.size = size;
+    this.bridge = BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate);
+    this.position = 0;
+  }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
