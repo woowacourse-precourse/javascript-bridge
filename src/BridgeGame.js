aa1e5c5aa1ac57803381e6a48b82aa5dcf5b596f
tkdrb12 <tkdrb15@gmail.com>
2022-11-22 23:21:47 +0900

    style : 필요없는 공백, 주석 제거

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,9 +1,6 @@
 const { generate } = require("./BridgeRandomNumberGenerator");
 const { makeBridge } = require("./BridgeMaker.js");
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
 class BridgeGame {
   #bridgeShape
   #playerMovingRecord
--- src/Constants.js
+++ src/Constants.js
@@ -2,7 +2,7 @@ const MESSAGE = {
     PUT_SIZE : `다리의 길이를 입력해주세요.\n`,
     PUT_MOVE : `이동할 칸을 선택해주세요. (위: U, 아래: D)\n`,
     PUT_COMMAND : `게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n`,
-    GAME_START : '다리 건너기 게임을 시작합니다.\n',
+    GAME_START : `다리 건너기 게임을 시작합니다.\n`,
     RESULT : `최종 게임 결과`,
 }
 
@@ -11,6 +11,5 @@ const ERROR = {
     MOVE_INPUT_ERROR: `[ERROR] U또는 D값을 입력해주세요.`,
     COMMAND_INPUT_ERROR : `[ERROR] R또는 Q값을 입력해주세요.`,
 }
-
 
 module.exports = {MESSAGE, ERROR};
