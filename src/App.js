8360b49953096473203beff07a753062fc444b45
kyeongwxx <kyeongwxx@gmail.com>
2022-11-22 19:05:48 +0900

    feat: 기능 2 - 사용자의 입력 값(다리 길이)의 유효성 검사 기능 구현
    숫자만 입력한다.
    정수만 입력한다.
    3이상 20이하의 숫자만 입력한다.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 const { Console } = require("@woowacourse/mission-utils");
+const InputView = require("./InputView");
+const Validation = require("./validation/Validation");
 
 class App {
   play() {
     Console.print("다리 건너기 게임을 시작합니다.");
+  }
+
+  InputBridgeSize() {
+    InputView.readBridgeSize((size) => {
+      Validation.validateBridgeSize(size);
+    });
   }
 }
 
