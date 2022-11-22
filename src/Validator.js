775d0373a3b195b20eba0a04c0ff57757ccd0c21
rayark1 <max980203@snu.ac.kr>
2022-11-22 16:56:05 +0900

    refactor(src/Validator.js): 상수화 진행

--- src/Validator.js
+++ src/Validator.js
@@ -4,6 +4,12 @@ const Validator = {
   ERROR_MESSEAGE_MUST_BE_NUMBER: "[ERROR] 다리 길이는 숫자여야 합니다.",
   ERROR_MESSEAGE_MUST_U_OR_D: "[ERROR] U 또는 D만 입력 가능합니다.",
   ERROR_MESSEAGE_MUST_R_OR_Q: "[ERROR] R 또는 Q만 입력 가능합니다.",
+  BRIDGE_LOWER_INCLUSIVE: 3,
+  BRIDGE_UPPER_INCLUSIVE: 20,
+  MOVE_UPSIDE_STRING: "U",
+  MOVE_DOWNSIDE_STRING: "D",
+  RETRY_STRING: "R",
+  EXIT_STRING: "Q",
 
   validateBridgeSize(bridgeSizeString) {
     Validator.validateInputisNumber(bridgeSizeString);
@@ -18,19 +24,19 @@ const Validator = {
   },
 
   validateInputisInRange(bridgeSize) {
-    if (bridgeSize < 3 || bridgeSize > 20) {
+    if (bridgeSize < Validator.BRIDGE_LOWER_INCLUSIVE || bridgeSize > Validator.BRIDGE_UPPER_INCLUSIVE) {
       throw new Error(Validator.ERROR_MESSEAGE_MUST_IN_RANGE);
     }
   },
 
   validateDirection(direction) {
-    if (direction !== "U" && direction !== "D") {
+    if (direction !== Validator.MOVE_UPSIDE_STRING && direction !== Validator.MOVE_DOWNSIDE_STRING) {
       throw new Error(Validator.ERROR_MESSEAGE_MUST_U_OR_D);
     }
   },
 
   validateRetryCommand(command) {
-    if (command !== "R" && command !== "Q") {
+    if (command !== Validator.RETRY_STRING && command !== Validator.EXIT_STRING) {
       throw new Error(Validator.ERROR_MESSEAGE_MUST_R_OR_Q);
     }
   }
