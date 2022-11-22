5399df2d7333720c23a52aa758519b94cc07c0e0
Sungjae.Kwak <sungjae8970@gmail.com>
2022-11-22 12:47:03 +0900

    feat(BridgeGame, InputView): 사용자 이동 기능
    - TODO: 이동 값 유효성검사

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -12,9 +12,15 @@ const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
  */
 class BridgeGame {
   #bridge;
+  #movingLog;
+  #tryCount;
+  #isPlaying;
 
   constructor() {
     this.#bridge = [];
+    this.#movingLog = [];
+    this.#tryCount = 1;
+    this.#isPlaying = true;
   }
 
   /**
@@ -22,7 +28,16 @@ class BridgeGame {
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(moving) {
+    // TODO: validate moving
+    if (moving === this.#bridge[this.#movingLog.length]) {
+      this.#movingLog.push(moving);
+      this.#isPlaying = true;
+    } else {
+      this.#movingLog.push(moving);
+      this.#isPlaying = false;
+    }
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
