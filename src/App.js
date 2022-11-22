a821cdfe68a5e85df0040ca2d85b9b11ab4e18df
Wooseong Kim <kimwooseong8313@gmail.com>
2022-11-22 18:28:44 +0900

    feat: 다리 길이 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+const InputValidator = require("./InputValidator");
+const InputView = require("./InputView");
+const OutputView = require("./OutputView");
+
 class App {
-  play() {}
+  constructor() {
+    this.game;
+  }
+
+  play() {
+    InputView.readBridgeSize(this.handleInputLength.bind(this));
+  }
+
+  handleInputLength(input) {
+    try {
+      if (!InputValidator.isValidLength(input)) {
+        throw new Error("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+      }
+      this.game = +input;
+    } catch (error) {
+      OutputView.printMessage(error.message);
+      InputView.readBridgeSize(this.handleInputLength.bind(this));
+    }
+  }
 }
 
 module.exports = App;
