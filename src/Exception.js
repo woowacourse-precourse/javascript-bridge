00f5db4d751c9f73b1dfcd283e7d05195d51edc0
cho seonghoon <chsida655@gmail.com>
2022-11-22 15:53:21 +0900

    feat: 입력에 대한 예외 처리 추가

---
+++ src/Exception.js
@@ -0,0 +1,42 @@
+const { Console, Random } = require("@woowacourse/mission-utils");
+
+const Exception = {
+  checkBridgeLength(input) {
+    try {
+      if (isNaN(input)) {
+        throw "[ERROR]다리 길이는 숫자를 입력해주세요.";
+      }
+      if (input < 3 || input > 20) {
+        throw "[ERROR] 다리 길이는 3 ~ 20사이의 숫자여야 합니다.";
+      }
+      return false;
+    } catch (e) {
+      Console.print(e);
+      return true;
+    }
+  },
+  checkGameMove(input) {
+    try {
+      if (input !== "U" && input !== "D") {
+        throw "[ERROR] 라운드 별 이동은 U 또는 D여야 합니다.";
+      }
+      return false;
+    } catch (e) {
+      Console.print(e);
+      return true;
+    }
+  },
+  checkGameCommand(input) {
+    try {
+      if (input !== "Q" && input !== "R") {
+        throw "[ERROR] 게임 Command는 R 또는 Q여야 합니다.";
+      }
+      return false;
+    } catch (e) {
+      Console.print(e);
+      return true;
+    }
+  },
+};
+
+module.exports = Exception;
