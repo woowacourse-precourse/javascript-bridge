607596399b9dbf02b5f738051d814188dd898226
sunny <huiseon37@gmail.com>
2022-11-22 15:28:58 +0900

    test: 다리 배열 길이 예외 테스트 코드 작성

--- __tests__/BridgeTest.js
+++ __tests__/BridgeTest.js
@@ -1,3 +1,4 @@
+const ERROR_MESSAGE = require("../src/constants/ErrorMessage");
 const Bridge = require("../src/model/Bridge");
 
 describe("Bridge 클래스 테스트", () => {
@@ -7,5 +8,15 @@ describe("Bridge 클래스 테스트", () => {
 
     expect(result).toEqual(true);
   });
+  
+  test("다리 끝까지 도달 했을때 true를 반환한다.", () => {
+    const bridge = new Bridge(['U','D','D']);
+    const result = bridge.isReach(2);
 
+    expect(result).toEqual(true);
+  });
+
+  test("다리 길이는 3~20 사이여야 한다.", () => {
+    expect(new Bridge(['U','D'])).toThrow(ERROR_MESSAGE.BRIDGE_LENGTH);
+  })
 });
---
+++ src/constants/ErrorMessage.js
@@ -0,0 +1,5 @@
+const ERROR_MESSAGE = {
+  BRIDGE_LENGTH: '다리 길이는 3 ~ 20 사이의 숫자여야 한다.'
+}
+
+module.exports = ERROR_MESSAGE;
