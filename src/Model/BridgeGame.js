c4e84b986c3ea28e497bb14bbba4143c2a4ac109
thewronghand <penfreak77@gmail.com>
2022-11-22 22:15:17 +0900

    chore: 각주 삭제
    기본으로 제공되는 기능의 각주를 삭제한다.

--- src/Model/BridgeGame.js
+++ src/Model/BridgeGame.js
@@ -2,9 +2,6 @@ const { SIGN, STATUS, KEYS } = require('../utils/constants');
 const BridgeMaker = require('../BridgeMaker');
 const BridgeRandomNumberGenerator = require('../utils/BridgeRandomNumberGenerator');
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
 class BridgeGame {
   #bridge;
   #playerAt = 0;
@@ -38,11 +35,6 @@ class BridgeGame {
     return this.#bridgeLower;
   }
 
-  /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
   move(input) {
     this.#checkMovement(input, this.#bridge, this.#playerAt);
     this.#setBridgeForm(input);
@@ -100,11 +92,6 @@ class BridgeGame {
     }
   }
 
-  /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
   retry() {
     this.#trialCount = this.#trialCount + 1;
     this.#bridgeUpper = [];
--- src/Viewer/InputView.js
+++ src/Viewer/InputView.js
@@ -1,31 +1,19 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const { MESSAGE } = require('../utils/constants');
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 const InputView = {
-  /**
-   * 다리의 길이를 입력받는다.
-   */
   readBridgeSize(callback) {
     MissionUtils.Console.readLine(MESSAGE.READ_USER_INPUT_SIZE, (input) => {
       callback(input);
     });
   },
 
-  /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
   readMoving(callback) {
     MissionUtils.Console.readLine(MESSAGE.READ_USER_INPUT_MOVE, (input) => {
       callback(input);
     });
   },
 
-  /**
-   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-   */
   readGameCommand(callback) {
     MissionUtils.Console.readLine(MESSAGE.READ_USER_INPUT_COMMAND, (input) => {
       callback(input);
