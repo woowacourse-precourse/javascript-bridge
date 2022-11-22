0fefcd621b0dac8e5952a3dab1e9fa81c2ed02bf
Jungwon Kim <dhkfkr134@naver.com>
2022-11-22 14:37:16 +0900

    add(OutputView): OutputView 메서드 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,6 +1,18 @@
 const { Console } = require('@woowacourse/mission-utils');
 const OutputView = {
 
+  printStart(){
+    Console.print("다리건너기 게임을 시작합니다.");
+  },
+  printEnterBridgeSize(){
+    Console.print("다리의 길이를 입력해주세요.")
+  },
+  printChooseMoving(){
+    Console.print("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+  },
+  printEnterGameCommand(){
+    Console.print("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+  },
   printMap([upBridge, downBridge]) {
     Console.print(`[ ${upBridge.join(' | ')} ]\n[ ${downBridge.join(' | ')} ]\n`);
   },
