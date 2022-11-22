203d7c513eac379e645cf9eb381965f4c74e7831
위성노 <dev.satellite.no92@gmail.com>
2022-11-22 20:49:10 +0900

    refactor(InputValidator): remove ternary operator

--- src/ui/InputValidator.js
+++ src/ui/InputValidator.js
@@ -17,17 +17,17 @@ const InputValidator = {
   checkNumber(number) {
     const isNumber = new RegExp("[0-9]", "g");
     const isLength = Number(number) < BRIDGE.LENGTH.MIN || Number(number) > BRIDGE.LENGTH.MAX;
-    return !isNumber.test(number) || isLength ? true : false;
+    return !isNumber.test(number) || isLength;
   },
 
   checkMoveInput(moveInput) {
     const isMoveInput = new RegExp("U|D", "g");
-    return !isMoveInput.test(moveInput) || moveInput.length !== 1 ? true : false;
+    return !isMoveInput.test(moveInput) || moveInput.length !== 1;
   },
 
   checkCommand(command) {
     const isCommand = new RegExp("Q|R", "g");
-    return !isCommand.test(command) || command.length !== 1 ? true : false;
+    return !isCommand.test(command) || command.length !== 1;
   },
 };
 
