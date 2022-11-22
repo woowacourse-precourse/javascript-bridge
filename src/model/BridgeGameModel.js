4c277ff7313ec7284d460fd3db1191e7e9600d44
박준서 <oponize@naver.com>
2022-11-22 23:39:42 +0900

    refactor(*): 구조분해 할당으로 import 간소화

--- src/model/BridgeGameModel.js
+++ src/model/BridgeGameModel.js
@@ -1,4 +1,8 @@
-const { SIZE, KEYWORD, HASH } = require("../constants/index.js");
+const {
+  SIZE: { MIN_SIZE, MAX_SIZE },
+  KEYWORD: { RETRY, QUIT, UP, DOWN },
+  HASH,
+} = require("../constants/index.js");
 const {
   BridgeGameSizeError,
   BridgeGameMoveError,
@@ -15,7 +19,6 @@ const BridgeGameModel = class {
   }
 
   start(bridge) {
-    const { RETRY } = KEYWORD;
     this.#retry.push(RETRY);
     this.#bridge.push(...bridge);
   }
@@ -34,14 +37,12 @@ const BridgeGameModel = class {
   }
 
   result() {
-    const { RETRY } = KEYWORD;
     const retry = this.#retry.filter((retry) => retry === RETRY).length;
     return { retry, success: this.isSuccess(), map: this.read() };
   }
 
   isRetry() {
     const retry = this.#retry[this.#retry.length - 1];
-    const { RETRY } = KEYWORD;
     return retry === RETRY;
   }
 
@@ -56,21 +57,18 @@ const BridgeGameModel = class {
 
   checkBridge(bridge) {
     const length = bridge.length;
-    const { MIN_SIZE, MAX_SIZE } = SIZE;
     const isBrige = length >= MIN_SIZE && length <= MAX_SIZE;
     if (!isBrige) throw new BridgeGameSizeError();
     return bridge;
   }
 
   checkDirection(move) {
-    const { UP, DOWN } = KEYWORD;
     const isMove = move === UP || move === DOWN;
     if (!isMove) throw new BridgeGameMoveError();
     return move;
   }
 
   checkRetry(input) {
-    const { RETRY, QUIT } = KEYWORD;
     const isRetry = input === RETRY || input === QUIT;
     if (!isRetry) throw new BridgeGameRetryError();
     return input;
--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,26 +1,25 @@
 const { continueAfterCatchError } = require("../utils/Misc.js");
 const { read } = require("../utils/IO.js");
-const { INPUT } = require("../constants/index.js");
+const {
+  INPUT: { READ_BRIDGE, MOVING_BRIDGE, RETRY_BRIDGE },
+} = require("../constants/index.js");
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
   readBridgeSize(callback) {
-    const { READ_BRIDGE } = INPUT;
     read(READ_BRIDGE, (size) =>
       continueAfterCatchError(size, callback, this.readBridgeSize.bind(this))
     );
   },
 
   readMoving(callback) {
-    const { MOVING_BRIDGE } = INPUT;
     read(MOVING_BRIDGE, (move) =>
       continueAfterCatchError(move, callback, this.readMoving.bind(this))
     );
   },
 
   readGameCommand(callback) {
-    const { RETRY_BRIDGE } = INPUT;
     read(RETRY_BRIDGE, (move) =>
       continueAfterCatchError(move, callback, this.readGameCommand.bind(this))
     );
