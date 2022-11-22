b7b732f77049fff8c0425b5117e60393c918ec4e
고동현 <dhjk35@naver.com>
2022-11-22 19:39:54 +0900

    chore(constants): 에러 메시지 수정

--- src/constants/errorMessage.js
+++ src/constants/errorMessage.js
@@ -1,6 +1,5 @@
 const ERROR_MESSAGE = {
-  invalidDirection:
-    '[ERROR] 이동할 칸은 위(U), 아래(D) 아래 중에 입력할 수 있습니다.',
+  invalidDirection: '[ERROR] 다리의 방향은 위(U), 아래(D) 중에 하나입니다.',
   invalidCommand:
     '[ERROR] 게임을 다시 시도 하려면 R, 종료하려면 Q를 대문자로 입력해주세요.',
   invalidRange: '[ERROR] 다리의 길이는 3이상 20이하의 숫자여야 합니다.',
