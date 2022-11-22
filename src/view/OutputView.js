2bef1ff43a237f500e135969959fbe9a27f62258
GuHyeonWoo <ghw9174@gmail.com>
2022-11-22 22:49:09 +0900

    refactor (OutputView.js): mvc 패턴 적용에 의한 로직 수정
    - 출력 로직을 제외한 나머지 로직을 적절한 파일로 이동 및 수정
    - OutputMessage를 참조하여 하드코딩된 출력 문자열 수정

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,48 +1,31 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const Console = MissionUtils.Console;
 
+const OutputMessage = require("../static/OutputMessage");
+
 const OutputView = {
   printOpening() {
-    Console.print('다리 건너기 게임을 시작합니다.\n');
+    Console.print(`${OutputMessage.opening}\n`);
   },
 
-  printMap(movingState) {
-    [movingState.upState, movingState.downState].forEach((state) => {
-      this.printState(state);
-    })
-  },
-
-  printState(state) {
-    const map = this.stringConversion(state);
+  printMap(map) {
     Console.print(map);
   },
 
-  stringConversion(state) {
-    let conversionValue = '[ '
-    state.forEach((element, index) => {
-      conversionValue += `${element}`
-      index === state.length - 1 ? conversionValue += ' ]' : conversionValue += ' | '
-    })
-    return conversionValue;
+  printLineBreak() {
+    Console.print('');
   },
 
-  printResult(movingState, success) {
-    if(success) Console.print('');
-
-    Console.print('최종 게임 결과');
-    this.printMap(movingState);
-
-    this.printSuccess(success);
-    this.printAttempts(movingState.attempts);
+  printResultPhrases() {
+    Console.print(`${OutputMessage.resultMap}`);
   },
 
-  printSuccess(success) {
-    const successResult = success ? `성공` : '실패';
-    Console.print(`\n게임 성공 여부: ${successResult}`);
+  printSuccess(successResult) {
+    Console.print(`\n${OutputMessage.success}${successResult}`);
   },
 
   printAttempts(attempts) {
-    Console.print(`총 시도한 횟수: ${attempts}`);
+    Console.print(`${OutputMessage.attempts}${attempts}`);
     Console.close();
   },
 
