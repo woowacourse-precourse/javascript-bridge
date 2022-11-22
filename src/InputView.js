ba01556e7b2ec4861a5d1c59f015657433c00ae9
Choejeonghun <saa00123@naver.com>
2022-11-22 15:57:53 +0900

    feat: 이동할 칸의 수 입력과 입력받은 이동 칸을 전달받는 기능 구현

--- src/InputView.js
+++ src/InputView.js
@@ -1,4 +1,5 @@
 const { Console } = require('@woowacourse/mission-utils');
+const BridgeGame = require('./BridgeGame');
 const BridgeMaker = require('./BridgeMaker');
 const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
 /**
@@ -14,13 +15,29 @@ const InputView = {
         inputBridgeSize,
         BridgeRandomNumberGenerator.generate
       );
+      let bridgeList = [[], []];
+      let attempt = 1;
+      this.readMoving(bridge, bridgeList, attempt);
     });
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving(bridge, bridgeList, attempt) {
+    Console.readLine(
+      '이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
+      (inputBridgeChoice) => {
+        const bridgeGame = new BridgeGame();
+        const movingResult = bridgeGame.move(
+          inputBridgeChoice,
+          bridge,
+          bridgeList
+        );
+        return this.readMoving(bridge, movingResult, attempt);
+      }
+    );
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
