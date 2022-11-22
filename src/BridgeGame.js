b460d7531f3c5d5daa29c43051f4901c0e1df759
김상두 <puki4416@gmail.com>
2022-11-22 12:12:31 +0900

    feat : 해당 이동칸이 이동가능한지 불가능한지 이동이 완료되었는지를 판단하는 기능

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,20 +1,45 @@
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
-class BridgeGame {
+ class BridgeGame {
+  #order;
+  #bridge;
+  constructor() {
+    this.#order = -1;
+  }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(input) {
+    this.#order += 1;
+    if (input === this.#bridge[this.#order]) {
+      if (this.#order === this.#bridge.length - 1) return "END";
+      return "SUCCESS";
+    }
+    return "FAIL";
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  retry() {}
+  retry() {
+  }
+
+  setBridge(bridge) {
+    this.#bridge = bridge;
+  }
+
+  getOrder() {
+    return this.#order;
+  }
+
+  getBridge() {
+    return [...this.#bridge];
+  }
 }
 
 module.exports = BridgeGame;
