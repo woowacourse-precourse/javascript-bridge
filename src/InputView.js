ae3cd172d33426afd1e1c4750d3684140e53da4d
Haram Baek <haram@Haramui-MacBookAir.local>
2022-11-22 23:59:17 +0900

    test: 예외처리 추가가

--- src/InputView.js
+++ src/InputView.js
@@ -25,6 +25,9 @@ const InputView = {
   validateBridge(size) {
     if (size < 3 || size > 20)
       throw Error('[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.');
+
+    if (!Number(size))
+      throw Error('[ERROR] 다리 길이는 숫자여야 합니다.');
   },
 
   /**
