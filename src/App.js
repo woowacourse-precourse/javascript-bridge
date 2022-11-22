70b2c76bf1b10a68150a3e1e4e7ff1d160be0a59
jihun1233 <leejihun1233@gmail.com>
2022-11-22 22:12:27 +0900

    feat: 다리 길이 입력 기능 구현
    - readBridgeSize가 다리길이를 입력받아 resolve하는 Promise를 반환하도록 구현
    
    - App에서 async-await으로 readBridgeSize를 실행하고 size를 가져와 BridgeGame의 생성자에 size를 전달해 인스턴스를 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const BridgeGame = require('./BridgeGame')
+const InputView = require('./InputView')
+
 class App {
-  play() {}
+  async play() {
+    const size = await InputView.readBridgeSize()
+
+    const bridgeGame = new BridgeGame(size)
+  }
 }
+
+const app = new App()
+app.play()
 
 module.exports = App;
