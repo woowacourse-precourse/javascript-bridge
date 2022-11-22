bc600e4db2169042348698527577fc5753b6dc39
xownsdl0 <xownsdl0@naver.com>
2022-11-22 20:59:18 +0900

    style: 출력 화면 포맷 재수정

--- src/OutputView.js
+++ src/OutputView.js
@@ -7,14 +7,14 @@ const OutputView = {
   },
 
   printMap(map) {
-    MissionUtils.Console.print(`[ ${ map[0].join(" | ") } ]\n`)
+    MissionUtils.Console.print(`[ ${ map[0].join(" | ") } ]`)
     MissionUtils.Console.print(`[ ${ map[1].join(" | ") } ]\n`)
   },
 
   printResult(result) {
-    MissionUtils.Console.print(Message.END_GAME)
-    MissionUtils.Console.print(`[  ${result.bridgeMap[0].join("   |  ") }  ]\n`)
-    MissionUtils.Console.print(`[    ${result.bridgeMap[1].join("  |  ") }   ]\n`)
+    MissionUtils.Console.print(`${Message.END_GAME}`)
+    MissionUtils.Console.print(`[ ${result.bridgeMap[0].join(" | ") } ]`)
+    MissionUtils.Console.print(`[ ${result.bridgeMap[1].join(" | ") } ]`)
     MissionUtils.Console.print(`${result.gameOver}`)
     MissionUtils.Console.print(`${result.moveCount}`)
     MissionUtils.Console.close()
