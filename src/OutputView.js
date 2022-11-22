c00528fab405d1f670fbf048c55dce32cba1b394
jin-Pro <dnjun2@ajou.ac.kr>
2022-11-22 16:41:57 +0900

    enhance: OutputView method is printMap

--- src/OutputView.js
+++ src/OutputView.js
@@ -8,16 +8,20 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {},
+  printMap(answer, input) {
+    const obj = makeMapObj(answer, input);
+    const map = makeMap(input, obj);
+    Console.print(map);
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() {
+  printResult(answer, input) {
     Console.print("최종 게임 결과");
-    this.printMap();
+    this.printMap(answer, input);
   },
 
   printStart() {
