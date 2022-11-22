0dd14c0f70da5041007c91d97b779fb8053a6369
kitez <duswl339@ajou.ac.kr>
2022-11-22 14:00:19 +0900

    refactor: Map을 출력하는 로직 수정
    + 규칙성을 찾아 더 간단하게 수정하였다.
    + join() 함수를 사용하여 더 간단하게 수정

--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -1,7 +1,5 @@
-const MissionUtils = require("@woowacourse/mission-utils");
-const Const = require('../constant/constant');
+const MissionUtils = require('@woowacourse/mission-utils');
 const message = require('../constant/message');
-
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -12,21 +10,11 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap(bridge, current, input) {
-    MissionUtils.Console.print('['+this.printMapLine(bridge, current, input, Const.MOVE[0])+']');
-    MissionUtils.Console.print('['+this.printMapLine(bridge, current, input, Const.MOVE[1])+']');
-  },
-
-  printMapLine(bridge, current, input, flag){
-    let result = '';
-    for(let i = 0 ; i < current -1 ;i++){
-      if(bridge[i] === flag) {result += ' O |';}
-      else{result += '   |'}
-    }
-    if(input === flag && bridge[current-1] === flag) {result += ' O ';}
-    else if(input === flag && bridge[current-1] !== flag) {result += ' X ';}
-    else{result += '   '}
-    return result.length === 0 ? '   ' : result;
+  printMap(bridges) {
+    bridges.forEach((bridge) => {
+      if (bridge.length > 1) this.printMessage(`[${bridge.join('|')}]`);
+      else this.printMessage(`[${bridge}]`);
+    });
   },
 
   /**
@@ -34,17 +22,17 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult(bridge, input, result, totalTry) {
+  printResult(bridge, success, totalTry) {
     this.printMessage(message.MESSAGE.GAME_END);
-    this.printMap(bridge, bridge.length, input);
-    if(result) {this.printMessage(message.MESSAGE.SUCCESS);}
-    else{this.printMessage(message.MESSAGE.FAIL);}
-    this.printMessage(message.MESSAGE.GAME_RESULT+totalTry);
+    this.printMap(bridge);
+    if (success) this.printMessage(message.MESSAGE.SUCCESS);
+    else this.printMessage(message.MESSAGE.FAIL);
+    this.printMessage(message.MESSAGE.GAME_RESULT + totalTry);
   },
 
-  printMessage(message){
+  printMessage(message) {
     MissionUtils.Console.print(message);
-  }
+  },
 };
 
 module.exports = OutputView;
