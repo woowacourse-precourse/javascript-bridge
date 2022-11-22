3f41d42b2704090b2931b8ed0f103fa65e4c33fb
jetty8013 <jetty8013@gmail.com>
2022-11-22 19:02:55 +0900

    feat: 다리 길이 사용자 입력 기능 구현

--- src/InputView.js
+++ src/InputView.js
@@ -5,7 +5,14 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    MissionUtils.Console.print('다리 건너기 게임을 시작합니다.');
+    MissionUtils.Console.readLine('다리의 길이를 입력해주세요.', (answer) => {
+      MissionUtils.Console.print(`${answer}`);
+      if(3 <= answer && answer <= 20) return answer;
+      throw('[ERROR]', '다리 길이는 3부터 20 사이의 숫자여야 합니다.')
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
