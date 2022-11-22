0e599b58bdc1ff3ff04e679d2cafaf006383f6ba
n0eyes <doll4586633@naver.com>
2022-11-22 23:33:17 +0900

    feat: 미사용 모듈 require 제거

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,5 +1,4 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-const { formatMap } = require("./lib/bridge");
 const { MESSAGE } = require("./lib/constants");
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
