efd04943b3b69cd978e6ae51c3ed04e6c44296a2
banma1234 <banma1234@gmail.com>
2022-11-22 23:02:07 +0900

    refactor: 예외처리함수 모듈화에 따른 다래생성 함수 최적화

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,34 +1,22 @@
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
-const BridgeMaker = {
+ const BridgeMaker = {
   /**
    * @param {number} size 다리의 길이
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
   makeBridge(size, generateRandomNumber) {
-    BridgeMaker.checkSize(size);
     let bridge = [];
+
     for (let i=0; i<size; i++) {
       let RANDOM_NUMBER = generateRandomNumber();
-
       if (RANDOM_NUMBER == 0) bridge.push("D");
       else if (RANDOM_NUMBER == 1)  bridge.push("U");
-      else  throw new Error("[Error] 숫자가 잘못됨");
     }
 
     return bridge;
-  },
-
-  checkSize(size) {
-    if (typeof(size) != "number") {
-      throw new Error("[Error] 다리의 크기는 숫자를 입력해주세요");
-    }
-
-    if (size < 3 | size > 20) {
-      throw new Error("[Error] 다리의 크기는 3 ~ 20 사이의 숫자만 가능합니다.");
-    }
   },
 };
 
