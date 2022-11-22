bdf015b483efa0f1f4224046ab13021e6405e453
recomplete <wantop@jbnu.ac.kr>
2022-11-22 21:00:01 +0900

    feat:result 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,57 +1,69 @@
-/* 다리 건너기 게임을 관리하는 클래스
+const { Console } = require("@woowacourse/mission-utils");
+const OutputView = require("./OutputView");
+
+/**
+ * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
   #bridge;
-  #move = [];
-  #result = [[], []];
+  #status;
   #count;
 
   constructor(bridge) {
     this.#bridge = bridge;
-    
+    this.#status = [];
+    this.#count = 1;
   }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-   move(direction) {
-    this.#move.push([direction, direction === "U" ? 0 : 1]);
+  move(direction) {
+    this.#status.push([direction, direction === "U" ? 0 : 1]);
   }
 
   moveResult() {
-    this.#move.map(([direction, isBottom], index) => {
+    let result = [[], []];
+
+    this.#status.map(([direction, isBottom], index) => {
       const compareBridge = direction === this.#bridge[index];
 
-      this.#result[isBottom].push(compareBridge ? "O" : "X");
-      this.#result[Math.abs(isBottom - 1)].push(" ");
+      result[isBottom].push(compareBridge ? "O" : "X");
+      result[Math.abs(isBottom - 1)].push(" ");
     });
 
-    return this.#result;
+    return result;
   }
 
   isFail() {
-    const index = this.#move.length - 1;
+    const index = this.#status.length - 1;
 
-    return this.#bridge[index] !== this.#move[index];
+    return this.#bridge[index] !== this.#status[index][0];
   }
 
   isEnd() {
-    return this.#bridge.length === this.#move.length;
+    return this.#bridge.length === this.#status.length;
   }
 
   quit() {
     let isSuccess = !this.isFail() && this.isEnd();
-    OutputView.printResult(isSuccess);
+
+    Console.print("최종 게임 결과\n");
+    OutputView.printMap(this.moveResult());
+    OutputView.printResult({ isSuccess, count: this.#count });
+    Console.close();
   }
+
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-   retry() {
+  retry() {
     this.#count += 1;
-    this.#move = [];
+    this.#status = [];
   }
 }
+
 module.exports = BridgeGame;
