389aa23b994deeeb558274b7afc18939f991cf6b
xownsdl0 <xownsdl0@naver.com>
2022-11-22 20:11:33 +0900

    style: 게임 결과 출력 화면 개행 및 뜨워쓰기 수정

--- src/error/ErrorMessage.js
+++ src/error/ErrorMessage.js
@@ -1,5 +1,5 @@
 const ERROR_MESSAGE = {
-  RANGE_ERROR: '[ERROR] 3 ~ 20 사이의 숫자만 입력해주세요.',
+  RANGE_ERROR: '[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.',
   INPUT_MOVE_ERROR: '[ERROR] 대문자 U 또는 D만 입력해주세요.',
   INPUT_RETRY_ERROR: '[ERROR] 대문자 R 또는 Q만 입력해주세요.'
 }
