48e739319ef6fe8fdb8bf7c55b523b8601124dda
hyeongdong <khd941018@gmail.com>
2022-11-22 20:59:29 +0900

    refactor : outputView 상수 분리 및 중복코드 제거

--- src/OutputView.js
+++ src/OutputView.js
@@ -4,18 +4,27 @@ const { Console } = require("@woowacourse/mission-utils");
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 const OutputView = {
-  printMap(bridgeMap) {
-    const [upBridge, downBridge] = bridgeMap;
+  SEPERATOR: " | ",
+  SUCCESS: "성공",
+  FAILURE: "실패",
 
-    Console.print(`[ ${upBridge.join(" | ")} ]`);
-    Console.print(`[ ${downBridge.join(" | ")} ]`);
+  MESSAGE: {
+    RESULT: "최종 게임 결과",
+    SUCCESS_OR_FAIL: "게임 성공 여부",
+    TOTAL_ATTEMPT: "총 시도한 횟수",
   },
 
-  printResult(isBridgeCrossed, attemptCount, bridgeMap) {
-    Console.print("최종 게임 결과");
-    this.printMap(bridgeMap);
-    Console.print(`게임 성공 여부: ${isBridgeCrossed ? "성공" : "실패"}`);
-    Console.print(`총 시도한 횟수: ${attemptCount}`);
+  printMap(bridgeMaps) {
+    bridgeMaps.forEach((bridgeMap) => {
+      Console.print(`[ ${bridgeMap.join(this.SEPERATOR)} ]`);
+    });
+  },
+
+  printResult(isBridgeCrossed, attemptCount, bridgeMaps) {
+    Console.print(this.MESSAGE.RESULT);
+    this.printMap(bridgeMaps);
+    Console.print(`${this.MESSAGE.SUCCESS_OR_FAIL}: ${isBridgeCrossed ? this.SUCCESS : this.FAILURE}`);
+    Console.print(`${this.MESSAGE.TOTAL_ATTEMPT}: ${attemptCount}`);
   },
 };
 
