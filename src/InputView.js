bbd3599b73dc815d378ca23bb5ba6ac0fa0f33dc
Choejeonghun <saa00123@naver.com>
2022-11-22 15:54:36 +0900

    feat: 다리 길이 입력 및 다리 생성

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,6 @@
+const { Console } = require('@woowacourse/mission-utils');
+const BridgeMaker = require('./BridgeMaker');
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,7 +8,14 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine('다리 길이를 입력해주세요.\n', (inputBridgeSize) => {
+      const bridge = BridgeMaker.makeBridge(
+        inputBridgeSize,
+        BridgeRandomNumberGenerator.generate
+      );
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
