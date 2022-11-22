67fabe018bed14cdca7db141c1cd2991f6cfd17d
SeungHun Park <tmdgns1126@naver.com>
2022-11-22 17:41:20 +0900

    feat: 다리 만들기 로직 재구현

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 const { Console } = require("@woowacourse/mission-utils");
+const BridgeGame = require("./BridgeGame");
+const BridgeMaker = require("./BridgeMaker");
 const InputView = require("./InputView");
+const OutputView = require("./OutputView");
+const Checker = require("./Checker");
+const Error = require("./Error");
 
 class App {
   #tryCount;
+  #game;
+
   constructor() {
     this.#tryCount = 1;
   }
+
   play() {
-    Console.print("다리 건너기 게임을 시작합니다.\n");
-    InputView.readBridgeSize();
+    OutputView.printGameStart();
+    this.readBridgeSize();
+  }
+
+  readBridgeSize() {
+    InputView.readBridgeSize((input) => {
+      if (Error.isThrow(Checker.bridgeSize, input)) {
+        this.makeBridge(input);
+        return;
+      }
+      this.readBridgeSize();
+    });
+  }
+
+  makeBridge(size) {
+    this.makeGame(size);
+    this.moveAfterValidation();
+  }
+
+  makeGame(size) {
+    const bridge = BridgeMaker.makeBridgeMain(Number(size));
+    this.#game = new BridgeGame(bridge);
+  }
   }
 }
 
