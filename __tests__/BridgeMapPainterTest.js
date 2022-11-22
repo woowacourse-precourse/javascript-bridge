f4b497808ab8b21ae749d027c4294cd40b00d050
ReturnReason <reasonz@daum.net>
2022-11-22 12:43:51 +0900

    test(BridgeMapPainter): getUserBridgeMap 메서드 테스트

--- __tests__/BridgeMapPainterTest.js
+++ __tests__/BridgeMapPainterTest.js
@@ -1,7 +1,7 @@
 const BridgeMapPainter = require('../src/domain/BridgeMapPainter');
 
 describe('🎨 BridgeMapPainter 클래스 테스트', () => {
-  test('⭐ moveCommand와 유저의 위치, 게임 맵을 인자로 받아서 유저가 다음으로 진행할 수 있으면 O를 반환합니다.', () => {
+  test('⭐ selectOXpattern 메서드 : moveCommand와 유저의 위치, 게임 맵을 인자로 받아서 유저가 다음으로 진행할 수 있으면 O를 반환합니다.', () => {
     const bridgeMapPainter = new BridgeMapPainter();
     const moveCommnad = 'U';
     const userLocation = 0;
@@ -11,7 +11,7 @@ describe('🎨 BridgeMapPainter 클래스 테스트', () => {
     expect(result).toBe('O');
   });
 
-  test('⭐ moveCommand와 유저의 위치, 게임 맵을 인자로 받아서 유저가 다음으로 진행할 수 없으면 X를 반환합니다.', () => {
+  test('⭐ selectOXpattern 메서드 : moveCommand와 유저의 위치, 게임 맵을 인자로 받아서 유저가 다음으로 진행할 수 없으면 X를 반환합니다.', () => {
     const bridgeMapPainter = new BridgeMapPainter();
     const moveCommnad = 'D';
     const userLocation = 0;
@@ -19,5 +19,25 @@ describe('🎨 BridgeMapPainter 클래스 테스트', () => {
     const result = bridgeMapPainter.selectOXpattern(moveCommnad, userLocation, gameMap);
 
     expect(result).toBe('X');
+  });
+
+  test('⭐ getUserBridgeMap 메서드 : 현재 유저의 다리 건너기 맵 상태를 문자열로 반환합니다. (초기 빈 상태)', () => {
+    const bridgeMapPainter = new BridgeMapPainter();
+    const result = '[]\n[]\n';
+    expect(bridgeMapPainter.getUserBridgeMap()).toBe(result);
+  });
+
+  test('⭐ getUserBridgeMap 메서드 : 현재 유저의 다리 건너기 맵 상태를 문자열로 반환합니다. (upperBridge에 O가 하나 있을 때)', () => {
+    const bridgeMapPainter = new BridgeMapPainter();
+    bridgeMapPainter.drawOX('U', 0, ['U', 'U', 'D']);
+    const result = '[ O ]\n[   ]\n';
+    expect(bridgeMapPainter.getUserBridgeMap()).toBe(result);
+  });
+
+  test('⭐ getUserBridgeMap 메서드 : 현재 유저의 다리 건너기 맵 상태를 문자열로 반환합니다. (lowerBridge에 X가 하나 있을 때)', () => {
+    const bridgeMapPainter = new BridgeMapPainter();
+    bridgeMapPainter.drawOX('D', 0, ['U', 'U', 'D']);
+    const result = '[   ]\n[ X ]\n';
+    expect(bridgeMapPainter.getUserBridgeMap()).toBe(result);
   });
 });
