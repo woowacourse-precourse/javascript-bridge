bbf3e45ef1de6d3773936c4f7dcc9b38871508c6
yunjjeongjo <two6885@gmail.com>
2022-11-22 20:03:00 +0900

    feat: 이동할 칸 입력 기능 구현

--- src/InputView.js
+++ src/InputView.js
@@ -12,11 +12,12 @@ const InputView = {
   readBridgeSize(bridgeGame) {
     Console.readLine(MESSAGE.INPUT_LENGTH, (userInput) => {
       try {
-        if (!Validate.validateBridgeLength(userInput)) {
+        if (!Validate.validateBridgeLength(userInput))
           throw new Error(ERROR.BIRDGE_LENGTH);
-        }
+        bridgeGame.initGame(userInput);
+        this.readMoving(bridgeGame);
       } catch (e) {
-        Console.print(e);
+        Console.print(e); //테스트 시에도 필요한지?? 필요 없으면 지우기
         this.readBridgeSize(bridgeGame);
       }
     });
@@ -25,7 +26,17 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving(bridgeGame) {
+    Console.readLine(MESSAGE.INPUT_MOVE, (userInput) => {
+      try {
+        if (!Validate.validateMove(userInput))
+          throw new Error(ERROR.MOVE_COMMAND);
+      } catch (e) {
+        Console.print(e); //테스트 시에도 필요한지?? 필요 없으면 지우기
+        this.readMoving(bridgeGame);
+      }
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
