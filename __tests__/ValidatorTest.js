9b665dd454a2a2a06cff4d8352d41a80b5658b49
kangju2000 <kangju2000@naver.com>
2022-11-22 18:22:46 +0900

    test(Validator): Validator 테스트 코드 수정

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -1,34 +1,33 @@
-const ERROR = require('../src/constants/error');
-const Validator = require('../src/Validator');
+const Validator = require('../src/utils/Validator');
 
-describe('예외 처리 테스트', () => {
+describe('Validator 테스트', () => {
   test('다리 길이 입력 시 3~20의 숫자를 입력한 경우 true', () => {
     const bridgeSize = 10;
-    expect(() => Validator.validateBridgeSize(bridgeSize)).toBeTruthy();
+    expect(Validator.isBridgeSize(bridgeSize)).toBeTruthy();
   });
 
-  test('다리 길이 입력 시 3~20의 숫자가 아닌 경우 에러 던지기', () => {
+  test('다리 길이 입력 시 3~20의 숫자가 아닌 경우 false', () => {
     const bridgeSize = 2;
-    expect(() => Validator.validateBridgeSize(bridgeSize)).toThrow(ERROR.BRIDGE_SIZE);
+    expect(Validator.isBridgeSize(bridgeSize)).toBeFalsy();
   });
 
   test("이동할 칸 입력 시 'U' 또는 'D' 중 하나를 입력한 경우 true", () => {
     const moving = 'U';
-    expect(() => Validator.validateMoving(moving)).toBeTruthy();
+    expect(Validator.isMoving(moving)).toBeTruthy();
   });
 
-  test("이동할 칸 입력 시 'U' 또는 'D' 중 하나를 입력하지 않은 경우 에러 던지기", () => {
+  test("이동할 칸 입력 시 'U' 또는 'D' 중 하나를 입력하지 않은 경우 false", () => {
     const moving = 'A';
-    expect(() => Validator.validateMoving(moving)).toThrow(ERROR.MOVING);
+    expect(Validator.isMoving(moving)).toBeFalsy();
   });
 
   test("게임 재시작/종료 여부 입력 시 'R' 또는 'Q' 중 하나를 입력한 경우 true", () => {
     const gameCommand = 'R';
-    expect(() => Validator.validateGameCommand(gameCommand)).toBeTruthy();
+    expect(Validator.isGameCommand(gameCommand)).toBeTruthy();
   });
 
-  test("게임 재시작/종료 여부 입력 시 'R' 또는 'Q' 중 하나를 입력하지 않은 경우 에러 던지기", () => {
+  test("게임 재시작/종료 여부 입력 시 'R' 또는 'Q' 중 하나를 입력하지 않은 경우 false", () => {
     const gameCommand = 'U';
-    expect(() => Validator.validateGameCommand(gameCommand)).toThrow(ERROR.GAME_COMMAND);
+    expect(Validator.isGameCommand(gameCommand)).toBeFalsy();
   });
 });
