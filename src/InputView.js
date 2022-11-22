24aac646d62d9aed0a51561705e193d4e0114885
Sungjae.Kwak <sungjae8970@gmail.com>
2022-11-22 12:40:04 +0900

    feat(InputView, BridgeGame):
    - InputView에서 크기를 입력받고 BridgeGame에서 다리를 생성
    - TODO: 다리 길이 유효성 검사

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,8 @@
+const { Console } = require('@woowacourse/mission-utils');
+const BridgeGame = require('./BridgeGame');
+
+const bridgeGame = new BridgeGame();
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,7 +10,12 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine('다리의 길이를 입력해주세요.\n', (length) => {
+      bridgeGame.setBridge(length);
+      this.readMoving();
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
