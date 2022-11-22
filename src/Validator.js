ad3293392e86c6f1fe3299958f1d65ea24e24ded
BB-choi <choibobae0809@gmail.com>
2022-11-22 21:15:29 +0900

    fix(Validator): isValidNumber 오류 메시지 정정

--- src/Validator.js
+++ src/Validator.js
@@ -26,10 +26,10 @@ class Validator {
 
   isValidNumber = (input) => {
     if (!(Validator.#isNumber(input))) {
-      throw new Error(`${this.ERROR_MESSAGE_HEADER} 숫자(양수)만 입력할 수 있습니다.`);
+      throw new Error(`${this.ERROR_MESSAGE_HEADER} 숫자만 입력할 수 있습니다.`);
     }
     if (!Validator.#isInteger(input)) {
-      throw new Error(`${this.ERROR_MESSAGE_HEADER} 올바른 숫자(정수)를 입력하세요.`);
+      throw new Error(`${this.ERROR_MESSAGE_HEADER} 올바른 숫자(양의 정수)를 입력하세요.`);
     }
 
     return true;
