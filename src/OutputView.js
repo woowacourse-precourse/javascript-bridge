78bcae0c6486842ca231f1a99df222ccc6cdaac8
deli-ght <minjju0660@gmail.com>
2022-11-22 22:40:38 +0900

    feat(printMap): 유저가 선택한 다리 모양 프린트 함수 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -12,7 +12,39 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {},
+  printMap(count, result, answer) {
+    const upBridge = [];
+    const downBridge = [];
+
+    answer.slice(0, count).forEach((element, index) => {
+      if (index === count && !result && element === "U") {
+        downBridge.push("X");
+        upBridge.push(" ");
+        return;
+      }
+
+      if (index === count && !result && element === "D") {
+        upBridge.push("X");
+        downBridge.push(" ");
+        return;
+      }
+
+      if (element === "U") {
+        upBridge.push("O");
+        downBridge.push(" ");
+        return;
+      }
+
+      if (element === "D") {
+        downBridge.push("O");
+        upBridge.push(" ");
+        return;
+      }
+    });
+
+    Console.print(`[${upBridge.join("|")}]`);
+    Console.print(`[${downBridge.join("|")}]`);
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
