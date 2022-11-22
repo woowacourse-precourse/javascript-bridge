77dd43ae4aa5fd8895fb85adf3c38dafbfd1bc6d
JSH99 <tjgus9966@naver.com>
2022-11-22 23:17:01 +0900

    refactor: 출력 형식 수정

--- src/InputView.js
+++ src/InputView.js
@@ -32,7 +32,7 @@ const InputView = {
   readMoving() {
     const validation = new Validation();
     let moving;
-    MissionUtils.Console.readLine("이동할 칸을 선택해주세요. (위: U, 아래: D)", (movingInput) => {
+    MissionUtils.Console.readLine("\n이동할 칸을 선택해주세요. (위: U, 아래: D)", (movingInput) => {
       moving = movingInput;
       validation.checkMovingInputValidation(moving);
     });
@@ -55,7 +55,7 @@ const InputView = {
   readGameCommand() {
     const validation = new Validation();
     let gameCommand; 
-    MissionUtils.Console.readLine("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)", (gameCommandInput) => {
+    MissionUtils.Console.readLine("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)", (gameCommandInput) => {
       gameCommand = gameCommandInput;
       validation.checkRetryInputValidation(gameCommand);
     });
--- src/OutputView.js
+++ src/OutputView.js
@@ -13,7 +13,7 @@ const OutputView = {
     let upBridge = currentBridge[0].join(' | ');
     let downBridge = currentBridge[1].join(' | ');
     MissionUtils.Console.print(`[ ${upBridge} ]`);
-    MissionUtils.Console.print(`[ ${downBridge} ]\n`);
+    MissionUtils.Console.print(`[ ${downBridge} ]`);
   },
 
   /**
@@ -22,7 +22,7 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printResult(currentBridge) {
-    MissionUtils.Console.print('최종 게임 결과');
+    MissionUtils.Console.print('\n최종 게임 결과');
     let upBridge = currentBridge[0].join(' | ');
     let downBridge = currentBridge[1].join(' | ');
     MissionUtils.Console.print(`[ ${upBridge} ]`);
@@ -30,7 +30,7 @@ const OutputView = {
   },
 
   printGameStart() {
-    MissionUtils.Console.print('다리 건너기 게임을 시작합니다.');
+    MissionUtils.Console.print('다리 건너기 게임을 시작합니다.\n');
   },
 
   printInputSize() {
