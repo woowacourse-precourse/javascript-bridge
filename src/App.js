dba768674a4c31e7a6091372d2b77ea4ca0ebc1f
leeseooo <leeseoooo@naver.com>
2022-11-22 13:27:47 +0900

    refactor: App.js -> BridgeGame.js으로 역할 이동

--- src/App.js
+++ src/App.js
@@ -1,7 +1,3 @@
-const { Console } = require("@woowacourse/mission-utils");
-const IOHandler = require("./IOHandler");
-const BridgeMaker = require("./BridgeMaker");
-const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
 const BridgeGame = require("./BridgeGame");
 
 class App {
@@ -9,60 +5,7 @@ class App {
     this.gameManager = new BridgeGame();
   }
   play() {
-    this.start();
-    this.startGame();
-  }
-  start() {
-    Console.print("다리 건너기 게임을 시작합니다.\n");
-  }
-  startGame() {
-    IOHandler.readBridgeSize((size) => {
-      this.gameManager.size = +size;
-      this.makeBridge();
-      this.readMoving();
-    });
-  }
-  makeBridge() {
-    this.gameManager.bridge = BridgeMaker.makeBridge(this.gameManager.size, BridgeRandomNumberGenerator.generate);
-  }
-  readMoving() {
-    IOHandler.readMoving((moving) => {
-      const movingResult = this.gameManager.move(this.gameManager.bridge, this.gameManager.currentPosition, moving);
-      this.gameManager.resultMap.push(this.handleResultMap(moving, movingResult));
-      IOHandler.printMap(this.gameManager.resultMap);
-      this.checkArrival(movingResult);
-    });
-  }
-  checkArrival(result) {
-    if (result && this.gameManager.currentPosition < this.gameManager.size) {
-      this.readMoving();
-      return;
-    }
-    if (this.gameManager.currentPosition === this.gameManager.size) {
-      this.printResult(result);
-      return;
-    }
-    this.readGameCommand(result);
-  }
-  handleResultMap(moving, result) {
-    return {
-      moving,
-      result,
-    };
-  }
-  readGameCommand(result) {
-    IOHandler.readGameCommand((command) => {
-      if (command === "Q") {
-        this.printResult(result);
-        return;
-      }
-      this.gameManager.retry();
-      this.readMoving();
-    });
-  }
-  printResult(result) {
-    IOHandler.printResult(result, this.gameManager.tryCount, this.gameManager.resultMap);
-    Console.close();
+    this.gameManager.start();
   }
 }
 
--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,3 +1,7 @@
+const { Console } = require("@woowacourse/mission-utils");
+const IOHandler = require("./IOHandler");
+const BridgeMaker = require("./BridgeMaker");
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
 class BridgeGame {
   constructor() {
     this.size = 0;
@@ -17,6 +21,59 @@ class BridgeGame {
     this.tryCount += 1;
     this.currentPosition = 0;
     this.resultMap = [];
+  }
+  start() {
+    Console.print("다리 건너기 게임을 시작합니다.\n");
+    this.startGame();
+  }
+  startGame() {
+    IOHandler.readBridgeSize((size) => {
+      this.size = +size;
+      this.makeBridge();
+      this.readMoving();
+    });
+  }
+  makeBridge() {
+    this.bridge = BridgeMaker.makeBridge(this.size, BridgeRandomNumberGenerator.generate);
+  }
+  readMoving() {
+    IOHandler.readMoving((moving) => {
+      const movingResult = this.move(this.bridge, this.currentPosition, moving);
+      this.resultMap.push(this.handleResultMap(moving, movingResult));
+      IOHandler.printMap(this.resultMap);
+      this.checkArrival(movingResult);
+    });
+  }
+  checkArrival(result) {
+    if (result && this.currentPosition < this.size) {
+      this.readMoving();
+      return;
+    }
+    if (this.currentPosition === this.size) {
+      this.printResult(result);
+      return;
+    }
+    this.readGameCommand(result);
+  }
+  handleResultMap(moving, result) {
+    return {
+      moving,
+      result,
+    };
+  }
+  readGameCommand(result) {
+    IOHandler.readGameCommand((command) => {
+      if (command === "Q") {
+        this.printResult(result);
+        return;
+      }
+      this.retry();
+      this.readMoving();
+    });
+  }
+  printResult(result) {
+    IOHandler.printResult(result, this.tryCount, this.resultMap);
+    Console.close();
   }
 }
 
