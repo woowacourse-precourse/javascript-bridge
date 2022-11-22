210397e5c7a38739404da66d049c3e7e4d2613ea
Siby1lA <psb4644@gmail.com>
2022-11-22 17:31:22 +0900

    test: 로직 테스트 추가

--- src/constans/Outputmessage.js
+++ src/constans/Outputmessage.js
@@ -4,10 +4,6 @@ const SUCCESS_OR_FAILURE = "게임 성공 여부: ";
 const SUCCESS_RESULT = "성공";
 const FAILURE_RESULT = "실패";
 const TOTAL_ATTEMPT = "총 시도한 횟수: ";
-const X_MARK = "X";
-const O_MARK = "O";
-const OPEN_BRIDGE = "[ ";
-const CLOSE_OPEN_BRIDGE = " ]";
 
 const OUTPUT_MESSAGE = {
   GAME_START,
@@ -16,10 +12,6 @@ const OUTPUT_MESSAGE = {
   SUCCESS_RESULT,
   FAILURE_RESULT,
   TOTAL_ATTEMPT,
-  X_MARK,
-  O_MARK,
-  OPEN_BRIDGE,
-  CLOSE_OPEN_BRIDGE,
 };
 
 module.exports = OUTPUT_MESSAGE;
