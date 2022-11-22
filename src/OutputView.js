2432bd21f63b5ca6e060201886f5d92cee4426af
rayark1 <max980203@snu.ac.kr>
2022-11-22 15:57:46 +0900

    refactor:(src/OutputView.js): printResult 메소드 상수화

--- src/OutputView.js
+++ src/OutputView.js
@@ -4,6 +4,9 @@ const { Console } = require("@woowacourse/mission-utils");
  */
 const OutputView = {
   MESSAGE_START_BRIDGE_GAME: "다리 건너기 게임을 시작합니다.",
+  RESULT_MESSAGE_HEADER: "최종 게임 결과",
+  RESULT_MESSAGE_IS_SUCCESS: (successOrFailure) => `게임 성공 여부: ${successOrFailure}`,
+  RESULT_MESSAGE_TOTAL_ATTEMPTS: (attempts) => `총 시도한 횟수: ${attempts}`,
   /**
    * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
    * <p>
@@ -17,14 +20,14 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printResult({ topBridge, bottomBridge }, numberOfAttempts, successOrFailure) {
-    Console.print("\n최종 게임 결과");
+    Console.print(`${OutputView.RESULT_MESSAGE_HEADER}`);
     this.printMap({ topBridge, bottomBridge });
-    Console.print(`\n게임 성공 여부: ${successOrFailure}`);
-    Console.print(`총 시도한 횟수: ${numberOfAttempts}`);
+    Console.print(`${OutputView.RESULT_MESSAGE_IS_SUCCESS}`);
+    Console.print(`${OutputView.RESULT_MESSAGE_TOTAL_ATTEMPTS}`);
   },
 
   printStartMessage() {
-    Console.print(MESSAGE_START_BRIDGE_GAME);
+    Console.print(`${OutputView.MESSAGE_START_BRIDGE_GAME}\n`);
   },
 };
 
