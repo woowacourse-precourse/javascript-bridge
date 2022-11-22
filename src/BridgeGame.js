a07e70a8ae0f39dfedf2ca9fdd801b4e929bc3e8
jungsmins1 <jungsmins00@gmail.com>
2022-11-22 22:50:15 +0900

    refactor: 게임을 종료하는 메서드 로직 변경

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -2,37 +2,35 @@ const BridgeMaker = require("./BridgeMaker");
 const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
 const BridgeSet = require("./BridgeSet");
 const { GAME_COMMAND } = require("./Utils")
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
 
-  constructor(input) {
-    this.input = input
-    this.size;
+  constructor() {
+    this.tryCount = 1;
+    this.overBridge = [];
+    this.underBridge = [];
+    this.bridge = []
     this.moving;
     this.command;
-    this.bridge;
   }
 
-  getSize() {
-    this.size = this.input.readBridgeSize()
-    return this.getBridge();
+  getBridge(size) {
+    this.bridge = BridgeMaker.makeBridge(size,
+      BridgeRandomNumberGenerator.generate);
   }
-
-  getMoving() {
-    this.moving = this.input.readMoving();
+  
+  getMoving(moving) {
+    console.log(moving)
+    this.moving = moving
     return this.move();
   }
 
-  getGameCommand() {
-    this.command = this.input.readMoving();
+  getGameCommand(command) {
+    this.command = command;
     return this.gameWheter();
-  }
-
-  getBridge() {
-    this.bridge = BridgeMaker.makeBridge(this.size,
-      BridgeRandomNumberGenerator.generate);
   }
 
   /**
@@ -41,9 +39,6 @@ class BridgeGame {
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   move() {
-    if(this.bridge.length === 0) {
-      return this.end(true);
-    }
     if(this.moving === this.bridge[0]) {
       return this.moveSuccess();
     }
@@ -53,14 +48,18 @@ class BridgeGame {
   }
 
   moveSuccess() {
-    BridgeSet.bridgePass(this.moving);
+    BridgeSet.bridgePass(this.moving, this.overBridge, this.underBridge);
+    BridgeSet.repeat(this.overBridge, this.underBridge);
     this.bridge.shift();
-    return this.getMoving;
+    if(this.bridge.length === 0) {
+      return this.end(true);
+    }
   }
 
   moveFailure() {
-    BridgeSet.bridgeFail(this.moving);
-    return this.getGameCommand;
+    BridgeSet.bridgeFail(this.moving, this.overBridge, this.underBridge);
+    BridgeSet.repeat(this.overBridge, this.underBridge);
+    return this.getGameCommand();
   }
 
   gameWheter() {
@@ -77,18 +76,34 @@ class BridgeGame {
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   retry() {
-    BridgeSet.beforeBridge();
-    this.Count++
-    return BridgeSet.repeat();
+    BridgeSet.beforeBridge(this.overBridge, this.underBridge);
+    BridgeSet.repeat(this.overBridge, this.underBridge);
+
+    this.tyrCount++;
   }
 
   end(result) {
+    BridgeSet.gameResult(this.overBridge, this.underBridge);
     if(result === true) {
-      BridgeSet.successEnd(this.tryCount);
+      return this.successEnd();
     }
     if(result === false) {
-      BridgeSet.failureEnd(this.tryCount);
+      return this.failureEnd();
     }
+  };
+
+  successEnd() {
+    BridgeSet.successGameResult(this.tryCount);
+    this.tryCount = 1;
+  }
+
+  failureEnd() {
+    BridgeSet.failureGameResult(this.tryCount);
+    this.tryCount = 1;
+  }
+
+  reInput() {
+    
   }
 }
 
