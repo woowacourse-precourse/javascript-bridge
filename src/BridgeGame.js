0fb07909d8724f0e40dede49774287d48b0540f1
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-22 18:37:57 +0900

    feat: 다리 생성 메서드

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,3 +1,7 @@
+// BridgeGame 클래스에서 InputView, OutputView 를 사용하지 않는다.
+const BridgeMaker = require('./BridgeMaker');
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
+
 class BridgeGame {
   #bridge;
 
@@ -13,6 +17,14 @@ class BridgeGame {
   move(char) {}
 
   retry(command) {}
+
+  generateBridge(size) {
+    this.#bridge = BridgeMaker.makeBridge(
+      Number(size),
+      BridgeRandomNumberGenerator.generate
+    );
+    return this.#bridge;
+  }
 }
 
 module.exports = BridgeGame;
