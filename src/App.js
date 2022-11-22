31d4ecebd6b313523a657397a5a81eb81696ca96
BaeDongWoo <ehddn5410@naver.com>
2022-11-22 21:43:33 +0900

    feat(App) : 최종 실행기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+const OutputView = require('./OutputView');
+const InputView = require('./InputView');
+
 class App {
-  play() {}
+  play() {
+    OutputView.printStart();
+    this.start();
+  }
+
+  start() {
+    InputView.readBridgeSize();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
