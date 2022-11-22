c8af0e322493fb810dc8cf047b019eb6e7a01e03
pione3r <jhyong0626@gmail.com>
2022-11-22 13:28:12 +0900

    refactor: change field name in BridgeGame class

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -2,35 +2,44 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const InputView = require("./InputView");
 const BridgeMaker = require("./BridgeMaker");
 const { generate } = require("./BridgeRandomNumberGenerator");
-const { printMap } = require("./OutputView");
+const OutputView = require("./OutputView");
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
   #bridgeSize;
-  #bridgeUpside;
-  #bridgeDownside;
+  #bridge;
+  #bridUpside = [];
+  #bridDownside = [];
   #attemptCount = 0;
-  bridgeUpsideForPrint = [];
-  bridgeDownsideForPrint = [];
+  #gameWin = true;
+
+  gameInit() {
+    MissionUtils.Console.print("다리 건너기 게임을 시작합니다.\n");
+
+    this.#bridgeSize = InputView.readBridgeSize();
+    this.#bridge = BridgeMaker.makeBridge(this.#bridgeSize, generate);
+
+    this.gameStart();
+
+    OutputView.printResult(
+      [this.#bridUpside, this.#bridDownside],
+      this.#attemptCount,
+      this.#gameWin
+    );
+  }
 
   gameStart() {
     this.#attemptCount++;
 
-    MissionUtils.Console.print("다리 건너기 게임을 시작합니다.\n");
-
-    this.#bridgeSize = InputView.readBridgeSize();
-
-    [this.#bridgeUpside, this.#bridgeDownside] = BridgeMaker.makeBridge(
-      this.#bridgeSize,
-      generate
-    );
-
     for (let i = 0; i < this.#bridgeSize; i++) {
       this.move(i);
-      printMap(this.bridgeUpsideForPrint);
-      printMap(this.bridgeDownsideForPrint);
+      OutputView.printMap(this.#bridUpside);
+      OutputView.printMap(this.#bridDownside);
+
+      if (this.#bridUpside.includes("X") || this.#bridDownside.includes("X"))
+        this.retry();
     }
   }
   /**
@@ -40,25 +49,29 @@ class BridgeGame {
    */
   move(index) {
     let moving = InputView.readMoving();
-    if (moving === "U") {
-      if (this.#bridgeUpside[index] === "U") {
-        this.bridgeUpsideForPrint.push("O");
-        this.bridgeDownsideForPrint.push(" ");
-      }
-      if (this.#bridgeUpside[index] !== "U") {
-        this.bridgeUpsideForPrint.push("X");
-        this.bridgeDownsideForPrint.push(" ");
-      }
+    if (moving === "U") this.moveUpside(index);
+    if (moving === "D") this.moveDownside(index);
+  }
+
+  moveUpside(index) {
+    if (this.#bridge[index] === "U") {
+      this.#bridUpside.push("O");
+      this.#bridDownside.push(" ");
     }
-    if (moving === "D") {
-      if (this.#bridgeDownside[index] === "D") {
-        this.bridgeDownsideForPrint.push("O");
-        this.bridgeUpsideForPrint.push(" ");
-      }
-      if (this.#bridgeDownside[index] !== "D") {
-        this.bridgeDownsideForPrint.push("X");
-        this.bridgeUpsideForPrint.push(" ");
-      }
+    if (this.#bridge[index] !== "U") {
+      this.#bridUpside.push("X");
+      this.#bridDownside.push(" ");
+    }
+  }
+
+  moveDownside(index) {
+    if (this.#bridge[index] === "D") {
+      this.#bridDownside.push("O");
+      this.#bridUpside.push(" ");
+    }
+    if (this.#bridge[index] !== "D") {
+      this.#bridDownside.push("X");
+      this.#bridUpside.push(" ");
     }
   }
 
@@ -67,7 +80,17 @@ class BridgeGame {
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  retry() {}
+  retry() {
+    const gameCommand = InputView.readGameCommand();
+    if (gameCommand == "R") {
+      this.#bridUpside = [];
+      this.#bridDownside = [];
+      this.gameStart();
+    }
+    if (gameCommand == "Q") {
+      this.#gameWin = false;
+    }
+  }
 }
 
 module.exports = BridgeGame;
