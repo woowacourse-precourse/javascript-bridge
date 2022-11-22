2afb71acd2c74abf264d7ead417e4f9e2ed923e9
Minhye Kang <alsgp9898@naver.com>
2022-11-22 18:44:35 +0900

    refactor(InputHandling): 모듈을 객체의 메서드로 받아오도록 하여 코드 길이 축소

--- src/InputHandling.js
+++ src/InputHandling.js
@@ -2,8 +2,8 @@ const { Console } = require('@woowacourse/mission-utils');
 const { makeBridge } = require('./BridgeMaker');
 const { generate } = require('./BridgeRandomNumberGenerator');
 const { GAME_OUTCOME, GAME_COMMAND } = require('./Constants');
-const InputView = require('./InputView');
-const Validation = require('./Validation');
+const { readBridgeSize, readMoving, readGameCommand } = require('./InputView');
+const { checkBridgeSize, checkMovingValue, checkRestartOrDone } = require('./Validation');
 const BridgeGame = require('./BridgeGame');
 
 class InputHandling {
@@ -11,59 +11,56 @@ class InputHandling {
   #bridgeGame;
 
   play() {
-    InputView.readBridgeSize(this.handleBridgeSize.bind(this));
+    readBridgeSize(this.handleBridgeSize.bind(this));
   }
 
   handleBridgeSize(size) {
     try {
-      Validation.checkBridgeSize(size);
+      checkBridgeSize(size);
       this.generateAnswerBridge(size);
     } catch (error) {
       Console.print(error);
-      InputView.readBridgeSize(this.handleBridgeSize.bind(this));
+      readBridgeSize(this.handleBridgeSize.bind(this));
     }
   }
 
   generateAnswerBridge(size) {
     this.#answerBridgeArray = makeBridge(size, generate);
     this.#bridgeGame = new BridgeGame(this.#answerBridgeArray);
-    InputView.readMoving(this.handleMovingValue.bind(this));
+    readMoving(this.handleMovingValue.bind(this));
   }
 
   handleMovingValue(direction) {
     try {
-      Validation.checkMovingValue(direction);
+      checkMovingValue(direction);
       this.decideNextConsolePrint(direction);
     } catch (error) {
       Console.print(error);
-      InputView.readMoving(this.handleMovingValue.bind(this));
+      readMoving(this.handleMovingValue.bind(this));
     }
   }
 
   decideNextConsolePrint(direction) {
     const gameOutcome = this.#bridgeGame.decideMoveOrStop(direction);
-    if (gameOutcome === GAME_OUTCOME.FAIL)
-      InputView.readGameCommand(this.handleGameCommand.bind(this));
-    if (gameOutcome === GAME_OUTCOME.FINAL_SUCCESS) 
-      Console.close();
-    if (gameOutcome === GAME_OUTCOME.SUCCESS)
-      InputView.readMoving(this.handleMovingValue.bind(this));
+    if (gameOutcome === GAME_OUTCOME.FAIL) readGameCommand(this.handleGameCommand.bind(this));
+    if (gameOutcome === GAME_OUTCOME.FINAL_SUCCESS) Console.close();
+    if (gameOutcome === GAME_OUTCOME.SUCCESS) readMoving(this.handleMovingValue.bind(this));
   }
 
   handleGameCommand(command) {
     try {
-      Validation.checkRestartOrDone(command);
+      checkRestartOrDone(command);
       this.decideRetryOrDone(command);
     } catch (err) {
       Console.print(err);
-      InputView.readGameCommand(this.handleGameCommand.bind(this));
+      readGameCommand(this.handleGameCommand.bind(this));
     }
   }
 
   decideRetryOrDone(command) {
     if (command === GAME_COMMAND.RESTART) {
       this.#bridgeGame.retry();
-      InputView.readMoving(this.handleMovingValue.bind(this));
+      readMoving(this.handleMovingValue.bind(this));
     }
     if (command === GAME_COMMAND.QUIT) {
       this.#bridgeGame.quit();
