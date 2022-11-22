592d733efcdc5ea6137070002ad0df38d29d6d98
rayark1 <max980203@snu.ac.kr>
2022-11-22 16:44:05 +0900

    feat(src/Validator.js): BridgeSize를 검증하는 validateBridgeSize() 메소드 추가

--- src/Validator.js
+++ src/Validator.js
@@ -1,17 +1,26 @@
 const Validator = {
   ERROR_MESSEAGE_MUST_IN_RANGE:
-    '[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.',
-  ERROR_MESSEAGE_MUST_BE_NUMBER:
-    '[ERROR] 다리 길이는 숫자여야 합니다.',
+    "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.",
+  ERROR_MESSEAGE_MUST_BE_NUMBER: "[ERROR] 다리 길이는 숫자여야 합니다.",
 
-  validateBridgeSize(bridgeSize) {
-    if (bridgeSize >= 3 || bridgeSize <= 20) {
-      throw new Error(Validator.ERROR_MESSEAGE_MUST_IN_RANGE);
-    }
-    if (typeof bridgeSize !== 'number') {
+  validateBridgeSize(bridgeSizeString) {
+    Validator.validateInputisNumber(bridgeSizeString);
+    const bridgeSize = Number(bridgeSizeString);
+    Validator.validateInputisInRange(bridgeSize);
+  },
+
+  validateInputisNumber(bridgeSizeString) {
+    if (isNaN(bridgeSizeString)) {
       throw new Error(Validator.ERROR_MESSEAGE_MUST_BE_NUMBER);
     }
   },
+
+  validateInputisInRange(bridgeSize) {
+    if (bridgeSize < 3 || bridgeSize > 20) {
+      throw new Error(Validator.ERROR_MESSEAGE_MUST_IN_RANGE);
+    }
+  },
+
 };
 
 module.exports = Validator;
