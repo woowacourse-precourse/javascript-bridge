fe2ed42c06dfb4d4e43da3319fc13e1ed1a325eb
marugy <gyulife7301@naver.com>
2022-11-22 22:55:22 +0900

    test: InputView validateSize() 문자 테스트 추가

--- __tests__/InputViewTest.js
+++ __tests__/InputViewTest.js
@@ -1,1 +1,20 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const App = require("../src/App");
+const InputView = require("../src/InputView");
 
+const mockQuestions = (answers) => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce((acc, input) => {
+    return acc.mockImplementationOnce((question, callback) => {
+      callback(input);
+    });
+  }, MissionUtils.Console.readLine);
+};
+
+describe("InputView 테스트", () => {
+  test("다리 길이로 문자가 들어가면 예외가 발생한다", () => {
+    expect(() => {
+      InputView.validateSize("A");
+    }).toThrow("[ERROR]");
+  });
+});
