9984ca4be3254e3440f8926f8cf2c56dbc9899e0
Sungjae.Kwak <sungjae8970@gmail.com>
2022-11-22 14:07:22 +0900

    feat(BridgeGame, InputView, OutputView): 종료 로직 추가
    -(BridgeGame_retry) 트라이카운트 더해주고, 무빙로그 초기화
    -(BridgeGame_isWin) 승리출력을 위한 메소드 추가
    -(BridgeGame_getter) 상태출력을 위한 게터 세팅
    -(InputView) BridgeGame 상태에 따른 유저의 다음 행동 분기처리
    -TODO: 라운드의 결과 출력 및 command validate

--- src/InputView.js
+++ src/InputView.js
@@ -1,5 +1,6 @@
 const { Console } = require('@woowacourse/mission-utils');
 const BridgeGame = require('./BridgeGame');
+const OutputView = require('./OutputView');
 
 const bridgeGame = new BridgeGame();
 
@@ -23,13 +24,31 @@ const InputView = {
   readMoving() {
     Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)\n', (moving) => {
       bridgeGame.move(moving);
+      // TODO: 라운드의 결과 프린트 출력
+      if (bridgeGame.isWin()) {
+        OutputView.printResult(bridgeGame.isWin(), bridgeGame.tryCount);
+      } else if (bridgeGame.isPlaying) {
+        this.readMoving();
+      } else {
+        this.readGameCommand();
+      }
     });
   },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+    Console.readLine('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n', (command) => {
+      // TODO: validateCommand
+      if (command === 'R') {
+        bridgeGame.retry();
+        this.readMoving();
+      } else if (command === 'Q') {
+        OutputView.printResult(bridgeGame.isWin(), bridgeGame.tryCount);
+      }
+    });
+  },
 };
 
 module.exports = InputView;
--- src/OutputView.js
+++ src/OutputView.js
@@ -18,7 +18,11 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() {},
+  printResult(boolOfSuccess, tryCnt) {
+    Console.print(`게임 성공 여부: ${boolOfSuccess ? '성공' : '실패'}`);
+    Console.print(`총 시도한 횟수: ${tryCnt}`);
+    Console.close();
+  },
 };
 
 module.exports = OutputView;
