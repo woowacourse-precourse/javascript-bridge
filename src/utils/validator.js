aebe06d75a3d135d5aae40a818aaa83f738d91c5
nalsae <nalsae0207@naver.com>
2022-11-22 10:32:07 +0900

    refactor: Apply constants
    생성한 상수 적용

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,24 +1,31 @@
+const { SIZE, DIRECTION, INPUT } = require("./constants");
 const { ERROR_MESSAGES } = require("./messages");
 const { WRONG_BRIDGE_SIZE, WRONG_MOVEMENT, WRONG_WHETHER_TO_RETRY } =
   ERROR_MESSAGES;
 
 const validator = {
   bridgeSize(size) {
+    const { MIN, MAX } = SIZE;
+
     if (
       typeof +size !== "number" ||
       Number.isNaN(+size) ||
-      +size < 3 ||
-      +size > 20
+      +size < MIN ||
+      +size > MAX
     )
       throw new Error(WRONG_BRIDGE_SIZE);
   },
 
   moving(movement) {
-    if (movement !== "U" && movement !== "D") throw new Error(WRONG_MOVEMENT);
+    const { UP, DOWN } = DIRECTION;
+
+    if (movement !== UP && movement !== DOWN) throw new Error(WRONG_MOVEMENT);
   },
 
   retry(whetherToRetry) {
-    if (whetherToRetry !== "R" && whetherToRetry !== "Q")
+    const { RETRY, END } = INPUT;
+
+    if (whetherToRetry !== RETRY && whetherToRetry !== END)
       throw new Error(WRONG_WHETHER_TO_RETRY);
   },
 };
