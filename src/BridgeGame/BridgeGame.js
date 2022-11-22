2ce70234191a4f3cd9d35c8b64b1df1f94219d69
Minhye Kang <alsgp9898@naver.com>
2022-11-22 22:10:43 +0900

    refactor(BridgeGame): mapMaker 인스턴스를 private으로 제한

--- src/BridgeGame/BridgeGame.js
+++ src/BridgeGame/BridgeGame.js
@@ -5,9 +5,10 @@ class BridgeGame {
   #answerBridgeArray;
   #bridgeIndex;
   #attemptNumber;
+  #mapMaker;
 
   constructor(answerBridgeArray) {
-    this.mapMaker = new MapMaker();
+    this.#mapMaker = new MapMaker();
     this.#answerBridgeArray = answerBridgeArray;
     this.#bridgeIndex = 1;
     this.#attemptNumber = 1;
@@ -17,7 +18,7 @@ class BridgeGame {
     if (direction !== this.#answerBridgeArray[this.#bridgeIndex - 1]) 
       return this.stop(direction);
     if (this.#bridgeIndex === this.#answerBridgeArray.length) {
-      this.mapMaker.makeFinalSuccess(direction, this.#attemptNumber);
+      this.#mapMaker.makeFinalSuccess(direction, this.#attemptNumber);
       return GAME_OUTCOME.FINAL_SUCCESS;
     }
     return this.move(direction);
@@ -25,23 +26,23 @@ class BridgeGame {
 
   move(direction) {
     this.#bridgeIndex += 1;
-    this.mapMaker.selectRightBridge(direction);
+    this.#mapMaker.selectRightBridge(direction);
     return GAME_OUTCOME.SUCCESS;
   }
 
   stop(direction) {
-    this.mapMaker.selectWrongBridge(direction);
+    this.#mapMaker.selectWrongBridge(direction);
     return GAME_OUTCOME.FAIL;
   }
 
   retry() {
       this.#bridgeIndex = 1;
       this.#attemptNumber += 1;
-      this.mapMaker = new MapMaker();
+      this.#mapMaker = new MapMaker();
   }
 
   quit() {
-    this.mapMaker.selectGameOver(this.#attemptNumber);
+    this.#mapMaker.selectGameOver(this.#attemptNumber);
   }
 }
 
