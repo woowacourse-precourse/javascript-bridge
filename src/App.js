871576aab705f0d353ade9352254310f345d5fd4
jeno8522 <68889383+jeno8522@users.noreply.github.com>
2022-11-22 22:14:10 +0900

    OutputView.printStartMessage
    게임 시작 메시지를 출력해주는 메소드

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const InputView = require("./view/InputView");
+const OutputView = require("./view/OutputView");
+
 class App {
-  play() {}
+  play() {
+    this.#startGame();
+  }
+
+  #startGame() {
+    OutputView.printStartMessage();
+
+    InputView.readBridgeSize();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
