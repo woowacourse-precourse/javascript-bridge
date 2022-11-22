ed5044ec19206ade5e34bced2421a30be54d9f52
Sallyvely <tpdus4159@naver.com>
2022-11-22 13:14:55 +0900

    style : 상수 변수 추가

---
+++ src/message.js
@@ -0,0 +1,16 @@
+const MESSAGE = {
+
+    bridge: {
+        bridgeStart : '[ ',
+        bridgeEnd : ' ]',
+        bridgeOkay : 'O',
+        bridgeNo : 'X',
+        bridgeAgain : ' | ',
+        bridgeNone : ' ',
+    },
+    gameScore: {
+        success : '성공',
+        fail : '실패',
+    }
+}
+module.exports = MESSAGE;
