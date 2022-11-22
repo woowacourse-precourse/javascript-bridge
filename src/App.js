5bb36c93029b61ac1bcbbaac516f233c41d09ab5
MIN SIK CHOI <minstones34@gmail.com>
2022-11-22 21:09:07 +0900

    feat: 유저에게 다리길이를 입력받는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
+const BridgeMaker = require("./BridgeMaker");
+const BridgeGame = require("./BridgeGame");
+const BridgeRandomNumberGenerator = require("./BridgeRandomNumberGenerator");
+const InputView = require("./InputView");
+const OutputView = require("./OutputView");
+const Constants = require("./Constants");
+
 class App {
-  play() {}
+  play() {
+    OutputView.printStartMessage();
+    this.startGame(judgeTryInput(InputView.readBridgeSize()));
+  }
+
+  startGame() {}
+
+  static #tryInput(readBridgeSize) {
+    try {
+      return readBridgeSize();
+    } catch (error) {
+      OutputView.printErrorMessage(error);
+      return false;
+    }
+  }
+
+  judgeTryInput(userInputValue) {
+    let judgeValue = false;
+
+    do {
+      judgeValue = App.#tryInput(userInputValue);
+    } while (judgeValue !== false);
+
+    return judgeValue;
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
--- src/InputView.js
+++ src/InputView.js
@@ -1,11 +1,27 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Constants = require("./Constants");
+const Validator = require("./Validator");
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
+  getBridgeLength(constants) {
+    let bridgeLength;
+    MissionUtils.Console.readLine(constants, (userinput) => {
+      bridgeLength = userinput;
+    });
+    return bridgeLength;
+  },
+
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    let userInput = InputView.getBridgeLength(Constants.ASK_BRIDGE_LENGTH);
+    Validator.validbridgeLength(userInput);
+    return userInput;
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
