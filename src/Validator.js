9e27d35e31a021671a960d57ba413179afe5af7f
seoyeon <wjstjdus96@naver.com>
2022-11-22 17:01:52 +0900

    fix: 유효성 검증 기능 수정

--- src/Validator.js
+++ src/Validator.js
@@ -1,3 +1,4 @@
+const { Console } = require("@woowacourse/mission-utils");
 const ERROR_IS_NUMBER_VALID = "[ERROR] 숫자만 입력해야 합니다.";
 const ERROR_IS_RANGE_VALID =
   "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
@@ -5,14 +6,12 @@ const ERROR_IS_MOVING_COMMAND = "[ERROR] U 이나 D만 입력해야 합니다.";
 const ERROR_IS_GAME_COMMAND = "[ERROR] R 이나 Q만 입력해야 합니다.";
 
 const isNumberValid = (num) => {
-  if (Number.isNaN(num)) {
-    console.print(ERROR_IS_NUMBER_VALID);
+  if (isNaN(num)) {
     throw new Error(ERROR_IS_NUMBER_VALID);
   }
 };
 const isRangeValid = (num) => {
   if (num < 3 || num > 20) {
-    OutputView.print(ERROR_IS_RANGE_VALID);
     throw new Error(ERROR_IS_RANGE_VALID);
   }
 };
