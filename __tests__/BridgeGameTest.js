b6da1fa86c7acceef993aa5f4f251ac2a9f451f0
daal2quf <daal2quf@gmail.com>
2022-11-22 19:05:30 +0900

    refactor(bridgeGameTest): movelogs의 map 그리는 메서드 변경으로 테스트케이스 리팩토링
    change:
    BridgeGame 클래스의 유저가 움직인 다리 맵 만드는 기능 테스트
    test1: makeUserMoveMap() 로 생성된 map에 'O', 'X'가 제대로 반환되는지 확인
    test2: makeUserMoveMap() 로 생성된 map에 잘못된 값 반환했다고 가정했을때 같지않아 실패하는지 확인
    test3: checkingUserMoveLog(): makeUserMoveMap()메서드 안에 checkedUserMap의 변수를 인자로 넘겼을때
    map이 정상출력되는지 확인
    test4: makeUserMoveMap()메서드 안에 checkedUserMap의 변수를 인자로 넘겼을때 생성되는 map이
    잘못된 값이라 가정했을때 실패하는지 확인

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -16,43 +16,70 @@ describe('BridgeGame 클래스의 move() 메서드 테스트', () => {
 });
 
 describe('BridgeGame 클래스의 유저가 움직인 다리 맵 만드는 기능 테스트', () => {
-  test('userMoveMap() 메서드가 정상적으로 다리 만들기 위한 준비가 되는지 확인', () => {
+  test('makeUserMoveMap() 유저가 입력한 movelogs에 따라 map을 만드는지 테스트', () => {
     const bridgeGame = new BridgeGame();
-    const userMoveLogs = ['U', 'D', 'U'];
+    const moveLogs = ['U', 'D', 'U'];
     const bridge = ['U', 'D', 'D'];
     const result = [
-      [' O ', '   ', ' X '],
-      ['   ', ' O ', '   '],
+      ['O', ' ', 'X'],
+      [' ', 'O', ' '],
     ];
-    expect(bridgeGame.userMoveMap(userMoveLogs, bridge)).toEqual(result);
+    expect(bridgeGame.makeUserMoveMap(moveLogs, bridge)).toEqual(result);
   });
-  test.failing('userMoveMap() 메서드가 정상적으로 다리 만들기 위한 준비가 실패인지 확인', () => {
+  test.failing('makeUserMoveMap() 에 반대되는 결과값을 비교할시에 실패하는지 테스트', () => {
     const bridgeGame = new BridgeGame();
-    const userMoveLogs = ['U', 'D', 'U'];
+    const moveLogs = ['U', 'D', 'U'];
     const bridge = ['U', 'D', 'D'];
     const result = [
-      [' O ', '   ', ' O '],
-      ['   ', ' O ', '   '],
+      ['O', ' ', ' '],
+      [' ', 'O', 'X'],
     ];
-    expect(bridgeGame.userMoveMap(userMoveLogs, bridge)).toEqual(result);
+    expect(bridgeGame.makeUserMoveMap(moveLogs, bridge)).toEqual(result);
   });
+  test('checkingUserMoveLogs()에 O X 검사한 배열을 넘길때 제대로된 map으로 변환되는지 확인', () => {
+    const bridgeGame = new BridgeGame();
+    const checkedMap = [
+      ['U', 'O'],
+      ['D', 'O'],
+      ['U', 'X'],
+    ];
+    const result = [
+      ['O', ' ', 'X'],
+      [' ', 'O', ' '],
+    ];
+    expect(bridgeGame.checkingUserMoveLogs(checkedMap)).toEqual(result);
+  });
+  test.failing(
+    'checkingUserMoveLogs()에 O X 검사한 배열을 argument로 넘기고 잘못된 값이 나오면 실패 확인',
+    () => {
+      const bridgeGame = new BridgeGame();
+      const checkedMap = [
+        ['U', 'O'],
+        ['D', 'O'],
+        ['U', 'X'],
+      ];
+      const result = [
+        ['O', ' ', 'O'],
+        [' ', 'O', ' '],
+      ];
+      expect(bridgeGame.checkingUserMoveLogs(checkedMap)).toEqual(result);
+    }
+  );
   test('bridgeToString() 메서드가 원하는 모양의 string으로 변환되는지 확인', () => {
     const bridgeGame = new BridgeGame();
-    const notStringBridge = [
-      [' O ', '   ', ' X '],
-      ['   ', ' O ', '   '],
-    ];
-    const convertedBridge = ['[ O |   | X ]', '[   | O |   ]'];
-    expect(bridgeGame.bridgeToString(notStringBridge)).toEqual(convertedBridge);
+    const moveLogs = ['U', 'D', 'U'];
+    const bridge = ['U', 'D', 'D'];
+    const map = bridgeGame.makeUserMoveMap(moveLogs, bridge);
+    const result = ['[ O |   | X ]', '[   | O |   ]'];
+    expect(bridgeGame.bridgeToString(map)).toEqual(result);
   });
   test.failing('bridgeToString() 메서드가 잘못된 모양으로 변환되어 실패하는지 확인', () => {
     const bridgeGame = new BridgeGame();
-    const notStringBridge = [
-      [' O ', '   ', ' X '],
-      ['   ', ' O ', '  '],
-    ];
-    const convertedBridge = ['[ O       X ]', '[     O      ]'];
-    expect(bridgeGame.bridgeToString(notStringBridge)).toEqual(convertedBridge);
+    const moveLogs = ['U', 'D', 'U'];
+    const bridge = ['U', 'D', 'D'];
+    const map = bridgeGame.makeUserMoveMap(moveLogs, bridge);
+    const result = ['[ O |   | X ]', '[   | X |   ]'];
+    expect(bridgeGame.bridgeToString(map)).toEqual(result);
   });
 });
 describe('BridgeGame클래스의 retry() 기능 테스트', () => {
@@ -63,7 +90,7 @@ describe('BridgeGame클래스의 retry() 기능 테스트', () => {
     const moveLogs = bridgeGame.moveLogs;
     expect(moveLogs).toEqual([]);
   });
-  test.failing('retry()를 하지않으면 moveLogs가 쌓여 초기화되지 않는지 확인하는 실패테스트', () => {
+  test.failing('retry()를 하지않으면 moveLogs가 쌓여 초기화되지 않는지 확인 실패테스트', () => {
     const bridgeGame = new BridgeGame();
     bridgeGame.move('U');
     const moveLogs = bridgeGame.moveLogs;
@@ -81,4 +108,8 @@ describe('BridgeGame클래스의 retry() 기능 테스트', () => {
     const result = bridgeGame.tryCount;
     expect(result).toEqual(1);
   });
+});
+
+describe('', () => {
+  test('', () => {});
 });
