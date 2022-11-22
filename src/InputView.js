e35ecfd9fbd225e34dcb0631b37fac474db28a74
minseok1109 <park485201@naver.com>
2022-11-22 18:09:17 +0900

    refactor: InputView.js 함수 10줄 안넘게 분리

--- src/InputView.js
+++ src/InputView.js
@@ -16,18 +16,22 @@ const InputView = {
   readBridgeSize() {
     Console.readLine(Constant.INPUT.BRIDGE_SIZE, (bridgeSize) => {
       try {
-        let numberBridgeSize = Number(bridgeSize) ?? NaN;
-        Validate.validateBridgeSize(numberBridgeSize);
-        this.bridge = BridgeMaker.makeBridge(
-          numberBridgeSize,
-          randomNumberGenerator.generate
-        );
-        this.readMoving();
+        this.startReadBridge(bridgeSize);
       } catch (e) {
         Console.print(e);
         this.readBridgeSize();
       }
     });
+  },
+
+  startReadBridge(bridgeSize) {
+    let numberBridgeSize = Number(bridgeSize) ?? NaN;
+    Validate.validateBridgeSize(numberBridgeSize);
+    this.bridge = BridgeMaker.makeBridge(
+      numberBridgeSize,
+      randomNumberGenerator.generate
+    );
+    InputView.readMoving();
   },
 
   /**
@@ -36,35 +40,50 @@ const InputView = {
   readMoving() {
     Console.readLine(Constant.INPUT.NEXT_STEP, (inputUpOrDown) => {
       try {
-        Validate.validateUserInputMove(inputUpOrDown);
-        this.bridgeGame = new BridgeGame();
-        const bridgeLength = this.bridge.length;
-        //게임 끝났을 때
-        let result = this.bridgeGame.move(inputUpOrDown, this.bridge);
-        if (BridgeGame._userInputCount === this.bridge.length) {
-          if (!result) {
-            OutputView.printMap(result, inputUpOrDown, bridgeLength);
-            this.readGameCommand();
-            return;
-          }
-          OutputView.printMap(result, inputUpOrDown, bridgeLength);
-          OutputView.printResult(result, BridgeGame._gameCount);
-          return;
-        }
-        //맞췄을 때
-        if (result) {
-          OutputView.printMap(result, inputUpOrDown, bridgeLength);
-          this.readMoving();
-          return;
-        }
-        //틀렸을 때
-        OutputView.printMap(result, inputUpOrDown, bridgeLength);
-        this.readGameCommand();
+        this.startMoving(inputUpOrDown);
       } catch (e) {
         Console.print(e);
         this.readMoving();
       }
     });
+  },
+
+  startMoving(inputUpOrDown) {
+    Validate.validateUserInputMove(inputUpOrDown);
+    this.bridgeGame = new BridgeGame();
+    let result = this.bridgeGame.move(inputUpOrDown, this.bridge);
+    //맞췄을 때
+    if (result && BridgeGame._userInputCount !== this.bridge.length) {
+      this.correctMove(result, inputUpOrDown);
+    }
+    //틀렸을 때
+    else if (!result && BridgeGame._userInputCount !== this.bridge.length) {
+      this.failMove(result, inputUpOrDown);
+    } else {
+      //게임 끝났을 때
+      this.gameFinish(result, inputUpOrDown);
+    }
+  },
+
+  gameFinish(result, inputUpOrDown) {
+    if (!result) {
+      OutputView.printMap(result, inputUpOrDown, this.bridge.length);
+      this.readGameCommand();
+      return;
+    }
+    OutputView.printMap(result, inputUpOrDown, this.bridge.length);
+    OutputView.printResult(result, BridgeGame._gameCount);
+    Console.close();
+  },
+
+  correctMove(result, upOrDown) {
+    OutputView.printMap(result, upOrDown, this.bridge.length);
+    this.readMoving();
+  },
+
+  failMove(result, upOrDown) {
+    OutputView.printMap(result, upOrDown, this.bridge.length);
+    this.readGameCommand();
   },
 
   /**
@@ -73,24 +92,25 @@ const InputView = {
   readGameCommand() {
     Console.readLine(Constant.INPUT.GAME_RETRY, (userInputRetry) => {
       try {
-        Validate.validateUserInputRetry(userInputRetry);
-        //다시 시작
-        if (userInputRetry === Constant.RETRY) {
-          this.bridgeGame.retry();
-          InputView.readMoving();
-          return;
-        }
-        //그만두기
-        if (userInputRetry === Constant.QUIT) {
-          OutputView.printResult("", BridgeGame._gameCount);
-          Console.close();
-        }
-        // OutputView.printResult("", BridgeGame._gameCount);
+        this.reStartGameCommand(userInputRetry);
       } catch (e) {
         Console.print(e);
         this.readGameCommand();
       }
     });
+  },
+
+  reStartGameCommand(userInputRetry) {
+    Validate.validateUserInputRetry(userInputRetry);
+    //다시 시작
+    if (userInputRetry === Constant.RETRY) {
+      this.bridgeGame.retry();
+      InputView.readMoving();
+    } else {
+      //그만두기
+      OutputView.printResult("", BridgeGame._gameCount);
+      Console.close();
+    }
   },
 };
 
