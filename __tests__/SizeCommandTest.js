f1eed5eb278ece9a14dbf8479faad20e11044f87
Leejin-Yang <ejin1996@gmail.com>
2022-11-22 15:46:06 +0900

    test: 커맨드 별 입력 유효성 검증 테스트 작성

--- __tests__/SizeCommandTest.js
+++ __tests__/SizeCommandTest.js
@@ -1,13 +1,27 @@
 const { SizeCommand } = require('../src/models/command');
 
 describe('사이즈 커맨드 클래스 테스트', () => {
-  test.each([[''], [' 1'], ['a']])('다리 길이 값이 숫자가 아닌 경우 예외 처리', (size) => {
+  test('빈 값 유효성 검증', () => {
+    const command = '';
+
     expect(() => {
-      new SizeCommand(size);
-    }).toThrow('[ERROR]');
+      new SizeCommand(command);
+    }).toThrow('[ERROR] 빈 값을 입력하였습니다.');
   });
 
-  test.each([['2'], ['-1'], ['21']])('다리 길이 값이 3미만 20초과인 경우 예외 처리', (size) => {
+  test.each([[' 3'], ['20 ']])('공백 포함 유효성 검증', (command) => {
+    expect(() => {
+      new SizeCommand(command);
+    }).toThrow('[ERROR] 공백을 포함해 입력하였습니다.');
+  });
+
+  test.each([['a'], ['1 7'], ['1e3']])('다리 길이 값이 숫자가 아닌 경우 예외 처리', (size) => {
+    expect(() => {
+      new SizeCommand(size);
+    }).toThrow('[ERROR] 입력 값은 숫자여야 합니다.');
+  });
+
+  test.each([['2'], ['21']])('다리 길이 값이 3미만 20초과인 경우 예외 처리', (size) => {
     expect(() => {
       new SizeCommand(size);
     }).toThrow('[ERROR] 다리의 길이는 3이상 20이하의 숫자여야 합니다.');
