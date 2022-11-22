1733891c337d290c6f1ba2a8328d379c23a3c946
kitez <duswl339@ajou.ac.kr>
2022-11-22 09:38:25 +0900

    test: Validation test code

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,6 +1,7 @@
 const input = require('./View/InputView');
 const output = require('./View/OutputView');
-const Controller = require("./Controller");
+const Controller = require("./BridgeGameContoller");
+const Const = require('./constant/constant');
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
@@ -13,13 +14,13 @@ class BridgeGame {
   #gameResult; // 게임 결과 ( true or false )
   #controller;
 
-  constructor(bridge) {
-    this.#bridge = bridge;
-    this.#current = 0;
+  constructor() {
+    this.#controller = new Controller();
+    this.#bridge = this.#controller.getGame(Const.KEY.BRIDGE);
+    this.#current = this.#controller.getGame(Const.KEY.CURRENT);
     this.#totalTry = 0;
     this.#lastInput = '';
     this.#gameResult = false;
-    this.#controller = new Controller();
   }
 
   /**
@@ -28,14 +29,16 @@ class BridgeGame {
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   move() {
-    try{
-      this.#lastInput = input.readMoving();
-      output.printMap(this.#bridge, this.#current + 1, this.#lastInput);
-      this.#current += 1;
-    }
-    catch(e){
-      this.move();
-    }
+    this.#controller.move();
+    // try{
+    //   this.#controller.move();
+    //   // this.#lastInput = input.readMoving();
+    //   // output.printMap(this.#bridge, this.#current + 1, this.#lastInput);
+    //   // this.#current += 1;
+    // }
+    // catch(e){
+    //   this.move();
+    // }
   }
 
 
@@ -45,18 +48,12 @@ class BridgeGame {
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   retry() {
-    try{
-      return input.readGameCommand();
-    }
-    catch(e){
-      output.printMessage(e);
-      this.retry();
-    }
+      return this.#controller.retry();
   }
 
   start(){
-    while(this.#bridge.length >= this.#current){
-      if(this.#bridge.length === this.#current){
+    while(this.#controller.getGame(Const.KEY.BRIDGE).length >= this.#controller.getGame(Const.KEY.CURRENT)){
+      if(this.#controller.getGame(Const.KEY.BRIDGE).length === this.#controller.getGame(Const.KEY.CURRENT)){
         if(this.#bridge[this.#bridge.length-1] === this.#lastInput) break;
         if(this.retry() === "Q") break;
         this.reset();
@@ -68,7 +65,7 @@ class BridgeGame {
 
   end(){
     this.#totalTry ++;
-    output.printResult(this.#bridge, this.#lastInput, this.#bridge[this.#bridge.length-1] === this.#lastInput, this.#totalTry);
+    this.#controller.end();
   }
 
   reset(){
---
+++ src/BridgeGameContoller.js
@@ -0,0 +1,75 @@
+const maker = require("./BridgeMaker");
+const generate = require("./BridgeRandomNumberGenerator");
+const input = require("./View/InputView");
+const output = require("./View/OutputView");
+const Const = require('../src/constant/constant');
+
+class BridgeGameContoller {
+    #game;
+
+    constructor() {
+        this.#game = {
+            current:0,
+        };
+    }
+
+    init(){
+        this.#game[Const.KEY.BRIDGE] = maker.makeBridge(this.#initBridgeLength(),generate.generate);
+    }
+
+    move(){
+        let lastInput = null;
+        try{
+            lastInput = input.readMoving();
+        }
+        catch(e){
+            this.move();
+        }
+        output.printMap(this.#game[Const.KEY.BRIDGE], this.#game[Const.KEY.CURRENT] + 1, lastInput);
+        this.setGame(Const.KEY.CURRENT, this.#game[Const.KEY.CURRENT]++);
+    }
+
+    retry(){
+        try{
+            return input.readGameCommand();
+        }
+        catch(e){
+            output.printMessage(e);
+            this.retry();
+        }
+    }
+
+    end(){
+        // output.printResult(this.#game[Const.KEY.BRIDGE], this.#lastInput, this.#bridge[this.#bridge.length-1] === this.#lastInput, this.#totalTry);
+    }
+
+    /**
+     * 다리 길이 입력 받는 함수
+     * 실제로 library를 이용해서 입력받는 함수는 InputView에 따로 존재하나, try{}catch(e){} error 처리 후
+     * @returns {number}
+     */
+    #initBridgeLength(){
+        try{
+            return input.readBridgeSize();
+        }
+        catch(e){
+            output.printMessage(e);
+            this.#initBridgeLength();
+        }
+    }
+
+    bind(value){
+        const keyValue = Object.entries(value);
+        this.setGame(keyValue[0][0],keyValue[0][1]);
+    }
+
+    setGame(key, value){
+        this.#game[key] = value;
+    }
+
+    getGame(key){
+        return this.#game[key];
+    }
+}
+
+module.exports = BridgeGameContoller;
