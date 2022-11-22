73d096bd7dc7ca5d1be822d8b207e61ca4723aa8
JiYoon Lee <wldbseja@daum.net>
2022-11-22 18:28:15 +0900

    fix (InputView) : readMoving 줄 바꿈 추가

--- src/InputView.js
+++ src/InputView.js
@@ -15,7 +15,10 @@ const InputView = {
    * 사용자가 이동할 칸을 입력받는다.
    */
   readMoving(callback) {
-    Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)\n', callback);
+    Console.readLine(
+      '\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
+      callback
+    );
   },
 
   /**
