b140fa1c20b328535556547af9a0430a48bbb342
Hungeun2 <bepoohun@gmail.com>
2022-11-22 20:07:23 +0900

    Refactor: 순서 변경

--- src/util/BridgeJudge.js
+++ src/util/BridgeJudge.js
@@ -1,25 +1,22 @@
 const { GAME, SIDE } = require('./Message');
 
-function BridgeJudge(bridge, commands) {
-  const sideJudgeResult = {
-    up: [],
-    down: [],
-  };
-  commands.forEach((command, index) => {
-    checkEqualSide(bridge[index], command, sideJudgeResult);
-  });
-  return sideJudgeResult;
+function matchUpside(sideJudgeResult) {
+  sideJudgeResult.up.push(GAME.MATCH);
+  sideJudgeResult.down.push(GAME.BLANK);
 }
-function checkEqualSide(bridgeSide, commandSide, sideJudgeResult) {
-  switch (commandSide) {
-    case SIDE.UP_STR:
-      checkUpside(bridgeSide, commandSide, sideJudgeResult);
-      break;
-    case SIDE.DOWN_STR:
-      checkDownSide(bridgeSide, commandSide, sideJudgeResult);
-      break;
-  }
+function unMatchUpside(sideJudgeResult) {
+  sideJudgeResult.up.push(GAME.MISMATCH);
+  sideJudgeResult.down.push(GAME.BLANK);
 }
+function matchDownside(sideJudgeResult) {
+  sideJudgeResult.down.push(GAME.MATCH);
+  sideJudgeResult.up.push(GAME.BLANK);
+}
+function unMatchDownside(sideJudgeResult) {
+  sideJudgeResult.down.push(GAME.MISMATCH);
+  sideJudgeResult.up.push(GAME.BLANK);
+}
+
 function checkUpside(bridgeSide, commandSide, sideJudgeResult) {
   if (bridgeSide === commandSide) {
     matchUpside(sideJudgeResult);
@@ -34,20 +31,27 @@ function checkDownSide(bridgeSide, commandSide, sideJudgeResult) {
   }
   unMatchDownside(sideJudgeResult);
 }
-function matchUpside(sideJudgeResult) {
-  sideJudgeResult['up'].push(GAME.MATCH);
-  sideJudgeResult['down'].push(GAME.BLANK);
+
+function checkEqualSide(bridgeSide, commandSide, sideJudgeResult) {
+  switch (commandSide) {
+    case SIDE.UP_STR:
+      checkUpside(bridgeSide, commandSide, sideJudgeResult);
+      break;
+    case SIDE.DOWN_STR:
+      checkDownSide(bridgeSide, commandSide, sideJudgeResult);
+      break;
+    default:
+  }
 }
-function unMatchUpside(sideJudgeResult) {
-  sideJudgeResult['up'].push(GAME.MISMATCH);
-  sideJudgeResult['down'].push(GAME.BLANK);
-}
-function matchDownside(sideJudgeResult) {
-  sideJudgeResult['down'].push(GAME.MATCH);
-  sideJudgeResult['up'].push(GAME.BLANK);
-}
-function unMatchDownside(sideJudgeResult) {
-  sideJudgeResult['down'].push(GAME.MISMATCH);
-  sideJudgeResult['up'].push(GAME.BLANK);
+
+function BridgeJudge(bridge, commands) {
+  const sideJudgeResult = {
+    up: [],
+    down: [],
+  };
+  commands.forEach((command, index) => {
+    checkEqualSide(bridge[index], command, sideJudgeResult);
+  });
+  return sideJudgeResult;
 }
 module.exports = BridgeJudge;
