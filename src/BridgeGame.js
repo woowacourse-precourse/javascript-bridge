bdb5cdd5c486eecd543f8ac7c5b11ce7b2360a33
sh981013s <sh981013s@gmail.com>
2022-11-22 15:07:49 +0900

    feat(BridgeGame): 게임 재시도, 종료 메서드 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,3 +1,5 @@
+const { Console } = require("@woowacourse/mission-utils");
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
@@ -32,11 +34,19 @@ class BridgeGame {
   move() {}
 
   /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+   * 최종결과 출력 후 게임을 종료한다.
+   * @param userMoving {string[]} [유저 이동기록]
+   * @param isSuccess {boolean} [최종 성공여부]
    */
-  retry() {}
+  finishGame(userMoving, isSuccess) {
+    this.mainController.displayFinalResult(userMoving, isSuccess);
+    Console.close();
+  }
+
+  // 사용자가 게임을 다시 시도할 때 사용하는 메서드
+  retry() {
+    this.mainController.restartGame();
+  }
 }
 
 module.exports = BridgeGame;
