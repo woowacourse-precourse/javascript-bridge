d2c156537b519932edc188f98362931a727b3550
SeongOh Park <qkrtjddh1212@naver.com>
2022-11-22 10:43:50 +0900

    test(validate): test code for move bridge
    - only for 'U', 'D'

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,3 +1,4 @@
+const BridgeGame = require('../src/BridgeGame');
 const BridgeMaker = require('../src/BridgeMaker');
 const BridgeRandomNumberGenerator = require('../src/BridgeRandomNumberGenerator');
 
@@ -6,5 +7,12 @@ describe('예외 테스트', () => {
     expect(() => {
       BridgeMaker.makeBridge(input, BridgeRandomNumberGenerator);
     }).toThrow('[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.');
+  });
+
+  test.each([[2], ['A'], [-2], ['ab']])('다리 건너기 예외처리', (input) => {
+    expect(() => {
+      const game = new BridgeGame();
+      game.move(input);
+    }).toThrow('[ERROR] 이동할 칸은 U또는 D여야 합니다.');
   });
 });
