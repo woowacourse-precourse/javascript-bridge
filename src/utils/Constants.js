f1bcc57750b6c4d63c71e488fd255f2a74a5d5cf
hailey-hy <lillyine.hy@gmail.com>
2022-11-22 18:46:15 +0900

    chore(Constants): 상수값 변경

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,9 +1,9 @@
 const GAME_MESSAGES = {
     START : '다리 건너기 게임을 시작합니다.\n',
-    RESULT_TITLE : '최종 게임 결과\n',
+    RESULT_TITLE : '최종 게임 결과',
     RESULT_GAME : '게임 성공 여부: ',
-    RESULT_TRUE : '성공\n',
-    RESULT_FALSE : '실패\n',
+    RESULT_TRUE : '성공',
+    RESULT_FALSE : '실패',
     RESULT_TRIAL : '총 시도한 횟수: '
 }
 
