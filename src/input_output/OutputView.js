9d56809fd31c0ed192ff25dfb2bf7df17bd127cf
RooT_Kim <bdd3452@gmail.com>
2022-11-22 17:24:59 +0900

    feat: 게임 결과 출력 메소드 구현 printResult - 게임 성공 여부 출력 printFinishMessage - 게임 최종 결과 메시지 출력 printTryCount - 총 시도한 횟수 출력

--- src/input_output/OutputView.js
+++ src/input_output/OutputView.js
@@ -11,6 +11,7 @@ const OutputView = {
 	printEnter() {
 		Console.print(MESSAGES.blank);
 	},
+
 	/**
 	 * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
 	 * <p>
@@ -63,7 +64,18 @@ const OutputView = {
 	 * <p>
 	 * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
 	 */
-	printResult() {},
+	printResult(result) {
+		Console.print(`${MESSAGES.winOrFail}${result}`);
+	},
+
+	printFinishMessage() {
+		Console.print(MESSAGES.finalResultMessage);
+	},
+
+	printTryCount(tryCount) {
+		Console.print(`${MESSAGES.tryCount}${tryCount}`);
+		Console.close();
+	},
 };
 
 module.exports = OutputView;
