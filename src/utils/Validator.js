c79c31883dab3c668b270c4e22e046f329fd2af5
sojinjang <jsojin6119@gmail.com>
2022-11-22 16:38:38 +0900

    :recycle: refactor: 필요없는 코드 라인 삭제

--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -6,20 +6,16 @@ class Validator {
     const isNumber = /^[0-9]+$/.test(input);
     const isInRange = input >= Values.SIZE_MINIMUM && input <= Values.SIZE_MAXIMUM;
     if (!isNumber || !isInRange) throw new Error(ErrorMessages.INVALID_SIZE);
-
-    return;
   }
   static checkDirectionInput(input) {
     if (input !== Values.UPWARD_MOVEMENT && input !== Values.DOWNARD_MOVEMENT) {
       throw new Error(ErrorMessages.INVALID_MOVEMENT);
     }
-    return;
   }
   static checkCommandInput(input) {
     if (input !== Values.RESTART && input !== Values.QUIT) {
       throw new Error(ErrorMessages.INVALID_COMMAND);
     }
-    return;
   }
 }
 
