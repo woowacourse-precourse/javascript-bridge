8be314b683cba15ab2a709d6b13fa1f502b759c8
Choejeonghun <saa00123@naver.com>
2022-11-22 15:51:25 +0900

    feat: 테스트 작성하며 잘못적은 코드 수정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 const { Console } = require('@woowacourse/mission-utils');
+const InputView = require('./InputView');
 
 class App {
   play() {
     this.startMessage();
+    InputView.readBridgeSize();
   }
 
   startMessage() {
     Console.print('다리 건너기 게임을 시작합니다.\n');
   }
 }
-
+const app = new App();
+app.play();
 module.exports = App;
