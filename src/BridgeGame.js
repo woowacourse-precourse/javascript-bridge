2c328ec4beabb02eed19a32cbe1212b9ba349400
2yunseong <dbsdltjd3701@naver.com>
2022-11-22 23:13:43 +0900

    style: 필요없는 주석 제거

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,11 +1,5 @@
 const OutputView = require('./OutputView');
-/**
- * 다리 건너기 게임을 관리하는 클래스
- * + 필드 추가 가능
- * + 파일 경로 변경 가능
- * + 메서드 이름 변경 불가능
- * + 인자 추가 가능
- */
+
 class BridgeGame {
   constructor(bridge) {
     this.answerSteps = bridge;
@@ -13,11 +7,7 @@ class BridgeGame {
     this.gameCount = 1;
     this.gameStatus = '';
   }
-  /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
+
   move() {
     const lastIdx = this.bridgeSteps.length - 1;
     if (
@@ -35,11 +25,7 @@ class BridgeGame {
   isStepSame(answerStep, compareStep) {
     return answerStep === compareStep;
   }
-  /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
+
   retry() {
     this.bridgeSteps = [];
     this.gameCount += 1;
--- src/InputView.js
+++ src/InputView.js
@@ -1,15 +1,8 @@
 const MissionUtils = require('@woowacourse/mission-utils');
+
 const OutputView = require('./OutputView');
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- * + 파일 경로 변경 가능
- * + 메서드 인자 변경 가능
- * + 메서드 추가 가능
- */
+
 const InputView = {
-  /**
-   * 다리의 길이를 입력받는다.
-   */
   readBridgeSize(callback, message = '') {
     OutputView.printMessage('다리의 길이를 입력해주세요.');
     MissionUtils.Console.readLine(message, (answer) => {
@@ -17,9 +10,6 @@ const InputView = {
     });
   },
 
-  /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
   readMoving(callback, message = '') {
     OutputView.printMessage('이동할 칸을 선택해주세요. (위: U, 아래: D)');
     MissionUtils.Console.readLine(message, (answer) => {
@@ -27,9 +17,6 @@ const InputView = {
     });
   },
 
-  /**
-   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-   */
   readGameCommand(callback, message = '') {
     OutputView.printMessage(
       '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)'
