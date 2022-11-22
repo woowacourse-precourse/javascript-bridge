eb5ebd6b01d58c7cd467c71fda5316ce76dfff76
jinyong <zhaqh146@naver.com>
2022-11-22 17:50:32 +0900

    feat: 1-2 이동할 칸 입력 기능 구현

--- src/InputView.js
+++ src/InputView.js
@@ -7,14 +7,19 @@ const InputView = {
   readBridgeSize() {
     Console.readLine('다리의 길이를 입력해주세요.\n', answer => {
       Console.print(answer);
-      Console.close();
+      this.readMoving();
     });
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    Console.readLine('\n이동할 칸을 선택해주세요. (위 : U, 아래 : D)\n', answer => {
+      Console.print(answer);
+      Console.close();
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
