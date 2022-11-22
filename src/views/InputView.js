371a06d8631406efe78fc1cee85f838cdce73446
bassyu <yrkdgus@gmail.com>
2022-11-22 13:30:52 +0900

    refactor(InputView): 입력 메세지 상수 사용

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,12 +1,13 @@
 const { Console } = require('@woowacourse/mission-utils');
 const OutputView = require('./OutputView');
 const Validator = require('../Validator');
+const { BRIDGE_STRING, GAME_COMMAND_STRING } = require('../constants');
 
 const InputView = {
   INPUT_MESSAGE: {
     bridgeSize: '\n다리의 길이를 입력해주세요.\n',
-    moving: '\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
-    gameCommand: '\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
+    moving: `\n이동할 칸을 선택해주세요. (위: ${BRIDGE_STRING.up}, 아래: ${BRIDGE_STRING.down})\n`,
+    gameCommand: `\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: ${GAME_COMMAND_STRING.retry}, 종료: ${GAME_COMMAND_STRING.quit})\n`,
   },
 
   readBridgeSize(handleBridgeSize) {
