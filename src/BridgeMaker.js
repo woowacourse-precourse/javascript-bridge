1c313b200836057acb15d3c2a5414633d86e1b7d
dragonfruitlemonade <tpghks9245@gmail.com>
2022-11-22 19:37:40 +0900

    fix: 예기치 못한 오류 해결중

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,9 +1,7 @@
-const { BRIDGE_MAKER } = require('./constants/Settings');
-
 const BridgeMaker = {
   makeBridge(size, generateRandomNumber) {
     return Array.from({ length: size }, () => {
-      return generateRandomNumber() === BRIDGE_MAKER.one ? BRIDGE_MAKER.up : BRIDGE_MAKER.down;
+      return generateRandomNumber() === 1 ? 'U' : 'D';
     });
   },
 };
