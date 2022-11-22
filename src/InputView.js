21302559ddf80cee73d5979d1564eae7a8c20a8a
lsy20140 <lsy20140@naver.com>
2022-11-22 20:49:59 +0900

    refactor: checkRetryKey 추가

--- src/InputView.js
+++ src/InputView.js
@@ -1,5 +1,5 @@
 const {Console} = require("@woowacourse/mission-utils");
-const {checkBridgeSize, checkSideInput} = require("./Validation");
+const {checkBridgeSize, checkSideInput, checkRetryKey} = require("./Validation");
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
