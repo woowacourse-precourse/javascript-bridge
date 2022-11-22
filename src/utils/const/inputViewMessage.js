6182734890d40c55d07a1cf2ec9fac835c98d2de
Sangjun-man <molung@icloud.com>
2022-11-22 20:04:31 +0900

    fix(InputViewMessage) : 입력 시 나타나는 메세지에 한 줄 공백 추가
    입력 시 나타나는 메세지를 한줄 띄워 보여주기 위해 메세지에 공백을 추가했습니다.

--- src/utils/const/inputViewMessage.js
+++ src/utils/const/inputViewMessage.js
@@ -1,6 +1,6 @@
-const INPUT_READ_BRIDGE_SIZE = `다리의 길이를 입력해주세요.`;
-const INPUT_READ_MOVING = `다리의 길이를 입력해주세요.`;
-const INPUT_READ_GAME_COMMAND = `게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)`;
+const INPUT_READ_BRIDGE_SIZE = `다리의 길이를 입력해주세요. \n`;
+const INPUT_READ_MOVING = `이동할 칸을 선택해주새요, (위: U, 아래 :D) \n`;
+const INPUT_READ_GAME_COMMAND = `게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n`;
 
 module.exports = {
   readBridgeSize: INPUT_READ_BRIDGE_SIZE,
