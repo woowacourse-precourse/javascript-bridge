6003eb23dc454bdb030178c9a08ba5f492307235
jin-Pro <dnjun2@ajou.ac.kr>
2022-11-22 17:22:12 +0900

    feat: InputView Method build

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,5 @@
+const OutputView = require("./OutputView");
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,17 +7,29 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine(OutputView.printRequestBridgeLength, (reply) => {
+      console.log(reply);
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    Console.readLine(OutputView.printRequestUserMove, (reply) => {
+      console.log(reply);
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+    Console.readLine(OutputView.printRequestReplay, (reply) => {
+      console.log(reply);
+    });
+  },
 };
 
 module.exports = InputView;
