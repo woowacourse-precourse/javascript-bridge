a4b8fca1c36b841f02566ce81504ff0024ac9169
OrangeChick <retww@naver.com>
2022-11-22 21:51:20 +0900

    feat: Implement function that outputs an error when U or D is not entered when moving a leg
    - 다리 이동시 U or D를 입력하지 않으면, 에러를 출력하는 기능 구현

--- src/Validate.js
+++ src/Validate.js
@@ -1,11 +1,18 @@
 const RETRY_LETTER = "R";
 const END_LETTER = "Q";
+const UP_LETTER = "U";
+const DOWN_LETTER = "D";
 
 const Validate = {
   bridgeSize(bridgeLength) {
     if (bridgeLength < 3 || bridgeLength > 20) {
       throw new Error("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
     }
+  },
+
+  gameInput(moveLetter) {
+    if (moveLetter !== UP_LETTER && moveLetter !== DOWN_LETTER)
+      throw new Error("[ERROR] U 또는 D를 입력해주세요.");
   },
 
   retry(inputLetter) {
