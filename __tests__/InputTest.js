446998eaeded977e34cc2a85dff64b08409d3d4b
Adult96 <djxor007@gmail.com>
2022-11-22 14:20:26 +0900

    test(InputTest): 유저 입력 테스트 중복된 테스트 리펙토링

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -3,29 +3,36 @@ const { movingValidation } = require('../src/validation/movingValidation');
 const { commandValidation } = require('../src/validation/commandValidation');
 
 describe('User Input 테스트', () => {
-  test('다리 갯수 3~20까지의 숫자', () => {
-    expect(bridgeValidation(3)).toBeTruthy();
-    expect(bridgeValidation(1)).toBeFalsy();
-    expect(bridgeValidation('a')).toBeFalsy();
-    expect(bridgeValidation('')).toBeFalsy();
-    expect(bridgeValidation(' ')).toBeFalsy();
+  test.each([3, 20])('다리 갯수 3~20까지의 숫자', (input) => {
+    expect(bridgeValidation(input)).toBeTruthy();
   });
 
-  test('다리 이동 U/D의 문자 확인', () => {
-    expect(movingValidation('U')).toBeTruthy();
-    expect(movingValidation('D')).toBeTruthy();
-    expect(movingValidation(1)).toBeFalsy();
-    expect(movingValidation('a')).toBeFalsy();
-    expect(movingValidation('')).toBeFalsy();
-    expect(movingValidation(' ')).toBeFalsy();
+  test.each([1, 'a', '', ' '])(
+    '다리 갯수 3~20까지의 숫자 예외 처리',
+    (input) => {
+      expect(bridgeValidation(input)).toBeFalsy();
+    }
+  );
+
+  test.each(['U', 'D'])('다리 이동 U/D의 문자 확인', (input) => {
+    expect(movingValidation(input)).toBeTruthy();
   });
 
-  test('재시작,종료 R/Q의 문자 확인', () => {
-    expect(commandValidation('R')).toBeTruthy();
-    expect(commandValidation('Q')).toBeTruthy();
-    expect(commandValidation(1)).toBeFalsy();
-    expect(commandValidation('a')).toBeFalsy();
-    expect(commandValidation('')).toBeFalsy();
-    expect(commandValidation(' ')).toBeFalsy();
+  test.each([1, 'a', '', ' '])(
+    '다리 이동 U/D의 문자 확인 예외 처리',
+    (input) => {
+      expect(movingValidation(input)).toBeFalsy();
+    }
+  );
+
+  test.each(['R', 'Q'])('재시작,종료 R/Q의 문자 확인', (input) => {
+    expect(commandValidation(input)).toBeTruthy();
   });
+
+  test.each([1, 'a', '', ' '])(
+    '재시작,종료 R/Q의 문자 확인 예외 처리',
+    (input) => {
+      expect(commandValidation(input)).toBeFalsy();
+    }
+  );
 });
