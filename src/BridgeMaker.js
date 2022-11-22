7a5c72d1ccb3f41ff043fa09bb7c705aba6babbf
osdoonhyun <skw1996@naver.com>
2022-11-22 18:38:55 +0900

    refactor(BridgeMaker): 다리 만드는 메서드 배열 코드 컨벤션 맞춰 리팩터링

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -10,12 +10,14 @@ const BridgeMaker = {
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
   makeBridge(size, generateRandomNumber) {
-    const bridge = Array.from({ length: size }, () =>
-      generateRandomNumber()
-    ).map((block) => {
-      if (block === BRIDGE_MAP.up_space) return BRIDGE_MAP.down_moving;
-      else if (block === BRIDGE_MAP.down_space) return BRIDGE_MAP.up_moving;
-    });
+    const bridge = [];
+    for (let i = 0; i < size; i++) {
+      const randomNumber = generateRandomNumber();
+      if (randomNumber === BRIDGE_MAP.up_space)
+        bridge.push(BRIDGE_MAP.up_moving);
+      if (randomNumber === BRIDGE_MAP.down_space)
+        bridge.push(BRIDGE_MAP.down_moving);
+    }
     return bridge;
   },
 };
