41c8a447e606683677ae0d81803a1ab4347502aa
wlthdud <thdudzi@naver.com>
2022-11-22 16:14:50 +0900

    feat: 성공여부 출력하기 기능 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -18,21 +18,21 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap(userInput, answer) {
+  printMap(Input, answer) {
     let upperBridge = '[ ';
     let lowerBridge = '[ ';
 
-    for(let i = 0; i < userInput.length; i++){
+    for(let i = 0; i < Input.length; i++){
       if(i != 0){
         upperBridge += '| ';
         lowerBridge += '| ';
       }
-      if(userInput[i] == 'U'){
-        upperBridge += this.cmpUD(userInput[i], answer[i]) + ' ';
+      if(Input[i] == 'U'){
+        upperBridge += this.cmpUD(Input[i], answer[i]) + ' ';
         lowerBridge += '  ';
       }
-      if(userInput[i] == 'D'){
-        lowerBridge += this.cmpUD(userInput[i], answer[i]) + ' ';
+      if(Input[i] == 'D'){
+        lowerBridge += this.cmpUD(Input[i], answer[i]) + ' ';
         upperBridge += '  ';
       }
     }
@@ -50,9 +50,26 @@ const OutputView = {
    */
   printResult(userInput, ans) {
     MissionUtils.Console.print('최종 게임 결과');
-    printMap(userInput, ans);
+    this.printMap(userInput, ans);
+    this.printSuccess();
+
   },
 
+  //성공여부 반환하기
+  isSuccess(input, ans){
+    let idx = input.length - 1;
+    return input[idx] == ans[idx];
+  },
+
+  //성공여부 출력하기
+  printSuccess(){
+    MissionUtils.Console.print('게임 성공 여부: ');
+    if(this.isSuccess(userInput, ans)){
+      MissionUtils.Console.print('성공');
+    }else{
+      MissionUtils.Console.print('실패');
+    }
+  }
 };
 
 module.exports = OutputView;
