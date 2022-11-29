6ab556343d9ec71071ee98f2945817dc8f7353bc
cmun2 <cmun2@illinois.edu>
2022-11-29 23:47:28 +0900

    Update 주석 update

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -3,11 +3,11 @@ const { OPTION_MESSAGE } = require("../util/Messages.js");
 const Validation = require("../util/Validation.js");
 const OutputView = require("../view/OutputView.js");
 /**
- * 사용자로부터 입력을 받는 역할을 한다.
+ * 사용자로부터 입력을 받는 역할
  */
 const InputView = {
   /**
-   * 다리의 길이를 입력받는다.
+   * 다리의 길이를 입력
    */
   measureLength(makeBridge) {
     let input;
@@ -24,7 +24,7 @@ const InputView = {
   },
 
   /**
-   * 사용자가 이동할 칸을 입력받는다.
+   * 사용자가 이동할 칸 입력받기.
    */
   readMoving() {
     let input;
@@ -41,7 +41,7 @@ const InputView = {
   },
 
   /**
-   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받기
    */
   readGameCommand() {
     let input;
