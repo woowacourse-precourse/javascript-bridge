3ecf194679f993f1cba509b92b21599ef31c0321
seongjin <ysjkof@gmail.com>
2022-12-06 10:19:41 +0900

    chore: Linter 설정

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "no-use-before-define": "off",
+    "comma-dangle": "off",
+    "consistent-return": "off",
+  },
+};
