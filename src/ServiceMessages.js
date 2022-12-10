d26fde2c8a95694843b2c18c8ded48062897ada2
금가경 <gageum0@gmail.com>
2022-12-11 00:43:07 +0900

    style(bridge) : OutputView 하드코딩 수정

--- src/ServiceMessages.js
+++ src/ServiceMessages.js
@@ -1,4 +1,5 @@
 class ServiceMessages {
+  static START = '다리 건너기 게임을 시작합니다.\n';
   static GET_BRIDGE_LENGTH = '\n다리의 길이를 입력해주세요.\n';
   static SELECT_SPACE = '\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n';
   static RETRY =
